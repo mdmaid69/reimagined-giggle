750e7211d385391bf6ff62edc01f921c27369271
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:14:33 +0000

    care walk you transport 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import getpass
+def get_username():
+        return getpass.getuser()
