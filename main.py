28c742d6944f86fc1d237661973325ce52480446
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:41 +0000

    inform cost suffer cinema business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
