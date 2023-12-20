73497b96b7e6da76ce047525bd53cd74bd509cd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:30 +0000

    reasonable payment clever live office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def extend_array(array, iterable):
+        array.extend(iterable)
