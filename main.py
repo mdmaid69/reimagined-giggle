292d742a4c014dc7f8ad316e5fb41afd47faf2d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:15 +0000

    walk suit 567enemy wear report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
