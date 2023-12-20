9358d1cb00dfa11f461d3702813384e57624244c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:38:12 +0000

    hide limit forward skirt approve

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
