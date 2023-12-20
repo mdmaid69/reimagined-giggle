bb438a86674bbdb365f955f867343efad8e986be
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:24 +0000

    awake mixture bury pupil recognize

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
