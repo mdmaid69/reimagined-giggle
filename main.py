6ecaee80a646498fa2f0238afb5d1f73b9639e2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:24 +0000

    record stay bear respect type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
