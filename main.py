e9d37457aa90db2b0980f9e23c16b7ac6aa87629
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:14 +0000

    giveaway occasion class no area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def is_odd(n):
+        return n % 2 != 0
