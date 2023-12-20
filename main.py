6750d4e554397920e508d5afad0e04ca8c069866
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:58:10 +0000

    greeting where another treat life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
