b3d044a8b6454f7a8f98f1a97228ffad5c891a4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:00 +0000

    height beat produce hollow try

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
