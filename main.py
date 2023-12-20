af835f60e54802b489a3045cc272621c86688cce
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:26:55 +0000

    winter tie find unkind tight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
