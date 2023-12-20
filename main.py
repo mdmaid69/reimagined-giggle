eed173eeebdc0e33d72f5228a15eec786c4e386f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:47 +0000

    offence home how trip workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
