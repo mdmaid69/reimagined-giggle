241ccab6153dd0ffcbc609d4d2c385deb09571ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:49 +0000

    jump quantity country relationship complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
