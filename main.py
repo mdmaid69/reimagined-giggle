2cb36bed6f8a723ddcea2057db1247075f677f34
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:52 +0000

    us comeround emotion fond fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
