a90529b43a55bb353a82ab90e39a9ba86e594a4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:17 +0000

    inside quick lower load ask

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
