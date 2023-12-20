f9f3a4317c61b1159fe93ea3c625bad37b0c591e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:43:19 +0000

    run chin each connected card

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-for i in range(5):
-        print(i)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
