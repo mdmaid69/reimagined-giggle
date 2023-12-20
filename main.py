cc6f9b227dca535235a0502edd044f14ea537909
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:46 +0000

    callout over write hear please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
