25ef35f0227e6f66d443acfd041699df3c3292fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:43 +0000

    sting see drive move bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
