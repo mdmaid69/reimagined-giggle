99aaa3fb2a5a6a62ae6700a69e5fdf4ba50ce049
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:43:13 +0000

    comeout operate off hear worried

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
