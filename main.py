c57ff51fe5b1d5d4e70f85ce109018e47b3173b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:18 +0000

    me statement lose terrible accept

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+print("Hello, world!")
