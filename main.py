e4423b76515fab72a6c3c8509de0f194e4ba20cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:12 +0000

    moment rubbish interruption space break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def divide_numbers(x, y):
-        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
