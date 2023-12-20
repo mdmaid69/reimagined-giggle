3f1cedf40fa1200761eae6653d5f165da710d3d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:07:08 +0000

    wish fire learn we promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
