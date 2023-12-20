e86b1df3bbba6fe7549531b6da34ea3f08f80bdd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:10:15 +0000

    breakin refuse fierce grateful draw

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
