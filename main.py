f445dd99e80bff836172049bc3a937711f2000fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:11 +0000

    destroy able rise float circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
