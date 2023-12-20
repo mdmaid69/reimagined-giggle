bc0c50014df4ac74efa61555ae066d290970f091
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:13 +0000

    congratulate study reply rush comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
