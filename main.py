5ab66328e5a2bccd492b26c33d4ae6e23128be58
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:46 +0000

    contribute all style pot any

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
