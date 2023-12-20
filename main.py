10c9c7c6995e5fdb9b66e936b87280bca113c5fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:53:51 +0000

    full getaway matter road match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
