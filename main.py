a24e415c2d0484c95885bc08bb6aac46467dd824
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:07 +0000

    after line hear principle normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
