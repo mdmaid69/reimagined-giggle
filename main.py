27a671e4b32ca81240a77fb08aa4e70539c3ad39
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:57 +0000

    straight and divide sink past

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
