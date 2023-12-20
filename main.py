9b406993f763b0bd6daabdb0b2529fd2c2880dd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:29:59 +0000

    wide number moral if gradually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
