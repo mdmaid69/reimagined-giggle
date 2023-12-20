470b78f736a67f73f50941b54193c642c8f90146
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:19 +0000

    board against suckme crash along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
