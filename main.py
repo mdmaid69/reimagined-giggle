3f793a143ebff6398b29ba6d36de6a1bba4feca4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:56 +0000

    majority undo wet preparation case

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import time
+print(time.time())
+def sort_list(lst):
+        return sorted(lst)
