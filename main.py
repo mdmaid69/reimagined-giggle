73bb6f1ed1625b359045e41d9607971e5fb5be6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:16 +0000

    name lead black precise fortunate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_str(array):
+        return str(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
