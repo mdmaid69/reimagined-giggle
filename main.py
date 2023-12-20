c4c1af48a8c2cb7dd3685b92952665af5c12ca5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:26 +0000

    train upstairs sure throughout read

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
