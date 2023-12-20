ffffae8c5cf140c5d76aee5ae8dbc86c4b97d0ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:04 +0000

    bar price pass do charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
