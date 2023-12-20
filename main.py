a294f922e2ab33374f5b0e7d9aa18856cf5551e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:52:33 +0000

    whatever keepup guilty lunch deal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
