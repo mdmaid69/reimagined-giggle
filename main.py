0354004b3f0bb9f5a26230e80e2d19f36b5f4894
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:23 +0000

    hold risk wise 573entertainment peace

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
