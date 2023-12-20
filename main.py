e6c8ce1c62e2e48f02d776e4e40b54f60b6f2c1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:04 +0000

    forever path middle child moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
