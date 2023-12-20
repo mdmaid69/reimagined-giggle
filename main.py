b7bc66c5413a4a3e242b77e77d77c3b428b23b6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:22 +0000

    important answer clever love popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
