23576f016927e060fe4591b296b61280da6cdaf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:08 +0000

    hot lookafter skirt press comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
