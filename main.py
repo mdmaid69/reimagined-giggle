c9dfea2387d8e7a2cb1aa86b41d3428723ac9ee6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:02 +0000

    apply race visit discussion turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
