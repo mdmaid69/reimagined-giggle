0e7e061432f865bd0524ef4126c9e73f6d31d750
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:28:32 +0000

    steep interview conclusion strike order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_current_directory():
+        return os.getcwd()
