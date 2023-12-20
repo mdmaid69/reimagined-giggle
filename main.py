fd74e8846fdb4b61aa75e36d43e76ce9ba6c658d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:38:31 +0000

    simple extraordinary Dd since blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
