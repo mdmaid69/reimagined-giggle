ffb595f3c6e1583c571b33d5e38f8d6e00ce7ccf
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:43 +0000

    value spray worst body price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
