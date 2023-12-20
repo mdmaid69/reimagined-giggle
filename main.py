461870c29a32dd68723423762b6efbfd646db949
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:10 +0000

    backwards consist blue shall hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
