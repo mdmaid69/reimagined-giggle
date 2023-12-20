54cfab13dbb8a0f566085f385667c4eb93fa0e17
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:39:16 +0000

    public bear trip failure content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
