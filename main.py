7edab6d1aa9a6f2a91f664ab71f3677105f8d3ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:59 +0000

    among pencil join better study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def append_to_array(array, item):
+        array.append(item)
