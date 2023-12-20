d7fce731e7f903a281db59ad07a873299881b538
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:29:17 +0000

    dream sleep tap unit order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
