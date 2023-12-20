079079f299bd2734b91518933d2b63c953907f86
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:33:33 +0000

    previous reliable comeout test risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
