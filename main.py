c1cacf1a8218ef63daf5314d8fb2084503cde1f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:56 +0000

    quick might home conclusion kick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
