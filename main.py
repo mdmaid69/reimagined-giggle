eae5a862c000d67e843ec21892c574495778f2f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:29 +0000

    picture cross single warm delicate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
