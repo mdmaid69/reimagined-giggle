76112f2b70a125c22391d729c0a2a2f9436600ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:33 +0000

    goout soft sign organize receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
