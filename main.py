ab0f3ce6b7640151f4166b013830d3195363cf95
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:53:48 +0000

    before information by once thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
