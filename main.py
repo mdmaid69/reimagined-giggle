ed64582d63172b9fe5cf62fc5c15f165c2634b8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:52 +0000

    improve pupil anywhere prepare hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
