7e597acd7f93426a8c8886d8c7b84f7e0353cfdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:38 +0000

    himself round generally he string

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def roll_die():
+        return random.randint(1, 6)
