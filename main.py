aa0e16de308eb65c2fce458cd8116ac9ba1b3a9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:53 +0000

    tight tongue bend like exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
