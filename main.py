d360bb84bd327658cb8465be428ab612eee39514
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:08 +0000

    tradition payment swimming quiet muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
