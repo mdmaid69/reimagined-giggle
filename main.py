36f62280630967c10f06f7de1674565174bd3fbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:56:19 +0000

    although wide develop need arm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
