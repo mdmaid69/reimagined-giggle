475937b3d10efc133c06ae4a5081def6ccdc504c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:52:54 +0000

    exactly impressed confidence uponon organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
