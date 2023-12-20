833cc9512c970f305b03fc7a137512077ec238bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:51 +0000

    basic roof suggestion better weigh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
