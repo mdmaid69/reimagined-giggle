4b3936e1fbb31e55f8befb0a6bd9b11e7e300091
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:58:46 +0000

    Cc relief sister her today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
