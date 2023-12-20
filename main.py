89ad0d14a5a9f32cb28033ea118e55ffbb797ef0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:29 +0000

    boil find suffer pure finally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
