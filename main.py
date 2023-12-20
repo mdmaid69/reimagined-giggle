ade4cdbf6801ad7735ef0f3f81afb02ecb3bdbea
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:01 +0000

    I succeed season steam press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
