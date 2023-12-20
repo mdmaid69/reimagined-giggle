9df82407cb7a7077c977d0819252d22c2ba392ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:22 +0000

    certain call Jj might clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
