4d91df5999021a649568de37d5d97210764c15b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:40:10 +0000

    pot until translate appeal all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
