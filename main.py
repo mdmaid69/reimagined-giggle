b540311ec2b0fa0574c928f67358e445ac9e4820
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:34 +0000

    bottom proud equipment force favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
