9890e30a118876897a885a87078a3c0050948f78
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:54 +0000

    breathe object evening regional personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
