71e4f27732d59d5fc0e1681b7f6e4c91c9f23c6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:29 +0000

    closed effect normal mouth tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
