feacdc64077ff11720636c26f8e3293117effc7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:53 +0000

    generally calm takeoff respect makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import time
-print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
