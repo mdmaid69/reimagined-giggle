0755e29e86456086f7dd6d540451dc7db4872a75
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:45 +0000

    bang rate target art visit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
