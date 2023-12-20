5848e8b87d0b1be47dba47d0e16402edaaac712f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:35 +0000

    write slip every future store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
