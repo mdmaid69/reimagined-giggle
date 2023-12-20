cd824e3614dfbce3bcc0356a599ce90ab54a45d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:37 +0000

    formal relation when immediately argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
