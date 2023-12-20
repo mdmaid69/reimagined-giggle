8dfc20b3743fa9b6cef8248dc97c6eb23f9c724c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:48:25 +0000

    independent theirs water pause commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
