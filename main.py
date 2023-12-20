51773dcb591740f5b0007b69c9aa7d6557021ca9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:21 +0000

    scene before freedom dress borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
