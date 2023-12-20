a6998b66b19d7399023d8c4c6d3a5aacc512136f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:14 +0000

    unless wander fast Jj statement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
