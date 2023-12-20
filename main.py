a70952680bd3c5da98dccc4d8daa6ccbb2ced9dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:03 +0000

    expression stupid under finish improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
