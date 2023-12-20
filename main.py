0946ac84a83b93fa65817270d9e6f9822df8f664
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:44 +0000

    evidence 572enter voice main disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
