2998bd841de900f6675d85853628a31bf5d5c32d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:21 +0000

    still slightly effect translate brief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
