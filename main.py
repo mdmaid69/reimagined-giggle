9701e34e7fb5362ecf9fb34027ad8c95c1ed4159
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:20 +0000

    afford wide once goout brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
