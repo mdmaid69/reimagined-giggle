3e56eee37588009ff80f686076dc31692132017b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:56:29 +0000

    give debt progress lucky satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
