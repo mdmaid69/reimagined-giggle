29325c7503c6fa3a0fb279e7cca1b2211f81785d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:57 +0000

    respect intelligent Hh live skirt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
