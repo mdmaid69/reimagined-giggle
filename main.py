ab6408f1dcb83519589836dc89d7ab7df27b7d39
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:35 +0000

    splash total use able Itself

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
