8a75f9cf24e508300590b9dcf6862953ae398c46
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:17:54 +0000

    fashion both discovery mix market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
