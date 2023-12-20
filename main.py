0ab6f08e11baaa1b815214b053b5cc079b517b82
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:16:24 +0000

    blame rush yourself Yy immediate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_user_list():
-        return collections.UserList()
+def sort_numbers(numbers):
+        return sorted(numbers)
+def add_numbers(x, y):
+        return x + y
