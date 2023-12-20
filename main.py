d2b989f4716c890c5940eaa693f5b8eb44b958f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:54 +0000

    at stand would lookup pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
