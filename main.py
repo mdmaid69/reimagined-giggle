e7b3f0dff8c59f4ea5bf5691f52dda3716023b05
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:40 +0000

    debt pump runinto tone bush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
