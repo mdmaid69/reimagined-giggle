f050eeeca46cc4c49aae537128d9be23f7173e9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:51:18 +0000

    consider properly makeinto range touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_power(work, time):
+        return work / time
