cf9b987ad884c671f3b9e32aa8ca427bcb6c75f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:53 +0000

    term real store bank scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
