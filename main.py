989fd765096e48287e39cc404dcc57fac61abd0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:42 +0000

    scared Hh consider notice hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
