de686bb95b41c83f13cf786ac8ca013090633e49
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:07 +0000

    die unable base check cough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-print(random.randint(0, 100))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
