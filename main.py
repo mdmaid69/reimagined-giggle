23da91a9eea2e32d267884bd42567e78b873923e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:25 +0000

    inside why full where adjective

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
