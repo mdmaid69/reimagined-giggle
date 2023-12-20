3cebd84256c762054b68003c58017e7b6c3cca80
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:57 +0000

    score justice doubt age politics

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
