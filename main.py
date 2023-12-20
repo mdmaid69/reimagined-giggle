310fc613dca98376883cddbc43d65fe936843891
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:09 +0000

    turn fasten cross holdon stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
