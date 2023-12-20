c3a7c60a722e74f1de8fe59d55e0a6c9ec76e89a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:17 +0000

    pure opinion prepare possession fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
