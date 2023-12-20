5ba34cbcbbc3021eec6b4577d98796f57218be12
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:35 +0000

    distant flow describe brief pull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item(array, i):
-        return array[i]
+print([x**2 for x in range(10)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
