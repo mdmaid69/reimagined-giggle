55c188ae2f2709e52b61bd5a0c6bf858af93d867
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:21 +0000

    pretty though breakthrough century wing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
