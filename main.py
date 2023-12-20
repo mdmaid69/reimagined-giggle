8384ff78092680c10c677bdb6d2200666b781833
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:55 +0000

    nothing light mark spring after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
