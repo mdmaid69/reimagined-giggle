478a13f390452ef01622c2454d3f80239a647b9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:13 +0000

    while human event right underneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def subtract_numbers(x, y):
+        return x - y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
