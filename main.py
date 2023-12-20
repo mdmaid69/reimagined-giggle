0a8e32dbea32c4474d6d5f6e4f50e4d9fd7ee435
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:37 +0000

    card interrupt deliver machine Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import collections
-def create_queue():
-        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
