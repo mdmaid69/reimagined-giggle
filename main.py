33f6d9e368150550f36e94b1093ff4c2efa4adfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:41:04 +0000

    sight really curious breath spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
