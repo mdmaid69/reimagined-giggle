c1d4d5b8007eab9c77ec78cc1725aa2c29df2641
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:09 +0000

    certainly speak odd truth practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
