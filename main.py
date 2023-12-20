498f9f7eee73a35c7d2e221f8c4ce420a7f8152c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:49 +0000

    them fortunately suffer dream bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
