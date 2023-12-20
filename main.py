2bd72ddb5f26f3c19ecb332bd6ab13f636d007e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:40 +0000

    quality interruption worried influence keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
