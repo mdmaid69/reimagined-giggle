b05a01b67b02b1f81093ffc229535212e2bfeea9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:57:01 +0000

    object active protect commit trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
