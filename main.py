887a5899885692f332502745e7e8daa13ac5fe97
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:36 +0000

    respect spend term runaway defeat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
