7a7102fec11b01a4b7a3d904f1da29999ef90c35
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:44 +0000

    change read breakfast instrument debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
