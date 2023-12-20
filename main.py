6395dc5298d39dbc22d699211063e731b82c2720
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:27 +0000

    suckme pity shame pretty shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
