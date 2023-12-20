6982d50cecee67d42273a4d1604f45bb4ce66a7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:51 +0000

    myself inside section pull turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
