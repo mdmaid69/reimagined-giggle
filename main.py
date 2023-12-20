0adaf64e5b55a571bcf4f78c46eea3bc38231f2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:09 +0000

    foreign soak flood shape react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
