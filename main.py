e578b501bddec513f6a040d69bd7692178aa1e6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:37 +0000

    fasten feeling about cut century

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
