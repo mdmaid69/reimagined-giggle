9e8e083248eb1702a13bca606fbc1696fcd3d625
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:31 +0000

    promise continue shoulder supply during

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
