5727db2fe859bbeabd586fd0e099a551daf5c0e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:03 +0000

    trade Mm connect excuse drop

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
