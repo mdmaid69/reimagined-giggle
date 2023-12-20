4e334b0e0a59094dc3a31324cc550ebfb103f0af
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:07 +0000

    cause fly swimming foreign load

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
