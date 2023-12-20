79ebc8ab679c61c59c178e73e1ab2e29c1360d88
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:31 +0000

    thankyou divide Rr please direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+def sort_list(lst):
+        return sorted(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
