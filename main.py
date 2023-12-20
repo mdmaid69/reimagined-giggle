ee16507e34715095765e8ff9e390cde2a0d8c653
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:25 +0000

    genuine probably boil like earn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
