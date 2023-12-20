d0b0fec0c328ff77443b0aadc11754b189dd2467
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:30 +0000

    material address upset plenty Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
