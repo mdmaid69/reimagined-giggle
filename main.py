fa2d7c597b25f1a80cc2db7811fad994f170a70a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:12 +0000

    takeoff bunch area themselves general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
