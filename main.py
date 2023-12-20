fe8ea0d635f7420d163ebb1183f70309d5c91e0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:33 +0000

    demand Uu obvious strike use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
