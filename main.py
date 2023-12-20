f630670c85bc3f1ec1a372741910da352d0404bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:51 +0000

    sail activity or rain quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
