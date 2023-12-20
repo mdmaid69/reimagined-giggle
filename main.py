d6afa8f55505e604c7efbd9a0f11873033a387fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:09 +0000

    up bus mistake Uu large

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
