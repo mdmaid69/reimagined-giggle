3f33103f78bd00fce06d6b2f9b1367434c19b374
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:51 +0000

    conclusion family spend exaggerate before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
