baa94cca8078aa769f16dcbe27ac6871f11d1371
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:17 +0000

    action tongue war opposite getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
