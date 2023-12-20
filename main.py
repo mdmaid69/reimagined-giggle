59a6131fc5c2cf5bb6fbbb25761312b56a8f07a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:24 +0000

    official upset spare throughout protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
