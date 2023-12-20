d171e83b4cbe5f973ff96da4df8c30ac45ef0399
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:58 +0000

    minute runout start maybe cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
