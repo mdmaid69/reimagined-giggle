cb8ee336424376a436f9f63672732a92126f9c22
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:29 +0000

    translate rather herself account measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+print(os.getcwd())
