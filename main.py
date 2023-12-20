843f23aacf6bce02faaa93112624a1c9b263a2e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:09 +0000

    describe design mistake meaning medium

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
