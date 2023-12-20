a9ecc0a1be5ec558f1394e265537f81ed28abcfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:17 +0000

    this probably turnup possible big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
