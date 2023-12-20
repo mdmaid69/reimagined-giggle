4f64a97a65fb4f66d5c0c99acd80f688d38cc0c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:25 +0000

    callup offer well quarter temperature

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
