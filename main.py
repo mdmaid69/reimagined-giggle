6688ac6356bcbdad4e1c7315177b28c47d18ccf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:48 +0000

    load sit major belief laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
