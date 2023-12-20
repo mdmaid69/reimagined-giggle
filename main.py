16b5beaeb23f92785615f4b03b96af6c84951795
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:01 +0000

    herself calmly chief sweep nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
