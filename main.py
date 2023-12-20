a2c97f68e3cc0457f14b327512bcd72e8642bc26
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:19 +0000

    combine desperate straight takeover nice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
