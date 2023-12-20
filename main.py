6215427541fec56bdbb7c544b92cb141723e67a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:38 +0000

    guilty as organize purpose factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
