c7a8f12a18b76f7729b11d0ebcee12da1783e73a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:37 +0000

    male block also breakinto question

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
