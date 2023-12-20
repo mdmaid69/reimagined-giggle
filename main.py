182dff5bc514496728460d20470dd45f8ee3f1ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:53 +0000

    bath unpleasant late born imagine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
