b8b3f0d56fd81a12a9347910003c6edd9ca822d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:02 +0000

    money system minute interesting position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
