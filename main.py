e463e4afd124fd99337210796894cde3f402d020
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:53 +0000

    out lead of listen favourite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
