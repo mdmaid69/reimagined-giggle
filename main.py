6f97d1d41756d5a2863915acb3c4fbc8eaa5d4ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:57 +0000

    holdup tear cope fast improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
