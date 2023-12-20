edcfb3fa95f43d3a356da2f13f00ce37d9b4b9e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:26 +0000

    wish unpleasant happy idea value

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
