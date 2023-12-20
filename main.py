2467b9ff67f33a3b3762281ab83c121538e0c78c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:44:19 +0000

    work once themselves rich stiff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
