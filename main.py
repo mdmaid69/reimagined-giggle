339de9030200ef968ee2a7485fdbad27220924f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:49 +0000

    past bad confuse colour Ff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
