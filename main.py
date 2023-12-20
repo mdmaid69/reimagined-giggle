854a6939e6fdd6eb6115344732f4bf7d4cbe05fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:23 +0000

    land discovery snow regard television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
