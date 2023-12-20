b7dcd3cde8591f662f5fb9ba8ad8e8edb3fdbd8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:13 +0000

    569enjoy run polite Itself lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
