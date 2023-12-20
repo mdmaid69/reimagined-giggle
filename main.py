1295cbd9419fa3979835f755787da75b46c75768
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:23:28 +0000

    eager borrow politics keepout personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_current_working_directory():
+        return os.getcwd()
