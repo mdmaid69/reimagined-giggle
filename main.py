fb5ff8b28d5d8fc55acd898cc82a4ae34ec7d6b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:47 +0000

    takeoff likely towards lesson excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
