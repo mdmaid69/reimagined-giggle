c745178929d211fff45c03e358979be61b342472
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:30 +0000

    pile tall far bag independent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
