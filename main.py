355185a046f6d539788744ce57c17494f5c85a3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:06:52 +0000

    comeover sure direct split explanation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
