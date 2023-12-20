e5c9ed75723a3ea9891b4e8db5edf77ed4994726
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:54:16 +0000

    at declaration dance fit answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_max(lst):
-        return max(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def clear_array(array):
+        array *= 0
