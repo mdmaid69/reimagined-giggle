6fa3ff213a8010d0266adef578cd6fd1c0ed1020
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:38 +0000

    cap one any fit away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
