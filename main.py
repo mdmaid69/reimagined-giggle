94f5d4bcf277acddf8024d3e05f3fa6dcd96acec
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:10 +0000

    level power centimetre month hope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
