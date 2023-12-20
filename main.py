aecaa69d4e1aef4598a57c9513f2fd62cc7e1edf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:20:40 +0000

    exactly damp either hurt mine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
