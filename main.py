855eebc0771704fec57ddf1bbe2e4ab84a2be83f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:26 +0000

    shelter full goback(to) move after

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
