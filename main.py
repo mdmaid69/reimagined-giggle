8186dee41c00bf603aa546988159e69c8fdff135
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:09 +0000

    long scheme extra shade guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
