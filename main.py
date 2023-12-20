a293321b0e131538f4482a74869249c4d1c0ef49
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:49 +0000

    copy giveout degree type equal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
