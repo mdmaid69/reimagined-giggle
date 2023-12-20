ebdada3ca94077aae3e2b951c2984c7b3e0b97ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:07:20 +0000

    beside creature juice attention characteristic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
