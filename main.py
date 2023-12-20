c981b0a39dbdda943be0b1fecc974debd487dca3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:13 +0000

    day movement degree identify clean

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
