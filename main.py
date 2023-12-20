d91ee1f832be39b7529bf32f6c924d2b745134c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:36 +0000

    safe argue switch statement rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_power(work, time):
-        return work / time
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
