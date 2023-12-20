31d377cf60f541d57dda37afe1dabcc15f1f0a74
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:46 +0000

    shelter talkoutof wise accept authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
