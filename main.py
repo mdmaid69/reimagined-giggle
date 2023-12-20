b514207d952bbcc0ccf946e7a290c4c221597b48
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:25 +0000

    very cough clue well swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
