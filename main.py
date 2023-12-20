efa5a9268b503d05f6d590df9a2568ae60f67515
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:58:23 +0000

    first quit whisper clean yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
