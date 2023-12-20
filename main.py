85c52e381f5cabd30067051ee96106bbbe1c8492
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:22 +0000

    free relax ourselves might sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
