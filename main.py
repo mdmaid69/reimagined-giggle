d0c5881af571259086455580448cc1d03ba062b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:26:00 +0000

    glad whom forever surprise machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
