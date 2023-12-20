158660d5bbe5585e91ee64a59afa72e556d66238
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:49 +0000

    gooff several not illness relation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import time
-print(time.time())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
