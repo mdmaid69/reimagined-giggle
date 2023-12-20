90055243ca5ba513aa9465ebfdfb47de78f8773d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:13:48 +0000

    slide treat model wide fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
