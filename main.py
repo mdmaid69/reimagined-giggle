c4d34ac8b31465c14d4bd89a99addeb7170ae238
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:00 +0000

    goon community convenient post Ee

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_power(work, time):
+        return work / time
