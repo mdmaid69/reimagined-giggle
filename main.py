160d3fd83843fe76afcc711b6bb03d6a8c461a7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:52 +0000

    clever performance activity thought bit

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
