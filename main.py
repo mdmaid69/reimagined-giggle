d6e38e561d6ea1fa52cc4d3991c8f0a080bc3ae0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:45 +0000

    mistake offend mine embarrassed project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
