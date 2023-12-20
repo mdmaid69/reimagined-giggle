fee925e21c9a45969a6d909bc8a3be88d2bede77
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:32 +0000

    if switch memory wonder prefer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
