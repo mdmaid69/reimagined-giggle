eba4a8770c5c85d03a8a572ca4f14390a080ce4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:30 +0000

    track plate succeed fond pot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
