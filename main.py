e66210f73c7b68119b195036eca7bdbb9d06921d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:27 +0000

    goon measure pretty complicated all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
