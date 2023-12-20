ebc7533f80f19b9b9995d4602e46932a89b00b8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:43:43 +0000

    alone roof system copy equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_power(work, time):
+        return work / time
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
