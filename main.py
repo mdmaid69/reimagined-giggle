22d8c5018ea44ed7b59eea137862960465d1455f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:26 +0000

    add still consult worried habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+print(time.time())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
