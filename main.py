565cb1e78de37ba9f6ebb2700c2bf585229155d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:28 +0000

    ride sport space finish Itself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
