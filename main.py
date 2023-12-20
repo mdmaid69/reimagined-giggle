e991df9d33f923868b1e2b4d9fc8ed725683a76d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:53 +0000

    ask short second bear reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+name = "Python"
+print("Hello,", name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
