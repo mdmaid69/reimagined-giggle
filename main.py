a6cd86adae776bb958661cc48ea5586a1824ff1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:52:26 +0000

    how tour read dream by

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-print([x**2 for x in range(10)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
