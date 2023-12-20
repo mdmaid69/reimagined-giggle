ac8f998ee6350d40fcafe7aa452d73ec1b79f6d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:29 +0000

    why treat rate lie fun

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
