3290603f5a60e2507f0bf3264006926f65c078b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:38 +0000

    not doctor speech criticize stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
