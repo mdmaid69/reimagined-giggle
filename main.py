ff22f2714883bd7af06b2521513083af913bb19b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:16 +0000

    dance surprised account harm crash

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def square_number(x):
-        return x**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
