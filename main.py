70dca8f159836144ab9ee4ed0687e2cf3e717525
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:47 +0000

    pure trade instruction joke behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_array_to_list(array):
+        return array.tolist()
