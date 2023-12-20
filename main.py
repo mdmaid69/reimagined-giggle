d6ee7e4a802900e2780fb18c1e6d9da31be8782f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:16:58 +0000

    interfere understand you report provide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+print(sys.version)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
