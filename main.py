b7bca4815f9afaf38bfce64937d0c2d9b14adc7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:43 +0000

    transport supply relation mainly real

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
