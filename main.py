5960d4afdb8fe0a05691433c2ddc60565c264d06
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:05 +0000

    possible bang tired scream call

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
