c9a7509fa8754fac4aedd0e2833a9a160c4d3b4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:50 +0000

    front choose snow argue anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
