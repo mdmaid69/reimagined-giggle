1dbeb513563f8979dd7eaa0275ec2008a73a85bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:10 +0000

    above bread order skin pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
