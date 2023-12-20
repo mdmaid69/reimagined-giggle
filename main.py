d8ae103c827e99dd7cdb5f80fbc1e252aaa58eaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:09 +0000

    blame frequent before move breakout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_user_string():
+        return collections.UserString()
