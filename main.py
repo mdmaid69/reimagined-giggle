9eada9a8f38d947e5dd507e5b8cf3b02422029ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:49 +0000

    quit much pity fashion clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_set(array):
-        return set(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def get_array_index(array, item):
+        return array.index(item)
