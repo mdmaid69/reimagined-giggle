f06b2bcdebe9a9f536f286c6ded3a14cdf8dfdda
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:30 +0000

    little understand spot lose criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
