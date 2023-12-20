6acd5f15ad8400a4e1e71800fd3d30e6f9c7fb92
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:55 +0000

    complaint check wet taste provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
