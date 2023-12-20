0afeea9214622eaba7e68c7a3b67c473326e00a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:10:45 +0000

    needle holdup loose Bb angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
