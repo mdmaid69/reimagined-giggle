723b1da08ec444a19a1d9afdfd3724bb22d356c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:46 +0000

    declare wipe Zz crowd convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def add_numbers(a, b):
+        return a + b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
