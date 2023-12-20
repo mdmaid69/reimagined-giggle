029c4be8b4790e8c0f823013d7ecaf6717c61959
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:35 +0000

    desperate detail he club cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def exit_program():
+        sys.exit()
