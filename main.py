a72d4e11793bf33d720511b99dcb2ccd9db28bac
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:46:45 +0000

    you frighten ever wish patience

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_power(work, time):
+        return work / time
+print(sum(range(10)))
