7e4afbc252d07d045bd1f030946a5076f3349216
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:47:00 +0000

    Qq complicated fast doubt suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
