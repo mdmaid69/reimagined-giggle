786e4b3fbb1cb00a303d0580ac40b5f6129d4c1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:51 +0000

    game pride thinkover usual tight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 def convert_to_binary(n):
         return bin(n)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
