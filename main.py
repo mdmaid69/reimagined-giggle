953099bdccb70d1259280cd8b2dd76ee7fc67cc4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:18 +0000

    suppose return back report result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
