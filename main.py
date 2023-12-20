d6207eb8dfaa9d250f1df77e099a0345829939e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:44 +0000

    software crisis platform shade suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
