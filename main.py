a593fe19d701aba9e4a205acd205fe17a090ad2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:58:21 +0000

    lookafter question owe mouth base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
