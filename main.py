5501e058376cffa9232c0f896f978158d17fef3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:29:29 +0000

    stamp deliberate soon clothes fasten

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
