6a26ce3e45408ee00385fc08cdf37abab57083a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:20:23 +0000

    yesterday safety and common nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
