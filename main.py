fd37f006706ac76790397ef3c998609a3275c199
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:56 +0000

    point rank common confident motion

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
