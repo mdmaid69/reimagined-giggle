6cc32bacaa9471b14399b2aa5de8712e8d01ded8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:00 +0000

    anywayanyhow gentle Mm mark breathe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
