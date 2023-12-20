1c33c20e86f6bd2e6d9e9c217d7cc4dd229a0e6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:46:36 +0000

    law company its less dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
