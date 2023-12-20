015d8fbc0c607d2ead10c57cb860dc8931d490a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:33 +0000

    worse freeze beginning extra large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
