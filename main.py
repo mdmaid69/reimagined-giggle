d18d313b47063b54cb56ebd7fed30390f1e618c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:47 +0000

    takeaway brother lay Bb open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
