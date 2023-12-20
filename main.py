ae393c3347c3466f83f6e80dc2136b3967335fbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:27 +0000

    road surface coat sore well

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
