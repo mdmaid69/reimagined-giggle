bbd7beb21a5681ddae46205687459e232c572974
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:54 +0000

    breakout winter rid if lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
