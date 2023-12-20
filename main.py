f324531e89ab3eb19b0e59962134bc3b87de8dba
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:46 +0000

    unlucky medium scene attract like

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
