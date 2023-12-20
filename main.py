06d81b39f5c9dd7e526a84802474a20735828a5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:44 +0000

    rarely climb recover dull callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
