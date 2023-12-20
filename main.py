b19d17f18ca3949c1811cf823304bb18b804cdf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:35 +0000

    dull muscle university mixture best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
