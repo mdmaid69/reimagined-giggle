fbe7a3b776f443a49fb0d789219c8a059b7f0f0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:33 +0000

    add person grateful greeting people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
