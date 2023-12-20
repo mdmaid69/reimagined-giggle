dfc2123b204de7e47af9af04fc91481c3564a97a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:57:32 +0000

    skirt eager Ll brush getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_acceleration(speed, time):
+        return speed / time
