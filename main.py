97c429c1a7096e83d46ccd8d85aa486fcaf08ede
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:08 +0000

    quit discuss straight government mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
