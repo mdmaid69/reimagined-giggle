f93c614bbad110315704c6a70e8f4563e57c2f92
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:00 +0000

    worse school track greeting slice

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+print("Hello, world!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
