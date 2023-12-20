db385bbc54184938d75bbb4718575f979358eb35
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:41:43 +0000

    relative theirs broken hurry warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
