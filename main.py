09c0d52d2980f2cfc62294bf0dce84079590913f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:02:40 +0000

    fire give rest release autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(lst):
+        return min(lst)
