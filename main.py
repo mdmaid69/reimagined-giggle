9f4194a10d6502de7212dcfc6996196f80dbdf3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:46 +0000

    married tear appearance on share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
