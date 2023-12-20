58ccceffd58f94ad5cb1d04b3474798734bab68e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:11 +0000

    whatever plain although match model

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def is_palindrome(s):
+        return s == s[::-1]
+def convert_to_octal(n):
+        return oct(n)
