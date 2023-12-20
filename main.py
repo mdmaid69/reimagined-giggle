e94d123ce81d1e492de32d624f10bcf89965edf8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:18 +0000

    factory them actually city group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_current_working_directory():
+        return os.getcwd()
