6f3eca0bedef664bf03278a60dd09302cbecfa30
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:53 +0000

    unpleasant wave again damp push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
