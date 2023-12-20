a8e88c710e89e9266d313bc260c55515021fbfa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:41 +0000

    disagree well but clever lonely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
