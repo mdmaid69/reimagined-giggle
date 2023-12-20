6e4cce67fdd3693951fc2a570577223b5a78a8bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:28 +0000

    classroom purpose mixture wire certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
