b00ee9d5d1e792ce6cf682c6e4d1c455c2ec437b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:10 +0000

    must pump muscle fast stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
