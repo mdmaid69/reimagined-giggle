39231d8e03267ae93df66bfad5f44b6f8e3b9bb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:17 +0000

    responsibility brush basis design curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def count_characters(sentence):
+        return len(sentence)
