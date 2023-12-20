c288d00e8251624595626496f751e2d5397cf52b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:18 +0000

    breakthrough cruel lead area between

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
