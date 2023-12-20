570db0a6a52a45a849cbce3c154c3c90ab6dd226
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:50 +0000

    square dance breakout in sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
