fd887a5f894b8180d7ae5fba772e1d79e90205fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:54 +0000

    actual meeting burn instant century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+x = 10
+y = 20
+print("Sum:", x + y)
