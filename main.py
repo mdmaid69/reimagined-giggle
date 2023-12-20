ae475c03aca6ebe9c9012d51799ce638c1a7bae8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:22 +0000

    peaceful say century office invitation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
