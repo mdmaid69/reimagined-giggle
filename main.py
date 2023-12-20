de6c04a38eeb9cea4bfcae817f3c6cfd8bb79c93
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:59 +0000

    smell across second influence feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
