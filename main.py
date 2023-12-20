ad86815bc8a5104918b5aaf087eb01e52b0bfd23
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:09:09 +0000

    close original natural accept cover

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(10): print(i)
