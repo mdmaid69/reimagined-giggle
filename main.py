c251cb80e9a1830c688f4f835634a914619bd981
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:41 +0000

    imagination dress compare live swallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
