646bbed1bab483726fe4c43792afb10b37b5c872
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:20 +0000

    song clearly noone sticky over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
