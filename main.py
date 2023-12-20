529f211ee0ce7d08b0f2d7b5b5c11fd4782e98d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:22:41 +0000

    invitation delicate test affect accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
