17c07bae5184817245b69e2cabc7ada9c3cfa7a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:37 +0000

    university nice maintain of card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_max(lst):
+        return max(lst)
