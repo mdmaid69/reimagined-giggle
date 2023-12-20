29e3595ce332934c3248709d29a3f367f014f233
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:24 +0000

    extraordinary then unlikely ordinary first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
