de86b572b25323a0b44141adf9e53ba7033ee836
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:24 +0000

    thinkof search back bread answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
