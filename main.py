8ccfbfbd065aa41cb122a2cd0179e22b5a29ed08
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:42 +0000

    show bringforward ready type fairly

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
