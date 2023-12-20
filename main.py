6766b4fa73c239c401518b0cfe279c0ed179732c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:39 +0000

    since kind anyone figure news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
