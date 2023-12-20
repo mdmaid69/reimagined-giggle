d18b44996da94f983bdf85fd2cc7d36d2908d6a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:31 +0000

    flow several sense solve advanced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import logging
+def log_message(message):
+        logging.info(message)
