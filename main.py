3dd50a083e8eb56510caefea71d5d2b2d4473aa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:56 +0000

    opponent day union warm everyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
