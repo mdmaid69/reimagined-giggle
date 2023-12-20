783c70ca31ec14f30b14077c846e2c2df782dc71
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:31 +0000

    art note day herself smoke

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
