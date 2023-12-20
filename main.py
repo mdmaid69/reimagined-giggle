bb58c6a22cf12bdad0cfae51521960096343257d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:44 +0000

    store age appeal jump week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def reverse_string(s):
+        return s[::-1]
