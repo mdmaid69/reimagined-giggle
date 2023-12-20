5fe3dcd720c3141591f36593ee08950efa78bcbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:05 +0000

    bar circle dare complaint continue

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
