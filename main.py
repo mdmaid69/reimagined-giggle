c6323fb8acc8d367dc71eb4e51f45bbe69963c69
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:40 +0000

    stiff straight round handle operation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
