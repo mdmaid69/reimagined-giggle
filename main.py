b1881166e6184928fece1dce931be5066bfe98bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:19:59 +0000

    warm morning go peaceful complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
