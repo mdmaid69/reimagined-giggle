b677c79d12d20acf49e5e55dd06fdfb0d2377df0
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:09 +0000

    great offer breakfast doctor while

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
