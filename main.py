a37a964332fc9cd6a1fe8c589a9e07e5c2569d9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:20 +0000

    accommodation emergency artificial each sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
