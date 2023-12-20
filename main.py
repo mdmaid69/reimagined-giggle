2b79d6c6adaffcf91ede2ac93cae3d506a4404ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:41 +0000

    importance gap operation since yet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(sum(range(10)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
