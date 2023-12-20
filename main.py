dcb284f51432043239a58618006d7266bdfe780f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:44 +0000

    full development relative patience develop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
