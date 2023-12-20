3b296179c1612143e25cc6d8535eff4e48f0626d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:29 +0000

    taste responsibility win street repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
