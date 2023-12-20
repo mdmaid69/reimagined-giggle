ee4b5c25091e965c5ff158c9677a476015918d8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:24 +0000

    set meaning avoid track cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
