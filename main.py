9fc1176ab53d0fba92273a23d782eb918073e4db
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:43:48 +0000

    idea breakup sail later recovery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
