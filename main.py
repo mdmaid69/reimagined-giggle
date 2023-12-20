2339751d5c56bb7a41ba69a64e078618f474c47c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:33 +0000

    world solve list say instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
