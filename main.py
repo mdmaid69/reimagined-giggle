4131deda7ac640a95cff507aa25183403f2ce1f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:44 +0000

    test innocent model repair advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
