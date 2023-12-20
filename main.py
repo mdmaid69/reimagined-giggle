2ccc8c9fb1286e3b46ae016aa9e0ab3a73cdf67b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:48:50 +0000

    uncertain rubbish confidence burst disagree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
