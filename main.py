4a11330c8b8848140c864fdfdc117359d1e3ad2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:53 +0000

    mail preparation tired hold left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
