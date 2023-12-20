e088e8e7214429fc38e4398ee84f60801b1e0ee2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:38 +0000

    under match stranger apart hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def convert_to_octal(n):
+        return oct(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
