7238b2bfa25b51f7a56ee43eea896c13fafcabc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:38 +0000

    much improve hello nature challenge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
