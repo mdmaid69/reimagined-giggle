b24af8fddd665d9790d23aa905156a5fd8f59baa
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:34 +0000

    runover turnup course date convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
