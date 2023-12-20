1a06ba341f90a6f219657e455ebd22792886a96e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:22 +0000

    comeout reach unusual open weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_error_function(x):
+        return math.erf(x)
