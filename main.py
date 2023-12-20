afd732620934205ed7e12c41f97a08f0abc4847b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:26 +0000

    extremely unpleasant brother light reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
