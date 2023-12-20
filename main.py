f83089b2209ce5f8c0fc0f67b13f6e8d5756ab17
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:00:44 +0000

    base sound rush zero will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_string(s):
-        return s[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
