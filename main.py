977e1a768a622edde82d10a0b93f66e88835a842
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:54 +0000

    cause to suppose brown myself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import datetime
+print(datetime.datetime.now())
