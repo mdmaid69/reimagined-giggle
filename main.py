404d1d217fb2f5ae489a18e2fd3d245e742eaf52
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:33 +0000

    country tip geton ourselves measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
