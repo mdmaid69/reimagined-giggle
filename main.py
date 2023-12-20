a0107198441a1d9d36c4f2348413d2aae9757651
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:43 +0000

    voice minority secret board progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
