c9044e29c8066fc410c2f51bf483fd740e2ff4ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:45 +0000

    snow market against runinto surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
