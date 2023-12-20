42bdd80744e68af17d99c718f4fd32d66ed94262
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:04 +0000

    wave unite mail big human

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
