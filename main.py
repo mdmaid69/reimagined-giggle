5a0959c25582465ef8b9238301b2ef72953ae68d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:55 +0000

    compete cruelty once fill band

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
