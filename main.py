afd5705e28caa7661b1898c5a48c05fbfcd7c889
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:52 +0000

    comment front anywayanyhow problem mouth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
