779881031d9d0a22842145eb3714bc794e59b3ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:21 +0000

    for produce cinema term total

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def square_number(x):
-        return x**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
