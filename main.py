286627cc9d7e1a274678d00c38920b479c490ea8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:41 +0000

    place split tell rely bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bool(array):
+        return bool(array)
