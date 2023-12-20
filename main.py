d5ed7209653bb8f30b572b4e025874d0244ad4bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:48 +0000

    whichever reserve pleasant unkind exam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
