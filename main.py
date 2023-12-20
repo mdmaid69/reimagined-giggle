0a16d381f33e58efb2177b1cbcaf77fac9412d49
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:35:48 +0000

    education develop past spring everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
