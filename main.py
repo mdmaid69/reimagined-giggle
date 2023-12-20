6ed0ad576583cdf8e66718e75d78125a2dd57fa2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:27 +0000

    damage belief impressed Zz lead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
+def square_number(x):
+        return x**2
