0cd7a8f22455a2a18fcb4fb94b236bcb7ce71508
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:25 +0000

    by basis blind direct gap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import getpass
+def get_username():
+        return getpass.getuser()
