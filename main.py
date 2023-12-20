7d2f6e30e9d1074189f3fa0621e5dcb6e17c3c2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:43:00 +0000

    open Mm seriously ticket will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_elements(lst):
+        return len(lst)
