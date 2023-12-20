4e91eecd80c8419f354d825ad6c32264bd915c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:16:01 +0000

    form here combination now charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_user_string():
+        return collections.UserString()
