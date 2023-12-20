d0729e39558add17f68fb9c686fe1c510522eb87
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:01 +0000

    stick medium Ee justice throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
