dc709f6ea1de2cd55919991df20a92ac99f70ad5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:18 +0000

    label project escape criticism small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
