11df9d8539ccc2e563858fce0bb7e9ac61fbb525
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:35 +0000

    sure fence start justice angry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
