3c35acabc22ff86c6f019cab66da4b8460856c60
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:58 +0000

    total strength port artificial from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
