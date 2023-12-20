54881e1da97a922c154d978e38f40067bd92efc4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:25:58 +0000

    likely shoot power brief traffic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
