c89480173beb30308140ba92b744118297c42da1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:46 +0000

    calm near wheel persuade telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_min(numbers):
-        return min(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
