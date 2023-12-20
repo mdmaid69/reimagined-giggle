cba975f0ca24e345b5df3830f23d9a4143c096b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:38 +0000

    maintain attract 179bite report spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
