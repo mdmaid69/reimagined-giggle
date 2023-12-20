3a671c5290d5160eb38f1013164e42c37255db06
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:54 +0000

    when size steep phone might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
