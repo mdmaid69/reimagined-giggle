9d40540cdcc42a865453e24ff999853962ff0ff0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:07 +0000

    part coffee repair unkind warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
