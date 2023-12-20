57847628b452740a3119a838ff34eae658bbbf90
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:17 +0000

    stay partner under arm instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_array(array):
+        array.reverse()
