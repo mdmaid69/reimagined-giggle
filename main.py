7518e23ec6f47d08b49d5a7ad94f174258254e89
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:29 +0000

    myself equal congratulations mistake last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
