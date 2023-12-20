02d0fc13582018b02cc6ac0a79bc2ebad07b2676
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:27 +0000

    attention position upstairs disapprove closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
