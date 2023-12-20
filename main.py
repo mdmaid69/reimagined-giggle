b611fcbf9ac8305751a852e82cd4d1fae338cb9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:21 +0000

    below definite pollution bound travel

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
