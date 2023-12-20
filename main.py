e77845de240301f29ca0ad913a0ced811343a31a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:38:08 +0000

    visit declare between exaggerate luck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+for i in range(10): print(i)
