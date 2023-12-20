cbe9e52bd5f63d765c45b5e8f3beb00cff118a09
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:47:06 +0000

    react official bend protest draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
