a1ca86ab7fc7729c666bce4ac7fa5784b61b1a76
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:51 +0000

    autumn relationship instant famous debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
