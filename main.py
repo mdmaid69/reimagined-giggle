a1145933fbcae385f846f8dc64a64187b9a5aa68
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:56:45 +0000

    deserve intend straight sensible press

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
