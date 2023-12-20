31f7e0d7e74947e52d1e4ede41b578b9af3495f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:51:23 +0000

    noise invite used release forget

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
