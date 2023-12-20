3b6842d952355cd9fbb6780c4f2faa660bfb9c26
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:01 +0000

    complete bright moon tooth both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
