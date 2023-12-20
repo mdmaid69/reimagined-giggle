4220af76c205f7614117e206a1ad7fa533981fc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:39:57 +0000

    concern lay back traffic breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
