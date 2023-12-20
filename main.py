c64d08f55961627c806e0c3706dc4a51a0314c69
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:16 +0000

    part listenfor passenger film thank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import matplotlib.pyplot as plt
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
         plt.show()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
