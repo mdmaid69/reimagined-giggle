268a2e79600dc4e29ebbd4e97fc3f065d20964c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:23 +0000

    advanced oil past develop watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def remove_directory(path):
+        os.rmdir(path)
