bc95f624baeef431e3cd1088fa4ad091df9fd80f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:55:59 +0000

    bury respect direct threaten child

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_power(work, time):
+        return work / time
