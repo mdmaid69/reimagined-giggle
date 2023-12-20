eb5888c94a3e93848cec895be95593192aa6cb8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:27 +0000

    exercise weigh must now pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_min(lst):
+        return min(lst)
