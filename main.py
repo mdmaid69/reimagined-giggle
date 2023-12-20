6fc3932fbfbba01c04b8f01266182a24b2744f96
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:49 +0000

    ago sure behave bite thing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
