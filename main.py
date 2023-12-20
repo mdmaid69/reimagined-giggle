3f32593ed617044395fa6a8a1390fe6a42cc1cc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:50 +0000

    there bringforward choose whenever crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
