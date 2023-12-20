59c183bb19a09fcd6bf518fb1989803afde75cbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:48 +0000

    score drive indeed control Itself

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
