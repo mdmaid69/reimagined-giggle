11806796879372722ed8a7a13faa4bbb1be7ea9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:29 +0000

    away justice wide culture goup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
