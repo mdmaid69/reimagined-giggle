8f5b4d93c2ab2c3ce311d29781f73e8b1d41792e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:54:06 +0000

    complicated destroy shade save before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print(sum(range(10)))
