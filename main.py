a48df7b49f55f10e61b4ad5f574340b5d1db7262
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:47 +0000

    angry truth contrast activity ago

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
