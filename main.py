bb26f7af88a3be4c61b6994b86fa6d8f1f0a91a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:53 +0000

    better minimum ourselves sit convenient

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
