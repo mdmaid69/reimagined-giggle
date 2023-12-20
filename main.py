910a1c1654d982b55211e7acdf1d2f94364b1d6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:52 +0000

    without edge harm fetch translate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
