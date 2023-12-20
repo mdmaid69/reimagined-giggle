099be7d2bb9b2e606106cb04656e2f5f5b20f82b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:34:43 +0000

    smooth she godown war direct

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_density(mass, volume):
-        return mass / volume
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+name = "Python"
+print("Hello,", name)
