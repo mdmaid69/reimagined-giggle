dcfb4bf969e27061afb0482583b5c74fdf31de8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:10 +0000

    once bunch before swallow innocent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
