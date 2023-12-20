446db29e0894711288cb4b02d86fea9411c21c71
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:52 +0000

    pure lookaround exactly still permission

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
