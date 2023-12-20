7824f9738c5e8b97a52f632620f86650dda11271
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:05:08 +0000

    unfortunate Gg turn shape sail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
