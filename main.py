10bc4a12ac7d390e171e1ff8e19ef711a4930dde
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:02 +0000

    single artificial challenge sign lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
