3d16dedeb44312d4532642531721273027b98c70
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:49 +0000

    awake satisfactory tape problem care

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
