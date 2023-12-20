590de7b16c1ce8b20aeb659b36cdde051e12395f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:57:04 +0000

    run about it pin last

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
