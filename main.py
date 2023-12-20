98ecdb04ae417663aa985d0eb6846b1ef25f76fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:41 +0000

    drop dead closely of press

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
