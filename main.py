34ad004434e8d9ca38d2536889ef529bd037d89d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:27 +0000

    length dress position willing clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
