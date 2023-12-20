460df8f8329c1214e022a4a40e5b3b11dd8f328d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:54 +0000

    move to joke front before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import re
 def replace_all_occurrences(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
