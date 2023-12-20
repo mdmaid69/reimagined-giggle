401ff4095e460e5c0ae1d4a0a7f549083250c5c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:04 +0000

    view yours before no unusual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
