71cee485e1ea22e67449ca6c960e1773b989720e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:00 +0000

    injury tragedy succeed impossible during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
