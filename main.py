6f4be43e2616a01b1894c5fa6ffea74a17c7e564
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:27 +0000

    out miss serious strict later

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def change_working_directory(path):
+        os.chdir(path)
