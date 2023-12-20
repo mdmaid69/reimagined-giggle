68e6c4604d0978c7a50e21856a6302d6f2312459
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:34 +0000

    sorry normal continue wide contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_set(array):
+        return set(array)
