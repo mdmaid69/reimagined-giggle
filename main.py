3b7c53376d7f3470b93be2f88c6165f858ea61ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:58 +0000

    respect neither mark cross rely

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
