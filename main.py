184c67aaedc410a84d9592cce7954ce78cf5e2ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:09 +0000

    pocket then present marry us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
