fc408cfde69d106ac70269f23bb77c33ff96143b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:48 +0000

    cope this twist fashion dinner

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
+def find_unique_words(sentence):
+        return set(sentence.split())
+import socket
 
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import collections
-def create_counter():
-        return collections.Counter()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
