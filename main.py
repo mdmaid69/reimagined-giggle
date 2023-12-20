227815519febaef7ba66fcd760381b6e9fffe1e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:49 +0000

    factory shut hang letter able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def sort_numbers(numbers):
-        return sorted(numbers)
+def convert_to_binary(n):
+        return bin(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
