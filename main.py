d7b5ccb42384c367d0e5d4cc09b2ca6dab360852
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:07 +0000

    consider effort chin mystery turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
