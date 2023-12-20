1302bfb79a364cc4768a5cced4a2ecf19b860793
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:49 +0000

    backwards hurry unable bet minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
