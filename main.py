2a69ecf000d96efe1b650184f9faabe39ed05e70
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:39 +0000

    minimum runover average project mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
