6ee25522ef9ca48312c3a28b8e1f7240be550c1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:06 +0000

    shape money entry land important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
