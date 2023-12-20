928913959ab17e070e08bd27af3664b755732e02
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:43:19 +0000

    partner flat plain wave agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
