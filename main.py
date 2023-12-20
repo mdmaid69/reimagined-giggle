b12da85c3df5dd31f007a9541fa4a3fce2978674
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:01 +0000

    cry likely hers touch line

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
