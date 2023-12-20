ef1840efdcf3be9d34155bfa187c38410fae8069
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:44 +0000

    innocent skin 571enough whenever sit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
