e0fa7fa355ef722be06a8528a1360fe7a4b2d7df
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:26 +0000

    better hungry moment throughout section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
