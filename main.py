913326ab7e71a553cd8d90255bbc06d398dae5f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:12 +0000

    slice around can group wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_current_time():
+        return time.time()
