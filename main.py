0fc0804e3b275b60f82dccbf87242d7ce73b345b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:43 +0000

    smoke please instead represent project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
