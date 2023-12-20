847fa343d72978b5d4f4cb819101c4ced8987805
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:19 +0000

    save excuse matter dance across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
