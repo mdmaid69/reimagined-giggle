b8d40c72843af45377e4ee21acc0ab007614b7d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:57 +0000

    freedom nearly keepout trouble visit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
