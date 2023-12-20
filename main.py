eeddb0b55d7e0fa51dca9841a36eba54b8b04613
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:32:36 +0000

    likely sight defeat dull getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_repr(array):
+        return repr(array)
