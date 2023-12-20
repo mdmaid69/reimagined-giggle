d42a7c399567056c816a8a4c65a6931d6ac27aa7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:56 +0000

    never desperate geton available dig

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
