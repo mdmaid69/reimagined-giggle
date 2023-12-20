dbe0d52d69d63f2754416bc0c51028c02c940a2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:07 +0000

    exit forever turnout movement sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
