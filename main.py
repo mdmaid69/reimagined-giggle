562fc916fdb18be621a3567f29f16e5ab36c4d27
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:27 +0000

    over gentle thing less funny

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
