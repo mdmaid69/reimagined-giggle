f3adeaa9c6f454f86607c5cbb3268e1f4e99f281
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:04 +0000

    them believe touch hurry copy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
