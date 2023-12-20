6213ba5d8d181142b81cbd3070a9e22c01e93774
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:29 +0000

    motion worth praise cheap flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
