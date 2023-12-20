5490bdbd0af38aef250d6f611dafba593e6e958c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:35 +0000

    neat sense drawing Uu bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
