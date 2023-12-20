0a594939845ca94d165cd154f07d35079f793f4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:59 +0000

    tragedy suggestion open slice holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
