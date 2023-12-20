526a71c36d30c851ca013f0f6e477792d1f01b1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:36 +0000

    expression listen ever shock exaggerate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
