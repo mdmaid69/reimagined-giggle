b81c8eb7bf0fa142a27b40a33fe60b16bd7609a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:48 +0000

    mine job reduction service butter

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
