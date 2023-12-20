ac4a36b1e01d6497a9ee7d1646ddd45cd9bf8124
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:28 +0000

    you tongue course hollow lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def clear_array(array):
+        array *= 0
