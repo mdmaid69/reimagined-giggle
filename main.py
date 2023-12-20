46970e2c882756dce56f5556616dda0a95db1766
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:41 +0000

    572enter professional describe twist past

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import time
+def get_time_since_epoch():
+        return time.time()
