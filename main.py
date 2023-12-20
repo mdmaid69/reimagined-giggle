0fbecf418d80dcb8cf3bdfa6ce5770901d357fc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:47 +0000

    fresh answer compare luck bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
