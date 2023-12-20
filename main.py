0bd56291617066712d8eec084e7e094af2bd89ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:13 +0000

    over comein combination pot exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
