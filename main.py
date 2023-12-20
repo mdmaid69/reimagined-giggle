f4f7f5e50266c7741896bafb1148c60be8ad6681
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:06 +0000

    provide port minute recover alone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
