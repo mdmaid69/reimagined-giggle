4ddaecbaaea105014f309e8b25ca7e8135a8afda
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:39 +0000

    university bath education sky separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
