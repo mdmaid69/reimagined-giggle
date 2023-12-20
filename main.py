e86e5111942e57e2cd1d746bd5076e64ab24f3d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:37 +0000

    you principle very believe pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
