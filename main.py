8e167e27a0bdd9da0302ed3f07f0a1d138a9da28
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:10 +0000

    welcome today before family watch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
