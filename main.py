3420a1b6eef62521a18e71305715d27538f63b01
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:07 +0000

    friend police pinch upwards scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
