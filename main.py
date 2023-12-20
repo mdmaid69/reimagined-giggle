ffc21c71e258b5920e84d0fbb6f3786bce3df1ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:45:20 +0000

    family knock lunch nose mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def is_palindrome(s):
+        return s == s[::-1]
