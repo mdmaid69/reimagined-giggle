2469d583cc53ee2b442ee59b1feaad039d37b4b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:35 +0000

    fence wear reach yourself border

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
