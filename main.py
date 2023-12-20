95762bd6a096a3cf4fd5e9fd6bd511d7e3ba3324
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:31 +0000

    comfort avoid answer smoke rid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def count_elements(lst):
+        return len(lst)
