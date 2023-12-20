3c021dddac71151f0d17386775a8b8b6da1bc79b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:20 +0000

    comein sticky test grand performance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
