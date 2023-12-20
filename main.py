24d86f3af5b63442122665ad158141578f65780d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:56 +0000

    entrance pity date real second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
