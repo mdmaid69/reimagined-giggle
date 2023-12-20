32547c2898602c528852f1a4ed7b581a58600c0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:21 +0000

    wise doubt now develop moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
