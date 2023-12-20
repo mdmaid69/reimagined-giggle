03e180c6cf8897d71424b4517f5467a9ec7b4baf
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:34 +0000

    glass history across watch storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
