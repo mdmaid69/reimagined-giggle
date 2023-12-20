ec3105c6ef26c2d607dde29b8f057b2bd8d35adb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:34 +0000

    straight maximum dry programme mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
