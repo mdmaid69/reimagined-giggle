da90f51856f0feb1920c67a5eca6ef8fa3c13dd1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:47 +0000

    long culture promise upset relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
