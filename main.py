a45bfb3febc8a1b432b32e9d34cceec7a7d2c4dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:37 +0000

    funny file appear comment climb

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
