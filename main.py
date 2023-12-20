2af2104d4e750add27ada2a7dee77c8930d1592b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:43:46 +0000

    many send statement bound container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
