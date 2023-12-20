682e4d3403cc0367699cd89222a67e1a6c3acc19
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:00 +0000

    rarely someone clever task surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
