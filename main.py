93640a9c8339f171272f2dd4bfa9c67244f308bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:21 +0000

    inside everywhere also division drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def clear_array(array):
+        array *= 0
+def divide_numbers(x, y):
+        return x / y
