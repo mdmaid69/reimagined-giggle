44682f203d78c69339b69142aa4b5696c835304f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:29 +0000

    claim nor connect straight contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
