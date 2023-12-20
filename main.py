729a50fa791ecee3a0838c909bb306aeeed22c03
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:20 +0000

    underneath what claim exercise wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
