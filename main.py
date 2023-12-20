0f9de42f0148ee9a0523d4ef523bb28a2cb62752
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:23 +0000

    sensitive spill switch path opponent

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
