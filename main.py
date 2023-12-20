a11699e6068ceba4bb7ef2d4e39f3cd1c21d37cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:52 +0000

    surface furniture busy stop account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
