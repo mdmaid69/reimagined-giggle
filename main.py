ed3f22512514cce033f3db3e546de0c1e615e1c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:42 +0000

    time border theirs read Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def square_number(x):
-        return x**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
