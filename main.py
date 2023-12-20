984465ee9bc6f21c40de8e87b62670934fcfe8d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:09 +0000

    hate goout false future total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
