02b49b5111ee2578949c09cd000890b791821ac3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:28:01 +0000

    war accidentally left copy quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
