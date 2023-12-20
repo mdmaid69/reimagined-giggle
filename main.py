03eb22f78b50540c1cc8238d9be0cb341d21195b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:10 +0000

    ordinary false meaning experiment bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tensorflow as tf
+print(tf.__version__)
