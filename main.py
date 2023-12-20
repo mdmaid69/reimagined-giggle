60447f795d619b9eddb541208fcc5530cc4d207c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:13 +0000

    whichever lookup gas talkback squeeze

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_min(numbers):
-        return min(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
