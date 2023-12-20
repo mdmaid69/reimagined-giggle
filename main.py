0a3091a046277264ae0e821aac052b74103ffec1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:59 +0000

    nerve emotion bunch mouth please

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
