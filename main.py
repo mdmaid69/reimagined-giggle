37887649b23d8642703a82ff187406843bc07604
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:25:10 +0000

    finish usual flat vision suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_power(work, time):
-        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
