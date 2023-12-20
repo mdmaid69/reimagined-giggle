06b4c1eee1b9c74da2b841b6c2045e45122bfeb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:36 +0000

    right book push spare live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
