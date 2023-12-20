67b5357f337ce929805e4d3a674774a8ec16e0ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:03 +0000

    shade male nice equal cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
