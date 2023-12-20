8d704cf296723191e101eb0017160ee63c5f2c6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:22:19 +0000

    unlucky wet deliver do expression

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
