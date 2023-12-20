daf886412e99422dc58761fdc76194c1f3422ddc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:10 +0000

    pretty cause ring confused face

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def convert_to_hex(n):
-        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def convert_array_to_string(array):
+        return array.tostring()
