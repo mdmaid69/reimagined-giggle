3b7007723d4739e720db2f63df67214acf8faf6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:23:39 +0000

    slowly phone crazy physical fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
