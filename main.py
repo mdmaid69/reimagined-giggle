7123d2a4293b2960034c9c850bb3aa92781e82b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:14 +0000

    cheque objection compete tight short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
