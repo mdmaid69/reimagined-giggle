cb47f878628bd193b4b309c760dc389aed7d62b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:40 +0000

    pocket poison relationship trade whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
