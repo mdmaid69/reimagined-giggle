9356e733d8a8274c0ddf6824eb140c2b280d1369
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:09 +0000

    special after we very calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
