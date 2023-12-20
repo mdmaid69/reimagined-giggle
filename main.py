768cbb8c543bbdc3b2f986a81ad7ee583d96b35c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:39 +0000

    possession group moment through slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
