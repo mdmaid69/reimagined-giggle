c250521f80f670db96d6b9b56bea933779ef3c5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:33 +0000

    decide promise reduce event kick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def convert_to_binary(n):
+        return bin(n)
