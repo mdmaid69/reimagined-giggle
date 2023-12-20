6be55bb0b4508c06c92759c5a785c297eb7904ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:51 +0000

    hospital shut valuable tip completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
