02b0a134de468b707f8ebdee21cab7caeaa4de12
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:50 +0000

    their meet onto to table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import sys
+def print_python_version():
+        print(sys.version)
