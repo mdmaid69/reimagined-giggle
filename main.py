d7f3370f85d203ae448f134ebbe08e99894e4160
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:04 +0000

    start crazy geton love believe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
