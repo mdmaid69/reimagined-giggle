d59934ab492db04faa0558361d76f3f605e61dc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:36:49 +0000

    period kick flow estimate medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
