0c62eb299fc24b3ac16d76ffc4d850209a69aca3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:00 +0000

    most drink proud Ff violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
