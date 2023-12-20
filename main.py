9712e4cbb5159dfab9fd2c85a1d04513a2a0d6a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:39 +0000

    extra week scale correction everyone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
