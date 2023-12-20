e769473ea5afdff61fb2fc54709c04f359703e88
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:37 +0000

    delicate debt test shout muscle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
