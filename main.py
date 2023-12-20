2cb3038ec15d405e850d38d3f5e008b2d461124c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:36:44 +0000

    wherever report this successful record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
