7a42ec5929eb7ebf4b67c435728a5be1c7d4d92e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:40 +0000

    relation remove shout let unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_min(lst):
-        return min(lst)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
