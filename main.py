d128b32c81146e5ec54afe690e274edc9bac03ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:50 +0000

    sky dead home include needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
