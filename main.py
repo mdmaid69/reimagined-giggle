7dc1b5a5e56fbffd4dc73bbd8c8f9cc0d118120b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:48 +0000

    try friend mention large soft

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+print(math.pi)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
