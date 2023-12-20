94ca8838bc794db7dd80213a808e9b3c0d0a342c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:13:34 +0000

    pencil drawing shock go stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
