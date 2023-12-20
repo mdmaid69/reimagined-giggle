61a03ea3b8652f7ce295f3116856254c6eedf55f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:08 +0000

    silent actually tidy bridge rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
