80ee320562f7d5f4717afb53474cda7193a472be
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:39 +0000

    better quick left artificial possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
