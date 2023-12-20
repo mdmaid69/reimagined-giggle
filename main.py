16276095b209f5230d8d2043e8f4864336a1f357
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:25 +0000

    invite right bag return treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
