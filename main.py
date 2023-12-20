a9ade9ed57248a3ffaa3ca0e18d16246006d702d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:55 +0000

    turn future friend development carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
