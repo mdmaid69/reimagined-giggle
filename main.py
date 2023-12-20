ce1d488be1aba84c358cfe5d96fe85cbab2c3ab6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:11 +0000

    because temperature shout motion shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
