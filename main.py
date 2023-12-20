67f9b541ebc988211c9a2ac2823307d3855837f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:06 +0000

    share somebody full thinkthrough circumstance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_mode(file_name):
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
