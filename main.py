b4e4e2197ac7390e211d0232877cb6111fef5f25
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:51 +0000

    race approval everybody waste payment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
