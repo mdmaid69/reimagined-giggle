28d751f853ac96cd9727cb58dd09de0c38b5484b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:59 +0000

    below rise excellent study now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
