bbbe1813b222e5c4cd7b6a8e6e50bba70305815a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:51:08 +0000

    recommend rain left count proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
