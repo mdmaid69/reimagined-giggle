fa2d9a2116ce58c04a0f7f3370b609ff80758c79
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:02 +0000

    exercise happen clearly last quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
