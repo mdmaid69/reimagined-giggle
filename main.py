2b204ede482c23fdfaf8082e34f33c0f060cfe4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:52:27 +0000

    advantage nice direction must married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
