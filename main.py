10bb663c6229a788b78109eb7cb01d63752b16a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:13 +0000

    problem regularly tight bright worth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
