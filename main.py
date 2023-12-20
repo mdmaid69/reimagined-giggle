68dcd581d63b7d9254d58026ed33d9cce937f8f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:14 +0000

    bus scrape half check collect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
