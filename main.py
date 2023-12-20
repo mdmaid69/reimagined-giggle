2362d0185e4f888b471ec3153d2a79f3e92d4f37
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:42 +0000

    load takeout replace their doubt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def find_max(numbers):
+        return max(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
