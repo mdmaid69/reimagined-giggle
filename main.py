37cfef072f3eaa9ccdd2983f54d0aba967866316
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:20 +0000

    congratulate tour discovery crowded turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
