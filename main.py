50e2ae8def22e8aeaf45e2cd27e4af732b510ba8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:02 +0000

    any mind mean contrast image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def subtract_numbers(x, y):
+        return x - y
