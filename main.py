604d4871c16d9317ebb3bf431509df09401d255b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:46 +0000

    fall delight loose safe Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
