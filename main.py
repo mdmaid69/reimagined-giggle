3172af5c9dcb7858f0d270cd73074cde109380f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:07 +0000

    comment container harm like trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
