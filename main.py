665c41f61219ccb2db34af87fea10d71fc3c31d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:11:56 +0000

    old take letter mouth nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
