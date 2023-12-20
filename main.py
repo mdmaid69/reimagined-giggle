9d5e54d85a17265128d2115d162da6a031d84b8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:27 +0000

    think majority here work situation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
