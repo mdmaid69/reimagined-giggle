1950bad258d99bf1738dc33a37144820bdc696b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:49 +0000

    tap if piece ours perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
