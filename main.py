335e6bb59a6079ac1a82786fc13d533acef8c217
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:39 +0000

    knock stupid part conduct before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
