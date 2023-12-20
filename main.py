7a1c36f5126b47b40d93f89c4750d3bfe981dbd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:20 +0000

    smile fence compete fault giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
