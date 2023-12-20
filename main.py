07d777a87dc788b510f3da2ebde92c20d3bbd082
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:29 +0000

    rough substance conduct Ll 179bite

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_pressure(force, area):
+        return force / area
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
