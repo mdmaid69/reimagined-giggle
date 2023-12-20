cfcd15a2015cee04886ab281ca53464874a29218
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:34 +0000

    pleasure pleased will sticky lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_sign(x):
+        return math.copysign(1, x)
