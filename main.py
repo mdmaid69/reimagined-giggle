f797e9e9a1e602f109d045ad0cedb2424f68a406
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:03 +0000

    miss poison in shade continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
