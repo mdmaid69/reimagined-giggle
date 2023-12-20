2186452766c3d714cf8e657aca6c57a03bb47d91
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:00:23 +0000

    who tap better clear transparent

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
