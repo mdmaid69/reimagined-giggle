8e2e2358a760c4ed872ca52067cb2ca8259badfe
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:23 +0000

    children answer reject neck pump

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
