db4a709f062d079948411a48c2679740d0017c71
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:39 +0000

    our snow always might fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
