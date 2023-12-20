00799e68c0a9bc6a9d2987905656688d1d932ae8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:48 +0000

    claim marry with her check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
