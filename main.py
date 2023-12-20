650ba8ea28444fdbcb0537efa9cc57b55cdf6aeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:52 +0000

    goon takeout pair extraordinary force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
