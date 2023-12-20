fe43a1425c738047a79424474a7ea5ec257d17c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:52 +0000

    sink card ordinary move sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
