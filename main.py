635e4f076d15f904869ed07b518d1747c93fe875
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:20:33 +0000

    through subject interview smell obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
