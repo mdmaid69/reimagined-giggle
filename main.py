49b12c7ee56ea479ba93477d20880f9e254ff396
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:47 +0000

    spot main unless laugh demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_work(force, distance):
+        return force * distance
+def convert_to_octal(n):
+        return oct(n)
