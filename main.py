e0045e0907c828416581408d9daf79c4edcde296
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:22 +0000

    regret write contain keepaway day

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
