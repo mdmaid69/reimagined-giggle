d72449ce086f0c113d30182388ea2b9df3784447
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:47:30 +0000

    party person only exactly machine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
