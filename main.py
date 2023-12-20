0d403c49362870a29f537d3e40e35dfb9eea70b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:23 +0000

    rubbish issue hard all Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
