48b3574cefc2819b1241b75131c33dccb35751de
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:13 +0000

    change save fetch impressed nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
