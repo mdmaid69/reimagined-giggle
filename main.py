df2efd646aaeac7ab0d27c6de83a9ed6bafebede
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:52 +0000

    split language safety march comedown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
