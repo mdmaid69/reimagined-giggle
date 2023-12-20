8b3e089c0805324ebe26d31609a76350386122dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:43 +0000

    better entry by lookaround mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
