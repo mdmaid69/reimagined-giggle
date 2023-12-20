8c0987904febe57d8c6bba96b9871de7d1e40a1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:34 +0000

    stupid easy old seem after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
