706ad32b07f90ec0dbb93c03fa923fac1996b26f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:32 +0000

    difficult sail certainly no little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def cube_number(x):
-        return x**3
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
