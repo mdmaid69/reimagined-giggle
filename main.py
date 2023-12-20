a4cb6e07a9f0a15ecc068b73f9b758a98e80331a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:10 +0000

    lock walk scream row ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
