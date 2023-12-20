dcac6dd9a4efef0cc8c64bdc3ae7bba51fed6a86
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:04 +0000

    even cut square flat instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
