7eddefdbe8779a01a58d6a4a6357b35a6c077255
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:51 +0000

    value married certain beneath use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
