56492e8015b12107fa731ab93123627376eee567
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:25 +0000

    rank design long afford authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
