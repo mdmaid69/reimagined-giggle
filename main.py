87faa8480a56db16fe2b6709b131f79be77c5a15
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:00 +0000

    cause anything choice tragedy polite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
