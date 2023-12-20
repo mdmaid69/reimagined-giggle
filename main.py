9abef8dfbc2f5e14f8a2f2e69c210878bea4f067
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:29 +0000

    pay task major collection emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
