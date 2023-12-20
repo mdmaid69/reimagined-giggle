5829a3bf9b2e642857aab74c46d7791331cc0696
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:40 +0000

    he keen become dangerous flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
