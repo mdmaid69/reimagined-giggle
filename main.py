5012b6075d6839c7b23685df8749a0cc44fda9e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:58:41 +0000

    raw head colour less why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def add_numbers(a, b):
+        return a + b
