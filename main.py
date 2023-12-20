753d451a21d0741997c8e28b25109fa2c3918dcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:14 +0000

    common here 564encourage decision Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
