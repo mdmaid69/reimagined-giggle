77e7f88d2483ceedb9b061df1542c7823488a679
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:04:29 +0000

    rubbish just rule catch meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_work(force, distance):
+        return force * distance
