56cfdbf0c346fe1cdcef73d08d0045a685f49fd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:34 +0000

    education love depend nor organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_min(numbers):
-        return min(numbers)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
