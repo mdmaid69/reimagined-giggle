9a89ff5b7b68a77e92f207bed98610fdf1920ae6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:14:20 +0000

    turnover rich 567enemy unfortunate include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
