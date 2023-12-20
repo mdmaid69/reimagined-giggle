bda4eff3886848126154200090d743b66602b1da
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:08 +0000

    world crowd no time afford

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
