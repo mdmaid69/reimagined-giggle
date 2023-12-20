9c357e6232a7d4b896a05d71f860991676c8f700
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:58:51 +0000

    thin or sign continue surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
