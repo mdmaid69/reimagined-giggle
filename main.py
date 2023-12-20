95f69024e7474e66a17598fbf7c81c372c2c5d82
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:34 +0000

    target disapprove before knowledge still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_tangent(x):
+        return math.tan(x)
