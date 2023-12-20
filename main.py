c7c48af2136b111000fd44cadfc727f78819d6fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:06 +0000

    tidy to correction away recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
