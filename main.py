b3f473900cd7abb76d36dcd80276f213cf976975
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:30 +0000

    else common near sit bound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def square_number(x):
+        return x**2
