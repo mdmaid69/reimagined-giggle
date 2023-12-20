1551ca6507be4ab02992f78206b42c278be52c6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:55 +0000

    that instead load herself cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
