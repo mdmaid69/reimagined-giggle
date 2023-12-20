9947eb115208caaea193179ebc954a65392fdffe
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:42:15 +0000

    bank share order measure song

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
+  def cube_number(x):
+        return x**3
