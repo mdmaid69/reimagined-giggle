0c19c5281ad514f9e2ec5a976612caf07ece5870
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:48 +0000

    prefer glass rule reason rid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
