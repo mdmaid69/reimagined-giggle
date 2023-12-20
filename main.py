da20072cc8470361caa0c90831a2e9105c2a227a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:52:10 +0000

    position light start hurry police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
