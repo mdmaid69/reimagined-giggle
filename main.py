395bd598ee84dda58862f893f576e155d17fe50f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:43 +0000

    opposite successful keepaway ground natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
