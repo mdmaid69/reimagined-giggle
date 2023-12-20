f3d68078e2cd0c06da60eb7d348f418c2c57d943
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:01:22 +0000

    list hear track find up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
