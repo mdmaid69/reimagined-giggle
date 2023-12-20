d7367c12862134e5a5edb802c851f56d6a76e2dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:16:41 +0000

    keepoff plastic weather centre around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
