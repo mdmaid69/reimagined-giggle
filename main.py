599449747c81df210c6e25289abb14293fefecd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:09 +0000

    all always block fire general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+  def calculate_area_rectangle(l, w):
+        return l * w
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
