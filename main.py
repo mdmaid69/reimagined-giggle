e824aedfb55ac39dbc2e6fef964f5a2f44ccc174
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:51 +0000

    comment example several taste fear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
