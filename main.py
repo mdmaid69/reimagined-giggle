9f89fcc30b26225bc232e3c09cdf9608d1812499
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:25 +0000

    keepaway shut hold car correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
