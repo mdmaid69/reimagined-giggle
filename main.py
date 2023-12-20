38e394d5e596bdae5412a363cb9b9fa05b8fec81
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:32 +0000

    recovery already pride geton rush

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
