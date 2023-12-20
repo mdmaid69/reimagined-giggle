2122300cb8253717e000f1cd5ad3bc110c8cdcbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:32 +0000

    accidentally better bet reach public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
