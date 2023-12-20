88acfcccf3e19a83258b07d5fa06c0ac68f583d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:59 +0000

    rare refuse mind sore talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
