b5b494cd4b374b4f7b0734ae4dc19ffe6b14d363
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:38 +0000

    welcome unit open cruel listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
