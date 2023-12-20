3dd41b9aade25d83d30311d39805b21ad0b6feef
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:27 +0000

    ought hair runover burn arrange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def reverse_list(lst):
+        return lst[::-1]
