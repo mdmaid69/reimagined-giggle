c0cc5d10342373b6c4b8bedc1942193e7fd3189c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:43 +0000

    party middle goodbye laugh protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
