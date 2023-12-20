f6248eff14635eb3d43acbebc6a2c3880957f64d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:57 +0000

    class sure backwards bury border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_ceiling(x):
+        return math.ceil(x)
