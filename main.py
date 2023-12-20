65e91a230d5aaae4dd145377f4be7d19dd8c99ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:55 +0000

    money discover somebody too body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
