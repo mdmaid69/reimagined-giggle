90cf1b8fd9abd324915aa77c2bac8a458221bd87
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:05 +0000

    boat rid circle Kk entrance

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
