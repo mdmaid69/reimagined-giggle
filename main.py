53a5368f0cbb5cefa14ecf8c706f6961cc574a41
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:35:38 +0000

    combine serious consider birthday type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
