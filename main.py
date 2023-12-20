20913afcb58b55af19e59a15c7c7141dbafb8d72
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:12 +0000

    need problem split cut concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import collections
-def create_counter():
-        return collections.Counter()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
