95a1d20dc36a0eeefec6a83a126df836277be89e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:25 +0000

    practice secret ignore least either

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-print(math.pi)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
