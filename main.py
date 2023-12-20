53fd15442f5b0b11e8b1152a91526510a6cfd593
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:20:08 +0000

    disagree listenfor always practical school

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
