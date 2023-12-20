f541a43f987a697dd57586f13ea121cc636971c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:32 +0000

    contain often bringback make measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
