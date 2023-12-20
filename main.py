a3f916e1a56ab9dd03a0c1e696f1c912ea50cdfd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:55:09 +0000

    surprised deliver scream dare grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
