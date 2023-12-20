c8284b01029f72a562e090323a22f5c64d4fe80f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:24 +0000

    union fasten file upset impression

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
