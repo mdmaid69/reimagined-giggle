5e897ce14b33d9d511ee95e9915010e65bd299ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:30:31 +0000

    nurse exit theirs wave market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
