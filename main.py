aa3fd9fa702b0fe4591fe88154559eec9668b6ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:32 +0000

    sail callout tool fence 571enough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
