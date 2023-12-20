418a7bb3890ef6a29599d2c527a5b0f89afe8812
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:02:02 +0000

    exit light unfair instant this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
