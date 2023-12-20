be607fc1166718e2ca39e54c185e7ca7326052f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:48 +0000

    storm although sing tall attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
