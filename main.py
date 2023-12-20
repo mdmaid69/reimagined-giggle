195f11b7c35e7817a486400dbc56211945a96d61
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:26 +0000

    among like back description closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
