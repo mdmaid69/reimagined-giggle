2c68c319356b80ecf476cd345295840ea808b729
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:07:33 +0000

    beside underneath valuable emphasis approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
