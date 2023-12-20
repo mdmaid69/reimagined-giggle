f8ae4fab9de9a0ead52e93ba264ae79420a2f578
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:08 +0000

    neither schedule run splash slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
