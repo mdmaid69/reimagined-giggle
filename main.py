60736bbb27a3d1da2eba75c94d2d780c0e361a17
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:34:47 +0000

    herself fall case year train

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
