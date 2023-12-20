c9a01ccb9b7f2b5266318f6df0b615d239c2bf20
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:57 +0000

    mean actual wave bottom nature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
