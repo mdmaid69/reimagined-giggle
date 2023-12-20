a3c949704e936f5ffe3cc4da44b26ddc742c9b59
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:32 +0000

    count towards length Tt bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
