16e2c4b7021f7cf98fffd44f778dbb6a4827ad92
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:05 +0000

    cap bag time violent description

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
