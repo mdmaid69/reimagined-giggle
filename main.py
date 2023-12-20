fbb2eb561c8980c0df84a855cf083bccd88d8786
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:00 +0000

    of action address harm vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
