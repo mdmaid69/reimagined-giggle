303d1bd93c77e3e17c5dbb8be7979f6215f82289
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:22:21 +0000

    keepout stupid introduction connected delight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
