b3e38e13104eac7825ca283115f2236a7423ea87
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:48:06 +0000

    behind quality jump possible grand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
