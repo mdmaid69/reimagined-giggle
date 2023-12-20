e9ab278287fa0724b5605899c331a21f538f4e1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:59 +0000

    trust desperate breakout probably noisy

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
