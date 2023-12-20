997febe0da80b28d3581a9ff85504d2d5f91ce40
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:50 +0000

    crisis even gap comeoff her

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
