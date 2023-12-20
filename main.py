9550b748c554caeb05eb23a5b0b810603f37a3a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:30 +0000

    with takeaway education design top

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
