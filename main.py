2ed8e54d998e39c995082f0d2768267150abae59
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:56 +0000

    noone satisfied much dance who

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_list(array):
+        return list(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
