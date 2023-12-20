b4fa627f9b5200d98ddaabbfac86e7d66b02232f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:43 +0000

    heavy crazy comeover too control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
