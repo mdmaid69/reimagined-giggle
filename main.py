21d3470f2cc9091509aad2dcaa57e3366604ab3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:22 +0000

    win scare hair crisis cheque

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
