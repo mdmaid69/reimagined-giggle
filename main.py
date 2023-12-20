91eb6e2c87850fc35600027b82bfbf38ba172d5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:27 +0000

    leave decision thinkof get create

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
