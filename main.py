d2d8f216065507fc9292b85febbc62e034f46f9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:35 +0000

    comeup age word bound environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
