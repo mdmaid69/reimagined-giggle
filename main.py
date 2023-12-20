ead3ecdb4d33b151240c55f24b682b24745d9695
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:44 +0000

    plant nonsense public cross travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
