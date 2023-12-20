d77b560b519d5feec6e43a328f700b136db6aafb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:09 +0000

    small switch complain smell drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
