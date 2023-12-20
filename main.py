2b0c315425f1e42270f2a35c5d53cf5e85a0b8bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:36:41 +0000

    cover rain price duty step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_typecode(array):
+        return array.typecode
+def find_min(lst):
+        return min(lst)
