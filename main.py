ee7c712494a506d8e6f72db683185f5231632d6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:28 +0000

    tax art tongue clever band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def multiply_numbers(x, y):
+        return x * y
