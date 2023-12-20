7a007b85b5974620eba7f3a1b1cca8312208afe5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:31 +0000

    sure persuade carry form knowledge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
