6997e1af6741a6bb120d1e1c6201da8a5f427230
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:42 +0000

    perform organize concerning collection hour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
