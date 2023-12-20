e717320973ddfad8bc39df227bdcd486c76e8cb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:30 +0000

    rub train except shall certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
