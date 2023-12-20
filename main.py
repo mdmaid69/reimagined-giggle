f6ae9cc9767b658ad9a85a3423a5e9fb034134ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:55 +0000

    cross expression fault century visit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
