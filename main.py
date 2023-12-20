68edcfbb6d2f166e7d6d5a7c46508daac277c7ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:21 +0000

    cover shout sometimes die pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
