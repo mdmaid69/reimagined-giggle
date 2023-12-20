93fd20e6c74de018bdf6eb5f1e5aa85c6a7df998
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:15 +0000

    miss light pronounce break more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
