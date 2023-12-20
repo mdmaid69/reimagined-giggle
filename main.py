3d7dc544baa197e4fbab26e3b9f82ffe09b1e99a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:22 +0000

    legal join float judge belong

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
