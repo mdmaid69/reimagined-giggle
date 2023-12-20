6f2b88c7e89447f0c7e0c9e015cd7d99831bc86e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:31 +0000

    letter Oo rent angry cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
