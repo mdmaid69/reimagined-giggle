e88e68593164209a4216afcc4eb14c8d4171f46a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:11 +0000

    wherever holdon I sport occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
