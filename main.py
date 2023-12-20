3b7981563a2e9e9b9bbe779e305915baf581688f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:37 +0000

    change deliberate official home swimming

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-print(os.getcwd())
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
