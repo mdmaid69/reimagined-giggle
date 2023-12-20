0a97dbbc5457101cb433a9ac8b00ff6d5ef74cd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:22:51 +0000

    object spot office willing breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
