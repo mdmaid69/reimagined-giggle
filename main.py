cfeea3da0a897b354355d88af5d86884c4899529
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:27 +0000

    afraid exit table crisis wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
