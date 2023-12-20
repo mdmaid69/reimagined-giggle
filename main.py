eb9faf23d50bcbc68a1bd79fbe7eea5dd4e9a863
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:13:19 +0000

    kind hot chin back remain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
