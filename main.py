e04fe80cd33627a0109ca661e28b576a7279f06d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:25 +0000

    main official team suit disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
