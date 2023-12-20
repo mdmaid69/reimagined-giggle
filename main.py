c0ce4da46c3d01bd8f095164d02f08c0d36d7fff
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:16 +0000

    mine shout turndown out right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
