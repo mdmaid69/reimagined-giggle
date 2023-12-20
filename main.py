9d40cf67a1ac93a9349efb2d37538c01d75e7f34
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:03 +0000

    blue dream Pp accommodation plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
