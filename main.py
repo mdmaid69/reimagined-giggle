1f0673ec76a4531ddc50b68394def8ae600254e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:28:19 +0000

    present of organization freeze bite

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
