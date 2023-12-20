08fe669b71990fb4eab6be9f5dd614f259f0e128
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:15 +0000

    together rush whatever threaten comeoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
