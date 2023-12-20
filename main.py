24a52ca41ecfcdc1613f7c9dbe8becf7d86bb046
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:37 +0000

    single post suppose divide into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
