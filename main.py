3d38dd6e7ec4a0ced28d426ae97aa9a935c9a979
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:19 +0000

    sensitive kill smart transport crime

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
