441bfb58a03902da44fcd017b5aee9b042ec630b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:28 +0000

    underneath patience somehow platform anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
