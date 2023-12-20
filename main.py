f3d2be7e82e74ee7ccc8c284fb6397a8a038f5d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:36 +0000

    know preparation all confusing smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
