683621fe10c5b786a65c1d538890f67ebf812d58
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:45 +0000

    nor process ground tape broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
