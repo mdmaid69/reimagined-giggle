b5c34a7656f0f795aec48809382aa61a6289f750
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:00:49 +0000

    sail care young tend attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
