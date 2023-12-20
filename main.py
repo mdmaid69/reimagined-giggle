f88fed0406c01f2367d90d69d0bbbfba994c5b09
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:42 +0000

    move sail extraordinary hurry store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
