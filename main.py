8d3c71c77b3ffa6f0b9533f475d21dbc5ab62c9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:12 +0000

    exist clearly goout comfort cheek

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
