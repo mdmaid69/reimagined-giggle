ef9c0e9f6f3dc96687e041cef663c0949dea8c6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:21 +0000

    address pair conduct there bit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
