9c975e123d8fccbfb95338b012cc0de8e4ee5823
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:34 +0000

    occasional stranger refuse no head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
