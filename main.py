7e895f95dc558ef99710aa48cba33fc8891c2304
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:16 +0000

    cut laugh freedom question clearly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_octal(n):
-        return oct(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
