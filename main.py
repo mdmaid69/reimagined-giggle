bb9ffe04c55c084ec4450ce7502376b1cfcbcca0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:49 +0000

    schedule 572enter form travel experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
