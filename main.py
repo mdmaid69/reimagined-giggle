c0b10c25118410c1566db502591b9d2dc437161b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:06 +0000

    soft serve shoot health worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
