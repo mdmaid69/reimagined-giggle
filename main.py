2a440f23e929e10a62d2ef7b17787d3cd73a10e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:49 +0000

    coat house minimum surprise half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
