21738a89cad19c155008b2abbade650d45b38251
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:37:33 +0000

    pressure communication breathe find blame

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
