c73dbd18d9ef45ddb6a0faaa0dc66bcd5b4d082a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:49 +0000

    thought comeout sick claim amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
