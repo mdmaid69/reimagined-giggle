a418f8e2b225043a370d1a5fd33c48b50e6a5733
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:25 +0000

    dangerous available interfere soil shelter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
