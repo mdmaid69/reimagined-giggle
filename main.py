b87af1b377065e52e79f010ff975bf0dc670d493
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:32:56 +0000

    brush attractive when interfere reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
