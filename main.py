10ee2ad671c2f674b5b6421c9f1240954f459019
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:31:37 +0000

    can giveback sight squeeze turnback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
