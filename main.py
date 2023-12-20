7b88d35f9da5554cbb7aaff2319589f2ea2b9560
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:21 +0000

    colour fear complete impressive branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
