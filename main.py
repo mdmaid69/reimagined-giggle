9ce526f72b2fb7618ce6f7d6ef56c9b6c0edf484
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:42 +0000

    fear advanced tense coat which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_base_name(path):
+        return os.path.basename(path)
