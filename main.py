2e01fc08f8df36866e9d3bb9ea2db88cd6c26084
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:25 +0000

    limit opposite fit problem city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
