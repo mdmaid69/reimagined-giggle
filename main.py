b47dce3adec6ab60a1d2d3daf82166322953f972
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:50 +0000

    ride Ll along watch pick

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
