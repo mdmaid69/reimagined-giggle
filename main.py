271731fe98de917fc083dc23eea713cfc3c3e335
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:38 +0000

    fence care close pollution grateful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
