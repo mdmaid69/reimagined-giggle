7d6d397c3712d7c959863ab8aa00bdfbf1f596f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:02:39 +0000

    pride waste opposite by opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
