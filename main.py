526961eaf9ec874fa1db1ef2a86e48497f452677
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:57 +0000

    handle 571enough window release print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
