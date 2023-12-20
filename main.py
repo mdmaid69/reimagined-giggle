61618da56adf6648fa62fe70a49c8db1459c4322
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:21 +0000

    flow morning lesson blame represent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import numpy as np
+print(np.array([1, 2, 3]))
+def convert_to_binary(n):
+        return bin(n)
