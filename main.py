cd7a543c2289a4b85aed16a59191fcc12d1fed7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:18 +0000

    judge unfortunate sign thirsty pity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
