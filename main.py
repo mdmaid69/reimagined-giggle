e6208870cad3aaf59c2cfce4a6759a0fed7d35bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:28 +0000

    lucky idea I necessary share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+print(sys.version)
