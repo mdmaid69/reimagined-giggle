abaf212f080180011ee320edaa7cb96fc527b48f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:00 +0000

    path proper money turnon by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
