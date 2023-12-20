59eaca0ddaf000728376133f44d93db45ceb13fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:55 +0000

    several definite urgent correct 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
