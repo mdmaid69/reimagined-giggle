07c02d4f26b3939003ba381a756f1e0f1ffca988
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:44 +0000

    land transparent whole environment fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
