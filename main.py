2ba2adaa3eac63e39b210c9bc4a958975bed95c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:42:44 +0000

    sink pin fair peaceful please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def reverse_string(s):
+        return s[::-1]
