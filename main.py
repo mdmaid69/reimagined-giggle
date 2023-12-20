c642f332b3ac6b67d9016a03012d9c91e5ca82e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:39 +0000

    dish holdout plan 573entertainment meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
