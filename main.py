fce2f60bbe6fc2cf803d109dc42a7468503a29b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:30 +0000

    spring turnback emphasis burst friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
