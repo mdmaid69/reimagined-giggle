e426a428d2a414c4f4c049c187c21324d1fa86cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:19 +0000

    gradually language contain uniform battery

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
