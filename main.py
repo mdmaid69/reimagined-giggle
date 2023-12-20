f3df6edc4e32e07a23e3a2c1932056e8d49f7c10
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:53 +0000

    reply worry water block influence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-name = "Python"
-print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
