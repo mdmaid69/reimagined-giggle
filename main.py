ef1be9e8871f0b3f05fad0d14807563c2759456f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:59 +0000

    basic reason sore equal close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
