3782836ef3746c1b2a61c36dec30212ed8635ac5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:08 +0000

    care hold brain city war

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
