8da473be6355ee35dd34a4f72d3e13e818a0b589
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:39:07 +0000

    571enough fault getup wheel bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
