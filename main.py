f9133a622e6f145b3ff4adc9da301acd9fa53de3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:39 +0000

    572enter congratulations after plan tax

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
