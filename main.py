174b9c6bfc9355ad4c2c7799d2bc2212cfba5df3
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:09 +0000

    prevent information defend shoot large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
