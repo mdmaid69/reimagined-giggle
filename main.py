87d4724942368e208f64cfef0d3cfdf1aae658f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:58 +0000

    hurry knowledge lonely block notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
