91e8a06617e33376d69be6ddc69e28ff47494587
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:27 +0000

    bye occasion lot tradition play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
