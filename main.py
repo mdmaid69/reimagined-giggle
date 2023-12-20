522e84a4b680a10a2f96aa0c6b5c7343cc571381
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:24 +0000

    foreign us clothes warning runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
