11a2061ec84dd30a06f9a291f5adc74d5c37dffb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:04 +0000

    Bb succeed everything storm fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
