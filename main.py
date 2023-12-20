5ff41882559f7e85b61bf0b860af017aecac3948
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:16:49 +0000

    breakfast generous apologizeapologise turninto battery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
