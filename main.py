ea1ea38124a3800abe8c827bd4fb52b3d39d486a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:51 +0000

    shake birth such star partner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
