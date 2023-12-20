5f9aff8d74609701e888f2339ac5322db6bd5857
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:52 +0000

    along callout thinkover serious pay

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
