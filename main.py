921da4c6e3a9e6a50f706f9f0552839d35fe64cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:55 +0000

    OKorokay burn describe conduct play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
