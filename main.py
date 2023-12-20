e58c1c2ad951fb7190d2d5ffac6eeb983465ecee
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:17 +0000

    mainly getoff regional crowd makeup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
