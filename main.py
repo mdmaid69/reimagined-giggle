cb9c687fbaf42da04c60e4caff131cb7aa9da336
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:21 +0000

    previous honest experience firm stranger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import os
-print(os.getcwd())
+def get_environment_variable(var):
+        return os.getenv(var)
