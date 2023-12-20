b1ef5c72e93d9de71fadb41598c7878d7562c1cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:01 +0000

    combination but change discuss everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def multiply_numbers(x, y):
+        return x * y
