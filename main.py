f74bf29939875640be97ad76b62ee13fdd354561
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:33 +0000

    broken matter make in fall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
