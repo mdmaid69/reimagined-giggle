1a813d72252e2108111b011459c6e04ce533c02d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:46 +0000

    emphasize wash bet design shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import collections
-def create_queue():
-        return collections.deque()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
