bd7924f51778db7377fddb06fcca67836a5ac6a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:47 +0000

    trouble set breakup stupid little

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
