fe6871c8ee6c2839d30763e80cde91618863633e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:27:14 +0000

    bend cheque entrance turnon test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
