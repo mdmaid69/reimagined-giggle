728655d5b96be69589b9ed81e659fb4125bdfe38
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:42 +0000

    major brain cinema confident beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
