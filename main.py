8623f63e05249ac302b3e4788c49b31f5d40476c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:54 +0000

    this provide plastic comeover minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
