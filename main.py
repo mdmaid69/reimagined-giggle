e4e04e805b5898d3a015d4cce99d9c3d3fb74cf4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:54 +0000

    runaway design must little term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
