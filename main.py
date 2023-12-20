7cac5b4c6666b92929589eb86421518241d53a7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:24 +0000

    pull recovery quick commit high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
