893e73235f86fd3e52fa17ffabeb48e0daaf4d5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:50:55 +0000

    accident object rush exam scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
