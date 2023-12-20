0e28a6396b9b267a07cc978bc3c45ae2d4531a93
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:21 +0000

    old detail scrap story wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
