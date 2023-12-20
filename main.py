2d8938cb9b672a6851fd4e97a6eb148982ef2426
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:12 +0000

    comein point work sleep hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
