55d8349a66b9c3b42148f38b3ff5dcd8b9c8bf36
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:43 +0000

    tooth slightly adjective building home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
