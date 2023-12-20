039a3819f00632bdf9718dd9a277d7cf6093187a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:34 +0000

    beat grateful wire behind change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
