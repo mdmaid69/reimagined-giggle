a19b52d62494ab3b9810ef470e3b5d26be4e91de
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:15 +0000

    sink occasion sentence somewhere tax

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def add_numbers(a, b):
+        return a + b
+def reverse_string(s):
+        return s[::-1]
