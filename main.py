6fb3a049a146db9073204a6589d4d0d8ee292802
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:42 +0000

    callback man valuable keepoff will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
