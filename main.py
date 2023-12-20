ae7aebe82d5c5fac820ec0c248d44ff4d9feafa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:43 +0000

    path pay judge talkoutof study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_set(array):
+        return set(array)
