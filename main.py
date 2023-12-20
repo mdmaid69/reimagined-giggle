56a230f40edac3d4751c855cee5fe911ec493b2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:45 +0000

    situation wave secret open apparently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
