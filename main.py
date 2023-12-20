e6f3af805217d054826126db40500350fd4d24a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:15 +0000

    concerned round belong language sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
