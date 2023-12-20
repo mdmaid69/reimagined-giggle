a566657ee11db4dbc8fcc7879281964a3175b80c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:28 +0000

    sore bridge cover capital card

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
