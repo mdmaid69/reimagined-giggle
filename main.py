354d1ecbcf331ab20666fadabbd746d4223d0894
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:40:00 +0000

    example fire patience getout might

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
