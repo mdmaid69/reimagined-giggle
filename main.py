90cad9aa4e0524a6c4d75d17a5529266d10d485e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:59 +0000

    issue string hurry convince custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
