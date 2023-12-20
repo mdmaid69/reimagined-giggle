df1da618827e09d3161b9f4a84a8d60d3cda0a05
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:12 +0000

    original keepout steal cruelty week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
