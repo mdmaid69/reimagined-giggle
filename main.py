e0765d9c731af83a37207724cf6de75026222923
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:25:22 +0000

    Dd spot reply church public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
