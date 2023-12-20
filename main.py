38334f6097e666883b3423586d20663f474506bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:26 +0000

    trick get tonight refer mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def is_odd(n):
+        return n % 2 != 0
