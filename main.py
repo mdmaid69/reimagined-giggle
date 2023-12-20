7a3469bdf3e2b37b466371419fdd462d0666ac96
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:32 +0000

    fit mention example clear shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
