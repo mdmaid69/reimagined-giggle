d50436953226690ef586df1dc528aca244db9df5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:04 +0000

    invitation perhaps convinced process none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
