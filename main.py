37d871e4612da669c9185312f891abfa3b0b59ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:36 +0000

    about 564encourage tradition closed system

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
