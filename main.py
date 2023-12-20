1c3bd983778a4d4483e9a343c1b42c89dbc57cec
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:33 +0000

    interview night hard except public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
