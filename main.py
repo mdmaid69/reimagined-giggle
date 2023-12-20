4ee1d9b922d13b1dd5fc667c0dc18891911e06a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:10:49 +0000

    cinema excellent either care wing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
 def calculate_arc_cosine(x):
         return math.acos(x)
