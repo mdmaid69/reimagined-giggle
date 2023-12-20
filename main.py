29132444328bb6d85ddc08879501d43471f83514
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:44 +0000

    blame basis comein view insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-print(math.pi)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_pressure(force, area):
+        return force / area
