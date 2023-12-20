0fd57b78fd98cc2167f2dcd866e64927b9aca075
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:04 +0000

    letter travel nor along bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
