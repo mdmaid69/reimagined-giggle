c13f3da7789c15b6d582ae12019495d7b29dca0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:49 +0000

    alone fortunately stick kind no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
