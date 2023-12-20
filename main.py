493a95afe2ee0bdeeedb50e0f6979c82229a8255
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:18 +0000

    pause approval appeal whole smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
