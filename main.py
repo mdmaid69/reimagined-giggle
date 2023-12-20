b897c2fdbbce2953a0cb1cc13ddfb140ae16fd73
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:22 +0000

    importance material region possibility whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
