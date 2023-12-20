6dd34e45bf2a2c49513d49b871d04d6ceac7e3c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:02 +0000

    remove train front number culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
