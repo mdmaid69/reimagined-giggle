a0e9027f0b2db91bca3fae2c284e7999aecb901f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:12:44 +0000

    mark notice steep note display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
