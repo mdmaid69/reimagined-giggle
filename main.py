094f9d89cb7afaed0c84b9fa514eeb385d53e0df
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:25:27 +0000

    deep order surface unkind wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
