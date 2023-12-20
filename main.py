c533e92d572076fa8fed5af19c4362edddd5a443
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:37 +0000

    statement fasten trust course giveup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
