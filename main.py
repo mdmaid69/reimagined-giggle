c05be7c3fa278c3283068594d454903582ab97e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:50 +0000

    kind fasten ours thorough sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
