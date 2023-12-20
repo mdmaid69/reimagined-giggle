20b3f38fbbefbc0d12dcc118c9e6d93d65f200ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:08 +0000

    fall return getoff he trouble

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def reverse_string(s):
+        return s[::-1]
