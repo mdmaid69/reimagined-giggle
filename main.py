0c3154c42da2aa50b4544941d29d4feaac32ba11
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:48 +0000

    temper glad sure read twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
