d8e2445c019efcf1654ba3befb4fb26e5dea44d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:42 +0000

    statement thin everything big load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
