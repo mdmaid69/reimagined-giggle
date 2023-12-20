2d3328ef9e9e6e9985431ceebb2cfa4db3525f5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:29 +0000

    owe plastic tie answer buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
