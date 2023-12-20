69e3e5bdaa8cd65733d45988eaf7f537c5881fb6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:22 +0000

    battery warm offence second correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
