f51468b9e7fb73f176936970e111e4711ea178d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:39 +0000

    depend everything damage outside lot

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_power(work, time):
+        return work / time
