47a6d0855df704457740dadcdd88aa6a8ecb1d76
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:03 +0000

    bear across realize such little

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_min(numbers):
+        return min(numbers)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
