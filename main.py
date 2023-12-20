36d3bcda8a89cb0a421a62cbebee717a6cbc7a97
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:55 +0000

    copy as recent badly concerned

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Characters:", len(text))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
