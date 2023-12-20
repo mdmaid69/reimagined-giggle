a8d4059f58f04176b3da01b5f25e62a1f7bc779d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:50 +0000

    too guess detail maintain old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
