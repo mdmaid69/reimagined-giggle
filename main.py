7b92bc6f2aa278c7307d463b504f494543c148c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:57 +0000

    appeal skin interview behave hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
