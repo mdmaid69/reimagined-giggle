431cfd51f114ccf7c9b62347ecd12ca5875acdfe
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:57 +0000

    between body skin description office

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
