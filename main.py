a746ad8c3310b04fdfa91e9d698cff7c21d27e27
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:52 +0000

    expression never put sticky use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
