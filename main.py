0aa7ee11210a78ce9f6b5f105a4c182d6565281c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:13:29 +0000

    drop main bang crowd vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_min(numbers):
+        return min(numbers)
