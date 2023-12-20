f7d2145695262f3a454ed7cb76c1c3662845b7dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:05:48 +0000

    base improvement old obvious nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def add_numbers(x, y):
+        return x + y
