94a51bcfb1cf58824310e4aaf23a16ec490a57a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:37 +0000

    string cause movement move approve

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import array
-def get_array_length(array):
-        return len(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def remove_duplicates(lst):
+        return list(set(lst))
