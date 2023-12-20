5a9eb385404eb0300aa3b885d83f9d5f66e5de00
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:10 +0000

    rub tidy dig window turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
