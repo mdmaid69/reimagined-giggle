4b4d79bc70db3eef3b2d9eb0011d6e173ad14371
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:57 +0000

    ball equal effort tomorrow whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
