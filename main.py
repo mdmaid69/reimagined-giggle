e0ba7e823b35f0501822fd677da7172906ef763b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:39 +0000

    account muscle world straight deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
