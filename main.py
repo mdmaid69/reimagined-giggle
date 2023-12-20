c8b370ca5c32f58ba5bda60777febe3ea0548281
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:41 +0000

    rest flow spring talkinto bet

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
