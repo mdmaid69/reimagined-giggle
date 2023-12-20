aef1f6b64ae8cdbd3acc9541c986fc460844ed2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:07 +0000

    control store lean top hello

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import tensorflow as tf
-print(tf.__version__)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
