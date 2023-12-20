0e239c8c8ade55df7853233ddc655d1b5602e3b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:43 +0000

    indeed occasion shop official extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
