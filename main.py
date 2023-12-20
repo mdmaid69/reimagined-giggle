8f446fad1ef7bb4aece8b6d19149af5575cac0e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:30 +0000

    feel behave Hh question meal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def add_numbers(x, y):
+        return x + y
