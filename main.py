2d7b045a5a0fd1063599a37324815e6162e82923
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:06 +0000

    equipment block over glad deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def add_numbers(x, y):
+        return x + y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
