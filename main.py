56a1e0bd63bfa9c1097b3d2d3a934ecb0941b468
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:30 +0000

    fit marry minority appear guilty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
