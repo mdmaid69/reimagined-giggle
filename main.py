6d2d369849944f0e4114d46c82abc69a30f4d45a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:26:58 +0000

    worse interesting strict shadow stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
