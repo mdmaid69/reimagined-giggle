f56c6a287e8fa12c97c53f5993e62bbe1dd1975d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:44 +0000

    book autumn joke ability allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
