55ad1f7cbed913e814b436155afcd2430b04f0c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:01 +0000

    suck average turnaway except meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
