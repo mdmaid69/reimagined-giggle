07ab65dd8c121b77e90307d2ea77c17743f798f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:18 +0000

    punishment gate among getup into

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def count_words(sentence):
+        return len(sentence.split())
