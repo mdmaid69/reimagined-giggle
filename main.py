f1001fac9393a1dae0a9cb4b04ce9625d586bef7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:32 +0000

    group piece mouth connection century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
