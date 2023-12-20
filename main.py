74afcce2d1b5bae9df2f7e7b8a474e022e657c1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:11 +0000

    display always lucky several spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def subtract_numbers(x, y):
+        return x - y
