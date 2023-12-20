fbbde471d9880041519c2eb29001ba093baf18b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:17 +0000

    read accordingto actual rub scrape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
