3f846d1b40bb181e693ad6f078b4069674957f3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:59 +0000

    tread child art hurry explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
