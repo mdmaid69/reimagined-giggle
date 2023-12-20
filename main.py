8ee53fbbe9a4236ff740dd5152bd461f5e0194f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:21 +0000

    bank protest steady forgive might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
