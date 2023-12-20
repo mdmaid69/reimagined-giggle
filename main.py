494410061e5b1c6d2a753b9f143f21958b7088c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:02 +0000

    about approval wind translate direct

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+for i in range(10): print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
