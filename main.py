8a46d6b1d7aaedd8c76adf956b59f073b19717fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:15 +0000

    value lean difference obvious maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
