14e0154a43a0dbfb04d764e28a085d1383ec405a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:56 +0000

    schedule charge spite rise often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
