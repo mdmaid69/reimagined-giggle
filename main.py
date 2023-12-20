523c2f30a0490a5cf50d70fb4418c5634b3374bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:28 +0000

    somehow afford matter nose giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_octal(n):
+        return oct(n)
