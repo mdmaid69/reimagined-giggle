db6b59abc7d553602bed958ca46a98411723745e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:43 +0000

    influence scale accidentally weigh break

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_circle(r):
+        return 3.14 * r**2
