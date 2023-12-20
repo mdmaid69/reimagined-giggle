fe682fcd439641365a91122485c9646705c97066
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:59 +0000

    attach risk giveout crowded receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
