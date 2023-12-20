33165c1ba1cd2b06f70d1c3fd91aef21079b33a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:31:49 +0000

    shallow classroom action finally part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
