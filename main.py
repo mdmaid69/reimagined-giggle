26e664f73e3f3a22b379446f04b6ccc507ab357e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:30 +0000

    but colour straight you world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
