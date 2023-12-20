789aec875140a26530b07ff09f4dc5b237b59586
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:09 +0000

    almost collect centre promise especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
