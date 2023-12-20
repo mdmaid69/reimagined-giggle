e761a0b5cd1d21fd1da368cbcada681960e48bc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:10 +0000

    approve programme unlikely natural protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
