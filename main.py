6878e616fe13407aa2ac2b3772f4e639904acd16
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:14 +0000

    ball left object fast smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_string_from_array(array):
+        return array.tobytes()
