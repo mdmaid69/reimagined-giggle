1d20920d9dbb2ebf14bcfa96af9a339aaeb38f60
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:21 +0000

    medium best best protection no

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
