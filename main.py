5447a2a74a4c5a185b403e0a7ec2afb0ec8c27f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:27 +0000

    getoff wing brief shine turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
