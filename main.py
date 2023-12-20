141f51f3ebc6d8c8e5c2e52bf479bafe2085aafd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:11 +0000

    hope note ought close entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
