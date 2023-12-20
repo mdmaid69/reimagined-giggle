1c1f301753f274cec0b8ee4d75cfe9a6754d77be
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:07 +0000

    phone hurt date violent female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+name = "Python"
+print("Hello,", name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
