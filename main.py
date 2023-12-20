6a7ae9f1ace15bf028528b01a166787b1cfeeb7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:35 +0000

    sweep stamp rest children less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
