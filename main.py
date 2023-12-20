3a5698c755f2e7a02ea3c75f452ad6fce4c345be
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:22 +0000

    shall keen port star now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
