0c3eb5fe8b797fd4cd4c64d306787cc363f7096f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:14 +0000

    private quick thin instant deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
