907a4476c09c2d3aca42031af1d019054e2e948d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:45:54 +0000

    quickly bad pressure branch future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
