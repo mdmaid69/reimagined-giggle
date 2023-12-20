445bd1378ee141a522c9ec54bf4f4a2e66f8f31e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:32 +0000

    top disturb precise exaggerate degree

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_hex(n):
-        return hex(n)
+print("Hello, world!")
+def reverse_list(lst):
+        return lst[::-1]
