11f8745b892a02dc65e7d2fe4fd5b8a38a54b983
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:38 +0000

    early finish flight book suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
