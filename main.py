8daa16364ce9754d6c1e6303b26f6c9373a7622a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:55:57 +0000

    protest offence song yet indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
