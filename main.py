9c528a5eb12af80125ba805398f68523e6849992
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:04 +0000

    friendly pipe mouth pollution win

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
