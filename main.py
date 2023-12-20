2cb564c8a05a9f9efbf1cfce10aa7902bef85446
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:09 +0000

    myself thought injury at mixture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import platform
+def get_python_version():
+        return platform.python_version()
+import tensorflow as tf
+print(tf.__version__)
