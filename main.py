7ce02db84ee95849198ee53a799917aba473b5b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:56 +0000

    note always skill black tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
