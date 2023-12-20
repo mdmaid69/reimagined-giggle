f9ba1be7ff552bd83d4cc48c7220aee75b0d84f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:15 +0000

    blame corner recovery embarrassing tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
