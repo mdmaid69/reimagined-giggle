698b2f16a8389167f518aceb7cc862c270c4ad1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:41 +0000

    sink radio sometimes steer impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
