1764cfa49579e3c6965853183e780c01eacc604c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:58 +0000

    table apply simple advanced more

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
