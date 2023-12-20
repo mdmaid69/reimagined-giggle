b7b5e5901287a5207a8ffee973dd5f99dfaa0332
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:20 +0000

    man now floor my tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
