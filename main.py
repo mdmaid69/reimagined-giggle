c9b0a5997197449e5015ddd906648fde09799f66
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:01 +0000

    why passenger ilovedicks party meal

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
