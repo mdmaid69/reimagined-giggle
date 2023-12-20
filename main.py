5ab2f745decf9f1cfe723909ad59d993c54b5cc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:30 +0000

    watch follow develop sea ever

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
