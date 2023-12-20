ffb6c7157135bda83835577b12e78803ea9ef3a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:44 +0000

    weak runout store describe ever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
