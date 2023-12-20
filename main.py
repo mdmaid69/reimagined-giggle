d621e1dd3d8c2de1dc9eba5cd8cbdd0e6466e1b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:56 +0000

    past comfort kind exam complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
