cc15ba1bbdb564845647d4995b7b232be4462fad
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:53 +0000

    talkback makeup gather previous trap

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
