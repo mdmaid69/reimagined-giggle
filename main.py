0a55f9865eec9770da9d22e2d12654ec849a30f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:38 +0000

    pleasant schedule opposite road live

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
