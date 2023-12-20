b09f2e73dbd4507e97e1c1d02be44c734cdb1a9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:15 +0000

    bored choose piece impossible introduce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
