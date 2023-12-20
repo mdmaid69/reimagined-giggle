33f013648095e0edb7db53ba66166d3dec5732e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:39 +0000

    hospital Kk complain lookafter tie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
