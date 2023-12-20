f793c0bdfc821e9e3e26c234dbe2d40c5e4cafae
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:47 +0000

    famous deserve basis sore likely

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
