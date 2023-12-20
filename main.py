dbb7763f6a381e35e73dec7e6cd8e1456cafa001
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:46 +0000

    chance smell invitation nearly heart

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
