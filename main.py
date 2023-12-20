3f5ef2540b494c5679b408f9b7c3ecd01b1f0a5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:59:43 +0000

    complaint warm previous curious still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import random
+def roll_die():
+        return random.randint(1, 6)
