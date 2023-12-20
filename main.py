fab917a26a1795bad8666c3aa6a19aec71fdba8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:37 +0000

    worry still wash system hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_tuple(array):
+        return tuple(array)
