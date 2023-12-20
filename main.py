08c23e1704baf32b47c392f0453fe65f22ac88dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:46 +0000

    occasional original agree friend responsibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
