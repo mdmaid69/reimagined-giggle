7b09ab10772b5b7969f5c9a982569a139b41566b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:38 +0000

    get power train bye mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
