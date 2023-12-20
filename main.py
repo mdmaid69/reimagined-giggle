ee56378ec5d50db7bbc7d3e842270b060747d769
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:18 +0000

    invite design advice dry now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def convert_to_octal(n):
-        return oct(n)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
