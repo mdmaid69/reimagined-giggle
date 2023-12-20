33a3722e86a3fad6f584b979ef3cb6e765074b87
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:29:36 +0000

    gooff uponon suit spoil of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
