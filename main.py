8d103e419bca83557bb6db362e6e5b9d906bc45f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:56:52 +0000

    dress start city sudden objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
