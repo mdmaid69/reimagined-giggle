e17399b7491147f1107681c21dba67b5ba0ed2b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:33 +0000

    one type speak fast wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def convert_to_hex(n):
+        return hex(n)
