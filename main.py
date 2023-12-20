32bc77668880de9eac38308fe91277def4e7d9d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:45 +0000

    explain appeal agreement that come

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
