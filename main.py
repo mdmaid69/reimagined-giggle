1d8e81efd9f0566eb439eb15fbd11e266bf7d012
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:48 +0000

    section allow communication breakin crowd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
