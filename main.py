d824b20215d43f14bda3b758a870e6b1da905522
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:05:27 +0000

    communication black sport innocent neck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
