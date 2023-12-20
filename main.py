2e9e8e328cbdf7a504030be41ce803b7893da213
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:09 +0000

    grade appearance permission cut value

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
