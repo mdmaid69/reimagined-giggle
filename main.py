8b081ed16100bac3488dc8495cbd88ba00508960
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:08 +0000

    stage watch shake poor powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
