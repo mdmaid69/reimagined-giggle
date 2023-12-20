cd07c81694a12837fdfe11a30c41962bd195db83
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:29 +0000

    main wind name attract regularly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import random
-print(random.randint(0, 100))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
