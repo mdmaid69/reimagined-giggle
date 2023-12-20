d500367611ba76b7b0536bbf5312212f65c93ec6
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:54 +0000

    finally race high alive which

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
