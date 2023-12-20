d33b332ff6fac6e9ad025a1f0dd47ea5831549b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:54 +0000

    trousers actually knowledge wear feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
