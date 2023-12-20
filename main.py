595e6b72f7e49d3530a0bfd3f4b9cacffcb43088
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:08 +0000

    bridge relief us serve his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
