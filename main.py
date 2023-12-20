2e8f634aea2ae06e168f42bc2afd063757a5bc2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:22 +0000

    keep freeze mean arrange rubbish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
