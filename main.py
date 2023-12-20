121c404dca0e1e20eb12d5383a830b9eb29cc889
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:06 +0000

    tired sing record pump television

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(numbers):
+        return max(numbers)
