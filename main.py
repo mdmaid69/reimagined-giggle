ea511eac6b2d9ed0adf10e9629b8677901cc2185
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:20 +0000

    decide blow other broad least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
