8b99a2a9b86cd07f0a3904ca35dcceb856899569
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:52 +0000

    plan ring idea drawing force

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
