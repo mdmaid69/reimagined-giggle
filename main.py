9040dd036ee4f3d2ff6d35b121c313e57ffd67f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:55 +0000

    cover muscle whistle he season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
