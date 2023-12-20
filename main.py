642fb80f01c5fc561694c178cebccf04da4515c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:34 +0000

    boat skirt tough effect whichever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+name = "Python"
+print("Hello,", name)
