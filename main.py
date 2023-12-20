17b69ea54924e42920e674bc7031c7c936120e53
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:44 +0000

    our room refer business figure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def multiply_numbers(x, y):
+        return x * y
