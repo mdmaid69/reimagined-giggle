72a04906ddc3d5258e76f0988221308624c9954c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:40 +0000

    month neck deserve frequent relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
