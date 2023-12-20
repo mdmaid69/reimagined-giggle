10eb34cec78879a1b46494356f6be1c939bba5d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:00 +0000

    crime skin whenever punishment grade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
