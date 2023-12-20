e761c249d5320ab7bd60e51829fba9b9e38e77ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:30 +0000

    second call information cheque impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
