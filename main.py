597216bb641ddbc48bf0220b5e0d92186d7ff0a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:55 +0000

    date future contribute serious drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def exit_program():
+        sys.exit()
