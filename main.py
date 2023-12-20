cfa80e9c5c14c0b16ba6b9b871eb3dd3f96e4554
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:45:36 +0000

    left plenty each wrong switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
