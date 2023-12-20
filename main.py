f56cdef2365f2e7537eac9feb762ffc9e3221cd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:49 +0000

    whether beyond best scare uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
