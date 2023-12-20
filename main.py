c967cfceace278a749cc21f9e835f9bc157fa68a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:45 +0000

    hide protest tread hers conduct

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
