931cba5abc4074650f2e45246cc5a7d84adaa157
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:10:07 +0000

    tired our though neither upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
