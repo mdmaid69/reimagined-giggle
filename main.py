4763988125b0c3f54996a322e43e008dee79e7a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:26:14 +0000

    honest trap listenfor share free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+name = "Python"
+print("Hello,", name)
