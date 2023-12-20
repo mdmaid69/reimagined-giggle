1edbbf9700402f3dc40f2e1d1b7f71c3d9a41470
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:56 +0000

    join unhappy extra smart collection

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
