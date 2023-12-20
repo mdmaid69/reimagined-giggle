5c3e2350bcb2b83b9ea45e656476f84172782a2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:52:31 +0000

    near cry Ll mail crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
