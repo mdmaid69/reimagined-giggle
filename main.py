857bcb8fdb1e8208bd98da50587e56dc3455eb9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:26 +0000

    square someone improve by comeoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
