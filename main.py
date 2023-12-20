561e5aa611f080dca9b7d8e855e61bfa055ba78f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:37 +0000

    mend ever long collect air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
