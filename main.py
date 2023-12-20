baa2b7d3f0d7e43c492bbe0a8c91874ff2e31a25
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:16 +0000

    hurry likely precise influence quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
