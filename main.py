fbd3abc2dabf14c6e79a20df567288e4c7f40cc3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:56 +0000

    dress degree shop motion think

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def add_numbers(x, y):
+        return x + y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
