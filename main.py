7436469c3e4545d7262a27f985b90c8fe8d298a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:46 +0000

    part agreement congratulate quiet disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
