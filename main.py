1ed9df72ade8a51f06359f7ecd243ef155287db3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:15 +0000

    answer move importance force contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
