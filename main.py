849416bc08c97988dc432c5ac34ad2b5926106ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:58:11 +0000

    freedom butter remark lesson finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import time
+print(time.time())
