9ceae6d84ee7c90a9a4e4afda5ff5dd3ea21efaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:46:43 +0000

    cinema shine copy skin Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
