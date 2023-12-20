bde32e3408fd4841001929268ecccf943262233e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:23 +0000

    region disapprove so disgusting exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
