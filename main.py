a99312c93db86e3b138c7dbe1e74a28e5c8d7776
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:27 +0000

    giveback set whenever fail friend

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
