c2362c7e2189212c3696dbf446be323d16635cb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:54 +0000

    pen birthday worst before search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
