eedd83a7836fad7b9f3feec0dedb665f6b2bf4f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:40 +0000

    fill personal whisper definite them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
