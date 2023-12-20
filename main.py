117a7ae199bc96c60eef364a9ce23d4892ffeacd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:18 +0000

    basic spray backwards emphasis last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
