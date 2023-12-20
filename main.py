b266329d50664c5b16edaf6cfaabf3ebabbe1f7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:14:19 +0000

    near announce spot through quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
