d3b3503025aa4119f92c0485fc26eab6654c73f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:53 +0000

    condition cruelty appearance shadow off

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
