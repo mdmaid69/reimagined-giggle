560ee6d9364e0bb16cd71ecce50e6e67a3e55563
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:45:33 +0000

    tap scrap material choose breakdown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
