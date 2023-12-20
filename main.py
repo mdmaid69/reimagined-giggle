e66e1f7474c09f3d1d8ee93e255ecc988932d916
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:26 +0000

    message shame mind post branch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
