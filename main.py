8c09c576a93f3dffbab5dd7de0d3712b1525a93c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:37 +0000

    slip fat opponent cum manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
