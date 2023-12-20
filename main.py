66ccbede142fe49f8f2f1464e4eebc1793787fa7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:49 +0000

    sky part job keepaway run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
