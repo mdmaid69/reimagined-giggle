a423f3ad0a9647c9b7157e6c873c44e97167be7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:00 +0000

    cold it clearly serious neat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
