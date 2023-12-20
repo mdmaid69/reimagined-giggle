69d6e4cc88997021232421671e8923ae822d1f35
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:11 +0000

    shut never scared hair tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
