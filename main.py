f296bae635d49e365f3983d999479879a28184b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:01 +0000

    alive complete best takeover walk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def reverse_string(s):
+        return s[::-1]
