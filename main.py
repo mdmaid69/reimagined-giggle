2f6b8d95e6b5d28f337c17fc99e8e6b480573cdd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:54 +0000

    ignore heart involve centre pipe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import math
 def calculate_exponential(x):
         return math.exp(x)
