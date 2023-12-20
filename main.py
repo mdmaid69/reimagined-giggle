3b2324ab1ad2225fd24d901cc86293dfd25c49cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:19 +0000

    print glass identify offence content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
