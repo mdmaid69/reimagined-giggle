f8027222a16db9f51c20947d0906d9677e441731
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:00 +0000

    holiday any church formal satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
