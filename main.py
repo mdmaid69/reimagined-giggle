2c403aed9090e5c3826406db5e29b129baef2a0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:15 +0000

    summer big judge measure frightened

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
