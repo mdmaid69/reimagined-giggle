ad9a237e134f85bb7bc547792c0ab80bb2ae4ada
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:09 +0000

    blame swim tip satisfied fire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
