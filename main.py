b36b6b610ae06e9336d12965f388576feb1c2a41
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:12 +0000

    cause warn well hardly hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
