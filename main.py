1c68ed6627e679fe360448ae230641919e59895b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:23 +0000

    either contact cost fine adjective

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
