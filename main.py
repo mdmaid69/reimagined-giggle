0afd993571c8bc92115f41013ccb476325dcd56d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:45:26 +0000

    its admire work deal lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(lst):
+        return max(lst)
