5ef6a305b476f80c6e91c5c8c4f698aee51c4c82
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:07 +0000

    answer impatient clean sorry height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
