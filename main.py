bbab95dbf80591af45e6692ef42fe91ba3aefbe3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:31:13 +0000

    total Hh basic except mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
