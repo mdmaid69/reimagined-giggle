2d1c0992e606a188c80afe35df128b61410052f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:30 +0000

    179bite bound clever black moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
