37db99127b0c4d147d7204711292c4a38bd4b09d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:04 +0000

    false brief evidence borrow tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
