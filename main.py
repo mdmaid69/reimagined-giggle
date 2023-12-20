12acde9d152a11a687e096f43eb894295c2a6255
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:15 +0000

    deep sleep second deal measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
