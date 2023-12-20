a5cee35e22ed07aa7408f2cfd2a45e3fbf6d4020
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:44:09 +0000

    mine offer hot kind such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
+def convert_to_binary(n):
         return bin(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
