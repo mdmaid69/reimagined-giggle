9af985bd05f3d56fb3538041f6846c95e013b116
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:56 +0000

    need middle 572enter law day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
