74102d50bf6732c8c8e876151800db02047e150f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:05 +0000

    approve bye fat 566end dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
