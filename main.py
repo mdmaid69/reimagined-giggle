1db512cd673f487a9b996d684cd4f7c1f6d52ae1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:29 +0000

    contrast ask can join dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
