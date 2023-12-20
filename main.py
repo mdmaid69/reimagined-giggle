bddce4d1246592a26d59ec643a272f370b87f57a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:56 +0000

    well build sing boat future

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
