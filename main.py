02d42d1ea0792e09f37024eb4f22151de281d243
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:46 +0000

    wet our from OKorokay disappear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
