eccbb1686787306b7bfa9972ae52041b3d253915
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:41 +0000

    raise public hard otherwise fork

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def cube_number(x):
+        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
