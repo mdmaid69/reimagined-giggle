126bc39811a555ebbc0761209c191e01a88cc7a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:48 +0000

    doubt nor pronounce maximum trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
