2b02a34ff58f68d188b4cf217abac1e94b3f2de6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:11 +0000

    matter ask test giveback pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import collections
+def create_queue():
+        return collections.deque()
