004f8c7a4510afd1c19de764b31d6cd151c68e0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:08 +0000

    alone reject mess impatient set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_exponential(x):
+        return math.exp(x)
