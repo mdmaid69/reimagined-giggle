304137aecf50feef259e1834f7f284f090d4fd57
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:06 +0000

    miss story past refuse rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
