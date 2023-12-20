2d7cd028d938acb0d0a24944b1008d7ba6e885bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:22 +0000

    clue Yy shoot unfair discussion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
