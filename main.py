d8039fd80981d41bacb9cbd8de87446069269fea
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:38 +0000

    moon dangerous exit 179bite same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
