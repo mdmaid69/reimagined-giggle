bb28f0b0bbbf8cadef1c12457a54b0e0328d9f7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:28 +0000

    moon roof responsible between Gg

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
