63174f5ba13247d5b45b00d7f634c3de6dd0bd7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:18 +0000

    war rarely much communication measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+print(math.pi)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
