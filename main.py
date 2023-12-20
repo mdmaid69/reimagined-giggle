9d07dbc5fb47884ac9ad88caf53e4f8c2454d15b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:07 +0000

    unpleasant purpose serious doctor majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
