11e55c23679a7db1155bc7483ae1a956359d9592
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:51 +0000

    pronounce worth meet common across

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
