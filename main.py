f70b5eed3c33d878cb0fb223fc272f65bfee954d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:12 +0000

    back example bringdown warning adjective

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
