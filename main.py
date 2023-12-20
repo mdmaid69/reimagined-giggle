e250f76207b343dd76606a972efc7ed6a6c4b4a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:28 +0000

    silent load zero production otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
