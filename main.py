d9ed523b59c708fb59f3a8154541b4de82a3a4c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:53 +0000

    injury original pinch form us

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
