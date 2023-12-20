630779fd2a8df7ce13e83b8bec50250ef3bfeca9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:55 +0000

    practice shut condition declaration taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
