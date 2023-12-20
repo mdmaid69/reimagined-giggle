0aedda925a946776bf924c45330ad96522468390
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:55 +0000

    advantage early experiment sight pay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
