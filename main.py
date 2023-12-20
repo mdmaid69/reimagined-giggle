ecd6fb229c44f4c03a5c6f74f130105093e78726
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:44 +0000

    bringdown nigga play share runout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
