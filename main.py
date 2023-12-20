07d23da71be754e91e0b9a0d7d4944f466d7e6bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:50 +0000

    declaration damp central mass direct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
