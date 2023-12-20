6fe432f3c955b0340b7bd866fbe01c55d9785ca0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:23 +0000

    degree beautiful someone brain swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
