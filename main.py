5c7aa0d4787472def5ef5a5adf10c482ec86466a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:00 +0000

    aim sort calmly turn during

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
