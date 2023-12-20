48920aeca6504145057db55ff5d2936de3d7bbe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:16:43 +0000

    bread shake sensitive comefrom demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_max(numbers):
-        return max(numbers)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
