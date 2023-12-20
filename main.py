78fdb25bbbce7d13bff19d86b321afdfb221c289
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:07 +0000

    runinto tip distance set comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_density(mass, volume):
-        return mass / volume
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
