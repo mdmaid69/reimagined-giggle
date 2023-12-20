9883ee744b2a101773035a064237fe2acf8304de
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:35 +0000

    hungry fault suffer forgive immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
