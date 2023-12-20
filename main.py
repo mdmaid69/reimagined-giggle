7f104d947c64d82d46f983df6b728ec97bf9391c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:52 +0000

    little spread division worth spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+for i in range(5):
+        print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
