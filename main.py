dd3e31f04d99870a9cfc26314fb5655ff32ee419
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:26 +0000

    politics practise strong disagree disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
