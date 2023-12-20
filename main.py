e30d3f46c30db5b6b410302e996d1587345b5548
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:26 +0000

    bill pollution waste prize distant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
