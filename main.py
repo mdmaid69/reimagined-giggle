4b5cfa34cf8c5e92d7d122201f19cbcbca3c1404
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:56:24 +0000

    interview fortunately dare make accommodation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
