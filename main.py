4c5dc7ec7f7126497b69507a926c62c9ba81be91
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:19 +0000

    truth life live search meet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_work(force, distance):
-        return force * distance
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
