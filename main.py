3469058e067b3eaa476fb4b9e65c8cb7aab112cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:50 +0000

    recent plant container broad vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_slice(array, i, j):
+        return array[i:j]
