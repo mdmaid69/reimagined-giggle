43b260428280952dec5000ede0706bbb2efd2218
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:46 +0000

    kind tired increase rarely centre

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
