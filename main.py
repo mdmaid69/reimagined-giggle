3dd6832a4316629ea7055673b365f81c2de45f7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:42 +0000

    memory occasion leave soak noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
