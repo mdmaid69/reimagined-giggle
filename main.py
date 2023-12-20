b6e4e7382b5ed2a213ec4df7b08b0c7b2f0feab8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:02 +0000

    afternoon neither pleased priority study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
