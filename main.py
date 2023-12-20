13d170cd0f4b67073144a7f7653d729c7c2ce711
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:37 +0000

    surface prove smile unit read

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
