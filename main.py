0ff38f17857738786fff754a396e5ebb944693e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:26 +0000

    would beyond feed sweep pay

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
