3d13a8dfc59b6777321b5e8d9b85f24ddb36211f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:28 +0000

    probably expensive sink limit dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
