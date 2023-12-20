30b90530efdcde00c6d76365eb3e732267d20d79
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:52:02 +0000

    spoil sweep arrange start possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
