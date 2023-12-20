ccc6f203ed81bed8ae151b2d39f2ca49cb4dcaee
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:18 +0000

    gooff please brain lookback(on) below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
+def convert_to_hex(n):
+        return hex(n)
