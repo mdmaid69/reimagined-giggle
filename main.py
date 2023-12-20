05d6293234b2686fb756750b54d48e1f583acb75
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:21 +0000

    closely through walk bottom ride

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
