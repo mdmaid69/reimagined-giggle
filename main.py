e1c0e61ca3e38524eed902b62feecbfc2badc2e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:20 +0000

    judge arm getoff advantage anxious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
