738deda91da20e6fa35277b7298c6e9ea21a9d98
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:37 +0000

    steep better trap support bottom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
