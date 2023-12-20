6934411532be3ca01f9342a99e73d6fd592a225e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:14 +0000

    near transparent attempt tap continue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
