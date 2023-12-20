47f45178b02c59a78289b7d354da8a55f9ebf3e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:36 +0000

    sign complaint cruel argument total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
