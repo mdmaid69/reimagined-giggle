4d8b51c300cf7848edc01fa75e97a6d4328d46e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:30 +0000

    either play loud whether sight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
