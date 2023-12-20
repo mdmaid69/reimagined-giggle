1096f78e1e91c9a37f50855c6372099c41dd9a87
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:57:11 +0000

    turninto minimum ride wait rush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
