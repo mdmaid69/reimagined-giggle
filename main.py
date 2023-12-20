bfb41a4ba882778f0d4fe18fec46ba01d0566e44
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:53 +0000

    have worth child shade hair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_set(array):
+        return set(array)
+print([x**2 for x in range(10)])
