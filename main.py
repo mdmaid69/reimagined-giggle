eda4019130d2749f5dace94b54abddb45324dbe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:02 +0000

    getaway platform him inform actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
