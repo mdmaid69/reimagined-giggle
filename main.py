def666b6d07cfe08a3f53a332a2f47ac603d167d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:09 +0000

    game fly callup guess influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
