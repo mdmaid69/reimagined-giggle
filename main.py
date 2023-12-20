428fbdca3315f99c0515fa8ec1b8447f60248e73
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:41 +0000

    crowded exhibition least uncertain getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
