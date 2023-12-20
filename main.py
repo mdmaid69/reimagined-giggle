4da555175856f337673e44301f8a9e66db43cb21
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:38:12 +0000

    spring vote short entrance wonder

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+from collections import Counter
+print(Counter("hello world"))
