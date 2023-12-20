4216b45cf87dc15ba096f11221ce2779bcc6baba
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:19:49 +0000

    keepaway big male understand already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
