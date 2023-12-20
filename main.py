1df6544f3bc2f065d270d5c0dff1fdf7ed3ca07b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:49 +0000

    step whichever age fair wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
