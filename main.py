3a75e8ed8f48b04dcf5a1f6efacbd74421168dd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:24 +0000

    private preparation search term hour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
