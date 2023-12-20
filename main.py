ddc9c45c7634b2f89d0bba5b50954363b6d80691
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:39 +0000

    cruelty space offence example choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
