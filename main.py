5a8593a89cc1a8cbb50ba3dfb55d21221f609923
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:02 +0000

    bother plan sort snow callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
