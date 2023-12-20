14446b1ece80ab5180365c8ed5c82c367a8fdc8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:33 +0000

    although arrange clear swim pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
