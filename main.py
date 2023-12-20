390f2cce9b01d58f70e6a7217334f0a1529f09d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:16:09 +0000

    smart company after touch set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def remove_duplicates(lst):
+        return list(set(lst))
