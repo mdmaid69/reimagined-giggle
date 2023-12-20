846eb1bce1ecdd93d3fcf121e63cd756bbf61cb6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:46:49 +0000

    once team meet serve discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_float(array):
+        return float(array[0])
