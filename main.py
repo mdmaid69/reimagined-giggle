07f152662f1c5a89b483b401b22f7ee503fdc2f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:36 +0000

    scared talkinto seem narrow here

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_area_circle(r):
+        return 3.14 * r**2
