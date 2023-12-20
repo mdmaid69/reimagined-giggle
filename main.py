c901b4e0666bc63a40807c6942d6b7f9a9fcce4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:15 +0000

    height follow dead there short

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import re
+print(re.match("h.*o", "hello world"))
