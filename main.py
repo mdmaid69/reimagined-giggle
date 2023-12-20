af3bdb21f2101602860556a2a5eea89223aecbd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:07 +0000

    tap thinkof church trap equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
