621384a7c100c849839e922ee3547e63d34f2f83
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:31 +0000

    aim port show taste under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
