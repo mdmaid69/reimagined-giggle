93fdbe41544d66f6a7a6b2229b81b0a29428f9bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:20 +0000

    lock us discussion explanation prevent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
