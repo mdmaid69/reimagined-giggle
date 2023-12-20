645dd40d42bfe8b84e1b87eb64c1e18068f209da
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:46:42 +0000

    excellent nurse damp attitude calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
