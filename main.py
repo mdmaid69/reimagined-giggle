8fccb77ea6303c90d98c87e45da9ccb202d7ea51
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:06 +0000

    arrange major receive surface development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
