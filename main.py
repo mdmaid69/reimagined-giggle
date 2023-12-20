8dc6ae317650c27f95f3c4df28402d37f1d17e1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:52 +0000

    face guilty attitude tall hang

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
