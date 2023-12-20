1911f32ab40276f2846e0725509a54c0edaeec84
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:30 +0000

    behind stranger life comedown male

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
