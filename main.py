b7016c4eea56bb99744a8593d30b4c0d80b7fbac
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:10 +0000

    appear fasten create fresh luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
