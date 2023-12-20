33049d546f625cffdc9c1dd419f7a9c4e470e29f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:43 +0000

    stranger bit might dear wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
