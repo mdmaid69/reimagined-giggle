a1bd208e88d0d9f29b65434d0a99fbd09916ab03
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:12 +0000

    risk runout mind draw here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-for i in range(5):
-        print(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
