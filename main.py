2f57548a5455ec73c547329f1e0415dcdb856957
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:14:37 +0000

    murder spoil unpleasant mean from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_typecode(array):
+        return array.typecode
+def convert_to_octal(n):
+        return oct(n)
