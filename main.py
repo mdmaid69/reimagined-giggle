e294e38c8dec912bafb62ae2f9bd875173d42c50
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:35 +0000

    hello delight settle duty knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
