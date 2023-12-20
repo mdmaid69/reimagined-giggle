366de65c246fd6fdb34a58aadfc137831bee555c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:19 +0000

    sentence wash holdup spring move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
