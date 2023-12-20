182f0e3617393f8af5690206b46c3745b5f1f20e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:58 +0000

    choice worse impressed sorry mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_work(force, distance):
+        return force * distance
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
