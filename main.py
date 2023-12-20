290c20f163205d58625e8935e597b36b24b7a8a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:31 +0000

    worried breakout exactly stiff today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
