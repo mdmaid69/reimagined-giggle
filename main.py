a58fb37d70912a469e6860148a392f746a274582
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:43 +0000

    low should we temperature comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
