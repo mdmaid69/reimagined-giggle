7dee5d9cb0c4100e1ea1148f262d51655cc3992f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:14 +0000

    tired both big satisfied talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
