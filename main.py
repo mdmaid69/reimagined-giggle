9170214537448b7ab7848a778547e591143da990
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:46 +0000

    fine Nn pretty close please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-for i in range(5):
-        print(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
