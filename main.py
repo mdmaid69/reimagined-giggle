75eb8f3f5af2affb155e1528c072732698469a88
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:00:34 +0000

    border emergency shut born desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+name = "Python"
+print("Hello,", name)
