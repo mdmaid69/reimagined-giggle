1f87535d0c9cfc9b3e7811dc3973b75e1fdeee38
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:14:03 +0000

    ourselves number bring dark exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
