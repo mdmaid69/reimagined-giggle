df7bc02aaa689ac2114875193c68cb4e23932131
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:16 +0000

    combination rubbish complete fond block

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
