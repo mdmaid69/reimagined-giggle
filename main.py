2a8617685baec7b04abf1c1f781c8418cb2d3acb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:56 +0000

    half production treatment complicated shrink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
