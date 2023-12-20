63d4dc138c8e3be0b7bcd088833a37359ec5212b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:37 +0000

    whether platform birthday together relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
