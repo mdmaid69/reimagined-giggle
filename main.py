d52fa4e517cc5294cb7957eaec54bda23589f0a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:44:41 +0000

    all hide money progress run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_max(lst):
+        return max(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
