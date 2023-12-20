0ab6b485acc7916cf673c730011da5bd18c54117
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:59 +0000

    rent comfortable muscle brother container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
