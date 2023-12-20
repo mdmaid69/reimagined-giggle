552f93bc07bbd3584b305a93fd1580668ce7c7d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:57 +0000

    turn Rr afford road expression

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
