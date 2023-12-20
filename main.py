dcba6cc7ea34e9d3cf3370caea86582e14ae4991
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:33 +0000

    allow treat pleasant never clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
