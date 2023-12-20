c01f1bc8070a2b1213faeb5a857a6dba7f57fe69
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:07 +0000

    begin proud picture post education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
