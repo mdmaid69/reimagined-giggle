8846107b92574aa5f1371467d0a22603b502cda0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:33 +0000

    water people light occur rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
