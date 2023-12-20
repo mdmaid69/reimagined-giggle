57581affd2bcbefb0462a93e9a40d2369866c9f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:50 +0000

    physical deliberately store 573entertainment hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item_count(array, item):
+        return array.count(item)
