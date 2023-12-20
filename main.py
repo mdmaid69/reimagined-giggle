b77019b2ba569760d238c0d50b751bdfb512268a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:39:34 +0000

    supply surprise model upwards callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
