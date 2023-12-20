574b3046696659e61847b696e69bd29003634635
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:23 +0000

    police seriously thing grow free

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
