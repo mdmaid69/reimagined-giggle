4ae3af5e40a7d7a25f95b5de6ae230e34307cbcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:33 +0000

    imagine around pollution mind scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
