4929b5556669bd18d0ac5497fa623c5d7d2bc5a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:37 +0000

    stay right check year clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
