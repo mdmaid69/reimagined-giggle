b38503f5b7a85476b3b3d63ad1bd09037da1bd98
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:59 +0000

    neck traffic unit hold bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
