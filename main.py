7bf8785ad7319cee88d25ca15e61cc75912d2376
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:07 +0000

    environment shelter through foreign give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
