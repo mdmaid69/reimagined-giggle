b4df8902db2efcb43926fa6cc4868211371cc2dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:10 +0000

    majority much against forgive possession

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+print([x**2 for x in range(10)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
