9eebdef39190c93ac5444e16b6d345e162715671
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:01:36 +0000

    say fork brown image loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_speed(distance, time):
-        return distance / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
