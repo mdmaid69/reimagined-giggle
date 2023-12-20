103c2a3a350b7ebfb1eee9db763507453ad2c0d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:20 +0000

    community morning above scheme acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
