29cf29658e567a3285791c256418574cb41cdaa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:21:38 +0000

    hair car recommend unusual imagine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
