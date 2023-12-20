81c7635d92d759538c8c2793cd165f76148f6f03
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:32:43 +0000

    tap average love else under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import getpass
-def get_username():
-        return getpass.getuser()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
