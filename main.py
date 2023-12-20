da679b3b3eb9af2fbcf7436770127f00fcc8b0cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:21 +0000

    impressive I translate inside used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
