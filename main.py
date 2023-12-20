a38df6b3681a7877e8f9c93d6b8e64c41955847c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:04:08 +0000

    shrink suckme tip score disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
