c5d37d2044495dd74ebb93536c60880c5f455dba
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:53 +0000

    natural protect lost recently over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
