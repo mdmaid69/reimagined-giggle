dbe80ff95b9d07bc18dfa7c57578efca2981f940
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:54 +0000

    advantage foreign release part giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
