dc318455c1038d4172908b8a7489ac7370f76005
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:53 +0000

    distant moral powerful name interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_directory_name(path):
+        return os.path.dirname(path)
