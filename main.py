091edc66c6b381e0bb0cec93e7b6cf4804aa8a34
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:11 +0000

    glass swallow goup callback noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_item(array, i):
-        return array[i]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
