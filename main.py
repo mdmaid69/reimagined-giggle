471c0734c1c0d9d73951e9d7c026e4e10d0e6b5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:09 +0000

    cough worth bring her modern

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
