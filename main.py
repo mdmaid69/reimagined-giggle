492802ad8702c207b2a565c267c35d59bbc6ce78
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:12 +0000

    market defend takeoff goup cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
