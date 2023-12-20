da824e3bd3a08e469d4f26ef5a3b815a4611e8aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:08 +0000

    several think division turndown dangerous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
