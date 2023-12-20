639d4fb5410b18bfb748beb9f083362c30a928e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:55 +0000

    takeaway description lookup successful file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
