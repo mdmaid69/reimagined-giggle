ad26b0ed6f5611ac9ed4121e2b24224f5b44dcf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:39 +0000

    break mark measure rest expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
