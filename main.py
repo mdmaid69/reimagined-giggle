76a7ac62c6e7b2245705e9af3a5f6e79c033b7e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:33 +0000

    closely wave destroy in include

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def divide_numbers(x, y):
+        return x / y
