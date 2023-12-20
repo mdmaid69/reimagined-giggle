281fc5237082a04ac12faaf4a399ef973dac3624
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:59 +0000

    command scrape character information as

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
