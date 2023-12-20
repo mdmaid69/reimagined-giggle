d9a1c17db63aa04494231bdf09f60368a728504f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:06 +0000

    getoff work environment change abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
