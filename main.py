fe4eb536d7cc696dd8a05370e644777a95e72b8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:55 +0000

    slice land quarter possession continue

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
