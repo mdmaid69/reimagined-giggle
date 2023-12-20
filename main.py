f95d3674ae900eaca9a78cb5ddd588499bb7776b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:15:02 +0000

    cook refuse patient dead congratulate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
