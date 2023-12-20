077c46ad8332b60b8b4434478adea8d6b0fa070a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:10 +0000

    giveback deliver apologizeapologise importance vision

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
