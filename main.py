fae6142d8239245feef4b54fc3c19a732942c198
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:47 +0000

    opposite try loose snow blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
