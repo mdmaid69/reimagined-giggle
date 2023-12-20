2f7a5a5ad6b57cc9ae704163d4467c98e223559c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:27 +0000

    mood suckme long represent acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
