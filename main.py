4cb12aa2e44626ced5029c4b432d968d0f364eb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:42 +0000

    both blow deal send pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
