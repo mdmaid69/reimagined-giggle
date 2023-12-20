f0e634020e0c8b0e12dc42f61b15f0022fbb2125
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:26 +0000

    manage house jump wing near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
