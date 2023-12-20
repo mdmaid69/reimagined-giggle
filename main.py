1829caca06b89d6329c24bc8b4ea778880f812a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:02 +0000

    pass blind treatment event what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
