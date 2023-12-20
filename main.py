40129de1272a55bda4ecc1ea97a479bb4c34e98e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:10 +0000

    unit waste tend question later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+text = "Hello, world!"
+print("Uppercase:", text.upper())
