178afcd77f72802793ef1b5669f2d3029a90d5ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:35 +0000

    avoid weak refer interest eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
