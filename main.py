581dde0e9e9d7b529a41e55b9e4c60bf124e8ae3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:18 +0000

    house pretty badly fast emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
