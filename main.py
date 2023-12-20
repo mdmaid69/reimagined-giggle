48f8221cf2bf9ca5a8326727bb821a18a004c6c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:25 +0000

    listen cross but joke gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
