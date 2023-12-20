1c2ff12a73991c45dce584b1a08138a8139e5b64
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:59 +0000

    realize actually OKorokay voice invite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_set(array):
+        return set(array)
+print(sum(range(10)))
