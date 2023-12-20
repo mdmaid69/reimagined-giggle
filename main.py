85aa099c131976412bf0b65746ee6564f1f5462f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:34 +0000

    cool country mistake late exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_repr(array):
+        return repr(array)
 import sys
 def exit_program():
         sys.exit()
