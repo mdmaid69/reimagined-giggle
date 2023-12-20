d5b5c0fb954559809b5deb1898b91370da201e76
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:55:00 +0000

    sure share feel much account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
