633ebd0c8232ba38e34b371c50e646036c2f62f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:30 +0000

    want past punishment appeal education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
