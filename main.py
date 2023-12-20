26ce99fbe1cc96fb995372087eb3833235d68620
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:31 +0000

    exact slowly uponon square communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
