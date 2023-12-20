0e4091b4cb2d65c76e9cb89f67bc4143c275c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:09 +0000

    achieve medium address sticky powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
+def count_words(sentence):
+        return len(sentence.split())
