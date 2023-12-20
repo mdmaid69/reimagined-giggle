c3b5c4a9499e51a8e049f9e3659bea457ec6ae0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:32:45 +0000

    estimate risk behind his getoff

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
+print("Hello, world!")
