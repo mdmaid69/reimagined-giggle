9979699f87f28d5cb02f1cc736dfef80aec1fa6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:46 +0000

    at aim story careful common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
