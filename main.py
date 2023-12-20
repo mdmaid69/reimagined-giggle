b0eda5c6867d7af13ca8705392edf967989d7e58
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:53 +0000

    except for spare image region

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
