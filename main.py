5d8ab96d15d3542dc45246bbb9599287c5f0a0bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:01 +0000

    turn phone thumb 179bite gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
