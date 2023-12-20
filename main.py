7aee8f156a6293dd93f8a8383ba53786d14f2038
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:58 +0000

    fence declaration spoil discuss chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def cube_number(x):
-        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
