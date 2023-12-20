c075da0e6778b767b3c61e49c567ce5e6e13147c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:48 +0000

    used false important weigh central

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
