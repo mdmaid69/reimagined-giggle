41c6a734b4b6003d5ec3155c1529ac8bcbb6838b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:04 +0000

    upstairs price argument feed backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
