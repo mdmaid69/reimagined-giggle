cda5e3961fe694ce73ef1eb3c610c6945cde6702
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:22 +0000

    bad prepare emotion remark feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
