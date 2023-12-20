32377f75be8790a14330188174882fbe61ec9bd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:31 +0000

    drop callat prepare fit bored

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+print([x**2 for x in range(10)])
