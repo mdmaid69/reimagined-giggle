42e2ebf36ee0e0a8e63e1df75dc81decdaa3e485
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:20 +0000

    before authority morning action recognize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
