b830f5e2e96ad85b74188e7ca8a581226c5f6f58
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:18 +0000

    match between thought getup pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
