f3bcdac38a5bf22853b36cfa3fc2f918b15f6ff3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:19:33 +0000

    switch ill remove secret everybody

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
