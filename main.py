4dd8786d0e8e98f8a95982ccfbfd2ef98572cd19
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:00 +0000

    knowledge naturally callback main call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
