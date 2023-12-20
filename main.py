1dba8a836e51ec8023c61341993f9c398ea57ad8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:05:42 +0000

    last sign separate generally emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
