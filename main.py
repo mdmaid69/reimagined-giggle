c71afa7d96eb24f884a3ffa2fb7cb4aff57efc9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:54 +0000

    holiday necessary available note strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
