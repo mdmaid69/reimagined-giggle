b07f2ed1be4ec6fde2f6fb1802ee821e18677acc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:23 +0000

    second Ff scrape majority perfect

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
