d538eeccf7ca0c0ec3d6910be86ac2ba71838937
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:45:40 +0000

    circle base over pencil measure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
