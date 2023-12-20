e5a0804a23bea534a63c15da6de63c2bc88c2db8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:51 +0000

    wet empty maintain comeup burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
