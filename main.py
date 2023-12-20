0b0ebe3595f33352395ba97c3a50b9476f80b2dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:56 +0000

    successful manage safety pinch practice

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
