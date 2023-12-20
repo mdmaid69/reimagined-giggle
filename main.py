fe2bb6148f3253f1bac2b81a8b297111590ed70a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:05 +0000

    certainly regret rely dead none

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_item(array, i):
-        return array[i]
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import random
+print(random.randint(0, 100))
