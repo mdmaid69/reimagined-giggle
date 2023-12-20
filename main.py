02e9159cefb584210a786a517bf17a9b55954793
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:23 +0000

    nor law abroad approval lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
