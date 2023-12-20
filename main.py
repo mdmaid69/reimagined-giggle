ba1cd4cdb57ad9f3710bc644816744d1c88e3d0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:39 +0000

    exploration ought turnout trust smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
