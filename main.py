a19d7beae783e03c61d55b3cc53420c5d4d333d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:47:10 +0000

    Ff square actually ball happen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
