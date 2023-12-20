97d1386922f54c09e314f5635981fdb44957872f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:19:20 +0000

    patient closed machine goon oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import matplotlib.pyplot as plt
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
-  def count_elements(lst):
-        return len(lst)
