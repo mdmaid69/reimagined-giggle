d100b03b899b4e700acd70bf78a44039cac8794b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:40:03 +0000

    hurry turnover live goup hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
