bbb9d0338b7b4084ecf8d60ad748c612e22671c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:33 +0000

    extra afternoon treat anywayanyhow up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
