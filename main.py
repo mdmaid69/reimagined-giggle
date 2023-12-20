7ffecc2fec9643d70e89f43cdbcf27ced2587ae9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:36 +0000

    recover let everyone Uu tired

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_str(array):
+        return str(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
