5434e1c811a88bccb855b53356b6d075593b5f91
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:49 +0000

    hot minor attitude acceptable beginning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
