cf91309d58cc42681341408c7bf6ad3f5f79add2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:34:01 +0000

    spring science flat mark accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
