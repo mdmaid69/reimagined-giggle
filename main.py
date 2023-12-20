fb81a0666d16fe11f509e27ddb05aae613d2b899
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:31:41 +0000

    trust raise this single welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_exponential(x):
+        return math.exp(x)
