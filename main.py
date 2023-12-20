43f663f4d1bcbb44791e0208e09f740542388842
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:28 +0000

    perform table cum wide crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
