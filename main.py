999a53c48f611357185615d4f40d11f3534b2520
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:10:19 +0000

    thing factory wet character skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
