f63083d1204300c2f8f2c7e0aee6614828aea761
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:39 +0000

    pile butter bye mention turnoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
