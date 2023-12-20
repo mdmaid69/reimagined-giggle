6ebf969b93854e6d1abd42f70d43971755bd2e7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:34 +0000

    spring lucky whistle transparent second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
