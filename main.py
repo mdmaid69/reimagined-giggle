6e5143adc9014f5c69975e3997815cd90690bed5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:41 +0000

    university which always tidy whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_user_list():
+        return collections.UserList()
