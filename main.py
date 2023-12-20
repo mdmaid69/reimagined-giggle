f0b13db5524979cfd93bf94381e56de6dca97826
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:58 +0000

    principle change sky behaviour pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
