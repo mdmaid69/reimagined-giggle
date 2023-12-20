714cd42042981aafa9380e2ac58fe28f16b7bb52
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:20 +0000

    murder birthday control suppose Yy

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
