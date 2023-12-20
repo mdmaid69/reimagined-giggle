2d167c26cda796c1434a1d73bf61aa58a9144b9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:37:53 +0000

    public maybe average better hate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_palindrome(s):
+        return s == s[::-1]
