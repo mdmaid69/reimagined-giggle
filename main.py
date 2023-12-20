2274085d6798b0c6b6971736d8561ae27870a194
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:13 +0000

    tired declaration generous afraid row

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
