2b83161d0e75c896098816ca1061ae744f9d18f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:24 +0000

    star confusing breakout safe high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
