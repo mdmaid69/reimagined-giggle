6a07f76692c6d474aad5d73ebc8252eadac1c368
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:36 +0000

    would mean crazy day sound

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
