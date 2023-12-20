05e4cd88031a0d5cb8c6ff6fed422767a6ac9493
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:20 +0000

    left slip emphasize whether drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
