71d1a62ac510c866b3de27fea6657c8599d7fd10
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:59 +0000

    ill write substance pronounce hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
