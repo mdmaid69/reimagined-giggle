b36c8affeee7a468abfb99b3cd7798ce8592a511
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:51 +0000

    foot replace ticket lunch noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
