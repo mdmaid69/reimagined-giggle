287b1e8eaf4d0c951d454c0c55aa9ee1b3c1e428
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:43:58 +0000

    shopping hungry black belong back

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
