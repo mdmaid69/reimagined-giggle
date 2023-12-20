9f1abe070179cbd26745f3af54f9b667c40f3f48
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:12 +0000

    inform lunch discussion error phone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
