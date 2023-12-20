9ebb0840fc0bb929d9b5733fc58fda303cbafaae
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:06 +0000

    bath calloff minor brain buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
