6b3b4a2cecb7d8e33a1f6733dd5347bb035e5706
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:32:16 +0000

    somewhere really cover telephone concentrate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
