5311325965ff8caf33e7163f73405195c49b3ccb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:27 +0000

    loss wrap obvious reasonable forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
