f9959a8a8e036de3c1efe697e45093c7e5e12c8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:02:56 +0000

    sell badly bus name shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
