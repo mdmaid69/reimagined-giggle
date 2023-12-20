11ca20243f6c4ede8da8a294ff22771e4b4643a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:06 +0000

    temper comment shallow sorry before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
