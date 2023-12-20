3cd176ed93b1c137d83b94c304f80365f36f7a03
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:52 +0000

    hair track tour balance afraid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
