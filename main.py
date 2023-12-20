d58a1acc18dfc4facc96be0513bcdd2dc171a8ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:50 +0000

    challenge wrong damp offence none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
