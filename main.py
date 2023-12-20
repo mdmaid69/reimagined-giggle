373b05fc0c0c25e1a517162384222b0f8c2ef4e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:55 +0000

    turn handle workupto Uu human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
