179f95ecee49703e08166d3d878e83de35287016
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:06 +0000

    fault night turn fire entrance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
