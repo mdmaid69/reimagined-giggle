89b09b570b81ce439d5827ed5e72ce2ac419f183
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:06 +0000

    want less production window rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
