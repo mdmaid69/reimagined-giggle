d1496a4ef48390b3cac771869f6e4ecd8487fa1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:13 +0000

    comeover anxious most takeoff tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
