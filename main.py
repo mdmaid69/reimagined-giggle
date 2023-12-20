2c85b2b7b39549ade50c1f2ec4b58362de0f27a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:14 +0000

    getoff copy free walk art

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
