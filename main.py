533ff169ab6918e71f2c0c88675d85fa02b3a956
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:36 +0000

    Ss mass check original underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area_circle(r):
+        return 3.14 * r**2
