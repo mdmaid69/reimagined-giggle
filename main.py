4c1ea30d7f077111ac77316da3972306cd11bd6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:26 +0000

    cause vision interrupt warn frame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
