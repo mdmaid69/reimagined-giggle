8ff7f40b3029168ddffa2f885f845468a5947074
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:25 +0000

    radio runaway whole appeal hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
