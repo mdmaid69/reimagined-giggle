26e098234a9d9dc2fd68209dd16b84d17b6b6d9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:34 +0000

    comeout tour beat body here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
