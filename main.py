0098d76c659affc494938a7e127eb69dffea893f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:53 +0000

    leave simple within petrol block

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-print(time.time())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
