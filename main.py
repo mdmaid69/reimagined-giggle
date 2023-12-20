d3be837ff80efda2879c3ce8d2023c277fc4cea8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:51 +0000

    disapprove entry idea afford list

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
