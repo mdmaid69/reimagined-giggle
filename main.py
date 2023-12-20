f603518aac235904fa50a6fe64ed75757a197469
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:44 +0000

    main control change willing platform

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
