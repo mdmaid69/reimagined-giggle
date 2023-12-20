8b172a9350438b82dadec03abeb9173ed8d750e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:05 +0000

    turndown exercise so whisper plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_max(lst):
+        return max(lst)
