f15889784f7bd2dfd2a58b6f941d84aecfe717ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:49 +0000

    doubt switch damage cap difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
