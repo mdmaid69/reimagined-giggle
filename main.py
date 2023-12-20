b2073e6516dbc5559949176a3db14b5c1e59698d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:24 +0000

    slice warning while proud patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
