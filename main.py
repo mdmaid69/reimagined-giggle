0b462f67b68b18f2fa9fb2651e8b482d6042f82c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:05 +0000

    necessary meal over value priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
