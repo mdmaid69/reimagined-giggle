ca7d8f5528bfcc32ec58411f94f3ee5586fa9603
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:24 +0000

    runaway bang pot contents deal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
