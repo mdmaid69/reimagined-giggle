179b2823f4f51dd9a5e269c6a6cc64f807651fd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:24 +0000

    stop modern government grade thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
