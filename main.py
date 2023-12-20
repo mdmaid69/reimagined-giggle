78ba8bc689bf6cce9c9ecc83bea5e9493c0499df
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:48 +0000

    scare dry one spin sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
