686ba6c9ee4f84cfaef3d6e6fbbfeece3f52f33f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:06:58 +0000

    whole information pity first usually

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
