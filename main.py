7f76669e269374b870e19a9a8cbfd453a5a38ad0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:48 +0000

    everyone result constantly programme buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
