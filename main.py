0aed8042fa3d21a938d32f7f5527695f46a5bb6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:42 +0000

    shelter find sore afraid wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
