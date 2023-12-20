d6edd9fe0835a763725f4d528d59cd894dde0865
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:06:15 +0000

    pay cry impressed match branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
