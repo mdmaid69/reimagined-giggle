f27ce1d2e5817215b78edf38ce83fae52d34a0e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:43 +0000

    software do little except climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
