dcc1fa0bf2c3d5ad3f7b52f4704adacf31cdfcba
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:30 +0000

    lead suggestion card quit across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
