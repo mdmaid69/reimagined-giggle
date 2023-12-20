4a3741aed46a3fbb48d65f0326f04a439ca07d0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:28:48 +0000

    professional scrape need clever busy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
