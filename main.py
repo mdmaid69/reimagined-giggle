76ce3d9e0dedd34cd5a56e45b17db42b0cb1cb5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:16 +0000

    intend anywhere test slide by

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
