356340aa549010f401552dddf90c43fd44eb2212
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:35:39 +0000

    talkoutof from occasion valuable attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
