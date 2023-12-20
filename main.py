04579d186d3a11d0203f69f9c6fcf3b8c4b5650a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:08:48 +0000

    past bottom university police bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
