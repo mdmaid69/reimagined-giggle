895f3be1092e99c4dca37b42ec571bf99ead155a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:19 +0000

    comeround suddenly typical bored cause

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
