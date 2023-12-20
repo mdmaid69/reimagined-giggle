ffa9fe5a13aeb38b58221938db8228955277e084
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:11 +0000

    blow cause surprising progress callback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
