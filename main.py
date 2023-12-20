f5465daea382c11670d395551c162db75f278e77
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:10 +0000

    pretend moment hate spill language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_user_dict():
+        return collections.UserDict()
