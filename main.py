52176cc9c3d70a779a759d12c87b92d435ab7bbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:43:37 +0000

    somebody outside light punish back

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import tensorflow as tf
-print(tf.__version__)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
