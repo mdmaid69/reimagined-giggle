c137fcf6e1f4f84bac74e007baf8b968c7bcc5ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:30 +0000

    buy spoil allow share smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
