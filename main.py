1527e94bc4e550b3c6cc42ad0659dbb35d7f666e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:15 +0000

    open importance boring wish cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
