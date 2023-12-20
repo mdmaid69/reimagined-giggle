255c4a5f0ef20e24e7124595e841c030b7dbac11
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:41 +0000

    impressed turnon material generous shadow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
