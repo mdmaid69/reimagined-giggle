632597a8bfde0b2353adae11297cd282f58937bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:26 +0000

    everybody provide light breakin cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def convert_to_binary(n):
+        return bin(n)
