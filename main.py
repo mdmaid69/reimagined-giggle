5fc4a2b1ede2f2e7513e55d271bd690ed8817a7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:43 +0000

    possession pronunciation train 564encourage shrink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
