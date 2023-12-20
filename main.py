d93fba5bae959b44eaf3438714bf87509b9fed11
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:03 +0000

    suit priority direct trouble schedule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
