c3735d65ee33f8a334ed87e29bfafcb825e39b79
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:57 +0000

    giveup week phone leg accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
