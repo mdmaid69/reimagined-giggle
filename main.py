23839f43f3013ead4b4859a817d29d1281687137
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:39 +0000

    jump correct consist waste fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Words:", len(text.split()))
