3d59323ff17fb48da6fb8832b7340f6d0999034a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:43 +0000

    spoil scrape especially complain past

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
