bee2d3925b1f9212a2371020b31f06e5d7813b9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:20 +0000

    if turn bush beyond disturb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
