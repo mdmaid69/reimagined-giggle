722c94a42d914525ec374f511f687e2e8b061034
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:21 +0000

    possibility discuss desperate interrupt upset

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_time_since_epoch():
+        return time.time()
