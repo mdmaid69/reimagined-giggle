8eb20ffcfbce112b3317ab2a56f605d3d02a191c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:41 +0000

    story see nurse treat turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
