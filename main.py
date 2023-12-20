666ecd880f3fd99456c2f848eb7f87e1af100b2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:52 +0000

    advice include protest grade Dd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
