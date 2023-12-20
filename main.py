3097ef12b0f7347a910226b93633aa01839abc63
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:46 +0000

    fall cool wide reasonable do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
