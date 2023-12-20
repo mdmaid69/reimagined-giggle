f6f5645f52bf04d205853cf1759b1682cf1c3038
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:19 +0000

    play maybe door best react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
