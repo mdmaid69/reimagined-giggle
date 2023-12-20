1aad34ffb97772c464f3b4bb5c748f4fb4e00fbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:51 +0000

    future relative block nose for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_array_to_list(array):
+        return array.tolist()
