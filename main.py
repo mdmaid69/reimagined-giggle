889b2cf8007b2e940b8d065e7df6929e519daa86
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:44:49 +0000

    takeoff congratulate head expensive goaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
