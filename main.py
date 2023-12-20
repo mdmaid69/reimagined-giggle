0fa8911f266c3aeab5c860bfb5a15f90727914db
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:50 +0000

    note blame unpleasant approval please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
