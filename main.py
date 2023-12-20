e2e5e8698542be8f394a4a007c5104be9d990dc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:49 +0000

    reaction her passage abroad confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import os
-print(os.getcwd())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
