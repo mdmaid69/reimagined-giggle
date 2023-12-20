a16c212e65a74bfb2e98d1d36ebc48910a54e871
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:32:22 +0000

    greeting relation listen deal course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
