dfd984082f1fd7a134152cfd1558c02edae4cd9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:14 +0000

    turnover port runout fire smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
