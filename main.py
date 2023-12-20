970d0f9bffde8573f89d32b5575a7ed1612c62a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:59 +0000

    list pressure write home brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
