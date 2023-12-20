5f46054937b74939a7c4d2ec0a931b447324070d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:21 +0000

    impression fault average comeround accidental

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
