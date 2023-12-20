11d9b1ce1d75bd1d760969b4f3e6704595e5a7b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:30 +0000

    smile tax real add direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
