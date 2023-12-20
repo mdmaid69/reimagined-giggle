de201c23e2bbbf46c68037d6cbf6b80df14c14fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:25:13 +0000

    peaceful success nature everything start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
