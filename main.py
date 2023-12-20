42dd7734f2ca53488ac68af47dd4dad45e5236e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:18:23 +0000

    they drink dream hope doubt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
