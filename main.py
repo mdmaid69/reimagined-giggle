673625f70d48c3be82cfca93e8291499c0180adb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:48:11 +0000

    best table habit ourselves behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
