b330592951e35b77afd34a50c5bc9cb6a29b2180
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:44 +0000

    pity calmly in enthusiastic last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
