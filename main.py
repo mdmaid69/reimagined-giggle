f5a127d289a53cf3fefa5a82d23746ba301bbdf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:33 +0000

    flat mysterious relax inside win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
