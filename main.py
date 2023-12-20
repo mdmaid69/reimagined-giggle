fa76de640a85fec8a07c275135c6ffbd53c858c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:20 +0000

    pure really interest towards freedom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
