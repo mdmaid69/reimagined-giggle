69ac6ae3f4548ecd86c3977a670e56058d066a61
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:28:23 +0000

    holiday fail press development fortunately

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
