37c5a1e220f0ac0860e83366eb08f7a2d4605962
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:28 +0000

    finally occur excuse solve classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
