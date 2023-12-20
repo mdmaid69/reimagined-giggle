a14139498c61a58d07fe44913e2edeaf06e3071d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:18 +0000

    set improve roof present any

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
