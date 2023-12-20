2f135d299dc85589fbff238b9798c668ae901932
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:28:25 +0000

    escape wise except turndown inquire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
