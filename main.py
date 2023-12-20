3fae27c68be2f259639996a817d5f6d2c4ea06bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:58 +0000

    low cost birthday attempt answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
