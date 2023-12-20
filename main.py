593c2f6bfcde4251164fcffe77cbe19d38c8e79b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:28:11 +0000

    like mess after coat record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
