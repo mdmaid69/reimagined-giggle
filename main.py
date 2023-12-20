a66c157284895d8b00da914e40b5e25c7143b47e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:27 +0000

    cruelty underneath float sort leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_base_name(path):
+        return os.path.basename(path)
