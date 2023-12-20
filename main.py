d5cc7c55ebf1adb40ce9f0f9d182a02e81aab7b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:14 +0000

    may bear favour damp buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
