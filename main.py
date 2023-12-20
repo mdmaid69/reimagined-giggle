aba11bc0df834e60ce2f5025287c687d91903890
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:19 +0000

    shoulder gap stretch thanks because

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
