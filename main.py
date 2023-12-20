b98c2b442a38c90e9160d878c0432340aa36ce64
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:38 +0000

    mother opinion shelter tell equal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import time
+def get_current_time():
+        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
