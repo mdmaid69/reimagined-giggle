c6f5049eb665077f69dba85a947aea60112babd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:42 +0000

    rather more politics then wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
