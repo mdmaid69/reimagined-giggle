7832a99285e355aac470bee2700ea9ad91cb2cb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:11 +0000

    relation similar gooff prefer set

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
