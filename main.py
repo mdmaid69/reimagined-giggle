a4c8ccba9c9c982dfdd13df2bb76a9df8ba3ff71
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:43:07 +0000

    eager fail organization get drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
