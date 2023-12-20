62af45b4da945a33df725546c6ec22ec8feeee0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:42:22 +0000

    owe slight handle school lead

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
