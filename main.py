35d4b24ad24d03457cbf423a09470ba200a165ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:08:57 +0000

    light influence breakinto knock guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
