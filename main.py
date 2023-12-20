e01a602d2e15b72aba23ae9df43492d3f9061ad1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:07:26 +0000

    impatient meanwhile new building cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import datetime
+def get_current_date():
+        return datetime.date.today()
