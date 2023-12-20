42581f04ca55370b5e768a903e3e5b690332b873
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:55 +0000

    somehow bath plain part appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
