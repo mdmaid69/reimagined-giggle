4f9ed0833199af1f80df4328f464c2a8d4fc8d9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:28 +0000

    show plenty something print along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
