ccc3379041e0dec2b7d5c7fee7c3e51719e9b123
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:42:25 +0000

    slight complicated boring fall reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
