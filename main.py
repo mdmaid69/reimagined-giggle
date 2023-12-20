c378125d86beea5035f7b7db1a9a8bef03d0e48a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:01 +0000

    slide unlucky honest language admit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-print([x**2 for x in range(10)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
