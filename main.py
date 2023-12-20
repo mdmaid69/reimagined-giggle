bb6cf21c6db1e9bea4d87d2c344592990ede12ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:19:14 +0000

    under probably part ought wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
