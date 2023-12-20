f48fd7400bedd78e651253a7d48f4755f5036bae
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:23 +0000

    eager means view soft lot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
