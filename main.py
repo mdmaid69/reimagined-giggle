9639e36b0b1e3524278fa30dd4e925be3497f04a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:36 +0000

    comeround arrange act thinkover strong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
