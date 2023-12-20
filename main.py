1c6d9c1512e7f6bd162fbca6ec3cb1d70c514c86
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:48 +0000

    relation act suppose politics delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
