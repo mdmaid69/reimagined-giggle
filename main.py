50c9985960d5f6b10d32d321f5179f4907b24e86
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:15 +0000

    flight run nearly keepoff beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
