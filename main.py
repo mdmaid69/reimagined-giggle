9d53e35500045a2b25d7603844a547f771dc0374
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:04 +0000

    enthusiastic sound finger smile new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def convert_to_octal(n):
+        return oct(n)
