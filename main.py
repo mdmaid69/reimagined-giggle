2cc2a28b6aaf23c0adb89bda72a03225b7810931
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:52 +0000

    back paper rare quarter as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
