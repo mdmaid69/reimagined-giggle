5829398d36b91ecf11bc672f4934cd42dcc1f5d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:46:59 +0000

    to arrangement 572enter neither dear

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+for i in range(10): print(i)
