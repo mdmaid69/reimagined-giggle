788632e05fd4cad4e47b79676813438fdcf5fcfd
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:18:43 +0000

    until prepare building confused butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
