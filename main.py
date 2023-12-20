d214a4b5a92c3fd5e8cb824e839c4f48c9378d19
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:02 +0000

    majority turnup furniture manner connected

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
