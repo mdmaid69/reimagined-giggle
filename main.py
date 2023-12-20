63410eecf380957aa55dbe949a877b6d57e0d21c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:51 +0000

    clear creature base there successful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
