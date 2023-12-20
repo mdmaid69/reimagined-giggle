5d2c9449d98f5206fd19eb699e900d5905851a3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:08:41 +0000

    calmly bet test grow breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
