5196aec6ad063f085b91c4705bc19aa07bb55ab0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:21 +0000

    fill not hour funny feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def count_elements(lst):
-        return len(lst)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
