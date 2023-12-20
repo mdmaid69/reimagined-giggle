67a01e1f36fcbb09d6eb569807097b10cf47c64a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:25 +0000

    separate stupid once balance goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
