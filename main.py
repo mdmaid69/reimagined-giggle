f292d9f63a353a8f81fe89339b6ea703b79bb357
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:56 +0000

    production confidence blind clothes swing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
