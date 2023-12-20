0b8bb44b2b5b45daf7998a1df8953fbd64348f79
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:00:37 +0000

    giveup used circumstance capital set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def add_numbers(x, y):
+        return x + y
