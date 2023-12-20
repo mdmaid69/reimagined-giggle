81e7f3fa41cd20c36abf06382510bdaff58f3082
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:05 +0000

    letter for extraordinary comeup dig

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
