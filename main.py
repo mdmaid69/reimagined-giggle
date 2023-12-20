6162b081644f100c8b99be7da44ab20812f0e656
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:19 +0000

    home tie during fight stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
