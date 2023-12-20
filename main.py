794aab296d792f1f9561c8136ab3a1b80aa70b79
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:48 +0000

    then respect breakinto check play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
