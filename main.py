9d77e55929ec0352c445be0f9b9b119e1bcd11a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:50 +0000

    long anywayanyhow exciting course public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
