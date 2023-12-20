de3631c4f1e3b9a10a6380ea486e5991fd3a6df7
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:24 +0000

    rule lead dance somewhere either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
