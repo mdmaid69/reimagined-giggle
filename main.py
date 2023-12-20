6ffb1ce9b202a90fb1a914e7ecf834e394ac58db
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:23:10 +0000

    prize young keen careless shock

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
