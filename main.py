6d6168ca0d3d985ec8e21fe190e31ee019e222b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:08 +0000

    cough breakinto mark closely house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
