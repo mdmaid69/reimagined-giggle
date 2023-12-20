8be1e861f7a82e376829ca48d8ac7f0aba5322ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:11 +0000

    fierce warm below sound matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
