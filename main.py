02936002d3e6275cfe29c9429b6b6c496323d130
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:42 +0000

    thick dinner base place sweep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
