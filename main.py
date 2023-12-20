b999358f9e50e5b56a1ed892ad55c231dff6db80
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:44 +0000

    difference march smart grateful miss

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def reverse_string(s):
+        return s[::-1]
