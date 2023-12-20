23ff1286ac4bbab43a0cd73658f92a47833d9b09
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:22 +0000

    region public watch keep hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
