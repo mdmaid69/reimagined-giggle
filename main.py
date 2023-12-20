378ced92b82e7e1883beb1dbb703818201dd7df0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:04 +0000

    block delight scale avoid usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
