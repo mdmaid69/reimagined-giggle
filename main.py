173d37cb45b63480f4288bfd6f1b946ffaf9564e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:49 +0000

    whatever 566end nowhere hold natural

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
+import array
+def get_array_as_set(array):
+        return set(array)
