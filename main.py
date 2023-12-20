a3c05a7940bcb9889988f9e3a68230731a86f448
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:53 +0000

    noise dream high straight clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
