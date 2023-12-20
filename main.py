ac4f3da14f613d9c64149160595e565ac667f8e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:20 +0000

    to Tt fire but steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
