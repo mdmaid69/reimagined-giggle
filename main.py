b9acfd544abd36edcc254b59c92ab9a9413798a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:10:50 +0000

    otherwise nerve Oo below pour

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ def calculate_payback_period(cash_flows):
         if cumulative_cash_flow >= 0:
                 return i
         return None
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
