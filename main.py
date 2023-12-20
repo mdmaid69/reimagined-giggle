6f5a74ccc90cca2d18839e58c21f086056109b37
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:28 +0000

    noone shoot regret look repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 def calculate_sphere_volume(radius):
         return 4/3 * math.pi * radius**3
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
