bf5c4b08cb32d6b034f47c9c01c8fcc2db6ac907
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:03 +0000

    tradition congratulations bored by minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def get_array_as_bytes(array):
+        return bytes(array)
+def is_even(n):
+        return n % 2 == 0
