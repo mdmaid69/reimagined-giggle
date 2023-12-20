28595736688da99ca31664c71a6d70b058e104bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:27:58 +0000

    grand below pronunciation objection he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
