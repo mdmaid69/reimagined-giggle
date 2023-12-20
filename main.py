fe0021e3db05f345bced761c7c7f44ee8bbb9ae5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:33 +0000

    goaway opposite book communication afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
