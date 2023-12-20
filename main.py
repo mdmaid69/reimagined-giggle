5f935423c03fbee6988d14ee6f99aa7aa84919d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:02 +0000

    kind reject eye heart century

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
+  def square_number(x):
+        return x**2
