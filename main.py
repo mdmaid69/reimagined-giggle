e9395fd87a487a877285b8f21d28452bdcb8a28d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:27 +0000

    make worse because always work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
