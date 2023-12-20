949acb5bd2596a5d0b386a6c2db08a227c99a544
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:56:56 +0000

    wave them tap regional splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
