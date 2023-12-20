c6a6dac18aaac10bd3e7bf28736579673f21b48a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:13:17 +0000

    drive give breath bag borrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
