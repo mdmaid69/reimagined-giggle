dd05cd0aeb3d69b0b8f0781fe1c513edda7b16d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:29 +0000

    card invitation unkind condition concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
