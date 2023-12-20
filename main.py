8bc86c16797f8f93824ff5e3e73553f4e7705882
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:06:17 +0000

    promise kind worst pure still

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_string():
+        return collections.UserString()
+def count_words(sentence):
+        return len(sentence.split())
