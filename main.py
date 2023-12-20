5d8fc3c8e8120ebdd9076fcdcd1208d9d9e96f6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:45 +0000

    health university nigga later meal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
