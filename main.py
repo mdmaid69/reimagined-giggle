8895dc6cc0d6a39732f28aa281554fdaad57deb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:11 +0000

    spring takeout influence bringforward mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def clear_array(array):
-        array *= 0
+def get_array_as_complex(array):
+        return complex(array[0])
