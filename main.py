7e5023e802816ac53791633a7566888fdb9d1684
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:11 +0000

    stare cost angry possibly thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
