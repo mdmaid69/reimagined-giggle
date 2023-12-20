0a159dea5f794d03ae5225342750125ac95b70d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:21 +0000

    food all factory price quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def roll_die():
+        return random.randint(1, 6)
