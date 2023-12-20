788b17ef82c1ab3f360b58d13a205edf73c39638
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:15 +0000

    just complicated typical inquire news

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
