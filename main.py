a4e51628b1072073e02f7079e3541bc26845cc9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:38 +0000

    tap approve advantage uncertain science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
