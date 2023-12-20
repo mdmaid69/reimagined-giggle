d18fbac85ee0c1ab8a8e36ea441b8ce9bcf554dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:16 +0000

    either deaf marry man round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import sys
-  def get_python_version():
-        return sys.version
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
