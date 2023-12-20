31347b1bc20da6c7235e97f00bdaf46bde2acc7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:10:05 +0000

    cap still plenty promise bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
