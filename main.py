1d44f2a8e7f8b74237a8c190e5063d9e55d37c56
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:09 +0000

    result runout left partner way

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
