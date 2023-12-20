7acf623881492876acc11116a395255458f9c85c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:50:50 +0000

    scream hold while sorry release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
