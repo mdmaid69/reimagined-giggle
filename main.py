a28927ecb897a329190585f8863d9d6285d914a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:54 +0000

    shut average complete sticky takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
