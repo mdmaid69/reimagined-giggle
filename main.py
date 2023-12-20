1be26a2c97506a322d10a57fe36b49dfa70ac9d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:39 +0000

    below joke ill air correct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
