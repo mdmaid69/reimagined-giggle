5deefab5b4702c8eed6f7ef754afd24ddd9a10fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:39 +0000

    exact bit never slightly move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
