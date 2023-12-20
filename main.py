e623ca3258c876ae9a27fe84cd3e6d32fe777e1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:15 +0000

    born thanks beneath suit measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
