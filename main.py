c7322b9274fd47ec1cbe65b2cf21bdcc439249f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:05:49 +0000

    worth disappear total hungry home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
