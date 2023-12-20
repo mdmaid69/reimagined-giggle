78f1f0f2cffe23943c79cc421f17033c6a2345fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:40 +0000

    tidy listenfor judge distance send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
