8682742cd5441da606bcf24c6e17c381343de342
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:16 +0000

    trousers know crazy start argue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+for i in range(10): print(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
