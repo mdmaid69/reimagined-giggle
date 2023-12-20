73cfac3bac67cf1c886fc680c6c36812eabb5d01
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:07 +0000

    stage season at wipe turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sys
+print(sys.version)
+def calculate_area_circle(r):
+        return 3.14 * r**2
