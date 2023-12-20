a50fa448cbeab5e92b96a07377512889825b2ddc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:30 +0000

    breakup quickly pick under complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
