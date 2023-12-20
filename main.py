e02a61d4a9b10e258ea48edf147f14b447fbbc12
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:11 +0000

    birth black set none independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
