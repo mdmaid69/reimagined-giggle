e67ae73a377afc62251d45c351dfe928930c9fc9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:27 +0000

    cheap hate stay century upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
