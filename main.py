f3529b88f74bc84c016f0998196fc2dec8f7a946
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:37 +0000

    sentence pretend view furniture goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_set(array):
-        return set(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
