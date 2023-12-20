cf22666c8f03ab00cf74c88d88ce789893164c7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:31 +0000

    understand fashion insist there recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def count_words(sentence):
+        return len(sentence.split())
