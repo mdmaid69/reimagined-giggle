a345e12830e9f17923abb10e36d1ea08fc14be4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:57 +0000

    mention relief uponon mess Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
