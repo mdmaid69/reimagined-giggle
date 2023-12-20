ab19bec7bd246d73389ae02c99e44896ea402a9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:12 +0000

    tomorrow serve although underneath frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
