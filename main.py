210d2f8a02417fcc94dbe5cc11121ac62675f3a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:52 +0000

    material direct programme cough stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
