35c08fd5242cc8391c2726c8414f7a0e873fae12
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:26 +0000

    expensive everywhere flow responsibility joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
