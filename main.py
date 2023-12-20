fed346d554bbf73ffd47d4ea3ed44c05a52f7aa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:23 +0000

    I dry lot offer strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
