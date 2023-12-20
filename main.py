5722635b10f1e6afc38a2521dcc1ee7864116e44
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:10:37 +0000

    trip otherwise apologizeapologise office spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
