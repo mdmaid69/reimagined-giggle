63864d3b2e1aa9630fc1910e2c254ed0769daea6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:14 +0000

    operate comeover matter frightened better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
