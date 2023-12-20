d9889a5b8df8d17c8459b2471be474bf714186c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:09:15 +0000

    occur everybody nerve main squash

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Hello, world!")
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import datetime
+print(datetime.datetime.now())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
