35c9404fc21958ead211e2f46000e0c7696363aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:47 +0000

    love solve burst piece important

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
