8dd8096ebd0a23f58f74265ec2c9217150eaae5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:34 +0000

    refuse ours record lead really

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
+print([x**2 for x in range(10)])
