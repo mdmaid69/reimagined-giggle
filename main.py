01998632fd9e8b3fae992e46ea4abcce0c39df00
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:52 +0000

    successful old absolutely shock noisy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_average(lst):
+        return sum(lst) / len(lst)
