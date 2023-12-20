7757cde2e41db964d655cb847ca7735d9f5139b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:37 +0000

    favourite high dish still basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import platform
+def get_python_version():
+        return platform.python_version()
