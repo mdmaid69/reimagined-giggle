bd5467eadf8767bd8e707c1da0745defc7b6e34e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:19:13 +0000

    where shrink dick environment hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_acceleration(speed, time):
+        return speed / time
