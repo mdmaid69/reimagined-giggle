d21890c03b5e196d334bacb42baf5ed274b89548
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:49 +0000

    tend goaway whistle run breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
