a77e692d48b20e59028ca35b22f740280159b83a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:06:54 +0000

    outside someone able exchange accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
