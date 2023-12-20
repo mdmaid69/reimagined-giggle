eeacd2f54a80ffd126606e81a404a95845cb6b38
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:57 +0000

    question pump opinion protest search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
