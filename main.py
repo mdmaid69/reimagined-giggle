81b2fa08568fa3524907706b71eed54a8bd4666b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:24 +0000

    just music welcome university estimate

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
