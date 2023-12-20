e07ccebeb2e00a9cbade3ba03609547c4cc265df
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:08 +0000

    at winter direction however duty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
