e51ea3790c200e74f4f968a25ae4bfbdaf9f652b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:36 +0000

    save number apply rescue identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
