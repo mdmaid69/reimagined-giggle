69387d5afb065fada1bfeb28a5df56b7f7ca970a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:13 +0000

    score comment pot beside child

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_work(force, distance):
+        return force * distance
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
