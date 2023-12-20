dcaed6f2158c8b5ee89c58dd0b26e03ab3a8cc24
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:17 +0000

    steam criticize pool mass active

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
