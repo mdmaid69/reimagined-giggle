d6601b6d8dd0b85bd735e2f9e2d15bd391ef7f17
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:18 +0000

    pocket choice positive cover ground

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+print(os.getcwd())
