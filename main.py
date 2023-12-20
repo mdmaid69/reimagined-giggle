4f123ec57fe552fc3475d8112fe603e91e25f967
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:54 +0000

    match Ww one feed woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
