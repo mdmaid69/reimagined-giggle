3363913b9ac2fcd6d5ba5c510af1790f54986780
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:47 +0000

    sit wind strike field hide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
