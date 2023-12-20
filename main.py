5fd9bf5090a3752d8505f5cef3694e7683d1505e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:42 +0000

    towards remark match blow healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
