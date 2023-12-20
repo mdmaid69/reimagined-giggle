86ed033ca41818f6271ded90c50ab37bfca10f5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:10:52 +0000

    sing set near suppose bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
