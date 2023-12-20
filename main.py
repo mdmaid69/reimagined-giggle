98b38adaca70effc0472986054ac0689dffcf12c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:05 +0000

    Jj contact profit beautiful past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
