e86b69ebf22990fcd5ae9c4d542f77467f80b330
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:37 +0000

    never national burst pause search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
