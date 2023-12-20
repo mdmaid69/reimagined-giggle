adcce4b576ecf60aa2d75a4ed224e224dc187867
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:44 +0000

    touch guard some further goup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def add_numbers(a, b):
+        return a + b
