096d407b1d075b5d7642d3c1133c6b3e7d5c36b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:42:24 +0000

    back expression introduce fine like

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
