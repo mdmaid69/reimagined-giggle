41b1529ab8792c061a4a935271a8aeb846d14521
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:37:31 +0000

    note wonder immediate cheque flat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
