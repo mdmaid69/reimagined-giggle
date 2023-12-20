7d2f486336ad91f0be3d4cb8a6f396d5a3efda93
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:56:41 +0000

    confused best flat above direct

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
