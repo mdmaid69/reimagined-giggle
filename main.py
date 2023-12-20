e18add75ebd53f250f20a00693cf4cc36b1999e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:32:45 +0000

    everyone sort mark spend lean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
