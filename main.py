1ff19e28718d787fa9bd814650ee907c516f348b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:42 +0000

    leave shout talkback principle turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
