c6257c6647071330dc876df83ba97e99872aecb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:49 +0000

    base accident any later avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import re
+print(re.match("h.*o", "hello world"))
