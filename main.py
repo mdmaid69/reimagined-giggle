b95391b0552e34225233fe21d7c2df4551eb1060
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:58 +0000

    tidy violence favour up fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
