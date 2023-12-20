057bd2d4e031536d1fe5558ab5ec7d1870fc6016
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:17 +0000

    bringback bet sit match black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_set(array):
+        return set(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
