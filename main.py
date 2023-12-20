0e56df1dac991b5bc99ecaa8f39bcf0f7d2c1fb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:07 +0000

    evening attach comeoff own question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
