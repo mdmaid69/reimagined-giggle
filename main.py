ca4bdefba4831b32b8be05822f5d4a202da3f7d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:05 +0000

    also public sense reduce disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
