cc4b0c21b6dd9caf8db30112026e96cec240cb88
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:39:01 +0000

    base report duty Vv conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
