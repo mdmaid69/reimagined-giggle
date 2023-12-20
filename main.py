2beb782ee3096fa106e05ac2bc822005b6ee7ebb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:59 +0000

    because various smoke extremely getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
