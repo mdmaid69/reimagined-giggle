b4f1f7e93acda87bf369a8bf94865824f1c79203
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:21 +0000

    catch voice ours arrange beneath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
