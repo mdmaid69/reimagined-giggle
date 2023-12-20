693f07cfe030fc298cd9400cad73c6859e40d8e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:15 +0000

    forward tooth earth limit everything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_hex(n):
-        return hex(n)
+def cube_number(x):
+        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
