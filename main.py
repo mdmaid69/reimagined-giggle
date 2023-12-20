8cd6e43cc8150abf0de9a0a14d4bc9c4a61cb309
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:25 +0000

    runover pen while fashion reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
