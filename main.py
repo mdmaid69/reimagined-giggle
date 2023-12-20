6a94824be42b4cf81cb66d162d8f1af684e28255
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:40 +0000

    welcome total matter relative note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def print_python_version():
+        return sys.version
