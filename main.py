2be31b7a5a9ba4ec9141a7e0d4978586e0015a3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:47 +0000

    Cc cut stupid free above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
