6f5ae96474772f14ff18ac9fd2974477dab2599d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:46 +0000

    passenger rent crowd division insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
