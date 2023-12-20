0313912a58f6c9cde57ea04b8783f824bbefd88d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:55 +0000

    concentration perhaps escape safe either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
