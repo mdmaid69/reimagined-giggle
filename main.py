eeb97ba461461f519d9ae3e942bf0aff791bf834
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:36 +0000

    deal equal fill even sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
