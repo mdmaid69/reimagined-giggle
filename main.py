779ccc7bc9c223e2b72480f71aa9d60b857d96b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:28 +0000

    expensive comeback Gg related funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
