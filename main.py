d91486997ab1a19e285c25c783a7f750ce2aa4e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:25 +0000

    discover attempt disappear spot century

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
