a7466b04d2ca65943ccb77e6e7cb720dc6ae5c6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:32 +0000

    put otherwise everywhere lead pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-print(datetime.datetime.now())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
