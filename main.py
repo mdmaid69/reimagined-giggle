ae77ee6fdbaf75388b2d5f31034869e9d96599aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:07 +0000

    generally clean plenty nerve actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
