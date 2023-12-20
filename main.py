eb7511701d5c9ba17336423260475d5f5de0f54d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:09:43 +0000

    sore own rather card work

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
