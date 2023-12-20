679acbf648710c1255f51c0efb3572d536f05b76
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:30 +0000

    regularly market type whenever stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
