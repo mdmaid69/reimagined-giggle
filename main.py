81ac364775dd9d9880bacdb2141759507377f84c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:29 +0000

    with dare under responsible side

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
