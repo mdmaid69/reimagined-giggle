40f41d1f8e4c7c9416ccfb89f4ea46af7afb3a98
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:44 +0000

    thanks nearly sport bush dinner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
