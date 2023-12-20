ddbec30b967487e8359b24cd084d557d09465ce3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:10:03 +0000

    arrive direction piece wall original

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
