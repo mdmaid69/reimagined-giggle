9922e7b3ad372cb16df1b6798c00289a005d1017
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:53 +0000

    attention strict herself check approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
