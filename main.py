4bde3d59d07a4c47992ece99e6c191fb8c0c64cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:05 +0000

    room quarter instead blue decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
