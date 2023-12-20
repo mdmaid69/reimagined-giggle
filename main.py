e8ee1f09063d1fdb30c3b626a27ebec27f59d13f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:34 +0000

    laugh before clue breakinto able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
