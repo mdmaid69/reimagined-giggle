4d12dbee033e836fc16655940f71ba195953a91a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:16 +0000

    hers comment train weigh point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
