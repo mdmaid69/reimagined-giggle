ca6516d0c2669e892b16f7062631a6ed1ae4647d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:10 +0000

    funny although muscle peace later

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+print("Hello, world!")
