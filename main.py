b2b7289b058e1ac91fe8aa88ddcb917eaed263ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:58 +0000

    listenfor everyone male luck spray

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
