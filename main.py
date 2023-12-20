62bffd196c784bcbabe715129dbad33a36601343
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:19 +0000

    stay freedom cross finally offend

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
