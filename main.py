0b987a9d7b0b72515e935c234ea5edca821b6c94
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:52 +0000

    like promise too accidental at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
