9e8e2322018a1b495a704d0d1ac76c5a346b3f09
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:17 +0000

    beautiful experience shop file one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
