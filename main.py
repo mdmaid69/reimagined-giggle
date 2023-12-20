38cd71828a5fb85d16b8fe1981fb2e538e25bb37
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:23:46 +0000

    switch against example zero wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def sort_list(lst):
+        return sorted(lst)
