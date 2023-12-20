401cb59be888ea48df937a4ebd65b0d7f53dcf66
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:10:52 +0000

    house measure leg once ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
