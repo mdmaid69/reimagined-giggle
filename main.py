bfe6e35461ce3029e8aca76b065925ccffe13180
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:53 +0000

    declaration obvious so rob noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_queue():
+        return collections.deque()
