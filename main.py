abc5176749e9d576fe690b2a75dff747b29b915d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:34 +0000

    firm destroy along say cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import sys
+def exit_program():
+        sys.exit()
