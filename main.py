ea976d441b6ecbf2fb54be6f449924b5d179beda
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:22 +0000

    formal excuse building advantage confuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
