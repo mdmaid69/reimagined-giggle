da65ed69ad6a0186733f3a7ca6547bb42be13fb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:44:03 +0000

    turnaway best sea turninto feed

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
