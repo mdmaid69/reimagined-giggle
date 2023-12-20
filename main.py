07e7493dcd7433256b9ec70bf81544c2b2ed720b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:42 +0000

    thorough deliberately complaint twist sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
