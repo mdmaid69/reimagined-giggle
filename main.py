532395eef91840d6094af6319155a75ba061d1a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:33 +0000

    error your breakup relative will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
