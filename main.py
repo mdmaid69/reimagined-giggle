10a5d3edf9b5afaeddd9a4403bfe79acff9653cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:34 +0000

    weigh mainly goon ashamed eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
