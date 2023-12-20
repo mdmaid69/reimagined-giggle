1decd96bd21ef505818e0b74781ba103b6bc8204
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:27 +0000

    watch square joke middle police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
