e426d88f4db513cca1b4dba15b20a0c64884a0fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:11 +0000

    take active government sort copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
