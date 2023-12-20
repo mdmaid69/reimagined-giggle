3cc279ed25f135ed763a3116a818c179396b1f51
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:55 +0000

    comefrom 565end clever his summer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
