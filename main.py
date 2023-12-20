7a029d073e6938d90ae7cc5ffbc6758d666f2c02
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:31 +0000

    breathe forgive hand plan minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
