6e7fa68dea3595adc355a3f8c0ad7687bfad4e8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:52 +0000

    breakout wear card well sound

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def convert_to_binary(n):
+        return bin(n)
