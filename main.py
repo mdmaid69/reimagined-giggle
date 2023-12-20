5833575cf1c50ca1f3cf46ccebe4656f22c4a5f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:43 +0000

    undo shine pain save return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
