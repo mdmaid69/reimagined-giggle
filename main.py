71d7ac14c31a9ea2ebb08d239352cb8df7d671bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:11 +0000

    precise object size range with

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
