a778f0c682f9443b2c10c440be63d2cec5cac51b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:05 +0000

    body dress through suck probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
