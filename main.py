f0d7ac98ce02aef8c55efa16146f9cc076710889
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:59 +0000

    pull minimum declare group fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
