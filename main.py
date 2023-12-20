eb555fc6fbf01e72cd85bc086448c5561ec23760
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:03:06 +0000

    inform journey amount please average

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def clear_array(array):
+        array *= 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
