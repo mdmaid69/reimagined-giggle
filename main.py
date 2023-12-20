49196cb835b2d0dab36c52c77e75a775fec8ab35
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:39 +0000

    sound interest too official high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
