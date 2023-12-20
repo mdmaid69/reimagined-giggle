0f9f436b107acc7777cff554c2f61ab155e44328
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:04 +0000

    result fire gap comeback afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
