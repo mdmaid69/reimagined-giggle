850f6bd7302051d2266b656dc30efcd195e4c9c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:08 +0000

    Yy will claim grow exciting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
