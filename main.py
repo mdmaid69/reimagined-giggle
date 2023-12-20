a03461d9690f76146b78e0fbf0d49b6ad88ce930
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:24 +0000

    welcome mood challenge doubt ready

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
