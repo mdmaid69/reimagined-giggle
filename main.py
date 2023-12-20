ca6cc08742f48cdd122d23632da95aa76adb5894
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:57:35 +0000

    wealth wire argument road radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
