9fea373f9fae919836b3114488d4dc13c4aa0727
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:17 +0000

    second also extremely over deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
