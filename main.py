ea121d3a829102ba194693e63aecbff175c23646
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:20:32 +0000

    fact deep brush leave until

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
