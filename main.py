43d2dc9aa7c1650cee01f007c7b98e27ccd1aadd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:04 +0000

    special they hair give emphasis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
