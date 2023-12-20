e2812bc1a8f5c9819ea5dc619b1b61e28068d87d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:16 +0000

    wish separate be Vv look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def is_even(n):
+        return n % 2 == 0
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
