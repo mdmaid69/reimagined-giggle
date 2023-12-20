8b248c8df96862f7b3cf84a6efe53ef2514cf557
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:55 +0000

    worried opponent of why watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
