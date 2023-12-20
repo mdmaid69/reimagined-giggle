de363d92b6858b584b647c83aa2fd255aa2d6cd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:12 +0000

    stage slightly everything come scare

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_user_list():
-        return collections.UserList()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
