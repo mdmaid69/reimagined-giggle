461243ecffe23edbb520719ebfb339dfbd3af254
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:47 +0000

    forgive theirs disapprove once aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_unique_words(sentence):
+        return set(sentence.split())
