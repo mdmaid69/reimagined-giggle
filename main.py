95602a12cd516d6b4d85d5a85e54199ceadf45ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:51 +0000

    habit window borrow if wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
