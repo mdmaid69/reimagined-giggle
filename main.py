60fc40d9b103c9c3891214ac65665b7bb48704a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:06 +0000

    matter worry cheque concerning path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
