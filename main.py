e5e5738db2d4f7e1ad4a89116d6f029226910b45
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:21 +0000

    pick giveup stamp mail open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
