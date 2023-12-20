2ed97dd7499b0adbd2239490656765e72f02af4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:04 +0000

    of something suggestion shrink like

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-print(sum(range(10)))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
