fbfa70248c1928b717c3545d6cf9f590447f6ce7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:53 +0000

    idea I probably style laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_memoryview(array):
+        return memoryview(array)
