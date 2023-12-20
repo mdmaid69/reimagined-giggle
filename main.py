c77d0a457505941129cfbd0e72bd27ad6cf00c04
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:27 +0000

    whoever judge speed peace reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
