38d47b3037d9b3421353a6a9bb3cd144dfec9ae7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:43 +0000

    against attention active blow none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
