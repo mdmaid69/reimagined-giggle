c5631093e987dd05022dfbe5cca8348bb6a65a2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:33 +0000

    brush tall squash last pity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
