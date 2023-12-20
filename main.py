492efed58a0f65bec2953d0f47ffaea4547db932
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:51 +0000

    left reply hang angry spread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
