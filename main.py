99c60ab3c8620b85fed88cedf328112193b345d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:28 +0000

    frighten crazy thin goodbye programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
