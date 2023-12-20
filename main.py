4f03d0cc2bcf5bd9322859268d36e02dfc527e49
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:00 +0000

    sad treat importance content whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
