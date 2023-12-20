b970cc8012b76a201395922e2e0ea9bb6c33ce10
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:54 +0000

    set although note promise blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
