8a367d02966fbff05932d02c8a0700e5dec2445d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:32:46 +0000

    sting next to keepfrom splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
