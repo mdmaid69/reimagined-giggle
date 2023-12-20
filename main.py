c7ce914aed742122660ef6f12f54a69ea9423a75
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:24 +0000

    act dress shock direction wave

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
