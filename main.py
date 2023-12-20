8c572f7abd5d3ab90aa4c733b1a1fab42e7c144e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:29 +0000

    sorry upwards Ii vision might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
