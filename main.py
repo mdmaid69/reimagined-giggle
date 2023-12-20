d836efdcd9accdeaceca5fc371672f5ff81d85ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:49 +0000

    force steam learn quickly tidy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_area_rectangle(l, w):
-        return l * w
+import random
+def roll_die():
+        return random.randint(1, 6)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
