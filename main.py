caae574f4e9fcbfd4ac3e80e39f34c015bfe9348
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:15 +0000

    front destroy genuine stay mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
