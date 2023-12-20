673ecb001b6857b4ca5d4124ca63c586db2c4ba4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:20 +0000

    slightly mess point do limit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
