56deefaecfb55af4c648afe5642fd00bf9ae7dc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:48 +0000

    describe sorry bread concentration approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
