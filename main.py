1a71c164125bb9c52875f2eaeea1bd0bc66e28c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:55 +0000

    persuade bet brief fear feature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
