3af564c2d9cdd7aa6f4bc0d94283f03c61561e02
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:29 +0000

    avoid string tragedy best spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
