c1bd6cfc3af4885372751dc9a3beef850da49499
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:40 +0000

    supply decide view neither far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+  def get_current_working_directory():
+        return os.getcwd()
