42118e08a56b07013fd3addae5992198b4ae10c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:56:33 +0000

    afraid Gg advise must let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
