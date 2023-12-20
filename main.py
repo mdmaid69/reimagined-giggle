d0c9b8b66b49677c03f384d49c17eb094bb417d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:57 +0000

    regret ready team allow delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
