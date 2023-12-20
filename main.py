24f680334ed27330519301784d367b41151bf08a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:29 +0000

    book steam breakdown mean except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
