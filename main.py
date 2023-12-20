61886c3ef88cd81304a31827924e6d1adb559faf
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:22:06 +0000

    spare used convenient warning interfere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def reverse_string(s):
+        return s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
