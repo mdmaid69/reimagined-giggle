7a043ff2c2f904d8f64487e05354367b2b28317f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:40 +0000

    thanks passage figure swim bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
