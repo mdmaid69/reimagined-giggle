021637aca36fd4c4dd5a58901ccf9f3562750160
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:30 +0000

    match tonight treat embarrassed exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
