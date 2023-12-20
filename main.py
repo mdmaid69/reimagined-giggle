beeb8fe5b539d43c9f4459463439c1ea12c877a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:15 +0000

    whistle force rain body rich

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
