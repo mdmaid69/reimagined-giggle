e83b393fbf1f3a0c96a162b84a3ab9739408cef2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:40 +0000

    exam Vv since tragedy fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
