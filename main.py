d5b6889ed07c8ce9639e2f68c2c9490685f83a21
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:54 +0000

    the single scream spare cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_floor(x):
+        return math.floor(x)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_buffer_info(array):
+        return array.buffer_info()
