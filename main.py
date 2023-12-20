2900b4d23ef1046e6d9f8075af096c0c0f28412c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:36 +0000

    through always attach spend still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_max(numbers):
+        return max(numbers)
