8da05df360877980c24f33ef7902dd59abf78af3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:26:49 +0000

    me transparent interview hit scale

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+print(math.pi)
