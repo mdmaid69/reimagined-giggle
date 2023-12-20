bb80342e64f9c2dc7ae17f669750a9ecb2a2af9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:47 +0000

    violence decide name turnout across

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
