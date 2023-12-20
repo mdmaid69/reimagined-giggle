bbc7400b90161a7abdfa4c93c7c3c3a25052e1aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:39 +0000

    certain pump clear thinkthrough border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
