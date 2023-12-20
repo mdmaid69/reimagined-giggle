643760383b42ba30a02c0d3d696eb87b1f58ea80
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:43 +0000

    forever else crowd cause getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
