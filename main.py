14f6a4b3d8ec85a92b8f8a8e5994db402b3af10b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:12 +0000

    coffee bottom 571enough why figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
