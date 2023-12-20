dbcd8f1bb1c9852646d0ee778c7e833418eadbb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:47 +0000

    urgent perhaps kick news curious

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
