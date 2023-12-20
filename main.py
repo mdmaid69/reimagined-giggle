d2211d0898ba84dbd44817a6c0eeacff5c161e7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:03 +0000

    stamp Gg style wash boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
