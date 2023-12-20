d7d9a39eb8d844318a2e64c0d629a069c2b3c47b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:43 +0000

    issue whenever meaning piece late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
