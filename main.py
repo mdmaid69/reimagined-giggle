c062eb7df1e8aae25066e1ff4b942e55c8846fa5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:41 +0000

    store protect prefer this production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
