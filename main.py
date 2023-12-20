6f6fe4a125c1181317bfddaae723315774b04607
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:12 +0000

    borrow plenty cook release match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import platform
+def get_python_version():
+        return platform.python_version()
