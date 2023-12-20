53aaba94f666a2416d3a4c9e1f0212e0e5a3a59a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:17 +0000

    exist experience excited argument extremely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
