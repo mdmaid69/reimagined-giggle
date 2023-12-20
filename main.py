08023f1420cbb1cfc7859543ebe16fe2ec3793f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:14:12 +0000

    claim glad since loss sign

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
