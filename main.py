91322eea0ddb5c04aaa94d6cda7ccad7ea012c41
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:24 +0000

    achievement take blow wave interested

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
