a577a5d8d77365d25aeafb351c300aec082eb233
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:21 +0000

    journey spring clothes skirt wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
