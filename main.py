2eaa6575546b5058d5e74d13380363b681cc10fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:14 +0000

    such finger branch Qq hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
