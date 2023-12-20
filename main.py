83b91989916ff6b9166bd45ae246ef282e23890e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:18 +0000

    interested pleasure amount neither pay

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
