271fe77c2aa2cd4b25d745eeb28a29455f973504
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:30 +0000

    between cross surprise relief Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
