b300e57ca9d0bc95758ddfce54437decfd156815
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:26:22 +0000

    already first lean comeoff wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
