18ece42534f1d82168ad63aa20c0cde0ef2a0e1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:57:01 +0000

    change further image finger plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_power(work, time):
-        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
