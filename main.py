6b2d569080083cf6986ddba2be62afb979acc405
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:44 +0000

    education group fault crowded instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 def find_unique_words(sentence):
         return set(sentence.split())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
