d1a091b3f53085721e218cf9f0bbe425fc15b973
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:56:57 +0000

    rank neither makeinto without slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_work(force, distance):
+        return force * distance
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
