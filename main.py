b13e8a3b433cc13613503a7be7412a31433d67ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:47:25 +0000

    wrong tongue offend cruel process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
