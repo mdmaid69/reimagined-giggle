052779f1bd197216aed397e0183099fc2955b2e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:28 +0000

    smoke shape friendly opponent square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
