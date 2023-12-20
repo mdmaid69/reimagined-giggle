a23d3f321bc02643ceb26cd05c80ef24a3b3ce90
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:18 +0000

    lay inside think worry yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pressure(force, area):
+        return force / area
