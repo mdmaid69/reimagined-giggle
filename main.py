c83bec53c2efb5c0946ca2d601e52ae2c13f7cf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:59 +0000

    evening unpleasant recently dish other

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
