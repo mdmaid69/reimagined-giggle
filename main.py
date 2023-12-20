0e02393cf252347d6b35bc1617b589961a3c135f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:03 +0000

    accident along I test you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
