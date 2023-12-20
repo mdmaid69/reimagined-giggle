d65d4c2b72e35f12eebbf933ac8e690cd3023d0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:00 +0000

    turnover scale desperate song extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
