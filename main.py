b6b7bedafefd67e926c4af7a57966b10fce71fc9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:00 +0000

    willing step waste mood country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
