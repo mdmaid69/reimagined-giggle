5b134cb24250e103d2fbdf9d5de3e10a90e385a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:11 +0000

    home tough rely hello plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
