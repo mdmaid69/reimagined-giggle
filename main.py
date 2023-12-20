01b2b57427e6a9c119c33bcc38a04cbdd08f7338
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:12 +0000

    Ww mouth not thinkof tune

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
