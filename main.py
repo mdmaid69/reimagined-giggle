4ad591b85374ca3e87fefe555f8956079c68a6ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:21 +0000

    argue must case drive deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def add_numbers(x, y):
-        return x + y
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def square_number(x):
+        return x**2
