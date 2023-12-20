42036b4e221b8df2f6e7d28b4a6b544250618163
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:31 +0000

    turnover genuine before impressed shine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
