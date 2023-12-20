e70b050d642840620826fef8fdd3c1e376066f0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:27 +0000

    trouble influence cost pain there

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
