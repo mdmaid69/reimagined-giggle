0aefcb9391d040bbae011308788acbfbeba5d2e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:21 +0000

    mark immediately moral gooff home

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 def subtract_numbers(x, y):
         return x - y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
