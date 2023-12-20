5ce6fc26fd2c3645bce373791148b28f8aa943a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:19 +0000

    hide interview advise machine fire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
