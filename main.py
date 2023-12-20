9bc5b2248d08e98919daa9a3ced504fd6d0e9451
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:47:16 +0000

    thinkover circumstance copy control dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
