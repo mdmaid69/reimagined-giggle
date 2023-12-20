5b5bcc595b2fcb897defaa058c31994d957f1ea4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:31 +0000

    frightened proof night cheap bottom

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_hex(n):
+        return hex(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
