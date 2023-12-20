2cbdbb61ff7cea9c55b543bc01d67d292b00d77f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:35 +0000

    steep intend refer set gentle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
