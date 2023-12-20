b54713acff03b58892999a610304f53bafd866b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:01:12 +0000

    feature importance straight sleep strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_force(mass, acceleration):
+        return mass * acceleration
