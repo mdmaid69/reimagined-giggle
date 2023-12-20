54d89460c56d446edcedbfb96bf4b4a35f46dcca
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:44 +0000

    practice board slowly fairly true

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
