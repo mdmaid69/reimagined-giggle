7b2d20261852024ae7bfb9645c21f6a77fb5d8ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:19 +0000

    different on extra geton comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
