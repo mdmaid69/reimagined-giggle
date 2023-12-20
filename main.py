fe7c0eeff1c1f461ecfa3e25860875ad12624fdf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:23 +0000

    silent substance shame approve performance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import numpy as np
+print(np.array([1, 2, 3]))
