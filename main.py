82802d9068c26ec8e4d67f7f6cec3c69818223b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:11:02 +0000

    storm corner become after stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
