058edff85025b6b93db2355fd4f59c8a0b4ff881
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:46:37 +0000

    wide fire ability unfortunate fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
