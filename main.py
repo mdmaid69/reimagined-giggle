f80d91c08824eb1f804b27c1e6cbab64ef2f7e18
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:59 +0000

    practise hand majority ill ignore

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
