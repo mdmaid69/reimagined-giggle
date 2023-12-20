05a36a1739612d00c998e7fbd26af40acacce2f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:56 +0000

    deliberately joke zero within hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
