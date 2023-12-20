67f4c86920a9a69e734e9b575066b4ec943a37a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:03:57 +0000

    callback trust trick interest show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
