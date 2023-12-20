3816974c0dcb0cb2fe98d8c93066909000bbaca6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:36 +0000

    convenient mention sensitive yet himself

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def convert_to_hex(n):
+        return hex(n)
+  def convert_to_hex(n):
+        return hex(n)
