6dcf7a7f7b0736ab079104c51d6f4d8676e4852b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:02 +0000

    nonsense doubt favourite break steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
