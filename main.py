e1198adc459c0966abe2deaac0ae96d0181ad4d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:31 +0000

    progress flat most positive improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
