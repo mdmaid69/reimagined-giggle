f9e1cc985ac70e6fe3789eef1dc621d09db2097c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:30 +0000

    minor leg warn last off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
