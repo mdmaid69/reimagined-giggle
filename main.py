3b075c43081d30faaa567b6e656d92561e6f4a24
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:25 +0000

    drink correct direct slice be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
