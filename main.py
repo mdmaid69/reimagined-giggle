f2a777351094a2f952369afed89e00c784ddfaed
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:55:32 +0000

    alone wish live shoot independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
