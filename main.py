b8c75d06b162c64d9c07135c9fe2b90136697191
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:46:21 +0000

    insist easy bread ignore acceptable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
