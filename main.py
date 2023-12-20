613d5e67fcdd66cf8a1e79202c9ce764f2429b18
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:36:27 +0000

    179bite burn bring actual seem

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
