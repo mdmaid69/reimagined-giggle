9878953c7c8b5340a47977b3c4b7184cd4755cf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:32:41 +0000

    increase zero opposite although call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def cube_number(x):
+        return x**3
