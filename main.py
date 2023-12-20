868ff9672a463007b926486d2e0f2c43370f8a8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:47 +0000

    pretty cover part concerned obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_directory_name(path):
+        return os.path.dirname(path)
