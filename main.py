858c6537813f967c6503f2fa27200d9fa3f38465
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:22 +0000

    swing best period either base

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
