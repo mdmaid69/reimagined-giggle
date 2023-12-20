01a84968ed4fb1a76700ea7366da427bc44344a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:30 +0000

    formal theirs shake bridge provide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-print(os.getcwd())
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
