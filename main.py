bd956d2b6015ec5713b400748d3f63fbd600dc96
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:26 +0000

    justice combination board onto transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
