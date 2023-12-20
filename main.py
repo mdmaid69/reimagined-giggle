d1a2195d2a6a08436e52dd0a1de07b412b040e00
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:59 +0000

    wait aim healthy whistle tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
