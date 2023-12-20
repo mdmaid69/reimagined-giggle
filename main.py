e19a38ae99ee2ba5769b55e335524a1b4668e2bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:42:56 +0000

    follow please pool old female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
