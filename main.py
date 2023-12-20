6b0091fd9e2ca7af0370f0a7b07ba0bb8d5aad35
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:43 +0000

    on unlucky survive apply shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
