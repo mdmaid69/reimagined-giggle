8d26988df3bacd14d1c13100f56044da2d23a2b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:58:53 +0000

    definite suit final fill boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
