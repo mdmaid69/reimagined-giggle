0cd54791c893bdbf1ff548889c12cdd186323a93
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:40 +0000

    thinkof sleep entrance minute before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
