50e29ddec6d56b62a1fd5385166d7e809dcd4ea6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:58:50 +0000

    search knock since mind hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
