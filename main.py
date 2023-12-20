8119d497ec1dc177e4d48867de69a444c8acb4ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:41 +0000

    neither absolutely poor eat slight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
