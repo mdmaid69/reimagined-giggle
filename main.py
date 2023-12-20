176723a68aed421ff220e16401df575947a15e67
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:27 +0000

    refuse unable lend impressed maximum

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Reversed:", text[::-1])
