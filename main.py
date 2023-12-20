f295e161f32f70a4e29a0bdeb7991504096a24bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:15 +0000

    string definite deliberate bother Gg

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_list():
+        return collections.UserList()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
