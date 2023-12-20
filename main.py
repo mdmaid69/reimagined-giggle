a1039f49ff9cc79f23704540eee575f2e534dcab
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:48:00 +0000

    regard cinema include spare warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
