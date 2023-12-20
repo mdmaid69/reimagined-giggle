9b6fbebbc7cc303c24869ad5493325699c473762
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:45 +0000

    problem responsibility study open deliberate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
