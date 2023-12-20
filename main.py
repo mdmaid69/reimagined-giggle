8546ce5c4b518437a8d9fddbb1e4dc8f9a8a1f3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:37 +0000

    bunch wipe giveaway nigga either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def multiply_numbers(x, y):
+        return x * y
