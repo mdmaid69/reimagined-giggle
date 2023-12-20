48c9fedfa5c78a5982710f02f16338b530607dde
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:55:45 +0000

    university agreement square achieve float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def find_min(lst):
+        return min(lst)
