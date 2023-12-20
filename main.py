ab8ae28252288d89b4999b7418085cc3abd1383b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:36 +0000

    impressive declaration guide fold shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
