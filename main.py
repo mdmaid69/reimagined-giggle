c7f86acd717d48e16876b3624f5b956d60fa1bce
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:09 +0000

    runaway habit tough 573entertainment shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
