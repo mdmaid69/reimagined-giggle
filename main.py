4239f530e6a2c71aac669cfd1a88de9f7b7a21ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:57 +0000

    cinema sky honest scene study

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sys
-def print_python_version():
-        print(sys.version)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
