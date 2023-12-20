bfb92bbe505f8fe6aea4541cc33dded2a8818ac5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:01 +0000

    mistake thick lookafter related out

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def remove_duplicates(lst):
-        return list(set(lst))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
