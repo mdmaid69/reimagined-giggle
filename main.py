62561419c60021f27f971277b00dd565c7e4a259
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:52 +0000

    once centimetre return decide room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+print(json.dumps({"name": "John", "age": 30}))
