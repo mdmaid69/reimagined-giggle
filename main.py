5de9f4bfed99ae2fcff686bc9523a9c6983daf36
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:17 +0000

    upwards cheap cover original represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
