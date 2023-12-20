dbb5b582dec940609e1a76ee40dda4c1b5ee226e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:41 +0000

    impressed spring treatment alone shoot

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def remove_duplicates(lst):
-        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
