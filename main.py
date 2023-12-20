f5398d9dff77693f9d88e2f77799199c8819e1f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:25 +0000

    black anxious Kk challenge score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def multiply_numbers(x, y):
-        return x * y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
