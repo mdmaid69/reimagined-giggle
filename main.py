9abdb63af708c3f93c0b5eb074248715bf1ae5b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:48 +0000

    national food record among gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
