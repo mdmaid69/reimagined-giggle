dbf0ae58a6fe27d78d3d6c6fef50dc8ea0de7eb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:20 +0000

    carry hardly responsibility loose oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
