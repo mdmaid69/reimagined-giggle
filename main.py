f88454ee93d3432ca8aaa204b7455f0bb9a52158
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:07 +0000

    tired lift beautiful cry offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
