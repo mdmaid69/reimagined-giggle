9ffd17b81f49f772896c6eaa8d2c95689ce7ef4a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:13:45 +0000

    weigh problem bringdown because broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
