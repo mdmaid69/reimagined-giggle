ed149602fdfe841a66df831f5efd257cdddf303e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:52 +0000

    turnoff town affect will mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
