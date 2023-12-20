e0f4d291d1a747bfe367022db64e79b9c6f89add
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:29:24 +0000

    answer zero ring generous defend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
