fbb85716026169d7024e10b8a6ac13c99fb52c48
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:28 +0000

    various special pass process familiar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
