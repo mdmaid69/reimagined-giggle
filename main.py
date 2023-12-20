0d90e3cad6777f32b0ee098c8ec2342574dec917
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:03:27 +0000

    callup tense rise bridge skirt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
