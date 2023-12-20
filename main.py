c515c0a1ba7a13e65f9ea570c65c0c7ae77a750e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:11:08 +0000

    moon poor lock offend operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
