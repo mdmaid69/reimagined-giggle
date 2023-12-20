94e1db70623b73e22033d63628e77520ce337b27
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:21 +0000

    lunch hide turn mood Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
