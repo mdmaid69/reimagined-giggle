f239d6aa927c6f35dbf370c97d25acb9f87c5df9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:16 +0000

    private bound just burn left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
