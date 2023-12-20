6ff1bc7289a55aaaab39a27b73878abef122e606
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:55 +0000

    against off suppose spite kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
