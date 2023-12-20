2c9d6ef820b85975fc0566765a5e52b1b52a9230
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:03 +0000

    emergency sad marry any shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
