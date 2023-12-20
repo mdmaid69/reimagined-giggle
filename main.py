637588712f06cc04ae2fa6eac8974961251a0a40
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:57 +0000

    secret takeout proof pattern guess

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
