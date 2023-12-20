6aff57709cb612affaaf4633c241d2e8c46aa9a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:04 +0000

    estimate confused extraordinary machine kick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def cube_number(x):
+        return x**3
