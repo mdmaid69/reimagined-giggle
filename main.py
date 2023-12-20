e530552692217326ef695b8943fff83743e8bb00
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:26 +0000

    advise bet almost anyone journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
