eb8636de3790f21abbfa30bfb9e02d71cdae0c49
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:03:57 +0000

    criticism search fault sometimes play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
