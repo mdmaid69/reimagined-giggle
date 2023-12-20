702eff8eda435e51febdfcc8b0b66e44dd7b9c50
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:22 +0000

    last failure tool date guess

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import tensorflow as tf
-print(tf.__version__)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
