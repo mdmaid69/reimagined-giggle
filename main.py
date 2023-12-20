728fa9cd9971a0690c96278adb4c3e49a6265ee1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:18:38 +0000

    scrape escape fall separate strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
