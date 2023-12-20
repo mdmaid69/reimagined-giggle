aaf90c1950f7038ed6a21cd96dac1485f7b91339
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:24 +0000

    because damp murder possibility slow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
