f8467eea8997fb73ec765228d30cb987f3f28310
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:37:30 +0000

    precise block fair protect other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def exit_program():
+        sys.exit()
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_list():
+        return collections.UserList()
