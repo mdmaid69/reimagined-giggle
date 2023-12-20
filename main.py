aa207a02be61fd6cf732d6059ae99e5bd498a917
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:29 +0000

    naturally same little during discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def greet(name):
-        print(f"Hello, {name}!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
