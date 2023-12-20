65ec69007b7ab884a86c6f719754b29a15beb0b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:39:33 +0000

    range kick separate settle reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
