bc820b8c4fc6c161df77f80eaa4c19802f8b8f38
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:51 +0000

    busy possibly star intelligent Yy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_speed(distance, time):
+        return distance / time
