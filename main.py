a3a947335260243c427d3dc2136a2ad542d33256
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:26 +0000

    equal just ago wealth period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
