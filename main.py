ac50a79adf123560f77feae18b6e51d142d67717
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:34 +0000

    bill stick dance pause stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_itemsize(array):
+        return array.itemsize
