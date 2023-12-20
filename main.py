f7b88b5cdddca85c6bcb6d2ca17ea5b342b846e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:41 +0000

    insist scheme recovery arrange persuade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def reverse_array(array):
+        array.reverse()
