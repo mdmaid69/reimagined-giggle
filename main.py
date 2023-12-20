f8ef1373d84cb52ebd5e69db819f999b28e88cca
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:39 +0000

    later slight cry interview look

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
