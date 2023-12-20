65b4ad8550afcf40b41ba7af6b8482802fe4b9e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:57 +0000

    hand sharp hard mine cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import time
+def get_current_time():
+        return time.time()
