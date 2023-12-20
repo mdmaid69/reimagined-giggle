9487e273e7382473714e2156ec9be61319e2464b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:20:58 +0000

    Tt 571enough aim something clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
