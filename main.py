667def4fa96e865c84bc8bf1b51d32af4ff67525
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:11 +0000

    dress control godown brave apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
