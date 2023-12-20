50065249cce12f442b1bd9e47e69797ff3cce3c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:17 +0000

    manage confuse closed truth pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def reverse_string(s):
+        return s[::-1]
