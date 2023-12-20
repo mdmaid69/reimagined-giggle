84e3e2afa7b4a10170a4cae88b40f69de4ac484e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:40 +0000

    bottom runover produce offer transport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def exit_program():
-        sys.exit()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
