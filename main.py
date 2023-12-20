2b5b817ed37cb4984a8b201292e46776b3d279dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:43 +0000

    answer characteristic mass innocent declaration

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
