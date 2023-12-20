158e27bbf0751616a4871a8f609663c2ad70853e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:10 +0000

    congratulations aim account while admire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
