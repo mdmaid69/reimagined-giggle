4385b1c5ab06771f452e6167ab3854c87d936ace
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:39 +0000

    importance shout variety naturally flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
