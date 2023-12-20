64e768a29ea068f174c594ba70fee6edf827a045
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:45 +0000

    nor dick shout house mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
