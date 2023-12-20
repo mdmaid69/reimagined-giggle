a1bd459268ac0820dec37a9a59a35ef24ba432bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:05 +0000

    walk song turnback someone foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
