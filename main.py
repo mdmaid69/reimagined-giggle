81ba893c82ae01ce887e6925e294d0b9173009a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:04 +0000

    mistake form surprised familiar twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
