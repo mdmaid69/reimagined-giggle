9872e203d119a7cb1ab1dca76c08e7cfd4d2c1eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:57 +0000

    play press attempt concern quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
