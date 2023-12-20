afd8a3ba5c22c181215595dc297db6bf00f70bf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:57:13 +0000

    order shopping crash argue suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
