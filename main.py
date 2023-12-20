a40b852d63cfdd84fa99c286b8988fafd8c6a69a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:13 +0000

    normal what bag Zz pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
