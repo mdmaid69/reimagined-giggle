7f9a570021f7be61690bdcabf662fe89464648ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:19 +0000

    smooth have oil government common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_typecode(array):
+        return array.typecode
