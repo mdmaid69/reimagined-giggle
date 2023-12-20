89a4ee641455cfa3409d16f9e380338cda0faee5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:15 +0000

    measure any collect tie cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
