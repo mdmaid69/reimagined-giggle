567e9c052a2c1557a1753d520a40210e07e29c31
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:25:16 +0000

    prize talkback spray often common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-for i in range(5):
-        print(i)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
