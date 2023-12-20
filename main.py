4b70490a17769bb5feed52c8bd857c3f40a5d86b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:16 +0000

    race find human song offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
