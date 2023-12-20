9afe3fba735ae604097edbb5d3ef9a60b0f12abf
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:18:47 +0000

    programme because his even advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_sign(x):
+        return math.copysign(1, x)
