247a620018599c5a82d72f0d4ddb8f96d1427132
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:02 +0000

    knowledge obvious contrast save bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
