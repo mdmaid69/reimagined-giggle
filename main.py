7774b2bd5dc090abb28995349488197c1be00751
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:33:25 +0000

    all ring cheque special busy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
