1ed1c12110efeede86386b1b8dff33c34756de8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:56 +0000

    forever failure shall feature serious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_area_rectangle(l, w):
+        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
