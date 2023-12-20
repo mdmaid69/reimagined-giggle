7da30e4125f0931cb72d985bdf8df7e17bd64363
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:41 +0000

    earn round unlikely question platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
