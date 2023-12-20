485802837e704a5b9b0ed10e132ba0bb44fe078e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:48:24 +0000

    risk dig rush middle out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
