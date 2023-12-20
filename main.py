696a23c76ee60f7ef75cefe87d00d6c4a5148f89
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:46:35 +0000

    comment silent lookup around Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
