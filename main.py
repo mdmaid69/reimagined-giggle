a9a46745deb039dc1d420909ce72bae3e6b3b9e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:14:05 +0000

    cry loud determined contrast control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
