ba5ad704e0dc3d5d505c64bea2d65b40e45b2f2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:24 +0000

    demand level better wish on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_max(numbers):
+        return max(numbers)
