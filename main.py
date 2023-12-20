02448e73a5cd623ebab7ec0169f9a9f6013dc2d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:27 +0000

    worse their paper better practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
