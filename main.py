3d153395ad139034a440753a3da7ef5904474e02
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:51:44 +0000

    fresh lower payment 564encourage track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
