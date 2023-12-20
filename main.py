74022970a51c76a5b5c3a87070046cd0b887248a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:06 +0000

    runover system production underneath scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
