6dad9bcfb6112f6a9c61a5e99c6cf29b9c696ea9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:21 +0000

    closed unhappy house division cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
