4b289e81257c973ecadf808726ca62d0a9be2450
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:55 +0000

    pump smooth warn fasten successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
