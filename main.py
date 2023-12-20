10eb0f9448aab6dde2fc8dd0d2f2e571ccf1cd91
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:12 +0000

    only change evening closely thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def square_number(x):
-        return x**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
