15e432e6c90dbac031ab01fb4d876560d42c007d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:42 +0000

    level Jj worth autumn unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
