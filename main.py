5eeca05c51b70942f256e25e5b164b42621461e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:20 +0000

    sale Mm fail position than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
