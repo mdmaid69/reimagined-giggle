e80ff51766d7cab4af8161c8c58c72d5a1411907
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:15 +0000

    569enjoy trick away prefer artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
