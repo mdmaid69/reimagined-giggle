2d7bc6101468e96210f2c72bcdbc19b82ef4cfaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:06 +0000

    direct do come market fit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
