72cbcfc8b4b82c860f8ad30fbc0ca845914f4c2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:54 +0000

    earn weak bus level number

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
