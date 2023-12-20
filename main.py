ed1d662b3d77525d80230b0b0fa46f4dcad599dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:59 +0000

    talk everybody strict dry cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_as_repr(array):
+        return repr(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
