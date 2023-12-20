a4c33f381965da143e0520e2b14ff90820b6c228
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:48:23 +0000

    control fail once embarrassing last

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
