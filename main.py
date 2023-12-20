49665b48b18e2fa7e692ce0405d0d517e6c60c76
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:58 +0000

    colour centre wherever cut mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
