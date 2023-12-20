df83591b3e354ebae0528ddafaf0f54d14af8086
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:11 +0000

    thanks wrong average hungry squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
