69faccfd1220a123d76aec62c520f2b4f9fa1d1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:10 +0000

    roll later system nature point

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_speed(distance, time):
+        return distance / time
