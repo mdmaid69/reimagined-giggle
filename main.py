98f6a7023d6da47bb8dc435f15dbe1cdc1d6e34f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:54 +0000

    often 564encourage include twist breakin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
