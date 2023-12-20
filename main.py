698ed12abd76680cda572b2e405938566c6adced
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:20:50 +0000

    nowhere pay used settle fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
