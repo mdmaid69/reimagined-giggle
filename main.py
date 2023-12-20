d5e5efe09aecdf2c205034edbf30d090e4fa2ee8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:27 +0000

    track innocent worse close wheel

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
