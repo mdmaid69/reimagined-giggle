91670b0ecdbfdaa2d253abf10da8600536ab75b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:55 +0000

    how save sky dangerous collect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
