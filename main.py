dbecee7cc08ec8f08aeafe3568f680c43de3dcee
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:27 +0000

    receive equal healthy no old

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
