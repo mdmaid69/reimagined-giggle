f5aa002c3c2a9124e62640fe8fb08daa2f2a7254
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:39 +0000

    purpose Rr pause squeeze put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
