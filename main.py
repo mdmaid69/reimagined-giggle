3841d1888a95306ffcd14fa98a152d3d154fcdde
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:53 +0000

    all fortunately variety trick somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
