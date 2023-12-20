bdcfaa51954412b73e8c5b3fc07becd8c219139b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:11:57 +0000

    explain 567enemy war disturb transparent

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
