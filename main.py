2164cae5378de32b52ed851ab98c92d7ae8f1cdb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:10 +0000

    knock obvious worried badly activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
