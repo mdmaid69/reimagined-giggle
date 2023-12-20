9fa5214a1d5aee15c7eea7de319cab61c2b66f67
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:07 +0000

    chat mad yours watch thinkover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area(radius):
+        return 3.14 * radius * radius
