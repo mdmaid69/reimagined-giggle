fd40192991199236738fb74ed3d762e64d1a746e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:42 +0000

    public for distant sort if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
