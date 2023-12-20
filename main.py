8da8fa638b0556c3ad50a05d158ca4c45f1f21b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:20 +0000

    point coat similar answer choice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
