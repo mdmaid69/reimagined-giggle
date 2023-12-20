4c22247a5f0739051852ea15f3ab4998b6924465
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:24 +0000

    night reaction criticism reply comeoff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_max(lst):
-        return max(lst)
+def add_numbers(a, b):
+        return a + b
+def convert_to_octal(n):
+        return oct(n)
