e0fd7003edc5afc93e54d8e30cae3a6105f68d54
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:53 +0000

    seriously his body thorough fierce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_circle(r):
+        return 3.14 * r**2
