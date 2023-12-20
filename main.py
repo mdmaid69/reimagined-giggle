39c9ba53f3268e569cf55ada7bf8e1a54f5641e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:32:32 +0000

    no juice sting step experiment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
