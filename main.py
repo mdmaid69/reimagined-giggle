4cf1e6a50ac356d302c3a4dbf20ec2a0aa1179d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:47 +0000

    file arrange around sick 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
