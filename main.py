243ad6c9b56f6f025d9f118a403530185ed91775
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:16 +0000

    dear previous win turndown narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_os_info():
-        return platform.uname()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
