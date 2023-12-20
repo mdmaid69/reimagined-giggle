cca06f0d2ddf945f9ef5e3cc32bc7a7ffeceffcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:59 +0000

    wander apart tip interfere cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
 def calculate_cosine(x):
         return math.cos(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
