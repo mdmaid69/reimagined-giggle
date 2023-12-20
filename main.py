393decd88dbb136fbbb83de783a6995fa3432a5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:11:22 +0000

    demonstrate sharp careful failure maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
