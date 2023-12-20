cdb627f242ce58a68c58818cab225ac27871ff4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:50:54 +0000

    so 573entertainment drive alone like

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+print("Hello, world!")
