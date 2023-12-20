cc3c1c0eda184595660e4e2d5af0a2c6b1b43892
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:34:13 +0000

    handle soil custom judge protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
