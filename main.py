22aca114ffcf23462f919fac8a451d7e4b57e44b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:31 +0000

    the top grade include house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
