03aa17322b0da25d783152b85e75d762a89fdc40
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:39 +0000

    direct back opportunity fold guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
