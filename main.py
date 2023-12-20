2e65c5d369bdd4452fb67ae7b87b78e3d1e67053
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:10:26 +0000

    dance because train connection Ww

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def sort_list(lst):
+        return sorted(lst)
