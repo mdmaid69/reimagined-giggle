b0bb1f94c22909029da7e2c68812c07625446cb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:48 +0000

    blue tape freeze earth takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
