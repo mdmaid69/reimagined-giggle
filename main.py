dd04a6b8f17211d490ea7168e60fe1dd24d8a2fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:49 +0000

    except central impatient lock instrument

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
