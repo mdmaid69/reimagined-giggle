9a34c993eafa4b4b8e20d8a4bfe4092d2f5ba66b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:14 +0000

    wave plastic hard show blame

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-print([x**2 for x in range(10)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
