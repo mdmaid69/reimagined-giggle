018580a4d7076a3bcad9701a49561e87c98ed276
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:58:20 +0000

    command squash worried stare all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
