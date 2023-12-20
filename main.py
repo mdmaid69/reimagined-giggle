cfb607855d6caeeca602743b684f9df5dad58820
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:01 +0000

    tap use phone her Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
