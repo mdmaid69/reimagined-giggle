7fd66cf105ec7bdbf0577722fbf7d5d4265a6a33
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:28 +0000

    money outside belief present correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
