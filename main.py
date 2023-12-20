996c8eba5b30a81f2955818af411e95fd191ec17
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:26:17 +0000

    relax greeting splash unite bath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def set_array_item(array, i, item):
+        array[i] = item
