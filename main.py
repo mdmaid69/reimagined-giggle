c4374c84e1949294e327be1a65f8d32ebf753524
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:25 +0000

    term small broken ball expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
