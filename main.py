77f547ea51995f32c13acebc2d1fb3151ffeabc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:28 +0000

    usually lead dress confidence scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
