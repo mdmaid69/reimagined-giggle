cbac1511efecc8e454079ced8a4ff9ad34f60131
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:11 +0000

    direct tall may cut 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
