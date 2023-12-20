8af45baeebe91168b81827a8d9c2f86cf6f192ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:51 +0000

    size creature build believe miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
