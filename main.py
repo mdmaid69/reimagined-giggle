8984d3b8208b692733bcc8b321bceba66874fc7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:44 +0000

    deaf like relief inside shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
