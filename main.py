6bb27af4c6684b505a9b40fb1ba1f36165711e94
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:47 +0000

    software strong tense hold minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
