0b83c91dfcbc0aa86e318889ed8525b529afc5ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:10 +0000

    place excited statement force night

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
