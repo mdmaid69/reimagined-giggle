3d36b2923c1b5c8f289f223f7f43e099b56ee7d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:44 +0000

    quality obvious from project post

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
