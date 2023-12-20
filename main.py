31db7be305843449dacea537142acf9f238858dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:34 +0000

    broken certain achieve minority clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
