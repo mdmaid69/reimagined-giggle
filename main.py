efdba3c9be35064e0655b7dc7e4febd29a4be362
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:12:37 +0000

    pretend opportunity science supply error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
