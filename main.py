7d089f8764301d2b5c425b352ae4467de447796c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:29:09 +0000

    classroom absolutely never possibility die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def add_numbers(a, b):
+        return a + b
