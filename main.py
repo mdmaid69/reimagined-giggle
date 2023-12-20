bdd58ff4e82b1a83829d627a7d18e4f254c8a51a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:58 +0000

    draw path die activity count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_repr(array):
+        return repr(array)
