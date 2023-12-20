62e54d7e6707cb82a7aed971ba6ab7af095f9980
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:46 +0000

    suffer comeround decision hungry far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
