feec9b025e4fb839d2b641de90b6c6781a8a42a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:21:57 +0000

    young busy delight dull use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
