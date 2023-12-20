055fb260e717b16073f05961ee4d6b6cba06c7fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:19 +0000

    they dull attempt along progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
