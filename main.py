4ef5337ea2b0495d2be3c350e439ebf97e718382
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:08 +0000

    bottom face calloff apart large

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
+def add_numbers(a, b):
+        return a + b
