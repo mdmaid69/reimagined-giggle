071b80035e7104526f95476037b1b0f7cd1aac5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:53 +0000

    save lead quality even your

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
