36ed237ccae282ca2b62f9f29ff2c022d8bd5c22
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:51 +0000

    heat shrink wild black discovery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
