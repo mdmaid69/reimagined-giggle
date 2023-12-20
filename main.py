89f4120b7eb7b24e0e4f29cacc1fa0c127b1b24c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:08:52 +0000

    exercise turn Oo handle workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def square_number(x):
-        return x**2
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
