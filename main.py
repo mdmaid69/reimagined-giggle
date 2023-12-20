88a7d03923f15750e085e96ad384b1635e249b48
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:24 +0000

    mind round splash honest leg

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sys
-def exit_program():
-        sys.exit()
+print(datetime.datetime.now())
+import os
+print(os.getcwd())
