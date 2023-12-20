73d52c7b9f7eb3ca8400a06967e0a093063e3d48
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:03 +0000

    gentle shall regret lonely else

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
