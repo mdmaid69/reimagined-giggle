212935920dfb395e38d8368cb94dccb33ae190a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:12 +0000

    fear cost plan you contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
