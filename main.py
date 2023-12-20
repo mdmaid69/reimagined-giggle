3d2b1afb4d444987b0216a5282b55ef1110678b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:26:50 +0000

    pump pleased exist address excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
