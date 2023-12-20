c799993ac500f49f7073014c07ea87491f55c30b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:03:32 +0000

    say mystery collection land last

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Characters:", len(text))
