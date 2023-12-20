5e187a60a1e596fe533b1df6f346c82802ff7cfb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:11 +0000

    imagine Uu standard apply grateful

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
