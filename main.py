8a46b15aba8cb9275b5cb473f9ce6e3cde0546a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:16 +0000

    arrange ring rob friend discover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
