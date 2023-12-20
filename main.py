116579ad2870e91e88429ea7c5a4c82e9e070b0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:56 +0000

    allow likely sound arrange please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
