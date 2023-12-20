2adb7b32dfa9a429441abcfd63dc4f2773b980a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:37 +0000

    afterwards different them city ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
