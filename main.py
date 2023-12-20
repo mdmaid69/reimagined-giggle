150a228a00aba4ab801697c1b06675a7249dbe85
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:01 +0000

    the familiar previous usual refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
