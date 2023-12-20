cd6085d215a7d74c0d0788735c3a91d46efc1465
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:13 +0000

    flood expect frighten fond broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
