ed98ea05348135a78d2741c8d7af171fe422c570
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:08 +0000

    himself to square dish geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
