5928647b1e818c88f7deb7b85eecdcbdeb0cf01d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:43 +0000

    fond though lend happy dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_factorial(n):
+        return math.factorial(n)
