73f4506185449967a626a4da5964ddb06f9db940
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:14 +0000

    minority thanks fasten repair meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
