78789bb11d844ee9900ffa4f21ed04808095218c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:01 +0000

    show estimate line above whatever

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
