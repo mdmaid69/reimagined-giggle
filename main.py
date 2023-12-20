d8991f4a565bb3e9d31142db1d1a36585a77a86c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:31 +0000

    talk performance pleasure result hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
