a9185c1d678ddd28a8f676dc1a14062e727fcef3
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:31 +0000

    follow whether stay market comeout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_item(array, i):
+        return array[i]
