d616895c13e3b941262dc456eebd652abc584b3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:28:00 +0000

    relative related mass between radio

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
