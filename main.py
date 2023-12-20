9aa2a1b74c5e69afcceedf64e5d3014ad6051a43
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:53 +0000

    translate what themselves human perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
