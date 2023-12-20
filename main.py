6645f95467112bf41577792c5cc7a6e05bba593d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:51 +0000

    myself birthday tell want address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def clear_array(array):
+        array *= 0
