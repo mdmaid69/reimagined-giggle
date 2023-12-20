98eddb288e20f26bf9a3cc629f80830c2d7e93b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:02:35 +0000

    bottom store prize rough this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
