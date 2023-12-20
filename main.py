246df1b57cafdb6a558e0df0a18a6c5d8b79e0c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:29 +0000

    address dare ball field relax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def clear_array(array):
+        array *= 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
