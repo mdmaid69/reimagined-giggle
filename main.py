f127d395a246cc9e7bff42c6941086664d71dd77
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:41 +0000

    petrol house neither drink consist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
