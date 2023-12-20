367f48e8820cf084a479ffb3666599ac6e7b1731
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:28:51 +0000

    tie challenge once later regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
