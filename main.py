dad8ab42178c95b351aab05aabf383bbff1df7cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:47:50 +0000

    fold breakinto thorough offer set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def append_to_array(array, item):
+        array.append(item)
