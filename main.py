cb09fecf623dfb55aae2339e0e51ecf68210b85e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:05 +0000

    spite slightly low top twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
