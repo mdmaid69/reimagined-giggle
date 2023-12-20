a1558dcaff8d47472a9f4e10d8b5afab36c9d2ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:52 +0000

    sore doubt bother vanish defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
