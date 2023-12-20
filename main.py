c4835fdc842641bc3505e181298761127d42659a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:35 +0000

    be often excellent school can

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
