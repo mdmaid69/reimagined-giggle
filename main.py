dd26c9ffa82405537a13fd4dbf52ecc078053f2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:18 +0000

    shrink beyond drop hurry forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
