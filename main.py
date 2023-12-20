9cdd718a2c9ceaf6a5e3c943650f6955280e04eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:03 +0000

    service calm bored human breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
