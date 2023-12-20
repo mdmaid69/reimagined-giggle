f7792d191f8674f4fb09e8bec4288fc3f26504e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:04 +0000

    emphasize hear loud create heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
