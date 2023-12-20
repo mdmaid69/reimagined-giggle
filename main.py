843b3b763b50902ae1eaad7e8e0fcdd622172aaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:32 +0000

    watch dinner thinkover comeback every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
