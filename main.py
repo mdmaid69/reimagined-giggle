f846d623ec50d0795080eeaed75557abb006e43c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:53 +0000

    official wave shelter wet embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
+import sys
+def exit_program():
+        sys.exit()
