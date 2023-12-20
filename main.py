92de5f5a5d4d64ed61599841759e1cef53e65911
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:28:14 +0000

    successful rule plan dress innocent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def count_words(sentence):
+        return len(sentence.split())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
