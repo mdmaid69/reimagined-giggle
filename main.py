b89806a2f9466fe67baf4620709f78e87a05b70d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:30:19 +0000

    weekend rank excuse stretch formal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
