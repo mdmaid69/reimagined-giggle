86e82d762fa464a012a7ebb416a6c6404a31a1d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:01 +0000

    thinkover possibly area closely room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
