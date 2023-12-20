9abb2beb789597a477e6189d42deff28bf572890
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:09 +0000

    fix scrape sad roof add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
