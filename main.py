07238054761b5d5a0ec5a7ccce70af4fe047fae2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:12:16 +0000

    condition dry official manage swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
