34ff049a39716265476066c7dffe18d47f92f77a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:50 +0000

    surprise anywayanyhow book left cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
