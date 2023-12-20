538cafa742b68c64ecb773b4f6c875778fcbcd7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:00:26 +0000

    refer love risk cost much

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-for i in range(10): print(i)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
