b462759e11dc8c1ab469705b769568b0cf9fd942
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:11 +0000

    play transparent own somebody play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
