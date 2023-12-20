21fdbaa9ff987d90b3f944a7881b634137ec73c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:51 +0000

    connected somebody tip offer obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
