15adbd8d93889fc1e89ed7e00cf33f89e88840f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:53 +0000

    run wish anybody especially rough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
