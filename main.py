e83c6c051f5f251771056aa6e641844a67227436
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:30 +0000

    later rescue busy whenever muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import sqlite3
   def connect_to_database(db_name):
         return sqlite3.connect(db_name)
