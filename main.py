e112cc1af7749e2a7bae2f47c48fa4f545cfa48f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:28 +0000

    attach getback often mother start

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
