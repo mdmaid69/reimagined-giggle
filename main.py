268d353dc214105097c3598b96fab41ad331af0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:02 +0000

    preparation protest method minor order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
