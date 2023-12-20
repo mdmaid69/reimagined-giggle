9ced559526116d7f5e73db32b3408ba049b3026c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:08 +0000

    operate fix burn warm opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
