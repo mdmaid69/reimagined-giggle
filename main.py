84b8c9e979a9ff853991b8fffb8226140e792004
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:51 +0000

    speak afraid stop on exploration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+print(os.getcwd())
+text = "Hello, world!"
+print("Reversed:", text[::-1])
