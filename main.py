b917540d707743e7cd0afbb4a9913a5a0d5533a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:30:39 +0000

    false minor window as glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
