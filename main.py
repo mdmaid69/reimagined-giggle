3ed5230b94fa1616341cfed28ea33b09bcb94f7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:18 +0000

    powerful shoulder bad actually ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
