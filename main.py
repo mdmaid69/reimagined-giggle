45e5e201d8696e581a8f15dc9197d7c5073e3e65
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:51 +0000

    turnup bet about except quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
+def add_numbers(a, b):
+        return a + b
