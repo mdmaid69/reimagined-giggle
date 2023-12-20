31643f4cc41be8515bcf7eb31c3f1bc964a60d9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:15 +0000

    light spend announce quick bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-print(math.pi)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
