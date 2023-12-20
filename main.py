5ce66e94042e37da2eda5ae441c52422d6976437
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:48 +0000

    practice late flow wash keepaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_density(mass, volume):
+        return mass / volume
