1a002c9af933ae0fa025fdb8a0d4221e7d83920b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:48:00 +0000

    thankyou excited display false show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_priority_queue():
+        return collections.deque()
