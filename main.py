262a37b5495092ab5b84aebbca277717e6069b9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:41 +0000

    across certain along uponon so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def cube_number(x):
-        return x**3
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
