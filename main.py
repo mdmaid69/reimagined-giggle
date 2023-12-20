9576abb588114fa987e5b4df25ad735da019e8f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:54 +0000

    pinch know lie influence should

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
