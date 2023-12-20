7bb019ab98dc1f45384deb2c0a75aac992f559c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:00 +0000

    fasten university section light mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
