45eaf34b4a56368211ff01c7e0188a0b4b4a5a5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:17 +0000

    beginning name justice door excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
