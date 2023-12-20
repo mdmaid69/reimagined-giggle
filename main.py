3e09b7b540a3d2ab4a812320cd234004616fa16b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:27 +0000

    formal weekend 571enough exit dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def greet(name):
+        print(f"Hello, {name}!")
