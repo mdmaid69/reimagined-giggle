2f81c405cf685f671ab6bdc380593acf4c5831aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:51:39 +0000

    separate lead avoid off callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
