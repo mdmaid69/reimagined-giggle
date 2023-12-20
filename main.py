8d0bc11dfd471c6f8489f2ff956af05da2127bd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:59 +0000

    I calmly question Kk neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
