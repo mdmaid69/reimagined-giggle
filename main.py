68995797bf10acecd6f8217a38bfeaca3890bda6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:03 +0000

    power splash they building flow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+print("Hello, world!")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
