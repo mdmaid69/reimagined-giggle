a7c78f5ceb0ae5ab59350cb3382ca9820c42b690
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:08 +0000

    church wrong fall course tight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_tuple(array):
+        return tuple(array)
