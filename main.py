6ec09dc29eaf06e065a196c88eeaf166c6f7083b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:38 +0000

    write cross morning hollow winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
