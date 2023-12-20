5bbd167644bda1e30f3b89db4a5ec94d3420e913
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:57:46 +0000

    noisy callback shape base try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
