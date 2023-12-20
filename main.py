6f48c476cfe1ca8d74712f8198a2784e244df343
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:39 +0000

    blue holiday allow modern have

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
