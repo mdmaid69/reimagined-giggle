860a05c1c07d8d747d7c04bee25c9854c6badb26
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:11 +0000

    talk importance plan like permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
