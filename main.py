f30b86663549a1a48b943805485515820ed067b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:35 +0000

    shallow believe lose home cause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
