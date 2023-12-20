7374ab55dc1464232905067f94bf2df2bdcba6e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:12 +0000

    present belong increase reasonable catch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
+def convert_to_octal(n):
+        return oct(n)
