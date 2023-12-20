ad4d0e6e6b130f918f325f184c9e81346da7046c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:43 +0000

    paper it autumn performance tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
