779b2c10242cb2c16c74aee8863643fa3506468e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:33 +0000

    top common memory keep remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.time()
