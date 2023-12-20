4c158d785f56da9357e91acf648e248b05934040
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:54 +0000

    sound head appearance original complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
