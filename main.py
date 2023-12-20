ba271298098e81120525f05edf5781d25cc98fd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:17 +0000

    will pay complain from comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def set_array_item(array, i, item):
+        array[i] = item
