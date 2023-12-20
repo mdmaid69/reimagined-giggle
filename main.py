da879d6262b835df713e0849f7593d84d507bfb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:58 +0000

    thinkover exercise absolutely schedule turnover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+print("Hello, world!")
