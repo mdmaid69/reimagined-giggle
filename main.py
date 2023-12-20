486a03edffe7af02a541350fe60b897f4ffda26f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:19 +0000

    bag pretty try mess with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
