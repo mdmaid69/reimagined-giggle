992558931545bce75cf0971382d4dca1520f81a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:23 +0000

    write schedule furniture average survive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def convert_to_octal(n):
+        return oct(n)
