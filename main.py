fcc30c0d82081ab40534f13eb7cb20bf500c39a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:19 +0000

    runinto unusual near pleased discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
