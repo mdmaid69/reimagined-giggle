ac60a91ad13894ae0470d9024c121a7c6006cf1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:01 +0000

    fairly pretend dare wrong although

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
