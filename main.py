0078958f9d7b6fc4c896790a605425c85df2b12f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:17 +0000

    few avoid touch dangerous dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
