55309f81d8bcc8cb5c4033d923ff1070c13dbd4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:08 +0000

    Bb ahead say later sign

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
