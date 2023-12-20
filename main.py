777b9c63258f15b634baf82debfb05729484d399
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:02:57 +0000

    train deal bet fetch find

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
