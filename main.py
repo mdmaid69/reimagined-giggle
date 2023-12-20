ae6d4dcf6d1c1d17a6eaebb4cab3d642a602d48b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:56 +0000

    brain wide custom politics could

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+print(time.time())
