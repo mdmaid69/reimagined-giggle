b14d4e06dfdf7096e22d3a12ff5117497c83cf36
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:08:50 +0000

    try quit hold record comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
