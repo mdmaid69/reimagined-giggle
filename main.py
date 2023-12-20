ceb4d5bba15aee6dfaf6123f35190e24bd148136
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:14 +0000

    shut table since spray as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
