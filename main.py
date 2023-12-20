2a08c489a30fc5e78953de4ed6f816a6d36df1ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:03:35 +0000

    visit poison invite later notice

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
