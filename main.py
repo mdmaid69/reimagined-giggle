f19c9e90b1189ce1659d8abbe121a8d90ec21a85
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:24 +0000

    steam forget knowledge relationship closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
