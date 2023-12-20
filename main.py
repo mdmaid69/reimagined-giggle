ff3f957ba978c658fee490a7b02003d6cb4c821d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:52 +0000

    bill accidental night official answer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_power(work, time):
+        return work / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
