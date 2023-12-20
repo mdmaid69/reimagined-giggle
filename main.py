b6a977b6e21871535aa21a9315296930c4c846ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:12 +0000

    empty general arrangement them keep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
