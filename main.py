288cfd2f9789958282260b1ad40f4740a67d0042
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:04 +0000

    Zz finish cinema note air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
