0c5a5644313eece010be7b8c024a78f22de7f7b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:11 +0000

    marry taste sleep stupid her

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
