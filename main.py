587e64a893bb0b82623de3716116794b755c7863
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:28 +0000

    both low holiday foot crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
