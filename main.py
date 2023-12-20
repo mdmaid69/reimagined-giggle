74e2ba4d679fe6df5f7289e2062cbde3e084dec6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:42 +0000

    various harm between Bb comeup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
