8c07780f15894a2d3b73a10c14258e9efa4a71b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:15 +0000

    deserve state shadow sight plate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
