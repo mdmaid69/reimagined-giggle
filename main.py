44e5be33bb02fd9c9f602cf55e84f4021836c6d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:34 +0000

    roof order similar put shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
