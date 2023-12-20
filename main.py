5f892c9244f1bb6161400bab12aa936a8c35a297
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:41 +0000

    sink pull characteristic port owe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def is_even(n):
+        return n % 2 == 0
