e40e7d2c37651cec638a1699cb7936a3ea408ea8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:33 +0000

    least into inside sale worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
