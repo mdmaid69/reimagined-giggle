6054ff893f89dba3f37827623cdad34a4c909142
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:21 +0000

    defeat favourite sale beginning pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
