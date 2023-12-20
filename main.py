4b9a567bc26fce2a37de099c3b6cab0805cd1c3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:31 +0000

    exchange account centre towards convince

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_acceleration(speed, time):
+        return speed / time
