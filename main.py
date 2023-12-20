16e282190ce890b8a343aeb199bba0b2957d5448
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:05:03 +0000

    getout probably holdup Itself home

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
