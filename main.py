5dd1aa279811b47389941ca5164102acf290af25
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:53 +0000

    dinner regularly bank repair imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
