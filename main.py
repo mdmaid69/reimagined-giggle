696dbaf43b57b0105f5c812e36b0ac8de87c07f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:34:33 +0000

    grateful in run travel ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def multiply_numbers(x, y):
+        return x * y
