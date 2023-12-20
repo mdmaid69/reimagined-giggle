10bdc2d4a2e5fad4c82c7471743430a3dfab2680
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:33 +0000

    building about we regret model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
