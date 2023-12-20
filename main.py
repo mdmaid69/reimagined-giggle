5f74b2ab3a0d6452674b760ac03a155a7119f6d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:51 +0000

    influence unkind Tt quickly step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
