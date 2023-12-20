39f84da7ad3592eb578c1dffaec76e290d098099
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:25 +0000

    suit meal bang pay scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
