0aa1d7789bc3c11b5ce4fca1c08240a785958c6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:11 +0000

    friend priority in close affect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
