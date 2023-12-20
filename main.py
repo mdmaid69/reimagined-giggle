a10a92fbe3c2dd10e4893b69b000e8b5b596068b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:27:07 +0000

    trade adjective correction shame defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
