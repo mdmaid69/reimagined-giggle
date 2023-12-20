9442a7f905924e1a2ce29ff9bb8c3f29ab6bf26e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:52 +0000

    government mass piece pump dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
