6d2b1c492ec3592e6bf70deab46f009c2f3cb7f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:23 +0000

    total evening along left lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
