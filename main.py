38a220e2ffa0e653f9bcbae9489925b9cb781587
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:27 +0000

    communicate below attempt wise of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_pressure(force, area):
-        return force / area
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
