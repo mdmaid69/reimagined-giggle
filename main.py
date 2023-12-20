efa32e8783f40cbb868ef7737857d0f9b3f1533d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:16 +0000

    factory month stiff attach divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
