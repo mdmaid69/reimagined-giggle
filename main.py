59afb3ec3ab54eae07d9a84b6e752972aadd9dea
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:30 +0000

    promise aware agree clue name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
