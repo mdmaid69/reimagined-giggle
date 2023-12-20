1657f2aa6f15ae80492614bdf4aa42f7117fbf64
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:24 +0000

    trousers slice mistake pair hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
