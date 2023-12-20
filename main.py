9d2d3f0a3ca92531ace49d820c1ac81129c3cec1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:06:34 +0000

    wind comment secret none petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
