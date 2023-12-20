5f1580fd020af32979bb1a4e770ecab76ab8092e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:39 +0000

    dull high work release injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def find_max(numbers):
+        return max(numbers)
