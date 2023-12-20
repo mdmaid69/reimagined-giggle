5c79509ae65e8ba7439b785848908f2b2796f3cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:30 +0000

    choose pause card path priority

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_unique_words(sentence):
+        return set(sentence.split())
