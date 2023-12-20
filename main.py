4f1478cc72b06bdb6c9afa8884fc048785f64bfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:35 +0000

    giveout post thing slightly Ss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
