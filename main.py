82ac24f5666aa8cb44036a21429a3586f6fd6a70
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:23:26 +0000

    watch failure chance whenever friendly

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def is_even(n):
-        return n % 2 == 0
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
