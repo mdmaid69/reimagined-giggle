04bb17c5b81c90098a13fafc5d9c3c661e6f1a82
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:00 +0000

    practice help afraid ahead head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
