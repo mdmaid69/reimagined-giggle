4f8484687543604360fa3c3c1f84100bda59e37c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:09:23 +0000

    so first deliver both convinced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
