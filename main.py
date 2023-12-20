5954806299df2570d3a1a369898057a6cd4a6b1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:15 +0000

    bet throughout change so Ii

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
