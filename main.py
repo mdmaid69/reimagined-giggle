254439d5a8cf78f3622da7293b595611f86ce716
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:31 +0000

    window space absolutely careful cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
