2bdc760b5dffd5528818acdde0ad33295cf1b511
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:52:17 +0000

    hungry file market stupid agreement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def reverse_list(lst):
+        return lst[::-1]
