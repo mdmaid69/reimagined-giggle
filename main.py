dd05c22daafe13655eda9aededb4040714054c86
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:01 +0000

    unfortunate opposite load ground slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
