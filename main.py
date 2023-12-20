426fbdd70dea59fad0e9480730d9b865e35db2ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:58 +0000

    circumstance always duty fault definite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
