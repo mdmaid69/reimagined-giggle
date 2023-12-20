67ce9eb43e58f8da7f85cf06c8204018df368520
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:42 +0000

    Gg sweep sure keepfrom abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
