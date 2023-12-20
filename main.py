6956d267d51060cd0a2a6f48ae0237dd0247ce0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:14 +0000

    defend swim ticket spot quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
