fe52efd5cd8e8058db7e69b969062bda8211bfcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:13 +0000

    goout telephone picture hurry skin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
