e434af27342bc5af18d2f2bcbd7d94e49ab19f89
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:04 +0000

    right rather float lookup basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
