f8811ccff71056fee005c2c5696a989d2cef64a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:31 +0000

    friendly community study statement extra

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-print(math.pi)
+def calculate_sine(x):
+        return math.sin(x)
