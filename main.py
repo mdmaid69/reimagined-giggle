c2e18821b7f651e9b0f977d97bf82ff894fc857e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:47 +0000

    sorry phone neither loud count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
