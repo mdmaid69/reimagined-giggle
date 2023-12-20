db10aa9603285d3634ac7792fcf293407606f3a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:32:00 +0000

    quickly concern unite lead post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
