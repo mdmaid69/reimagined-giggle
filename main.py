e48562221275e0b2c542afe23b7c1583010d36f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:24:00 +0000

    close effect under tense total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
