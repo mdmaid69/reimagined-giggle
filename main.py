125c070e71d7aaf64143539e0035e9b3f78886dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:32 +0000

    real produce deep settle study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def get_array_length(array):
-        return len(array)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_array_as_memoryview(array):
+        return memoryview(array)
