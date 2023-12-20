00ccd0532c59e5eaa81964665d7c4142f4a648d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:41 +0000

    getout slip mention track pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
