45d5c5b26066ebe16dd99a2926103d46d85853de
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:34:24 +0000

    everyone argument recovery too of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
