2f9eb4d2ee559945a9e9645d97d096ae75df0bed
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:56:26 +0000

    pump build period surprise possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
