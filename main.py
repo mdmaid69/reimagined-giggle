c06ac2da845d5746f6e7b57996bf94316631f7b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:09:49 +0000

    convenient sit breathe forward healthy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-for i in range(5):
-        print(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
