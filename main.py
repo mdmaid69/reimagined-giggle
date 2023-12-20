4222e3694122a697638f9eaaa1dca0cf9cb1ebe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:27:54 +0000

    keepfrom afternoon much harm answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
