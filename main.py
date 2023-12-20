95fcb50959fe9ab53a991b4df6776c8a3697b5ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:51 +0000

    move belief voice tomorrow act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
