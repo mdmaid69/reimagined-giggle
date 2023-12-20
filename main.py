00a08eee3078dddef7c0680aa50add8414daa472
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:08 +0000

    heat record last surface represent

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
