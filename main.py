fcaabf7875c25811d4ffc15ab2f4100e1bcd4ac5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:08 +0000

    fault proper present dear push

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+print("Hello, world!")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
