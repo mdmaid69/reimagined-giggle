6edf167eafcb4bcf7b443224ad0a1cea05307323
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:25 +0000

    number view possibly whoever accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
