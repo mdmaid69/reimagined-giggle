54dbcc97a9e5a199f7388aa63e4bc52f8e5e8a46
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:23 +0000

    go move section rent throw

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import sys
+  def get_python_version():
+        return sys.version
