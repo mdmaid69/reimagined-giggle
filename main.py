09d03d43d260adc2c643eb90cce6f6661b5fac67
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:37 +0000

    safe progress immediately city disgusting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
