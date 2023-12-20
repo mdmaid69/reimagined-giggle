9ae285fb7c6c2bec74db14af6224d30ba4329c01
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:01 +0000

    voice none brush issue prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
