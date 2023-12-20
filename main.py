cbc2fa73d7727762e6f1858bf7af61d93a5a6a4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:08 +0000

    probably inform curious tip spoil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(lst):
-        return min(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
