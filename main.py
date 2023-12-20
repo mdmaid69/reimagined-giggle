ce833189f53e1b5036fed51ce69412fa26a14612
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:43:15 +0000

    silent gate middle proud beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import sys
+  def get_python_version():
+        return sys.version
