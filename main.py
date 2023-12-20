230ce374a4732e3e82a49342e474341727b8e1fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:31 +0000

    opportunity clear confuse properly load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
