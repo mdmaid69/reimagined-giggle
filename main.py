640eaf110f9a6c9563b73a056bdfd6946412d555
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:07 +0000

    discuss thought trousers pain burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
