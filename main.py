c59d2881000f6f6df65afac31da2103b1b02186f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:51 +0000

    female hurry like plan fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
