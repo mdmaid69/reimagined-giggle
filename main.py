e601b8a9d33d907ef8efc3a605269b79cfc4a481
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:15 +0000

    concern cover prepare wherever keepout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def sort_numbers(numbers):
+        return sorted(numbers)
