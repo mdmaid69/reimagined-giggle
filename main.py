f3d4344838e32f1c5e3c6eed2618a761a85d92ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:30 +0000

    principle spite foot tend pin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
