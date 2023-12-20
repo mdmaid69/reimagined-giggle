46444e70a606aa9011ec84fe95f3854de1b54b6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:20 +0000

    challenge scrape last judge cheap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+name = "Python"
+print("Hello,", name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
