411db443d9d4dd456041bc6658e40f526569ed4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:21:06 +0000

    protect get warm wise bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
