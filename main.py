967baf120dca6718ebb8c227540d9d4bb5e2eca5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:25 +0000

    healthy maybe admit tidy connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
