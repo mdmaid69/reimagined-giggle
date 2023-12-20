3806b4f37c0550cbf73320d37814a219bb6a0494
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:19 +0000

    talkback market before amount further

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
