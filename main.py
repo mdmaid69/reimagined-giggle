c524493da45056dafaee78c06cdcc29ffc001be2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:49 +0000

    upstairs complain group pronounce noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
