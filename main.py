266ddd29cf838968a409f3b1fe068011e183225c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:55 +0000

    receive radio design early so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
