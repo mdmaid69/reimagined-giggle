7854f1b55a02263df1a27702b9a2c41a9bd431bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:33 +0000

    rest easy persuade chance mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import sys
   def get_python_version():
         return sys.version
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
