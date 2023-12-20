daff4cd418db64c11007f9e8733271c9772e8b77
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:50 +0000

    accept crazy protest couple wealth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def remove_duplicates(lst):
+        return list(set(lst))
