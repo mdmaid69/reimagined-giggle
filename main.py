13fae94f8d393912c112dd96a30fe69c7b3ef879
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:18:43 +0000

    frightened behave thirsty business suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
