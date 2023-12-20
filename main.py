8def37482125f972090de66e63486be3c42a18fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:14 +0000

    Vv special appearance open large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
