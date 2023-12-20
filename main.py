ef9e44adeb6d4a600af9ba6eb166d110d9f2eed5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:18 +0000

    idea run turnoff shout state

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
