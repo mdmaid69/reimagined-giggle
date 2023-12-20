6d712b36c0c0b90685c2a3e4a72a5ec6813066e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:30 +0000

    nearly touch song steady area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
