027426a252fa509c458a7e71e86d47920dbf0cec
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:12 +0000

    eye news loss near mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
