36b8bf18b6c410479e7bfc6401b7614edb7a2e74
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:03 +0000

    manage carry confuse phone male

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
