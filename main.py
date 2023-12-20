c86dc8f4d8cf9739cc7d1ca8646e838f7a694720
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:31 +0000

    spoil choose talkinto bear his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
