32b7240f7c119d8d84ef8cc9533c81450955758e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:49 +0000

    call project success suit distant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
