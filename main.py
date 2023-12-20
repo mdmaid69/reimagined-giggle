abff90a3f30a3d8619a945f9ec9aaf1f6dfb5a71
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:29 +0000

    pure rest mix list morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
