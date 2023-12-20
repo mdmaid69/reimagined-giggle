ae952bd6ce568dacd0458dcb23f474b1e6b03e84
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:55 +0000

    pull afterwards match post fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
