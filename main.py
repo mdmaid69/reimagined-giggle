ebfff58f87d23667a982226ee09646548507457a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:43 +0000

    unit week result clothes gap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def is_even(n):
+        return n % 2 == 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
