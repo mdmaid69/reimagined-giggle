38893d63889ef699dc4b0227dbb188c6000343a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:56 +0000

    572enter advantage believe tidy safe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
