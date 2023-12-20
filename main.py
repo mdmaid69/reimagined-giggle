3badabdfd47a8dac7ea31d8143402a2f7933d662
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:41 +0000

    card praise run kick tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
