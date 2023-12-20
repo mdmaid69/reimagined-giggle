14a13ccff04a632ee8a044708e4d24f3554af543
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:30 +0000

    571enough drive nowhere promise check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
