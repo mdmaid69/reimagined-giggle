79941723dcc4884945d50119d0d8f1813787b895
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:21 +0000

    dinner stage Cc task rain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
