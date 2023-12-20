75b6515bfce4114a1dd6bc2ae463f13af3819c2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:32:42 +0000

    fork approval sweep suddenly spend

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
