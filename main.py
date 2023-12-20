59eb5eb930f0af0f2b9a40b3584c43a2b7707dae
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:07 +0000

    language piece bad look Qq

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
