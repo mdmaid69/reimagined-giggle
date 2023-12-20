dea2728e830af87c491154436b176e216c9f684e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:47 +0000

    remain habit back popular future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
