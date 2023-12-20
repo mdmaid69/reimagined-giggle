8c85ed1967f5d4858c344b881a98ce551e3f7218
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:21 +0000

    business top principle father advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
