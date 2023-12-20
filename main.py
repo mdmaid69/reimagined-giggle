499cc3a45d1fa77e1a731307da4fc9b36e9ef466
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:33:47 +0000

    plan ourselves cough crime recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
