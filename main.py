1994de242af4a2c0e6f1a7eb671c9c23fb796417
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:47 +0000

    point present illness far sale

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
