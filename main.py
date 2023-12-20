c3c1b24c047d9298fc778feeb1c44e3aafdd3e38
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:23 +0000

    drive environment spill question idea

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def subtract_numbers(x, y):
-        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def sort_list(lst):
+        return sorted(lst)
