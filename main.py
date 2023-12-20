3deef4297a122ade98daae29080c84a92046edbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:47 +0000

    concentrate principle then tone accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
