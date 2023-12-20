7ca622ab6459e10904b23146ad45792ec14ed301
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:18 +0000

    scrap traffic rubbish treatment bound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import random
-print(random.randint(0, 100))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
