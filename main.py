e829535ef1e36b24b6f2847ec325e6db657d0714
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:58 +0000

    pretend use stone produce partner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
