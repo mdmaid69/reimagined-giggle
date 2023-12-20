bed4d6245208235484f5b4db3ca8f9e0cda041bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:11 +0000

    Uu uniform what or burn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-print(math.pi)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
