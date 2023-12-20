1298ed1c9d3fc8f91e20cceec2ed184888eef10b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:51 +0000

    pocket develop set speed behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
