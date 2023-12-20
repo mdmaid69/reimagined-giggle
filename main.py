3f4465db57177120d095b1123935f13467616db8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:51 +0000

    young battery change food on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def add_numbers(x, y):
+        return x + y
