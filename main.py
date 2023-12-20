fb043ed350f8937fe97b5895a7b5a6217cbb2c16
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:45 +0000

    help period nothing complete compete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+def cube_number(x):
+        return x**3
