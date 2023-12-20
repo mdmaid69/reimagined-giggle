b4ccb7cbc737a055b3c476c754136c14572541c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:30 +0000

    hour later square dick fair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_characters(sentence):
+        return len(sentence)
