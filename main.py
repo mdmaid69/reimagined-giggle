d1fd57b2d9374dde64eab3b9709434bc1f2ed593
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:45 +0000

    activity issue rather run face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
