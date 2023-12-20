648c6bff723560a7909e53a4cf7f5b42ed9233e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:14 +0000

    tradition trust star read surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
