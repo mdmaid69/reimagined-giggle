41813b432fc7285de770828645fac3adf2c17bf8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:56 +0000

    appearance pen name part onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_array_length(array):
+        return len(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
