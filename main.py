628cca7fa846bf57fd530d6c041e5beece4ffcd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:58 +0000

    die probably fact guide interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def convert_to_hex(n):
+        return hex(n)
