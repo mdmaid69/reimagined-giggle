1246581f7ee906014e41015716e8fe1d97241990
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:10:05 +0000

    turnon opportunity contain dish still

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def convert_to_hex(n):
+        return hex(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
