1c8258c4eda055288d8bbc3456bc2e4c770d0e00
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:38:22 +0000

    attract embarrassing earn really such

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
