3669572f37d3683bc8df5a2508ff94be6156324a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:13 +0000

    introduce study load straight tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
