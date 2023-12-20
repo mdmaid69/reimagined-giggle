729a413bfa5061769e3390d943629c8dfe7ef7a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:26 +0000

    artificial wheel bend story whoever

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
