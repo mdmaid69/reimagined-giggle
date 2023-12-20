524063565a0165467a1bf7f495478f631ad05f8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:21 +0000

    home club ordinary female ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
