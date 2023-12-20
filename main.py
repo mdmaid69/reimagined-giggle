069e148d6d3bb6aa3ce6ef4ba9b6b6e325ed7e7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:36 +0000

    obvious because hand pity block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
