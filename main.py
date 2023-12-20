a13e5397b4521ec3a33112c07f69b90ddcd2430c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:42:18 +0000

    flood difficulty brown rain basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
