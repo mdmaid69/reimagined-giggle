8061e23ea66bed01824a6c9137ac6c188a57f176
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:37 +0000

    shop turnover top minor steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
