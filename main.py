930a89ccc4dc6cf73a813d1cd3c431c49194b159
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:43 +0000

    tread fire put tap drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
