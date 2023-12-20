e16ced19ac1c8bb9d9c57b52d40f30fd2acc0b31
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:54:09 +0000

    sentence country comedown brother general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
