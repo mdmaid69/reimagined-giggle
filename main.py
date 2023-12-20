d1f5b3cd539eedaf6a9dbe9639a8592af2a6b5ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:34 +0000

    feel Ss prefer arrive may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
