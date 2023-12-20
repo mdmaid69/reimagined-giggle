e0a2f4b5e37bca9b9a6cb2b195e4f4ffb55b2a74
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:20 +0000

    turnoff try trade uniform spite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
