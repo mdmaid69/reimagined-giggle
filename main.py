5e50abb2e8f075776138db9c6f67614746c83dd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:30 +0000

    fine duty any worse little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
