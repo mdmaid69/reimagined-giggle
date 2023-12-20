cb5af534243ed2c395b4f8be0f24874a7d823285
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:50 +0000

    bush 572enter fork mess mess

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
