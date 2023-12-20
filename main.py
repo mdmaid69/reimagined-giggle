bee5c5975b069df6dc422103c7ec5ec41efb890f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:56 +0000

    lock mad characteristic boil walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_float(array):
+        return float(array[0])
