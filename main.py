5a27e3d30d58c90ea0cade712fda665aa7b26015
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:25 +0000

    compete weather die smart cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
