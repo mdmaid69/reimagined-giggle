f739ea498ee3e1f9572c5863f52122ef584cd53c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:19 +0000

    shut confidence decision claim sell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def cube_number(x):
-        return x**3
+def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
