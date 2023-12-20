f2a348353608553d8d3a91c7e613d6d5575b1d0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:07:28 +0000

    print grow person offer rate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
