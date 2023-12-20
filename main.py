ba47e2d0145643abdf5f07e48178dce166d6c51e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:11 +0000

    keep unfortunate cause last correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
