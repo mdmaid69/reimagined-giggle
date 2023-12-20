c42daa0019c3e8711c016d81c70541c2ba10286b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:06:05 +0000

    complete beyond tune shop cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_floor(x):
+        return math.floor(x)
