e4f8b8e00cc9e2bed1e479345d01887e0d1c300c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:43 +0000

    number lay keepoff thank used

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
