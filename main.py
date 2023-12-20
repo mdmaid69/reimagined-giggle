7c1f0e6e97cd62d0f037da1ed40e9fcb1c3cd08f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:40 +0000

    attention never sensitive future aim

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
