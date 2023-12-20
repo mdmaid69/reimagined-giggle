5a6f1885ffed3179ffc130066938b45d40316a6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:58 +0000

    willing consider hello when store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        return sys.version
+  def sort_list(lst):
+        return sorted(lst)
