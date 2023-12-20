bc2210cdb06c642a2012f753dd65d05c8c7f7b28
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:49 +0000

    real whom win should confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_absolute_value(x):
+        return math.fabs(x)
