b0b5411486428591f36ca7a261a52f56b44d1095
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:09 +0000

    cost himself dull key must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
