ed4d123780363a1b1a5f4a04d0d505e79860d9cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:04 +0000

    argue especially matter pity certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def convert_to_hex(n):
-        return hex(n)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
