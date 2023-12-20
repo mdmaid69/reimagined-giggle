6575e107987882381156e64e00d50f74ba23ae8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:11 +0000

    fast common whistle mind lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
