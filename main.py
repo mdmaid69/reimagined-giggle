4aa0785a57d06ef82dfd883de00ad6d3699fac23
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:57 +0000

    claim nurse suppose violent plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
