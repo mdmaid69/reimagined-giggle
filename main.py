37f8a36422220661f20498f315b8900ea96bbc22
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:57 +0000

    hope surprised custom blow scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
