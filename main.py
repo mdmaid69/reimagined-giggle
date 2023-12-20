447132905ceb45d4b1593867c5d4e1aac5ba453f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:01 +0000

    comein opportunity generally yourself justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
