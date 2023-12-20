146d6dc2bf2ab7b3ca98bbfcc4e1b183e7dc09cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:05 +0000

    tear slow Cc available exaggerate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
