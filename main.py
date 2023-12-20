6ab8d17d0d9b08511ae88b61135d13923de7229e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:32 +0000

    may ordinary it business fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
