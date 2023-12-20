ed5dc35f27eed3d093e3ee8ae301ff850cf1d199
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:42 +0000

    question independent dream extra order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Words:", len(text.split()))
