e625191281b8bdb14eaa955ec6daf5a8ca4fbc5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:24 +0000

    original colour prove present score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
