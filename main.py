1460f24e1e62eec80b54846646ac9e3bde364fe1
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:08 +0000

    relative depend cope neat commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+print(re.match("h.*o", "hello world"))
