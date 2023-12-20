aff0d351a843f34ac6cac2912e9903108d21927c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:41 +0000

    invitation position intelligent comedown damage

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
