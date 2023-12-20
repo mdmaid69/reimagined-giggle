5e57ee6f72ab6ede6ae25fa9e5f8568126c6f20d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:41:30 +0000

    supply feed neither keepup fall

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
