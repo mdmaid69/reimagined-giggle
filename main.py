96ce8c57ed427af4df08a63a7cbe76d5708d2e5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:52:10 +0000

    tear wind bright prize keepup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-print(sum(range(10)))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
