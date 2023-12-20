2dccd0c9bda657082973da273ab54a576288590c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:24 +0000

    too information advise when store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
