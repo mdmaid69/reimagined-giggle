946497ff4f22ca787c77906f977021d9dbd1d6a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:40 +0000

    strike live union weekend father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
