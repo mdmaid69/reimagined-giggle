b61dbf0c42b7b628c2fba1acb8fcc575a4b9f1cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:25:20 +0000

    forget imagine tooth every square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
