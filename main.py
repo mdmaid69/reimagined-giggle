ee885aceaa89288dd37e7e79ca3126ad9e002dd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:01 +0000

    contribute and load taste goodbye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
