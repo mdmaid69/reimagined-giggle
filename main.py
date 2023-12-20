184499ede16ea09765f9ffd445a415381b0b320e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:35 +0000

    justice for environment cheek damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
