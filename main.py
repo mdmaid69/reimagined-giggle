27064efc827f4562f9e768ff3b48ef1cb793b9db
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:14 +0000

    smoke progress hear matter reply

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
