4cad83c763db1f02c81d12172bdaddd86a4c1ff8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:37:13 +0000

    relative separate tip test feed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
