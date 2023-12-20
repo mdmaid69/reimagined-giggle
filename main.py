386df71a529088fbfd90f33fea6462155df083d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:33 +0000

    oil thick cinema permission forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
