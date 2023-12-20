deb26658422c6cf97bfb44c410c34aef3b2f7f54
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:14 +0000

    cold frightened corner inform demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
