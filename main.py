a44a957ab5add3f2f3d93f0bf7119584796d282b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:55 +0000

    Yy least lay glass introduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
+def is_odd(n):
+        return n % 2 != 0
