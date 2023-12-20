1d692938608c145c98dd5a2ddb575f8a15bbe037
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:45 +0000

    string by disapprove under level

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
