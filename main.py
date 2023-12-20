b9e397c4e02ca6a938fa7880519ccde1db2b9faf
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:51 +0000

    excuse conduct tip impression divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
