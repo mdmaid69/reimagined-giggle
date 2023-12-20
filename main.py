88db1661a84d1d315fbf20e3c683b8245ab29676
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:44 +0000

    nowhere young minimum attach turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+print([x**2 for x in range(10)])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
