d587cf870134c6a978fbb84661dee6fcea90a4cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:59 +0000

    type anxious coat sort result

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
