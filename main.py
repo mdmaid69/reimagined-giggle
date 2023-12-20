bad922a637be5c4a54ffe01e268d71702d100081
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:17 +0000

    thick bin tread may listen

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def get_array_as_memoryview(array):
+        return memoryview(array)
