4b81c25a9d1c484a2c6ab309feba284b239e1986
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:25 +0000

    connection permission comment final field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_item(array, i):
-        return array[i]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
