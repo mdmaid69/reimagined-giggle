aa8bd851f77f77d870cac9740d8a30656fcc5d20
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:27 +0000

    ignore spin female awake gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
