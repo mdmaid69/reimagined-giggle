066442a5e7880fc7d20f620a4b3670ab7a809574
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:09 +0000

    favourite slice climb even might

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
