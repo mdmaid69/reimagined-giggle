a221b984ab358fd4a060bd517066231628faadb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:59 +0000

    print occur spare zero trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
