44eb6de679370e696d4b2077dfb38de604644bbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:09 +0000

    translate storm dear look favourite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_typecode(array):
+        return array.typecode
+def convert_to_hex(n):
+        return hex(n)
