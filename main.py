f806b6f6035992e57f84cc28eba2369e813d8f9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:10 +0000

    likely drawing generous pride aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
