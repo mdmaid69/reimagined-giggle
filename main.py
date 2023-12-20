47cffb45e4ff963f3d8edeba2e93f7bf823bf71e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:11 +0000

    decide apologizeapologise worst chase considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
+  def sort_list(lst):
+        return sorted(lst)
