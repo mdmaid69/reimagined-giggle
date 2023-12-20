2f3648f41aa90860689482c5f4be120903afd70c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:36 +0000

    fat represent closed main generous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
