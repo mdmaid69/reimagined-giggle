0efed6e495f193e63756b1b1a9c61a828caadf24
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:23 +0000

    open common refuse nurse ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
