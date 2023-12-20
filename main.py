6d108bcb9062a91243b357658cb3491354272836
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:31 +0000

    popular seem shock hot unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
