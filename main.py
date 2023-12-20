c9d818e022df063a5a1a235655e4921bf5d83e79
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:38 +0000

    new lost hold uniform against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
