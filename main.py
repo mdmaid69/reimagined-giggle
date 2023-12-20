1f9a0d1216f26c7eccdb41f0cfa8ff10e8d87529
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:53 +0000

    satisfactory refuse to across pay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
