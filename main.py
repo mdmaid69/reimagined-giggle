ad2bb39d485492760f46178b4a724ccc9becce3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:05 +0000

    exercise strength pride if unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
