7334c6cedab2f57effc60c939ccc7edf3807d392
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:31:33 +0000

    dream estimate and fact dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
