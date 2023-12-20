5d5b95f7346cf67d43d63925bce5147858fb5879
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:03 +0000

    whichever production morning uncertain container

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import numpy as np
+print(np.array([1, 2, 3]))
