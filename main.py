e5f385027a14586dbf3787ce8e8ee752b26c8169
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:52 +0000

    perhaps eat everything hard again

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
