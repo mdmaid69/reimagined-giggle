aa24e1f2c7c264d07dd9e67d34fae03fb99d8464
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:43 +0000

    very luck rely release disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
