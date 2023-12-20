b9cb2b2aeef833c652927e4d558e0a40f384ae4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:12:23 +0000

    display respect address lucky lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
