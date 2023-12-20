a55c6dd6a6d09fea733b8d9eea2c5ac13a67027c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:56 +0000

    shock night week trust recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
