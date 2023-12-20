71c9a548742f9255c211455e04883e9a43a6fb13
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:38 +0000

    search slow character clear feel

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
