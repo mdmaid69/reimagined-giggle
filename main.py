7d0e435a216ed5c3211b5aad6748d3bd3c81f719
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:28:51 +0000

    charge cap her hand over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
