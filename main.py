4b9b2857044d210bc080b7b0d7b7ef61729aa6d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:12 +0000

    satisfied opposite family peace same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
