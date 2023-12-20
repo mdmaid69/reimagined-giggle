81dadc8e2101e34a0191542b34b8cf3795002397
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:53 +0000

    ashamed regard scare pen now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
