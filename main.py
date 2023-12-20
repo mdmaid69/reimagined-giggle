99a5cae25cf7ae9b79ab2da3e814ea0d68909728
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:43 +0000

    glad plenty least big swing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_unicode_to_array(unicode, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.frombytes(bytes)
         return a
