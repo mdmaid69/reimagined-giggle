76acce5fcdd64ac5d10800db5b0546395872ca99
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:17 +0000

    invitation steal leg couple interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
