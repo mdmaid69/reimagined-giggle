c03700e4d6b3e7763d7b3277600f4b1ee4c96fb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:01:58 +0000

    tune any central hard thought

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
