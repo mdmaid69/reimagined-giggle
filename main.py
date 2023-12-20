cddea87760e87e3db1b277ad55d73e7e001c50cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:15 +0000

    rain truth study turndown nervous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
