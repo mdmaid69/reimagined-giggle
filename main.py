af7a40a9a4a736954a37e6ef304a3e7ddb9e5104
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:15 +0000

    model several sharp people hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
