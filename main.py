1ccd34c3a0dcf1a94c4afef7d90b2f3045d1aa8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:29 +0000

    for terrible mail offence bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def is_even(n):
+        return n % 2 == 0
