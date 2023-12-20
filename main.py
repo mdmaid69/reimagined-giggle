fe5c8ad42e78e926eb103a405f4b8eb2e6a42d33
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:45 +0000

    flat same still live either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
