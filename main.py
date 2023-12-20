79d743db80a895f3d41decef0e467d27e776436c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:30 +0000

    real none nice turn straight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def sort_list(lst):
+        return sorted(lst)
