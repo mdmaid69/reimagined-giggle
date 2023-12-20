62040c5fb0c0007222f7112386b4574288a78ed8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:28 +0000

    forgive concentration offend luck battery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
