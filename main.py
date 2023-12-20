d4f28f8495fe64676c3c2e9fe0da52aa558e4115
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:27:50 +0000

    flow gate outside trust bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
