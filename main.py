efb0957b21b0935dc7f81cafebdea7a1af115bc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:47 +0000

    famous responsibility replace another marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
