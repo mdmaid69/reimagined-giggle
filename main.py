bf52e335885cefd949c5e06510fb0c36bb705aed
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:11 +0000

    zero evidence ill gate makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
