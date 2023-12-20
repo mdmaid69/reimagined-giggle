c085fd395a5eb85d2357231f0a7a49eed51e945e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:16 +0000

    maybe getup finally come fast

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print("Hello, world!")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
