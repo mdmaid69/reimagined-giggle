d9ad22f38ed59a49474f9bb409d2f6fd96652e54
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:45 +0000

    learn talkinto people satisfactory office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def square_number(x):
+        return x**2
