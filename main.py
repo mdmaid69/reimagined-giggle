c682bc6371993a3bdf38a1bcb3a3ff91d9d1d5ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:36 +0000

    serious top onto acceptable admit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
