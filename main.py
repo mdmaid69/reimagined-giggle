380f26ac2819aaafd015cf42a9c902fa64f74603
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:35 +0000

    criticize discover attractive scheme characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
