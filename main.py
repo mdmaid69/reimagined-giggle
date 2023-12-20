f67b147286007d6050e910e333e1abbb5385acea
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:54 +0000

    shade behaviour offence goback(to) spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
