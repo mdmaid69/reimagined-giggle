5e748d4d2155470dd6d2b69ae016bcfd7c73a987
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:55 +0000

    extremely surprise proper fix minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
