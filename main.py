4d47a4a970dafcfd4ac73cdf347c64b7ebea0db5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:01:06 +0000

    book false work sting sore

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
