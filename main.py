985ad7c1317cbf5bd844a78f08ff69651975dde4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:04:15 +0000

    quality temperature consult dish loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
