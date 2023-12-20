f6dbe02c97e2781630d66a54c9558b5fe8ae73d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:54 +0000

    nonsense quick married trick involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
