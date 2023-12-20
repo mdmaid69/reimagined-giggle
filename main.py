fbed7683e6a164043d2ab8ab88f0373de3a598ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:04 +0000

    glass strong please send light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
