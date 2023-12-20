17f5f8490a740da66fae8eb4d508882867209c35
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:25 +0000

    rescue admire no constantly shake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
