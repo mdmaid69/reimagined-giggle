bcc259294b15c9921ae1ad4cb9f2d11fd9f88ce6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:25 +0000

    rid attempt never lead various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
