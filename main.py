2c14aedcfef35eb89b49d72fa944e60e223d8851
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:31 +0000

    mouth support deal trade 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
