fa9bb2a70772d79c2020fe18424b869dab2f1f21
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:42 +0000

    goaway order deliberate scrap trap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_odd(n):
+        return n % 2 != 0
