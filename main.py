b1ae395b78b7dca14cc9bbbd5d904647a71e11e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:30:41 +0000

    anybody sink somebody switch makeinto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
