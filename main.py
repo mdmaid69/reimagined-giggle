122a148b678d0f1c2d05367bc9c1cc4a6c235ab3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:58:06 +0000

    trade experiment boil material interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def convert_to_binary(n):
+        return bin(n)
