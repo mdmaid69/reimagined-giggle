c8b9fd519907b6b1f97302b9447e4061cb00f37b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:44 +0000

    however bar estimate owe invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
