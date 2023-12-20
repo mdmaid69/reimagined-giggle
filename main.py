9499ba30d82bd18386dabc8c9eae143fdfacca18
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:43 +0000

    law anybody almost takeoff Nn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
