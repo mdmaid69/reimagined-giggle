568a0d6fe792622cb409df7a7e20ff7106b57538
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:39 +0000

    whichever perfect telephone shoulder information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
