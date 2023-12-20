ddfc264954f6433c469aa2b709ddd47b13abd83e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:05:24 +0000

    straight operate know side general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
