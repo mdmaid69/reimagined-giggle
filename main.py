6787fc4b973306cfe3ac13819f7a205bdad511a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:21 +0000

    breakthrough us chat sign bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
