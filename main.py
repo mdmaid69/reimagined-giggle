ee1732fe65c1c558dcf9f25a1a2c79e799cc6476
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:47:37 +0000

    sink without film invite complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
