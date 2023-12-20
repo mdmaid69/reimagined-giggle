1a7bd01f5c4464f0b867b0235a8c26de78ffd10f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:33 +0000

    study defend effect pocket ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
