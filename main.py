397383b5ca587ec154045422565ddc7a5e21088a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:41 +0000

    suggestion objection refer emphasize this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
