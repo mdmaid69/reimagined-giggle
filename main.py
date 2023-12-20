99cf9b860ea90c5639d3087e9fc07b39782a49a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:35 +0000

    home grade excellent question sweet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
