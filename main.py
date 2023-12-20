bd43f3732b670936e7114fbc76dfa7163c69eaa6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:34:51 +0000

    buy lot part promise town

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
