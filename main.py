5235c121c4b649f8dc0c3a1373e3e27a7e3b5af2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:08 +0000

    flat match emphasize achievement he

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
