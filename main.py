ae4ed651b4a86131cb134ded3160df15693dd182
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:02 +0000

    school appearance no shelter belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
