7ce3f90d86458d9069a59fbf7b0f735873ec0549
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:16 +0000

    mother sometimes treat neck hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
