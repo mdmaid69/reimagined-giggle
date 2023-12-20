dee792d6e337939f2830fd4fd66c9cfca0ba6b5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:49 +0000

    floor small arrangement blame gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import sys
-  def get_python_version():
-        return sys.version
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
