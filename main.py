0e7f14f794af61c03e6941f71985cac571f38c85
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:17 +0000

    otherwise base rate slip Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
