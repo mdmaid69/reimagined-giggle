31c850e3af382bd80a5289ae8e2564d47dc55343
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:20 +0000

    arrange splash bit swim thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
