7b31d9f254aa13b96b5dcd418ea9b16322a635b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:52 +0000

    short rather generous male way

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
