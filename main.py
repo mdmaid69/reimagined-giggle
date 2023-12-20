98821b01953e688be8d0bc644b60192232eb812b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:01 +0000

    frequent why delicate old sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
