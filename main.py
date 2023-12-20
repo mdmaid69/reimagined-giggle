191a513edf6a279b31534e54ffa3808b7793f27b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:25 +0000

    permission square track on now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
