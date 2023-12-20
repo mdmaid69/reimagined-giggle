890a544ef3f3b36090978c9b97394497096c6cc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:48 +0000

    become holdon mess broad blame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
