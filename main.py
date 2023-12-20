2c8a35b592877e59490fe1139eb5af81df54d277
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:59 +0000

    birthday comfort neck both moral

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
