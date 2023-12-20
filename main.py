ad011ff8af362aed378410d9e02a93acbe10cd4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:30 +0000

    contact climb future number so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
