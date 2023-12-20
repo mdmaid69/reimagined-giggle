3921a5be8a8b2bb31ceceec39105cfd31045e6e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:17 +0000

    pocket opportunity coat size injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import collections
-def create_counter():
-        return collections.Counter()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
