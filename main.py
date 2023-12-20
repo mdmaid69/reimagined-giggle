72b7a613802f3cde8290266308831dce566cff78
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:54 +0000

    wave yourself suppose tell contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
