b243c0a1e3cc9284ba77f67bc9c55b70ab368294
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:33:58 +0000

    study similar clean study sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
