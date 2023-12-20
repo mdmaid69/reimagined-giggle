9a7ac0f6c7522fccc9eade84534e8479597795b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:22 +0000

    reserve easy have load rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
