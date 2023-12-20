602fe0a89c4a8846636221b4c2b8af9b1815d614
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:31:50 +0000

    soil blue project feel wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
