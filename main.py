5b0b78ddbddb363c19030dd891af3a00bc5e62a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:44 +0000

    out smile twist us any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_work(force, distance):
-        return force * distance
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
