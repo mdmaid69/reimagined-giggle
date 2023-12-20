bc28973cf9dafeac6b5230765c0ab04e9376745f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:37 +0000

    tongue appeal get ear collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def add_numbers(x, y):
+        return x + y
