7d87c8f7ba2857499e76197805c609e623a30865
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:25 +0000

    whoever heavy understand probably call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def square_number(x):
-        return x**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_work(force, distance):
+        return force * distance
