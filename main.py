bcf3f23d079b64dc7319064b705bd870de0887fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:43 +0000

    contrast open post clean touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+def print_python_version():
+        print(sys.version)
