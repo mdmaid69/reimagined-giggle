dffec1b0fa22fa89a8f53975c51572efe4890092
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:21 +0000

    stage room embarrassed knock further

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-print(os.getcwd())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
