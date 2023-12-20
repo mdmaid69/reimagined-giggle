af2e0e6b90bf3374d586795ffa832a9be5af6b3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:05 +0000

    picture begin spite upwards race

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
