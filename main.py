4cb56eff296f392c7b9db539c2b4ca5d9c6fb916
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:18 +0000

    sell rise exact pretty above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
