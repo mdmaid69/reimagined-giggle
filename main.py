9fc284981ffb4c1292a6d29d699b8828e4d4d2b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:07 +0000

    most through uncertain earn round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_min(numbers):
+        return min(numbers)
