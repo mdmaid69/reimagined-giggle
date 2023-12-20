d60a82411306fe2624c1bdd7b4fc938ea8f0a71b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:15:59 +0000

    affect burn wave or definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
