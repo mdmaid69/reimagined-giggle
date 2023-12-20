b0895129cc2a06cef54f77b66bd92fca4496896b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:41:00 +0000

    stretch thanks snow field variety

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
