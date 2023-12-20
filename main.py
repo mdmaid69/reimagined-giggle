d21ba553d43939b34c33d3134355d137f1a74716
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:38 +0000

    lock advise bored replace shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
