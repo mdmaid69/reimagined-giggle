b7a1076bc12a6e2e9e6cc3a753163870f44ef7ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:05 +0000

    through waste shadow emphasize declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
