5da6280056e624a2ce5bb589abc877534e4d1bbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:10 +0000

    round comefrom basis stamp clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
