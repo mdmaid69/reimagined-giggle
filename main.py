b2d84d69c0d22e79199cac756f6bb74348dcc381
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:36 +0000

    cross open guess accidental purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
