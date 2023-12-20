43ad5c393363033043b42d87fc60879d89a23e04
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:05 +0000

    charge Hh form clearly shoulder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
