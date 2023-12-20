0a2f5b322f7b6af05589d1e45466a7458b71af0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:50 +0000

    takeover reduction Bb dare getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
