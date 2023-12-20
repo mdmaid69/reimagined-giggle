cf12f9dd76caff581e0e4e2fbcca653db85d1932
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:38 +0000

    justice black adjective call comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
