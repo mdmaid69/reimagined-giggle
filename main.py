3700279690064513cb7e2514a0a5efe800fdd2d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:23 +0000

    down them protest since appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
