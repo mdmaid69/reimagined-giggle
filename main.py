635dfcf567578e262073dde25c2e83bd582f65cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:31 +0000

    odd expensive in eat ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
