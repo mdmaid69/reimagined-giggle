a0729b40e448441c70a868f5278a61a5deab9660
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:00:05 +0000

    constantly ask region Qq correction

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
