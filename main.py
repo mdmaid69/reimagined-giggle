0c87976dbe852ff03e7e86709e9f22f11b448aa2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:05 +0000

    feel argument behaviour fact cheek

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def append_to_array(array, item):
+        array.append(item)
