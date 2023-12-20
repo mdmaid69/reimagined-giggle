50d87eb006ad90f36bff319499070f2a416e5de2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:36 +0000

    system communicate point trap decide

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
