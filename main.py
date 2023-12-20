80f085ada9dc2cf18bafeba70efbe87f4cb6d510
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:33 +0000

    listen play brush friendly pipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_bytes(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-  def count_elements(lst):
-        return len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
