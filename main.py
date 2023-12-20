befe7d789cee7e54adbc73e7bfb0f8c1999bbf0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:50 +0000

    line adjective average worth creature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
