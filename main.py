4ef2c26619b86ef6644ccfa1a30a38bba50107ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:38 +0000

    style time shut heavy circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
