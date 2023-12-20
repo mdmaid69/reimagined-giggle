d172ec8c3e1e6d7b2d5003fa8d6243187e693c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:50 +0000

    accept accidental polite lead reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
