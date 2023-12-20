d0f4669c21fc1f714fe56cda2b126cd6e5c4caa7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:36 +0000

    now accept game up supply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
