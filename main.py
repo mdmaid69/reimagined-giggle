e472fc6f6e954dcee4b1f515dfdfe4df573bc860
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:43:23 +0000

    production leave relative get expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
