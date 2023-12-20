7e5b128d736a269466734bca07d5fbda6c4e563e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:21 +0000

    joke attach probably advise damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def remove_duplicates(lst):
+        return list(set(lst))
