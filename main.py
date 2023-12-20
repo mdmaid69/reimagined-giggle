ebbdee0c18b27ea341636532ba9a2e26d5f1de5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:47 +0000

    boring children sick approve so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
