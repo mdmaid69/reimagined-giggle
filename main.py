337aee205ca6b585910ec7c4c24b2c535bd7dc33
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:07 +0000

    exciting doubt to willing continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
