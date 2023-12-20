230caf821737a3feb42c4170befeaacc219cff6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:25 +0000

    throughout car ashamed properly press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
