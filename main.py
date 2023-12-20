5e8c3ca7f42a2bb2bfde1f72b7bb4710915bd4b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:37 +0000

    impatient house fashion takeback follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_work(force, distance):
+        return force * distance
