14de43ffb2a4c24d9531cfef712cda119824cc94
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:35 +0000

    Kk production trade bet talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
