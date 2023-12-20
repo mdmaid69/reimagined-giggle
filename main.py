6caffaeeda448031f29ba13260703f4e72bda75c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:04:11 +0000

    accidentally exit punishment change minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def count_words(sentence):
+        return len(sentence.split())
