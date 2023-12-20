09bf3d0f4c6213fc5e24ee88bdf5c48a56fe4407
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:11 +0000

    objection deep believe that goon

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
