424b032f5ee933a0fbbc656bcf45c0c521f9dfe7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:21 +0000

    lonely slice cost record stay

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
