577d07d448cabdb310673173f0b0d761fc3d26f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:46:55 +0000

    failure poison food his cook

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
