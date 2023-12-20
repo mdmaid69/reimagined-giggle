3c70df583b4b89ad22444d204c0d86026a1387aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:05:16 +0000

    home giveback apply let crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def is_odd(n):
+        return n % 2 != 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
