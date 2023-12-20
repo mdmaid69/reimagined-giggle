f9a8828053a25822b7ccb8dda5a7fffaca43141e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:36:40 +0000

    sound rub strange afternoon false

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
