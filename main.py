f6a8963b95072ff77658ef68a83fd33b058461ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:03:39 +0000

    break lookaround spend clear complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
