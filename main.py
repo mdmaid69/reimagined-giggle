57ae1e0df4a0c43e24acc67669c443152af34e26
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:58 +0000

    equipment getback container disagree cough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
