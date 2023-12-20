3f04c64dfb7e028b43c8dd5e0e2f21f978e73fbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:46 +0000

    legal rescue refer gas opinion

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+print(math.pi)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
