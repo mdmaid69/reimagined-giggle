1011cdfd9708cddf51c9d3b02df904f933bb0c35
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:54 +0000

    pronunciation obvious disappointed crowded deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
