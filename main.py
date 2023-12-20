0439707c1337cd42a512cb16edc80ab615e79bbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:26 +0000

    see divide somehow arrive Hh

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import re
+print(re.match("h.*o", "hello world"))
