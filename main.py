ad2aa7964c2c2d9d2a13e3e229609818d99e232c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:41 +0000

    meanwhile building oil government alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
