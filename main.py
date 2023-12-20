e6405b8664737fe4a851656ccaae6baef928b41c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:46 +0000

    refer out patience hardly 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
