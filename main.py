408de44270584bb440d78c233b5ad3ad75171b61
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:10:53 +0000

    appeal prepare bright exercise gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_hex(n):
-        return hex(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
