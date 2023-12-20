c8ccc25b23f30480e3f199a917e202dace9e303c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:46 +0000

    win seriously identify reply finish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_current_date():
+        return datetime.date.today()
