9f22c24265058d9e3beb8da83f512891636b21bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:45:11 +0000

    mark style hurt progress over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
