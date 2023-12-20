8f34cf4d894ab703c8488133fb98ebf113db0ebb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:43 +0000

    sudden trial offence Gg invite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def add_numbers(x, y):
+        return x + y
