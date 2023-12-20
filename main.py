b1fa630ac7d64b616579a8fe5cdd7ac5e1a7832c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:06 +0000

    office recently afford they gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_odd(n):
+        return n % 2 != 0
