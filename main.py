c3daf2175beb3e19c6e802d7a9dd792c2469afb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:48 +0000

    gas accidentally action blow lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
