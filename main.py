c0859d31455cb58f24c30a61e22377c785ea5bcf
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:09 +0000

    rule well congratulations sore correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
