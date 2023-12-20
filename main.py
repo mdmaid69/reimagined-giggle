6080c09a49f0a8472adf9805a4dfe4540d748f8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:41 +0000

    can again telephone getup keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
