a744100b3baad53014381bb30b401e0b877e4ef3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:12:54 +0000

    grateful refer condition rather Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
