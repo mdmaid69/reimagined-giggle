cf1e0f111be7b9430b94b4ea6cb48728a109a48c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:18 +0000

    tax whatever brush peace rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
