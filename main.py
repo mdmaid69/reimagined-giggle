bc2544de351bf741c8e05d43ad68a9215aa40e76
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:33 +0000

    dig forward thankyou block slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
