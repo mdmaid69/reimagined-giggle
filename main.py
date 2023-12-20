d32617b0650585a2a55b64a059c7f582d798bdaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:06 +0000

    might sleep murder breath objection

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
