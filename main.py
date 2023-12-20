ad61175f1f9fabafa7b6003114eb5c3e2ae854ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:01 +0000

    conclusion expect matter operate justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
