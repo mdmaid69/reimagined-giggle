f8eac38d99d6db66e13700af891c04436d71836d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:07 +0000

    size bread meaning politics watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
