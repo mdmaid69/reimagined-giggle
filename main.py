3ba78300fc0fd1266bf911c5c0c1c19325def308
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:15:37 +0000

    bag official circle proof principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
