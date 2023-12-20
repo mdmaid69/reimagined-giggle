79f6dc924c9ae96f837fe0f3ab28120e5798253a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:41 +0000

    big apart shade fork wipe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
