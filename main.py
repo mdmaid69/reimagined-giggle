ec0fddd494884459a3b751fdf846a6a6d085cd85
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:10:57 +0000

    measure brown pause giveback sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_length(array):
+        return len(array)
