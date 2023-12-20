c32d8bcc968b4ef413e8b84afee013591ad272e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:48 +0000

    address 566end which rub influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
