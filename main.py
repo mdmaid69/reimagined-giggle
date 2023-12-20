300618f01bcad57ac551dec4c4396ecd69b3cd34
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:02 +0000

    active measure target factory waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-print(os.getcwd())
+def calculate_floor(x):
+        return math.floor(x)
