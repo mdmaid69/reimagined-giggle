1ad086d49f7e6e3167aff5aa3fe3808d8fe807c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:56 +0000

    day sit whenever well provide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_density(mass, volume):
+        return mass / volume
