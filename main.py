57853db5188e6f1217f11dda269acd8004175851
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:18 +0000

    doctor interruption quiet live bill

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
