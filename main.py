cfb488df3cfdb0abec0fb596949f5d05ff627471
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:47 +0000

    far crazy quit powerful fear

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_length(array):
+        return len(array)
