f5580289b2c708fc31fbdf27f1e3cf64dbfc42a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:04 +0000

    song influence doubt even interest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_max(lst):
-        return max(lst)
+import sys
+print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
