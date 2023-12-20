cf14ded81f285bb4447bb5600eebedbe24735212
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:31 +0000

    present rate 568energy go general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
