f7feea576c9b185273f1801a96d4d1d44155d496
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:06 +0000

    hold rest ahead balance plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
