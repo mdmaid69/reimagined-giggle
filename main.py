a57dba40f8b21b17d26b53f2208fb8e71cc846b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:38 +0000

    brief cap sign instead switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def reverse_array(array):
+        array.reverse()
