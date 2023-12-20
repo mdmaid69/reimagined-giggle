efa49e2232bbe7d324651d80f8770f433e9298bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:34 +0000

    family usual lucky importance along

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
