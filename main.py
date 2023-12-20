3aedcdef9361c7eeed2c4e3013986cf53ad64355
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:55:52 +0000

    instant stand now physical naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
