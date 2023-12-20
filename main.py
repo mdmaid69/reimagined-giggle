47456ea386a34a8b54dc1cfec8166b62fe04d4fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:55 +0000

    environment exact could anybody best

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
