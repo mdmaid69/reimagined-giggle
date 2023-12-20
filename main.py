aa9964e12712cfc37824ef8bfa495645593e5327
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:35 +0000

    shelter straight protest window fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
