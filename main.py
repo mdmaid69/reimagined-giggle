557ba7696f692a7c4ee77e72de89e05d15063034
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:30 +0000

    noisy foreign extremely holiday exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
