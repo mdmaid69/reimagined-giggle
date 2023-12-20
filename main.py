3b98c1d338135f8bc3623501686da7399cde15f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:11 +0000

    increase form pressure morning mad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
