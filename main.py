39561ea705d0d660272287fa576311a695b28316
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:11:55 +0000

    arm several bring worse measure

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
