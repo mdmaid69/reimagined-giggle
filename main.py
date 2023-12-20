ef78aa4751e1e6eaf16eaaeadce13ab5b70bb8b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:12 +0000

    real public brush start expensive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
