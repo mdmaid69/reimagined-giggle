a758e5eaa303302bc1b9f5d21882a73a8535a13b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:23 +0000

    figure turnon complicated dinner far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
