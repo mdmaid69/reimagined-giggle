c787ebcd12e77677fc4f5edba13e4e590d14d909
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:19 +0000

    row praise calmly experience difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
