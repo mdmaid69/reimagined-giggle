345186675f26d6b855d86e2c4606b67ce7f671b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:52 +0000

    hot watch purpose floor bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
