83ae9edd6e05ca26f82b0508b28f553d3cd5fb7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:52 +0000

    relative understand play above impressive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
