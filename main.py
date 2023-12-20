4dc39659f3fd7daabdeb9291d3413c8d5bb07b14
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:53 +0000

    scrape insist not will early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
