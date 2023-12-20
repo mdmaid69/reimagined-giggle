1f04b8253abaac9cd57e9bfa47f2e5042c9dc512
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:57:10 +0000

    success along tell national cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
