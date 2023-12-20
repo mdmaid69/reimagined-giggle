aef97c53425950b8dd6dce598d3bcc47daf29cea
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:07 +0000

    hold transparent no compare machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
