8d941ad5d2e0470f9a69090de3b450ba8cd9310e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:09 +0000

    geton mainly stand owe summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
