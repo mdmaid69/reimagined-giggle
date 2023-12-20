98f67dfb677a32a6181ee477ae978a83d935441e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:57 +0000

    afraid direct brush person fasten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
