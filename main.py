476effd143fb14419618236ef7395bfbf702db88
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:13 +0000

    account wild race thought see

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
