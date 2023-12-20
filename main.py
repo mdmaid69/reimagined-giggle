d250f456102e4fe2e0f02a459feed3f22c5b9f27
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:32:30 +0000

    ability scrap perfect bottom if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
