bef9e92ede35370b1d4f3ba8e1f4a7c9b0563026
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:09:38 +0000

    but pride wide determined relax

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
