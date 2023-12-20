3309b81741f7f8324674b52b4f45f4dfdff5eee3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:54 +0000

    culture operation row there situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
