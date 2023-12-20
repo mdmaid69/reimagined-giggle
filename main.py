829545029d38f023acd4912b345a6f858360f93d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:42 +0000

    across convincing Ff standard able

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
