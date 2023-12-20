81b275330146d4c423ad3db52bf41720cb7d6f84
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:06 +0000

    air meet tongue Nn alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
