72beebb440f9ba953c19e9d1ad20419b247e0f10
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:43 +0000

    copy greeting steam old system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def count_elements(lst):
-        return len(lst)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
