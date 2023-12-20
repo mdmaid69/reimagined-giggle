d203a741e23f6db0d31af2eae173b4f8b3a3e641
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:04 +0000

    brave except please simple me

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print(sum(range(10)))
