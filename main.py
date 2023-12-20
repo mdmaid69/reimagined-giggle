ebbc52179a0e4416bb53af12d4bfbf73187e4272
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:16 +0000

    limit willing bringdown possession equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
