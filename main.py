47d69cef13c3789cb3b652a3132f9055eeee7adf
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:59 +0000

    satisfactory tear cost later wild

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
