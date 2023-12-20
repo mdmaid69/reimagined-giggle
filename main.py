6985bf4c98ceb5b0a15b4275d4822320ead11959
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:28 +0000

    dinner wheel prepare worth scrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def clear_array(array):
-        array *= 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def sort_numbers(numbers):
+        return sorted(numbers)
