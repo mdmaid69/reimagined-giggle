1ec00162f9b3a9a427bac994979182ba918781d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:30 +0000

    exercise cruelty cough suit emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
