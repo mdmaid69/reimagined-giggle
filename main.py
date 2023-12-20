1e1600ac8291bfe12589503cf9935fdc108904a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:38 +0000

    standard belong why crash area

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
