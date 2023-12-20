8640c94186c9f3dfc73555ba1d83e7bd5bfa170c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:50 +0000

    convinced convenient family card professional

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+print(os.getcwd())
+def calculate_area_rectangle(l, w):
+        return l * w
