0239d9e5260cc570411570abb01a1f19e062f35e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:41:47 +0000

    sea knowledge culture task only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import time
-print(time.time())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
