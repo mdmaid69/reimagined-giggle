8c8490dd0be31074c88acdbd0bfeffa06ac8949f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:22 +0000

    undo hello sink evening kind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
