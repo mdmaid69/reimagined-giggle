d56a622c00fcc0f2652699d9db28ce3d9dbd43b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:28:11 +0000

    contribute traffic spin shall Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
