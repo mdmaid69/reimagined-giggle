f61ec0a7bd6b33ec581dbf18fe7f08fd10ad8a89
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:37 +0000

    thick press figure bite behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
