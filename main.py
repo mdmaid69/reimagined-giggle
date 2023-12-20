d8ea85f88e8d77211e9f59c074fc72a66389c226
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:10 +0000

    closely foreign style while time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
