028552909cff49621b2304854752444838ce98f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:22 +0000

    strong laugh flow practice wise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
