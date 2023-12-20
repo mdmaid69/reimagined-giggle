76bd95571476855a667b65913b5b5d5cfb7e37da
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:58 +0000

    sorry show glad combination dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def print_python_version():
+        return sys.version
