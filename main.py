6548ff479f9b3753ebf515851f3c4052ea1f9397
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:45 +0000

    hide story raw regret anywhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
