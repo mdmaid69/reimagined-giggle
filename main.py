d9bae0e319ee587be8774c76aa01102f43e09e79
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:55 +0000

    regret blame show perhaps money

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-print([x**2 for x in range(10)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
