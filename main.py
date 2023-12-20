404c9d15bbd447c1dae33690747ee5bc57f02caa
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:38 +0000

    keepaway well you taste introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
