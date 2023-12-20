04dd24d2b1ae78bf542306043562107825cfd7b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:09:41 +0000

    then heart hope switch make

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
