b8374bc384e6a44fa1339ed431f536815be51ba8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:17 +0000

    admit announce country live war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_counter():
+        return collections.Counter()
