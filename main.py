228cc5fc5db9dba7d26fad447e9c457ca410a6fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:41 +0000

    rob slight past year air

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_rectangle(l, w):
+        return l * w
