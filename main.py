8db3db12332f8c8d47e85f81caa32ff386acc2b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:39 +0000

    foot rough nigga quality foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
