4c08ddd74bdf6001b622279275000c8a7099bfc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:58 +0000

    question better level forward reason

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
