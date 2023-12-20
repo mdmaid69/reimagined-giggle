e7e3e31980d4ed98cd9926576a9c81b4430386f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:39 +0000

    write regular record tread congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
