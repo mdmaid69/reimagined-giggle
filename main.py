a1e632dfda1a9b9e166b85c9c95940e04e95d088
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:35:10 +0000

    weather one scene fast fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
