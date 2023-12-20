3d14f7edffe78fe9110cc2ad94fece6ec3f3585d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:44:20 +0000

    demonstrate exit same mean achieve

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(10): print(i)
+def is_even(n):
+        return n % 2 == 0
