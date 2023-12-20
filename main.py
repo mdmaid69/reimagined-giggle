1e3add8838b6dc36483de2a9bc53e2f03ed09f1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:26:08 +0000

    love through until software never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
