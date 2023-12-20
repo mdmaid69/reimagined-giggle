41c56c930018b9d2db601e9f2a89c9dd5da219e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:11 +0000

    prefer mistake weak error path

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
