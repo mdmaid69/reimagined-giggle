95e1ed34e5dedfbb5a14b4e96f8d16c93dd86a76
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:37 +0000

    big radio blue soon square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
 import math
 def calculate_euclidean_norm(v):
         return math.hypot(*v)
