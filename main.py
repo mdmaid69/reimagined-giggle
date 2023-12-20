584f8cac3ed2ab92a86884fa6d671f06a9ec277e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:53 +0000

    table friendly new table cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
