429e336c7f725ce02af6ab0ef377d78261b66df7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:09 +0000

    material period mad enthusiastic culture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
