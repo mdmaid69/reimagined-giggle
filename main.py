6dbae8e5f48ac7303f8a06cad97b9d0b58b75862
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:53 +0000

    compete petrol touch excuse sea

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def multiply_numbers(x, y):
+        return x * y
