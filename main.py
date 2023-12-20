2eb9cf53f143a2deb1a0c601996ebdf73bec7ab0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:58 +0000

    nonsense odd might rain sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_unicode(array):
+        return array.tounicode()
