5daf076e8bf1c3b8fcf3f22435e97d9ca8447803
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:07:49 +0000

    cover shoot rule turninto comeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def greet(name):
+        print(f"Hello, {name}!")
