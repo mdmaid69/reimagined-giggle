f67e134a43689089d4021017d0c998e3e4034fe4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:07 +0000

    thin set now recommend pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
