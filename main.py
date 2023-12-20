89923a3984f38de933bf4f558ab85c112887e70a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:25 +0000

    stick safety stop dream charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
