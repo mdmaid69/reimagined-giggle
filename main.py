9fe103686bfdd7f3c1ebedaa19ae6fdd3c6aaf98
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:36:43 +0000

    become secret equal now government

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
