2cf1fd3ba056288b4b7feeffdc73f7131f553305
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:20 +0000

    apart disturbing phone aim mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import random
 print(random.randint(0, 100))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
