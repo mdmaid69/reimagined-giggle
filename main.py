55fbb90075bf27694e8d093e8f7ecb5268e4743a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:50 +0000

    last float dress separate match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import tensorflow as tf
+print(tf.__version__)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
