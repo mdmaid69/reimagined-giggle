f870555bbc09fa8c56fb74adce22dfcc4bd955ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:41 +0000

    refer pride keepup balance forever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
