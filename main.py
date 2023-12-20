b5ed6ee9f2bafae51aed06e8fd923f449f8f4f7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:15:04 +0000

    explain cough label breakout use

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
