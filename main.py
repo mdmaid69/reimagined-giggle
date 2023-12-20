72fdbb5faf72dd34aab48b18a82ea0633be83f63
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:14:04 +0000

    Ff section task pleasure beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
