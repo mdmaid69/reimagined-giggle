0fb32253802f127a1e9a07b50b06d10955d9df74
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:37:33 +0000

    me report space anybody present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
