5e28af7bc1218d2d61ebb1ada44e2208ac71894c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:21 +0000

    turnaway obvious far spare ours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
