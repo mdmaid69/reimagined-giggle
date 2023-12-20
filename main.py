34705319244d261ea86d3c2ecdcf8b885f6791db
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:00 +0000

    unit naturally sort most wrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
