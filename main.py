901bd8d1c806da6fe7cd46b407207e437391abcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:06:39 +0000

    drive rather pile punishment vanish

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
