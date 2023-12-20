79ed5e3774d5a4afdca72311e212ee98948d2620
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:36:37 +0000

    height otherwise equal correct chat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
