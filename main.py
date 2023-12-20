b8c28bc791cc2b6cb245396d6725bd5307500b9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:28 +0000

    over cost there health trap

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def convert_to_binary(n):
+        return bin(n)
