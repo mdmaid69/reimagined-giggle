45d59b145a800cea3e09802f91dca5e94eb7089f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:01 +0000

    machine single live book up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
