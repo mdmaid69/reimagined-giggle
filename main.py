d599237a1f6a8d1db5c49c7aba204073fe05d346
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:07:19 +0000

    spring nowhere border noisy petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
