eec469ea442dd0a43a4c1dabd7a48a9b0daf80ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:36 +0000

    season reject bank shop so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
