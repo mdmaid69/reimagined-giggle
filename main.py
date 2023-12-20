b0a56529fe35703ad533064e490c18846bf43a6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:07 +0000

    upstairs late our Ss relative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
