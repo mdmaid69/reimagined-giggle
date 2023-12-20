50a24232ebebcb05b1deb4f7c367b20235dafea2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:42 +0000

    press test clearly separate proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
