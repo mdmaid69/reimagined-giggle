938839d5dc636e429682d58ef0e483a01d23bad1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:21:04 +0000

    operation downstairs wash sea hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
