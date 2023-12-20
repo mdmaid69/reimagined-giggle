5e44c03903be807a5a350aad663df0365d0161d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:20:46 +0000

    whoever pass ordinary 566end tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
