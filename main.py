53dd1d269c26731342578ff332d2259b77462698
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:58 +0000

    Ll crash hand bread keen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def reverse_string(s):
+        return s[::-1]
