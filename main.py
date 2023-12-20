29e46f403cf660a2f390c886f65df7596cac0df9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:52:59 +0000

    she workupto never cum dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
