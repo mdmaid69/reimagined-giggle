1e6ad93280b53f831b27c44ef47160f468bf9347
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:43 +0000

    neat half better obtain unlikely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
