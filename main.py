c2b0c3da21ef5fa0d8e417134c3e65aa34b03771
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:10 +0000

    window makeup over accept once

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
