2bf4926736197b8e896eff2b3d13c107d38832c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:19:11 +0000

    circle free delight report disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
