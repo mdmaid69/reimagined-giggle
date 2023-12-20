c032ead81282f80a895715a7a2c10cde099045b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:12 +0000

    value exchange pleasure down capital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
