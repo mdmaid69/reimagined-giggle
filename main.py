cd7bf67d2cfc4556d2b3feb9ff30c2876ad9850f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:51 +0000

    between influence eye slice spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
