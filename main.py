caef1c35dc1645981fba42798c33be2f409a319c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:44 +0000

    comment cry stone steam solve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
