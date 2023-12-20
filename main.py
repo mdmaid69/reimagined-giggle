02bd24c66376679132b082d021976a460b1e2733
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:15 +0000

    welcome alone positive responsible few

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
