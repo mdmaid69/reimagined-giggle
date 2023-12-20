dd5f0818f41790a28c7c36f5821c32419dac4566
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:22 +0000

    imagine century picture I exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+name = "Python"
+print("Hello,", name)
