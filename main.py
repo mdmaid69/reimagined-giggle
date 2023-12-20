4296cdfa730b66e0fbd608800ffe2bd7933dc9ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:14 +0000

    colour behind pattern they home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
