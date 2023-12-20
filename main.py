e9cb5c633fd7dcbf604d109f12e2d99d46756ab4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:46 +0000

    direct turnout except hit fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
