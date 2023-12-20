8b7760c4fb6e591fdc36b587192bbd6992cb8544
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:15 +0000

    music exchange lookup trust pipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
