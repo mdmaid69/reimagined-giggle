3055a25d29df87f764af9e68445d766349cd51b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:43:15 +0000

    thin duty system you truth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import sqlite3
   def execute_sql_query(connection, query):
         cursor = connection.cursor()
         cursor.execute(query)
         return cursor.fetchall()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
