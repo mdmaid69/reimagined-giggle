30903a29293043d4d7b8164765c4693372cfb20d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:49 +0000

    concerned authority unfortunate laugh steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_exponential(x):
+        return math.exp(x)
