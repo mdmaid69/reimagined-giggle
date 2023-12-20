2af91eb1fc639956ab4e5e0a8075960be453ce2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:11 +0000

    away related nonsense official ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
