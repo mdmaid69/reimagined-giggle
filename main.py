c16ddd550dbd8779b02b5d80d668058c7f2899eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:38 +0000

    lay modern dish regional trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
