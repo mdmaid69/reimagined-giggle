74c5c4dbe67841e8d90f8080b9f64c7dbbb6c4ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:47:52 +0000

    foot influence contents beside warn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
