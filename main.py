9151ae00e5d86fa235273f74c941a849077b5b91
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:21 +0000

    distant custom flow great future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
