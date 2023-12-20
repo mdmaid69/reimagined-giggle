46d8f333aae9548d402f445ac52be2939e325c82
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:16:39 +0000

    event sign form hope butter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
