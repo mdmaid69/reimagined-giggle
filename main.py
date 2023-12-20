cfe51896db63408e1a35f1cc0c0c3cc9a3464aeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:56 +0000

    cruelty stand concerned meal reserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Reversed:", text[::-1])
