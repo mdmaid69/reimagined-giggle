f5074fe79ebbed2b3f924b2cb0f0ccd0f3573fde
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:13 +0000

    now material people ready stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
