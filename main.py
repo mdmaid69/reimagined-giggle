72312cfcebb87d400d1d8e03259d595a75a80e4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:58 +0000

    choose pleasant decide loss taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
