522286f30f388f2883f181fb826f6c07fc5f1bd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:34:18 +0000

    wash mean blow real moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
