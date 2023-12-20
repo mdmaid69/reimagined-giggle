122d913acafa69af52eaa4da513e7d13d9fb3130
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:43 +0000

    recently house side her her

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
