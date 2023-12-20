1b208a4289a0f23ee773693e929f6ba1d15c99b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:11 +0000

    I the yours out advise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+x = 10
+y = 20
+print("Sum:", x + y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
