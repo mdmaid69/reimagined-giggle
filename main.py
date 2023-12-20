f3a9b3370324f071000701426c5d6130c2ca1f43
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:10 +0000

    scene hate Kk health please

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
