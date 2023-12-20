eeb98f4485ba77c7cdbded32d8ad40fa8d619de2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:22 +0000

    thought interview region window deserve

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_power(work, time):
+        return work / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
