370d365e950871c69335ee6cbece287c192375e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:56 +0000

    pure bother list agreement trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
