7021d155ef0200298898199da03c1faa6bda0b10
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:41 +0000

    wake answer shallow gooff else

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_area_rectangle(l, w):
+        return l * w
