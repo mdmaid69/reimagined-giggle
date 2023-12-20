9f608295b4cf9f7c6c93d3cab3c153bb922852fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:02 +0000

    track my strange stay thin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
