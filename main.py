df2ba9e99d321ffc927b7c070d973899f8b34b7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:40 +0000

    nowhere getback thinkover there equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
