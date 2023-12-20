8be9d9f553d69a93eebe8dead55752c176d7719a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:45 +0000

    thinkof practise cheap curious hospital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def convert_to_hex(n):
+        return hex(n)
