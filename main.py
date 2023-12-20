11daac7a98aaa69eb2c7e2c2c265662bbbe4caa2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:07:41 +0000

    Tt judge rank win study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
