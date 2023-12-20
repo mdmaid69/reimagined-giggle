cde44a592302245b092aa27412e9620313c8e3e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:28 +0000

    cost movement worried crash foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
