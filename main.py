f79569fcaa1a6b42a86670c816d35cdf490016d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:31 +0000

    arrive rub occur sorry brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
