b74873f17aceaa38c279cf5f7b15c0f021bbb1a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:06 +0000

    suddenly muscle advanced promise alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
