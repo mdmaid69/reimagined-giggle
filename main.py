47060a96ff9c98c2d0de36a7ee75ebbb501f03a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:58 +0000

    back fat position Ii our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
