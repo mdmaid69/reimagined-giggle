ba292aac76d797a38e6f0ff107925b5598e47736
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:26 +0000

    yes noise light callback manage

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
