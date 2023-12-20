ca2142a60f595abe256785464754f81ae57282cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:44 +0000

    introduction late woman consist lower

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-for i in range(5):
-        print(i)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
