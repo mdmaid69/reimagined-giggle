0a81d461dbdb7c3d76aecccce08b1db5e4378f01
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:12:08 +0000

    actually connected production both emphasize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
