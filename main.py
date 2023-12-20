433b32756588ef8ed181d6a8e07d7120e19a4eac
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:16 +0000

    heavy interest us separate guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
