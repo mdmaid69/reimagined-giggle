9b5023b154fbbeb53f9f590ae2b407b2a19ddb31
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:19 +0000

    transparent early weak live callup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
