2da6afc5e408717c9558f8393e75e069e842aaba
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:42 +0000

    desperate call night ago the

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
