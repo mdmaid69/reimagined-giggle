631db633d325448a24a95caf6bf66a1da7465d54
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:26:40 +0000

    pronunciation soft train phone check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def convert_to_octal(n):
+        return oct(n)
