47464c323092172a56ac499c3a335a645e47cfb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:51 +0000

    do fierce Pp choice hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_float(array):
+        return float(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
