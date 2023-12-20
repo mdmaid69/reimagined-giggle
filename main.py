e683ee821e54ba57e3a53de9f24d70ef9843d9a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:32 +0000

    central level wheel pattern watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
