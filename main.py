19ed8c28989fd5bcb05b14c51e3c358a5ce3bd08
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:46:47 +0000

    turninto pen crash brother comeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
