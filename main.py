7f21249db745b86e7ecaa2d2766e0b3cdf69e4ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:06 +0000

    equal arrange exercise sit circle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
