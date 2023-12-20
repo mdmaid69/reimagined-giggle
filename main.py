fe8efc31fa7807b37fc4b814b9fbd054c61c876b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:26 +0000

    nearly hold off contact thumb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
