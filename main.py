9cb54ac2f441e3b763245f5a1113a82442ebfb9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:56 +0000

    pretty about produce rough service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
