70338452c8bb9451109f9cd6e5c52c08606d5f43
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:23:38 +0000

    under tour community false system

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
