c96c77e93ad0fbe46616fd7c74d41731cfda415e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:43 +0000

    hungry taste wrong unite no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
