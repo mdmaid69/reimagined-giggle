4d43034ae06a4c5cc7821699b83bb41fac5ceab4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:35 +0000

    dry him round tune beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
