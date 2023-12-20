b30b6ce2250ec54fd3cab34dda1722e4fe82e6db
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:58 +0000

    period patient relation blame funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
