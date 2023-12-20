8b7c37bcf19d55ed8562dd6c3e6b9c160534ffc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:57:29 +0000

    mend emphasis takeout tradition concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
