20d4635a8a6696555b6aa31de977fc7e9c95f163
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:08:43 +0000

    bend praise tidy unit dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
