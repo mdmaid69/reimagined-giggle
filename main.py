89c6d65b31e4a134f4fc635ec3d9d4cccd15e194
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:41:40 +0000

    rush valuable plate safety against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_memoryview(array):
+        return memoryview(array)
