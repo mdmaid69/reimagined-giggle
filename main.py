ec62816bdaae60eb09a77ac35d8d93a279153679
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:42:18 +0000

    custom exchange material dare silent

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def exit_program():
+        sys.exit()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
