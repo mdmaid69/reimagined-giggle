037077c4ebb8f19cc794bb3ef75c937ba50ad1c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:56 +0000

    often though hear clean praise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
