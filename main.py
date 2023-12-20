99705b17030fbfb344827cbe26f5aaddd2d60a18
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:49 +0000

    fall hit nowhere how desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
