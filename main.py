c10becd457aeda198065408bc1a6cd8ef765b100
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:57 +0000

    identify imagination off touch hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
