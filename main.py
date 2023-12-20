7207b512ca5ff2ab2dcf79528ae707d6fff5462d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:13 +0000

    about last vote live consider

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
