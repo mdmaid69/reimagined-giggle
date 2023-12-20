fbba6a1a08fb7ab4af590f45e16fcc9505d9d76c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:21 +0000

    ground arrangement country become muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
