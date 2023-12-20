2c581e7dd3aa99b0b378031fa082485ce3fdda88
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:59 +0000

    giveback negative boring bringup Oo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
