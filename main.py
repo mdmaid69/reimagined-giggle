b86bada2778468575440b602e03b7da1a2fd698a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:38 +0000

    breakdown gas relation peace key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
