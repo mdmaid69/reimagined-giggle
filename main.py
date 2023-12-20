66db5c404d771a57db8e846cae157ab0a6ea8816
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:45 +0000

    hard wind film perfect street

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
