8397f10106aa131dfba88305979c75389da56c19
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:05 +0000

    rescue strict realize any programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_array_to_list(array):
+        return array.tolist()
