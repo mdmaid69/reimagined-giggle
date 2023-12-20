6e4263589dbc2b9fc6e4eec2453fad051f635f31
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:52 +0000

    relation evidence average much stiff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
