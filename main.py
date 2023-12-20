c51aaf1fbcbe53a73225d8b28c5dc22b0320e169
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:09 +0000

    debt breakinto unhappy attention make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
