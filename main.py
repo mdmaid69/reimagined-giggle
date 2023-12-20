c6a4b1f48a9f25649a7fe4a2b0c558aaf7395265
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:29 +0000

    afford everything language because delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
