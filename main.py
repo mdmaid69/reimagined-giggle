c348abc5522d35e56fd4e0c964713a76a1d5d01b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:08 +0000

    food move office skin table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
