8a10a5eea17b489c5a097c74a7416db6c25234b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:19 +0000

    sign sense justice opinion fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
