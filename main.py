3ce79f781736a649f6a6e799990bfe4ab132f427
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:39 +0000

    left convince cap file point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
