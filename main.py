77eb32738fa07b203a4b4bcedc59fd75643415b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:26:40 +0000

    573entertainment walk connect fun autumn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
