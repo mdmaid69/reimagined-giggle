ed2864063c91d281c689dd52bdb0af8574ac8b29
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:46 +0000

    freedom work chief average mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+print(os.getcwd())
