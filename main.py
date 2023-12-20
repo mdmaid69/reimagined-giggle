5031cf9de0a40389597536c6cd5041b8e562b93f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:06 +0000

    effect moon pile degree calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
