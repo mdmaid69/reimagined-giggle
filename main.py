32d830f7ded3b4091916e383f57bbce241ad8cbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:01 +0000

    separate power work impressed quit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
