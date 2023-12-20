e78f3ae118d69ec4a6615c6ca68786a4add42d81
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:29 +0000

    smooth guide anybody only refuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
