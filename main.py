987639d32d68a7d83393e1c30e948c945f18166c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:37 +0000

    immediately drawing learn later scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
