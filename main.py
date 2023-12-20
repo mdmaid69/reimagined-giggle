e7740c1580a8fffbe2aa2df630684618cb2f555b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:57:04 +0000

    pretend forever them right big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
