c99f97399e50a0926c90aab782f97f34b4647482
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:40 +0000

    genuine ring touch involve knock

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
