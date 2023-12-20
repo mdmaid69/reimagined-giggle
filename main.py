b5763c47ad07069c6ddee256f5af0e0a540d5a6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:48:22 +0000

    know left complicated close bringback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def convert_to_hex(n):
+        return hex(n)
