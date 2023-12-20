59046c53f79940e4894f78b43272dee629bf0420
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:03:49 +0000

    length split themselves Uu thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
