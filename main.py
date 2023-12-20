d92d81e7e9260aae046bd5fb6fd6642db4162d97
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:44 +0000

    yet runinto confused mind obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
