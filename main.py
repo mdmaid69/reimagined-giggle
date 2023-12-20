c3df5c6f9dcd882656f01d65136c7d01cfaa6216
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:20 +0000

    mistake force extremely radio fall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
