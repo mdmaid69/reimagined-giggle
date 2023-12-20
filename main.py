a2e505ccef48e514ef4b1db53c40ad3ca97caa50
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:35:16 +0000

    prove material month receive best

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_density(mass, volume):
+        return mass / volume
