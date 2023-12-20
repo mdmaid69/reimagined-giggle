dac410fb3b81c5e33e092fc26a2c626f0a34de8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:54 +0000

    operation study the soil fierce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
