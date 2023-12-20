13169f281aa6d535bb9425c5d97dddba378ee273
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:33 +0000

    know murder comment introduction refer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print("Hello, world!")
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
