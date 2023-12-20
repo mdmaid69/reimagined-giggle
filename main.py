a4740c5ad723c9bb4434fdefd9e593fda4bd69cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:41 +0000

    grand take near lookup path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
