5c79c9fbaaff5d661a0229043788db9a2b2ab6d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:52 +0000

    across dish pinch trousers reaction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
