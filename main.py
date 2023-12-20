a4d37b078cc710dd6d1e7ebe19b76c00635b9807
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:22 +0000

    deaf smooth week realize entrance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
