c2746fa03b55b7f69894cbf6c80b64f4237e4f7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:23:23 +0000

    already copy sore arm view

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def square_number(x):
+        return x**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
