b2c778675e5e27610a112cdecf4467675e9d1f4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:14:29 +0000

    bag cheque loose best idea

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
