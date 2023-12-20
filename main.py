bcf3bf81102f78121225be6f8e8cd0ccd2ce8bce
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:45 +0000

    defend themselves glad smell lie

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
