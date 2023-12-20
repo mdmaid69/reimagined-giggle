4ef38c099050e1bf5fb831cd7d748d5a0e16986f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:41:28 +0000

    lonely holiday deliberate major yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
