12d009e6aedbd389c50881029bbdd13bd9209f17
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:17 +0000

    day plan scheme anywhere eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
