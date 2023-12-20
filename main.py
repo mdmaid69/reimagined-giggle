df79f8f559ac94019b7d3e5a53f092212524e35a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:28 +0000

    example gift except coat nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
