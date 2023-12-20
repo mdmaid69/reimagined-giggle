63c0d278c1ea2f1d9ac4078e16e041929032da60
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:51 +0000

    person behind fierce phone capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
