e56d7a35116fcc127a52f2040fdb019e5e8f44be
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:36 +0000

    tall minute thick reserve clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
