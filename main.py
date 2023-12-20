0093170684569103116c0e5f95779007892b3522
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:04 +0000

    lookaround rough spray amount tip

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
