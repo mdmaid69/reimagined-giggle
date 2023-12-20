45e792a19f02e295dd046a17ee2339bf236fc2d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:52:06 +0000

    escape much objection spoil lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
