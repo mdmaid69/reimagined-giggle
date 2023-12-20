ee24aa94a0d9ba3d5d00b83f8ea8b29bd317a1d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:34 +0000

    off sing beautiful office peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
