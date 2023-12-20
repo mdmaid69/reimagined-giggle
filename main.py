029a30e8131984fdb85d9088528edddc87351178
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:24 +0000

    prize mind careful female out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_current_directory():
+        return os.getcwd()
