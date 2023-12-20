b8a6eccaae82621a2aa3514c7e770b85b6772a58
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:11 +0000

    too deliberately downstairs read early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
