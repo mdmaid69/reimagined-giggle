74a5960f4d2e37b10d44e171813b7bf34db6734b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:02:37 +0000

    nerve pass arrive neat next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+print(math.pi)
