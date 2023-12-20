e3938e4665472d8bd21d973d4092a5caa0442e4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:01 +0000

    embarrassing guess tip direct ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def cube_number(x):
-        return x**3
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
