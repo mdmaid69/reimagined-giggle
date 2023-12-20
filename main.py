79f39134ce25170fcec0b25d99b58ebf462a12ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:19 +0000

    wall cause ago sleep computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
