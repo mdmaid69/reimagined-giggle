f34ffc197f6d310fd5a7b5c0d5cf5cbc898e0729
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:31 +0000

    report mark independent result advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
