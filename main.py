4adc8e35b3d3301d4a3243ba7dd109dc038255c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:50 +0000

    normal deliberate my soak black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
