83bb57edcdc87748d714cb9db072cdcbda5399cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:17 +0000

    they maximum trap scared how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
