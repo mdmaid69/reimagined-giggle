75e2a2677d8fbcafa956650c984b5274653d015b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:05 +0000

    gate in safety responsibility century

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def add_numbers(a, b):
-        return a + b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
