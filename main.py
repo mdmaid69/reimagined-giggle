7ec2f8d8b768f5056cf75da47d138e0c65040441
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:27 +0000

    cum taste almost note used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
