8f0219a6acd7a4523956b5a874b63ec11b507b5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:30:58 +0000

    somewhere keepaway pause approval show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
