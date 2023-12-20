1d0f50f679a40225b3c4ed22b0e91e17f58aa129
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:57 +0000

    refuse little tongue level condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
