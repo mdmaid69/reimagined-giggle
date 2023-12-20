6c6701e443522c1f5bc15061c7066447fdce19ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:07 +0000

    evidence quiet day many run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
