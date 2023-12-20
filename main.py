e13b2dd96b3571c8e53d7d974d95ab147d278366
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:39 +0000

    breakin position stay comein tongue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
