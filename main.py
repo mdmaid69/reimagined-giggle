ab3ba65c931e3966d0362f26b2d3f7878d7181b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:08 +0000

    criticism Zz sudden wish fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
