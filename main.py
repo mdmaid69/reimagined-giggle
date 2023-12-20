2572843dddf5a18022e0aa9184957e009fc51844
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:11 +0000

    left question problem fast country

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
