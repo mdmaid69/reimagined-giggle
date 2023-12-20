fef881570cbe331bcd126d7af2e1c81f611bdf77
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:11 +0000

    declare dick seat over 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
