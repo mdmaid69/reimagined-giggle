ecac939c4b98129494edf3fd74f4405a75404a26
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:30 +0000

    terrible taste develop study date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_min(lst):
-        return min(lst)
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
