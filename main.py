aa19daf1bb1668d39639fb8a83bbdd4b73223c9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:05 +0000

    innocent silent square legal either

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
