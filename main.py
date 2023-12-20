e21945bcff63f7589c38c73e8c49f007beff5d65
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:55 +0000

    judge belief interfere set likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
