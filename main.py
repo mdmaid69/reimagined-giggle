8412c95ab8b1fd74babb79a22bda5bb67a20d3be
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:53 +0000

    disturb lookaround arrive tend wash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
