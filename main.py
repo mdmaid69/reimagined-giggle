c565393efff769fe5045a15bb54a7a42d2999ea1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:36 +0000

    visit bring talkinto comeup bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
