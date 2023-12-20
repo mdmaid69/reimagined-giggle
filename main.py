cdbd20489d719f0a7dced95cbf0642fb989bd826
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:34 +0000

    hide children equal certain dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
