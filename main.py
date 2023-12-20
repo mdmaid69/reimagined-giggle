78dad84ec6c9a3bc00e9e1670edf41043a5f03e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:05 +0000

    once objection sky emphasis familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
