68452c4bdd717f22a4db95d57e4ddb7cfa5d8837
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:55 +0000

    board takeoff announce ask peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
