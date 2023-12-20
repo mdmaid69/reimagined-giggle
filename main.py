60949d91141ca550c2afb68c1ae2425525cc5d25
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:14 +0000

    whole port story maybe hang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
