5d60f52d218ca4bac8b35057e09f388541d9f90c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:48 +0000

    advice pattern yours rest turnaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
