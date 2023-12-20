a3914aa1dfebca4dd8bf41cc05cafebd9aef69f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:56 +0000

    choice real lock herself takeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
