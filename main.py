17872662d3fae42797cd96d14048ab039bcc3177
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:09 +0000

    fire most hang bound break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
