add10901bde6dc51282f1c10195bfa00ee237bcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:36:24 +0000

    damage store must want state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
