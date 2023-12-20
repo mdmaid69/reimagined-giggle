23d7779390efad367af3938d4e98141704dfd148
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:03 +0000

    comeup half protest married roof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
