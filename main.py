9931c16b2979d253d6204485547b1015c86d58e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:09 +0000

    arrange safe turn sick raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
