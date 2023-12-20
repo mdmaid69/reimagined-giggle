74c14fd1b543ad81cfeebb0f922cc82322faf06f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:29 +0000

    sudden let any already comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
