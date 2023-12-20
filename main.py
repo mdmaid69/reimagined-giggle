4bbf46a7f4c1b1a99f690ce1f1bae7da4d46aa70
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:04 +0000

    by backwards sky attach catch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
