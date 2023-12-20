0fb32eaba6063c76303af673d05f541d333a8fd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:10 +0000

    undo takeaway risk cinema they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
