de1a9ae590db65bce89c1994149dcda70e4b9a5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:39:31 +0000

    throw post project turninto hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
