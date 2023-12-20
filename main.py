1c5cb441671dc8119caabaa2f483e707406d00cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:43 +0000

    recently information favourite entry complete

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
