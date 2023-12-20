22b1115e8d52cb403e531af6b8f15e6b31ca02f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:31:19 +0000

    figure comeround thanks manner strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
