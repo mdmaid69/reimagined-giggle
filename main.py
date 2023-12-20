0ea3388c61181b1ac2d13a1da922961d84e2127e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:52 +0000

    other bread polite reply refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
