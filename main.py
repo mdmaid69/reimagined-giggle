d66f3ac052facba4e5ddbf4dcdfc2a4c047e78db
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:44 +0000

    lie tired worth anyone measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
