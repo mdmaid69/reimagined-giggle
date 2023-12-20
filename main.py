028aec2832811ff10b91a9c6d06325811bd9422a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:42:41 +0000

    some second conclusion uniform weigh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
