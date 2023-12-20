f49e5668d0e9d16ebf7cd686af712d2e323d6c7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:08 +0000

    broken escape pupil rid rarely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
