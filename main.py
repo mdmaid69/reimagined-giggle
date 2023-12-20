73e25d06686b16b127f3e2966892b0f2ae272b02
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:49 +0000

    comeback address dish turnoff down

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
