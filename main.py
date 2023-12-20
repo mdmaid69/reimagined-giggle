7298a9c884d0b91a7544c4ffebf3905b7a351b0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:41 +0000

    town gooff education wind tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def find_min(numbers):
-        return min(numbers)
+import math
+print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
