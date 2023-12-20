ca66b62e06e4a74aa9b592d705861ff18b88d66d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:58 +0000

    opposite comeover either balance wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-def get_current_time():
-        return time.ctime()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def convert_to_hex(n):
+        return hex(n)
