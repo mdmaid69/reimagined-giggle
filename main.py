6c30ed1777422e67f4290ed60f5ebd6dc32411ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:15:11 +0000

    now consider declaration slowly pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
