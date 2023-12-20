681fb09563ad010494f06a540998e32de1c149e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:36 +0000

    match reduction really skill usually

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def square_number(x):
+        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
