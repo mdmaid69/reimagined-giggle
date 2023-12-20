f0cafa434360b543f139fb8346794c4b0309ce3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:53 +0000

    valuable dark narrow support amount

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import sys
-def exit_program():
-        sys.exit()
+def find_max(lst):
+        return max(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
