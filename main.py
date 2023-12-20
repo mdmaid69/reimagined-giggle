28ffc29a0e1a29c93dfb3895c7c557b0a22dc4c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:18:43 +0000

    excellent clothes left search will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
