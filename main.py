a93ebca9582c58e08552e71f318dfc215fba5857
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:54 +0000

    conduct you bury Yy continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
