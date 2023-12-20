d67f8a5a2ed7d6e32c0645ed4eb3aff5ff4a0aac
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:54 +0000

    goback(to) alone attention principle shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
