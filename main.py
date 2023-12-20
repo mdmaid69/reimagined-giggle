dd5ad8dd74e118ce1a5610f61f321a6ca13d6730
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:35:26 +0000

    joke please fill mix bottom

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-a, b = 0, 1
-while a < n:
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
         print(a, end=" ")
         a, b = b, a+b
