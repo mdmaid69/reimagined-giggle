b161f4b69d1d72b0e7b4de15c5e1ea96c7886029
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:26 +0000

    tomorrow money record knowledge shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
