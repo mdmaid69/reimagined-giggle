1a56825318b245696d6e729383926c21550036a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:04 +0000

    trust thorough Ee anything create

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
