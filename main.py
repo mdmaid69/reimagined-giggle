28d50af678fc3c693f356d49574a28c1b55d72e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:40 +0000

    we their unit consult correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
