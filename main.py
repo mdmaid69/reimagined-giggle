57329780ee8ef757028a84381c0d4a22c1cf33af
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:43 +0000

    thirsty against down schedule open

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
