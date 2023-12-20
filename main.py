b8f3e917312482e7ce060d09db3505ffb23eda73
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:37 +0000

    smell warning around pronounce giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
