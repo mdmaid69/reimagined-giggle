85dca9b9a1d05d83580ffc503680449ba79f7c57
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:23 +0000

    congratulations glass hair wonder anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
