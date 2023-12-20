521b871b9f2ac2367a4efe27eb60d5f1871e7869
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:09 +0000

    grateful of tonight force tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
