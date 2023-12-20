0038c4eaf92f8b1bb5feea3e993849823a59209c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:00 +0000

    programme trap last opinion study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_pressure(force, area):
+        return force / area
