b1dad97e3584003a8e2b0dbf6fad924dac97a886
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:20 +0000

    hair payment lend not morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
