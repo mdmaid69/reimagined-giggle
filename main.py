eec850334934752ce7cc397aff62b5ba212ede3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:48:58 +0000

    obvious mysterious square unlikely smell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
