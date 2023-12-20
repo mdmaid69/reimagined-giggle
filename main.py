2087c4a92f4499ddcda9323f0c399678042156a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:05 +0000

    hurry emotion along spill keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
