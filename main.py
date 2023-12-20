adaa0acd96f28ec0dccc87da9d01e0b5cb033dc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:54:23 +0000

    act rush temperature environment embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def change_working_directory(path):
+        os.chdir(path)
