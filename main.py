9f9bf0b8fc605eaeffbe86d1a857eb05ec07c0ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:14 +0000

    clean break impatient character back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_current_directory():
+        return os.getcwd()
