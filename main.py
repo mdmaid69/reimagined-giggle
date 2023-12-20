584d5baa80d8f530f46de52ef00359dca504fba8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:02 +0000

    exchange sweet talk wind let

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
