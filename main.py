3aa8aabb8bf490f8edc6f45cbd5214cb83100947
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:38:58 +0000

    tip return order dance takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
