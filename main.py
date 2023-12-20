2b0c3481b747fcbf786bc10e7085632f0aa5f5c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:03:41 +0000

    lend shallow sad stay secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_median(arr):
+        return np.median(arr)
