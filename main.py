5baaaac4b2f9a20dbeafc08f8e199f95360dfdb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:14 +0000

    edge mine onto practise respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
