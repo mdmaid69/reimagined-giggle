a90cef3ff2eddfdbb20d866f2c20e5414a6bae48
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:10 +0000

    use innocent ear serious explain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
