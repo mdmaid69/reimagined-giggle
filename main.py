e0bf12414e59160d625850a1d275519c55ce62f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:17:32 +0000

    object forward off check OKorokay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
