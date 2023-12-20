f07e0d46dc78caf48f6c9171f107d165e468d00e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:41:58 +0000

    season dance contain tradition close

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
