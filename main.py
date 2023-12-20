5473d7802a419750025b9d7189565ab3e647f91c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:55:36 +0000

    child awake kill sail unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
