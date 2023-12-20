2ff5ae4be3dfb2811280a5d55d436dede3b6e5ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:06 +0000

    favour scrape knowledge I address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
