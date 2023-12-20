bdf2e81a22074b76e84971278caa373111a77695
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:28 +0000

    change attempt number noise that

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
