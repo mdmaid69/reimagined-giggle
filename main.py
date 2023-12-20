e5e25f884e0d4a6d4ba3129ea6291d37fde94639
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:18 +0000

    earth mind centre my heat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
