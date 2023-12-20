eaba95ee03a7c5a44655478ed1625126eed8c0c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:25 +0000

    occur swallow someone relief follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
