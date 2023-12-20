8d0e90947c903d3a890957ae72f482f307912d97
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:09 +0000

    fit instant distance breakup underneath

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
