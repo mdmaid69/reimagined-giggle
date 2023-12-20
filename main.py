6d26f110eeb2d361c75dfd499cccf88cf0769b08
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:56 +0000

    win though cause damp shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
