b7c4ac9e65593de23f463584d3efabd89a3e7f06
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:59 +0000

    rise edge it calmly comeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
