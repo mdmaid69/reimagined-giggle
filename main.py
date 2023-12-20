ede19da60ed187e8d88bfea87285fd8dc61f8a84
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:32:18 +0000

    regret increase compete smart generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
