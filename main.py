09c55f250a002984f81de9d6860de4790e211e36
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:59 +0000

    refer fair programme odd part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
