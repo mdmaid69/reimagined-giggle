ce330b09992d98b60f5c8a89bb0e0a093c5601f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:52:06 +0000

    correct land slice ground factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
