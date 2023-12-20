ac0e06559118c45724a907473fbae3488cf4ea24
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:39:34 +0000

    of trick finish shopping seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def square_number(x):
-        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
