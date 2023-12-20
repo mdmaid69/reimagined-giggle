2b319a1c28f796daee74f6a7a6d05feb730ade7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:32 +0000

    let deep combine confident to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
