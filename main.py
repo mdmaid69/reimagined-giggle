7b15bc7b805c655c132cda9e09b2cf66d1d83b02
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:39:47 +0000

    mine emphasize secret nor something

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_stack():
-        return collections.deque()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
