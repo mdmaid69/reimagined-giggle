7c885f370806585df12cc03a1579d172450ccb73
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:04 +0000

    air different fortunately ask rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
