356d7c7241b7c38d211ecf34cbba1f379bb97a3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:07 +0000

    fair concerned smart free event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
