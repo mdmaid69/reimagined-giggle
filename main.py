55d94bccb417942cea71f7bcf7eebfe3ec26381c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:08 +0000

    back file turninto mine mark

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def greet(name):
-        print(f"Hello, {name}!")
+import sys
+def print_python_version():
+        return sys.version
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
