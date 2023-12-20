2822e44e48009dbd65256cdecc36008a5e23e5a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:18 +0000

    direct kind die clever decide

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_pressure(force, area):
+        return force / area
