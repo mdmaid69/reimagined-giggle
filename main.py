afc80ea5d1efc0169274c43008ec151737cf5847
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:43 +0000

    food majority himself during holdup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
