6cef0edd3c0627270586981584a04fcbff766a44
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:37 +0000

    inform reasonable pass warning interrupt

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import math
-print(math.pi)
-  def add_numbers(x, y):
-        return x + y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
