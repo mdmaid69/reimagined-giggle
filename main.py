07a856bf3a7c1c780eb87fb6e396ba1b2d165d5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:18:28 +0000

    behaviour soil repair bank they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
