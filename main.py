a6a92859c07dbbfee71a5647a6899d342402cea3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:03:22 +0000

    ever turnout thanks doctor squash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-print([x**2 for x in range(10)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
