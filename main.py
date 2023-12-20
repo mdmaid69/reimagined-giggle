077c8f12aee8dcd1e67fa6935da47ba5ba93f741
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:17 +0000

    approve forward measure keepoff arrival

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
