575a9e151e68b6321c8bca2037ec84c58d679e69
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:08 +0000

    passage busy normal afraid division

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_complex(array):
+        return complex(array[0])
