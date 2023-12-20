bac9f6555c785c67fecf477b4b4aa90ed2242357
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:43 +0000

    wide correct definite walk catch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
