0fed2e4a37ea19a3c02b551773d1714d417b1e92
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:00:52 +0000

    medium roof smoke pot congratulate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
