a4abb099751f8ff8fabc5b37490b60a5fcd06867
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:46 +0000

    file admire rule sound comedown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
