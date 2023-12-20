974f800dbbfb6f329f0ce5870803f19912370b07
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:11 +0000

    speak taste compare same exchange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
