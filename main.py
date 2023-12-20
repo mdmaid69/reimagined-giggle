9d0167843028099f5b02084afcedf7cdbffce7aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:29:15 +0000

    point free collect remain answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
