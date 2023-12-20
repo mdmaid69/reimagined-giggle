df6f6f6dfc57052f2d749c7933d5e7b36e816d1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:16 +0000

    awake wait study soon be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
