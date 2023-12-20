46e8dd38b6ebd36969b6a778653c0f03d2bd0169
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:56 +0000

    by square order comeover pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
