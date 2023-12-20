4e3dc34cda66f420db1894f7d6b6a310c23302bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:57 +0000

    well with thank determined children

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+def subtract_numbers(x, y):
+        return x - y
+def convert_to_octal(n):
+        return oct(n)
