ebb6dc700eb0780a8a6c67b553005aa0170682c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:30 +0000

    goback(to) breathe workout bringup mess

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_average(lst):
+        return sum(lst) / len(lst)
