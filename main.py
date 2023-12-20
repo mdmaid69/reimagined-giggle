67404b36701c0bd91b1b9ed196a5c484ff1f9d24
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:38 +0000

    aim ready year male lose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
