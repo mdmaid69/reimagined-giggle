6afa7e2d068e66c4b125a1508f4cac3d422c28d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:01:04 +0000

    what poor contact load suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
