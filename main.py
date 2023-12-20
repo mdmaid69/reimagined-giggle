abd20a0b695ad302996e61ecb039505052f0adfe
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:19 +0000

    plan sting upwards minimum frequent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_current_directory():
+        return os.getcwd()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
