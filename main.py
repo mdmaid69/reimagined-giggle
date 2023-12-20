c7a5fe3f13618eda6b2087b27ce4e10cc60a11d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:41 +0000

    fear silent goodbye water trade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
