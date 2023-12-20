02ac8f47abbd3db052c7f2579568794b201c6bd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:14:28 +0000

    cinema responsible Cc reject pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
