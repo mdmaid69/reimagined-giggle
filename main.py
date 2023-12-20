2f1f83992ad813c64ef804a4828a2a8e380de56b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:47 +0000

    turndown could rely young unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
