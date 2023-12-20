6c4d51fdd21a80531f3623123221ebc711baba0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:03 +0000

    under 568energy result pronunciation politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
