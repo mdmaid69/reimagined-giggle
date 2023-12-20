6bb6dd024bd427f83262b563d208271d3773596d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:31 +0000

    good hospital close fire nervous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_binary(n):
+        return bin(n)
