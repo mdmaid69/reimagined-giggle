6fe6f7c01e30748c2e5881fad7662539eb9c3dc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:40 +0000

    stiff deserve keepout cry her

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-print(time.time())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
