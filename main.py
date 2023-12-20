89cd1d7571362628f3b1f300f7abb9ff06d8f4ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:07 +0000

    perfect pronounce balance turnaway emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
