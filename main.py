e6b60f6cf01622e74514b365ef895e6aeb4cb068
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:11 +0000

    himself clean height thirsty only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
