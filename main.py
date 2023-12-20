d066a2bd6f3f208100bfddf02483063fd08e64e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:42 +0000

    better anyone be undo force

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(sum(range(10)))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
