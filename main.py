9f85d1625808845132023fa5475e6539a4a4c12d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:56 +0000

    bringup burn slice mouth worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
