af565e22d4b719adbb70f485a8f4d42bd94172ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:08:40 +0000

    tape thinkover authority opposite solve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_length(array):
+        return len(array)
