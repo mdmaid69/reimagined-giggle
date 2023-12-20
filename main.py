1fe6fc338a56e58c87af67cceaf02c22d438cb88
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:01:07 +0000

    stupid beat minor centimetre include

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
