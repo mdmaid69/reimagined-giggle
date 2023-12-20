32ec0d24903d545d53fa05b01ac9dc96a64b2ac8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:45:58 +0000

    wind through careful persuade this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
