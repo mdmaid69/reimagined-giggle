664242214944b1b0b060fc3f532518c28c69ecfd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:09 +0000

    refuse nearly somebody uponon Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_square_root(x):
+        return math.sqrt(x)
