551186b30fc8ef04f70ba1ba127116640dc82512
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:31:22 +0000

    catch wish word pause wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
