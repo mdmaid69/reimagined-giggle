0260aa70aaafd21cadd81f7d5a213e730e581d5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:53 +0000

    final blind trust attract dig

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
