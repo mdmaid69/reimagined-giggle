fe1b391e823f6471e3a4b1d439ebbec3b53d9cd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:38 +0000

    artificial separate inside unfortunate powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
