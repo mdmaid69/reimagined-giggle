d2932512938dfe86b43108f4f8829b3d5a28491f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:28 +0000

    murder separate cope allow music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
