ca2e6e174371708c8e3c4790711e29c952ffdf3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:43 +0000

    pleasant trick bet OKorokay connection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
