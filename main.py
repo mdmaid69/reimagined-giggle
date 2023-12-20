386129518b19f1bb98aa04b13f6cce3fab2ec144
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:38:20 +0000

    later way why open wind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
