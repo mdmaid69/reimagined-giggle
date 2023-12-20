28af80b7eb24f61158910fe0dd1d6363197ae435
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:45 +0000

    succeed raw something ready event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
