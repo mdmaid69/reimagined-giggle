cd9191f48213664f4a315a9fedfdd3fb0f76e932
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:47 +0000

    forward back front by operate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def square_number(x):
+        return x**2
