ec96d132b288a14e17f8c47ae9d0469fc0d8a08f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:57 +0000

    science indeed division bringup cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
