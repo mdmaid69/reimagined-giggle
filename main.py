8c45fc1eaeafee97b393b779eeef560a06441684
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:11:26 +0000

    truth please children trip study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
