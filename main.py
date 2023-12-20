ad9ff14ca693d0db566d63b533dabcb1baba93ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:06 +0000

    nearly regard distance undo indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
