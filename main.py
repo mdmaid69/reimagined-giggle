7509d4b125fff10ddb60ae8197db86652f40de54
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:18 +0000

    few square outside nowhere forward

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
