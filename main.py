aee94614a8ca4db708cad728636489bf961ade7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:44 +0000

    as any pour goup turn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
