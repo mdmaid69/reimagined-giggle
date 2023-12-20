7968a3ad515eb59438b2ca247eeb7bf4fa482c58
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:02 +0000

    protect your this petrol waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
