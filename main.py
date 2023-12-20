30302959bec76e493d8593422cf57955a29ad705
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:34:34 +0000

    somewhere niggaacademy close so season

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import json
+print(json.dumps({"name": "John", "age": 30}))
