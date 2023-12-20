ce5704fa35594f571df3e757f6c171a36357ec59
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:27:46 +0000

    embarrassing difference worse gather environment

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
