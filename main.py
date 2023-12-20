1f3ee51d390fae6cc80689f5863e621d89a552a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:33 +0000

    length their base use mess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
