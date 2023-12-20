2138821c86199cf8b6dd058de396dd46f909144a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:40 +0000

    combine coat window suckme opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
