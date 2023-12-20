f2c8b1e516db1ab4e339883864171a7fd93725b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:54 +0000

    sure recover new design react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def exit_program():
+        sys.exit()
