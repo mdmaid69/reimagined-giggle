bcc5c31a8088fbe55134082fffdfaad25bc711d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:17 +0000

    frighten deep such lookback(on) now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
