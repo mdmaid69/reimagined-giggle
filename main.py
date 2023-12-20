e2d32cef7b4043ef7024a49a75d6bf97e2840acd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:13 +0000

    relationship pay book cross long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
