49e9bd8eb7c00ec66ee6315ce43e91d0944e0bbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:28 +0000

    progress practise agreement accidental disturb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
