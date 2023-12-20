d96b0dc76d2061450be70aed7f4a2fceb967498d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:50:21 +0000

    project flavour lift circumstance hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
