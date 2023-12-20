cc7559a947cf9eacf04da9770372cad3a463dee9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:34 +0000

    empty comfort stick taste flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
