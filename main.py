8b850e2ae3124ce85fcfe48ed16f7e75e18e7f7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:45:42 +0000

    once method his telephone slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
