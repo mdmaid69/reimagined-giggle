aac9e558791823dc797914c9c547b9fc81cc57a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:08 +0000

    skin act class body aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
