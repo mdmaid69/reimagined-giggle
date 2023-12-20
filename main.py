3dc63e90abd0de87efd16f7a9703e844fed9ede5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:08 +0000

    contrast wave speech little report

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import time
+def get_time_since_epoch():
+        return time.time()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
