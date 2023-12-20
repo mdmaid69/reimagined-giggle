8a60e7dd275465c4cb1bc228081eba93885ced75
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:11 +0000

    OKorokay history blow target off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
