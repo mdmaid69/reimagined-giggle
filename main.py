d3e65d2c0f96fca992fe24ec47cd022cb9293561
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:37:16 +0000

    proud turnout pin strong we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
