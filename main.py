400b974b968c3effc6cd65cea9cd2fc09d65abaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:51 +0000

    defeat either mean tidy relationship

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
