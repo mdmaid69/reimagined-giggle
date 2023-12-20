13a51313badf7f768dfd8228fae1802f4f9ed5ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:33 +0000

    everything in risk own fast

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
