6c9b0ac4f42b8bef6492cc0bcd12cbfea013ecd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:37 +0000

    honest trick steal port low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
