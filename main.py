49048f7b7b02ddde026ae910ce946f2ad93ca1df
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:21 +0000

    he part fix crowded pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
