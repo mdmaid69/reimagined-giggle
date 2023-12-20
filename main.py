0f03220254054fafe758a2ab82ab7aa475fd0ef4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:16:26 +0000

    broken callback relative seriously gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_priority_queue():
+        return collections.deque()
