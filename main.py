9966732ef5012c58eb230278f883143f39ac0137
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:37:05 +0000

    concerned it course frame generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
