55895723ef2919e9f4f97b96390104471de2c66c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:54 +0000

    receive change storm believe juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
