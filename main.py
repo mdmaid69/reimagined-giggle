8ae2343020a8f358b6949825de305dd58a0f517a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:23:17 +0000

    however rarely bored concentrate giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
