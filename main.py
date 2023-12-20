24141864effa775b8085d921744d41df2d534b89
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:54:52 +0000

    shape explain callback block negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_typecode(array):
+        return array.typecode
