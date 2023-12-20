d0de49154f6980357f0fd0b8557625b1552bce49
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:11:47 +0000

    together opportunity impressive flavour close

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
