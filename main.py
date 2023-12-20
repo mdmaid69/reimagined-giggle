434c26e19b93747dbc7f4c91050eb9d8180ad52b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:57 +0000

    free weak through benefit check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
