99ffda7c0bbaca29fdd2d6bce8c8200fb3d6bbb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:55 +0000

    throughout 564encourage case physical rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
