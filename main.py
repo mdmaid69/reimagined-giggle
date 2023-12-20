3bdc73c25f16ca12a2ee23cb2fe7f4c58e1b75c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:33 +0000

    worth bear anxious touch block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
