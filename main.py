1e59d9826e50a9147dcbe77f80f9fdd0ac4f064d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:05 +0000

    state seem never touch sore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
