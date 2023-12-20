ae4086afce9c46b5e59102c89719c2a2c1ea3928
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:55 +0000

    both century result flood suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
