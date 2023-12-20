6050a32452b638b93659ce218234700c08beccc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:27 +0000

    autumn perhaps creature connect expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
