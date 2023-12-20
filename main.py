9fc17cdd89ddcddb16bf953e5461b5482ee6daf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:20:53 +0000

    theirs tax suddenly software unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
