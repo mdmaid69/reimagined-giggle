0a7ab3318fd9510985636856e4ed91a12414cc0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:47 +0000

    crash other strong keep bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
