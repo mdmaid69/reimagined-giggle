ddb178ecd97c51c076dafbfcc436139427285efb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:35 +0000

    swim charge computer same tune

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def square_number(x):
-        return x**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
