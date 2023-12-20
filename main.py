0315215f44d2584d8c924be77fa487d6ac93b949
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:18 +0000

    meeting measure body alive unusual

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
