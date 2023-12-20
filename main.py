4cba0fd92b3dce65663b482050258875896d9257
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:02 +0000

    towards thirsty cough uncertain mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
