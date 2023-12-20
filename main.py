ed56b011d09d26d7054fb161a29622be772c139e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:50 +0000

    do treatment body receive sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
