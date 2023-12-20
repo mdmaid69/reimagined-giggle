fecf43afa4f75e2bb9afaa50870f4aac4c74a81c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:12 +0000

    shopping among field crazy ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
