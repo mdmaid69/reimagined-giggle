445a7e1ee5dffd75dae7666724b48de76a84c660
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:24 +0000

    consider travel fit thumb gap

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
