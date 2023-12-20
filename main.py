87a761a0db3f3b04b0546a5d90556b391ffeecb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:02:10 +0000

    pour offend study brown home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
