aeb3af24af3c25b2fec095a1e716064ee38dee7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:16:31 +0000

    answer buy complete class sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
