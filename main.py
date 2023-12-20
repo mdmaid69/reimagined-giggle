64a5b20c62cff5cbe8de9d72653a03f563c586fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:15 +0000

    without free slow he habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
