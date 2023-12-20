95d99e617807a808a9423f68ef3dfd3ba5b4387a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:33 +0000

    along war cheque which Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
