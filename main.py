04cbc090179048394d9f92777993b33ccc6ff9e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:38 +0000

    Dd chat possibly fat near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
