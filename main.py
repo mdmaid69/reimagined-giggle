2e4a1bd72f48e883429c2e151fd1cbe1595aa54a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:00 +0000

    major creature rubbish lunch invitation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
