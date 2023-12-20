b2d7fc20ff848166f41fe2d8da8ebe9ef861c04b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:48 +0000

    available man anything activity deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
