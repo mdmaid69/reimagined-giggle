257a47e9b8c98c104d9d8d8c1cd693ee6214f470
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:13:35 +0000

    juice loss by few imagine

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
