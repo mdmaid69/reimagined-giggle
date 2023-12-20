73f36b79d38980d60f30299db2e2e5d5cbae848e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:39 +0000

    age argument weekend deliver gooff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import time
-def get_current_time():
-        return time.time()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
