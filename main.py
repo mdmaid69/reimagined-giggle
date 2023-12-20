0b9940339e1ec66b11cec5fe50856da51e2aef14
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:14 +0000

    downstairs ahead talkback shopping live

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
