2e1dbc43a91220b73c1afdb5a38b3dfa802efb42
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:42 +0000

    switch flavour inform long father

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
