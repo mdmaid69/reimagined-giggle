42c3698e72a3be9278e542c7317328d1db95baac
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:35 +0000

    to platform needle squash at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_max(numbers):
+        return max(numbers)
