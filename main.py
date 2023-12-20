86a55745b6cdc9749ddb48135b8d30f7f1c730bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:26 +0000

    guess time backwards better dear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
