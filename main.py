134251511d263487a8c3351aa270a0b352019b11
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:47 +0000

    straight cross partner pity doubt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
