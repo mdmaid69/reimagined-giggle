e10d7cd0349a37bcd7b009ad0a7c3d495b51f57a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:59 +0000

    breakinto knock substance exact you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
