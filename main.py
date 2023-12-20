ad4ee03e8413bc671b838246252260ee7a44e590
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:17 +0000

    idea hear below draw destroy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
