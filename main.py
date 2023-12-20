7d79451694ffdaab2897eb879c944573675d47b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:03 +0000

    force set shake weather uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
