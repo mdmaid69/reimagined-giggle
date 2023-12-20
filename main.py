c344ecc5df3a96fbbb5ef2b4f09c7cdf4f77c7cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:43 +0000

    influence too round allow describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
