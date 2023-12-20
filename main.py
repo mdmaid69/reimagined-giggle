502bc1e310154af50016ba338bb22908a534f86c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:27 +0000

    takeover tip bet walk suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
