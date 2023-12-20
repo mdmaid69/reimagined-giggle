2ae86c60076e12f960dfb338b04fe5bd2966e73c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:03:08 +0000

    peaceful Zz night accidental path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import platform
+def get_os_info():
+        return platform.uname()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
