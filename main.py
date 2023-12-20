9d27e703c6ce6567eefe5f12828e92740377e6c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:00 +0000

    before turnup mark mess Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
