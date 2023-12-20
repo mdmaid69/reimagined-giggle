88567bb63d3ffccc8f4abb00fa0d8ba3aa9694ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:35 +0000

    poor breakin influence experiment experiment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
