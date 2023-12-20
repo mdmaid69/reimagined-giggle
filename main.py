1dfdf8afddb94ff590ac25b6bb5798b69dd2f29d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:55 +0000

    tired order waste mention lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
