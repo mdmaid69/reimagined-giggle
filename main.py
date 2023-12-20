9a344c6bec228136477a39ab329e8a3af802a4a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:55 +0000

    phone appeal size face suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
