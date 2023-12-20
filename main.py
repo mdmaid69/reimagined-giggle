1de9a261df3013c9248e16feacac93186c9d55d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:23 +0000

    awake breakout willing steep enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
