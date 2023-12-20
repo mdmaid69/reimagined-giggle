6892773c790a7a57a4161052f92baead217417f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:46:30 +0000

    talkback necessary takeover past grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
