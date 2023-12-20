eeeff30d5df5ce434185d6444c4b1cb5916e26bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:48 +0000

    about cough giveout damage breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
