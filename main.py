f87dae538d4d85a210a623767dba59a6c0cf4c39
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:27 +0000

    along loan block trip turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
