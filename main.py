adb143f586e3075c65956103cb09936fe874555e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:06 +0000

    bound doctor healthy swim attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
