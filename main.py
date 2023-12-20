aff424c3c325d0a61c43d5f23e993dbb68549324
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:27 +0000

    top next goup desperate effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_current_time():
+        return time.ctime()
