ce49fab0e3eb510fd94de00207bd30cfa7e5e179
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:47 +0000

    sit cross dinner commit wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
