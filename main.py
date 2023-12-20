5257862b50618103539e7cfd8396d06011440892
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:30 +0000

    schedule impression sail wire think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_area(radius):
+        return 3.14 * radius * radius
