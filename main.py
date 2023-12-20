0c80d0a198115cbd7c19ea180d7d8ef77c38f950
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:58 +0000

    collect catch unfair improvement excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
