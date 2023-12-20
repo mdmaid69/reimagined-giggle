1b6473e3c3152f35fcfbac471392242432c8201b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:06 +0000

    twist someone arm bin trial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
