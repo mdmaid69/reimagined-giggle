442c5c2855faf099b614423fbf29b7f7b1b10697
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:56 +0000

    complaint come crash clear pay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
