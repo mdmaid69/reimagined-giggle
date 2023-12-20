47f1e3e31b3e206171cabbb54c926c80087d7869
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:30 +0000

    trip thought transparent consider distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
