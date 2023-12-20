5e98b17508809cb519dd6381dfcc3a1328264b8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:03 +0000

    sore influence small he drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
