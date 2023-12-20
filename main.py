9b56af080b26ea4d755fa71bd7427738d5e52337
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:45 +0000

    everybody before unfair machine or

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def convert_to_hex(n):
+        return hex(n)
