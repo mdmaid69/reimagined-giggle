7671f92440f18a2eea3ab53696c906e485961a2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:28 +0000

    life actually complain contrast charge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
