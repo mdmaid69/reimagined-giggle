2dca5158f014be5a899ce45eb1ed278a88c77a22
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:39:58 +0000

    hardly turnoff negative proof careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
