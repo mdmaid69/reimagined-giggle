3db3dccfe22575822bb28d0fdb60ec7f823a5558
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:43 +0000

    pretty tight friendly geton movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def greet(name):
+        print(f"Hello, {name}!")
