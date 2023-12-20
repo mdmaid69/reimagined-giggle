da4d02ba2668f05b53f0428243287039afa3d18e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:20 +0000

    attention threaten poison yours feeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def count_words(sentence):
-        return len(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+print(time.time())
