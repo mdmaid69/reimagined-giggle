34d998fedb4dc539b883f0c7e96469f20a906b6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:56:23 +0000

    sort 565end thought calmly force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_str(array):
+        return str(array)
