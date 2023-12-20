ea8dd6b7b5c72418674bfbe22110fb27350e84fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:05 +0000

    transport culture advise bringforward habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_odd(n):
+        return n % 2 != 0
