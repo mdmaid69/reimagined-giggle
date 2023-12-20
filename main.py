2c126c8daf88e4542f74906fe40ff86dd0add1f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:14 +0000

    attention develop goodbye adjective nigga

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
