4725baaa2be69651d2ef91eaa0958c10d4c6f589
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:22:23 +0000

    else bring turninto type again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
