9a99a329500246d923ffa2ee2ed1a16517507666
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:29 +0000

    strike record land them trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
