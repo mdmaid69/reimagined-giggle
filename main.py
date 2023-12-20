93085cfb292628f4f43520dc739bff4b386ebcdd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:29 +0000

    abroad stir evening case height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
