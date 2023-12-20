324de562e26909bdd361d186561f24d5d977640d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:26 +0000

    adjective woman wish pot definite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
