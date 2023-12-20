543c1d15e81db757efd95156647c7e4b29e5ece7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:56:00 +0000

    actual experiment movement hot contribute

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
