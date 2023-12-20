f0a11dde90f2985e5713be52a106ac4d56c651ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:59 +0000

    piece rush split what break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
