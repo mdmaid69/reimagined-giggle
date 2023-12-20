31af266e8a06f973c82be02b3314b15e0372fbb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:40 +0000

    window sound gap careless sentence

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_tangent(x):
+        return math.tan(x)
