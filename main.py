c58fa1fd0a1171d6e269d06778319eb2162e90c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:51:14 +0000

    estimate listenfor frighten wash false

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def remove_duplicates(lst):
+        return list(set(lst))
