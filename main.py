1414322f57b608e4d79fa9bebd106ad4aa563dbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:05 +0000

    certain official they radio lean

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def subtract_numbers(x, y):
+        return x - y
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
