8099f0c10bb575e3f9cc385efce0f80ed57b1b54
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:08 +0000

    convenient bound round drink flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
