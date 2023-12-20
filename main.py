1956fa893f7f901c90e48550faceda8e789726fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:09 +0000

    mess lower fold even crash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
