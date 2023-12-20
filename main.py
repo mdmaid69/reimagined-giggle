704a300b2b23f553a845f9bbac08da59584b67ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:53 +0000

    disagree needle tidy sleep yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
