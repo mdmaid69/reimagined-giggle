1afc4329e9480e5167c43d675ab65c4053f93d31
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:48:42 +0000

    substance plain reliable attempt strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_power(work, time):
+        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
