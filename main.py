b9f54bbfa4517cbb45fcc729282c4f84a7509862
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:37:26 +0000

    shall simple string welcome few

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def append_to_array(array, item):
+        array.append(item)
