527d225fd416a77e700d86435fd2a4c5dc65158d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:18 +0000

    pinch subject punishment equal cover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
