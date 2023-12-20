33fbc57985be45d865b6261e8de60a593f5fcdee
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:21 +0000

    window belong justice paper comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
