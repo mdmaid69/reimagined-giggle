8823ac3d6551143f8b15e61191bb1f1b01df015b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:27 +0000

    manner demand claim alive switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
