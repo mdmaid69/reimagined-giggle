a8b61d7cf909f2e21e32dad873db82fea22925af
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:53 +0000

    push pattern favourite tool temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
