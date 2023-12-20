4bca7a232c9eb916d6922e9c54d830f995aee2e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:45:24 +0000

    pattern thin will board programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
