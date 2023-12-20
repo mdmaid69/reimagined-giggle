1c82cc96025ef8c9606aede5e5f8db532608b00e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:22:45 +0000

    uniform takeoff centre its mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_length(array):
-        return len(array)
-import logging
-def log_message(message):
-        logging.info(message)
+def get_array_typecode(array):
+        return array.typecode
