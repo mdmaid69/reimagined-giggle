9ddf9781d42be3f0c0fce41c4f37c22391f8b286
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:56 +0000

    star open hospital actually post

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def add_numbers(x, y):
-        return x + y
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
