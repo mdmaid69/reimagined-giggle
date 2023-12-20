754329b221fcb1c5385a366f088faa362d1ccef6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:41 +0000

    rate trade communication release surprise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
