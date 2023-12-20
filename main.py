0ed5eeb44665fd70e37536bbcd654ed48d042735
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:24 +0000

    rain turnover bang exam impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+print([x**2 for x in range(10)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
