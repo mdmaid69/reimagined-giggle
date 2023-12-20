f10c5b3cf2eff6f6c6959dbbefc24ec85ad8669e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:57 +0000

    minority any admit brother bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
