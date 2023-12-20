bc23e127241e1bb017185dac666928a3e9c9c0fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:24 +0000

    keepup expect custom however exercise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def square_number(x):
+        return x**2
