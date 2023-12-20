826d60e09fc9c2020172588252e8a1767a082b2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:00 +0000

    bush ago consist concern comein

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_list(array):
+        return list(array)
