43ede99a7d0358774212d958d02e5cd253ad926c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:47 +0000

    defend appeal job again country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
