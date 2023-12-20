fe8f2508eff804d65638ed9ac1b65d1ca88df6f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:25 +0000

    children remove typical underneath forget

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
