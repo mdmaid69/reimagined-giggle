2898cdb2b32a44eb7467afdb057836bce62dae7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:54 +0000

    physical male cinema ill future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
