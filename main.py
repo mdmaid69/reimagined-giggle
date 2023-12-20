589b58f0ff20bcb1d0610b56cf5e96b27a682b41
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:48 +0000

    to attention need name traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def split_path(path):
+        return os.path.split(path)
