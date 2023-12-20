60f70fc2916d33010363248fb8103b43894cdc22
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:56 +0000

    scared whatever sign who practice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
