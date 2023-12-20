1236d628fed195608dfebfc02a9a021bcb2f8760
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:29 +0000

    trip juice sting passage print

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
