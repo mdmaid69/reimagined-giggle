0d2527e6f8a136a964c903873d12f0d7b6f70127
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:48:16 +0000

    to about keepoff moon back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
