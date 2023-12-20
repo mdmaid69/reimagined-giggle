311d98b58797cb5fc1e9cd51f51e88eee60af27f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:58:52 +0000

    likely welcome under damp love

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
