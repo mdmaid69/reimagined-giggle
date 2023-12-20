52d95e057398b38db178f46c4dfda0c12ef0561a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:41 +0000

    split treat because climb geton

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
