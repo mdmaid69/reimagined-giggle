f195d8edbe42fda6ef61740e2f561dc63cce085b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:05 +0000

    character runover regular open accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
