f98e3f4889f76323970e27e3561d7ec0f1e672d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:22 +0000

    shrink slip squeeze concentrate herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def get_current_time():
+        return time.time()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
