3a5f9399e331605b94a435cbc169891ad99a9f82
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:38 +0000

    team whistle whom holdup genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
