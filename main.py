7746866d2e6d08c58f5b0ac84fcabfc863d2756f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:09 +0000

    before unable but deaf concentration

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
