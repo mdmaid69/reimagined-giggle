c72d359b53eb93f40c030b2cf11a78117388e78d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:05 +0000

    subject safe common behaviour accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
