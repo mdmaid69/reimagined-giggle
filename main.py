5e2c025a23f92ada5df600524f87234ca84293f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:01 +0000

    hang emotion workout listenfor comfort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
