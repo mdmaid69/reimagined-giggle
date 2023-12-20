97ad926587fac65bac9afce252e794a619593b12
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:22 +0000

    science matter settle life responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
