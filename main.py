a3a2a26f75ae458702f0aa9109f68ead0bb9fb4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:14 +0000

    live subject dress evidence thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def set_array_item(array, i, item):
+        array[i] = item
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
