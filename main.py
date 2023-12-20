01827252404458cde97b36850699dc63a43a4800
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:17 +0000

    only safety there search aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
