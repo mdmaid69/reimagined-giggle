b99113799e488a440d603922a255fdc6b4712a61
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:01 +0000

    break post married private evidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def is_palindrome(s):
+        return s == s[::-1]
