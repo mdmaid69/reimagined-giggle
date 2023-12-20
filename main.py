79d360c7c1a5af6abee3c84cc758930506973caf
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:49 +0000

    whatever 566end shallow happy Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
