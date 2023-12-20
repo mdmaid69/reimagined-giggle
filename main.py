6a55a1a510ff4023ff30bf2ea0dd1fdabee5c00f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:19 +0000

    decision arm recognize slight impression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
