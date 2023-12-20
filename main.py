b78ed11ab7c3f1a2839ce108a43a2715f6b7b2fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:34 +0000

    furniture sweet direct secret black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
