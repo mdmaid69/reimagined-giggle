109b900dfdd0e5e3d53b275b0eea394f1ddb0b13
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:03 +0000

    gift positive early size suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def convert_to_octal(n):
+        return oct(n)
