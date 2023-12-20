376549f0ae3ef3d5d91fbf52647783a83d4ac68e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:10 +0000

    beginning stir Cc give learn

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
