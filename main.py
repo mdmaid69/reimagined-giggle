4fb0d1136d35bb183371b1d011ccd50bfe416941
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:28 +0000

    condition present put they Nn

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
