d6f6ed1ce0e3ca0a062a672fd4e2756619341b80
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:59 +0000

    pen speech close organize goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
