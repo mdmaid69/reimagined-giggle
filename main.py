cc316457f5e78dfb8232cbdc8f1e28c0d50e72fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:30 +0000

    silent dangerous male him responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
