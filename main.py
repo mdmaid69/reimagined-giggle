835c518fb6cc0882435d4f9b25993b092744c2c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:14 +0000

    attitude powerful your be forgive

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
