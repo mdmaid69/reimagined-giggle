930984b3de954d8af78213044d54e3d37cdb8ded
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:09 +0000

    real allow breakout possibly afford

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def is_odd(n):
+        return n % 2 != 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
