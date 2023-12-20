93bf967b99f100ef20e1b7edf06538d0975a74cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:19 +0000

    father voice watch eye rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
