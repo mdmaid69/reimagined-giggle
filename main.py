e4081b0f08884bf4750c66f1793d0f332b83b50e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:23:40 +0000

    shopping minor lesson call string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+from collections import Counter
+print(Counter("hello world"))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
