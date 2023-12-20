4afb1296428b9854a0cce348f5914f758e62f451
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:52 +0000

    average seem drive defend need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
