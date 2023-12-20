bed332a59d0671d5df43d9b0b858d37022ae7af4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:51 +0000

    excited too peace disappointed shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
