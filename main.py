7b5f3f2681b4ef156af5f1eb0fa03971827a385f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:18 +0000

    shape rubbish male hungry lookafter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def is_even(n):
-        return n % 2 == 0
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
