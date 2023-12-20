206135f055a2709f3e702f0d29a9d52ab306bb10
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:34:50 +0000

    unusual number calloff disappointed along

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def sort_list(lst):
+        return sorted(lst)
+for i in range(5):
+        print(i)
