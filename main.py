cd511b3a0931b2d6781e9e5214ee6ef0f9dfe6d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:02 +0000

    rough cut book art destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
