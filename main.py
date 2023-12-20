0c27d53d20a85f7c2a5c907a21aa102d888e84e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:10 +0000

    down ago limit breakdown meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
