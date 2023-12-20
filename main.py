3a6e4d59f8e5a0e12b24da2cae126e894694bef5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:16 +0000

    so whatever fault may one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
