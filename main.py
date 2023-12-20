3b690a72906c3f5b66a068ccbe447d90dc4c68df
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:31 +0000

    part community order turn official

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
