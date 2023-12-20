35bb75543420ecafcc131b74d0f2ee51e5708551
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:24 +0000

    noone behaviour task why name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
