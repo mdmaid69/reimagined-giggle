848b25f80df7e6a5973822f132de39ef73208bf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:35 +0000

    legal satisfied will anywhere tradition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def cube_number(x):
-        return x**3
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import time
+print(time.time())
