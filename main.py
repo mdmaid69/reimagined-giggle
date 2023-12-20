2eda08ca256d777c7342efa284bbe201903881fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:42 +0000

    lean attention week neat command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
