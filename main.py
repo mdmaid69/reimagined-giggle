085c981f2ada5fa0171eee492eaea86a607831ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:23 +0000

    argument chat fear permission fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
