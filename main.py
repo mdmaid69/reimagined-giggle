d12ecfb5551a890b021ecd596e4d0c51a0b9b4b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:14 +0000

    perfect before move quantity consider

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
