9b8a4e328fd73d4c7022f8d1362f88182b57b5a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:29 +0000

    wherever interview mainly touch wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
