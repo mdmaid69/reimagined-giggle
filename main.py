4d8fa523ea5b187d5a9cce9c94051aca64a69c3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:00 +0000

    unpleasant willing target rain guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
