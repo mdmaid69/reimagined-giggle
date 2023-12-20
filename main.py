a4461003e7f3c71a7ebd5c3d99f1ffa3e4a25919
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:13 +0000

    unpleasant about middle bother edge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
