5ecd5f7cef1facd0da9b7e78742a9992b5d51bfb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:23:27 +0000

    somebody set crime its switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
