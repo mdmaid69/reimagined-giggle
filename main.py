a1a43ba54fbfb9ea470964b96e3caa3d847c8516
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:46 +0000

    cook married issue name sorry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
