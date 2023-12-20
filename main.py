0433193c02afe4d6b35aa83a4da1c3a2f396cf3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:02 +0000

    combination thinkthrough great fork pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
