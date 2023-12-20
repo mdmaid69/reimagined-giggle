720fdb8ed1c48b9d72b2593d0b85ace159f15883
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:11:49 +0000

    care answer firm permission traffic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
