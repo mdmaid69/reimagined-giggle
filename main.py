b6ef04aa0c0b924cdf8394d1f0ee86415f39539a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:42:30 +0000

    matter great next strike trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
