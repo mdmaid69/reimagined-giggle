89806374528e8fec7da027a58dab2d0294c02085
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:00:18 +0000

    disapprove exploration goup lead centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
