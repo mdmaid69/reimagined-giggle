5da79f36724fb3cd7d5c6415a3f4ec75f32d5051
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:45 +0000

    settle gradually us can weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
