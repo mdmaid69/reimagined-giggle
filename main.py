23fe988e1323bccdc000df7258afc174359c78c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:00 +0000

    world at payment inquire myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
