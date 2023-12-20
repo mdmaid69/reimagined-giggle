eca79fba59270fb1e3401cdc3bff14f6b3806a6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:30 +0000

    hers fall talk ticket man

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def count_characters(sentence):
+        return len(sentence)
