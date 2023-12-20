88866add89c5cd7c602aa40bb16836eef2ed99e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:22:30 +0000

    under control real minimum reserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+def log_message(message):
+        logging.info(message)
+def find_max(lst):
+        return max(lst)
