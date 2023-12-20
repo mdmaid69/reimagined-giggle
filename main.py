987590a2eb2d7da9c231208f16c7c6b0c26a53a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:16 +0000

    turnoff uniform fair sudden almost

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
