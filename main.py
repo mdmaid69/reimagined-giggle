ab7c940d14115ba89a2d0b1d003671865107959e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:13 +0000

    influence murder family dark sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
