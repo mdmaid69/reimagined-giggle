514e0241db96908239ae42cb8701fc5aaec2c6fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:31 +0000

    politics taste enthusiastic flow recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
