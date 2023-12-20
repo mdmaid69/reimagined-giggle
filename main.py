ab4d7e06514f4182de283791c846dff91c6b2538
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:00 +0000

    motion history stretch problem nerve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
