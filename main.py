901a35815cbcee6d0ba780487336738e2604f471
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:04 +0000

    sensible stamp rub famous cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+print(re.match("h.*o", "hello world"))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
