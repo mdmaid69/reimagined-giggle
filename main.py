5884659c97e8b33f6de453610aa9ef3d92bfd238
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:43 +0000

    search fire precise float crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
