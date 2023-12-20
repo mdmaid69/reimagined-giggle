2e6977226a0494ad09c0a18cca89b99efb93c0b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:34:41 +0000

    shop reduce favourite attractive avoid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def sort_list(lst):
+        return sorted(lst)
