395d02e6516e0e4574b74e952be1ded345c95550
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:15 +0000

    break measure plant wide complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
