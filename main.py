39faf743f478f0676dca5e1923be8a56af1a0cda
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:05 +0000

    total year begin moon crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
