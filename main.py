44df5dd8697ba64a9fc7129dd719e32505a4b99e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:06 +0000

    safe watch simple begin error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
