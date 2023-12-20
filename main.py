6cfa6a9888bc0a299a837fe894233d1c8b00377e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:04 +0000

    project straight communicate finish concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def find_max(lst):
+        return max(lst)
