03e9266640792645d26bc4fe186b0d4466b008f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:44 +0000

    block lend before he bringdown

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
