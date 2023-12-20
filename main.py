1f63afc7eaa8fbd9b7f0ff5013a55df19e327949
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:47:44 +0000

    bury impressive switch modern late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
