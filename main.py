94185cf84cc212770a7c58831b52b51fc53b413b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:23 +0000

    judge thorough fear imagine comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
