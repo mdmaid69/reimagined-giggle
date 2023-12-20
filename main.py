5cc28a969625f6274399ad5456a047586f9f5c88
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:27:53 +0000

    step keepaway chance middle how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
