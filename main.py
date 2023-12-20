55f413a89b119eca9576b25ec921c5582d106209
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:47:16 +0000

    fear come compare its here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def convert_to_hex(n):
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_hex(n):
         return hex(n)
