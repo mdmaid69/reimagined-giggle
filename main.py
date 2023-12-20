438c5b98af0cd5aa32e50d7e3547e423349b305d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:50 +0000

    deliver light sail scrape limit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
