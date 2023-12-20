8aaa42b7bded82f227ca87ef5af011628e59b37d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:20 +0000

    sensitive exercise place make pleasure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
