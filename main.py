8dd1f84393cd227e81115cf895bbc663629ac099
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:03 +0000

    careless task autumn ordinary treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
+def is_odd(n):
+        return n % 2 != 0
