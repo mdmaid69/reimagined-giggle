792dce3faa41430a385c380c77e82de99f3d2c9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:01 +0000

    lend broken use thinkthrough pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_current_directory():
+        return os.getcwd()
