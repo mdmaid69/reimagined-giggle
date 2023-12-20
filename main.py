7944ac87c2a05142d8194af95e81652816b2f195
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:55 +0000

    pleasant fence occasional handle report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
