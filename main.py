decb54519e8e942e93a3b265e5c8618506c53ae6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:23 +0000

    somehow exchange meaning shock truth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_even(n):
+        return n % 2 == 0
