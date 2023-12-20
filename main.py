a37552e518c7ab69b9b0cc733c66e0c6fed12ca0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:29:46 +0000

    provide neither control machine information

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
