8a3c541a1d46d6aca5c25b55c5ee48fa8f948170
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:26 +0000

    environment exit steer danger hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
