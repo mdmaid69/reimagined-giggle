2aebe4431abced7de7325a6be79492c595c21bef
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:02 +0000

    around certain burn port return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
