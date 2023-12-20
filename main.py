66d589ac3e818510430b4f4ca9fd464f6187b3c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:22 +0000

    club getoff call comfortable Rr

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
