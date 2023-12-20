548ae78e9fbf216dc7e53df9d3dc5b83ac07c8b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:09 +0000

    support glad pencil luck win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def calculate_area_rectangle(l, w):
-        return l * w
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
