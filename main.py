e7f4bc203d9f3db2f65bc6119cc916235c632912
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:40 +0000

    underneath deep breathe recommend shout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
