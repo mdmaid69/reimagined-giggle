2631754489f5c23cfe18b11be1976683c1188152
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:35 +0000

    connect getout fortunately tie repair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def convert_to_hex(n):
+        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
