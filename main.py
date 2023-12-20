7c663332fcd4b5025f97f496e829ed1dae0f3934
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:48 +0000

    frightened sister block plenty force

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
