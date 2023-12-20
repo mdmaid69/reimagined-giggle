b9e83768f70db846354e16a2bf3dac9005ae8db1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:48 +0000

    grade build leg scream foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
