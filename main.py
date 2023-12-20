9840d295150da73418a792f0304479a037ec052f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:56 +0000

    fortunately achieve spill study glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
