be4a98c3118bb363b1803eae0d64967a1551c343
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:13 +0000

    embarrassing frighten worry never generally

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
