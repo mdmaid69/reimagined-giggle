827a8a93b1f4d815a24be32e7847635c5ddfcf9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:32 +0000

    dry spring ground push square

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
