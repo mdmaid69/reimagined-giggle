dd2abc5cac1be44a5d141623c249141330acde5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:22 +0000

    crisis speed picture organize give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import datetime
   def get_current_date():
         return datetime.datetime.now().date()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
