91d982ec2ad2bbe0330bfc8accbd132c8ed8b3ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:40 +0000

    sleep storm confused common shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
