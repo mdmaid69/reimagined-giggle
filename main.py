8b9b6df4b60047ff53a4c9f16762319d683c5118
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:27 +0000

    anyone such smell luck pay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
