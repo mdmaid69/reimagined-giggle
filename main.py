a39caaada3608248190bfac9179327dcd79fe370
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:19 +0000

    slide see experiment meanwhile meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import time
+def get_current_time():
+        return time.time()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
