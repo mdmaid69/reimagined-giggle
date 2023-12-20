0e2661a7eca0c0dfd2182a997bfef08e3b0c065a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:09 +0000

    trade order grow spite suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_to_binary(n):
+        return bin(n)
+def calculate_work(force, distance):
+        return force * distance
