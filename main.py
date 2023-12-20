ba4e440315aae05e10a38d18f4ae3bc2fb9ae86d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:43 +0000

    discussion hurt teach strike care

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def convert_to_binary(n):
+        return bin(n)
