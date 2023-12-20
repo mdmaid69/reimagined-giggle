2fff5697bb755c5dd96f393fd3f48c13b77557f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:07 +0000

    human careful niggaacademy bringback past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import sys
+def print_python_version():
+        return sys.version
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
