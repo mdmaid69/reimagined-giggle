0d2e5f344a286a4dcaef990abbe174574ad4c2cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:29 +0000

    never kill their tap discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
