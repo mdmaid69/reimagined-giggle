8ededb5344b1b718a2c1327d6efc83ceacb248a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:18:51 +0000

    beat below anywhere giveup trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_logarithm(base, x):
+        return math.log(x, base)
