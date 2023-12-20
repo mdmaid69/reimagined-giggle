c8e77433a8f872a783acb5b558a1d9dafcf32993
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:49 +0000

    pressure apart combination bet move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
