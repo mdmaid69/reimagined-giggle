93c96b4a9d6c7f80365724a9aa331debca63cb95
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:09 +0000

    news surprising disgusting family great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
