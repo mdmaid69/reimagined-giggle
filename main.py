ed59732d4edb26e894645d82bad56e0ed732c2a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:32 +0000

    cook guess rather lock from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
