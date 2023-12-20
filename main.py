b86dc69b8f9debe86a4c827bf49425705ae9eb25
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:58 +0000

    worth instead base basically advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
