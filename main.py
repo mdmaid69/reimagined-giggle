adeb0f8b5bfd16e7e17648b98d682c8bc49194b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:26:55 +0000

    dangerous love chance turnup school

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
