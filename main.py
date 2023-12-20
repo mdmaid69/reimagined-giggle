7584e4c14807b6f93b7ba8d3337eb4dd03ad6235
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:39 +0000

    reserve fond detail impatient contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def remove_duplicates(lst):
+        return list(set(lst))
