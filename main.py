4ffcb18a584a0948e2d785552fee49271bb761cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:23:21 +0000

    concerned independent represent process great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
