0e38af15d0fa1a0b79ae85b2902d795aad4c32b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:37 +0000

    can very shine open land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def reverse_string(s):
+        return s[::-1]
