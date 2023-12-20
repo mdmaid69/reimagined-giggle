39c067c24b9688ccb726caeb60ea82fb264e7fe0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:01:10 +0000

    again best limit get demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
