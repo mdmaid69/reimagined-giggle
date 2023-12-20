cb206cf20afae8d3b872a79258432e5c4c56d672
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:08 +0000

    beneath whenever artificial basically charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
   import numpy as np
   def calculate_standard_deviation(arr):
         return np.std(arr)
+  def convert_to_binary(n):
+        return bin(n)
