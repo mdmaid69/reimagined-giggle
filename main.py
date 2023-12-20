4f1cec8f10a52466f2e70016f037d54233aef946
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:51 +0000

    mistake understand 568energy wherever final

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
