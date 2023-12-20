0130f46cf68ab2370353da98025e63c63b4be99c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:18 +0000

    side suit blue mean equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-print(os.getcwd())
+import time
+def get_current_time():
+        return time.ctime()
+import time
+def get_current_time():
+        return time.time()
