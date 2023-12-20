d61849151f5553bf8d7af7c2a5546804709e4355
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:26:08 +0000

    immediate tool lucky mend over

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
