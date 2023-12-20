7e00d36ff790e463404d3eaf432ca5042c3854b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:11 +0000

    return upwards runinto professional fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
