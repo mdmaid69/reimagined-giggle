60e91d32c7e87d277d8f1a54dc08985e524ca53d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:47 +0000

    sudden bus public provide middle

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
