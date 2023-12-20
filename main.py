be6c0e1ca08fb0db1ecf6d9a982b226367db5436
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:47 +0000

    variety remark walk upstairs design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
