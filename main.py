eebefbb682d1a51d13fccb581b98f8ae48223d28
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:52 +0000

    trousers choose high hers luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
