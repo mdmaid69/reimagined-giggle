e10abe91efa8c34d64015c4864645600c302d154
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:29 +0000

    sleep private spill may goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
