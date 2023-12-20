9fe70c42e77afd99eb8b50addd1119e4fa8d1b0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:27:44 +0000

    destroy doctor stupid development live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
