cf5bcfb85425152c0bbcf7a4e189d76b67d6d291
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:08 +0000

    path head most behind edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
