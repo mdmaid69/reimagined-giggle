c7529882052e7d140b74952eb218a7345fa87f28
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:33 +0000

    study exchange ring hit result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
