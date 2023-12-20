313d624ad7adb5d8bf650a9c31e701fd2e7c4482
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:24 +0000

    rarely similar run vanish objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
