4a85e8ee0c8720c64dda7e9eb3a886580f45102f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:47 +0000

    moment excited I noisy water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
