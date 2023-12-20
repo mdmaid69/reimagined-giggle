31ae590517783c9c878f44c6095d25faa313d46c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:08 +0000

    opportunity lookafter wherever in reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+def print_python_version():
+        return sys.version
