b9b9ab5395c8ee387693bf74d113b577f2ea6eb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:28 +0000

    callback height thankyou form perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
