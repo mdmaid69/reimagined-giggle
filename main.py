e0c9049c90a19df05f3d54f531e4f4f41f36eb61
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:29:26 +0000

    interrupt arrange murder likely pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
