5c6a94b6906dd6b632ba2d2b9147c3ad12ddfcf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:28 +0000

    interview satisfied measure power part

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
