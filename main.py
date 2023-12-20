eaec92e6315741ecb114132069d5d92a04039834
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:50:01 +0000

    month callback without Oo pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
