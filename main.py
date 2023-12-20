04864c8de55d78b830ed5511aa0e7a3a77b46ce9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:16 +0000

    future in whisper weigh getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
