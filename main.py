be9bbe1e506962715f5d17b10428ae06d0002be1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:20:02 +0000

    giveaway shop certainly cheek chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
