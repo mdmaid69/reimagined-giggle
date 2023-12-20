a137f13d6de8547f70ccfe0e7e2d90b330bab1e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:33:26 +0000

    work cruel laugh flavour success

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
