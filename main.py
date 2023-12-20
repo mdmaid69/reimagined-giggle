c77b2bb852e1ed9194c678d09953657cc7f6804d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:56 +0000

    experiment listenfor matter themselves track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
