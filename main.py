ce21c7995b57aeb97fd57626ca2bacbb98e90419
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:14 +0000

    anxious sleep steer sure section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
