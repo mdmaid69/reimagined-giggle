5ed769b0b7baf0722bec782236032abb9d8001b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:35 +0000

    well well description sleep round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
