23c0fd72227ed6157904600f2a63be7d11e99b45
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:33 +0000

    steal legal height slip medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
