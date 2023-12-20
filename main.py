eadbe77bdaa67757667fc617159755f650b75c08
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:58 +0000

    hurt hide sit charge shout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def find_max(numbers):
+        return max(numbers)
+  def count_elements(lst):
+        return len(lst)
