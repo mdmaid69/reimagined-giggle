890284d2b0d8d50706dc0ed2a5c19c897692a4a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:02 +0000

    crowded rush smile do cruelty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
