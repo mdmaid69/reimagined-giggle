dae58f11da299eff5fb5949d5e35f7caab5f462c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:01 +0000

    in performance storm food his

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
