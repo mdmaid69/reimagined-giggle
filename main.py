0d8604163e6806a4cdf919d8c762ce4eb51d56db
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:01 +0000

    survive error where down exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
