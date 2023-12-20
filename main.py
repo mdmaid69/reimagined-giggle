f7916a75e00d6c43c4fdacf6fcc90daaea1593ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:14 +0000

    section positive practise cope fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
