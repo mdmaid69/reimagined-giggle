507a5124e7cf510582041cf78a171a53fb1e2275
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:59 +0000

    try rain practise break after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
