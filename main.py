53d1ab0272d48902c9a8a732bee96da98b50caff
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:50:22 +0000

    room wake test come unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def reverse_string(s):
-        return s[::-1]
+def subtract_numbers(x, y):
+        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
