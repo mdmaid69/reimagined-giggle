69c4c0d3487ffe7b2421d28e80fad6252f301616
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:42 +0000

    petrol accidentally we arrive eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
