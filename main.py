1e073a6c334fb2970bdbf7d57595afb53dd90ab6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:19 +0000

    sensitive press conduct argument pleased

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
