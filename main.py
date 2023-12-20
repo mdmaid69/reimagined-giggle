4276e9147075af030333a25001492de45f71d5f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:57 +0000

    ear injury lunch beat occasion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
