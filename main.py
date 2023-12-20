80380364985db34449677907e1b4bb1bacf99a93
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:04 +0000

    attempt gap community kill concern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
