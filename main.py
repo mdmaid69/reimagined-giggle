6e945894d7db532a7c454a560e88db010c13cbee
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:42 +0000

    row mine crash include aim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
