0c8b5e0313e2bb872845adaa68a85cec0146626f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:52 +0000

    when whole little squeeze expression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
