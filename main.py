ff531c5411d0ada9f67d99914c814b534e0467d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:34:16 +0000

    performance air almost law habit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
