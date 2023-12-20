d422f1bd22e7785fb5819d5df90266f2c3610f51
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:11 +0000

    tune cough fall disturbing late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
