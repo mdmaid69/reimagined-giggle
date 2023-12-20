59dd7da3b436bb6e715c72adbebecfb6b8d71395
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:35 +0000

    order whatever work disappear fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
