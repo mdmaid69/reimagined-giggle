ea4ebd6582c856e66b4e8b4a30e3b09fb7f87754
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:12 +0000

    book Gg depend Qq crowd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
