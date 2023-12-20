bbe804c8722600e5b49bab5c4325e98a84a17a8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:07 +0000

    skill hope trousers file waste

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
