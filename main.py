4115de45220fa331f14e77edd30a098ad9106ee1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:11 +0000

    Yy notice certainly lower press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
