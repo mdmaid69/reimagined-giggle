8ca5035d26c170d74df1a39e065269d1abc3a258
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:04 +0000

    quarter will clever since wrap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
