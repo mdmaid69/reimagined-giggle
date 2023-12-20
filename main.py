e23e1ab7ee9d18f9f79e9a3be6536c67c2f5ed99
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:09 +0000

    treatment fix condition less injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
