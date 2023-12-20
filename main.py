9f1a26613aa10f2a06ccf64dd19ad881d453436d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:39 +0000

    gradually heavy it task this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
