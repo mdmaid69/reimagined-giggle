2225166c5c43bfca4a67e24753a6d2fbcea7c8e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:01 +0000

    protect keep exaggerate look last

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
