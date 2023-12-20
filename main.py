a5d14f01394db6974a0e34ae62d037187f0fe42d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:09:45 +0000

    attitude cost sting tradition satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_user_list():
+        return collections.UserList()
