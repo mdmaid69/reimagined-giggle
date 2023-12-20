fdcbb005426d813f16c1f96224ba4587367115df
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:34 +0000

    practical shake basic little shake

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-print([x**2 for x in range(10)])
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
