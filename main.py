1b5ba28c51785e73e4cc184e457d72b2d96cb164
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:03 +0000

    last free record circumstance force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def reverse_list(lst):
+        return lst[::-1]
