9380d3192981429923d09767a94a8f85b1a41674
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:07 +0000

    wind powerful even trap begin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
