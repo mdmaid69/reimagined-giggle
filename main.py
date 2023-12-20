c9070be504081f9c9f7acacc07437f6e6aff1316
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:51 +0000

    wide careless clear trade quickly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def delete_file(file_name):
+        os.remove(file_name)
