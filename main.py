8383415826dca67da64f9e7b1c1b097763771b1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:41 +0000

    brown related that happen fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
