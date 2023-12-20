1a483875748e933605396ea2492aad6632e0f611
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:36 +0000

    tell well broad whatever any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
