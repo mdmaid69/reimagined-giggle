3c5a4b069c6b20f6b166d5018b85e6836d97585f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:22 +0000

    goaway birth waste what they

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
