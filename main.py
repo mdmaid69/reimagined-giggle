0c3a72e9e89d8acd3eb3fb2976382a1962c0d370
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:14 +0000

    lie precise recent radio smile

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
