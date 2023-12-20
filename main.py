e6c9f6a1510c41dba5ab7e3c5a34aadb32f121a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:25 +0000

    will may secret debt throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
