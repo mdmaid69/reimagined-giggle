9971a593645be1b57f74b02ee72c844549858f1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:21 +0000

    class ticket spill blind ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
