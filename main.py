d876b9d5680b8479fc556b8b81056d9eee7c8dbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:37 +0000

    draw could programme early recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_min(lst):
-        return min(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
