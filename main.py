53c7aa6f5c049894ef654047fcd0350803998ff0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:04 +0000

    move ocean beside watch suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
