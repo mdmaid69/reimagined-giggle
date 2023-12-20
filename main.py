2acae3e1ede8597f75741085601760eded43f9c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:58 +0000

    cost marry safe goon reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def convert_to_octal(n):
-        return oct(n)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
