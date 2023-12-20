61c3f3192d0e678310b2c2197918aa566804cbed
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:26 +0000

    little issue careless thankyou broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
