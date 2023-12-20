25e06c1e69abaab3d076465676b135c3df9b1b64
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:05 +0000

    tear lift foreign strike previous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
