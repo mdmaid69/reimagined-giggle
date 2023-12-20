d353dd7b37aba564193a2913ffda8627c4282e25
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:34 +0000

    mistake order bother share telephone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
