48a2b69bc4d2ecef59f68ddff5925353771e91b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:50 +0000

    right laugh once tie whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
