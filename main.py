21368ed837806d4a3f7264e48ef755892c245289
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:19 +0000

    friend confused bite bin coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
