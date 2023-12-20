6a83f5bb45d70145d819846a2be851fa41e6f097
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:36 +0000

    organization law pile recently learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
