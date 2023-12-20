be09a723d0f495dec71ad5a0b04bda34d661e8c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:41 +0000

    evidence period permission spite money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
