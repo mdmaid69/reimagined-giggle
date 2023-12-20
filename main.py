60c94914e87a98f58965051d7205ef1ec5502699
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:32 +0000

    guess drawing consider imagination business

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
