5d6561d7a445eb1f5e39f7565228a334658a8621
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:16 +0000

    angry town go kill temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
