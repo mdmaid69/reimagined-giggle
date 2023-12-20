d0d9621b20f6cc59bf2d514288a0e951ca1b0acb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:24 +0000

    type touch present unhappy hollow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
