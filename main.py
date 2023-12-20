ae0e2dcf76f051d5d80647239d3ebd84082a6d82
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:11 +0000

    certain fine stop doubt breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
