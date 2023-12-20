6b97fa0c0573108e6168169ffeb8563e8a6f5500
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:33 +0000

    first preparation tongue check emphasize

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
