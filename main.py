4dbe1e37f7d58491b5b2ff3408224cbb22c13882
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:13 +0000

    relative broad university cut unit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def add_numbers(a, b):
+        return a + b
