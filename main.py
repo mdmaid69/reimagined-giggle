0dd2a329a78af76e30d08f3af55064f20a3d8d70
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:03:41 +0000

    moon relation track activity protect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_exponential(x):
+        return math.exp(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
