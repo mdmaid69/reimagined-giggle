4383c63474b4ce39fd7e77ce9ae7b2dd1c9febf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:20 +0000

    father dead callback invite interfere

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
