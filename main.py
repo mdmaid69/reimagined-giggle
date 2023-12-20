2639011dc2765f906ca3841f7a05867af97067cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:27 +0000

    test hit 564encourage damage error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
