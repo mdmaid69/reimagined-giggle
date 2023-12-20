32513a4c88be6bdbac91b313b4e32d07df09b0ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:23 +0000

    possibility possibly into quit manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
