fb8df594f27c35fb0aa86d3fe8a70f85146ea259
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:31 +0000

    government team language bother precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
