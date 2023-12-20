fce3a7ca4370c5de0ead7f9f022a722360e05a1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:05:43 +0000

    advanced speed breakthrough belief just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
