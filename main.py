69004324fe4a78208a540a4110601481e0e9f0b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:38 +0000

    rise note crash rule field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
