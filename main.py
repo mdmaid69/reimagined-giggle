480fc666eacfff3baee7a1eb173a84c0c9c21f26
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:02 +0000

    label innocent eye laugh variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
