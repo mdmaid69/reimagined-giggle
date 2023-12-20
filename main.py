a5146377da3fb22b65c8f6420663eadd40908807
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:40 +0000

    solve thanks unhappy drop name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
