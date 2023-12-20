f9fb3ed55474f26956cab9575a5ead06bfcfc4a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:11 +0000

    transport moment petrol unfortunate deliberately

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
