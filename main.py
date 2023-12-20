e108caeb86227a84e45c8619436f08f2e2b4faa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:20:47 +0000

    congratulations board as skill exchange

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_exponential(x):
         return math.exp(x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
