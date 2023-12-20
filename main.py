5c8a596c9706567dc582519c4dbef007a62f041c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:12 +0000

    contribute together anybody accident feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import random
+print(random.randint(0, 100))
