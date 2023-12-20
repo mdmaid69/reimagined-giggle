5fa5b137604bfe3446561e922438c1434dbb8354
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:11 +0000

    good mend dress hard strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
