a5e281d93fcc99ac1796f6ef6797a69830da041f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:02 +0000

    cause base limit desperate to

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
