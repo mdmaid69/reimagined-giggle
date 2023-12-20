e96eade17ec82f897d8ca1c2b28c3ff93ce628e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:06:16 +0000

    compare best dream exit string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
