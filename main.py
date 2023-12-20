cfcc428ad20ddd8221ab484cbb16874965ad55b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:24 +0000

    change story unlucky fire born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
