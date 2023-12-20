c3942384d94d00b27f4d57383f922473cf549ab7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:50:38 +0000

    life crash dull but new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
