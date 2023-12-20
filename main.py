8cc89d8da25552d4f2ed31debfa851401920d5b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:45 +0000

    laugh whom congratulations finally turnaway

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
