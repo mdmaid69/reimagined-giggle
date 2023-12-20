a68c803fb16b9434e430b20344cf49c7afcc0680
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:33 +0000

    black attempt attention centimetre shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
