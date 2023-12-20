c43c2162b06f493440fbe6033529f9cc2195c6f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:20:06 +0000

    below children lie 567enemy drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
