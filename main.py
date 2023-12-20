003e9f661d25e1223f52d336e6fde362aa9583f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:02 +0000

    will nice intend quick pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
