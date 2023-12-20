5d9f5f83fa620a5202c5e1858338bf5d23135b7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:22:56 +0000

    level especially help return no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
