82acaaf15c320d705a19a3f68c0d49b874ff4585
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:33 +0000

    board rough cook innocent risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def add_numbers(x, y):
+        return x + y
