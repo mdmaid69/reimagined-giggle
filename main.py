c648eb67a17672e790980dbf0d6346cd672da904
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:34:24 +0000

    scene music hour remember calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
