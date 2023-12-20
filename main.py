36c04e93233bd009de4e50dcecf8b8b65200aaa0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:53 +0000

    whichever official lookup leave ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def add_numbers(x, y):
+        return x + y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
