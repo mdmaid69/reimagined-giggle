615e701c96d945495369664b7f5a60dae2b68c16
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:56 +0000

    fortunately gap breakinto brush careless

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
         return None
