fe9d28217d70a982e3d93b047f726cf94992a209
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:28:44 +0000

    especially accept Jj harm mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+print(os.getcwd())
