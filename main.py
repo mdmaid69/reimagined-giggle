8f17027d80ddbfbcb46ad844d69151ddd5041aea
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:09 +0000

    board rain direction activity shrink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import sys
+  def get_python_version():
+        return sys.version
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
