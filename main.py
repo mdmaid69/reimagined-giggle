bc301b9f6ac0ae63289ebdb7783ce86b07b1995f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:31 +0000

    coat wonder afford cinema catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
