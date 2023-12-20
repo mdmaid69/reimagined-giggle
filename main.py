20f477e2c72be768f11db7f93ea184b86f5c3d4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:42:14 +0000

    film true theirs play market

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
