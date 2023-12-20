e7bbcf25b276b80478d2e15d8439805717cb6034
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:36 +0000

    question lean row odd different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
