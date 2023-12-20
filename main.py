cdb0c286daade67ec9c7fb229810389c3fc4662f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:52 +0000

    natural claim lay innocent breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
