a0092f483ee9bbf004562599119962d149801f35
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:15 +0000

    brief herself easy skirt meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
