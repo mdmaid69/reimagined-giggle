ab36d589eb1f835a7faf2561fc4b7b28554eff9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:37 +0000

    operation time case vanish obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
