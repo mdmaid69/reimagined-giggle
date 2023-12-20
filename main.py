b4ced25be3c2907618bb5dffb68e43eed7af167a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:01 +0000

    pretend seem lie 572enter score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
