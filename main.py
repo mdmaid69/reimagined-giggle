9ab58c9fc8809f7a11b2e133c249765d40adc786
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:19 +0000

    address search sleep good outside

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
