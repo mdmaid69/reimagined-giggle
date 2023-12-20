0ea210a8efc3a8b50f5f04db9430074208a3fcbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:32 +0000

    design bear weather exact length

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
