af128a9a8ff36d6c9e9d2329650484ec2e95c4bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:20 +0000

    meeting approve emphasis noise debt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def count_words(sentence):
+        return len(sentence.split())
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
