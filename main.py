f865500c70a02bccb7a379601464650d8724ced5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:32 +0000

    instant angry play pupil voice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
