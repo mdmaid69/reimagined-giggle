b861f622c25f67a34df6753054a34e534fef3928
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:12 +0000

    reply table cope age off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
