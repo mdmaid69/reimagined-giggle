439430da52367c7d9f1ed7b9dd52075d5e369dee
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:35 +0000

    pity pure plenty view judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
