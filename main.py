b195e67e15d0ac0e755809496c8ca79affcba8c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:40 +0000

    in lunch spray past yesterday

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
