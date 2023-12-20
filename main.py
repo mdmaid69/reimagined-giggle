5cda4437f604fb872514d64fa46eb29ee5004562
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:52:09 +0000

    behaviour ready appear angry this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def cube_number(x):
-        return x**3
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
