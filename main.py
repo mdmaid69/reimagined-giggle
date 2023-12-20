cb4729acac2a7eb54318e3617a34acaf54256c7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:39 +0000

    gas person violent comfortable keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
