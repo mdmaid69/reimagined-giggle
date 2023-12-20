b866fe680aa99c6282881134dc44a6cafa331134
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:41 +0000

    appear getout progress spot table

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
