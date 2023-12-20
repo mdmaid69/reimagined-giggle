f700ee7f9cd3acd1e6c80ee578784dff54d2ae7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:14 +0000

    picture meal old account anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import random
+print(random.randint(0, 100))
