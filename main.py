8ba48ac35c5215838bd409b39e97bd05e20255ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:38 +0000

    limit opposite see subject block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_itemsize(array):
+        return array.itemsize
