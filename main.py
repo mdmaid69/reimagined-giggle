5d647e292922fa36a38b8c47c2b65cadd43cbed8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:51 +0000

    ashamed drawing workon from independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
