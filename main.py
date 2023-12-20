283c05c515b95b16464d55871b13e8269ce6a1df
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:27:43 +0000

    own live fix serve wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
