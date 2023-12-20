047c5b11adbe82ce3cba91bb6cc7ec3e3cccf0b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:32:04 +0000

    exist job unhappy goaway public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
