b1f1d2b30c1e2ac96d1f4c890a13302d4b9db91c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:05 +0000

    worried tragedy minute scream reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
