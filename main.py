e17af8a88e02144b99ea3c438086f046770c8bde
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:57 +0000

    degree major anybody success kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
