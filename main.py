36eca447e983c41b47beb3c229a82b0623e08992
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:11 +0000

    factory disappear scare force face

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
