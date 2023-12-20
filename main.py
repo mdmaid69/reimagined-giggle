b7cc4e3c0a5d9a641da574a79005499bc7cf7f0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:11 +0000

    personal niggaacademy decision now afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
