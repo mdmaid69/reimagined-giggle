f277dbb55d4b965d49f4062b0ab3353a75590194
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:56 +0000

    tragedy influence worse measure approval

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
