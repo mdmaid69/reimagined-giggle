a7e91da72021e11fceb3d1987b4cad97a46b85cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:21 +0000

    pure roof limit regular development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def sort_list(lst):
+        return sorted(lst)
