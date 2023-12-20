97eb97e6d25a2c15f026f2b58f25ccf90bcb5206
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:45:01 +0000

    long stay active concentrate greeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
