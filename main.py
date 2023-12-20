4275b5b0fbc13897b618e0741b96ff9c8f3f945e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:20:15 +0000

    block sweet hair getoff news

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
