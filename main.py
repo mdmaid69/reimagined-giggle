65676c6203da79d1150eb2d59db03a3dd178f397
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:07:44 +0000

    maximum quick wash hungry any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def convert_to_hex(n):
+        return hex(n)
