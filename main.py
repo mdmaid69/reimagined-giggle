95523fc2cf19b752df7dd56205930f041d1b1810
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:52 +0000

    weather trip strike strange serious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
