0151d57b5cf33cda5c9897923b11a55e659a642c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:09 +0000

    surprised leave drop couple can

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
