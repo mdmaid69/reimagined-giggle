0147f71ccf5ad68ab1c6448f3d47843b6e02ca54
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:02 +0000

    either interesting object rise cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def append_to_array(array, item):
+        array.append(item)
