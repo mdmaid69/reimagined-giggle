0c14610a20a468b99a9ef45202af2d5459f4185c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:50:10 +0000

    draw country programme tip air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def clear_array(array):
+        array *= 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
