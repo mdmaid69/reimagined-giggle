e2f544bf3056cb841d41480b45c422d770aaac70
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:13 +0000

    difficult something everybody indeed ticket

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
