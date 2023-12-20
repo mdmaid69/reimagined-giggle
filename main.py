80d81b3276b006ac9a92062b9c9ee3469ef8e95a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:54:01 +0000

    complete brown save equal determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
