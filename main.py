cf0d6521c7c0d12243d86b45fef8c075a2a634ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:48 +0000

    best cover concern Ll public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
