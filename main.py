7741e8193064bdef0eb8d9cd5ccaad55fafd5555
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:23 +0000

    pour objection payment strong computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
