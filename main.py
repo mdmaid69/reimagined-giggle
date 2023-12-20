270af8c3e2c6929dd2fbfe3f78157ee5ca28cd90
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:52:25 +0000

    track length exciting destroy tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
