615cf2744131f3d646d936997da6a0fda7c146f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:07 +0000

    small thankyou turnback there together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
