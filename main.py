dbab03731983992665a8b124092e0ac0e7ccfcbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:56:55 +0000

    soft discover several combination block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
