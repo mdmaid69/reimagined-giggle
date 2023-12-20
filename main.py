0cdb891ef42aff0c2be1490bc7fe817329d98236
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:33:56 +0000

    imagination neat good discover turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
