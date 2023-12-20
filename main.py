1b6af40a5ed2699deedaa33e753b903b72dfe07b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:11 +0000

    natural have chin comment different

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+text = "Hello, world!"
+print("Characters:", len(text))
