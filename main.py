0914457a1afba2f9343cd3efbf635f59369e45b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:42:17 +0000

    idea far middle owe dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
