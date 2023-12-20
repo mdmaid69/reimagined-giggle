6202469254bba18471ad4c9ca9fcfda249c84a08
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:40 +0000

    foreign target takeout hurry violence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+print(sum(range(10)))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
