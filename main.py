5f3a6792ae70c0013ada2f1a4225abdf144b5679
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:10 +0000

    cross block tap stretch breakdown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
