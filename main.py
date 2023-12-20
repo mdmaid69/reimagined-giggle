0fafdf797fd9e9e815b0c1a243f290710fab71d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:56:35 +0000

    us breakdown shut noone scream

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def cube_number(x):
+        return x**3
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
