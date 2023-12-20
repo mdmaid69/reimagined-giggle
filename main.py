2b5f924ef248dcc75e066b2ea108fcd395d9436d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:25 +0000

    cause pull still be neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
