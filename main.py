48b982547bf68c3facd636e34368f6a386134952
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:36 +0000

    condition whether immediately habit benefit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def change_working_directory(path):
+        os.chdir(path)
