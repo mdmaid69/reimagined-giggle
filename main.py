1720b1a9f97eca006bacf4359255e2b3e934d613
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:30 +0000

    expect increase muscle popular comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
