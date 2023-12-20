43b2afbfc4154b32fa1e008509bbc327263214df
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:26 +0000

    contact spray call odd false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
