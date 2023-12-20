6e02b1d4db3dd85be2c3949f20ee7603d1785487
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:35 +0000

    relax move people really seem

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
