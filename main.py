24e20af0dd135eed54d5991bd126510af3129250
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:06 +0000

    rest ordinary takeaway fault sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
