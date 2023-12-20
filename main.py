dc400d434b61e06b728fb1f1d8d0649b609dbc46
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:48 +0000

    lift pile affect continue content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
