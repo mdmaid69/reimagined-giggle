deaa8c6b5113adb01f67d0c37c78ca9e024d4ba2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:54 +0000

    convince name steal flavour price

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
