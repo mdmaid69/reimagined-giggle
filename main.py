6b0a4c480b03d19c12ed4915b05c3ffe317b3fa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:39:45 +0000

    prove Nn alone class argument

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-print(sum(range(10)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_int(array):
+        return int(array[0])
