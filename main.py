c96fb712f22c0153c3e20e64185688844732329f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:42:01 +0000

    loss worst communicate machine so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
