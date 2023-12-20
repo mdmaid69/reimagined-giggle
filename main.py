7a32b395bca0585b6e9d219078c6501b4047a0d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:36 +0000

    broken sink degree spring then

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(5):
-        print(i)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def count_characters(sentence):
+        return len(sentence)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
