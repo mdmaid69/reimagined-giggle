328cfdb8e58b74019b7b79c047fbf0bb4fda3ae6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:09:49 +0000

    right run search copy substance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
