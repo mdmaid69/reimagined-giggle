37424162f7a00fd63726dd182cc2012f983e21ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:06 +0000

    company concerned vote you closely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
