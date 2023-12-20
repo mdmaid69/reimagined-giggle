19a408799de98509c279ec1651189a2ad8ddc547
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:18 +0000

    cum rule mean set measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
