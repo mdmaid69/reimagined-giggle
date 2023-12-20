2fea135f29c248bc18ae850244a92eb45f7259b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:28:45 +0000

    previous family trust table onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
