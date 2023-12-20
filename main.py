7abbc90c17339b9057ef594f323e6a0499ab0447
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:26 +0000

    seat part surprise like damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_base_name(path):
+        return os.path.basename(path)
