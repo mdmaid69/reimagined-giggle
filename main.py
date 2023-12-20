ef450018f08e519aa27f070caeabbdfb9145a6ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:05 +0000

    contact identify distant formal task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
