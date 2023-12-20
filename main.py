a458c618160c5d5ee62362af126aa5b766fa5b51
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:27 +0000

    rush appearance strike border better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
