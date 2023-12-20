c9275fde88a15d3337085230b807b3288c514d01
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:07 +0000

    rest let float combine region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sys
-def print_python_version():
-        return sys.version
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
