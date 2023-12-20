5ffe72bfe9f6facb7bc28e8c7771b98120cf28fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:10 +0000

    motion quality whistle little protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+from collections import Counter
+print(Counter("hello world"))
