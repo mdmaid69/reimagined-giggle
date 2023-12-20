fbf830745c8a3d6a8bee5f703204785809f607a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:50 +0000

    threaten repair sense crowd present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
