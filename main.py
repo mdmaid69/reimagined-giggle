84ea9bdd2ee8c393b91a674f3be18fce1dc4e7cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:48 +0000

    lay flat downstairs equal performance

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
