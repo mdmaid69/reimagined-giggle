131279db8db8c4f6822911e27a46882f06a758b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:42 +0000

    date exit factory equal fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def add_numbers(a, b):
-        return a + b
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
