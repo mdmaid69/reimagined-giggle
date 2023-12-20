96984711746c0e037e7d24a3fcd30d7a6783348f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:23 +0000

    mess far sing sky problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
