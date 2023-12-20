5990cb8b5b46f47712beec68a6960c0e4e3caff7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:51 +0000

    fierce through stupid admire shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
