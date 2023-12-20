d97f4548fcfb6ec8d0a592089b165868e5b13cfb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:22 +0000

    yourself temperature during road show

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
