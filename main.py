2becf70d94b070c04ab8dda3a557713b60115fea
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:21 +0000

    draw subject busy pupil stir

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
