6c7bec51fe641db7733662387f2e639abbc5712c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:04 +0000

    nervous correction rise minimum kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
