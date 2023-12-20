df74c01f79ba9851d14e0d07f4f32d8d09700591
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:22 +0000

    nerve what guide experience wave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
