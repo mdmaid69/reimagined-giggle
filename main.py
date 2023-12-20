ca6ff59df7d893cdcd1724c838b6baaaa85f6682
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:41 +0000

    neither include break approve ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
