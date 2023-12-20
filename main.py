2830a251ee83b4ecb617160ebe51783d7bda49b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:33 +0000

    giveup proud apply behind convince

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
