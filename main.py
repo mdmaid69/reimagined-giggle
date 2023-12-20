63032b4b721a8ade1595f1e9ccb1b8265998e206
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:18 +0000

    condition callat loss hold delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
