9d448571510ccf3d3ba5c80c50ca46268dbcc066
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:57 +0000

    public anyone getback change learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
