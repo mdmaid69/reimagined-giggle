3856d5838d10fd8e2cafeb92ec815ddae74e4b50
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:02 +0000

    advanced grateful lesson favour last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_min(numbers):
+        return min(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
