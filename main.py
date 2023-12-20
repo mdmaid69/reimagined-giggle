cb2b73c51824fc1accd66b097655ddc2eddb73da
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:54 +0000

    shall narrow cum night side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
