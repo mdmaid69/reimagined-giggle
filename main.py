4431b5e2ce765f3202d66424f75491f871358f88
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:55:35 +0000

    mine that stay special curious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_acceleration(speed, time):
+        return speed / time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
