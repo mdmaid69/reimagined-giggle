5b2f5cf98ee1503f1b864fa1f89ec40eb5403d81
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:18 +0000

    comment wet share pause serious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
