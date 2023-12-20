8e0dd44f566e965b8ae59356ad86dd31d43b1bbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:49 +0000

    actual direct bend block report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
