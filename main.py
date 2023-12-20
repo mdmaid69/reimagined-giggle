abc6c6b9af8dad70ed3278a15d98566bcc6b0a41
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:04 +0000

    building grow interested fire distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
