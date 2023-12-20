8642c7b57fff247b2e59e6c6d6f8a0a74283e15c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:24:34 +0000

    danger empty talkoutof estimate impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
