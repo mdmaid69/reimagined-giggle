378525b99ee4edbdf23a0f4f9f15b735bc0a5e56
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:40 +0000

    either complete off replace object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
