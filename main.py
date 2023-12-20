9756afc78b00e5a3e2584e029d5159690b804df1
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:09:30 +0000

    concerning turn relationship floor congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
