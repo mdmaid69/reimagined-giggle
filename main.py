5c0607775f143c5dcafb70ed411578bdbe6e08fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:19:09 +0000

    wish unless offer character hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
