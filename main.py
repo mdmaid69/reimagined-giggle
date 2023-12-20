10a7e338a53c74bb1fe6608f6f331828b47b903f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:05 +0000

    decide supply life workupto eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
