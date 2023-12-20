42d53afc0ccb40809988f6971de26dd241a59bff
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:54 +0000

    Ll declare break measure proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
