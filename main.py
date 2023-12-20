6ce3a05aa64e15fc700c359cdb7230170d948311
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:34:46 +0000

    lunch cry taste other recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import datetime
+print(datetime.datetime.now())
