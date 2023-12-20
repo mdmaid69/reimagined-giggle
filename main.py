269417b67ccc98cea9922f167c51c1fca797578e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:06 +0000

    down mainly between hold bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
