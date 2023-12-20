c82f8119c4cb1666792501555824e29198a5f97e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:23 +0000

    listen apart talkinto low deserve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
