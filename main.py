30358ed33cc551a7b66fd3d45baee9e9cb9b6cec
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:43 +0000

    fashion shrink guess shopping practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
