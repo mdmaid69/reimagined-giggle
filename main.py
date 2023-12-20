ef2e1b4af7b38d19f20079c552ca557c5dbdd1cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:23 +0000

    rest callout floor still mad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_min(numbers):
+        return min(numbers)
