eb16245b565f314aa23e23cce5811aaf9ebc3ddd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:00 +0000

    calloff ask swallow into ordinary

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+print(sum(range(10)))
+def find_min(numbers):
+        return min(numbers)
