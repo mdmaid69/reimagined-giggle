04c7051910b42b9839b32c550c2f88e378bb5435
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:05 +0000

    everyone hit opponent cum cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
