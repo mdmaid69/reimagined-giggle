8ad708116d5807d7608f8a1bcb37a5111676ea93
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:51:42 +0000

    nothing Zz effort trouble correction

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
