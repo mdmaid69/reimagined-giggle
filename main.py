3b7d05232ea6f8643374b1ccb7ad653ea08e903a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:55 +0000

    satisfactory activity sick upset tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+name = "Python"
+print("Hello,", name)
