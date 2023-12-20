942af65fc98cb20f4878a08b898339c3932432c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:43 +0000

    holiday suddenly weigh few trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
+import random
+print(random.randint(0, 100))
