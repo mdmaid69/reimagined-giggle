eb50350b00c2ca3e9b5d9e5d1f4549dde26a4fc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:42:06 +0000

    Zz round satisfactory fast above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_work(force, distance):
+        return force * distance
