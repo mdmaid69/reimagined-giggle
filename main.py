fb37d1e64cd54defcf4ba77999cdb20766343f2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:23 +0000

    path possibly lesson flight effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
+def add_numbers(x, y):
+        return x + y
