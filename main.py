b502c9ae76cc38d823cea582a20881f8d383e720
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:08 +0000

    geton buy gradually match spring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_string(s):
+        return s[::-1]
