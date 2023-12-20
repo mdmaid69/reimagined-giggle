bb94835fe8db44bb5ac87c098d5c8c6b4b74d026
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:26 +0000

    statement friend hang start influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
