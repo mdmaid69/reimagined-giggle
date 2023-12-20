35dfcba178097ef7ccc2efab07c44232a89a3a99
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:58 +0000

    post unable description pretty choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_str(array):
+        return str(array)
+x = 10
+y = 20
+print("Sum:", x + y)
