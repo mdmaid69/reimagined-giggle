8677e506925342e9e245c7210718e0c2bf5e76e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:17 +0000

    thirsty scene appeal basically creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
