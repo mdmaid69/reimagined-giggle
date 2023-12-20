66318a528e6aed6717625f0f87f87de69e2949b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:00 +0000

    neither approval comfort hide team

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
