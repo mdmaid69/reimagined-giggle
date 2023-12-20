f445365dc8f535b116754a1aebd5db1d554ddfc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:17 +0000

    record plastic against confident last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
