8474b1ea402d66cdb7c08bd81ce7c311e5400a9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:56:06 +0000

    that pass to mind reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
