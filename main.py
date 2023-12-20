4b6fa25ab62145d15712b7d921e0c47d2542fa65
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:10:03 +0000

    only standard study develop giveback

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
