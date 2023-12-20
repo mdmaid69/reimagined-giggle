d9b7d599d5964d0284fc89495d5ce00cbbaf080e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:06 +0000

    mine fair immediately safety just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
