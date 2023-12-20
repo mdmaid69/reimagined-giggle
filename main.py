425357cee86f53b25a07a720565ea886d39cb264
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:39 +0000

    only suffer one pull language

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
