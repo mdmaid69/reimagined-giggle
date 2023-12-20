398bc679d8fbe951be60e49f86889fee01051ba9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:17:54 +0000

    brown uncertain pile test escape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
