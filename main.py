970de05c73d24a41299e59b3a7669c6a4fbc433f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:34 +0000

    demonstrate once force owe ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
