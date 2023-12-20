40b1ae9617fca485840b01da801304aba34491d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:28 +0000

    pair interrupt tradition side activity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
