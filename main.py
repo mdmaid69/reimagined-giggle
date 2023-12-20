123df480932a889bec7ec33b2ba7eafb97d2e219
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:56 +0000

    touch human arrange match criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
