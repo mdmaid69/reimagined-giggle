c12dfabf54f597774394140e044ed01580492552
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:44:00 +0000

    Vv dream there cut noisy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
