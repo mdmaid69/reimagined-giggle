9707722183130a3b550859e044313fb6cc2906ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:51 +0000

    young deserve ahead lead worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def count_characters(sentence):
+        return len(sentence)
