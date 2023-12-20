94cf59e388115909a185150fcc02da8b19f06d8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:10 +0000

    this whenever great sweet note

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
