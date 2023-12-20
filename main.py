ec42ee27828330c57682364da8d39bd859091503
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:40 +0000

    even letter serious taste hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
