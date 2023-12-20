1e0619f26b90a976222ab4f4a8401cd1eeb3b0b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:58 +0000

    crime factory fortunate bar press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
