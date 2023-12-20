3fdc4a1c8b68b458b3a65b0795d01c57de3c056f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:31 +0000

    inside moral label surprising thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
