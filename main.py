5f867416a695f6b67413d5e5ede582d1abb403dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:26 +0000

    sad advantage slightly centimetre getout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def subtract_numbers(x, y):
+        return x - y
