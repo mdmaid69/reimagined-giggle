ac9716c1ec748660bd9900f06fc867f28a1f38db
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:24:03 +0000

    naturally every threaten lend contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
