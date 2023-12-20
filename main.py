cf814d8b2faea2baab486005d6212df001461078
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:44:47 +0000

    sort guide sound lead machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Characters:", len(text))
