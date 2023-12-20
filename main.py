8509f1530cb9db099a3e5a6650727e9b471cb074
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:58 +0000

    accordingto war comedown deaf town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
