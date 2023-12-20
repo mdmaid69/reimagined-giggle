7c9104464916b16b91a85fee6a0a4bb788c91a65
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:43 +0000

    difference taste concern eat support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
