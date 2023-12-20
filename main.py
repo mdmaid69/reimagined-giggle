b06ab1e4c9737ace395da236d34e0eabd6b5c274
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:05 +0000

    abroad wave answer term mistake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
