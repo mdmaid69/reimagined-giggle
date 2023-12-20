67ef88bdc3b6fa83d66732fc64cde3bf68dd1579
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:45 +0000

    runout about strength sail subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
