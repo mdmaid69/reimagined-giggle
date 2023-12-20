95db3a48965e5b39a859a4dd52d6435cfb2e221e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:08 +0000

    rarely some anybody damp her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
