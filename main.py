8474510229629c89d21edc3232e73885574d3561
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:26 +0000

    but before port heavy bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
