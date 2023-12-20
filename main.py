a7f74d269b33869dd3d54f7a03a68d43b44bdec3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:58 +0000

    sure public poison weak outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
