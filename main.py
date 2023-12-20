71edb0b80a587b0136a74c16c0fd5b1616ed97a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:26 +0000

    handle clothes bridge escape always

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def convert_to_octal(n):
-        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
