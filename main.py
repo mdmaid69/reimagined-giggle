30ba4c52280267949fe1345461004283a75a1d78
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:07 +0000

    mend bother dry criticize underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
