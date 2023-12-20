ead13ce2b99f4784e3a6c590963c4e889991181d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:37 +0000

    rule project better forever skill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print("Sum:", sum(numbers))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
