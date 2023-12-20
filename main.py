a8324d76a92eb82e1f1979a2e4cfb881719508f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:59 +0000

    hang shade deep sense dangerous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_current_working_directory():
+        return os.getcwd()
