233399c1649effc59e15853e19c493a3bfda6e22
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:46:51 +0000

    stir more interview tax win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-def find_min(lst):
-        return min(lst)
