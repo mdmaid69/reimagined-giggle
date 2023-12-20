e382d7e4cad6c3152fc144056a473ad58bf312e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:05:50 +0000

    plain noisy chat confusing child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.ctime()
