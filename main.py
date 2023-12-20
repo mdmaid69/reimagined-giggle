afa4ddbf10fe8504e42659ed85b170a8e778ec6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:04 +0000

    age event shape appear point

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+print(sys.version)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
