7d9dd584bc3a5e9df4e3a2f8855481abb9ded6be
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:25 +0000

    separate schedule cheek obtain ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
