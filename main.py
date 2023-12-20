a3d3052f02d3905d21eba3783a7b189d8c7207a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:10:14 +0000

    tip bottom close unlucky popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
