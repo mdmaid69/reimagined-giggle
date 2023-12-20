c6346cfdebb51b140cd71ba87fb0af8adab8bc10
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:41 +0000

    equal read key morning pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
