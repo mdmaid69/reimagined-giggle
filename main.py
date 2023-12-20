86eb0f828383d3ee60810422f648295fc33df65b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:35 +0000

    action weather inside several whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
