fe01eaf0090ef6bf82b3743fdb0dc452419ca266
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:35 +0000

    everybody shall pretend greeting will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
