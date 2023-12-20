d4b0626bfd4572c036071ae426e66c60bc6f243b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:45 +0000

    real pair dream arm flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
