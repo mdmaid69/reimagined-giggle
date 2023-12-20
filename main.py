2ac760dbbd937b72473b6966c598b5035644b33e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:13:16 +0000

    common shout his hollow land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
