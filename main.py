876b1a57992a6a24d2d7c18efa50726250f8d520
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:50 +0000

    himself pocket nature unite conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
