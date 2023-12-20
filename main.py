2ebed5c7d4ff67595edbb314fd100bb93cecd725
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:09:30 +0000

    turn control produce welcome warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
