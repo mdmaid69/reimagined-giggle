c305ef7a26ea032a164f7c15401dd53e56a99e11
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:58 +0000

    kind school spot place square

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
