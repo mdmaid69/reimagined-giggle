faf3fe7bc76f2d13bd3c790a02c78717f01c441f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:14:54 +0000

    expression that burn bin attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
