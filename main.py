c2d1150f9c4359b830bada66ad33988757cd6d79
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:43 +0000

    closed violence song claim clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
