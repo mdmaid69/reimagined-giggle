c86b6f4a6d186ac2e19b900b8ee34cf3cebe3ec6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:32 +0000

    complaint brush attract reply fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
