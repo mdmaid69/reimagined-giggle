0936ad713b2f5c9dc444edf3b6d6294bcbbe9e38
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:16 +0000

    runaway centre sensible basically year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
