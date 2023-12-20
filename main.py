611c66314d51ff4b6191cb6a35f195147aff492c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:22:33 +0000

    need be sound argument meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
