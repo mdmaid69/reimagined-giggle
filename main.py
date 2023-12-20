ad23a18500a4de4fae83352112996e0108c45c3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:37:10 +0000

    better lunch dull spring interruption

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
