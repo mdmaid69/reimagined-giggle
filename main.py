c8dd95e904edd42d676250aa69fa364088164b14
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:40 +0000

    various steal keepfrom rarely contact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
