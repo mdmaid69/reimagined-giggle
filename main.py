8f3c96c94fc8a33afe883893b4233ff49dc9e6df
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:34 +0000

    progress keepaway classroom treat show

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+def sort_list(lst):
+        return sorted(lst)
