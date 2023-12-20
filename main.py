4c3ebb2cc560333ac83063be5a525ac94be842b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:24 +0000

    away tool smile before shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
