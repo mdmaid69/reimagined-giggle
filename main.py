e7d3391cdbf58f8d62b81441bb2d09a899a2d8f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:31 +0000

    close spray weigh safe run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
