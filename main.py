5b765f0e58477baded6b58e0ee42c7d4adce9319
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:02 +0000

    communication correct constantly favourite whether

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
