5a7fb9ad0fb14c917868cd8a2412b1c755f833d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:12 +0000

    fun succeed snow paper train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
