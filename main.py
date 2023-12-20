13ca1e4e5d34e7112fc522d4977de487e0675c41
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:11 +0000

    shame manner pick ask base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
