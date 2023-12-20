304eb0dc602c343cd30e90c34502498c1e59d580
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:47 +0000

    excited land part past busy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
