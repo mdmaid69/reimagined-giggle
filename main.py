57ec01a2a160da6f25dfb06b3fa5f5b6b8694204
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:19:51 +0000

    whistle who unless ride variety

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def divide_numbers(x, y):
+        return x / y
