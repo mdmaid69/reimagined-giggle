d931f43f02cb40a86746a863bbceb045e94a1aab
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:05:35 +0000

    responsibility left damage opinion cruel

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
