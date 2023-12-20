f8c3db4defab0a370762c835ab412b4551edcae0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:39 +0000

    extra interrupt store near Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
