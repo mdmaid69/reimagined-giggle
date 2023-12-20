dd8a0739a48a5193f84b17d081aff51b7fee4c17
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:04 +0000

    he tense stay eager allow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
