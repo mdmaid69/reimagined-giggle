924b52d970c48796fd4c9fb1fabcb9c26e560596
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:57:27 +0000

    offer fond fine around name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
