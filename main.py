a17f35f3371d64cd0fb4eb82a34e8d943fe9789a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:27 +0000

    behind tax equal regard past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
