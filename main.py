28ed90a2ac34b8017eee5c3af7ccb076516655f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:55 +0000

    refer ignore grade outside opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
