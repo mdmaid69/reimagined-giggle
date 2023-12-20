8652783b88fe074cfa63cbfbd879dfa45d94347e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:51:46 +0000

    fun after piece about stone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
