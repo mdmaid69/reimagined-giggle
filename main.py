dc5868ad7b82b9fa80d488671412ab9a9fb54a13
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:29:52 +0000

    part show pay nowhere workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
