e0c6af370e732010f48683f0aa1702ec5b516950
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:16 +0000

    upset serious blue hope spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
