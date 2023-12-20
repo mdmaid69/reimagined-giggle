fde21204cc166f3a10f44b463856d613842af0f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:32:03 +0000

    imagination talkoutof advice guess Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
