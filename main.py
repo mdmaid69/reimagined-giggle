3636b315be69f39112440e0d9699238e3ead50b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:54 +0000

    rich shoot always design price

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
