8659db8e6bf506a64b3105060469c028e83fa4b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:27 +0000

    communicate create anywhere punishment head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
