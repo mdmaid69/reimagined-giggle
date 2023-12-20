95f0187108434bcca739644f0aa2c88077d5e284
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:05 +0000

    since bright thought weather anxious

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
