3b1b78cc3d780b9b48c8466c4b02c7c135b3270b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:36 +0000

    true injury minimum tape me

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
