9a3d99954866b8bdb4109f81e2c444c07acb06c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:18 +0000

    edge club shelter often tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_list(array):
+        return list(array)
