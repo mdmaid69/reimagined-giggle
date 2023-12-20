fbc88204a435ee263d6ab2e8d464ad0c86878add
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:30:13 +0000

    way strong without heavy scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+print("Hello, world!")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
