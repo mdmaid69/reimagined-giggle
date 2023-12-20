ca955db442514359c4bd5c261446389bd949b3a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:03:57 +0000

    foot test thirsty generous lie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
