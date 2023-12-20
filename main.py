5a8c5930ea95deebbbd25c523669208261123a17
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:35 +0000

    century cross suppose book secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
