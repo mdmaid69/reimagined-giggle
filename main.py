a9d38aa374e4992b5bf6236404eae8924d7dda2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:04 +0000

    second regret female rubbish birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
