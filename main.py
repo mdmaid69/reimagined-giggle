bf85e1028800318d2c1b13b55e4ce97efd25e710
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:05 +0000

    beneath entry my cap forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
