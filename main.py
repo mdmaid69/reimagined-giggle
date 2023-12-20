da488501307c1006f0fcf5ee770a7766569b9021
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:49 +0000

    scheme press motion muscle play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
