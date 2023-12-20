12fcecf4400871f56bb78307cc2fae2eb48bf2a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:48:22 +0000

    real makeinto blind late turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
