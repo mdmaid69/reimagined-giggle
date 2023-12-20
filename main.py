7b791341df84608a4ebb6b8e799964c0865e3d57
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:33:07 +0000

    variety spare bury remark dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
