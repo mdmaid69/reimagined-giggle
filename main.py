059dfc2e705c758502d7b88e1496aa6d317495f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:20 +0000

    past question less indeed however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
