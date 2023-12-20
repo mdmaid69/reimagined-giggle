e177474dc59cb5a6baf32dad8b60a0967320386a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:32 +0000

    government fail fire length like

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
