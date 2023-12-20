7f8f732c334f3250c6ddfe8f0c52ba062207aabd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:27:10 +0000

    her deliberately trust effort list

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
