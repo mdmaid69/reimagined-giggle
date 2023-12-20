049ffd0f618864c348fb752bd06e9346893fc4ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:11 +0000

    tool famous many separate split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
