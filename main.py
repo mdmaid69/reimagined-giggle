4c0d09118eb30437173b7881364ae408e65e9b8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:34 +0000

    recommend she jump achieve else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def count_elements(lst):
+        return len(lst)
