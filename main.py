33e53f521f1bdb8167b8db129b47d0df325441bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:12 +0000

    goout dance bin right fall

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
