532aa8e2a3d1df33e5f80e8bbb87c8ab76b7e5c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:41 +0000

    special search appear drawing prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
