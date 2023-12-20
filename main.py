f46eb49316a5a879988c967ef4e23cec73b4ec00
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:50 +0000

    grow classroom busy compare Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-print(math.pi)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
