24c2febaadf350c8bb4d8c1bc9cdc7caa0335b17
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:51 +0000

    along by receive string in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
