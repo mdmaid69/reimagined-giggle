d33420904bbe395a92c721fc5e05ba673d09b156
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:12 +0000

    backwards afterwards include scrape wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
