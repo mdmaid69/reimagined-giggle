8f0301905ba6f248ecb4df23a9f744fb60e5eace
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:48 +0000

    record careless alone offer progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
