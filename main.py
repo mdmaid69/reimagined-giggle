c8b93658b651acc92d292ecbfdb41d06184a5475
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:08 +0000

    steep workon dress dick view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
