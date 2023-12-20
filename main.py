661d0aac060a61b04a462d4d603b2be44bc54451
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:32:01 +0000

    spread address fetch pour depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
