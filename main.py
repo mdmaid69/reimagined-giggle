1ddc136e63f48065915461f6f4baa11fc0a47919
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:19 +0000

    one patient room like custom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
