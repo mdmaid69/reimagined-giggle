616cc6abc8f04adf0a0f3e1508fb807da383d38f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:47 +0000

    loose getaway breakinto by careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
