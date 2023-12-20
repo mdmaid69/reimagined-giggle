93e0eca6e0c6e4a256b2ad52390b2effa8f7257d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:22 +0000

    company honest lookafter although share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
