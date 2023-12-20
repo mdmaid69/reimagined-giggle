d439290b670bc78e51e0b82ab8b9e1a1f1680534
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:38 +0000

    government authority call near crime

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import random
+print(random.randint(0, 100))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
