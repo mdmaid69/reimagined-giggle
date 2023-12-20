3888859fff71de5b46cf2ca9d3e37cf07c2f4295
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:23 +0000

    thought money phone battery breakup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
