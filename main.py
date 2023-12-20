dc189ad1d2c4bb6d759cd129caa47da23c4debe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:48:52 +0000

    follow heavy seem their wonder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_max(numbers):
-        return max(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import time
+def get_current_time():
+        return time.time()
