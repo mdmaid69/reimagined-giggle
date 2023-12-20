3598f89282c0770d1deb9bac99552174ec8db71a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:54:34 +0000

    about test you key occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+print(random.randint(0, 100))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
