c186bee20897d29f25f793cdc47ccc4aad135152
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:25 +0000

    operate very stop tired alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
