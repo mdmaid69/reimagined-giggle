c13b72c47351cbf499e5e10f092bac20f1c619a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:38 +0000

    live live will boring physical

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
