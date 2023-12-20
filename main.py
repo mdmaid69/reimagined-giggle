3cc18dc64f811d4418799852dcc14ac738ab5a03
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:55 +0000

    trap contents shrink sister field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
