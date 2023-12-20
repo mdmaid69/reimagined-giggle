09b73cddcb9da665df22ecef24c1fabc07229816
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:44:54 +0000

    situation thumb fun write brother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
