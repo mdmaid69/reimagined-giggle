a59c322390a4860546f723b640dd443d9f7ca931
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:22 +0000

    confident wave sure throw bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
