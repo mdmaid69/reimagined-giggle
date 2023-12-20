e4011790dc6d341bd1896e9b6e37277128d77fad
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:28 +0000

    slide ill low invitation giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
