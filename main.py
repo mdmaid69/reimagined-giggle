6941e8bc4e10025ba56c97fa7fb805a2974f1ea6
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:21 +0000

    work height none recent breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
