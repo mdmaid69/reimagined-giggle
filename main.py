97d6756cdef64f4711fb5f3493a507ce83aa0f83
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:19 +0000

    Gg Nn underneath effort either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
