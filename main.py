3605d4ad4a439f228d7ac4a76d3cc90b8a2fb7e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:42 +0000

    stamp science appear design whatever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
