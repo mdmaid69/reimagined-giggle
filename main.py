7e7262c42a8db18e4214adca738bcfff72a78bf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:02 +0000

    find possible lose race trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
