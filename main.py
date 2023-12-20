e66ab5598808cfcf1938d7a43192d3e7823a5321
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:11:06 +0000

    workon begin hollow later 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import subprocess
 def run_command(cmd):
         return subprocess.check_output(cmd, shell=True)
