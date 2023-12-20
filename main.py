9a8d5588fa74dbae67a9e97fc2350d4b4375b449
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:28 +0000

    it neither schedule watch strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
