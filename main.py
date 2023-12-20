dd69045edb01af4425fe23dbc8e9d65c58bbe0b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:02 +0000

    side suit brush flat style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
