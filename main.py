ea830f07b4cd52007b1f0101a9e1d3858f0b48f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:49:27 +0000

    cheat eat pretty now mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
