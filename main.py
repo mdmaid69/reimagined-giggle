36cfb0ccc81dec24fa9ce1c2bb3eace8b3f829ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:20:04 +0000

    mainly closed influence claim lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
