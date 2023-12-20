d2fa486ea896f2cca23fd3475406304697fcf002
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:27 +0000

    contribute burn extra behave child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
