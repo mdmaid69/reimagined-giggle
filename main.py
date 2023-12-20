564c2f74d8ac5d7d1a4c459dd0b4c966aada681b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:20 +0000

    grade rule tool never nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
