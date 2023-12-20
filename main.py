5700f48dd3802471e9d5c2c779e518f34f0e22d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:03:46 +0000

    Itself come downstairs protection remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
