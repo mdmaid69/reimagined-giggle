3821d4f49357fe167fccf57b912c53165b50f429
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:29 +0000

    pencil her desperate drive tight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
