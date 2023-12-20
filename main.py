93150426f81aee871a27ebdb8e03a3ec033b8b00
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:52 +0000

    plan some holdup instruction pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
