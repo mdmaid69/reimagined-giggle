65c1cc3bd511074c2b7aee332e2ffd625779d82f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:16 +0000

    confuse opportunity fresh part large

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
