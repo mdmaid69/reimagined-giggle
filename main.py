ad2f1a35cfd41e5681ab29aefe5b264875226ef4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:27:13 +0000

    Ll edge bottom sure bury

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
