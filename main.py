72b55eeec47e7cb35edcf868045f18d1c26dc7ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:23:09 +0000

    comeup vision partner use bring

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
