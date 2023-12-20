e1ea7fc86a8c753ef4501ce5bb774c8fed87905c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:05 +0000

    hold strict trap need copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
