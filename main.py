6fe5605f8f2ab2664f54252106aac63846eb598a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:26 +0000

    pull dress achieve instruction comfort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_binary(n):
-        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def cube_number(x):
+        return x**3
