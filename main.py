9e21c46b46ff54db5ab21c6114a560c7bfb36279
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:27 +0000

    nervous ahead advice consult certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
