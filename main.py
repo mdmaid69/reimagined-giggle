6c2c046c09165999e24d7fa096993eeae0dae99b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:40 +0000

    about runout coat Jj smart

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(x, y):
+        return x + y
