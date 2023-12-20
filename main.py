a7eecd9470819ae9c6ed50022df5750dc9348699
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:54 +0000

    identify aware deliver equal possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
