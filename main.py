75cc300587f8886cd238546f15021ddc69bf7134
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:50 +0000

    sell correction great clear kick

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_even(n):
+        return n % 2 == 0
