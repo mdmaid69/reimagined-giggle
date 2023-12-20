84085686ace1fdac68e808f10238af6971e6888f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:50:11 +0000

    day oil trousers fire over

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
