7d40d56fd56d368de7b206e5602a2ae86e639bbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:17 +0000

    method blame strict combine sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
