4ae56ceb7815b481687c5cb9293b16e39c877a25
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:36 +0000

    row lesson formal terrible none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
