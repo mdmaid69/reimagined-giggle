2054574300a55e0a1e9cce7f5ae9be3c73497b98
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:22 +0000

    leave ring fasten opposite negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
