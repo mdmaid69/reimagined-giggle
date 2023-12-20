e5498795a0139d617c003ed675572d500c4cbb70
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:58 +0000

    pencil powerful comment yours praise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
