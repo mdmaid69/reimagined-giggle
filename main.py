c858c429210f0652cf5117d847cb6c1ee6681096
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:39:32 +0000

    suit protest lock check fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
