2a1042d0904edbe3a21e8e887c1501055358c7b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:36:37 +0000

    callat age OKorokay obtain introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
