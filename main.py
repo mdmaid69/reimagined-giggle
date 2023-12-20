035e42ee5b29c2c02e24a5d82b337acf8611a618
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:01 +0000

    that threaten fun public film

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+print([x**2 for x in range(10)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
