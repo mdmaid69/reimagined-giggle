adf1968dfbf3eed5ea02328f6fcd3b7cd3dcf4e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:52 +0000

    trap birthday squash discover frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
