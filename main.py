c15b69e225505b4c664490380dbca7108bab6d64
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:31 +0000

    write related towards sound wise

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
