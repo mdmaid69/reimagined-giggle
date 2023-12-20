1eca54f55d4db693175e5c59080bc155aadb73e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:13 +0000

    pretty damp later float equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_area_rectangle(l, w):
+        return l * w
