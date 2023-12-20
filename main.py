1dcf8832d4664d72fa907e13ad985327f13b8334
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:55:04 +0000

    passenger happen popular but Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
