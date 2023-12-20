8d3be3bef6f49868f7e7e474bf152f6b0b13e4ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:50 +0000

    slice soak surprise street invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
