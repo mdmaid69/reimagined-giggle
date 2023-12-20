30f6a08873f190e3f259d8e703ae91b4af5d93f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:21 +0000

    trial mouth accidental present wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
