09a2d3619c48372a1c2a1cee602bc91a0c7be9db
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:53 +0000

    summer cause fairly save down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
