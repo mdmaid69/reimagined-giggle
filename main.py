d221bd30c7b99c012acbc31fafa13a9b0d4ae9a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:39 +0000

    strength praise comfortable glass wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
