21977e7ca53630df1b57ef77a70915458db44976
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:20 +0000

    crowded quiet avoid should back

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-print(math.pi)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def multiply_numbers(x, y):
+        return x * y
