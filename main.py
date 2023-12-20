ab79b0329191ce85498949aa84b0e5c93adf85c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:49 +0000

    round convenient detail please soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
