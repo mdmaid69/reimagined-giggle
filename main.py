058d2ccf15e4643bf92fa863032003ab411e5c45
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:22 +0000

    uncertain concerning swim petrol communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
