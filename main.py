254ea318fda1756b12de04df079b7db7f66a6be1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:59:44 +0000

    measure bad born wave create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tensorflow as tf
-print(tf.__version__)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def reverse_list(lst):
+        return lst[::-1]
