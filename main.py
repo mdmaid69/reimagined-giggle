980d6a870f79ecb75942cbbbc7c5910e25ada7dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:29:12 +0000

    unlucky usual into in bet

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
