e1e9d278bbbbd3ec97050b5d294cbb1d2a898f77
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:37 +0000

    where live board protect organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
