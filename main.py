aa9e34b161d5b26518084691140c6109709b344c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:10 +0000

    software steal calloff ever refer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
