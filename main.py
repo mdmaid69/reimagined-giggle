029a0e8dd9d8da94c879bbb7ce2ef20eaf59ee8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:52:50 +0000

    favour interrupt talkback petrol score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
