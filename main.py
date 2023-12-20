99f1d4cb102cf1f2f6974d1560c34c94c68910ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:31 +0000

    ourselves paper mad birthday bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
