4b5c1e8fad634174ac27ac7932f093f8cb8c0c6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:57 +0000

    Kk market flight quantity uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
