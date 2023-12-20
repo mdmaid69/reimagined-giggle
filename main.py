d2e33f38c3948321cac8ab14e142103f3320206a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:48 +0000

    card long small among eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import random
+print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
