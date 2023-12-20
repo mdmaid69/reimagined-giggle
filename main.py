bafcca9705cbdf293da6745e9c91143c3e0d2ad9
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:57 +0000

    guess sleep frightened reach quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_floor(x):
+        return math.floor(x)
