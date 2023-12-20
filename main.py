293131dca5007b126aa342c77908887ae751db82
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:02 +0000

    brief occasional well sorry Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def append_to_array(array, item):
-        array.append(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
