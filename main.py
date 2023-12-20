61c8858972a3dbb791f6c033599efb9575a90b8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:06 +0000

    argue which sing design climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
