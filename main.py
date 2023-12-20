3cc5ae41d623e76d199eb0712c9eba7441ede416
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:09:22 +0000

    rule foot correct worth reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
