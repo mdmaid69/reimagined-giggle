ed9e5e1241a02a87d6f67988b83ec46bfb34bd5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:59 +0000

    impression crash concerning woman high

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
