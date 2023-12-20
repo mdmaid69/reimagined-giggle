12cd2adb75ff291f1001fedb23ce3c6a8c531312
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:59 +0000

    secret roof generous lonely considering

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_characters(sentence):
+        return len(sentence)
