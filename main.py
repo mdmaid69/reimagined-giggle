91af0476ab53db4798aaf0fa46dba10447b7835f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:31:39 +0000

    letter sticky breakfast now yet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
