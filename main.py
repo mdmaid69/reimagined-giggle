ae0c4b36e353971b8e770eae34cbbfbbc35a1ff8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:50 +0000

    cry size undo after tone

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
