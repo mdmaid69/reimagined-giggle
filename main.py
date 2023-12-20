ddd17efcd1b6e9e7c2e3a4227597f6a983c0dae2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:55 +0000

    recent round interesting surprise few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
