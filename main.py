0ba0fa19245b51a2abe80fad0b2f36cae8980f22
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:31 +0000

    please check gas sure comeround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_array_item(array, i):
+        return array[i]
