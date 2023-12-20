c384cd40d51c1d04a1beef9db7f57652deb1ff27
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:51 +0000

    thanks form game until collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
