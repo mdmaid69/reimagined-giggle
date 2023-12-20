ec5fe02b4807066169033d6f8e66ce26261f5770
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:48:43 +0000

    split authority hold effort violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
