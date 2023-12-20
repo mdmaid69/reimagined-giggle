13065b4c0f7798a2ac682e02dee12276519b9d55
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:18 +0000

    benefit left Cc tax train

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def is_even(n):
-        return n % 2 == 0
+def square_number(x):
+        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
