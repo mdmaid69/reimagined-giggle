cfc3174f1bb74c17a586ca1cdb915480ade1da89
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:16 +0000

    worried difficulty press feature between

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
