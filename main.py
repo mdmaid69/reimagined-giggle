30ae60e89b72d25c2117d1c0b1deaea70bad006b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:24:03 +0000

    expect supply crowd tip point

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
