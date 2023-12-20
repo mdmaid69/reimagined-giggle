5a179bd08073d70c0242013815941aadcd7bf330
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:30 +0000

    finally radio though crazy none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
