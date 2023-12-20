371039de938657c289bfe14431e3fab186317f53
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:21 +0000

    improve them breath most circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
