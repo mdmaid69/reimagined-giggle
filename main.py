53e6bdeba9ab0d05487851051e14fe6071950a6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:24 +0000

    everyone improvement minute decision for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
