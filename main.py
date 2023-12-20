a6d3f562f617586be499836f15afa3dd618c98dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:53 +0000

    ear wait normal lookafter expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
