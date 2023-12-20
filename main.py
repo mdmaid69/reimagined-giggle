cc481d24b22b468255e8bb7dd2bd8c8cd15e5316
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:15 +0000

    sore cut congratulate spot round

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
