2345758f8bd21bbb91f2ae15c650b6821dafce78
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:45 +0000

    collect flat talk must whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
