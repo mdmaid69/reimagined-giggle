3aa71c1cac28642b5843b5faacfc4fa5ed08daba
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:11:05 +0000

    paper fall fun anything actually

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
