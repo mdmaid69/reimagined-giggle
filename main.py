ec71e26684f76db2d3777533c66412208efbd477
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:42 +0000

    open ear development slice form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
