83ee297c5a6d255675f8cf318e7ad15dbd544d9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:42 +0000

    hardly trip along swing godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
