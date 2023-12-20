646ff1c9341ef173a38ffbebfdea86ba1536e9da
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:13 +0000

    sick computer dull bringdown father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def add_numbers(x, y):
-        return x + y
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
