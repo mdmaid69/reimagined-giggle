bcae741b169220c672584897433b2823165a72e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:50 +0000

    tough properly band clue deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
