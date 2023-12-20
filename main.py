4aa5d38d1e69b643f697b5efb6f70fb3e0438ece
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:48 +0000

    she invite splash appeal clue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
