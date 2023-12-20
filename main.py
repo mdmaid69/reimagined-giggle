20242fbdc2a9b11db302c0bc2b8e3eb74b12c6f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:06 +0000

    chat exhibition programme natural crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import random
+print(random.randint(0, 100))
