8ee51cdd0f70f7a275f013a126b60826764f316c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:39 +0000

    prize come against search lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
