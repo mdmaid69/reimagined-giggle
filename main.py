4be0c7b7b8b78465e37664cfe561a28e1933b540
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:27 +0000

    anxious not whatever distance flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
