6e39333c24db927f73830915ce32fdb9229775e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:18:57 +0000

    safe matter buy post breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
