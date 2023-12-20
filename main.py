2c2dcd38462ea5fbef5d6bc066afe65e32459aa9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:37:32 +0000

    ticket spring keepup violence direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
