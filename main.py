25f5c18d90f7cfd2f5cd47bcbd4633db417b3198
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:53 +0000

    bunch level pass class material

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import random
+print(random.randint(0, 100))
