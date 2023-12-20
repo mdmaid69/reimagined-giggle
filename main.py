555cf3b535e9cac6bdddc7ff048a8bfaec3e1f82
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:58 +0000

    understand mark begin write listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
