174bb373666ba80c59c4b0782b5d37dad861404f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:30 +0000

    medium band agree contact me

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
