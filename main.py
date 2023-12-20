08e6ec3ec3524f2d099ef907af3c7e0b25d87f8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:58 +0000

    suffer fire wake swimming embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
