117568e6bbac37cfd5a465b5bdec88079f881a11
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:15 +0000

    use so 570enjoyable fight rush

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
