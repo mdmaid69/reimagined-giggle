52d8dbfb71579a9c144d31a0e3beaf6aeb6cfe7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:21 +0000

    basically crisis name bury mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+import re
+print(re.match("h.*o", "hello world"))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
