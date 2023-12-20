e820f5294a4e9088dda16e06f249779a368d60db
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:02 +0000

    by charge amount order frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
