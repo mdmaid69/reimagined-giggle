7b1eb9bdaeec5177420f19e5df063bc778d5e7a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:22 +0000

    gift error skirt narrow cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
