4d2c0b03dc2e826a4cdf4dd63d8042efbadea55b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:33 +0000

    deserve advanced become health next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
