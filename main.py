a8952ec1c537683ed47cdacee73aae35f1ce7862
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:56 +0000

    harm row 565end record somehow

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-for i in range(10): print(i)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
