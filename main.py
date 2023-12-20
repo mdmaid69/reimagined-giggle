22ef4e7ac3c4a681ce42cb4c6a064207fdcbba25
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:15 +0000

    question post programme separate description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def count_elements(lst):
+        return len(lst)
