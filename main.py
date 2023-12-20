af2ac9ce19e7524461897fb4400bba7b376d4984
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:27 +0000

    substance movement Cc point spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
