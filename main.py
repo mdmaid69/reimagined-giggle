df2ff9400f2ef1e8cf1868b3931747bd6138e485
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:41 +0000

    close radio handle test pick

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_min(numbers):
-        return min(numbers)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
