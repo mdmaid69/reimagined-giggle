6e491b1a941b5150c7774f66da56114eb8b7da24
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:59:21 +0000

    weigh sell discussion drop reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
