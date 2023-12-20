6dd1b1af6077e11ecf757ab51ee4c970343736a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:04 +0000

    calmly spoil spot solve will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
