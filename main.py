5410e06a9ed6d99c206c876ee1fc5a9d85c3e952
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:27 +0000

    takeoff disappointed accept 564encourage slightly

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
