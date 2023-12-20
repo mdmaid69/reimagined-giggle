7e2cd519bb955a85945166e6928f1d6032e77bda
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:57 +0000

    separate disagree die entrance dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
