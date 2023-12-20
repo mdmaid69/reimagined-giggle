5813bc4ed27cfca813fbccb7c69dbeeac8b8de49
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:40 +0000

    violence hour rise calm ahead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
