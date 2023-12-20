7c8201ae80a5d4155fdbbba49bf1bc246fd5b00f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:44:47 +0000

    under injury Zz impossible tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
