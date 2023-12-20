3691c4eaf92acf8831d045fbb61d3d214ca18397
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:35 +0000

    from reaction connection suit comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import tensorflow as tf
+print(tf.__version__)
