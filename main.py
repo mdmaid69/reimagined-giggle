d246efdce5fee4e1161950d6bd72a6ad019e9f6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:14 +0000

    deliberate arrangement range adjective in

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
