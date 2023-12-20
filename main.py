e56b4be3d80dcba323820ded595933d39196a429
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:32 +0000

    corner give declare snow where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
