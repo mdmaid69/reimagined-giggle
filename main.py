2a7a33882c260eecb1720ec8a0a39ebb6e5cb3f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:57 +0000

    health natural raw star trouble

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
