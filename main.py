5f5d21a5bc0248910f5316e639a9db9d170a6c80
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:15:17 +0000

    difficulty balance Zz frequent afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
