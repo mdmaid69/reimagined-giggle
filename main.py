420d029634c75aeaa89219c6592e562a8659ea28
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:09:49 +0000

    tongue sudden talk corner odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_array_as_tuple(array):
+        return tuple(array)
