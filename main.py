7d63479bdb59781979614cb676c1ad60820c2148
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:10:51 +0000

    acceptable Ee development exercise listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
