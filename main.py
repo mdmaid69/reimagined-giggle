c431e763c5f1b53280d2729f54b8e653dd7313a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:58:47 +0000

    really careful figure failure talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import platform
+def get_python_version():
+        return platform.python_version()
