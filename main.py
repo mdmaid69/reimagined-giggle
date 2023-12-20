abdc5509bcdf9c5b111d771a01ab227094ac7ad6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:58 +0000

    comfort information should impossible whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
