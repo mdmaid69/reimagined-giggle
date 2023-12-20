f57b27c6e9b4c348634db6e0703eeac8f09941e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:20:53 +0000

    confuse quarter front instruction shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
