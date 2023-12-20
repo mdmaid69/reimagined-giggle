1d34feded4db06aa6df3d0cfc4b2026089bf725c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:30 +0000

    write downstairs not about exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_pressure(force, area):
+        return force / area
+def multiply_numbers(x, y):
+        return x * y
