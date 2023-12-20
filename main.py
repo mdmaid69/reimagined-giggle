2cef676563195c2e9260231b995588b4a373d736
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:13 +0000

    successful pupil sweet better feature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
