01613d51b26c71d95efb3ea3e15da8ae24648377
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:58 +0000

    fat how live may bridge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
