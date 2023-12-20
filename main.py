519e6478a2072fbadbb152c7b67c181c6a0e4c0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:22 +0000

    geton along square while condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
