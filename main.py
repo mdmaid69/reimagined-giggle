0232538b87728132230c675951aefdab9d90faf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:33 +0000

    anybody listen thirsty simple entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_typecode(array):
+        return array.typecode
