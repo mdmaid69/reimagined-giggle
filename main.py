65ee9ea6b66b1e0fc5755edb4056d4abc7a4d9e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:19 +0000

    ignore cinema smell recovery soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
