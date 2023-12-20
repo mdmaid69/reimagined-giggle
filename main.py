aa4442e66d5d6939c9abb0f5fca5bc9392d24a4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:39 +0000

    practical actually violence loss do

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
