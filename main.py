1ef2866c32c6fbdf56d3dfb4bbf5225c41ec8360
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:43 +0000

    eager left drop should pretend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
