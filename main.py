a92196470f0dd9780705a3900e6e62d0039cfc67
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:43 +0000

    noisy obvious praise public please

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def cube_number(x):
+        return x**3
+def calculate_power(work, time):
+        return work / time
