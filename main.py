33cde02ff4c590ab4fd2ab97a2e2b8725007795c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:06 +0000

    last than go chase prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
