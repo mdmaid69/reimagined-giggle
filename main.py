9934250df681e92b1eedf82ee5ca9700055f6681
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:32 +0000

    this partner win mood almost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
