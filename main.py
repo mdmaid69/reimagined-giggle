73833fa8edc3dfe04fff110b8b9a3bdacb272065
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:57 +0000

    practice another contact plastic behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
