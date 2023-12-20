7570e29ccfff4f7ba80d75adb3dc23b37cf0cdad
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:01:22 +0000

    male kill prefer turn closely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print("Hello, world!")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_float(array):
+        return float(array[0])
