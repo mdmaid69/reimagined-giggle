5a374da38559f5da70f1c966578540db5336d5cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:26:46 +0000

    ours entrance another scared word

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
