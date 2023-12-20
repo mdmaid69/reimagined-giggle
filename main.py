57b9c4c8310f38caba74634c46a9e814ff0fdda1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:49:49 +0000

    ourselves flight worth welcome forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
