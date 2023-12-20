af7fa406aeafc8d698259e64f5a2ec96248c322f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:00 +0000

    point phone bend natural introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
