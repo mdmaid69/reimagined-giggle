2be70ec3c8368973d25816d0f37a2e9190d759d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:52 +0000

    lookback(on) must minimum science sense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
+name = "Python"
+print("Hello,", name)
