67a33c6de1137953792f828a4d86501519bf13d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:07 +0000

    speech agree original Vv awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_min(numbers):
+        return min(numbers)
