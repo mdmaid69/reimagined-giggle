0381175e736b22e0d82f003c5029c18e240f6e13
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:21 +0000

    coat appear story future herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def cube_number(x):
-        return x**3
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def count_elements(lst):
+        return len(lst)
