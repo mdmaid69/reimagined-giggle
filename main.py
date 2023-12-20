04ef7c5b6aa5ef797db9e24d3592af12033ffb39
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:02 +0000

    thirsty influence follow always Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+print(math.pi)
