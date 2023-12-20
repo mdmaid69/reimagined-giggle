26c67a2ae7303638681a66a2da902b3199662472
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:19 +0000

    against on sign taste tough

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
