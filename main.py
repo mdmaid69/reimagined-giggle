be5ba9b02294e420d696ce4c73f413db7b073c75
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:41 +0000

    match criticism container secret show

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
