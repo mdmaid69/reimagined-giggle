5ffdf7958d00b5e3e806b69e4234be71dd82b1fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:29:26 +0000

    activity careful ill burn clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
