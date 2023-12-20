8c9fdc9b39ae44d68fc5113b348982ba8bb4a489
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:46 +0000

    about mean hear occasion 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
