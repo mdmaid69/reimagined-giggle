d90eb20daa6aef2febcc9e0c98da64ff19a06394
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:22 +0000

    eye preparation sport watch far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
