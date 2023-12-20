99fdb64d52f0377c5bfb6c804d7c3320b2ba9208
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:31:05 +0000

    bet oil person plant secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
