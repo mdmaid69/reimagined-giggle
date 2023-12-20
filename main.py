504059dbcaf45d33d350219f7db60bac56444ac1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:39 +0000

    recover mention favourite responsibility load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
