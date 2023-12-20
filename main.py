6b31a9388345e47e0e2a57f6d7536209e686be0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:03:25 +0000

    science tidy themselves quantity smile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
