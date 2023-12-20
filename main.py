fd0d6abb8cc3a3146b50f0637ae16d2ea803c47d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:17 +0000

    conversation plastic along block silent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
