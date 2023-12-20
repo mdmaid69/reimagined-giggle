9aa9d37dcac9b32830c91fbabe42fbb50dac8898
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:10 +0000

    game callup plain return translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
