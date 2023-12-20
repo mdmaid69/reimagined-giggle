ecbf3488d63521f5d06e388b2cb87cc922458d54
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:13 +0000

    on them backwards winter stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
