b521d801fabfc6838c446b69e8549a8ac59c1c16
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:12 +0000

    carry tradition burn carry statement

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
