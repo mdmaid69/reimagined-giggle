4b71d29bc39d18f706e805a73a2288ad5023cfe8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:33 +0000

    shoot concerned takeoff appeal pour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
