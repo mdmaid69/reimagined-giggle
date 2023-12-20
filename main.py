fba7b1a316fcfbf14c2e45858cd3239e6ad6e64e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:11 +0000

    shrink personal progress undo whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
