32578e8782ce32093dae307c52b8b907e5af1e8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:12:33 +0000

    unlikely cause combine show free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
