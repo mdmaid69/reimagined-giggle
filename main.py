5abd7b9de6a0f3a2043ec2ebdd00e946a3c2785f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:18 +0000

    emphasize discuss party achievement destroy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
