cb2fa8f18f599743ba4f1cf1fb28a9cbda27ce8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:41 +0000

    Uu far post happen still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
