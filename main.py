64bc0d23fe815c94f408056d9065716f07b640c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:45 +0000

    again stir meal worried stone

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def reverse_list(lst):
-        return lst[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
