ea51b976a6ec483ca7b6976c08f4ac4b13ceef6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:55 +0000

    other square attention thin callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def convert_to_octal(n):
+        return oct(n)
