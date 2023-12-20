41e373e054d91a35657f181266c259200ae136fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:08 +0000

    tone wipe tall deliver beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
