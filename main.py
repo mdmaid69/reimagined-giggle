04d595d21f5e15232e7e04fcdf52b0391d5bbb68
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:21 +0000

    getup cheque Ii glass centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
