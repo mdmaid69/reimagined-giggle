72a61a2183ff58b18ca646a8d20ba5e2a7f8b6b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:11 +0000

    she difference sound callat some

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
