6a0b238bfad7892d8cb4eceeedcbdf547f02861f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:02 +0000

    size giveback fetch watch estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
