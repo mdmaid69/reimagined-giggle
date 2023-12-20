a819534ad2cfda55d9b4c362feca9ad96cea3ecc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:18 +0000

    follow medium thumb appearance ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import time
+print(time.time())
