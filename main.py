6ce3128b3aa2c26a1e4203c60e2f7bd3f1473d7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:27 +0000

    anybody exchange quick opposite pure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
