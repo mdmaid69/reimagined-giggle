cef2580ef6cacc86276127fef22c89f487817abf
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:59 +0000

    promise hurry empty flat answer

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_power(work, time):
+        return work / time
