28902f94dc9bfaf5569abc0999311f74c0a1ae6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:23:10 +0000

    day scale reply happen plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
