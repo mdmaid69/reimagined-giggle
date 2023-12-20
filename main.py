5d2a7f9c0bcdc9daa8f63a8b87262e6ac86421ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:24 +0000

    slice impossible contact tool clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
