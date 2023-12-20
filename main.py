a915f7450f3539fb4e57a371cd2e458f2c8949b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:28 +0000

    immediate experiment case disturb water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
