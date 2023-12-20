ad62d8d16269c27f8c7e84d43a119dcb52f7e679
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:40 +0000

    pocket have not argument combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
