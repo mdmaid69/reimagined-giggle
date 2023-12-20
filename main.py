ea2466a4246fe3a434704a055869148c6cad4c92
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:07:00 +0000

    warm thorough line sort reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def divide_numbers(x, y):
+        return x / y
