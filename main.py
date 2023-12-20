79ea5de2f5b6b9f09f61d5b83727c28cf8ef9146
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:23 +0000

    influence achievement make stir passenger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
