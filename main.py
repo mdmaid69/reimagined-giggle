ca4052929aa60a401c7531af26d53cb8517d3e21
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:23 +0000

    mother remove fine clean advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
