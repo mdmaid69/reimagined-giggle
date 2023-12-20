c37bd8add9e2d4791596437a3aab0a106f693e5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:31 +0000

    bye about tonight turnon face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def count_characters(sentence):
+        return len(sentence)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
