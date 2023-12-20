cc27e040ee7e6539988b62f15b69a85826a4bde4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:43 +0000

    at rise geton into boring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
