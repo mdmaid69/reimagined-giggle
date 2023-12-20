ff41d7d138613df2765ab56c3b0427888aae6782
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:55 +0000

    action science smile brave pressure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_list(array):
+        return list(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
