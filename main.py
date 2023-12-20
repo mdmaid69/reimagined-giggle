1d30940fd47c38328aff0ce216152d2c5f396b7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:32 +0000

    praise minute aim anywayanyhow law

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def convert_to_hex(n):
+        return hex(n)
