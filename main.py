36567cf090e6525284568848d117fa3852d81fa2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:47 +0000

    statement outside journey class loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
