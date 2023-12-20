68c986d225c5bf33f8ab79cbc58fa99636a28cc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:02 +0000

    explanation circle miss favour experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def create_user_string():
+        return collections.UserString()
+import collections
+def create_user_list():
+        return collections.UserList()
