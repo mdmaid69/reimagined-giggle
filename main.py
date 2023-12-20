c890fbdb2377eb6d27b1741c751225d03f65f34a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:14:33 +0000

    nigga thirsty insist type part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def count_characters(sentence):
+        return len(sentence)
