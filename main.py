9263ac1ec547ca4b5d187d172e7135dbd7b499f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:01:29 +0000

    sport trick collection spill call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
