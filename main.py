d391ffca4e13f0bcebcb7ebc136f2e59ddc0a92f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:27 +0000

    support manage between square afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_list_from_array(array):
+        return array.tolist()
