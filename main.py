f51a776f6046de2c8338507cea6971bf04515cd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:52 +0000

    offence climb ashamed gap us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
