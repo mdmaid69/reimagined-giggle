abfa6568a4f432148941922e1b95a057a1719d74
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:42 +0000

    rank accident male my seem

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
