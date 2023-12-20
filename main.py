5bc339f2c2445c8a441d2d258298cd34a0134be3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:40 +0000

    free dress prize connection want

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
