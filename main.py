813bd546c701747e1e6c1d86dd7d52cfaba51ae3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:43 +0000

    free only equal second factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
