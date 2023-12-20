0d6ce23dac36dc6a07814d1a150cad30c8204871
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:02:18 +0000

    various discussion point family out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
