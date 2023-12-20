668c86f8b336d4b398694a610491f270347c46b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:25:21 +0000

    union smell delicate while use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def reverse_string(s):
+        return s[::-1]
