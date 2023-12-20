1640c616d59ab114311b1dce14ba01fc0cc71051
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:37:05 +0000

    cinema act ring permission permission

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def find_min(lst):
-        return min(lst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
