dab2f54f5b8303c260f579fb2dd8b8f9f9cd1efd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:51 +0000

    remove murder near obtain explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
