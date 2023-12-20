adaf9dc23253e4165c929d9deb3232fac9fc193d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:43 +0000

    however bound star long bored

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
