9d4b2fb56e7696faa2a4270860e145d46db2bc5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:54 +0000

    switch picture doctor fond ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
