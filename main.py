b3eb1d82025e36bb661aca191255bd78d1b71ec0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:38 +0000

    taste cost experiment copy either

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
