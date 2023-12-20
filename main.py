71bb37b2720e1d072e383ab8c9a7724fc1c349d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:56 +0000

    spot partner responsible cool fit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
