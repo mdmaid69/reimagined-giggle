d31837734f34ba9a4f671b321800407be816965b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:56:30 +0000

    accommodation you almost provide claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
