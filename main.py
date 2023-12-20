033597411cdd24cd6b22dc499d16512d4b95fc16
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:56:57 +0000

    early check form instant store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
