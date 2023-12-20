db25ab385fcda5e067a07a21b2944b274550df6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:22 +0000

    corner gift fall improve repeat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
