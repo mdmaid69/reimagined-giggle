9abd977ce7eafcf01ec60d4d2c4efaa3e107a6bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:40 +0000

    class curious woman danger wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def subtract_numbers(x, y):
+        return x - y
