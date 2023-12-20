6d18a9992b3774a82254e5cf3c3d3e892dad2483
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:53 +0000

    Qq stupid takeout of rise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
