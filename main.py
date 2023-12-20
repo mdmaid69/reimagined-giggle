ac5fe5977055dc0e0d19f7269e9acbbf3e8ec579
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:34 +0000

    tread damage attempt strength joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
