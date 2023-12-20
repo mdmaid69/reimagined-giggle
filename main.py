0b27c052c2c06e6b1e385e9a6f7b9cab2c794e21
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:04 +0000

    as fall remember nature defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
