932663a70640dc43af685bdcf1103ad252af7382
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:09 +0000

    enthusiastic original giveup turnout soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
