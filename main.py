0d301872b0d4b5c1395721e91cba1906abc2b3cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:05 +0000

    display squeeze man backwards bringup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
