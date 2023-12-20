5c6f7e3557fa2ca482f3be6b31aa174d528d1600
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:46 +0000

    part replace argue house right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+x = 10
+y = 20
+print("Sum:", x + y)
