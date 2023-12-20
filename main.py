0a183cee752579c8fb28faf6ba7d1791e2ad9056
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:24:08 +0000

    sing lucky limit language thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def cube_number(x):
+        return x**3
