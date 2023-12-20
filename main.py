c31bf286eafa7b37873293816d8e52d0c3b2be36
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:34:07 +0000

    repair early final contain pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
