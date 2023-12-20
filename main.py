ea660794acc5ec9ebb5e134558d8b26d8555d687
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:55 +0000

    where on aware punish hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
