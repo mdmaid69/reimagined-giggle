0231b2a069104a494ad08823da8b199d675d3379
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:10 +0000

    happen child thank cinema file

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
