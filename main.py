55666822f2dbb585b199b6d76d36a6791eeb2438
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:12 +0000

    pause runout region funny suddenly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
