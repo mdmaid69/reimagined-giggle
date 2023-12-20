d616139c14b6c0badfa056ffdd3a889098bc648d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:52 +0000

    their introduction least across suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
