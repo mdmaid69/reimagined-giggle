47f2de564acaf4e6e99d3b39ef445d6e1c123607
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:17 +0000

    anybody compare Uu squash read

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
