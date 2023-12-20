5b58186c71f15d1f1e4798ba1478974aa2b9227d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:30:10 +0000

    hospital thinkover walk after new

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
