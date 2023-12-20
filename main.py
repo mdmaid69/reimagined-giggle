a6e73356db4afb6b08405407293750b0f6daac5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:17 +0000

    fight favour no off now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_palindrome(s):
+        return s == s[::-1]
