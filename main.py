7eeb700f719dca58ec86bedd7cf0f5f6382606a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:02 +0000

    small history supply provide uniform

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
