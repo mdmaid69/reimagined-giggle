f93e5b7cbb0f0d780a7b453c7f92f7b42b4c95c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:36 +0000

    work release wave operation pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
