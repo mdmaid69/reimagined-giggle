cb28001ca730da2f303301c3a5280b1976f79981
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:48 +0000

    wish card politics spite train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
