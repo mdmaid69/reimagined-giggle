f7c009e0e7d0ad4990b2a1a8f734eaeccbde4b94
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:24 +0000

    suit wheel hand careful shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
