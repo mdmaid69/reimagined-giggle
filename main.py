8cb98cc7246058de0ddc811efb4579b18b6ba605
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:33 +0000

    acceptable comeback earth stir near

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+for i in range(10): print(i)
