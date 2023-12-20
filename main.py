d961e8e3b8d8852733b380ade135c5ed6e405c99
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:19 +0000

    love warm Nn uponon wherever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import tensorflow as tf
-print(tf.__version__)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
