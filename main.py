7db4555018709f89e00df6415faf36dc1033b1e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:13 +0000

    up wish sky ourselves then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
