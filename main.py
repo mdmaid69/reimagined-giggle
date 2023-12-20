365164e1edb8b2a68a9be96b70f947a56cb699f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:53 +0000

    opinion trousers earn behind it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
