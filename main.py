7d93b97ecd1be0fad34f51d25358ff325059136b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:08 +0000

    action and trust lead nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
