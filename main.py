00513fe465a0e097a65104ef490c44264112bff7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:37 +0000

    satisfactory love pleasure future this

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
