873328f20082a834995b3e85ed2bde521443e314
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:04 +0000

    deliberate least some interview swim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def count_characters(sentence):
-        return len(sentence)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import tensorflow as tf
+print(tf.__version__)
