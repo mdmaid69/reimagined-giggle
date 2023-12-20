1d2bb34fbb8758dab4e687da18a74f9370c46118
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:40 +0000

    train reduction gooff rather shopping

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_min(numbers):
-        return min(numbers)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
