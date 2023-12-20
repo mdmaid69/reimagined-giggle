224364e382c87b72ca9a045ef4c08452dd99d3cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:51 +0000

    dress organization interesting block dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
