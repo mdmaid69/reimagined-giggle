d1bd2c4563d8635cdb29c6671a217488be8034be
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:51:36 +0000

    find scream unlikely dream turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
