b3c48db1b1741365e148176d6463c4f0ea774699
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:59 +0000

    whether crash cinema support pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
