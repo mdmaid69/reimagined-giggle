44b7a71ca9006454cb2f285e398179df882f2bcd
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:07 +0000

    crash fence move damage question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
