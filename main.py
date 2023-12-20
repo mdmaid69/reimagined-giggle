4477597814011953a7625841c63ecb505efae44c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:13:09 +0000

    classroom punish owe straight holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import sys
+def exit_program():
+        sys.exit()
