0f261b49700b5b7edb1e08817679d22f2f99a5e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:07 +0000

    I throw now fun trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
