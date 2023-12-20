5045722455afbb7f814bca49ec8332b45827f3af
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:46 +0000

    tough direction leg repair team

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
