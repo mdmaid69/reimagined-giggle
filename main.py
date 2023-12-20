94d983678eee3bbdf2c6cc27abc9114cdba74169
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:44 +0000

    afternoon race answer real blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
