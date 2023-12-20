39301242dd298370aafffa3a69c703a72dda45b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:17 +0000

    sleep afford rain factory dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
