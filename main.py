cdd6a9713d7cbf0ebd763b6eb005b30a9db606f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:48 +0000

    maintain decide circle performance drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
