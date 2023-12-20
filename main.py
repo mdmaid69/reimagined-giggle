2ad37d27200e102d76b25678e74933d459530582
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:37:56 +0000

    angry evening treat scream backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
