4b218dbd71c2a96770949e1a370a234f1bb4b2de
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:28 +0000

    worth mention attention empty alone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def subtract_numbers(x, y):
-        return x - y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
