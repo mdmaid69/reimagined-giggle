b808241afc632207a4ae9df0cc99269ec3aa4f63
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:34:37 +0000

    excuse turnback frightened understand curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
