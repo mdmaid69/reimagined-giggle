ca10490dcec1c762042a93f37957878a53a58fac
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:21 +0000

    stop underneath difference then since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
