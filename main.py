7175aa57e1c5b40bed95c88aea993463485af24f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:21 +0000

    squeeze Ii nothing gift prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
