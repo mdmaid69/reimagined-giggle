0b3a354a2fb7c9cc5184d974adce25cde42c36b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:02 +0000

    cover rough swing giveaway exact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
