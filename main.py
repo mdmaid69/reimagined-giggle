b858408e5198059c5be790d55b1ac26d3f843bc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:38 +0000

    teach strength anywhere shrink secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
