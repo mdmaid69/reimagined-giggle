3cef418934984f1a86d0e61a2692c14ff2322ff8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:36:26 +0000

    talkoutof comment sell cheek equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
