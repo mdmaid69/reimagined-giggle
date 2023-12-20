62517a3b5a6052ebb3f0b5bb9556f39d5bf7e9ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:25 +0000

    software trade flat letter loud

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
