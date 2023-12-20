cb47d20921c6518603bb2399bee81e130e6d5291
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:38 +0000

    search nothing cough regional university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
