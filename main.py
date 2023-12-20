4c9b93ae520cf17b9d4abb4a368fb4ad61d445bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:55 +0000

    what hit quick collect error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
