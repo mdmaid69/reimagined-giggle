a0c6b89dfc7e7e08c4297e1a3e62a094213271d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:28 +0000

    cause away side black computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
