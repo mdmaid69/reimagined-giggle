38515c9758927a7cd20c47508bdc48846cc907ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:36 +0000

    strength forward as bye traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
