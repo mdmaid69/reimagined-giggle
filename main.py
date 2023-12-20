6ce241d32a397cace20f2ff9110e855468b247a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:12 +0000

    against Qq lift able forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
