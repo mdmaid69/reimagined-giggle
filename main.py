6c6166c633f26a80100dad8c4f612225dd24b861
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:52 +0000

    related government neither workupto he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
