bfe442c51b98d0ac9689d85cd76d10a918da3fb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:32:02 +0000

    frame inform runover separate famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
