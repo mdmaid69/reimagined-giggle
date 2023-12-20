587349069e5483062390b8e2c14d863466027577
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:14:51 +0000

    Tt wrap guess angry strike

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
