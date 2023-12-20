14afc8d2db0cd15fc714389e556304b97afa0be9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:23 +0000

    difficulty stage convincing no front

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def square_number(x):
+        return x**2
