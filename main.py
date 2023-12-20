a83967e4c5272c920ad5f104c39dfd8c53cfad6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:50 +0000

    unit responsibility line reason maximum

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
