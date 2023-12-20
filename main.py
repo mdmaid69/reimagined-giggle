6c8419639b1a8e183a123e8e6873857164dcfcaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:45 +0000

    us after aim definite expensive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
