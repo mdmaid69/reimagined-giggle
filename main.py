e774bdd4f8f028d9ec02a361a6a83175d4843998
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:35 +0000

    clever recommend always admit brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def is_odd(n):
-        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
