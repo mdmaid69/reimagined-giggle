457919e7041a86d7de8849d1beabdb486196b5b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:57 +0000

    meeting corner late personal tooth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
