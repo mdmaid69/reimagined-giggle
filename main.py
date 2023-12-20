ba7bf1d23417d2d099224ec6d4e921a128919b35
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:40 +0000

    fork tough job neat face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
