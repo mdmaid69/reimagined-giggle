3baa0eba819a291cdec53d009fdaaab1c956d0c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:29 +0000

    bang defend great journey neither

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
