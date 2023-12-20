383a9fab57347207bd9c55ddf89883f7ea91029e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:46:39 +0000

    pay load get set shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_list(array):
+        return list(array)
