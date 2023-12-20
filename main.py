2547bbefb1b27abf121b0c79aa2054ce05240ee7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:22 +0000

    occasional extraordinary die separate capital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
