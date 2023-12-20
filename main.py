c83f941a88c8c0c40e26b30b88f904a34cf76472
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:37 +0000

    appearance join stupid Ss somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
