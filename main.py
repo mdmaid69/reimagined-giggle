f1ff5a08662c9f40c5b4b8205347ecc814381c63
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:11 +0000

    flavour feeling row serve accept

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
