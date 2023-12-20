474e7e26cb05cb3d4c2f434e6087cdf463e491f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:22 +0000

    walk couple guess different pump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+for i in range(10): print(i)
