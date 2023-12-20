b9f67fa1c2a748d1df76f457a97de9f0e5a55283
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:52 +0000

    fault cap bound weekend gooff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
