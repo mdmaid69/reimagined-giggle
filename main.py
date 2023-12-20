45f2b7d211ae949df0b8c59d10e5e21d7b5adbe3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:20:32 +0000

    fit information mixture worth criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+print(os.getcwd())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
