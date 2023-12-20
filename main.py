a5b36b8b0ea24f6f7d181ccf19ff7f2a71dca20e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:06 +0000

    represent body show fit check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
