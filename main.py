ea557e9451861f65b733293e86af61473225b30f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:30 +0000

    foreign broad target them scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
