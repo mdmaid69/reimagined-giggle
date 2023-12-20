81cf1a0aad59c6130b6d0fae7c132b8e990d4302
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:28 +0000

    before communication bridge false punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
