57631452f0103efde5deb55ad545cbe2bf318419
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:09:03 +0000

    hospital match spill damage ask

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
