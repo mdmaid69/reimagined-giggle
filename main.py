fcefa2bc78f99a926858c026612f2d7e0e14bedb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:25 +0000

    reaction travel circumstance lead rare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def square_number(x):
+        return x**2
