1714173e6793fe253044ef4d79012b9543c4c7db
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:58:04 +0000

    excuse sky Rr either used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
