36bf550471fd77d917818add8ed678495a836df7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:56 +0000

    use post gooff love Jj

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
