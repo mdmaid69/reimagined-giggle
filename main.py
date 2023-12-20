aba83c35d936423361cc3e1a2e9b07ca9a5f9165
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:12 +0000

    fork petrol image claim welcome

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
