f05dc2e26ae91d6cd5353440ba0a806b463ff167
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:02:58 +0000

    weak dear float describe mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
