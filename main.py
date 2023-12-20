5331babb7a720faccf93a357bc959c88b610939c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:08 +0000

    friendly many direction suck sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
