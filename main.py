67f8907140c0c44a315008d423719a6ead52e3ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:27:27 +0000

    approve contribute weak peace introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pressure(force, area):
+        return force / area
