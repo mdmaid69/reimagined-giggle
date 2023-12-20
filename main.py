aa65970d8d1f3f577e92271d6fede163bf663c28
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:02 +0000

    morning whisper generally team science

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
