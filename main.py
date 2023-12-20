c5ab263c0e7d40895bc99769051efd9af9c4cd21
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:05 +0000

    handle bill kill cool experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
