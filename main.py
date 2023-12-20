5aff7af2d1303553efaaa1647faa78774058b306
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:06 +0000

    nurse health partner hear underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
