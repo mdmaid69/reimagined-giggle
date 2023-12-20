9ef8a499cdd4538a2951831d96d066864d568e3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:54:01 +0000

    bit sing power talkoutof refuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
