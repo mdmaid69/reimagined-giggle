e55b9d1fd513f20684524cbb083d05a061d0bf77
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:37 +0000

    question sister convinced welcome godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
