e5241f9536a1b3d269d1083e80bcfc93168b85cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:18 +0000

    niggaacademy also tune produce lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
