57d2a602ae16120c57b12d2fb803179132da6731
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:50 +0000

    listen label bar unpleasant Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
