bbadb120f9c2c7d7fa6f9e5d9ec4e4e16dcb1c7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:20 +0000

    rent approve measure I introduce

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
