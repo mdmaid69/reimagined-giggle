75a912026fd73569114fd6522dec87ac8c0a53ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:23 +0000

    preparation company walk land school

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_repr(array):
+        return repr(array)
