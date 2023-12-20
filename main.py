81ce9878ebb749dde3aed931311084fb94d8f47d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:16 +0000

    circle praise famous to butter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
