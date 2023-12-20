2467dabc04a5b67e3f4999214f196dbd4eef704e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:45 +0000

    undo emergency chin agreement shock

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_min(numbers):
+        return min(numbers)
