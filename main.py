4f20080856231b04430a460755be5f3ba8989e4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:21 +0000

    story earth hate form look

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def get_array_as_list(array):
+        return list(array)
