d0fc1c65c9fd2cb436236e1809d1784c03d75c7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:14 +0000

    creature ordinary organization rich deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
