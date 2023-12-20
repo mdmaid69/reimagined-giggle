918064140e66eed9b336d0c0246d7b29aeaeff20
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:19 +0000

    what hurry show destroy judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_string_from_array(array):
+        return array.tobytes()
