bdcd3b9df83ddc25417f48ca7f6481fb538534c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:43 +0000

    sound hot feel important act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
