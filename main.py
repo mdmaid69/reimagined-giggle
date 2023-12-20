127f0fcef48697cc1018b5d7048ea33498077d49
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:34 +0000

    increase at region runinto decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
