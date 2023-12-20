70ba9c26038a154f0c15c55c0e7a7ec4895acaf2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:45 +0000

    me patience comeout memory complain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
