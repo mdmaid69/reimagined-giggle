13e9535c3571fcf3d74a9106295756db97373822
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:22 +0000

    pronunciation direct guilty organize pride

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
