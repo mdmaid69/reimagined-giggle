bafc98b3adfcef1ea76dc8c82801232d05e78c38
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:44 +0000

    exhibition along turnback female traffic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
