1888d5f6b9f160b16561d3b4d44088ce7cc2a02e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:06:50 +0000

    know touch rescue pay measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
