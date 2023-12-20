cd1fa4eda75d55a6a3dc73ff677302a9eebb96cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:40 +0000

    today exhibition sore position dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_user_list():
+        return collections.UserList()
