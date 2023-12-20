c3d5064be752a48129f109097ea264256926deee
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:28:57 +0000

    grade will last occasional stir

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_power(work, time):
-        return work / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
