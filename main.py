3c764516195c002dee96601b41533c934d684a3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:32 +0000

    live instruction part rich holdon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def square_number(x):
+        return x**2
+import re
+print(re.match("h.*o", "hello world"))
