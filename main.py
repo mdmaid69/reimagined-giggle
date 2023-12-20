5b3585558a95c1112a682c3db7df6be4753ce772
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:54 +0000

    content delicate trade mess minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
