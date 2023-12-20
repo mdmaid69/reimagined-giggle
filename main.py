71c838f4c982c14ff92ef5d5a3f39e2c9fda0ff8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:27 +0000

    reduction year climb become smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
