f2355b69a686dd930f2a1c44367112aab4a9630a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:29 +0000

    basis should of tap buy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
