5c64b3766ac98dc77269d3af002fbf870c288d80
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:18 +0000

    sort care nature neat benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
