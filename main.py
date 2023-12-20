cdd91614a9ef25f1fc9fa4b320a9bd183e397c3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:48 +0000

    judge cause bend please early

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def sort_list(lst):
+        return sorted(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
