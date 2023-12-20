a1587b55616eef7c080d18d235634255f094c162
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:26 +0000

    copy professional boil soon cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
