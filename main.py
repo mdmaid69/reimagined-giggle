e4bac57aff977df10e73502f44aca764b8e81b17
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:53 +0000

    offer describe slow band week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
