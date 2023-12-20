c5b4681edee8a2e20b80af3629879722f31e3281
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:21 +0000

    holiday contain ilovedicks maximum Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
