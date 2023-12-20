b53efa6f281d7cae493bd930e6a4af7fba46fd50
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:00 +0000

    thorough hour stranger pass almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
