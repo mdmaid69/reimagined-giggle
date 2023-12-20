268ccd9683712b2b7523b5cb128753a1f045cccb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:41 +0000

    government onto impossible frame getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
