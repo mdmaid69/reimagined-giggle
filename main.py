ad51af370622758f094ca7e012668fd417e388a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:58 +0000

    somebody reply fork at steam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def add_numbers(x, y):
+        return x + y
