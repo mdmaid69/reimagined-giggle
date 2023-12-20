609eeb25d94facae1ec0a790b402726adb7170ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:23 +0000

    combination play law stage whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
