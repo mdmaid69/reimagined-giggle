70569b287da3de2bf73849f2883f7ee03d9dd2ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:06:25 +0000

    point coat formal otherwise behaviour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
