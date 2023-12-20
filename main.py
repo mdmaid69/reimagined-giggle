74362d7111caca254baf71a1e450ca086188b97e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:10:44 +0000

    scale ill press eat ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
