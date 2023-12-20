2e94b4c16a581055ae9f7ad8c5dbb4db2ea114b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:41 +0000

    disagree even loan suit attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def print_python_version():
-        return sys.version
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_list(array):
+        return list(array)
