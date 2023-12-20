785f7f35b1505627c2c5192cdc06f11286c98c2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:32 +0000

    stop harm too gather Uu

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import sys
+  def get_python_version():
+        return sys.version
