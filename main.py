d49075caead18b31574bf2fb4245c43d418001e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:20 +0000

    task finish strict opinion authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
