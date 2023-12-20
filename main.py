47ba5e85513703c10743c59c699b8b3becff9a50
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:34 +0000

    normal unfortunate prevent touch luck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
