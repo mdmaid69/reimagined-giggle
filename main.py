e9625ce45d538b23ff84fecaa34b5ddf886a6680
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:59 +0000

    report advise swallow plant crowded

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
