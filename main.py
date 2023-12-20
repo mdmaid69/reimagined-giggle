fcb6dc24f333c3e9dd0b11d840b8bbd2242cdafb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:07 +0000

    rub wire join write combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
