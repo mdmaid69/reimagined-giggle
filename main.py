4dac2a5e741638e4ca88eabeedb5a127939113d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:12 +0000

    set wrap improve attention nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import time
+print(time.time())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
