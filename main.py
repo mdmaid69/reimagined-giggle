fd5ea2032dd3774f5ee5bd3b846535915eb7a803
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:16 +0000

    schedule rule noise system course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
