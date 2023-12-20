0c6c22b84c3ce752023d1918b3b925cc38b1f132
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:54 +0000

    decide mass home make mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
