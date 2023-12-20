ed9f2b6efa41322396ad3ef75549817752e18b7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:49 +0000

    kind direct turnover door cut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import platform
+def get_python_version():
+        return platform.python_version()
