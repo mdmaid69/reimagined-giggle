c2f667f7096cbc316f3d5b1a54b374b75c9a69ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:04 +0000

    regard final frame grow until

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
