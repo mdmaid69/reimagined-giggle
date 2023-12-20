628ad132f3796452f60eeb2cd09fee88f8b3ce0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:16 +0000

    lookback(on) constantly lot any lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
