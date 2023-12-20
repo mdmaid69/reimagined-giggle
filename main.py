e1c8e5f186dd1b66b8ee645fc6dc73bda3d9bf21
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:39 +0000

    stare much boat everyone natural

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
