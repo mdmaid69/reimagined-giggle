76855c74ca38e083e4d5398ca142eae9509ce0ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:12 +0000

    pause holdon order relief hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_sign(x):
+        return math.copysign(1, x)
