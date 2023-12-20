659919cba955021695b8f636a7cefc28ba3087ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:13 +0000

    start finally cheque building sad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
