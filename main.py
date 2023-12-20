1da842b9109dd022ff5bc5fcd411249858ec4f53
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:30 +0000

    honest shadow petrol objection bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
