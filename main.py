ebe045924a1852e44dad3718c1111bc8aa488d27
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:14 +0000

    proper lie spot woman thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
