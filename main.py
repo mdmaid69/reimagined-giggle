12fc9305d855e6650ea74d7789c0051be2ccf421
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:05 +0000

    later method replace definite ride

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Powers of 2:", [2**x for x in range(n)])
