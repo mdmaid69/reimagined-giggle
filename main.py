f4a9923cb4b5e10ceb797cbf7f13a4893c2e7fca
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:47 +0000

    taste gather office warm maintain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
