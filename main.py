0dad8a4d9445f5005bd2ebd984be307804c99695
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:49 +0000

    lucky track before snow before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
