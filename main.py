f7c708941f28bc967f9dc9e92352459d8a2b34f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:05:43 +0000

    tie noisy steady bringdown order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_set(array):
+        return set(array)
