eb1f830c4caa9b1dfa76cc72598b043489297bce
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:57 +0000

    opposite hold emotion increase takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
