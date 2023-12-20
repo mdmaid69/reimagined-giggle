9226667813ae3749a9758f9211fd468da75f8819
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:57 +0000

    interview workout dream shall priority

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
