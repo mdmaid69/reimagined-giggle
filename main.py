c7b8c1f4f383253c4f50f1253c50da0dc526a446
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:28 +0000

    judge deserve command edge load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import time
+def get_current_time():
+        return time.ctime()
