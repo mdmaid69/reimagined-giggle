0398ac0245b50862ce78bc9a879fa9b3950aac4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:35 +0000

    cry lookafter imagine deep interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
