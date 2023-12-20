f75b3d249a2f53f812184ec367c7cfb0ab0b4349
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:51 +0000

    not software Ee project moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
