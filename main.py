5763aedd12589e9e1d0b582f4f218f5f5b331e5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:07:04 +0000

    rain rest swim black anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
