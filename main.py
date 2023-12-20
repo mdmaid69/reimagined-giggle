aa9f4dcfa6a1118b4d79135cafc3bc0c0aa9b277
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:31 +0000

    uniform spot case someone bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
