9f7765ec3427009c61b66568974631c8f082dc50
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:36:07 +0000

    loose never keepoff in half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+print(os.getcwd())
