ef16d4ace5e07acd3aec0b91a05e4a154b80036f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:24:09 +0000

    give confidence separate pleased later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
