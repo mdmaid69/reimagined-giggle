be94cd72441522d9e7a0d18a3385eedc69832444
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:18 +0000

    downstairs second Bb back case

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def square_number(x):
+        return x**2
