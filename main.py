4e29f89239eb580b434c1032e3583047154184f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:13 +0000

    comeover treatment operation friend tell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
