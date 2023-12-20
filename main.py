d92d6f72752a95146a76b42de3d658128dee43b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:08 +0000

    sore pen maybe ahead suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
