a6d2eb2aa76955fb353138fe8065e6586e0029f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:57 +0000

    wave game ask once how

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_density(mass, volume):
+        return mass / volume
