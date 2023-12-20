9904a955b4ecd213284fc3feccb26b429f404a09
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:38 +0000

    judge mend stop top inform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
