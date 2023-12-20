4373c32be3fff8f0640affd74e12e4108529b0ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:18:33 +0000

    dress case square supply size

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_odd(n):
+        return n % 2 != 0
