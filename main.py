5a9c6576a3727867bddb788d39329623d0392d0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:56 +0000

    church method last legal port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
