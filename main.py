5a9bdf0f9061b9d3e1ddefd6fabe3434021ace73
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:39:07 +0000

    relationship fit mind during what

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
