52e0679af2737ee3c27f7f1b5c76df4772d22a69
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:40 +0000

    loss classroom drawing forget abroad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
