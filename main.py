ebc9dbeedb9962f1475add294fd273611415f00c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:28 +0000

    unlikely sea world expression stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
