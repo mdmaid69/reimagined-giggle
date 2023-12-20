9498b99779992e56fff5dbe9def8cded282e463b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:14 +0000

    point sweet wide bottom stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
