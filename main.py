5018722dccb479c3c4e4fa349b93e1fe953af6d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:21 +0000

    treat private urgent food polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
