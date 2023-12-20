10ee8ee864488666d704a094b775b77fc8c32449
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:22 +0000

    hope dinner brown soil bad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
