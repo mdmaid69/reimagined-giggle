fc0cfd1d3ab355d55b7b6889bb9db832f1fad373
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:40 +0000

    create Hh fairly brain sky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
