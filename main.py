dae8f37049727b493bc921a3f11fd26aae8d7452
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:22 +0000

    in field left Vv sweep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
