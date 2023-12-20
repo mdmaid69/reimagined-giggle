13629cc4bad05243e7c80a3938d608b136a93152
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:56:00 +0000

    regular crowd break emphasis damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
