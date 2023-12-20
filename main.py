f2786542820653605b08edd1350ec36667423de3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:57 +0000

    crazy point connected tie or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_words(sentence):
+        return len(sentence.split())
