cb036828c8cde1138a3ed99fb6ea1c30ec329c2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:43 +0000

    broken fly mess arrangement illness

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
