ab5b71bd4b6446ea67404298d73606b059912e94
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:19 +0000

    until thankyou steady best needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+def add_numbers(a, b):
+        return a + b
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
