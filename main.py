dbe8320bc558bbc8225504fcc1bdcc8bd05f17c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:06 +0000

    main by complain steam sense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
