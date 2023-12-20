a143d3ddae090897ceae3fe2fd9627f422a16da8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:44 +0000

    hang breakup sign recognize allow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def cube_number(x):
+        return x**3
