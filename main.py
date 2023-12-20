47a83d5d544fdd58ffab61c051bf49fc16db12bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:58:52 +0000

    right sorry deep floor punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
