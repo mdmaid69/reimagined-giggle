5f0fb8149289be3d85aba822e0e6c8465fedeb43
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:31 +0000

    hard plastic runout check advanced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
