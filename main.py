95d3d570d663b3fb9d916d93fd093704ae009bb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:58 +0000

    my little door switch country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
