92a1e1d5160925002ec6a52f1d254a319f3f0eca
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:43 +0000

    experiment immediately stick hole objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
