a0a1c79eee761afb2ba7cf4e082f1efc1c26ab77
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:16 +0000

    pretty sentence crowd safe wide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
