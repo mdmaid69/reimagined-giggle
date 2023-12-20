3a72a4f53212aa87307c72eb377acb3b7ef3b7d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:43 +0000

    press again distance cap next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
