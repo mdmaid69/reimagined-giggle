ef35854848b4681445d506d14b4f0c4cc9a3f69e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:18 +0000

    destroy spin pen bound like

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
