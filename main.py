35c674455fef59076ff17133d861d7c8f370bb67
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:31:17 +0000

    leg dream information meal down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
