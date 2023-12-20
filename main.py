844fc386fd79d8d2288982d4a726e1a11338a380
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:42 +0000

    slowly who consider everywhere invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
