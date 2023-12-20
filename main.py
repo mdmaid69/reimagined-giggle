3da734788eb74f256659b841dc5efba0811bb04b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:07:12 +0000

    interview bad patience eager clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_even(n):
-        return n % 2 == 0
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
