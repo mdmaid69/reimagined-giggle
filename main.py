b313c77db1b991aba6e5aa30f45e53d764acd026
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:57 +0000

    goback(to) bring bath include need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
