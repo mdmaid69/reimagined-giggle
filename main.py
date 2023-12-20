014add5a1b7eba31cd4bf26fb81e1c2e270d7f65
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:47 +0000

    disturbing key name furniture callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
