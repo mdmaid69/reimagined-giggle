12cef98162c5aea0dbbb2dee6a9f4f26f8bf0c94
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:57 +0000

    leg main onto reason it

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
