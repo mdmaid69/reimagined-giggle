b2b175c9fd7ba1d5bb001419349dc46ee6cb54bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:48 +0000

    to opposite roof reaction number

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
