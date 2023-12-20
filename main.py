ad506b9c5e665746989e83defac7a826fcb8ca23
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:44 +0000

    crazy Itself friend murder pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
