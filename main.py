92698be97c1092aa456d17d333ac8a33fb6e35e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:51 +0000

    just early complaint by regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
