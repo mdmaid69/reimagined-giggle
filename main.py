c4f7653c2482609eec48082a61b158ff8403463d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:41 +0000

    form chat they pronounce wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
