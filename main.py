024abf56a7f4bfd0eb3f045313b0996937262a8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:42 +0000

    holiday copy ourselves instrument count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
