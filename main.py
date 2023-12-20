aeeb432983300d1e7ed8deb9f903d6ba904eebb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:38 +0000

    stone injury person lesson professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
