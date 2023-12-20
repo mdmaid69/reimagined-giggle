e3d442382f916f81669864a6b66099b151ca600c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:52 +0000

    bury remain quit should them

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def multiply_numbers(x, y):
-        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
