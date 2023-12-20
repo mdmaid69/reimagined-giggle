1ec75eaccdb7d212df6e662aabd13ea8a94ce375
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:27:58 +0000

    whenever payment advanced blow bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_repr(array):
+        return repr(array)
