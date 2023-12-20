f631a204a6823df834292f54fc67cd08708ad827
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:50 +0000

    call path ought forgive spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def square_number(x):
+        return x**2
