88a7d1f6f4bcd36d340bee43c086fbe72cb9a958
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:02 +0000

    guard autumn they I opportunity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import platform
+def get_os_info():
+        return platform.uname()
