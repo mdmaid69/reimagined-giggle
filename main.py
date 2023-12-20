bf4be5c33efa8a20658bb2b0fb95907a2d70f4b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:49 +0000

    chief hold exchange mass storm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
