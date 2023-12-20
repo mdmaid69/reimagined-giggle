a2078fd058ccabb47d0dfbb1377316fe6ec13408
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:20 +0000

    raw left holdon near suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
