cf6f9300d26fc8d4d4acf08aab68ec3f9834bb3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:21:51 +0000

    shade wear sentence before sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_acceleration(speed, time):
+        return speed / time
