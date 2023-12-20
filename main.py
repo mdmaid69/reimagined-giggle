28d93481c057e1b2380b67dba2fdfa45bfc83fcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:21 +0000

    correct sink fault grow still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
