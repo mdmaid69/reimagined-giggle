fdcd5b800fce9545b4d1e45eef08c179c34ed999
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:03 +0000

    wish believe company team immediate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
