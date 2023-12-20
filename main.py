a5b0fef75d4177abee03beeaff884d884e46fdd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:26 +0000

    difference history progress sort eat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
