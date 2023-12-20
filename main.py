7058628bfa065f5ecb4e7a955a4271bdffa7bf1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:49 +0000

    cross sad collection paper clean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
