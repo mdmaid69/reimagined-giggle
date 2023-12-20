dd9c618dcaaa9d2fbb6c3a08f1cececf68cd2dd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:53 +0000

    callout involve search turnout gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def reverse_list(lst):
+        return lst[::-1]
