75ab08c98129a3aa86f3cecd34f4f68aa294771e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:12 +0000

    better impossible introduce improve honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def count_elements(lst):
+        return len(lst)
