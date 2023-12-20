7606e9b5c91bbcaaecb97b574ddda3b1eca33106
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:20 +0000

    unlucky pretty congratulations unfortunate cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_list(array):
+        return array.tolist()
+print([x**2 for x in range(10)])
