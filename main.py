8d78800303a499c5aa123887c8adca9f2c04753b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:18:30 +0000

    fact pleased stop replace breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
