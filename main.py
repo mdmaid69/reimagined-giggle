c8a93501caa3dee3efb7ff964ac06a97342af9f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:37 +0000

    dead shout as consider create

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def count_characters(sentence):
+        return len(sentence)
