2b35372b85caefbce769b6c34dd0e138aea9e8b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:14 +0000

    influence sport stone deaf I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
