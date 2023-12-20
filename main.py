0086cb1ca8f986bd9423c86bd99c1a0b915a48ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:29 +0000

    arrangement minute roof time charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
