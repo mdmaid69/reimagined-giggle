e89036357602f698589e57c31da4e8b3491ba1d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:10:15 +0000

    below sale burn unlucky introduction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-print("Hello, world!")
+def is_odd(n):
+        return n % 2 != 0
+import os
+print(os.getcwd())
