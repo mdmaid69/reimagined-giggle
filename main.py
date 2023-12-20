4c80372a184b92eab23f95d74d67cf20e3e3f118
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:10 +0000

    them him wheel excited level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
