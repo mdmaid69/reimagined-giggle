b8ff0abda73ac568a248d10b642066140db93e34
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:01 +0000

    yourself makeinto mark sorry also

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
