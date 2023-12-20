a56ba316e6cef837772f07a3d31d594137962656
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:36 +0000

    pin turnon middle contrast climb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
