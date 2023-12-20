48d1ae92852516acd2dd0b34ceaa69ea3de64179
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:52:03 +0000

    swimming combination free cruel first

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
