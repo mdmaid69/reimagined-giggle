a0c718c6416f4c47ec27a027a57aaed20e7f46e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:22:41 +0000

    battery justice convenient godown charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_speed(distance, time):
+        return distance / time
