1b928d099732bfc9d90ca3a5051570ca2772361b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:04 +0000

    quantity pick thumb programme worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
