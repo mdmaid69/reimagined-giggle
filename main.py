c1ed77798a9debff2bf5b45553b69262d0ad8154
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:41 +0000

    television suggest not getback order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
