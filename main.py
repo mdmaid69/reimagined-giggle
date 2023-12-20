f217a3493a8c1913ee63e8706e324d94092b1895
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:50 +0000

    properly 565end confuse tool risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
