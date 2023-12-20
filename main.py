27c48444493584cbaad317133843f21d4cb4df8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:40 +0000

    callout argue appeal mainly bridge

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
