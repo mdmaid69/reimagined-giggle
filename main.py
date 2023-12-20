608b08c800676224e6c3060a48186bc6126a5f02
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:14 +0000

    rank difficult aim brush clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def convert_list_to_array(list, typecode):
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
