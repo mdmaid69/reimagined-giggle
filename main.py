ea161f5546ed1fa25956e9b54c9af850e8c1f5f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:38:06 +0000

    spill provide likely tongue interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
