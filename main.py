5a78621532a7ad2daec2d92443ae7044af1903cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:02 +0000

    print contact return runover half

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
