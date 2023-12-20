780babae3b74e4b55a2bc375afd980302454aa77
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:32:47 +0000

    over properly consist pleasure open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
