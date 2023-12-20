f9f975c82cd092e7ed7ac513d366f8fce78cea65
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:56:19 +0000

    holdout emergency record welcome probably

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
