df1517aa8bd1774869d6b5c237838ca5390ba678
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:07:34 +0000

    be still experience university action

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
