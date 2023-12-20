6a98b784e3d4cdd5949df5b409cc97d26e937daf
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:02 +0000

    notice person lock keepout consult

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def add_numbers(x, y):
         return x + y
