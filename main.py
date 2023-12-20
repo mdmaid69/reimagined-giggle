99dd91a5a6b91c34401845f1fba8f953d6501702
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:42 +0000

    narrow programme dress failure Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
