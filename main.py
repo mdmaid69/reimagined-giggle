eb638413ce5798efae9874ea1c95e8d854e4ac1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:20 +0000

    arm one majority 179bite into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
