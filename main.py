bbeea87be96e1ee743e0574c134e0b3f5cde4bbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:56:04 +0000

    force have understand half order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
