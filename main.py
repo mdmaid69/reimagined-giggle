6d59bd49c1719b999f8e21a192061fe02cb968a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:03 +0000

    go give calm song behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
