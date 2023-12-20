fbf91fefe1884ee5d48b4955a059051a78efdd9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:29 +0000

    loud flow set mistake cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
