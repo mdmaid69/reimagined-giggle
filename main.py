735edfa543e23050f4eb52568ebc4f7d76fcf5a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:32 +0000

    middle behave kill thankyou holiday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
