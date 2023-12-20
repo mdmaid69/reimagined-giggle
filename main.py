7bf1991fbde21b6b2da7665789b79fd69725cbda
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:04 +0000

    dinner tidy generally dream slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
 def generate_random_number(start, end):
         return random.randint(start, end)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
