289ab002498abaa49f9325b426df9b2230ed9321
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:12 +0000

    emphasis breakup finish fire whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def get_array_itemsize(array):
+        return array.itemsize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
