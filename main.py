c0c40cb9c3b6a817b76016c4ad67ac595990bb19
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:15 +0000

    difference year reject cheek they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
