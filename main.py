63ef437658c1f96451041e9b6569768760bd7d48
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:58 +0000

    dry experience moment rather 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
