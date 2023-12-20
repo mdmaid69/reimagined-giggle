a23db38cbe97152675b656efe4d9b83ac1fb49e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:36 +0000

    valuable will law measure patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_set(array):
-        return set(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
