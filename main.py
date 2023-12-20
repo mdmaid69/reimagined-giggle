e3626fe235ce681bd8c0b024c1a72632ab442687
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:51 +0000

    more something blind dry reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def convert_to_hex(n):
+        return hex(n)
