59a0d7532cfb2125021235ceb581b3309695f833
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:36:51 +0000

    smell window guess car floor

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import unittest
 
-{body}")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
