7c14ef3716cce5e029444e11068423ea412d086d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:29 +0000

    badly lift check buy see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
