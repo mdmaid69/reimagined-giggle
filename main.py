312a63dc33d6718a727863a4bcebd37753894d56
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:16 +0000

    left begin smart statement reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
