b0fd19b7eaf09d7fab06e433a8eb558d7b81a48c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:03 +0000

    somewhere just callout nonsense sometimes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
