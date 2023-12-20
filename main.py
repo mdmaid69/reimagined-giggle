29ef95768fac5ce366e2cd99a559b840befee676
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:04 +0000

    arrive willing part ever programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
