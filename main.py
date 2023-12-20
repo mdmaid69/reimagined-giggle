e3ed1d58bf47f5890c22832fbe27cdb9baa8fa37
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:35 +0000

    ocean treatment pleased keepaway frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
