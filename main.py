5cdce48ddf91fe1d2a122d6444a21cfda015dd99
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:14:20 +0000

    set rent warn friendly hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
