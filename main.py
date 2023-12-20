92475e6cb74977ef7a60458775e18ba33c081253
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:52:58 +0000

    height steady space down today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
