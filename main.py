aee295ba05e6da893bd454502249a245aaa330d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:18 +0000

    language fix height goodbye strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
