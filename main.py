7214fcefe508b8354e720bcefb7eeafd7dbe3664
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:18 +0000

    either hungry move uniform sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
