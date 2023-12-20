ec3e9f20e7a0f644e7ef056f3708484a45d401e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:20 +0000

    press below raise ignore move

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
