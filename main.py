f61d1efae32451ec9b16d134ce90f991894f24f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:19 +0000

    calm discover cum hold runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
