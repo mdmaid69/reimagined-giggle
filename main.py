b69d07c79f7b8e763ea36f4c38a9283e2b9ca2cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:26 +0000

    past dangerous honest guilty debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
