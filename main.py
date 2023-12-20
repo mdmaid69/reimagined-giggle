dc113df6d381ae7aedb263235c5f548a91f439d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:34:12 +0000

    hospital heart must broken plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
