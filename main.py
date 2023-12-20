4b6f13e4a2dd893749bbeda446f6525c8a0abe2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:36 +0000

    uncomfortable who than test charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
