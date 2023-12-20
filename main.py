7219fe99d9751af55329c48c6deb7b40fb3cc08e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:18 +0000

    bunch actual holiday soil touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def reverse_list(lst):
-        return lst[::-1]
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
