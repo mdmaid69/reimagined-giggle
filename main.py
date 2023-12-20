2be631644e98302daf6314dfb8be6d609371f0c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:04 +0000

    herself attention remember regional pleasant

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
