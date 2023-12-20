fa85cd96a9ff2ccc831d9ec5c700eb92b83722a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:55 +0000

    show touch watch air send

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
