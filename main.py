b4a4b2bd821a5c015a52af969043c4f08aadf7be
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:25 +0000

    fun Zz innocent mood workout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
