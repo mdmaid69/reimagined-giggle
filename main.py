0962150027523259a783076a8425e314ccdf81fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:43 +0000

    pencil extremely trap necessary explain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
