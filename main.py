a2636b85254e85dbb3198ee8e5db878896cb835e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:16 +0000

    dull what regular lucky guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
