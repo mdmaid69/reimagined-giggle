b8e51b79c0740f18190da21d58269341d43f2c1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:54 +0000

    respect attitude nor fasten sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
