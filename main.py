03913b9ec84fa3b8a1236c3c51f1ad06db204c09
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:59 +0000

    mad distant person pleased also

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
