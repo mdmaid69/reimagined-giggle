462bd5b1756b96e35175b944b927276e9a244504
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:14 +0000

    under instant partner except correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
