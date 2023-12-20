f644bc11efd35536e4585ae50ca84cf24e3599d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:02 +0000

    afford stand secret left one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
