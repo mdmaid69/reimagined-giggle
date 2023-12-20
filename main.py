4cfcce9dbc7f3e0e8ddf00bb2ee0853c8829e4f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:02 +0000

    blame song frightened upstairs way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
