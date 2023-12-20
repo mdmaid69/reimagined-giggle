78f53acd67952124972c3e5db6da13c7145e2c92
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:37 +0000

    characteristic accident holiday close to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
