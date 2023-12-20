35a644f1a29f0d427a834d4fe68aacb1a197c9c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:10 +0000

    spite knock which advantage treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
