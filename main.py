47774f825fbe4ebce06caa4b619b30e7cfc99645
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:45 +0000

    failure claim evening artificial know

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
