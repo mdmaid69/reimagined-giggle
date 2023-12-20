ad1c4a9cba0dcd4a6de0d58659bf6e98fa9640af
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:09:54 +0000

    wrong creature field weather small

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
