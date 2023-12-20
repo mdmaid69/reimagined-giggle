76bb327c1e402823fc82fc4c8df608bfd13e89d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:38:12 +0000

    heart this happy mass suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
