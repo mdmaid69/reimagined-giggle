8d478e4e218e6a004343f40df3f738ce4fd30590
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:42:02 +0000

    plan office genuine ours heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_min(numbers):
+        return min(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
