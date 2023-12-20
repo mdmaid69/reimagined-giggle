74267708b523e64c486bde906bac6902973896b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:36 +0000

    yesterday treat suckme action weather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
