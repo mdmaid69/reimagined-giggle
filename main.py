ef939086fc5c7d9003dc1e1bb0b2682535fd7c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:27 +0000

    importance furniture fence mad shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
