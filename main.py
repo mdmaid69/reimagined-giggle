86c9876f7aacf98e3a8c35e0e412e1b10133b392
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:39 +0000

    dark that reject vanish steam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def convert_to_binary(n):
+        return bin(n)
