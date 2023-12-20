c6ea21102e425a2e205981354f1ab40326fc3ed9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:31:34 +0000

    breakdown last accident plenty hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_typecode(array):
+        return array.typecode
