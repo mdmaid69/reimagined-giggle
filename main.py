e6d7fef4876a87240920a8248d6629535a0cd5f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:54:43 +0000

    hurry hurry copy inform past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
