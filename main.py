cf1a12fa513c5c954c919198e14ae6bba265616e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:07 +0000

    quickly set eager eye inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def convert_to_octal(n):
+        return oct(n)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
