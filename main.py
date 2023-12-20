6fb6c82436183dd262c1f2b62cff4ffa32f3c2b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:09:04 +0000

    appear delicate wide respect well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
