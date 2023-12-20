be25d3bfdcb27acf2a62bd23396582a9c999134c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:55 +0000

    order idea thanks harm steer

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
