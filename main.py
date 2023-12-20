f08b0297f3ec8fd08242b69801a327033d6712b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:58 +0000

    recently on consult recent near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
