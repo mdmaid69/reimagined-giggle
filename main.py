c3fd57a1fe3bdddab0f6b61768621e1a9266aa57
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:00:19 +0000

    free avoid burst nature exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
