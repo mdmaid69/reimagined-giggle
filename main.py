a1065690d1617bab2e2ccc7593d609dd3a043f84
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:39 +0000

    away discussion niggaacademy guess care

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
