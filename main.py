b7121068fc82b716e0303e78c663a89a46887fb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:03 +0000

    serious form outside target welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
