4bb891374942786c88da75b08c33299c1911dd52
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:38 +0000

    person plan limit surprised concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def print_python_version():
+        return sys.version
