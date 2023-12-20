961da538a836261a3a4cc808e136a052cc1a3a04
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:35:08 +0000

    proof still pity slow mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
