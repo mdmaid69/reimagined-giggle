788b5968c9469a962f5850f9ce9bd61887798f45
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:27:35 +0000

    if world anyone immediately across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
