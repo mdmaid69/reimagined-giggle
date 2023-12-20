b34eb47812f10f7270809f9a001797a56c2b2d06
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:41 +0000

    improvement powerful ask dull squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
