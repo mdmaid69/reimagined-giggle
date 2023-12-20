93b1f6e03e96fd95b9cf8b450cbb819ce8bc7ba0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:36 +0000

    spite criticism pot change murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
