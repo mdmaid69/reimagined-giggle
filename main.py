cbb42e82a5c1293400dadb9083a78220dee4a31a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:33 +0000

    right discuss past independent explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
