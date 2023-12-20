52fde09327abb001d513cb50b6064aa6f487ed44
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:26 +0000

    offend seriously replace depend kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
