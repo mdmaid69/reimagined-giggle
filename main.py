947026cab965d620b4361bff300ab146006f7d98
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:12:37 +0000

    load surprised unkind doubt lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
