71125dd74b8c46098b536786b8f9fbe1b09e0fdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:36 +0000

    live how maintain perhaps consist

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
