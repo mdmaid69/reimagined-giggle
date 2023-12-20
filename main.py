5edf1af125683007c825fd58e94ff312e258fe71
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:46:57 +0000

    getout rich burn cross container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
