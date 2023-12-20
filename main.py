ef191acaa0b5666b2038a0c95af7ebf7eb6422a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:48 +0000

    example along instruction university comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
