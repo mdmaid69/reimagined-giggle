f4d5c1794366b59663ae0a0304c285445c78e50d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:39 +0000

    month choice pleasant upset obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def count_elements(lst):
+        return len(lst)
