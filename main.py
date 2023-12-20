b5caacd8e31ca88b54a2db961aebf72f2223814a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:58 +0000

    building letter study mouth Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
