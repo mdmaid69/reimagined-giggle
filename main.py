cea9e56f20c458730336648222f44cb1949dc6bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:26 +0000

    sink takeover communication add stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+def is_palindrome(s):
+        return s == s[::-1]
+  def is_even(n):
+        return n % 2 == 0
