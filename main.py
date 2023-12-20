cbaff6ad32d14e6a5787ae9a6c23b570f671e513
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:03:51 +0000

    interview time geton pipe extra

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
