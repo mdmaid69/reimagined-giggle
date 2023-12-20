bdbcb1c94d42333c1786c5cc2a1f751169a69cf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:12:48 +0000

    needle although innocent round turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
