3eb6ad26f839034d18036753be3eb0b2d09ceb80
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:40 +0000

    hollow fear let godown upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
