bd1076ea3908c87f483b056ffc60b8656ee916b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:12 +0000

    control upset welcome rush burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def print_python_version():
+        print(sys.version)
