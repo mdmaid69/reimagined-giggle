9cfb00b330ec41127ded5ad535a7424d2fc11cc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:05 +0000

    birthday lead tune unfortunate welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
