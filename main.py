ca9eb259af5c8d2a6f7bdeba719b313e29e66d08
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:03 +0000

    plant wheel such each human

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
