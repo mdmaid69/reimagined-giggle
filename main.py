b622eb08a3374b538044b0ed59cbb73122d665e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:29 +0000

    pronunciation keepfrom entry slice glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
