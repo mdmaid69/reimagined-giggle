7a4d9cf054e9813f7a80f24949a1f5e2cf5b762e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:19 +0000

    habit raw once comefrom music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
