927d03b66f005636a5c02291eec05519141dd795
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:29:32 +0000

    responsibility stupid move offence address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
