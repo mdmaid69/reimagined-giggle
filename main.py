18f26d1a44b25a2d07b5339e30ce20e5f5a007a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:06 +0000

    mind star makeup convincing excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
