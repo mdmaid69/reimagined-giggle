8f918aba6739ab626eb4a80b6b27154f65e4e58d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:30 +0000

    blind slice either advantage attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
