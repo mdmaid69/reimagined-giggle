937f1efe678784d257916702a013b2c026181a0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:34 +0000

    dare nervous concern count past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
