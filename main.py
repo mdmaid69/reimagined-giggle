a8ff39d597b1fd348330b723519b7397a2fd5be9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:59 +0000

    better often mean form change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def convert_to_hex(n):
+        return hex(n)
