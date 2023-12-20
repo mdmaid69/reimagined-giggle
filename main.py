fc799269707f9d239b9a5d1804734fcce4f952a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:52 +0000

    lonely speed firm little book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
