cd977271d36c6bfa6eb2e915515cfbfecb77a0d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:43 +0000

    war opposite trap black challenge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
