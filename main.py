f4374cc0929280ac5cb4372565e66e0cf2d561ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:28 +0000

    draw amount force warning move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
