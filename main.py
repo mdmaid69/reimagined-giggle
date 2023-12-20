ec0a4298c689993d74357c030ce36df75f6dc7f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:52:03 +0000

    Oo file measure defend they

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
