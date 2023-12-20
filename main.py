fd5e78bfc5602c2290cdb9940767bc59bac036d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:45:30 +0000

    pretty shrink distant at common

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
