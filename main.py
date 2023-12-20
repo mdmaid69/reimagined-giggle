94f8d9707f0d8adb122990b2a26b68169ef1fe5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:52 +0000

    typical bill splash afraid burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
