0c8bf74c3967dc1a36f8c7b59ef0ac292f507a20
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:40 +0000

    hand minimum ask measure fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_power(work, time):
+        return work / time
