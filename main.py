a3335eeb09dbb5a4b48d437067c6d1efe2c74cb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:52 +0000

    group difficulty mainly offence here

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
