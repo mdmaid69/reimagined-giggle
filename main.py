86a18af1d78442c65041c900b93e1132f61ab513
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:30 +0000

    spring alone too down formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
