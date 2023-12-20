757f0cfcb4fa628247a5dbccb210e354383eb93f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:31 +0000

    shade letter interrupt Bb content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
