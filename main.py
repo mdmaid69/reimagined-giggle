06d7e2e1c9e7c39bb2c2db463c8ed48314cff42a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:19 +0000

    brave pretty now behind rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
