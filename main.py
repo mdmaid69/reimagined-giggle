b901d3b10017622821a302aa293cdda335e28267
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:01 +0000

    between attempt bear tone few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
