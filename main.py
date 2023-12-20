a7029aa1efb038fe69e9a1633e591cd6b8532ae7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:21 +0000

    violence comment impatient noise production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
