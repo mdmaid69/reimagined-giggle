1440701b3a3a6ee9f0012cc3e463ed133552f7ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:38:22 +0000

    vanish freedom shut shout call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
