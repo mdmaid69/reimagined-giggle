2ea126df9e5d33dc97f10f33f3e926d43142de81
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:04 +0000

    trick mark appeal tie load

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def count_words(sentence):
+        return len(sentence.split())
+text = "Hello, world!"
+print("Words:", len(text.split()))
