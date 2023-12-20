8be148f545d58dbbd2d03fa90226775f34294950
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:09 +0000

    production hungry tradition so principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
