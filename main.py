1c92b378f47b02ae27d3951bfb3955c9289c5ecb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:35 +0000

    appearance relationship shoulder dream one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
