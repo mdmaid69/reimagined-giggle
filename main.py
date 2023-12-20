49b9ba44bd0fc73e3bcdf42a584976d1630e1ae9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:56 +0000

    call day occasion male time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
