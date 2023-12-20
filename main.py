3ef1c92f7b2c4d45a44d4b5d307527cf0571c82e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:44 +0000

    health before lookback(on) tidy rarely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
