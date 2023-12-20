dd4ddb9f8c54001968c50b037f86db91dc0c6a87
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:39:06 +0000

    ball count scheme invitation anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
