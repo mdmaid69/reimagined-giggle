479b55526acfd2914756a3414cf2a44857dc1fd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:46 +0000

    fill bringup pupil content runinto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
