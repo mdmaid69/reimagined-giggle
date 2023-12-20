ca615f79a42241a6aeed217f556de00336ad17f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:13 +0000

    illness thing present store recognize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
