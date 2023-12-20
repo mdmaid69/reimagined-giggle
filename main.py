ac257af2300abc24fab6b87e1aedabf919f883f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:01 +0000

    battery type connect sting goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def add_numbers(x, y):
+        return x + y
