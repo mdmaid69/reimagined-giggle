4dc4d6eaeedb5451b8bb5763ab213978175f5630
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:42:53 +0000

    570enjoyable simple along anyone offer

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
