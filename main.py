8e41e3ccaf6e449e1846ab1137261ae4f3a0d085
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:47 +0000

    somebody thankyou noone pretty advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_power(work, time):
-        return work / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
