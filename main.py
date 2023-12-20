d3b9e89aece7b6598f3cefa45a294fc371ef9c39
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:04:17 +0000

    sit over operation chin together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
