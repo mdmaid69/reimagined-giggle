cba4563f94c9c091c5c948f450ba2aa484ef54bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:53 +0000

    me tense prize sister oil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_power(work, time):
+        return work / time
+def find_min(numbers):
+        return min(numbers)
