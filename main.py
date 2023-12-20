9947d0fe2398c22c78dc8bdd24912396c749fed6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:45:46 +0000

    truth high warn run record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
