0b3abc5c2581b8847c3fa6db2329cb6d14be638f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:51 +0000

    goup light less among acceptable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
