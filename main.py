77bb5827111a728de0cbd1f90e6b9f9ff54a81b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:15 +0000

    thought contrast choice guess row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
