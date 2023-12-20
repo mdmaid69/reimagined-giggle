b1eceb4d9c5b1a94140c9a88c541db51a606ebdb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:28 +0000

    wire related hurt bored instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
