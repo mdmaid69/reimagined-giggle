91dd523ae87565fef0806f56235855d032640620
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:24 +0000

    breathe coat conduct talkinto underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
