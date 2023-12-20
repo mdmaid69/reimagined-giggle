5d5e9ac7bfa634e3ae46369f802a4fea5d2fb1c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:32:41 +0000

    earn modern early dream habit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
