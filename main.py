cca5aca7be1bc1765c4d48fa152bf3fe3f8a311a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:47 +0000

    load contribute comeup frightened train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_max(lst):
+        return max(lst)
