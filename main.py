8e2bdd137a43b11ca5dab75afa0c2642aaa1bd68
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:16 +0000

    hate look throw dead repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
