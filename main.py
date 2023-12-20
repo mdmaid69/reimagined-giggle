eafea0f6b443f18ad3bb25e67d6d781af1c7cf2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:50 +0000

    wheel instant artificial everyone fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
