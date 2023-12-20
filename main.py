2e5cc605b3c1890902fad5ab9ed0c7daac32255d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:12 +0000

    shape press common thumb switch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def reverse_string(s):
-        return s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_even(n):
+        return n % 2 == 0
