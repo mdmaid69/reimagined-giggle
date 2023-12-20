8365e1c9ba03d52b207b37a42fbc2cb2be59ce10
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:48 +0000

    communication wrap design spend backwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
