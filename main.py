b19d97bcdb629da3492fa0973f3fd524bad965ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:34 +0000

    appearance pool finish wealth language

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import collections
+def create_user_list():
+        return collections.UserList()
