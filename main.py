66218ec724f2dc8eaede377ed2f0121d3d86fb3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:06 +0000

    opposite takeback coffee ignore unpleasant

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
