6c3270b27885baa0e403b114a9d179ee42a6a12d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:37 +0000

    slip decide person thought noone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
