0d31cc6049346dac40a7723851794c94217fd192
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:06 +0000

    sport manage tooth getaway zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
