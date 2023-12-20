5ee51b97d290699935688e15eec93077196492d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:23 +0000

    favourite male round maybe interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Cube numbers:", [x**3 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
