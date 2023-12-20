ee41cf6df8859f681772c95c4bb6f58d59fc0b0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:43 +0000

    argument point rescue bright translate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
