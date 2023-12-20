e35a14c8ed9f3efc3652221c2bacca9d239b2854
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:09:17 +0000

    whom sign 569enjoy match left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
