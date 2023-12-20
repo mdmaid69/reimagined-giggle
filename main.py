3546eef9217f3ab2025389154b004711fbf70ff7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:20:15 +0000

    sell weather relax middle debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
