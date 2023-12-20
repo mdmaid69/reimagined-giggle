df9127aca030d1815324972d89cda39e6563510c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:21 +0000

    expensive boat light breakdown once

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
