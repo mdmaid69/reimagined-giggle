6a9437e1282e2fa0c658e1990a4bdf71de32456b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:34 +0000

    and complain wheel to tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def remove_duplicates(lst):
+        return list(set(lst))
