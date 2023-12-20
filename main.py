d9db8f096eb45d1efebe3fd44ae8f824691085b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:11:16 +0000

    remain comeout commit perform conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
