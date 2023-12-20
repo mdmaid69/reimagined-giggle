d66bcae4598b1c00960c688fda82f0473fd23fbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:26:50 +0000

    soak case quality unusual needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
