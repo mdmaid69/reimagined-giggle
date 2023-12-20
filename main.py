856e6facdb291780cecde213df5445305e52b505
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:38 +0000

    own dance between office benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
