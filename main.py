fb472db7d20287e18307dcf462e16d653ff26474
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:10:04 +0000

    drawing immediate quarter occasion long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
