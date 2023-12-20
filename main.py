87d6e1442503fb03a51f12d9f2568338a2ca602c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:15 +0000

    settle free by funny whom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
