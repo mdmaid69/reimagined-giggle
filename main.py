39fc558b77bcb79e45330bb0288510ceddb3260d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:25:02 +0000

    impression there sad note listen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
