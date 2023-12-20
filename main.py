f222d573178d073fd2a7d18c5f7b256d47b7b7b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:51 +0000

    circumstance pain runinto believe comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
