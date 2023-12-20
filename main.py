267e7c1099b9f84ca95a020179343b97119002f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:49 +0000

    answer proper face statement murder

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_octal(n):
+        return oct(n)
