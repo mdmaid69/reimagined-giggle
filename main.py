cabec5cf56106e2c4f4e006348baf104b4085fd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:16:03 +0000

    evidence method Ee certain anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
