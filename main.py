d85b805ad28d6e5be1df62cb928d08481e734a2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:49 +0000

    theirs bring boat total recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
