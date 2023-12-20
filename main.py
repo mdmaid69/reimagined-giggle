c4fc058dfd6d236ef7e579618c430de7b8544f2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:19 +0000

    interview contact lean sort hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
