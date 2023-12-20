3f8ac0f587a6f0834083a9210d32ce52ddb48fca
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:06 +0000

    border to search wing through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
