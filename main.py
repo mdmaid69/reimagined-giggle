799f105631a70cb6289a3c761f7588eac5f99563
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:55 +0000

    benefit need swim cry brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
