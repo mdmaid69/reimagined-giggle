bd2b5159ec0f92c7b956b07ebb687bc872d22121
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:16 +0000

    expensive promise thin either wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sys
+def exit_program():
+        sys.exit()
