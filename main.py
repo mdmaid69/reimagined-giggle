6876a1c198e3f6bb21b84543c8456cf3b7e68235
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:23 +0000

    water blame together best blame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
