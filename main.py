9dbb3d48b09a693b1d53a7e2aac898a4c18c8653
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:11:16 +0000

    hour prepare breakthrough talkinto who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
