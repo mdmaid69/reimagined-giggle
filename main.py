4f88c9d7cfc289954323426c8713509242d07a2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:21 +0000

    light pleasure rest purpose perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
