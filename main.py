1987e880e72dc7141b3ce82dba9e38667e446b2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:49 +0000

    achievement interested breakout debt chin

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
