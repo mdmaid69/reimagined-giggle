1bd58cf16feec4f3e67b7165911cde6b88d81460
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:21 +0000

    crash destroy dream cope feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def count_characters(sentence):
+        return len(sentence)
