09a32597e87f11eedbd688e3fdf4ac2fc1623b9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:07:32 +0000

    pay careless extremely anywayanyhow interview

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
