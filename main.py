135d32960ff8a16eaf6a7d76deebb0730def97ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:20 +0000

    head Ff watch somehow succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
