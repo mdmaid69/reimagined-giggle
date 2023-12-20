ebc8f6e1bd9fe6eee21df61d0977fe2a39d46d02
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:09:59 +0000

    scene regular get throughout comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
