9ac1cc5713bd659dba6092d21631d22e6fcfcee9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:13 +0000

    Kk frighten eat come division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
