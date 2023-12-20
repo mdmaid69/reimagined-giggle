0992028260526c796d29ff22fb5d9674275300a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:39 +0000

    notice turnoff age lost base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
