9c430b5dd29ae0bd1a2bbd3c3decd9e674e00679
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:56 +0000

    admit healthy sensible sound star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
         return oct(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
