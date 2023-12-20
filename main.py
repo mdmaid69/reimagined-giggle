a5b40067db3b88aec4a48cd2403affb93407086f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:33 +0000

    uniform country tell challenge win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
