af1df6320fe6b0946302f70679bb3ba8a40b120e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:57 +0000

    prefer young part exploration invite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_pressure(force, area):
-        return force / area
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
