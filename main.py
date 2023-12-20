5411d7f236c424bd923bc1b9e4bd603d950643e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:40 +0000

    lead man container apply godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
