91f4876750a0d1c8414e4c278c0f1941cc2232d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:04 +0000

    house convince breathe instruction runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
