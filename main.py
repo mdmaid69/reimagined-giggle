c8078cf29c1776119845bf2084dae35b74b57e01
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:51:10 +0000

    question turn recent cost us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
