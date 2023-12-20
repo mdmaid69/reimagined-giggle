21372a4135a7c1975499e70f2bba6dbdedc628eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:54:51 +0000

    bringup favourite exam news worse

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
