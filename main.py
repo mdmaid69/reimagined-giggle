6c1e4583d60b02b1961d2ec8fe49a194d2521d47
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:49 +0000

    people choose either Qq quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
