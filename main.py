d6ebbb833fdd8a1a6b69ef96fc2353f9baca1aa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:38 +0000

    tidy as turnaway record goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import time
+print(time.time())
