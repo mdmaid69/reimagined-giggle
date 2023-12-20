1fe9798c37c5b0ffe06a32e0660d3809b97b2be8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:21 +0000

    note corner shrink rate vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
