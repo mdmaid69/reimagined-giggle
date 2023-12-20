1b1f19414011b3d5d406c14cf2fd06d0ebd3d597
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:36 +0000

    concern quiet whichever opinion appeal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_unique_words(sentence):
+        return set(sentence.split())
