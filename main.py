36bf2f81b5046f7a84b386cf181151c5dc25cedf
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:54 +0000

    test crowded joke I traffic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
