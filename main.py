4b9d59b92edc6e925e5f35cd025c54d758d49887
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:02 +0000

    clear brush inquire score right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
