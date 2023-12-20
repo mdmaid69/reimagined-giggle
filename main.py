ee82883bb60950238d1e99d3f07405b206514fb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:27:28 +0000

    turninto same strict exercise keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
