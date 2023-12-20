35a2b99d0d9195b7fdf28f75edce18b1d24ea903
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:39 +0000

    suckme winter live land problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_power(work, time):
+        return work / time
