0034b20e4c320b65677b5096e0962b233e67d474
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:06:13 +0000

    shall obvious correct dead breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
