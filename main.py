bae61fe4f7ca590a0fa5aec77a3f9a399f879521
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:21 +0000

    artificial improvement hollow somewhere giveup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
