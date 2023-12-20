f434c512f48268708e25277014e83f4276699a35
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:47 +0000

    again muscle final coat save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
