09e4a5a65343a339cde1a2340a75be6b46308f37
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:44 +0000

    correction comeover prevent now stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
