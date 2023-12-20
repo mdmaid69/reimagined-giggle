3fa919815f32aa8cabc23c1ace915ab2abd74444
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:47 +0000

    machine for share remark body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
