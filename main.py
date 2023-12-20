30aed83176216624a94a3562653127ea15ac03d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:24 +0000

    bit upstairs land choice make

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
