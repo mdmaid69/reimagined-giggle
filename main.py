051604fcee843ca5ef12623c1eca90f16ed48460
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:46 +0000

    mainly complete give right giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
