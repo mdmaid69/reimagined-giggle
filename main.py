c8ddd7f4314a1a9bd2746dc034c0608371610a73
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:36 +0000

    consider smile direct joke minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
