c7f3a756b3a2c6733b393d6d70f4b029adc6b27a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:15:40 +0000

    film we time look last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
