a474400c603431d736fec028bc2c60b945db6085
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:01 +0000

    question combination prize himself tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_float(array):
+        return float(array[0])
