3dc8091e0a2a7bc2c5e06d883c4ce7aa06fce574
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:26:54 +0000

    independent report negative stir lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def find_min(numbers):
-        return min(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
