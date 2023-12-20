78d3a0b81c2f7193d723c1018c3ec261467445c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:43 +0000

    here giveup weak careless fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
