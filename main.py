1d9281209a62885478e76a1c6ebe5a6df540ce13
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:57 +0000

    570enjoyable date scale borrow character

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import sys
-def print_python_version():
-        print(sys.version)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
