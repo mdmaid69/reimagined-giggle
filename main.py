48c1bfb0b4a211d077d6532dcd7d382e1dd3fc20
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:43 +0000

    complaint trust operate pity sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
