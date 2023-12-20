f280b5066493e4493dae299d57213e75c6c39c7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:37 +0000

    arrival town dear through furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
