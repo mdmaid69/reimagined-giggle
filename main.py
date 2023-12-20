33afd6a686a7cb560ea9f831bbe7950c5ff63d7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:55 +0000

    admire genuine join enthusiastic breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_odd(n):
+        return n % 2 != 0
