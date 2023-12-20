a8438ba5f5591d551a58d40759ba5146fdda9321
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:47 +0000

    fault strike all sport Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
