231995944e69ab0fa970b5b318f5a244b9ec6dc4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:07 +0000

    object quit raw almost small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
