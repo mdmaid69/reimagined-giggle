41ac19675ddc5d7ff179df1ee8512974165daa26
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:17 +0000

    pity follow breakdown base aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_density(mass, volume):
+        return mass / volume
+import sys
+def print_python_version():
+        print(sys.version)
