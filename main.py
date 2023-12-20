a2286141f96e0910907ba55b1f293347f4e0eb23
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:20 +0000

    radio extremely government exchange condition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
