6dd0cb91b3331cfccf2b5e379631029846cb8692
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:32 +0000

    through sure create sleep but

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
