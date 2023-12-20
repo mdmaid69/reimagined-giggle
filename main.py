bbf4095f3d856a4a1458f6fe3a9c6201a0fda3d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:57:32 +0000

    search colour splash post where

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
