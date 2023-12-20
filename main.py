1f8e840df2e0e2946b38a9a7129b7a281f0b2a6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:15 +0000

    nurse check shade trip limit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import re
-def find_all_occurrences(pattern, string):
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
