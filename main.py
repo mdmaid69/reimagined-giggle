116b952124824ad9540a55bb56fcd540bac52c2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:58 +0000

    brief nearly sort oil warm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
