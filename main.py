4fb1b417492a458417294d4e27dabfb0a9329941
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:41 +0000

    learn authority yet ability many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
