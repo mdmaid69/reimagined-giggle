36b34655d86fc61313127a0ea69ad49ed22b31bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:55 +0000

    brush class appearance opponent runinto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_work(force, distance):
+        return force * distance
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
