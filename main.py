36dfe07adb6ab05207d776267b52db34a3177b22
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:51 +0000

    thorough pick country favourite judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
