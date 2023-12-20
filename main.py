d70f41bcbb6d6ce6d33e833c3a166adadcb3f8b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:55:03 +0000

    capital fit cum odd while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
