d1ea1254ad94ee643bd451a08c9b39379c342172
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:17 +0000

    hurt afternoon take silent opponent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
