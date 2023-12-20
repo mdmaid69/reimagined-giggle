8ef52b2b468749255cee82ef7bca9160e929a711
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:27:41 +0000

    word along length smile time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
