c944b3fef8017c84fcf0306e2cd050f447cc80c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:58 +0000

    various fine guilty ride surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
