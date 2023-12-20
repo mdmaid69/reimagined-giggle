131e58b0acf01e9db39b32176dfccc6ca258b9b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:07 +0000

    comment after teach off Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
