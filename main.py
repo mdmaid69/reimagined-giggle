57764acd0e5497459b00d1990575a243ab2a12b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:02:52 +0000

    pleased lonely relationship give eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
