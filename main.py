0507ac4235b19ca3598d7130cb7da189ad04e49c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:03:02 +0000

    bus tough common creature strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_length(array):
+        return len(array)
