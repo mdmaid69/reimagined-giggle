6f999c6bddb07c249ed1f687086a2bd9121ef162
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:35 +0000

    wind whether combine variety discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
