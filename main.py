5614f804d8286f5634b521a59520e7738f844b32
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:51:24 +0000

    worried shade unlikely song marry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
+def calculate_density(mass, volume):
+        return mass / volume
