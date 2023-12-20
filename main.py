f6ad1e75a1abaf3e1e238233d7df7b05edea2079
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:00 +0000

    your contrast scare sharp what

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
