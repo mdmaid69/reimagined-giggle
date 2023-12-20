31e3a591f62537e9fc6efbf8d849d6b37a9b4d0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:19 +0000

    calmly lie persuade complete belief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
