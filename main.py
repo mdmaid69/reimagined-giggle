a676e5cb184d87932c35315e8d5596d3610497d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:31 +0000

    raw hurry damp behind scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
