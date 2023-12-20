afe8a58d4d272bdbc43e133c44290972a1beaedb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:14 +0000

    depend squeeze lift score judge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+print([x**2 for x in range(10)])
