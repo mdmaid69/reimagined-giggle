3d82d608a90ee1749dcdcd9d135dbd8fcc20b86c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:11 +0000

    nerve emphasize remain bound funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
