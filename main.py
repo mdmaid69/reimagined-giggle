d460db3b224043c99546142062592ac041aad4ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:42 +0000

    improve tool calmly attempt disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
