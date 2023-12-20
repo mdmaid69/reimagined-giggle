edfaf46ae6d664beb1101b9b19e5e93a15239f6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:50:00 +0000

    share claim tonight kick principle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
