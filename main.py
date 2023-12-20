1064fdf64da8feb87585aa291c0df6b8d0924050
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:19:21 +0000

    dare content project opportunity kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
