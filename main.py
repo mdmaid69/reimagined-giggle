00810f2d62380220cab8b48caa6dc73ee3112a03
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:57 +0000

    split exchange offer hardly tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
