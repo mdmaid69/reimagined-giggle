42d3464070814a65ff2fcd320293774c74387483
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:46 +0000

    body same unit contact war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
