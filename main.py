85ca49bbc782aa03621abbcbdda342e3ce8c546d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:33:55 +0000

    call near mend corner one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
