a0d78eb3bf73959507a79df23bc3eec3ed506850
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:40 +0000

    wet condition interesting human book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
