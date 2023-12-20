00fb40e50b4d5a095b5cb247f1ce114e97115dac
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:16 +0000

    cruel instruction public intend be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_str(array):
-        return str(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def count_words(sentence):
+        return len(sentence.split())
