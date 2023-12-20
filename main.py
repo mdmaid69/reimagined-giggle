9bd8c554cf66e5bfe0bdaefd5279b753b20cd242
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:24 +0000

    cross really no buy ball

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
