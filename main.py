9cf8103e12844ce4da785e0165a9e2b1a4cd908f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:47:06 +0000

    way sentence sting strike fetch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
