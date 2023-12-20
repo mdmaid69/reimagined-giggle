282aa9bb45c2697d39f9c5c210a8b0d1b5c007ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:27 +0000

    Yy gap pronunciation piece impression

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_counter():
-        return collections.Counter()
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
