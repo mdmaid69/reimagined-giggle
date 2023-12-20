9febe10df1704587403728db3780be789ffa8e39
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:18:17 +0000

    improve successful suck view underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def get_current_time():
+        return time.time()
