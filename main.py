3d14d07546cb9dbb95156b59843508230912f005
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:33 +0000

    put demonstrate recovery rubbish effort

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
