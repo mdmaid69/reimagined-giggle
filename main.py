6df25065d350731b3b7c1feb21b035cd85c2edf8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:10:12 +0000

    no lower justice realize excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
