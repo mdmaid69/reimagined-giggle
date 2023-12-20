9386643bc1b180c6b40ce01f1eb7e204ba8572cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:58 +0000

    vanish thought scrape serve complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
