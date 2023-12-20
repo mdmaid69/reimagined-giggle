3950fad24aaf184b7bba47029458256389e5f99e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:49 +0000

    near zero proper fall Ww

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def is_odd(n):
-        return n % 2 != 0
-def cube_number(x):
-        return x**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
