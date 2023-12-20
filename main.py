1226fa5660f34deacd622c40a4623202b50afcbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:52 +0000

    club extra command stick steep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import sys
+print(sys.version)
+import sys
+def print_python_version():
+        return sys.version
