6c369a54a814617e75910bad0312bff359b9b2bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:00 +0000

    disgusting nerve collect community special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
