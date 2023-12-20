0e06322f83e4cefa6dc7db0b6f11f7375f554091
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:07 +0000

    wall speak expression completely neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import platform
+def get_python_version():
+        return platform.python_version()
