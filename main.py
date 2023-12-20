220c6d511f9b5de444288549b3cdbe03a9e9eba0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:10 +0000

    night conduct crowded light connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
