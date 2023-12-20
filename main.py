1a0c845f7e902c6df1b8036719529e281b556399
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:45 +0000

    whoever agreement rank turnoff sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
