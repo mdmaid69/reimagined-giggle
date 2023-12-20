8864aa5e6768e4345a82934b1056b40d7fda17c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:44 +0000

    when urgent second cinema reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
