155dbc1335c1966c3b0dbefc0009d4c282bd8b90
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:09 +0000

    chief concerned opposite measure steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_acceleration(speed, time):
+        return speed / time
