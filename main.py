d0995cc87902d8f36740bc6efd7028c59fcfe205
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:24 +0000

    sing wrap face Ee close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
