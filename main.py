58451ee50db53a74005f2d421c0a650b46362f06
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:16 +0000

    stupid sharp borrow translate fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
