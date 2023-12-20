dca5c600c7ff77a764e74d9a58f7feea47c7b527
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:26 +0000

    everyone turn once thing anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_power(work, time):
+        return work / time
