630feaf89aca8a89dd2c81bf2e9fc948513477eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:26 +0000

    well stop sail warm past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
