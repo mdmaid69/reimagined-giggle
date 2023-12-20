e57c4766764952325a1143ea7f915b448b5f01bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:35 +0000

    tradition turnout possession week switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_set(array):
+        return set(array)
