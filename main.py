3787cd046eaffe2e9ab712557c270ba76a342dba
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:51:43 +0000

    cut hold prize uncertain national

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
