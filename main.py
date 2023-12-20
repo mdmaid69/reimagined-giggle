acee51c1b927cc9979ff3f3fbab7f0cd7cf62b3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:46 +0000

    emotion fear organize Tt inside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
