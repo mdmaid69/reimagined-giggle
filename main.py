b038410bc35b060283b460737898346395253487
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:08 +0000

    possession shrink brush wide sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
