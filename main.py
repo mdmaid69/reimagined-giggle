f54892805fbe1bfd2459584429a06aba8f576468
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:51:11 +0000

    so payment taste represent ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
