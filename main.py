59da98d7401622e76c4ef1ec1934803dff836d36
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:12 +0000

    compare must Ss music feed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_density(mass, volume):
-        return mass / volume
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
