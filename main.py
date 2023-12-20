f538c11d66c9170535c8389cb09197a237083b2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:38 +0000

    road rest train spot rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_item(array, i):
+        return array[i]
+import sys
+def exit_program():
+        sys.exit()
