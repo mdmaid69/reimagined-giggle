21e77cf57346cc58aacd4fc6699a49e1e854269e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:06:52 +0000

    bury importance become flow except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_item_count(array, item):
+        return array.count(item)
