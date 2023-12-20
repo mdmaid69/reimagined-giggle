7719c2ce3dc9d6e062c2580cc1b6f5385211c8c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:41 +0000

    conduct more copy run scrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_pressure(force, area):
+        return force / area
