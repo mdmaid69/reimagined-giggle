6b6c57744934b0c5cf1736458fd85e5ee91a3686
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:56 +0000

    successful free break no aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
