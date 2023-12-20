6437754f8b0597d6c9bd69773e176009743ebf31
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:44:12 +0000

    arrive home suit step admit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import platform
+def get_python_version():
+        return platform.python_version()
