906aaeb8540c8c09d70b540ffb7edb3dcd213541
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:30:43 +0000

    shall side slice famous disgusting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-name = "Python"
-print("Hello,", name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
