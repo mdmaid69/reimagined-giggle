098097f59f33d66fc231d4ca3e3b079100840199
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:43 +0000

    copy scrape body minute long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
