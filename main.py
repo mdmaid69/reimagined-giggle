5319ff8ccb403831a6cea711e12ac8c1d3240651
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:38 +0000

    respect turnback comment skirt major

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
