5b33fc0f7acf54e664797f437c9d01758a1b7cb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:05 +0000

    discover sure room frame but

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
