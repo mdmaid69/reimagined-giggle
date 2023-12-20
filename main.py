afcdd5188fdb053f0e2a39f7ead88f81b438f771
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:19 +0000

    front separate cruelty vision Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
