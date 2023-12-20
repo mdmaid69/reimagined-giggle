48885aba36d4b2859ee19f3ae7b06f20e2dca6d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:48:46 +0000

    range middle means fetch watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
