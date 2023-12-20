b495691dd546945f55b7cc1029dc3e72c9ce6a8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:55 +0000

    provide male boat wander nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
