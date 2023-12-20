199733cea2de09f61692a984d8576d883a85d79c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:49 +0000

    obvious brush human order reach

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
