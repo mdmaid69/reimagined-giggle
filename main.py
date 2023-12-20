7485f7daa8bfaf02db0f34096a413a56ec5af95f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:53 +0000

    explain minor insist concentration ours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+print(os.getcwd())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
