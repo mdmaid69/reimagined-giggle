67f97dad5915d31f4625ade519bc42750021cfc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:54 +0000

    performance comeout pure nerve behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
