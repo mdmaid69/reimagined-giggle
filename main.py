48a939d0132ad79ae026ad235a91f9255491d33d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:59:35 +0000

    draw anyone conduct second interview

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_speed(distance, time):
-        return distance / time
+def cube_number(x):
+        return x**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
