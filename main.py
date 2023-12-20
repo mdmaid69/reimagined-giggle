9c75430860c75d879ad41517f107d9fbde2cfb17
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:44 +0000

    might closely certainly stick sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
