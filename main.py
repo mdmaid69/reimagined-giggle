f4c43705562ad1670e503dee272d0eea4ff7e0bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:00 +0000

    else answer brave skin excuse

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
