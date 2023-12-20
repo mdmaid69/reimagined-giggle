6c341d422d447a95daa2e3e1b8833236574bdcfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:02 +0000

    danger curious sweep except once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
