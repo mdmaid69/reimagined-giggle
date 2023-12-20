63b9d34f74dbcafb6a2e7a74a4d9b159674dde20
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:23 +0000

    frightened fond clean still ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
