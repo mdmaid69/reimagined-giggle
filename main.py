6be4fa30f0db8922e70c0f10a79e5b6f7f9413cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:01 +0000

    wake yet think silent wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
