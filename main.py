626bb56396a421af74668dc83d28ea9511911753
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:09 +0000

    judge giveback arm plain amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
