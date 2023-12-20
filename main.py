f9505426e5392a4e65b1487d6817222b8fa77ec5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:31:01 +0000

    bother path loud company balance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
