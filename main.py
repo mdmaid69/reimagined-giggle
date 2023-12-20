dfd8783532ad17cdb7aa308ef83070097d943000
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:24:15 +0000

    cough fine programme fail bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_rectangle(l, w):
+        return l * w
