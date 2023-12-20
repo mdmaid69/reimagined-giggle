0aad2853455d4789acfb059b1b49ae02be83c0b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:02 +0000

    afternoon available right disappear sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
