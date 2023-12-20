4d9bde40c7dd0e553d595badb40d0dd50b14e1aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:22:18 +0000

    himself city foot light needle

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
