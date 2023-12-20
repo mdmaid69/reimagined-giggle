39ff873fff832c910e9481185a6dde811db27813
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:06:36 +0000

    mainly all Uu 570enjoyable whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
