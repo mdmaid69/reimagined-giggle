04c4bb89d568eb6a08707dbf42e585bd6b3fe074
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:13:41 +0000

    giveup judge flow jump never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
