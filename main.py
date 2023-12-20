c034bf174894c436b784ff89fc25dae2d6e73348
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:19 +0000

    crisis so prepare here Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
