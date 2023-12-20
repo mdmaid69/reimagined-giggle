a732f15e392ab3dc220a2e432e79332dd09c2c0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:22 +0000

    spin rub arrangement already apart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
