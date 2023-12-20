db9362ecb8121a6d09c3140d75a41e3b385ec84c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:43 +0000

    argument dark unpleasant demand goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
