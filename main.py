081fb41cda2901798880ff2aad80b06aa63f6d8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:37 +0000

    doctor dick sad list justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def reverse_array(array):
+        array.reverse()
