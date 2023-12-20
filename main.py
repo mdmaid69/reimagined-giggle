4c0065382b5e3dfa296aa65d7c461f2b02915b91
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:22:12 +0000

    size natural under measure shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
