0717d6d6586922929d83bbdb20debf21a8ec9b1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:13 +0000

    develop hang closely platform stand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
