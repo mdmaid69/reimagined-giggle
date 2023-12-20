fae6df11d1c23e2c5f35479c2cfcc4cbd2ddfd45
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:05 +0000

    possession sky high sight workon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
