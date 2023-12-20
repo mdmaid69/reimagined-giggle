d0fd09fa12b872f49b2e7d1a5a51a6e128323473
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:32:45 +0000

    bye cost miss sore half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
