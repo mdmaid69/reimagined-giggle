0af3f3e942b7f9d8a1b5b50a6b5ff7499f71688d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:17 +0000

    wake interview fall summer afford

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
