026ce402941d12a62fe5ee5f119ffb1091df8292
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:05 +0000

    yourself join left cheap famous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def convert_to_octal(n):
-        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
