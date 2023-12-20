6d9e909491352cc245bcef532388e7b992a82e06
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:05:44 +0000

    region platform friendly honest minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
