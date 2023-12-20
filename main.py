fa45bf1dc9d098a994cad505a7d38bbfb253e602
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:01 +0000

    mention geton exploration wing even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
