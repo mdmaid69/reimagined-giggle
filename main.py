3cdf289a84fdf1ab3bfe163f78a9b1e85923d84b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:18 +0000

    feature best objection dead calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
