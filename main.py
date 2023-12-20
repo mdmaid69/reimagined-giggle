08ef4e98aafa0bd83e63db6c55984039609b8ca7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:46 +0000

    noone wash hope taste there

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print("Hello, world!")
