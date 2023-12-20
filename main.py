a312b26fcfe455a4a6b47c0fbdcdabd7e0bf5e73
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:54 +0000

    Oo arrive limit drink Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import platform
+def get_os_info():
+        return platform.uname()
