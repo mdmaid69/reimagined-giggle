b33b23438595945489aca6f311598d4fb9bd2a5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:10 +0000

    past mass sport tradition learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
