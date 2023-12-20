41b93006df87541548b3ebccc0f979545c3eb242
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:30 +0000

    prepare trade seem blind base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
