7905e1e57c825ca31b485cc8d3f7fa079b00572d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:28 +0000

    pull mouth rule ticket love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_buffer_info(array):
+        return array.buffer_info()
