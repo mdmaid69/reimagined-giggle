a645f6e856bd3360fc4a93d84f596299392f4d2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:26 +0000

    straight in exit hardly from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print([x**2 for x in range(10)])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
