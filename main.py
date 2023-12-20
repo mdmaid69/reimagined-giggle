43232f6848ca52e05843126571ec8d378c28e704
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:54:04 +0000

    land negative machine also bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import re
+print(re.match("h.*o", "hello world"))
