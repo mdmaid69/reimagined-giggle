e7f827df74c471dabe519b811a17171d8ec61d02
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:01 +0000

    neck makeup file bottom delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
