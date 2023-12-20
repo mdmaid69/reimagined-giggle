e47328c4a10d2368d23a9ddf552d633509902d91
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:02 +0000

    burst slice partner thorough discuss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
