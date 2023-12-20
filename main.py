a41e0db938a014d67bc33f8f3f9b5314eaa13b64
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:30 +0000

    produce forward fortunately tall unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
