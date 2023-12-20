6fed2e4d0219c528de48543b01ccf885ba158a45
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:01 +0000

    prize wave store swing right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
