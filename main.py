8dfb00d9fd56daae600bc57824c7fb97f5d9517c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:22 +0000

    right weigh neck gather minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
