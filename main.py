aa032e9abadc41a7984a8ef57e5b6022df624fe9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:27 +0000

    unkind glad fit afford be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
