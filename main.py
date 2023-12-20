bac0b6818fbef68ce80f3b70eec836d36c656222
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:49 +0000

    final there plenty mouth short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
