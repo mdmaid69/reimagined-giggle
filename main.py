067956b77d4337413a2fa22731508bce49ee8f26
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:22 +0000

    rule mixture upset deep polite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
