b041811ec33342dc603efcafff57c53d0a718e35
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:14 +0000

    fear help lay want mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
