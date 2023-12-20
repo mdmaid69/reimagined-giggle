8470275839ea2944e7c4f4f94f6621200de86783
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:49 +0000

    somebody sorry store rain temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
