6981db1df47227d80f9132d919de37f5df1cfe0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:23:12 +0000

    consist problem record take themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
