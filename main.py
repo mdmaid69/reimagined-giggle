8565694b8362cd55f79bc14efa90d49629a1dbb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:35 +0000

    evening stamp contribute brave interrupt

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
