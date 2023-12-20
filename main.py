ff53018b0613d508293f5c42dade6437d93232e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:32 +0000

    scheme further forward beneath comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
