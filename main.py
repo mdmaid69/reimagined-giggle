93e694e789da2739e1e717f3c48a96b65603605a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:37 +0000

    rob light pride smooth better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
