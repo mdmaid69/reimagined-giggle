93e2821e7a570b75b2b702e81a91a91784e82539
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:28 +0000

    whenever question start song unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
