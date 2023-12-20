97b9f2ca25a9e559ea1303a5dd97fa392130f605
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:35 +0000

    practice refuse day dare face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
