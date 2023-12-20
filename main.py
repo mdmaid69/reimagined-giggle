234e9c12c975412cbc81a40106c8ce4c22ddbaef
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:32 +0000

    drink blame likely go speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+text = "Hello, world!"
+print("Characters:", len(text))
