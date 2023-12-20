d97016ee10835d43e3c3b53834ce9f59862c33a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:29 +0000

    rarely beat conversation chase Pp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
