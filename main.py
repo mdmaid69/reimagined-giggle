7aa3c63df34a194226c6aeac5fd47f5824fd2ffc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:27 +0000

    tap floor brave gift change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
