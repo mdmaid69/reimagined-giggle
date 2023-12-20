fa1b1317907e267d3e9fdd7259afe7eb0f0d4f03
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:32 +0000

    sticky tired uncertain cheque used

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
