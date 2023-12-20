e24929e79a1422ce952ee47cb7c513378583d355
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:39:22 +0000

    challenge lesson open section will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_min(lst):
+        return min(lst)
