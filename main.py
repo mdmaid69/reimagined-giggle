2b38c019b562b78057fe01edc8176dd5dc238eab
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:30 +0000

    plant bound dull judge polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
