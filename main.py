1cebadf2e1c2431d6cde877db236f1d31cb38060
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:34:07 +0000

    judge score sink contrast runout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def reverse_list(lst):
+        return lst[::-1]
