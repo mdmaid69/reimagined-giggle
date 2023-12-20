b774d7ac811afd85110cc2196ef59039154bc292
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:10 +0000

    hold guide most buy pronunciation

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
