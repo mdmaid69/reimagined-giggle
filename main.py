7f3dab6c7b3ed23553f12910ecbbe8f8e538213e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:58 +0000

    visit deliberately period joke Zz

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
