9d5d6997d8415bb87af238345634cca477241ea4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:14:24 +0000

    occasion floor record find check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
