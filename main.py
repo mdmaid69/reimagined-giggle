25da7d2b383b9d504e254fae55c86d6aeeb91e73
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:56:29 +0000

    freeze by case contrast throw

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def multiply_numbers(x, y):
+        return x * y
