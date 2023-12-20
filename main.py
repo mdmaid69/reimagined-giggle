e8d23913f712db7a19bd152639039aaca6ec0486
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:10 +0000

    female explanation excellent admit we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
