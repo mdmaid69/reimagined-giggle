66a36cc5ac755fc3deb471a68ad3dbb3c7e8bc92
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:22 +0000

    home when public generally programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
