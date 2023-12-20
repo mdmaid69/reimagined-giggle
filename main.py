7f9f4ae1a83c0613fd006a3bf7ddb49c66f7c3bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:09:55 +0000

    example Cc opportunity past reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
