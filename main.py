aafc5d775a603c7b947052e6195664affeaf9f52
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:50 +0000

    score kill discussion record relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
