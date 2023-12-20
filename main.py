3a107a23ea1467c24d8f7c0e6e6eca3dadb4c1b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:38 +0000

    Dd perfect accident immediate all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
