a4893c7473fa5124ab472a59ab07f0ed1ec51f57
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:31 +0000

    runout passenger help personal window

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
