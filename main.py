df7c8b3db640c761a537970e3fff73bec6e5747c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:19 +0000

    welcome identify scrap contribute away

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
