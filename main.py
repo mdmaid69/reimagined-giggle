169665c00a6c4127d6bf696174d106e05f07bff8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:17 +0000

    kind positive news soft 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
