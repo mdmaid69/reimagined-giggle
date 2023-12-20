0e7bf9bea3cfc792e367385fa9dc348148d12a58
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:04 +0000

    squeeze factory apparently clean now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
