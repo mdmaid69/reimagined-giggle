a2f28cdda3840d20c2e5ac8208e16245cbf14b66
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:33 +0000

    pause agree think pinch whichever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
