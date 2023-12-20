88e3aaa4b5d6c2ffc5042cca164edac8a5687b5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:38 +0000

    would proper although lie square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
