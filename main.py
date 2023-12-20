e230544f6b2d5ac20b616ff045cfd91e7f9671a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:09:14 +0000

    university whatever involve terrible murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_speed(distance, time):
+        return distance / time
