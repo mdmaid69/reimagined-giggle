c1e5c5f729fe7f4026a703a264806a2e52a7c461
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:31 +0000

    talkinto convincing bit practical mix

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
