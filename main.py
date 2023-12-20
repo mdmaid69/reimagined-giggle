b9d17a3880a68297aecb9b4ba01edda7ed6e92ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:16 +0000

    approve callat border positive someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
