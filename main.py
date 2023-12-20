13c84ebb956b3220ba337ccc9388ba6ea3039747
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:15 +0000

    callback wrap right each become

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
