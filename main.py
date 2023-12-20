e67069236ebd6c82b7b84a785cb63012321bfb65
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:40 +0000

    ask autumn unfortunate born arrival

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
