6d3ae9477c9da683f2669ef577015a934dbf162d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:13 +0000

    idea message sore future succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
