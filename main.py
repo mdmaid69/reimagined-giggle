6380f2bfd71b54325d920631476b31715f4890ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:00:08 +0000

    final workupto table throughout different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
