9cb639de18c824480ce095117ecfb06afb8b4db7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:15 +0000

    round crowd trouble tell study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
