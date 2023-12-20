97872699d8cfcb9425197e3c850b1d3fd40b26dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:27 +0000

    half software afford public land

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
