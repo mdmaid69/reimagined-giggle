3d21cb2f205f83f4fd423c78c5fd354073f699b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:22 +0000

    priority 179bite government design learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
