928ee15d956d4f55bbfd7c035ded5fea846c31eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:37 +0000

    slice drive defend chat bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
