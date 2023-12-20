0e1b7df42a6ee64e4dd4fc11a0384461c5df6e5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:53:40 +0000

    answer full forgive minor deliberate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
