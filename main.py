b2d2f3c63bdb348ccbe91057bc739aa64b598bff
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:15:42 +0000

    shop brush extremely bear bank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
