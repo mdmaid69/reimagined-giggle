4493eed9acb0dab034691a3ef660660b2657ee3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:31:59 +0000

    there shape light trousers float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
