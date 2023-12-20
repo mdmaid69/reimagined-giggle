7e3c762ed09af7c80d02fb1dfa993efeb429caa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:57 +0000

    branch furniture complaint so dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
