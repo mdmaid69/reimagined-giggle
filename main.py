c80125739b77d2e8cedb8b7386dfa9f26c4190b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:07 +0000

    happen once breakdown blame communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
