e0c799a07acc4bf3736caf3e6a5666b67d77e96d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:28 +0000

    target market pleasure time wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
