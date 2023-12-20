17ea2e75f0f859af85d3f0fbb0a436b4a76daf5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:15 +0000

    represent level experiment surface passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
