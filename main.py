26a6ed3b7a9b7405517e0880ee28484e11bcf07a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:10 +0000

    rub minute future welcome row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import time
-def get_current_time():
-        return time.time()
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
