3423568d899a0554846da61afbc4557e85a163b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:14:35 +0000

    failure evening path giveout block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_str(array):
+        return str(array)
