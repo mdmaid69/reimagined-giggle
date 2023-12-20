f3d6d1c0492a56bb81a20e223569d54e2651dc95
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:25 +0000

    risk voice part bin apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
