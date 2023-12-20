1842998246f05a0e9927784a30cf00bcbc7a13f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:09 +0000

    decision whom workout bringup spring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
