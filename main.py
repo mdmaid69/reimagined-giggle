b55c5e1bf5e760093678cb236a1dc11e8d4ed2bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:14:39 +0000

    especially it block degree goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_odd(n):
+        return n % 2 != 0
