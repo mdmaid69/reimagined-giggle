bd5be74b2bd8dd0887f3859c51e3c28ca9d58381
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:39:48 +0000

    goback(to) rescue prize information top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
