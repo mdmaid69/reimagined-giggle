ac5518d250000b5dbd8f12885ea082d6acec077c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:50:34 +0000

    after substance nigga herself right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
