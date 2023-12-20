b9a6d1fa523bb2c773fbcf842dd4a37e2efc6f7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:00:22 +0000

    but criticize public touch the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
