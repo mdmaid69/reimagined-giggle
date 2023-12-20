dad6604900be5a12be5c3892b43713366e425827
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:19 +0000

    fond like direction eye compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def exit_program():
+        sys.exit()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
