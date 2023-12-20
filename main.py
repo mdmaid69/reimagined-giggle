ccd1c18d1f3d787a54220831e580a18b30a60a81
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:23 +0000

    he speak seriously discuss intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
