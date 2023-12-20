34be61bf6d142c05046f142d0b6770febbe65c95
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:25 +0000

    getup ear mind doctor choice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
