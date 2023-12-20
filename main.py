48000bec13b5803ac9dbf474ecba5acbc55c403d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:06:27 +0000

    tool stick tour start hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
