631b29d09e6faa79e969dc82720560cde0f54c5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:33 +0000

    division command crash board large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
