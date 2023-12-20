0e8056841eddb8a91ec4952ed5e3fac3f59466bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:11 +0000

    sink holiday above strict work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_work(force, distance):
+        return force * distance
