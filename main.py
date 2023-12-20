e7c65727dca2f07789ad1a9428a1a14a71dc2d3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:03:31 +0000

    getback suffer amount anywhere keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
