6908b13618426241b8c652b19304647bab516d02
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:48 +0000

    round corner wrap surprising build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
