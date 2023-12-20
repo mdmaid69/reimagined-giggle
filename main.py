2af3f5699fb79c531a120e81ce7bb78a2ac755e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:54 +0000

    decide next next whistle communicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
