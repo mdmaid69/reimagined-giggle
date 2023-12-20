1815a80e46041cd112a608a7a95c2105916d0bd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:55:22 +0000

    wonder finally hurt turnon comefrom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
