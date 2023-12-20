78ce8096e80714ad41430c6cdd6b9229346ad579
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:38 +0000

    convincing meaning pause prevent field

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
