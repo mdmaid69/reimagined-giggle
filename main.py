2f4935b4297c572b5265e9bbdeea8e680e02fe27
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:41 +0000

    why course goout though rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
