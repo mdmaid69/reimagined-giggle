55e88359a3986d2563b2060961e3a0841d231c3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:59 +0000

    suggestion affect for cap phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import numpy as np
-print(np.array([1, 2, 3]))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
