2a86aeb0da2b3d3ce3952a016de4bbb628f0afd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:50 +0000

    settle concerning coffee turn natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
