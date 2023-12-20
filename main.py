dedcb81a9385abaf7633591d5884d4e37728fadc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:29 +0000

    no perform sight note reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
