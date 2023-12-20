cf75b625df79d49777299b68a16de1e6575ca65c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:10 +0000

    steam reach concerning anybody view

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
