55ae3903a61fd4a82c8fdf67297f06215924c4e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:02 +0000

    wrap reliable turnout gentle area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
