31b6984bb230f5466070753c3ccc252e49d93aac
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:07 +0000

    wake border divide command hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
