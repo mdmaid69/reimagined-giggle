96db2c20c8f4914dcea912d8b553df8329f785b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:39 +0000

    blow task behind smooth he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
