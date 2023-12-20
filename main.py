ee2fab9112bc4e193b025285b6f53819fbaafbe0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:54 +0000

    lock realize slip film correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
