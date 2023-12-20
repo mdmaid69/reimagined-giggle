c29eb246ed15e27c19da16e3329d1c473746ea94
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:01 +0000

    keep interesting guard pleased long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
