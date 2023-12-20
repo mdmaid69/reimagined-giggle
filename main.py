ca36be505e7090572b865adc2f79af72edfc0ccd
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:15 +0000

    border that pump as bread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
