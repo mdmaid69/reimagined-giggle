eabc022d1c1e1e9383e803bb5898262ece0c3b6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:41 +0000

    everything circumstance reply tie who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
