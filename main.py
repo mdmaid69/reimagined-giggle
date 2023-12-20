914b1f15dc925471f1441a791ef37d220b3c776f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:38 +0000

    machine preparation private by base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
