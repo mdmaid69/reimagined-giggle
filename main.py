ba00334c3a355e6b67c63fc19de9c873b43dc603
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:08 +0000

    original open other cruelty exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
