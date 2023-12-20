dd190bec44c92002de21b339a8393db9069e52ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:42 +0000

    injury identify place temperature rescue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
