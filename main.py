3dbb8dd7bd8263144de162076d8bedcbf3c14968
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:51 +0000

    hardly programme Kk fast race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
