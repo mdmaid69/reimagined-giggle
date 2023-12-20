1741e3c9a8452fc1d1cad4de9c0dc081ee3b3ffa
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:36 +0000

    nervous abroad feed top shoulder

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_area(radius):
+        return 3.14 * radius * radius
