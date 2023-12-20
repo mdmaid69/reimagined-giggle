b47ef4f5a5e66a69e856fed4953d62e3dc66ed1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:09 +0000

    circle with match unlikely compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
