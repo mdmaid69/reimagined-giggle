d0e704f11fee8e35810d27277ca2d959619d2462
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:23 +0000

    advise best stick custom crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_octal(n):
+        return oct(n)
