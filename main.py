4a2d02292a0273ec8c9c7eaafbd8fd4c2cb1b2e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:45 +0000

    relief concern gentle warm discuss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
