006af505e8320f143b4cff00820ac0914282d6e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:44 +0000

    hello support instead load sick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
