c025e305e4f156d8e9b6ba135e949765ca609a3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:22 +0000

    Zz downstairs list until meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
