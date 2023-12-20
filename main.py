a5ae4c7d1948ff63b48df994a9197744f1d0a216
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:52 +0000

    limit burst only shine quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
