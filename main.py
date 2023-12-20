8069c50c8436606982ab3943ca0293b2fe24b4f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:40 +0000

    bridge ahead plan road communicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
