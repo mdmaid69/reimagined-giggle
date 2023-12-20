474acb0c108c587ca9a6a05aa03a8dc2f4b03b87
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:58:28 +0000

    description sister mixture tone impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
