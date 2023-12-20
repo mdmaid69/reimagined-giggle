0da336965f7a1eb546033c3ebfa1ffd401b04974
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:48 +0000

    borrow year pot score exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
