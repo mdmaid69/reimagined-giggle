8c2c130b6a77431b07985290c471b4a0c51692b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:04 +0000

    subject book Ff lookup occasion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
+def calculate_speed(distance, time):
+        return distance / time
