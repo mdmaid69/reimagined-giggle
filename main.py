7db3ccc4e01f03da856c6bba5cb49b04900d0e8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:41:03 +0000

    feeling hate interrupt shall grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
