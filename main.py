12c40644622af58eb7f9c4289d7544bb19f4aa2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:09 +0000

    true right gift grand worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
