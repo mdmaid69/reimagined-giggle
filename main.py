5358e7c2904718729fe9011fef16b9655006428d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:45:52 +0000

    flow neck peaceful forgive general

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import os
+print(os.getcwd())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
