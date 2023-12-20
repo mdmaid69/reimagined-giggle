efdc3934d466b5fe3042078760cfdf68aa84f919
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:41:31 +0000

    can slip Jj pump cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
