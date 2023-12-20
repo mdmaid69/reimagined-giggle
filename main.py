e65908be05529288422e3bb4bdbada756f99d4a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:05 +0000

    material direct film ourselves broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
