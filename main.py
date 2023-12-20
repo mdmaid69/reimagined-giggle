7fa4a97553964d05a3a471f11f680cc824da4a47
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:48 +0000

    getup mine sale back finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
