8b50776950744ad9c2fe79c86bc26e5c27a570df
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:20:42 +0000

    breakup true dish unpleasant after

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import json
 def load_json(filename):
         with open(filename, "r") as f:
         return json.load(f)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
