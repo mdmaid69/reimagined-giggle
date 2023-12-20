9166462eeb59d7730f7f67405296f34fb74d80e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:51 +0000

    new trust you their take

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
