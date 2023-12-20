08fb963d3380fd7524569142dcd6d5d29ca81846
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:38:16 +0000

    suppose about lock black sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
