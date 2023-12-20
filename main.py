4d635fc679ee954685932d8a5098acf7a32b94c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:37 +0000

    drawing price laugh tax entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
