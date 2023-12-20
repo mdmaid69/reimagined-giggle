73394e9d48544241247bfc919d049ddff88407ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:56 +0000

    nor regional apply trouble stiff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def cube_number(x):
-        return x**3
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
