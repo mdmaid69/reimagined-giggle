d4ea19b9e7f1152fcb2ece5733ed369573873033
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:41:43 +0000

    back challenge movement pupil language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_stack():
-        return collections.deque()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def square_number(x):
+        return x**2
