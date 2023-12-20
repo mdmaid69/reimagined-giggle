e548dd630d085511c950f94c7ca831cf05e94ae4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:16 +0000

    grow insist welcome brain study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
