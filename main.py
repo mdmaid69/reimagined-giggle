2e597f65007762c75e11279b941db09f8cb3f79c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:45 +0000

    remember communicate event generally record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
