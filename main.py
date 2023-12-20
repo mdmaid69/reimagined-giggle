a3f51d79ca05f38b202ada4d9bc0bae894c8874c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:04 +0000

    through open quarter whisper tip

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def sort_list(lst):
+        return sorted(lst)
+print("Hello, world!")
