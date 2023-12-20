9c52f869a7e037105675e57ace5363c2003d2673
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:35 +0000

    collect born object welcome surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
