dcab29a406e641fd95dedd1699d59f7d51c51c66
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:23 +0000

    solve slowly false arrive law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
