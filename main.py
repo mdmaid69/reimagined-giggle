2768880f9e03b7aa2333c0168ddc592d3a954c96
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:34 +0000

    step Ss closely comefrom talkback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_acceleration(speed, time):
+        return speed / time
