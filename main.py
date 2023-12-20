673bb94859a9999a4e69f3dd4810a9fa2f5f0e2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:42:26 +0000

    careless over smile father rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
