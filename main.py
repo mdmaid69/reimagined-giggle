7068e85a5331b8d217716e321a0001f44e21fe06
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:55 +0000

    command emergency risk store us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
