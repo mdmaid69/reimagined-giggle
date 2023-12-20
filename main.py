73ca40e45974e6e483c09722b853861bd9271460
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:31:34 +0000

    fresh loss light raise Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
