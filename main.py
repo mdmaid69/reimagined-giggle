66b72fe54f2c466d986f1abf72afc6cdbdebd3eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:09 +0000

    takeaway file right lunch surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
