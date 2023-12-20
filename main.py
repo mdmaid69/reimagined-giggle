683f10b667675976050741a5d75f55fabbdc6540
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:57 +0000

    fat hour objection reply minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def convert_to_binary(n):
+        return bin(n)
