01ab5093e4fb601efd1e96c79612f9446cc5ce20
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:18 +0000

    light slight step feeling exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
