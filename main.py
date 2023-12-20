416cd3dcde8305f7df56de5f777e79c8a912c863
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:19 +0000

    spare meal thought achieve reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
