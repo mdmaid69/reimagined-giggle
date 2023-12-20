ce1b965089d316308b4e3e3f76a140cb3a7aa3bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:28 +0000

    smart Cc spread constantly science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
