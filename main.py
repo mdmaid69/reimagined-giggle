4fd183945809fbff7b5b1fb794eba82464f8efb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:24 +0000

    modern since improve bringback less

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_length(array):
-        return len(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
