5da6f6bab49df9dc375b416c794f41036a72bfab
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:05 +0000

    majority loss comefrom recognize shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_set(array):
-        return set(array)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
