d5b37407b88ef773bfe4eb45abb24dd035c52ee8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:07 +0000

    before news how autumn them

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
