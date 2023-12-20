e48e36c73410c1f318d9d128f37e5c4edb54b758
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:49 +0000

    touch shrink beneath little reaction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+  def convert_to_hex(n):
+        return hex(n)
