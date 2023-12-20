c33e0525d1cc9dd45665b0354f149b583e435a63
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:20 +0000

    pattern lay house comeup say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def is_even(n):
+        return n % 2 == 0
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
