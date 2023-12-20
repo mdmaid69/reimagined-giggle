0cdff95ed55663aac013d41d63a22894b8b2bc99
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:17 +0000

    chance comeback suddenly date available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
