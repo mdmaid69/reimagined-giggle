62751e111a0d17546ab1985ee5aa2ff81611e5c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:10 +0000

    movement ride wild behave second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
