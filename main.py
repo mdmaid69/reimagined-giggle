4e199212e07710ebd159e0ca1ff61e3a7624f05e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:11 +0000

    soak everyone needle unit can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
