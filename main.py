413b79c42e100680098aa4a491a9ca7dd4f58d90
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:44 +0000

    check means after sale upset

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import sys
+  def get_python_version():
+        return sys.version
