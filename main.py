aa28dcc3762aca33facf6cd484896f2ab110571f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:05 +0000

    rub odd argument across hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def add_numbers(x, y):
-        return x + y
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
