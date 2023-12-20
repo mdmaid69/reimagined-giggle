f065d421c773b26e6a1f99dc0523fc43c26ed87a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:03 +0000

    lookafter drink computer mind use

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
