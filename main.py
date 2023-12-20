02c8d078b4e969b8f74e5d2e385818ab7308ee0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:06 +0000

    profit doctor smile brush clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
