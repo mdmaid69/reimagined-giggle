1593e0f4d1ca83572fe30669ac79e437d893ba5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:09 +0000

    doctor clever refuse woman direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
