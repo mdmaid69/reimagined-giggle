fefdd3b45b90357252f65c2578456969e7c27676
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:23 +0000

    yourself interruption dull any fix

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
