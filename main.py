754cd9f00311871173e74eb7a7f6c391a324b00e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:37 +0000

    impression slice hard tap slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
