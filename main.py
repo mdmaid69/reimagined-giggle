c756623aba3146ae918f05c7c2147ad3ea4b9262
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:50 +0000

    turnaway string action card turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def reverse_list(lst):
+        return lst[::-1]
