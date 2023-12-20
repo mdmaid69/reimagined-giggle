34d9356727de6ed138ba1d3dc856c93046fcc762
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:06:09 +0000

    disapprove runaway wind raise uncertain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
