6fcc2714b6f41447db56ca60201ff3eb31d4529f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:03:03 +0000

    anywayanyhow smile clean escape turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
