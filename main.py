9755c0476d0bc2472bd74f6449b26d9bbc06155a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:50 +0000

    system press impatient government case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-print(math.pi)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
