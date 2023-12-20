b9b2bc884a18aa16cce0f22f0fa1cf2acaedce33
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:49 +0000

    shape play surprise wrap clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def exit_program():
+        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
