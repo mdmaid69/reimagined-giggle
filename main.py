a5987782f01ab9040ddb616eb3a7ac6fe3e6e2bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:30 +0000

    unpleasant produce mysterious heavy here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
