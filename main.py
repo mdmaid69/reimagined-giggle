474dc4ca213fb6ad16362cf3f5c99f37278f25cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:08 +0000

    announce trial switch miss cheat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-print("Hello, world!")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
