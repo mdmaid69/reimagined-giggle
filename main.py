2151011873a767ba4db7dde85091baf20f05250a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:07 +0000

    shut callout include release fork

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+print(sum(range(10)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
