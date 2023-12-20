be0fcdabcd36cc7fc6bdea2e2a9ade1b740c5b22
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:32 +0000

    tomorrow sting back art duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
