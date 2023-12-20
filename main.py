e60ef4ed007c95f35419e93d8ab848f21f0e627b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:35 +0000

    pressure interest speed autumn coffee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
