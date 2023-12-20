157cfe06f319dd858ef8a01713e8ff4a79c31492
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:29 +0000

    mine worry fine separate arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_even(n):
+        return n % 2 == 0
