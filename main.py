53c8e0fcf8fedcbb997ce0fb7072ace3df398774
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:06:13 +0000

    history length accordingto goout characteristic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def reverse_list(lst):
+        return lst[::-1]
+  def sort_list(lst):
+        return sorted(lst)
