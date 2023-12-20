b4f843646de0be9ba7f698cbd1813a810248abe8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:16 +0000

    debt keepout side Qq in

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
