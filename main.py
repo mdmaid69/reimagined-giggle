fe17bd04eec15baec33cd7cb88753aaf54fb81ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:53 +0000

    scheme certainly mood flood contact

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
