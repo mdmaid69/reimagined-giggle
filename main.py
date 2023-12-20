f5163c918e3447faba053d8adf04d4cb51fc6782
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:06:19 +0000

    extra little middle storm only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
