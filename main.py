1ff840cf50a014bc029db7143b6401c3e730c1fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:25 +0000

    impressive yourself mad under doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
