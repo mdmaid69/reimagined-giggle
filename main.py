6a99a360276e42060a9766d74885e6f909cc5134
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:54 +0000

    roll wild block back create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
