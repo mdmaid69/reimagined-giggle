8f22f9c3e96597ffcd5bdf8ecb87efaa6be576de
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:46 +0000

    rain back pure include close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
