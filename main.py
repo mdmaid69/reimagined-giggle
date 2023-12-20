690fafe10e00a1e84f81861def44dc61a2cc06f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:53 +0000

    matter decision long tend undo

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
