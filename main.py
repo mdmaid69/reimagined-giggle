3a287df9178240c4afe999edb76a23621000a288
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:19 +0000

    side detail wire formal woman

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
