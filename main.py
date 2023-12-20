88fd9e310e5d97d64b704014cd0bb0ca5992cff1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:50:22 +0000

    tear careful every our notice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
