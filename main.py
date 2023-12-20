ebc27bd11ff94e945b0bb88503943063cf1bff10
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:20 +0000

    physical schedule their teach like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
