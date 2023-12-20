1b3b545812cab1da0ab1452d176b0f3a07b68e88
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:03:23 +0000

    guess piece imagination opportunity he

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
