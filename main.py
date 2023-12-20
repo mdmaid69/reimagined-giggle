4d60f07776264cbe052b7929a24f8ca2b3510b2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:55 +0000

    low you many future funny

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
