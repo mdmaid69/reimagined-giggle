46fc56dc180ad925fc14f21bc0bc7a3fbabe3f21
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:55 +0000

    card active lunch duty control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
