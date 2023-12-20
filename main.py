9ab7bd5aa5a0a88e2fa730fe266ad67d63761603
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:06 +0000

    university exercise late sure practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
