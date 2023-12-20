fabbc78ceb7338c4692c5eab3df70f68388a4886
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:24 +0000

    safety thankyou mainly confusing combination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+for i in range(5):
+        print(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
