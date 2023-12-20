5da5f6333142a36605a2c8fcb6ffb58e84c31a40
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:35 +0000

    along keepoff treat several work

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+from collections import Counter
+print(Counter("hello world"))
