7a7f90a0e451a57b018c59dd38d386a603c44c46
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:18 +0000

    succeed church trip clear save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
