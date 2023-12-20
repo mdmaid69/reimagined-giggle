14f74ed65416f9967219bea95e284cadaa93650f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:48 +0000

    environment Oo tough occasion cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
