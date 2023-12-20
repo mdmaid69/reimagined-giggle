bcb3bef906bd490aa24d0e5aae01631df173dfe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:34 +0000

    shake fit discuss listen move

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
