63bd26650e1f20514ec2f42af61d7828a4916cd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:48 +0000

    one characteristic shop crash search

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
