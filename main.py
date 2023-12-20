2c1ecd990efcdbedde25e6f2f2052514219aa25b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:04 +0000

    birth progress ask their exact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
