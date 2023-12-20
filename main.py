18102667a8393f3aef9f5badfa650a8cf0de6cc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:33 +0000

    store excuse mind remove piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def multiply_numbers(x, y):
-        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
