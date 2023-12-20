9fe9bf90156039ff44cfddcb0af2809d15af114a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:57 +0000

    bite constantly usually urgent recovery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
