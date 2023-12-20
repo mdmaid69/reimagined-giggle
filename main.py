04f895a38ad02c69f20ff81b6096952c9f4aef1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:32 +0000

    job crowded understand advise reach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
