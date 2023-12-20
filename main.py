bfa14162de5a19e1484c8224081db60ad0bc88cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:58 +0000

    avoid similar survive wide no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
