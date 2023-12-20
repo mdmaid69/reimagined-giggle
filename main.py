4aa516ba5103a1e9e483de51e7f4b50c6baedbed
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:41 +0000

    score your slip unkind wild

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
