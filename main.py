7c5245d53dbf4dfdd284ec80de0727725e769906
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:55:11 +0000

    half concern gate zero approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
