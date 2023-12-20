f1ef9d3db569274680bf00e4bb962319deccea9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:36:45 +0000

    talkback treat sometimes careful correct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
