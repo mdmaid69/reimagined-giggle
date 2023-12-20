cfd09d3a46ce6c1553c7db5689477507c25d64fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:21 +0000

    repair pretty sudden there building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
