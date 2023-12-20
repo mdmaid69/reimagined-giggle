e7b061262e5f94d2c5b74ca57dcae4d502cf7583
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:23 +0000

    suggestion nerve damp danger flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
