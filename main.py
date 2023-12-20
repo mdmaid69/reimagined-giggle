3c8889f0859f4cfcf913c953ac5e3abb530413c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:52:35 +0000

    afraid nigga runinto behind already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_contains_item(array, item):
+        return item in array
