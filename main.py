f99fb9b0286ec327913609389626a60504409e2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:55:21 +0000

    claim preparation agreement keen equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
