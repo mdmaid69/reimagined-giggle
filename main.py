4d6cbd3ffe39bd3976ff452f81ec93cb0f64f967
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:39 +0000

    loose trap share bus probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
