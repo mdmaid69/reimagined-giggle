7d4484569592c0bb27f6f84511e500c88519e4b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:45 +0000

    moment callout punish oil vanish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
