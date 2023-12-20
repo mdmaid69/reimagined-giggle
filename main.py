2a00bfa4f6628c2a858869b73926de965a191984
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:49:50 +0000

    extremely spill save somebody scare

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
