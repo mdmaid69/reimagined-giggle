9b424934c7b4db5d0323956433b0edf5fd455a23
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:34 +0000

    below success influence correct lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_int(array):
+        return int(array[0])
