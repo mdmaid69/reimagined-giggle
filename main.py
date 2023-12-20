78b1b6830b60243ff879fbc4178256fb8c86f02c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:03 +0000

    fix refer suddenly hate take

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
