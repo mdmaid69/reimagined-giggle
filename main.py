ff95a759136b742944c9e94260aef27dc41e19bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:21 +0000

    experience politics deaf suffer lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
