b4b2c85cadb145853a33583f9f75fabbb8149855
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:35 +0000

    pay force run explanation from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def sort_list(lst):
-        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
