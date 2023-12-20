cd30bbf35431ebe344cc901ecd2f92dee36acdbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:51:59 +0000

    formal Nn compete event activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
