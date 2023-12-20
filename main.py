615b97f2d2097fb369fd05cf10d968152e5103d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:44 +0000

    travel Pp broken care fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import tensorflow as tf
+print(tf.__version__)
