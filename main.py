44d3a7bc6531f7df151ac0d45572b9c068272270
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:15 +0000

    since drive sharp poison tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
