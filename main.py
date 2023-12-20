a1e86f949944a20cb3bf902b4106054004011bf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:43 +0000

    challenge skirt comfortable clue talkback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def reverse_array(array):
-        array.reverse()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
