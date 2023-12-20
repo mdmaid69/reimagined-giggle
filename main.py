a12c5ac398c1977a47a8f8279184550b18f5bbb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:33 +0000

    bank brush appear trap dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
