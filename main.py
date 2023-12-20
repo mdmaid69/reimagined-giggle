d1aa44404a25e63a1fdf6cffe6cea088f60d7b6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:18 +0000

    rent stay necessary funny tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
