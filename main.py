d0da7d125017c3e5d1904232ee0b9e7640b3f409
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:52 +0000

    build model deaf the watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def convert_to_hex(n):
-        return hex(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
