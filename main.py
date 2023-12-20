8c6737968421d097a27b76db7b8799c8986e28c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:21 +0000

    advise mother person visit issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-print([x**2 for x in range(10)])
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
