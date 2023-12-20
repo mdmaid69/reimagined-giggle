225c871bd042788bca54345e3f3bf280c48fa543
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:38:37 +0000

    better cause name drop excellent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
