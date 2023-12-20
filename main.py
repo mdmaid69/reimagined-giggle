ba092cdb358ee30c8731e4abb7d57845c996f1d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:39 +0000

    equipment tour after feature then

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def get_current_working_directory():
+        return os.getcwd()
