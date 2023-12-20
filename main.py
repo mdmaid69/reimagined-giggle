66caf91fbd0718d46b506c4f23915f193f8d0798
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:03 +0000

    buy suit class in manner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
