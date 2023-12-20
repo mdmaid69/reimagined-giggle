e2a2d0f962f4179c8a04e9bf851e1e3645596100
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:32:10 +0000

    confidence disagree although each slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
