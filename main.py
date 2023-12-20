649dae86340587e7017f4969c3282c87a34d0dab
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:43 +0000

    acceptable club try software Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
