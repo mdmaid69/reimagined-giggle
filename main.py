68eeb3ab35d51d430525291fafa95e3bbe9d5c0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:25 +0000

    alone pin building consider respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
