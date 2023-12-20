750de03fc1e283b0679526cb5e22a6215d33fbf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:47 +0000

    happen other any swallow get

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
