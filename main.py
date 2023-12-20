1e007c4e37872dc5b66bf4a5af22514d38072f21
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:32 +0000

    unlucky sit blow ring past

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
