f93075fe22295f5b79e79751e2369f67e41baf44
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:54 +0000

    thick he partner free aim

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
