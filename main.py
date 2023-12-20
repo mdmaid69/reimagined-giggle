9f3643f81e78f61b6a93166ae6ac0a0d07c1f872
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:06 +0000

    embarrassing about black coat mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
