e5a7bb5836c74cd0901315e30c799a376c99fee1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:02 +0000

    pleasure key cross pump check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+print([x**2 for x in range(10)])
