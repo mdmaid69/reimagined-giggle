b51006dc68e68dc4bea7893354351eb0c82d347e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:44 +0000

    mine touch turninto trade professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
