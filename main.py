5c8de5c4dafb3dfcf52cb346ede28d2ab7a4505c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:12 +0000

    concentrate bringforward ability near tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
