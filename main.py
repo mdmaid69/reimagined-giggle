2f24d54201ca0c51c9de928311507206c4428f49
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:28 +0000

    strike cry crazy travel impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
