6a012888c72c455e61b7632b1bcf86be1baf3565
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:36:13 +0000

    through occur wake arrange agree

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
