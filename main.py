eec1af71bf310f714dfdbdf163aba4a1dc021d5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:45 +0000

    comeup fight expensive indeed feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
