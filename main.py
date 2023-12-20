e2c1222afd272f26508bb5ca16dc05aecd0dc515
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:15 +0000

    show centre takeaway name worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
