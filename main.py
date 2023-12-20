f4e0df28761791faacbb56c79456636fb01156f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:48 +0000

    table together yourself sure never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
