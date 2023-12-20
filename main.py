68779e70f77f4189a3442fccf3364545078ce572
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:29 +0000

    drive fence yes across set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
