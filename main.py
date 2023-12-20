5c64e9c9b72dba2e3386f16bb2c7a874c20e02e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:42 +0000

    care edge goaway know thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
