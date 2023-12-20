4c8389a7fef9b7d27749b257a456bfe8bbabffc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:54 +0000

    live after fill stir unable

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
