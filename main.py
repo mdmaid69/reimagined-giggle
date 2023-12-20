1e5ce12165e94dc240b62badbfe7e639a1e93418
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:43:40 +0000

    lead press visit year wish

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import datetime
+print(datetime.datetime.now())
