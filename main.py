eec1a827650181b5519999a3a35bfb528933cb08
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:09 +0000

    circle illness once behave condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import sys
+def print_python_version():
+        print(sys.version)
