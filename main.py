d15fc5bb927e1e1a65e55d9cf1cd8b68840be810
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:03:44 +0000

    help vanish guide really birthday

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
