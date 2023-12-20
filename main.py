b533c0dd6ef9351d15d8ae5d71dbdaadf7d082c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:01 +0000

    inform seriously black callat roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
