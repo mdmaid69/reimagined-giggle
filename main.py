d1ce70771151509ac4bce484ea4455168b4a6e7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:41 +0000

    truth very might hear would

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
