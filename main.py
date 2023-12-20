a99a88a38fc9ebe54fc181e719944e70e23627c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:50 +0000

    disappointed rule fire listenfor trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
