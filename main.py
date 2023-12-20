f66678b3fef4d69e65483157b70c9bd5823eb572
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:24 +0000

    yours flow own balance noone

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
