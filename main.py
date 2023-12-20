f2a79932e2cac43dc9949b9c5ef72573d8e43f46
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:32 +0000

    catch boring fence considering successful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
