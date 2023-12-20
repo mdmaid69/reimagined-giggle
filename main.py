646021700b3cdd4fbb28e71cb9b281e5e88237b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:34:06 +0000

    smart once probably familiar mess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
