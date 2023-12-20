5cdd4fd1943dc9046562437f73c9749c224f8313
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:09 +0000

    sort cheat turnover history lunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def square_number(x):
+        return x**2
