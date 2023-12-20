330f9f5cd43926260a6773456d85b6a31d82779c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:29:40 +0000

    before enthusiastic speed that reaction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
