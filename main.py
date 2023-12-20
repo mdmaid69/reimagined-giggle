6e539bae473913529d0149db188fc22fe808530e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:00 +0000

    turninto run nervous evening trial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
