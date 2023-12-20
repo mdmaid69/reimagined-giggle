3f9e8b65a73e856a80a685a5d70ad84136ee471b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:09 +0000

    whole soil truth introduce music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
