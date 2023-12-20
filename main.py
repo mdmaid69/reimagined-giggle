6f0c2ef38a33d36463c70f145ce6a4c94d7935c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:43 +0000

    doubt bright smooth decide impression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
