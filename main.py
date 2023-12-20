b0c4dab7b5de4ccd308d77701d8b6b8a6ac55f3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:27:07 +0000

    content bad down change along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
