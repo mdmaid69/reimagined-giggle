40d7b99a63f12d012a7c0f4ba3002609533a3fc4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:56 +0000

    cry notice set sense include

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def exit_program():
+        sys.exit()
