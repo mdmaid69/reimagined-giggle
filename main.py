f3e62a390f115c91018e2acbfac5096e90316405
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:03 +0000

    circle port or load truth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
