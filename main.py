65bc88b06f2587de1a76bbc75bdae2bb5c07b504
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:50 +0000

    certainly brother even never act

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_current_directory():
+        return os.getcwd()
