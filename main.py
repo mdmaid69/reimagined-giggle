0c05c0109c24e2bf925337c180c79c63a378c146
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:17 +0000

    badly confident dig owe drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
