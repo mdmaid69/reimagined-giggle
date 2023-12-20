cbfb9bdd8d4f1d8113855c425316891a4cdb1cd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:56 +0000

    main progress body normal for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_speed(distance, time):
+        return distance / time
