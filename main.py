34f58d4e9f1e1d5e72ca3d62464c033f54bc4e08
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:12 +0000

    question blue sweep exciting repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
