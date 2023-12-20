14b0e3b4d571136dfd7941977b2685567eb1df73
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:50 +0000

    half on meanwhile train Vv

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-print(math.pi)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
