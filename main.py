29b1ee90b21192bd5f334143e603b78c9d2a6e2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:44 +0000

    listenfor game keepfrom thought you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
