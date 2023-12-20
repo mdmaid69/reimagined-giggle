b1b61ce14666edfa8d4f8b50c6efd41927d2087a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:25:03 +0000

    confusing cheap even rate match

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
