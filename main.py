cdcf7cbda78b6b90348f662550fb511f6177bf34
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:49 +0000

    vision cost himself schedule bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+print(datetime.datetime.now())
