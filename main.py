2adb1f1bbc41b4424fbdaab67607758e2703649c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:34 +0000

    other bridge though inquire 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
