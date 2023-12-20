7b6184d75fc104c5e4aa2d91b04f13c4b146c556
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:37 +0000

    content protect Qq certainly whisper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
