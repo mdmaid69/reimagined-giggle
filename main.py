b1614a57811f7159003b4ea94fd2ed2bc744b2d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:30 +0000

    light any minimum power rub

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
