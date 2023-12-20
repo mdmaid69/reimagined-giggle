d47ba8e012e9195ef01f51b1c0a3988b785bbba2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:36 +0000

    claim hope public squeeze loud

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
