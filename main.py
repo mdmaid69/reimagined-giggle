b10e646c0010b0d25c38868e3b099accba7f0365
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:12 +0000

    money flow television fork thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
