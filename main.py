9ced5be923de92e04be0c40af3212b618f7b531a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:23 +0000

    teach but determined takeaway natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+print(os.getcwd())
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
