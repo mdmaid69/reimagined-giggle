ebce2c6cf739217f9689942f475b2d54c81c2828
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:46 +0000

    round myself getout land understand

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_volume(length, width, height):
+        return length * width * height
+for i in range(10): print(i)
