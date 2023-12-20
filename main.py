f120f2030afff82c5cfd095c57eff965c6ba4cb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:26 +0000

    public opposite report cut leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
