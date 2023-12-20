722c814490049c60af50e0c1c58f43e58e6f9fb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:01:57 +0000

    go godown disappear average afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
