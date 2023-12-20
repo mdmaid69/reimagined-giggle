7819cc07e093bab84bf5a5f4da3ba8f8209aec88
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:55:59 +0000

    their think mail press speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
