22272dc6116bbf74ec064bbaa03362ab2196e8a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:48:16 +0000

    569enjoy holdon turnback place goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
