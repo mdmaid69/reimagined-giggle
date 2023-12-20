b64aab01d289915e92a7a28f493006a92d2b06b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:58 +0000

    gradually failure equipment represent agreement

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-print(time.time())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_max(lst):
+        return max(lst)
