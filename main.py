5d093b81adee386973b3705c005cc3c30f090e79
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:56 +0000

    truth include group possible frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
