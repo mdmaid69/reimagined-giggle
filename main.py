51dba692ca7f05d82ab515feeb67060a65b109b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:36:14 +0000

    now successful check eye impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
