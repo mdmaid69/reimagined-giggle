80c1320177ab89b79d939a0f916555d194d26cc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:37 +0000

    cry guide get base scheme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def count_words(sentence):
+        return len(sentence.split())
