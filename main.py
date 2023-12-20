820f35cf6280b779c2e26fabd96df9dd345b5880
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:27 +0000

    noise cruelty press free nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
