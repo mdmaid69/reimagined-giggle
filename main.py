397f8a82cca50c99edac61ec7c0a98a689a5d654
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:57 +0000

    lock motion eat minimum consist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def print_python_version():
+        return sys.version
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
