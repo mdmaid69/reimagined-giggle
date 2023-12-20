8e84aaf8cbd42df980a9042f3de9577c61e686c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:31:28 +0000

    interrupt hollow practice design improvement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def subtract_numbers(x, y):
+        return x - y
