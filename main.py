3fcda1cd36735f0bca9fbe21b7dbc3bcb023ce8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:57 +0000

    arrangement able violent precise reasonable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
