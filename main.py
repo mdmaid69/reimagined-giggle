eb0f5058c7f2c8de4fc969fbb71dc4a7faa3c37f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:07:51 +0000

    why Ff favour direct dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
