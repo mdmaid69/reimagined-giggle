ea60c9c0d478785af862f6ca4a798d2f45fba0db
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:38 +0000

    exaggerate noone comefrom rest record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def multiply_numbers(x, y):
-        return x * y
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import getpass
+def get_username():
+        return getpass.getuser()
