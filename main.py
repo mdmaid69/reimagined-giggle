df124baacc753ae8d9a654cd7ed95d40a1ce0d31
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:54 +0000

    strike up treat touch regard

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
