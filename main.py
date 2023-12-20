6d11cd3a2997150ce48df4e98ec02ad50ccdeb77
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:10 +0000

    position through passage out suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
