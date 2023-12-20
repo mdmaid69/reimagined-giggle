18115d8c5bbe3d23a3b670ff9d2358885433e1f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:33 +0000

    prepare pressure beautiful maybe file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
