20078876cd879b0a22d3ad173e9d5769215012aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:11 +0000

    admit impressive recent accidental doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
