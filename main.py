f836d7807a93fa97889e6f65e4d2da61b07e358f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:18 +0000

    work stir personal hold Ss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
