f97b6001dd6a663b0dfc2ec1c816a4a06e31baef
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:39 +0000

    sign strength left compare clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
