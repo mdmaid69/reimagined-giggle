e6ff7dd399ffdf20f4a2f9514671fac21d44ec85
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:08 +0000

    it violence rich round spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
