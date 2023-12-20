5854f0cd4917b7f5a041331a172884e9fe79dff2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:45 +0000

    purpose score development swimming many

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_item(array, i):
+        return array[i]
