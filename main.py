596feffbb1ee14684bd25fd388f9140f2bc02113
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:43 +0000

    flat attach Yy anyone test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def square_number(x):
+        return x**2
