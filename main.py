15383c8767c43b077edc1780e42a9b61a041ed34
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:29 +0000

    content calm worst contrast clever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
