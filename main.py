698ca39dcbe123054bf17b7492f65b1257f3a5bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:58 +0000

    radio friend tomorrow cheat attach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
