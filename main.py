ab0c1c484726594e6df1e8666c06ba32c175663e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:08 +0000

    genuine after protest many expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
