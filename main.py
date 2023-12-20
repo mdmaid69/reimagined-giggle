d97580fe91b8f7e8820d60e89b09b2be0a918e89
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:04:23 +0000

    season apologizeapologise forward now 564encourage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
