01e10a81b308e954d0d37612d4c280cecdda529c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:13 +0000

    miss care cool union there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
