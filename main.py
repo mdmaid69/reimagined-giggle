723d91ecf5e7208de7b387e11588fa75136ddc2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:08 +0000

    embarrassing sky schedule team disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
