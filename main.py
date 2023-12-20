d876a523d66bdfe2fbda9ca21237f8a1d5ae0c5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:30 +0000

    cover full abroad divide accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
