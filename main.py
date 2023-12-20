d60e375a1e1970e049777a58f751ff2e14d5319d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:15 +0000

    sell boat prefer decision everything

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_max(numbers):
-        return max(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
