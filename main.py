70ac4872bb8fc15bf8b1e2aa7ef8146df25905df
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:04:53 +0000

    precise city turn pipe lookafter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
