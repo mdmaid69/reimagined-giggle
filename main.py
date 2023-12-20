78ad5e89759d950fc11ea2325cd2461f52679005
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:31 +0000

    breathe various excellent order uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
