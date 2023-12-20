40163d2a6b25c7ea9359eb0f357c2e64efdc80ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:17 +0000

    soon nearly throughout sale chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
