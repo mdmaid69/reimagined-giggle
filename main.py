9d1721ddfbfca95e09b9152f09585f106dc85731
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:45:07 +0000

    trouble interview still address comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
+  def get_current_working_directory():
         return os.getcwd()
