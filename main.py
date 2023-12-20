c9ffb2f83ce4e89bd186eaab4f922aafcc6f1207
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:11:50 +0000

    switch simple broken pass bye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
