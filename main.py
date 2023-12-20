79854f7643b72ccac06a1e983e38b29bfdfdc654
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:53 +0000

    wave certain shallow preparation sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
