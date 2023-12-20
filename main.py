b222320d17b75be7b1d61774ef42c27aa781233e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:00 +0000

    off room way coffee hit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
