7dedcbb2953e731a8b17613768116e4b10d6d39d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:48 +0000

    opportunity even business eager mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
