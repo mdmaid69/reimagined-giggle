a90a9752ca63b497b76c5815fd35a260e6090a49
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:30 +0000

    glass conclusion mark score dear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
