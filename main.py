e7d2a962080f649764e5884000ea7cdc5465a9e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:53 +0000

    deliver instead shop onto ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
