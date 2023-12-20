a5532deefccb466ad560c952ca988d6ef461bc02
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:00 +0000

    flight tonight wherever ball please

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
