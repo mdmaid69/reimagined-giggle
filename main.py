052e3c29a27b0ac2656c2f022561461097c2325d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:54 +0000

    however some pair top middle

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
