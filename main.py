f376b1cbdf9388e9e8cff77c4eaa85c6d1d0c9ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:31 +0000

    drop hers century bringdown storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
