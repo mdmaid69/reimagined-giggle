8bdeb8f404cdc5aa840d6a1995a9300be3ed0c97
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:13 +0000

    I life warn glass lookaround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
