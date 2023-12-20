cc14f7f4142b25a4cbf408ff35d5b8d65bf358af
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:15 +0000

    contain especially final free upstairs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
