a6d34da4e731ec71e0e3643fc0628a01eb17b03b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:41:33 +0000

    true base official some touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
