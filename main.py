c41ab00193ef3b8013814c098343c4ca002ec338
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:48:05 +0000

    tap question sensitive minimum pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
