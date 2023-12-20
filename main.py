63ec9f379008a03fe241c519ca911571bba44131
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:01 +0000

    exploration immediate port pattern anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
