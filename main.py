c21e28b6daebde7c61e18e269071c2988ac40709
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:26 +0000

    escape feed surface fly comein

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_min(lst):
-        return min(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
