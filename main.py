9be1cfa9efff97a0cddb484c631f8ade9bf23d18
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:36 +0000

    blow importance upset disappear blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
