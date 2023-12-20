ee3d8ab0d62ace8fa4ce1de66a1adf0154a3a7b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:43:30 +0000

    cum would quality impossible case

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
