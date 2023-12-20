1d9c9616cca4f0d9d0aeccf9010219b537db7826
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:32 +0000

    mad pronounce young bread to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
