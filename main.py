922d971f3c93021e2c65bade73d0b0ae448f3a0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:35 +0000

    brain hers mood century sting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
