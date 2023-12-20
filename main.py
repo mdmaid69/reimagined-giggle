98b2fb5d6f110f0beca54285c04ca054a8275ade
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:54:24 +0000

    reach suckme estimate dead last

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
