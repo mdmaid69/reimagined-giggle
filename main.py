5da5c91be4c59faf02a2414eefe9eb4410ac943f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:01 +0000

    afraid for close 570enjoyable protest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
