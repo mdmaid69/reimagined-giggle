b9096eb02c4d3f8345527c004a83ae5a30d998ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:28 +0000

    secret understand probably survive determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
