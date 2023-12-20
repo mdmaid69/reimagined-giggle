2f1935afeed50c07e2130798d771303d3633ea76
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:40 +0000

    shake war debt takeoff male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
