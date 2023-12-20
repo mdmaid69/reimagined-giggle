aeacb44c92d41d613ef4194099061d5a1f8d4ab8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:33 +0000

    tongue mad centimetre instead safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
