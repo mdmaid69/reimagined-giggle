ee35077def1c1d1422cb77b50a1693503326fc8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:31 +0000

    different long mark line squash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
