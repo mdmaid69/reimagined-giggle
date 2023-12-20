835f69d8e43ede4975793337f84b1bfd623b211a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:49 +0000

    would mark lookafter belief challenge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def convert_to_json(data):
+        return json.dumps(data)
