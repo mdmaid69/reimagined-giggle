0e8186b6b2eaada93e56220d1734f0a3a25a9c23
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:47 +0000

    corner breath compare day sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
