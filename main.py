53104fc1904475c8ee60d55c0f840861f881df31
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:54:37 +0000

    maybe unusual by threaten careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
