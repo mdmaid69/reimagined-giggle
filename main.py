2b406ffc61520495dfc85d94652c9622ca60eebf
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:26 +0000

    cope telephone like lie mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
