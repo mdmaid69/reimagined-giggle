be2133496b29ac1d0b5abf6ae2af7d63abad1825
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:15:27 +0000

    one within include exit accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
