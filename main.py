307e8d3a1de51d0025c43b47733cccb8d23e5a8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:58:43 +0000

    sport she neck letter narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
