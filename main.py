3264f068085e8a2a0092115275163a0806783ed6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:55 +0000

    task downstairs fold art further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+print(time.time())
