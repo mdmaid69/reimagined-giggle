81d0035c29aba70d1c7b96ce85de6de2c6bb4d81
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:52 +0000

    rely belief several around write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
