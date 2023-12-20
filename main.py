4f7926c4de7980e3f67e15fde405510ed4eb942b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:11:43 +0000

    sudden opinion roof giveout quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
