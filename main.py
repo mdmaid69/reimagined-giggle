0a10e6f9f8bf8d2af7bbeb1153fcfd3510f34e47
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:35 +0000

    maximum steep tone handle interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
