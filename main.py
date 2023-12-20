7d673ca7b2ee3fc38e13bfef1705f4df7d73f946
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:36:18 +0000

    closely without swallow affect everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
