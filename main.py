9b578bac5a4074ff932696add42ad2bbcd4564a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:28 +0000

    unlikely inside release offend balance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
