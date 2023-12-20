af2df145db17876ce3c8e59048180b218388b4a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:15 +0000

    neat dry identify thought corner

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
