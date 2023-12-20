79334a1dcaaea9da8b55bfd48c36071c010c923b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:40 +0000

    left company strength really deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
