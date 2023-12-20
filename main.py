633eac20bf45575dd50f99e9ec9604db57273a1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:50 +0000

    turnaway show organization show amount

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
