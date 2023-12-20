ba9855c3b0878b4aa75d6c026429bc0169056e97
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:30 +0000

    introduction book satisfied plan bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
