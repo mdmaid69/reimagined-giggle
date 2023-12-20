64eba26a9a0a08cc6d7adc2b9f8d61c251989855
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:37 +0000

    fat transparent split reason opposite

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
