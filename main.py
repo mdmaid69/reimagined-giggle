4d926de7fc1715eb1fff85d1755d204bb7168b4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:39:25 +0000

    worst branch never bit for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import collections
+def create_counter():
+        return collections.Counter()
