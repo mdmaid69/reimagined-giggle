21d240ca40fab7ddca71e97b6485b2b1faaab8ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:29:05 +0000

    hand butter length smell draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
