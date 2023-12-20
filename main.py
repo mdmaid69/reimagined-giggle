393bfbc2f93fec87097eb7cf6b4a86e9016f026a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:48:32 +0000

    payment film again deaf town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+import re
+print(re.match("h.*o", "hello world"))
