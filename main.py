40888a2db875cdfbe9678eed9d8f7ef2ad712069
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:34 +0000

    field centre thumb natural fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
