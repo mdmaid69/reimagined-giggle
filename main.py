f2ae9c30b8e1df33bf5dad3515c7d11a313ca823
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:47 +0000

    partner fortunate show shade base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
