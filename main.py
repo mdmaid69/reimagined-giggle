8b5194fe25c699a1512c764ac0aba111040c2fbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:27 +0000

    shout forgive what go church

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
