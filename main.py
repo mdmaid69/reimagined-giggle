639a6c3f75137678b8a0fadf87e926865ab1e636
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:55 +0000

    less expression balance dance frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
