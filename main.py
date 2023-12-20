429c319bede2d0423aa65c7c6cb4b479bd4d3507
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:25 +0000

    admire shock wall again roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
