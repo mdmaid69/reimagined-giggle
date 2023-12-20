62a2f2bdc510db5a243447d05915bd49b8869e06
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:24 +0000

    difference both sky rest Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
