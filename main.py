15c627e1fe728bbde6c2928f66408aa7749e5f78
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:43 +0000

    fix beat voice breakout acceptable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
