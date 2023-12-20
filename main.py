80596c4dd649ae1d97d4685480e1abaf47275683
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:15 +0000

    little male bring deserve instruction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_work(force, distance):
-        return force * distance
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_exponential(x):
+        return math.exp(x)
