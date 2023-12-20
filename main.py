80b88d9c2b8751e8f498f10f9716e97b933ee946
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:07 +0000

    develop wise put undo prefer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
