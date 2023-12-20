416e1db0115fa620708c6a6752c49f16e5c02f84
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:24 +0000

    work goaway anxious greeting set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
