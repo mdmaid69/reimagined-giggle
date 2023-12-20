4259c721acdf243fc0abc42950f861e1ab155421
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:45 +0000

    heat male mess occasional lift

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_list(array):
+        return list(array)
