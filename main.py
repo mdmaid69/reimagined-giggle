f0be44ebb5934e5201671111fea64f7dbdffaf87
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:19 +0000

    either noisy fetch on underneath

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
