2297e4b6512c59d427f1046bee608b2f355060a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:59:56 +0000

    spread prepare goout stand wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
