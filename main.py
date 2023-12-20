40439ca285febde2762c274f6a3e11c22e320560
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:25 +0000

    imagine organize talk taste through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def exit_program():
+        sys.exit()
