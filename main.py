44f9f9604dc3091b73841b79815f44703e35a748
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:55:58 +0000

    wave report raise so miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
