f04bf09ba763b73aa19a0360fd2fe49f85c3e693
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:41:25 +0000

    neat shadow their act offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
