dae5807c1fdef2c16466191fc15b2085f28e86c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:52 +0000

    noone order find wash matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
