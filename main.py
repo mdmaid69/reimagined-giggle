4b140a46b9698cf4fb1415de4c04baeb94d9cb0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:45 +0000

    fashion for can negative Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
