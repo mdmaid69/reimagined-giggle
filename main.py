b8779ad4a34168120722f2b0d753189f21a303bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:38 +0000

    rarely feeling roll justice book

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
