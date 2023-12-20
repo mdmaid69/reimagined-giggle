2ef7b616c74121deaf3c285e9f7f96af38f0b543
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:04 +0000

    drink claim never judge pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
