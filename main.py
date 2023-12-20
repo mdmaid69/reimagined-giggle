6c81e273fc07312036607435010791da1f6f11b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:29:28 +0000

    yesterday definite matter temper value

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
