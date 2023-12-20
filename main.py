5caefb3840f5b8b29682e3a5f2f3b1171606fcc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:12:58 +0000

    survive snow vanish die prevent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
