4d0342abe73777e96801ce8b58be91ec09fafadf
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:02 +0000

    free attractive still doubt succeed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print(sum(range(10)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
