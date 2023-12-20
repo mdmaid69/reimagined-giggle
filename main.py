6ca7fdedc11375975e73737943dcc417163423b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:24 +0000

    see ahead firm concerned scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
