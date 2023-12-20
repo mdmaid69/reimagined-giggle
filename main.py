1e1bb211f78cd4c668acce7940baf0314849acdd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:03:43 +0000

    tidy scared mystery family mysterious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
