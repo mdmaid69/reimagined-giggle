760883503cab141754f24c7763b183fda3187c7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:42 +0000

    finger cheek trade centimetre fortunate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def subtract_numbers(x, y):
-        return x - y
+def square_number(x):
+        return x**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
