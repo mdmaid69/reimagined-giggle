90c83260f48473d865b65eaac527d14986e610ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:06 +0000

    more track accident popular innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
