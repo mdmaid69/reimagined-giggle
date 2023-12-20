6f7537344b179d85e25ce023f6468f5ab896310d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:41 +0000

    few custom dream keepaway black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_ceiling(x):
+        return math.ceil(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
