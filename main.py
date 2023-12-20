42189ea55da8a442bf30ea1b358dee448a5a4d2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:36 +0000

    handle crazy lookafter certain limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
