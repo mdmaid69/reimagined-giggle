db000c6d9f680332c8cb381753106a0b47ef5d77
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:47 +0000

    approve brush bad off reliable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import csv
-def read_csv_file(filename):
+def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
