39d401e2c7864f06cd85daafea8988519ca17456
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:08 +0000

    during sweet treat certainly exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
