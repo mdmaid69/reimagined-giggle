3990a0e183a3e35faeea54d5300dbd1baac53d2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:11 +0000

    variety bottom half remain pen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-name = "Python"
-print("Hello,", name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
