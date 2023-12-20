53829c29e9197a833af5eb2b3660ef08a8bce6ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:01 +0000

    both sharp uponon clear poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
