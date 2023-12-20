94ac8b09c82e5f8731172a6b0e132efa22abcd82
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:42 +0000

    separate seem safe journey couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_str(array):
+        return str(array)
