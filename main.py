a03e240102f61e77c4174216ec303cd9f1d4424f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:34 +0000

    sudden thinkthrough certainly completely promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
