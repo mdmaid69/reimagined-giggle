54eee59b990b99901428319e63cc712523a3f326
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:56 +0000

    rise notice direct speak him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
