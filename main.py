f7ed3903bf910d795502d010b7190df9714eb5a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:16:16 +0000

    learn else meal minimum object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def convert_to_binary(n):
+        return bin(n)
