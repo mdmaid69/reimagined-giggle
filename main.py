c433be79e7c5eb94d1347044d0ebe9564b9673fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:42 +0000

    back remember arrive both breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_list(array):
+        return list(array)
