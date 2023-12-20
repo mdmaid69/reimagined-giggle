b3d4371aa1c89e8270dbc0efa50c1c84b4df8bef
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:39 +0000

    how deliberate little past bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
