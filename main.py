e8021e6ae3bcef1dc29b9cfcb5cd546848f351cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:48 +0000

    experiment tap drink confidence insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def reverse_list(lst):
+        return lst[::-1]
