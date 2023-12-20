2b3e567be1e0a959b06b28fee6def40ee29621e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:56 +0000

    respect father purpose home escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def reverse_string(s):
-        return s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
