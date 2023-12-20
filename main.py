ba295f3111e37ce27e0ad9a92908f32900f8b421
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:49 +0000

    middle kill one interesting part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def is_even(n):
+        return n % 2 == 0
