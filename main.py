790b7ad34385ad0d8fbe187308cfb77f11cc378d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:52:57 +0000

    from carry letter this main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
