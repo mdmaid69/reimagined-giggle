db086110f44508a6ba0c7547891e53c2e5457aa5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:06 +0000

    all circle distance place flavour

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
