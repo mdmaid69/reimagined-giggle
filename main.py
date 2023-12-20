ccf2fa71df60d5de60c92fdc3cb1354ef98a7d3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:57 +0000

    discussion news environment calm angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
