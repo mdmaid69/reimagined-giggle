65f877f48fc5d696b216d90bbe5013ef5c951fe0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:25:31 +0000

    operate mine splash aware turnaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
