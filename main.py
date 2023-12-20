37a007910f660df4027782d9a4ab48678d554b63
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:55 +0000

    thank meet discover suffer offer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_as_repr(array):
+        return repr(array)
