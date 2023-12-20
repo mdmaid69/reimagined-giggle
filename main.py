b58c436fd45844c19eb2d6399b3a68e07919c43d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:51 +0000

    choice family slight hot talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
