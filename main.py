663a7e576fc4123cc30a56fb9894ee506def6910
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:29 +0000

    tight replace study back emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
