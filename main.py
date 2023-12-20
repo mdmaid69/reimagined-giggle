84546d60d475a808d0a19c49969c3f4bd68fd34f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:17 +0000

    shout various pile exit coffee

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
