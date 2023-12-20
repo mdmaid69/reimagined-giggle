b27bb639bfb835815b7074f3008bdb3b6eb0e498
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:19 +0000

    chase tread petrol people strict

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_floor(x):
+        return math.floor(x)
