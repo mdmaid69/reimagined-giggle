ee74204a468b6eaf642c64c0d7b52009feaf4a09
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:05 +0000

    influence positive reject surprising provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
