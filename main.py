796b31806051c576eba088159cfba2e3f0fa3276
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:14 +0000

    pressure argument store field exercise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
