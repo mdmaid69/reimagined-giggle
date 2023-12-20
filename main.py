11e3557e2aaa5866c8addeb8a02ab7dc39f06e86
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:54 +0000

    row again within centre afterwards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
