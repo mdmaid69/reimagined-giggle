7e520140b880815e5ff477dee5fb0262eb2429c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:29 +0000

    risk fast least school Oo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
