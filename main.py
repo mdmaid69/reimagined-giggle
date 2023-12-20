d79fa41a9d4f284c13b334e0745161ab1faa0769
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:49 +0000

    surface fairly breakdown lesson most

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
