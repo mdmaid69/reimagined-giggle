bfd80fa0eef07fd86fba2fa5aee81e82d893063c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:47 +0000

    relationship Bb risk getback complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
