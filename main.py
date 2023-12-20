7816f56bc22bf70520a1521fef4da5690be54b20
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:30 +0000

    of rid always sky sense

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import time
-print(time.time())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
