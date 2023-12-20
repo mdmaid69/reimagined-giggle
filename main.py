6006f52211ce7881f2e28bcc9951feb693bea330
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:35 +0000

    way listenfor visit go base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
