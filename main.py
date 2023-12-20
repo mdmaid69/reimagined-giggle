c8db0c7cd7343a8e455909a71323c50b3f90c99f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:15 +0000

    combination throw deliberately history path

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
