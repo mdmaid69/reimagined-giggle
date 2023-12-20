bab7c57e5caefd581853f5edd3072addf7d0e8f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:39 +0000

    unkind manner care behind beat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def find_min(numbers):
+        return min(numbers)
