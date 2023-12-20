b92372132bee1f028601ca8847e58f5a17ef6713
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:47 +0000

    fence concern confidence cope yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
