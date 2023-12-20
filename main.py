b50ffce76ea1abd195c6a315016f741704276809
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:38 +0000

    otherwise squeeze keepaway kind exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
