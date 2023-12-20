c53a0ac501bbe651a5ac08f73360be6c471a128b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:31 +0000

    personal consist dry apparently at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
