5ea38ca65c5ed6676d80820a2448a65bc97e6240
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:49 +0000

    life breakdown sad stop fire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
