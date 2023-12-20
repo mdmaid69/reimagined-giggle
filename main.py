597bdf7d3fccd7e0dae8834531bc640321842ba3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:11 +0000

    everyone phone workon motion business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
