10d4b35741dd9dfee95f81c59f469fcec670834c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:06 +0000

    quantity Mm visit trip succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
