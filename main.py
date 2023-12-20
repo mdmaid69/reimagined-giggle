6077c1c2060d43903afc9fc6ba68e35548dd2a77
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:56 +0000

    give Uu cheap normal this

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_pressure(force, area):
-        return force / area
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def subtract_numbers(x, y):
+        return x - y
