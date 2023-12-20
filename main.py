c1d65ed9488eb5b349769e975706e8da4808e6b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:09 +0000

    above national high obtain risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
