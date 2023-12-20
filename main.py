bb0317ddbf05fcabb65108ea8920b912b51bf9c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:35:14 +0000

    accidental all movement approval exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
