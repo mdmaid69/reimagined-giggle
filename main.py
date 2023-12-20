c761b1bd92153920aa558d2d05ff2f71e19b292a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:50 +0000

    intend plan permission perform dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
