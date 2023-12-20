bf8340f2f5a4f800fac88c92c3f71238b85c75de
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:16:16 +0000

    hardly stage memory admit actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_power(work, time):
-        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_area_circle(r):
+        return 3.14 * r**2
