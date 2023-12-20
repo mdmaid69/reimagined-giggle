3e2df40b0455c6193e0142670ee33c91be99454c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:42 +0000

    argument swimming obvious pupil conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def remove_directory(path):
+        os.rmdir(path)
