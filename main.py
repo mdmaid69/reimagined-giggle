04a99c3fcbe417cd0a1ed6ffffe7274d5365e2b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:30 +0000

    place anxious distance brave pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
