e32f88fe44de6280d8f430768ca65e67b16678b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:36 +0000

    injury same home normal imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
