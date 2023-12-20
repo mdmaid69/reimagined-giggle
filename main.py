6443f6340ad01d8cafca88e84e548dbefc91c0c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:32 +0000

    afford comment he top support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
