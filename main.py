34867d5ce6fa49eef116a82be396fe901467f0fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:30:24 +0000

    likely back vanish fair spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+def get_array_as_repr(array):
+        return repr(array)
