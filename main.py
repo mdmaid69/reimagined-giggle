d83120db3af7781be4e95c18392d88c0b6aec184
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:47 +0000

    question cost total drop broken

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
