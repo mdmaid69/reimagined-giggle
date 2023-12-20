36f6dfdd639caa02142ff84e1a16ffbe7415c61a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:55 +0000

    deep young since fall certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
