c7a952264dfd4524d585749999dcb23d5bca326d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:26 +0000

    decision vision dress obtain cut

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_pressure(force, area):
+        return force / area
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
