c2bd1cb8f4aa2445cd3c252ce0203c02138c3197
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:00 +0000

    any successful round born 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
