f28d93bfe03e9ccc72730c14d9cb262888341a04
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:13 +0000

    class shelter understand help respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import collections
+def create_priority_queue():
+        return collections.deque()
