1ba7aa63f909cd8389fe69e09a8ed4f4430fb0da
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:48:56 +0000

    meanwhile poor anywhere row ring

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import heapq
 def pop_push_heap(heap, item):
         return heapq.heapreplace(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
