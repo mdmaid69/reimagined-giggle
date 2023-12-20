a7077e74da5e8c3986d9f52e2258d8e536be43be
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:17 +0000

    politics involve certain motion hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
