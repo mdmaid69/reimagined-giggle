afae5c8ef4ecc67c5e9466ea8dfcfeb1f1f8e047
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:09 +0000

    standard agree disapprove bringback burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def clear_array(array):
+        array *= 0
