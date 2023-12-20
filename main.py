01390774fe5eb304cd041bfe8fcb8259a74aa163
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:16 +0000

    influence accommodation song tradition couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
