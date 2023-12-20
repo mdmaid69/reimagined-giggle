5882ed63c795db83ace3d19ea11c8ffa9241d178
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:15 +0000

    aware peaceful mystery squash health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
