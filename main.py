79b1ccaf008ccfdcad981c136b920723f646ded9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:08 +0000

    back objection loss doubt benefit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
