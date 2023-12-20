e96790e3cccb7c5983a46b83e2e8917f169752c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:09 +0000

    discuss similar regular ourselves important

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
