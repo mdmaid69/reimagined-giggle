e72b9f6b06566db8cb53ad56b4ea8870a7825408
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:07 +0000

    illness broken radio pupil yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
