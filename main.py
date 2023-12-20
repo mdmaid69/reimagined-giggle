0778eb60307802778de68190bda9698cc8027fe9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:51 +0000

    seriously creature ride lock later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item(array, i):
+        return array[i]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
