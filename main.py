7a4166e81fcae32162829632a83be838662ced5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:53 +0000

    do reliable cause play their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_max(lst):
+        return max(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
