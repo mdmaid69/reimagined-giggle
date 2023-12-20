547fe7dc7943d6ce4453392ee7d44bff27174c60
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:17 +0000

    supply accident agree getout far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-print(sys.version)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def sort_list(lst):
+        return sorted(lst)
