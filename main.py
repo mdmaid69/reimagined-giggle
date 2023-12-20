6a010ef72b6d52b96f494dd03bd96cc8e5141243
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:07 +0000

    different wide object popular fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_power(work, time):
+        return work / time
