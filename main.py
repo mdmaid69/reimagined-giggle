01ccedc6f4b644ee947ff3126d2bb5b30c585a3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:40 +0000

    software ball have business sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
