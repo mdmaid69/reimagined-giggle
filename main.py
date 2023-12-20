e35e4cb446e1067baefe61c1ec047211730d03f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:08 +0000

    clean sharp within claim stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
