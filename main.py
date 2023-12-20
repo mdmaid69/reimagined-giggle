3e9c7be893d900587ce6a4c6c303a7196a55f569
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:57 +0000

    sister join close hurt step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
