062f235f630d022bd50ddf662cf4faa845f6eea9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:37 +0000

    cry message wealth terrible spare

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
