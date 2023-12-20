f8172871dd70381edb766b5cfcf10e73be68b3f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:39 +0000

    scream turnon lie within happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
