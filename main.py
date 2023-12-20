e46ae3ac343b58c40630edeb019e148fbb0facb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:16 +0000

    middle hospital score run complete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
