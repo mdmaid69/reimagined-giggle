5a9e98c5d8b6d9914cba6f7d4366a38ff90d6b17
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:32 +0000

    knowledge escape instant tomorrow forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
