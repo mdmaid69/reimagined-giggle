8573c03a5b00b13edc1bcc436a36b144bc7232ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:27:43 +0000

    involve choose lose communication healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
