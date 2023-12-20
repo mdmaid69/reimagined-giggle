aeb96fe169fae21c4a50f3ba9a5da269f8c5e856
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:44 +0000

    harm sensitive attitude hope book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
