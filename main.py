f312acaa712518ae733b30ceee952bd332f38477
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:10:12 +0000

    each out plenty offence pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
