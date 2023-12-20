4d94cbf6ee841462ec33d2f4fbde1f2b72291e00
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:08:41 +0000

    project public somebody Jj speech

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
