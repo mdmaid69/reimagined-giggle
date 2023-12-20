cd7acafed213fe009da319d09487905cad3bba93
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:48:00 +0000

    neither pay high own war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
