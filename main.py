64eaa13a3ae176753cf241e9f8aa3b8ab7c6219e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:01 +0000

    tense hospital later visit present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def reverse_list(lst):
+        return lst[::-1]
