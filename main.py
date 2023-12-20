70e48f2a916ffa58774a733c980279ac86f0a572
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:55 +0000

    able on against dream dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
