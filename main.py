2e45b1adc63eb0889a47331059dc2cb22fd8bd32
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:33 +0000

    agreement false confidence holdon stone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   def convert_to_hex(n):
         return hex(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
