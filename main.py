4fdd38f0d1364f674088ec7b49f861b47991c78a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:50:33 +0000

    inform system table shade refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
