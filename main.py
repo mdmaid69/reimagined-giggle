e735e7d0ca8bde814026d3a4df53213e76a408f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:29:28 +0000

    exhibition cost march healthy Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
