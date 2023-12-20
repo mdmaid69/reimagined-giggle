2022bfad4824565fb6d431f89844547c9d800a51
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:46 +0000

    share dry divide dress rather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def cube_number(x):
-        return x**3
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
