6f2ae175818ca2c06a43e08723f2c77579325ccd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:07 +0000

    at flow pretty its soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
