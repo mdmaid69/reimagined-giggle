0ebd94aa74e9a76809248e1bf7b4293124f1998f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:18 +0000

    stamp all funny pain Gg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
