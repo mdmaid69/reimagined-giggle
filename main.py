6e80a2ce06d31b30101e27c802f927f74168cadd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:23 +0000

    sure return rare cum voice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(5):
-        print(i)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
