5f7efb95de75100ff7e18db765b466b5277e87dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:19:47 +0000

    godown unhappy sky praise anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def remove_from_array(array, item):
+        array.remove(item)
