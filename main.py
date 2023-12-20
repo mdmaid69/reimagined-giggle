6f82c003d09803e95fc80fd3bc06a2fecc9acc3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:11:56 +0000

    ticket common will description part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
