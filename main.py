3abdefa7ea0a940dcdae407469a57bf407fbf508
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:50 +0000

    less home whichever collect various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
