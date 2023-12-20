7d828dd956b334611afcf7fde2a1140dcdf1687f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:25 +0000

    recover compete mine special should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
