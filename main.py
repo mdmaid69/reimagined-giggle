f3a7f109da129736061bb77a857f9e10665e2e92
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:55:29 +0000

    fence music worse believe squeeze

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
