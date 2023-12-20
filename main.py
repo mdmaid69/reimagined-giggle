81b9fba8c4659a1fbf260b88be2c56661547d32b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:53 +0000

    prevent react past worse dance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def clear_array(array):
+        array *= 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
