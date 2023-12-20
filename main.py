e615a7a20196788704956846390a08b9dbb609fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:41 +0000

    key attractive ago person nice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
