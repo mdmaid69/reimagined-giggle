c3315a0c7ea94cb21470f90256a1ed02d2b0bf03
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:52:02 +0000

    question tape getback trick foot

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
