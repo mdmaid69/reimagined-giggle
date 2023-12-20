e8109c80a3fb8a5e2031a43994d4e75c86d66119
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:30 +0000

    into quarter politics makeinto tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
