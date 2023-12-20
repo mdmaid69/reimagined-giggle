57976cdcdc489b4f0bbe381861f41a22db6b9c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:14 +0000

    store fasten charge rush bottom

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
