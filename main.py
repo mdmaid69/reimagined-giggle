c8bf7324713243a3a1b18ffafddaa8fc967c2147
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:03 +0000

    right slice truth passage only

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
