ae8372194364fac24662129341e163cd96f87653
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:37 +0000

    produce proud question prevent Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def cube_number(x):
-        return x**3
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_binary(n):
+        return bin(n)
