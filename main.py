8a66e00dad90c0abefb93d3c03a6ab486bbaf886
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:29 +0000

    their suddenly after crash party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
