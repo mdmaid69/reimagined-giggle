bbf6bb5b32e7ee791dec3f01a931786d0365050b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:50 +0000

    emphasis love body used appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
