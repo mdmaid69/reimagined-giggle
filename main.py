a533461b84958b3e77bebac71752ab9a8a76b806
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:19 +0000

    country giveout grand famous detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
