9ee52cab7939eadf0772d8565698587771961870
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:23 +0000

    hand awake relation achieve fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
