d393e9a5759186a7d68fb39aeddb6224e6665a6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:31 +0000

    bunch smell tip thank coffee

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_typecode(array):
+        return array.typecode
