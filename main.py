fdf04d519e3c5174aecda78d7144d0d9b8086d09
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:11 +0000

    fetch uniform able whisper wander

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
