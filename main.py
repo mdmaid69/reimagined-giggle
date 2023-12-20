4274d17aa01d87a6e306390fdc550e947d0d1b3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:42 +0000

    less extremely regularly innocent pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
