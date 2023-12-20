f119de17457dd4712b9505bc4e0fa96357012625
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:33 +0000

    beneath stupid name enthusiastic determined

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
