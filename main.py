6884dd3dadaaa12a1bdf8de76f3a7b77342775c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:03 +0000

    cheap positive mark whisper open

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
