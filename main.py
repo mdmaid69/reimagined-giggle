d61ea06c2b5e8db0ea0956028c2f69313d4356a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:46:55 +0000

    continue record space treat add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
