20e4e06469f5eb07fd373ba22d9c811853796632
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:11 +0000

    region pain strike tap last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from collections import Counter
+print(Counter("hello world"))
