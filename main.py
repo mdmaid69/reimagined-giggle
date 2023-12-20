8915a182283099817396efe89f6787b36664032f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:50 +0000

    all previous basically middle steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
