008ec1cec55d051e899f0ea111897c653222dc20
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:53 +0000

    badly taste mean university back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
