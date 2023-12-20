11d503ce741cddd3a108013c7a8904e465c0af39
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:08 +0000

    flow friend impatient scrap bill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
