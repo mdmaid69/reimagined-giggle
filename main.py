579a36011a587ea508bb1013ef6e7baa23568249
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:39 +0000

    squash almost unhappy bringdown best

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
