469a72ca5738637994cad0cc968d2ece8914d696
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:45:43 +0000

    cost father separate wish label

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
