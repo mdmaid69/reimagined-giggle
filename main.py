3f23112e03b0038bdd0578ddeb1e45c45e3d705b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:51 +0000

    catch platform react switch treatment

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
