bb3dd038710d401ef3a39a32a16dc0173dc60f6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:37 +0000

    recently sound herself chin still

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
