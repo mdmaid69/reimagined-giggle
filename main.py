18a3382170251a8c5ac2210be02a69fdfc194237
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:34 +0000

    stamp transport usual real risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
