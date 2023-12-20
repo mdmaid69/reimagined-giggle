e0498aca5fdd0f77b2d6eda814ce9ab5ee266b41
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:33 +0000

    undo problem safe shop work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print([x**2 for x in range(10)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
