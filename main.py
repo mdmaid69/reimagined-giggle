824abd08902ef5d7f955ecd741bdd9ba2ba299a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:43 +0000

    middle believe bringback sweep accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_speed(distance, time):
+        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
