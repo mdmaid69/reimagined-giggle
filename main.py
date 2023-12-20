a8145e460a7e999bda99342f396cd725d08356b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:35 +0000

    reply smell shut admit opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
