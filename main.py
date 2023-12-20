f3cbb9003c4293e79fee405b61e63262aa86ef5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:46 +0000

    majority demand board birth may

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def count_elements(lst):
+        return len(lst)
+  def sort_list(lst):
+        return sorted(lst)
