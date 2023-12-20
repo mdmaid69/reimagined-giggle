535a1ef3a8dc75ebe4b2e879932d9943d2717ae9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:23 +0000

    objection border stretch colour eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
