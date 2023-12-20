30465e1bf03c00b9716117a0858a7a3092c4b66d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:37 +0000

    turnup introduce as point exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
