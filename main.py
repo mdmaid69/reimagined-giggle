027fb651591f0eefb7b0be662d0fa977e5f5196c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:40 +0000

    disturbing why dig rain length

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def find_min(lst):
+        return min(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
