dec3260c856271e5e697ca121c64b46fbca05ddb
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:53 +0000

    acceptable trial travel trouble tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def multiply_numbers(x, y):
+        return x * y
