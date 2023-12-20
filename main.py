9f5ee929b2f6bbe012bb2e3096acee2c8e0c4753
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:47 +0000

    crash add nature when level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
