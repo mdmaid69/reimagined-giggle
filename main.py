e75dec4341158ad0478ce9d393a7c02341223a36
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:58:39 +0000

    funny effort ahead afternoon soil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def convert_to_octal(n):
+        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
