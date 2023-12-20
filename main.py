e793b424aca4b5a35c8d192c6bbb798cc450bf90
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:29 +0000

    might excuse nurse system relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
