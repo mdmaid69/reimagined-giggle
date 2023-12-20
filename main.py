9b832aab659e7df4323dc8643982da221e7fdb4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:37:28 +0000

    common general slow spring depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
