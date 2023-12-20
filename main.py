2ee37fc9698d3c01476e7a07300c9a9fde19d97b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:13 +0000

    mixture answer behind intend take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
