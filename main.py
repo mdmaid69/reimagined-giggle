3a45cd1d99bbd5f5be89b16f81677e2fbb7352d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:11:05 +0000

    stay study private meaning grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_length(array):
+        return len(array)
