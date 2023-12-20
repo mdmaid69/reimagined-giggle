efac49716f029bd3db78b68e2d6b304bf2ba8bc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:33:49 +0000

    string watch famous eager fortunately

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-print(time.time())
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
