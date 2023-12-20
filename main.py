6a2f361450f4f27dbdf6a5bd48164f86cd5e34dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:22 +0000

    niggaacademy compare separate workupto young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
