1c34acb789edac00e2b280039b56f984d158aa45
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:37 +0000

    exploration relation big keepoff rubbish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
