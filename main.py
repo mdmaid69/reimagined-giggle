2133715ba134241e80030f169b1a6e014544239b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:25 +0000

    world mistake regard snow transport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
