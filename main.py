899622a2bf191878b51d7faa5ae5a19dad27390e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:15 +0000

    part steam prove offer skirt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
