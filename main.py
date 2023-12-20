3f6dec8f0c16ad9965c4bc54d136cdcf0c738207
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:48:42 +0000

    design friendly later declaration another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
