44dea12ea4f5adaf682f6cd68fc10fe1d7516b82
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:04 +0000

    law cover impatient press fierce

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
