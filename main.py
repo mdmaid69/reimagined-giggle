7ccf6c7bad3eae70ff0d284c05d3daa9d0d65c91
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:03:10 +0000

    cap storm burn press success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
