c4cb2aebfb45aeeb6c44d67fd41e5e63124871a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:22 +0000

    her increase clean congratulations workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
