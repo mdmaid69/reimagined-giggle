15b46af1bf8a5e4d8140b8b354d5ede1040ed568
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:18 +0000

    emergency 179bite shake married listen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
