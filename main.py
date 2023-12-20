3b667aaec77abe009b24530f41bdcaf5de6f6678
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:56 +0000

    the injury respect sport embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_force(mass, acceleration):
+        return mass * acceleration
