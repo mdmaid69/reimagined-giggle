7172bb4f3f77ab96065fd9783404a5734e9f1b18
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:47 +0000

    press face fashion aim occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
