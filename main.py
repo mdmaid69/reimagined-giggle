8bb2d290cbbe19ec72b9d3d29a8ce6608b8064e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:55 +0000

    somehow empty Gg trouble sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
