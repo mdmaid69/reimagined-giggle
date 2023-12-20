aaa77c63c23c71cd24705046ad12809a46f965fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:00 +0000

    remove thing stop comefrom hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def convert_to_hex(n):
+        return hex(n)
