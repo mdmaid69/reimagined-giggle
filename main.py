8387c427b26aa8374ea6b6a26fecaf68f1abe794
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:07:00 +0000

    disapprove claim wing direct history

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
