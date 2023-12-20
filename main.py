c9eb3969ca01ea6f8d6d25be27283e63423c6225
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:18 +0000

    lower spread street comedown frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
