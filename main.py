bc3fb776f4445986e5aa9d2190894e686dd50d47
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:34 +0000

    hot high turninto anybody debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
