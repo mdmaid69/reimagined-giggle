c2fd36cb45e3159f3ba70dc4c0d0215dd1edae9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:10 +0000

    part after clear pause last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
