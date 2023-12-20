75ad03fbfd070606c744efa7686a133678206a4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:15 +0000

    off classroom general inquire sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
