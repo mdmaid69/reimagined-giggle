b3e3eae93021bea700938964302c5a8364c2b279
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:47 +0000

    health hold picture well level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
