696c9d269aa2942f3931e59bdbfb7afc2ea73baf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:20 +0000

    tip hole however century first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
