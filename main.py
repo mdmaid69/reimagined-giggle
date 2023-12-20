3f4b0eb75341d5948bc6976167ea6b2ea2b54bbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:33 +0000

    general reserve quickly new joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
