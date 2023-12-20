590b429f3991f7aeb1f65b8e820d08d99572d239
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:58:17 +0000

    similar future cough female pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
