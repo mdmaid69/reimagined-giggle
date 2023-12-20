5d8e4a1bca038e27a52ee192becde07b1f4181e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:23:05 +0000

    Bb in soft lean notice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def remove_duplicates(lst):
+        return list(set(lst))
