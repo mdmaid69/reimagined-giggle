77b03fb63df91d1444ef7cec0cbde1cc5698c480
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:10:15 +0000

    note wind sign treat brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_speed(distance, time):
+        return distance / time
