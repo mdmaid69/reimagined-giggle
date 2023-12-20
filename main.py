867b568a0bc77def9719ae486c01a402480f8bbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:26 +0000

    hot Ee trade speed surprising

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
