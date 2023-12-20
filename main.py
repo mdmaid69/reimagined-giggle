1068b234d01aff7e8f78309b39cc4753f5fb59b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:31 +0000

    Ww considering declaration stone value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
