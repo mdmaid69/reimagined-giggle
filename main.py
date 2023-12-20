763a17fc0fdf0d387c9d35081716e45c8ea51f9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:54 +0000

    length ball history accept basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
