855b9cc7bf5848ae0a51305c62e017ebb1fffdaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:51 +0000

    wet relative himself process sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area(radius):
+        return 3.14 * radius * radius
