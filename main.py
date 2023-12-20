6f0d59c978770af1ec561def54124e1074df4bc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:21 +0000

    uncertain translate create talkback decide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
