a937ca2bf0a82ab487cc5d0f1270cb0c3aac7b36
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:49 +0000

    imagine pump comeround set wear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
