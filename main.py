6c90b1c2ecd72011e532beaa9cf3aad831052d44
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:25 +0000

    after best police edge famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+print(re.match("h.*o", "hello world"))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
