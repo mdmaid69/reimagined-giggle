f54b3ae3996ee845c531fe4de1d0614f71c93651
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:41:43 +0000

    flavour count uponon call difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
