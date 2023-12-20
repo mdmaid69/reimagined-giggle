544e6ed9ee858bb75e61e0d65adef3200d4db7f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:47 +0000

    active steer average nose interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
