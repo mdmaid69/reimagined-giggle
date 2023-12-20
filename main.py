ad6a130e6b8fd21f33877325c1c24f679a46ab17
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:55 +0000

    decision rise awake truth limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
