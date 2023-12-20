ee376ee777f2eea7b1d0f9855c4aa76b50b644e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:01 +0000

    bar neither sometimes present after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
