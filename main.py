e06c932af0e4116410f3ec4b970f4ad8f357a771
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:49 +0000

    mix degree agreement hers ours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
