2246b898ea13a74d88678995c8b3dbd745f20aef
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:20 +0000

    claim interfere neat mail 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
+def calculate_pressure(force, area):
+        return force / area
