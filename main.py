75f0712d10e9c06611fe9cdd29bc2c4d35227fe8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:44 +0000

    tread have score disturb many

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
