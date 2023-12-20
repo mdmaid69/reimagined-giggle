6f2eae6b7ada000637c666ec79b4dedae6e3b650
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:52:51 +0000

    black single sensible convenient smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
