1bd1fdfd54bfa60029235cf230bfb7a649790ce8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:11 +0000

    whether almost often then birthday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_list(array):
+        return list(array)
