db868727c5bfe61cf2f4bc7ce982329f91fea965
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:28:57 +0000

    satisfactory rise rob fat nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
