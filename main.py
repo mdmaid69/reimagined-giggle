84223f6a464232cba2d64988b0b2ee93d47a8a91
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:02 +0000

    right money tradition glass however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
