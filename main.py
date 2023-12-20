26b1bca965fa38afa281fbf18b7d2989801ec214
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:13 +0000

    complaint call pen win switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
