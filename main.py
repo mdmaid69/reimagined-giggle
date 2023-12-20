6f82de4e8a29ddd74a382d59866430462cd82a17
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:22:38 +0000

    moon office sting talkinto attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
