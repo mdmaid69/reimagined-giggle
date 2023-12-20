34cf9328c07f72b81b5404559fe8073aa72f472e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:24 +0000

    deaf mix movement include contact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def sort_list(lst):
+        return sorted(lst)
