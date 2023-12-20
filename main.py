df6fe90bbecd5193712b527b184e0be4e6fcb07d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:26:49 +0000

    lie record jump mood beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
