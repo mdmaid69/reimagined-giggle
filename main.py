d7e96794e928c4b861da3cef4e18c46188966b43
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:41 +0000

    last share consider unpleasant war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
