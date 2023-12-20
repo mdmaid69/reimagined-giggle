f86bc9b79d41eb9c337062ef9e42fa31fe66e342
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:46 +0000

    demand ourselves interrupt old convenient

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
