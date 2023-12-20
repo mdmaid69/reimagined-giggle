a6fa806b69268bf920bce0fe39783f7b9e15e1b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:19 +0000

    argue comein left relation along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
