3f595b3c801c042066194bedc6ec4fda58678e6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:29 +0000

    remain smell on pull scheme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def convert_to_hex(n):
+        return hex(n)
