93d87947eea57300ad86288f39c5da4587aec5ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:30 +0000

    tip length ours cross deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
