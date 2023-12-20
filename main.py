821be577e873e76790949d181e264c9bbc6fa505
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:47:39 +0000

    spring opinion owe split dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
