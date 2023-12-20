6f6ecda2d0f071d428435d34968cc1f96abd01d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:32 +0000

    bang flat command come wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
