8a894a5075d06847f478ab771f410f19bc31a4fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:57 +0000

    yesterday task weather public straight

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
