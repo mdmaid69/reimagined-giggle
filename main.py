6989285434cdd1a9150a3969e5c1944e34a95574
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:42 +0000

    mark count strike thought everybody

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def cube_number(x):
-        return x**3
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
