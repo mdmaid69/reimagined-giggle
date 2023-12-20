3213221cae3d4acff0321d7ed02e62a4972fea9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:58 +0000

    but disapprove demonstrate mark experiment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_string(s):
-        return s[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
