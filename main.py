5f208018726eb85c3a4083ca3a302adfbc945d57
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:05 +0000

    branch over only past behaviour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+name = "Python"
+print("Hello,", name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
