6acd1f1c8618055bb29be29e30c1a60f2304a049
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:15 +0000

    occur impossible result live achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
