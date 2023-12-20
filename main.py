7dabcb2615a29bb3fe1ad16e92bc9e7f38a0737e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:51 +0000

    display increase noone behind book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
