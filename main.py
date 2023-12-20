6a31ec7b7b94de4095590c763b50ab173f65a312
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:01 +0000

    common square fork earn Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
