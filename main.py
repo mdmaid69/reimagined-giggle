14a8deafb56bda7e822d4e40c56c0eeeaf64b76a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:28 +0000

    represent discuss cause type receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
