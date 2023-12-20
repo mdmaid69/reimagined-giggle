a1c74d7f2a40cc208a5040f4156f7d6116e65266
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:17 +0000

    somewhere usually rule operation port

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
