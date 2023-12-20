b26813f28684c041aedba0d3e4fdba1fd637ab5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:18 +0000

    before hurt top scene cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
