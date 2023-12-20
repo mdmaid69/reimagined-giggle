87fe910c0e1473d6b2c42eaaef4d7618a6bf5165
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:32 +0000

    Yy generous kind dig best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
