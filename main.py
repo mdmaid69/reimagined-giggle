24d5cf06f48385bf915ccd38000a185a947a3341
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:33 +0000

    point sign label they inquire

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def clear_array(array):
+        array *= 0
