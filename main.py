4140599db7135d51f6af8adea38ddb07daff8cc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:56 +0000

    bringdown pride cope exciting pin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
