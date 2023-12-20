9ad9bec58cc814aa4947919da75f2dd1e8a5046e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:22 +0000

    boring behind born fault shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import logging
+def log_message(message):
+        logging.info(message)
