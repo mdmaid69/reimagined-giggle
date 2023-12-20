c6a1d689d6c9c21fc14c818a81549987b31222ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:55 +0000

    avoid talkinto later below strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
