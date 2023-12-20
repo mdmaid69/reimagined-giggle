ad39bee69ebacb9b31aabfddf1212e1957e4f956
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:19:57 +0000

    learn draw land impression dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
