157362552d4fa2850cfd07509706370e9718a0db
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:36 +0000

    here bank across slow freedom

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
