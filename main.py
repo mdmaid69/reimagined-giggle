929fff75317b434f00e52718935443c245227cb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:13 +0000

    holiday giveback immediate summer hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
