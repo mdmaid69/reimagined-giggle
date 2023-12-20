eeb1401e184cbe7c38a1d7540faeb7eb6d765b86
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:43 +0000

    close admit interest shock afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
