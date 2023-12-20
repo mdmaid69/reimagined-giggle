b54251be39c11d80e907f7011d99a387babd4ae7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:48:16 +0000

    season move office show breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import time
+def get_time_since_epoch():
+        return time.time()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
