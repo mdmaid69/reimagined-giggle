65819f9ef31d9c38f95917b79ce0038371725ea1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:45 +0000

    must shout soon usually pretty

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
