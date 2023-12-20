8c2038bce40fd09ec4a1c29394c5bb52552f48f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:34 +0000

    Mm suckme admit workout however

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
