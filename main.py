ebd512f80c6bb9643d867c7d5b6f3ca96b875eef
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:28 +0000

    remember fond noise mad report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
