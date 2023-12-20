954cc1733c70cb49237576627f0c2fac1b6f106f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:44 +0000

    pleasant roof pair club which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
