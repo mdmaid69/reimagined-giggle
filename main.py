b1042b88600001cdecc5a0d6c82441e6bc0fcc0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:58 +0000

    nose disturb thankyou recommend difference

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
