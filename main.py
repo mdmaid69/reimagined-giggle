095866ea8e18232f4e66e327cd9ef1141584400c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:33 +0000

    slip repeat cheque speak workupto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
