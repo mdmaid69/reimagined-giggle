06e1ae1151ccec18d5132f5dc3144a2fb9fd9a75
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:03 +0000

    whenever muscle healthy trust ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
