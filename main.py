d73213c807e7164304c8a7b24c254c09547d2f93
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:23 +0000

    support across custom storm Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
