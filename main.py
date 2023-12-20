74ada12962f9322a08b602eae793219827cc4f8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:09 +0000

    song hungry language turnback indeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def convert_to_octal(n):
-        return oct(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
