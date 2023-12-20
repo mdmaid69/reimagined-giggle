c53eace213466f88138fba8f3516854d48f29b2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:48 +0000

    goon ourselves system drink estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
