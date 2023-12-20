df4a0d98123a04727d8a5fccc3d860049887c1bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:22 +0000

    live address further tomorrow trap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
