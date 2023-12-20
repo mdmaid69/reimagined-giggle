f62e4ee7437399aecd3b63fe7e9a576daa2b659a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:59 +0000

    nerve also listenfor education disturb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
