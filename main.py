b1c2a4bd8a2872ad2194cb1c7de6d9d4cbb0392c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:44 +0000

    of strict this question ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
