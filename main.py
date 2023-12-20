ae4dbca239272f7e6ef3487b3152aa6ce61f506c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:40 +0000

    work personal comeup reach 572enter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def cube_number(x):
+        return x**3
