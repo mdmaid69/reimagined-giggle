b53827c9ddeff3918bc755961bafb6fa283d7b5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:17 +0000

    intelligent drop health study team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
