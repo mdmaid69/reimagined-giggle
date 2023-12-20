1c8e08dfff6143e09f286f5fadb13b314ffa7c3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:23 +0000

    project identify spoil character neck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
