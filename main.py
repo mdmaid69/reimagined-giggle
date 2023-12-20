e79b94b29e69afbf9cef72acfd7b13a570ff5783
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:01 +0000

    worse contents method professional noone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
