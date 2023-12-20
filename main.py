dd6a797ff9c8f8373210fe880b18cb190d884fd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:24 +0000

    please extraordinary about everywhere makeinto

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
