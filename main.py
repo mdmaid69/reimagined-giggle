0a296a78fb980043c6aa389b03e48a8acff195f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:46 +0000

    drawing discuss Vv mess tight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def reverse_string(s):
-        return s[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
