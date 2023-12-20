358f36a16f4a768ab27fd509d2ca953bd32d109a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:20 +0000

    glad chief name ticket decision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
