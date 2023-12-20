3c3b814d5310ecf795b2a0bcfb143d9b4868ab09
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:51 +0000

    spill wave respect throw miss

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
