b57ebdb08d1f596166cd374a14fc380c32132b94
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:44 +0000

    beyond legal set back along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
