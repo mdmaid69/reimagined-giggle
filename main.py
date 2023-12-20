1675561977e4b2ec7455afc630533a14df29b708
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:36:01 +0000

    reach runout suddenly school take

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
