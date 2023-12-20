08188148daa80aa93b8e387727c432914f24f756
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:53 +0000

    treat government equal hour neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
