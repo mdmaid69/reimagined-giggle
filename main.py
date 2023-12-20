c5140cf5b6dc352bf03fe16a09a39169cca0ff8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:26 +0000

    pleasure estimate lower closed concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
