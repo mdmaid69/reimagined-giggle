1cac0c615b684575bbdc378195865011d6c63f34
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:26 +0000

    how still exit bet top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
