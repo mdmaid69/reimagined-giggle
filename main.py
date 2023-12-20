cebb15336aec9bfbcb6331f624774b600c0d0753
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:05 +0000

    bother home like past lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
