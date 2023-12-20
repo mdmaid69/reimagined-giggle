356143c8d41a5de4d850ae6644c5e36fba5ff9ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:14 +0000

    least effort set pupil discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
