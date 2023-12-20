2a49310dfe8143eabd934df2f6423986887d49a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:25 +0000

    patience way commit meeting acceptable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+  def square_number(x):
+        return x**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
