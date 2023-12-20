2fca8e176d73263919275a3621b25357a0e2a6cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:39 +0000

    eager expression lookafter convinced raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
