f21908aa133d38a0e03785e404c91528e8999544
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:41 +0000

    say subject lock pretty common

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def cube_number(x):
-        return x**3
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
