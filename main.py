e06c08c6f954dee9e0b24d950ff0d764a7a0e9b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:09 +0000

    space spite spread mind confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_absolute_value(x):
+        return math.fabs(x)
