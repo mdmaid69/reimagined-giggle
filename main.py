2711a2addd61165b2a256d142c17f28fd08a29e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:15 +0000

    drive this stamp mess talkback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
