d31e14625e3f9c2af3de9fbe324f6e4c178b06b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:58 +0000

    argument that refer brush Oo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
