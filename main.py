0793145efc361b74d66fc1b2b5b2b0645c84c0f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:17 +0000

    bringback probably back believe this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
