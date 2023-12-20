046e892e10ff202f002d2b6295a26be5a0522c37
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:02 +0000

    lift bush murder uponon spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
