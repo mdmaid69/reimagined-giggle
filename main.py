33bc5082b3d357c83885fb6c6f3dd05cebe1da84
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:41 +0000

    afraid stop sensible job course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_index(array, item):
+        return array.index(item)
