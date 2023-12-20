f0ac93f96fc62c98efe478f7d929680c635acb2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:02 +0000

    lucky reaction consult action free

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
