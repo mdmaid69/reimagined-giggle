278d3ddea5f64c80cb44fac2b380b4b9c471c0c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:36 +0000

    effort direction important considering definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
