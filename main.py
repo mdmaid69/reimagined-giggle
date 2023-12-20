ed41c50b6c06f180bb9981ef5b686ef04026087d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:50 +0000

    stone possible almost message radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
