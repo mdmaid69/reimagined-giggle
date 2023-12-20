518089e20e1b2aeaea1693748f631da1ab244fec
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:56 +0000

    extra runover brief hope 572enter

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
