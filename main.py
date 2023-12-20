f783b39e78cd323679e10be89ea5b06cce73baff
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:55 +0000

    spend wire dead crash trick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_queue():
-        return collections.deque()
+def add_numbers(a, b):
+        return a + b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
