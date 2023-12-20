0180fa7882c2cb3d029368f9f0b6a2c95b7ef7ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:18:34 +0000

    contact speed calloff position bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
