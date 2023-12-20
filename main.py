2f51a142b1b2f0ee9a33566812931d66da1f9816
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:18 +0000

    performance exaggerate upset real hard

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
