d0a4fdd375fdae70a2181fa13e8ff27cd3ae57ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:51:09 +0000

    decide interview check after any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
