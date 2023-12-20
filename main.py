cc3561f6554b8eca9fa57b101712ea747fcea46b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:30 +0000

    prefer wave trip idea grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
