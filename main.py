e3e8fa7cf372d4eaa62918c2c0caef0f3122d174
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:49 +0000

    burn pure load winter run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_sine(x):
+        return math.sin(x)
