cf76f52fbec32980c130f8313eb8ab9c26ab43e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:51 +0000

    mine confident accommodation somewhere state

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+from collections import Counter
+print(Counter("hello world"))
