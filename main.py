e9f96d33097edde38a8af9c2fd773e7d8d58d915
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:02 +0000

    spare cold perhaps lost pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
