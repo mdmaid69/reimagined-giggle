2ae1839c9c0f80e143d9f2705f9e36c31135cda8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:14 +0000

    opportunity season sea some feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_error_function(x):
+        return math.erf(x)
