d4ff60c25bf42d8e6cb0f742dee55ce3406ebeea
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:36 +0000

    chat lead thought imagine occur

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
