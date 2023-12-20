86dc83527cb89e593376f2c3fbe202daff417d26
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:48 +0000

    ready speech name attention afternoon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
