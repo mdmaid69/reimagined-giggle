b92d3ed8086fa0ba2e345d507d745984e67d3807
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:09:57 +0000

    thinkof lost far decide attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
