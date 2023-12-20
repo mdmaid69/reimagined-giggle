38e161f1c12a9d3ceced31468b7778a57ed475c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:13:37 +0000

    there contact that heat price

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
