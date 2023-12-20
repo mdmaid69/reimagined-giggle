fba603223744c59e5410201a1305f5193b562313
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:26 +0000

    scream prevent himself invitation suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
