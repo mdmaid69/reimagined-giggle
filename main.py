98b8869a213136e870e9f29a5da6bea1d7c72dc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:39 +0000

    common centre form serious dead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
