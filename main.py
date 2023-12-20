85a3b3a4de9f1ed9939ded03f832e6e34c4815cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:04 +0000

    depend send ear debt address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
