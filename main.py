ca8f2b5d9dfa603304a597c48949a0295a5d9fa0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:49 +0000

    turnon snow runout violence minor

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
