97c157857d1a5c46e705b51e470ff9e7935699f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:24 +0000

    cheque official reduce niggaacademy company

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
