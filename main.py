39e0e59b36370f92c7941f43078d1ca50c6b19d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:56:05 +0000

    stretch develop congratulate might choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
