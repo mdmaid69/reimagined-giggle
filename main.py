5d3bf9e96f45dcb798fbcb902305a05c7141923f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:04 +0000

    tread fast community behaviour experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
