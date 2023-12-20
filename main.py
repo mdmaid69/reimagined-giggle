fa1f1e503e9495696547d50b0bdabd7ae1390ad4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:49 +0000

    dear computer yes warn remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
