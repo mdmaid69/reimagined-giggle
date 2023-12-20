42e748e925db4669cda40bfd8db83b06db433720
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:45 +0000

    uponon listenfor rob professional callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
