4baee4e1330afa0c9871132079c0e6f85a9a8e07
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:27 +0000

    direction runaway beneath name commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
