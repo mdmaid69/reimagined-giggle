aa2a085c287910e0e30ab2a0207cfca4c535cf66
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:31 +0000

    fly boring thankyou deliberately last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
