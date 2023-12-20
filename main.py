ed9f0129ea1c29ff2e33b2b24d892e2171ac5da4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:22 +0000

    in send profit decide stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def count_characters(sentence):
+        return len(sentence)
