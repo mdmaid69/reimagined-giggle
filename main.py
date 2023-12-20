377b475947abc507adeb65bf020773c28bc3b469
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:22:31 +0000

    clothes keepfrom couple house tool

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
