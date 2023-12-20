da6c18fd45cafdebd65a13837f83f6a3790606b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:48 +0000

    conclusion tape declaration deep have

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
