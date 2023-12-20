ab966dadcb3c446f434665d17fc61078103efea5
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:36 +0000

    tidy music sort opposite outside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
