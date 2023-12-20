0cff311e889737b98fac0881ea711be1f5cf9093
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:07 +0000

    use teach vote pull complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
