21bf0f9b843e6ebc2fc1833ec4dd6e8c24aa0300
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:57 +0000

    smile because embarrassed relax rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
