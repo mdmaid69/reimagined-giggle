f59cefc390723043acc287cbb5be161970aa254e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:47 +0000

    judge hour poison very damage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(lst):
+        return min(lst)
