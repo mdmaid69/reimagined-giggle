1d88f66765a1864900ab95e47e9e58b034ffe6ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:56 +0000

    reach clever university other use

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
