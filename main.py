6e5e004a35328abf82da53e0b709c751630057b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:07 +0000

    century invite bother trap Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_odd(n):
+        return n % 2 != 0
