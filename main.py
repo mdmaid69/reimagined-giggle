4656d33eaffe910e8fa2ae083e1fd9c5e8e1015d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:13 +0000

    kind suggest minimum freedom workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
