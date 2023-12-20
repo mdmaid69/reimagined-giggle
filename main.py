664ec9b380417c88f13d626fbd460255df5b52ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:46:21 +0000

    exact mother plate brief happy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def square_number(x):
-        return x**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
