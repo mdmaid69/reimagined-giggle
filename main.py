dfc9c6f56132d4b7e1cccd470004f17abec44ce8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:21 +0000

    happen body escape exit block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
