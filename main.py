a99778a822d088323ea5b0828456bbd892f702b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:07 +0000

    so tone really test talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
