1445cbe47da9cbd5e9f9370b38200e6410e2fb58
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:19 +0000

    goout visit crowded correction powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def sort_list(lst):
+        return sorted(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
