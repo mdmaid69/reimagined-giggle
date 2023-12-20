a6be62a996a35f7e77369abb2a1ab79e08486cb6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:52:07 +0000

    passage expression fit please borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
