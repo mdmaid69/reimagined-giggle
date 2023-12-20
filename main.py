a994d2e1a31f4460405fd6dcbc13132be777b275
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:01 +0000

    share no shrink attitude hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
