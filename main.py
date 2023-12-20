946a2ac20cb97fd15687360bf63c2f24c8313c2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:01 +0000

    conduct turn false sale respect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
