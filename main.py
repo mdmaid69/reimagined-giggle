117971a74e66b462fe78ef9572432bb7deac3e00
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:44:26 +0000

    communication normal calm kind cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-for i in range(10): print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
