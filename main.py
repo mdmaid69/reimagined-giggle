9178fcd82c572a95801ed7c4cf23fe1820a629ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:10 +0000

    concern complain instrument car crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
