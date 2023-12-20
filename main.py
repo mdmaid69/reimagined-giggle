3973869f06286af7e259b41acd7d9045e88548c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:56 +0000

    passage fence conversation discussion accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
