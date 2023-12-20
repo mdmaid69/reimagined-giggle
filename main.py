c684de8c011cb373a9ddac87674fa54cdb411425
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:24 +0000

    activity live used gradually search

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
