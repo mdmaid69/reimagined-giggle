cb089b756bb39a950d6ff871f96c086b397c2743
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:54 +0000

    achieve shut soak phone surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
