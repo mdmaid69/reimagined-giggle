53e89de7df044f6a35cb60a13a1fc7a7b994ac7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:34:23 +0000

    damage cruel politics use before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def multiply_numbers(x, y):
-        return x * y
+  def split_path(path):
+        return os.path.split(path)
+def find_max(lst):
+        return max(lst)
