70eb4ad7a2a596fb18c90d672ba534c0370f053a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:44 +0000

    left emergency organization preparation once

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
