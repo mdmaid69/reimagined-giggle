4c4f0f3fe66cdb7234e18071da7bdd5435bbbb72
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:13 +0000

    look drive confusing rely exchange

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def exit_program():
+        sys.exit()
