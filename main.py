370da54208ddfa328c596fb7eb0288c724c7ad1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:19 +0000

    which arrival throw settle objection

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
