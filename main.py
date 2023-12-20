cd481e868a49c9f836a7e227f6ecc6efbb276891
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:55 +0000

    allow escape successful lookaround wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
