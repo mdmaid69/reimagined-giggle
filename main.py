98d2cc27f78c4a8061613177c0b0a41babbd5b9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:03 +0000

    complaint feeling do excuse fall

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
