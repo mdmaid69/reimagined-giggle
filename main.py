405bdc039897c4e742d4c974d77e05e4350070f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:40 +0000

    persuade book mind priority him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
