5aaa6b3547b0f5d42e306a5182ee03c4ff7f4cd1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:00 +0000

    but Ss thought suck area

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-print("Hello, world!")
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
