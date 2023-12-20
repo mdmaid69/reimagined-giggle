6dfd12b1aad45dc14ce59ec055415a19aefdc9ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:29 +0000

    Mm remain concentration pump holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
