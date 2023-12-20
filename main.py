635ba7f7f88917063026ce823b3d6cfb8cc00976
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:14:18 +0000

    plastic comeover wall concern plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_binary(n):
+        return bin(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
