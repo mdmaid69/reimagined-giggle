8d5c5d2e21d5d05f48b80fd262e3f9a9dbf27bc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:03 +0000

    stone die shock warm sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
