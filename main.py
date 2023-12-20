034f164fea04a24abe0eaab4687253066b34424c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:26 +0000

    history spend plain invitation instead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import os
-print(os.getcwd())
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
