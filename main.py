edb0cc67cb4095c6950913c7233e6ad10fb2cd90
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:57 +0000

    tour go spread destroy pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
