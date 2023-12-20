fb1c7ea76ceaee8f3678044cf48be2c21c5ac640
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:49 +0000

    cough neither question suck information

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
