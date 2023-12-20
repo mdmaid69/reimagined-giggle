240602a2f3a9165c6a499aa011df4a1d4596fa7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:33 +0000

    may whoever soft look when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def find_max(numbers):
-        return max(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
