76d9e71462ec296ecb4cf5cae2a1259d85f923d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:14 +0000

    direction sale little priority book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
