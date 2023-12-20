1dda7f70e1c1a6e565cbb0035ec28e7513a168d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:43 +0000

    beginning gradually interrupt preparation but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
