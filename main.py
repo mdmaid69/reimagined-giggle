b0b7f624ccc2fe3e2e85059ca6802cc5492e74e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:44 +0000

    forget dance possibility Hh laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import numpy as np
+print(np.array([1, 2, 3]))
