f80b8786e4fc73e678aa5f9c5fff9c03551e8af3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:57 +0000

    goon cause centre pay fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
