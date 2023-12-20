8c1fc1a8d0e2c7d38b401998b4639bca2f03ff34
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:46 +0000

    gather benefit calmly noise art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_current_directory():
+        return os.getcwd()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
