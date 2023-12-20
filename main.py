b9a41a6a9e07d5e89e25de5ae3d32176d718b4c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:51 +0000

    hope flow giveup board wash

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
