6306d95820b1d1a7a0656316463b3801275b9a63
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:33 +0000

    relief between argue reason really

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
