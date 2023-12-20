6f9d52eaa6077a06466b895acc9aa6094a1b582f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:06 +0000

    extra neither classroom do precise

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
