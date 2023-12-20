5625c253fe6b1e9fec4f3cffcc563a15d74ea4fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:35 +0000

    generous lower view speed mine

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
