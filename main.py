4bebda071e75b3b3d6b581df99536e4efc110e16
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:15:51 +0000

    remark free central else crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
