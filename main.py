328d511db30597b2d7579388e9e85302efbd0dc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:40 +0000

    wet protect side during exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
