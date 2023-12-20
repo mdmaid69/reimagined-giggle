3fdc38d11e61227c16944c89f93a7b1be85fa0f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:42 +0000

    temperature furniture mark clean care

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def log_message(message):
+        logging.info(message)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
