f470d061e34cb7c8e2c91b296f73faa73cccbb00
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:58:56 +0000

    stiff label apparently partner personal

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_min(lst):
+        return min(lst)
+  def reverse_list(lst):
+        return lst[::-1]
