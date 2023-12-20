3712b252512425bd4856dfc0459a275dcedcfe15
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:06 +0000

    possession float off whole before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
