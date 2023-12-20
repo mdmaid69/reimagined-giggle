d5d67261cffc6c93b3266b3a129d92c8570c1326
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:18 +0000

    angry only own prove spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
