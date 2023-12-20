ab86baa7d08642daebd42fcfe62fb996b27196cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:27 +0000

    reply listenfor control job most

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
