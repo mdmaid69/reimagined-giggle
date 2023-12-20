c941ad7ef84546a0186b76a5b842cb9267bccf95
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:18 +0000

    Uu material worth keepoff expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
