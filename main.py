17d99e61e5a5c7a1f14e2b14b7670304db25e1fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:38:26 +0000

    successful contents cross regional study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
