82b4a4b3d35312f75aa05678cb44ef017480f3fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:38 +0000

    everyone payment Ss say introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
