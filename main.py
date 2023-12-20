6e3a9be9664157a119cbb69f24118083f6630cce
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:25 +0000

    comment drive paper hard history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_odd(n):
+        return n % 2 != 0
