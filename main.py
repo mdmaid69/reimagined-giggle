5f4fd2b4c93303763d92b2acd23729776b373560
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:25 +0000

    disappear poor win represent live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+print("Hello, world!")
