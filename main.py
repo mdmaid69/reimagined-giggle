6932667f1214805bdf5e9ba53f127b5fdca7eea8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:29:58 +0000

    fork prize anybody want port

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
