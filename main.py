5be8b399c1b5df706ce23c0ae03831ac0a5b8adf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:18 +0000

    ready together indeed agree crash

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-for i in range(5):
-        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
