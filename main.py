dd6e9e191bed02c7e3122565369b81522aaa5ddb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:08 +0000

    runover greeting need getoff community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
