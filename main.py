e387099158201b0b4722d9dac6dfc6beaa1e8c0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:55:12 +0000

    minority answer follow use means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
