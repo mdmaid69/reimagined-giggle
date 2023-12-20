919025cd95d20e998fd9d158b2f6650d60c68024
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:36 +0000

    quality eat rub fire rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
