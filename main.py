89930483c7e1a0ff731bfbf0a7321279f8df1588
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:56 +0000

    combine out and club plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+print(re.match("h.*o", "hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
