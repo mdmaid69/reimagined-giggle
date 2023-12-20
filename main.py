c0e5ec7d38090fdaf856cd2b94ec1442069a8f18
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:38 +0000

    somehow powerful instead would future

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_rectangle(l, w):
+        return l * w
