948b7a30d30854bfc15118974885b1fd556e3ed7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:28 +0000

    downstairs cheek if confusing flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
