6727961ebff6031fc08d79e442248f227ed1dbd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:41 +0000

    dull which appearance force programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def is_odd(n):
+        return n % 2 != 0
