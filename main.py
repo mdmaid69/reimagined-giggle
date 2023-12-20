2a1f9b9ab74d41620a29d7150da08b5dd4006f2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:13 +0000

    agreement whichever pretty decide major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
