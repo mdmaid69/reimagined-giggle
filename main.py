2711168d987acbc9d3ccfedf427b2d5b9b02877f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:55:56 +0000

    tell rather show flight introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def is_odd(n):
+        return n % 2 != 0
