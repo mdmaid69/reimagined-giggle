11bf6a653456cc95c5760104f5ce5c4a1a02f9c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:30 +0000

    declaration have part fortunately vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
