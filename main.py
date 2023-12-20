6be2e13796f4e62f8c807b88962925bbe91205e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:57 +0000

    suggestion attract good giveaway gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
