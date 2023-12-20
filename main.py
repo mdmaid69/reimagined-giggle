10df7f03c5b032434b06309bdd40e304f7eb4fd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:23 +0000

    small low different brain medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
