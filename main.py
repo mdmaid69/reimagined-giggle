fb8c648f3e30a32e2d0c675d321069bb8ecfb286
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:51 +0000

    figure content container interrupt careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def convert_to_binary(n):
+        return bin(n)
