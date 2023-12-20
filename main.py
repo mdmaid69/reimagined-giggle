785452cdb11ee0bef59089fa2ff563322c69b63c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:48 +0000

    describe skin fast love dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
