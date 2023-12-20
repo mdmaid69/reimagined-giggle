25af108e089f0089442bc57d4efc168fd4e1a51e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:26:31 +0000

    run sister other draw club

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
