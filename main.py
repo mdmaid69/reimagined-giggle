7e0390a0a3f0f9bcc898ca1516487599ba75c4de
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:51 +0000

    here open contribute declaration lie

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
