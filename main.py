08efdb57c3485e2d41d72dec79f52238d08708f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:16 +0000

    expensive it fault trust slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
