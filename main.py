842bf632a2c9be23b21e40da7404ba7d5d28eb91
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:42 +0000

    temperature adjective spite order fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
