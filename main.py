0fcf149c533947e075b81b30e50336a3409c31bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:44 +0000

    spring walk powerful history ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
