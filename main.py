ccdebde692a70c5e30f5cf6555416b497e29a49a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:44 +0000

    scrap difference steam Uu design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
