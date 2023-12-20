cd5cf83a22bf46c04f28151cc7019e10089c36b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:58 +0000

    tone card welcome middle thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
