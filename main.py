c6a83b835778005449a9072e3dd789245eaf8195
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:33 +0000

    me right exaggerate sticky sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
