5aa90664a4f513dc97bcbcc2ea66f59a25f09e6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:02 +0000

    excited land purpose manage cruel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
