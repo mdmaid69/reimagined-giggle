0e45cd3da3762c9a273baff859cae9082a7cbe0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:12:07 +0000

    crash content measure needle print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
