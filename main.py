08642f3b1228f2c94de271aa16102510d3d96cee
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:40:47 +0000

    bag bringforward leave ill do

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def square_number(x):
+        return x**2
