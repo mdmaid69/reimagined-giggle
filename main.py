5cf88012f9f8d742491209d283c560f942e9ca0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:08 +0000

    dry determined sentence your rank

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
