2243ec9634665ca4c8ec7042b1866b3c6b61eb82
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:07 +0000

    handle yours general mind pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
