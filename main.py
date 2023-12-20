9d15e9ca0f9b10b3089106972244b8116869ccb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:49 +0000

    lot neat direct negative badly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
