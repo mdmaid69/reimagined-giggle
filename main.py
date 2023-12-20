e41b2d94682d5cfde2d60a3e626e869af20de2d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:49 +0000

    Nn part on goon comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
