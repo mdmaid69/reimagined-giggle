4b94b16257d6b1ee5c32a7be91181b5ce01840df
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:13 +0000

    most month along lookup nowhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_max(numbers):
+        return max(numbers)
