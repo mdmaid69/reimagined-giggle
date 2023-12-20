919d40a930062c6c09db1db8862204a4d8b49cc3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:00 +0000

    trap blame glad something rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
