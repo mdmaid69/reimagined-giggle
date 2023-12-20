64375d152235c2ee8f91b6972df6ebe875c72cd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:05 +0000

    change excuse angry every later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
