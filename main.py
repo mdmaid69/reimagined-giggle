50ba211b811d29245e8b6e77f5d091afda15fdce
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:24 +0000

    mistake country justice ignore excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
