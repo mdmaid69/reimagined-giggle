6d389f5c690685a80543efdf27e7063e0bfe85a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:21 +0000

    limit worse right talkback course

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
