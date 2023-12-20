1b79360489dd16c80613f06db7d2508ca9a13fb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:36 +0000

    criticism value sea callout 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bytes(array):
+        return bytes(array)
