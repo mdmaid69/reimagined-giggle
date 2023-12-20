fd926a21d59cb602aaf1d44f87c32876f979f6d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:05 +0000

    feeling defeat dry holiday model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+print(math.pi)
