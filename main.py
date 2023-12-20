5f6be09e39e2cb34254349b756cb0a722c8e6edd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:36 +0000

    bill balance manner silent fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(lst):
+        return max(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
