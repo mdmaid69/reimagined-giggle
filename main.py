8ee2f41aed532c1c9d7b7b1fb969d6f769524ad1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:50 +0000

    sad him shop turnout base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
