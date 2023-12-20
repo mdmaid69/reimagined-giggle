599194e6fd942668a0b44e1fc5428c3557fc0b27
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:52 +0000

    wash neither defeat land tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
