9b48eda201bbe90450f4e05a1b96ab15fcc7734b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:22 +0000

    wide soak anything base complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
