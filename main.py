6c38792a2896d696098c81125db640f49d4991db
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:00 +0000

    powerful character drive among protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
