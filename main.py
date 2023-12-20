a8464a9f779970752166beddd49f509574b51685
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:23 +0000

    unpleasant key note thanks hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def count_elements(lst):
+        return len(lst)
