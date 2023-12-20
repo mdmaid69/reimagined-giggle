21251e7d70497395c01148eb13cdf823d4ea88db
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:47 +0000

    lunch concern mark slice Hh

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
