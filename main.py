f6c28d5b5fc131c06740a226bb3dba23053d2f58
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:35:35 +0000

    path crisis method in shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
