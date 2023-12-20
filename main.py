abebc29ee06778a50010761a0dcf16ed4d222b54
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:09 +0000

    close habit stick science rarely

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
