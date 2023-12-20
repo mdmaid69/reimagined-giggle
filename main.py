04c476446e539046408b2bc5fe8b79fe257634eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:38 +0000

    city listenfor first check ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
