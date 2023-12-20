6f1c6b443ce813f99cd90a0e756463932ed025ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:23:08 +0000

    guide world sticky cap 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
