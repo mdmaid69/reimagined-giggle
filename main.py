f66537e727a20c746a40c2238e49fbf4b903528f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:41 +0000

    regional home 572enter promise sink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
