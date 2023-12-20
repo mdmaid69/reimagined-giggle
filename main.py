4daf375de56ef8862c57122e6c97e55a72ab441b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:35 +0000

    actually any swing failure negative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_volume(length, width, height):
-        return length * width * height
+def is_palindrome(s):
+        return s == s[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
