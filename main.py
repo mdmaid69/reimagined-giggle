840a7447264c155f3d95a9215f41dfbce78e6a61
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:49 +0000

    pity trap of month coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
