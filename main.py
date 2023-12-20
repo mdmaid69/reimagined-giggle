8c57c8425228077e9acf42eab243f0a581917481
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:22 +0000

    we bringup space trick side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_acceleration(speed, time):
+        return speed / time
