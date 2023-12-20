2e1e74432812548c44b5e29f6ee1232c3bda460c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:12:46 +0000

    protect objection Ff avoid consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
