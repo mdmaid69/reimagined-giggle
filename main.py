ea8ed3fe520c851404838daacc6f3f2609946bde
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:38:20 +0000

    everything fly length pull plant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
