e0cc5c27583218febfd97a47454630573c67c55a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:17 +0000

    pair scale reaction original chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
