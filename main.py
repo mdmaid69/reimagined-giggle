cd6f5a98bf96a91473663263126bbab1bbbebe28
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:48:09 +0000

    group people breakin right tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
