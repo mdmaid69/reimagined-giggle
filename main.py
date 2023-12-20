e797f7f9f1a3c5d860adaf17aa8923a2c5807363
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:36:38 +0000

    567enemy interview consider introduction correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
