01f5566b68a9416f6429590821c19dff5d9a795a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:59:02 +0000

    black introduction rely surprising skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
