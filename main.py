96a53c41876289faa3d5e73b571d6a3d0c11bb11
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:33:50 +0000

    plain hate few afraid brown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
