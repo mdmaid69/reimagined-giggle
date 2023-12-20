4998e007456c7180f7088a2cc70ae03d302fd8ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:17 +0000

    advice on model match direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
