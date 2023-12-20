c89b0c541a011740aa16d8e7bbbd3625515fc4a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:01:57 +0000

    near fun suffer various hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
