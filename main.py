a0a7754f5bd70192fb5c380fa59aca8adc237b4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:33:02 +0000

    over destroy profit confused somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-print(math.pi)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
