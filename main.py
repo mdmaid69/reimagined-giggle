beecf8d0b89ab08db7acccbbcab014390602b021
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:19 +0000

    reaction suggestion patience involve human

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
