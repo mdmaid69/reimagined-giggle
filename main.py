3142f5588fe3928938b97cad8258412b3272e263
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:51:04 +0000

    stare suddenly noone concentrate head

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
