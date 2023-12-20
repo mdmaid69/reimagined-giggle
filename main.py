d1d8a8968f041a9823f726fc39d71cb5885204b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:58 +0000

    collect quarter little improve fly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
