151d9af7d550e9ec33d08112de302dc9c18a9463
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:10:56 +0000

    plenty disgusting discover main to

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
