594b280cdceb452a4fae14654094e14e4efd4b64
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:58:03 +0000

    entrance will recognize unkind splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
+import sys
+def exit_program():
+        sys.exit()
