bdcc242fda0e482ac58a393a33537ab11d0c20c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:42 +0000

    war branch with roll regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_current_directory():
+        return os.getcwd()
