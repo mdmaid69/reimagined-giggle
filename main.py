003ce28b1b4cb8f369f08379e85e36e8659d5be4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:26:36 +0000

    something person church basic crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
