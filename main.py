1ce3ec48967b63a4259378262f9f5e8d794dc8db
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:01 +0000

    charge scare trousers raise callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
