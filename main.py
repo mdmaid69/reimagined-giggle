a97fce5a13f9a660f693d4b6b6deb8b7190f069c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:50 +0000

    afterwards control many aware sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
