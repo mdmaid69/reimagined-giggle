94d3c30eebc3162ca8fd0b490f1f6bc29a0e51b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:54:54 +0000

    well neither so sink mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
