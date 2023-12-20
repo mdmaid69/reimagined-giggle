b018e63356babcd220aa157be444a1445f474040
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:39:45 +0000

    pause expensive light spray keepaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print(os.getcwd())
