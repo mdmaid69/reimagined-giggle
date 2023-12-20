294e3976fc8ff82f5d78fe12f6c1b66f40d0d08c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:07 +0000

    card cheek normal especially embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
