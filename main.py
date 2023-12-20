3aca8706f3bd331625907004054d1829f187e8d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:45 +0000

    shrink peace excuse performance smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
