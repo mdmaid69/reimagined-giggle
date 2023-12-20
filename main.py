db98dcdec96fbe86c3f1f3effb646714719fae75
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:12 +0000

    show between contrast comefrom personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
