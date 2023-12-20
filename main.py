df303fb283e1a4e8cbe4842f0991cd83bfb45e86
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:05 +0000

    wind act piece natural roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
