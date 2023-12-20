e4fa046c5ec0b282b61785fff211ae5a68553118
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:38:25 +0000

    care comeround sometimes cruelty amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
