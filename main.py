8be9acc72a8d551604bcf0acdffa2796f29748f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:52 +0000

    scale comfortable bag drink basis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
