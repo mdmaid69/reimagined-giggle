de6fce04c77db0d327a509a484aed65a8922e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:29 +0000

    should guilty sell knock likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
