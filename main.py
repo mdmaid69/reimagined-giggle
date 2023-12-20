77bca14c24b58ae8bf591aef38770294c55bd8d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:57 +0000

    pay bush bear base fun

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
