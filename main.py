f40d1403de6a31f1d79807b415c5ad07182308f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:20 +0000

    your warm inside beside rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
