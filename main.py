1252bdaadc91c9353f18c3c1461362e2fed61352
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:29 +0000

    comeback band sweet beautiful breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
