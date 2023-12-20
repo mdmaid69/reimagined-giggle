22e3aef5099289da796bc919438e4adf6ae33f95
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:28 +0000

    exact climb short from unlucky

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 n = 10
 print("Powers of 2:", [2**x for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
