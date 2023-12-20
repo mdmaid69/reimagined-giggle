51aa1a231d5d9957eda20a229a9e2357d72fbd3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:21 +0000

    exact first one determined insist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
