84b6eb4123ac22933e592495556f39973b382898
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:06 +0000

    conversation Dd bright key lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
