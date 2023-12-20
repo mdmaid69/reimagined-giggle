60481fce9c54ad7cf48c62eeff1ecde41427866b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:09 +0000

    responsibility breakthrough remove comeup culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
