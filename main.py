21fe3dd692b3ced331febad8cf96635f8ae87b6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:34 +0000

    her crash rub more today

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
