c524f41df2d4e862ff8ab6a96a268e2c2ef69df9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:02 +0000

    variety medium leave outside container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
 import math
 def calculate_power(base, exponent):
         return math.pow(base, exponent)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
