0fab616e315c17377453adcc25522629aae916ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:22 +0000

    hair ago world limit separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_work(force, distance):
+        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
