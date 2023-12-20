ef4177fbe74f350110c4d07dd3feb395c91b6cd1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:14 +0000

    show statement cross degree police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
