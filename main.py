7376a66fb90d47d11c69195c8604b2ccc67bdb51
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:55:11 +0000

    male perhaps trip top prepare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def get_current_working_directory():
+        return os.getcwd()
