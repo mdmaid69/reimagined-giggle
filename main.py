e3f62f96d1c87f822a1a17696432360f87ebad4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:43 +0000

    ocean face single hospital show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
