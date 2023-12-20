a7ecef2afa5a0c1409e0e0eedf435a0353a431e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:21 +0000

    trouble wise flat involve bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
