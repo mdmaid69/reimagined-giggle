88e73598f895c6263438459fbd3d71f851ba8b70
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:56 +0000

    interested load every owe study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
