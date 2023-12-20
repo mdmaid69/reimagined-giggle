af296cf52705e635fa43eb4f9b0afa442574d8b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:13 +0000

    dangerous speech turnover street hurt

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
