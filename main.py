a942a1b7d368d3fb02bfbae91a15702d4dcf02bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:50 +0000

    visit Tt uponon turndown shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
