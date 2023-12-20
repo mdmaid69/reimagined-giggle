3a1f92da23b7504ed25c580332ff88c521fa9218
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:58:24 +0000

    aim several pleasant consist less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_memoryview(array):
+        return memoryview(array)
