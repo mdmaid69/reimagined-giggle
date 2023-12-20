f77f2c219737ae1fe2655bba2905d2a4a4ed7c4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:58 +0000

    572enter father face doctor ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
