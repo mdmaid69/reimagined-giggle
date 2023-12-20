2ad5df9fa5e61a68a997d5a98d17e365a2de9d1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:11 +0000

    rarely little bottom head exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
