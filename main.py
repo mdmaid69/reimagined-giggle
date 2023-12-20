210f8707f734501e0e5aacb0c0c7940db8b66dea
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:15 +0000

    arrival work prevent means avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
