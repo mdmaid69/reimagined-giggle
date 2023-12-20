d180af8122d7fc21a7a4f416bdd5a38e06e8bfdd
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:44 +0000

    Ee certain crash active become

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def print_python_version():
+        print(sys.version)
