b1ca6e55001bd29081b02b672da909c6653bbe3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:27 +0000

    heart tall can eat brown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
