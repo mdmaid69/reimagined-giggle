8ed64deec19363ef966a94b3607fa95a44152667
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:43 +0000

    equal out person typical set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
