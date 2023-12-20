f550944226f860c4ed2b1ab68b04cd837112eb3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:43 +0000

    quickly band make by profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
