b18d70ac972a43a6aec08da7a916563e0ad5a92f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:22 +0000

    search 564encourage describe just face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
