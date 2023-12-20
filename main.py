3517615c888a2c55614847142e96e05435ee4d1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:24 +0000

    worried everything unit class wherever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_repr(array):
+        return repr(array)
