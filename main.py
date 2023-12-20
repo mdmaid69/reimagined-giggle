71a154af729a644d0a1bf9dc888ca9849ba3e1a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:16 +0000

    bottom temper remember war born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
