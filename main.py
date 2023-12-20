547fdd86af401776e6077510439bacc1e0f72731
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:44 +0000

    telephone turninto among mind plate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
