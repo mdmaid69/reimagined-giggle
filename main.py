20ef25234a6934529f788037d43581458c4c4001
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:10:21 +0000

    workout level grade drive from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
