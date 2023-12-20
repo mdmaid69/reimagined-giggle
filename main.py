b0d07ef144a8a164a997c38bb82723c24c5be6cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:51 +0000

    ought without spare sail anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
