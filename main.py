6f9a2d5201cdef70422c62ff98abb9a4d1074962
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:16 +0000

    minute holdup calloff perfect drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
