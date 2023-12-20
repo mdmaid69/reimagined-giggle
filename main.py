a66568591695fca2fa7374991e94904298230542
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:54 +0000

    paper serve occasion protection copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
