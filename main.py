f21949f8a20b490dc72ed984bf7960a47a2ed978
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:06:50 +0000

    ride flow reasonable band break

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
