e27284b07b8393c46f543e5cb80d76924c2398f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:21 +0000

    today save male regular aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
