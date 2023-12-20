e0c7d036ab8d3b7db663358c3d1eab2f2f59f280
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:28 +0000

    any guard plan way bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+print(os.getcwd())
