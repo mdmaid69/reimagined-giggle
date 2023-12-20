2eebd37b88549a585a5a46051a79d756aad5fb7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:13 +0000

    tough dick unusual repair interruption

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
