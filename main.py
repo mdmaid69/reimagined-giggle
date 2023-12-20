9d4b99ebba46b07fb2106692e4140e9d0b45021f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:40 +0000

    ashamed store even equal workon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
