34d77a747485ed8646db17df7b91be6fd6bb65c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:08 +0000

    afraid row innocent hello public

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def subtract_numbers(x, y):
+        return x - y
