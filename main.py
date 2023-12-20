28a413528473f11ac6672791f0aa818ffae82e23
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:19:02 +0000

    talk air moon Bb sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
