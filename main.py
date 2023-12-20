d318c257f5fcb48d41463fed6a8c29b5371d07dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:13 +0000

    turnon sleep bend gate by

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
