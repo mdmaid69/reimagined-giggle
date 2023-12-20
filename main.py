e52ae6f5235f7fc12f668cd793701ce5bcaff4d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:28 +0000

    clean usually back nearly sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
