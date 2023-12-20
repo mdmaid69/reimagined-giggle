1988d7789b24b0713e6838d05e21d0dc7eaffeef
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:18 +0000

    hear progress question band turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
