efe6934faddf26c760e821faa186594189f8435c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:48 +0000

    reasonable bang keepoff sale wrong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
