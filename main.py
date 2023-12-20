18ad57266e30b41403e0b55eab7278690d642eef
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:52 +0000

    form together up big bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_volume(length, width, height):
+        return length * width * height
