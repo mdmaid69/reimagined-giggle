c88b296b31cc70f7fa60c20961f3ea7a08f34179
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:30:28 +0000

    preparation copy loan responsible ignore

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
