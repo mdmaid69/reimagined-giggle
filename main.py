4c810daa038d2d05904332c5d7d36aa45260bc27
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:55 +0000

    mad trouble mixture direction turn

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
