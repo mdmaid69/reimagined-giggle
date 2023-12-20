4fff849350980fa7e04dddac3bebacf1f2423ef1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:40 +0000

    once accommodation close first issue

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def convert_to_octal(n):
+        return oct(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
