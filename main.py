2838f26636a9b05b180098cd84faf07efd7464b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:34:52 +0000

    money though swim not chin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
