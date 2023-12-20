a5ef60cb95fb85ec21674b58e208193efb1bae1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:11 +0000

    purpose bang match sure air

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_min(numbers):
+        return min(numbers)
