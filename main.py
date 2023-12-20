23a8e46d7ce2343d96741f523dcc2d05b2f1a17b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:27:37 +0000

    train impatient your give Ww

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def add_numbers(x, y):
+        return x + y
