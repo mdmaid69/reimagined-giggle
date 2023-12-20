1010481c7fd1a06ec3b0b9f2b65f24eaa70fe23d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:10 +0000

    computer basic creature clean stir

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
