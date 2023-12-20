7230737a12f0fd34db7b456934205ef975ad559f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:36 +0000

    spend impressed think slowly frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
