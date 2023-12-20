a15d1f83115a1377a75991510a821652f902f92f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:37 +0000

    Oo breakout aware easy image

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
