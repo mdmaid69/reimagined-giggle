ba76422536c796895a9b2a5833760a9cf4402002
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:15 +0000

    maximum pleasant leave each computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_float(array):
+        return float(array[0])
