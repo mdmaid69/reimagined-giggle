961ccf1a24b18002e464298e0a0608622e6a3bd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:54:35 +0000

    block rest extra alone test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
