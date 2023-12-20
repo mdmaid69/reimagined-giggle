04fe7e41ab1e444d1030efd814090be975920136
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:03 +0000

    interest close help correct Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_words(sentence):
-        return len(sentence.split())
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
