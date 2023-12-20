d437096e4c45d2e5edcff11f1fcb9507c34f63e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:16 +0000

    lend space refer often ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def square_number(x):
+        return x**2
