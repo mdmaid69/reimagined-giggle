44724fb74a1af92f4dfff372cd3dedb68098df01
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:41 +0000

    far visit excited climb group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def is_odd(n):
+        return n % 2 != 0
