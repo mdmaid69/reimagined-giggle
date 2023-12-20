d1179ae4a3b8222026953fb8ea5378d9c2488005
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:06 +0000

    compare spin count drive schedule

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
