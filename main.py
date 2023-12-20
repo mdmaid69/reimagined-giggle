e2875e307429883ae18e5f2513e384b3f5883df0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:04 +0000

    jump cook set careless her

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
