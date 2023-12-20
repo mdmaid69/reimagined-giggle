cb4dc947bb4cc5fd434ff8c7430bc0fe70c7fb04
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:31:21 +0000

    story high Yy present common

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
