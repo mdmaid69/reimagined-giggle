6d574eceb07b36772b1c67bee3c9f6ea5386cad8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:44 +0000

    doctor stamp to children Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
