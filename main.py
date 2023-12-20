6b2b5267eb9e79975d14d8e20ad3ae600d65e108
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:52:08 +0000

    return target discovery comefrom past

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
