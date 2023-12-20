f93435c326c0fb7746fdbc28f30b828a6ecadb36
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:43:33 +0000

    rank original exercise extra shoot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
