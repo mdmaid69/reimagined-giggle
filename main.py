6426ed00fef9fca0a81cc7a45626c9493891a77d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:27 +0000

    several determined sense ring correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
