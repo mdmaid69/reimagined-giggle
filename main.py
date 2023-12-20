52aa9825a572fd236d269cda9d8ae46a3929fce7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:36:28 +0000

    takeback worth detail weigh ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
