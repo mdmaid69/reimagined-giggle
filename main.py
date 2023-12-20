b5b62b182ec16d2a91b1bbc3317765be3ef0b17a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:25 +0000

    before worth intend closely wave

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import time
+def get_time_since_epoch():
+        return time.time()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
