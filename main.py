53361aec7e892638629fc8f1f6a93bd8a88f9d5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:22 +0000

    getup almost tell suppose rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
