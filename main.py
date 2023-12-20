9da57aa806ad2adb36ee8481b873f107133f4e60
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:47:54 +0000

    content family display several value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-print("Hello, world!")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
