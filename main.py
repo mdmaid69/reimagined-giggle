2f87760db328909bead788de11597baf09b16c25
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:19:54 +0000

    lean on figure keepout project

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+{body}")
