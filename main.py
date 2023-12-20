4abee1e9f620c265dc29a3e1579148d84df59f9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:46 +0000

    forever uncertain fortunate stamp godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
