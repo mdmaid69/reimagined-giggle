1ccc34f1800917d47020d57f4a61e4ad4eb95f46
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:47 +0000

    smell advise cum suggest Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
