3f0b738fc684837adbd7ab8fda648c2b004cf7be
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:52:56 +0000

    final connect final fear glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
