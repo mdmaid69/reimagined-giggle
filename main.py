a58f7d2e64f0e3b4f46eea6cd566078459276158
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:13 +0000

    sick guilty official frame poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
