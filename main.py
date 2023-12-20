bae76811630799835dc5ee93c5aa613ef33c047e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:17 +0000

    grade knock address heart pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
