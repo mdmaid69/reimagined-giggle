fe43f1b1861a477fa32b260cf2a6940c7767e5bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:04 +0000

    afterwards term treat pollution embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
