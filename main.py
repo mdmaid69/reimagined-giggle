91cb0cdd59c80806d4e0aefe884bdbbd79a691ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:19 +0000

    shrink turnup both payment makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
