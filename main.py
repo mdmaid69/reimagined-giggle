a67ab7096f2d9db6740f09fbd4c95921652a6b79
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:50:59 +0000

    hand shape sail service mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
