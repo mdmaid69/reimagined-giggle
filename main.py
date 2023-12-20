c2acb4e91be9d8dd488f4606fce442efbc96ff92
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:53 +0000

    second cold on bush somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
