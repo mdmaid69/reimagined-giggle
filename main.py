c4ab87a6a452a0de0bc1d4d8943642d7997cab28
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:03 +0000

    play machine from mine Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
