39a6e808ace3c992f132e71b578540d65f624fcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:36 +0000

    description ear intelligent OKorokay connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
