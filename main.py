a03a3b3aabad2bf07ddd375559981a30cf3d2b13
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:22:13 +0000

    disturb danger introduce minor unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
