2977b12e9bef035d9e7b47aca7281574c28a5692
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:07 +0000

    of calloff instrument about scheme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
