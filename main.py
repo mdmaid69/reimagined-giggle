f67683b3b96bce59c4fe00d0ab1a825a12aac19d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:10 +0000

    plenty frequent offer fasten maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
