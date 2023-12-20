82056e4afa3cb7d9221a3d8366b125cc52f74a0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:15 +0000

    regularly type manner smile dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
