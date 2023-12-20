441cd1411ed1330e20f0412df272f8eb02d7e1a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:25:32 +0000

    drawing shout already ever else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
