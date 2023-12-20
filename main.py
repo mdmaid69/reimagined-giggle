8944cc491feb9cf08c1556182885ba01846e0319
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:52:11 +0000

    square bag frightened though neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Union:", set(list1) | set(list2))
