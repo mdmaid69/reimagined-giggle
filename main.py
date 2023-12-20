0ccd28b9102d04a398b036862f45ef5f21f5816d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:25:00 +0000

    apply cry gate fire winter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
