81f9ba33c797f48092d2336c2386a992404a3439
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:27 +0000

    message container before wherever arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
