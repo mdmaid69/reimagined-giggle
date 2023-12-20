d2b878d0430d5f065cdc1f22727c1806b56acb80
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:13 +0000

    part wealth intend sail throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
