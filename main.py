ca18e5fb4b5b19de545ec377ca06918a2eae1209
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:13 +0000

    bringup separate live false back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
