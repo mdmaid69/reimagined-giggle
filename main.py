9c5581aac3a60bfbc95fe091cf03f3a75b2b1cd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:50:04 +0000

    why eat favourite old treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
