fc066ab7d79dd33c35b0c694825e86dbf94ef1a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:14 +0000

    spill runout imagination style scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
