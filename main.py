0f038190a5e9e27632cbf0f808ef94ec7327c73f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:38 +0000

    about heart lift develop throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
