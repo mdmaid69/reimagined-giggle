40740497e03ec07e1e21efdfb4eef9a29e6277b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:18:32 +0000

    error hit result advice shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
