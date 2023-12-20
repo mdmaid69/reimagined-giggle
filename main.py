372cc2d4bf47b435bed9865f3eb4be6ea8452f69
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:11 +0000

    take each language excellent real

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
