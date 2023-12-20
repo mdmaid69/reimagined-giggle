bac1e38b1f4616cddcc9d3066e40463ab439a10c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:23:50 +0000

    anyone holiday tend cheque could

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
