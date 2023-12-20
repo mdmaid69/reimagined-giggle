478d5ed51aa96af4f39d52964d7768da9ad04a24
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:56 +0000

    sign noise round 569enjoy many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import time
+def get_current_time():
+        return time.ctime()
