a0a1f5fead2ab9c5b50c1e5bdb1ce66e686b7676
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:19 +0000

    suckme theirs crime excuse confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
