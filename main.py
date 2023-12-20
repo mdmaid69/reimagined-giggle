71da75b13a53e873041147272136879c67dfb391
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:29 +0000

    wrap sister inform advice fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
