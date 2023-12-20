f5b209fa2303d3e56808a0c6df9f1d77e5531f38
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:29 +0000

    runaway minor phone strike kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
