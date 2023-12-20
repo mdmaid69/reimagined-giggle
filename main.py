32b5f1e35b2949bb77122b0e30344c40200fb04e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:34 +0000

    ashamed when silent relation error

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
