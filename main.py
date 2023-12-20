4135e20b570d245dbfd36891c61599325d73acdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:48 +0000

    turnaway Ss hide forward knock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
