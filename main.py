7bc1b86e83b56f33166b8834dbfd8a04c11fbb74
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:23 +0000

    pronounce live earth sensitive back

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
