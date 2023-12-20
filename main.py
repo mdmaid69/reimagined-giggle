91ab61f9aa2ded6a3b7d439afd1aeec30314ef3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:41:34 +0000

    press thankyou only man conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import sys
-print(sys.version)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
