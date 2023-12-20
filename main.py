d43b1ec5281a14b7f8342bca29648ff5a64aa715
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:37:51 +0000

    base strict letter valuable across

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
