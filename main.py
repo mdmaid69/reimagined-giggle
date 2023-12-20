7f4b97c65144656652b0ab8d28bc13774969bcc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:00 +0000

    wise strike hour target hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
