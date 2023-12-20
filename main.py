827fad8a2e210f491e6546b79cbac38c5047a416
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:51 +0000

    shop band value port also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
