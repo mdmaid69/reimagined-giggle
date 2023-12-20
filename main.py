f9a6ed336083788ae09126f5a49dcbf4502b3b17
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:23 +0000

    material priority take after trade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
