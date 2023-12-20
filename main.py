6e4677a1d7537b871fbf629c1046b43e4d7129fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:52 +0000

    Ii absolutely introduction punishment shop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
