740cd8e43f5c6bdf2682865af81b221f02d1cf74
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:48 +0000

    stop criticize dry produce careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
