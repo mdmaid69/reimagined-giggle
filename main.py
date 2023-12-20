84f28b416d50d678aaaf7be79c1ad08b6c5f3ebd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:52 +0000

    own authority sleep medium more

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
