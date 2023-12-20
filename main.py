97dd0fbeb7db3e5add223774a33a382447e24a4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:56 +0000

    shake passage valuable opposite except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
