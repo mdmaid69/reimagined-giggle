ce3fc5b118d1ba17133155bdcf37c5e136b5bcce
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:17:37 +0000

    bang swallow please pay twist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import tensorflow as tf
-print(tf.__version__)
+def find_min(lst):
+        return min(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
