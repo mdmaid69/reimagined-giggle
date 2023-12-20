24fc1e14d241d00245dafff5aa15adfec43ae3f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:27 +0000

    score broken team exaggerate mend

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
