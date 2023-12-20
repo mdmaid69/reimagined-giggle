2467a61f249dd85cadf047b07ea9c49dfa7d4bbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:28 +0000

    speed occasional attract meet vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
