e83d443a9cf71a649cc8431747b582ee885c96ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:15 +0000

    copy sport corner shake common

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
