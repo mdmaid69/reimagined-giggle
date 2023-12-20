8d9c59d605861e1612870ba8b3c0355ccfc24693
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:35 +0000

    danger conduct noone action also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
