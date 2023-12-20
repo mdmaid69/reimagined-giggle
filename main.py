ae6dc02b66d3fb9670b58d1665b4ac5f04c6d178
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:36 +0000

    during turn gentle wake smell

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
