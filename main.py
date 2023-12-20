8acf3aab97b2ea13ae16caa6a698161a46d2ea3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:08 +0000

    pen pity final runaway leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
-def log_message(message):
-        logging.info(message)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
