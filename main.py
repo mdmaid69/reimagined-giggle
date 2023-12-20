ecec4427a500ca73c71501f50159d1523921803c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:37 +0000

    Rr clear roll project reach

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def square_number(x):
-        return x**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
