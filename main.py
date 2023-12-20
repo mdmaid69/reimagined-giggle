087e23ab22ece4f7b74668b03f59adef686b353f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:23 +0000

    size calmly on lost press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
