301fc559065b794ec85ea02c47f1003b1656781b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:23 +0000

    some lift lead clue slowly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
