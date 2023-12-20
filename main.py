ac33755a902810bb9fdceafaf29bba5e895a7a98
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:31 +0000

    along judge search after ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
