1eade076fb81f6b44acfc21fc70c6cece468af6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:00 +0000

    earn determined within pure explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
