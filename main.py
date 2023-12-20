deb1b8a27eef68e732a4343e468e3ad19baf9d08
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:07 +0000

    centre matter reply wake bringback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
