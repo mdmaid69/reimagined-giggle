5344f57cdbe44ae81e2693cfe9c74ef34cad9061
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:57 +0000

    shadow its hot us pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
