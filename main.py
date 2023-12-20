2222fb483c19c1fb7de2fd4f79552e07512838eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:58 +0000

    accept note century phone move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
