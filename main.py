19e107124a6ef581bfa0a9d4defbc2edf6852ec8
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:40 +0000

    satisfied mysterious she makeinto family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
