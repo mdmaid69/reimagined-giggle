512717e2d3ee489ab3c64f88a4645b2576fe970e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:01 +0000

    avoid apply goback(to) like container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
