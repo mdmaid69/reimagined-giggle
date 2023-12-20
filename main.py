7327811cd5f1bda9ffed530e3146f2949adead54
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:13 +0000

    activity closely minor guess fail

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+print([x**2 for x in range(10)])
