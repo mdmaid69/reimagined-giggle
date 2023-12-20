31cbc877d4f6a769841b9843dfcb28fec4056a19
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:40 +0000

    bright approval as swing score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-def roll_die():
-        return random.randint(1, 6)
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
