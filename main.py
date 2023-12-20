ec04234eaf60548ca0222f5f4e25e24f8d511831
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:04 +0000

    guess about actual floor split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def convert_to_hex(n):
-        return hex(n)
+print(math.pi)
