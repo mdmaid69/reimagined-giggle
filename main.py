e533efa3e440139c417c1fd6eb5062fd85a4b2a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:59 +0000

    road improve allow male joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
