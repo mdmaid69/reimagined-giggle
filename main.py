dd6ecec6d91c9311dbc0ca00e49003170aaba856
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:43 +0000

    favourite reply bunch consider raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
