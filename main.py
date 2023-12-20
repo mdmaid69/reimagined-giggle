4a8beab9b98646bfa925b2a6c88cd80a7d7608de
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:42 +0000

    but feeling Jj cause jump

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
