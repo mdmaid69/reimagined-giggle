fd006c1765dc8d8fe4de331bbcad971a61c6f14c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:49 +0000

    prefer healthy accommodation keepfrom bury

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
