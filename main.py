15cd8c5b4605fac100b14f75f0f899f363ef1d94
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:36:24 +0000

    bringup neck perform present show

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
