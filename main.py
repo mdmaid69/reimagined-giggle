d653e34ddf0d8aa64f7ac05d650e5009d229c5d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:15 +0000

    justice better left study already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def get_current_time():
+        return time.ctime()
