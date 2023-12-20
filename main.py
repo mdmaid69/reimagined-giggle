2721353b2541343e334a91fc9cbb0356b3d2a6a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:54 +0000

    almost small passenger authority in

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import collections
+def create_user_list():
+        return collections.UserList()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
