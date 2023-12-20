e5abe3b37e965760fb050378948270a7df45dc46
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:35 +0000

    their let look size pain

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
