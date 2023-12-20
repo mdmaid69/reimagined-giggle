e6e61e5f19f6b170dea4d32c0d4094e310131f95
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:00 +0000

    find bright proud taste pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_max(lst):
+        return max(lst)
