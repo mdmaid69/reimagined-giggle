5894fdc1366f05791f1e737414fdf1db5eba2e92
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:09 +0000

    thick murder charge under minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
