0f9a385b7e890216ed8f374ce7471adc580292a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:29 +0000

    switch bear offer ever tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Characters:", len(text))
