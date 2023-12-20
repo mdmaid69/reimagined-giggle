10f15055498b60adb2fc4a886eec97900efd62f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:08 +0000

    luck gate smooth ill fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
