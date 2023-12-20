4a6a2a9444f8d8bd30eab2868859ede699c221ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:31 +0000

    direct computer few careful admit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
