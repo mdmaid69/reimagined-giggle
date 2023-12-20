f7617b6085124696e4105d2c0ee7df73ab683aad
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:03 +0000

    escape rob Tt listen estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def reverse_string(s):
-        return s[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
