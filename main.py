7f639bc50bf09a472284aff9b52b07e32174c02a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:40 +0000

    gift opponent within honest almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
