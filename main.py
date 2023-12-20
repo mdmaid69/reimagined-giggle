85d4ca72df069ca5f8fefead94dfec8c58588052
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:49 +0000

    prize real towards inquire protest

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_acceleration(speed, time):
-        return speed / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
