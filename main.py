511135f549dac906303c82740be58aaf0c37c9c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:48:49 +0000

    quarter post responsibility cost disappear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
