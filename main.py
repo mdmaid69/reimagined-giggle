94b787c24a4fb71d90d68f4cba28606bd4701498
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:02 +0000

    distance once scared you excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bool(array):
+        return bool(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
