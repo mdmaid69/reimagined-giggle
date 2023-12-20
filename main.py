59c260c21d5bdf8efff225d306f1d58cb7f38ddc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:36:41 +0000

    each exploration attitude practice hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
