56acb238580da2a88fdd5948508d98e9c0ab8058
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:55 +0000

    seat course film what sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(numbers):
-        return min(numbers)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
