3c5795d54368a45c52a87d096b0965b507d4af8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:08:44 +0000

    may bad alive finish challenge

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
