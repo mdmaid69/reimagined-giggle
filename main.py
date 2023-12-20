0a29dbe7c9e270e5bce2429248a4148213650e5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:58 +0000

    description debt want hurry clothes

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
