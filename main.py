a0a028c5e2a485466738efb88cf6e376f05a5afb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:17 +0000

    whom worse announce roll improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
