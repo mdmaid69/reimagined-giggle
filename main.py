52e558a2bb4ba5ed3a3d84d46862dc1bf8b1c9aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:44:59 +0000

    support comfortable possibility innocent leave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
