2f0f9db08c4580a518d1e408c08a924e7f0d1e00
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:17 +0000

    Nn oil moon not fat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sys
+def print_python_version():
+        return sys.version
