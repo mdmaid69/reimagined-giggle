fcd74333b4288c04e37bd2c447f654b92c49c656
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:54 +0000

    mind decision couple Tt Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
