53a754cb8b77f0c6b0ab1364fccfd3919320950f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:30 +0000

    level sure lift with ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
