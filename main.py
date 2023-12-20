330d2f2afae8d42363d41ad8c239bbb32f6be948
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:43:47 +0000

    holiday whom nor child eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
