0ef4f8d219b43ea7ed43bd81053a3b319d475534
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:49 +0000

    announce determined example matter hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_user_string():
+        return collections.UserString()
