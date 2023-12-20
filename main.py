cb728af4d851403411084f5dd5c1f1dc15ad8c5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:19:55 +0000

    contact hurry autumn today godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
