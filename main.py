bc0f8ff869023badcf83905ecda233168291663d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:00 +0000

    translate mark forever schedule land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
