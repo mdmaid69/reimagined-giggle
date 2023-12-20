663c8596f838039c5cdd1712e78245fea089777b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:26:29 +0000

    different Itself shock object mouth

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
