74838f14341c435bf88e9082034e638827824a0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:31 +0000

    uncertain issue thinkof meeting bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
