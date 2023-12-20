eb340b72a5dcc0f0c92d5a9a2efebefac5ef59d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:40 +0000

    flat nearly possible method sweet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def find_max(numbers):
-        return max(numbers)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def remove_duplicates(lst):
+        return list(set(lst))
