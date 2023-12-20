68a8e5a6a7aec233ad4d8d08062d94583485f5a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:40 +0000

    ourselves generally bank pity fortunately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_array_as_list(array):
+        return list(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
