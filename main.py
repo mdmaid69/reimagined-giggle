d5d7f8b9a59fa8fd10ac80a1d3bef95788de8d21
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:03:33 +0000

    angry port appearance dance around

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
