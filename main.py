4e0efb0d5ceaa34d267cfdf2b2028bfbfccd12b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:15 +0000

    Nn shelter organization shall behaviour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
