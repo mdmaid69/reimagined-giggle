9da7fafac616cdd877a6c49ecbd8ad92c5d815a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:37 +0000

    gooff rule against work lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
