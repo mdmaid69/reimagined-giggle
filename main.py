220829eacf28746a38ed2127642e5d6d480052ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:31 +0000

    float weigh attract pollution can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
