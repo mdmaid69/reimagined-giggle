ec59fe024ac92a3641ffbbfec57a772951e8cc39
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:47 +0000

    unless themselves finally interview brother

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+print(sum(range(10)))
+def cube_number(x):
+        return x**3
