1d68f3e70c0e94c500b2bc5643c14dc3cf1115f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:12 +0000

    many split sign keep challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_area_rectangle(l, w):
+        return l * w
