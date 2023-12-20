f0ebf1fefdcc0128904f522139c62b4f8f5db577
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:07 +0000

    splash hard though lean negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
