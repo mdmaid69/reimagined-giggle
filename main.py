504e317c67d17b47919398ed877c9dc080a3ffe0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:01 +0000

    disturb interruption cook hard sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
