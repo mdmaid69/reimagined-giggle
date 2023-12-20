89b87a970bf8ad9def5023f71811afe22f5c4061
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:48 +0000

    about suggestion sleep week long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
