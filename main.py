34c8f0d5d11fe3573e48be3839a337122db56a6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:19 +0000

    pinch mail direction afternoon by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
