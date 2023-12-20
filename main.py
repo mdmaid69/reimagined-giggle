33ade4e265248fd4c499f06b5d95a9816e59c10b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:50:13 +0000

    back view goodbye interesting damp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def roll_die():
-        return random.randint(1, 6)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_max(lst):
+        return max(lst)
