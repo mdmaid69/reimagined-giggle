b61aed0b76af3294d1de68618ad444f6365afd7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:55 +0000

    show whistle argument cut time

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
