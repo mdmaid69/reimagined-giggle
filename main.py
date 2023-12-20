ed8807a85377fa51eac4fa1f68650c7c5bfef784
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:06 +0000

    off Ll chin steep stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
