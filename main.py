91099e9f494467f42ea31d101d738708d375d2d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:23 +0000

    row thankyou cry wide wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
