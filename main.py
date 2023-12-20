cda4b9b108d6f263d179ee94ab4e084184f2e103
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:28 +0000

    her model tooth run near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_unique_words(sentence):
+        return set(sentence.split())
