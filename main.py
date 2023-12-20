1c7ce22860450a435074a019e2dcdb825c78cb04
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:21 +0000

    partner before across hurry evening

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
