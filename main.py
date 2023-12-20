21ca649079ef3b677e40e2cd315408b6a5516095
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:56:05 +0000

    mistake turnaway along build task

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
