89e96565ad43194aa0a3b136e2e456687be02ac9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:07 +0000

    day sister type example trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
