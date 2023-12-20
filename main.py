e9051a75a726eda0d011d48a8de5f2ae80047673
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:45 +0000

    danger fold little getup town

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
