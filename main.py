dfd1ff86adc8b28f1e8f0642558ba8698cf3f176
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:02 +0000

    him believe turnup live hang

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
