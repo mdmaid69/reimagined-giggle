f88bfa2bbe132dfcae457ec884b9cd2afd08507b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:13 +0000

    regular sing answer underneath attitude

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def reverse_list(lst):
-        return lst[::-1]
+import tensorflow as tf
+print(tf.__version__)
+def calculate_work(force, distance):
+        return force * distance
