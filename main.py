4f105687acddab97ad043b6848c23b2454a9f645
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:16 +0000

    ago squash stop meal but

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
