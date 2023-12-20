fda97546f750488db222509f39946fa43d7a5229
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:19 +0000

    square attempt combination address uponon

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_work(force, distance):
-        return force * distance
+def subtract_numbers(x, y):
+        return x - y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
