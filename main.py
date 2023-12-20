a0eaea737cc250e93742fbfb43d8aa393874b504
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:50 +0000

    comeback various Jj need whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
