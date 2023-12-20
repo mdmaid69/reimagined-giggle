a83aeef0a9407ecaa02912f8a950d620015fc692
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:48 +0000

    evidence never teach fast blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
