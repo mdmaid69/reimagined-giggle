dc9e1b047369a22774f4663620604606c6d6aa15
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:55:48 +0000

    tough movement better proper push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
