cae73ec442696b51f4700ff632f7c0fbcfce2482
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:05 +0000

    object behave niggaacademy make work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
