e37e1462d524c2fa510c31d2d180e3c56abe3306
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:56 +0000

    light bring yet wrap spill

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
