3dc2d7322df3e52522134a899e09c71ca82ae427
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:16 +0000

    trade favour callup concern crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
