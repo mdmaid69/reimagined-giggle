de9e394a33247a969f68d3f735e50b4be03cbf25
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:22 +0000

    grand total spend popular care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
