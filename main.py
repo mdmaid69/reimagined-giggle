7e92a05acf67c5bf9570628abc9694d29db14d3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:18 +0000

    matter contact our talkback rain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
