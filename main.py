a2bb4f30f5fad058ea7c8a3f620af72a5a634830
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:12:52 +0000

    leg taste regional borrow worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
