7ac0f19feb9da5b6880e9136c7f075847f247667
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:46:51 +0000

    if support print bored fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
