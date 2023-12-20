fa3a4557cedea0e7900f94d1dfeb2a735c337c98
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:22 +0000

    give platform wire free can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
