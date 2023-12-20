4fe023b3dc5f613a39964d00539cd4296872a073
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:41 +0000

    live while excuse pocket excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
