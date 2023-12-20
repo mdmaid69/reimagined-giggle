b6f209954473d61ce168cb973555fc9c06c4d5f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:36 +0000

    slice stick modern impossible contents

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
