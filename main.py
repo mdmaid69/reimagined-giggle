205fc3098ee58593761cb645327d63408ed1c74e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:48:15 +0000

    distance production might dish respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
