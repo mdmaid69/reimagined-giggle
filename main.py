22792593386dfcb1f828ad7b5f7f6101d1fb27b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:43 +0000

    use record unhappy nor me

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
