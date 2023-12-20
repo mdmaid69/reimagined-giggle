74cc3f0f2391494c9b0dabf6f32cdf8bb1dd45ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:21 +0000

    once stick crash poison discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
