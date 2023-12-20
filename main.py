072b49567c274ffafc8465300908823c6a616b48
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:57 +0000

    respect cheque see height constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
