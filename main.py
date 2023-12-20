17d7669302df233ae57d9b9c2a8d7592a693d1a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:32:18 +0000

    giveback sure lookafter bus else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
