b7ac972c933475e9af1b5fe0bf08139a41918747
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:37 +0000

    dry experience whenever original relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
