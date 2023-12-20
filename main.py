79411ecd7215f7bb8cdf331b4bf0bc9a211fba78
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:16:41 +0000

    whichever cool pretty content pocket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_max(numbers):
+        return max(numbers)
