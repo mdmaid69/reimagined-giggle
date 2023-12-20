627fded96a25921c6825713d4289795cc224f2e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:04 +0000

    exciting rush neat plenty weak

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
