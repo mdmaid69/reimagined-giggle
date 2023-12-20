de62763fe0f52ba5966145a8b5f551f69f169596
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:09 +0000

    powerful oil dish I mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
