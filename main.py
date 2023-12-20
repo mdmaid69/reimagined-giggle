6f684b520000e8be62b01543421f58b25d22aa96
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:41 +0000

    trap borrow family control after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_current_working_directory():
+        return os.getcwd()
