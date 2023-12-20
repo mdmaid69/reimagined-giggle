d919e0b800a2351862cfa538f7104ce16b9316db
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:26:46 +0000

    someone state stone getup reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
