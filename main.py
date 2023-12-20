3eed9fe9c89afff067051a09b35acd5301632bc3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:55 +0000

    water him rain murder design

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
