108c5a659211de064f0e029c496f597b20aa2c40
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:10:30 +0000

    book ashamed lead care myself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
