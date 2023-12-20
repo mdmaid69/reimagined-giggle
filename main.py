1b0fa8861ab1bd2c6f119c1cd688bf44df9b7738
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:07 +0000

    second unlikely waste seriously increase

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def cube_number(x):
-        return x**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
