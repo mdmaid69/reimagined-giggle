786b5aad8d7434cb55bf7a3577b2b59cea13f687
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:58:08 +0000

    swim actually convenient method wealth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
