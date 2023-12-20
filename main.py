214cc7daf8256b4702cb44a9b793863edcdd706f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:24 +0000

    willing getoff plate must shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
