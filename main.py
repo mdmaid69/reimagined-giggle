dde1ad2649ef8836f6ab381be255b2a8c9ff3064
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:47 +0000

    authority perfect familiar prevent pot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 text = "Hello, world!"
 print("Uppercase:", text.upper())
