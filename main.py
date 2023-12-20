8b036d927ff04f5633b45896b8b5bd1e449eaf4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:20 +0000

    introduce principle around perfect shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
