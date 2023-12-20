6c565645b7c1d5c259c72bbbe044fe85ad5869b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:17 +0000

    worst grow string introduce near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
