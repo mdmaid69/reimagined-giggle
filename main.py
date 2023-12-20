37b01aab848529951579e3728ea75a0219ee14f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:43 +0000

    sensible service fond leave our

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
