a6542e6c07d8cb5e7eb8205608014e70fdff67ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:57 +0000

    determined capital refer land point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
