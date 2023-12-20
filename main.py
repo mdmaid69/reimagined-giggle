f54f4fd07cd5f78081d37a9a8d1e70e6a70d874d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:58:11 +0000

    knock free object pause touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
