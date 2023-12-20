daae00eba85231df1cd75f4d668f5de18056ab8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:08:58 +0000

    light cut eager us welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
