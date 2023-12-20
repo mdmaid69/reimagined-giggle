1ca35e67673577480966ea702466db02bf9704c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:34 +0000

    war fairly remark design bottom

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
