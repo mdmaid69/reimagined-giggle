b1a90c66209ed3947a00e126c9cfb77d8a947407
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:48 +0000

    little spot return rescue shop

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
