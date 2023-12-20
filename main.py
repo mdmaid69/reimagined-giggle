4b9af36292962049ad255ad7262f29a2357b3b37
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:55 +0000

    office foreign community ahead fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
