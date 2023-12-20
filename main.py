d7b2ec076051760127d2c19f063efe3ac99fbefc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:09 +0000

    advanced eager would lost direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
