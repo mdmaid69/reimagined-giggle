510036d29ecde2ee11d2a9e951d1419dd4e86421
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:49 +0000

    book even mean object polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+import re
+print(re.match("h.*o", "hello world"))
