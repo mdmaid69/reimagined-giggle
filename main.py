38e48564ed411f75ac359779e4f4b07df31f7f3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:20 +0000

    mess crash damage set top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
