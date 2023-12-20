9470e5b9d922161207ba8f61e13424d68ad11cca
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:39 +0000

    under pretty free let wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
