00068e7400f70699c357a8abee7d2ff19f106eb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:11 +0000

    shame serve train negative force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
