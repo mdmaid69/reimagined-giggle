4daa29118c6dd9a346bbeb86beda4204a4c15348
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:07 +0000

    sensitive beneath careful absolutely allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
