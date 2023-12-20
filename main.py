dfcded4220b493d6013452cbb033e3673cff9446
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:51 +0000

    follow pair bringforward lonely certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
