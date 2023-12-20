7a866765e61493418d8ca8d2281731f0b59d5d75
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:32 +0000

    fun organization size refer right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
