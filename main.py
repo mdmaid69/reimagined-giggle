bd002d67bb317a04466d10c61cd85c79c0096ed8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:08 +0000

    sound sensitive such print apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
