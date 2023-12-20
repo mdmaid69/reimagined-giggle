a8894858caa91231653e38913fa805f4d0d384cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:07 +0000

    steer believe cruelty strange wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
