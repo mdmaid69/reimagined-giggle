f07bd4f83b10c45ef135b6db7eb7140dcae69dc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:52:44 +0000

    attitude treat across morning interrupt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
