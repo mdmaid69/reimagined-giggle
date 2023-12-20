2b2e8f4a88d81eda0ad9b816d52dd82fb6415182
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:40 +0000

    to book really try breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
