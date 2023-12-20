0a1464cfdb2f87ef5801b522481c8c2a4684215a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:09 +0000

    duty claim voice underneath fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
