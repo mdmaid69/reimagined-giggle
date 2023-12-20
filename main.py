5619590c713a70d91bbff4abe39f6507714deb6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:04 +0000

    software anxious gentle trouble enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
