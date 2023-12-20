a8e4a888e93ece62c637faf478e69cd2cc6c855d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:35:04 +0000

    land pure exactly smell bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
