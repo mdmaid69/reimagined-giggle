89a465e82df72108b35c4d0ef52a8c68d2e6ac18
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:57 +0000

    section moment anyone talkback level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
