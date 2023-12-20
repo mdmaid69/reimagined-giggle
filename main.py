20f0d1ff41b52aa85b42d910323ddfb4eaf12e36
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:53 +0000

    turnaway century part nonsense fit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
