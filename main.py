dd712a2b9584015e2931656fa6a9ace7f136fb16
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:35 +0000

    software OKorokay Yy insist sit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def square_number(x):
+        return x**2
