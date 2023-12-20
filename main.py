2cfaec612aa3a399a8308e69409d51ae7207e552
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:49 +0000

    free taste light sorry guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
