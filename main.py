abcc345b345b9ccb5b934594cc1d75cbe8369e75
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:11 +0000

    operation nonsense happen holdup knowledge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
