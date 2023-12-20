d6baac1ce5fdbd8f060fa8fe1090ff952891f574
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:41 +0000

    measure concern clean talk life

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
