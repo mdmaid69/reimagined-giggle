c163068403e7dd06df8031cb4c10a4a71fa05f79
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:30 +0000

    also giveaway search scream hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
