2a013afe8c9495d303519bb1b95f888916b44c1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:08 +0000

    interview peaceful goup exchange left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
