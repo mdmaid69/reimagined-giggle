41a5f1583f8cd9093271ed1bce8bf4a140f1a112
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:36 +0000

    runaway authority precise mad length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
