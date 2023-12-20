aed89d915e0191ce35f90db0b903e9a04ee59e3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:22 +0000

    future meet clever below feature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
