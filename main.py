dddcf7995b96cf8907526df9d5c69746d4e9aede
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:37:38 +0000

    nature pain public respect discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
