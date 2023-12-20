1313c4c3914ede0199dc3b676ea0327cd79d0464
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:47 +0000

    solve sudden respect except period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
