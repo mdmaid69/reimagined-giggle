39c4c4a757fcebfbd340aec9a3306e6ca11ce008
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:16:32 +0000

    shock recover mail properly phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
