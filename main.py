a3b5b6f4c6d5b1290512f14361946b33ea89f467
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:15 +0000

    best say door sure occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
