ca70b79d4f335c8b12bca3f3d23ad3c40ce24b0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:06 +0000

    expect control wealth experiment nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
