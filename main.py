b7b36c5af40dbf5ba1899d3a724b71498ea2124d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:21 +0000

    absolutely like black sleep how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
