8ae676d4904c7e2ad9a608516da2f46a90458404
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:27 +0000

    spend insist change just another

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_rectangle(l, w):
+        return l * w
+print("Hello, world!")
