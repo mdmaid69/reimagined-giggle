affefb882ddae9605efdee4e5203322e16c145b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:59 +0000

    even evidence worth introduce benefit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
