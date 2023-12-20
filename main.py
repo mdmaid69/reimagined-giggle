1a084e8093fb87872d6ae86cc27ea898e6f64582
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:41 +0000

    true embarrassing instrument boil cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
