049c9b0d2b6ec904d45b057df93c89054eaeb10c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:57 +0000

    getup any convince innocent sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
