3b6816fa6f038ce835e897a041c923cd74f4698e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:06 +0000

    pinch surprise oil wave cheek

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
+print([x**2 for x in range(10)])
