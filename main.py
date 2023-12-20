346ff5d3222e99545d30cb8384cab64fa44f77a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:13 +0000

    completely upstairs gather rid exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
