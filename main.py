0320a4eaf434ab4d546de71c6f86c45fdd590aa7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:40 +0000

    peaceful children tip neither part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
