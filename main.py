fb24d29d6b8b047f35f02c545e0f303855e658ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:00:38 +0000

    battery attach gate shout around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_current_directory():
+        return os.getcwd()
