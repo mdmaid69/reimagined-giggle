ac4d519751db722f6a2e8ff1fb469f5dfc3fc8e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:52 +0000

    journey wake tend rough takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
