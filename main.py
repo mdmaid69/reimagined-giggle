4482574a1ab4526245ea9ce66c1e2116e5add312
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:43 +0000

    practical affect winter judge smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
