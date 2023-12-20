668761ae039d23f7dd7f51fc7588aff212266fb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:15 +0000

    shape agreement shout past only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
