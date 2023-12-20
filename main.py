2f1f1204ea4f3354c520508ad8d5e06dfa4e0f63
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:01 +0000

    prevent sink reduction man second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sys
-  def get_python_version():
-        return sys.version
+  def split_path(path):
+        return os.path.split(path)
