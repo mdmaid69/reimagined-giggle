5d335d6f1c26f2376a2f17ac8fa04d25875f6241
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:12 +0000

    twist gift something rescue tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
