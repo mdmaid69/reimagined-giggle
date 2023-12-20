f51d21c2a203013085ce5774d5548010c68b8e11
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:48 +0000

    during getoff trouble female except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_counter():
+        return collections.Counter()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_repr(array):
+        return repr(array)
