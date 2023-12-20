bd6eb67e4b058158e242bd62879b2cd0440313f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:27:19 +0000

    passage behind sore cut bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
