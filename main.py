bf3555dede9b133d6c70a088313db3f4484bef3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:26 +0000

    relative plant lucky blind not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
