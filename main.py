2cfa833340070f347bd7021f8934f93a46d5327e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:27 +0000

    sea stiff autumn tall payment

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
