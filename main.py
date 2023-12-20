40287c2956407a318ddeedcf7b868463fe90eaa5
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:40 +0000

    tight Nn rent office over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
