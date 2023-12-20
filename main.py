24797511219a9a67da3c33f86f6ee68c79916364
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:55 +0000

    principle love combination whisper keen

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
