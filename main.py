20e7f0e53feb5003651fb22daf9f0275fed5cb3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:23 +0000

    understand cook remain calloff criticize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
