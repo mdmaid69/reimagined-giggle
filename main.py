aad2d59ce949a9bee4324a2803de4e6ed8689e5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:53 +0000

    memory tour please dish listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
