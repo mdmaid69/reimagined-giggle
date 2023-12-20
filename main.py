7271463e7150c18db265418022de7810313c1e89
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:50 +0000

    guess print spring breakin private

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
