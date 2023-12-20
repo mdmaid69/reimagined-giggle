337b67c620c0918c4a6afe6d5a50f970cd68b420
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:28:17 +0000

    Nn side gradually best get

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
