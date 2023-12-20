bbd1dabb0796f8c9c7190819d1959d080f13ae55
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:51 +0000

    first base round bottom surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
