26387a57a3cdc9588938b36cbee2650c356fa48c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:45 +0000

    allow leg heavy pleased grateful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_pressure(force, area):
+        return force / area
