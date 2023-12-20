61b3389e41fd602ba8b586fe33c665170dbb2e3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:18:13 +0000

    cheap anywhere lay related woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
