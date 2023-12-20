389193992b565e12033d404b91426b4608a7db5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:05 +0000

    expect length hospital expensive I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
