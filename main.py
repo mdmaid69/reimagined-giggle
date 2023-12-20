596de515419bb627621705a5b690aa26cbc96f12
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:32 +0000

    teach interfere hungry give instant

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def cube_number(x):
+        return x**3
