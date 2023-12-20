bcd52a634733994f849a6f48eab524d453df0bf7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:07 +0000

    wait could person steam legal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
