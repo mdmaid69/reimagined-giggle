c74fad3bd2bcdcf986f89001f28acc065feca757
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:56 +0000

    taste smart really human car

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
