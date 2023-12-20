d6e9408b1816d1ade35dafe1a4985c9f951dfa9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:07 +0000

    present air little where leave

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+print(sum(range(10)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
