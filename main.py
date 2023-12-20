dcdab763f73b6d146dd0a827c1c373a49021215f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:30 +0000

    official agree hear pot attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import time
+def get_current_time():
+        return time.time()
