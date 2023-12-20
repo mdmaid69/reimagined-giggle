427db42983b24d9f743c7a618133826e4cb1bd28
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:10:57 +0000

    scrap ocean with contribute emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
