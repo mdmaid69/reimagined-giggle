c6f0957437da01c8252d654bf32d7af0c59da4a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:32:15 +0000

    attention value burst concern because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
