71fda8bd82343964dd539fbe43a5615b29816622
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:01 +0000

    bath callat drawing rob branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
