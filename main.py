31f1ce7973725741cae7972bf190e694fa5e3af5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:15 +0000

    in press believe petrol treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
