bf13422988225f6d4ad23fa23cac27670f0de64d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:55 +0000

    567enemy exciting tap pass thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
