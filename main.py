98c00a68c50d352f535bc67224eedf452bb4b94b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:11 +0000

    actual home closely you school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
