90c66a7752d1583828b79d0a0f2f87cf5651b403
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:07 +0000

    miss uponon wherever unless bang

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
