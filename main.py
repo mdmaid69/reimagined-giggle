648f9e82429ba3d1d316597accb110bbfe007dca
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:29:38 +0000

    myself play concerned aware house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
