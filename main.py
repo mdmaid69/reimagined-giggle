5a9edba834befa0e3e664820c8e154d008f4ceae
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:12:28 +0000

    prove belong safe rule here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
+def calculate_acceleration(speed, time):
+        return speed / time
