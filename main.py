9938eed228a28ddd24ea76d19029757622f35b00
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:45 +0000

    left combination pick sad laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
