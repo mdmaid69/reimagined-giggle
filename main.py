35a7b40e7ced159bf5b75b161166991cfa025ecc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:14 +0000

    goaway promise front neck beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
