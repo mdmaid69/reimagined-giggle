2a9fd0fad490c56fe171255b2ab25a1c5185f869
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:26 +0000

    event real come let attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def multiply_numbers(x, y):
+        return x * y
