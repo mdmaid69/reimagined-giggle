8f85d6998fb730471f56ae54c998a2ff6f6d6163
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:12 +0000

    lock forgive polite light follow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
