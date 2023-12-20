ba23e2d5e893adb43bcbeddf953ec0fcac04d24f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:47 +0000

    furniture used press involve check

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
