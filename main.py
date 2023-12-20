1f1b27f482589a0b4d9cb371c19d23d7dc4a376c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:42 +0000

    tap ocean 179bite marry decide

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
