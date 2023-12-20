88e814acc97a6d3b97842c1a12c2db2b45983e88
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:11 +0000

    exact foot break narrow pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
