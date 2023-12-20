5625f85c2f9424e9f1020c95cc5e96ab8d205d70
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:39 +0000

    big brief shallow tooth tread

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
