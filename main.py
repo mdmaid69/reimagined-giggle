7edc5ef093ffe5266121349200d25918983aebb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:43:27 +0000

    black pain both stick last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
