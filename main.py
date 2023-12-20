4dabb518bdd07defce0a73a61975360b6beff1af
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:04 +0000

    fence steam pipe tend run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
