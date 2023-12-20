90ed70cce87a9c19136d4f73cae6482643e307b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:41 +0000

    nose downstairs lookback(on) basically crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
