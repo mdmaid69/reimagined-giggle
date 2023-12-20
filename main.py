bc3f02e217086f8fee85fb0cf372bae538415421
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:32:14 +0000

    exit certain range Vv rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
