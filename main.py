878260de42ccab04326a63da7016afea525f3f2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:00 +0000

    rest track attach ready smell

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
