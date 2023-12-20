0bbe48f89f78576c6355406eb710a6f3973097fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:05 +0000

    exist stop connect contain steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
         return sys.version
-import os
-def change_working_directory(path):
-        os.chdir(path)
