bd55bf614dbca87da335f935b2c2577c5e7714db
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:30 +0000

    crash conclusion decision mine bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
