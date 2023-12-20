a3935233d3644ebf01c7be0b11e6b54a57f88b17
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:47 +0000

    safety birth least talk experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
