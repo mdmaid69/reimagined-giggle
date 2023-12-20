a1c0dd80af9673575997c3b3c382ef3090b4f6c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:57 +0000

    workout ring along swim describe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
