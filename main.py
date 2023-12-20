d2fbd5b1186abf1b8acdd0848c41167f7aecf430
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:43 +0000

    steep shrink Itself if difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import datetime
+print(datetime.datetime.now())
