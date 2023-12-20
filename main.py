802902ad941ffe01f7d404e55da6e7d77fd1b468
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:51 +0000

    Yy like fasten middle any

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def cube_number(x):
+        return x**3
