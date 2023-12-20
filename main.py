82608fe86f1e87c755de5aeaac9fdc420e2a8050
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:22 +0000

    lookup disagree home match beautiful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def exit_program():
+        sys.exit()
