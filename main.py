98afb771a8c966bb03432e39b2c69b2224d53e9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:43 +0000

    obvious repeat star swim finger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
