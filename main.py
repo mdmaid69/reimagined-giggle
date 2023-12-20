d62a4f582b45c50aa9f3a3108f34426ffe5fc4f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:30 +0000

    watch match wish least argue

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
