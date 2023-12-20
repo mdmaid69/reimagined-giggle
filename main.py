30be0b85a36d4b3e6606eafc2a89256c7f76a4c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:39 +0000

    commit themselves next takeaway description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
