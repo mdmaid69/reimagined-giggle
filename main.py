d8b2ba72b9e807389d4d9667352d032f2b676502
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:57 +0000

    many lose emotion content some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
