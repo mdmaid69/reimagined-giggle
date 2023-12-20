f0de433d63082d350d7fdc2e59cae894de3317e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:05 +0000

    convenient satisfactory flow so firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
