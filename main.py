7775a27c4fa40e062266599564859bf184ea18f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:24 +0000

    flight wipe alone aware sometimes

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
