bf912e2b4b62109b64d4dd4639eaa6bdcdf51d4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:11 +0000

    strict laugh food regard next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
