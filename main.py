8b1930c59486a498ba3a88b13b21a80ead499d59
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:56 +0000

    sure likely danger praise lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def sort_list(lst):
+        return sorted(lst)
