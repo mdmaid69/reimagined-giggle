0936a1bdbd417e87f1463531ba0bd694c9fb590e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:00 +0000

    apparently later maybe typical upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
