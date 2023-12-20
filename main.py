a3fc746310d5be539358606776ec9fecc28d0422
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:33 +0000

    nowhere last bad Bb 179bite

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
