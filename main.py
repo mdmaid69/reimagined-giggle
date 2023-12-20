8a2d671bdcec96bba9b20ffed3afad597fc7d0ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:23 +0000

    march manner welcome store takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
