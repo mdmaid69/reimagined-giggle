167d27e346dd5e0ad9d90ae442cc75829ed89e7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:08 +0000

    real along constantly behave heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
