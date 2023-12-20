b5ca058029a76a676838e64925bd74de48685d37
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:11 +0000

    style unpleasant distant brother along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
