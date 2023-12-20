ce59ff86a9daa39a215be8bc7f997a457053445e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:14:14 +0000

    count worth introduce negative time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+name = "Python"
+print("Hello,", name)
