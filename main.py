9d556477e8862c46e3248eee21343635de83d6e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:22 +0000

    tap dream sign pure plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
