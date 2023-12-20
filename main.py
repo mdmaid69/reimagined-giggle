10cbb2cbb7688adcf7f623fcf4c9101ad340b6a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:28 +0000

    wonder debt move shade either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
