84b6ef9fb99719ce4869ff0e052d85de6d8495db
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:46 +0000

    unable entry wing fact easy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
