1c7afe50a735df2f827c9bce8cc10adb54d034e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:36:01 +0000

    effort hair reduce definite pretend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
