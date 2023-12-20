d3d32e4e9eb91172a073bcc381d091b5c2f89529
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:53 +0000

    language successful respect this circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
