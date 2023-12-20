462c61514a1c95baefd597d72fad58dd8594aae0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:15 +0000

    Jj ready square roll precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
