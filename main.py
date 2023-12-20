a49273f8fa51fdde0cc640a05b2a04c64f41b002
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:27 +0000

    say load middle support most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
