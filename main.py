c1a310403a49fc8406e5e0342370cf83d4f50bda
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:42 +0000

    difficulty top rent brain music

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
