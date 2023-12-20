4f472c23fa6d37d15bb5c6bc27051f12b9826c9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:05 +0000

    detail sea dare deal owe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
