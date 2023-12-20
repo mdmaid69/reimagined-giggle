5b953e38595590e9270142e20e1cbe495b5f0a08
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:52 +0000

    teach material actually life so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import tensorflow as tf
+print(tf.__version__)
