d1107571b0baf93c95570a098776c68e95d5379b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:53:17 +0000

    joke trap runinto pay wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
