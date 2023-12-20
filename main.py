64dc20a17df7aeac58982051de425b5539b4fe25
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:26 +0000

    contribute left satisfied various complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
