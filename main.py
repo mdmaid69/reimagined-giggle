86e597750c405b6db2f63c98c4bcd69e7fe83d6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:16 +0000

    recently factory calmly fold breathe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def convert_to_octal(n):
+        return oct(n)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
