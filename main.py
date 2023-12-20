7ee9dc7e0504ea28c0ab66ff42f678e2ff4997e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:44 +0000

    copy thinkof keepup strict blind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
