588665be40606446906357bae96ce54bba5929fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:49 +0000

    listen talkinto rain art trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
