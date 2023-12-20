8aaeccb6c31875312d23b0ddebed145ea9d347fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:36 +0000

    phone expression 566end water conversation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
