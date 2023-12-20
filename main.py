e263efc1162c3892f0d100e8a8d911e4c11696f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:15 +0000

    zero school manage carry wild

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
