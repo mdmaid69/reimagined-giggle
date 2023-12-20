f60053788a5861506451d820c14f004223451059
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:29 +0000

    afford harm pair determined used

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
