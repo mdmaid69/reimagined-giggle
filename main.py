841c216235e9fc38103fe8f64b6cb0b86beff47e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:46 +0000

    card strike trap narrow uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
