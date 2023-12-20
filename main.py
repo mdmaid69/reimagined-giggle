2d38f9896651b9dc6584191beae7aa8c1d86c016
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:30:11 +0000

    tour takeoff rest smooth behave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_power(work, time):
+        return work / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
