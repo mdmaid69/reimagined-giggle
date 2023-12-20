83f07f791a6d644cbf07da895f7056f786d646ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:18 +0000

    bear takeback when out building

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def is_odd(n):
+        return n % 2 != 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
