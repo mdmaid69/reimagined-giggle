bb045530ed0afbadebacab1f5f3009c3a305fecb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:47 +0000

    appearance sit even size pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
