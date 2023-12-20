96cd9dc13b10d9de73f2b543cd5c8bd805729148
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:40 +0000

    Ee partner positive equal friendly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
