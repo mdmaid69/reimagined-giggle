4e4a909c3955f48a882e73f8cbdd100519b85a56
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:05 +0000

    wall gate impressive wipe reliable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
