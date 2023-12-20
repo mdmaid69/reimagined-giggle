5e400f52d77488d8533d07bb779558f59f778f27
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:01 +0000

    food increase both pause cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
