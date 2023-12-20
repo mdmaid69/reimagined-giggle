a78eecdcccd55a2a3ebe1999a816da1a2dda1882
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:54:20 +0000

    foreign busy you twist remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
