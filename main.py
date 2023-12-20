d0d53ba316b93057a5cb2784b73448e368552497
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:55:21 +0000

    attention before there short backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_area(radius):
+        return 3.14 * radius * radius
