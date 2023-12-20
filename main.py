05a339a3dfd6aa9391695bdd30586d50f32f9bbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:09 +0000

    makeup quit obvious shout difference

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
