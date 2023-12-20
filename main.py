df8f8c1efe11af424e0dd0298bd84931187ae216
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:39:51 +0000

    sensitive cause plant study good

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def reverse_list(lst):
+        return lst[::-1]
