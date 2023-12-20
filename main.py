bc1fe7f678ae09fa99be0bcb84f89170901f88cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:20 +0000

    copy difficulty giveup friendly scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
