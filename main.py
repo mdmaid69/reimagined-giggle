341f45bed6d611d6122034b85c07e353c054b0c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:23 +0000

    except emergency remember reply callback

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
