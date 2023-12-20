f70af90db1e60c80d45f9bfa9d61513e980b7f16
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:52 +0000

    state later difficulty also turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def convert_to_binary(n):
+        return bin(n)
