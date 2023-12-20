bc8ee6679319ae3a8a91d48398bfa451a05caa95
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:00 +0000

    anywayanyhow mainly afterwards bury of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
