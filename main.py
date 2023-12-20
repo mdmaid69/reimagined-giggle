9c46425c446467bf2f074648ae30cac246976597
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:45:53 +0000

    greeting half hold bag advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_elements(lst):
+        return len(lst)
