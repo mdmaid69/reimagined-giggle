2568c828229283b9919aaeba12b060ebf5569911
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:06 +0000

    petrol dinner marry would shame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+text = "Hello, world!"
+print("Words:", len(text.split()))
