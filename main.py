c6ef95ce90176953eb3d63615bf8b030a75378ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:11:47 +0000

    congratulations situation shrink set gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
