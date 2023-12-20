50ac03a8c43692410f851b404e62fc0f9d37b58b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:00 +0000

    market people bye warm none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
