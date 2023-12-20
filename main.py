1de2c24362ad3c86819a27ab35b4c1c183e7d884
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:44 +0000

    unpleasant pretty market way since

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
