40cd41fca536318cf20475eefb0e91d9499df043
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:00 +0000

    earth fasten unlucky hurt interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
