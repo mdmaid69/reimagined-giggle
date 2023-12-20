9fe9c9fe62e9919740bf76e8bf1cd9b09fa6f013
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:03 +0000

    last second into distance foot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
