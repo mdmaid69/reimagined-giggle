305075bab4ff3d3f9690c5a9d52a9f493668e302
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:23 +0000

    thought father able search about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
