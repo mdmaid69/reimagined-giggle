379e3c7194a7765c78ba8a92288909712350ae35
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:11 +0000

    but authority temper later hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
