721f4b59fcc6903db828dc1a0db36d5f7f819a57
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:23 +0000

    expensive study age tooth small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
