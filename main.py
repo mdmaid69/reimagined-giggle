8fd35348d24d7a43966ea45aebe909af5fb69c36
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:49 +0000

    balance performance release woman scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
