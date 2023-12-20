d93159d48db397e5733897ac0db5a2f79496b2d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:46 +0000

    ear service taste invite calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
