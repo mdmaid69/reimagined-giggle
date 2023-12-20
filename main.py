b40c822ac7e6427f644852836d4e10c429fe5901
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:05 +0000

    neck century need goout however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
