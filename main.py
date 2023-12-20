8463891c1710de15aea0f7acbac3938e981ca784
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:39 +0000

    whistle whenever simple successful all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def count_elements(lst):
-        return len(lst)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
