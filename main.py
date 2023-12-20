1ed8fad22f5ba580ffea58dc6f23d81cb00b082a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:21:24 +0000

    quality gap neck coffee middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
