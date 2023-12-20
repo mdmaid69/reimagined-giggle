14dc969bfbde2f222e9fd491e8b024ce01cef628
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:14 +0000

    form guess criticism somebody colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-name = "Python"
-print("Hello,", name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
