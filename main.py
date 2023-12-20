3c27d1a01766693b4fae6cf52517a37bd322ed07
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:10:17 +0000

    emphasis turndown wheel fast knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_speed(distance, time):
-        return distance / time
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
