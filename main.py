20c213ec546b1bce5b1c38276f4831c7d0089e43
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:56 +0000

    tidy storm free concentrate generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
