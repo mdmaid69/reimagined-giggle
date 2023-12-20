0b41c1a172c533ec71f982fb38b6e06dcd5fb8c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:57 +0000

    discussion achievement include pen afterwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
