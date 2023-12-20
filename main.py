b05ea0c9fb050a47c8e4f9c7f8cbff9dedb56819
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:45 +0000

    our depend funny ours collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
