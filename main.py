040c5eb7ded4634805842741279059f7dc8d29f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:01 +0000

    also shop tongue force mess

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
