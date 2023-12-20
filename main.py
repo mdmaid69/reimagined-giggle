ee4ed11b399f44b07dd84c9952a30959bf01e68a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:03 +0000

    fix to cum wake describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
