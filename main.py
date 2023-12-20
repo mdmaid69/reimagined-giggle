40e973eb1e716f891c3fa8fd13a1cc23ec735185
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:59 +0000

    immediately picture pure car crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
