d12990b07bab3d0cd0f0aaebb5c0bad102c34904
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:26 +0000

    car score unlucky reaction dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
