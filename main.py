318a76ce20a23b19159dd0187dd2bc581ee37cbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:32 +0000

    occur month lookafter build fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
