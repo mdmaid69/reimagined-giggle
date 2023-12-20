5d39db2fa2af77ef2c1e8abf2e7f5c118b5bbd4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:49 +0000

    high serve process pay ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
