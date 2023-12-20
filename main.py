78b86056d6715d247f5141085d944c4361fcd7fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:55 +0000

    precise mess fat fall ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
