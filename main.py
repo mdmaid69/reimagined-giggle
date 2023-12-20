3be78d182e73394d17e76cd71669ca24b0dc415a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:35:49 +0000

    catch wild poor future quarter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
