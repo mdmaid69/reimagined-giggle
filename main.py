5553ff45e2e55e4afcbebae8a7a5c9a761dee034
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:03 +0000

    callback steam cut season less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
