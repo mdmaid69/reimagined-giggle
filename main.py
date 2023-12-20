23a92a4e0372f02747c7e7e1a5dc820b581aa057
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:52 +0000

    once border greeting worth blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
