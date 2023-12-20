62573b8ad9227c0ac356451ebf9c959951e4bb45
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:38 +0000

    actually return safety operate after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
