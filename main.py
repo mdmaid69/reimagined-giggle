eb257d29838034d96bb80a8b20b152c725598f78
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:53 +0000

    measure breathe open fond guess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
