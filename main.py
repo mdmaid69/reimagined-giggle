e1f810242454d5a1846d3d59578f09f0ecfbd87c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:48 +0000

    unhappy fall tight add brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
