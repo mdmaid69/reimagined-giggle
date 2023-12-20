392f4a8dedb240fd8dfb305d3fc53d0f344d4c07
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:51:48 +0000

    successful hope well different winter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
