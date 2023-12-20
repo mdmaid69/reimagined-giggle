4e93399a353d1b069d8076c0eff5d88cbd1eda23
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:17 +0000

    object curious joke play region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
