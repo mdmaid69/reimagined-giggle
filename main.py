cc5caa622f42b70a7605834215a25a36b832c9f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:24:11 +0000

    laugh phone traffic base direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def reverse_string(s):
+        return s[::-1]
+  def square_number(x):
+        return x**2
