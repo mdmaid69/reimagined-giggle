f0556209041d1e9db1d4a4d7d7a9f75ce45397bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:13 +0000

    exchange gap reply active that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
