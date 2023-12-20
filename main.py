0c9682da79ce0531bf3673a2121e6190da09e7ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:30 +0000

    back expensive slow too talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
