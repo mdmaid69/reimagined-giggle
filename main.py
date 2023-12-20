c27128aeb34508b64217a619855786f00f70090e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:39:43 +0000

    fall up minimum throughout lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
+import sys
+def exit_program():
+        sys.exit()
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
