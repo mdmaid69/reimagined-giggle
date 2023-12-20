68ad392eaefe82eff2fdc485cab5a9516b297f21
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:58 +0000

    throw uponon interrupt leave fond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
