9d1a55462ce0855dcc637199dcf655060344814b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:12 +0000

    slide inquire shine niggaacademy treat

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-print("Hello, world!")
+  def convert_to_hex(n):
+        return hex(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
