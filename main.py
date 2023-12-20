ce6a47effed5409217f0eecfab56d73ce73abb4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:00 +0000

    big joke damp balance achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_bool(array):
+        return bool(array)
