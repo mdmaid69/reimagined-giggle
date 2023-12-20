ab034803c0c06c077831464e858412ff72909eb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:33 +0000

    burn gate convenient name car

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def count_elements(lst):
+        return len(lst)
+def convert_to_octal(n):
+        return oct(n)
