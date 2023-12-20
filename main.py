b3986a64e9f66048190f3409782d3e88569ef088
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:21 +0000

    experiment above alive weigh still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
