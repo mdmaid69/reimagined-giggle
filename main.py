725fb590884bf0ce17f606b3e686504262bcbebc
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:52 +0000

    curious scared learn basically maintain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def add_numbers(x, y):
+        return x + y
