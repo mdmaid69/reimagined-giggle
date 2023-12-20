0ad104601b1c79858f548804915ad2a813ad359d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:26 +0000

    tough illness cut beneath exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
