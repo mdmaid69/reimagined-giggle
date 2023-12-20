be0440d1ee2e0711665636dfe76ffe096e6befd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:00:54 +0000

    pupil custom stage and fly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
