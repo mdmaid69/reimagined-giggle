66eea07d24ce3b62b17310c6a7c15e25173e9fee
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:02 +0000

    bad hold commit Rr reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
