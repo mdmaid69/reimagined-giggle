9344760b8d3cb672089ce83c0033ab0fcf4977c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:47 +0000

    push its succeed opportunity declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
