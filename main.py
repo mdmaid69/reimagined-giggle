7d9ddddcc3776bb33f58bc738424cd7d82306140
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:15 +0000

    region consult come charge necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
