a618e452685c08b98f1dc20ec5d6c3415f1507fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:30:41 +0000

    gentle board anywayanyhow thankyou attitude

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(5):
-        print(i)
+def reverse_list(lst):
+        return lst[::-1]
+name = "Python"
+print("Hello,", name)
