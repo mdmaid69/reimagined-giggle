b08febefe2337acbb648ada915dbbc0ef4341435
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:44 +0000

    brown bored fill again remain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
