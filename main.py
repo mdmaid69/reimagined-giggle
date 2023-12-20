01b0e6a481e40461ca3625a2c042dd542cb1ff06
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:37 +0000

    strict usually possibly minimum expression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
