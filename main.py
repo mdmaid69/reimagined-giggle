78ae14183cf10861557b4142d40e0b10a2abf3e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:08 +0000

    week mean throughout film attempt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(lst):
+        return max(lst)
