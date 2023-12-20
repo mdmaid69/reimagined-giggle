a589bf075a1eb69f28508f93cae1ff95157d5f9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:10 +0000

    around experience minority really professional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
