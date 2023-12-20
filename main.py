d64743ff8ed3514063dbc1ca998a5b353518788a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:35 +0000

    report file round because surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
