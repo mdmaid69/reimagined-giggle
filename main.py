a0bac0835e1edc544a54966086a988cf74cad6df
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:32 +0000

    Oo without giveback address convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
