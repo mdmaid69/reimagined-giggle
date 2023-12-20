ef28b29cc1c856e9cb96a8384c447c9c823e3403
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:00 +0000

    tip wish secret past rush

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
