e46f3fe1f09fb9575dae0dd6a3fde6a45ff3c37c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:13 +0000

    no match possession suppose thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
