0affab7bd6e18dc76ebd161d6037331ba44078de
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:39 +0000

    change win recover eye feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def print_python_version():
+        return sys.version
