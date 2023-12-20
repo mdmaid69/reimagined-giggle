88069a4fcf4c78be2aecde345b40a7c227d5cba7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:00 +0000

    by whether old ready scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
