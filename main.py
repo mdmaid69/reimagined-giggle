fe75eaa999c9a718531c710bfa512003e9f4d08e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:26:59 +0000

    rush breakthrough extraordinary grow marry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
