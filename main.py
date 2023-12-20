a5efa520c7032b45b444e94ff381932908832445
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:42 +0000

    free period door action allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
