82a0f4f6e53f3a5dde681e9ec61852c8bd8b618d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:49 +0000

    test pocket than right have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
