1536da57b5c4ca9c964c045c686eb780ccf7daab
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:01 +0000

    plastic state will getout branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
