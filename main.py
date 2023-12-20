ff00a2e477cd698cf2d856f7885e2b2944266ac4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:08 +0000

    arm direction hospital hang door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
