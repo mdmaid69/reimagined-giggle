bb0d63e3ffd2e1bd54935d2d66ba9d27b321ed55
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:53 +0000

    this illness call harm exhibition

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_list_from_array(array):
+        return array.tolist()
