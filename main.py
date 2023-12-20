7c8bd192bcfc0ee8db2657504c254496558f723a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:44 +0000

    apply share chat recent depend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
