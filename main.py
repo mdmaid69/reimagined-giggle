93ac4c309f0b2d472f0bd21c923552449bc2f69e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:14 +0000

    prefer list him memory ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
