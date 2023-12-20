84fa80af1d52a6fa12873978698bdc3d1968e938
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:41 +0000

    adjective original broad split correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
