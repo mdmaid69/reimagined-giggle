cdee1176c519242a6e6de63e80c9146fff27db9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:57 +0000

    wash one cry rub away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def is_even(n):
-        return n % 2 == 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
