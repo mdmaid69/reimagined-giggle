3400218ffdda609863c0dd0147398c6ca208c6c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:23 +0000

    order speech change tense belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
