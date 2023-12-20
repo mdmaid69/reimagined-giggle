dd60a841211865198b4ab67dad301ae27d2dd8ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:42 +0000

    ticket 566end throughout wash stare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
