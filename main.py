9be40a6fbea0d6358af551b66b817a114906b010
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:03 +0000

    also slowly wide influence face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
