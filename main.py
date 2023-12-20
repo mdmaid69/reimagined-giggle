8fa76f8a9f6f8d9ec8da10e44368d09a2253850c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:16 +0000

    war smile fly we something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
