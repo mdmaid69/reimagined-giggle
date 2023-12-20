f7e6d14f591d98b44e847d1dc01e6d299b13719c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:16 +0000

    definite slowly many shut glad

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
