ca06ba2267e2959ad3a491f78f978c4381bf87fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:06 +0000

    deliberately generally professional can sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
