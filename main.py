0a558269cae71cecc78fb5434ec50aab8ef5d71d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:44 +0000

    trick dick range close switch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-print(random.randint(0, 100))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
