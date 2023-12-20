64a41ecd2ec539768a61e5ca0f0d39a9b464d32b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:30:19 +0000

    about convince art through result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_base_name(path):
+        return os.path.basename(path)
