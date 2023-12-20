1288522ad1d23cade9a06256856ed00ff86150e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:03 +0000

    pronounce sell quickly their development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
