7689514e67bfb47cf147daea48b5140e1185411c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:20 +0000

    turnout mine odd frequent skill

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
