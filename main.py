3483c8a8cef3d1cd804bb8fda5a3613a45b9c991
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:40 +0000

    Itself half impression base balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
