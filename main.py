2e402e8537c8969c74ccd395dbab8ec559093231
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:34 +0000

    birth one be spray window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def get_array_item_count(array, item):
+        return array.count(item)
