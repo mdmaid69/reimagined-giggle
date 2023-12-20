0dbb937510184f7d022af91b6e26af496f324498
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:20 +0000

    poison stranger cause stay apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
