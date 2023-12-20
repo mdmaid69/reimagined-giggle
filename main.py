3d370178b9ea1000a8e070f20feb229f02384fb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:56 +0000

    flow dig about rush godown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
