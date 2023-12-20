2c88d056ab0c3a6d5e4c0c25e36e0bd60ef9d060
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:34:39 +0000

    main certain up control doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import re
+print(re.match("h.*o", "hello world"))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
