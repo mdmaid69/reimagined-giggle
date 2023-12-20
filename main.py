27346b264ff5fa91b6b2419ae7033aee321f8dee
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:16 +0000

    godown through however proper back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
