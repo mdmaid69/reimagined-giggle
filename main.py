48e8b6add1b2caa7df0a65148df61995c4257e31
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:33 +0000

    Qq wide ashamed affect lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
