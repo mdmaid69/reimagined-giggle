53b51e25f88c804215ff7d2655c84ed11d27a717
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:58 +0000

    shoot one tone notice before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
