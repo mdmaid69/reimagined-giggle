378db31e580cb9c3bbe9fd897b30e3e94552646a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:34 +0000

    represent speak guide evening brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
