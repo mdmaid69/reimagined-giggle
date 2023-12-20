c074e75379b525fc3ae3e9c7c7bf988679fa0169
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:58 +0000

    home tidy height exact sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
