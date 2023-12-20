a59cec854186f179a3415f5192f33845d44ac7f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:19 +0000

    while move compare unfortunate university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
