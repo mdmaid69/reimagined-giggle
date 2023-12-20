d68eea86115b39c43acf0c9424628f98f768f648
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:18 +0000

    naturally turninto practice who black

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_list(array):
+        return list(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
