43704663336034e48c78008d03d78fd71cef0dc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:38 +0000

    fit rain touch impressive correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def reverse_string(s):
-        return s[::-1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
