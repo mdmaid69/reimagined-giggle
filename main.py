756226b0b6d107e586927533ea9898db466aa615
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:30 +0000

    criticism regard search depend stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
