198098d712b2d35dfa3c26039700e9deee84ba33
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:30 +0000

    radio guard include laugh underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
