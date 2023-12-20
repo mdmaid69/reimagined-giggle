d35e0c5e9028dfc4096d6e991bba826f4b9ebf36
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:27:11 +0000

    criticize OKorokay bag drive naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
