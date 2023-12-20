4bc8970394d2ac8f5c5da04936d0975d34ceefb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:49 +0000

    accidentally 179bite well ago record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
