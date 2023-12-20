888115c6a6cdcf8eedf430fed1d6be93db131b14
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:37:00 +0000

    afternoon situation damage middle swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
