1d7eed340798ec37dd59465c9a35c93b748a4da1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:03 +0000

    tense film no fasten arrival

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
