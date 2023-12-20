ebd3c1c6978651d8f147e6b568c5ae40a85965b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:52 +0000

    middle separate talk aim test

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+for i in range(5):
+        print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
