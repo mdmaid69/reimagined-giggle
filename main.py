ad0be0ef3716581da1d5a1781d9d4ffe190ae7f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:50 +0000

    strict ticket police proof force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
