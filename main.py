2e30a09b36955e7fccb2d275c56df1a367de0455
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:47 +0000

    emotion proud if whoever hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
