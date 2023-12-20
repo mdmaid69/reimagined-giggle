755fce0b1beebd073252e7c65bf580a8b5d7df1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:17 +0000

    frighten claim explain occur figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
