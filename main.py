5c4b1dbf2989f61086d8c7c74f7065d4ad35c668
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:17 +0000

    thin moment key minimum easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
