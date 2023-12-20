e8cdd7d22de9979b07f06ab3af86535e290724cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:45 +0000

    argue refuse rarely exact increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
