4e437ebfa3cf86441cc707681357571f2fa6bd47
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:30:06 +0000

    beat dress dear pupil clothes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
