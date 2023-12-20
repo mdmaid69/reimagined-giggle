ac0a1c0a6f6046f514d89019c245976b4d6aa368
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:55 +0000

    refuse ride coat plan taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+print(os.getcwd())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
