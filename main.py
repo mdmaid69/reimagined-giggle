af8d1a93d8970d51b7de9dc7774002e8ed675995
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:09 +0000

    out loss secret unkind minor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
