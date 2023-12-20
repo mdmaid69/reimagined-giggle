8343b6ac38e140371320c16ea7e51b398abd1d56
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:56 +0000

    feature leave keepaway describe open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
