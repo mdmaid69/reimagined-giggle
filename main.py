4d025fc255be7094b10054f5eb1af6c0e0e2f1c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:33:17 +0000

    calloff party neither new above

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
