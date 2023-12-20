3a47aff8e8865f6ad896bbe3f2434c9b41ce9aa0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:48 +0000

    guess concern climb single destroy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
