6164d90ee1a10ac7dfe383d502f65612967a04d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:25 +0000

    word plan need opponent run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
