9ed883706414e9be8c8b5a98fbae19cdffdf7224
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:34 +0000

    trap brother stretch contain manage

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
