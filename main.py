c8362978794ec4eea7ba4462b03cf40fc1c86920
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:26 +0000

    face cause remember schedule father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
