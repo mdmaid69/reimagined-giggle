50561f70712ed5a1eccdeb3e3a3035ecd92f389d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:59 +0000

    workupto date offer himself tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
