286f312a9577f9b7c05ba057d11412d9ea59f622
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:51 +0000

    pressure waste level check view

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
