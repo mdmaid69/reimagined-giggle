49965d540979d918d4d048163a722046b733374c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:09 +0000

    knowledge mind drawing sort whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
