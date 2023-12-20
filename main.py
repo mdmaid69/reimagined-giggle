4de64f70a9f7be52905aee4da001c12804c77f2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:51:07 +0000

    repeat furniture job secret strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_current_directory():
+        return os.getcwd()
