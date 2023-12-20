80577979a52787be2ba4bf76d4d22932ff4c9c4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:31:15 +0000

    reply welcome moral search slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
