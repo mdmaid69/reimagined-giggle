aa55b0e8e7cf6641d8dcc8f31efa943054701e02
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:21:20 +0000

    cry hungry area force rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import sys
   def get_python_version():
         return sys.version
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
