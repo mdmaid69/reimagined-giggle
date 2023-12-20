e8c64f1afaa572a4f6157f5d58f081d5ef146a9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:59 +0000

    runaway getoff ourselves nerve battery

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_array_to_bytes(array):
+        return array.tobytes()
