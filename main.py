669b8c959a4043b7587a8b4ad09aeb7f205b40ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:52 +0000

    finally grateful watch relax contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
