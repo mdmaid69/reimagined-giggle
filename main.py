e7f67d7c7f62b81d39d31cfecad5556a4bd074b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:56 +0000

    form properly lead press nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
