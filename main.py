375f5bf815973b33d24151b06623d5c7076f39f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:04 +0000

    distant raise line various recently

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+print([x**2 for x in range(10)])
