5e58c3435ba511a0e3bac5f5a93204c9eac0c14b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:56 +0000

    sort sky freeze politics early

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import sys
+def print_python_version():
+        print(sys.version)
+for i in range(5):
+        print(i)
