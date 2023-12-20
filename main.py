cc8c79b7580495d940e989e834316a82d5277ce7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:32 +0000

    study stamp opposite feature young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
