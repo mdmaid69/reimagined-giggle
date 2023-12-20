16bfc42497a341312f6e11b5e50c6aa08e496b34
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:11 +0000

    mysterious until rarely temperature stage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_min(numbers):
+        return min(numbers)
