1e71a58b5f77a2720317cad3a3c346d38d9f7582
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:02 +0000

    heat question part payment buy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
