96a66794bf9536801b6e1fb755601fa31eb428ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:40 +0000

    promise worth wish climb vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def get_current_time():
-        return time.time()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
