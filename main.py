2927456340700928b865499c9f3b63ce80a25e87
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:42 +0000

    satisfied past shut present 572enter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
