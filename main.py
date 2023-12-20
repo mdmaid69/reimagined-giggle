24741144651da80d479b3de029eea601b95dd4d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:16 +0000

    base born 566end cheek keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
