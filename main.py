af9405cd53f54a22efea17a45428bc99d9b59a0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:41 +0000

    some office fail test cause

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
