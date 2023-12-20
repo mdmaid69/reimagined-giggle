6abfad67b1c4b379b28a0c50b03c8ea9928b57f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:26 +0000

    listenfor pen comfort mistake everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
