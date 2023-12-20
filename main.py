a2fc522337ae0f21c3a1d9191319abf610c808ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:17 +0000

    ahead worst drive practise prefer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+print("Average:", sum(numbers) / len(numbers))
+def divide_numbers(x, y):
+        return x / y
