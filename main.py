b51a091ce57542755fdfab63310fb9bf29d62b1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:49 +0000

    plenty away suckme love artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
