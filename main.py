5049f13051aa92f6198fd380a92a2f60f675d00f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:23:28 +0000

    quick beautiful major admire aware

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def is_odd(n):
+        return n % 2 != 0
