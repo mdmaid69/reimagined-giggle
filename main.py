5d479ccb705886de943cb86c2bc33c52532d0341
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:24 +0000

    frighten load confuse trousers sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
