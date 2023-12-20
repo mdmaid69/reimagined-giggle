4e4a8143a98b1fdddce5a285f67c652a8799369d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:21 +0000

    pull important myself night Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
