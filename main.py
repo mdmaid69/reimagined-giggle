474d629e2968e906f5c47a6d037a01ebdb9af497
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:58 +0000

    hurry start consider part condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
