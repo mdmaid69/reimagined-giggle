39f2617de866db94d05cd5ba33b3b49138eedadb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:29 +0000

    thorough disagree everyone last pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
