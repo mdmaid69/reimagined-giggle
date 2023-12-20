0ebb3406aed67fd7b4382bc9eb78468a1f63f30b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:35 +0000

    yourself shallow distance possibly period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
