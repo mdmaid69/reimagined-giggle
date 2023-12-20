0e673b48ac7d60e04ae17738c76ce0329daa6e82
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:19 +0000

    too curious settle Hh reason

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
