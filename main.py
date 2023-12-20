904987d235fec20cefaa475f93d0892b67ec4744
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:56:01 +0000

    complete regularly marry patient border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
