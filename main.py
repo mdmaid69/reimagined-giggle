3928771456181d0a38f4a798ad1dec4a27221c06
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:27 +0000

    earth connected sound turndown another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
