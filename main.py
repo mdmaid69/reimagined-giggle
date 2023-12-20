1906cfa0c2ad52a5cf747fc1077e1cdd536b1c31
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:14 +0000

    feed ability 567enemy between stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
