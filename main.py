6946e302851df93aaeb6714bb2d0788bb4101fd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:26 +0000

    opposite recommend future correct ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
