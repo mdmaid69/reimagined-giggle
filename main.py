8cfe8bc3eea1cb1bc0d5c0f501b6346f0385d98f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:34 +0000

    shallow shout powerful tool luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_working_directory():
+        return os.getcwd()
