5bfdafa81b8af6dd88de320ae8851b6e332b6622
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:43 +0000

    ability turnback crowded defend afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
