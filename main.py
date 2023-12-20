5bdb2532d926d41fa3871e03e400496cbf7ed8aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:45 +0000

    rain back sharp colour rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
