3fbec48cd34cac22463c84897d304943f52b1d8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:20 +0000

    learn age ear relation disappear

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def print_python_version():
-        return sys.version
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
