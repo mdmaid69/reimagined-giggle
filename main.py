43da28b7613b7d992fe5a6fe2ad7c5a19591f2b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:44 +0000

    nose challenge murder plant sense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
