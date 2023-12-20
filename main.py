3fef32c976122e5dc4c40113d1cb56948a3e0490
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:50 +0000

    improve serious person manage piece

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
