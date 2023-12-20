7311abf69b445abeea950e83b6bbe38e321005a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:11 +0000

    though smile call team impatient

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def exit_program():
+        sys.exit()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
