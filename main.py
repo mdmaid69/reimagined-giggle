1762ce6025659ef3fc8f274f634a1abe341ebdeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:19 +0000

    practise breakdown basic nowhere rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
