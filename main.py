e6f64a38366367fa8201cfbe58e1edcbba82806c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:07 +0000

    snow with consider until nowhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_error_function(x):
+        return math.erf(x)
