9fb33c12d45d43b0223a05227a3d2a0391db2702
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:50 +0000

    usual happen dead to 566end

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
