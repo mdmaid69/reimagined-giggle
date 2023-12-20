12d2e22711f6c8a7cdfc263f22882c5b597a31bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:28 +0000

    adjective evening sort could why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
