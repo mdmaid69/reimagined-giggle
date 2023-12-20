6b8a023785c96ab0c36d24d6eee3b091fb98b177
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:58 +0000

    guard recognize leg sentence position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
