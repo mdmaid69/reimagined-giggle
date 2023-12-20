92a4ed016b878aa2b4c9af2b9ca3f882b6439d87
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:29 +0000

    lead work under cost custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
