1e24e8914f51a689f789f131054a5079fe8c823a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:42 +0000

    round fold throw build wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def clear_array(array):
+        array *= 0
