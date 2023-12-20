a252587631e67cb53d6a753d37634b2ce8cc0481
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:55 +0000

    formal tape question strict argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def square_number(x):
+        return x**2
