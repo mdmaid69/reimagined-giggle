4cc9cb10e62f27ec9016efb9b96354f1e15884f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:07 +0000

    push measure hot old meaning

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
