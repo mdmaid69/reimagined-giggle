79b18e41ffa8cc25bf6ea50aee4aad0e273cdf8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:40 +0000

    sudden print above no interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def reverse_array(array):
-        array.reverse()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
