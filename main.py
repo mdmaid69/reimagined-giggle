e18520dbf0004ac02f1de9c8a448707b77b44a42
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:23 +0000

    chief separate blame equal can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
