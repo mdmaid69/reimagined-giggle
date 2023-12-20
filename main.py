6f13085e0a7b158099f42dc8e81e89f653205363
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:37 +0000

    paper score butter exercise loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
