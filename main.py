ec1ec49f36276d5bd86c7a8629bf232c3fed403b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:37 +0000

    ability failure vote suggestion cruelty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_max(lst):
+        return max(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
