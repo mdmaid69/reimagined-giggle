2688a5fbedb6da3e3ff683fd8e9ba0d88c30a1d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:08 +0000

    neither nose profit proper shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
