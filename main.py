074fe349fea4a3adaff75115ba1d52f2952bdf04
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:04:55 +0000

    immediate promise disapprove eye repair

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def remove_directory(path):
+        os.rmdir(path)
