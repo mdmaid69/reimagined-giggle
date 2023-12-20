df2d6796494b5678ab55be916bbf153eadd4d6f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:23 +0000

    comeout shadow combine off gap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
