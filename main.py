11a87820282924ec0ac2a547c34e3e88f4f44b50
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:18 +0000

    difficulty grateful breakthrough peaceful eager

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
