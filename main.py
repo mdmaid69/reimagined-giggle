425307b20a6a6116f1c41992b483c9bf7cdd7439
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:51 +0000

    damp children charge large scheme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
