8bc9bcc2f9cb5fda96ae603928953de05fddc62a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:57 +0000

    holiday swing interest feed big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
