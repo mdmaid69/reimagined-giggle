54d93bca4ac6a59e946ea4d00a3200ff1698e68f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:42 +0000

    increase when news pencil sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
