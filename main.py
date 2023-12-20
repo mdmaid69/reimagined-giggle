5e71ee9df97a80f9b9ad3e490a37e6f8df5390b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:03 +0000

    crash none quickly arrive deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
