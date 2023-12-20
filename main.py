e70c590e3c4e5f685ec65bc09ffc3b939153b00a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:50 +0000

    tie pause obvious giveout nigga

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
