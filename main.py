687f019ac5e33361253e667cb11ea3516a3cded9
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:57 +0000

    godown giveout to organize slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
