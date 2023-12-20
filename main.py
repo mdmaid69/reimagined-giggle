3f3324bcc9ebd20e7fddbda34a6dd1d121185cf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:50 +0000

    debt fault field equal suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
