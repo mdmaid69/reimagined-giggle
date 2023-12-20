622ea8d9767293a4809d15cf1799a489ad8b291a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:11:16 +0000

    school nearly cook true curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
