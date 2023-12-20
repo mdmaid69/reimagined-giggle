146ed2f82a9891c11c9e5a609f65f192b36ea3a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:00 +0000

    uncertain makeinto judge necessary possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
