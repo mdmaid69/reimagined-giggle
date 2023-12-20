d755b5db607a2bac41824643c4cfd54cf0828a16
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:18 +0000

    view 571enough beneath previous smell

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
