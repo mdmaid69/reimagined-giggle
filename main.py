e31654ec12f84a83525c58201a21532884897d95
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:41 +0000

    force collection trick slip lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
