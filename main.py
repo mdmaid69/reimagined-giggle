ff94ac8bc1f673bd3f66b99b09517c9b57deb29b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:08:02 +0000

    while union division kill holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
