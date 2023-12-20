d0fff15714dd8caaebe4610fd64930abd4097c1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:11:35 +0000

    show occasional thin tip disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
