22eb7036def85be3e057708e5f72ac69366ce7a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:47 +0000

    feature who confuse hour ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
