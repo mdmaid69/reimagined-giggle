9848230e0277228566885e78067c8e11be04ed89
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:53 +0000

    fly comein unpleasant feel thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
