4fc2583d5ca83316e7884d7849ce993177c7b938
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:21 +0000

    Bb thinkof proud neck warn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
