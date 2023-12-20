9fd5b0ed722e4ec0167c084dd49b46ec2f83b857
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:10 +0000

    appear mind cry spend suck

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_unique_words(sentence):
+        return set(sentence.split())
