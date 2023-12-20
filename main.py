de301f1e169e1d8c290f2862af8b2605b0618d02
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:44 +0000

    voice join agreement century close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
