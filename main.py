9acd9fc1ade00c353b572b603a110387d566f5c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:17 +0000

    turnoff paper loose breakout circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def square_number(x):
+        return x**2
