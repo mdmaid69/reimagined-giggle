a9975fbc11236d2434ff59bf962e6383b52d68e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:54:48 +0000

    cost wonder deep attractive study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
