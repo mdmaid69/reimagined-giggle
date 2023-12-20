86b0f7e98865fa4958b065dc9ddd650a2c4239cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:47:18 +0000

    before blame behave generally injury

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
