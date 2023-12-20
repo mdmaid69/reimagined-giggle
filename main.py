26a858f0286354e3d15a72ed8986b96ee5713d85
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:06 +0000

    cap swimming welcome pleasure fire

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
