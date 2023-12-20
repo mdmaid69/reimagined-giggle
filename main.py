7972ae2fa0c56c0be69b84017ab6cb8557f68443
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:51 +0000

    murder worried shall solve 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def roll_die():
-        return random.randint(1, 6)
+import re
+print(re.match("h.*o", "hello world"))
+def count_elements(lst):
+        return len(lst)
