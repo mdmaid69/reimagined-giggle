4e337bf36338ff667a308572ae7e99b8f9a56cd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:38 +0000

    murder can debt contrast chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
