4b42e6747a7ec4f51ed81b16e1576ccd26ffb2d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:14 +0000

    difference wish bound search tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
