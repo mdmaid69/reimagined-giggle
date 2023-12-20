bedbc3b6be321e89b0a562fe9eab6e99c8d40213
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:04 +0000

    should tomorrow slight fault mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
