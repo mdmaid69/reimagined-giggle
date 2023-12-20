08628f326a895fe9b8621914eb0833d01d3a4c1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:44 +0000

    push right figure lead maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
