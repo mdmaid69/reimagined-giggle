51c2a69a078ebd023e26cb92440535995f224335
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:02 +0000

    office hurt need already mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
