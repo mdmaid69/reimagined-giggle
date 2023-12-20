54145fc968bb47765cfd76b142d0854a8711dbe9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:00 +0000

    tight negative dry noise later

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
