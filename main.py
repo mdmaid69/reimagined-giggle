edaa7a4c70161d87e614d424ff884870c61b3cf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:18 +0000

    tap deal much excuse operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
