b79a47ce9ebf5fe53fc000a6f953f0c12fe03e17
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:16 +0000

    explain down pinch entrance thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
