2eb2bb06d2278342a9fc9efc36ee2f3cba0286cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:05 +0000

    follow fierce doubt keepfrom period

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-name = "Python"
-print("Hello,", name)
-print("Hello, world!")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import tensorflow as tf
+print(tf.__version__)
