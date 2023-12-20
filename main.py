f75be8278719f518f795efafb96c6972e9a5328a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:55 +0000

    least change it fear 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
