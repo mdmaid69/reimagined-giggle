23b35fc755e4b869809a00791a8e9c91911a78fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:17 +0000

    off likely makeinto dull future

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_hex(n):
+        return hex(n)
