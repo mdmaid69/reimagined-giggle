5e23b1d4f82cc1e6fbcb27cbca5f2964950340a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:40 +0000

    pretty necessary proud approve long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
