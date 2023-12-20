5a0bacbf197d2dd264378119927c4d5765ae9393
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:11:20 +0000

    autumn phone worth shut use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
