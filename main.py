aa3499e631c59567252b9cf1afd5970c88874977
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:44 +0000

    distant vanish behaviour doubt shock

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
