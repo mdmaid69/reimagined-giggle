8d2f868e3f1653647bcf0ff75c74cfef8ed66c6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:39:32 +0000

    him activity goout mark amount

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
