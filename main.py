1c1b92308bd3062e4809bb98abed2183384df6f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:03 +0000

    shrink violent various turninto sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_pressure(force, area):
+        return force / area
