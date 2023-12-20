74c73fedbc8b848ad1d678d1285cefb3b88d9ed1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:19 +0000

    talkback meal subject communicate rid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def add_numbers(a, b):
-        return a + b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
