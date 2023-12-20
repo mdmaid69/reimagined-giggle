c1a096f4860a0637ba5193a54158e4f77d5739e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:11:27 +0000

    arrive possession attempt to contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
