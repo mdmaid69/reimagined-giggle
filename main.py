f173840ce209e34ec4b9a94c06b852b44b9e0559
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:19:34 +0000

    standard easy neat Itself exhibition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
