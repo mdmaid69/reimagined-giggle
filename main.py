2eb9e269a095cd168884151004902da582929f95
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:28 +0000

    back state sit rate payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
