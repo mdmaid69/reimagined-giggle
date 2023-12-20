8a08e6ed13c01829d4dafe69b446d16b15014b3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:38 +0000

    cross behind exchange holdup settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
