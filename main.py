7d2265afbff7f289ec53d6f25cdac6f833a7903b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:21 +0000

    relative actual exist plenty hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
