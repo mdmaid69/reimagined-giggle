c4a6765e59e327107b51ced2a59323be77bc12f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:45:46 +0000

    paper reasonable office branch building

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def reverse_list(lst):
+        return lst[::-1]
