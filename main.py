4945fdfb2134bdb0590c2e094fa5a306425133ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:21 +0000

    but last seriously pollution develop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import re
+print(re.match("h.*o", "hello world"))
+import collections
+def create_priority_queue():
+        return collections.deque()
