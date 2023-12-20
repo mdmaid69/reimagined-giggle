6f457dfc9d41e35beaa8aa7407e030de990bc001
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:42 +0000

    result care move pause you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
