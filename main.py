3dd00c12e63470036b09d51ccf1c6907d52d968d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:39 +0000

    this track guilty fierce aim

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
