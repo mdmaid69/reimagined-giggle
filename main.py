c2770d3aef1ffe3c78f708c7849db728624af14d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:57 +0000

    brain practical difficult age blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_volume(length, width, height):
+        return length * width * height
