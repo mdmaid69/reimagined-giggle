396133685cef7554ca5e06acf784ae87d65448ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:34:39 +0000

    protection tongue news sell belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
