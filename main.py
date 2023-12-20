667c0dad63fa2fd5cc06e575b82560dd2614d3f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:52 +0000

    show open bill pile trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
