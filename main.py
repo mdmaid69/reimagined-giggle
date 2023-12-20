d7a889efcfd41e43755f06d6ce7a5d44c70fa2ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:26 +0000

    plastic put care description patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
 def calculate_root(x, n):
         return math.pow(x, 1/n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
