6d79b5194cc6bef6340e22eda18f196f27c0d236
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:20 +0000

    idea judge attempt worth telephone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
