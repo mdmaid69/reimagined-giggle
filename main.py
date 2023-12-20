beb94297cd56a207be7dbfaadd77d477945c0ed6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:23 +0000

    pronounce damp clear sleep easy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_typecode(array):
+        return array.typecode
