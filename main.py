a543508b6347180813ee4c74dc1e7e0734c047eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:57 +0000

    treatment completely their couple handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
