54cd7aeebfeeedb828a00352b98198354764e0d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:18 +0000

    rich otherwise report hope lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
