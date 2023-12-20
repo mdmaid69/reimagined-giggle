18838fb21a82976b9e708d2a6d47faae83141008
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:39:15 +0000

    hide to reaction short never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
