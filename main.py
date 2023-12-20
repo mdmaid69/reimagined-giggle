b7a3666af2edb5e073984669f2a0243dfb2b4955
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:32 +0000

    young back fine head copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_floor(x):
+        return math.floor(x)
