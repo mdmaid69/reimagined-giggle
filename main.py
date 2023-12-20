ff2fc443e3755b6614e20765b658b3a3569115bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:55 +0000

    once turn offer 179bite tired

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
