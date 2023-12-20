0d4d077b0e7ab5814ea90670bb5b29d35fc55b12
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:28 +0000

    bang creature human middle apparently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+print([x**2 for x in range(10)])
