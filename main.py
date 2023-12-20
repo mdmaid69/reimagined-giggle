45e37d53fbd4d4a851b873b46fc3995627c9039e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:07 +0000

    remark callup check dark shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
