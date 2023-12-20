ff742dbf59a5b4778a16c5d93e86c22d0821347f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:44 +0000

    convince brush effort disapprove herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import sys
+  def get_python_version():
+        return sys.version
