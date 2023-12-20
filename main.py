ca68b8d825b79504ccc24cf8969da15dfab8fa54
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:16 +0000

    impressed past complaint chief regular

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
