452d5d854d995f917117d2ea53b4c35223b7aefb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:08 +0000

    whatever damage succeed unit live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+def find_min(numbers):
+        return min(numbers)
