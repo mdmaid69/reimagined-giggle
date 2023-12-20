3e856d0d2847309604a9fa64850706caa5387a48
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:39 +0000

    probably press programme collect miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
