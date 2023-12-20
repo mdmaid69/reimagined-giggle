7ed8f62677c958fca1e0c3b12809c60392ff5052
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:59:04 +0000

    especially less play walk matter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
