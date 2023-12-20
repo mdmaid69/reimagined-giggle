d97cf9ade99ee308aa27c5e8931a725a2d4f654b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:19 +0000

    unlikely sink sad custom certain

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
