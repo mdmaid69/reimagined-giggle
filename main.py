5d30b2aa28260cc28b9c097d2de3539cc651bd92
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:34 +0000

    cum reasonable obtain likely cool

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def remove_duplicates(lst):
+        return list(set(lst))
