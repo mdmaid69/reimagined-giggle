aa98b69bd76fb0ef19d8a2dcb0ddb050a1ede2fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:53 +0000

    unusual test wheel either keepout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
