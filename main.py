05306d9b1885c88a94dbacb43f5e07b81c9a9157
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:07 +0000

    anywayanyhow uniform mix it cause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
