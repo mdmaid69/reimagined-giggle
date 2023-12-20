3088e65d54442886cb860887c31660f7cbb3fae2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:36:30 +0000

    even nose from lead relative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
