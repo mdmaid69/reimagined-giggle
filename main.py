5bea1c9940de5eaf4debda37d2aa175e864d14ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:37 +0000

    bury problem object sister later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
