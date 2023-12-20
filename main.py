446b0121d8d163efb7707233c4ed7ea221f1aa22
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:15 +0000

    rescue laugh height splash typical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
