fec8cecf985022318f69be300c649ede5103d58d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:15 +0000

    scheme mixture plant reason rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
