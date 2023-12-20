20c12912c6de16ee89ccb08b825dd6da82510346
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:09:21 +0000

    out communication serious many strict

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
