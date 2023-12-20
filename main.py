207d2c25fcdae690c17f28341bb90b40f9482d15
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:01 +0000

    hollow furniture worst previous rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
