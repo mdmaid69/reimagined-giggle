65079782a7c57f3d984ff292f8f385d69d0439c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:10:12 +0000

    corner by plenty project noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
