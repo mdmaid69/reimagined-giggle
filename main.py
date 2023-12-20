fec8a12b330abf8076ce49c4a7e12c8f0634b765
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:56:31 +0000

    situation behind bin frequent weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
