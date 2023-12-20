c39ddea6ad95101446a8082772df40a5f6fa53ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:54 +0000

    feed save estimate window punish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
