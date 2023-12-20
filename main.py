2939217cf4e776b1d7fc42d318bf5f803b1f089c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:01 +0000

    book thumb getout money available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
