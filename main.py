55c82e571037ff8ff2421bd40636b1972078b4f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:53 +0000

    average afford unit tend breakup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import platform
+def get_os_info():
+        return platform.uname()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
