fff9475a1a19d79f62cf4cd927e539f1f69f1454
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:11:41 +0000

    reject gooff impatient favourite onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
