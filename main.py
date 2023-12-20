c161feb57340326f04203068a4199a7329c428f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:26 +0000

    remark forward word agreement brown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
