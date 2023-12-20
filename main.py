f49eaeb4bb34aba0c9deca8a8f5143028c7f867b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:06 +0000

    female remain corner destroy class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
