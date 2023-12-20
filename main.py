ff8d9241eff49c906c61bec6e01e61d531218016
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:10 +0000

    damage open classroom once heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
