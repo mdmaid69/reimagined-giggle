d14274680244a0fd5d8733640d40a81247f97752
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:51 +0000

    first convenient fear damp excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
