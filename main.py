5fc9d78139c26aa934bab9f6fbfc4dbdef65555a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:51 +0000

    comeround uponon add play rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
