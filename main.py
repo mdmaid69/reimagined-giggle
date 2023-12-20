897852851dbc645381c33e673652dedef7322f38
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:03 +0000

    rain tell create damage far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def cube_number(x):
+        return x**3
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
