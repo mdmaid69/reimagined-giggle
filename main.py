ddd4a8ea776ef51a3490af5fc5a87ee1e698a09d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:11 +0000

    us general study gentle muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
