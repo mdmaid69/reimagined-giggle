058a7a562e1a162f0506def3d63eb84c5bf7f350
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:06 +0000

    winter deserve steal accept cut

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
