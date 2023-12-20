abe72f674a80339985ad7c0020fd0e59a9de029a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:20 +0000

    reason breakinto time project swimming

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
