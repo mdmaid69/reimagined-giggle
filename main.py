35296d602dfc21673a390f8a0372fa9b658f852a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:46 +0000

    lock tomorrow reduce best comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
