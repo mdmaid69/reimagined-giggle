c105d7fdd64a3207b2404d3bc171c98c49deba87
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:20 +0000

    shopping Ss cover war fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import time
+def get_current_time():
+        return time.ctime()
