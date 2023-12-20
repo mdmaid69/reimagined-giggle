f8518340f592e8fafa6b57b79bec5155c872d956
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:44 +0000

    point Mm brain hospital sudden

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
