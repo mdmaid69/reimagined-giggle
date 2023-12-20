ab211f8c5d83f5616f7e7f6a0b4b62f0c578fefb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:50:42 +0000

    preparation miss strike wing compare

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def greet(name):
+        print(f"Hello, {name}!")
