d5bcac91dc156adae6094ce628976b87bf35c967
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:18:45 +0000

    begin tidy like programme interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
