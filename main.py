4101e5bb65a2b691590baa27a826be1e00ce2b80
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:04 +0000

    introduce getback yet disturb Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
