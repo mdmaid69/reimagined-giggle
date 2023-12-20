54f97bf7237d321e92dba46878e50a36548be6e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:52:40 +0000

    term chief forget today loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
