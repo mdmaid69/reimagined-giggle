0044dab156a72ea95f467e9062b489ce9012c7dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:39 +0000

    certain direct wonder count develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
