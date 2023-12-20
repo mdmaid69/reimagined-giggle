99146af4af669707411079c04135d8d3f0b37069
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:34 +0000

    sky cheap frightened react pair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
