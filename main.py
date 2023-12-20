823eaa4748a866c38c1c841bd949838842797d18
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:42:40 +0000

    scare present rather chance beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
