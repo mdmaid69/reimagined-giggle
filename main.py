a9653768d43acfa5c01c803619b16fc1c4705514
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:17 +0000

    fast target himself hot exit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
