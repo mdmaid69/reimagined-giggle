16898610a3daed5f949287100f7019e0fb097e8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:37 +0000

    last expect machine demonstrate hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
