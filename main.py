87a117b4ab613aa286258b50ddba94376291ebf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:16 +0000

    wheel speed clothes answer taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
