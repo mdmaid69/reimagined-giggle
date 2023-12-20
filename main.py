7379befbbed1b36d9d37ff96261473ae2f6e70a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:57:01 +0000

    high silent yet plastic 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import random
+print(random.randint(0, 100))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
