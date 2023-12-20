229e79e631a39f46878d3bdebf1ec9a3811a9e44
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:16 +0000

    soak move minute tell satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
