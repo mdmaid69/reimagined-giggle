c94415694dc415c8ef2fa93994fd6b0912f5142b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:09 +0000

    return properly otherwise favour promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+print(os.getcwd())
+  def convert_to_octal(n):
+        return oct(n)
