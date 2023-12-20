bf39227e16f8eddbe26b30b672b4cd313f70991b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:12 +0000

    feeling at early mess rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_max(lst):
+        return max(lst)
