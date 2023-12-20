f3444f91fc1eb7efa6d27b68f1c03597c52db575
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:07 +0000

    common soil discovery enthusiastic belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
