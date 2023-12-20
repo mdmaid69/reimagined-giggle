b140fa66f9d95641623d1f396b773f1d15ec5732
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:42 +0000

    phone constantly sight medium big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
