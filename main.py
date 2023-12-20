c8106ebbd8b65b172aabd485c4c2f62b91a6d780
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:22 +0000

    news life image harm rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
