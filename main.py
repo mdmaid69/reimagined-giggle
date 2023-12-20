73b3c1a944170dfbc46eae16341f77bd41ca1deb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:03 +0000

    private trade play complain body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
