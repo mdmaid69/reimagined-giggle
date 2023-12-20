1e0508eeeed1c3135ea8720ca63a960fa97833ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:38 +0000

    boil clean what over tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def remove_duplicates(lst):
+        return list(set(lst))
