1fcd64a6c2cd940b73504c6feae4f52274e89817
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:48:17 +0000

    people line circle record comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
