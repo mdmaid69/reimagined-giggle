7144918d41d0bb19d0f6113cb6c420b5130b897d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:23 +0000

    on train command survive apparently

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(numbers):
+        return min(numbers)
