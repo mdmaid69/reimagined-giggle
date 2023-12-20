90e398ef92cf425a784b215c1d426aea4c2701d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:59 +0000

    copy feature mean style exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
