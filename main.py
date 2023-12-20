5ef98e3e335d3a7f571a31340ac049237d51a119
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:23 +0000

    Kk transport backwards loan door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
