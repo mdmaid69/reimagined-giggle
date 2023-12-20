530db5bc0dacdebcc59272a3f82784c4da968265
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:43 +0000

    right stretch bang address Hh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  def convert_to_hex(n):
-        return hex(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
