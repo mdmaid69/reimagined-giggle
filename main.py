e2c57075ae5eb7862d46168a431c3ba20553c46f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:48 +0000

    hurry file stone foot check

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
