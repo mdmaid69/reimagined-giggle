3b97154f9f7630681de5b065d32ff0b017e9e503
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:39 +0000

    though kick quarter mine really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
