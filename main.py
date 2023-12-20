ead395cbf4187729d8ebc67992d01f8ded11d385
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:57 +0000

    judge borrow cost invitation table

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
