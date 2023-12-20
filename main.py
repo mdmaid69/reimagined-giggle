b9e670a1a2d88c38a2f9733fce79827ddbad05bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:15 +0000

    exactly can direct Bb shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
