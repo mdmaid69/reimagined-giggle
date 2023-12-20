a9068127ad797960f1914cdd18e63666228bae59
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:11:49 +0000

    worried minimum naturally dangerous wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
