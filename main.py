925e731b32fe682067cd2c4a639d0521ce083fc3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:49 +0000

    easy prevent somehow shout serve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def divide_numbers(x, y):
+        return x / y
