f6e41448cd3e0bb4eea54cacef986ffca68b080d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:45 +0000

    us note Rr flat tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_int(array):
+        return int(array[0])
