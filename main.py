9e07520ca9b1eb71812a87f12f1ba34e7c12e189
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:20 +0000

    wake out light survive protect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
