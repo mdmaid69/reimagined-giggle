5c229b8ec4fdd701d6f31238ff3eb407799500fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:56 +0000

    goodbye separate towards murder convinced

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
