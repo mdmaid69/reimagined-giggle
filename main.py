c29ad228b97c69488ac4677df0f01b164c4bb8df
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:11 +0000

    new onto seem welcome everybody

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
