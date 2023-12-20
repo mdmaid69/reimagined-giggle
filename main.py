114be8aaebcada23f071034e0dc723d77c272e79
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:36 +0000

    best none broken avoid sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
