fd95cf63ba02a8c70c1e8e2b0a85d6650ba360fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:42:24 +0000

    like here tip control this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def convert_to_binary(n):
+        return bin(n)
