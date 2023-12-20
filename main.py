32f64ebdbec6f77a4cda896872edb6caf378dd09
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:53 +0000

    dangerous new floor congratulations before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
