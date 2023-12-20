59e8d6d5b8b0a40c69f3ea248f11397680d323a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:24 +0000

    turndown offer study shadow believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
