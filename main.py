b1c1d948d56fe418fe23474a3e020978645d60ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:42 +0000

    comeround slightly pride takeaway describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def reverse_list(lst):
+        return lst[::-1]
