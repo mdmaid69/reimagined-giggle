e4facaf3ab383fa7086eff0c818e13c0aa928d1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:15 +0000

    thin sort organization hard equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
