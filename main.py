d80df4db98e94dc6fdfc87a7c3e7789eb88def0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:42 +0000

    rush herself hour means sight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
