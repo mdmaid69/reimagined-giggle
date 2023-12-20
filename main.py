3ef56d8cbdc532ec4581001c618f217dea8a080c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:23 +0000

    568energy whenever favourite dig fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
