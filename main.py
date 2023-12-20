0e91b2d8581c92c03deb2b264576390ada1294a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:12 +0000

    cope level on anybody duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
