3a03bea446555e181cd97d69ca16050cfc8f9f48
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:28 +0000

    range strong show design breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
