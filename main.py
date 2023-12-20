3bb1a8466300c54f8759f9e363215e95d13ecfdb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:45:30 +0000

    in evidence dare noone sting

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
