fca1447cff927be42d3de026002cd4dc3ab615c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:22 +0000

    turnout problem beyond sting tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def convert_to_binary(n):
-        return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
