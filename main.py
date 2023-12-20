aeb7c690d368e86af7ff51ec0068edf32a34fdd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:07 +0000

    not sometimes completely pot sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
