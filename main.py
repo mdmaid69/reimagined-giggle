92e3d81455a6b76d6f0d5892b023cf0db9e6a956
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:01:00 +0000

    month second large glad mad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
