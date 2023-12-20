206b9b8564ca3fbcd3bde4cdf6477109dccdce4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:39 +0000

    average minimum consist callback flow

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print(sum(range(10)))
+def is_palindrome(s):
+        return s == s[::-1]
