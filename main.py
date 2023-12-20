2b25e59c9b2faf8c35ca8c51344bd83309d37301
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:11 +0000

    judge date swing influence show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+print("Hello, world!")
