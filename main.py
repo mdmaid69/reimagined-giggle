ed6ab63174fca19e258f2660c6a9edcf0b48d53e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:54 +0000

    government undo wish for week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
