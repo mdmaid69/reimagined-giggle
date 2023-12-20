7d697f19af239a6e7560e221fa6e2436a218db20
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:39 +0000

    exit hide direct guilty really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
