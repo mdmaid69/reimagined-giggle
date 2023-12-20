dc2dcdb5b306cbe6f2e55ee61e95c13b38d31298
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:06 +0000

    pour decide definite slightly physical

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
