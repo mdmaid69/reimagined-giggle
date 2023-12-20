95ce90b99a67dcf57a919c4e0b7dfc3ee280c6ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:51 +0000

    takeoff rest react odd thinkover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(lst):
-        return min(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
