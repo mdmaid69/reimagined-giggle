a3330cc66f13857be44363f2ded3036ca143fb0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:12 +0000

    poor corner pronunciation touch prevent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def is_even(n):
-        return n % 2 == 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
