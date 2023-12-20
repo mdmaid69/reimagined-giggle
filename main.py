004fe3b818400432ab95739d8cfb19c971a834db
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:09:07 +0000

    after giveup square it certain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_power(work, time):
+        return work / time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
