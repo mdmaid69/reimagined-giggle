d8208f4dfa534a5d58f0ccce9771b80b8c57ccec
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:35 +0000

    behind car discover shrink today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
