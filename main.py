d73873a8f8b2c7a1c2961ff8aecce572194633be
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:01 +0000

    grand independent agreement large everybody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
