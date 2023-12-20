29cbdb3d9733e6a14602f2ec8d774c320b944c67
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:10 +0000

    excuse someone room dangerous factory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
