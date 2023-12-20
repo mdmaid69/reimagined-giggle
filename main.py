b3a51c080f45c08bb3a3b304cc3442176e7208c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:22 +0000

    do drop thank like fail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def sort_list(lst):
+        return sorted(lst)
