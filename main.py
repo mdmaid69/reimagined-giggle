3b5c5fd7141ae5a8e1b6df615d6d65d381acabaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:11 +0000

    cut basis goodbye that raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
