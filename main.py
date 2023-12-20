3e55c7b7df30f8c5e236c6a4928ad3eea715d6f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:08 +0000

    comment image godown obvious explain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
