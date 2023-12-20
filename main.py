d1124105b2e0eb376928d38a79230ef2d9b755e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:21:09 +0000

    naturally reason definite party stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_as_repr(array):
+        return repr(array)
