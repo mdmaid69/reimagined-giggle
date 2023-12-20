68fadd676a6aeb67759f2387b05be5aa0b545cd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:05 +0000

    exhibition order various radio maybe

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
