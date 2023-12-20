a4df5510e015931242b9c858d2a37c9b3958847e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:22 +0000

    leg no on offer Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
