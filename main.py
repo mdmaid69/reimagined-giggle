41ea251049989cd5e83d9e9b35a70c488996fb72
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:42 +0000

    switch disturb holdup tend trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
