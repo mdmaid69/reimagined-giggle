84afb7e17e43e1e4f0b820afd3896df4f73b10a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:28 +0000

    myself easy respect trust convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
