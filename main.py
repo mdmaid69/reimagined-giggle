059c719e1aadd47a9dd5ce9cfd2dbc039a626635
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:29 +0000

    turn criticism born broken exchange

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+for i in range(5):
+        print(i)
