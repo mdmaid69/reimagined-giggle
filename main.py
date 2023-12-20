3a55befe330dbd8ffecda6e2bdd9522f70096b9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:19 +0000

    with nor visit skirt Ss

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
