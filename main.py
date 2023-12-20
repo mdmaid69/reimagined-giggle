2f84974762c652137a65aedc2ff54af72a1aa878
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:11 +0000

    distance involve with shock delight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
