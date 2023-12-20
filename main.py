203149d21e1195a1843b7f02cae8f8a2de416247
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:55 +0000

    or accept manner hard none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def get_current_working_directory():
+        return os.getcwd()
