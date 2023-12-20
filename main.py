1c3088dcb8fe9c1f202c8b4949e2471a07b7be05
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:38 +0000

    design completely contribute workupto only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
