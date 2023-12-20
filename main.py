77a16cec371064cc49870ab323a8aa337cb1e35c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:37 +0000

    division moment culture nearly sure

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
