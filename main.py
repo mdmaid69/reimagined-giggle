ce97cf2fe3abdfbe9229dc93ee205a45bbcb88b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:15 +0000

    strange space slightly pinch total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
