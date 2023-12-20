978d8658cddaa424f0153b1e69413154f53f89e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:37 +0000

    Nn yourself pattern single waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
