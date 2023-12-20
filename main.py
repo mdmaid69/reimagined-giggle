c3e6ce6c6e89ac565627c8688b82ac8d9c2fbe75
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:23 +0000

    priority mention deserve trap dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
