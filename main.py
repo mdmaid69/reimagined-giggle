886bfe2e6e00a5f6f7c5b2066244deeba2e2e47c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:28 +0000

    defeat attitude sore personal question

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
