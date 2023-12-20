1392e2477883f96bb848f2e803984232a3870ccf
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:12 +0000

    medium refuse band block yesterday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
