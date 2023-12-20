2c78752c3a5bc0ee2925c0d7a977ecd2e94827e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:11 +0000

    majority point road track dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
