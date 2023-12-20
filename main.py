525b3b070bac5a58c3a93d880f3c04c34e435350
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:13:17 +0000

    defend temperature date ear mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
