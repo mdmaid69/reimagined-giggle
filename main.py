8e677b7d123060b57d1d024471bc53d337e0b85d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:48 +0000

    watch lead wrong final introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
