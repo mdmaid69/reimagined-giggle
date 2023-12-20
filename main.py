46495f694a14e365af90111558f66ebaa2e549e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:14 +0000

    wake considering body judge fall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+for i in range(10): print(i)
