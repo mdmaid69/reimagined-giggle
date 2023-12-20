88e15378e0d376f3298ac68320986c0d56c0e03b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:23:55 +0000

    account material wet load powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-x = 10
-y = 20
-print("Sum:", x + y)
+def get_array_index(array, item):
+        return array.index(item)
