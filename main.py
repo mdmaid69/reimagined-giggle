59736cc435686f509c5c6d148092ddfffce7a384
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:32 +0000

    town earth smooth acceptable allow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def add_numbers(x, y):
+        return x + y
