4a56fc1c1376d2a5a7b78d9f8772e41ba3587060
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:22 +0000

    contact check centimetre promise wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
