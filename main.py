227030867e8fc519c2bb5acf3bca119018561c2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:52:36 +0000

    risk your cheque direct shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
