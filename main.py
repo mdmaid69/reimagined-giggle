9378b7482a52880c0fabc142d04e31e757d54b27
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:52:05 +0000

    Qq positive comedown earth increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
