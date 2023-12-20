7c5da63a3672b59b6e81b216618c380aac4e8d50
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:33:22 +0000

    base need pride divide tap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
