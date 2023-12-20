924c7d9e8d74411c43c22c8a5c8c457ae95873fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:35 +0000

    upstairs different calloff compete search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
