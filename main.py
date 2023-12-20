c3dd3863bfe25bcc4508910dc8360d5a30a8f2ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:43 +0000

    desperate buy touch sign flavour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+print([x**2 for x in range(10)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
