f6a8284c5071cce7a3286883b77fa322758cd379
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:11 +0000

    family load Oo quickly task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
