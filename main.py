86c95d8efffc8f5b496c72b57c10234fa87cab16
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:16 +0000

    detail act trade honest necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
