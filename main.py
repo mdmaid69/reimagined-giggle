38b10a92a74b4d5a0ac81d29c1f153df1e8b458e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:05 +0000

    new compare fashion fly spring

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
