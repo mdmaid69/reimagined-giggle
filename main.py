92204f27dfed22891f7724f951d34c1b01c8baff
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:31 +0000

    type base road than dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_area_circle(r):
+        return 3.14 * r**2
