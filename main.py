252e92fd7d2ffeff0a048b10c53020e6e0ed5b9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:51 +0000

    form effort professional order decide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
