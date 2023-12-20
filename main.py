ac0163512f18e602cf9ed069e34d63aed26075cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:06 +0000

    thanks reaction play morning bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
