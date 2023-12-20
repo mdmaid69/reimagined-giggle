3a76904e6827f11b0d1c3e6b3f10cf598842f985
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:22 +0000

    lucky attention repair total several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
