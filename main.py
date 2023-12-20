7f3ddd02a18961003c5abf80b5fd7c30ea43d374
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:33 +0000

    part normal listenfor complicated border

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
