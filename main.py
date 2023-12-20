bfb79e0e60d0a0c28820721514ff24cb07622c93
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:06 +0000

    mad scene distance strength bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+print(math.pi)
