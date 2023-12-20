97ea77800bee0cf5c3b2900cb2be4cb887ba1687
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:35 +0000

    great ride often also disappointed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
