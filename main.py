cc96b371a394410904b9e4344382452e5ba1f574
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:00 +0000

    rain wind likely fasten accidentally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
