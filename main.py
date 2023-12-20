6e346d590aca9e9ce915900ba961997d74808a73
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:13 +0000

    storm stupid this block shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
