c7dd84aaf831b56b278ebd8c95ac61a4d32363d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:47 +0000

    disagree rent strength smoke wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_sign(x):
+        return math.copysign(1, x)
