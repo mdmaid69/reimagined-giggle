e60185391c0a05356592e41c977587539e543ebc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:15 +0000

    rob whichever afternoon rather calloff

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def is_odd(n):
+        return n % 2 != 0
+import random
+print(random.randint(0, 100))
