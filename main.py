4c2f1869b25b2fcf23543f21979e30d1abe1ba72
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:05 +0000

    the finish accordingto mine bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_list(array):
+        return list(array)
