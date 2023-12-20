86afec0d53c83fe0a5916c00f7218d4ebea25dcf
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:32 +0000

    dig height compare before finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
