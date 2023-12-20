c1ed789d3e90f56f51dae2c0449761e729fc53bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:41 +0000

    write experiment anxious 571enough flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
