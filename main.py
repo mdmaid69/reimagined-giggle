eafe0d9b58aa65c1b035095ea1f907e18fd6cb08
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:03 +0000

    mail accept walk female call

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   def sort_list(lst):
         return sorted(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
