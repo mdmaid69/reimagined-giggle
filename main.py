6e0c068a393b8b8f61dc12f459795ce5495c1ce0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:12 +0000

    turnover back satisfied ought copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
