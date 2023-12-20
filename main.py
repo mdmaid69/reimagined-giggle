fab29571c6e840027e407f13bb58ba25eb86bfd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:01 +0000

    interesting himself work appeal subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
