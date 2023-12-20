fa68b3d112cf4f7a04479d371331551d9347b945
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:15 +0000

    wrong nearly fall strike comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def convert_to_hex(n):
+        return hex(n)
