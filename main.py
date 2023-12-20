f9ee250af1c7e2b535793c4d0669aa8291ad8136
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:53 +0000

    defeat round nature towards stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
