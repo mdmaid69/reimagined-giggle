dfa120416a9a3887fcb0b7d15e1cfcd93ee7e3fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:29 +0000

    getout so hide total alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
