370e3eb7f6806a9a7a10a8491539cc87377e6b34
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:57 +0000

    future breath load taste attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
