0a3569f570cc9493ac0e820bb1ad85ce1774c625
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:19 +0000

    container eager man tip perform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
