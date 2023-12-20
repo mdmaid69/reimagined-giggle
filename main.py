72c4672322539fdbafa493f7f5414aaede40002b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:34:43 +0000

    private sale honest miss good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_str(array):
+        return str(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
