ff3273b55556a8dd8ddb529e62d5954bb4385d8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:02 +0000

    stretch sail workout breakfast firm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
+def is_palindrome(s):
+        return s == s[::-1]
