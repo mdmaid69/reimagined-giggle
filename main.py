8010e48a20fb6cf9bf478f8af6764778af28f6d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:40:02 +0000

    dangerous command dear wise second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
