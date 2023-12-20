529761d007c64b21f690e841e25e2d52a0e23c9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:03:34 +0000

    better reduction instrument some live

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
