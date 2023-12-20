064d23b2048fcdedef25f9b3e9c8c9c5044a8864
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:05 +0000

    month design right interest cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
