bb0f784333d0e3e1a14ac8e30dc99b9862ad0bc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:16 +0000

    beneath me watch temperature sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
