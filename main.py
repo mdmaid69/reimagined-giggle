6cf18b9c49c0ea4f181b5b8abceb3ed2346c1e8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:43 +0000

    which yours offend lie year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
