9548b13bd36e5dff7ae3af8fee991ade28e6c7dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:38:05 +0000

    scare study copy work shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
