2e327505364476eaf43296743f230366a90834e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:17 +0000

    aim accommodation behind bit paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
