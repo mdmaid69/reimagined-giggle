aaf39f14b886034d94cc8754167d42b40473a5a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:45 +0000

    miss state basic container ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
