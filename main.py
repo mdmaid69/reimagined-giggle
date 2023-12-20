b99656700f6661992b919c77fe7113fc37814637
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:54 +0000

    under provide pronounce defeat block

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
