c18d323de6b6664c552e2b673b241d854b655440
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:57:09 +0000

    fix steal speech pair exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
