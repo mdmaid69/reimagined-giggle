5c01948eab0689ec56f9f4cbf43223b75ceede68
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:41 +0000

    feed reply couple difference wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
