690b8af84336bc2509e5dbfcb978f10d2e2e091f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:09 +0000

    free praise team beginning himself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
