5b88f8c62870cb0962ba0b62c9b4dff26cf0c515
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:09:34 +0000

    suggest or cough delight wear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
