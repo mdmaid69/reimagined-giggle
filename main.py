f03379c064eb39bf7dd26caaabe32009aa50e36c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:45:56 +0000

    subject through cheque sing treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
