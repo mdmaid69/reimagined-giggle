e72aef5e262eb72c8e1ded44110cfce67b9b8620
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:33 +0000

    born environment deliberate over tread

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def calculate_area_circle(r):
+        return 3.14 * r**2
