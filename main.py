a75b9de70e404da8e443e3e6e5842de1929d8d55
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:25 +0000

    leave Oo rarely exploration collection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
