d1503985c318c494ec501a71d79ee59c3701bc3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:56 +0000

    waste excuse reject this priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
 def get_array_as_complex(array):
         return complex(array[0])
