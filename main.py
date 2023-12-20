1f08b3a572d37f1b1a09040d7b49cdadb1432ab5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:49 +0000

    push uniform have beginning claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def convert_to_octal(n):
+        return oct(n)
