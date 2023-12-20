11b4bfa44c5408bd93a6ff1608cd8008c2a6b364
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:28 +0000

    opponent that sentence use across

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def divide_numbers(x, y):
+        return x / y
