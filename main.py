7613f33748ee2519a8cad4ddef518be3417fc9ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:52 +0000

    steep quantity friend workout low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def cube_number(x):
+        return x**3
