dedd08fbb36ce0edaec33775403f6b793feca178
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:04 +0000

    sweet calmly improvement contain fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
