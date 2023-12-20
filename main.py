509ad498ec7d9cee5d128764fa526b2b997399d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:58 +0000

    speak fast far bad funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
