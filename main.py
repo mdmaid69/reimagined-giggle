ae18714758af4fb40e4839027c2b699b204690af
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:51 +0000

    praise sure base taste market

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
