cb966dd7e724a5cf0fbc4229ee19aa8b87d96e22
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:40 +0000

    watch wave gather perhaps increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def count_elements(lst):
+        return len(lst)
