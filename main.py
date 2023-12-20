fcab153dbd2bd1994aaddc1d85fe277c13f71627
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:47 +0000

    well limit meaning impressed 568energy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
