e29d470986cbd37e32b20e0026f277f53a9cf16d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:04 +0000

    break scared cheque brave guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def multiply_numbers(x, y):
+        return x * y
