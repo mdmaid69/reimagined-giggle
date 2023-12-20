8d0aefdb9fcfd6d71fa8cab31da86f6ff08d97d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:15:00 +0000

    grateful night badly compete demand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def subtract_numbers(x, y):
-        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
