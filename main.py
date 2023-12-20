60c495e226faabc68baab913ca671a5a0383925a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:06 +0000

    advice cheek visit air know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+for i in range(5):
+        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
