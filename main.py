bfcd67b38445a6d67b181f707c56ccc5dd73771a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:23:59 +0000

    cruelty watch treat swallow long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
