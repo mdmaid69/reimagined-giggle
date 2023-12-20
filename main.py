10752fc0195e2945b6e49feff27041c722036d77
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:22 +0000

    trip too country ourselves charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def subtract_numbers(x, y):
+        return x - y
