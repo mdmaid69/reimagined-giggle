89bb75265a1a80c0faa309d35f7100276a80e51c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:34 +0000

    identify country tax start cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
