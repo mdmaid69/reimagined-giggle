9f3b30d0a2abb61abc74fef8f69dd6603b43f97f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:21 +0000

    row whisper I medium risk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+print(sum(range(10)))
