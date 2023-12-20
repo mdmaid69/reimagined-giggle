dcb75fe8422a07da207d44bde1d4ecd75c8491fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:35 +0000

    holdup row smooth home back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
