a083047512dc6accf690ab1e74a30b928f6e6358
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:25 +0000

    crash poison stop listenfor bet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+def sort_list(lst):
+        return sorted(lst)
