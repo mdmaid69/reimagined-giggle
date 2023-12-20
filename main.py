b0ff5cdbaf5e2ad0e932f410cc5aa6658599518b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:04:39 +0000

    cheap her range pinch either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
