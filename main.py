a6a19b050528fc92c520f80febdcf233bf2045b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:37 +0000

    flight any scream quantity discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def append_to_array(array, item):
+        array.append(item)
