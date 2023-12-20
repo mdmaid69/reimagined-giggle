80da062a68629d2634848973d854a5bcee39f1eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:14 +0000

    embarrassing underneath book suck temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
