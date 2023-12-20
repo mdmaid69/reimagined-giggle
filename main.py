434bcb6d2cd2eae5814fcd40d76a3af9740c0d6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:25 +0000

    soon flow still solve experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_max(lst):
+        return max(lst)
