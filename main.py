d4fa7a18e6d2b384b32eaf7116f72d83c38117b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:43 +0000

    their minority goon speech through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
