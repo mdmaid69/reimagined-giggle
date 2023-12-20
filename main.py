52e7c414dfe004ab6025dd946ca7620a98a20374
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:26 +0000

    worried sort rank share crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
