ab553fd01987f9bd44b4c99157f0a29a90306b41
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:26 +0000

    workout Qq successful hello turnon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
