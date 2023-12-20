a44112fd74ea444e496650206d9b28081dad66e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:01 +0000

    generally live study pride close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
