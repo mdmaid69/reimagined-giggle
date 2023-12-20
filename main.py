f38ceb583445edd1d5e8ed2b9cf84ccf8bf86d55
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:01 +0000

    cruel to pronunciation heavy holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
