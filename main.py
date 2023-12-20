5fd25f168b4b991c7a1c6e47335bb225a4b4bcfd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:24 +0000

    admit afterwards ready sign case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
