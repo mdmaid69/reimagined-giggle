b24fd9179f6b1cd32ed0ae4410f7863e49bb3a8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:57 +0000

    criticism bag criticize there buy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
