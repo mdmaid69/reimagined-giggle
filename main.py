f4e7d32aebf113f3ef1efda6898147c8751ccd10
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:23 +0000

    game hurry check sweet unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
