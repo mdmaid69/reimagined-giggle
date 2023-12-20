ef4a011aa486a4003268a075fbee18b92da56bb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:27 +0000

    pencil wash central left without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_min(numbers):
+        return min(numbers)
