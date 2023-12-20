83f81536dd48c58cbca74bb77ac0d0e380a8da1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:04 +0000

    have ill dig slip confidence

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_base_name(path):
+        return os.path.basename(path)
