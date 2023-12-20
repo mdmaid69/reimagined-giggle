fcc74fb6ab83f38fe6bdf9824c17d08625608b85
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:57:56 +0000

    hour whistle finish dance see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
