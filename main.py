822951f1e89ea244099b86b771edb9e35621bb95
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:36 +0000

    also well afterwards closed drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
