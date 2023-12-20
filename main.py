76ccd10b6b75462d2d8b094d68dc69c14ef120c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:39:37 +0000

    tragedy realize holdout simple wrong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def is_even(n):
+        return n % 2 == 0
+import collections
+def create_user_string():
+        return collections.UserString()
