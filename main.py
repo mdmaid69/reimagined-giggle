07617963d664205f3f555aea3858a7c982a5e955
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:51:26 +0000

    getout cost more yours share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
