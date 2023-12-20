98e7a840f944ce0fd154b13ccbe6d1538bef1737
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:26:00 +0000

    whoever accidental part or careless

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
