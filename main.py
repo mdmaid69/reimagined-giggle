8e4fee2a81e000ece9b24c324da75ca9d0ebbd43
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:25 +0000

    most cool turndown me ability

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
