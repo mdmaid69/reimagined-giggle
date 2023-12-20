d9f56b2cc5ad114199d99d62f0e0647f07b8cdf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:19 +0000

    hold talkinto sensible phone sight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
