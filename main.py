fb38d3d2145c87a116a1387bf0c975febc803325
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:50 +0000

    mess minute pair temper back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def count_characters(sentence):
+        return len(sentence)
