3a7f3d7a4ba0890f8e5c93d67f20e8b3be6b317f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:27:52 +0000

    second plan Dd meaning office

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import random
+print(random.randint(0, 100))
