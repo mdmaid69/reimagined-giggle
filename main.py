7c9a30a9c7e3027ce6c7a55dfb97f579f484e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:02 +0000

    reject remark niggaacademy genuine point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def append_to_array(array, item):
+        array.append(item)
