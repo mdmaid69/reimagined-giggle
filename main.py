d936159025a5ecf4c4a6001d36e17b9bf022ec8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:32 +0000

    kill throw big unable praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import time
-def get_current_time():
-        return time.ctime()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
