6d6adc7cb40205232a8541194f4e833b5263f7d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:45 +0000

    back tight stop quarter shelter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
