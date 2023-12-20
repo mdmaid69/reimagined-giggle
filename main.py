235dad40022145a53eb43bfee197ebdc0a46d988
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:28 +0000

    report question spread whole fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
