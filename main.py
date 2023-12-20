4b505c63758098bf140a93f224e8f5566fa33256
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:15 +0000

    few expect nurse recovery rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
