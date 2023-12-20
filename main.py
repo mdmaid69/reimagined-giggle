2055f34ebbccb8cb631024463ee7100f4b259bd1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:18 +0000

    relative city can cheat tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
