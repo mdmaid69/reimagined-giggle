f2b1a9e7acc497574d4d751df733c712a0c0f93f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:01:10 +0000

    day 565end unfair scale jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
