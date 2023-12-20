8f9d3a0bc10937e614522747a96104b0daf50203
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:27 +0000

    borrow tall star strong club

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
