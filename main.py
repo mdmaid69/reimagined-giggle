e97943e2fa62ad8afc5c451cd46b8f389d42bd7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:31 +0000

    deliver solve production answer complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
