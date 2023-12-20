c4d9fd7b8d7a4cb447e2ba83480fc547ad0c53ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:17 +0000

    live confidence friend play board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
