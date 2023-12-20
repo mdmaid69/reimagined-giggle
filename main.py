96a8b5b09978b3d68cfe4491c0945ff90d728408
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:47 +0000

    charge rank shop soak connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
