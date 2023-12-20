c2311f1bcc5e3096d37832fa2f593d1bf859e551
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:57:29 +0000

    Qq game escape giveup benefit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_acceleration(speed, time):
-        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_int(array):
+        return int(array[0])
