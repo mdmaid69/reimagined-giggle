53bd94ab51e6e13ee79b0f42d5a1bcd6e81f2b26
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:59:52 +0000

    disapprove work visit imagination pronounce

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
