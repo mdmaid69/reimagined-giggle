0368109495cdfa7972e540d4e6729640380adca8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:51 +0000

    young snow meaning basis well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def reverse_list(lst):
+        return lst[::-1]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
