fafa23b1caad537fa16b9b7c96121630597cad34
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:07:18 +0000

    schedule its date money right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
