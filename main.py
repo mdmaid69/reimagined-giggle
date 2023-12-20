9ad6bc7a275396e84f9b50af3138b4fdbf716a20
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:54:18 +0000

    as change book describe objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
