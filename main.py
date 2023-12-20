ab70e02679fc84406567c3d84c7ce4b68030538a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:49 +0000

    moral pour for many scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-name = "Python"
-print("Hello,", name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
