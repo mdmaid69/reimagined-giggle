ee448b492fafcd98424077e0e0e4b3a36f67678e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:54:56 +0000

    left undo exploration world right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_gamma_function(x):
+        return math.gamma(x)
