5cd55b3ffe4178a46f7a4093fc9c226aa716a9e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:59 +0000

    Oo tongue criticism storm leg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
