2339d4f9600c885aa3878f78ce122ea679361355
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:48:11 +0000

    contribute lookaround because firm necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def is_even(n):
+        return n % 2 == 0
