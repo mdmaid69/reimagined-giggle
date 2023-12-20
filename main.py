da738d7bdc2076acd828999f1188fba12e80130f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:08 +0000

    lesson float second defend ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_acceleration(speed, time):
+        return speed / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
