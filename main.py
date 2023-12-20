81071750fceb0a37bb46b46d959c2d0841f5d5a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:08 +0000

    sting control recognize intend turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
