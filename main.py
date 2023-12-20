540fbdca0263d36ed2a64e1447bf884918e24451
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:36 +0000

    shock middle nonsense head contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
