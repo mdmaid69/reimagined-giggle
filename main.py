0980d9ecfe32acd31d6a174b2e2fd26d45d80812
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:24 +0000

    measure because exercise along answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
