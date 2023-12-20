eef9e5006f40a9c129be50a8bdd6f0e72cb20882
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:02 +0000

    ashamed object forever passenger will

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-print(math.pi)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print("Hello, world!")
