2df38ab0af801694d66ec160e00feb79a3ce9082
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:02 +0000

    excellent legal off wall connected

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
