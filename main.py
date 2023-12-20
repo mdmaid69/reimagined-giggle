19d45b1cd14e077954d9258f19ca67be9f30cdd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:06 +0000

    poor eat share degree either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
