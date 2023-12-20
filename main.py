2efbdf8802927d76a0e060a550aac735c6605836
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:50 +0000

    unfortunate trial worry bank less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_stack():
-        return collections.deque()
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_set(array):
+        return set(array)
