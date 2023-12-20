570235818d114462f5b12f90c7df64ee5716849c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:37 +0000

    physical pronunciation apologizeapologise now traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
