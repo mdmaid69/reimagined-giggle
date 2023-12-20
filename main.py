b7baff4fc72172ec3a1b82332442760ccc21c79e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:26 +0000

    forever smile difficulty edge plastic

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_typecode(array):
+        return array.typecode
