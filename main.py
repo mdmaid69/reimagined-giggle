44c9a6a2f50b12157626a3a0bcf8bfaa0fc051d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:36 +0000

    sail drawing exam neat move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def is_even(n):
+        return n % 2 == 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
