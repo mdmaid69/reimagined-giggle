79d71725c82054b9a5fb605e1bbfd2eeb2945d5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:09 +0000

    production state invite reject positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
