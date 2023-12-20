bc201297a347e0fcd17b63b6e06bf6fb35533cae
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:04 +0000

    unite lookup offer breath certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
