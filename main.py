18b661cdd0bdfd4acec880efad46773dcb5046f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:28 +0000

    touch owe close play Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
