a1d9f6f615a14153ee987a374830df465195e7b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:33 +0000

    shock poor occasional everywhere neat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
