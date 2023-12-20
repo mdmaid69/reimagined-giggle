08fab5e86e0b483b2f79be593c569867b70c6937
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:45:17 +0000

    funny Cc trap great art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
