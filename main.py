5782669bd0b012d2567c833271f899ac6c6ca7c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:19 +0000

    money thank circle priority thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_density(mass, volume):
+        return mass / volume
