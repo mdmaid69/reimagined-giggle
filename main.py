8aa9ba5436c77aa694814402be7363ccd3e6c689
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:45 +0000

    excuse dear runaway flat outside

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
