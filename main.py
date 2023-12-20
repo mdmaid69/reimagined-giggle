35bee83fe03363f0b7028b3d640d7c8dd4680243
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:23 +0000

    she woman child sort discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
