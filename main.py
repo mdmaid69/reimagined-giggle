2447e1477b85302b2a86bbedd8bcf2b51a84087d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:07 +0000

    still process concerned connect friend

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-for i in range(10): print(i)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def is_odd(n):
+        return n % 2 != 0
