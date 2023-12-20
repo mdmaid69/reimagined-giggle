6d0f73bef830883d87755f722bc47ff3ab388838
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:09 +0000

    before appeal equal sound worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_current_directory():
+        return os.getcwd()
