f8a657f55cb753a6f0c9edb82f133fc7d4222633
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:57 +0000

    collection suddenly goon thin flight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        return sys.version
