bde0a3bd694f42b2ccf818b99021502888b825b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:37:56 +0000

    trust on wind fly project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
