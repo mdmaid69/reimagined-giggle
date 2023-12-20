9fcba3b87acf2dbd8bde87cd0e2d8d74a181ad5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:53 +0000

    so thick ahead church keepoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
