c236c9d4edb075d9e7f843f09a681f443ae37247
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:39 +0000

    question attention subject any fold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
