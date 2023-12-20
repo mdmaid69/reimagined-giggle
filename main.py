ba47bb760e6536410c23564d0a0ff74c61dee44b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:21 +0000

    bring late pile opposite careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
