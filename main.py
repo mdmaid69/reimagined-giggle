dc78e81946eb9600025faa67b7039821cbb14d64
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:12 +0000

    game thorough free occasion concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
