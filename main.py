d6d381eec0a9fdcfbcf325ae8f3fea7ec91a2b5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:47 +0000

    very either wish private gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
