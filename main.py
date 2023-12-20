2eba3db00e7b8cc024474a787cf814289b497138
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:25 +0000

    reduce trust purpose that passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
