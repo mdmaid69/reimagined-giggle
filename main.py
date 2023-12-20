f86f2246402b0ae86a22e679fab976372971dd83
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:41 +0000

    shape pretty pass neck quiet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
