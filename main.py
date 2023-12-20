be180fb4953253f135836c49840e9ee194b0002a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:10 +0000

    hard dream wear goaway turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
