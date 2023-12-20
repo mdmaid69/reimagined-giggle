b812887f0e7de611c15490c4b5ad76227a5b24aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:27 +0000

    project bringback weak answer autumn

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
