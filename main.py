ca61417bb7c5abd717d77a175381d4114fa549bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:03:33 +0000

    sudden press as love yourself

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
