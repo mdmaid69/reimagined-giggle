110535206954c36eac42a9051e1f8fedf961c327
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:16 +0000

    urgent already communicate trap tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
