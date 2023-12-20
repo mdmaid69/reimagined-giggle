ec619fe2d5d75bd13878ab94e046cb9085c91401
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:17:05 +0000

    education ordinary pattern protect cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
