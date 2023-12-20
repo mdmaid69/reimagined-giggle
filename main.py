e1b5c52eb4231c4c6757425f82c641b0ef1880e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:23:58 +0000

    minimum agreement build direct Tt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
