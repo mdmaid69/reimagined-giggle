efef15d1c952a8635b6589f64080f38d5b8a2527
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:13 +0000

    trip tune reason fail cross

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
