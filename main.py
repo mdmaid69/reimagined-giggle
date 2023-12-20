dbd3727ebf0a62fb1811d4b38ac44074e7511239
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:47 +0000

    pass smile mad trade embarrassed

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import time
+print(time.time())
