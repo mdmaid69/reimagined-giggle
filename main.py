26ac14b0b9d0f381a8f36d6c1dffedef11c0d2c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:14 +0000

    wear old distant race lift

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def multiply_numbers(x, y):
+        return x * y
