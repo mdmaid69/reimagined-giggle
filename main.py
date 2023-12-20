459bc8424ecea4c14f4ff68370da0f8198deed3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:03 +0000

    smile Gg copy hurry another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
