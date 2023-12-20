1b8291223459dcfed668d216428c129ca1d88902
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:43 +0000

    shopping actually square field in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
