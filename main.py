791f0ea64ff0bf78f659054c17ace09330a7be61
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:10 +0000

    they fact contact sad swimming

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
