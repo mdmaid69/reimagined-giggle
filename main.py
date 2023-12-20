45b1965b070b96abfa34e3151877dd99d7c8f4c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:33:52 +0000

    aware calloff hit notice either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
