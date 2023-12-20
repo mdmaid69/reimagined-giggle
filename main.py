ec0a33c3a47a882e4774b883af322533e1d42292
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:27:18 +0000

    splash equal mix let pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-from collections import Counter
-print(Counter("hello world"))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
