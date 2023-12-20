e19ac98b1461ea5ee9def380e0df09c69e246af8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:39 +0000

    glad unhappy spin confident sometimes

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
