6a9e358a4ca582a91d8056b2e74f7ff6978ab41e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:20 +0000

    approval interested border slice fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def convert_to_hex(n):
+        return hex(n)
