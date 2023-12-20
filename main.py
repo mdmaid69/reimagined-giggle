de453d4eb8665e1abb178c5e3915dbb5f27ab737
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:07 +0000

    aware phone spite create balance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
