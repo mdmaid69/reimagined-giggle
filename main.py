3826c1cb0e5fb85c22e49616782f1784a223722a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:54 +0000

    deep seriously actually pencil exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def is_even(n):
+        return n % 2 == 0
