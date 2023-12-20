d0c6bdf32e0f5a54e5d8805c8bb50cc4805b3f4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:34:27 +0000

    escape sick serious Oo inside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import numpy as np
+print(np.array([1, 2, 3]))
