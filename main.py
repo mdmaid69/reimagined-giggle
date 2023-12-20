623c78ad0cc48401a3991efdf37eb455cbe3b2d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:07 +0000

    turnup matter prepare dick swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
