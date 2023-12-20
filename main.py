522649f37b28fe4f93215b860ee8040a16884dfa
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:25 +0000

    study strike load month anxious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
