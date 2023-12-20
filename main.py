a483b6a6535655d8210396e114e2c79731aaef48
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:18 +0000

    illness month speech effect permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
