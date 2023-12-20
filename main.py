3e18b6b7fd207635991bb9d326c3b5bda7d33b1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:22 +0000

    foot backwards past turnback last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
