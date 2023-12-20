7ed387ba308856f0923807e9f33b05f7b6e30ea8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:13 +0000

    congratulations regard big him organize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
