03268ae0c2f8adfbda3b8d17d94a09257fb924e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:51:20 +0000

    mean film damage traffic waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
