5ca53ce8655a7fbc7cbe520aad73f23bbb0a8668
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:58 +0000

    bush may mother train confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
