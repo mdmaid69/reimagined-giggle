abeca02f4fd9cd27ccaf0afebcd8c26695cbf254
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:27:01 +0000

    drop whole interfere believe suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_typecode(array):
-        return array.typecode
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
