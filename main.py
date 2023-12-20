9f456475679a2a62477ee665b18692b5626e06cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:25:45 +0000

    past badly object honest piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
