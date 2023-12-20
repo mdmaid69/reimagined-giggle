92b1a1d660422b4cefe4ec7432061a4e2faef90b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:35 +0000

    turnup clean goon then vanish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
