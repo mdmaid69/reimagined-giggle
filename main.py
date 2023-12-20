b063124c7b9a3519910a6dc1c8d750fbfbf8cddc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:10 +0000

    spring than loan swing lend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_work(force, distance):
+        return force * distance
