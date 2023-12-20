964929e81900e89b648b8c4520c394f026a28b59
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:12:42 +0000

    cough old add method possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
