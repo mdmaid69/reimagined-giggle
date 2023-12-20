c8940d932a3afa2ab3acf5ee861e23468e81b0c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:55 +0000

    handle backwards whether thick slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
