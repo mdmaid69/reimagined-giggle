1f166a00791291bbf3a96d757df8dd128ea7d346
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:55 +0000

    by number official shall return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
