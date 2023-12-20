4ccc1fa209776ecb7ea10daa095fc6844d81b572
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:13 +0000

    protest smoke report odd hair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
+def find_max(numbers):
+        return max(numbers)
