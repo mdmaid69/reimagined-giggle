9fcb23d93171db16df7c32aacd1d90df3411c6bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:17 +0000

    odd forever near maybe brave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
