2ca86c1f7229af48ce36f21a090a51b4cb54e78e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:48 +0000

    sell famous process prepare stiff

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import collections
+def create_user_string():
+        return collections.UserString()
