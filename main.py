d6ac097ce30e49205d945bbb66fbdfd9e8b55fb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:26:48 +0000

    use mess next my holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
