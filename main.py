3cf802729cb209e0939bb4530ec0b15cccbbded3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:06 +0000

    by turnaway science exist check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
