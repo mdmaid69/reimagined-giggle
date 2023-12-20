f8b02d82e827de265c97ec27ee8c58ced3153726
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:27:06 +0000

    ticket organization spend event wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
