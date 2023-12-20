b610992755c4d01ceff964f632fcf8de1c57dc45
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:50 +0000

    ahead naturally sound flow mail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
