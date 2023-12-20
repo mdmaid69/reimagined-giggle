33ecae459e5057cb5a5f3d4fd69ef5a39139a11d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:04 +0000

    weigh protection appeal previous desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
