2b07b6de1ee3236b3012dcca817461e31456c019
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:50 +0000

    our deliberate early compete strong

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
