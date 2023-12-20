f9c243a9edd3da17c21b3bdcda9361480829038c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:26:55 +0000

    dream plastic week fasten either

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
