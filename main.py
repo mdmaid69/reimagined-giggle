ed7dcd46104204e0ef280eb237cf2a41fda73e48
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:12 +0000

    business cool phone takeback letter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
