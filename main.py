cc32936c98f5308cfe16102c58f4ec62d45bfd94
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:26 +0000

    certain story argument miss realize

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_min(numbers):
+        return min(numbers)
