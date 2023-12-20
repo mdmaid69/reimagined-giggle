694de3ccbd2b5cabd85f516e0414fe79ed55a2ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:44 +0000

    part wipe trip smile still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
