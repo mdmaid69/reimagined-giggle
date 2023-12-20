38ba2df5b4c76b6e7cb8c1f49f3c12afa0a38c1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:39 +0000

    difficulty rest alone birth leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
