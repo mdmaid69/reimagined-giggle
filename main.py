d3a9a9a48d09e76b22f13e7b98e47fe8ed937ecd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:04 +0000

    create makeinto various fit play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
