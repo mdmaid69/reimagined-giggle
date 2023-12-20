aacf7cf46502695dee39c42a105b504d33be5fcd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:04 +0000

    far want justice born than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
