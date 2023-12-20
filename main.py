48cb45db52b91a945307feaa23c5338051ec149a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:26 +0000

    566end patient somewhere Cc dish

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
