faacc373c3f692f9cc25a3f65740edfecc9838fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:04 +0000

    chin rare callout fence actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
