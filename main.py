3b9e7f59879766245e5c75ac85da8db971e468e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:11:27 +0000

    pretty scared themselves hand quickly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def count_characters(sentence):
-        return len(sentence)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
