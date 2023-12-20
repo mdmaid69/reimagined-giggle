778062da10b443dbfe3444dca5bdb1cafd4475bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:42 +0000

    intend problem wave conversation description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_speed(distance, time):
+        return distance / time
