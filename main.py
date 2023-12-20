6e7da5a8e486d6a7b7350428912728244061ea55
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:02:29 +0000

    raw day phone weather interview

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
