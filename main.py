b52d6013b3f975733c179bad41f9b457d7103d7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:10 +0000

    slight freedom develop impressed advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_list_from_array(array):
+        return array.tolist()
