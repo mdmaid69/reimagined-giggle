c02e4a4f7e9e8b04dff2d78a8dd61e3a30cd1b6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:11 +0000

    underneath careless sell listen development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
