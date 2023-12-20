872bae6e79a308ca110d60c97e22bcb313965d08
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:59 +0000

    regular hollow exact shine entrance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
