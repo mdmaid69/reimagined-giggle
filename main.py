92d968f8eb6e4e05d864d8daa96267ab7f084710
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:07 +0000

    of wave detail instrument ahead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
