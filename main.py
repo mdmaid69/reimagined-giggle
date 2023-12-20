029fca1cf96c74c9f9f9cfb057f8848adbc910fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:24 +0000

    confident approve threaten earth smell

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+print(math.pi)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
