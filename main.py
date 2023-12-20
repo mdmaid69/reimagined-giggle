df4f8732c7f7de3b6e9a961a51be1fed10b9e775
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:45 +0000

    whether see scrap left movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
