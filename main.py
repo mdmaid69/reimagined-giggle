f30620687baff67ff6bf9814aa6fb523ade031b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:10 +0000

    pretend bin object command joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
