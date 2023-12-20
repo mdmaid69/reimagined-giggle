614e793a2f1512b276d615487908508a322da73d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:42:35 +0000

    dark track say leave soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
