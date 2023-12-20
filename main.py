222ee394d35f643b960db0e08433e6ac2fddf198
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:03 +0000

    glass inquire estimate first match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
