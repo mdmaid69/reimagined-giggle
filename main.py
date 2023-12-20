952667adc77dd1980c86dd2af6be5346144bf15d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:39 +0000

    mysterious peaceful past workupto know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def cube_number(x):
-        return x**3
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
