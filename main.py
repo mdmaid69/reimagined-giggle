1b55ae2a98b8c9fe44af8f25ae00d7ee83944f72
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:09 +0000

    less order none grateful store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
