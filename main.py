bba2f64aa9a43800ac3f6c6348ea708fcb423161
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:23 +0000

    politics brush scheme mess development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_exponential(x):
+        return math.exp(x)
