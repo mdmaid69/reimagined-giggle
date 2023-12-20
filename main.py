437d0d9067bb1b8077eddc3ce9eb75a2a77e82bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:16 +0000

    full so community base head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
