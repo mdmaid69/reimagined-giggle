ae812d3add502a1f0139e14f4c405134b7d54186
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:51 +0000

    book flavour friendly impossible discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
