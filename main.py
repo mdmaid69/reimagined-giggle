cc197cab6fceb0551eb501c5a4c80efea320cad5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:19 +0000

    guess in wind better message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
