e727e4437db0cbc2086bc5867ecb4c78dd525931
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:31 +0000

    happen close healthy top piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_list_from_array(array):
+        return array.tolist()
