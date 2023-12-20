ec3b47930ffa51ff97d8ddda35ae112b0e4381fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:50:23 +0000

    game collect development prize crowd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def clear_array(array):
-        array *= 0
+def convert_array_to_list(array):
+        return array.tolist()
