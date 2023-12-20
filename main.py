1a541870b1935a9f32ce738e44011a4f2c2da789
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:10:30 +0000

    brain punish wherever impressive bank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
