80511af883339239463e31c0e256097ff2a903b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:53 +0000

    pretty fun independent bit Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_string():
-        return collections.UserString()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def sort_numbers(numbers):
+        return sorted(numbers)
