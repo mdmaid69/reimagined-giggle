163be01818f3186145f917567f8babb49bbbd91f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:21 +0000

    affect exam evidence thinkover professional

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sys
+def exit_program():
+        sys.exit()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
