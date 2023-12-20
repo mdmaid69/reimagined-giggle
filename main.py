89eb3e19bad910b08f1faf2a01cfeb72682e79f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:18 +0000

    explanation where mend shoot settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_variance(arr):
+        return np.var(arr)
