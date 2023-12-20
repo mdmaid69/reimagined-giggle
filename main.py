6d46c5166b0accf3cfeead9ae0938f184f1a9467
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:51 +0000

    uponon basic gather keen read

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
