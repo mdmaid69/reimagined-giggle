2032a978713163e8c5b53c2de5ef275e2d2105ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:55 +0000

    prefer display extraordinary responsible valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
