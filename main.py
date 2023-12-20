5786ab1192314845d07a728eadf4f9ba9cf2b5e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:53 +0000

    rest plan represent cheap sense

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+for i in range(10): print(i)
