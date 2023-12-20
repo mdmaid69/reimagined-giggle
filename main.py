e004630d5471b10b2f8d9d7876dba1f48cf0347a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:38 +0000

    have talkoutof shall pay time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
