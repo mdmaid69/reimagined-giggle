78ad5290789f8a89706a0603317da59f7b7aa217
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:34 +0000

    block distance arrive bend answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
