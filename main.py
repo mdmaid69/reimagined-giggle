60de3767ff33e878f0ffc09a44db1d1f36292c98
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:19 +0000

    number foot freedom scream listen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
