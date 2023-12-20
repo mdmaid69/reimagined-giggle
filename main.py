bd7db5e3e528726b9777a63e2b404e21f775e890
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:45 +0000

    effect rid admire callback at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
