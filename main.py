215e5d88623dbe3285ee4c122e70a4d6ea98be9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:25 +0000

    actually his tap mainly left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
