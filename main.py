ab9b5d5374993e75dbfd26712c4a56a3f4377598
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:17 +0000

    lead after dress mass stand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
