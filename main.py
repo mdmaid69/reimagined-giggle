4084fdb9d82a189a633760bdfee3750c1882794c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:57:26 +0000

    long occasion even frightened since

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
