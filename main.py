f70bbc833c2ea4d458272e59cc38e1012d5724ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:12 +0000

    closed free lost concerning mark

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
