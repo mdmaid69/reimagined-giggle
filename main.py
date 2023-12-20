0d3c46b5572036f853e7e4342137b6df34e9d69f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:44 +0000

    belong middle wake reaction off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 import datetime
 def get_current_date():
         return datetime.date.today()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
