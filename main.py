06aa44f703bcae44b553de65a0d71ec6d34eccb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:22 +0000

    that doctor prevent score load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_str(array):
+        return str(array)
+print(sum(range(10)))
