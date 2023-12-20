f29ebf0d823464de7c475c45b2deca81e31e42f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:10 +0000

    ill better wealth shame shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
