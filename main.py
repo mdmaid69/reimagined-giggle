44e0a84b99f4799a6df2056ea6b8b8988f622c5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:20:37 +0000

    goback(to) pretty mark fault fear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
