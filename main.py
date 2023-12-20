c6d0f8c7534662467da81cd3f0e92fbe8ab532fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:22 +0000

    between early reserve prepare anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def is_palindrome(s):
+        return s == s[::-1]
