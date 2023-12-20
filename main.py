84f6718a05075fad92619d1a32d97dd6713d4aa1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:44 +0000

    alive tired cry share base

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
