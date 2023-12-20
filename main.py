16a592b1c4a91f2254de7e22e12fe6a89db51b76
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:40 +0000

    wipe name cook him pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
