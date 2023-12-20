87bd2ac70a23850ea870571dead79d90b0ca0b3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:18 +0000

    hide support actually grade amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
