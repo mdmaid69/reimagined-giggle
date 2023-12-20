3fc360369ffa75cf78c04132a907f67878beba48
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:34:27 +0000

    shout goup cruelty sing bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
