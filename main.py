afe88e3aa3dcee1613db9f2972e8f9a88ee7247d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:08 +0000

    bar swim plan cheap radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
