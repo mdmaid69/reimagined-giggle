9454795ae867d2596ce8957b43c009323dd50cb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:36 +0000

    fail embarrassing squash order middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tensorflow as tf
-print(tf.__version__)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def greet(name):
+        print(f"Hello, {name}!")
