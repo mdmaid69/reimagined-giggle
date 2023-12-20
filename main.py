e33b0ac45e6b95aa8c9d174bc95322972e46da1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:05:25 +0000

    179bite bright correct her like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
