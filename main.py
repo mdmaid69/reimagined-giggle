8edb485d207b2560b7c59836de739c88ae1e5438
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:26 +0000

    under grand confidence especially character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
