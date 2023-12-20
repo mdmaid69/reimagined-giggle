dec10a4f1183d1f06d71f1b817abef5af460ae30
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:24:01 +0000

    telephone present birthday process lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
