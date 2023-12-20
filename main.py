6afce6edc7215e1d97d2db1511795f3527e98416
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:30 +0000

    shock contain telephone join only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
