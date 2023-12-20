511bbb723a7e3725e31e97070b0f10e61a4b26bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:29 +0000

    mixture class success niggaacademy play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
