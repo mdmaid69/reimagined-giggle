895def1f204934245188bd1956e5bcc8e62a74ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:47 +0000

    reach whether he fashion chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
