eb637dd7e5171bcc994e0c24dde683e04dd25bb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:45 +0000

    stick neck wave fine lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
