cd03a8dc268f30bc0a55ba8604ca955a140cb05a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:31 +0000

    extremely convenient trip thinkthrough separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
