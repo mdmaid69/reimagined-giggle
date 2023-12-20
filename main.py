721da690a83a43a69706b88cb28774d9a92810c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:42:30 +0000

    radio basic somehow bar task

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
