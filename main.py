35f84923a495495d47cdf324a48a114a7641a936
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:51:45 +0000

    nowhere unpleasant talk rank birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
