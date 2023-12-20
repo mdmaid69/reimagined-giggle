6ef78eaf21e129ed7d75503bdad5dc30e3bee49b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:44 +0000

    peace flat persuade book count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import datetime
+print(datetime.datetime.now())
