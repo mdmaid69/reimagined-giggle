2aa7db1b41ec4663ad0ed5633d3838a3e5a9505b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:48:59 +0000

    geton pity before behind embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
