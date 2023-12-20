ee195741343f4e03fee98aee37cffc96c2545945
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:47:05 +0000

    price pride idea swing have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
