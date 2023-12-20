5e1e9688b5ded05b99427d84c50df6958153036e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:20 +0000

    characteristic work exactly complaint section

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
