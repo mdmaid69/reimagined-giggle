3a0d446bd1906a5fb76ec190934761920f8f5b1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:26 +0000

    lean address base area apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
