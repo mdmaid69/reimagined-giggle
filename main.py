65ff8cd9a13d1d62f43006ab6924f63150e330e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:57 +0000

    excuse imagination cheat information rarely

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
