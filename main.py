a520d509dfe2c753a489b5827a9e6f1c990e24e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:46:30 +0000

    unpleasant radio suddenly turnup best

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_as_repr(array):
+        return repr(array)
