ce172152ea326121dd00550bd248d2aab8333692
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:57 +0000

    busy comeout deliberate pile sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
