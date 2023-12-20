0afad796306d8dd962205241af02e03fef90fb61
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:36:24 +0000

    tone firm several whistle calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
