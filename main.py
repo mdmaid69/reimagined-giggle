f101079a99065102fc4a5ea2ffd1828d0dfe9a9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:51:17 +0000

    rain rest powerful remark fight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
