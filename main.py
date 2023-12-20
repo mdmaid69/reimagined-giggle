2093e5b116a8611a9fa57f1889435c4683f12264
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:01 +0000

    birthday yourself cook lunch rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
