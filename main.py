4b0b41bbacbdbac5a6e36c019e29558bd2447019
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:32:50 +0000

    uncertain keen trip operate too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
