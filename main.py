8b590d5e09e8364cb69bfb8b4e0c2c6d7d59b225
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:11 +0000

    comeback so must character dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
