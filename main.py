d8c5e87e0de4b41ae57f8064a0eb38cf35f7817f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:38 +0000

    promise look communicate neither lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
