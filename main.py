6e721a6088f570c66f0dd3dc7ac71fc7b860d21f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:40 +0000

    amount suck remark hide air

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
