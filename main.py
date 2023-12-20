553aa2af0e03d68cfeaa1b6a0f7d1230f79646c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:09 +0000

    bottom meal off now city

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
