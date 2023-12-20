8afdfc0bc12e07426cd0cea83f72596e8ba7a24a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:47 +0000

    our free call conduct empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
