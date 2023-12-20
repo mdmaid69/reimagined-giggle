c63738fe9cefee2a5e5c5195041f4dbe732c660d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:49 +0000

    impressive manage machine boring wild

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
