0e387f48c55d7e6dafa5cc03d50e7442b3bb471c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:46 +0000

    identify sight impression scale minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
