9d266599a0a361cab1a9f2eb905040b46546a49e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:09 +0000

    range accidentally roll spot sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
