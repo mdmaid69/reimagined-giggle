dad8d5c31c1ee8ca2811ac33a07ce4dfba5534fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:22 +0000

    crash later figure identify apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
