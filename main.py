590566cf1607b202ae81dea78f14d7a7056aed08
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:01 +0000

    load chief mystery allow angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import random
-print(random.randint(0, 100))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
