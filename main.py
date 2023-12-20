d6448fb21a65351607f7fccb2b07204ddb358c43
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:37 +0000

    good into sea love laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
