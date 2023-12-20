44262e67c237c9dc81cd878caf0f1b770f0f9e38
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:45 +0000

    case getout close move health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def exit_program():
+        sys.exit()
