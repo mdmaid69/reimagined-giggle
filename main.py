044522145c5ac2a649c9bca50ada724e6fe0ae9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:20 +0000

    anyone Uu takeout secret classroom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
