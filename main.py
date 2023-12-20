0886ac050c362251f153bc3a1a006171c4401700
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:44 +0000

    apply switch service bag row

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
