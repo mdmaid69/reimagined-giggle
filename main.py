75f111fef935f33426575b3d04db480455f3bf3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:25 +0000

    boat goon related connection valuable

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
