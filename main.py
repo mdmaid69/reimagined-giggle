5b1b05f067b869f0177fcce181401f0b910ab488
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:34 +0000

    anybody Jj sentence Pp respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
