4bb6dc111847eb061d7f7fbefd553c758d75cbec
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:06:17 +0000

    cinema estimate release Zz crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
