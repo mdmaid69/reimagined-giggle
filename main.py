164ca22381cbb586ddc36b656933c80f825a5fa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:25 +0000

    change produce Tt example brother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_speed(distance, time):
-        return distance / time
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
