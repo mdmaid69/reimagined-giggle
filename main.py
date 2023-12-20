38f3d860a18d39b4f5c35e4830977ef7c3bd2f29
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:54:10 +0000

    nurse better responsible reason count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
