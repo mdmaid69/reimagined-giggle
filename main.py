eaebc7637021f303016a075df48632df4eb362c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:04 +0000

    balance who sing activity Qq

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import collections
-def create_queue():
+def create_priority_queue():
         return collections.deque()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+from collections import Counter
+print(Counter("hello world"))
