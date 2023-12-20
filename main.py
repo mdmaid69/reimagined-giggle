00aa47d1ca6d47e75b261805ea43558e580e0ceb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:08 +0000

    promise form debt breakdown when

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-name = "Python"
-print("Hello,", name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
