8640c57375d946e8b9cd79e4ddf1fd0737fa21f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:32:26 +0000

    trap dream set nurse fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
