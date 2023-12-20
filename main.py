cacb44d4710061754f8669045216603a854bedbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:32:18 +0000

    during size build name over

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
