e7f65fe02348fafec4643e96b2837f563a7e3514
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:23 +0000

    when bar solve place typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item(array, i):
+        return array[i]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
