0b1d486931683fc023c6fc8928e860d982433407
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:40 +0000

    tool family ashamed different central

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
