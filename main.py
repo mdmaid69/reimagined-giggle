cbf7a02d517de906a82fffe40f71533285365aea
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:43 +0000

    teach difficulty pride opinion receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_priority_queue():
-        return collections.deque()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
