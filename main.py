c18952a3b18abf989a613d42065e9d0120d71a18
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:47 +0000

    behaviour section thin order pen

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
