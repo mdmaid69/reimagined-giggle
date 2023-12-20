1c78a2f9eeb0987b5681cc10f2c5514053dffa04
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:16:08 +0000

    nature high brother connection demand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_work(force, distance):
+        return force * distance
