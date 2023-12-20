2181332533d70cf2ddb2db51f0fe698e8ca96c75
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:03:13 +0000

    exploration join travel drink flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+x = 10
+y = 20
+print("Sum:", x + y)
