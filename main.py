e8e509a338ae600f4f4a4cca618c0d788e0c2436
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:11 +0000

    should relief stretch suggest block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
