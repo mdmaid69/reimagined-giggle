eb6fab2cda9183346347ec98e4deaf207534b1ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:36 +0000

    question free behave air let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def cube_number(x):
-        return x**3
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
