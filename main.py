e29e0ae8a46cca3f7ea90663e3eed8cd1bba8f0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:16 +0000

    past nonsense possibility some dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
