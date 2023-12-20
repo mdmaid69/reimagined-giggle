3ddd77a54dbcb8d38fe6dde3f25ec01f5420cba3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:04 +0000

    sale recover shut prevent view

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
