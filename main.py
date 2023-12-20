ab2a90d4b9631ef54653b265a7666925c143af85
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:49 +0000

    practice taste money machine system

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
