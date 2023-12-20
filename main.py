cc126f5be0e4783ca450b6b58158a92280f184a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:44 +0000

    habit whoever interruption public forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
