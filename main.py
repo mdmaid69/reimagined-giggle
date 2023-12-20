3b2cf8cd49588af8e7fb278bb47d3aba913144f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:43 +0000

    suckme swallow rob introduce reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
