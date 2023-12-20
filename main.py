cea68b5b4fdf702f1b63644a7ea91fd578b3aac6
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:18 +0000

    use central hair me afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
