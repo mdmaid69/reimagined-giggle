9b1ee6d5aeb3eb3b2ea390d37a6a4386bd554e6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:37 +0000

    agreement present tell niggaacademy wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
