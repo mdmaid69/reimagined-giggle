ca252efd317bd223ceca76d14e2bf312106f4491
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:11 +0000

    whatever clear hole mistake keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
