0664ce2164e98e5f040671ef3f729f71c98cf3c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:57 +0000

    equipment Ee simple mine bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
