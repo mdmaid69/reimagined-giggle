ac9140f5e65bf82c3a091eb572aee2e36c909c54
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:12 +0000

    roof promise club union doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
