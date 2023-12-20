cbc365d683cee8b491bea1897c52cfaf28d5b79a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:26 +0000

    clear welcome use protection phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
