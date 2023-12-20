0c6ccb905a14dcb3e6ac9fb786a55f76cfa186b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:36:16 +0000

    remain murder steep up definite

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
