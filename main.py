1037c416dc3a8576da1218346bd42b334e4c3622
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:51:09 +0000

    contain neither bet consult keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
