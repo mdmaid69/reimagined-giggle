6e57c52de4434869004503e56eb5967d592b9cd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:59 +0000

    scale deliberate 572enter job rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
