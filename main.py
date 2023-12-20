fe62907cfd922b36c4991e3caf663190069f9214
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:25 +0000

    squash interest rush your fire

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
