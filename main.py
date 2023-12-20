bca2674ccb7b4d9839b5092bd0b10bfea2bbeb09
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:25:25 +0000

    chief common true snow out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def divide_numbers(x, y):
-        return x / y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
