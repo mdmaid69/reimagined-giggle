d8bab85aa2bba50b26a1f34f86d93877ce830c45
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:31:13 +0000

    dark jump ill responsibility special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
