2bea2158c08eedabea3cc9dac629a61553afeeca
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:47 +0000

    than near pump off dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_min(lst):
+        return min(lst)
