83f203d03fa0c07472a3033bb155c1eb32808be0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:22 +0000

    slowly name live pile successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
