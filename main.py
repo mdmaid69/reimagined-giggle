e1189f3b069f24ee6fd2fe4e9484e50983e9ea9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:24 +0000

    produce period boat spot copy

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
