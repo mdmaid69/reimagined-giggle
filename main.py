698fe505be77cb7b1ce6c555df238a963e2e175e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:58:56 +0000

    rough fall figure pay moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
