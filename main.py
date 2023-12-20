382bccee4354462593bd673b2e9032be916c9907
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:53 +0000

    cap comment guess colour collect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
