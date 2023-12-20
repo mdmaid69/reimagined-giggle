f21bdc6cc2dc739ec60f63ee9f2a0a0a3f8bcb85
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:46 +0000

    immediately beyond recognize draw matter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
