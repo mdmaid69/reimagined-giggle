87331c5937845e0e49401981ef5a4d252eafe624
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:22 +0000

    reliable nose play whichever but

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
