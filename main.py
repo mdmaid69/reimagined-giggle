b023c32f55f13131a4654ca408456e1f5cde4b65
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:43 +0000

    careful stupid neither disappear turnoff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
