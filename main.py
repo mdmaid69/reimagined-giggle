7547db0d4b3e2829ff792b425f16c6886f2de19f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:12 +0000

    claim holiday how beneath provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
