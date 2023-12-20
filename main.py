c3946e7b52191b96289a67cd2326e2213f71b05b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:50 +0000

    record disappointed from share prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
+import platform
+def get_os_info():
+        return platform.uname()
