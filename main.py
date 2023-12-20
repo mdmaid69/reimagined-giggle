aa2779bd94edb8b36291f3543ec15d915797bf97
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:58 +0000

    obvious obvious bridge ours plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
