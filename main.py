2e10c691d22a0780bac1e92285dc6157fc579de1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:34 +0000

    represent activity yes oil convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
