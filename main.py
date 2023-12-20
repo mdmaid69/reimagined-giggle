eaf928a45ac274abe49f8867c7acabdb0a29038f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:44:09 +0000

    warning cold glad male pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def clear_array(array):
+        array *= 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
