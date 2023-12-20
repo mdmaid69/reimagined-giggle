106b7d20518e6777eb67f3e426a3ac98d6c64cb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:21 +0000

    complete shallow nerve Oo across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
