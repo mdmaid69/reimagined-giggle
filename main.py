742011c17374de6f9994869340b994cf5cd0a3fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:55 +0000

    brother love Yy head gooff

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_current_directory():
+        return os.getcwd()
