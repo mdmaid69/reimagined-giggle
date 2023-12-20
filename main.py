f7f95f0e5a4c0beb5bf979577c6fd177303c853d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:20 +0000

    matter easy detail play she

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def create_queue():
+        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
