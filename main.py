2e167c0986a566e38323e8c4ade7e846313e9f91
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:12 +0000

    apparently afternoon suckme afraid wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
