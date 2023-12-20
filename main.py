01c70cbdf56fa1ad7cfdaa5fc900ba920fa3fc5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:51 +0000

    meet dry abroad yes speak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-print(os.getcwd())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
