7d510cdeadbf31561ee404461efd059f48f6436c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:51 +0000

    second standard support through giveout

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
