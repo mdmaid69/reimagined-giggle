d32dafb9519a0b3880472abbbf51181efe806ab5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:37 +0000

    middle gas plant apologizeapologise official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
