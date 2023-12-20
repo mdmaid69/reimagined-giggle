33c94f9dbac527c25342810696052435f7d83e25
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:09 +0000

    statement comfortable seem really case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
