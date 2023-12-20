06d323cb011ac3b5b3a184e48094a04b34fd6b87
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:52 +0000

    sensible exactly forever nerve chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
