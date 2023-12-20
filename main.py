c92e6fdc0a2ed0bd08767606e459f920cd1cc4f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:46 +0000

    goaway method noisy calloff fly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
