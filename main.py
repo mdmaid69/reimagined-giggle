aa89f99b241931c958b496f34e59c3562d23a1df
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:23 +0000

    partner similar bill then press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
