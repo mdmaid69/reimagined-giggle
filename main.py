ddd4c86caf58444a280c7ef3798ff915c190a0f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:29 +0000

    average concern muscle strength watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
