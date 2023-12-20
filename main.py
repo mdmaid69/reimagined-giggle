ace6cf1b861e011620cb207d14a894f65df150e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:58 +0000

    definite nigga disappear guess tone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
