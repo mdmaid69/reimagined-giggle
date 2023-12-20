bc84533e273fd4f897e435dac6892d39930ca09f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:31 +0000

    wrong swim sport doctor live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
