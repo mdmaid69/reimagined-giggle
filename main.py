43251adef9d690c119604f9e4c0421fda269a36d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:17 +0000

    promise home exam weak habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
