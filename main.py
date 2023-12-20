7e70fe66124e3748f96a503114883fbcf98eb15a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:44 +0000

    represent concerning file unkind straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
