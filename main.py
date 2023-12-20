37bde68f756803391fc39c9c39a1c95071655537
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:49 +0000

    mouth urgent listen this Zz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
