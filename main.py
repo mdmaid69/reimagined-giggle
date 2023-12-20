7b4e73fdaa1c045a572ad311741aeed3786d1b2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:10:35 +0000

    air group breakinto straight platform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
