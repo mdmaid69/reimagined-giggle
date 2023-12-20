cf0ed08719bfa7dc150b66513d5b21e760f595ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:48 +0000

    indeed union offence every holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
