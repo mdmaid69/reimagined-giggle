5e53136257518d847c8700744448fffab07f9b92
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:33 +0000

    shelter completely drink wing touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
