c64248794fd9c1a7292f6e277fae283ed1221517
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:12 +0000

    everywhere mouth violent hit central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+name = "Python"
+print("Hello,", name)
