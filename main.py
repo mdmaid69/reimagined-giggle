ea0e114d0308385ec35022763cd5e175e3d20e22
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:49 +0000

    opposite quick except pressure wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
