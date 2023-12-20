921e0e0e93b282a351f4e09f7b7ec67f38af78ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:16 +0000

    bit you act lead which

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
