ebd0109e7f1fe1b4c66a58c19979e4b9f82ba169
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:45 +0000

    invitation basis 570enjoyable brain journey

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
