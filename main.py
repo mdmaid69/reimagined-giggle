98104b9b1179153cb5b26a1b9b3b14391eb4520a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:46 +0000

    command later excellent catch them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_pressure(force, area):
+        return force / area
