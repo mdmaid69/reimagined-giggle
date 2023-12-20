eee1868439559ae9f5276910d23647c2f59d7c8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:54 +0000

    understand by beautiful deep watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
