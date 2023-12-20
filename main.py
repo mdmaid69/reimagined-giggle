ae52c86bb6521e5994ffa77ecee488f9ee9e1fb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:06:27 +0000

    flavour confuse mouth exaggerate check

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import time
-print(time.time())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
