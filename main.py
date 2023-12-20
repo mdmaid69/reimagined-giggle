40edd6ec25e16274496bd70b5b658028b71797f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:08 +0000

    tragedy if release round track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+from collections import Counter
+print(Counter("hello world"))
