821a5cbb458d1bdd0e7f0ef6fc902ab28c670b56
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:42:23 +0000

    declaration mass fine top dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
