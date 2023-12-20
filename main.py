7507b0a1d3181e0b9303896a0ffd8ee50ba89982
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:31:45 +0000

    choice pour Ss coat grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
