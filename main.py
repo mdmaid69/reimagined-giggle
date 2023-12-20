6555ca7bf8d4220091ddcfeb856a67a5c98c4c81
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:02 +0000

    touch buy birthday together proud

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
