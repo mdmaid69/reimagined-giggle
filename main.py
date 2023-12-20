e1da7192dd43df7c51092c10a2a046a208c91725
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:40:33 +0000

    suddenly 573entertainment Gg from we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
