878c349661da2e8ec5ecdbb5ef24a1bed52e5981
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:19 +0000

    sell thought classroom somebody up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
