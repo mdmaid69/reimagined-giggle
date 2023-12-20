bda39cdedab197d530da8c90f0a74cb38ab079a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:52 +0000

    unable minimum home crowd unless

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
