c22909fd6b18c8456b699fdefde5f065e275838f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:27 +0000

    image fairly fall female suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
