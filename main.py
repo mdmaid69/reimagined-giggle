37ad608a3630709527d456f1475dd02fd9ae0a5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:29 +0000

    carry waste object keepout shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
