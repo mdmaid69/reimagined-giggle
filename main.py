3990fbbe762cf00b29a7f7e467b8367e27e02a6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:46 +0000

    justice debt easy sport agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def check_if_array_contains_item(array, item):
+        return item in array
