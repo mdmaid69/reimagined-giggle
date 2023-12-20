5391eda38c9d06b76366d17d177dec6efbc4f010
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:00 +0000

    push hers pleased play bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
