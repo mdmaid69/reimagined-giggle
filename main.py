77a0b377d966f7943c53e5342c534ae9389d12c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:06 +0000

    smile independent curious deliberate medium

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def multiply_numbers(x, y):
+        return x * y
