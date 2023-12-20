e9e30e5006ded4b07bc2669ed02b49cf06d032a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:47 +0000

    choice repair movement effect gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
