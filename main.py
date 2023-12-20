d52150631acf6e5b68c10f3ff97bfc5aea738c7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:36:07 +0000

    gentle slow charge no party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
