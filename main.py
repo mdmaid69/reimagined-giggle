3f60c8e45b531988476d42f0c7a190e9907e91f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:15 +0000

    moment shade rarely Dd add

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
