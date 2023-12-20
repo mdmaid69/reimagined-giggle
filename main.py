a1aa8a06bf02fadeebc1fb69d405404efc33df70
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:04 +0000

    straight call round song order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
