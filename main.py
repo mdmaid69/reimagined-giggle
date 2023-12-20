630055d646fb9f2a874984a83c33d0f8cd2fe7b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:04 +0000

    slowly minor curious already professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
