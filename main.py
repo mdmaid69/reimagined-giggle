96148eb7a4be42bc125d0a1bb3b1bbe313750e90
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:38 +0000

    talk train possibly nearly against

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print(sum(range(10)))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_max(lst):
+        return max(lst)
