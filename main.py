aeed0196b3dd16e7cf085eeffd574aa63e23bc50
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:33:57 +0000

    protection defeat blue leg shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
