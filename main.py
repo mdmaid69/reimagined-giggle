a19f81edeae4554a01a57d5cc7a37311fce9ee04
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:17 +0000

    full entrance lookaround chin transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_speed(distance, time):
+        return distance / time
