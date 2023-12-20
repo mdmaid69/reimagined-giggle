0fa9c6146065b9afc33670f9401bdda3ec14797c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:38:21 +0000

    pocket insist lead fierce course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
