14618ca70662f041c5a30d45caa6cd30a224e707
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:55 +0000

    burn clue takeoff copy bag

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
