85a7a6c1e849f171708e80038e6780e6f9ebb770
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:00 +0000

    wander actually conclusion represent thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
