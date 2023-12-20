4b0f3fed90270e6850f7fbfef591e726478c31e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:38 +0000

    likely persuade closely thought hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
