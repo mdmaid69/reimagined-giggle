62d521860fd0e06700a6956a91e15579baac5b06
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:33 +0000

    past thankyou tool shut speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
