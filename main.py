7b260c5dfe5cf20b754ef910ee002ce6bf96c2f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:18 +0000

    keep previous union warn breakout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
