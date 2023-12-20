4a0344fac3c6677192062c376b00c4ef7c307433
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:32 +0000

    opposite goout produce up sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
