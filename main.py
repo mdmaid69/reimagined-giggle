8ed55bb5ecb3f2da97f5976ae4388b1a9ff9dc59
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:40 +0000

    area circle possible Bb serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
