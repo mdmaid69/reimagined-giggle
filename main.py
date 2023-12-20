db1ef8af1b8c855445d179747bb327c505a98782
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:31 +0000

    culture present attention goback(to) dangerous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
