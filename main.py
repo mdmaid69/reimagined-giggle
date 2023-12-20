634d2091581091a7abdec51385048589e4ff2810
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:24 +0000

    middle large right estimate slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def clear_array(array):
-        array *= 0
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
