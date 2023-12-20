99202f1fce6e145b53a5b57c91125586efa21d7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:56 +0000

    holdout they slide path shrink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
