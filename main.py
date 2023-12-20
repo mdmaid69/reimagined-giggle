1a7131323c8b8f625ce05371d75f01c02003808f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:04 +0000

    defend flight fashion until underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
