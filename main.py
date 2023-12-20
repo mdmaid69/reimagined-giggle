8cdd70e7a805b3913d08db103e33eca309d2ac21
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:34 +0000

    expensive lookup Tt play mend

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
