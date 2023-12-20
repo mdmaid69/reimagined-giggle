9e79d112d3d5db8426104cbff1879591135d7378
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:16 +0000

    fat among influence light field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def sort_list(lst):
-        return sorted(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
