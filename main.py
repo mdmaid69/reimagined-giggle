7c1e8de154d5b8849dd6c282d3df566364704a07
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:43:36 +0000

    delicate turnback fasten age temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
