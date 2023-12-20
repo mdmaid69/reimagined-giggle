3946206dc048b5f930db26e4fe183a7cb1410ad2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:03 +0000

    term example have wave stay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
