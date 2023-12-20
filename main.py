d1a28ef0d6607c3369b5704887c4a356eeb2093c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:04 +0000

    back Dd them backwards explanation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
