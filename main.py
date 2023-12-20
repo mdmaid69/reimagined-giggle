f5cd37a15a1c09770c06e234863855b46caa7a5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:56 +0000

    interrupt worse anyone reduction evening

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
