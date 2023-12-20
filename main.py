a3a4a3c9c7416d5080c153773d37d603922fb326
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:09:09 +0000

    trade project doctor satisfactory inside

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
