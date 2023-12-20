b8179e4908a4970ca115f193433669069bee8a15
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:53 +0000

    owe start touch swim lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_set(array):
+        return set(array)
