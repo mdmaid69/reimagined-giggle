0cb584e9f26fbb31d5b49c37a8b0004c72daafe0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:51 +0000

    warning home handle negative right

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
