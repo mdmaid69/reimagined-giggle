ec12e5a1c135401b4b213a6e79c96d32a03f16f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:07 +0000

    keen thing drawing age blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_length(array):
+        return len(array)
