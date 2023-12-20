c24ed9b0573b0fd5f154dcb858b392bc1a88ed8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:46:50 +0000

    rescue private very job pleasure

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+print(sum(range(10)))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
