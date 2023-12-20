852f8663faa86f0cf03194ce76b495617062bd2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:13 +0000

    future sink bury react building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_power(work, time):
+        return work / time
