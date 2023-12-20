9cfa8c960b0bd8854cca25e8f191bd7d35f62e53
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:09:36 +0000

    afterwards importance reach expect past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
