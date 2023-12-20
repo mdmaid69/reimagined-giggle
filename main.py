257a1758ede1a6d1bb3468408c2932392b1c1fde
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:36 +0000

    band holdup alone slight he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
