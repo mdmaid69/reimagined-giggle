c37f3afa0b8d19c41c5b64d1e99c7aa295b5e07b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:28 +0000

    hold bad bet boat provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
