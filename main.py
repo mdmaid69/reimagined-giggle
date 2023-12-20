c85928a23e1866447985028d2d1ce4527b2363bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:44 +0000

    damage suit leave visit ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
