cba8ffbb4cec5fbd60beadedd1f3e11cd0bf597b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:22:05 +0000

    gradually dig boat tell proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
