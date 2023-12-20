2acb994a2f3ce7764856cfc6ad930c76eb6a76e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:52:58 +0000

    declaration may represent not shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
