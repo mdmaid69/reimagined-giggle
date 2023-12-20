b7ac81ba1e03d7e25c47471e1dd7b324f38330cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:30 +0000

    us flow report then single

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
