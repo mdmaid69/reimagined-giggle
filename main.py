b549afb62e2b56f18ca00d2b6e5bab50b1fd6512
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:07 +0000

    simple holdup need offer doctor

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
