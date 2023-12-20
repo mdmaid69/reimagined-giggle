88f1dc601415f9e42551402c112af0308b34cad6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:10 +0000

    greeting write imagination 572enter comeoff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def get_bytes_from_array(array):
+        return array.tobytes()
