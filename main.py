7f47993546f04fc2830191ae0845fe751c32a5b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:56:06 +0000

    Ii pronunciation sort private guide

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def is_odd(n):
+        return n % 2 != 0
