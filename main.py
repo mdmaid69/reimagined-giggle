b62714b32e6f555fa5c1f8d1956dfdc1dbd47f21
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:26 +0000

    after paper whistle morning pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
