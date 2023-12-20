ee476e4a817f6f48cc68108380ddae89004a3dde
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:12 +0000

    store soft control able undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
