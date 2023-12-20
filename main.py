f1e8dcbaf5634d81ed9e9db9020a4323773c9501
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:04 +0000

    regular so certain blame apart

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
+import sys
+def exit_program():
+        sys.exit()
