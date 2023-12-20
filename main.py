66a92bbe40acfb397b1dd6c4223a833570798d7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:47 +0000

    keep large summer course typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
