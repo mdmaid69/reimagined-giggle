6a39185f316075aa0c4cb8bb0676004bc11cfc61
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:13 +0000

    negative fill prefer broad sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
