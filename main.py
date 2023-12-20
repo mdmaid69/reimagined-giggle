232f8b1d10dd2d8cfe2ca573aeaa57b806a6bd58
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:53 +0000

    practical quantity alive form appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
