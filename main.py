ba35a0b7393ab24bd6c71fbc045d24cbc3fe5b15
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:18 +0000

    blame health weather concentration angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
