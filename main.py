abf90f3ad57ba64280c4703fe598d2fa2d485906
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:09 +0000

    company design this defeat noisy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
