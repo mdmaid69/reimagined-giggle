71468327b0ddc2187d84cf19a4442f5648c35ab6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:02 +0000

    night comedown up unhappy definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
