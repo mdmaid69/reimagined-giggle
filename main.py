de171a22d0f2f481bac5d5c7d50c98283310d858
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:51 +0000

    forget finish hole declaration discover

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
