3142afefc450f016e5a33e301fa4e34930aa4e51
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:09 +0000

    bin else respect hole book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
