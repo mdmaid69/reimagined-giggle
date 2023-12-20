abba305760b28485866ccb5b70e641a30d67ce50
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:18 +0000

    ride outside description Hh commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
