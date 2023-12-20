e13da6e52b6057743204d74d14fa32c7d423b299
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:40 +0000

    pretend limit attempt string advanced

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
