004f70c8f4949c28363ba18b73f55214e7034b6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:28 +0000

    plant call suppose than tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
