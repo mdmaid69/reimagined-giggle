0c365616d8dd0f9d5a84ebef7f4bef5babcd7105
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:47 +0000

    travel promise standard depend partner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
