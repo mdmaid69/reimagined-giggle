ed3667ea58542c47600b247546fe1c6eb0a322e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:53 +0000

    can 570enjoyable performance interest whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
