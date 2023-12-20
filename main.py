21f66b09344a880068d1398855e5779cd4fd466e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:47 +0000

    deaf except advanced free through

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
