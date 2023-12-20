1698dd738ffe1042793d9c3139990b31ed37b45f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:41:51 +0000

    reduce escape popular phone mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
