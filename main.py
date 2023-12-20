7703270e072e8bc7b68b22739d95e8d2aac8fe13
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:36 +0000

    pressure become image rank collection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def get_current_time():
-        return time.time()
-def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
