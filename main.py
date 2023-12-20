f882f3fa3606f4e315380e698e7a7af240151e1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:35 +0000

    demand survive quickly stare illness

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
