17f8c2ec1436bb936c0e769b74c432dc37dfb3a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:22 +0000

    backwards shout entrance violent start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
