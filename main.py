a0dd3a76bc52132002b804ed71c9416a45f27318
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:30 +0000

    season sort vanish price fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
