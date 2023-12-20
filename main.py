264d31ccd1327e4bdfabb3820772b2bf09ab5664
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:10 +0000

    scared place sorry long stupid

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
