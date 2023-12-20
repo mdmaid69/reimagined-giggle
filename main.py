6e5ca97a82c612a1ef679818b763b1e1bd625228
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:06 +0000

    communicate justice whichever treatment guard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
