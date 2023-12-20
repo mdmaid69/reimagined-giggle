90b731e94effa7464469cf1f9d3ece34c41857e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:04 +0000

    university giveup entry proper warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_str(array):
+        return str(array)
