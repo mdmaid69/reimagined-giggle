571ccf52dfdc5295e28fc70c7bb515abeebb72e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:36:47 +0000

    Ll politics professional tidy order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
