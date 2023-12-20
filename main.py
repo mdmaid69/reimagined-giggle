b16b4c941d3a9c4dab853087db5a37fb94468b90
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:50 +0000

    tune along bother pretend stamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
