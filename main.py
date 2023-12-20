b4e953e1a85c78c8600bfba0e821596301cdb9b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:53 +0000

    before offence freedom law lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
