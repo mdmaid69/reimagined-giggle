a865bf99af78ed285ea4c5de932cd9178753e899
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:52 +0000

    shock twist track get shake

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
