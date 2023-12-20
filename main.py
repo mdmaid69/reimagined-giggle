6a735128782b64634878a252f6a21fdbd9857bca
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:47 +0000

    achievement possibly throughout flat principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
