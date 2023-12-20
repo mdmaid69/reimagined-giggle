9ec038ce95922babf75b82c7af07746a741c5032
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:57:28 +0000

    broken development nerve woman splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
