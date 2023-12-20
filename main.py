3f4182b2361ea1262e68d35f799060d46c815baf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:16 +0000

    similar turnback flight standard contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
