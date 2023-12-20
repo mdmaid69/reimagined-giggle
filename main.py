bf8223fefa90b6d08ff10aec295830da37f6fa94
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:10:14 +0000

    drink man except peace wash

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
