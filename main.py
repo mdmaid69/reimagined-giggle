a7cc209171b13033611355ddc84c96ec333d6421
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:38 +0000

    lead manage open prize soft

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def reverse_list(lst):
+        return lst[::-1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
