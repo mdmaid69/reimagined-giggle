7d6fa4c6efd20c47afa4f2070f7251a874e22542
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:45:54 +0000

    569enjoy help block face powerful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
