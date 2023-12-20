2e70dda9618c8ae9de724c68d922dc11f65564c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:27 +0000

    theirs head rescue admit shopping

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
