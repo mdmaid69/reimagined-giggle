cd900fca9eecee95d48b1328ee608b7271cc765b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:42:52 +0000

    hold hurt trap along lie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
