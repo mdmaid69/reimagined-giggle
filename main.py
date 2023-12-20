e67ecd5f8a03fe3798bb61643368ae0130d43b98
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:45 +0000

    hang stiff month cheap sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import re
+print(re.match("h.*o", "hello world"))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
