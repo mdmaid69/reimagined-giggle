e0bdd1577b393d5090a3ac359760bca9c9bfd360
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:07 +0000

    without settle bringback area statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
+import math
+print(math.pi)
