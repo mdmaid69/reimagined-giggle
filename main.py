d87e742c473725b8fdb0683c847165f380ce7cb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:32 +0000

    dish reply turnaway organize present

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print([x**2 for x in range(10)])
