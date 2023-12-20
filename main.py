1c99eacff46f5fcbdb697184e3e04f4b594b7480
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:50 +0000

    aware sweep love let plastic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
