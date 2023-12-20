a9e8f9df31967f5c6987ad7aa585f6f413f843c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:49 +0000

    believe repair lower promise picture

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_max(lst):
-        return max(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
