4ede3ced05009f905604c813bac60856b08eca49
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:27 +0000

    mouth pool touch worth crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
