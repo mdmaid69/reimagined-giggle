6884e68c7a74de0566b026a508c1cbd650bc0f8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:42:25 +0000

    region mother needle statement bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
