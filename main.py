2ba55af4f20b21a39c085294e02bf888194f81b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:01 +0000

    free home dance pair lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_as_repr(array):
+        return repr(array)
