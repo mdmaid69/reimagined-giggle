4758c03818b46b856b2202013e5d9e8122f02719
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:17 +0000

    official return cost dick extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
