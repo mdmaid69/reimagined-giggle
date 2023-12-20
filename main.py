2e0441c03b160257936706b08d6838b6cff5911e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:17 +0000

    turn arrange worse instant ashamed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
