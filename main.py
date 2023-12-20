10125fe83c722bb75911fbf38aadfa04763d6409
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:26:17 +0000

    side expression protection runout exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
