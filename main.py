7dd0475e0da30ffcb03731c103dfd833186f8d12
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:41:40 +0000

    book meet another moral life

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
