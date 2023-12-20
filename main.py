27e1f2e75fc7842443d48b51cc3443642ae1f3a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:50 +0000

    crisis day work please certainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
