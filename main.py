08c4e826d8e2874dc895666000ebc9343ed5795c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:30 +0000

    process make 565end deliberately there

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def clear_array(array):
+        array *= 0
+import sys
+def exit_program():
+        sys.exit()
