95891e081dd51c36690363240437aa4fa3d88023
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:39 +0000

    sense inform another die tie

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
