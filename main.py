e0700669d43726b21ce661aa3804856b25894d91
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:51 +0000

    forgive on lean command hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
