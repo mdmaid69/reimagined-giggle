b3b65571f1e37d075a6cbfa61a411c1b55d9453b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:05 +0000

    frequent bringup guard Uu question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
