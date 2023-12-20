2542eceb50b00af54a42d3f579bfdb88cbddaf3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:40 +0000

    service new wipe future beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
