77e1068eda431db1c0f675153909c3e0ff4c482c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:14:26 +0000

    zero science plain exactly impossible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
