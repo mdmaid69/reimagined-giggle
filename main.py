cdb9c4c9219ed50f2694e2808d3227c8f4f90417
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:43 +0000

    stupid direct maintain brother rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
