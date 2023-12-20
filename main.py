eae836a802811285b32528b2f554aa37d7b9f1ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:14 +0000

    shine community treat accommodation much

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
