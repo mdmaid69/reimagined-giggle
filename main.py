835adf94b2d69da1d44676fe34d91b591aceb041
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:12 +0000

    example uncomfortable turninto century start

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
