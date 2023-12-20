de842b4b2b09310239514a63aff9a3c262b51d2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:30:51 +0000

    you walk difference price show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_power(work, time):
+        return work / time
