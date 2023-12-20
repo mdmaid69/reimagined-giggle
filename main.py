71e1721caa64db002ed24eb5c7ef0d3e46350a98
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:46 +0000

    how like contrast turndown needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
