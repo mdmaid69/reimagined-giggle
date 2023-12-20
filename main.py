62b2afb2bcce662e253d4f8825d8f1341e1731cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:10 +0000

    practise somebody trip tongue ocean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
