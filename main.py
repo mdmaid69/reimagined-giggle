ea702a659e041c9fee501602c5cd8eccdb85da41
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:15 +0000

    advanced shade method send male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
