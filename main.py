9b1d73f982a683b5b5151fe8458a2fbaf7fedf05
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:16:58 +0000

    even carry keepout tomorrow solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_speed(distance, time):
+        return distance / time
