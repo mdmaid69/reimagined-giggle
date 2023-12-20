21d7e08525f7f94167e97727c6dde9d37a3a84bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:04 +0000

    stare pen demonstrate along force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+name = "Python"
+print("Hello,", name)
