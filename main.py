3c9c8753145f787c22744af5b806a234ebd12bd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:56 +0000

    fly concerning accident little central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
