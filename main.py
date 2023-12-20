bba5a165e18ce9b0998456e15d9626d5f4a112ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:27 +0000

    police difficult treat check owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
