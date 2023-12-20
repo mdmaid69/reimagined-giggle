65115591d46b964a348def03967f59b883f5ae33
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:18 +0000

    combine regularly fit tough sensitive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
