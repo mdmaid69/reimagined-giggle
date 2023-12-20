2968e0843a87f9ceb32360345366baf964698678
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:42:20 +0000

    slow interruption compare last talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
