a1f1fc40961c7e9ce218cf7510239e1f73cdfd97
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:25 +0000

    confusing let aware right nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
