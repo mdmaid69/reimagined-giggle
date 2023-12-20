6605c3e73d1a5fbd1ae9449573e9914cf23c9eb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:12:48 +0000

    depend steer among design satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
