2d7c02913712b08d8e857892a5f62f9c636e0803
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:53 +0000

    during wind set report lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
