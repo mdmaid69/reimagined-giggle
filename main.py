856bd84cf1c1f81e694664cfaa431ef7f36ab1e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:42:35 +0000

    reduce seat nervous grateful professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
