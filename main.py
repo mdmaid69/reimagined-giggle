8a16766e8cd74cd6364bdac319858f5d6cb4fcb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:14 +0000

    marry university proper typical shine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
