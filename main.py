50014285f5bdfe0ec127e328ee41e479eb006ea0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:36 +0000

    guess free lay set delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+def convert_to_binary(n):
+        return bin(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
