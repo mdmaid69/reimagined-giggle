1cacdf8d16cbfa12a06e4fdf565d593a42753cad
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:54 +0000

    nearly become put date cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
