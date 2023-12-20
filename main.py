e133cf2b4e121ab4544a6c0a750fad3f7c39d4de
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:37 +0000

    comment whom think discovery description

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
