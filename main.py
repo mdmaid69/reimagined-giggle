5a470abe276a46c410785e084502dc61b9e4b219
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:25 +0000

    petrol argument away prove marry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def subtract_numbers(x, y):
-        return x - y
+def convert_to_hex(n):
+        return hex(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
