8bef484318d963feb6cde159184c303b5a7a3e58
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:26:31 +0000

    experience scream motion hear win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def count_words(sentence):
+        return len(sentence.split())
