62cd24e615803a21f52f93b868f5d4e9c13770d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:01 +0000

    turnaway dance range put image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
