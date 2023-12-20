98e56b45f39dba84082851b86cffc25d19771f6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:25 +0000

    hot difficulty concerned strength plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
