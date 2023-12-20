2edf6d693f16579543e31ea5f6ae5512d5fb7264
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:27 +0000

    throw rent objection decide step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
