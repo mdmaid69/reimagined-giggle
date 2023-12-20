e24c250411437d1650bb67b5de8471c67f9ee5f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:43 +0000

    chin neat pretend finish shoot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
