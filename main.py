6fc459c6137e6d9148d99501249e4573835738f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:23 +0000

    fetch honest formal grow obvious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
