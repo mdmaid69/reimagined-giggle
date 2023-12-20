d69c52b35ff0da4eb80ad76fdd74a1df3aaf44f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:55 +0000

    welcome curious ring plate marry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
