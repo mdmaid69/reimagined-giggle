9017a49b80b47c424d6dc217042798f780b2f140
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:13 +0000

    theirs group whatever estimate skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import sys
+  def get_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
