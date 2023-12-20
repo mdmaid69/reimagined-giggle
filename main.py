8d696bc442dff060f88655ad695503dba61e17c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:36 +0000

    in hello poison black talkinto

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def print_python_version():
+        print(sys.version)
