593eddcabb4a91ab72e1a3f8239b7a01586dc93f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:31 +0000

    action board free continue undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
