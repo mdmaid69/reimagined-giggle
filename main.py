a19b777cf04f6357e5e2887ec1b5c6813e415876
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:04 +0000

    spend behaviour joke term but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
