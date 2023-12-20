fc75f573fc90126118001d88c682c973a1b0cd53
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:13 +0000

    prefer convinced unfortunate list contain

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
