a312f2af520dafc33d0d81a56974de2563f4b560
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:16 +0000

    lay trousers communication treat plan

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
