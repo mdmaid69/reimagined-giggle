426e60e652846517bc73577b2e072b56b33b8356
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:16 +0000

    trousers everyone final steal perfect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
