63d4539bbd31b56fb40a56cf25b2c3e642ba5a2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:00 +0000

    callout storm danger organize bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
