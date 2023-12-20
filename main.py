95c6f8fc13aeb8c7500b030ea5b564189c616414
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:33 +0000

    date block minimum sail could

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
+def convert_to_hex(n):
+        return hex(n)
