e70467608bc05d1756fb4a6e74bf9bd3e112cde0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:26:00 +0000

    light wall off gap cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
