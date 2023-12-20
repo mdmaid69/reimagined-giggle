070c7b424393095b12733e4ee196a9ae93940f65
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:59 +0000

    unite meanwhile week recently continue

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
