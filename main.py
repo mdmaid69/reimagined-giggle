49419ac55a106c7ad74ca01e4fc01257d0ca32a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:48 +0000

    cause switch rate table open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
