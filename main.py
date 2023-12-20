b1c7769ae99ab62bd99b8e1e1cf50e80d71595c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:45 +0000

    invitation passage remember meanwhile sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import numpy as np
+print(np.array([1, 2, 3]))
