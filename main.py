b5dca897490c521c07c7c975be1fd7abf04e3060
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:08 +0000

    gate name thirsty happy there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
