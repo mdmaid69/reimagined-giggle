5e91fec8bb2aa329ca4f72906e2bcd61b01fb6a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:25 +0000

    turn even do wing regret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
