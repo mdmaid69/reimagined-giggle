6cb7b02f057d885504b9d97077151cbb213117c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:17 +0000

    fall search skill holdup giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def cube_number(x):
+        return x**3
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
