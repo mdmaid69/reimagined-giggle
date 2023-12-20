44a4fad99d5ff796d8268eb018092b1af7274c47
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:30 +0000

    least fortunate correction exchange cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_str(array):
+        return str(array)
