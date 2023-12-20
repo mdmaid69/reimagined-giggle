f7f0ff15785637e05bd1c16707a135d55ea3d697
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:18 +0000

    direct lookup across noise personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
