bdbde8ca67b2eca2fa1b5b6633775978082fb559
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:51 +0000

    hold reserve tidy along church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
