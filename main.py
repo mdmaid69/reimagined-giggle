ef3f02963c5f051f1f3c31ff4bf03c025cada6e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:29 +0000

    book come lonely goback(to) exercise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
