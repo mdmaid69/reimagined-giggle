424cf559883f36ed275cb5e06359e01d4a5d5930
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:44 +0000

    correct 565end band taste remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
