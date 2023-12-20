9f830675f38a02b0a518bb245581f49bbdfc69fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:53 +0000

    justice stay relative young pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+print(math.pi)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
