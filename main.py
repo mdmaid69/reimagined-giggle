be08570e07d072705162b0951c31e814029654a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:37 +0000

    abroad condition bush runover believe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
