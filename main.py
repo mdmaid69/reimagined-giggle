acba308a2be4f82ef2c2b2cf87a9dd508990cb34
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:02 +0000

    serious welcome young it breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
