227cb58bd2538ca24c4d4965c8cb5dfd2e4816df
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:25 +0000

    should see principle actually small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
