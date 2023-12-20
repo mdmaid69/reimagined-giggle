5ef1c519923ac919e9f9d1032dd8e04fff6af79b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:13 +0000

    left target not mark emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
