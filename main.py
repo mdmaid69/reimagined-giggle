cc09f21a2efce031c7b75bde8d351faa8e7f7ab4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:32:06 +0000

    market giveback defeat disgusting smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
