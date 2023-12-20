71f1c0b9d1344159394dbae547700ca70a0d0073
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:41 +0000

    Ff left contribute spot birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
