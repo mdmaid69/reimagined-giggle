8b9104d52d5b123b9466077b4c6976282f8c84ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:14:08 +0000

    train also bringup vision estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
