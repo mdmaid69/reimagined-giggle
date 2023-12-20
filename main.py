4aa5a187c5ba20639b27d0ecd6fa29f2e9fb8b41
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:00 +0000

    pupil turninto complicated shelter bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
