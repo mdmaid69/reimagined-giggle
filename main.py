6f692f3d58a918a2d216d693846b8517fab00f87
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:51 +0000

    after suckme explain reason considering

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
