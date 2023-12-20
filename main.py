9cf907dbd7f149ddcf3ad15fbc74b7ef0e127dc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:22 +0000

    somehow reason occasion that quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
