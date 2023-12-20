705e84b60b473eddf482d8eb38305cdb56eb31e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:56 +0000

    dry hole objection fall workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
