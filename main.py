c70987512bc607754a57860b53e71afa17f1e2e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:46 +0000

    plate private familiar remark certainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
