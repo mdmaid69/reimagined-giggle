762fc0bf9e7a8b7a2b6d4abf1c8f212484674852
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:36 +0000

    bury welcome positive majority ability

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print([x**2 for x in range(10)])
