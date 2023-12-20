d956fd22229cb3caf93d780e210b9ea4e069737a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:18 +0000

    wash apologizeapologise present right trial

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
