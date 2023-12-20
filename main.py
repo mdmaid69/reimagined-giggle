2873dbf477e9edf9e56697e42632f4ebf4766f98
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:17 +0000

    report pool earn by pocket

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
