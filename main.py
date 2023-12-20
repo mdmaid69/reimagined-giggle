139ae011631f42c61e97d3abd825470602c8f224
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:52 +0000

    ready conclusion roof straight work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
