bf70396f293f58b67495bcd8a2bf5dcad636c05c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:58 +0000

    male boat storm beginning explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+print(math.pi)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
