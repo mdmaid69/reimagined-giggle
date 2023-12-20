daddc9d282e299396777c3a6e109fec59dedf2f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:57 +0000

    comfortable keep watch smile rather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_int(array):
+        return int(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
