dab44fcfe9d396f566305abc256e13b5e21c523d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:36 +0000

    dry politics letter cum centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
