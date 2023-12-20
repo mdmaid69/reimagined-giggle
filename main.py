1dd5bfa26659e6e378bfcccbec5558e201229848
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:36 +0000

    software well obvious skirt land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
