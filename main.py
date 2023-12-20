0ee68ebf7846b507c894d6c7cf9f5913f1a97d0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:25 +0000

    voice mind price petrol favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
