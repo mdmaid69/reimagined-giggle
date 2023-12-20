07d9870c0aa3467b2c5c6dd99c2433192f836efb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:28 +0000

    carry worried build burn cruel

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+print(math.pi)
