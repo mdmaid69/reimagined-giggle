e64c0ac0fc902938e9a261de7adafe86aeab871b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:32:19 +0000

    over fact ill describe whether

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
