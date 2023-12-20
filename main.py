8728774da25436001f9c03aab40a8aa0309acab4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:49 +0000

    without confuse put damage flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
