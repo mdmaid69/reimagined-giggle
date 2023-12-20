8456950c756f6806b60de82243bf25d4671c7226
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:03 +0000

    back or the you usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import collections
+def create_user_list():
+        return collections.UserList()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
