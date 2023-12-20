67e95437f2fde6ccf77a726e6e565c4fe2364223
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:07 +0000

    full concentrate comein block myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def append_to_array(array, item):
+        array.append(item)
