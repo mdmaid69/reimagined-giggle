61cd25fb09abc6c3ef6cd10e2d0cf3d435b1f82a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:17 +0000

    smell various skill profit age

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
