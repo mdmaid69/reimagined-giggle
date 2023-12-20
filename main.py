9190464c1f92e1e7a5398ee3f9cbf174f1e5aba0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:18 +0000

    most whom imagine shall mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
