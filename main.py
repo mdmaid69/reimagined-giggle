d62df87ffde71ab4c5bfaed22a12ee2cbbf2a996
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:22 +0000

    work repair crime pattern quickly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
