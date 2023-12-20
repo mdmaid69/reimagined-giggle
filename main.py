8e1f952628bbcc13fa085c01929ee390e7b29aeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:45 +0000

    marry flow brother married can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def divide_numbers(x, y):
-        return x / y
+def get_array_buffer_info(array):
+        return array.buffer_info()
