f14f764171d50dcb331ae4030168f10ec505afb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:13 +0000

    fierce wide actually way fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
