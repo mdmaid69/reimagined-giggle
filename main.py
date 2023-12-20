c10c45516a633ebf85dc21f1977a5e2fd9bb252d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:39 +0000

    afterwards travel grow show Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-print(os.getcwd())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
