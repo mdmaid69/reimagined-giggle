fab019977a839fa8e4cd8fc9d4a9038683c83be6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:28 +0000

    variety gas offer date contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def split_path(path):
+        return os.path.split(path)
+import time
+def get_current_time():
+        return time.time()
