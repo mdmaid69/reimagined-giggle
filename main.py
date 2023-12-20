94feaa0509e808b7b1bbe5ba6a4ffcbdfeb86d9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:20 +0000

    thinkover weekend unhappy demand see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
