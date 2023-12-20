d4d73df302905c33687db1ca16286ecfafc26e41
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:43 +0000

    cool unlikely actually trap comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
