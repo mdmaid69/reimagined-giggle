a33f78d1bdce088a94e06488bed5b7cc2ab7f876
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:19 +0000

    makeup takeaway sting rescue today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
