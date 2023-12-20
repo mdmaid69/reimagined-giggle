8098b3e2db499d219393b83ad9b8c92577703557
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:31 +0000

    past finish sense mess imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
