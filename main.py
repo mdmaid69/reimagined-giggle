18cb9207b09e23f6b37c12b08cd3dc1e0e4904f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:05 +0000

    example platform who desperate behind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
