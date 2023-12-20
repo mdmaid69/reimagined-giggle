a108acfa16890eb2fa7e3800e8a906b7317007d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:44 +0000

    ever pay Jj account ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def clear_array(array):
+        array *= 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
