b17852c596f5ae1486626b29b9214a8f80cf8947
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:50 +0000

    factory cheque throughout transparent on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+print(os.getcwd())
