b9aba6abe14e9a5b419d070e400cf070e499d80f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:21 +0000

    especially usually unkind scrape flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
