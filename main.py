41c6696723d2b34a086a65b58e981e70dea27b25
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:24 +0000

    rough thanks loan giveback lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
