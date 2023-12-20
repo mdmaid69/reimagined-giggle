61295035760c332e9d5d7866b5002411e6ba8356
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:20 +0000

    waste equal ought famous complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
