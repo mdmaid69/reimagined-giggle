1064c1c416adba9751877cac11a88ccfcfbfe94a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:18 +0000

    loan hurt mother recovery turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def is_odd(n):
+        return n % 2 != 0
