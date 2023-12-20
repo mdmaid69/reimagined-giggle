2232a76da014470b6c918db89bc8f26b54980fb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:36 +0000

    goback(to) border ourselves wander note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
