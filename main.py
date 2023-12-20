a2082f500f08ecb49e430c4135f591f62e661998
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:50 +0000

    hear purpose goup factory shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
