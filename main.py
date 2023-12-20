fae3c41af2f6802825b912098a3ac581392c5852
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:01 +0000

    study practical proud charge base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
