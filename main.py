907f1cc90d78c259d2c11793e05406b5ee8ef0f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:44 +0000

    pay same transport proof pride

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_speed(distance, time):
+        return distance / time
