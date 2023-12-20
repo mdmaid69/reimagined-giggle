86afb67a72d553e92ca8f1358423ad86587913df
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:57 +0000

    tip interview but rubbish dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(x, y):
+        return x + y
