38485fa4949fab3397b52fc86b9629bb2b7c5c1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:26 +0000

    tool land comfortable production public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def multiply_numbers(x, y):
+        return x * y
