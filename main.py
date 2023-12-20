8965183aaf80d9f721839f5763c3e027cfb267ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:39 +0000

    similar disgusting discussion only container

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-print(sum(range(10)))
+print("Hello, world!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
