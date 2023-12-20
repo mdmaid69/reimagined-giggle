b8dc17fc0f7e0ca2d63f8a2066f0aad9160e545e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:20 +0000

    pump mend dish community rule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def subtract_numbers(x, y):
+        return x - y
