f11cdaf9b95ff79b999a26bef580688a19564902
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:45:30 +0000

    pattern giveback visit since entrance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
