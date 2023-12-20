3dfb66ba40e799f1484a2d2c9c125b6f1a22fb7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:59 +0000

    recent moment catch callat friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
