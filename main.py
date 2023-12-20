6b46858184ebf13b438567e6aa41999d755d6c8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:50 +0000

    weigh sing discuss shoulder past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-print(math.pi)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
