8c99246da6ca9704c0e6da88bfa50e7a2a8c4da7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:12 +0000

    admire confusing hard correct word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
