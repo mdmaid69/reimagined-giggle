fd31282c6c785c91d6fce8167e14f88cb7f4afd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:37 +0000

    motion sad personal available confidence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
