fa6ff1b10154c8605dc8955f2129d74872be7460
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:15 +0000

    mine yesterday danger sport tune

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
