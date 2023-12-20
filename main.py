cdb6fcf487f0072e6849aadbb99f1be6155ce3eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:33 +0000

    press block claim left exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
