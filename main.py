12139dc215eb1733c9b4b7fc34486b9b57431053
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:59 +0000

    between climb old lookaround breakout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
