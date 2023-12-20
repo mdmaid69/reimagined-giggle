e08a70718d706c5090f8d23b383c6e2180221b2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:19 +0000

    shout here home command sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
