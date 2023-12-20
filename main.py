90ce1a2dd069d031dc8a8dffd3f54b38f335c7cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:42:49 +0000

    woman back check tax to

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
