cd62da6dc3ba6c091f7ec5d73b7e90429ae8f634
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:44 +0000

    lead string speech number runinto

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+for i in range(10): print(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
