b87082104eedf3513a9eb53237e5cfe4cfb12f73
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:13 +0000

    likely ground prefer born lookup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
