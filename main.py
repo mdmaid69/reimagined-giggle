41383ae3c7be88f70d1915cce4fc7cc64289970c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:55 +0000

    through transparent conversation possession surprising

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def clear_array(array):
+        array *= 0
