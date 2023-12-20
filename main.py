68ebcb21cb27c971fb78206f623703a36cdf8898
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:54 +0000

    direction wear to shock lucky

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
