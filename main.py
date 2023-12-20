b23c2e8adfe00fc4a8522594914074159e9fb3d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:47:52 +0000

    fire under development giveback slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
