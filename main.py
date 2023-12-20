af1a70a612c72ceb91adfc78934c32617189aa3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:34 +0000

    crowd container fortunate best see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
