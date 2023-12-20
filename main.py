0ce3e0c9b2cfae061d01220fd36245a5a104898c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:45 +0000

    wrap area force rob later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
