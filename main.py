b6d02c6f98196ef5e50e6581856371c4bd706945
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:33 +0000

    child acceptable protest up splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
