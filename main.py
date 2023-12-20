742781fb78c513b1640e0b3a142cfc358b59a5c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:18:35 +0000

    bringdown later bang now expression

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
