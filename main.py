c55d29458c681e8176296c569ecca569669738fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:24 +0000

    exaggerate operation shopping true place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
