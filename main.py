bebe8324c31ce2a19dc22aed77608592912c7c53
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:20 +0000

    appearance most scream intelligent connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
