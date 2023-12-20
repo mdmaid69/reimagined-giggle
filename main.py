a483da4f96c20ff050859034ac78558a26e9b584
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:08:51 +0000

    badly lean knowledge mystery approve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
