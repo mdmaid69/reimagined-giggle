a63ffdceeee1c91028fdace70caef6709e33d081
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:38 +0000

    blind address order few lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
