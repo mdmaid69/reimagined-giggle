ae4b5c549a8610311bb6ba745dd36af8e026c483
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:25 +0000

    threaten wear emergency professional question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
