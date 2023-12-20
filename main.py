8e93f9db7836613f9743498924a46792cb178598
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:46:40 +0000

    later goon instruction health getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(numbers):
-        return min(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
