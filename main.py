35d5540018fca6614938a0ddef4e9444901ce02f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:21 +0000

    raw number man many wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
