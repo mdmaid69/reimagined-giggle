e1505218b84c33756b542f630c754d8464ecba40
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:42 +0000

    minimum minimum extra worst receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
