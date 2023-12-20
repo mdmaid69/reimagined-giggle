de54a925a4b932e9155d2c679f7dd1af604d0440
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:36 +0000

    similar yet alone age about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
