4f6bdeaa66a138d039d3ba78869a17fd742794bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:21 +0000

    interested nowhere light goaway quickly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
