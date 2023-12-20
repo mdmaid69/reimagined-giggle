8d5321afcd90fa03892d67d3eeeddf2625bb4bd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:50 +0000

    to bit lay pair apply

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
