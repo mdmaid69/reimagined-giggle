994801b65937d5f0f4c9e617a5bc73ca81b341ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:48 +0000

    reach project spring help Jj

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-x = 10
-y = 20
-print("Sum:", x + y)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_unique_words(sentence):
+        return set(sentence.split())
