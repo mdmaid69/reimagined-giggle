8bc5bb032958314ccc4ff6e4e8c8d32d78b16062
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:12:28 +0000

    light repair machine line only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
