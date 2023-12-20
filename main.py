a686e561646e5c5cc5e7635e3e52be40636994d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:27 +0000

    wing demand great shoulder principle

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
