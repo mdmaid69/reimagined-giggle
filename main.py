f604cad1e72feb203cf69c3a9384a5f9ae8c423a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:41 +0000

    yes afternoon bill needle unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
