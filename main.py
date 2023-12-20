86d70cfd0b7df585e83b2916f5e236536e3795e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:53 +0000

    talkoutof turndown week start run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
