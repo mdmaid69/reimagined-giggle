4382a5ff28c1d040dbd174322e1a9bee9f8ca871
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:08:42 +0000

    achievement defeat pupil frame benefit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
