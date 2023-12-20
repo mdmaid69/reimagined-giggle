c4b05fcb58f4602ecba14bdbd7b127b326c2728a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:36 +0000

    considering pay importance inquire level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
