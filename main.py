6a3f01df1b3893207bf4f3870d596f50a9faf6b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:12 +0000

    statement set purpose pinch OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_binary(n):
-        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
