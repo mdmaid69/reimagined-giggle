f21a607776fd2ca637724aa9c3aa58013a1fc2fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:25 +0000

    stone divide meal degree game

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
