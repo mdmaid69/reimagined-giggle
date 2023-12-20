6d119db2909a98cfc5909c0f5c9ff03bce26c1d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:47 +0000

    within some finally little tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
