ace66201e1c3bbaba045a41db5d662c7f8d2e69f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:12 +0000

    promise circumstance ocean sticky slice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
