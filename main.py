d84f162f75155257fbc769db5a210eca98e90395
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:04 +0000

    zero very circle uponon climb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
