2fe85bb119b334a5a7fcc29f9a6aa1b76cc9d7e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:18 +0000

    dull programme represent about matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+name = "Python"
+print("Hello,", name)
