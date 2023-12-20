3925d8ba467709b3698e5eeb6f15601c58efa9c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:43 +0000

    fold software prevent drawing Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
