2c6e9cca57abb0b36c16d22a85cd6a492626b93e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:11 +0000

    effect command pick something arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
