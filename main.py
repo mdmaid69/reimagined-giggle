e2fa7b8bb5761a741fff6eb0d0953762f8f0ee36
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:08 +0000

    worst pipe react call instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
