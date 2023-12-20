cd5f5a21e77b671b8c273531458ebb4dfb73484a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:36:46 +0000

    slightly cough sign last embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
