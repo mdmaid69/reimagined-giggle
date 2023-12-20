202f62802a2e0470f4fc4797749ac2cec6455b4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:12:48 +0000

    argue own education accident comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
