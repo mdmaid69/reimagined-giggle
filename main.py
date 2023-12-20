551c804447e75a494d737a65c9e4541fa8eb40eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:34:35 +0000

    theirs plastic fairly quit university

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
