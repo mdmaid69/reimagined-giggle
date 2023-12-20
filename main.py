708c61ce2a0f18f2786c3a18e5406c436da71128
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:27 +0000

    responsible too unable wander feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def find_max(numbers):
+        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
