99b166c95a1c400ee385db30b5fa9be7f32d9fdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:54 +0000

    challenge day advice contrast 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_user_list():
+        return collections.UserList()
+def convert_to_octal(n):
+        return oct(n)
