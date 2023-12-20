a7044d1dc4414ba8c0e63281c24690b30eea6c5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:53 +0000

    address off thin defend leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def subtract_numbers(x, y):
+        return x - y
