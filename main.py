45a2704f0f0638986ae212efdb2734af22d56959
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:44 +0000

    machine pile thumb yet capital

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
