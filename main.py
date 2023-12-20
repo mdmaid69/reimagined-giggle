47e4f1f724b4c2e7b50644dc1fcd391aa9040dc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:26 +0000

    other shout people in well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
