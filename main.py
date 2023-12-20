fba0616a0c8044326b44afbf5d85ffaa6b03b2cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:38:42 +0000

    star shall arrival wake need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
