b6f66d3fb421e3e8bd60d0f433dcd2b8a298e0c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:09 +0000

    definite judge content switch pin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
