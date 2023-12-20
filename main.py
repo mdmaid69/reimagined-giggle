3710d83b2f703084abd14ff533573c55dee29c2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:13 +0000

    peaceful loss obvious band fight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
