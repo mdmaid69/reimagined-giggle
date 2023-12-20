91b0f45529f2c9e52f79341a8c93fc212b519cd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:52:08 +0000

    guess level attitude clever second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
