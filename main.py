403fd423c676db73be7c6b15288576a053ff858c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:08 +0000

    stamp upstairs another profit issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
