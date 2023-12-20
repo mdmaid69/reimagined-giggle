765af236395c7140679e4a210562acb7840e9c22
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:50:30 +0000

    pity free ear church me

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
