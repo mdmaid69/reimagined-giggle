53437aaa96dc3be9631a34bddb324793f3f64c22
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:05:04 +0000

    history fit or either claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
