e76cc3feb5d70559a22248b5616b651c146f838c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:18 +0000

    light feel string return pattern

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
