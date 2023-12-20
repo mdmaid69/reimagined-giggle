8a659443f186277151d203ded5e8d22c7f7de275
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:36 +0000

    big sound alive but capital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
