a97e42132e759ec733829873d6026f04b0f1f101
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:38 +0000

    thin thanks preparation commit bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
