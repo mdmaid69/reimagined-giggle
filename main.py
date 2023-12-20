1d3f31778ea7105e707ae0f69699b2d5b7b2249f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:51 +0000

    workout march mark wrap around

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_pressure(force, area):
+        return force / area
+  def square_number(x):
+        return x**2
