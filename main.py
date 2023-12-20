c2f753880f6f754b4f9ee426eece6d89d5d0c943
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:56 +0000

    quiet answer order sick ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+def exit_program():
+        sys.exit()
