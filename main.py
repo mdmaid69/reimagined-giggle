391f491cacddd62482cdf36a938a3d6c3f4e7db0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:12 +0000

    offer demonstrate length curious today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
