10d65618cdac2700812fcdd1996d78007d56cd17
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:05 +0000

    none detail aim black beneath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_density(mass, volume):
-        return mass / volume
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
