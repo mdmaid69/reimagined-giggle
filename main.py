c456035138f65bb60a41a173f0e2a8e266dcfe8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:58 +0000

    influence image face hardly exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
