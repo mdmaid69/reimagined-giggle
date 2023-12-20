7cfea410c7eec148f9dd5754d3937425a80ae8d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:04 +0000

    maximum touch laugh control noone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def count_characters(sentence):
-        return len(sentence)
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
