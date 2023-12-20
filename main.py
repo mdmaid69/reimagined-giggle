50e8e89488524722a176de4c3d8a6afa9363af05
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:25:48 +0000

    float correct increase tape average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
