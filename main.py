ada53e4cd8f86acfa504e646a06151fd22e1fb15
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:39:15 +0000

    dish possibly value low partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
