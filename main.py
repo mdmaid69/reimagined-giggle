1a490a4abf2272b53e410cca4dcd5a8692ac579a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:50 +0000

    embarrassing chat battery turnaway keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
