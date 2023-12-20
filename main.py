3fea03f98c5d6bcda56fba58ad0b32c1611f59b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:56:27 +0000

    bang workon emotion whoever past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
