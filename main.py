d7537c27171a9332bc7c1813b2009b615bb20694
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:53 +0000

    minority the moment risk lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
