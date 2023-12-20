1bb3c2438f5730a19f231a42bcc983c62f9368d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:06 +0000

    scale waste choose available centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
