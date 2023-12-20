14d68cbf49cba7c9e6c9df2a0c85bfef7678d133
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:00 +0000

    method illness fresh doubt lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
