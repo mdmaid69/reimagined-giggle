64ee5bf9319699474cfc039ac86326a9a3d3e83f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:59 +0000

    point like attract front wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
