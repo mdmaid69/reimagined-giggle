61c3d0876eea14cdcfdbc54e57d286c79cb68a23
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:36 +0000

    use soon step positive story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
