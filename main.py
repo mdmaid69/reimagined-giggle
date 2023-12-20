eb47e8a0837e18024c28d51fdbdcfc6217b426b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:44:43 +0000

    explanation disappear care long announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_bytes_from_array(array):
+        return array.tobytes()
