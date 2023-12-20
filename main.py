0bc571f2d5eb3e908bd5deff1445f6eb8fcee8e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:28 +0000

    awake face help reply practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
