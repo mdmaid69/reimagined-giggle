dc34f8262701630c3433e51ac5ff792ec17f4380
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:17 +0000

    fortunately thankyou suggestion belong scene

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
