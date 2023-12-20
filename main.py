51639c24489c94aff51827aa95d710ae4fe5880c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:44 +0000

    however moral none slowly runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
