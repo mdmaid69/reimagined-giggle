a21fb6584b88450df532bbe0bd419b11b6b4fde8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:24 +0000

    else intend wear must run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def convert_to_octal(n):
+        return oct(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
