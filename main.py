494c7011d09f26241e97ee7720aaccd35b9f79fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:32 +0000

    noisy willing aware run bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
