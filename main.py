dfedea693ef52580b3423eeec923c21dc7cc800c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:57:15 +0000

    party sweep machine complete them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_density(mass, volume):
+        return mass / volume
