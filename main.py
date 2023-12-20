322b008d8292c1619710c9a72c3830f9138e1fd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:11 +0000

    lower snow programme quit past

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+print(re.match("h.*o", "hello world"))
