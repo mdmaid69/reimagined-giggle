bdf6a9a2f7bf8864729fe1740635c0be5c1c5b7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:52:20 +0000

    belief bottom flat worried 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
