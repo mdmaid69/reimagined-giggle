edb44c7ca04bca71481fcd35e38adac936f3bee5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:37 +0000

    treatment prepare copy unless opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
