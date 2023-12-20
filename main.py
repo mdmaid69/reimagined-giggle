09e2ff74ccf34f608492382583fd5162fee6bf32
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:57 +0000

    soil judge use since whenever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
