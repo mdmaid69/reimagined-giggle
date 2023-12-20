1b0fcc83222de4e1037cb399d64bc58f3b7c0032
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:12:50 +0000

    leg relax feeling gas smart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_bytes_from_array(array):
+        return array.tobytes()
