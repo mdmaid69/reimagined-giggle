a9bbe8398dba033b3fd6d2b164ef64052dfbbc69
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:28 +0000

    favourite frame scare wise introduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
