9beb198d9f53aee85bb9314be172f3e2e473380c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:41:47 +0000

    lend before rob letter official

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
