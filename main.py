cb4a89aba01e891603bf54083e27ed1d30651113
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:31 +0000

    mouth look keepfrom size display

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
