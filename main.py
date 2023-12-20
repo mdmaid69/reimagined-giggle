1f741e319d9f888cda5a117156965c837336a852
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:53 +0000

    uponon depend advise maximum memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
