5d88b734376e3db6a82ca6455c834a33602f784d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:29 +0000

    danger holdon basic wind accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
