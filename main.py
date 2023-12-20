5a2cec5b11d32a5bbf29339d434f933fb7c00cd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:03 +0000

    promise over plan main shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
