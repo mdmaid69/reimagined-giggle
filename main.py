f86619cb5d9ba19597e1d9cbb62378fa1e63b641
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:11 +0000

    feeling embarrassing comeout main my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-from collections import Counter
-print(Counter("hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
