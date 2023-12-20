891d6261df504f6476c656fce246ac4770487b35
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:31 +0000

    pay knowledge dish rain disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
