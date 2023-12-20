4134eb77295b0c58ded12f03d5616d3c80e0a7dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:13:07 +0000

    callup sink satisfactory estimate half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
