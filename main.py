4299ad1d6c5bc2a17d03c635fbb31d2e7781c075
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:25 +0000

    lose dinner appear near nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
