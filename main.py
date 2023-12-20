e25c9f2a9075d207eda6a7b26120ad6b1b5b8412
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:33 +0000

    else actually duty supply other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import time
+def get_current_time():
+        return time.ctime()
