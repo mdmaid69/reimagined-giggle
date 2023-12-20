a8996b311ceb0bd57d75634e9281cd47fdb5d742
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:03 +0000

    appeal in friendly brave complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
