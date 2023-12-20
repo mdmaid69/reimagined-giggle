6b65d65c57fe7c77bac587d25f9569f645968179
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:48 +0000

    confidence wet eager pretty chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
