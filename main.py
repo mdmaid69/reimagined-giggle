21408ecdd24c1511c186c2e28d0032f64177db48
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:21 +0000

    hurt grow tap prefer joke

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_tangent(x):
+        return math.tan(x)
