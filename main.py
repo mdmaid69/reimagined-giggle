182ac5b34a40ea2cba648f685b62c7411bff01c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:09:47 +0000

    practice complete provide account entrance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
