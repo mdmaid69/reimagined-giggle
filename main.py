93179ccc6cf70b843d94df5b5153e8b951da49a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:13 +0000

    brush block less season condition

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
