f67edb4ea638eae743df41bf0b3d42a81228b024
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:57 +0000

    love occur right demand backwards

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
