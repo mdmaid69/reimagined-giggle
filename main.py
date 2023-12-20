59d1cf53aa51217d969f29d5d2c9c91661e2808d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:00 +0000

    approval them breakfast exam explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
