87e79870a460d322338ae7bd4e1d0850303f8bd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:57 +0000

    first wealth high turn thinkof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def square_number(x):
-        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
