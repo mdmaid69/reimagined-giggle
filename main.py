61fcfceaabcb91a006cd90b844c7cb57423bfde0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:55 +0000

    performance patience main mad post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
