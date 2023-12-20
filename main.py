3560ec15f85360a2b9f4bc78c6a7cc117bd771ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:06 +0000

    exploration level divide match murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_power(work, time):
+        return work / time
