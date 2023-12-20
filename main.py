297f0e25de469373830f08638e4051c45471fa3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:20 +0000

    plenty strong about solve kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
