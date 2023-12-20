7d6126b33c0bb16b0796fe70e3b7edbb59f410cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:21 +0000

    contrast middle dear talk Gg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
