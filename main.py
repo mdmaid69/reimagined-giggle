dcd66625fa2af0fd77391a9112a4b9d6b2698829
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:31 +0000

    alone Dd coat mine complete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
