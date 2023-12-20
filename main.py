9ae7ce8737272cb36567f2f873d19fb277e17fb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:37 +0000

    noone Nn attempt splash next

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def subtract_numbers(x, y):
+        return x - y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
