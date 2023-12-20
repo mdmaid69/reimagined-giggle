aa64eae466a954e9cf72dcb50cc6160a6ce01cf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:39 +0000

    steer take loose absolutely brain

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def sort_list(lst):
+        return sorted(lst)
