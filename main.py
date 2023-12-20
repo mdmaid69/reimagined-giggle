1581a6855359a12cdcdb38647165df2aefeae401
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:30 +0000

    affect turnback person getoff like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
