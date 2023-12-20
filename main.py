f68f455b433fa23104ee05b4f7ce7a7701c83012
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:44 +0000

    might boat around keepoff sell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
