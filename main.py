bccc500c1ca65b417cfaa8b5b7d5e1389c70dec6
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:42 +0000

    shadow mistake on breakin excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
