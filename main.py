a9aa5f3e5f1b8f1fa3d6a874a21d47d23e1f5c57
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:20 +0000

    react something year half apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_list(array):
+        return list(array)
