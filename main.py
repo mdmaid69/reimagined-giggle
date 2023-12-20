f74236c3b169d3112199b82bfba7d9569374eb48
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:05:33 +0000

    mail corner objection influence guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
