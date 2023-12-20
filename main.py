e55bbcafa4238ba3df38bd0b042e3da65602d316
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:59 +0000

    become perhaps use knock new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
