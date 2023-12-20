94a227d9b7a5eadce73ae220346b3fd3794038b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:52:12 +0000

    file case drawing thankyou exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
