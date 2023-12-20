b918515ea6b09ff57db3a96198624420117a3ca7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:49 +0000

    goaway take minimum next lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
