6338f5228c7b8d2b6d91ac4621dbb31623e81081
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:20 +0000

    hungry word another mine agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
