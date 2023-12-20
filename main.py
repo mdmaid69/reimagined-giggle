b91b1fc2c54c5312b42e3e185baf6462ee2b39a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:10:46 +0000

    honest right achievement practice course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
