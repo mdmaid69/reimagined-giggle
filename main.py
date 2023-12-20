a54b6bd420dfcedd5ed224eb55744127127116b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:09 +0000

    mind add description this put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def split_path(path):
+        return os.path.split(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
