73d55827a61feb08161eab0e51497aa9d41fe64b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:05 +0000

    gas loud some weather communication

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
