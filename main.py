d6377a0e92521f502eb2f78c40738484b0deb908
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:04 +0000

    radio operation inside relax occur

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
