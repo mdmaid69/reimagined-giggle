7c04a0e9db478e8205f1c329a7b9f4fd8c97a2b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:11:18 +0000

    print entrance central invite poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
