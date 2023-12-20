69c4c7a1769bd9d8ac645bd36230dc2923dc6e38
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:04:14 +0000

    slip crash protection relax find

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_to_hex(n):
+        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
