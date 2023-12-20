14d227ffb8d7621a772b70934c3029342a8f0d3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:02 +0000

    fierce beginning thorough last snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
