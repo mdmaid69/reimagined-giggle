3164974864dc09a17d145cea084b0fd95740ff47
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:46 +0000

    comfortable turn most fun handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
