1a81e59e43202ca65de9c42a5b7c90217484c190
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:17 +0000

    fashion stir learn excuse load

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
