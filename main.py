c6afb39daaa24a79f31369ae88415312e796126c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:01 +0000

    talkback fit blind bright sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
