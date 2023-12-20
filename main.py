d1095b2f7ffde936a43795fd8451cf8409542751
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:28 +0000

    stiff act plastic closed guide

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_pressure(force, area):
+        return force / area
