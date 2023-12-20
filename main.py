7bd4796a30837cd44b337c7ed6bb660eef3a7f53
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:50 +0000

    criticize sort operation male wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
