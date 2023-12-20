ec95237dda7a759a681fe657d33b45b764f4862c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:25:05 +0000

    drop tough learn everybody regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
