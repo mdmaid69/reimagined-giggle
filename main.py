cfce95f77430a4aba4404dcb4bbffde189bc02a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:45:44 +0000

    goback(to) computer suck recognize dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
