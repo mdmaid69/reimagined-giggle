bc4e451c06004303d8b80c25d1690317552ed81c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:26 +0000

    someone priority fast take holdup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
