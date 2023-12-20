75b286ad29da08e2eed96b2c8428e67fc009938b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:55:17 +0000

    firm talk agreement instruction figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
