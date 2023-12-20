c3beaf96e82c0a8e15d3e69626070fbe6077d9d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:13 +0000

    change comfortable disturb give broken

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_repr(array):
+        return repr(array)
