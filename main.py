381b4a0d9d0f3e398b2b45ced775dcff0dea453e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:25 +0000

    weather listenfor shape range keen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
