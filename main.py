2ac88a019a0ec548713d8918fb604535f4823c38
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:47 +0000

    judge but sick ilovedicks risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_density(mass, volume):
+        return mass / volume
