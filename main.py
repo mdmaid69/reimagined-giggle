59cc1c1312cf059c7deb4e5585db9cd6432b6b6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:34 +0000

    expression somebody cover broad bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
