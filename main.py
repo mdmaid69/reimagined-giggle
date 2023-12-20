fde2480d36c79b657b0309ac487397af3a8d048c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:02 +0000

    Uu transparent accident left bright

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
