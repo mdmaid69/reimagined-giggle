72f16eb105be5e85af70d65b0f7d1d840e695a88
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:14 +0000

    exactly very hurry why first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
