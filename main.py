c9318146e325897a1f90d6da55cf3244d42b373e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:02 +0000

    address pattern invite situation contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_array_to_list(array):
+        return array.tolist()
+  import sys
+  def get_python_version():
+        return sys.version
