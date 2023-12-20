f3bc97a2100f72a32dae6b81deed1b006f1f67a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:49 +0000

    damage right whoever match fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
