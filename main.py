53af122050843cf2c58a664c591669406894a1dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:13 +0000

    idea match generous house our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
