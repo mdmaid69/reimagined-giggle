5dda5730f005b2f7eee1fcec27f376137f7dd270
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:05 +0000

    accidental path drive smile bring

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
