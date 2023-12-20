5b737b2919fedf231c57b0dd80ab870ba55b331d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:06 +0000

    body slip length church pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
