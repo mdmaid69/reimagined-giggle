20431c53e514084b110d79528da3f84b6c784135
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:51 +0000

    justice heart turnback throughout variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_float(array):
+        return float(array[0])
