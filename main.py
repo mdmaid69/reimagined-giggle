19b91b7391c5282be2656dac963f780ac494754a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:09 +0000

    dull discover smell keepfrom please

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
