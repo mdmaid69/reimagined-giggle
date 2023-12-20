f682c27c7840a366baa445b0b5e9a48540102a17
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:30:18 +0000

    around connected lucky ignore mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def multiply_numbers(x, y):
+        return x * y
