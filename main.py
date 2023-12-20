608ae69dee5f92fb595ddc7ba5c6ec5e145eda5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:26 +0000

    turnon tired yesterday record confused

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def count_characters(sentence):
+        return len(sentence)
