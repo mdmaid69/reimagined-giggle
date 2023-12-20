9b4608e8955919bb9bed5904fc9dc7a79fe97f93
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:17 +0000

    block butter replace wall nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
