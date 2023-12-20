c5699c33469b13b9cb8fb323c195fb3e71563470
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:31 +0000

    quantity estimate callout preparation Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
