d0b1d3180944272a6b36047fa358e66ab3c0dc3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:00 +0000

    treat nothing treat stage measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_tangent(x):
+        return math.tan(x)
