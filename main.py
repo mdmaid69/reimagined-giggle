751e1248458a34f6f8c3d2f76b9476332f1aec46
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:22 +0000

    soon your since sell string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
