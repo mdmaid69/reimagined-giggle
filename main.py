5fc3f829e545c8ef822586ed26099e8945507c3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:55 +0000

    shock recent substance 569enjoy pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
