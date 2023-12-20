3b0c4405f470b2ccf4405e145c1902ad53dd9cd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:42 +0000

    question dangerous emergency system work

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def reverse_array(array):
-        array.reverse()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_item_count(array, item):
+        return array.count(item)
