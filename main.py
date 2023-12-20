82c20883fdc0e368d8dd36a16773cdb4263c99cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:14:57 +0000

    whole load flat wide runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
