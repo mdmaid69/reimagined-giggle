d4a5156e06b890bb1f94e3aeeaaafb8e0984e0df
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:24 +0000

    clothes live greeting safety careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
