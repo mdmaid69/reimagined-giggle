4b33eada5043e8eb9a33ce0ed9beae497476f29b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:07:05 +0000

    comeround money too noisy holdon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+name = "Python"
+print("Hello,", name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
