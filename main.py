2680291e65d972ebda475ca5ab173c6edcf9f110
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:37 +0000

    cover appeal instruction silent indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
