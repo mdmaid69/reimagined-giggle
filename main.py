6cd629fd2e5b3e912355fe197ab0552a7f32fbc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:02 +0000

    meet statement cold apart moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
