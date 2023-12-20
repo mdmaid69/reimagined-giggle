6a7c7129cee901dde850ff60decbd6bc450af02a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:09:28 +0000

    seem town also post mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
