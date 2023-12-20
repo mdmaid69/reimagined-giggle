5ee90830dd51532b286060d24f0ad9a99801149c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:15 +0000

    best everything ourselves wake curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
