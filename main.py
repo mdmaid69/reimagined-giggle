8a7ba2f0a94c227562eec2bc498b2b27ba1f06ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:09 +0000

    sharp bin even grand shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
