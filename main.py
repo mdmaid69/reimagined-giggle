d03440717372bf640fccc001b59ad7681bad6818
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:14 +0000

    near make yes business wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
