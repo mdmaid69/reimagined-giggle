fd4e674538b50745ff012269caa2d121c37ddd61
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:17 +0000

    anywhere 565end such emphasis runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
