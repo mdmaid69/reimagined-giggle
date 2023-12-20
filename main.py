47a8e93790a7ebd7a32e80287346385dba214416
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:13 +0000

    conduct easy transparent uponon blind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
