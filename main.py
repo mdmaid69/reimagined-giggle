17d5439198d638d9807ac989e97081c0217c195f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:47 +0000

    should disappointed responsible it flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
