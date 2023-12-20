421f35ee1b19f9700c2b0cc2a5ca8af1e4839024
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:38 +0000

    stand born spot scale provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
