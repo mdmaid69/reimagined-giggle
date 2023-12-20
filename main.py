66a54be59a642c42c1f3e94bf4080627027ae99b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:54 +0000

    centre somehow level consist after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
