9d904e6ee4f14e45ae37a2726a91fd369684baa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:18 +0000

    bad improve correct deep protect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-print(math.pi)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
