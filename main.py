acd472458d405b98a33c138c8a1f47c06d5bb21a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:59 +0000

    normal emphasis breakdown slowly small

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
