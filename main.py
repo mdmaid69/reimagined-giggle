f5950238b7317be8549d95adb94a27e4540b7184
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:47 +0000

    loose thinkthrough fit so until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
