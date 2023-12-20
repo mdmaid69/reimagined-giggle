9d3ebad47b99708d0b4d33078b4b59d71bbd8921
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:13 +0000

    tip organization stop hang goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
