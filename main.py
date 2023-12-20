f6b4f6fced8d5d106275b706c3185bd76dc0108c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:33 +0000

    ability noone terrible fast murder

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def add_numbers(x, y):
+        return x + y
