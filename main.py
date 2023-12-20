d9b2235b7efafd5dac0df5d9cc8fff544c6288a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:05 +0000

    attach previous lay talk mystery

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
