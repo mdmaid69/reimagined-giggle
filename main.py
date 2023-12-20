871fea8c1204724e201008abff6406a30fdf19f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:07 +0000

    birth best area cost willing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
