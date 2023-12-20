d001ab35c5ba7b37fa2ad3337b33f683dad7a76a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:10 +0000

    wherever entry getout movement kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
