3bbc51652ee5e45a28e40c3925badc781e2ff26a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:42:31 +0000

    claim reply talk wise ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
