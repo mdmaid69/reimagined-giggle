6531637b3db4fcdbe95296f8fc68e5db5051cf86
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:07 +0000

    shake who minimum sad ear

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
