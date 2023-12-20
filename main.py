530468e139435330f7a18d01617189284982685b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:30 +0000

    wave niggaacademy payment congratulations head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
