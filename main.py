a9a2d80938c268df7968490ff2e638a6e9046d33
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:05 +0000

    dance control previous cruel strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
