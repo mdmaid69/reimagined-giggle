ec9972940ff8fa05f1fb66fdd949e460602c1eb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:28:34 +0000

    chance skirt invite action 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
