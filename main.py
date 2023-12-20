b7c514ddd92c23d32c34add03cf7f445da3c99e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:22 +0000

    history water favourite private damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
