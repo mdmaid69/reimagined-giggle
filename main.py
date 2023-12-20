52af2108f50f35e5b0082ff8cc460904fe87b357
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:50 +0000

    true boat able record climb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
