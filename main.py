7abafb0c6e4f7be97937dfa03d5c32b6116e7420
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:13:26 +0000

    shout movement special Ss set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_current_time():
+        return time.ctime()
