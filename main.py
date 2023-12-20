af2f6dabf79fdc5d42281d94c06f5c5a8e658c93
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:51 +0000

    cross workout ticket ability scare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
