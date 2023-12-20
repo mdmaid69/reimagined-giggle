dc7e80e0814fd4271dcc7944c41970e303d542cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:55 +0000

    apologizeapologise protection keepaway Oo believe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
