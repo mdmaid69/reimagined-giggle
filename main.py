63e28137d652c3ee2543541c77ef53c8395e9d22
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:31:48 +0000

    wise make but file classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
