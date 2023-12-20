d97d87a3d619500371939cf410be887d0baa3fca
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:11 +0000

    matter depend degree snow through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print("Hello, world!")
+import array
+def get_array_as_float(array):
+        return float(array[0])
