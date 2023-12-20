f130a5e1e973182e171a5133b08a69f82c40a304
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:09 +0000

    reply children plenty through interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def get_array_as_bool(array):
+        return bool(array)
+import math
+print(math.pi)
