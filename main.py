579a4800ccd05d8c7fcfb5b094ade5c9d516c3fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:19 +0000

    above give address urgent earth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
