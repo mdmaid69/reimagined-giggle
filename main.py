2880588aa53631eb0fd779ac0e6ffb0453136a57
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:45 +0000

    soon object judge influence not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
