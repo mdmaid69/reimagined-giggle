a9cedc5cba9d9723ccf721e342041b0773c5046e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:14 +0000

    566end well takeaway concerning sure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
