28498ac87e43db558e6f9899837dab79dfc3fffc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:45 +0000

    ready drink dry admire mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
