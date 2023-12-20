2d34b997a70879b7930323380163aa799521a42b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:48 +0000

    notice sell confident disagree everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
