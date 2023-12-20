d8a124ce2965908d58592537c7d61ed65c3a2e9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:29 +0000

    spare above both criticism entrance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def square_number(x):
-        return x**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
