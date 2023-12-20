3bc8f62bbcbdc5e1dca65ebcd5931b73d8cf4d40
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:10 +0000

    comeover cry nor division pool

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-name = "Python"
-print("Hello,", name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
