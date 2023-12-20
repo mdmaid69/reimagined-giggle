dc92d11515710d0c03eb3b92538a6ff52c07956f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:55 +0000

    immediately payment compare workon fit

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
