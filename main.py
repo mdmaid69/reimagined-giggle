09d845c519b8d3f898a7b2b3de9d7c69f6fb491b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:31:48 +0000

    insist stick that content nice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
