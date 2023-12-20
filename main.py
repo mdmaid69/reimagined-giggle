972e4cad2a870d355846b502d9251cd85e7de2b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:22 +0000

    control quickly abroad downstairs sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_min(lst):
+        return min(lst)
