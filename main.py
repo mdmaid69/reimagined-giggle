145ab8bd7d5c51cb8d7aa1f176a23b6032c553dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:15 +0000

    fast top present exist guide

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
