4047587b242287ab6ef8eeccc77eee4b9053bf6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:21 +0000

    hit alive satisfied fill factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
