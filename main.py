751efb00efadb8d9f93878d2ba02fc2a62b6db9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:27:39 +0000

    strict bury watch fail join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
