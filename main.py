599c776438580bff58a7ff5be31219c41b0d5803
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:36 +0000

    content warning race rub count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
