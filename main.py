1bd124588e239b92fa89ab160bf34beb3a63ae59
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:58 +0000

    present very advanced match tired

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print(sum(range(10)))
