5ec6d34c5290283f93e853f2443438ae04b8ba74
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:54 +0000

    rain doubt goon bottom nurse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
