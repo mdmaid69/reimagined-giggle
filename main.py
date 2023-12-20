9101fa429285a2df47c5506672c936db0adced2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:38 +0000

    rare tap concern speech begin

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
