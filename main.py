45bd0c2668c6f7b6e9a1d9baef7230ac3156c954
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:56 +0000

    comeround long combine home design

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
