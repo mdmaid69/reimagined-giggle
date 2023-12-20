f568a1f07157028c83083714d7fb6c9ffaf4adc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:29 +0000

    coat callat sound raise importance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
