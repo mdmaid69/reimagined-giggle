c5c13199659f0175f0193988e6fed91fa02eb41d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:44:47 +0000

    tour splash rest 179bite further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
