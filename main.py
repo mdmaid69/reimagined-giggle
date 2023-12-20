33e6ccfdbd6f3d46169a8ceda9ed9549c8fec28b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:03 +0000

    pure operation embarrassed so stretch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
