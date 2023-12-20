f5a4efc67c09e4a2025b28073aec4c103b23b691
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:13 +0000

    reliable fat space hit stop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
