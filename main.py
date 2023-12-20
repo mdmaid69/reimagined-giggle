999e75bc8f6a996b49508be4f22c3fb6dd896d58
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:58 +0000

    corner sleep frame very plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
