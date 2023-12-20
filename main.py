d68fc8cd0a2a524e48b998246565a67885d86486
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:20:17 +0000

    you really maintain giveup twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
