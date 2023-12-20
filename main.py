626ed058177c181edaa299dd22bee0edfcd982c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:51 +0000

    address exchange draw happen treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def print_python_version():
+        return sys.version
