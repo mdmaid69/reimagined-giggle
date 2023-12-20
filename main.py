d3d070e13913af6f37ac833d0407e969f6dc148f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:47 +0000

    praise miss formal relative sister

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
