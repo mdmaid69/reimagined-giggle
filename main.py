dea4f3c25e9545080fe9c2a3ce1bc99e1ab736e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:24 +0000

    mention wrong drink behind just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
