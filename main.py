3b0005ea055078d0df0f654571c9935125d15eac
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:24 +0000

    ring television before country frightened

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
