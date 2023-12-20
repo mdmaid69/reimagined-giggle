0aa835c72eccc77db64b516b6d27efe76c521292
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:04 +0000

    walk downstairs unfortunate prevent light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
