37ed15adb0af31895e89ad784bac807398da1d2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:24 +0000

    load tense after store myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import platform
+def get_os_info():
+        return platform.uname()
