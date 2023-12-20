e6a262d7661a2fbe8574acded839df2913a6e03e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:17 +0000

    never commit treat produce cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
