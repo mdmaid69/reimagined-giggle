4ad6dd2404db69fdf289c57beb598a67f77a92f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:54 +0000

    anywayanyhow force afford whether steep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def multiply_numbers(x, y):
+        return x * y
