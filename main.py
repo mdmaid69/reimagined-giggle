ac2ba5a7992c1f9a76dc64e0ef4f95fcad2e7929
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:59 +0000

    whenever hope set store face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
