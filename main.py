1e236c9acdef52326b013d247fedf8ddacbbd5e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:14 +0000

    prove behaviour introduction container angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_work(force, distance):
-        return force * distance
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
