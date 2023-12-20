5ff8d4d677f726f46b5cf559c3a210b0955be633
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:47:19 +0000

    that frighten inside great extraordinary

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
