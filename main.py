a8604043e0e8941c2e3ff9b3579c73d5078a23a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:49 +0000

    wire hang force whole under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
