a194b33678828aadaa691f83e73b0aa9a5e539c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:24 +0000

    benefit clean official by maybe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def subtract_numbers(x, y):
+        return x - y
