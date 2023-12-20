d1d715e66de073bb87db12ca7ed2e032bed9f920
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:27:51 +0000

    union always listen exchange keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def clear_array(array):
+        array *= 0
