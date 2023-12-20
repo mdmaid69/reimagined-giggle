f638f9f50f19fe2a3c8d8df053e40d5d8ba6ef05
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:12 +0000

    moment pour fit time minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
