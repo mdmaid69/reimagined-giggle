195b9a10571bf82ee1f8745071f3b133cb5c560d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:30 +0000

    together poor book myself comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
