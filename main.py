6e0d65646d5dae3694181ff1a7dec74cc437f2ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:25 +0000

    bringup lead team gas event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
