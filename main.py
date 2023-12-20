1c2266801543148bc9eb0d02dbfde92efa17a374
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:02:38 +0000

    brain lower supply comfortable measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
