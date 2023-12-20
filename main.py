88907df3513f9e636c0c2e5f8acf3c5926a45941
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:27 +0000

    habit tonight onto fresh no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
