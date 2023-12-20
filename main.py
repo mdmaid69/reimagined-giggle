ce0b25cd70a194250226eb04c2df62ee44f4a959
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:15 +0000

    deaf independent fresh chief search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_min(numbers):
+        return min(numbers)
+import os
+def remove_directory(path):
+        os.rmdir(path)
