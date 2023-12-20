547d4baea21a16f111c2a27d9103a1bfe5aef516
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:05 +0000

    money obvious expression today confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
