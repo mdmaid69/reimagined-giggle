47938961a622fbd6e3a4c431c3c2bf50c4cc5df2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:56:54 +0000

    hot stop stop closed patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
