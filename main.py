581cfb8dbde1f625abcc9389cb9b5d43e45293ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:06 +0000

    this love sense voice round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
