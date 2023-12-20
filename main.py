b7de76a9d85737bd057884f8e55e07c47f859955
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:41 +0000

    cheek sound thin apply even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_power(work, time):
-        return work / time
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
