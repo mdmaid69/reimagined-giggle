1cebb7bbdeda809ed6b537d298d39df221bb2a13
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:44 +0000

    lift tragedy instrument language press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
