8e4be630d7d4577d119da45f062dce04cbeb52cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:32 +0000

    bin large getoff invitation art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
