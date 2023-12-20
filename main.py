39fb164e4a59ae374f600cd49b693114931e4fd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:43 +0000

    influence provide importance heat keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
