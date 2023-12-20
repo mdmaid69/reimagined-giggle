ca2de1d35e73db6a14aea2d30816cc359e0136c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:54 +0000

    thankyou listenfor bin opponent some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
