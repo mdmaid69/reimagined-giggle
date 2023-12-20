72fa1e594ea8af5f684b99f0546fbc8b58f0796b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:45 +0000

    Hh 565end stranger store turn

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
