828f53ab1766b9453ee67a6b54e9d6f090924a2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:59 +0000

    consult stop pretty bunch sale

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def exit_program():
+        sys.exit()
