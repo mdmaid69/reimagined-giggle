2aa2707406e726a0eb152f9183e0b340903f85ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:19 +0000

    guilty careless loss recommend file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
