98da6547cfe86b334ad55525181f8a97005cedf7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:37 +0000

    slice back neither touch generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
