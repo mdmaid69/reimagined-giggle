0347515902d6370ea6d4e8a24af9746efbbf5c14
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:22 +0000

    dance equal scare invite importance

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
