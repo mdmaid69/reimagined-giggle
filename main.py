f9f34e3b83a34ab0f19f992c6f9edf739940f4a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:35 +0000

    turnaway turnout lookafter wash bit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
