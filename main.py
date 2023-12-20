1f5454af12fc0331c817c8b5c8b730a641a12127
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:33:59 +0000

    attempt keen agreement please suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+name = "Python"
+print("Hello,", name)
