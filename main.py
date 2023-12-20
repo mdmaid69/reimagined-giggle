7933e85da132f99752c548c1c16fcdf0b4f62875
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:28 +0000

    anyone note ocean grand runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
