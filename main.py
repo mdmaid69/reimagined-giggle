5ad57f56b0619fc2f454845563193482c0f51e70
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:33 +0000

    together maintain fetch start fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
