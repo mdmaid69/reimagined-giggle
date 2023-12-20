6bbbc07e297b7217c66da4fdc95c5ba6edc7d665
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:01:28 +0000

    trial look piece detail bottom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
