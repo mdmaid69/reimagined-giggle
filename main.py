d06b63a67f5476d1e6cc1421ad602adfd552bd7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:14 +0000

    reaction settle read seem very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
