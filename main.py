2bb68c8ea77fcf8215515b2cbf63d3166c9a4c5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:48 +0000

    lookback(on) free business any hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
