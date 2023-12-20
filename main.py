6f079a8b1715cc2d7d2317d56b90e3d4c08685f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:42 +0000

    gate soil explain see bath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
