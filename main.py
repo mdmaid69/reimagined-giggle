8b1483d9c7f0b86ca0d4924ccabeca9299f607fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:48 +0000

    season chance break trip mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
