f0441d848a2af8f0b4f59bebc13c4922ac7dcf13
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:16 +0000

    shall bottom emphasis pile key

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_work(force, distance):
+        return force * distance
+def sort_numbers(numbers):
+        return sorted(numbers)
