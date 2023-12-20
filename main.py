e3f7bce3814614179bcee06718cdde02c83147bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:51:23 +0000

    force ahead still bringback giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+print(sys.version)
