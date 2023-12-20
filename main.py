76d379f2dfea9bb8887a294f7ad8298040d55c9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:18 +0000

    history war independent anywayanyhow guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
