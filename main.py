88ab19fa60a53bab53ee4da27bf8c05c29a9f6e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:31:52 +0000

    reduce Hh should communication bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
