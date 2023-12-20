c8b85ea9c076b8d16372a8f34c0ba47d7d157c19
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:23 +0000

    mess wherever forgive activity easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
