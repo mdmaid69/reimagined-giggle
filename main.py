02184c6b51f114f3593faae4908afd84df6cde3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:27 +0000

    file turn shall shut dry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
