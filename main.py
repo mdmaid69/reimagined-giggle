561c38b3b0f08c6443ad7327082fd3e924aa4500
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:17 +0000

    lesson turn action nose fresh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def reverse_list(lst):
+        return lst[::-1]
