8fc60ebf3b2c35e594249c345d5e9ab16decbb28
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:08 +0000

    familiar gather clearly flat runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
