6c26064e8403a01a4ebfaac2ed61639e13bffb4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:03:45 +0000

    attempt plenty colour tongue peace

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
