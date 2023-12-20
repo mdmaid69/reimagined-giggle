7282491f5e9995ef5b781a9d45104f51f34be6c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:10 +0000

    there healthy something holdout strange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
+def calculate_power(work, time):
+        return work / time
