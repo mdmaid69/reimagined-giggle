096088b138afb846e57cb258b9ab2f38fe709972
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:40 +0000

    crowded everywhere across too 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
