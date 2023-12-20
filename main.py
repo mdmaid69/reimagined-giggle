bb09dd95d2a822e6747c3631e17b87c16050366e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:07:29 +0000

    split understand twist they worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def square_number(x):
+        return x**2
