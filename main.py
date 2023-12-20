19e6fe0f65ff7c5f2d906ffe2f4f502d721d09cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:05 +0000

    shout summer stay modern ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
