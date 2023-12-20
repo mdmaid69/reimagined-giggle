a6f1a0f911e7d67f4cc213bec518d708f15a902a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:07 +0000

    dead him wave mass remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
