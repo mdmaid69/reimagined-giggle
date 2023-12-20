fb5bf2584742dec26c0f4156fc22caa77a6e6239
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:58 +0000

    foreign short 573entertainment date since

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
