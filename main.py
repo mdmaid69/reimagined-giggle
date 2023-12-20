41fe1634312a9894ea9915351972bee6ec9e4dc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:25:49 +0000

    lower begin lesson become programme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
