b4dfbd51dd8b605b1e1c32296023e0e885d7a598
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:20:58 +0000

    sad runout community confusing light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
