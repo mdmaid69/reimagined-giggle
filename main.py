9122dbb4b4143d0bc8a801a382335c403f9b65b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:25 +0000

    high less ignore talk me

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_current_directory():
+        return os.getcwd()
