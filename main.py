e89bb1f9ace72d01f0177cd812aab9c3f725cf24
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:32 +0000

    Bb interview issue early guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
