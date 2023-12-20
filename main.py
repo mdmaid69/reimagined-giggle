16a9777253a8baa86af490daaf5029b32eecd160
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:04 +0000

    well star grow callup blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
