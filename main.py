f8e5e1ef9f104a05e05b03c686868e6fbf3728be
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:09 +0000

    shout present risk start uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
