51e797f274a72e72b566dcb2c746d7c242fcd1fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:35:01 +0000

    string solve instrument average Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print(sum(range(10)))
