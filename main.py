b11566d9cb2be00f8f6c1a3af3c4711f8197f230
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:41 +0000

    back embarrassing find only light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
