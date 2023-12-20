4c1731703915e96fd3838b763e67cd2aff79718e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:01 +0000

    side simple any command fly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-print(sys.version)
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
