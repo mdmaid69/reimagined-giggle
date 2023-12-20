4903556d6fee5ac9e5ec9896fe5d04d6e6f9d3d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:55 +0000

    takeout morning busy loan extra

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
