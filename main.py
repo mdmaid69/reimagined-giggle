4c2ff26d282068a96c0e35f9c9651d53d597d64b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:58:57 +0000

    confusing flavour last hope tooth

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
