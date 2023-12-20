44e7013505512edf53bdfe4813303821c6edbcf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:41 +0000

    shade comein within powerful move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
