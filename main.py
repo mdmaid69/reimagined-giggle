6f78b3d205133b0eef4a1475f1d6009c1280af19
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:55 +0000

    stamp pump house distance distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
