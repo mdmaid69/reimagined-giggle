94c325d690e20ae384a4dfb2313c15e36720e213
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:57 +0000

    outside previous minute too start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
