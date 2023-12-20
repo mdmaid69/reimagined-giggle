3724d9642627eb7ef9b5b8d257cb78a001bbaedb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:21 +0000

    hope stranger off interested talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
