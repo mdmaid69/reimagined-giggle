b44729f541b191907b9b80afb8b8e51decf8edfd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:27 +0000

    separate apply report refuse announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
