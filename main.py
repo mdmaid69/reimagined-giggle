f2234dc54570d4d05e6fef61be05817b73b61293
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:21 +0000

    turnoff tall sky plenty children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
