21f442ed12e145a01733816717737e2b6bb8515a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:35 +0000

    long lift group expensive food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def clear_array(array):
-        array *= 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
