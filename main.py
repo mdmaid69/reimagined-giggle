e7a14011b3efd6dae63fc50e94c9cbf98cfb72fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:29 +0000

    their tense write imagine declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
