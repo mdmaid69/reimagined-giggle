f1eef51ebfab72911655b63bdfda37f1f4542868
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:29 +0000

    ball place bringforward bus rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
