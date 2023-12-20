d529741acd935950e9f2cd0759fe83bbd86d04c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:25 +0000

    foreign them contrast hand under

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-print(re.match("h.*o", "hello world"))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_acceleration(speed, time):
+        return speed / time
