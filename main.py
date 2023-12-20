88302a9c5308331711190cdb679afa89d0f8fdb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:28:49 +0000

    juice large embarrassing gas Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
