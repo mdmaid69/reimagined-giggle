e0144a2338d3156fc7fa62d36db3506070bb0e0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:11:12 +0000

    firm minor argue together nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
