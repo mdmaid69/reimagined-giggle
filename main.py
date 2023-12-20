a0ab03985eb8d9a4b374e648c6b6e1eb35be67ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:56:41 +0000

    anywhere trust lucky continue factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
