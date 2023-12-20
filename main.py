944c6c8a61068036b14fedceab3cc1fda963fd65
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:36:51 +0000

    improvement dish touch along smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
