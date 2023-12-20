e43847d8b7e8cdf49557e655502da3d3c68f3dd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:54:51 +0000

    shrink opposite boil bus it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
