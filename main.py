1f5c16cde23bcfba6a11a13ae9cff1c102db9d39
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:09:21 +0000

    involve study right cruelty flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
