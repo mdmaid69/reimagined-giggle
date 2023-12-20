5407256f05d60ddc107063c7820744b059e3c450
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:06 +0000

    Vv child awake best base

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
