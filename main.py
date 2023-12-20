1198c8dc9b931ef8c4c0eec91028dfeda98697b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:29 +0000

    them advanced strong desperate estimate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
