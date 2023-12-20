4b91e7ac2936813cc67687aefdcedf6eca0b03af
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:30 +0000

    often careful hour cum tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
