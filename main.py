1a94dc73496be86b6d1fcd5d5439f390ac5ee5bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:56:12 +0000

    exam unfortunate rain round order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
