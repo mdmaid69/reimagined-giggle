23428c8960b570f3042ef2d8b38e404a1a0cce5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:23 +0000

    second but needle fall breathe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
