e59e23ab1a037fdfe8762c281a2ba312b6fcd0ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:27 +0000

    board mistake trick murder maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
