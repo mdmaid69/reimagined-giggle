ca76fd8cf17e68b1b713673d39d61a1af4da5266
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:41 +0000

    breakdown window scale marry couple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-print([x**2 for x in range(10)])
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
