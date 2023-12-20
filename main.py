cc30dd1e943a597946d974a02a15dd76facea2fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:36 +0000

    568energy waste afternoon pin frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
