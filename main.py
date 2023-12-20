3dcaa2a77692bac4af0dc25c303f2913ee391618
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:16:35 +0000

    dance 573entertainment tour permission spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
