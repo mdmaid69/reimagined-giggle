f0a99149385ef07b090c0a95ab01e04581937dc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:40 +0000

    choose people act which keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
