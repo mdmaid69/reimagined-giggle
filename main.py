60abadd4f06cc4a51a8df662648f7c43360f59d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:49 +0000

    apologizeapologise future admire polite judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
