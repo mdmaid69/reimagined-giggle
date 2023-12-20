edbdee473265aaca1f7549dc35af9e60a1cb82b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:38:28 +0000

    woman deep border destroy ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_min(numbers):
+        return min(numbers)
