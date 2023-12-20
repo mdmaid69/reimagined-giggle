77f657bf54b367bc0ccc31b53f67fdc076e4a44d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:46 +0000

    minute voice go disappear moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
