2b255a6d8b8abceba8b5ce7e9671efdc8e1baf77
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:31 +0000

    pretty move shock object anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
