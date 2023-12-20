02d202d44ff5666e97bc7ffed3de2f806b478acd
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:38:52 +0000

    offence equipment experiment education after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_max(numbers):
+        return max(numbers)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def reverse_array(array):
+        array.reverse()
