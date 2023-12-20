b55458909f872f716ef629e94e9cd205ee7ba3c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:44:56 +0000

    send address dead open common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
