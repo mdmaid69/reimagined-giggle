3742b90542b065d3011d66edbf2d2a108c5b2c40
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:02:40 +0000

    first get brief thanks attach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
