b6c19371f0f81efb68fad8ac949a7d0754ade972
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:26 +0000

    fun worth forever shadow someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
