c3830fa1bad6ef798ab0b333d1ed3efb35aad8fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:00 +0000

    sign education sleep creature earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
