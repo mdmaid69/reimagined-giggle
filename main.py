f47c63b92dfe503118e2a00f91ee510a88395878
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:10:07 +0000

    laugh hope vanish Nn creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
