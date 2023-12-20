943e6ebc50ef09452771e06bb5386d12e5ee5ecc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:12 +0000

    spin worth demand rough interested

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
