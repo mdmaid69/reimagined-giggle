c003c9510e192ebae3d18568fe4af2b09ffef9a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:24 +0000

    perfect obtain splash dress demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
