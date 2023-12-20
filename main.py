da7cfefce9a5561a5d9e7070295653325d9dee01
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:02:35 +0000

    quantity step minority space total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
