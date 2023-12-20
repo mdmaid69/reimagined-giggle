e8fe0d1d6bca554055becc8ce0ac6b403cff4321
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:37 +0000

    foreign further let finally over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def reverse_string(s):
+        return s[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
