c28961d547fca3808641893c2d1215c9551676b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:21:12 +0000

    bright centimetre idea priority 570enjoyable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
