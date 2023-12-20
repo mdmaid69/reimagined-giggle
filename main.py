5ff2da42d590c24675d4c7be01be15d33fb8c538
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:01:44 +0000

    stay basic level anyone myself

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
