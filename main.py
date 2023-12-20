b12484665c858cba4f857990df69d8aa20073f72
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:33 +0000

    exam guide 569enjoy fashion laugh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
