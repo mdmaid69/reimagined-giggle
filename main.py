f6181c8fef43085122b09b51f5a6921d3350b296
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:42 +0000

    might claim direction turnaway street

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
