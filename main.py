ae127a19373e9c1bac11d77f38cf9dfb783e8c89
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:34:47 +0000

    slow agreement touch pass fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
