08cea2385ea2545dbf4d8a83e5a75e33f09728c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:27 +0000

    usually before stop slowly dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
