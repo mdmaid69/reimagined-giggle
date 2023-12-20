51d1161866d8246d2020d0f833ca5fda8dbe0d28
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:55:52 +0000

    sound sense poor prevent cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
