4f0972144fd6f11a4b70f34796779b76f4c2e9b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:07 +0000

    message side thing bringback meeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
