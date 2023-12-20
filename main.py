5db5f2f9e413a29696a4c3f0e115796d7c1b8eef
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:07:12 +0000

    protection mystery except himself board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
