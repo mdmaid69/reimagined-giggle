fd1b5263cc3cf8f649c3a2d877badf7b657ae3bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:10 +0000

    absolutely especially exercise stop certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+name = "Python"
+print("Hello,", name)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
