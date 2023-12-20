c0f1cdd2f54b0f5065eefc831a014afcba74012e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:31:26 +0000

    generous bin display pity comeback

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Hello, world!")
-print("Hello, world!")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
