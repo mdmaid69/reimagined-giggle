9f152ea6f5d030530cadd8a7429cd293ec77282b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:23 +0000

    complete wave we father strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
