cc4282f1712d249713df6c9b5d250bb0f34f7ea0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:26 +0000

    build tall community pair finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_inverse_hyperbolic_sine(x):
         return math.asinh(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
