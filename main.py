a471adc47e16c79a1cfc3bbf19f4ead56b58db69
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:29 +0000

    charge flow seat mess shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
