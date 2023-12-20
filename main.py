023c98f56f57618eefaba5484fabfc6b48113dd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:32 +0000

    keep back strict already crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
