711de9924df1babc54f6fd3f7501b61f9a136070
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:51:23 +0000

    slide whenever bury arrange furniture

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
