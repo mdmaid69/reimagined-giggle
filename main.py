614ec8a9020bf601074739f7675c332017193668
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:11 +0000

    thirsty snow government accept pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
