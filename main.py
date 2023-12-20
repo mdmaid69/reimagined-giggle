abf47c2a19664f475d1d2c86cb5926f7970d2b6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:07 +0000

    after serious present past throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
