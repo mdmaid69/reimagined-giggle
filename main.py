6e7efaa90cbbb53c2f00dbaa47346df01ff43e8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:52 +0000

    real schedule bank future voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
