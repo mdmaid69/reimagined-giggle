f348a2d106e9c6009c3dc8afccf73e1dcdbaae07
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:31 +0000

    dream terrible once Yy mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-for i in range(5):
-        print(i)
+print(sum(range(10)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
