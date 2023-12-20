1aee7844fa0525ed80ddef613a5251be1749ed7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:17 +0000

    beyond history scared nigga solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
