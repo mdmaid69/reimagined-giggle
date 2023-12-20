d65cadf6cd7733b55554a669b0c956d7b788fa2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:26:50 +0000

    increase clean sweep bend prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
