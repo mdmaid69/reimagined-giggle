67f49d23d22bdc55a84302f983a2c07486faeaa0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:44 +0000

    note disagree advantage winter emphasize

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
