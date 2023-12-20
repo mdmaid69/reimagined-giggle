7f4f4fb3d8f057ec2a7ea98e37df6b5adfb74a40
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:30 +0000

    again strength family invite dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
