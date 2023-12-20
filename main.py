01b63c0b830e28f1b08a232c8f48aec5ad92418c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:50:09 +0000

    turndown tap lucky instrument die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
