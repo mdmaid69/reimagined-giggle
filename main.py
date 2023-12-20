99d26c0dec96fc8fdf4bd7d1256dbb7dffc0c5c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:30 +0000

    afterwards organization scared before pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import time
+print(time.time())
