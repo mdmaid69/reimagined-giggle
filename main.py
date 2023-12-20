cd20de08e2a92f7665dd77ef86f4d393dc8f3b49
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:41:17 +0000

    difficult fortunate whom ahead place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
