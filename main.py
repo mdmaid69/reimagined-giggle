c7299e6461e26bb5fff6adf0c4248a66e41d2599
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:03 +0000

    sail father determined OKorokay time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
