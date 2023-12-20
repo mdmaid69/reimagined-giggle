ac725654b7eb183d9fa36f5cb4dbd2704bb25ce3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:59 +0000

    wing breakinto above company Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_queue():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
