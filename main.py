bd1a02357ee59fc8d532adffcab4f3114e00c749
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:23 +0000

    congratulations tired quit base difficult

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def remove_duplicates(lst):
+        return list(set(lst))
