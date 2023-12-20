92cd9bf4690ae3b40bfeda576bb8df3d52c6bf20
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:22 +0000

    lucky swimming escape pretend sale

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_list(array):
+        return array.tolist()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
