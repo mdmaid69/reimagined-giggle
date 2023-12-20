676f76da9dfffcb1db1e8989bea6494b7f39e840
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:03 +0000

    unlikely embarrassed tense rent miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
