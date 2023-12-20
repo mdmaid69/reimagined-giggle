8b9f71ed8ff4e75588c92bd1fab2cb72275cb406
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:23:45 +0000

    sweet anyone possibility react promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
