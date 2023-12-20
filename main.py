d38ccc1bacdb4edea2dd4d4086ab4a91829bd57b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:25 +0000

    blue sweet pass school same

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_speed(distance, time):
+        return distance / time
