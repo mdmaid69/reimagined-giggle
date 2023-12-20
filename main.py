933b2833d1a7ba80bf35024b28fcedeb0ec611c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:33 +0000

    careful pride your brush so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
