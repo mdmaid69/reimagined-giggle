bfe90415711735f2fd119341c63eca74f9cac522
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:34 +0000

    behave definite meanwhile round violence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
