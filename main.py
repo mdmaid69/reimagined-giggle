2ac850450aa6b95579fb709ffc7115812680685c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:37 +0000

    above stare sensible getoff round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
