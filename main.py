2797085061ed4de6f455717e0560564d72a3448a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:30 +0000

    stage forget cheek rub heavy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def sort_list(lst):
+        return sorted(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
