2aa9bb788c271374262c7ca67ce38eb27c4fd2e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:16:51 +0000

    remain clean us lucky swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
