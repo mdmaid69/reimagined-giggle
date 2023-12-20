f2c61ca432774b39bf42b35369ed4dc8469f4889
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:19 +0000

    forgive freedom entrance furniture true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
