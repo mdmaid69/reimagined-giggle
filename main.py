0aeab3211c811eaeabb9c6abf5180e41148d2255
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:07 +0000

    answer relationship tread used comeround

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_floor(x):
+        return math.floor(x)
