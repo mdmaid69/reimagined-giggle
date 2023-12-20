c3cdfda7c8bbfd5a9b57abadd495afc9d4efc259
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:07:34 +0000

    away picture old fear beneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
