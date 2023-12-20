c815ea9754d404112dd1dcd009df272bb8a5f238
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:35:25 +0000

    luck dead grand alive charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
