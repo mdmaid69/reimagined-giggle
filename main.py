208614233621c8480a5b910a1d2416804eb7a879
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:09:34 +0000

    invitation correct willing fair promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
