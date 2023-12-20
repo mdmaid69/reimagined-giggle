3763d3128e3604d1caa277708c21c6393e5b660c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:53 +0000

    family hair miss dig separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
