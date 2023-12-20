2d4653da58d8b6dea16e97dd388b82d149960ee9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:49 +0000

    grow drink rush congratulate tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
