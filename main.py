9b011edb38abce9f04f212fe61f3d59f4adf9c02
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:26 +0000

    object speed him however collection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
