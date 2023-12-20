b6e991efbc9425414bebc7be3b2e1d3e3ec9d9ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:26 +0000

    preparation test charge later better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
