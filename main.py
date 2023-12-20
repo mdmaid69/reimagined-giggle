f0cd2a86c0491b4af27fe7033061476662f120a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:22 +0000

    contact friendly surprised this half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
