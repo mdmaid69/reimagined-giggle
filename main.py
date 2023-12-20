33dd3885b80a94e8ec2efda8bb5f8d14ebe70aea
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:00 +0000

    talkoutof serve trap then poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
