b2c3d8a4db3aaaaa601f08176f5f5b93a75a9031
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:51:16 +0000

    draw tell possession stage company

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
