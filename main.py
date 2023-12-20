fa38f2bf04d75feb6f6a84594fa174ab90d5e047
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:27 +0000

    tongue switch habit occasional birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
