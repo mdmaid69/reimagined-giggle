0c2e098f25afbe4951d3192e87d335a7c39a3dba
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:03 +0000

    female keepfrom life clear ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_bool(array):
+        return bool(array)
