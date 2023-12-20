bcd36f587eeaa38941323274a48b6cab98cb57c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:13 +0000

    throw sick next charge polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
