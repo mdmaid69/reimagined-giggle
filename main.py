4a821ba87d7a0087aec5275e08eb3255cf81d717
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:33:47 +0000

    play dance grand waste successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
