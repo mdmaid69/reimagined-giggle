2556634cc05c8b167bee29284553aebdc6edf572
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:51 +0000

    lower right inside definite conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def sort_list(lst):
-        return sorted(lst)
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
