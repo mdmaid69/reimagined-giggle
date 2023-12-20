b2356a6f412bc0fe848a2163a47155ad3a1f82a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:04 +0000

    platform head healthy Dd ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def count_words(sentence):
-        return len(sentence.split())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
