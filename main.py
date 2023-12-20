3a43a56546a3b4b83bc6b9b474b9d2a565534372
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:38 +0000

    job word petrol giveup whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
