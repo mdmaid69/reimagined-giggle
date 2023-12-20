0aa7a2abfe719c2e6cb11037e72ea27822e0f2f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:51 +0000

    shake squeeze company either score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
