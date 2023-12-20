b577005f856f9fc298f478cd239b42f77a0213d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:53 +0000

    spare point man break turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
