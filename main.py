dc55cd65bbff10601213e62f03482d0148f2ba98
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:04 +0000

    tired equal excellent beyond history

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def set_array_item(array, i, item):
+        array[i] = item
