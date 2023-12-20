b98725bd22335aef4612e3d03af2b996215160d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:46 +0000

    spread outside consist comfortable Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
