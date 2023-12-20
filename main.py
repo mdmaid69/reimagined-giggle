ae5011410d9fd9ddda769534dd278390b9c8668f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:26 +0000

    our little design trust praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
