9eb00cbd8f35a11820ffdc7d0979ecdf9ccd96a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:52 +0000

    week gradually bet nor into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
