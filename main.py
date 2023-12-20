6035f8fa578b09f1b87192c039dd600a4e2b6fac
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:58 +0000

    area nearly comment ring holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
