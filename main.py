5b96a364832be4e1102c5f321371cf71f12106a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:59 +0000

    well group minute spend let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
