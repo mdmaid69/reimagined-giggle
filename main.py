09bcdde1b92c61df4d9b33e6c553650d37ce903e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:07 +0000

    pencil gift stamp close public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
