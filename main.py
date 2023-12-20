b8e6568dcc8e0fd0a4407b7bfd38374796d44938
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:08 +0000

    tense communication miss effort road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
