a090342b4d3eb1e5053361a94e259fe719bc67ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:02 +0000

    cruelty flight related coat impressive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_user_string():
+        return collections.UserString()
