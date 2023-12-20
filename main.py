2b4a6324952af60f2bf4057ce1592aa189a02958
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:43 +0000

    freedom damp mark sad person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
