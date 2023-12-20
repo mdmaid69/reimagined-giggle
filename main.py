575e519067a3990a9de5bc1f3a4268c4800930d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:51 +0000

    radio apply raw runover congratulations

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
