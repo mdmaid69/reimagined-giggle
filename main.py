447dec17840cb08ea208f91b7a780211607b47e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:03:14 +0000

    contrast confused unhappy relation behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def exit_program():
-        sys.exit()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+print(sys.version)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
