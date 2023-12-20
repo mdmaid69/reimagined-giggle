382a334ebf3c042e207a9564763719ce631c348c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:19 +0000

    arrange put anywhere file funny

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def is_palindrome(s):
-        return s == s[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
