93b852b8de504a8e42c6dc6ad3bbf655c4b4ac5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:38 +0000

    bad bottom thankyou need steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def square_number(x):
-        return x**2
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
