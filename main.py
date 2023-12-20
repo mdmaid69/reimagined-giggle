e0e95df4753d3ef55948e567cf6280d3361a14e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:06 +0000

    store lonely plenty ordinary any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_unique_words(sentence):
-        return set(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
