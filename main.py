b9c8be2355c049ad5741aceb6d24427d80bd6d4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:12 +0000

    spray still possible likely next

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import math
-print(math.pi)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
