1dba0fecf856ae27dc118040fbfae018cefd060b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:32 +0000

    variety university during care substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
