555e8bab1ead21269029f45fff9cf08fa7d1cd1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:21 +0000

    tape thinkof preparation development happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
