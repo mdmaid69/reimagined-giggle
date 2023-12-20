f62838a194584667103744e3ad7aeab25faf6413
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:58:23 +0000

    physical rule warn sting move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
