8e3e9bd31708e4f76786aabb8b857b689ba366b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:47 +0000

    thankyou open talkoutof church show

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
