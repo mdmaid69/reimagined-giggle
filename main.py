04e4c75140a267f6849b011eef448c2c653f4115
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:41:49 +0000

    quick main draw absolutely pain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
