7ea3ee083a313e663f1862dca336c82954f096a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:34 +0000

    wild name circle last familiar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
