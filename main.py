e127c3173bdf4dc47b8bdb44fdd475704b0993a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:25 +0000

    round own difficult murder pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
