6aa84eb5c6f758f49aa5dfa8e00dc9f0fb625872
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:44 +0000

    wing hello flood trust that

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print("Hello, world!")
