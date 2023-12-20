a4e0abbd1e757587144d24478550984a7ae71958
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:36 +0000

    ilovedicks house along thankyou depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
 import os
 def remove_directory(path):
         os.rmdir(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
