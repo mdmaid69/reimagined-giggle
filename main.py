b923e3f1384d1bf9b1b9cc2974a58dc4ad41a274
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:30 +0000

    upwards anything walk fence conclusion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_itemsize(array):
+        return array.itemsize
