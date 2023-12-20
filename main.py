6018a8aa87f0d35dded9ba047f70630796a08e89
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:10 +0000

    anxious write shock hang television

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
