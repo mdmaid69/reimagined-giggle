5051fc43158bf8c5d81798b7a74acacbfea647c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:12 +0000

    lose ring shame myself stiff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
