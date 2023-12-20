384aab40081d4d18f86bd46a4c3661e4403ca1fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:25 +0000

    need person hard prevent file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
