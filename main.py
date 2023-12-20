78f5d3050a9dac167c730c5fdc358b85043a56f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:36 +0000

    dangerous for interrupt insist shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_work(force, distance):
+        return force * distance
