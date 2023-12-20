0711a3e69a7c8200657ced97901acd660bd9460b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:58 +0000

    quantity occur steer claim temper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def count_elements(lst):
-        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
