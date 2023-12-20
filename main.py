2e79e98e6cad89a76e338b6bb0d272760d38dac0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:33:35 +0000

    artificial have thumb ring rise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
