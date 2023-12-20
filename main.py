85cc46ef61c322c14e1d453f3404088fc0f03f50
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:35 +0000

    human badly life loss type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
