6b2fb6f84332b96378086655c0e632272277d39d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:02:39 +0000

    score slightly apparently soil thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
