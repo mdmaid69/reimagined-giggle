fb7bbd64057035ad95324fed50f2086c9170bdda
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:51:00 +0000

    winter match division coat usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
