c3772f911ebb380da39df4facc4e829b9cde05aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:40 +0000

    deep blame impression cry disapprove

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
