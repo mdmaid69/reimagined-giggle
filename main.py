0de78312b2c9d7e08624412aad1cbd46f90f6a54
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:49 +0000

    boring use prefer sight introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
