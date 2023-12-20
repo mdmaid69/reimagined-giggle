a9f227fefdbe03fe530aa41f7a01d1c303a0c70f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:03:37 +0000

    doubt holdout discovery luck heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
