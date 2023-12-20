9e2f86cf4b5d163e459fbb5acd9f7f343a25f449
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:03 +0000

    cool expensive feed stamp arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+print(sys.version)
