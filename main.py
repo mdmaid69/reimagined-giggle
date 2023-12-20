72d169a9d604e4336c3c3260e296c43933f84620
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:00 +0000

    likely play unite woman giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def is_even(n):
-        return n % 2 == 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
