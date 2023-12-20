9d494bd35bd7fc63addea7d38256fa1b8353a370
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:45:32 +0000

    second pass wish answer measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
