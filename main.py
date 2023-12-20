1e373f3789d1d79576c50e0528e227606a3f0b43
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:59 +0000

    stir unlucky pleased next mess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+def reverse_list(lst):
+        return lst[::-1]
