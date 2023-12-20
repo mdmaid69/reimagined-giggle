0f843f04314d7885e9483cb9c5ac83a27a81bb4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:27 +0000

    exaggerate let sport steep see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
