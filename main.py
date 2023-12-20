e52bf4068d132eaf7fef560be94bd98d23f8e13f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:10 +0000

    drive exploration successful demand accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
