630aa8a8889db21f365ad1c2e9d5c2860b89b3d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:08 +0000

    school cut way sale weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
