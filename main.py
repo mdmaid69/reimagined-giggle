bc2485b4395b8ee0dc176e410a9dd978195e22ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:16:03 +0000

    check joke far like unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
