ae9b6a20c8e6ff344b10d237db07fddd0be89dc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:48 +0000

    extraordinary react drive contrast pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_bytes_from_array(array):
+        return array.tobytes()
