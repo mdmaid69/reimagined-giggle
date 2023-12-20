2882cfbbd18ad382f8583fce9c8927222edccbcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:02 +0000

    chance know whistle deliberate estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
