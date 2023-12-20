b0427d0fccfac46f5c779fbf8b583ffd2c7f41c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:56:25 +0000

    probably firm shout pen evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_str(array):
-        return str(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
