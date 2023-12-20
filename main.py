ec4a3176a44306f1d318411d7baae08e08fe4d86
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:37 +0000

    nothing down reject congratulate feature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
