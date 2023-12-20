c005c56c4e2289c0709e78e7a64932635e2acba7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:46 +0000

    strike vote turn can experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
