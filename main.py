75eb09fa13134aa84ca1317b6fb11c1ed10aadb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:04 +0000

    several mouth separate than perform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
