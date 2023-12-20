1b7098009d7790bb1f8de3dc330164017db1a318
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:25 +0000

    turnon method onto able 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def convert_to_hex(n):
+        return hex(n)
