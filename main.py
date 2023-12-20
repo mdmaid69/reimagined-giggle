628a3f643267d3646a8eb122d4230a6990d8dd52
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:06:51 +0000

    bus believe difficult lift hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
