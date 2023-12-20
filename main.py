ca3288c423e8d9df5cdbdab250cb619daa9c1dcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:26 +0000

    concerning good who interesting new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
