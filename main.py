444d684975051744475a75c8eb7bb3c7cb509654
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:14 +0000

    cook holiday wake shadow body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
