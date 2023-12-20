77b593fd42365c9287137e327c611c976b0b371c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:01 +0000

    debt accidentally cause roll boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
