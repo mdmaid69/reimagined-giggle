0da775cad7356d17c558cde71a44c4f01ad9f83b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:57 +0000

    especially flood better mark partner

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
