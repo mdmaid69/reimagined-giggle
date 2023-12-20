1a33b868cf5d0b48d4b2df05d26d242b22405158
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:13 +0000

    567enemy keepoff circle no fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
