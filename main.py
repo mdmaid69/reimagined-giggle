805c00eb0f205604c83079dacab3c79596a88bd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:01 +0000

    really alive upset new they

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
