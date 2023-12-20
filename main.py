98052e45fdbdf09d2ab4e2436bcd938c368ca0aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:39 +0000

    accidental empty fit tradition bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
