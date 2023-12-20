ae1c3381434de8d4eeadd494b80d876ce7edf2f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:32:54 +0000

    enthusiastic afraid instead settle like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_string(s):
+        return s[::-1]
