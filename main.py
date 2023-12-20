54e95be14e066e81b845e918372209812065204a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:28 +0000

    grade nigga draw divide swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
