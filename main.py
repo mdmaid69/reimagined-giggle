8d9a389768930bafda792ea8d9da5bb00be97d07
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:19 +0000

    whatever outside danger show habit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
