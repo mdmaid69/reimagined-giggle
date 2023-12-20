0a608c463882aa9ba681b50758940b3d3868787d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:29 +0000

    where fault side family know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
