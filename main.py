7d11f64decaf2de43b2e73c9fcc6c1eb3a25e258
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:41 +0000

    get cover claim better book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
