931e4829bd272fa9194dd9c8071a3692a1bd5b23
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:18 +0000

    score abroad country mind cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import time
-print(time.time())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
