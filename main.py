11d0bb8eea9d3268a4d1eec41b10b152e74a7d33
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:12:45 +0000

    Pp increase age genuine hope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
