b185f4ae23f2e19b4bed00f69b60f8f8c79c3984
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:28:59 +0000

    invitation future finish exactly season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def is_palindrome(s):
+        return s == s[::-1]
