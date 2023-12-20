44ffb67f9176cd7ab86c8cf312e095274c64e0a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:07 +0000

    preparation achieve petrol upwards condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
+        return l * w
