c1922dda56ff7774280645ecafeba62b87553a2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:25 +0000

    by fill imagination combination drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def copy_file(src, dst):
+        shutil.copy(src, dst)
