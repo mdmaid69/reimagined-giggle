165034092b872d4946889c3bfdc51d5e0cee5e88
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:22 +0000

    Ll chase wonder provide front

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
