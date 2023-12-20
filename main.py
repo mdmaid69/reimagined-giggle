1558c4072bef184e77bb367598120135e64eaf04
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:53:34 +0000

    path comedown strict production sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
