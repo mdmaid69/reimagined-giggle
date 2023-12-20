05cdb58e6005dee6c2147d15aaaef0866adf3548
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:54 +0000

    company sudden turnback find giveback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import collections
+def create_user_string():
+        return collections.UserString()
