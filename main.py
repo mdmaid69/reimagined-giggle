88688e7466f7fbe78f34bdfd15ef11478b8ad2d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:27 +0000

    load whole mistake bury occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
