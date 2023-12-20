d5ea4b8d29d38d5c0fb64c90b2c95db490341190
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:49 +0000

    borrow suck progress knock surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
