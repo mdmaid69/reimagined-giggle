e392ce3b981760142a1f7227cbc42d4e89a8a990
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:16 +0000

    grow inside pause evening she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+print(math.pi)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
