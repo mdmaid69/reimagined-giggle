18a38d62435d517ba0fe19592ca166fcfdfee83e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:28 +0000

    birthday language land turn quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def change_working_directory(path):
+        os.chdir(path)
