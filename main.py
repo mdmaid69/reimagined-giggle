728943965fdb1afb7d4ade67424ba6756bf42797
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:27 +0000

    much back though cough minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
