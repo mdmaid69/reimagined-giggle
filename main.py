52dc5f97feafef9182a21d9edabbb64e9fd531e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:36 +0000

    stick start conclusion note towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
