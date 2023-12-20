d589f129cef5a3520fa3eeb3f1a7b3d3648226eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:36 +0000

    plain journey race exaggerate connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tensorflow as tf
-print(tf.__version__)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def reverse_string(s):
+        return s[::-1]
