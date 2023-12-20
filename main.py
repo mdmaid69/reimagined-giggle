18b9481620f4888944cd6678019b9c5008dbd4ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:12 +0000

    foot long control best trouble

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import random
+print(random.randint(0, 100))
