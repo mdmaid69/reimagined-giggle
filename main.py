fc7dfd38ed07886ce953be015d3cae84f067e76b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:12:15 +0000

    slide standard influence argue Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
