8fd910973fac00dacf5e611fe5e33733e60fb664
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:35:59 +0000

    far rough juice effect wind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
