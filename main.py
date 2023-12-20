f28236e3b65ba55455d04ef7ab57bd4efeb64104
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:28:43 +0000

    goon how part roof confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
