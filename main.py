eaa6234e49f15b894b678ffc00624a409935f79c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:42:18 +0000

    dead grow godown stretch state

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
