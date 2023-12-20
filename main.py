5c225dbd0237a289a5172c7785946c5e89a04c77
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:52 +0000

    commit male little that afternoon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
