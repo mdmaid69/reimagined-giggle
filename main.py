fea2fa8ca0f618f8eb2fde25d6cd415225e7237f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:06:01 +0000

    strong crowd thinkthrough cry be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
