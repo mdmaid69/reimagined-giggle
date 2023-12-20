6405deeaf53644137c4de5bfb054eb05c1b878bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:26 +0000

    human worth reject show place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
