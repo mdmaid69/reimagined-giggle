1e213a0c31693e5bed50a4a5744d2a146df53e64
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:56 +0000

    skill confused picture offer consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
