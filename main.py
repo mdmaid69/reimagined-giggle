f8462331a7d49369417c57353c9afe2b71ee1a14
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:46:26 +0000

    adjective poor area boring heart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_density(mass, volume):
+        return mass / volume
