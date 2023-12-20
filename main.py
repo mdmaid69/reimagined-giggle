e2a00148983ff393c25a364dd7beb6ca4484ef16
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:15 +0000

    often risk workupto wash content

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
