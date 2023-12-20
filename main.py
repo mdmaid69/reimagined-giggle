71f40e60e77ea55ca9682e76b22ede2ac1b21284
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:23:03 +0000

    difficulty turndown persuade situation 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
