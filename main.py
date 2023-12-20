7495921646a0387cbf7dba9c9bc2a4c301b66c61
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:19 +0000

    well thinkover 571enough onto side

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
