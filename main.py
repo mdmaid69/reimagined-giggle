2ba977803a180d029ec877c610453dd7f6ea42d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:00 +0000

    wipe slice convinced dream legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
