fcca5e7a04dae703b2b315a99ba4e481af9188b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:15 +0000

    mess rarely performance obvious voice

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def add_numbers(a, b):
-        return a + b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
