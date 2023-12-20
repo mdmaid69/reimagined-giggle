5b635d95e6fc70e87053e98a53c8638e9a1c671f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:41 +0000

    generous prefer business bringdown lunch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
