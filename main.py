3dbfde34f939d89e58d2a79f06944381d2db630b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:21 +0000

    original hurry like pronounce underneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
