d3bc3581d30d3dfc162e8acc5134f3190b5f8558
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:58 +0000

    slice Ee progress hello relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
