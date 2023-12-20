75531ffd523c79066889146f611b70d003244a7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:40 +0000

    embarrassing once under minute mess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
