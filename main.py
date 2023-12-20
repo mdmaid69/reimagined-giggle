c3a6338294a448d1ee37d75ecaff448924df63f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:54:21 +0000

    age tight cook seriously speed

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
