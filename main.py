fb947aaf570b894cba9445ea8a0f1c7dcb18be80
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:50 +0000

    lose tonight Mm disgusting common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
