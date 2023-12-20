894906230e65c42ea805cdebc7ba8aa14742c9ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:33 +0000

    exact sorry underneath preparation inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
