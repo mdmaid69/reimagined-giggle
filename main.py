b6aa9f59655a21391805276ddeca37142a1b36c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:49 +0000

    proud number measure regional deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
