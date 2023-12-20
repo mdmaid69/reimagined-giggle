bc3ab951889408dfa13f8d5c43b13f18755fac0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:32 +0000

    someone disturbing mine interesting to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
