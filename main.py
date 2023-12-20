219545a8f6416c0bc359735d5bb28b3b9246dd01
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:08 +0000

    justice average repair mix shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
