990b5d41bc7809d60147f27cabe8e3e902754f95
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:09 +0000

    find murder embarrassing favour love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
