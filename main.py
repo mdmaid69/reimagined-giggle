3767e227cd486603734eb380ad8501933856fa2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:04 +0000

    sound case wing keepup breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
