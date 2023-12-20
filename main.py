e335d36094ad11e51936daf8dd1749f44f0526df
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:23 +0000

    yourself onto gradually obvious keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
