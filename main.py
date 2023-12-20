e91df82660688051d5a72481d3f3134540af4182
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:21 +0000

    the capital true clearly opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
