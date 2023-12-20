a5cd0f1dfe501ff130908cf653060697cdb03e98
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:49 +0000

    opposite fight mail slightly weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
