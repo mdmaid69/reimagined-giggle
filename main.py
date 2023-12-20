8fd4564fc9b000cdc757d5e56941e84086291fe4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:14:07 +0000

    slight hollow crisis funny odd

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
