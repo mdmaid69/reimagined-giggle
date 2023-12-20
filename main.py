69a76931d2105df259817ee10d5cb193c693f374
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:39:08 +0000

    morning guilty back dangerous medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
