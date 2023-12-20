f86fdef7f96b65245c88fa2b27e9321fc0fcd371
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:36 +0000

    tragedy sorry squeeze step owe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
