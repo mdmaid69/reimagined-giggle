29918effc15f6ad08de8f180324d288111fe2fcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:49 +0000

    release slide combine fix mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
