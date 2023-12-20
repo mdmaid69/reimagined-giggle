fd7e66bab178670676f4a39542a825b945c864f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:08 +0000

    round other Yy interested hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def is_even(n):
+        return n % 2 == 0
