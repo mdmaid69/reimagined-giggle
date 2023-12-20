4d7e30cd9686f054f16e8ef11670590a86ed7d4a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:57 +0000

    thank issue callback holdout no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_length(array):
+        return len(array)
