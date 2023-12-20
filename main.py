f024bd36fbdf8c4051a5a42df0b62f33a3a1c850
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:35 +0000

    large afternoon proof place face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import random
 def flip_coin():
         return "Heads" if random.random() < 0.5 else "Tails"
