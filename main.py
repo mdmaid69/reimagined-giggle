0e69e5338cc10bfff6eb8c07c2e69fa38f1d1d94
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:23 +0000

    fortunate just round cheat proof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
