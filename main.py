edf83737dc42fb44b5e409e9737f0bff14f11bab
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:10:49 +0000

    bridge voice comeup store community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
