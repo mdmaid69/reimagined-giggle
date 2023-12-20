1de9261b776a0f1ae4c634e1c1dea9bd6bc534ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:50 +0000

    under about temper border offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
