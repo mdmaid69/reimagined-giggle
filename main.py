66a3606bb1e7ff846d7430629e6a604d405ba85c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:15:20 +0000

    thumb ready remove trip through

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
