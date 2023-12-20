648afaf58ddbc4292dd5b21c1afcc273e5881971
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:55:38 +0000

    proud less ourselves progress theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
