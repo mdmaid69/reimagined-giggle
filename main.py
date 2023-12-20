29f51a5b35f151b4360bcafdb6b323c2241d7755
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:04 +0000

    hospital date ago young scare

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
