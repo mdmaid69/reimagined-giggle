65738e8708688ca5307411e30a9fc1ac189fad62
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:51 +0000

    figure union steal yourself always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
