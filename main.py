14ae6dd75873512bbcbb24b84364fd5b036001a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:07:51 +0000

    mainly knowledge list weak himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_density(mass, volume):
+        return mass / volume
