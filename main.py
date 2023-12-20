1dd775ce8c02871987349b15a8221e51f1cc7e3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:31:46 +0000

    head beat tall plastic difficult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
