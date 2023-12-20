fc9d36179fad9064d292b5c900a9609437aff921
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:30 +0000

    affect further negative worth confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
