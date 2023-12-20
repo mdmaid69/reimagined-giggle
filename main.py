7530d0142bbf51b0f1d30bfcf9286fc3d6959457
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:35:51 +0000

    attractive cook regular taste road

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
