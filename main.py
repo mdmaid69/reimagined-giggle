910d9329fe54104c1702d02ad50da3137395ca21
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:45 +0000

    clever exact character word bright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
