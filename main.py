36461b9cfcbac7000c72ab8eec45628abc4187e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:51 +0000

    congratulations another arrive soon married

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
