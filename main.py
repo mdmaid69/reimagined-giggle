f01bc5045a5a4a87c46b8591fc5dfe9629bb6a59
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:12 +0000

    nurse keen concentration lookback(on) image

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
