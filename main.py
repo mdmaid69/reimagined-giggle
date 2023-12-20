4595e20b6f4995f100149585ad22c9979c534ec4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:56 +0000

    live gradually shoot in wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
