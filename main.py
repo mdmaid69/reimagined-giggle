6b2379dd89495b5b953821388befb44839dddc66
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:52 +0000

    radio insist risk dream decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
