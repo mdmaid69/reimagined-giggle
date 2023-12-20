195cc1dff00db5c3b8ebdeac3511c1718a9303f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:36 +0000

    thinkthrough by lesson Qq treatment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
