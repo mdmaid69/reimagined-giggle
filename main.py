ebd1352d9d6fc2586c73d8c5ac2e8980063ac74c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:44 +0000

    exciting habit goodbye release peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
