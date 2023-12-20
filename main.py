82e2ab96c6f27be30c946b2f322dd363519eb6e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:18 +0000

    lay post boil runover scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
