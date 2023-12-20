fe4e67c8316b73ebb3352c4d538d27b227c754f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:09:14 +0000

    appearance suit her storm far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
