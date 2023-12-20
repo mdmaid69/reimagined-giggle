be8c97b5350e75c123d3cf71aa3322c6077d07c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:03 +0000

    do guess machine later window

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
