a60460d72d0ac5ecec8f688955a14827bcc5f9e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:17 +0000

    simple shadow support blue stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_index(array, item):
+        return array.index(item)
