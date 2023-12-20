5294b1e8b7a0c7b613395881cc54d3a3c527ed0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:36:36 +0000

    risk thankyou wave effect contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
