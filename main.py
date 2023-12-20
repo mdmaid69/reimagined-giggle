31aaba8df3f04fe1af7f9205e10c8eaa172e5beb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:53 +0000

    belong best scheme turninto television

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
