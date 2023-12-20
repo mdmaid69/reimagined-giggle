0e8e1687bd9e7039247284e439fc8420603e03d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:04 +0000

    film dance honest work keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
