ee6d14f5a5abd16bbf98099527f5ec262d87099d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:22 +0000

    through inquire callback plant material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
