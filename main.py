0e8018e7b78fe1ac066f8311de3a2b807df7ae28
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:03 +0000

    before spite frightened excuse exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
