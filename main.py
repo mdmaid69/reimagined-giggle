e189ef40fd30bcdc271dc9f829d213b473722a71
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:30 +0000

    entrance degree blue basic jump

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
