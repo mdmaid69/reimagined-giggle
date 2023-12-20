92c85a8d98cdee6e9df6c3b32daa653c02c69259
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:25:13 +0000

    worried confidence murder exam hurt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_bool(array):
+        return bool(array)
