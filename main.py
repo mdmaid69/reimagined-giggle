cc4cc2bf1763da32c055d8c03a3a4d3c2434d25a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:51 +0000

    breakfast quality tradition means imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
