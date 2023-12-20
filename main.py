1939de939bc3cac7f05618c6303fd3892a4d3118
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:49 +0000

    simple wear dull film vision

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
