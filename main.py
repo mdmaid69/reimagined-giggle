4af9b57cdd0ee5b0b7c92a0366110e21c6bcc70f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:58:42 +0000

    average kill call wherever actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def remove_duplicates(lst):
+        return list(set(lst))
