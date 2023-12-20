a045f04bb5f9026b78284aecd5fcfd148822def8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:56:31 +0000

    face through guard without stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
