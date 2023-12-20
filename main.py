ac53a6c00a2f6d484206e9761a2538d6f653040f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:33 +0000

    offer talk fond push 179bite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
