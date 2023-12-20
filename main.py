ad1c3e748a894707cf60a676cc6be63e10ed08d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:33 +0000

    thirsty minimum steal willing pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
