0d036a015b660b8b87e021ed0e5c2af26963eeac
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:29 +0000

    middle switch trick throughout land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
