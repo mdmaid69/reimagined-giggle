80863732aa3183d04cc0c93d0f0e056f70f1a990
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:23 +0000

    fetch towards office warn tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def square_number(x):
-        return x**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
