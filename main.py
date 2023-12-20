07c4e7059b94732a05cf0289ebe77acf296b3ed0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:19 +0000

    disagree finish hollow question murder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_power(work, time):
+        return work / time
