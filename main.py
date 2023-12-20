25fff8f422dece4816b34e20f0292b24eb67b8fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:41 +0000

    undo size sometimes workout hello

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
