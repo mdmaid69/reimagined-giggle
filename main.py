40b4a041d9182bfa33187e80284d34de3772e42a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:27 +0000

    slow rescue cheat its mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
