b703e1b0a8e3cda88999338677d66410770a9bea
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:02 +0000

    press wish spray continue matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
