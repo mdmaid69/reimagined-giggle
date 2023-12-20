4eaa97dc81429e5b4407d28fba5eed46199b1082
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:42 +0000

    walk need information leave score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_typecode(array):
+        return array.typecode
