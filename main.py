d738526a0c46e4bfcdf6cb5dde1ee63b64e3300c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:35 +0000

    mainly sight boat Ee connected

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
