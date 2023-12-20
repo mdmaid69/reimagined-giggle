23668ffaab73455ba1f938ad3f37fe7036989a55
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:17 +0000

    blame beneath usually extra patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
