dd2471a21407e676c05610117cae851c9d2f0598
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:13 +0000

    keepup surprise possibility shine almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
