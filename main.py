3847d475df2101284aee46815f65f9b4e8754db6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:17 +0000

    term pipe straight shut force

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
