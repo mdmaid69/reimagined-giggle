14e0455a047fbdd4eb66f1b0abfcf99feed4e94e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:31 +0000

    address bringup rank high negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
