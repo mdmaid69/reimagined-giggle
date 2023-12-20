8c929a30e3dba019b4df21195ba10449d42f68de
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:51 +0000

    already must 571enough which section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
