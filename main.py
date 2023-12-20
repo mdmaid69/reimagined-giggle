18150b5811f582337fa960ad7cc02f6e9df74fc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:14 +0000

    spill breath conduct project murder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
