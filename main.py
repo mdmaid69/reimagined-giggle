6f1fc0197ce0562f9b4c6f94c270d8cb7aa70f74
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:38 +0000

    hour foot shall Kk interfere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_bytes_from_array(array):
+        return array.tobytes()
