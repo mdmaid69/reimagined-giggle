99d4ad306e55956d539d5fac559d62717cb01137
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:40 +0000

    centre mood urgent round yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
