4c8290b636754f89062ee496a3ee0b899a9a7aab
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:28 +0000

    train concerning turn bar punishment

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
