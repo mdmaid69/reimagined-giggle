fd74fed848f51a94b5ebc1ba57a933a70be0a115
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:23 +0000

    concerning splash rubbish confusing one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
