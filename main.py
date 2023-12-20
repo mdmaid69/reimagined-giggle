95a2519649171d93899c2432798294e67431e07a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:39 +0000

    lost warning race whisper become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
