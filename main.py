a58a090ccb65a2c28b702b59ddd6abdd58d60d97
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:16:52 +0000

    depend match transparent both godown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
