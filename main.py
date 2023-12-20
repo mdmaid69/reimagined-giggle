36eb3e9fe7747c485480c20d386b9ab25789a40b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:41 +0000

    protect instant except or squeeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
