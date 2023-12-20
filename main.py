80a23bad66e2e9037b763781dbc68129409844c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:45 +0000

    sure information plan yet sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_queue():
-        return collections.deque()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
