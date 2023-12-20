86334da51882f8813c08c8c97603106ba542ed0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:02:17 +0000

    street realize when command study

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
