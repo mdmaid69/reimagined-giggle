31377e6dd148916ed0ca1398405cd1c1e69c6824
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:04 +0000

    split or deliberately pain invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
