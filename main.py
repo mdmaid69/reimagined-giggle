4f84790c05873f5f42da4ceec6b27907ee7880a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:52:49 +0000

    side sure concern watch keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
