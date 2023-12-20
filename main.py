b80a657edb585454644dc79ef60b5d5e1ef42dd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:55 +0000

    future group ours reaction correct

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
