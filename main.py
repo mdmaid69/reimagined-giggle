84307e2345d49ba99943a23b2dfd1b178dfb331d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:44 +0000

    excuse needle continue limit related

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
