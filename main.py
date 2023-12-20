d35b1bb13d03c979e021fc12f4e0d00a7d9dfcb6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:54 +0000

    lock regularly form fond improvement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
