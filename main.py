7dbd09da7802c185fc853a3f7ec2e4c02844e22a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:52 +0000

    separate tap breakup mysterious principle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
