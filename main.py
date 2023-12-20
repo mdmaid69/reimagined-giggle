2b345a0eb63d1aca622588115406d6995b262970
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:13 +0000

    remember organization part traffic start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
