f9e7b985296b10f71c2b02f16b632d06814d8854
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:51 +0000

    eye separate thinkover kind cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
