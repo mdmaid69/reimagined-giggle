ccd84a3f13fc714779f53bae3cdb45c520bd9530
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:49 +0000

    cruelty argue turnover plate design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
