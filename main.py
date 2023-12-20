8aaa0eb1b085b17eba77d400a808e43e928724af
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:02 +0000

    divide fear mine bit tidy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-print(sum(range(10)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
