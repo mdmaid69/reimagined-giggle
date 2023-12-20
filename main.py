68e27b182f414713a76dec0f14091630b6f6a04c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:37 +0000

    raise unite similar he comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
