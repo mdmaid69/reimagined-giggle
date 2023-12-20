f2b15b6400278d64c1f39dd0417693335e4cd5a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:12 +0000

    rest disturb push press bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
