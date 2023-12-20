52b2af4918d720095da4aae9bbbeff7b3fa2ea64
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:25 +0000

    bang wet look interest pay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
