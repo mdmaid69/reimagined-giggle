8c3277ebcde54ec0b91e4c1678c4c17b25585ed1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:48 +0000

    range wind hope rescue work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
