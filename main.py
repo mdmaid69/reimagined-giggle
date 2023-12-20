25345fc5c0998636fd5e4d888b9de150297d3b17
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:54 +0000

    present exaggerate tap software moment

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
