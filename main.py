3c31f070b05e61475c6e7801a9bb76748a9f22af
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:31 +0000

    spread closed same prize receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
