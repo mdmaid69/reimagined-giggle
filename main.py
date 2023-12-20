fb745157725affb09a63dbecfe5ec056e3fbe0be
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:37 +0000

    introduction sign comfortable mad correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
