f5e3567b01d4bba600c107ff899ba08080ddc1ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:08 +0000

    lean comedown buy male pronunciation

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
