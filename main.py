a6d5a5e5011290a00ec089e6536043247645ae40
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:25 +0000

    impressive pencil fire nowhere ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
