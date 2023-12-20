b475478f54a04904cc2e1937d47f3bd6743a5535
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:52:08 +0000

    guilty maintain try me careful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print(sum(range(10)))
