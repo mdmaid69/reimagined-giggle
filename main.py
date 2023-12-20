63ab395af9d6eb4a38470c84a6fd012c13274de5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:33:14 +0000

    price push official close key

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import datetime
-def get_current_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import sys
+def print_python_version():
+        print(sys.version)
