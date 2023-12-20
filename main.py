c10a053b5bd6c7ab66ea76657c15315d9aca352c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:39 +0000

    stamp card somewhere except emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
