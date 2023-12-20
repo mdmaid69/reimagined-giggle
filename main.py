b287de1a6610fd3f846101d93abf8cd6583960a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:43 +0000

    raw remain harm always eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
