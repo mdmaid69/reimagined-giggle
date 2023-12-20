72548b5f67a36a731194e91f8f9767579916f3f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:00:43 +0000

    artificial write see more trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
