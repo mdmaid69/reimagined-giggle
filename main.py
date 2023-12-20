9705d7eb1273ea6c086db37f1e93f0e7669765f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:59 +0000

    clean us music decision drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
