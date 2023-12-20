50cf6459be2d783e112e4f41933faf19bf7310b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:56 +0000

    afterwards say familiar her wing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
