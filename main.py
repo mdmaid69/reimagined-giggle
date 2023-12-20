444a462e03973248d33ceba6789a49e518db5668
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:33 +0000

    environment surprised laugh poison design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
