129045973732d587662b89cb5cdc2eab01077f33
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:50 +0000

    dear pretty basic after then

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def convert_to_binary(n):
+        return bin(n)
