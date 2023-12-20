ca58a057e028aa592cd5065adff93dcfae5f1e49
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:51 +0000

    own come likely load live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
