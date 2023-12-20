5851f6cfd51c5b6ca2f83c5b39d94ae8af6313fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:30 +0000

    joke cough tread everywhere flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_list(lst):
-        return lst[::-1]
+import math
+print(math.pi)
+import array
+def reverse_array(array):
+        array.reverse()
