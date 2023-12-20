75a9122d52b48ef3ee31abcaa493cacab5fc9c89
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:31 +0000

    set convincing information wind to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(5):
-        print(i)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
