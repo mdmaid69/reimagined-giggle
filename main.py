eb8970d504fe2ad543081025ff4a7f843817142a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:08 +0000

    polite offend deserve false steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
