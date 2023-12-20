5b6ee00d54ddfdd21dc1d54735bcb34b6f7dee06
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:33 +0000

    shut gift agreement mouth forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
