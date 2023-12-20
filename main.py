824290763de44dce290ba00ff87f40d3cce7103e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:56 +0000

    if neat juice bottom any

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
