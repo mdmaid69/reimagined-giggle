c33965b7a06dfa7546dfec57fbaa3a7615c59848
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:20 +0000

    separate hot personal or loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
