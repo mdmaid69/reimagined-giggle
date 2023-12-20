c72370f46e2bb05ab8608514faf5af34ca15915e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:43 +0000

    child voice travel point ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
