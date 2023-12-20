1bfff7e5a0d0c05b75dc27a01bdbd78dfacb9e3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:28 +0000

    touch respect underneath continue entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
