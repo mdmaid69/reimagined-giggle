17215f265b54f44bb44e4da0ebacd1e13b120795
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:15 +0000

    dry flow design powerful hate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
