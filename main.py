35e65f49a1e9c3d6e986d74ce592073885462f3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:15 +0000

    her guide 565end right compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import random
+print(random.randint(0, 100))
