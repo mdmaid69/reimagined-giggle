39960de153e14d96000e40d421a29c5c8fb7f650
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:11:12 +0000

    exit treat dinner across load

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print("Hello, world!")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
