a1766f4a24ad844f25bb6331b34a04085d809beb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:08 +0000

    hold alive creature active Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
