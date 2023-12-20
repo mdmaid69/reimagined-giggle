73dde6384e69801cd809ae4a773ddc68167ca6ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:05 +0000

    whole distant describe exact boat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
