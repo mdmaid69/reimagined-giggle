4e51ccb1bbab1b30a23a3881d5079eae7020a171
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:26:21 +0000

    hair lie respect get fortunate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
