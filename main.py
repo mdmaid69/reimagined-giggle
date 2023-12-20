a3f484189ac7febb97cfec5a4f7a8e6ed7d8dd01
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:04 +0000

    broad late book visit dark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
