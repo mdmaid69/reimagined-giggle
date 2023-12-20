18b61fe2e4732cf1fb3084dd62c1f0ddefbda667
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:19 +0000

    related battery clue try fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
