2f0175b99a9ece833f34a082426eb2d63eee0c1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:07:32 +0000

    matter shadow national unable grateful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
