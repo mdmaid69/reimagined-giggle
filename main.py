c0e82dd3ad2b8196895acfdcfb9e0293924aa0ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:49 +0000

    work whole apparently along food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def check_if_array_contains_item(array, item):
+        return item in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
