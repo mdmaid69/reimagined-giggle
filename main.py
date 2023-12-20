ab5bf279348cfff4cd7aa41985e4365e475d7a43
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:40 +0000

    understand maximum style rough lunch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
