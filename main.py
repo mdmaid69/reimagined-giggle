0973ec9d1c053cd38234ab1fed8104e9794fca56
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:11 +0000

    protest exaggerate block understand very

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
