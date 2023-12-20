7fe090d61d48977f77ac8622479edc1da0b93b38
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:06 +0000

    shallow leave our except regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
