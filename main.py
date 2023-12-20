59ba34745051b8a05155019364df0f4085d95888
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:56:55 +0000

    reaction education name telephone accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
