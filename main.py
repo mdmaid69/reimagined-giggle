0a925dabc5d03a71c4e05cfc426ebef1bffd2d3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:48 +0000

    community wait comfort bang circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import json
+print(json.dumps({"name": "John", "age": 30}))
