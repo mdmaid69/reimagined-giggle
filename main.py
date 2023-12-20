cc4d8512690af14f7e16d0d5f49b527955701e64
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:56 +0000

    town feed hurry sleep keep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
