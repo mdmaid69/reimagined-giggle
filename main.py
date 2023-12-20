8ba0c670756b83337d81cd77a33734518514abd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:19:53 +0000

    half number blame love unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
