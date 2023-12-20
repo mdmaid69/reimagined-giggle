459fdab5954c64a0fef2105b80727c6c9fe8cd2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:14 +0000

    example happen runover neither shine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_density(mass, volume):
+        return mass / volume
