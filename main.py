5bd8fa1d30e93e0120bb75f2d07eaf2924635926
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:00 +0000

    comeover minimum extra meaning bank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
+  def is_odd(n):
+        return n % 2 != 0
