176cd83b0f88da2084f3d5b49b80bc263ee4d37c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:03 +0000

    fast careful separate yours may

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
