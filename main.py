0cfab49127bd9ea34a617e668ad1806f65042d11
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:04 +0000

    long right persuade backwards pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
