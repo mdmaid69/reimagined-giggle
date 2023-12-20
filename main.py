ca8ec72703ed1a48f47d2e704d3d519f73f2bbe9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:51 +0000

    space report develop keepup 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
