f22a02c4e4b95d0df9ad671c7968f2890eafa9e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:39 +0000

    keepaway we pollution lost shop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
