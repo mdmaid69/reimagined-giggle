7b975b82ead8efb534dd3a98ee5de8af4c537a8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:14 +0000

    sweep order noisy what hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
