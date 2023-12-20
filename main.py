24ce78d333afdcf0222d50d179cfd5aa7db85438
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:16 +0000

    work exam obtain over birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
