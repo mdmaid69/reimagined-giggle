84a591fae138ab028f5d5fef10eae50132280ba3
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:30 +0000

    necessary pour by correction hand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def convert_to_hex(n):
+        return hex(n)
