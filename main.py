18b70862f7ced3cf68601f23dabc25eceb8d2f64
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:37:54 +0000

    not far present somewhere might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def cube_number(x):
+        return x**3
