a87783cc493d91817569e9d5ba28146d3fc70c32
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:00 +0000

    safe because snow breakthrough all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
