7dd7ed7c7d8bee55982c2060240653b4a1ebd1a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:13 +0000

    clear Oo slowly male story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
