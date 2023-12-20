22ac9c34d81cf295e00acb5d70217ad04141b8c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:03 +0000

    priority tongue stare hurt never

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
