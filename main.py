a90ff64aa72f3c0a952ad546dc83f22b608f4d5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:20:05 +0000

    book burst appeal period well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
