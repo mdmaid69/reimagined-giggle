6f65e33a7a036106d560e5e821f830dd5e0e65a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:44:05 +0000

    then breakinto space rob takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
