dd70de7c34c26a48f74806613578c88a444ff2b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:28:25 +0000

    sleep last stamp only early

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
