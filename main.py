cc04b893824c65edfe3bec93ce294d9083cafe21
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:01 +0000

    fold production enthusiastic criticize large

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
