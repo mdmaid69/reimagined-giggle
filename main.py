7a8be20e3868037a577db0167f640a68286ba40f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:42 +0000

    trust present slightly path worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def sort_numbers(numbers):
-        return sorted(numbers)
+print("Hello, world!")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
