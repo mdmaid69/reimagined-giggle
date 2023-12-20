7f276a7bdd5c6bbb9cbf5914cca9ba7c7dc0aa86
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:48 +0000

    splash turnback correct sense own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import time
+def get_current_time():
+        return time.ctime()
