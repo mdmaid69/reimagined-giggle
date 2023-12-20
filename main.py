4a480417e2c4974c65a3c300a5acc5c9e5b7f758
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:37 +0000

    spring theirs miss backwards tear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
