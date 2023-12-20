db06f6988376a80a06f28b1cc11cfc3884b4879d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:26 +0000

    punishment broad Cc wash government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
