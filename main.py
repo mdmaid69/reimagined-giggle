c5e6a3b5d363b3d5ea32dd8ae06d3c40ef8779bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:38 +0000

    present control damage he car

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
