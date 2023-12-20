2ae3a17f9752b5b22ee9225718081c09973c8275
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:04 +0000

    failure OKorokay modern command gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
