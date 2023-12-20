acc5cb6d2425e36d946caf31c548129ca0b3a005
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:31 +0000

    uncertain substance choice behave mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def cube_number(x):
-        return x**3
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
