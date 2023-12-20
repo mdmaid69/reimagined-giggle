550ea4ead18928e2cf46728ecb9d6784e7924705
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:05 +0000

    reject mood Rr continue patient

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
