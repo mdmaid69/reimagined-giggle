e0f5bdb19ffd4fff6d807bdd0dedd65ddcb94a67
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:12 +0000

    slide relative ought continue count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import re
-def find_all_occurrences(pattern, string):
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
