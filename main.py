a9501da1de96ccc8eb36ebd5f6fb7f4af869448b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:35 +0000

    fire environment respect share confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
