af0c8e2d55432556105e5ce751f3c755f667f007
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:00 +0000

    free service likely punishment correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_force(mass, acceleration):
+        return mass * acceleration
