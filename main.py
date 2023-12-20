50f4aa0ef03885a782c30a4914797b21530ef63c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:01 +0000

    sure female runinto dangerous reasonable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print("Hello, world!")
