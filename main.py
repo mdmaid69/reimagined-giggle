3bc05fdfdd2d08746a00daffd2ab13f946bf0e14
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:31 +0000

    connect university separate replace finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
