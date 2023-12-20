85f7b579758136b7a2290f999e0ae377e1f6a8a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:35 +0000

    underneath yourself quiet slow calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
