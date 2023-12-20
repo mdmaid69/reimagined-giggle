99fc8d5fe2092b2bef5590886b89f3fad4de6063
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:08:51 +0000

    lend health container demonstrate negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_binary(n):
+        return bin(n)
