f2c4c4f2a9418214ad44f5a0c4099a548876fd13
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:49 +0000

    rank bet scare surprised influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
