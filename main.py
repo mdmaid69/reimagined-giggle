4d1fc76772a32407db3c02dfa12fbcb5c7a75bbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:55:13 +0000

    Ee notice pass search better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def square_number(x):
+        return x**2
