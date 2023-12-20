22cbb7e94274949605bade6c7906a7794f1d096c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:10 +0000

    shock habit marry tip computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
