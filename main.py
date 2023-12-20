eccfee47911476ee70a2e9f8195849e002488cfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:12 +0000

    determined tight dark train block

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
