e2ac32baaba65a44318799a170b80cf07bce477a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:42 +0000

    each future criticism declaration contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
