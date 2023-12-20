da76f0751650291d1a4e23b77cd3964cf866a723
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:08 +0000

    field climb lookafter smart nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
