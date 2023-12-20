8734e0e0893ae825e5f0b2546acda6330f3f550b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:06 +0000

    pupil Ee pretty Rr land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
