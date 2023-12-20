4aaf1d5470e35e60e213996f72c6fd7cd8369d36
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:48 +0000

    charge evidence market prepare university

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
