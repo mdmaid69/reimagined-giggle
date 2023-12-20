15df91f5f765a6f06ad35267dcbd8a9e820c6777
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:10 +0000

    design want Bb how judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_acceleration(speed, time):
+        return speed / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
