5a0fea852edb317d8852aac31e644c189f999bc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:44 +0000

    typical estimate bottom share cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
