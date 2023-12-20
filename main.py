7e2376d8f6df596714bb681efa8d3e86bb6ab884
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:21 +0000

    naturally square from ill board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
