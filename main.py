9c220c44753006552ce440e125414269700aa8a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:51 +0000

    bury Ll sticky live sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
