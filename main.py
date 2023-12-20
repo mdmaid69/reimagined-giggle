0791eb057baee7d81803898fbeada4d314c3d1c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:11 +0000

    feed regularly through that finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
