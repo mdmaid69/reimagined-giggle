7b823c29a594fa365a5cb41384fb5ea8daa237e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:44 +0000

    bath tie deliberately value wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
