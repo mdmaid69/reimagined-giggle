c06369b0766505190e7fd871bb93d729978967ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:29 +0000

    choice confident rub with protection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
