7fc6fd858a8ef8508f41abacd839d43f20f9698c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:20 +0000

    side dig software fair head

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
