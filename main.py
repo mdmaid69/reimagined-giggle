1494cc1244c7ecb1c371c8da71da8e71eaaf33c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:42:45 +0000

    cost solve right emotion office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
