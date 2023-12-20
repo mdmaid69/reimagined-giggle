cf81141884275461abfb9b39341c8a884a355aa8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:04 +0000

    bunch agree measure themselves snow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
