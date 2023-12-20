27f0ae5e583e4518caf42cd4988f0248cb1d615b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:13 +0000

    bottom reaction period touch arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
