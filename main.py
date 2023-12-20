8e42242928aa4ed94fc52d001ae801d8d9afdaf7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:04 +0000

    roll under car exaggerate unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
