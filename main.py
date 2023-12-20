f5e9ded948a7d385c551a22cb74cdd3e2e3c8827
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:00 +0000

    possible beginning hard fail Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
