578e4ea3425f90f0771ada97c188e827a54395fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:37 +0000

    creature fear slide mess seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import datetime
+def get_today_date():
+        return datetime.date.today()
