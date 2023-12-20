58fca193607eddf882109dc0590ce9b9ad468101
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:09 +0000

    sort however accidentally petrol nurse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-for i in range(5):
-        print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
