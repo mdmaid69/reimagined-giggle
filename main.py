a2e7483b6cd789f51e7894158465f10618a2ec11
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:42:58 +0000

    respect immediately personal name contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
