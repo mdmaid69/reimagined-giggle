4cd3bfd97a6763390dde516155c30da010e15092
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:06 +0000

    bet majority deep embarrassing harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
