964693ae46a2ccb2edf63dcbf2cb1a5f515ace1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:46 +0000

    clothes start stop weekend nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def cube_number(x):
+        return x**3
