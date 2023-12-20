4a3700104f4f479c6ffa91e0d9c612c131a927b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:51 +0000

    if sensitive believe easy tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
