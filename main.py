610e7497f5590c2c4f970c7ef3a4485f05c692f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:37 +0000

    full fire language hold light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
