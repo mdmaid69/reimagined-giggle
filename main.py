0a4a7d014363a4163e4fdf8c8ef2f6ca335dd987
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:53 +0000

    operation doubt active form before

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
