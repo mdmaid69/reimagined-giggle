9031e5c5204c1b421483cbd3f809b4e73eab346a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:10 +0000

    get bury sell responsible friendly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-print(sum(range(10)))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
