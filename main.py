9df81c9932cd39592a4374e80bf02199801b6861
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:15 +0000

    side history effect hurt reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
