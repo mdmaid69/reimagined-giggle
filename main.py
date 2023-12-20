d2ebd0da32512a2549a6056f221054be2957a684
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:58:17 +0000

    frequent eager address finish speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
