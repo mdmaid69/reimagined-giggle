f1df894e85cba779cdf6d48b8a03204891f1ba44
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:42 +0000

    wish swallow comein unless mail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_min(lst):
+        return min(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
