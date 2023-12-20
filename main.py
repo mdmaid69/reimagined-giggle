4306891963a9a675d4074a777b4dfbde6fb316ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:02:31 +0000

    old back stop hurt short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
