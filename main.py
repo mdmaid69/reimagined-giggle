155e0167fc43e53e3aba817b274c2e06a2abf40c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:07 +0000

    recommend admit smoke relationship furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
