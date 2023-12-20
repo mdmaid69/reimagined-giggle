2c9c73ba803cb5261cc46f3d2bf7919ef4444f33
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:12 +0000

    press unfair delight fond themselves

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
