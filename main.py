ad789612d78da1f7981aa9c370f58bf2111c6cc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:30:30 +0000

    amount tooth exploration workon 566end

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_str(array):
-        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def cube_number(x):
+        return x**3
