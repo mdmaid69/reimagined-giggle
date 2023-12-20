515f40612c0302539ffccc19fe57d2fad6c7649e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:32 +0000

    spend imagine bad return standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
