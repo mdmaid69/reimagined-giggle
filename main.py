85e14083e5006e0841eaeb68b907f44a953fa157
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:25:45 +0000

    sign leg claim stiff perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
