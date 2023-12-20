df40c94d363a6215c1f5bb90ff6ee98c1c7f8dd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:38 +0000

    unhappy government report lay Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
