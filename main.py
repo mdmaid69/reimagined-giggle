595209a465c72d31e5021202a456c9260109d525
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:22 +0000

    hard runinto brave think division

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
