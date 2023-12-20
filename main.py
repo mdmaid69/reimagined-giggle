4934c2c26418040894b12fd083904743700ce21c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:57 +0000

    tread turnon find to stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
