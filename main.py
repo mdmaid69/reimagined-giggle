817f14f7812be70afae8092f4ed8c8e85aa93940
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:01 +0000

    matter much bother neither by

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def greet(name):
+        print(f"Hello, {name}!")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
