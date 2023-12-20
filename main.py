81e3d98a7bd325b8b6ffa7fa1f03126dc5362d00
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:04 +0000

    shut reply convince deaf mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
