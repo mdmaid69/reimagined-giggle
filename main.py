a70c1f22e9152acd22fa0d8919f04307d01b5758
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:09 +0000

    insist form cost match expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
