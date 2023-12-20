e7be45cd8136fc37719946da22b09c8e765a7bd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:28 +0000

    lend distant treat near patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
