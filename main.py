36522dd6ac65d9e262f70750042ddfbe5ec1bde8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:17 +0000

    motion remark hard appear powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
