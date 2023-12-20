733726850716992783a7b147fb804d7f0104ab1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:48 +0000

    call rest myself battery lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
