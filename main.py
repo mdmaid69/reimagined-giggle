77182a87ce768bdfaf1391f1cc53f4af6697159b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:29 +0000

    waste female lookup by plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
