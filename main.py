09c75f63c5c8ef82d9f03d3b9b815fabc51af2a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:19:11 +0000

    beneath regularly aware war do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
