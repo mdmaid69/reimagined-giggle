9ff444f43000ffbcbb2cd84855974eb71c7cc60c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:26 +0000

    belong permission accident permission forget

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
