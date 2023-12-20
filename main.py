04d557c731e8ce217e7fb764892e9d25d0aa69e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:49 +0000

    address rain introduction confused herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
