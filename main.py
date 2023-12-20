6106d9db103c4b87accf7ae0736eb73f16dfae17
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:12 +0000

    minimum himself neither hide talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
