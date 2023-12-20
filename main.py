70ac8fd71f40fbdb9bc6435c04f598dfc1289538
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:16 +0000

    public Itself characteristic wash common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
