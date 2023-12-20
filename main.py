d6505c27e52f5f359686b75531d0b3271945e12e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:33 +0000

    list crowded field tradition rule

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_priority_queue():
-        return collections.deque()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
