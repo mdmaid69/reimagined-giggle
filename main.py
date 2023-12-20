09fbfabdaed990bda1aaaee7f77981f0e4db5f67
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:31:56 +0000

    thanks concern tune rent snow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_index(array, item):
+        return array.index(item)
