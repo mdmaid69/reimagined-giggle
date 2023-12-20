c6e1cd5342fd488efe89c6778909173787ccee4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:00 +0000

    stamp discovery find pull pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
