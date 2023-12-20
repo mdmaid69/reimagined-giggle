c977750d1851b591776018c060f709693ee73627
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:30 +0000

    admire adjective vote different scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
