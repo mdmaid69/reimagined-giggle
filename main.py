e12c76c1d5f76cf45007fffbbe185995a88c42c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:17 +0000

    suggestion copy hair funny important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
