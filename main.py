d28438ac7f1920f4cafde1f90733b298983e347c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:15 +0000

    give goaway ear interesting reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
