ed061dd5776e1c3c087a07c87aa9f07d6b77c8ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:21 +0000

    state continue peace advantage dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 n = 10
 print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
