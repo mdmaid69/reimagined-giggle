a66518e29e8305ccab038306e118e7bf0f28c28e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:26 +0000

    hold stamp be arrive discover

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_current_directory():
+        return os.getcwd()
