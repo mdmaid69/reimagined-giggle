e0feeaee769e70bc30874d894e44bf2b651b7263
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:19:00 +0000

    fix passenger smart firm offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
