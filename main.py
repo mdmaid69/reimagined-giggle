1fb263cf204c6c6cc90c7b36299174081e6618bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:16 +0000

    die uncomfortable beginning impressed slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
