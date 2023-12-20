8679e365b6e8ae91cbbb33e959018f19e80d4cd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:12 +0000

    question remain shoulder active reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Characters:", len(text))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
