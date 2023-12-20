61249d0555725566463cfca9320fd16abd00417f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:23 +0000

    never condition health over drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
