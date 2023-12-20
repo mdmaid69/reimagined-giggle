1c106511a4f09ee0281d7d80ffc6d2d6b10d8d72
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:09 +0000

    grow strange female heart journey

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
