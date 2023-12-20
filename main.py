2a706238c3c7175dd635fd8aee4833db6add3080
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:51:30 +0000

    figure similar load club breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def is_odd(n):
+        return n % 2 != 0
