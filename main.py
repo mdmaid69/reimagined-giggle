dc92a0d683d29c7828aec33506bd9582392403bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:26:50 +0000

    escape every complete crowd beside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
