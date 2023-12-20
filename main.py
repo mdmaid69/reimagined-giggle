742b52f0400b9edd2ec6d423b1c8f9d69561fc7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:13 +0000

    chase suit below murder card

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
