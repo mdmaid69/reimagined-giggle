72c78b17973f57a100ad62a414516efd4b24e7aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:45 +0000

    anywhere anyone which traffic tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
