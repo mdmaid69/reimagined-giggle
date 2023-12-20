7cfdd230bfde694f93265e441708f9b21f403dd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:19 +0000

    smell against allow watch brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def cube_number(x):
+        return x**3
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
