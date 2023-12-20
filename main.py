6a3bf6f7aab4fd84cfbe5c6b0dc9dfdf620d5fe3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:18 +0000

    spare Uu attention plan callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
