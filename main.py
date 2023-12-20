2d48795137d5968991a5715419957e68b24efa29
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:39 +0000

    hollow bath snow carry stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
