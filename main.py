b1131971303edeb00c025f5f917ddf2444c24bd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:28 +0000

    benefit punish suggest then anyone

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
