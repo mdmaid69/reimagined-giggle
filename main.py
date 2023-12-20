68eb8f74eb7e6bf181654eb6b7f4f589ebb2961b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:25 +0000

    forget emergency chance light big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import getpass
-def get_username():
-        return getpass.getuser()
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
