bfcaad39d3a941c57f61e6f016bef3644c884920
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:01 +0000

    about old thinkover serve hungry

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_power(work, time):
-        return work / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
