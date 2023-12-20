155c243573e00042d6ad2d3da4e5aa67fa2f599e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:41 +0000

    consider situation joke smoke guide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
