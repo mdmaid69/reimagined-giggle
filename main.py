74fe4aa29dee1d7ef2549063bf5ef53c91df9a8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:51 +0000

    march off important Yy moon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
