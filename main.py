bb5eda023ffa1d6eaf72643a693eb37f66f18db3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:58 +0000

    line control else duty workon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
