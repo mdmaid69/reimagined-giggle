d0c105e4d9ea668413c630a1cccbcb8a4aa452d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:35 +0000

    beginning ilovedicks blame embarrassed spot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
