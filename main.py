fcdaec8a157b67090d23a262e07e440ee653831d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:42:18 +0000

    say food ever guide fact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
