48eae0a9e72e15307243bacc8b5572ecca4c7f29
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:49 +0000

    single steer accept to sale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import collections
+def create_queue():
+        return collections.deque()
