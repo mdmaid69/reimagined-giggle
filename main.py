133b9d3a3808db1734f85ac72323c529482f1ebb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:45 +0000

    wipe often aim rid work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
