9bdf49daad6bc0506b1ac500b970e373b205d0da
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:27 +0000

    fire tough sing argument cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
