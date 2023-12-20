dafcb86b256832d0c7982dcf79f4c73ceb4c375e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:12 +0000

    mean about cry favour genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+print(sum(range(10)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
