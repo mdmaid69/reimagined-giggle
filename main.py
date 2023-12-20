05cffa4f80d8757e840e0e86cd0b847df1bd9bc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:03:00 +0000

    pressure callat rub disagree blue

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+for i in range(5):
+        print(i)
