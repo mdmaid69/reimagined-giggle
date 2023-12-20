7747c3df6203305fe9ea46d2decf512763b789f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:19 +0000

    trick spot high plan Pp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
