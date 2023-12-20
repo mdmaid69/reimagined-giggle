a651b4ecd0a7afc59161454b8cfbd76a269734e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:08:43 +0000

    breakfast greeting receive check 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
