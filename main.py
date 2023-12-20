f9edaac997c56bdf4a8c8c258da5739797d3348f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:25:50 +0000

    come burn speed punishment flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-print(datetime.datetime.now())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
