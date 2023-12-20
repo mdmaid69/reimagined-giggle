c461ad59dc24b62b35d46ec9faa40dfb3f23fceb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:03 +0000

    always receive sink invitation say

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
