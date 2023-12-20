bba69b733550abed1a134030a59ad0c0481f7615
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:43 +0000

    dick so clever back form

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
