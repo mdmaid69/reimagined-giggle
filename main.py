460134f5cf63a795bc7c181893ae621549703b59
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:27 +0000

    close against remove during anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
