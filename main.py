5f3d72355ebd3a69a722adb2585cdfce186a42b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:53 +0000

    trial danger frame strike up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import random
+print(random.randint(0, 100))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
