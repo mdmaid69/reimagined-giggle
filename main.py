8ee512ef0e39ab37f9318056c8c3d083675e0e6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:17 +0000

    brown sad Ww themselves usual

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
