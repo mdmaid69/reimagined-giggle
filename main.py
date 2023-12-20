c91544d5e0a6447bd7bba031927d60238d01eb62
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:56:02 +0000

    occur male week announce accidentally

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
