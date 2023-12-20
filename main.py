b07a21fa8e4601e6a702a745b4a6cb14aa89fca7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:35 +0000

    foreign division surprise shall wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
