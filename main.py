21ed651d82f42b7f5c2e6ed7538d197b6a02cbcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:45 +0000

    least callat forget will as

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
