8a63621635244f2d18da67c3fce6d8f495af1c5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:53 +0000

    thorough hit warm available duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
