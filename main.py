01822ea355dd6d324ebde1cfdb8b6418f867b011
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:53 +0000

    deal work cheat education hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
