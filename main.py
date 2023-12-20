7246cac05764df5768280631701a10aa1e8ad360
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:15 +0000

    lower spring call pity prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
