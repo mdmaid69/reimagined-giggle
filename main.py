69a6037eb87db65bc6a4c90ef1403b2ab9142a49
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:58 +0000

    century everywhere original mean big

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
