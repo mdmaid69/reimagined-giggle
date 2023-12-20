86cc98a38edf9668a8fb60947b66d0b8a1892013
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:30 +0000

    practice force pure accommodation angry

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
