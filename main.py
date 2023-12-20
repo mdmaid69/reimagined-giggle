e001e3ff1604778e965ce964edec785b67fdb982
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:07 +0000

    entrance unable sticky beat live

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
