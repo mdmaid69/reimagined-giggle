0b89317a6b2530972c70bd168c07ac6f7aca98b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:06 +0000

    stage pity Bb arrive sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
