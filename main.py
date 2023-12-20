d1de1bca057768bdb1e7b13303cf3c63fe4dd30e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:58 +0000

    experience ready fetch flat impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
