1a35370c7e63eb57fb02ea81f405df5e0d741d8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:51 +0000

    deep under unhappy final objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
