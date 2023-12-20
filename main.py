f3685b59b1ee8c770e567ed0d7ee2f38501a5b47
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:17 +0000

    below float original valuable contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
