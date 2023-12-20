6792b317be3605ce6ea9971be5dbb5bdf1e7adfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:35 +0000

    thankyou recover pocket degree anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
