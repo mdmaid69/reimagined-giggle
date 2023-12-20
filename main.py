40bb3578ccc51a3f9f6d8a8152fee1cb0cf9b087
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:05 +0000

    moment nature will negative breakout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
