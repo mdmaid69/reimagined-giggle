b52bff4436e1017c27976f42f5137b7a30bdb831
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:21 +0000

    gap free age middle recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import collections
 def create_user_dict():
         return collections.UserDict()
