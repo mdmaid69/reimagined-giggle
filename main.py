67167ad613667bd0001da7d94d9ca7eb5e476ee2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:51 +0000

    bang mad drop practice help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
