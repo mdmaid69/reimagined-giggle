5a0bd82cc5f22bfca5e4c153df0347fb0c02ef59
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:17 +0000

    temperature bite class rush recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
