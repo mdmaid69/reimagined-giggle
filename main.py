a0684fb2544449f4f18733fcd2cf4cf3e1668ad4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:12:01 +0000

    funny build involve onto kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
