5e0114659b50d1f2b1da3f253cfbe812d4692fba
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:58 +0000

    what this strict suggestion simple

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
