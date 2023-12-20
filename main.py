cc39c4a9203cbc66ad44ca14987abb5df740e229
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:34 +0000

    grand all relax promise build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
