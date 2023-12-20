84a3648f7a043d58db8234f74ad218412a6db95a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:03 +0000

    again difference trap vote please

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def add_numbers(a, b):
+        return a + b
