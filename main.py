80edd57b07a6a1a4bf18599ade4abfd105427ff3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:40 +0000

    trick sky waste scrape quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
