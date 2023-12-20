bf965355876022367a3b7dc077571e7c87411bd5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:25 +0000

    determined talk influence loan woman

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
