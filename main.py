fb8d4611d87b99ad377860524268a9b3b1711d94
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:06 +0000

    turninto dear standard flat connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_directory():
+        return os.getcwd()
