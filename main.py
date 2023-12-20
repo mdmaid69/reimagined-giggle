3db6547da9a1dcfc399f5c49f3cf53ca2de74c93
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:28:49 +0000

    injury walk culture thirsty comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
