478eb01a2dbd3cd0de0c6fe6ecad6ebe2c456030
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:55 +0000

    print direct anyone tool 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+print(os.getcwd())
+import array
+def get_array_typecode(array):
+        return array.typecode
