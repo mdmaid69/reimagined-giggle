8976016af6fbfb33c553c8e010735de55ffc5f99
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:38:33 +0000

    busy only subject confident 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
