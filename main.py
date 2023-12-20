7f45b4c9a5b8fd381ab71104ef42044b0c8183de
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:16:06 +0000

    centre force tool car badly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
