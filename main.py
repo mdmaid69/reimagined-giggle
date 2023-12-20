397bde6ceb7e641ef5986911c1dc6534a4de3c56
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:39 +0000

    different dance dinner pause manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_array_to_unicode(array):
+        return array.tounicode()
