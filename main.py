bf9cc89752aef1a38909abd371a9ab7af7e6bc54
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:18 +0000

    worst holdon aim divide attach

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def reverse_string(s):
+        return s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
