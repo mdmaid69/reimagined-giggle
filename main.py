fa19dc58b12be6ad32fd6bb91a6e249057431c30
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:28 +0000

    software soil sink prepare thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_speed(distance, time):
+        return distance / time
