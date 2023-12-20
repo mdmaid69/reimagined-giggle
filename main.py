81b64feabb0514cda4a605c1ca0cc2a486fe00dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:40:19 +0000

    tax please rain lunch nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
