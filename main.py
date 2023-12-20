35ab6144c3d76119212d1c5e4cd459145f66c4c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:05 +0000

    wander before rid react environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
