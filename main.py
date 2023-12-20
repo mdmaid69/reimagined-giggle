32c0690109a50fa8eb4a6876883eb6c2a93ce599
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:24 +0000

    borrow exercise interrupt programme though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
