48c85c0505ef5c0e369146a3b56de7475d084d8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:01:53 +0000

    side opinion limit party herself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
