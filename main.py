219e34e9669fc8cba1d6ecede943ff0f5a120d5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:43 +0000

    slip destroy tend proud threaten

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
