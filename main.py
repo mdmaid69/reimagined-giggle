d53ed0ecb37233a8ebc12966610a64a20c138910
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:56 +0000

    thing outside inside solve wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
