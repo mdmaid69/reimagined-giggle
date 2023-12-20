ae8988833a4d2481cb2cd1c067a7919cbf4d1a71
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:14 +0000

    mouth use disgusting food in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+print([x**2 for x in range(10)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
