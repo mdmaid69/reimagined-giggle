76b4160377ae7e7680e4ca0435dc353d8a19eecd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:06 +0000

    expression temperature notice after good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import random
-print(random.randint(0, 100))
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def append_to_array(array, item):
+        array.append(item)
