35097f2d737a4e0cad406df77a2f83547635f0c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:27 +0000

    excellent listen attractive government wing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
