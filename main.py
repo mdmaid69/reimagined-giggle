283ad079fe8fc5a95914d1dbd52f43fa15c4f1ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:23 +0000

    hate ready Cc command popular

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
