323a19cbc7ccdb130641aead1f11b60a6cf95150
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:44:51 +0000

    their rest age roll miss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import sys
+def exit_program():
+        sys.exit()
