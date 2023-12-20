dc654665a545be40e5ce4f972fd7052403e5957c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:57:06 +0000

    afterwards ticket mind hair standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
