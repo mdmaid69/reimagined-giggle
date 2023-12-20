d1111320a521e6e8dd65006f7fae1329b9bfd440
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:05 +0000

    tap copy call both rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
