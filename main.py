1369b9c2e0006adc473046656f9736d56831485c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:21 +0000

    wash regard thing vote climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
