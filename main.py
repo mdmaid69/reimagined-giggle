ad666eeafaaface13bb96712762297651a7e819b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:17 +0000

    make match risk fence declaration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def is_odd(n):
+        return n % 2 != 0
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
