226f3548044cde2bcae943cb730fb632ceec4173
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:07 +0000

    careless score leave goback(to) finish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_pressure(force, area):
+        return force / area
