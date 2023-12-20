c6584b380e9c6b5526b11f3847b0e9cf93efd7f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:04 +0000

    pay ago their grand disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
