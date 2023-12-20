27ee7e8ff6e19e28589d72d928a966b77a5e4db9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:48 +0000

    behave war mean means authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
