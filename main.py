c209a89bc898aa5b1376a9d207d5a9bc6c43cc1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:00 +0000

    finish pretend each welcome through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
