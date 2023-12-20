d71948875251fe01372764bfd0678d957fe3f41a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:22:17 +0000

    rich exchange condition accept pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
