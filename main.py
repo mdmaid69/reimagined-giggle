7a2323ab301581a6fc4054cd51acf5f0d1789433
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:03 +0000

    port shop tap beneath situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
