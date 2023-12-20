1997c7faaf25a5c74c7e9aeed4565a69ad148f4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:27 +0000

    shape uncomfortable round offence task

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+print(sum(range(10)))
