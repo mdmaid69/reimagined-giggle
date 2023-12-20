72c4dfc7eb3e8261c5ed08f56da4f92f6debf660
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:41 +0000

    ride fork organize disapprove accidentally

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
