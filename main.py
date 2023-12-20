9b4cfd0e89202db9ce03c774c3735364d471f7e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:47 +0000

    care tone crowded autumn 570enjoyable

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
