5c8d4450be0724021ba5cbdd03f8a39490184830
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:33:06 +0000

    flavour count then somewhere course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
