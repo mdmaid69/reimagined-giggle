f8857334713f21656ad3c7f2f4a4b96f60cba010
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:02 +0000

    rain move neither difficult disappear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
