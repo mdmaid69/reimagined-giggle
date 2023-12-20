7c3858b031850df9d1023f5eb913ca9b71967458
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:26 +0000

    clearly slip body fortunate content

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def reverse_list(lst):
+        return lst[::-1]
