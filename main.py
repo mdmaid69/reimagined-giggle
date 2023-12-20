633ed10b6bf2f2a55f0753d3f2e3692797c5f4a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:40 +0000

    comeout sink cinema mind lunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
+  def convert_to_hex(n):
+        return hex(n)
