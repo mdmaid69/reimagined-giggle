59353c604e4b1bc51ebd73fda26983cb7d18ef54
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:35 +0000

    flight fold uniform escape equipment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
