92a409090bd4ce1b40e55d0d037e0e0556abf3cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:10 +0000

    soon pleased neck principle equal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
