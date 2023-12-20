a5a7adae0e16183018d01a2b8e6c3e84100d95c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:31 +0000

    check guess nor fire happy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
