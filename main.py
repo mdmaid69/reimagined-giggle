8a90f52fb50fe9d923c25ce41c7e081852067129
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:23 +0000

    study during bang relative lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
