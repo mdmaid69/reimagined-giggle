160c7c9be5cce1ff8701a48b25e148463aff41b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:27 +0000

    kind introduction tip buy post

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_palindrome(s):
+        return s == s[::-1]
