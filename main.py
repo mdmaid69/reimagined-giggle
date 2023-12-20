41be7d7fc3c7b359cc6d6b961f70dac642ed4fd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:18 +0000

    protection alone part absolutely bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_floor(x):
+        return math.floor(x)
