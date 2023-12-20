74428d00bc50e0ff5c1e69111c9783275bf7d6ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:11 +0000

    owe wish personal action relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
