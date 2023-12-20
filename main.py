a6a2bda9de7283d960831b3cf1e65db6b0653454
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:03 +0000

    consider bite tear vision meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
