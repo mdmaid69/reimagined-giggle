fc66c60f1ef01534557c79c9a78a6da1cb95abae
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:45:42 +0000

    attract matter one hurry bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
