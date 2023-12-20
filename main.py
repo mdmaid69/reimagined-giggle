936b4f3865e560b8d8b0d3d95b4bc1d118f6fc94
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:13 +0000

    interview crowd by watch wrong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
