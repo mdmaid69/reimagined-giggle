18976f676a90adb0c64c7a1a44531db7a8c6d03a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:10 +0000

    talk contain say square news

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
