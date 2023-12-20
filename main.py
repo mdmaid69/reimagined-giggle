9ddb0d24b27cf77dee3943069ff12ee24bdda6e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:16 +0000

    confused trip breath keepoff offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
