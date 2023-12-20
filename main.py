d3bab33882dfcbea58859d01b914f1b0190c5408
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:05 +0000

    down journey wish school group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+name = "Python"
+print("Hello,", name)
