2b1e87631a0e5d6413ec581f3a7aa1e1f297076d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:59:15 +0000

    failure home possible practice space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
