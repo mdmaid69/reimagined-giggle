fce1d0b37f401162f12a1429c891b442934f1583
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:27 +0000

    little safe record battery market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
 def count_characters(sentence):
         return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
