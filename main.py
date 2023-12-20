bc384b3ec87f90d56b23aad2ebc302247d89da41
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:37 +0000

    least scale during wherever proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
