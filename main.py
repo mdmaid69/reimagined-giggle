a4236b8fc1260f2fe40393053ea236f3dc8208a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:51 +0000

    crime polite apparently mine disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
