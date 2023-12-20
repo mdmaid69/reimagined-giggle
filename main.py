f621f7892be5b645349c85df3e619adb5761ad5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:23 +0000

    skill stop strike on activity

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
