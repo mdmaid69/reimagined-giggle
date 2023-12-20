2ab8f7686aff6ca927bfeb9cb4f1889123ec66b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:39 +0000

    equal whistle write minority guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
