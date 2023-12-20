b6478a49fab029a4dd806de3f8728222b6fe6c91
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:45 +0000

    during wash winter fault impatient

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
