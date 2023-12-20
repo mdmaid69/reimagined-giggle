92e1040d1868746f21f513a0f982e27d772637d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:07:26 +0000

    still sensible keepoff nigga store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
