c6c946d1a89e7e6945c3014067f7d2dd6a26f3e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:37 +0000

    appear by pretend national interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
