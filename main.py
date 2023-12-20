97c308115a08a5a62ff2b874816e0fe9936be0b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:31:53 +0000

    late unkind quickly build soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
