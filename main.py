512b2ef1f2d18fb8e8f922715fbfb4dbea673484
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:29 +0000

    smile pair invitation rather force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
