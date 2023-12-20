2661286c8aff0fd8c50bd70fd66b8b4b8e009a01
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:13 +0000

    least film plant well 572enter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
