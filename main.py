254345b0abb353fb5d3c9f080cf85be2d481b50e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:50 +0000

    test guess anywhere so car

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
