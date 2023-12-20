1ce873d55b4ded66b7e981984057c4a9d3e882df
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:26:16 +0000

    dish bang claim example display

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def clear_array(array):
+        array *= 0
