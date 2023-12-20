574dc300fa288061b35fc04d45ff16745d05c8a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:34 +0000

    bread attract pool flood touch

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+for i in range(10): print(i)
+def convert_to_octal(n):
+        return oct(n)
