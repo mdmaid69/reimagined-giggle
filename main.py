73c186928ba45139dab2b1fc2ec5f3784c23b164
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:09 +0000

    back kind set opinion test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
