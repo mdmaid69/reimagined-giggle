ea798c55a9f2599865ed4ad77de451a1044533bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:57 +0000

    can phone permission shopping favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
