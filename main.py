9f80b3e68824fa51bb6e4e2da93c6413daf8885d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:51 +0000

    do room skill balance permission

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_bool(array):
+        return bool(array)
