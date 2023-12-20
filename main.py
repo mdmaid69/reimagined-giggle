cbc7a411d06a99b164cca546d5c455bb34b30ffc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:39 +0000

    wash snow Mm takeoff yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def subtract_numbers(x, y):
-        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
