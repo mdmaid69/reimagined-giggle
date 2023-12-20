e5c8ef25afd8ab00b89fc7590e68465d2cb63c41
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:52 +0000

    slight no dead trick radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
