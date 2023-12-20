8198b904a2a8ae10ca317e1b6586d5b1ce262dee
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:05 +0000

    guide century show frame fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import os
 def get_current_working_directory():
         return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
