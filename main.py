287b80a98e5c87c43900e850f8cd931bfe5581dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:29 +0000

    takeaway target content area sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
