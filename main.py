dc0f30c96df1624513227a2c4f1c4afe7389d517
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:51 +0000

    square confuse model platform school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_tangent(x):
+        return math.tan(x)
