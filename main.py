8e3690fa7b15d4f5215ce3dedbdb4a5e73fc090c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:33 +0000

    accommodation actual race split certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
