25fb5610e917f837cc84b7e34fac64b10237a691
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:45 +0000

    rescue heart event murder proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
