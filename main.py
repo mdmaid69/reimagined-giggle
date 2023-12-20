02eaa897dcecab503b1dc34dd5de03f1f27114de
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:30 +0000

    Yy sit wherever Tt confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
