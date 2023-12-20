eb99ffdad7ec673ad2f5b37e355d3a386849c23e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:19:35 +0000

    cold shout argue load advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
