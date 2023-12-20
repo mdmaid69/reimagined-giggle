6cc8c94ec218263283fe1b84d03ec1dd3c53e7a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:03 +0000

    exaggerate complicated identify somewhere argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
