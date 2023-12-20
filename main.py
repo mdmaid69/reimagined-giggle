35d25994818ea95b359bf57a1a2352fdbe59d08b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:47 +0000

    offend furniture give lucky smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_density(mass, volume):
+        return mass / volume
