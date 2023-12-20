673e884ba89f16c67986e3e4fadb8a46a9b69e79
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:48 +0000

    inquire together child protest congratulations

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
