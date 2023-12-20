2e9f8400c1a31e17d69038c81a0fe58ab6c9aa1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:28:29 +0000

    pool about star amount swallow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
