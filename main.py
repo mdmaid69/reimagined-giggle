e54bb010a3e7598fe519f9a6b668a7164dd58d33
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:44:45 +0000

    commit future man cover bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
