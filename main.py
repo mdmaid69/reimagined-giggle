e047b17a286d23b0da62958ad5d05c1aace98d07
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:27 +0000

    set trap in trial target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
