c1b914dbd819c2fbf64958beee8669e572d1df88
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:05 +0000

    operate frighten workon sharp fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
