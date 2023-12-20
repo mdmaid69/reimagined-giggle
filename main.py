b579f163e0f9d2001d2db0ff028d94e33e2f93c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:34 +0000

    else size model must save

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
