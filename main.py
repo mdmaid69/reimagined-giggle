e81eb0d4538f3e4c74e3a5ed05697fd907b3422f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:10 +0000

    rank play fix trade improve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sys
-def exit_program():
-        sys.exit()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
