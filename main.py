48b943ccd118d3b99a73133ca0248c67d0ceb376
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:29 +0000

    yours arm bang report lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
