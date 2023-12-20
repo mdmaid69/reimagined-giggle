0bd4dec2a18d0f4f51112aabd8501016850e2dfa
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:09:29 +0000

    embarrassed environment rough professional storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
