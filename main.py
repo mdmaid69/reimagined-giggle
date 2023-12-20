f100c1d0eaaf0ecb27fe82eaa32d786bb68003e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:07:43 +0000

    sharp prefer comment damage legal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
