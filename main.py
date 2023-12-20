30bff7599675505fdb0b559710e6384ffe1a0a85
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:09 +0000

    take dark block opponent practical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
