bd314bf1dc8b45a137227c3736a4f850c1be882a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:23 +0000

    light tooth advice public pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
