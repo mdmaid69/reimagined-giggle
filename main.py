f7100153c479fcc693259ac890e10be0ba61ad3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:02 +0000

    wave slide laugh aim callat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
