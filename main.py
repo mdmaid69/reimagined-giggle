c5413f92043197f79d3b3545fc4cd7d7b6b28d05
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:03 +0000

    drawing type thinkover common welcome

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
