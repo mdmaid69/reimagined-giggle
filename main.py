d685f617aaf50929901b0557b58401e3ec4eb23d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:54 +0000

    wish pleased later hope month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_string(s):
+        return s[::-1]
