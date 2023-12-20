14d77710ac96c1a9c44c15e9504ce5a9d25baef5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:17 +0000

    tidy equal estimate disapprove interest

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
