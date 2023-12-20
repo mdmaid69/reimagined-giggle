f021a29dacd6c76d629656bab1add5132a07c7db
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:07 +0000

    as study check serious mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
