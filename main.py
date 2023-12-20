74c2281dc131866e42d9469295d2ec77bded1c84
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:00 +0000

    cope pupil foreign gift tune

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
