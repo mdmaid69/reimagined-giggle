1690c6629e3cc40f7b5effb32015497b732b6653
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:58:27 +0000

    address accidental bound lend human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
