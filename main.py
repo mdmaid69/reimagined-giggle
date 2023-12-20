380df692fdadcb94ce3d2cedf0f4c9cc767dcf11
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:00 +0000

    song stamp squash organize ball

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
