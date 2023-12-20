d0da889b8ef035867e6dc467b325c0f659651810
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:17 +0000

    right sleep drink flight unlucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
