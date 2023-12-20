aa40d7e72c21e4936519f4a053049b93f2a4e98f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:27:04 +0000

    practise contrast insist bite mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
