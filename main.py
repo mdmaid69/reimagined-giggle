738bb2dbbf5422ea19e50d936bc38b12e5b6d690
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:08 +0000

    favourite occur visit hear she

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+print([x**2 for x in range(10)])
+import array
+def get_array_as_str(array):
+        return str(array)
