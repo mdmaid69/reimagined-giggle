d143a8e0fa1635718dda071760296fc414f69807
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:37 +0000

    week moon song gate name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def find_max(numbers):
+        return max(numbers)
