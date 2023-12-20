c00b3f5b4426e651b62bd911048c5bea09fca5d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:15 +0000

    complete turnaway great receive tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
