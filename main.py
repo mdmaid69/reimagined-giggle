90dc709566a46ba928158402af88c07e6b10ed5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:00 +0000

    floor agree protest recommend future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
