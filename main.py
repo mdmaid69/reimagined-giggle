0a71adefd423bbead9cf401e474261745b0fb041
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:23:29 +0000

    centre mother connection interfere then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
