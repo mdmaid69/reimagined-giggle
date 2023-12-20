76a79c14bfd7a0f09b35a0786fdf326820075b38
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:25:50 +0000

    extremely anxious below spot anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
