842942075dc4659d4e3f556d80538b56f5ccd6a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:41:29 +0000

    direction mine from Bb Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_current_working_directory():
+        return os.getcwd()
+  def is_odd(n):
+        return n % 2 != 0
