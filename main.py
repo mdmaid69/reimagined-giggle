a6bd8ed1b826c6a61c6d1cbd753516297959c09a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:47 +0000

    come lend house sick work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
