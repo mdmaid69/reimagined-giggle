c0edfc2747c221892488096953a148d550db1726
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:46 +0000

    order air surprised its swing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
