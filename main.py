2e5784776fedb945b73512e3a23536751acbbba1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:10 +0000

    holdout pour convenient consist strict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
