05b0104ac7405f4a4c317736c100931cf1c130dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:00 +0000

    course wrong interview matter surprising

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
