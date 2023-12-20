bddbf88f3770043f2f4be4ed7f3fbf46b5d888b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:39 +0000

    string you distant find somebody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
