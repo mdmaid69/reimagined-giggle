5b704935ebdff77d1c6ccb3d9b627edfa43f1861
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:34:21 +0000

    basic live strong closed film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
