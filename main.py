96011f856112782cce1ef83f39ddc2d913063f8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:12 +0000

    hope traffic form snow cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
