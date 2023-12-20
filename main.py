db3c5b6dc70d86d10a16ca7a0740efb434acb61a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:17:28 +0000

    negative mind clear custom trial

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-  def cube_number(x):
-        return x**3
-  def subtract_numbers(x, y):
-        return x - y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
