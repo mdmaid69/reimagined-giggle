d4dae5dd1257c246434cdb16514e95cc48258fcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:06:11 +0000

    secret supply container likely on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def square_number(x):
-        return x**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
