47e729755e0b1c300b9214c59cbc802d5b435391
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:26 +0000

    neither trick search mean positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
