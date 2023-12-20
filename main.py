d47ef5d24a2082dd0df1de61335494416704def5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:54 +0000

    dear 564encourage whatever disappear arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
