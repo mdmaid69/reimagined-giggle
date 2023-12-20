1cbab4872a12eaecab4590085688f34640045d23
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:43:56 +0000

    pencil burst sit substance flood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
