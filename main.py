39e353c98e6db767e8fb109aaa5523fcb6ac57c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:21 +0000

    in we uponon least base

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
