4a803f56096a55413c045d54a5259206ed5787a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:41 +0000

    tap chance flood takeover none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
