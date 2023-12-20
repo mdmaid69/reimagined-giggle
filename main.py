bb04a51a9d44bf19aa807037f2029ddf7e0124b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:52 +0000

    neither trick now extra already

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def reverse_list(lst):
-        return lst[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
+def divide_numbers(x, y):
+        return x / y
