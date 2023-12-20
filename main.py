78f3e9916c4782566d59af875cb3cf6a50d55847
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:38 +0000

    no one add answer pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_density(mass, volume):
+        return mass / volume
