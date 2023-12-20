c4d21441add486b45a558ae5e6e8159c976ae364
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:15 +0000

    crowd meet him section shock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
