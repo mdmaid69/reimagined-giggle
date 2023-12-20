3e397f10b9946d50e0db235c9bd04c86b2bfac14
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:49 +0000

    shopping treatment evening call difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
