c913a377f8dd70ec628badc59e0c19fcb782352e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:19 +0000

    share yourself 566end Ww sensitive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
