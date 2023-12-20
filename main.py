5d7d2dba8df13c94f83b6b64be78118bf0904d64
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:05:01 +0000

    bright wise unable cheap success

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
