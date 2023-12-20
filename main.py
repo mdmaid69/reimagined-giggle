46a2f41e750e2f49b7129e7b41776cdf68de821d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:18 +0000

    wait steam break big excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
