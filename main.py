1058ef87478e0bf7e3ff1fc5382f67107425c858
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:22:24 +0000

    flat polite almost possibly half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import collections
-def create_user_list():
-        return collections.UserList()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
