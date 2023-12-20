ac7a5409f801cc2f4e4655d04bad1ae5c135f782
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:25 +0000

    quality message callup stamp swallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+name = "Python"
+print("Hello,", name)
