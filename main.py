823cd5161cf7e7bee681288fa8752a9dc8e24504
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:54 +0000

    571enough strike mention cross smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
