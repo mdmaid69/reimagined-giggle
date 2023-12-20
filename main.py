91e2fb2d4c8b91f5b6b27c81dbc75a172e139579
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:15 +0000

    will boil profit dream justice

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
