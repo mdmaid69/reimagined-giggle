450bad1b367eea77f586e2522574523228129e14
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:02 +0000

    vision walk getback unkind trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
