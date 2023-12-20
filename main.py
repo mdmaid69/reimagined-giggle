cd180219f2ba263b7da3a852e0db5d6757a2fdf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:51 +0000

    market class practise appearance worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
