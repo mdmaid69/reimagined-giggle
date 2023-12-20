601670bc8da21238729fc39f296be6d27f8e3340
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:26 +0000

    maximum use front slice list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
