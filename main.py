f0072b2ff5d0baff9515f6427c369f7591ebe301
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:16 +0000

    sink strange guard lie anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
