935c59ce8054943a1c4c71ce9f6c94db518ca53d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:58:45 +0000

    amount display spill kind giveaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
