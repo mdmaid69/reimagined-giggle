6dcb1646f139d1fda86848f834ed7e487b57f2e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:51:29 +0000

    gather wear about next burn

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
+for i in range(10): print(i)
