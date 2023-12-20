24a8c465bdaf022f311d8707be72ca10b3c4b350
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:16:28 +0000

    tend Dd familiar even wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
