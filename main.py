af7c7b9ad79801500d68af5fcd7a0993ef802111
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:13 +0000

    temper appeal badly order wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def exit_program():
+        sys.exit()
