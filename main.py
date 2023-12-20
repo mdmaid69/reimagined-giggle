84eaea10589ca19c8ac295ef76f4d787a14292a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:34 +0000

    stranger float mysterious music bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
