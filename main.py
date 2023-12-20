01e5a939ceb0f12b1baf4b01ff73529585fe2694
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:29 +0000

    workupto lay team Uu spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
