edfac0c2b124486aac6df099891f76d78f1fe199
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:04 +0000

    actually crash reduction swallow upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
