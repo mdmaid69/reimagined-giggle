862a9daa0b2f154e62233393e2c7f6e425f32249
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:31:55 +0000

    frame fasten noise seat come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
