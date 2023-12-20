e03dbdd5460dc251e7a75b789603e9928760cb43
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:56 +0000

    light clear sight minority invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
