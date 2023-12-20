60787710875ae9241543784c3f380a690185d3bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:30 +0000

    noone uncertain 570enjoyable comeout action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
