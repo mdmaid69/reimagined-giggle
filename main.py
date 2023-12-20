59279c716bc1e034437085d3bfbb46a14ff7fb6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:07 +0000

    advice store finger before characteristic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def is_odd(n):
+        return n % 2 != 0
