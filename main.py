1f30b222fdad28b0922a4a4d02c14ce2614a6257
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:31 +0000

    identify minor real lead Mm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
