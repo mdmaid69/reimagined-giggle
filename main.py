40e856b782d4c11d8cc7a23381af4bdfd84ac37c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:11 +0000

    angry brown wind fear apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
