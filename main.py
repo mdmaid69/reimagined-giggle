0f9ebb8a8e677ce329b271f0737eabf36ba8d712
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:50 +0000

    middle surprise same compete few

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
