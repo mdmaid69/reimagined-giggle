378fbe7a74b738b2949ec343505fe519097376a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:07 +0000

    drive prevent kind intelligent wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import time
+def get_time_since_epoch():
+        return time.time()
