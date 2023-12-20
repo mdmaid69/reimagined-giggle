eac585bb3de7770b307c613bf84067d81aedba92
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:52 +0000

    workupto fetch close recover wait

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
