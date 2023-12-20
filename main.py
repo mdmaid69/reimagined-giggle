a7c9f6b6a555565991dc6e417f99c2ae11eb3180
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:02 +0000

    ashamed could nature surprising must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
