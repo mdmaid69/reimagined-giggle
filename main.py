840637b5d5bd27d5653f97e9666bf356c73888d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:22 +0000

    nonsense human yes various emphasis

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+print("Hello, world!")
