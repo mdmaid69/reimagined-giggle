4300c3eced0ae9ae41c0c602555f8834df0f5f49
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:30 +0000

    similar prove influence persuade cheap

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import logging
-def log_message(message):
-        logging.info(message)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
