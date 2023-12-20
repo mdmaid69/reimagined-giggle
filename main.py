729a737df358ab64c9353a3d1110102703d006cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:26:23 +0000

    purpose please best disappear disgusting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def reverse_string(s):
+        return s[::-1]
