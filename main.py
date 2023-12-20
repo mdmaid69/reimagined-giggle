516dcdbbe25f957fe438e29715a948f7d8d0fb0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:39 +0000

    refer fill concentration move final

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
