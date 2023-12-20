576962740a6abb00155b63bb59a70fba81a12d4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:09 +0000

    slip shadow burn comeoff letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
