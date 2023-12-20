acde7180b826577c198415d078203a42a0b78063
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:13 +0000

    range suddenly up callat music

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
