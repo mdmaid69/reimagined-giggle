7404996e2fcd72ab374529f50f9c527e7ab2dcf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:03 +0000

    Jj Ii dream murder price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
