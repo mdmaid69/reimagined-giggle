59f75a7d0a80595edec91108c465b99efc67b579
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:43:11 +0000

    apply practical kind successful bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_min(lst):
+        return min(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
