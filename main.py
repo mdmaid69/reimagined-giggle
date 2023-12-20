e41eb09261021059cc30d8f5f0ed23b523ad7a5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:59 +0000

    lock table through discussion fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def cube_number(x):
+        return x**3
