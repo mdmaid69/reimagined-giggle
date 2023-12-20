527228af526bcccf1ba69c78ae21b753dde05773
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:28 +0000

    accidental by rough agree suit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
