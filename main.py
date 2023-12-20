d8c4c12283404de1d2bddd401c2b42d1510a3d70
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:58 +0000

    measure various convenient storm across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
