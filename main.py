bc42798d608012a896673d03bfe83de8ac6161a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:07 +0000

    interview Hh divide tend image

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
