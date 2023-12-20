dfe5bd7875cc30fc9ff86eff562524ac4f4ae9aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:10:49 +0000

    offer see week march top

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
