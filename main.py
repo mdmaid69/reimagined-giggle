9af497b103961f7f02bbb976cdf86d9cea34ff69
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:42 +0000

    determined with post involve art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
