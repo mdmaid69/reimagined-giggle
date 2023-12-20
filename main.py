63ee4d6ae271b7ace8f41330605421b521faf5e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:02 +0000

    exercise survive regular hurt during

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
