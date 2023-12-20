2f1a6f0f58d316b6ed40a3d8b5e01e405ffa1f64
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:41 +0000

    sense appear lonely ground tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
