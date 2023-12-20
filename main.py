68fbe4363b9035dad3095d5cf01d01fccb050b6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:45 +0000

    year translate string involve 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
