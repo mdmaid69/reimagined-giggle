f683f3345d3cc81b0dcb194e69607b350533f938
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:56 +0000

    forgive gentle other habit tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
