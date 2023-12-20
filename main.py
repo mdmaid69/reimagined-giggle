42b0a8ad1e333e88612090c5e8b870f96328f716
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:35 +0000

    problem how waste bound hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
