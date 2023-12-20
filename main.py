f85047df636f63f60604015465f91f8ef3f24ff1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:06 +0000

    slight spare warm union fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
