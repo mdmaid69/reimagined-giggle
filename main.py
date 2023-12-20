a8d76ef8ada64f600cc7308f042edd701c39b826
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:07 +0000

    rescue pretty characteristic noisy country

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+print(sum(range(10)))
