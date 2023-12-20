5e04bc7beb3eca1c44826ff4969d13285e118d88
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:35 +0000

    disapprove tidy reply treat eye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
