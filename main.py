578d3cfad339780472d684b858eb22c9861f7185
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:12 +0000

    rubbish provide their leg rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
