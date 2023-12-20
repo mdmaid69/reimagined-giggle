2b306a460b9a35cd35762087841535fed98cd0fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:57:37 +0000

    value mention trouble concerned consult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def append_to_array(array, item):
-        array.append(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
