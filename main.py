56f233c09c258a6aa3a38337ed46cc35385d84b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:13:58 +0000

    quiet hurry listen grand instruction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
