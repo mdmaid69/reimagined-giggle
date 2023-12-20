fb66f3e9fb218dfb0a891913e85453cd7761ebbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:10 +0000

    range before any pay human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
