a9360d6cc699a082288e414ac530b19ec6e7722f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:15 +0000

    intelligent another favour where left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_area_rectangle(l, w):
+        return l * w
