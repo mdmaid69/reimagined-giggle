0d4706fb92a0bf855ac6fb76b8b69f33fab0804b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:50 +0000

    behaviour thanks us try throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
