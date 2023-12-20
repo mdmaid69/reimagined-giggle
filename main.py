511882d063cf2dd865eac82eb991459bc7cebc56
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:19 +0000

    wrong imagine somebody fall plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import re
+print(re.match("h.*o", "hello world"))
