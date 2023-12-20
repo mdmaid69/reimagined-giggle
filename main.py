babcfe520aaa199c861de5f1c5d3db9888690a9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:57 +0000

    taste advise form Yy away

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
