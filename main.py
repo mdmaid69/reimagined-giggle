ba01fe0fed5506e6438afdafb1ff909d2cb7098a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:30 +0000

    question minority fire nerve reasonable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def count_words(sentence):
+        return len(sentence.split())
