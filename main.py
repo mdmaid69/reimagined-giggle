cd548ea425836659945a7c77cba441790bf7db9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:09 +0000

    square legal dull education finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
