3d76805144755c0991913014781f36e1d4c7a94c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:28 +0000

    that pool professional power large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
