4808f44943a6d4f2dce9bda92dd41ac1304f0cf2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:12 +0000

    prize awake learn collection close

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
