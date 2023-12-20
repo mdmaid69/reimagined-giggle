18107dd2d38afd592e50abbc98496e430ec943ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:02 +0000

    drink fear 570enjoyable beat during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
