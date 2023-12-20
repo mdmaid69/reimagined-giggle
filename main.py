482a613fe37a6af63fe6cecf93f09c325ed3caa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:16:14 +0000

    blame dinner complicated at will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
