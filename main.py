97a0f1f885431e49e8060a819f9efb0935c07915
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:11 +0000

    move public provide phone mainly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
