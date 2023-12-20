451fce5aca4e596f75e66e015a549dd308f94685
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:02 +0000

    cut completely remove squeeze level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
