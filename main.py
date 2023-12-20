6787b819654964aa8808d1dc583b434714bca181
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:40 +0000

    material air bush straight attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
