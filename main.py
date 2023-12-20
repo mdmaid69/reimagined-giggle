762ffdcab79ec02a9a8d212b83617091e6cc2753
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:33 +0000

    influence dry question priority error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
+def square_number(x):
+        return x**2
