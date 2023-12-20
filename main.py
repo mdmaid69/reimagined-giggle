d80af9bcd1bfb8979ab0750c90006e7d51a57994
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:43:22 +0000

    confident cool mean doctor large

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
