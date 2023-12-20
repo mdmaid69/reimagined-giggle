44cfff768cebfb9f484c16b8423daccb0a816dbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:26 +0000

    near wild performance clear substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
