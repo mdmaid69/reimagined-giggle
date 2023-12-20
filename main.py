5692be564e5fb47215097790428d04ed67fc2ba3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:01:36 +0000

    worse avoid behaviour recognize somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def add_numbers(a, b):
+        return a + b
