406502305f0b6e7e10b97f9d3fb796278ce05bf8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:26 +0000

    realize tear accept on claim

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
