9ab7f3d7602d213df0b45c82ca10a94c7c4f46f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:02 +0000

    question absolutely present admit hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+print([x**2 for x in range(10)])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
