cadf430007c2eaaca212bf4d374c2b02953a9b6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:58 +0000

    anyone front serve cut scene

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
