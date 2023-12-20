53db777b0fbf7618711787e581d77bf938a824d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:49 +0000

    gather beyond train quarter into

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
