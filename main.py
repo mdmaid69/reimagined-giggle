1779f94e8423a5d6aa7f00c36bdcc4e802d97e92
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:14 +0000

    lesson file free thought turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
