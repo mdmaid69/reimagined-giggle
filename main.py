7aec24fe39250855f60587a771bdac5d8bda3266
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:55 +0000

    check name appeal save string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
