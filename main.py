66d50488e5d2722e1cb4e76f1b421f0995da26d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:47 +0000

    lookaround rescue offer word mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
