9ed64ebada3a3e092806a78bc804565e4a6e04eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:45 +0000

    put breath stretch gooff powerful

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_str(array):
+        return str(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
