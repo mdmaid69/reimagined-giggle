2a775b4e6adf2a23eba9fb31d9b4c93385d41fc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:10 +0000

    commit remove reply rescue answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
