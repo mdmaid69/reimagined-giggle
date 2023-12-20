09342aa3eb905905f6e0dcf703700a81049ece09
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:43 +0000

    left tongue loan workon leg

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
