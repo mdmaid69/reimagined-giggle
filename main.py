f84ee602fdaefcfe9abf230005335b42494296a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:18 +0000

    dig search change must bringback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_force(mass, acceleration):
+        return mass * acceleration
