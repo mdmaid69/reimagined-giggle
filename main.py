379b1b557a513473b155bfb88841f8534002d450
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:19 +0000

    quickly ride tomorrow plan take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_cosine(x):
+        return math.cos(x)
