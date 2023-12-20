a6a36227e5e9b9f14239febdbd4b10eb398a9cf4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:07 +0000

    OKorokay holdout loose basis raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
