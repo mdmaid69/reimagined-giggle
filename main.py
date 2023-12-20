2f773475431859cb04935fc43b3173dd013657d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:59 +0000

    179bite himself care responsibility love

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import collections
-def create_user_string():
-        return collections.UserString()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tensorflow as tf
+print(tf.__version__)
