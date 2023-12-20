9141ddc0e0c7340caf49b9e9eeb3d237bb6ccb9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:46 +0000

    fall comeover not let stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
