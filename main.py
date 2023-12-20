becf2f744a310f71e18dd42dc19680aeb9711535
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:28 +0000

    tomorrow read march 573entertainment play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import random
+def generate_random_number(start, end):
         return random.randint(start, end)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
