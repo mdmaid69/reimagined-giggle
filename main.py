c902a35779324ecccda6a236c4a05fa9e99c6c1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:09:45 +0000

    table size cry nor temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
