56cf08358788b7d862a0173284d3fb016e40a265
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:31 +0000

    appeal ahead pen art clothes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_length(array):
-        return len(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
