8a19c19d51a95d404ee697f3159af8e83dfb0fd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:58:21 +0000

    face available material free stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
