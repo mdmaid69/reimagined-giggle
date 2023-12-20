79476cee3fef75dc1a61ff0f304ac7c1d35c98f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:24 +0000

    various Oo error yourself service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_list_from_array(array):
+        return array.tolist()
