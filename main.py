85a5d172f633650ffac57edb47324ef5e1c9f263
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:44 +0000

    our somehow frightened tear part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
