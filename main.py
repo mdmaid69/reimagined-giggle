aaa0d35d3fc6e7ba69be2c22e59f6925b86dd591
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:07 +0000

    clear among refer loss oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
