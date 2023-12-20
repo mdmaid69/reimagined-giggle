1fe9d0263c03c2c7e023bd2150cc615d460c2a82
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:26 +0000

    recovery relative school but fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
