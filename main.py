1c88152c7083a50c837cf8f64aa50b12c56b7e12
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:39 +0000

    pleasure office convenient lift let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
