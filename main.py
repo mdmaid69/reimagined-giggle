e4d81d76a2f4b4f52e3156648fa5ff3a33268b37
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:24:17 +0000

    without boil ready especially round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_bool(array):
+        return bool(array)
