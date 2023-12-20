2819658dcd4ce089c674b6f9b3738a88d3349bdf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:40:17 +0000

    hang market last story meet

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_user_string():
+        return collections.UserString()
