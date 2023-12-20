ed3e53c8b2b52a568183e6dbcd78832178377c67
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:58 +0000

    house which speak pinch impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
