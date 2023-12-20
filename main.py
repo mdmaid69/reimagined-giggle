2c528860a2c9add9b70dbe4a7477945f262acb95
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:02 +0000

    increase estimate satisfactory to shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
