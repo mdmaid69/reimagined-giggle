4c18a96addbb1973aa380eb4d6b21c0d9b9a99f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:44 +0000

    class delicate amount open giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
