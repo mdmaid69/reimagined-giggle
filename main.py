6b80e67ebf206dbd64e8789420b011b6d1400197
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:53 +0000

    like group friendly brief alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_list_from_array(array):
+        return array.tolist()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
