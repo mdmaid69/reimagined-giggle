18c9ae8cb50cf575216ec25e51d5189b2fff280c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:59 +0000

    building none certainly nervous pause

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def square_number(x):
+        return x**2
+  def convert_to_hex(n):
+        return hex(n)
