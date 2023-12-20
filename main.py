b8c4c7f577e25eb398f32c82f563468d50a6f643
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:24 +0000

    picture getback impressed full this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
