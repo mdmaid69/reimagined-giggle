762e6b3d9e7a839a4ae5b439131b6d848dc70299
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:21:13 +0000

    ask along if embarrassing offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_force(mass, acceleration):
+        return mass * acceleration
