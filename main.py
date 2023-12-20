4ff4fdaa16a361b4e129a6961d688c5e275734b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:19 +0000

    please his petrol example appear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
