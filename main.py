833e6f4c3185b0b0dfca6824de4e22f5e50f223b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:42 +0000

    spray relationship ilovedicks invite over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
