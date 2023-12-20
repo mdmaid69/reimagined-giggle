44ea1731ac29919210ad84a9f7da67205ed6e180
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:24 +0000

    fashion reply kind visit married

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
