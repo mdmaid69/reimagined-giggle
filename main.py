0d951a18fa2cd305582747990ca303b4a8f59f10
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:23 +0000

    party tense terrible expression bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
