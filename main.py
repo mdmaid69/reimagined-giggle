17ec071299190df5c86c175d88a123bb70039da0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:47 +0000

    cut test thanks mystery 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def count_elements(lst):
+        return len(lst)
