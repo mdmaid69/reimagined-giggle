31faffef371c575ad34f3d5c04af0557f2f11a61
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:22 +0000

    obtain recently getout accidental object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
