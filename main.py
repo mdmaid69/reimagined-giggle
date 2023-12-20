087a385eaf5e39896d4de49163af32c688f7e4b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:23:47 +0000

    Kk this nonsense bother see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
