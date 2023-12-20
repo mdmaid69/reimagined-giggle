30720146383e0fd96c3f87abd9c8dced491e26b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:32 +0000

    expect invitation custom use door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def sort_list(lst):
-        return sorted(lst)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+print(os.getcwd())
