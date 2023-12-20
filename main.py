3ff9f319d7c8457b77da3fe8d312238779072425
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:10 +0000

    basic emphasize nor poison fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
