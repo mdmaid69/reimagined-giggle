bc76be79492df1d977e5528e9eadaaab266fa434
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:59 +0000

    attempt organization star fine figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
