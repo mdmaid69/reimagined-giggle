40b2ed62038eacca39e07b6fc93cf6d005d809ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:07 +0000

    show walk unlucky straight innocent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
