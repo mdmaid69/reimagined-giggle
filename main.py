65bda1939d796a7de06d82367a1e68ffb3e616fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:06:05 +0000

    loan approve attitude feel inside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
