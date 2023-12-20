fef7088be0efa14c6fec1b38d574ad16f98bb03b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:48 +0000

    left dig notice constantly branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
