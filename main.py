55dbcbebd528f1df775eece71e695d3404ca2e81
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:54:56 +0000

    concern scheme load splash early

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_speed(distance, time):
+        return distance / time
