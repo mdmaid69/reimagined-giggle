be7431da4cb983fe372a9fcf789c349e41097e68
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:20 +0000

    my beyond 179bite healthy basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
