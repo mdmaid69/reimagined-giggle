373e6e1c17269166bb62d73afc540ab113c47ed6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:42 +0000

    remain space action quick argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
