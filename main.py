92a51e9fd65fab206b85054613cd6b99a2f613b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:35 +0000

    list brown ability contact pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def square_number(x):
-        return x**2
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
