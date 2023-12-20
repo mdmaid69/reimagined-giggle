ecd35449dd2bcaedf7a50aff40fb024b87984b9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:45 +0000

    joke far wing when able

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
