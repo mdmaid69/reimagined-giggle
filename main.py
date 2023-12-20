27257f4d411739f462abdcabdd0d8cf62780c149
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:26 +0000

    onto immediately lost right godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
