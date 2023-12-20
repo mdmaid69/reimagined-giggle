c261143bfb6db37f68a664bfb416ad92c41b3780
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:30:39 +0000

    open contrast again limit film

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
