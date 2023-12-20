8710a23ceb0cef1f57c5ad8561fa7698868102e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:46 +0000

    butter order reach play neat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
