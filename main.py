49117e99457b2a60399ab4ced30cd3b20fa02a69
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:22 +0000

    interfere perfect Tt journey slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
