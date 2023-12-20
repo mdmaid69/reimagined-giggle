2635b0f46a28c0042b36d3fe72464bad6e2ed8eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:11 +0000

    exhibition let lead guide round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
