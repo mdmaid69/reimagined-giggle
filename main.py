7da82d1a5b0ff1c4c740e2e4aa7fc70294ce1868
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:43:49 +0000

    violence Zz invitation ignore slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
