26b1e6adfd683f5523c405ec59761df06ed74f0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:09 +0000

    charge meeting afraid exaggerate busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
