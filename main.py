908ca4b302ae43f1fc7f97083bf68e63a97f6633
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:34:01 +0000

    below translate stand apart suffer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-name = "Python"
-print("Hello,", name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
