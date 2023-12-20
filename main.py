f02ea2f14f9bd33ec25e1ace099a6547e3aa14f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:13 +0000

    new settle pretend education foot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def sort_list(lst):
+        return sorted(lst)
