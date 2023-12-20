c249fd4859542b62783361bc361afcdbf294e8f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:40 +0000

    secret tidy anywhere part lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
