4ad353c0a08754fc117f7b8ac595b02d896fa771
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:41 +0000

    love separate deep address means

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
