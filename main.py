1cc49e2340585964cb996550515909e85d12a53d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:46 +0000

    stiff like machine respect touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
