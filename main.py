503f977395cedaf73cc67c79b300a1ce5e2a6178
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:07 +0000

    frightened expression language measure might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
