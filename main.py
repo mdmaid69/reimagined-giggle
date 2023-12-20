e90fcfd749f7029bec09f03f230100604647c602
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:20:16 +0000

    show something error hers content

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
