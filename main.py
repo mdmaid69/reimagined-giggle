dc6ee653f5a6ac03d7b97ceb1c5311873c5cf25c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:56 +0000

    final away pattern interview judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
