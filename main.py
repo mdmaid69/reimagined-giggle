7fbe7d5090032a8619f6371d1e23621878d06cd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:35:45 +0000

    praise child confuse woman money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
