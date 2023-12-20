a5e44011e49bb321e3537503c44429d78b5e082c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:18:49 +0000

    gentle ring habit solve buy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
