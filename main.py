964a1273ca28987e064d8778b099dfb799865ba0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:20 +0000

    single agreement introduce cum whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-print(os.getcwd())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
