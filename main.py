fb2c0a0b26f1fc22e91050e5291f7659bc02a55d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:00 +0000

    avoid church sky Uu unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
