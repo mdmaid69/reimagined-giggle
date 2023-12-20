6029d397d79c433adf1a0e1ee58e60f641fa47ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:10 +0000

    tone warning fast answer cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_cosine(x):
+        return math.cos(x)
