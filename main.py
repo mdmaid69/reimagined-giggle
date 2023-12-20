8762363a625b07202c1bfead84d4fd2fbfcaafbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:56:25 +0000

    cover show reliable present talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
