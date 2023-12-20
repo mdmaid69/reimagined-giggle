9c410818a5a81496dbbc1a55c198dd983c90d3da
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:28 +0000

    but only strike regularly guess

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
