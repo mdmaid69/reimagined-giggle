0c8a7bea7a4136dae808eeeab3b9f02821407479
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:59 +0000

    disapprove notice switch price science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
