7a767971212f24c3f2e2b265f1c38adc873a0972
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:23 +0000

    woman performance something Gg breakin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import logging
+def log_message(message):
+        logging.info(message)
