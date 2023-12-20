c4d3d98b58c262c01fd1227bbaffa19519aabe3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:15 +0000

    do rent become instrument hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
