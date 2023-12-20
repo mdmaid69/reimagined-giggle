c0c5dbbd3adb804a65e50a76075f9b7aa99389ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:41 +0000

    apply will become come scream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
