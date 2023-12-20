0a08d983ba25b2f17ac080bd7beaa51eb8c6a861
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:05:51 +0000

    quickly me mystery character black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_as_set(array):
+        return set(array)
+def convert_to_hex(n):
+        return hex(n)
