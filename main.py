ace5395f2a24792721d01937c198a8ecabd8124a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:49 +0000

    fashion importance shelter start bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
