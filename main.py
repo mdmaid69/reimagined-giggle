5d9d0ff58eb082c7e374d7cdff83664f35a24340
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:54 +0000

    gap memory guide study scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
