235d0a46cb4d0b7d57938e8872ac0cf2826796f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:34 +0000

    lookup recover splash standard breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
