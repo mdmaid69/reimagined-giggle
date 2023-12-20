2cd990e8dcb43f675221966f928840d1bcf80eb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:24 +0000

    replace protect survive party match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
