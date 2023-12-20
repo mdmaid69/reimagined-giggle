998c182325231ab4733252394d4c978271056c7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:03:02 +0000

    form design doctor event fairly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
