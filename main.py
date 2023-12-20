db2e8ed89d43b0172d2e22045c249898a0db902c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:10 +0000

    inquire impossible instrument key follow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
