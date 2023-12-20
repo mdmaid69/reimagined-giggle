311463b7e8413a3209fb2a3908b10f747f4ba0ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:39 +0000

    aware book kill begin admit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
