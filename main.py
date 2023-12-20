966404fb50298fb048807ae6f15064a01c2dfaf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:14 +0000

    interview pollution another deliberately nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
