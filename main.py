16238cad805f9a62b0acbbe78b2126ce7a4e0ad8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:10 +0000

    famous music cheque talkoutof pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
