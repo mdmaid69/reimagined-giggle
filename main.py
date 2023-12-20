455483c999e94188a04e3b690626ee6a794f2a89
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:19:17 +0000

    injury hold doubt across actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
