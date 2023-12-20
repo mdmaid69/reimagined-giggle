65eceb9997a58140afd23d324d2f49c7812b574b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:18 +0000

    patience than shopping winter notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def reverse_array(array):
-        array.reverse()
+def get_list_from_array(array):
+        return array.tolist()
