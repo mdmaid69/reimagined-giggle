534eaa5629ee946ba47c372129ea46bb0844ca81
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:26 +0000

    serve rain anywayanyhow now stretch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
