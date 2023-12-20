f577ca1ec7407c36b877d3e2bf522d0dc47525f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:20 +0000

    deal runout warm mean throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def print_python_version():
+        print(sys.version)
