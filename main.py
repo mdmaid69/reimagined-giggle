d0d96968637fba4d6fbe0e82daec01d755199050
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:30 +0000

    achievement hot something same everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
