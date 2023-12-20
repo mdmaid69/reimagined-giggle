dbe1a4befd793e1ef05c7116f189433acecd22fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:38 +0000

    progress still sign lay building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_dict():
+        return collections.UserDict()
