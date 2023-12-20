574591e9b8ce9309e0a1ca8b5f28ca3899129b57
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:09 +0000

    sign print sad furniture 572enter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_typecode(array):
+        return array.typecode
