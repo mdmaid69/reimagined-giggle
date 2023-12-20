4f9916855bd5ee29e41ed896957dc9ec611178cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:30 +0000

    plain row 567enemy sleep follow

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
