1c571f10f5242765ad365fe9091d93a6e8107052
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:11 +0000

    indeed official surprising workout sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
