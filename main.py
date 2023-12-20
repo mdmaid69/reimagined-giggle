827c47e7a57fa88c45f0478495ad8513d37f7064
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:24 +0000

    beyond previous anybody pencil general

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
