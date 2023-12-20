98139c2c1fde0d6de71a4a951405b94406e4bd73
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:07:57 +0000

    detail far class agree prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
