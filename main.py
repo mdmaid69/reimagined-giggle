3d8a4f294c018aa0d58fbb569816530deeebe94e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:24 +0000

    secret start flavour next single

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def convert_to_octal(n):
+        return oct(n)
