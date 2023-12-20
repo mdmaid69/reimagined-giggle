95533c83bac4d8c0804c5b99618ca7eeb959891a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:15 +0000

    lesson pain copy cool doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
