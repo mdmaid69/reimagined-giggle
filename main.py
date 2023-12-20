13089b0842e571aa68c913866dbf16558c452d64
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:37:36 +0000

    noise lie walk sight reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_tuple(array):
+        return tuple(array)
