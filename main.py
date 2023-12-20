f5c86b601388804358e3af106f02179eec8150dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:19 +0000

    keen maintain tall account entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
