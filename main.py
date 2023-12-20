46093e596c1450eee446c4a07942e341ea5951cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:43 +0000

    though inquire steep private artificial

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def count_elements(lst):
+        return len(lst)
+def count_characters(sentence):
+        return len(sentence)
