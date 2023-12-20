1ebcfe77b246e2dd4b78a6fff4c1ecece0a50682
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:00 +0000

    centre typical list operate throughout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
