07fcf34d8addfbb93105ba632d2a99e4d020a3cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:53 +0000

    lookback(on) beautiful who tired sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
