395b777d4f1178a33a6de80325fe81f66e4c6edd
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:54 +0000

    stiff proud content lonely giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
