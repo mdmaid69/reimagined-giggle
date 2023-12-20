cf767c28f9f30944e5b9206aaccaf38b2c7a3a42
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:06 +0000

    learn proud keep before damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
