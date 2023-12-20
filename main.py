6f34f6bcfb5b9dea52b5907e22ad0b72be8ed523
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:58 +0000

    comeup stamp takeaway quit promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
