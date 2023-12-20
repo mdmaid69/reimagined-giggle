5b311992504fad77850ccd3b6d8877d5d2e14a69
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:13 +0000

    because as within related odd

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
