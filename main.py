5d2504274460349fb49616758577f62fcfc65677
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:41 +0000

    Ee awake deliberate cheek especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
