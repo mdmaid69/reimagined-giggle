b888e8cd105a566c0ced239e3756dd4eb5b9bb52
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:05 +0000

    use talkinto race become make

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
