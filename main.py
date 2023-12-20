16bb898786d62ebe50f500f6846d9bd509ebdb61
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:11 +0000

    giveout trip ashamed disturb judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
