9e127de5eb85760c06449176c833d3d94c8ac212
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:52 +0000

    easy long ground criticize Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
