3dfca1e9555835f87df581e8e83b05cecb64c21e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:42 +0000

    recent cheek better pen big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
