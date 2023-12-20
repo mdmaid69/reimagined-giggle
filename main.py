1a0b030bd01441268ce1f488e00c5d270fcfdb5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:27:08 +0000

    serve field shine unhappy sell

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
