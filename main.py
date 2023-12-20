387c4d3a093f2cbd3f7347ca46acad0378c5d0ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:16 +0000

    Ff about rubbish bush gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_complex(array):
+        return complex(array[0])
