67580dc1fa8affb1fb397767d2a8f68efce34677
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:53 +0000

    level basis embarrassing imagination row

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
