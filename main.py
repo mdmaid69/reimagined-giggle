965cd6084a9df9173df30d82858285bb86cec3e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:25 +0000

    state peace book allow rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
