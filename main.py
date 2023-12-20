30421e7e557fb58f1fe61156f9166b1fc1c85514
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:43:03 +0000

    evidence pronunciation design pipe everywhere

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
