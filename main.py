388bf2f0fcfc9d14f05026378a06cbdfe5ac8f8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:57 +0000

    side point plastic number build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
