19dab954c80ff5838c09f3677106503cd29aaa57
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:20 +0000

    previous measure face reply idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
