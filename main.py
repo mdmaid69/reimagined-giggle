2178250beaca246ca43b4a5137381760e71d1487
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:58 +0000

    innocent sport display sail tidy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
