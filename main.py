4cd454e3ae1cece856c11d238fde3875db82a200
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:33 +0000

    usually children fetch same area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
