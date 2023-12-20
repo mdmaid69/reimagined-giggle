2b99870140d8f7816bfce499186250450a888780
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:17 +0000

    indeed before bill unlucky key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
