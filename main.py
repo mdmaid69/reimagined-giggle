b90ab3ead7dddae1063dd0ff24c04a08cdb42ac8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:41 +0000

    disturb Mm considering once advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return math.factorial(n)
