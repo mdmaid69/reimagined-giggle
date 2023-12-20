91b04fc60e3edd23eb1d004468592295c83fa0f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:48 +0000

    want warning between hour stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
