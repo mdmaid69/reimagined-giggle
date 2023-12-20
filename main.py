a649418fc9d3db8f2b3d0745a586f92630215b7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:17 +0000

    wide house along tense match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tensorflow as tf
-print(tf.__version__)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
