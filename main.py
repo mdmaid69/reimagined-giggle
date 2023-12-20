aece4b6efa6d970728117f2873e7df1ac3ef68cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:42 +0000

    market closely just whom everybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
