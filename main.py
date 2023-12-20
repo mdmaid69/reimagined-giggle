06604bbcecf932742d03c37b82772f337dd9fbea
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:31 +0000

    spray me blow chat for

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
