0ce90d3f70a2e9d418bf01d21187378bf3544a1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:44 +0000

    loud dinner speed plate hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
