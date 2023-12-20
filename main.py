07de2d57dcadd83697227f01886b17b41a40a017
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:34 +0000

    recommend arrangement why flow trial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
