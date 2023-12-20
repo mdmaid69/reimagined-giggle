6208e66c08933d0c3b7fdddd4192a5dc5ebe421b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:13 +0000

    completely delicate head your direction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def count_characters(sentence):
+        return len(sentence)
