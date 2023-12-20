37b1d29387954fe526b59b8d238ea1d541d4361a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:01 +0000

    lonely squeeze tool term physical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
