6eb29c3bcd508362ebf3f79d9273a904ac9856a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:58 +0000

    wash boat poison discussion change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
