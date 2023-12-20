59c3bdf046e7ed5f24bb313a30f58f4cf6afc30d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:32 +0000

    prefer theirs tread only still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def count_elements(lst):
-        return len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
