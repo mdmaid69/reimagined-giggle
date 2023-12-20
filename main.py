3b3828fc95c52a33978a60b7cd8035602dd2ad24
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:10 +0000

    house strange uniform poison healthy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-print([x**2 for x in range(10)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
