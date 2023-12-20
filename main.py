607068fa04b42c14392226acfd248495480ea7fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:17 +0000

    give deliberately attract yesterday seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
