7fd2aec4f148a2aac4460c809871720cf05fd433
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:56 +0000

    badly answer tidy pile university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
