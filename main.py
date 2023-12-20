9a04645e968191413931ce816481e7a4e25c3f8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:09 +0000

    original possibility stay seat picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
