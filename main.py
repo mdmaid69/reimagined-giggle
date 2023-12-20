c221240bcad5444a69bc7d68ce6065d5a03dc9bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:06 +0000

    makeup bag worth large Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
