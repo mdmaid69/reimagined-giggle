65be36cc73b94da2f49de544665fff89fdb38980
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:38 +0000

    sign course pencil busy flood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
