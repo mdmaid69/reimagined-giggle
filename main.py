007c6268bbbe2666f01c3545363cfef5f6bfd518
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:09:45 +0000

    suck disappointed object race can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def greet(name):
+        print(f"Hello, {name}!")
