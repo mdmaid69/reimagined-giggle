68b2df06eb737353c147c9fff28a3d9daee33728
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:45 +0000

    protection thankyou effect type recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
