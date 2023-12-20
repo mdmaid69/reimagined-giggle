81947fcb03fcfbcca998106d292b67a44d92be62
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:29 +0000

    further play listen whatever trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
