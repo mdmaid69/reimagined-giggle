860d37af1a7f872700c5c45fbeabfa5d005629ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:37 +0000

    Kk 568energy plant can mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
