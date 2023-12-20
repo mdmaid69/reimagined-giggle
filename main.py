0fa8c8e53f50c9d070b8f5ea19356a39bf992a63
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:36 +0000

    correct advanced put telephone spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def square_number(x):
+        return x**2
