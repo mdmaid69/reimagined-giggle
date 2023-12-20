ad6a224bf26cf8146f22175c5f3c585512b60b60
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:35 +0000

    react theirs bet succeed public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
