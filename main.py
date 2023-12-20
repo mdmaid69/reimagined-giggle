4623bba39b471a065e05b8721be752e21263fb62
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:43 +0000

    official properly correct value first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
