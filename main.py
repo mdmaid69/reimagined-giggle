e75c277795c47b1affca07b5b24e573f72803416
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:35 +0000

    start future 571enough responsible stare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
