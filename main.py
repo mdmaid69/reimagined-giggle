61ce047e6d4787e52148f3bd2368f8052ba07194
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:03 +0000

    step voice value record frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
