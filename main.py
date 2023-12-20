3a20218fdb6925a411966af08eab176f588177d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:54 +0000

    afraid soon spot born dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
