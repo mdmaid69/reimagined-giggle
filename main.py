ce925bb87c3701cfcb23588f6fd85ee940bac94e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:36 +0000

    journey lock measure corner middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
