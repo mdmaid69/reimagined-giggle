6c32b9cb138688bd369ada4146de6980d5af2a37
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:45 +0000

    active repeat disappointed sorry before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
