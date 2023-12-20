fbb3a34fa72cb7d7ef4c5f920fc7641ecaeb765b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:22 +0000

    getup degree meeting public blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
+  import sys
+  def get_python_version():
+        return sys.version
