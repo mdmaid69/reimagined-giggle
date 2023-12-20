3ef25c65ea1a319c8833430a22e4db937fc896c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:15 +0000

    list so new step ignore

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
