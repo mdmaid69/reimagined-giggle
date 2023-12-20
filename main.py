2dac68f40738af24ad33e3acc60a5621997f28af
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:38 +0000

    satisfactory doubt forgive identify children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
