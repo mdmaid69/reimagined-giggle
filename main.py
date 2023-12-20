00be3b5025a230afeeca56a0c65e34322e35d96f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:09 +0000

    importance new tight change many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
