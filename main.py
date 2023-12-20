084627d64a28d65e163efc10314e6ae363611351
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:52 +0000

    indeed comfort also count favour

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
