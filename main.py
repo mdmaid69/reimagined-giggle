eb5fe91567a0821b77cfbb2c7a62a672c3d99113
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:06 +0000

    wrong fun event breakup owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
