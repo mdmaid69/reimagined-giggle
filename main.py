b546003302a5cc21def1491e65ebca15bba5ba82
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:31 +0000

    performance he delight sign name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_exponential(x):
+        return math.exp(x)
