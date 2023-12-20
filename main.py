200e868e383105f182512e9a54ab365235460439
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:52 +0000

    crowded matter worth hurry unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
