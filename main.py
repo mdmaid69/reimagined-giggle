be90e25ff11e512ad9afcc54524668b698cb9217
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:23 +0000

    notice doubt heart goodbye light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
