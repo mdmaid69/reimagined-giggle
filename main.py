f6cf4bc3413d540ac746ee56025117ddaebd48d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:51:08 +0000

    improve no base unhappy reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
