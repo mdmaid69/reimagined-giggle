18a2a12ad32c26c741fc61712d8fc84762e18cb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:21 +0000

    sting slip lower soil either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def subtract_numbers(x, y):
+        return x - y
