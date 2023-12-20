8e924b42fb6596ed4042b8ce2a2e1e1b6ccaf9f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:35 +0000

    ground relative rough project achievement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
