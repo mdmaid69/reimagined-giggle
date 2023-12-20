5a370bd46c3c06a6707cf02aadd2f0029e516286
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:12:34 +0000

    aim also finally basic church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
