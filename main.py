19ff14843175b8f473fbbafe3eef9be7e024ec23
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:15:09 +0000

    rubbish pronunciation guess angry then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
