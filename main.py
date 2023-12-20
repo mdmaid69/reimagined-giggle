cd550506701a20d31cf2a70a466e547148ec3067
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:09 +0000

    compete bringback hard main sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
