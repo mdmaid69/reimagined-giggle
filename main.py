101b2c5665940792e27e2913e2a6d631a073afd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:49 +0000

    favourite classroom breakthrough murder tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
