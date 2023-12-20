ccb4c4ef68346b6fb269544386780e9c20be045b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:54 +0000

    fill once patience kind company

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_binary(n):
+        return bin(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
