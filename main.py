04e15a2ce513fdebbe63958f34b98a2a68cdd897
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:29 +0000

    within threaten attractive reduction welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
