90f0c4bb23e500d8fee59d37d154674f573277cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:26 +0000

    office spend collect town communication

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
