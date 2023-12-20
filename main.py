388bc3d2e4b5a423dc9d8a9359e24ec288cba3d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:14:42 +0000

    you generous impossible computer song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_index(array, item):
+        return array.index(item)
