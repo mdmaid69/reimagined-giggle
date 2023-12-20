f14f8896e9d0874bf836d07411bfb6ef1285c5af
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:08 +0000

    ourselves divide close cover language

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
