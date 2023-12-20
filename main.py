a7953bf32f60be349c860896073b94453071253a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:52 +0000

    situation taste near talk soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
