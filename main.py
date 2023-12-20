db6a027b99edf4d52b7dacd17a1a59ce4bd17b2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:19 +0000

    organization majority will band have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def count_words(sentence):
+        return len(sentence.split())
