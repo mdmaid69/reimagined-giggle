0ae2bc864d16fa783e488af559ab24d7da6ab113
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:12 +0000

    opinion gate train hand declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
