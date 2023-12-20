e05883dabbbf10bd75bc0c01fc07ab17bdbb47f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:08 +0000

    play every sharp doubt class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
