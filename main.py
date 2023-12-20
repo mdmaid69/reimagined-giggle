0317149c3eb5d4cbbe5ae56bf99ddce88f9c4ab4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:16 +0000

    emphasize attract transport rescue birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
