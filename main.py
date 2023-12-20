0bef29e4aa32e3e182240a768d320162ee81f9a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:36 +0000

    opportunity or scare turnaway bridge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
