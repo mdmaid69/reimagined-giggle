03997d2048de62249f901c6273c93df0a247c216
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:42 +0000

    male smart grade height majority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
