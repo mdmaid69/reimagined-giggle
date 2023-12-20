5948b42bf41056eeb264b1c597f632d4ffb40db8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:05 +0000

    neck mix pause feeling vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def is_even(n):
-        return n % 2 == 0
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
