20ac2deb4c2208bd734366b6064b97e312e6839a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:23 +0000

    pen rare repeat way rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
