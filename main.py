db2c6ef4a2e98c57daffb1dea976334ce815ab2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:21:30 +0000

    practical takeback disturbing obvious culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
