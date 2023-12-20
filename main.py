7fcb0109fa2da96ee32cf2ba62f97bd2c945d31c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:11 +0000

    reach behind responsibility sweep culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
