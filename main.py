784b9f8b224f42415f36dc18d907fbe1ba1da71b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:59 +0000

    load forever raise perform getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
