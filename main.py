a84a3e26652cb2ed1908c7050b4aea40cb004665
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:51 +0000

    sore can fierce certainly manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
