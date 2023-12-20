60df6a1476a926382b1f5a872811977c0e57d2c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:49 +0000

    advanced bet also powerful funny

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
