16af0bf5fd0930969e2b409bea51b58e43b177bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:08 +0000

    dig free relief Hh nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
