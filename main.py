45768a4e4bcf9d47809cc12fe057fa566872910f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:31 +0000

    action bend everybody grand difficulty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
