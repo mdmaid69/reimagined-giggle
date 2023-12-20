836ba8547580b742297ad29a272e8e03e2f3f393
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:49 +0000

    warn meanwhile knowledge measure address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
