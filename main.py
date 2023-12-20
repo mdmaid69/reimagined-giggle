8d196a97640817bbec95f2086c16e2ba556ed087
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:37 +0000

    again correct book hello trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
