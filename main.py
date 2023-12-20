4e0f3f0c5f1ddeb7a058e6a6be615e95793ee75c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:32:52 +0000

    study suckme fix turninto bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
