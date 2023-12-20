b5b77c3d3a0b88a9124d2de9399e7b12e1ad6280
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:32 +0000

    wherever getoff thought sorry reply

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_min(lst):
+        return min(lst)
