8e356636df7fa5e1bb77aa9d6ba9d366914d74eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:59 +0000

    worth nervous pretty polite still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
