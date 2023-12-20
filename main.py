33e51b7e46c6776f158766ae3c4d13268bf87782
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:20 +0000

    borrow see voice back sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import sys
+  def get_python_version():
+        return sys.version
