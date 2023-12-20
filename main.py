8e37f7ac6488fd8521e167dbedb4fb7b8c8faf1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:06 +0000

    miss destroy we runinto shape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
