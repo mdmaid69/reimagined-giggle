fc161427e1e10234be214d64089e532107cd1457
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:22:33 +0000

    authority seem lonely workupto force

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
