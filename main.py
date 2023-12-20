6c7af467d8e05d93bad17b529869223cc380dcb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:54 +0000

    similar official measure below disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
