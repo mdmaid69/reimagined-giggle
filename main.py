54ddfc010ca43ffbcb2c09e63622a0831482c606
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:47 +0000

    suit performance base example die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
