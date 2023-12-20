e11aa33f81f46a2e91342cf05182878b73431cb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:12 +0000

    later lunch correct onto surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
