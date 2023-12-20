276bb0adc9ed61db0f4316f4a8f7b5a57b3fd67d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:20 +0000

    paper exhibition wash though add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
