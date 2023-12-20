8e65270adf06aba58a2075524c8d0359046bf2e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:13:50 +0000

    plant genuine moment 569enjoy set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
