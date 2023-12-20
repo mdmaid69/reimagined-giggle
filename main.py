ee025812dbea0716e5482e769ed2dbd313c2e045
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:40 +0000

    Yy afterwards sweet pinch height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
