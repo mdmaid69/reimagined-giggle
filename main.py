125f2f6a3c5df8b1d42858c034165dfc16dbcde0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:04 +0000

    forward get forget bang dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
