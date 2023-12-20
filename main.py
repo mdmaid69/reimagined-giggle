d54a0f0950b11259f3b32cdf601519b05a5b9531
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:11 +0000

    themselves chase blame or satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
