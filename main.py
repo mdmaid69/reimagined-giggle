31c62bae20865a53775a1176c4dc9ee7971829b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:02:27 +0000

    off train beside free improve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
