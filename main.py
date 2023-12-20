760e262bec8c48ea250cbb70ae9dcbed55e29a01
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:09 +0000

    look Jj lookaround splash survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
