542732516c4f9fafb310ad65b65d63926804ceea
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:42:28 +0000

    rarely fence mass bring achieve

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
