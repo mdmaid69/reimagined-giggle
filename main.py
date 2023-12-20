e75c696d45214a59b9d1bcf5d0bea2f88219ebc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:21 +0000

    government negative rent rub ashamed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
