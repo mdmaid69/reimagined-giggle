c045f3f1e4d5408958fa39f8f736c6d6ef3fa8d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:47 +0000

    defeat judge Bb fit level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
