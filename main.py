cdc6a33da6a5c8148517402a8ded011986b456a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:07 +0000

    difficult turninto family pick gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
