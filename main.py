ccfe292a86a3b6b4524e19d50a91cd325801e7ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:14 +0000

    lift approve paper string pull

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
