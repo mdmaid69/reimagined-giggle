67fe81358a29351b2abdafda1f66ecca880728c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:45 +0000

    time goup burn warm spend

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
