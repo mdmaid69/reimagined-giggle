807ca4a6799ee5cbd8209045f587cca2a349f109
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:50 +0000

    protest bite suck under wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_hex(n):
+        return hex(n)
