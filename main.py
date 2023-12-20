9c30ba1781352f780411bd660b0c5dfc479a7807
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:37 +0000

    the town off damp the

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
