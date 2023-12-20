00150af8adca1e0978188767c343bee3a12accd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:42:45 +0000

    before chin finger phone bank

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
