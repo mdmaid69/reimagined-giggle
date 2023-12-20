0ce217425b83a2c59b9669743b6663754fd9db95
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:04 +0000

    centimetre violence head drink undo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
