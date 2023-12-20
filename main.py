4e36a03575c1040f8d57b7b7482620f464751708
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:18 +0000

    study pronounce government goback(to) basis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def convert_to_octal(n):
-        return oct(n)
+for i in range(5):
+        print(i)
+def cube_number(x):
+        return x**3
