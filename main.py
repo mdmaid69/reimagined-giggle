9874f64f99db586378ec2975820066a9c595c5b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:05 +0000

    comein benefit back truth list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
