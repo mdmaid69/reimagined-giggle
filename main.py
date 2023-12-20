ebb7af3cdf8e0899729be0c6e98ee1ca29c8e904
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:10 +0000

    mean recover instead rough place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def convert_to_octal(n):
+        return oct(n)
