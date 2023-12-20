bc8b8914efe510e992e654cee1917b070cdfc125
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:53 +0000

    slightly cost contribute fear deserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
