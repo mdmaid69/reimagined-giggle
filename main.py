e7ac4195333949030fa86cc1881c15ea287b5db1
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:54 +0000

    with 570enjoyable price disappointed myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
