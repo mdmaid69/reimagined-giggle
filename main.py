a30203f9be2ecf60a870a28843056d6f7d26d1ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:42 +0000

    quit trap mixture line Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
