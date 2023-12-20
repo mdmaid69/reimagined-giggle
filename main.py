f080b05a27a2329b8c5d8cbf5acfd2f69f903af9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:15 +0000

    walk squash produce object turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+print(sys.version)
