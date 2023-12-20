bc394f73116c0a04cb8638857ba26febcdb97710
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:56 +0000

    immediate mail remember tape noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
