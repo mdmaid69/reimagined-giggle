fdea74825f50f63d292d32f2d1557aa53b8d1420
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:32:59 +0000

    birthday progress emergency pronounce war

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+name = "Python"
+print("Hello,", name)
