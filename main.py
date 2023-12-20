08ace49dbaa7d21499c9f2a45a3b82c07c73be73
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:14:27 +0000

    characteristic say count leave distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
