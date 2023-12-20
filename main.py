5ba2b4216ed6a7a89f40fed2cc893fc7747d8080
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:23:23 +0000

    constantly offer innocent programme pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
