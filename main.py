e6894e839e5bd80a615bf59e9133ec8aaec4d25a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:38 +0000

    swim Ff after cool within

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
