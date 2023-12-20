a6ce200eff42fc104fa39cee68e2418435fcf7a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:42 +0000

    guide point immediate breakinto far

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
