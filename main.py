a3cab13b313e09f20c66bd979d05ce275066c278
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:21:59 +0000

    through interruption warn treat wise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
