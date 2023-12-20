6d0e7b98974d9c99a7a5429b0ce31f43bd621f13
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:56 +0000

    luck finally season if means

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def count_elements(lst):
+        return len(lst)
