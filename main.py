ed9d3090c4a2348cc5a7e3e22de766a82922ab0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:11 +0000

    regional pupil poor take quantity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
