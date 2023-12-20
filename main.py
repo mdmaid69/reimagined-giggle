e7e7cc13b913db07481a93c7a60f5e172593028a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:56:36 +0000

    emphasize light without priority illness

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-x = 10
-y = 20
-print("Sum:", x + y)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
