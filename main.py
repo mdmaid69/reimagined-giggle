eea95856a400ba5c667ff423a28d6a5f5b1e4bd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:21 +0000

    interest thankyou demand sore Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
