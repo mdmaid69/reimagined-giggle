0b1d2b6d2b845ba6ae891f7d50f570d8fc914ff1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:08:16 +0000

    clue worry direction taste right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
