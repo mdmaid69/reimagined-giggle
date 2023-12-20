1a04c125ed2b0cd31d028ed260feb0bd071c1234
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:57:06 +0000

    feel refuse mend train visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_str(array):
+        return str(array)
