156bd2fb4bf4b234041cdcd3173a8b2c5e620cd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:32 +0000

    skill little instead bang stretch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
