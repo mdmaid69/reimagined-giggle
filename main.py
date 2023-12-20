238537479653f924c2a840924f0f0efc61dde684
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:34 +0000

    discuss drop tune Ff criticism

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
