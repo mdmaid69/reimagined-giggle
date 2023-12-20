0ba6844baea8644040b64630c99113a105e0bd94
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:37 +0000

    beat whenever flood joke sore

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
