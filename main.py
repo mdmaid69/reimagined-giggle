421d2730a38aa94032cfb5a3d933824c1edb0e97
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:10:33 +0000

    offer search anxious wrong brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
