b612ca1d5c44b742fe729b759eda480ecad87ce4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:34 +0000

    hot steam herself protect duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
