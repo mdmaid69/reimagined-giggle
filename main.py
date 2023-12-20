f57398ce7e3433e1d23dd70f10e4c981ba345271
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:58 +0000

    appear factory consider clean possibility

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
