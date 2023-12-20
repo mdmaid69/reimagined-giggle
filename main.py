09b122cc617c826772a41a4da6f50a199af44210
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:24 +0000

    problem join plate section welcome

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(lst):
-        return max(lst)
+import tensorflow as tf
+print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
