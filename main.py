ed4b8fba716f2541dde6f8831145f2f0d27ec3e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:49 +0000

    computer clothes surprise through something

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+print([x**2 for x in range(10)])
