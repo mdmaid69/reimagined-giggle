726be83ca28b39d883a3ce21c1946b33d9503062
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:36 +0000

    temper recover detail between coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
