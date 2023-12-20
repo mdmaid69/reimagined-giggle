6dc63a5056ec45a54a914275ae82b7a625191b62
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:49 +0000

    coffee base guide plant valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_typecode(array):
+        return array.typecode
