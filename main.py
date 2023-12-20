17e6ceaea554bd5c66e698e0d38482ded597f29e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:55:48 +0000

    public agree whistle print sticky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_typecode(array):
+        return array.typecode
