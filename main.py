dfc7f8aa9c619048537c4c55dd46e410bf76c770
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:15 +0000

    record introduce freedom communicate sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
