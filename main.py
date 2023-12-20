db923ce173785693b754e55369a2755b70fb9083
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:12 +0000

    jump loud but generally jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
