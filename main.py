153d4342bb1206a1692384b37e5d54537bb50fb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:14 +0000

    never frequent press mark favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_user_list():
+        return collections.UserList()
