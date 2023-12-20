e8424c417c23b138fab0a90bb411f3750232d1cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:39 +0000

    track responsibility plenty cheat curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
