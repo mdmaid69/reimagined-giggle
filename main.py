660890c3550ded2262520d5dbf3558e045b2a9a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:10:57 +0000

    dare will star sticky tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
