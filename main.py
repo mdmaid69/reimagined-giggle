b63286ed5b40e2cda7c36e07913b0862d94aab81
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:53 +0000

    plant apply wonder part no

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+print(math.pi)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
