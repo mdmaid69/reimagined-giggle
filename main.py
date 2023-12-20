3b7a02b8a9cd3c9e6a4233df8c56f8cc41a9b7a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:01:32 +0000

    Itself print loan sink finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_string_from_array(array):
+        return array.tobytes()
