09c1b87914c6deeca0074cfbd1950f79853a1ce1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:23:58 +0000

    apologizeapologise section confused sit very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
