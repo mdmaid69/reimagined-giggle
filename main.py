68b229bb37e21d41470ff86336f8a9d373a23069
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:50:50 +0000

    finish steam guilty word exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
