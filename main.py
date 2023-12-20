f84645c00b550ee33d713fb871e8ec51a5cd23eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:48:21 +0000

    neck split himself thirsty private

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
