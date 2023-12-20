9897535250583499354cc7e9c91463086d63d0c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:10 +0000

    smile sentence concentrate bad fail

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+print(sys.version)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
