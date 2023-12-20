eb8f5e1fb7afabaf46fc7cd5b412b0bcf4227998
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:07 +0000

    improvement into sign cause definite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-print(math.pi)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
