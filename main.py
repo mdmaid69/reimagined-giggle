81d4f44892b299030c88a51f4708b5e00e4d8433
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:53 +0000

    speed television badly shape say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def is_odd(n):
+        return n % 2 != 0
