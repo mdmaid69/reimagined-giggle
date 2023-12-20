3039c88cc1abc1b04cf0358c103a3e7d85e88f25
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:23:35 +0000

    sweep look even belong announce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
