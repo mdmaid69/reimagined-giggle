2af5203324eeaf0842bd1c999269fe063a04d5de
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:28 +0000

    turnout respect crash have declaration

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
