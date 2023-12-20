a7720d5c2ff29364d40889742aab77f4843bb407
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:51 +0000

    pretend tip negative previous generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
