ee7f902d5cc3ab9f905c233413b08a84e78877c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:04 +0000

    godown make accidentally getout spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
