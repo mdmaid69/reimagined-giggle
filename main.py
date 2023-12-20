46170bd3b168388dc6bbf47d9056dbcc30e4c2c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:30 +0000

    know worth burst science listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
