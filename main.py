6db029a64b30233868e122a05ea3bef5f8812f38
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:49:12 +0000

    aim not whoever deliberately brother

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
