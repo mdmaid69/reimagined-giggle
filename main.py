98dae5925465f69608e88ef2faffc293e3f28b62
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:52 +0000

    getback level control promise create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
