bec2dd25e57e8ab55b892e83e499c7d1325b399a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:36 +0000

    occur quarter left fold entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
