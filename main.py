cc49ab3c894bd219850d0652de79d59a5a6074df
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:23:57 +0000

    belong comment cry race cheat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def reverse_list(lst):
+        return lst[::-1]
+text = "Hello, world!"
+print("Characters:", len(text))
