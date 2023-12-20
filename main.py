84348e8722da96ec20b07c53768a02a95a0ad81b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:53 +0000

    second let plastic fortunate plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-def exit_program():
-        sys.exit()
+  def is_odd(n):
+        return n % 2 != 0
+def is_odd(n):
+        return n % 2 != 0
