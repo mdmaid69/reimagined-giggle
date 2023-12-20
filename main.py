db49f41d4d71e7ac7900e481730cc8de2f9abaec
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:53 +0000

    image turndown pump drink Jj

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-print(sys.version)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
