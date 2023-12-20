fbb6f3e53806daa2b0d6d947e3fb5be808b06277
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:58 +0000

    pain stone ball occasional mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
