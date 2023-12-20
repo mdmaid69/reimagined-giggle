0d636c249ac8764a90a92db9c5fa3e18dbbd54a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:23 +0000

    cruelty politics organize afford kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
