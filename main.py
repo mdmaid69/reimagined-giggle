8199f2f288d95be56964080bc0af1cf6583b650f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:42 +0000

    job waste soak common accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
