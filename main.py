f17288197e56c17278d864b4498f4638a7b30811
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:16 +0000

    strong cum tonight off feel

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def create_priority_queue():
+        return collections.deque()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
