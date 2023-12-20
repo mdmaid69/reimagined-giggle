4f8c0b93cf06230318ad98ab3d122a06906dac04
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:38 +0000

    rule interrupt style fall content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
