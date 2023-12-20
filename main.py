b631d0f728f5149bf9ddd18c2c67b3298f549351
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:04 +0000

    partner sit direct leave either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
