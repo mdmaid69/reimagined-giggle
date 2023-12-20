7f28d739f1428a1ad2b91a8b13b05ac0cf5cf76f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:10 +0000

    give nonsense grateful relax getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def convert_to_binary(n):
-        return bin(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
