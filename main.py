934fd2effd45d9d205a6d79a667975e3dc3fc0ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:16 +0000

    phone why slightly suppose exhibition

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
