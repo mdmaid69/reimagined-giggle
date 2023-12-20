f6006682a96e19f4e85f5cb590d7e601522d6274
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:55 +0000

    oil lock address find polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
