249274cc4d6178b8947090c670c31cfdb92d09f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:48 +0000

    quit shine team so regularly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
