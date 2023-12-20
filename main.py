30a52518e99f948cd3ed9a80b89e5ae3b12e59a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:58 +0000

    equal contrast sweep sign order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
