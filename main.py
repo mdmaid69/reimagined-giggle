14a134203e754a60fa5aa6f360bac78ce37966b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:31 +0000

    border left damage accidentally mess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
