3ac73c9b2bca457106edb1ace8797b6a22b8cab8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:11 +0000

    turn attention slice acceptable broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def is_even(n):
-        return n % 2 == 0
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
