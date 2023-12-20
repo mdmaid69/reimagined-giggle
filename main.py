4c7d45697970940e40117e52894ab681397b2724
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:21:20 +0000

    572enter broken nervous cry hurry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
+print("Hello, world!")
