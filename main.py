b7fc62f862cd812dd39bfb37bad4590d2c98fa66
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:55 +0000

    post plan explanation over pool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
