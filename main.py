d7e97893aaf9aeb3f0f8a7a303458ae162f6dc9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:31:55 +0000

    lunch comeout born place appear

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
