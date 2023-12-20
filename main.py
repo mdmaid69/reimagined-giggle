b4bbcde8eca215905a6ce2b659b4f0da6334e38b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:00 +0000

    dish press runout one near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
