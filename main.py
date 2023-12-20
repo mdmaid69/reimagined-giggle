c6d675d27bcbb0717c7c8fc00726de6414f3ead3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:12 +0000

    rise pollution somebody grow chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
