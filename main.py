34f6412bf73a31ebb322f43e32a7fd819f3fb678
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:19 +0000

    national trouble mystery statement answer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
