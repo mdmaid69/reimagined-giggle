b6353a92c684e50691a08449d2d4bb4e4917bc1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:56:50 +0000

    share spill illness seat the

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_min(lst):
+        return min(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
