a00a5cede1684bf81ca84d9c374c26b06f12f30f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:38:54 +0000

    gap pencil today attention tough

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def reverse_array(array):
-        array.reverse()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
