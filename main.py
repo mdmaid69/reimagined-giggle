32ff0b15d7c7716f3627bf739db1142ab3a1c6b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:10 +0000

    bad appearance deep press better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
