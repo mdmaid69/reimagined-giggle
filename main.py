a7d424f24b7564544147bcab135a5dd2bc6c1637
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:34 +0000

    offer cum score invite quiet

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def square_number(x):
-        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
