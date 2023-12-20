8dd1538fb1d71856ff4bd12c9509dc163b164804
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:35 +0000

    risk character stick complaint bored

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def find_min(lst):
-        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
