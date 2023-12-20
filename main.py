c8a4f49615143497d9289419d682e7462547d396
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:37 +0000

    fire effort count remember embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
