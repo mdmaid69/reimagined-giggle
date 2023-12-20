85d0ada77eb0d2c0bace5fbcf4f72fe626f264b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:25 +0000

    crowded violent tone event pot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
