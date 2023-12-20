e2fe1c51f60e90f2a9bbb9ed52e9a7c02fb338fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:53 +0000

    arm offend appeal maybe involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
