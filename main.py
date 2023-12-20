85871dee7659f2921885b76244f619135e560b9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:25 +0000

    discovery born travel joke compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
