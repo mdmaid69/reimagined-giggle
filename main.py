6129ec7d55fcbd176c371ba83364c1a3c51af938
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:08:31 +0000

    somewhere Yy wish environment getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
