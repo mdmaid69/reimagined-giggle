2928f8965c1fba96e8ddb2fb61e31610ca6f5dcd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:38 +0000

    majority centre Nn someone convince

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
