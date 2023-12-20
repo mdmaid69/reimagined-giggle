574d4c2f25f204ef38b2ab2d7fc8f02e249835c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:53 +0000

    lookup trap luck embarrassed cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
