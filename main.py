d4c90d424d60358ee79a76cfde366b7baae47852
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:50:29 +0000

    quarter too shelter regard high

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
