2a0dc5c5a89838abc9699b8bc9c0b6f2ac1e054d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:41 +0000

    agree mood professional comfort at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def square_number(x):
+        return x**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
