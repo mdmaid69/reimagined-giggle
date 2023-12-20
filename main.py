80cdb060e55a8f09aa8a0be4ef2c98e26fd9890e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:38 +0000

    steal reply couple relax wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
