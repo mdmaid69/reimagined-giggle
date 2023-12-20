77141811514daeac62a0804beb954756da428ca8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:17 +0000

    intend violence car command unlucky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def exit_program():
+        sys.exit()
