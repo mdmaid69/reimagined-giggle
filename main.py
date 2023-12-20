22303bd43b6f09afafff3c2e11b38b28250d0226
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:07 +0000

    couple neither steer become feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def subtract_numbers(x, y):
+        return x - y
