76e8f1ced9f07a37879ec1b28a1ea93d6197781a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:58 +0000

    chat wash least precise dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def remove_duplicates(lst):
+        return list(set(lst))
