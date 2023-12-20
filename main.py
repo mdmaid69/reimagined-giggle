ace4e8344b9be6b90bc5ef72b389c089739c8785
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:59 +0000

    waste greeting honest all month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
