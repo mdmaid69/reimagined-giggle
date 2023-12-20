41392648b005a278ee7ec8ee9bf33d8f3ac303f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:56 +0000

    defeat at recover best play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
