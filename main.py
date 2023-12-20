5f403d12c9660b40bbc7a5b0cbe0334f17bfb265
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:32 +0000

    plain fold fail bringdown also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def is_odd(n):
+        return n % 2 != 0
