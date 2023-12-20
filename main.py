aeea30cc8ba4763c5e325b1054195c071799f3d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:26:31 +0000

    treat extremely better power sentence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
