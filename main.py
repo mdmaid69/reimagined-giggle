a98c5414dbea2968978cd80bf81655cf262b8749
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:28 +0000

    Tt hope friend scared flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
