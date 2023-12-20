98b2ba95c71355cfd685c8db5ab7619c30fe1541
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:38:27 +0000

    list determined lower money scared

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
