4807e84d8e435177b938c513f8ebe4e4327fc002
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:40 +0000

    what how rare trouble finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
