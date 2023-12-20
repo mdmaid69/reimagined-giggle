eebe9a6631fc11ede999cfee2442f4b7d9cedf3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:36 +0000

    lookback(on) translate importance fill lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
