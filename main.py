6a46298ee1d5e7edfa5e0ab59a45210082395d3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:34 +0000

    undo slightly taste impossible instant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
