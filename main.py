84d269a26c3d7c1a51fbaa927ebccd3aef400102
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:59 +0000

    hurt phone leg play lie

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_directory():
+        return os.getcwd()
