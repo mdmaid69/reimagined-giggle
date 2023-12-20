fd8c2892b5eda3dba13c8cff9ba63b1c1e157895
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:14 +0000

    whistle prevent speak combine cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
