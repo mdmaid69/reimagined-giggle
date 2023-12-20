e910ece892ca397ce8c29d7ed87aa240bc1ba40a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:18 +0000

    breakinto Zz use future ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
