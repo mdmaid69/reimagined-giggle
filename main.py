8a463ebe2b211a684d4caff351da626fcd8235cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:27 +0000

    exactly put steady mainly accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 def calculate_pressure(force, area):
         return force / area
