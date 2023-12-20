023b4b1fab9a8ff7703deebee9a787aeed6d43c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:11 +0000

    really maximum comment accidental runout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
