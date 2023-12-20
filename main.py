dc43b32273dd62e9bd1e9630b53ea54d807df40a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:11:22 +0000

    order quickly performance hope off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_logarithm_base_e(x):
+        return math.log(x)
