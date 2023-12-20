216523641a4791c9990a358e51d74aa3bda5d664
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:23 +0000

    like zero key Rr trust

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def remove_duplicates(lst):
+        return list(set(lst))
