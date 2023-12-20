839af6d8022add5f8c6b69441180c31a123a6cb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:57 +0000

    thank guess see real cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def square_number(x):
-        return x**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
