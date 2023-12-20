f5888d15ec3a10f86f401379e6f240872fac6863
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:53 +0000

    treatment cut better unlikely stamp

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
