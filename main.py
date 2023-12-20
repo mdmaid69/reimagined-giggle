3ad130f8181b6fd51d4f7443a53f79120c325e9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:16:41 +0000

    choose cinema general trip exchange

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_queue():
+        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
