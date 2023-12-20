546be3b1e21aecdbb5e821d0fb3b9081ec0137b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:40 +0000

    remember breakinto slow its offend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def square_number(x):
+        return x**2
+def calculate_power(work, time):
+        return work / time
