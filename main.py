9d54ba346a7c67063e48a8a040d7e265237b39ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:40 +0000

    when bottom high strict front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
