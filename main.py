d607a29f40df8bab97af1fd172217d88becc3732
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:08 +0000

    Zz welcome choice wrap rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+print(os.getcwd())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
