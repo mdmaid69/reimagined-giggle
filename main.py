99119a2cd1e19de3344a7a7db35058e8817155c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:46 +0000

    outside beneath tax bound rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(5):
-        print(i)
+import collections
+def create_queue():
+        return collections.deque()
+  def convert_to_octal(n):
+        return oct(n)
