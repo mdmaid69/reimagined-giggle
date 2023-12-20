0e5dfcf93b1dd94df4b8a3b966d003309e80a578
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:48:06 +0000

    disagree front risk disturbing future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
