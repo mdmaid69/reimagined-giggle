9aa9f5f6198fec4de5a55691695b736e04be78fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:33:59 +0000

    pin weak lot tonight brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
