356f56033ea7ff8011ea810abb204ebb57fb08ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:38 +0000

    Jj letter Vv passenger winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
