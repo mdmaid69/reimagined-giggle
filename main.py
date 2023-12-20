cc16bf235eca1176b260ccaaeff4f5d0f1ff601e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:11 +0000

    one moral concentrate easy takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
