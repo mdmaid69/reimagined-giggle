9f5b801a759da94915baf6392ce519094d416ed8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:08 +0000

    invite pause interrupt calmly voice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
