d1ade7e6e7af309ce0e21b1e6094a1151cbf560f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:44 +0000

    like wrap comeup not poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_speed(distance, time):
+        return distance / time
