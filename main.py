f87926f615e3359a7eef74daecf83b3696918caa
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:32 +0000

    quantity breakthrough ourselves turnout art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
