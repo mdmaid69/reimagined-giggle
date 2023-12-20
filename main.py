dd944b07a761208b70fab139e9ef9851820585d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:07 +0000

    actual difficulty degree control injury

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
