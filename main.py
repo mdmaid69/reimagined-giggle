5c69d5925e1de88dec54e11365e3c87d6db7e025
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:49 +0000

    raw today giveup future provide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_octal(n):
+        return oct(n)
