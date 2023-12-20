d45b63d07f4ea9726cd65ccaf6efe9925e37e8ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:04:39 +0000

    mood involve reply final start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
