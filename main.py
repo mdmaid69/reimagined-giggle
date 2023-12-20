751578b8343d3c0a3218c3fedb8f17128ab8f933
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:34:48 +0000

    crowded against still afternoon arrangement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
