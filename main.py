b2e39db6873aaada1c2a1d2138e719d66f65ec64
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:09 +0000

    listenfor dig geton 567enemy giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
