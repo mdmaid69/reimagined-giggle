a043bab6b1a3234431aef1caf2f63d47b6409a78
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:37 +0000

    everything every shake mysterious visit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
