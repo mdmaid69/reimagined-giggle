4704f81d93c38cddb53faa37fa928bb3cf4e6a16
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:27 +0000

    cover up skirt level born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
