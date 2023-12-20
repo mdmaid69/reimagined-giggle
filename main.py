7be214f1b2b2820adc0530d95da73c975411e103
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:43 +0000

    Mm platform length plain thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+print(math.pi)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
