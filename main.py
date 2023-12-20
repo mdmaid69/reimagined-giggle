af9914fcab249043cb23633861b60676193d3d25
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:49 +0000

    over television shop square lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
