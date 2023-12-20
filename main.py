90bc89c074a82b4518f2f360b793b4972ca54090
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:42:54 +0000

    exchange argument cheap rather declare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
