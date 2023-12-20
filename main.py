2c11c4a3241924d66fc52922040035d1d958ee0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:19 +0000

    view regional smell wing innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
