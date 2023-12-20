9feb12196e9691e519c600a8e25bb0ecd6798ab2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:37:58 +0000

    bring official stop geton temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
