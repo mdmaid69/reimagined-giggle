eeb57fc9b82878f9d2782c6e1c61ed9a1815401e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:01:47 +0000

    serve cheap constantly indeed road

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_area(radius):
+        return 3.14 * radius * radius
