2b699d95a260a1ee0f6c32d103b6f01aa3f27c83
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:02:41 +0000

    but get fork and gap

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sys
-def exit_program():
-        sys.exit()
+print([x**2 for x in range(10)])
+  def add_numbers(x, y):
+        return x + y
