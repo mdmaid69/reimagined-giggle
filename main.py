cbbbff068c5875e665f2903c7162155f125b6379
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:43 +0000

    tax tough drive them produce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
