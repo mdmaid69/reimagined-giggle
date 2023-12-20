12328f815916f4cc2a169b1333d73d8a1da605b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:26 +0000

    throughout sight remark funny aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
