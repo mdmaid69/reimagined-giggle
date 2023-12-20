9b705a6ed50d11b809191b68f850f6566ff2cb22
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:11 +0000

    wind interest discuss 573entertainment mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
