d5e35cfeddedf068b17b6feb31940a91b7ff8219
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:14:52 +0000

    corner future room gather promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
