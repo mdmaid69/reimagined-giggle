5f958e13e6b4640c9ac992e040538c42cf689626
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:29 +0000

    equipment together rain fear foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
