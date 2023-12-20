fd92b5545b43fab2a7b516c6dfd829ba6a93b187
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:50 +0000

    tip cross Cc pin guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
