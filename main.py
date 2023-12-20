60551a9622f419649ed7893aaaccbcc188585e66
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:59 +0000

    lend shout glad off see

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
