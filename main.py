6148edb7ab5bfb0a7dd81e5ebd35e54a2858c020
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:49 +0000

    careful contribute fire on back

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
