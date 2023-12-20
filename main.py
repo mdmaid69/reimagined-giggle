2b97fb3de34247ec2016ba035c072c36ea9ea4f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:51 +0000

    original quickly intend embarrassed shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
