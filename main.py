dca4ccd2f56fb9b7482fc7e0593e4755e21a93ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:22 +0000

    light ocean sticky 568energy 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
