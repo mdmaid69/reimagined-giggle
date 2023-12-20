abc58e33717d482d91938b9e06e916d1a01fa1b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:09 +0000

    stir hold completely snow beginning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
