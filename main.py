67c10233b17b850d8097d4b6aa0da67cb1aceb72
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:42:43 +0000

    car clothes through chase from

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def print_python_version():
+        return sys.version
