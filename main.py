ba779f6131ab27efabf0fd8f4467d752dcc4176c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:03 +0000

    clean runaway slowly urgent gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
