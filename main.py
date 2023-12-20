b20c979c4dcd9d9fccfacd7112329970a41662eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:50:34 +0000

    occur mistake shake block chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
