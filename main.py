342b9acec7355b467a12789a0159728d6d866790
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:11 +0000

    centre lot arrival storm weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
