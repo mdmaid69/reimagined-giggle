e3da25cd9e62faa855db8129df5f64bb6d24e38e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:35 +0000

    live new from separate control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
