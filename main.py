b07b744e65d27329231e604bec60c05c70c79c2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:39:37 +0000

    release method slightly disagree alone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
