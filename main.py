2c3df1b95e0e9cc193b56cb9577709523dd6f670
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:13 +0000

    bad invite bush course long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
