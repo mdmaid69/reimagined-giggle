baa4af82cf31bd474499799e237813388db875f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:07 +0000

    new suggestion sink any still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
