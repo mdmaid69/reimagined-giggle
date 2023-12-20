80f1f247140b0368620f3d4ad14916fc923d9389
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:19 +0000

    Yy responsible discuss balance mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
