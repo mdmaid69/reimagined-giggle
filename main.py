68ece658faa42c84aea5ff803c7ed9c345e265dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:14 +0000

    unlikely sport patience apply shake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
