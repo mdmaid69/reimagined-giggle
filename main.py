6db5f848678f104bcccafde3816213d7a741cc68
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:59 +0000

    takeout present whatever tip child

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
