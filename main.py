96cb45c177ec62d3e06466ea6e4671846b7a26d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:27 +0000

    expensive empty right influence unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
