42e458ab7d18738b42ea6a286d877237a74a308f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:05 +0000

    organization considering cook muscle taste

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def find_max(lst):
+        return max(lst)
