362be5adc367bf85dc25b6a37247fbce6942b217
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:37 +0000

    determined star spare shade sound

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def reverse_string(s):
+        return s[::-1]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
