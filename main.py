b8857e39986aa518c5fa34112c62d8433c5473bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:53 +0000

    niggaacademy attract that set but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
