f36171139e79cd4b47dd9e9cf15faa1b49c51366
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:50:02 +0000

    contrast shine murder once beautiful

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
