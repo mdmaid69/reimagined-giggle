d06ddc178a6f0e29ce27a247346975abf9e90083
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:43 +0000

    herself motion Qq escape reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
