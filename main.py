50049f0d00aef3182bf66462a77aba16a4aecfaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:03 +0000

    design embarrassed teach patient each

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
