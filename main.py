2bb56b868556911d2c6e7b6b2ca55e4bdc4d142d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:36 +0000

    relation loss paper fun experiment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
