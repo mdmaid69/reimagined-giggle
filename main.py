e84d33d4a38337a1304a8ff0631523cd8eefeaad
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:34 +0000

    intelligent well close clear left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
