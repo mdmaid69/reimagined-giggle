6d7e85fb359999807dfe61f129ad50428be9d1c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:28 +0000

    expression point stop quiet bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
