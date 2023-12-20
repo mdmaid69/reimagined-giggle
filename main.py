356f3cb397f09d7396fac52c444e6ac248c0c3d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:36 +0000

    rub copy between opposite description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
