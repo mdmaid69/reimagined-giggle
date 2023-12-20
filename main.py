6dad1258f3a050f6def2d40fcf0faa96c2a9459a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:53 +0000

    whom fierce zero contain turnback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
