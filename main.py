8496636bb143722ae24df0c69c6f950de301109b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:31 +0000

    fit tooth brave getout loose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
