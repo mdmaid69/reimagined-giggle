c4a311071006682af350439572e586177bc6ec55
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:00 +0000

    embarrassed debt pull over either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
