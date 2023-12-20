76c430167a9cc310eba46b179412bf0668ba26f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:37 +0000

    leave goup gift paper form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
