07be3461b1454adafb6c9131b6395a490e9b28ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:10 +0000

    constantly upstairs ring praise worry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_stack():
-        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
