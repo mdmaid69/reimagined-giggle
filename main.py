29b1b6feb415dcbbd097b1695e197d2fe8d0240b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:32 +0000

    fine snow steep pay possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
