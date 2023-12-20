00a9a13f3b18fb9dc7736276a279d0aa38063227
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:08 +0000

    government centimetre makeup intend communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_even(n):
-        return n % 2 == 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
