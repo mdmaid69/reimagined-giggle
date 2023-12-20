8ac33af227afe1ea6dcbba254588ca59df189151
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:10 +0000

    direct completely slip try intend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
