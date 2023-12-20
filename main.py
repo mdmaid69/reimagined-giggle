9d7026c96da039d2b57235c86573c6c3d6ae7c07
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:49:41 +0000

    bad bored quality too ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
