b2e716d1a05fbce4579c6cc33d4b4117f13f3e53
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:53 +0000

    pause tip beat machine burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
