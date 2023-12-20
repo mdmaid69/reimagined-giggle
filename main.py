1b5f9922acfc4b089de7cf5cdfe45ffa8b084d22
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:27:02 +0000

    traffic woman trade catch failure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
