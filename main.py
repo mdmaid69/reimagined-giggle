12e12bdad399bfb512a6a179712c3b6fe45be943
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:54 +0000

    ticket read dry corner spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
