dc39ba28b4809cfc2a9e034800f326cc53b5bc55
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:19 +0000

    compare attention grand wind sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def split_path(path):
+        return os.path.split(path)
