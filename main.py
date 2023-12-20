44d59cb27b1e8fe906d24031146279bed206dfa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:22 +0000

    smooth whistle concern mad female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
