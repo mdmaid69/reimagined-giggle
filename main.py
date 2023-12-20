b5136b08335e6b90ae09c6e7d1fd97b3a4d4d65f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:15 +0000

    mess hardly relation luck realize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-print(datetime.datetime.now())
+import platform
+def get_python_version():
+        return platform.python_version()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
