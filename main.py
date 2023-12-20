ac742c6346cc96e4ac3c038f1eaa7c2dbb958af8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:09:20 +0000

    rush argue thorough herself shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
