77e1f55975b0f0ca3a93cac284820ab6ca63fae5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:23 +0000

    right impressed public now soak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def sort_list(lst):
+        return sorted(lst)
