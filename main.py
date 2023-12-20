1259ff8495f71c606902b6836a74794eb9930067
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:12:52 +0000

    teach everyone Oo yesterday artificial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+for i in range(10): print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
