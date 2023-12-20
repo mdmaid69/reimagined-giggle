c7279327f7737aed48c9423b2e458641030c38cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:10:11 +0000

    blind show callup both area

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_perpetuity(payment, rate):
+        return payment / rate
