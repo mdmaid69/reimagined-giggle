09ad2ebeeddcef3fa8d2daeb97f38a1f4f4d45be
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:20 +0000

    reaction talkback damage play mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
