96adbe2065a08d0271dbc35923aafdf223fe130b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:39 +0000

    dance carry number gooff greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
