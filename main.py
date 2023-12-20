d60cdf4eefadbe83e4c8a1a36b4538460ac431a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:16 +0000

    will now thumb watch sticky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def reverse_list(lst):
+        return lst[::-1]
