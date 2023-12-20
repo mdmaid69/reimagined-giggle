22b80755a824abed0a21154d7e7762a3dcf16f01
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:09 +0000

    major content discover stranger cruel

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print("Hello, world!")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
