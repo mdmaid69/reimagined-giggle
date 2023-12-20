1d600670eb1ae0adb36da6fc0b08bb0bdd181dee
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:45 +0000

    have dead mass hers straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def create_counter():
+        return collections.Counter()
