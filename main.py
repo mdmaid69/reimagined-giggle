47b6708cc81a50e10f016dbd6e0e3b2292c5cfa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:27 +0000

    thick laugh partner the minute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
