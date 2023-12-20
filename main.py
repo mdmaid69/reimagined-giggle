6a39592063a10566265bc6a46c1db9248ae81098
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:35 +0000

    could if neither sting group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
