75712427e35321770e560ed7816771d4efd56098
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:35 +0000

    inquire wind border emotion die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
