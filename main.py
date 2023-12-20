bf30267a0ee4bf72b104f5184c670fc7ce7e227a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:12:47 +0000

    wheel chance getback describe comeup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+print("Hello, world!")
