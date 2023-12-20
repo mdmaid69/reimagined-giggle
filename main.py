4de9beb4a672edc5748ed18238e22588a5c7b016
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:37:17 +0000

    past anybody exaggerate disturb temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
