6b3f61cbfefe6bac398a2874583798fe3a05a497
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:07:34 +0000

    joke afterwards create silent sign

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def add_numbers(a, b):
+        return a + b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
