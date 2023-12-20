8db43cf8e7e913deec4c4e4cf6e34b64401dc3c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:01 +0000

    report people put best Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
