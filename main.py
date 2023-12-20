f964423fb6711679cb708ca8ae2dd54bcf0a8a21
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:46:00 +0000

    hope doubt national rise share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
