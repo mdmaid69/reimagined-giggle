02a50940e64164ab155581a30dd8e7f9d1e95f70
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:33 +0000

    learn calloff mainly stand reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
