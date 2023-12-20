ba87c169d3ef6e0420909aaa20519ab3bf99142c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:32 +0000

    practice below search send unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
