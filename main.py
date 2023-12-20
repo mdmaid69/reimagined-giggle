80408d48b52408bcfc076c36631e7e8a57fecd8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:12:28 +0000

    what half hurt judge by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import logging
+def log_message(message):
+        logging.info(message)
+  def square_number(x):
+        return x**2
