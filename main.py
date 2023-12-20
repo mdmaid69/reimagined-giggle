a8ddbe3df8f15dfda8d964eae0a4be951154eb1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:15 +0000

    arrange away steep bath blue

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
