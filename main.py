a0fd3eba88da468fdf3c1518700dc6e0f0bba50a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:28 +0000

    steam disappointed television man dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Characters:", len(text))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
