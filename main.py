06bfb4d5db4cf3e3bd0d00fd0539f09ceabe37ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:10 +0000

    relation gradually agreement trade past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
