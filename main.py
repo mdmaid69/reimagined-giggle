c1751cf77ae58c8e6d1b9abe877d42363ad4431f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:13 +0000

    operation system switch nearly affect

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-print("Hello, world!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
