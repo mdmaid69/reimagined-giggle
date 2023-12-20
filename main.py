7e2fb518ab4224e0e805f18c16be292c18f4fe04
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:08 +0000

    oil rarely any grow innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
