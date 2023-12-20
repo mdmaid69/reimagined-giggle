05655f980b0592a9d7b9ff3f0b7bdfb8137cc737
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:48 +0000

    attitude large stretch comment progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
