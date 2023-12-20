1c080012f5e129c7016d1c797f50ea7f6a3efa97
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:21:01 +0000

    immediate trick minimum Ss face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
