9b66d1c36a68d5e6794897b6d04e311c0b1b5582
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:03:54 +0000

    teach among keep low study

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_area(radius):
+        return 3.14 * radius * radius
