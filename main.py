4246aabdbe40e50141faf3f67b244a72a1b94f9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:39 +0000

    here disagree scheme character might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
