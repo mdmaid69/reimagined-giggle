74fe2253feca85fe494623f16846ef8b46eb9a30
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:47 +0000

    comeback factory worried score position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
