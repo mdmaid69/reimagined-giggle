157a5379576cba4c94025e2095131259e194e011
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:59 +0000

    unit break little buy confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
