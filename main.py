62ed98bd84fb9228f47129c192855e693a5862a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:23 +0000

    imagination makeinto used exhibition breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
