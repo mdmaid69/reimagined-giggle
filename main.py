336d14dda98c1a973cc2193cb35615d92880414a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:50 +0000

    switch cover morning score explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
