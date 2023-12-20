48300b97e97008201add304f31060833d8e374ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:24 +0000

    terrible share flat police bored

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
