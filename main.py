c104ebbb61f276037e33f70022d3cf9fd039903d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:01 +0000

    somehow emphasis stretch little thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
