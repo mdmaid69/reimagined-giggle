284657de607e55b6d0ac6953cc389a3dedb68f3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:41 +0000

    talkoutof common please reasonable traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
