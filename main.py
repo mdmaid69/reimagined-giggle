548bd365c39b06ecabb658b546b1c9f4dc3c1ef1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:16 +0000

    Cc trouble black place count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
