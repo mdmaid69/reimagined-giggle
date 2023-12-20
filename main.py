b4f3ec543afa4cbd5ba6a8bf26e203eb57142b6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:44:24 +0000

    pencil not dance further getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+for i in range(10): print(i)
