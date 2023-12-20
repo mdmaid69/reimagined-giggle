61afff1d9b1149c8264435e1dfcbb266c190199c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:28 +0000

    natural sky opposite serve ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item(array, i):
+        return array[i]
