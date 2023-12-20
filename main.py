88b81cbb796e65918221f75313edbf3670e7dc18
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:11 +0000

    almost depend meet region 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
