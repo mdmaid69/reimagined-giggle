2a8c148f39a6fb9805c9439cd3dd08aeeb996db0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:26 +0000

    use accidentally ours path course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
