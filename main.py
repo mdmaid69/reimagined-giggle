763395cef49eba93e7abc9f288ba15f4f0f2ec54
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:59 +0000

    mark fork trial extremely complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
