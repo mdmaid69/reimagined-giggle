b1423a5b5c7a178ea337f70e8984688a59790273
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:16:58 +0000

    mine bother reaction bend goon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
