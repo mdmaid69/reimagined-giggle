92d817f16ade090d26f1ee53679d47c2d6a87a37
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:42 +0000

    oil stiff children divide watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
