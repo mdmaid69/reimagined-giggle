96d097a38da104e378bdb7530d0f4f9e42b75b0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:43 +0000

    clean insist reject recently passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def clear_array(array):
+        array *= 0
