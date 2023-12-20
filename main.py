e377147c6b95ea388655c066a0b093ec9937bdaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:22 +0000

    friend comeround touch lonely switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
