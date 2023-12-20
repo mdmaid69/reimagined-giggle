336062d8f72fb6e29538d3d5b277dee953c31c6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:59 +0000

    exam Zz plain over pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def greet(name):
+        print(f"Hello, {name}!")
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
