8d7f8eba5e00d6d4d5fdc10e810d07d35160c745
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:09 +0000

    comedown especially correct less attempt

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+print(sum(range(10)))
+def remove_duplicates(lst):
+        return list(set(lst))
