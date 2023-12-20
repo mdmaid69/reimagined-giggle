bbb56ad18019029d984d1151f5eb9f3e98fb1662
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:23 +0000

    getaway thought everything fence talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
