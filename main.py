e8d621811a12967f642118a7369e536447533c2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:34 +0000

    congratulate rain remember board back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
