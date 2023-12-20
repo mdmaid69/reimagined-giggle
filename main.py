b321fa8ec3ccea1cb38701ea0e6dfe76012ed972
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:30 +0000

    violent bar terrible obvious regret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
