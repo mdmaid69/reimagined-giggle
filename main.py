1260840b99cc593c7a8aa4e936d99d08568e392b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:28:41 +0000

    sometimes invitation drink again birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
