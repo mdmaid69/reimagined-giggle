68360dd1d01aba8e30f15e82377c598820bbc5e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:27 +0000

    tense tread satisfied switch movement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
