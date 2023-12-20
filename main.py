bd8416ed09198f1d793cdba3f4277e4d536aa738
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:45 +0000

    generous both service rest rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
