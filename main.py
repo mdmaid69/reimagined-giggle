27fcb1567bf461cc1199211b3e653e7f378f5f01
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:33 +0000

    first doubt motion name professional

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
