c2fac06f61ea1f5c78f2f4e9c7394e87c94e1f75
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:27:43 +0000

    other illness knock have independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
