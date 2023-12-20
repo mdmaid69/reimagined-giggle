4707ec8a51b18de7a2d9c7ee3fe62f065b93f2af
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:35 +0000

    part distant male perhaps evening

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
