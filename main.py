eb1e78496f0c783056276d98147a497a2e14beda
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:23 +0000

    cost left damage getaway upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
