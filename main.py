8530fa17b7bf75800cad3d6d8f8267feea361519
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:01 +0000

    neither probably accident else before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def count_words(sentence):
+        return len(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
