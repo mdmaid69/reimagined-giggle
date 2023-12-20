e750e460d2d224b3657011b9a4b70f94abc5d88e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:41:25 +0000

    after in of shut fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
