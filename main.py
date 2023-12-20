9a462460edd8cbcdbfea8f8f402fff669ebce1bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:51 +0000

    generous ready joke needle very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
