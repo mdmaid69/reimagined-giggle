32e02ffd207a2694752bd816efb23b5a7cd335a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:15 +0000

    grade than path upwards defend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
