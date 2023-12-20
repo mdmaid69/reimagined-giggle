1bde3897b664a32ace0574aabd1c01e885ba7b63
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:14 +0000

    flat embarrassing talk considering bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
