421b972267d86e65c79dbbc5d0c897310b6911dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:14 +0000

    rush paper entry turndown worst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
