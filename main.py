4d211f597d9ac86da28b034b4b81e95b4e195fc9
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:39 +0000

    wrong clean improve scheme become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def sort_list(lst):
-        return sorted(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
