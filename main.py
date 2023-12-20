ec75197a472c7b44f1c4bdcd96227b77539021fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:31 +0000

    improvement line movement promise time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
