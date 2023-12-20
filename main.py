6bea60b6c6dc1f9fa82ecd1ae76177bf92a311cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:27 +0000

    noone seat 567enemy behaviour correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
