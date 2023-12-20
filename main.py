e8e86d25135aca57ba9902290354687c02c9ac76
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:43:32 +0000

    autumn circumstance season her evening

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_hex(n):
+        return hex(n)
