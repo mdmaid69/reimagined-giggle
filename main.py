fea81f06bc12257483ee5072deccc210baa3f7de
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:01 +0000

    clue cough confidence zero while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
