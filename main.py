0f6abbe5d85a42eacddf0d6e6b9b61477a33c51b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:40 +0000

    very generous enthusiastic fair avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def append_to_array(array, item):
+        array.append(item)
