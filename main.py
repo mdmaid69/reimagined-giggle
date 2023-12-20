bf6ac29a0f5b7b30b797ef6a5265db3cda261967
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:04:00 +0000

    onto understand window space funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+  def subtract_numbers(x, y):
+        return x - y
