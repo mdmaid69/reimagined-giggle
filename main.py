6307043dbb20ca7a4b7e2df921cf933f2557d552
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:20 +0000

    fit satisfied relationship body sudden

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
