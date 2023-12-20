42b3e46423733c1b3bcc081c8c99fc59c81aa101
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:08 +0000

    walk behind name have thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
