c2b5664ae97287eb5e02152b4eeac2521393fe61
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:46 +0000

    control judge warning cruelty lead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
