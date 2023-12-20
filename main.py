2cd5a582f7e606080ec9c3bdb2b50698f56bdca4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:08 +0000

    beside so dress mistake rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
