3314f28a08013eb9ec6aedbbcfa63dad34d852e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:09 +0000

    behaviour nature eager declare club

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_min(numbers):
+        return min(numbers)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
