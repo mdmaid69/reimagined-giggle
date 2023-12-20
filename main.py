e6582e5865f68710f99916ed6ea288aab1aef8e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:30 +0000

    hello pass century dead consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import tensorflow as tf
+print(tf.__version__)
