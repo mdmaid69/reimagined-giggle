0afdf313fefa71573d8fc5188d1257f19575c3a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:58 +0000

    opportunity occasion office maybe favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
