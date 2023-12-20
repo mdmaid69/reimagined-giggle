ee246919a6223ac64769ec6fc96180fecae7dc08
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:08:49 +0000

    brush hit expect produce possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
