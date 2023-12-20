3e380409bf3ea55b699e3d4310eb53c603360726
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:59 +0000

    sensible plastic greeting sign performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
