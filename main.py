b78b4e89c6a94c4044d795cb009813117bc91630
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:32 +0000

    slice all like next to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
