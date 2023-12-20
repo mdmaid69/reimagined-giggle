ab08570c60704b12f22327dc15156e7fe35805b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:45 +0000

    content suit lesson argument smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def count_elements(lst):
+        return len(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
