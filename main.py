2eef4afbad63f9dbc1f5927cc30ec67145d8f1eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:07 +0000

    sad especially several strike confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
