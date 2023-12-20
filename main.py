08dc21b691abb801d34c1b467cee29e825cd53b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:12 +0000

    whoever excited we instant reduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
