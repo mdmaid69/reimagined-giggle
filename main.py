8d045128f3d89def5188c4c92fcdfb134ff4fffc
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:59 +0000

    say pollution dry Gg throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
