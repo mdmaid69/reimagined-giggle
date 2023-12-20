30e261cfdfaf22e353a805638e977962eb27c5af
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:38 +0000

    perform press future listenfor workon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_rectangle(l, w):
-        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
