bfb1798868c0d61570bf148252b204872e43ff16
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:23 +0000

    danger visit wave wave government

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
