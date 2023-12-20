494fb5724db5bead9712e5fc71f5ff0b09c8f3d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:46 +0000

    dinner backwards desperate survive yesterday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
