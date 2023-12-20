7b85c4f17bb1e0a8c882d35b29df908e497edb80
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:14 +0000

    get why action behave steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
