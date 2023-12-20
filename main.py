64cb7ab750d3a9215a332ae17ce0cd7b841f5824
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:04 +0000

    check at hold goback(to) Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_density(mass, volume):
+        return mass / volume
