a29b6d1f9ff0c0e1018c395a296ab2a0e8b91573
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:04 +0000

    agreement cut story professional hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
