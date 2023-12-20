dd49ce185114b6d7dfe03407ce0d10573b6264f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:40 +0000

    book summer declare ring still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
