825c8370b7934d7af355ec78df572b2ea3c66103
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:53 +0000

    impossible destroy plastic game strike

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import os
-print(os.getcwd())
-print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
