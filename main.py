651a754217eb8d3d3994119f54081088a0f5805b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:34 +0000

    568energy hole fast appeal sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
