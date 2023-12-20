32df746a3f94b5761fa77660e8187e87db8c815a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:32:04 +0000

    noone contain broken sleep get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
