f0fa01cb3c50f85eeb6bb33c81e55911c5cda047
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:52 +0000

    doubt comment conduct careful run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
