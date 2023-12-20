f137749f690875f3afa24543885ff485b730eb92
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:15 +0000

    pencil trap since rich great

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
