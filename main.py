984681f818c11fe80b543169f8657474e08d0df2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:41 +0000

    young attempt equal theirs compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
