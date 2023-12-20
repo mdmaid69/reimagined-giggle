0e4f5218c2192fecc90d504fb8658d41561617b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:33 +0000

    whether pick call now call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_list_from_array(array):
+        return array.tolist()
