96bb6229887f4a7644d3f0aac826014cf32be130
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:40:48 +0000

    prepare above work couple you

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
