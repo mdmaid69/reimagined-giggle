278b735d51a0a00709bfd246970f5ac3ab39003c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:08 +0000

    runover later behind slice flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
