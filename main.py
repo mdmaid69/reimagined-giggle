a4367f957803c9be99f2bdd7b9c5b3e3e6ae5ee0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:34 +0000

    continue ever relation private rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
