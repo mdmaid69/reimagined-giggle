6b671def4617043986d08af475e269545dd4b9a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:25 +0000

    telephone shrink pattern crash organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
