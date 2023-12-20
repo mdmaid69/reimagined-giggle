a9620b9eab3efc7a84128d0659c24e3c559d8c5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:12 +0000

    only measure fault government length

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
