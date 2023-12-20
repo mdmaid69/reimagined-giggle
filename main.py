b6d33afb3abea49b245d987d2c64d610a7dfee01
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:48:17 +0000

    since position frightened joke respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
