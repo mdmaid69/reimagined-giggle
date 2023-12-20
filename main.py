7f619368f90da0a23284117606c19b61fb5636f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:14 +0000

    punish 568energy best ground show

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
