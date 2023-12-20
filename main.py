d2af980119d8b40ffad1d40d510d621c9873d02c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:18 +0000

    succeed wild exaggerate form convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_max(lst):
-        return max(lst)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
