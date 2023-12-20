5e358929f1ad7457be5785f5a877586d3c0988fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:19:19 +0000

    basis related crash below become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
