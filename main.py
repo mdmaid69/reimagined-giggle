1b3d7d7a75ed71a6754ed7615e2316bc9615b87c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:53 +0000

    bringup away professional luck car

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
