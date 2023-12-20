95fffaf8205aed8bbab688d5f0b3da470c922845
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:06 +0000

    familiar change fork press male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
