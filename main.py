69792fb6f71c9ceadfef2f5f5012c77f491a35f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:56:41 +0000

    film pool and follow opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
