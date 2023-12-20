ae8dc6cecc1f1488ae19e2604b1b261102b78e3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:17:32 +0000

    everyone slow fair pleasure supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
