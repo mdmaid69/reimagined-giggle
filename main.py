e14ad1e2652e215752a65527a2740b92136c6ff6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:52 +0000

    easy border cross pause string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
