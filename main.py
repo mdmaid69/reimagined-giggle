e220e8cf8a25ecb22d9bb589f65a3650fab70ae9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:44:05 +0000

    sort available subject exciting sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
