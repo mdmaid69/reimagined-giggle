0da8b654df9f40d24d22e12dac63e73b515b5428
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:53 +0000

    people cross unkind pair together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
