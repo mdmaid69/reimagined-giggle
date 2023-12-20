35a7bbfed7ff4378572b3987d4fad375969a1f5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:20 +0000

    relation hurry tap conversation kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
