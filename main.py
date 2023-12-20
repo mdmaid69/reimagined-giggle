23a578764b65625ba095607e1e6a73b0362bb956
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:32 +0000

    else even actually consider feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
