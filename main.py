2b71df2540dabe5c0b04cffb5d29f235d9bd72d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:34 +0000

    hospital ground sit murder positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
