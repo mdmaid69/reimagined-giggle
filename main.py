39a3b206970786e686db9ce52a1ff7327fcb0d1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:37 +0000

    Ff original heavy honest choose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_pressure(force, area):
+        return force / area
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
