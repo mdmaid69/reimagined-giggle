935d08b049336d0ca62aff9f400260b2271af5d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:36 +0000

    race could across tip buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
