c245a940d1f3e128a6841b1e8b9ef5cc5fb65cef
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:13 +0000

    release settle as scream used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
