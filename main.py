dc4a3d97f1bea37e1bade7f99ebe84e716a5e073
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:23 +0000

    age run deep hate valuable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
+  def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def square_number(x):
+        return x**2
