2bff3f3bbc499df1ade59c434e8a8ff927f2ae84
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:39 +0000

    drawing land breakdown common bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
