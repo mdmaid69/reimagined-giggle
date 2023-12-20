80b843d6384dba8dc2fbca3b0a1ce7ae50965a9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:47 +0000

    live breakthrough priority sting unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
