846a052cf67c38aff5c205eb5b98d165f7568a8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:57 +0000

    objection love fight final concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
