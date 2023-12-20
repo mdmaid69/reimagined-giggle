0a10c1a4925fcc910562c6ecf4435929dea60ca8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:38:05 +0000

    soak brush pot calloff break

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import logging
-def log_message(message):
-        logging.info(message)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def clear_array(array):
+        array *= 0
