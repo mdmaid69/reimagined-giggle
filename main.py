4b9741da47789a1463c899b63aa5021446d55f71
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:54 +0000

    here promise so part Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
