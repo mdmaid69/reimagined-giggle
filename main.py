53343ce1c5cb3343060755dbfb5ff6956b511039
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:36 +0000

    coat house listenfor car affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
