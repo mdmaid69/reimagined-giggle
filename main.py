75b008d19e26e4fdf60b3b38bfe735ab7bacb38b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:08:56 +0000

    fair Zz somebody crash spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
