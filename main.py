8ef2b5083fb035ca940c52ed563f1867f09e453a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:59 +0000

    tidy survive muscle quick port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
