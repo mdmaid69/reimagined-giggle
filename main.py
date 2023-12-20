38ba7d2cbc1a27b3eacc0a03e5384d48825d8515
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:50 +0000

    valuable force mood brave uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
