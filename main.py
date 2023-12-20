83983997cf02452dd0102892f363c1c9bc557dd8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:23 +0000

    no room bit beginning accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
