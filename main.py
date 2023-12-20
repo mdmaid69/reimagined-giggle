54da7130ee858ecfed20ecd9303c9cc3ec621e0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:32 +0000

    wish hard spring anyone pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def is_odd(n):
-        return n % 2 != 0
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
