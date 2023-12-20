655bbf31be38612c455d9ced1d0f1d0d6e1995ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:20 +0000

    steep recognize regularly related bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
