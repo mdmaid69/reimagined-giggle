391bd3c01f8f3f00d6f69c281c8697c13eedc5e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:22 +0000

    callback takeaway tool heart flavour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def square_number(x):
+        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
