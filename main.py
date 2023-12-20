9e637ff4071a7d5a09f094a159a85749b39ab9a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:38 +0000

    cope risk book either object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
