14f68107d4a9a97ac0dfe74e0115b4e876760b3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:26:53 +0000

    past report noone pity fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
