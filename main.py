aad0f0daa90821f0a0a70a695c42f21f16404339
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:59 +0000

    negative afraid steam show deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
