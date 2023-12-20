ef7eccefda99db73ae3881a6a3e2b2c225a47472
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:38 +0000

    bang company hear without morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
