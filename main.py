cf6a2be6d38b7515ef26a2c3939692e9a5d12dc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:34 +0000

    hang hide value disgusting ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
