41050a59d2c088f519e9526e4c97579e93c3a8e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:34:37 +0000

    guard path test characteristic noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
