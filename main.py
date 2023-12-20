3994eec33b2c30711cb6d058ca83559c46966305
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:57 +0000

    suppose clever Dd relative drink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
