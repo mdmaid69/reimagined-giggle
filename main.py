cf76172c9b20e7854fa51ec2a8e146b7cbf68920
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:51 +0000

    keep care similar copy face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
