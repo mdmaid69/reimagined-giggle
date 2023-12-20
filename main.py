19fd027de2c76e69eb418b391e394d97d55f2390
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:16 +0000

    limit fast somehow until trial

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
