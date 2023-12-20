95478f55a5e2bac680e39789a97f7e85cb7ce567
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:10 +0000

    during mess crime basically unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
