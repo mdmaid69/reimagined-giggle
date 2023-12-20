0acabc7e82b6e97d879de6e186a1f0bbc1fd219c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:07 +0000

    uponon takeaway why push create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
