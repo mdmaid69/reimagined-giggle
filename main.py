01ca2860b029b9e6180a2274c893bfa008e9ccb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:47 +0000

    surprise large low drop impression

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_speed(distance, time):
-        return distance / time
+  def convert_to_hex(n):
+        return hex(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
