9838f6e61bfce3bfe7c800542db3c88d205c952b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:42 +0000

    occasion reaction principle distant key

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
