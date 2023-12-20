16cea6e5a4b3c8278d2225c37de7c4fb4dbc34d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:16 +0000

    there one guard embarrassed flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
