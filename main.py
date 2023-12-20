cef7ee47a9a77ffccfdc53a3b9ee8516aa18d164
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:58 +0000

    sensitive just communication neither mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_current_working_directory():
+        return os.getcwd()
+def is_even(n):
+        return n % 2 == 0
