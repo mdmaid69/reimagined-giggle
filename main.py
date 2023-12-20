c1109dd506c837e3e35f681545f3c7d2a4ecbcd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:28 +0000

    waste hour bite float so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def remove_from_array(array, item):
+        array.remove(item)
