a27d3b12895428fe5bb588e2750d6beeea50e61f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:26 +0000

    bored connected usually subject wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
