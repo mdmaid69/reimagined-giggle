268526d6fc43466f4a519d3c0eed54e47749b5d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:43 +0000

    accidental emergency collect push miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
