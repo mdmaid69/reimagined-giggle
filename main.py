5e54d4cf43952ebd2e2c0a7b843b89fa7f09a0d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:17 +0000

    turn book remember notice just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import numpy as np
+print(np.array([1, 2, 3]))
