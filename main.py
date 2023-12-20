d450a3f9a6ade74a2b3e208374bb39d5b52c6419
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:39:48 +0000

    start tape shrink waste count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_str(array):
-        return str(array)
+name = "Python"
+print("Hello,", name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
