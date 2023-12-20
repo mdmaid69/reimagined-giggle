0b7e22c0291e1922d0c9f1e3cf0faf5f18e3fc71
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:29 +0000

    personal fact unable announce cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
