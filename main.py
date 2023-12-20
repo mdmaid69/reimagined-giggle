5000df7d1fa016d5772518c0b427657a07411541
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:50 +0000

    friendly thirsty suddenly side march

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
