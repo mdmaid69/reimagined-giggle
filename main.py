197f77286bc6ae1e1ed2c0ea809f441ff3c5e302
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:38:32 +0000

    with understand watch miss skill

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+for i in range(5):
+        print(i)
