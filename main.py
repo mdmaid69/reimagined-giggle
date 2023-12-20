a01d2b0b6094bb03440394fe149bd17bcf719a72
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:38:49 +0000

    capital address whistle tour each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import re
-def find_all_occurrences(pattern, string):
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
