1c00d85bf5082f85545c95087ccd7a8cc0fb3655
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:56 +0000

    upset desperate mention basically doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
