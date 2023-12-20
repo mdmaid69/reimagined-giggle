3e791cba2b96c92108e23e2160be2abd076a440a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:06 +0000

    rub succeed within copy generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
