f3dcf263c0a5863d006c8ac46e6632c2034ebafb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:59 +0000

    abroad scale tragedy several think

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
