18579d563ba7bb7873a27443d3ef5aab89c7a456
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:42 +0000

    tomorrow confidence dangerous pretty surprised

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def exit_program():
+        sys.exit()
