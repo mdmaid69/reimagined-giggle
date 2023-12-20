0595cf281e58dcade601dd6d5cca37e9c336efae
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:42:40 +0000

    field bush practise could statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
