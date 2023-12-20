e9c6ab4f406382ab50b0ec597a4980847fc8021c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:23 +0000

    progress transparent pen after like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
