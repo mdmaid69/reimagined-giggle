eddc317ca319621623fd4bfe61ccb9defe5dcffe
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:04 +0000

    case expression kill add private

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+print([x**2 for x in range(10)])
