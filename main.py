2e71a9d8605e59753a3643bc21caf1c2d3216d2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:04 +0000

    whoever taste scrap clue nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+def print_python_version():
+        return sys.version
