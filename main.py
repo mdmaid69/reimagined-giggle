29c62993a0ae23b69275dd6a470c9984bc0e9995
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:07:04 +0000

    seem reject move difference keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
