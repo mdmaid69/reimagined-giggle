7cf80e649fa05fdda40ae528cf486b21a40fb6c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:50 +0000

    possibility performance top less last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_floor(x):
+        return math.floor(x)
