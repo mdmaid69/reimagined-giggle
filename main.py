ac6f354e049147abc262d5e03023515a064b29c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:30 +0000

    risk right coat search ought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_list(lst):
+        return lst[::-1]
