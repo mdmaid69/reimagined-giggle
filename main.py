f9b496750029cf850f0be7875ba527a5ded6d57d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:46 +0000

    march sell shoulder touch practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
