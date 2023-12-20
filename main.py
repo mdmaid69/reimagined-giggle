0b6514507f6ba0c6a4782238bff46769a5e77bca
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:54:35 +0000

    shout considering dead work value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
