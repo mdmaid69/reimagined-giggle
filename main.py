366e69d65e8b6e9188d946d6563a98896875f8ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:41 +0000

    difficulty pass quit young common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
