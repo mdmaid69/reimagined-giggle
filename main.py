b6337abaabbe1468291895a93dbe0d54619159ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:27 +0000

    runout acceptable ball search spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
