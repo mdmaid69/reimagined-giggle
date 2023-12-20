ff33a3bbf1f2d9215842f242be47bf85fe1c27f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:54 +0000

    connect possible hungry waste surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
