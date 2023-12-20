ec3bb9927405d16cd75f8c714a42d18fdb483f60
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:27 +0000

    Ll not jump add cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import time
+  def wait_for_seconds(seconds):
         time.sleep(seconds)
