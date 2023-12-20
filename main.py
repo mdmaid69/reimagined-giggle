bc53224ab5deebd3bc4c3999d69d1e8f4476af31
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:18 +0000

    lot before middle impatient dress

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
