78d44fad26f121f12a2ff824212abe6665cef02f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:03:20 +0000

    head wherever finger cost classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
