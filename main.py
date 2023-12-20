524041f6311e6c4688f9f5d27d2ff185bbe0f33e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:48 +0000

    treat push either soft prepare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_sine(x):
+        return math.sin(x)
