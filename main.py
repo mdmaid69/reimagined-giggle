3138ff32495cea233c21c607fa229a72b70c99c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:58 +0000

    early do move joke tonight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
