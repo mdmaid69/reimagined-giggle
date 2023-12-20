5c944940f3046a8e9499bf6825f7451ee4ff071a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:02 +0000

    cause tour length more really

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
