dffaf5c44b3143714ceb027781d6aa3b684322cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:56 +0000

    unfair accordingto reliable recommend loud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
