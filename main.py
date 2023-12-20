da3e5c99648311e3a883e2c3003407ed7c582ed7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:31 +0000

    unpleasant comfortable view formal crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
