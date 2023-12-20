a3928a6263542bb59f27e801affb4229477e701f
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:05 +0000

    ourselves describe letter Cc steal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
