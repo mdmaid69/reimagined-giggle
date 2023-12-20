bb31fe950b4dd33a453b0216c5544cf4ed8922f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:56 +0000

    base scream towards maximum high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
