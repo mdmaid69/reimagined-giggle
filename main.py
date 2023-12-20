694f2e00dc63cef126750c53ec27bd12a4cd09fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:23 +0000

    strict true church high curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
