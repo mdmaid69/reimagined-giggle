a51603ec7c6f522fd2232450551d9eaab393ec33
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:33 +0000

    search unfortunate takeout tip available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_volume(length, width, height):
+        return length * width * height
