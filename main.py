70b8726f9b2a9f25d2b48e678c2af0d9ae810f50
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:20 +0000

    murder expensive talkback that tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
