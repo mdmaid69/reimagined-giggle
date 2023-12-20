fa15394af56eaf469c41ddaf47e1137efef73c17
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:46 +0000

    off sick brain hope stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
