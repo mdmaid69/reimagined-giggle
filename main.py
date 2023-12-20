ba30a8cccb5f3146a361f69a7514448f0a12d3b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:59:02 +0000

    ilovedicks comment suddenly piece perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
