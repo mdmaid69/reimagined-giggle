c9a77c6d3cc6165a6eb582300010e6b965593f99
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:18 +0000

    moral wall search scream heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
