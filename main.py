00e15f3b201c1e8a57417086e678282ee09bd18e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:59 +0000

    relation drive pour fine Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
