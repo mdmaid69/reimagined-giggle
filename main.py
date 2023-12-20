a239aa332ec2fc24f25d07a388e8ce90167f57f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:09:36 +0000

    travel project government in blame

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_current_time():
+        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
