2735cd9ebde79f84e409f59ac57d5c015c3844af
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:32 +0000

    defend earn still sticky study

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
