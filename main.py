9332525d2d18215231b535b8cc4f2a47d9a52e76
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:45 +0000

    similar deaf sight before official

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
