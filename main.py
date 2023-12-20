95ecd4deab4e866ac2011b4e48cd31a6e1563775
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:23:41 +0000

    maybe pull better keepfrom murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def subtract_numbers(x, y):
+        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
