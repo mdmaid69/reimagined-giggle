3894e20a79e32d1dc33d3722185928dde86c0fec
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:50:30 +0000

    pass influence tough case noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
