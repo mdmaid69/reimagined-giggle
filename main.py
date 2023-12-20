5876a13c37701d54ac96e912311bc66f484ad0e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:54 +0000

    to any pleasant light pity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
