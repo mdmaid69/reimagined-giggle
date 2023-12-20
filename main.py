52182cde940e9987d6c8fc4a773b459b0bdb8629
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:53 +0000

    release criticism improve each wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
