3cc83e4de51f6566637a9f54ebaf779bdcfde196
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:37 +0000

    opinion both interrupt could good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
