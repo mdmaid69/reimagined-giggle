e435043a34bde5d0696cef265911b4d74afcf440
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:15:48 +0000

    gap sport fail treat month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
