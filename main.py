d5cec820100c6394a679cb3ef7e06c2d3cdde950
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:07 +0000

    born calmly entry succeed burst

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
