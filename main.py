7397caac45d6f3d1a90ae7fbd2a62ba7e6b00d22
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:41 +0000

    laugh dick thank true decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
