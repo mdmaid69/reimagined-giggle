2548b4ba1cd6901e0d5c52c488d80ab8333412c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:28 +0000

    face before entrance peace everything

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Hello, world!")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
