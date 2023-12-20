441bf417d201801fd070a788b4de4c0aaef823e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:03:51 +0000

    paper store score head interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def cube_number(x):
-        return x**3
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
