64d348f197f8bbca1337bfa39e1e6dadcb3637e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:11 +0000

    bag sound ride negative plain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
