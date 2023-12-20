9845fabc57c25d607594905a420ba75e1a963ee3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:54:31 +0000

    past supply pause female tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
