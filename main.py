082386f3df12a164add6c2137be13c1f847b06eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:49 +0000

    than store ordinary listen last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
