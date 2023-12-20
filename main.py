6cb5fa5f0b2988bdf8431fe8ffa185e234904951
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:46 +0000

    operation surface he excuse thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
