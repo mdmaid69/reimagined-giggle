7275378c0e6cc65f645298b867f9288b841f53c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:12 +0000

    accept raise risk manage defeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
