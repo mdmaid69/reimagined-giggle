cbe9d41d5d2db7743ab3f67e4d30e2fd88e93d9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:23 +0000

    warning majority else like identify

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
