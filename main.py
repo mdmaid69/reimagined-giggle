68ea9a978140cd7ea43fe4342f6e9db60f174626
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:36 +0000

    scale trial thinkof praise deserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-print(sum(range(10)))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def subtract_numbers(x, y):
+        return x - y
