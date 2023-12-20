440acdf1eeaec11cf744407b5f18e80500b74512
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:11:14 +0000

    punishment cost pair trade against

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
