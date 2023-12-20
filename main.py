61497f60b4bd4ed342e033592537fe89da8a9307
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:16 +0000

    pride catch accidentally advantage giveback

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-print(sys.version)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def convert_to_binary(n):
+        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
