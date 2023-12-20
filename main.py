02f1a6ddde25bd1d3182f0c8240f0d018cb2b11c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:23 +0000

    confused please amount getout badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
