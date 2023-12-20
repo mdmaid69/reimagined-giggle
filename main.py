67a8b70837f59b3cfad725f6afc5dc06a74569c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:31 +0000

    positive hurry Nn herself region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
