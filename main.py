96912c2984a6d65c89963f839c0c35da8cdf6fba
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:09 +0000

    game immediate sharp entry any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
