bb21ee5866acf2e0a0432a123de1557211a8342f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:08 +0000

    earn strange mess equipment Kk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+print(os.getcwd())
