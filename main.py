2131439c86c278b4d1a415f265eba260234591f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:42 +0000

    avoid mystery blow invitation noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_as_set(array):
         return set(array)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
