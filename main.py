466556f5e05d4b50e257db9b50e428d327a4acc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:27:39 +0000

    convincing square dull deserve present

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def add_numbers(x, y):
-        return x + y
+  def square_number(x):
+        return x**2
+def reverse_string(s):
+        return s[::-1]
