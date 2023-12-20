582f6a7583acd17a8a3d1534acd16294cefa80dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:49 +0000

    roof coffee excited deliver negative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
