256976244dbef76d48c4d4ac13b8b29db0397232
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:09:05 +0000

    service Pp the repair Ee

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-def calculate_power(work, time):
-        return work / time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
