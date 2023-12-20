cc3265f980f87ae0d0dd1d13d3f94a01634fdf41
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:03:22 +0000

    generous balance uncertain difference precise

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_item(array, i):
+        return array[i]
