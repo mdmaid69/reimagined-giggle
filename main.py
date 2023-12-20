bf04bbb6354efdc593ff90fae29d3dcff05f38c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:14 +0000

    guide practical bath Uu fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
