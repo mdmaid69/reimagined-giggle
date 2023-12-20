e8ad67830562e52a8baf013161a8dcb552393ae8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:03 +0000

    gap watch check quit confident

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def sort_list(lst):
+        return sorted(lst)
