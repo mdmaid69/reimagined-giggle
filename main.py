b8844839a4a3d45acce236ee2c7c83c8f88d092a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:30 +0000

    people abroad female waste takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
