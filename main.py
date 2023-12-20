5fa47273f59a08e82ca931f48b794abcc7f72ae2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:46 +0000

    ground organization uponon lean something

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_floor(x):
+        return math.floor(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
