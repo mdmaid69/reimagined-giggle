5130db2625760e2e3aa4612338cab71d7f0b9417
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:50 +0000

    division near declaration shopping try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
