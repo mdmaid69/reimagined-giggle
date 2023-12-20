9bad661137d3fe044e80585592102176e766d42f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:33 +0000

    exchange instant study concern sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
