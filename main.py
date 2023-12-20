34afaaa83d8ed8a61d8358840cdeb6775dceb935
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:28 +0000

    equal impressed priority journey stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
