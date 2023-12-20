034165ebf9e7cb333e89506988c48310ba0227a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:12 +0000

    pull free rate cross identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
