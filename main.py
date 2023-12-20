b5544d47a1f42604d7e7475cfa925f24409c41ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:59 +0000

    front luck concentration above bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
