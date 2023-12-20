3b274149d96a769acbd919dd9d3097a7909d7f27
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:03 +0000

    agree read mean range ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
