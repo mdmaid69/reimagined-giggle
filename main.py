02e82e4b3e5e020ca2e1b773adbb1f281ef35784
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:44 +0000

    rate report save instead pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
