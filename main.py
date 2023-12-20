15b61d28a53287bbf9727d358f5c4f7135a92166
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:22 +0000

    inside remain accept against refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import time
+def get_current_time():
+        return time.time()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
