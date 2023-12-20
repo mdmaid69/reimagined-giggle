91c59a4a62258d8428b7c7657c159dc19233bc3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:46 +0000

    habit match holdout left show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
