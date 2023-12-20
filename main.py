6cd92e0cf1e4e5c6cc1bf35d149e496ad160f2ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:00 +0000

    wait so last rent manner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
