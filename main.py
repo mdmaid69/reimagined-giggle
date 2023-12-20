db16e5838ca77430f8b53bac02ed27555fd4e671
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:52 +0000

    reason glass minute branch write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
