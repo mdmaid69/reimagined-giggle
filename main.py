c162aeb49a74d1918751c6769065b80323f4cac9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:04 +0000

    company favour Uu birthday manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
