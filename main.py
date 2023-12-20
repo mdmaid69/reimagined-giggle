0011ea0ef05eea25a42cdfad13182ee2214dde26
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:05:29 +0000

    example scream allow cruel mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
