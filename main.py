a795e4bd84a4863bcfba5a5a2a37431305cbc05f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:27 +0000

    cross keen save approve save

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def convert_to_binary(n):
+        return bin(n)
