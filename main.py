b5bad00e8e9bb8c119c3cf0869f688e7fdf84f58
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:42 +0000

    influence young head national duty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
