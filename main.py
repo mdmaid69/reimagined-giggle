2b1e2b505a682faa5dca61fc75b6c7655c2b22e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:23 +0000

    sit limit problem road major

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
