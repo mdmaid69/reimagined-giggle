656ed139d7af6b8f20ee334cd739306213ed4103
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:28 +0000

    behind line discuss nice school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def reverse_list(lst):
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def reverse_list(lst):
         return lst[::-1]
