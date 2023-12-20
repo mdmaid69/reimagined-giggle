93e285a6deac998893f3c6de983b884c16ca3fe5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:50:12 +0000

    ourselves takeaway strength sometimes runout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
