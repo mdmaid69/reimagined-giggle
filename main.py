b872aee2094acfc65cd2e9899cb41d814cae059b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:33 +0000

    crash walk kill book imagination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_index(array, item):
+        return array.index(item)
