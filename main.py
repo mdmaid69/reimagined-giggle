33c295fa4a95b0ecb058bf8d493fa071ac3c2d2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:51 +0000

    doubt after almost continue house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
