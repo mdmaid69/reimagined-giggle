e9b872b7afb600087c22889e257ecc704f540db0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:52 +0000

    meet tap achieve thing himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
