fe4c763ac2e6bb5f9eb3be9ad0aae4677c40ba72
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:14 +0000

    speech emphasis short rule spot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
