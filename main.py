573a16da23e53909735a071238bc3ba9c6117e6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:06 +0000

    under Ii afraid lonely now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
