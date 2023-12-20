99df89ca1409d39c5f9c38365adb1ccf4e5f83d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:39 +0000

    that welcome grand kill claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-print(math.pi)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
