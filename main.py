c878d8b83fa3dcdf3f5f94e66f3f270231c0086b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:24 +0000

    head workupto score road smooth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
