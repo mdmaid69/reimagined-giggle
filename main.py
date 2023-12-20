afdb30739bdb1753aba84e4b7dab5c442ab9f805
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:40 +0000

    answer return motion strength Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
