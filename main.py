1ac2e7398582fd267562210bf95592f9ed00732a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:25 +0000

    home whisper favourite this rank

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def multiply_numbers(x, y):
-        return x * y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
