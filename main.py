bdc20adfe02e4b02baf5b27cf5e9ca73c87ee962
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:22:03 +0000

    plan influence pay sorry control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(lst):
-        return min(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
