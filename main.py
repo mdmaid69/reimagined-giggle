fe76e8e69f154950a3a94f396a05c4f820907568
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:20 +0000

    goon temperature sort without sweep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
