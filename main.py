76038f2acaab8f7cc84fac7a5de306a0732cc47a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:32 +0000

    sail runout consider punishment OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
