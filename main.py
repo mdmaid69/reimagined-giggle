6af767e21bad07d94a2a266c4230ca8182d9cbc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:58 +0000

    hair eye ocean post smile

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+print([x**2 for x in range(10)])
