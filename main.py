d2552314ef67c633d79108468ebace5672e10558
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:03 +0000

    ring circumstance consist feel clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
