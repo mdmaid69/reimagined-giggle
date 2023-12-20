489326c71d6812be3c5299e7be70fab4409cf16e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:51 +0000

    sign tradition music tip inquire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
