64ea016e462bf0174f9367ce604cdbff50b0339a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:14 +0000

    show mean suit impatient apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_current_time():
-        return time.time()
+  def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
