1ff4b462756db1a2a653e8512fce0c847da21cc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:50 +0000

    bad another splash memory Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
