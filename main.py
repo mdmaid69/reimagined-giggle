409441d96974bec67ed73ce948a9a97233c5f155
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:00 +0000

    appeal his underneath slightly goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
