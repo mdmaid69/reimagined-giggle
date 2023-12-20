7ea63d53ab7d24df3c75bd79e0715eb30b9d551b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:16 +0000

    position pronounce combination loss ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
