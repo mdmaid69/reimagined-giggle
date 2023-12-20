bf45c6228d2c999f41ca5b61e7818d81b3abfd19
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:52 +0000

    unlikely keepout temper answer report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_pressure(force, area):
+        return force / area
