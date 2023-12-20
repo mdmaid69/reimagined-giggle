9aee89cc94ff72b9afc597d61ec624b2d82ed7b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:12:10 +0000

    Vv callat Bb personal anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
