e5c47321687f8f29d0cdf8cd64f9ed97b99fd6e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:14 +0000

    familiar cheek Nn favour sharp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
