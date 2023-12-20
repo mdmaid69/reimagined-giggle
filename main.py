12717838c67eaec35d64945fe26c0209e7133782
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:08 +0000

    bridge turnout hope phone refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_pressure(force, area):
+        return force / area
