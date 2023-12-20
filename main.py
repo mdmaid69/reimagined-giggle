6e0860e1638d2b8aa5400e5915a008338b1c5834
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:15 +0000

    maximum Rr scare settle callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_binary(n):
+        return bin(n)
