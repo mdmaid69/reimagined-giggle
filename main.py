23e44aa141719601254bc3714994ae9ea1d77975
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:11 +0000

    typical string present size weigh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
