aff844165cbad022bfeea86f1e961c3646622a03
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:03 +0000

    Uu raw brain command other

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
