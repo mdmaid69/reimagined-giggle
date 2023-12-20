f88a1a161b361df358561d0b3d431d0ff87a46ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:17 +0000

    face project roll work slow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
