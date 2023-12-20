06416fc5ae7384c1fbb19a554988613aff52f05c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:13 +0000

    suffer goaway to like wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
