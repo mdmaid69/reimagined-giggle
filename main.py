2a746a9e0b400f0313a990f832e569683fe8bbc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:08 +0000

    woman 572enter fork appear bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
