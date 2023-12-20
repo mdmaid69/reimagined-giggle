a4b7fe95ff74c3701b054cd294fef4602a671d54
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:18 +0000

    war adjective friendly trip politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_floor(x):
+        return math.floor(x)
