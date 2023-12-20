45d122bc77af2da66db01ab8625081a16cc9eeb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:11 +0000

    central breakthrough hospital event they

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
