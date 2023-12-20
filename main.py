ae6665f063298db5845ad990151b6fe5cf8df3fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:15 +0000

    Cc shallow error theirs onto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
