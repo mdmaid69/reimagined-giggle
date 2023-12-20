6e78c1ac728f381e3ffec0ca828e5c384aa96c7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:17 +0000

    make stamp everywhere evidence runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
