dc4321d293c4759ec2f52a585d953a7ab5c80f05
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:09:24 +0000

    emotion crime lonely persuade hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
