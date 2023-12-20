5d9acf4323508ee4a741c9aeb0eace134174670c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:39 +0000

    workupto shame petrol strength herself

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
