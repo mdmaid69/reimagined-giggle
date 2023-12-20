48125a59b4f7d11cb1062e11b32928b9a857cde1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:06 +0000

    callback story independent related punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
