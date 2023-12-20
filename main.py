cf34485a75e93c2d26b76a65838e61570b2c7504
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:31 +0000

    national appear active avoid violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
