a49809fc492a623d6f008e2e24d6d22b567a2402
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:25:34 +0000

    Ii drive flow dull wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
