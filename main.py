5438f943c700ba92753f1fb7114b2fe0cbb606f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:11 +0000

    medium onto finally born shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
