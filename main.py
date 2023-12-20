a67d5dae0c611681997ae354e38f4c3d49167823
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:27 +0000

    lookup instruction suit save town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_max(lst):
+        return max(lst)
