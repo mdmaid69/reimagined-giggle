097cfeba412c6a897be47842521acb1fe971553b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:50:05 +0000

    objection serious talkback your like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
