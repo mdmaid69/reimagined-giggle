b1346ff21fcf646ca71f1868409db5f9babdb450
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:32:36 +0000

    type through so serious combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
