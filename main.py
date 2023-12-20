868b1358414f1c6058124e3b93d65bd25c7527cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:07:30 +0000

    gap have wealth that honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
