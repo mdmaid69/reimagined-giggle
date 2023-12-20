becb948097c76c90f2957bc11146f17de31bb6d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:46:19 +0000

    unlucky everything hold surprised mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def square_number(x):
+        return x**2
