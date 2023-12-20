92e02e52634bf506f978122e31a4b5ec71b40245
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:54 +0000

    shop switch community work war

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
