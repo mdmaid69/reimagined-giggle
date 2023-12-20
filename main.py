31b11ca220e2be9fd402f16dce68c39e2f7a2fb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:41 +0000

    remain through 566end smoke attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
