708a9e0bdc6078aa9b0f4eb1f2afebb8a77c74b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:17 +0000

    needle capital immediate taste possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
