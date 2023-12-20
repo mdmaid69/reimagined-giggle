65877b9ae4ca0b3e4cc40f88b94d1678aaa6497f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:54 +0000

    part Zz guess sweet unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-print(re.match("h.*o", "hello world"))
+print("Hello, world!")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
