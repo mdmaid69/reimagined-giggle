08f1472f8495524a2ee973c03d929e44e62b8d8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:23 +0000

    become example comfort excuse relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_work(force, distance):
+        return force * distance
