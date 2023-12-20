10217541070c0b57e4d7accb48dcb3a6c08169b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:49 +0000

    apply runover serve reduce tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
