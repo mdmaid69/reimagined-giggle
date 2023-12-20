7cd35ab17f335f67c4b60328eb7acb2a28f77d44
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:12 +0000

    mad completely convinced factory block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
