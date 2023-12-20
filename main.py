0a999fdba6e76306a7b66890bd78cadc2b080821
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:05 +0000

    skirt mistake board listenfor congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_length(array):
+        return len(array)
