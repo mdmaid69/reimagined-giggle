393852babfd652bce1d4b90a04a72db8fe49f798
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:14 +0000

    wherever extremely children national size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
