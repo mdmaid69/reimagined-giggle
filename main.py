0204204a3e4f98e0480b174ab76789c6ca32af84
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:04 +0000

    over drink slowly dish powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
