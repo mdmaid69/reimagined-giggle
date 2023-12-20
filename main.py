3292c5522ca6d2bbe25bd77cde2493a5f7b3090b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:43 +0000

    target comfortable slowly exciting Zz

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
