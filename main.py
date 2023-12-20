5dbacbc600f3a4ec2328d48195e451d969b0b073
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:30 +0000

    joke explain fall information kill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
