f251a16b7c8a2ad99fe7d5eedfeff4320fcdc4a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:06:22 +0000

    cum command watch play remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import sys
+  def get_python_version():
+        return sys.version
