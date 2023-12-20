78271340e9f3f46a27671521dcfd5dae01871e32
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:44 +0000

    book plan machine relation innocent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
