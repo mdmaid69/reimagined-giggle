54317766b7df3927d1c071d77483b9bce071151e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:11:24 +0000

    study dick turninto arm force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
