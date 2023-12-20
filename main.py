47ecf5db4e20cabad066580b2a4ac329294554de
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:22 +0000

    nice confused event club lie

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
