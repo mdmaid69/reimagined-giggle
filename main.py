22172b94eee20054cc2b02199ea0fb6a130c2db6
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:56 +0000

    other female deal unit announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
