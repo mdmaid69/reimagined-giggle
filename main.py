7ca8ca6a3fb904aaf3589534f50e352f163f9f9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:04 +0000

    offence authority discovery unfortunate street

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
