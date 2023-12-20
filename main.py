ac6f6fd494c221cfb087f08342222af37caf7ee1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:58 +0000

    keepoff impatient knock theirs minority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
