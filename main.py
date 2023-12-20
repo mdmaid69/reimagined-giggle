6c25dc7d14b2c0048da0200787a53c240c92316b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:15 +0000

    post soil label greeting fire

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_area_rectangle(l, w):
+        return l * w
