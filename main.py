c2fa167726910e859186347dabb4111cb844be98
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:07 +0000

    slow obtain total theirs attach

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import datetime
-def get_today_date():
-        return datetime.date.today()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
