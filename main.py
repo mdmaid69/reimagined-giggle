6358968520455dd110d9cad7cde4b3de986860dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:45 +0000

    front fork pipe year suffer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
