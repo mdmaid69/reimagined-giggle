007f101600e8f12346d9f3559e2b3b27e552037b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:43 +0000

    why workupto odd coffee cheque

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
