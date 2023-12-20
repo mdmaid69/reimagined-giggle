5fed8783453e0af08575eb070e43497d764c3482
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:44 +0000

    practise clothes Bb change limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
