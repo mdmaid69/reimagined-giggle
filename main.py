099e9593791c47d53a8ab5240b173e9640ac1ec2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:38:28 +0000

    separate object goodbye collect failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
