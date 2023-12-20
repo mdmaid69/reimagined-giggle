a05daf9eb334312c804c503f353f8b88dab8353c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:30 +0000

    contact eye principle turnoff detail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import array
-def clear_array(array):
-        array *= 0
+def convert_array_to_unicode(array):
+        return array.tounicode()
