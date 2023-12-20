cc576c6880c3fe3d8d7295f19065a890b4f5bf66
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:03 +0000

    goup down brief war breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
