010a5b4e5e682665945ffcf16729fbe3c4bbe9ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:58 +0000

    dark imagine word fall afterwards

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
