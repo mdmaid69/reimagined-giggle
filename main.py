06b74afc92b5e8a017b7eaadefb6a90156b9dafd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:10:50 +0000

    burn sick consult slice hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
