53f974b9665f28f963ea6cfe84f0b83b2e8167ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:05 +0000

    introduction warning visit business here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
