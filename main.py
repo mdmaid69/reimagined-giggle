a9ff307bdc850c196c6e19fba675119ebf451a28
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:28:02 +0000

    spend allow stand own flavour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import sys
+  def get_python_version():
+        return sys.version
