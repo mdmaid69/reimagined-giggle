4173b2e7690a3355e9bfa99c6aeba7037b284242
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:25:43 +0000

    glad press exact time national

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+def calculate_pressure(force, area):
+        return force / area
