1aeca0e06c3475c96e876c096980789f9f935516
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:06 +0000

    explain police trip flat lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
