780a32c2260cb65e4a04fdcf10783a4b6747fbfb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:59 +0000

    very poison sting spring some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def square_number(x):
+        return x**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
