06fcadd8e9b28a7c1c75bcc124d3c5f3e23f1dc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:00 +0000

    gooff suggest row lot goup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  def square_number(x):
-        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def add_numbers(a, b):
+        return a + b
