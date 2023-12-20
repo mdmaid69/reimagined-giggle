ddb22ba1d5c4d465a4cf32a50aedead179bd942d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:32 +0000

    they promise bear refer today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
