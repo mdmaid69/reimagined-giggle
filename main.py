b8c1570dcc9a18dc1d8b61a154ab46974262cb49
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:44:17 +0000

    better war turnover noisy drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
