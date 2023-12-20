0a5a3f92212826b5b13f3382a01d84eb38771821
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:51 +0000

    conversation train trap science play

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
