41d16eef278ca1e14f067beb5fad584709ff5760
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:58 +0000

    odd afraid please sleep read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def get_array_as_bytes(array):
+        return bytes(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
