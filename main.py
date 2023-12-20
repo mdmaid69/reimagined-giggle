95bb6f60966a4d35e202b9c933823e9302321a46
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:08 +0000

    owe old centre block through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
