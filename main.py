03073abf5bc3569cc64bd64c21311401246f9dd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:28 +0000

    sad image mad cruelty answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
