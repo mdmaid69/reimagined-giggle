723decca741502654dfef37bf2ac720c83ed829f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:22 +0000

    calmly stranger unkind such prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
