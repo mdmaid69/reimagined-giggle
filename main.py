ab8860ba121b74db37085a6fc60c3328184d95f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:10 +0000

    must carry least term pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
