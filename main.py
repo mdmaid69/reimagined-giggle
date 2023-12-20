eb7fc277d79d0a6192cbf5eb0409704d87612269
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:50 +0000

    scrape poison clean even decide

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
