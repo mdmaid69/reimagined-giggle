6645cc64bdba7e3087559fb9252cfb1d58b7f2b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:26:13 +0000

    rain common switch distance fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
