3df1c3d739b611e88979c4d3521cf62e21aac570
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:47 +0000

    government become generally estimate squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_area_rectangle(l, w):
+        return l * w
