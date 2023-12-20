1c89b3aff36b96921821f26d5d1e004d95501de2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:52 +0000

    confidence takeback war will difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
