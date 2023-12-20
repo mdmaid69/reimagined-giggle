7f84b92b49c6c99ea5012d14bb845651dd6d772f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:02:19 +0000

    full respect national several shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def cube_number(x):
+        return x**3
+def find_max(lst):
+        return max(lst)
