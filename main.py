b748f071558a1aaa18c2436440bf75cc773a8119
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:26 +0000

    beautiful necessary scream both cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
