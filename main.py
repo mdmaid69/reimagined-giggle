c61d27b9c3dc938d093905067c7a2a43e184cc77
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:46 +0000

    crisis nor apply model 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
