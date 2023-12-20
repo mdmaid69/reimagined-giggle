2e9e71b6077dccb4b315414a358400a55f60f8b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:27:56 +0000

    not comefrom arrange add himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
