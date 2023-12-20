5100d80ea81e7452e10975c4192e70a90f685daf
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:40 +0000

    success ours me production none

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import collections
+def create_queue():
+        return collections.deque()
