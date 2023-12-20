6e62a47525b5995eeebd8fddb300e5ffbab9a943
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:52:41 +0000

    mass afford rise improvement radio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
