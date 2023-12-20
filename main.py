9c7d010b8feb35304520960ba5243428db27e3db
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:26:53 +0000

    fork balance eat successful limit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_perpetuity(payment, rate):
+        return payment / rate
