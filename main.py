11d82f119e7b8066940ff5a52f506d24ea7392fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:27 +0000

    sense unusual production slowly habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
