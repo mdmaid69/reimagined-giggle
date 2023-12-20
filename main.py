f6881a1cb965355b7967e359425f5ebace0fe63b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:25 +0000

    cinema stamp club fault now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
