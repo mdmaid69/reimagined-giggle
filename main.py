36a3e50a35e2f9c558308332c04ca4a2bee0fe0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:36 +0000

    connected stupid relation tread statement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
