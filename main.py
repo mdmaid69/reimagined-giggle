93885c032e72eea54a04d989a101685348856424
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:33 +0000

    count by wash discuss well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
