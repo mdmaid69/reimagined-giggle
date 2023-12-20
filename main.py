3b6f4b527d0b081393aabf0cb603ca7cb7d0cace
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:19 +0000

    shallow mail waste success crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
