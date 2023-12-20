a7ee7b364eeaad11b49c9194abadaf62e97a6d4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:06 +0000

    live Bb scale town responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
