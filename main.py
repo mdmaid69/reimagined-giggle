d80624e53edd0484e9743d4ec8cb2ab9fe9643ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:53:21 +0000

    repeat surface improve clear upstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
