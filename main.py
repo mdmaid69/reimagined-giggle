c297ac24362b4d975f6568ac209caa717985cb14
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:45 +0000

    clean guess his rough main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
