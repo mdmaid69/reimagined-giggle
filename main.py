f3d254580517f0aadb73769dcdd16f663900fd88
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:33 +0000

    possession service importance cost Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
