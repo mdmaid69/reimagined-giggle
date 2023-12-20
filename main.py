e682889351747c86c4a27c76b296da7b6d5465b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:47 +0000

    make add cough in brave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_volume(length, width, height):
+        return length * width * height
