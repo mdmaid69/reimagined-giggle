119bd49413edd6b8a6c1c7716004b7712e92cc57
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:49 +0000

    back path fair scene related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
