e8a36587f599168860b8ef2ae940552b0eca9a39
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:35 +0000

    lie any idea birthday popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def reverse_list(lst):
+        return lst[::-1]
