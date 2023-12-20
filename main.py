1aab8604fd9091b96d4fd0184966d0d74463fbb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:47 +0000

    bridge imagine seriously way why

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def clear_array(array):
+        array *= 0
+import json
+def convert_to_json(data):
+        return json.dumps(data)
