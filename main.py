bae6eeef010373706de40ebace66560a2ca453d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:16 +0000

    pressure weekend burst suck forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_array_as_complex(array):
+        return complex(array[0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
