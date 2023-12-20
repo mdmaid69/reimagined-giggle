5ae7167d804a670226910fd4f7bae03b043d567d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:22 +0000

    event vote unkind smile appearance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
