5a35630493aae332e806985a96d632089a31a379
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:40 +0000

    can desperate heavy aim sorry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
