fab4eeda522ac8994365af9f02d7a1dc74946987
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:04 +0000

    practical brave last because there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
