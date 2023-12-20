1f6a0f200a7c11ef34dc4e27e64282d6cd784683
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:59 +0000

    normal goout wind boil confusing

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
