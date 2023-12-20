aeaca0840320d47014b9c0b3690f54b9894be6c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:12 +0000

    freeze nonsense feed Rr couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
