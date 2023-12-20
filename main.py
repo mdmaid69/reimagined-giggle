d32287ab328ae64478e7d6bc3d99f112db35cd8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:33 +0000

    Uu burst theirs effort cook

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_repr(array):
+        return repr(array)
