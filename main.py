70e258a605d585d88c14304bc9e4914625becbd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:22:26 +0000

    model steal object weak Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
