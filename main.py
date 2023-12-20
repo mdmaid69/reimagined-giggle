2875114a100afef1efd4f5f2fbce99f99f0ba97b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:05 +0000

    strike open giveback head point

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
