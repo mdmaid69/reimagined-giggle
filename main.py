5ad9b5da8d3817862f680d33663ebf965af61464
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:06 +0000

    post twist nigga intend somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
