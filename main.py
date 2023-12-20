4123da125909d6f438f04d0cd53c9d71be1df194
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:49:25 +0000

    combination lower forever save exit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
