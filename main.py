d88a41e0da3032e6d97502e33a40d701114ffe32
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:40:41 +0000

    slide speed interest curious surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import os
-print(os.getcwd())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
