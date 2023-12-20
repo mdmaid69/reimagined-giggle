c076c0015f52b693eab808da2415ad22e11dedd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:16 +0000

    regularly downstairs respect pressure software

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
