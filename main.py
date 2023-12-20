fe8979a2c08cd0d6f7f1c38e09f6197ffd621d1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:45:49 +0000

    cry prepare relative bend legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
