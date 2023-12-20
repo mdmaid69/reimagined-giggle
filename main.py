6a93b052456967e264f401880450aad83463ec89
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:35 +0000

    extremely first tip exit one

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-from collections import Counter
-print(Counter("hello world"))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
