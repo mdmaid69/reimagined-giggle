438007d7d4db80255e1ba9a6c5a5b03b15df17da
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:42 +0000

    innocent anywayanyhow single rate valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
