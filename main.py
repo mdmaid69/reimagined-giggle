202f1454f4e5d43761fc59e2f1cfc0f7362cca89
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:49:20 +0000

    battery lend calloff remove create

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_float(array):
+        return float(array[0])
