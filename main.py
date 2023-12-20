33bc86d4a9e25e5ca246898bb5c8db43445e2f72
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:58:15 +0000

    least top behaviour like sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
