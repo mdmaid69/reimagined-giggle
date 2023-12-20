b1174b112929318429fae9388ccaa89bf316d85a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:28 +0000

    giveout difference reach cool stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def count_elements(iterable):
+        return collections.Counter(iterable)
