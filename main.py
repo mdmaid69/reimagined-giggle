4d357f666d1a1fa75077ea3d688e20ff24ce5384
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:06 +0000

    moon public gap build scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
