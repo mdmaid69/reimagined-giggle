b0104475c586b599e51c92bdd9529523ed3fc456
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:42 +0000

    Gg happen zero positive enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
