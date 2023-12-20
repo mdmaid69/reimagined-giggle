c9464996761139f88dc9b3c0ad214e32f75a43a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:46 +0000

    deep rare stamp circle deaf

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
