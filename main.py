3655fd9ab624eba749650723b018311350f2af49
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:51 +0000

    communication noone bottom middle clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
