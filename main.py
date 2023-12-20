523c37803da5d53a44c94acfcc4f7d53d242f313
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:08 +0000

    control good brother possession badly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_bytes_from_array(array):
+        return array.tobytes()
