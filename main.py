232d1b28fc560bd1709203c0bd836e2e33912890
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:27 +0000

    minimum run special platform shape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
