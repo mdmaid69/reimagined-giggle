0e8a4bbf02fa1a79c552ebfa5724d1f8a76f0e84
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:37 +0000

    share find nice whole by

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
