6bc122e6f3a81e03acc3c05e7e0ca8e3cb5734c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:48 +0000

    shoulder godown open runout regular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
