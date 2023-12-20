b18b4b3e74640d8e05dce3bbf134905d1d4d48da
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:40 +0000

    frighten flat gradually this sign

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
