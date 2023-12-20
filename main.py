68ffc24dbb9be2ae3032fe8872039dc9750c14e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:50 +0000

    willing finger pull picture ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+from collections import Counter
+print(Counter("hello world"))
