54a7baea834b74b9a6d1c9d8180b786eff1b6e17
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:36 +0000

    test dry heart entrance open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
