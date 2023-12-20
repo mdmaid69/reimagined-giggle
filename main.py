d68c4528395ac91c6e3d35819f750ec84c65a3e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:34 +0000

    today bus big sink education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
