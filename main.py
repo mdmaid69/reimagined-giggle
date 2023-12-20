61a7576cbb07acbb0bacb37a55dd699e9a2aac0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:22 +0000

    tired doubt really spread train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
