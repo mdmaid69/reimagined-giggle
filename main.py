81927cc662efc475f643af24cb8590c31c4760c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:05 +0000

    term country Tt niggaacademy check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
