581fe53a0b02648eebc1c7d65ab5e4c170ead43d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:14 +0000

    explain offer worried step rely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def reverse_string(s):
+        return s[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
