3156785e1e1498adb95b1cdb93ed8d54e1799a2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:06 +0000

    meaning simple when again aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
