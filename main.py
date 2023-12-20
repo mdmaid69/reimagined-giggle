5c1560d2c083869392a9d0a5b03a11c57201e09c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:31 +0000

    vote myself commit knock through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
