7ad2b1c41f6229665b47641509d8423ebe4cb9bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:01 +0000

    his behind win boring hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
