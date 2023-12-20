ee4698f6ecd26c4253b2d3c3aa750fa0e7fc422b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:41 +0000

    occasion no final basic plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
