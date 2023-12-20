9e7e4fb445960d7bf47a9964c3354f8350ceec08
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:26 +0000

    glad sorry criticize hard search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
