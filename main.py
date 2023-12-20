950124901b1ea496cfce4c5e257077cfb0b11362
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:09:40 +0000

    far whole trial shop tidy

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
