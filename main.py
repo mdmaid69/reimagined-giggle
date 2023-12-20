6ef676833f269e9f4f0119e9ce5187cdb1b80bb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:55 +0000

    hurry tone fasten preparation tense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
