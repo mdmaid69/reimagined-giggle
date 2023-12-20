f1667ef85a7bdebfd4e15181733624fa3a4edcbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:00 +0000

    splash 571enough work machine office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
