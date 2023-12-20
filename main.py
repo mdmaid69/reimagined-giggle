5ea9979e8a84b22e26a72b23aceee6b127e9d7e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:21 +0000

    chase hospital lose absolutely almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
