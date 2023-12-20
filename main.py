0e93b00592798f154eb163b4a28c32220b8f534c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:27 +0000

    precise Pp lie die form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
