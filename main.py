dcd7d8a37dae6090dd0f209b22436871b3e6fd9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:56 +0000

    spin Zz ring crash praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
