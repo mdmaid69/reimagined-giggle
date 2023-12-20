b485859d9519f4600d9f30b7c03afc8e7bae6e61
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:51 +0000

    proof official breakinto class reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
