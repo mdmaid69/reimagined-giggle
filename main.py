d69dab1bbedb9c2808816e94eb4d782e3fdb7607
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:13:20 +0000

    combine measure single major she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
