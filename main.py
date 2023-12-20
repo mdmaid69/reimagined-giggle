f03e89566adb73b3355114dbd61e20a31cdb9521
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:19 +0000

    figure price thought sort chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
