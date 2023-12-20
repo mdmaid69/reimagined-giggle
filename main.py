c239bf7b43b3aee2611faa9e85149ccf00324dde
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:51:58 +0000

    exercise accept length curious Tt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
