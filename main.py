8d036e1992015c5dc47f80c251491743538d60ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:46:04 +0000

    tread terrible path afterwards confidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-print(time.time())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
