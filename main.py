0e8359dbaf01b71e59f208099bee639d8f8eab56
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:50:31 +0000

    generally crowded criticism fear more

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
