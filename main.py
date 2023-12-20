4333410ab6ca1aff8a540e0d1a5d40524192ea57
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:42 +0000

    involve first allow behaviour by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
