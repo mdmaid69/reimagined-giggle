ed437568de1045eafefc8bee6dcc9f0dd3e8eb5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:26 +0000

    mess indeed reject tape full

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
