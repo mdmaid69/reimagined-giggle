a0cf73aed133e8dc4f1a3ac2c67189569a82c818
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:43 +0000

    arrange own could where exit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
