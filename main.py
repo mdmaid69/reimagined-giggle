83a3f9e2fdf1cd511646c4de718fdd9c6b45a959
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:11 +0000

    tear quick acceptable during key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
