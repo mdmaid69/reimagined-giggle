802792f0bdce4c712c0743ac936735f487e4a392
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:25 +0000

    bang like stop favourite prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
