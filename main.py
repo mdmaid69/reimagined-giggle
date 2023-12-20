233c3ee7c2d784b4aa14159a128b32d8cb85c9f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:56:58 +0000

    little always fire or police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
