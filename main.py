437c96dccb4046bef9ec4c31a92eecd17440299c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:11:02 +0000

    smell finger pause tragedy should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
