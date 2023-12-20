565277b7adffa4df4b24d8329fedda04e09a1636
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:27 +0000

    mine say start company age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
