c035c25b5cbbdb8b376c632f3d9f0933ed263500
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:47:29 +0000

    lookaround clean thinkof hot to

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
