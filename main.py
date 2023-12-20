70841ef31b5f275c7272e5fc4559e5d953db92bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:54 +0000

    always mixture gap apart flavour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def is_odd(n):
+        return n % 2 != 0
