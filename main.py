efae999cbf1ffe6bead6e6132d24d95e7cf08d95
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:57 +0000

    rank sale concentrate round like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
