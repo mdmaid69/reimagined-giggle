7ae34d3a5200b269abc988ddbf8ae3d0c94dede7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:18 +0000

    improve drop address takeaway old

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
