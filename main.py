899b2a1be25d0be3bcd48c2a3dd6cc4b936402a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:23 +0000

    runout rub factory probably lookup

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
