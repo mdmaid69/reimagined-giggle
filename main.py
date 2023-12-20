393ae0ed0d99d930107d805c71acf34ea82d8224
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:02 +0000

    scream love kick walk runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
