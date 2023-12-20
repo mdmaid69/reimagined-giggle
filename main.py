50d2270af86dce7c54d791c6b041e04c40948695
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:02 +0000

    post apologizeapologise compete cope big

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
