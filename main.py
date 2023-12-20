62d152eaaf5984e8393c3dcf0b84e695f475cab8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:14:33 +0000

    before perhaps comeback emergency do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
