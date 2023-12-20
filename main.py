00a952187801558d090d39c58fc729b422ad3acc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:40 +0000

    rich brave home music present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+  def reverse_list(lst):
+        return lst[::-1]
