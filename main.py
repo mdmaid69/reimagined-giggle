7ef13694826af0fbf8c1846cdd99848f44364a4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:19 +0000

    left wind bring swing sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
