f0f908fc8dcbd2d9f7adccf482cfd6691d4284e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:06 +0000

    at turn style unfair equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import getpass
+def get_username():
+        return getpass.getuser()
+def add_numbers(a, b):
+        return a + b
