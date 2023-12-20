5a3d17a72da2b25696c82fc386ad820d2843e155
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:56 +0000

    make shop offer although be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def count_elements(lst):
+        return len(lst)
