286a8889eba5e4289c710530cba74c43067f74a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:31 +0000

    promise tell introduce Ii comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
