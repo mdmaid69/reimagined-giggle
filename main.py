e27ea428e24d784a227906cfe87f71b57bab76a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:58:55 +0000

    sad come successful weak common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
