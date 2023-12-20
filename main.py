b155afcd27ba510af93d14973260ff4c0193855b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:43 +0000

    wind plan fasten combination spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
