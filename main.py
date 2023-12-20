e9f3150c2b8abc0b5eb9824be421cc51dc09eac0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:21 +0000

    interested mother half priority workon

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def add_numbers(x, y):
+        return x + y
