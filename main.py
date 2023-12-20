f28b29a084e2377b67e23a332df9a787c8afc744
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:13 +0000

    anywhere moon exam pin concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
