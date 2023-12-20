0bc19037ef404f30baa8f4a49e16660664a49f02
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:49 +0000

    suit proud uponon safety scrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
