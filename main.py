079ea705a52c87ec45aff949019c63cdd45f28d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:02 +0000

    main risk mass towards collect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
