1a52b9a63cdd5dea81c64f37d60ec9840fa7875e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:28 +0000

    agree tonight plant suggest recovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
