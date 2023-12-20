d53140c0c2a90a20dddf20de0313dcc9b19e292d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:20 +0000

    duty negative lower raise bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
