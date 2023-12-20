0afa6fc5df674d6f0db9393346e4ad6791291abb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:54 +0000

    make past guess fasten equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
