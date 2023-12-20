fd5cfebe3494140a29c1aefb72282ad3b1fe53f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:15 +0000

    strength mouth learn unhappy own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
