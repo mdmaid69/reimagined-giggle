08a20eeec0eea4179bc88254cf4acda6eb8fd3ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:25 +0000

    figure label fight Ww breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
