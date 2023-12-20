d54fd4824cfa1da514c246423f9aaef700c40da8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:17 +0000

    behind size scared 565end choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_as_int(array):
+        return int(array[0])
