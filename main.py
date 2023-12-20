1dce0bb3ac626c41c6237cf68d30291b2444b9a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:07 +0000

    nowhere total lose prefer neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
