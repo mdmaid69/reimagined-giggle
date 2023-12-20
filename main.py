ddd926f265296993374286be0b85cd6a4a98e80c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:15 +0000

    cheap hospital storm aim wear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
