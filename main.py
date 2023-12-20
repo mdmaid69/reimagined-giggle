606187abd3fc80a1c3b02364d31fc95e9c1c887c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:26 +0000

    period imagine ocean pressure spread

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
