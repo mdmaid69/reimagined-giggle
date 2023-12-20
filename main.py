e387ba3ff56f2433186f0c8565e8798e1554b6d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:40 +0000

    software high noisy interview certain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
