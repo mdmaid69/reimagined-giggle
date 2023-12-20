c1d046d87848ff2dab5e30d6061a95ef268235b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:29 +0000

    contain shake cope order report

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
