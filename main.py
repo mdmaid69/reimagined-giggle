2ccb6a2516c8e37c9692c1f8a708950566726d94
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:12:00 +0000

    feed radio beyond behaviour getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
