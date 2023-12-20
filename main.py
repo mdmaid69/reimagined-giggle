86cc96ae3cacde5e4a6a1aff8a58db0b37c3d424
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:15 +0000

    hard glad right choice doctor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
