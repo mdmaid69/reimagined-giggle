d30c11686ffc68bccdc5c9b07163c89b8264e247
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:27:07 +0000

    sorry fail world before present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
