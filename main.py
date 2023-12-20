15944e0e8b579216037f018da4abc2fa9ad0e9b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:40:35 +0000

    statement ground never exercise set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
