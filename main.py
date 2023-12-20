7084680904969ff5a61b26547192b41a203df6c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:15 +0000

    without music never choose probably

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import collections
+def create_user_dict():
+        return collections.UserDict()
