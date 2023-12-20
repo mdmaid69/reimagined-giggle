1640cc81968179d7507927e566e3a2e6b4e3cdc9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:39:56 +0000

    hang damage rarely keen jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
