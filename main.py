7805ed3fa7b67c941586b85bfa78acf23ff46acd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:47:54 +0000

    show satisfied list meeting uniform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_length(array):
+        return len(array)
+  def square_number(x):
+        return x**2
