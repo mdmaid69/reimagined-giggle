fd012c46ffb9bf445d8e8729fdbedbd8f11d1584
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:17:12 +0000

    door introduce getoff trust freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
