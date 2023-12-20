0ba29e4f3838df890128ccce5282d0e27e3af1b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:56 +0000

    physical scared life software top

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-print(os.getcwd())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
