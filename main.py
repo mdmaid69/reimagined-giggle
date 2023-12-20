ddba4b2a4d9c186ce435bf10a599e177937164e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:02 +0000

    popular welcome file traffic national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
