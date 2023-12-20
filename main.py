0d8e285f6f4dabe45829b93156a55429b956f767
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:16 +0000

    sing confidence visit angry passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
