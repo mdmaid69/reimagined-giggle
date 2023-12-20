eb660b490f9d97a5bceb623ba8387d292a22cc85
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:41 +0000

    vision build translate whisper wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
