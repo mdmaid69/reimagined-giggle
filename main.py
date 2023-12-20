7fc33796c7a6f21a29113cea051270a8f6a535c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:49:52 +0000

    freeze interruption minimum store tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def exit_program():
+        sys.exit()
