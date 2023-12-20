46158195a665f7fd90004dd263e254796e03cf2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:16:30 +0000

    temper first violence another pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
