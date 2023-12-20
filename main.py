69c808f628f017a45a4bacf7fdc9116446197505
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:47 +0000

    pull scale study accommodation Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def sort_numbers(numbers):
+        return sorted(numbers)
