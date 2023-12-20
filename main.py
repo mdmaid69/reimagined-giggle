1a2f1831f152af8259e23abbb84e49bd2e953b43
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:55 +0000

    considering born mean special used

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
