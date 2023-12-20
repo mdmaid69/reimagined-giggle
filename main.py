d0882e3e36c60810e680afeba0a8b973805f59a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:34 +0000

    maybe breathe constantly base cheek

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(lst):
-        return min(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
