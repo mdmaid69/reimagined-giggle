ae0934c7c6d5733109f283fcc26625cb0929a940
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:44 +0000

    minute wall insist Vv design

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
