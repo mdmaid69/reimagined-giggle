62e757ce566089185317fe95aeef8652d85c0e51
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:38 +0000

    violent nice suit question together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
