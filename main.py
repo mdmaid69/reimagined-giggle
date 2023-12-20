c9887474585e4584f36812c331291ee3ffbc6c01
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:56 +0000

    close information fit agreement unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_ceiling(x):
+        return math.ceil(x)
