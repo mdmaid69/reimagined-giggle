c572e56281d77ba0e667c8865158bb9bedfddbfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:46 +0000

    it bringforward worry about software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
