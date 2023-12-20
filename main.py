d5f47c3bd7e0375f29768f63b2a822465fd76bed
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:11 +0000

    opposite name light control delicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
