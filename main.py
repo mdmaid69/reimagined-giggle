cc3fcb56001802806498027bc764cdaa6fe101e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:35 +0000

    peaceful doctor future prove reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_odd(n):
+        return n % 2 != 0
