13547eebd75ca29f206996fae41d9d89a7bdb0c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:27:41 +0000

    sad culture ilovedicks help suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
