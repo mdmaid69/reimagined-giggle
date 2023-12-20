7edd8a587f2a377af4f0a9502be5f2217dafa519
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:02 +0000

    every deep force cruel constantly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
