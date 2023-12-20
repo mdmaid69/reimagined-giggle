01abbf62eaca8a78beeabf41f34703684bc6ad1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:22 +0000

    close middle so lower expression

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 def find_max(lst):
         return max(lst)
