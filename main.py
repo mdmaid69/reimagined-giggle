dfc0a55109c201ca9582f54101df02e9d31418d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:36 +0000

    similar lose flow clearly steep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
