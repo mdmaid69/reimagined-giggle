67316e2da648984b6160e24302c9e63f960f2e9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:07 +0000

    Oo dead tone roof admire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
