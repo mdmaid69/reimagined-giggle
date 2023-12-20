9509e4b65ab4bc95312901e5ecc07db7101ea80f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:27 +0000

    entrance lonely poison traffic shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
