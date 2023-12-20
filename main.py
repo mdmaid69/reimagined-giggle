a7ab96da48ccd9ddae541831408e9eb70746a5ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:53 +0000

    rate touch below laugh science

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
