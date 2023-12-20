3b4544b23825d4ef228043b5972901cfe1881933
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:56:22 +0000

    tip track phone guard uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
