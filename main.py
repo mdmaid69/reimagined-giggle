670c5f4faef848d0731dccdecbfbe5988cd4dab1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:51 +0000

    include young hello noone before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
