59d42d0dd42b3dda0c4535468e29db606ff9543b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:13 +0000

    179bite principle back will several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
