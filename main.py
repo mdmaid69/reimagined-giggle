de09f63d536c4c6c148a7afd9effdb34ffd34236
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:01 +0000

    nonsense secret hurt pride whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
