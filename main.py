cb3411a04a2b0eae9beb7f0c765cf0163bda42ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:29 +0000

    upwards score left peace post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
