7df65adbf226aa172d21f93c3e237cc0b8dcea2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:06 +0000

    grade error control teach boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
