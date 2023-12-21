230fed9c311fed2aec31f04fcff989f4d61931e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:21:49 +0000

    reduction big pretty listen arrival

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
