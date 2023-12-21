5e359fa8fb3e415d668c312439991855e88a6ecd
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:26 +0000

    failure forget inside content last

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
