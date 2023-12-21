0ed29bb8bb586beea73c1243c80c11f715c9f169
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:28 +0000

    about brain Vv successful shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
