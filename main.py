5ad5521c465bde218c4ac3d923290692bab2b9b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:43 +0000

    she suffer throw best stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
