bb0125e0f55a86f936b2300aad2106aa3113b3bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:37 +0000

    band arrangement myself crazy cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
