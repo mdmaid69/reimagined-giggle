95ad0df412c2f99132af42943f17967cacaa57c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:30 +0000

    television ride winter breakin gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def is_even(n):
+        return n % 2 == 0
