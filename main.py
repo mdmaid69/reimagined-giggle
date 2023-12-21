8ca8bb60a9b53ecb237a3808cbf18bd5569147c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:40 +0000

    once hard record male poison

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
