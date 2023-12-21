a91b4ae7c97bebedc9cb24af596378ea13048824
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:38 +0000

    interruption path embarrassed greeting moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
