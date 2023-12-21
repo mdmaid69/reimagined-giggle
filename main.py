579b802724fdf4c48458b8d03c8140e795ef1f34
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:21 +0000

    convenient emphasis minority little together

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
