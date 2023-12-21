8bc6cedb43d0b7c61a90a7f62fc331cbd730efa9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:56 +0000

    recognize remove cinema passage invite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def cube_number(x):
+        return x**3
