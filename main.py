1e67a534e13a4efb61e937cbe181a2b1e6dcb597
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:39 +0000

    substance comfort post ball protect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
