3fd68350920523fe636c2017d38cbf7c28a7d688
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:39:32 +0000

    leg both hard here transport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
