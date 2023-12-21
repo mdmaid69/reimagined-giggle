3bbb3b412df21a4654bc9bb9ce2553b3ec643772
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:32 +0000

    battery disapprove new talk convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
