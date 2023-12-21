380b11a1311b5fd4c7e9a7d087d880664ba4c24c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:31 +0000

    design blame goout top aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
