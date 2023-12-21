0bf332fc502d7be1e53b20507aa64e6b875976bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:54 +0000

    spite single police long length

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_index(array, item):
+        return array.index(item)
