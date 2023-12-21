fef8373ea03bd7f10011615c09b07a16d80df648
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:22 +0000

    half human tread which little

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def remove_duplicates(lst):
-        return list(set(lst))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
