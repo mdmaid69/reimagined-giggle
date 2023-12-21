0f3c9310264a53b55a5a2ce5e657ffe0cfb6464c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:11 +0000

    slice steam escape every neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
