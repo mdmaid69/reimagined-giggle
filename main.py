a0b9e5fd8eeb71b7e386254043cd929b8279e516
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:49 +0000

    callout question even safety window

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
