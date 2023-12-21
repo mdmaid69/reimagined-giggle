d90f02bcd758c233561bb51d86abc91d41bf1904
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:31:35 +0000

    cheap length month shall juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
