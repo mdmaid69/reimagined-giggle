8080ed1afccb08abb62366c422fe759c56b94320
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:57 +0000

    guess persuade offend create lock

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
