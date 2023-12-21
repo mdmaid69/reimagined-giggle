5ffbd1a53e1f4baf66f98d0363ad2f2a4be8130a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:12 +0000

    black leg notice evidence development

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_priority_queue():
-        return collections.deque()
+print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
