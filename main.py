f1e45e9162ffb3a7dd876281b0bfde0d4be58f45
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:37 +0000

    part life purpose beat care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
+  import sys
+  def get_python_version():
+        return sys.version
