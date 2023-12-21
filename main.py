eee70ea1c30c99cf73797ce3028746f780a542ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:32 +0000

    owe fortunate remain getoff lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
