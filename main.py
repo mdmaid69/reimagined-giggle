ecc74a754e8c491ab4fb1bd0b85ab43662d91cbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:28 +0000

    freedom lock sharp lift truth

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-print(random.randint(0, 100))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
