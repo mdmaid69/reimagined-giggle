02f4d7ab1a5b9a2b6a0b5354dde914c2410cb650
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:17 +0000

    luck cost professional deliberate deep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
