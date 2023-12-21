26d00b4d71c0a598e88b1cf9d183917701aa3be4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:52 +0000

    throw lesson that whichever proud

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
