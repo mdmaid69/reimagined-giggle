ecefd9c4f8d5bcced8b34785aab3caa1e26d3263
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:16 +0000

    destroy intend only boil standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
