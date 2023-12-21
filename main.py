358a49fc2d729dcb9fba676d21c91d1cf224db17
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:47 +0000

    turnup them minority connection typical

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
