175982f826697f4c56fda93c1954438366dbdf8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:54 +0000

    shock office fat sure breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
