65c9cfa31230472805e1dad692bceae256b5bcfb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:16 +0000

    such book niggaacademy steady steam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
