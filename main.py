1d2db0735157757b2a90d9e98466428c7b01c7f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:33 +0000

    lead prevent cope taste previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
