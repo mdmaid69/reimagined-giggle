bda87ec8e0153ee96cd29830f92d27eed84f6f26
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:03:28 +0000

    distance herself occur if trial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
