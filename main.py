59e6cce1b26f044944a3ecbd044f96ea7947d872
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:47:09 +0000

    goout whether damage flight respect

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
