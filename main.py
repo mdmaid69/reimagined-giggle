924edb3e1c64e13062d4475374a748efc91e1527
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:08 +0000

    wise murder guide Tt clean

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
