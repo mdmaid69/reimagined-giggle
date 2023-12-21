eb162516fff7508102dcf0d9370d1a5d85a97ecf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:39 +0000

    land famous pretty alive bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
