cabfcde2d03db264d46bb1df1b31cb1550feebf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:56 +0000

    petrol personal change serve curious

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
