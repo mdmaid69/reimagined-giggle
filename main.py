37200a4467a26e06def0e79a69e4c42b06cb6022
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:38:41 +0000

    touch rush about cruel reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
