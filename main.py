5af30b4730317fa6268fce7ce0a0d55020089d45
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:47 +0000

    object strange modern above Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
