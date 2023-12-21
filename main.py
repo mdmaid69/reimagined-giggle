52887bbbe8bec0f53959ee28fda89fac3a4ffdd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:57 +0000

    beautiful callback fashion last cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
