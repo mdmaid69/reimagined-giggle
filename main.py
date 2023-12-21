84b00f1985c18c966f792266ec040e2e75d3ea2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:03 +0000

    correct public doubt full wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
