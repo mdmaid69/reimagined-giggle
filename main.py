0dd0637cb2c7fbe938198c68a73f4a49548ac140
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:45 +0000

    anybody invite smooth callback find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
