96ae5b62a07110c674a8eb2227e31fde4127a5dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:51 +0000

    prize thankyou sure group centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
