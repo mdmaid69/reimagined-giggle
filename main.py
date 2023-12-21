346881262a1619e90e31365917c1979701ac84e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:09 +0000

    meanwhile move slice tool weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
