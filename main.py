8332261b2551f6fb902cc9075a56011a7f2177d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:39:57 +0000

    wash wide fence modern present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
