c8c8eee7a0cac6cba8a891b21680a8558bc04400
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:30:35 +0000

    fairly actually would present Cc

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
