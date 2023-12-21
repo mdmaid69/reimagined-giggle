cb90153b1867ba1fa1add33923b71c0696b16796
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:25 +0000

    weigh twist funny tidy Yy

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
