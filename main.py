fe6ce830ab209a9d5a2f1b9540609f141d083df3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:57 +0000

    concern convincing lift own occasional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
