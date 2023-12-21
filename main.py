c9eaa150a22e1b7a2e1a56cf78ed59ccc9dd2271
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:39 +0000

    judge bang cinema majority understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
