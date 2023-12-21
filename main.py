ed9723a258a6b7c095da2b395827739995118cb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:47 +0000

    design Kk hold mix nurse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
