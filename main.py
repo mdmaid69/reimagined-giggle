3ac9071ddbe231e0302e871892446e4a2b6d7c8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:18 +0000

    strike beyond roll moment picture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
