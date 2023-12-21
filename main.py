7de4f388bcdef350cd64cfdd8d19c77aeca4c7e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:09:13 +0000

    grow mention we much talkinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_pressure(force, area):
+        return force / area
