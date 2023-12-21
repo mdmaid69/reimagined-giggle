b996619f4eb0d2b83246754f02ba7d3cd529ab38
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:01 +0000

    famous loud many forever cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
