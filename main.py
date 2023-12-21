9056e36b366222c1672930ffd520ccdc248e24b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:28 +0000

    safe careless measure purpose refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
