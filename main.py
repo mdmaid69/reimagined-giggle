f334399b1a0e6c53d919a634dea56b8693e77014
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:44 +0000

    exhibition necessary forever impatient spare

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
