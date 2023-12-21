af6d3b6d183958ebd8c0d750a0ff7637378bd98b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:04 +0000

    getup violent owe argue onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
