4e22a6857474ddf23bfa9575423a54ac7655593a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:15 +0000

    believe come meet both geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
