c96931658f7aea0ba3ee1bc84ecb156d5aaeb688
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:10 +0000

    risk Tt national thank surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
