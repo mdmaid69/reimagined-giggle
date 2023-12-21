d220fd10a7ac7128a89a98f36db35c981ff1cd77
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:01:22 +0000

    through tall control nigga our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
