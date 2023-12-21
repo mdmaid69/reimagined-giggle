75c75749fb4af2ee29a2f94317fb02fcd930ad00
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:32 +0000

    afford achievement either look private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
