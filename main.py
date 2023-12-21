d375c22c2557cc3d80b7fc173b000cdd3df0a67e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:20 +0000

    raise error whom goodbye sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
