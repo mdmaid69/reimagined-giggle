3335937b0eeb3947ab92c46b09d9606d8fafe810
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:21 +0000

    play tragedy funny everywhere activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
