f78952bb93c236d21a5ea1158318eb8e583cac44
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:14 +0000

    necessary result adjective damp success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
