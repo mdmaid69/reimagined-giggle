20929ef1c772a80402c2b316564f316531a57943
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:48 +0000

    waste doubt town no chin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
