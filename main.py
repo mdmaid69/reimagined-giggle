1e19293aacf45be0b5d78459302ca9cbfa809900
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:46 +0000

    better case happy future lead

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_power(work, time):
-        return work / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
