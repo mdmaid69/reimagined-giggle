e9eba2384cb371a84ee247cd4bcc1662173c0a97
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:34 +0000

    sit embarrassing guess once important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
