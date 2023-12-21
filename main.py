312717e97015d05bb763721916cf52afca1f180a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:07 +0000

    division field butter gather back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
