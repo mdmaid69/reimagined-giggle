5d0bb97951327f8dfd02bf1a8f2ce333f5975fb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:37 +0000

    disturbing discovery closely quarter centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
