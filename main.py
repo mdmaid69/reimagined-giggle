759e2c834750927bfe1ea78405abe05d163da8c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:58 +0000

    manner holdup balance apparently pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_density(mass, volume):
+        return mass / volume
