f0a6a4e8c179e789cf6b8333e01bac9c473a9abf
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:13 +0000

    point common issue niggaacademy rise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
