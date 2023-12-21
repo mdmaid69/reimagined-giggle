d41cd45a4b8ac24c8087de8866e18e44d88050e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:29:19 +0000

    effort sea hold comeoff not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
