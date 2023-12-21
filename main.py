f4fa0508c4e4125ef17647308dd18faad9542692
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:13 +0000

    yours please activity present sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_current_directory():
-        return os.getcwd()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
