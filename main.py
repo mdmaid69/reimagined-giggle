0747929e0be09b4f49b0abf6822d277baf4d8cae
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:52:14 +0000

    description bush combine contact attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
