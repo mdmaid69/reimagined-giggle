cdfe9fcc1665b79357aa4543ca1439d4bc91801e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:15 +0000

    whatever about thinkof how practice

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
