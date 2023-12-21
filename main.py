8f5cbb668672baab043449c7a8ac52759047c68f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:10 +0000

    funny Qq through no month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
