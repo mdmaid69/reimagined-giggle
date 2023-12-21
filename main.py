277a45336b9d0fcb2be509a9dc7ee61d5f1ec45e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:46 +0000

    release tip section sting keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
