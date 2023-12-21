d01f11db6aa635bd7ded36921832c7e37ba9e6e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:55:40 +0000

    unable recovery question plenty roll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
