16898d24034e454d42982e2506ffe0f834a8365b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:33 +0000

    consult minimum rest violent obvious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
