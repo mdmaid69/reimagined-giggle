00150de82893cf2a41659b17102abacf26f6c463
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:06 +0000

    doubt waste good splash steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
