0f06c3ee810287f9fd0deb9630477ce1c48589c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:54 +0000

    raw sudden excellent unite extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_work(force, distance):
+        return force * distance
+  def remove_duplicates(lst):
+        return list(set(lst))
