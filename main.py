7942c1472ecb629af8003d8e4875082536aaf448
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:16 +0000

    long stranger 572enter crazy lookaround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def count_words(sentence):
+        return len(sentence.split())
