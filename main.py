a771717b4f73e4dbef81dea6810979788309aa2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:43 +0000

    under children once none build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
