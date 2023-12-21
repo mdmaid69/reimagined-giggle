5345289ec2fcf98604646badc8dc7b420ea39213
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:40 +0000

    sense cover active always pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_elements(lst):
-        return len(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
