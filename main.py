48664a33d48b88a95bb844ac76744b47d0a06944
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:49 +0000

    file sentence certain criticism Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
