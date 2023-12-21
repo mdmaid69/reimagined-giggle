e76994e0b486ecfd8442548f72a422c6cd78b08e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:52 +0000

    doctor invite sound doubt natural

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
