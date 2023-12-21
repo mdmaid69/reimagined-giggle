5c4df5d261c1b8a45af3d414a242e8f412c755a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:05:33 +0000

    hide persuade complain lie professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
