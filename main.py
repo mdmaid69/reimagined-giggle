6b652714803b438487c0a90dcafca7bbc56319fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:57 +0000

    crash fly blame petrol measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
