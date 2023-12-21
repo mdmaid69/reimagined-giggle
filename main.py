a69ba9733116bdfa663a43b0f4a1beaa574e77ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:27 +0000

    accidental Cc shop positive cope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_itemsize(array):
+        return array.itemsize
