3ba27d3aedbb4230f2cb7328dab36c36e5ce7f4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:13:32 +0000

    everybody mouth hers challenge clean

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def count_characters(sentence):
+        return len(sentence)
