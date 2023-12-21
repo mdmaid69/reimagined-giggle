63909ef8e66df2bb3a8da25b041c74bd6f837e9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:49:00 +0000

    tap same or post reaction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
