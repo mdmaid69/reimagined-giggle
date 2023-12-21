61ba229eff21d8aeef2acdb9e742ce3cf0a32d02
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:10 +0000

    opportunity fresh rid progress risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_int(array):
+        return int(array[0])
