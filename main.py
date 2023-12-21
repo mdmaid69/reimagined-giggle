c97758bbce35e8561adc8ec48c80fa1a1bf40c91
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:33:56 +0000

    wherever means regret slightly plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
