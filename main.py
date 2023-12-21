fa227dfe480056b8eb21d54c92ba6b35b5550950
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:39 +0000

    win importance test pleasant live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
