3247775740c65cb87fc47eac3b69ac03d815cb5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:03:32 +0000

    work send listenfor load object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
