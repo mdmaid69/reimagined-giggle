306d0ebedcfa85b092f7697b17132f6d3a77322e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:46 +0000

    push talkback ought once up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
