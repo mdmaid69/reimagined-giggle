ffff289c415017a8fdeac82b7f1170a5215e1a42
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:42 +0000

    in else female ignore through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
