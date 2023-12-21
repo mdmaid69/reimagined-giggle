96641a4cfc0463ec8cd59b634fdb4e3b13dcbe03
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:01 +0000

    mix aim sale answer level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
