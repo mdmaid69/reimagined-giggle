91ae278c7f81246cfa87bf2a246a53710d10b425
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:55 +0000

    give equipment spend before prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
