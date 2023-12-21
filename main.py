ee266fe3d1c0a97407c19e1edae05adc86323921
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:29 +0000

    pressure crash thumb please pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
