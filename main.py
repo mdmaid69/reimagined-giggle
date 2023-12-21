bf2e15c77fa34bc0ee42bd565ba070069a35c460
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:01:09 +0000

    turnback experience pure though comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
