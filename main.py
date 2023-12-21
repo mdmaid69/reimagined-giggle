f27ed5a88a898bed74773b440e7245acf71e9fee
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:00 +0000

    tough radio rescue behaviour prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
