e78354513bbbc95c081aaad6565e7fca70d5bf7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:55 +0000

    occur callback progress trap test

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
