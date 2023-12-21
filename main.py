b86736e2bef0612ee7f090570fa33af5cb5aaa02
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:21 +0000

    sink pronunciation extraordinary shake measure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
