ff99bf427c02dbe507252f4c9bd9269ec70159b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:31 +0000

    the one round bringdown large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
