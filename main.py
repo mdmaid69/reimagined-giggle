fc6ace8878f33c8f49deea1e5e8e48f51000f701
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:31 +0000

    hair table so spend impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
