7f034810ae18133b9b0a98dc3fe293e075cbfbcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:56:58 +0000

    sweep try height condition today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
