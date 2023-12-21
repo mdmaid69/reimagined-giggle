e309344824e447e5fd88547016bdb66fc69a58c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:43 +0000

    broken floor child disturb test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def convert_to_binary(n):
+        return bin(n)
