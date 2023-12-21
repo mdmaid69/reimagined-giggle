2a88871ab358b73dcf6db88ca302719ad4e40f1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:01 +0000

    keepoff away help young include

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
