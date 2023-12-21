da9adf45171c0e8c12cb82a7ad39138bdecde07e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:44 +0000

    attractive passenger order path container

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_str(array):
+        return str(array)
