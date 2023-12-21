2c347d0b8e6c9a18466c81f132f996f1de493974
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:15 +0000

    pause advice test imagine turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
