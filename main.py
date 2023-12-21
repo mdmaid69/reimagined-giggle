b31a14e3a9340eeba888c9e7b54d12d429b6f20d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:03 +0000

    cough succeed spray criticism pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
