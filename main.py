db6736083db31efb9596271f121f88ca4b4eef50
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:03 +0000

    satisfied frequent odd crowded usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
