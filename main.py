9edc5560b5f997f59d4b2acd52de6e7d4eed1cfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:16 +0000

    unhappy pour question lesson smoke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def create_queue():
+        return collections.deque()
+import array
+def get_array_typecode(array):
+        return array.typecode
