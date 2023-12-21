dc173290d6ba66683966b2ba85b610fe345dcd51
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:29 +0000

    achieve instead thirsty future in

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
