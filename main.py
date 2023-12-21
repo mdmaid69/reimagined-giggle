d8a7102fa9763cb8436e8660c51ef30aee65508e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:05 +0000

    movement goup unlikely avoid cough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
