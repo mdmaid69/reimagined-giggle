28b90c2c13d711e1b1dac8e85a389573b13792bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:35:28 +0000

    appeal boring slowly team switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def create_stack():
+        return collections.deque()
