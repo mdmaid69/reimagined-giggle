81cff44780603a19ea78c898ebd281b39212e5bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:46 +0000

    their downstairs history put attach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
