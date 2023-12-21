2e18a6dcfde5598b69e9fd699df3e11401ec0655
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:45 +0000

    ashamed bag movement test generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
