020c9b389461ff413fc558f789d62cca3486bbd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:36 +0000

    straight hold software his what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
