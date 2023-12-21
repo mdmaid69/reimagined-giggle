587789238851823cc6d028dc702652700795e48f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:10:15 +0000

    danger clothes protection hold carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
