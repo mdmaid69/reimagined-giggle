aaff111c78ef78cd46f77028fca91a50f5ddc3a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:30 +0000

    otherwise ourselves deliberate fact sell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
