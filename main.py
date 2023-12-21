a27d55cffe7e6f910e80fedf88362f7e71a85fb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:17 +0000

    bag nervous change return design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
