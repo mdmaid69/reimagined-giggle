0074ec54698b9b2ba35d595dc915201b9154f5c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:21 +0000

    big fairly roll fact please

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def is_even(n):
+        return n % 2 == 0
