e616da3342e9ff15d624424e43477162e479b647
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:36:08 +0000

    anywayanyhow mad ago drawing comeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_min(numbers):
+        return min(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
