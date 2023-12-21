6ea02a3c3205a8fc97a0323eff387eccdd535610
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:20 +0000

    curious dear goout ready connection

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
