1d94c60af00293cdb63991fe3088ac426c470590
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:13 +0000

    trick defeat pretty steer each

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def cube_number(x):
-        return x**3
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
