05fd81fd78e2f85189b057388d339658b9987641
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:13:03 +0000

    frighten valuable clothes authority workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
