255b7969f6aa357e91853a0c8e11a72065ec8cb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:26:51 +0000

    price last front warning partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
