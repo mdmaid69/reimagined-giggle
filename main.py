35d7ab34b9f4132eed2cf036bfe612bc2e32baf5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:07 +0000

    comeback seriously send brown wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
