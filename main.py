f925a3d43332e92da0aa5b1a8d88ee9702397701
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:13:23 +0000

    welcome violence independent date once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
