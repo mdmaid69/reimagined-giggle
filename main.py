2adb334ff42dffe39130b75e6264c75b7712fb90
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:20 +0000

    friend cheek politics neither cum

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-def calculate_pressure(force, area):
-        return force / area
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_palindrome(s):
+        return s == s[::-1]
