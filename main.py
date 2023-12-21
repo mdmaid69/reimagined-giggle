f41bb10c0cd09727fb441b8f575c671cdc29bed5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:11 +0000

    event be pipe wide whom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
