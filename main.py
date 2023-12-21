8846a9670238aefbb0e0504efc9b3cb21217feac
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:04 +0000

    opinion injury breakfast easy idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
