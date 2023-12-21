47c7514ad04d940f75f5703087be394796a70195
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:05 +0000

    spring freedom thorough label left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_speed(distance, time):
+        return distance / time
