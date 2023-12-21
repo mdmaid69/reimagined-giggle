7be135380c6ea976c29279fc51c6383d4b6ea9a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:50 +0000

    bye few by wall road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
