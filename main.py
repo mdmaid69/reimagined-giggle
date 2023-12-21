90b5e7ff4322d7ddd9d1f49d76632915cc9eee5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:47 +0000

    present important under your sort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
