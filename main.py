29625a7b757e4092edd34968d71d579682f6e1e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:46 +0000

    steady phone dull danger neck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def square_number(x):
+        return x**2
