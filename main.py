c7b6053f3664eff4b9267c66bffdaf2935ca507f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:52 +0000

    proper pure aim key knowledge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+print(os.getcwd())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
