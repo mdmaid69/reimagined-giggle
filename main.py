890a32c9b6c0f7a1985577b416bf8b66681ebb1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:11:54 +0000

    powerful drive notice cold thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def is_odd(n):
+        return n % 2 != 0
