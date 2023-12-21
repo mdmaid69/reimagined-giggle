508814d86386f98c1c0d21dd8420aa924d6552c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:33 +0000

    cross skirt hospital consider tidy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
