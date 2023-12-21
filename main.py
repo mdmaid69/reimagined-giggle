e348fb8c472903f4120609b21174ed6b74cbcd11
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:33 +0000

    form rare refuse ready mind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
