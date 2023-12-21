a5738b73bc990fb8d1461e627dbd2d6c8319aced
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:06 +0000

    card list confident recover after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
