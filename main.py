f0557efb87d0e4bf8d3165171380c4f3b2794001
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:22:09 +0000

    threaten plain approve set disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_hex(n):
-        return hex(n)
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_today_date():
+        return datetime.date.today()
