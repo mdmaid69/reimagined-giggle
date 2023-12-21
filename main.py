f9c3ed02bd7a5b668cab798a673c1f9ad8182c98
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:35 +0000

    prize course wealth doubt charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
