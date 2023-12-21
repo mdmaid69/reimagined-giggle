ddcbf784224802491bd8805ca6632c5d2d713fea
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:19:56 +0000

    fine upset clear behind convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
