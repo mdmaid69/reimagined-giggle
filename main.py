c49ea5e40973c3ad7c38746d19289f30f2579d19
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:03 +0000

    seat extraordinary she scream grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+for i in range(10): print(i)
