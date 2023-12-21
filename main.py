88dae93e2ce039aa3a47de146c49079f58f9b247
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:06:00 +0000

    Uu go Vv remove whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
