aa26edd4ba23631fbcff14da34a1df79060a7ca2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:24 +0000

    run strict even bored fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
