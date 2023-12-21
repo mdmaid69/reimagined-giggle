f8c4f02ce2e91325027753adc53749cc74a5abf5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:43 +0000

    capital afterwards emotion either container

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
