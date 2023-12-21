7203d31858515fb483c99e87028a5a670285a1d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:59 +0000

    exist finish decide responsibility breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_user_string():
+        return collections.UserString()
