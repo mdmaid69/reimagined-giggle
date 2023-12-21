16ae8d09aa18ce96d606ba61faeb7b20b890538a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:27:42 +0000

    popular entrance hurry disagree feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
