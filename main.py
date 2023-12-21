cacd1ff3c2a4d4c7a288599f0ac3d63076b01327
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:43 +0000

    its serious hold television achievement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
