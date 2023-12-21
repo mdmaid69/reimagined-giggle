d86bbbcae3007f2e9e8f3b5858b802d743dde1b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:44 +0000

    ours progress cool field explanation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
