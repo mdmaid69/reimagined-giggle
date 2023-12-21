3c7a4edfef8662928fae7c15f2b79cffe03eae55
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:12 +0000

    behind swim into well idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
