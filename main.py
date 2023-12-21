b3bc77cf2bc9269039a1a396f7a8ee353b99fb24
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:25 +0000

    unhappy scare carry drink free

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_bool(array):
+        return bool(array)
