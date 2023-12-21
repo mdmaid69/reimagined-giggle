6659373372c7ef2d4f17af094599a3c65091f694
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:57 +0000

    disappointed save fear cost fix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pressure(force, area):
+        return force / area
