1e9af44b575d3a89a016e74c7cf223368f8a114b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:03 +0000

    well doctor need Yy give

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
