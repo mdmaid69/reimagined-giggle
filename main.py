08714a7d9982cca8f248ac813450b3aa996a8a38
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:29 +0000

    shrink open winter uniform name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
