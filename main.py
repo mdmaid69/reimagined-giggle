3744e5eb8c541fa3c6f987f58e9935e2f4dfb943
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:00:10 +0000

    fear beyond easy hole big

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
