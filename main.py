a441ac9578457d3e0b29b42ab212abe2e330709d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:37 +0000

    disturb waste skin stick praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
