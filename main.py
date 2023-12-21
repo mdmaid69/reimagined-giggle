e9d9347c99f30416332d4d94142e67a1ec866c4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:13 +0000

    lookaround zero name official number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
