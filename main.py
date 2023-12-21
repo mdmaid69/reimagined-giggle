3b70a61c6c6031adc77ecd44157ed1cd2593dbdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:47 +0000

    weather mass suppose sure instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
