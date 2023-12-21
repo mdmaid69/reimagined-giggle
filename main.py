0da13023e193ef40004146ee80daa99619db5277
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:45:42 +0000

    wonder I ability holdout push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
