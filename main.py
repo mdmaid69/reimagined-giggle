b6c10803d5599b7bcdd330a7b11eb0e6bee85b8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:31 +0000

    job favourite once lesson worth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+print(time.time())
