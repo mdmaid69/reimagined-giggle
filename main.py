bc26a1ba2b9702245b46e841f068b281132323f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:24 +0000

    characteristic Ww change vanish single

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
