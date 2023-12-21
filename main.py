32bd67cc2a105bf579a638daa00a7f1faabf7c1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:34 +0000

    permission concentrate able estimate copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
