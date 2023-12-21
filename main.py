a8cc18af5b0bfbc84a83ca7e063c7313e12a23bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:35 +0000

    mine in total effect next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
