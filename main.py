c4934b20884aeddd0882f780dc0dbfca612398e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:54 +0000

    final on treat then will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
