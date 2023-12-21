2e3118a1d4407a36b55629b522492f170536e7e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:10 +0000

    somebody bite takeoff class almost

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
