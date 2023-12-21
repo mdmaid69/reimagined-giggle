ec62e6bd775c0e8484eafde2705fa8c1b7fbaf5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:45:05 +0000

    sudden lose direct equal excited

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
