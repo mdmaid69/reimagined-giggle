45a3dc6e0476c084eff67b98f4f49ff797ccd503
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:42:06 +0000

    entrance suggest around collection handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
