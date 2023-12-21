c6913f69e44387d48558bd2a326529c7df849c43
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:03 +0000

    term flow entrance quick pencil

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import unittest
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
