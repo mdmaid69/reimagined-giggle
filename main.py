e74fca17c5f55e877340966a8278909e22bad3fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:52:19 +0000

    live substance maximum week their

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_acceleration(speed, time):
+        return speed / time
