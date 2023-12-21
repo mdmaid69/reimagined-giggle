5c48eaacbf8362e18500684bd5ebf7dd83c90438
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:29 +0000

    material nature week hope happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
