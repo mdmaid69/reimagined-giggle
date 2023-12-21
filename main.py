3c076ab69aba4356695f7cbb20aa170c9f4a2f76
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:51 +0000

    sail afford phone cheat measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
