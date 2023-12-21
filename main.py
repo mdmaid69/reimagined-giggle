ac6d584bfe4c60112a19916c01e91f8c8da6deaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:55 +0000

    thick private attractive fire succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
