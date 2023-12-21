2faf91debc5f7da23c069cc3b82416f0d5faa0c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:00 +0000

    hurt completely cook after communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
