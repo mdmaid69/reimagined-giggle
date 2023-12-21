eca9c2c65b7723f309aa9d9ce3903b3388e06682
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:34 +0000

    show makeup pen disturb no

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def cube_number(x):
+        return x**3
