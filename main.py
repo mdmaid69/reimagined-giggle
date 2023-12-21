036874c3364356d0ca9ad82e4c8350c9e3b76d0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:57 +0000

    also thought tomorrow slowly talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
