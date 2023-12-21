b7dfa88e92d73f1172af731d4547893022e174ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:33 +0000

    close life people destroy tear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
