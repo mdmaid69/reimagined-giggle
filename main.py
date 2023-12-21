be4eb489c7a33f6a33c239408d2b797a0328740e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:21 +0000

    influence justice share letter exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+name = "Python"
+print("Hello,", name)
