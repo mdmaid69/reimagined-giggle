48582fc0d9ce3f2e88b32b0bc60ce4d8807bbf00
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:21 +0000

    precise naturally swim female put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def add_numbers(x, y):
+        return x + y
