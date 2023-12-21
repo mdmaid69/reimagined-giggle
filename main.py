68ffad3a6e961a16cf3702bea7f55b9cc486a36f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:35 +0000

    hate thin father gift account

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
