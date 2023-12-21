2cba3d74ca9020ba6cacbb74a6b083154188a86b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:41 +0000

    cover star before address old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+import time
+def get_current_time():
+        return time.time()
