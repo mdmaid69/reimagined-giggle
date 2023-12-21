ee7b0fed269f986a96bcaa6168e4e8f2dd9fee97
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:39 +0000

    slip left hollow trial weigh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
