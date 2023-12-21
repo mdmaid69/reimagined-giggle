015cd666e278e3535f27926af643cbec922ec328
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:11 +0000

    beautiful talkinto excuse out refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
