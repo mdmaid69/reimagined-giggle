cc575f0c7b54161f99b0c3c0c88dda63d1a55e87
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:08:13 +0000

    identify tidy regularly ago truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
