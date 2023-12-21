872499531724dc631ca289b308f432fdc124064c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:29:36 +0000

    class importance criticism account turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
