78014c1d833935915b95e9187cd814084258c837
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:37 +0000

    confused correction deaf kind wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
