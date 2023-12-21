05f5e18baee26cd9f0a259c870e6da1d22689cdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:04 +0000

    dear rain offer obvious smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def is_even(n):
+        return n % 2 == 0
