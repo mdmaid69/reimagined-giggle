fb7ef93df6b3ee06194757b35f8a42ad64debece
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:21 +0000

    skirt less ring steady underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def find_max(numbers):
+        return max(numbers)
