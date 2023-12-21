94507178f790f784ca769ba615758b12965670f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:11 +0000

    demand unpleasant independent practice moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import re
 def find_pattern(pattern, string):
         return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
