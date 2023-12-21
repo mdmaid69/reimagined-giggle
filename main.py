eacfeaabcf7b3f10c4cd4901e0d4c2b7acbad63c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:15 +0000

    post Yy scale lie past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
