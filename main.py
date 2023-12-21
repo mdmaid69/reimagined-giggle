3eba35a52022d738bdd6e5bec5f9fe562df69590
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:31 +0000

    party brown worth name really

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
