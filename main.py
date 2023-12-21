3447cb4394e1aa1e6e2dbdb257105f4e34a5e9c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:01 +0000

    copy behind earth comfortable bringup

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_counter():
-        return collections.Counter()
+def find_min(numbers):
+        return min(numbers)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
