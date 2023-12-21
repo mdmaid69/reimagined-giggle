6c3c410311bd571900494246495f647b25e934f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:48 +0000

    tip set worry often possibility

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
