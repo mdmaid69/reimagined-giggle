84d5c885aed500cda4e13978ba8267b44c2555bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:04 +0000

    spite bear half represent reduce

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
