30f53fd13cf34bd5d281eb7a10617d4eaaf7110a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:07 +0000

    cheat direct within although Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
