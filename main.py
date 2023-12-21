5ae4a34e278cf22d6f72c3988f1cb78a591fffa9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:04 +0000

    below ashamed flow improvement emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 def remove_duplicates(lst):
         return list(set(lst))
