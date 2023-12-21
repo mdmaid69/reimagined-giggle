ff732a38d363c11e2e8fb3e1114098e1b0eae061
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:15 +0000

    change tell along about flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import sys
-def exit_program():
-        sys.exit()
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
