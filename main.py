6b0fa393e37412bd206a4fd1532b00cedff77295
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:43 +0000

    just where repeat welcome born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
