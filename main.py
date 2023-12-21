3ba80094cca44df56009cedf8772052a869e7ae6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:36 +0000

    write hit criticism above meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
