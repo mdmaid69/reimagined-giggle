392635317ea1e2531ed1b9285329395c6777870a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:08 +0000

    prefer protection there dance yours

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_power(work, time):
+        return work / time
+for i in range(10): print(i)
