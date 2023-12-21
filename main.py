bf071b072b8654e91a2fc7fbc136b97ce33ea306
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:06 +0000

    fresh blind less fresh treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
