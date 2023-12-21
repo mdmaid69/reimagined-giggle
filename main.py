6670d43853d88ab8a53c7376310374753547da02
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:47 +0000

    better gate quality voice market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
