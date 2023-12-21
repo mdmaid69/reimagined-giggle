731e515c2ca5f06e41d572574afeefc73759d193
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:43 +0000

    like sign food discovery hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
