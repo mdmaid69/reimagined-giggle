798fb415e8cb24aafb0410e32835aa69b3b22400
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:36 +0000

    medium interested stay last popular

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
