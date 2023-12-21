0b4436b31ae9563404ba04ff8b308c7d0ea89cde
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:21 +0000

    which sticky please himself male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_directory_name(path):
+        return os.path.dirname(path)
