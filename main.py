6c2d4f5d4daa51894d078abe8de38b0a2638285e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:21:23 +0000

    thumb food Itself lunch spot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
