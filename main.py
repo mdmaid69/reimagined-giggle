c7d523fc80581768b8faee8d2131bf2da75f68c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:22:54 +0000

    we lookup favourite breakin character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+def get_current_date():
+        return datetime.date.today()
