9569624d0fd7ac703dd9c5702ca717e39187c68c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:13 +0000

    argue stand praise advantage detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
