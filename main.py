fef329eca13a3ccd1ba4185e774b1b1295b1e4cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:36 +0000

    flow sign keepout life follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
