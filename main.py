b910c70c6284677bec9558f566e003730eb0b9e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:03 +0000

    young spill goon lucky shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
