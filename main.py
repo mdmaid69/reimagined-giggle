e91f81ad5fd1f5f9ed669091f33ee6afcd664dd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:55 +0000

    set mind terrible year mail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import time
+print(time.time())
