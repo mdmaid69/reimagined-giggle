3c029f2bd9f17428105388ddad3b7dab7eef19a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:44 +0000

    minute force afterwards dance sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
