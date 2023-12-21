5f9fe3f2ef7dbc931b76844fa2776bd46b2bf7d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:24 +0000

    still test defeat experiment preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
