0fc57a3ce421de354579a2030ced7f928a098e2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:59 +0000

    spin fun stop soak either

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
