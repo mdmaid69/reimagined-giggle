e14b0bb4fb94299ba9432128cf6e1e1f62193f2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:32 +0000

    shoulder warn normal shake ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
