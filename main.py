7b208442ea328266f5fc34ba61c11b6f8dc5f14d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:55 +0000

    against interfere cut completely takeout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_min(numbers):
-        return min(numbers)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
