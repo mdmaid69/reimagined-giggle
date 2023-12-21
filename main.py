b5335bc196aa1fc3851bd6fab9e4e95be200ac4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:34:59 +0000

    challenge holiday free joke or

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
