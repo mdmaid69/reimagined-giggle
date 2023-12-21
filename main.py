5bd416f3c5df8bdd3ec44e1e63fc41c411212c78
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:45 +0000

    my smell possession which receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import time
+print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
