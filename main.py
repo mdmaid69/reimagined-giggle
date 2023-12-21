b4df550463c6ee7c610398847a95b044e2e2a992
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:39 +0000

    unless precise probably cook future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
