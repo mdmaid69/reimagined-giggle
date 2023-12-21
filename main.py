91b957bf1ad7605213f030a83782a6ba6cab1b6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:38 +0000

    compare advice debt path new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
