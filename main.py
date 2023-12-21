d3cf7fa0a7e97406955108101b4647e7fec382da
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:01 +0000

    determined importance grade on lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
