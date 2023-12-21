b7830eefe8a990e6f7578305b2bcabdbc5fc5f6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:21 +0000

    interest uponon muscle responsibility later

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
