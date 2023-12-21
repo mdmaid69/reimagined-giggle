49c3cc36936920ddec06e987805c4e687c81ce10
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:50 +0000

    knowledge you level design history

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_acceleration(speed, time):
-        return speed / time
+  def cube_number(x):
+        return x**3
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
