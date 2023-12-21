b54b57310d92da8b7cf1ff2a80768068f0c7ad67
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:07 +0000

    gradually slight boat safety tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
