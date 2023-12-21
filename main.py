4ce20476fa57ffc7fe93a2fdbbbffc6c2a94279a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:59 +0000

    basically shall tune hot imagination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
