72dbe789acfab3ca2b9cab69cddf809f26e30312
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:59 +0000

    their finally short related his

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
