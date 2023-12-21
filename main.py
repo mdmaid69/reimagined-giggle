fdfcf0f7d60a790f5bb6280f874d8c232f7acd74
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:49:13 +0000

    cross turnout eye clue sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
