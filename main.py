69ca7161f4607b6142a52d0aa72a1126c27d4873
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:15 +0000

    same interfere unite convincing possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
