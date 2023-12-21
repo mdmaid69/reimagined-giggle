8a02de82c75539541c42e22fdf4001b2182df0a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:37 +0000

    quality nor congratulate low frame

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
