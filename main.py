ab72c252180dc7f339d87b2674ab0c10e6ecc418
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:39 +0000

    remove precise collect 572enter pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
