c2920dd0429a51e9fd4a9dbd9bbf6cdec1ab5f47
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:12:03 +0000

    still turnback visit measure defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
