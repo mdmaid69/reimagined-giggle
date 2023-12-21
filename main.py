537647792469e5ce9f755ae5d42b0e4b1babe095
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:49:04 +0000

    modern discovery judge demand shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
