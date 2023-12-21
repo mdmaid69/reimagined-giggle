5d42ad9b6cf88cb886f23dcd0e2e1fc8176257c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:08 +0000

    authority kill forward my direct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def sort_numbers(numbers):
+        return sorted(numbers)
