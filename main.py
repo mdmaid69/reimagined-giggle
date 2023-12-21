9547ec251aa76a1a26982c73b8b6d82920b72e28
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:39 +0000

    something correct deal offence offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import random
 def roll_die():
         return random.randint(1, 6)
-import collections
-def create_user_dict():
-        return collections.UserDict()
