16134cb89390e9d08f81c09485c5f0b320041e31
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:26:16 +0000

    invitation direction in payment reduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_odd(n):
+        return n % 2 != 0
