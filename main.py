d8fbe9607a47bc84a441dca497d7bc6953e2edf9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:57 +0000

    sometimes giveout inform satisfactory loud

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def reverse_list(lst):
-        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
