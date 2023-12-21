66fa901d61cdff435e9761372769471139473db3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:33 +0000

    flow apply callout frighten print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
