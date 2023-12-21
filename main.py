ffac8a189345994a4310b95be179a8cfdde70471
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:02 +0000

    relief light road estimate place

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
+print("Hello, world!")
