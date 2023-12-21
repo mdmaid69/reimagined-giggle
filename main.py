35e66b6c3b8401843c424c027876257ecf89d759
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:49 +0000

    scare suddenly foot late circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
