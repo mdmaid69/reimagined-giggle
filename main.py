e15479b089f6c120f583ef861af581fa34b22983
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:43 +0000

    gate bush argument mouth fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_palindrome(s):
+        return s == s[::-1]
