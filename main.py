c3e590fa049b0faa41284057b6c9d0cfac1dddb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:25 +0000

    ignore person without wish floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
