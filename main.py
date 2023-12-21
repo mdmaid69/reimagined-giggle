ead414b983ce831bbff6f7e4273cba3fc586ec25
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:10 +0000

    come suffer correct consist sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
