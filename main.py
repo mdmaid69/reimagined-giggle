1fc6870481d778ffb6794795772f308fcc7a784a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:07 +0000

    recently runout scrape beginning Tt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
