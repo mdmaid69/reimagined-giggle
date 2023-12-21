35bd8879011f9bafb73ab04e915aa573062b0a6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:33 +0000

    avoid demand hope form roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
