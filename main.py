b41f2b114275867312d77534f76633892c77fe45
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:17 +0000

    mix badly sore really thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
