1bed995d17457734de5b7c8d95bc093641c1fcd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:25 +0000

    freeze less mother switch arm

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
