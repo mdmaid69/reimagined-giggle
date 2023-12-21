27993e09d63707e2f74770a24fc997fb82f3e235
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:22 +0000

    Gg thirsty keepfrom whistle reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_min(numbers):
-        return min(numbers)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
