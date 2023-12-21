d75203fd96fe5099b623a5b22ff935818ad166e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:09 +0000

    now challenge indeed success tell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
