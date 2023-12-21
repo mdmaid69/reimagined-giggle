72aca112e8a9753b65e59fd1cbc0891c92d9bb6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:13:51 +0000

    large idea build leg reason

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
+def subtract_numbers(x, y):
+        return x - y
