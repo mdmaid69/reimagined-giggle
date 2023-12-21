a8ff4eae4e872ae79fb7866d16775a6664141175
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:48:41 +0000

    favourite frighten getout patient closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
