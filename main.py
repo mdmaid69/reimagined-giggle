84154c7f974647a2d03e86fe0aa306562e1563a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:59:11 +0000

    over unhappy behind cough slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
