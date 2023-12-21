6f343be154e4d9dec363748325aacd5695265835
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:31 +0000

    wish refer hollow shoot him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
