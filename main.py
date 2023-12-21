33866d9ee4e06f37f3921773bdeb1294514406e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:52 +0000

    risk comeup who flight fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import json
 print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
