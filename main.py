a1b02e832fff8f597af591b4726f369dc5f0ec0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:49:36 +0000

    we light put afford press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
