6285870a6ad4e9a0014cb39cb7a931fe3450208d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:10 +0000

    collect worry must nurse appear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
