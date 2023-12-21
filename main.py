59ef6395d6f966b61586427e2c3db0b8e119415d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:00:19 +0000

    respect pick born ask strong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_string(array):
+        return array.tostring()
