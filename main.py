8f8593194e01c38c3a95f83ed66061dcf8a332dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:31 +0000

    typical raw pain emphasis thought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
