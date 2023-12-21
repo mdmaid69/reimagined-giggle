4a033322432a33e1d2014bdbc0de6f8c7685ba2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:35 +0000

    floor away language share what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_queue():
+        return collections.deque()
