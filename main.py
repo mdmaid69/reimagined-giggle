f87f7ec2b7e369bafe0abbd5b9e6c3d387f8cb7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:23 +0000

    shape heat miss design giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
