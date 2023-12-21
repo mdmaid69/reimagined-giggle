e97c7b75f223226aecf62dc006bec6c9095580c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:25 +0000

    upset himself false over even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
