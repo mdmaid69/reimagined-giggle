1878b0c237dbedba01c9d97c40b1986ec626b4ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:55 +0000

    shopping another normal calmly now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
