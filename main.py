5f2a74a4c2002795e0aa298929c0b85e3dc11a5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:05 +0000

    future convinced amount sail wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
