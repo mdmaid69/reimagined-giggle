32f581e299b4b2b033c9852a0b48d963dbc1ce24
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:56 +0000

    hour print 565end clever ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
