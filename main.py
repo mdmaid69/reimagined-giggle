c9c1542a6e4f0b9d5eab57f5d398aa6b92556331
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:09:57 +0000

    crime reserve by declare conduct

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
