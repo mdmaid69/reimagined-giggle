d41f29146078a5140b809c7b7c7d8548a4cdf2a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:02 +0000

    paper land mess doubt greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
