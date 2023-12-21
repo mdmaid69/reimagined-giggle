82caa16f5fa0604314165d47f8e57b1e9e5c9a3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:01 +0000

    step blame test room slide

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
