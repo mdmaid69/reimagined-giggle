07c38ecae8ee30e20d55c27566dabb41db176992
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:55 +0000

    petrol hurry list more bringback

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def convert_to_octal(n):
+        return oct(n)
