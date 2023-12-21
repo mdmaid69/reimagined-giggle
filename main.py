68432d905570801d3b9f23d7998fb95b4410f349
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:07:42 +0000

    heavy there thick store behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
