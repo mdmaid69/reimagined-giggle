b6a7858e222efade0a0adac25542099268da296b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:56 +0000

    public classroom nearly wide interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
+import array
+def convert_array_to_string(array):
+        return array.tostring()
