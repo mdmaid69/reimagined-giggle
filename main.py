3d119ba2bca06973a9939e78bfee4062d62f8331
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:27 +0000

    rain please far necessary care

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
