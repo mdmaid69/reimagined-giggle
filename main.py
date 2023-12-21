dd2a25b8b56a55ae09c71a66348bc9b08515d461
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:17 +0000

    join dream suckme conversation Ii

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
