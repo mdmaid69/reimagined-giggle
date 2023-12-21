9d82973e0eb947405ae3f8c33db3d2443bf30071
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:13 +0000

    sudden home absolutely fasten interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
