0143fabda32c36d7b54b58b7132cc3e805057d2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:45 +0000

    dig part cap future die

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
