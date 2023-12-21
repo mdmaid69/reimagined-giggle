b0df0afeb55b7be8a0b738e5ec736947d3818ef9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:51 +0000

    feeling touch fail ground prize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_speed(distance, time):
+        return distance / time
