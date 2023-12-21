c86e47b6cd0f791a2acd9ea369a7955c3e4b3f28
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:42:13 +0000

    dream wake honest sensible complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
