196fd8728a002a8d9a4b4245c53ff019b989bfc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:05 +0000

    quickly store open moral challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
