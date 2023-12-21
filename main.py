198b7a42c85734dacbce50d653f3d0018f6aae3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:20 +0000

    opposite trousers dance perform worried

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-def multiply_numbers(x, y):
-        return x * y
+name = "Python"
+print("Hello,", name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
