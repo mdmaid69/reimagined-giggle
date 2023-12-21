d114bf9ca761fbc1b2804768c158e3370e8eda3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:08 +0000

    ring rich mine group test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
