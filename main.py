792687afa9230c059af49b4cffa9d79faa41359a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:11:00 +0000

    first paper poison often far

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
