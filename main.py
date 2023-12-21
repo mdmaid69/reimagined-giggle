bb3434823fb35d9c322bac56fb42e58c55170914
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:06 +0000

    opponent under slightly design part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
