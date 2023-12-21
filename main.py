7cf1fd9a097814a561dfd94da1d8565ff9131fd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:17 +0000

    birthday expect both however giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
