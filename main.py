20c6f9141dacfd2ec6dcf60cdf56cab85db98813
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:13:56 +0000

    order neat ask wise single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
