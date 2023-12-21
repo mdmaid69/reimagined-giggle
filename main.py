6da88279f193f799a37b44c751c72b0d378fe953
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:19:58 +0000

    base formal accept behave knock

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def append_to_array(array, item):
+        array.append(item)
