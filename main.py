a6ba07aa092e84471297bf4a03baabe205b8ca1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:56 +0000

    later sea suffer bill hour

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
