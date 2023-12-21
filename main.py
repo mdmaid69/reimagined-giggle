d02b7180e858684a2cc189b90aba61ad69eb86be
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:39 +0000

    short trip pool definite store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
