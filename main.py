02dfa8072686917eb8dff76fc1cec07cffdc56b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:03 +0000

    false wash front run can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
