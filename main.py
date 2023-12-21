957151d1063b10a5a6e8fc46bd5521656ee3428a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:51 +0000

    importance when hollow spin mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
