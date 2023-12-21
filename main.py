cc46d54ec3844857c4d5ab436564d01da304f7d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:56 +0000

    in reaction wonder second shoot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
