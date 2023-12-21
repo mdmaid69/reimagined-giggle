671a424c3af7de04bbb6bd6ed1254e268d44fd41
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:40 +0000

    wealth holiday major colour threaten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
