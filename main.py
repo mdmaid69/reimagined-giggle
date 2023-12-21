783e635e064df8c129c948704891af655a5a1183
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:35 +0000

    profit Ll work comefrom reserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
