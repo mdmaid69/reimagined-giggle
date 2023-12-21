3f46e03296f74f431f6fea550a627c68492ba19d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:37 +0000

    his line basis workout partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
