69a17c2c0912ed0b4ce1e8c404a6d352a08012db
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:11 +0000

    goodbye else chat include lesson

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
