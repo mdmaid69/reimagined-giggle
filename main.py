d52d9b0a8fe55e3d162288457ac3047d2727744d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:23 +0000

    late material read certainly soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
