de3ffeaeb809ad769ad16a2b51cfb6d4ebeabe64
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:27:30 +0000

    pattern broken stiff stranger gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
