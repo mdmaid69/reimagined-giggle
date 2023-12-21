1c49b30ceffe2f0552a5b927003560d1123082b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:07 +0000

    holiday repeat forget crowd draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
