8fecd49add7e7e3be9b58ba2bc361361282c8790
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:39:35 +0000

    lend whole spend cheap bang

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+for i in range(5):
+        print(i)
+for i in range(10): print(i)
