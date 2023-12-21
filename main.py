8afc3dcf8752ff8c2eb964e00c985c1088ad4070
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:33:30 +0000

    peace hope crazy rain treatment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
