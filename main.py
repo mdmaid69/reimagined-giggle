0ed0c6176a967b3989a33a3127a7a88ceb519b83
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:38 +0000

    sing schedule place demand bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
