37f545a0a08ffeed5229ee57519b788c7f3e6972
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:18 +0000

    Zz 569enjoy radio pressure so

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
