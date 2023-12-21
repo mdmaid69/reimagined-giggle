d4781a1b1ff3b52b839d5d9c4fdd5f7b7c799497
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:07 +0000

    house false offer turn pour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
