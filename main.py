12ef5254e2d81beb58702433d9a0a53435350c3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:21 +0000

    condition rarely frighten wing smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
