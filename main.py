cf7225d1530cd77ba3621ca6f8c29543068af03f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:48:11 +0000

    listen allow authority on goon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
