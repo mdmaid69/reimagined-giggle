dcbeb1dcfed0259181e681f80b3c793557ab8e68
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:49 +0000

    work press present guard government

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import random
+print(random.randint(0, 100))
