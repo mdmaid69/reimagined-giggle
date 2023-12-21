ee021a1da39798e24419aae044a747fd5472dde4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:21 +0000

    couple gather characteristic result disappear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_unique_words(sentence):
+        return set(sentence.split())
