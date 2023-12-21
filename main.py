75944a0b706cdf9bcf97a85e38342f6aa0eeb956
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:40 +0000

    thirsty giveout fight stiff slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
