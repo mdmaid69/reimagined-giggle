48211a9763a1b9b8f33e37e26b71244062f6b620
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:45 +0000

    worry exchange fork prize shall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+print(random.randint(0, 100))
