cfa426862bca3a6f1fc04f3df989be6767aa0ac2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:44 +0000

    activity sting final make reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def add_numbers(x, y):
-        return x + y
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
