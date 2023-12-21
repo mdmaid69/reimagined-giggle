d397df3960d1a51d3c6acaf4df8959a472f7f1f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:26:47 +0000

    dish unite easy argument star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
