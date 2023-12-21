0b2d630425e9cdfbfb4c4c9340fe5d07413e8524
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:00:01 +0000

    not comeup translate deliberate advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
