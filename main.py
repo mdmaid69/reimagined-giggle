fc84f7c27bf5697de09a2cc15bbabf5eb81ad553
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:47 +0000

    plate equal breathe naturally emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
