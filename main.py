0300088414bcce81ad62df06df96e336d3c33bf9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:43 +0000

    clean politics believe circumstance cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
