b6b040af8366d4c562934b0141be6fc59684fc60
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:43 +0000

    cough water minute top cruelty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
