86d0328383cb98d1839363bb113d0efc84d59da8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:04:20 +0000

    maybe pass goout pot rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
