de0db4c52228700eeb9e3596492be62fa06ff8a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:39 +0000

    song successful although lot influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_list(array):
+        return list(array)
+def cube_number(x):
+        return x**3
