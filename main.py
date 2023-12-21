6d5e44c6702624f937aff752cb99c42a2b6ddabc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:46 +0000

    private communicate proud write boring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
