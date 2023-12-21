b4d0a6d9b4d02f12841c0bc06f8cc1b2aa5b9b20
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:38 +0000

    breakinto reserve shout lesson ought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
