76ff840764945b927f27baed96c0b550a60642e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:51:16 +0000

    offence yesterday among still inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
