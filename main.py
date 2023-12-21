47903d72804a0fa2c874516eaed48b1ae50ead62
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:06:45 +0000

    war science 568energy us stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
