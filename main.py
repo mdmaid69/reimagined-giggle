6ee51911c71ee0e82ca6e11b900ab6182bedc198
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:52 +0000

    regular compete regard notice contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
