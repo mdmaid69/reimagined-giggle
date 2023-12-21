e6360ad38511d7ce007260de462464c699b2d332
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:48 +0000

    union grateful sorry pain different

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-print(sum(range(10)))
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_cosine(x):
+        return math.cos(x)
