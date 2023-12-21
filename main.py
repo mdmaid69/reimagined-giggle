1a0836933e9700d8e9f036fa3087f3128e6446ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:21:29 +0000

    danger main schedule raw about

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
