a4f5b743c8f861bff079dcffaa50248bdd359f42
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:23 +0000

    benefit nearly please either take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
