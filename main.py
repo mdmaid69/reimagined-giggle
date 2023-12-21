121e69ca61cda9d6bb38e998827dea8fa5ba23a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:41 +0000

    comeout comedown meaning interrupt anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
