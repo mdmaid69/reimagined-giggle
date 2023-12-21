5e86e8b1346b259201400c481d2f004bdfd3db9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:38:59 +0000

    wash control rise crash goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
