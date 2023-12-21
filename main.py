b98699b6668dc022d46e0ae945b898598e9d6e3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:54 +0000

    professional skirt story blue impression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+name = "Python"
+print("Hello,", name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
