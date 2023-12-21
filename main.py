8fc1f9af31a4f4b50b32f8bcebe5f50589bbab16
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:22 +0000

    call goup exchange somewhere young

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+print(math.pi)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
