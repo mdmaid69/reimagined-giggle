8bafa38485a57cc74fde594f802691cd803a3aa8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:33 +0000

    single future seem late wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_str(array):
+        return str(array)
