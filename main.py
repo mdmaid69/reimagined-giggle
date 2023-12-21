aebdaade389d92c847ce6727d7fa2429e292bd74
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:17:55 +0000

    tight offer surprising sticky main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
