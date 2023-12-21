0dd07cc95c39b51d1f94c1d89b206039d92b1496
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:12 +0000

    office service so account around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_counter():
+        return collections.Counter()
