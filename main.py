9733dd53dace5ce50a53093218782644e25a590d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:27 +0000

    honest pleasure sink fit change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
