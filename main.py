385ff4bb89a45f0212167a35c46cac98d6536347
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:37 +0000

    opinion start your shut row

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_work(force, distance):
+        return force * distance
+def calculate_density(mass, volume):
+        return mass / volume
