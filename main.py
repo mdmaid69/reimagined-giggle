f9a7bf9b5201c56d6c822ea4ccc905f626adac40
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:14 +0000

    now shout music pump drop

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_typecode(array):
+        return array.typecode
