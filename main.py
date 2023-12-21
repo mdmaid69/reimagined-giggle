6c67f8666c47b5c74792c6bf216fac56c4c68e03
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:45 +0000

    the ticket set personal wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
