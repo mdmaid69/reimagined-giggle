6d819d58e8c8013e9c28459998a0e2a709746433
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:53 +0000

    noisy Dd offend shout independent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
