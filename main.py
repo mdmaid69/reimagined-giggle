6df0da9b4a392d40e13d9744440246f0b4eb8e5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:06 +0000

    nose include key insist run

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
