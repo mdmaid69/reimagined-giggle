90a76f8e10ded01a30d27aff64458776d410a9aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:39 +0000

    famous visit waste guide get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
