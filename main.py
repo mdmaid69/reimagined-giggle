f2db9e1e484c2308eb610aacb8d484efad750f82
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:12 +0000

    impatient left criticize cope hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
