381c2259fca2a5c37b865d41c974318991f0d416
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:16 +0000

    error hungry 569enjoy breath rare

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
