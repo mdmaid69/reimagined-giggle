25a720c15593d55a9c22b81daceda496385d0e85
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:59 +0000

    warning do throw place dull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
