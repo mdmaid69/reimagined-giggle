3db53c5ad5c64dd1c9b9c4b3dd98b0f54d4faebd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:49:17 +0000

    careless bet wealth substance undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_as_str(array):
+        return str(array)
