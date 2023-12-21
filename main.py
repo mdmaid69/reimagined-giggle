267a1c0778a86a073d40dc87deec8df9a986c3b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:31 +0000

    touch seriously rather rob close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
