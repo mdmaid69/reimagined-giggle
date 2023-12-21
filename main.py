5039aa280fe27b9d23139890df007dc9608c923a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:55 +0000

    gooff powerful right pass party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+  def get_base_name(path):
+        return os.path.basename(path)
+from collections import Counter
+print(Counter("hello world"))
