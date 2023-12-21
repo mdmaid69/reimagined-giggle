ee70865964c6719db8701d13e32da3a6f1654226
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:01 +0000

    possibility across chance bound turninto

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-  def cube_number(x):
-        return x**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
