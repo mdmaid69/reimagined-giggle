77a760f9286d29694a8bad3d46718c2f4fcbd012
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:38 +0000

    cruelty ours hurt provide lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
