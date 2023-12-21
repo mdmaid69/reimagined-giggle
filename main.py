f21752c3aa02559a8b3e54bf7543d4bd5d6c71a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:35 +0000

    kill Yy communicate draw official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
