5c360027499e81fdcdae1e8f9baa38f5398bb528
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:18:56 +0000

    along design book underneath turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
