233ec065ae83f12865dc63e2f45ea3050e25ccee
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:17:14 +0000

    much still getout perform education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import time
-def get_current_time():
-        return time.time()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
