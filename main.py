6dd1c283d38bccd691ac20ba7ef77fde39b289ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:47:03 +0000

    giveback either pour finish activity

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
