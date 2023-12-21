660471d4f07feb4afe38655e3a56aaf18b6fc391
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:13 +0000

    power spring sail cut always

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
