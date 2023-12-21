a389a21d62fdb4d8714f68da54138cd2322dbef5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:55 +0000

    excited control under takeout extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
