7314230adb9bdfcbbfdcc8ce379d11cda7b7cc1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:18 +0000

    key excuse change likely pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
