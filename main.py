8dd5ea723bba66d923e2f0be3995fb01b08e6e35
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:57 +0000

    place reliable plan still famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import getpass
-def get_username():
-        return getpass.getuser()
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_str(array):
+        return str(array)
