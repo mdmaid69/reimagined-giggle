145ef428713e84f27df1fe4c49ca8b4436eb804b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:01 +0000

    regret adjective represent ought female

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
