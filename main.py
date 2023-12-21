16355f5bdf37e7858d10d558dcde812841d82177
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:38:18 +0000

    anyone state trip quickly connect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
+import platform
+def get_python_version():
+        return platform.python_version()
