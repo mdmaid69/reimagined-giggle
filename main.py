1dfa966294fc10f038a9eb8a0c449aefafa7c14a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:26:37 +0000

    slight my meaning strange colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
