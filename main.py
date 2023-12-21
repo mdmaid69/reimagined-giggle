b141908404198f42e08e3b8bc7c1df65bebb47bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:14 +0000

    underneath criticize organization moon instruction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
