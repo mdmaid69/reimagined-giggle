e625fba0706c8df5262313f55f78faaddd26e109
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:28 +0000

    must movement tear nothing there

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
