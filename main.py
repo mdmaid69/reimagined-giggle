bfbb93f1a0855446fec01ae372ebacecd214e718
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:53 +0000

    involve translate miss Cc promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
