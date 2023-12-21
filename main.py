79f90aab6cb57992feaed3e9f57f993f0ddc5c7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:34:34 +0000

    accommodation borrow appeal die stone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
