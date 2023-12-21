92bec6616fe0a350807ef5bb48d141b957c98e19
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:36 +0000

    make settle cap upset rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
