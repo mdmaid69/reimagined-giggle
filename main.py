2e1346e4db8bc35af666fa2dc539e19bdcd75c31
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:29 +0000

    unhappy drive national minimum middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
