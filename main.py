863f8b080af69c823bfa8db7915a5f42db4c10ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:40 +0000

    niggaacademy weather surprise branch love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
