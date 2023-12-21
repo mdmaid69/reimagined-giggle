166f34c22131cad4ba1c154f96943a8d6250f611
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:53 +0000

    tonight opposite flat Ii will

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-print(math.pi)
+import sys
+print(sys.version)
+def cube_number(x):
+        return x**3
