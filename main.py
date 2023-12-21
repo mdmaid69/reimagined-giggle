810d172697922cbb0c76ef4fb528f3913fc6fa53
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:06 +0000

    mistake original tidy treat wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import json
   def convert_dict_to_json(d):
         return json.dumps(d)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
