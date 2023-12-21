e9a79ee0284b24fd5ef20c97dcb86f25916d76a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:40 +0000

    either upset everybody dangerous pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_str(array):
+        return str(array)
