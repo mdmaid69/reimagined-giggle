0309fd24d4f1ade7bfd4eb39bb4c1291219a43f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:58 +0000

    since dance action length separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_str(array):
+        return str(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
