e1550b29d5d620ad50c916df19cbd759259f5dcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:13 +0000

    news organize how interested place

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
