98f8ad18529e834195e29ad9fdefc202142e8e0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:46 +0000

    extraordinary anyone unfortunate base blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
