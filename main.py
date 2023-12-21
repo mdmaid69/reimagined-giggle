27242976d678ea6394cd54c881d10ca85dea2763
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:53 +0000

    I unfair talkoutof fresh wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
