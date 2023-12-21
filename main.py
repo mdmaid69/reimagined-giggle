d1abc8f2bae0fc475fd894638bce30be6c473eab
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:34 +0000

    beginning reduce general extra part

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
