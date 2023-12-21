7e0862d5ecc31a7217aaff7f0f0ab4f4b453e60f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:27 +0000

    claim prevent transparent disappointed everybody

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
