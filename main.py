44258957f1cfdb20efe9a28c6eba4f4df62b21d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:55 +0000

    tend gift common aware geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
