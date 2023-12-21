1c572ee8800d048751148da1b4d916f502d3225a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:11 +0000

    visit hard meet field ours

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+for i in range(10): print(i)
