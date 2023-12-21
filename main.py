6bc57982a5d5e605c7a1cc8329486548c86d1ca3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:50 +0000

    step fit intelligent tone explain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
