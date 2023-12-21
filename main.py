1af26b1634e6d2de8a08faa6ceb99e6a1ff48de1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:00 +0000

    turnback operation surface power together

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
