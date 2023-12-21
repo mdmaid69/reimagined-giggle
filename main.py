117de08a1b122c82bc5910d187e2c736041a9964
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:46 +0000

    type strike cheque manner could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
