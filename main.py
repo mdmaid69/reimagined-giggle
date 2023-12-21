a78329747e7acbdfd2161eac134754bd0576cc48
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:00 +0000

    bridge worse prove matter limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
