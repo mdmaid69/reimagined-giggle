06d8ac9c4f6b4fab35a32e35cc1eab2d657e88f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:48 +0000

    mine autumn male check fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
