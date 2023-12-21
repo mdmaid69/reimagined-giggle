8c5d5352a568f24024daeb65680956c1a590a7ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:50 +0000

    valuable rare dark then wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
