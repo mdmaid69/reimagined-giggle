ecac5faaa0a4309e6346c5874e5627a040daf270
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:52:22 +0000

    strike great receive slip result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_as_set(array):
+        return set(array)
