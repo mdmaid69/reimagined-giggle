05dc95855465980091be2f1f75ac315f351d47dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:12 +0000

    dinner middle he several mean

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
