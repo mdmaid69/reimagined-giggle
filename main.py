ebc5ce617cabe59e12f2142507dacb4d1e11037b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:16 +0000

    window lookback(on) say work splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
