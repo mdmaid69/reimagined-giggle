82b481f701df1f9172380d001928c94196647d4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:28 +0000

    ashamed tight come pile sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
