2e52e0aaded2375fb50505ac1ad77d3634db8497
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:44 +0000

    stick only powerful goon quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import sys
+def exit_program():
+        sys.exit()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
