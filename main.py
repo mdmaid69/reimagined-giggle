2c39eb08385d7b4e4a314680e9adf4584db986d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:15:38 +0000

    carry takeover stage contact affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
