2c510680b84f7d888ab0b31ac0cc503af3398380
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:01 +0000

    belong complain none surprised estimate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
