909c6809d4d368c87bf7c40e882fdf5a5df61298
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:05 +0000

    system quick standard moment ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
