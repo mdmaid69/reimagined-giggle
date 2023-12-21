7859d52e08eba0aabbb8ae3cad05c856e30bafe4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:52 +0000

    chance capital plain exercise runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
