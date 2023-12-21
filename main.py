70fb590e958f964a5fd667f707abaca45904b8e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:31 +0000

    could cheat issue serious chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
