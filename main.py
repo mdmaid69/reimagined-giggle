d5a7d38788f9c01971668228534945ffd5e82277
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:43:17 +0000

    wander Ii trip sure practical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
