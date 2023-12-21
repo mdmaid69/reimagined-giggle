dcc6dbf52196c9ece5003e1fb12ec2af2b33e421
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:38 +0000

    though aim bottom right interview

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
