566a2a4fa0b7ae6d8a2e3dfed114caa234af834d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:40 +0000

    forever bright wash open my

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
