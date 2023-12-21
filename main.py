619a26743469668c419a0e42659f3c200828a1d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:17 +0000

    traffic nerve fond afraid fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
