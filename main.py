931b455a4dc72bd9d6c2727933dcbaeb0b13a9dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:15 +0000

    without health education holdout than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
