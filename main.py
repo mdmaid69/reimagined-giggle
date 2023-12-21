f208f0c07faa28eab798c752fa8c105e6527edee
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:26:59 +0000

    shallow square enthusiastic previous no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
