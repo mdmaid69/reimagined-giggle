4cbd8424db174620193a6f7f81125d9edb259d33
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:43 +0000

    dream climb fill base lock

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_item(array, i):
-        return array[i]
+print([x**2 for x in range(10)])
+  def calculate_area_rectangle(l, w):
+        return l * w
