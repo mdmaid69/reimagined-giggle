70c243ddf85d181200203dcbf91d2d0629ccf921
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:32:33 +0000

    path cough because shape please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
