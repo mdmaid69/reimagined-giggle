368e80a4456872ce74118894c8333e36c4622819
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:51 +0000

    comeoff advice bet arrival ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bool(array):
+        return bool(array)
