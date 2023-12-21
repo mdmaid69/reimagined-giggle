3e1a36c0c4ef989a926199f3c24c96bff9d8a9e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:55:03 +0000

    plate last blame slide shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
