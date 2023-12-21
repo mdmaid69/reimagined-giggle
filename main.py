4bbb45cd6708efa20e4a44ef349b37b22db8d627
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:19:28 +0000

    under shake share scare party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
