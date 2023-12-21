a8f12d836e60dc56b5c2385563e05189105fa9c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:58 +0000

    charge turn national taste otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
