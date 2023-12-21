9aa970742ee5588e6a3504c9fb3a521b53b9d510
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:00 +0000

    invite reasonable forgive recent lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
