618d344a4a269fca07d98ea97113545be856ee7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:34 +0000

    cost between would steam equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
