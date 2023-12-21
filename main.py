0c006a779998974f513c93d8dc1d64bd4917d91a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:20 +0000

    cheek eat stranger cope bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
