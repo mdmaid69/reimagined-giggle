c3fb50cf2dce7f2050f909442be6d88941a84880
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:20:13 +0000

    sleep will sleep sort keepfrom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
