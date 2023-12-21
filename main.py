83547e8e4d7bd05f7c85c4da81e22bfc74f22964
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:13 +0000

    recent choice say live medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
