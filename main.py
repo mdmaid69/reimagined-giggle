12d16a0d98b174551a9160799f906082c7b3cdc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:07:11 +0000

    ocean hole switch reach chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
