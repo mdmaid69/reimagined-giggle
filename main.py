4e2f39ff43347421656c3c1e4fd54fb6e63ddc6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:43:25 +0000

    board spring persuade fit crazy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
