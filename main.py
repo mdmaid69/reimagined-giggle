a90c188ceb72e8aca5f88f08bb3c278a60659429
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:08 +0000

    show practice heat trust lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
