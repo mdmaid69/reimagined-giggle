2e7006ffdc08096f154972fa2beb26bae339b560
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:46:57 +0000

    lucky likely use mood closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
