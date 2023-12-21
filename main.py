8a114a8a414d302f29555120d1bb8e85f2b33a79
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:45 +0000

    married grow later any whether

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
