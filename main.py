c90ff3e28e33be21e125108e044bee3ddf0e4a58
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:09 +0000

    somebody stranger tear event slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
