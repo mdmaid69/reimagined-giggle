e99be40d1b52a0912fb504c05e01294641c462ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:00 +0000

    cry conclusion far welcome report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def split_path(path):
-        return os.path.split(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
