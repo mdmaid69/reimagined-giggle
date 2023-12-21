e9620d1ff9eae29bf7c6b96856723130ee92e20f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:21 +0000

    priority delicate drive Tt protect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_min(lst):
+        return min(lst)
