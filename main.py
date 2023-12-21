ae6993f07da33e80a9b17cb730a0195fd8351f83
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:07:28 +0000

    shopping narrow breath border demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print([x**2 for x in range(10)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+x = 10
+y = 20
+print("Sum:", x + y)
