743ff08e1d428f9747f62ec4a44d2d3480d6dc41
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:58 +0000

    rub score I scare destroy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_average(lst):
+        return sum(lst) / len(lst)
