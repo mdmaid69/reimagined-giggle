78a1f4b8f3d54f98e413b93508b9b9e8d42abcbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:10 +0000

    crime mass attach hers month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area(radius):
+        return 3.14 * radius * radius
