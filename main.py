4b03b52e398eb1a9496f7e6bef59c1e49ef83638
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:49 +0000

    perhaps brief drawing stamp street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
