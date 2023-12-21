c58e74fe0058948b3f90bea49674595abf77c3ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:56 +0000

    stupid music secret middle pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_pressure(force, area):
+        return force / area
