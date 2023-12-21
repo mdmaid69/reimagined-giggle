9b21c379fe079a2d993850a114a7d24dde118616
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:48 +0000

    beneath say shoot jump part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_speed(distance, time):
+        return distance / time
