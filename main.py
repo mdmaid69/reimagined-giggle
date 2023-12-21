2a86b3f080570b4424c28c75cbd44bdbd10b640e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:54 +0000

    joke disturbing Ll heart touch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
