7bb0238012a3224968ea64a44b4eee0818680a12
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:46:58 +0000

    word cold snow colour exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
