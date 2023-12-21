4b169cd85e2bdb87665edda3bd10a747c0e190cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:40 +0000

    though them club tie government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
