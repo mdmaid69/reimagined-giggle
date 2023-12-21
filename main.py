5ffc7b26e486c47fd067fdcae56074b741b47372
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:12:20 +0000

    appeal rob turnon choice production

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
