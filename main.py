f1853ca5266115c16b6aa565fc2ddfdeb7897482
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:02 +0000

    sentence comeoff winter throw corner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
