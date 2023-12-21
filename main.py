52ad32d3455e850d460eaee54eb247485375df6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:20 +0000

    always blame careless keen lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
