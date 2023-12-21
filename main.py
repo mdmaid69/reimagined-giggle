95c11b431d5716a79988b83e7982d1f27419d64f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:51 +0000

    rule than key dull power

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def print_python_version():
-        return sys.version
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import os
+def get_current_working_directory():
+        return os.getcwd()
