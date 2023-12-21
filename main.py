4cc3f1b4a4998aea7996a78624c74229edbfd5f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:25 +0000

    Kk anxious human hot unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
