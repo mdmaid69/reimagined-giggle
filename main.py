52c97580560021bbdbf93cbf225cde00da7b3312
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:42 +0000

    no closely loss amount ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
