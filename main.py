0e9a102a6d5d3bf61962ae40a6d144ab257b0342
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:29 +0000

    blow grow together battery write

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
