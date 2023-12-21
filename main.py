9210d65e9daa3379211b11ee094537a2ed5fe07b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:17 +0000

    busy praise hole exaggerate represent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
