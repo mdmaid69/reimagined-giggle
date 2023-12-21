de2f0104354ec4a853af2706504bb6e335d095e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:25:17 +0000

    opposite break lookup yourself like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
