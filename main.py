3734268c542086d6a6da4c5929bce8d05de04ecf
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:22:48 +0000

    intend certainly sky from cover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def exit_program():
-        sys.exit()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_user_string():
+        return collections.UserString()
