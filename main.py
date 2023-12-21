fe67163d915af34e62843aa2b2b66c17ebcef20d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:00 +0000

    ever land emphasis crowd fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_binary(n):
+        return bin(n)
