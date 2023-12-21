2a4256613ee78a5531f84eaf59c89dcf37dfa056
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:45 +0000

    behind side accommodation confident introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
