b065b258264504037cd4e8206d9780c31baa3008
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:35 +0000

    busy limit notice experience tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_speed(distance, time):
+        return distance / time
