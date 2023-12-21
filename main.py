e77675e1f6fcd58b462f7e5ee65fd6d3482e2c4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:57:32 +0000

    lead wild method experiment same

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_array_from_string(string, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
