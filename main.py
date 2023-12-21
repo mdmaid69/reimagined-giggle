ee2184f55119864d4695d162ab8e2bbadf750171
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:51 +0000

    comfortable entry drop ring dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
