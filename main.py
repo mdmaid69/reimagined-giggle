9dae9987ed6946dac0a9f9d9ac8e378bd069ded8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:21:59 +0000

    catch cold argument bend read

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
