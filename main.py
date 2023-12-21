26f52601ed95d0faed4e104dfe31039f2bcb2d1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:39 +0000

    smart Nn concentrate towards old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
