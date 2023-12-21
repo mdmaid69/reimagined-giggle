ddbd98d99b0992e3a6ceabb133740426c72a4fa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:30 +0000

    ready round again mess use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
