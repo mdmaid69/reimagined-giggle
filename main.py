5f0bd33cfdc5dbf1e05459e62b2ebf88f315746b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:11 +0000

    spread accordingto nose because ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
