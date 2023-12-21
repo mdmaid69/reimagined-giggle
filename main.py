46e1889e96109c19b0fa47551853b9e5e5b49ea1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:44 +0000

    print sting meal sort praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
