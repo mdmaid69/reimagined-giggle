07582590eb9540fbca9a1971779c6a705119b80b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:42 +0000

    gift weekend form left 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def is_odd(n):
+        return n % 2 != 0
