3f0a059ebeadb9d113e7dc1c586e5adfdd1361b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:33 +0000

    sad contents godown makeinto day

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
