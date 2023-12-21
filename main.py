d15a7a62f83449d4d2004138df750185d74a14e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:52 +0000

    free plain raw announce good

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
