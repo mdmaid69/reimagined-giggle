cf6db6ae1933ca94177e7e38726761ea9ca939db
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:53 +0000

    detail nothing only recommend case

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
