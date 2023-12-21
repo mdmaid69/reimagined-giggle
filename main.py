e6ae0bbd5974d10604078982d386c4a576d3b6d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:27 +0000

    manage process traffic still birthday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
