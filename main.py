bcea12cdff45f310eabe1f12fec3ab1f8414ab4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:05 +0000

    switch heavy later Itself wind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
