8be87456b89d4d1986da401e681a1c64c27044d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:59:55 +0000

    voice tired mother light work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
