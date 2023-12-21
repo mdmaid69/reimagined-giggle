655b898c723970c1f7f1ac1af7d28a2136b7ba2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:03 +0000

    afternoon confuse in write report

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_min(numbers):
-        return min(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
