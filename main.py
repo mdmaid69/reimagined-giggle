19020475120b6f8749a07406c0ade7e0e375c014
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:51 +0000

    enthusiastic accidental since 179bite answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
