99eb6c2ec7cb9a3542aff6ddbe3e387f03dd4927
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:23 +0000

    door frighten contact pull result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
