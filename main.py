a3b1e6e2a97f98f7ae71397cbcc0d413daa734e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:13:28 +0000

    physical press produce pot memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
