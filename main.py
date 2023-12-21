5a065234c4731aaa68499589412441e73c2280ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:17 +0000

    murder murder group top it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
