d6634e5300c01746bedf4bc9947480af5adbf478
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:26 +0000

    shoulder disappear guess total board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import sys
+def print_python_version():
+        print(sys.version)
