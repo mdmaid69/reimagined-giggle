1c3a14e8ea4ab3a69c09595e2ff245f2d629ec46
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:28:05 +0000

    minority worst himself turnoff plant

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
