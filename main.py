c87a9e17eddadcb42e03fde01717a16127480ed5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:30 +0000

    fix want speed side however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
