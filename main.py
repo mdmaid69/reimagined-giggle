38a7f9b48ca2a084b3324eef2b70ecae768fb01e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:12 +0000

    die anywhere 571enough major backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
