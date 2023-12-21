1147fb11d1a8ec2d02b15afc0d37cf0f7b02c872
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:51 +0000

    happy bury smart again squeeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
