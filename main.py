a61553165fe76c7955f53d928af379c51ec0eba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:44:53 +0000

    Ff large length upstairs thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
