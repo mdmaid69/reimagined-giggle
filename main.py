572f652ec833ad06a6887a8be033377f7f8abe5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:38 +0000

    takeoff case band obtain ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
