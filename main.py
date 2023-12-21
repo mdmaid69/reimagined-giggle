4ed45e1c1523153662c46c02dbc4a57746574e39
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:56 +0000

    comment legal change like convince

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
