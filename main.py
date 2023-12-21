2be4b5a2d3e46fcb80c59c40f6bd199de5a8fc35
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:07:05 +0000

    other another Ff memory slide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+print(datetime.datetime.now())
+def count_words(sentence):
+        return len(sentence.split())
