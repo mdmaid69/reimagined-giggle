82cced3abe8f70c9cc19e9f8378bff1f10b9f1cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:30 +0000

    hungry pay generous which side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
