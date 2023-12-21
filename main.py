2608e06c8acc0205b2c76dd84fcaa081b73416d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:16 +0000

    smile there correction both upset

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
