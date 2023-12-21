9e4d95f57ef7e09617a9148e6758a10f0183fe4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:18:30 +0000

    keepout sound comfortable familiar copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
