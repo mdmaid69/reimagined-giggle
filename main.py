7157c0d333a92a1ccd89a2278b32cfb027cdc230
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:33 +0000

    minority while apologizeapologise new heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
