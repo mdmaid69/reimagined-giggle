629bd2ee4f51fa8fb5f999bc35ee398e5eb0b5be
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:13:27 +0000

    failure thing act go turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(lst):
-        return min(lst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
