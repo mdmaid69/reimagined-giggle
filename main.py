d3921a8017e64bf7b04518fa0af4526cd6da99f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:54 +0000

    married real music exactly more

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print("Hello, world!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
