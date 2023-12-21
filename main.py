12effc7db7c22ff52b23e215810494f3f0e5b18c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:47:52 +0000

    set sad lookafter use formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_power(work, time):
+        return work / time
+import array
+def reverse_array(array):
+        array.reverse()
