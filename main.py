3dea986922b888659f06118369aed1475af30f80
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:03 +0000

    beneath slow produce bite yet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
