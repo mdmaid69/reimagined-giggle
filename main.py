f641ad227c768e8188b6033811ccdbe7d29f4fd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:35 +0000

    direct continue famous back likely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_area_rectangle(l, w):
+        return l * w
