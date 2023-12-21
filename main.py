302186a715b11366cfaf80c07d1aa372c70fe96f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:43 +0000

    another subject object file consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
