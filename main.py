b805d2af0396ab66f850cdad55b2e365cca3e90b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:11 +0000

    properly ever disagree send scheme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+text = "Hello, world!"
+print("Characters:", len(text))
