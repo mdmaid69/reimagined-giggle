e57c1dc6286bf09ddf820e3f3cef0a7edfbdbfdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:13 +0000

    regularly care go slight suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
