8c00b4fad2f663988d4d480184e14d62b610f81d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:30 +0000

    power mine frighten comeover bunch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
