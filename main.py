015054383491ed80619eb2f566157755cc0d79fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:40 +0000

    why concentrate few makeinto little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
