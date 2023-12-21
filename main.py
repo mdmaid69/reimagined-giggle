69457f779dbabff458e2905bdb8ee2c0b6963c8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:20 +0000

    sweet him interrupt watch closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
