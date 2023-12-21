be208fc57033e93f9f0a137c00661d5e53abf91c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:00:33 +0000

    sleep first language conversation beneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
