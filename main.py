3c69c01a97bf0f92f81b3f8824be5fab9a68b58a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:10 +0000

    problem with suck Uu attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_current_directory():
+        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
