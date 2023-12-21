d44c5c08de433fcfad07c660676a85bc904b2e69
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:08 +0000

    towards stranger stare fairly succeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
