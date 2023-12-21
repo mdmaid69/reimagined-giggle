d3dda93cc7e4590302f310c1fbad55e7c65bf923
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:21 +0000

    laugh joke light terrible failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
