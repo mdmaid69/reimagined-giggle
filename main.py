ff80cd578879992aeaefc37a2333bcf00ead7d00
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:05:56 +0000

    normal crash difficulty child respect

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def divide_numbers(x, y):
-        return x / y
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
