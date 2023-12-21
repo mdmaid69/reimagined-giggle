562586ca21f3d13deb2a0ab29a6da853a4b27df2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:49 +0000

    need copy other mine gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
