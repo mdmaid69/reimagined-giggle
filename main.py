f9961b502ff4a2524370c74bf816146046f28870
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:18 +0000

    trust Bb alone again kick

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
