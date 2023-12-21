3e9af12a02251ce70b0869661dc50dde2c0a92f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:29 +0000

    advise 566end colour available total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(lst):
-        return min(lst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def square_number(x):
+        return x**2
