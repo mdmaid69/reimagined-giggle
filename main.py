b751e5f1b25732f22df01e71a43d1deb396a78a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:11 +0000

    takeaway giveout could foreign big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
