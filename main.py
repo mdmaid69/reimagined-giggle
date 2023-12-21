ce6c2a8045b09a6b83c5d505f6d578540a282437
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:36 +0000

    file either hello full start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
