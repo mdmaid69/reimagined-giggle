f0333afcb882eaece5dc1791cb2e90d1ea979261
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:28 +0000

    underneath lesson account crowded takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
