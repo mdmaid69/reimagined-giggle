e0cb92873e1f832c87deae763ce1a538c1673ba3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:37 +0000

    check waste ignore town flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Characters:", len(text))
