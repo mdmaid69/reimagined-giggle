00a57b8bf198b205f23267720390d10b1f3d67d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:17 +0000

    unpleasant get abroad below broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
