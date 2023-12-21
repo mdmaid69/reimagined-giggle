67c831d242ceea25a23d3d29b74d5f63b53cd989
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:15 +0000

    there deliver weather constantly bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
