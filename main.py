9c4e86460e917f2e5d0f50136d618849b696a8a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:44 +0000

    deep collection spoil task century

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
