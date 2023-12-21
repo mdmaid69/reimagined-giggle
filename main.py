6993a9969d6cee939b2c8c859544d0589a56c48b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:24 +0000

    breakinto door cheek balance march

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print(sum(range(10)))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_max(lst):
+        return max(lst)
