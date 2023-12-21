1da80861380ffb62ab792959dad42846cc63f106
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:26 +0000

    crowded the either radio game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
