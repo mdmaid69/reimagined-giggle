e963a7bd777488175cc0f6e606ee9310f7cf406b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:22:44 +0000

    lean unlucky water risk however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
