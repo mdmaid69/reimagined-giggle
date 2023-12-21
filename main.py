5452bd08ed2946c9f2b7ba8a804147ed32436241
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:14 +0000

    recovery instrument weak die his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
