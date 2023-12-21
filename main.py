e197f9d5f4c627b6cf8f3bc4e9526cb19a4ad130
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:26 +0000

    very like target me succeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
