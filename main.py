2248c10c6f2a53e028402fce99e16cbd2045d33e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:11 +0000

    favour ourselves butter upset pot

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def multiply_numbers(x, y):
+        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
