b116b18d110b7ecb7ee009ec6c4661aad3e328cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:57 +0000

    police standard part offence scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_length(array):
+        return len(array)
