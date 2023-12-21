b6e6ad1bf5dfe0e094049deb79a000e22dffe542
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:40 +0000

    disappear sure sport collect brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
