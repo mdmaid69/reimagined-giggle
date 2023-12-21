5eb2bdce7a88269ec898895a1efa375eb66772f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:38 +0000

    keep Ll wash could crash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
