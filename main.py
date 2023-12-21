23afab61f884f7fb8899e00ebf1290de2ffe7ada
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:46 +0000

    cause careful after idea arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def clear_array(array):
+        array *= 0
