f59393c5d42bff860ff39b866c85da6734733c88
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:15:42 +0000

    Hh temperature disappear forgive authority

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
