b18c5f9d9b0c14f3cd9a9bfd77b9f1b7d57724b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:19 +0000

    steady myself party innocent squeeze

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
