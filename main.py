fb484f562dce9a7554a97fbb38a930d958167c53
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:40 +0000

    giveout onto floor march drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
