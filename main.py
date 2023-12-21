267b9cf18e22e0110d140a1aa20e3b6e21e7a2f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:37 +0000

    concern rather each boat sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
+def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
