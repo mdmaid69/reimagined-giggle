88847cd9d0f1fbb1acb0051ed6656d07a0a011a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:29 +0000

    programme ticket neat bus maintain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
