94cb7426192f16e06fd00caf519d5281e2cf0e3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:08:03 +0000

    bet tell whoever welcome compare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
