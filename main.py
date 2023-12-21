27aa151dbcd22925fdee97b3014110ef16c89de9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:51 +0000

    rob matter invite trust above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
