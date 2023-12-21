a05772e70d70ae6cfd4ff47d5a9f00c957246b6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:41 +0000

    nowhere late side tip begin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
