f6d1e5f38f29a28c0f08effb22dfb59274e07ebf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:31 +0000

    its better miss schedule in

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def square_number(x):
+        return x**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
