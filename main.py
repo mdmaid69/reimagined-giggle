ea3a966fd9333cbd291350e40330502ccb298c24
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:41 +0000

    look introduction scared ahead closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
