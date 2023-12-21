dacb046130cd46a74f8fcda8b57acd251a174503
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:49 +0000

    also noisy choose solve admire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def count_characters(sentence):
-        return len(sentence)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
