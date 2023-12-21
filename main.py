5641c65bf9d733ffcd35423566cc7311a6a21dcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:39 +0000

    follow vote workupto seem dress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
