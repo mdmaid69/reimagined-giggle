01b38273ea49d0246adb68b8f8314111ad9f1786
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:44 +0000

    every unless feed sign picture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
