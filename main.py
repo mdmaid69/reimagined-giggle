5dc983ddd7cbb48be83a6fcad2f2c1cc856526b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:47 +0000

    aware escape rain track centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
