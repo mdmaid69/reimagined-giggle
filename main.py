b038458b510f5fa50e5433bae0c2e888f7e6c498
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:34:50 +0000

    behind lookafter talkback police frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
