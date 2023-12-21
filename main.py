0dd32209dd217197a52a1d7a97a8d448ac56cea4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:14 +0000

    frame file speak decision then

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
