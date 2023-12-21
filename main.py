11f6205beb1146ea8923deacb89b71ba96ab37cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:35 +0000

    about story loss strength plain

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import array
+def append_to_array(array, item):
+        array.append(item)
