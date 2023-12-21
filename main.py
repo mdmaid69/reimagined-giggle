6a9a2cf815621a0e9845e14fdbc3802b34a4cdc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:09 +0000

    callout fly value more since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import sys
+print(sys.version)
 import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def roll_die():
+        return random.randint(1, 6)
