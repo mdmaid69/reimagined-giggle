89d608ac30b530ecc7051466c215b7356be387b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:06 +0000

    pool community use Ll careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
