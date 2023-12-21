7c0f69e0f049132d7faa5403d9f40c7f06197e1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:55 +0000

    criticize rub wise wild might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
