7bd9e71e9cb4f95db673ce5f894da05fc1077083
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:51 +0000

    secret bottom exercise not commit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
