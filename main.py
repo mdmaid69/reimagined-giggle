7c8de8509fbaf390edeccf631f6f3f767cb5763d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:42 +0000

    ahead keen lost move safety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def add_numbers(x, y):
+        return x + y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
