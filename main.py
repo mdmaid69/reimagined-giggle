0b8cb1bceb0ea201cf5e80fca332ca201b8394d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:50:02 +0000

    walk bin rough receive slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import getpass
+def get_username():
+        return getpass.getuser()
