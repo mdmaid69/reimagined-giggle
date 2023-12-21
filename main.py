83c873f1bbe273a82b1634958dc7a3f2d1a2fdb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:09 +0000

    across joke hold treatment persuade

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_list():
+        return collections.UserList()
