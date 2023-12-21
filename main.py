af935bc2882041eb4d3722280e6ff936d3f6daf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:50:53 +0000

    bag its great trial deaf

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
