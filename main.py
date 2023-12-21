98263d80771659a392da46208f92daeec5069eea
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:01:35 +0000

    pour respect tip rough direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
