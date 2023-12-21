f4aec033357e07a368d8c674608fb05fa6297ca5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:06:42 +0000

    beautiful road sticky protect Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
