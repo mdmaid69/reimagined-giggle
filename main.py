647f05f27c876354b7baa8abfd38d606977ab9f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:41:56 +0000

    touch difference bottom brown interruption

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
