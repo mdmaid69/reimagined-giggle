57ad8be2f81a94611ec10c4a7cfc0a3ece50e302
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:24 +0000

    book bar male interfere big

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
