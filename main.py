521121603d9484361e240d46e8200753ed44d51c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:39 +0000

    tooth impatient measure matter roll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
