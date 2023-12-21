7e76666ebcd88a836aa3fd7e484d71881e30ef05
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:06:25 +0000

    improve total success behaviour similar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
