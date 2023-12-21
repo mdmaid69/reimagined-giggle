8ecbad5631e710d5db8da5273f205482583c0328
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:41 +0000

    block also moral cruel wherever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
