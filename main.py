7f531b6e218510265635075b7e9d131ebd3623c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:33:35 +0000

    later without extremely exaggerate subject

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
