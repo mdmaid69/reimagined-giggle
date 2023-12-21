e02d6018d2310afe1cdeb48e9c5c849eeabc20e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:31:55 +0000

    whistle wait formal house brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
