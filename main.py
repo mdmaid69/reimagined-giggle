e1fa0579970a0ec64d18c95d288344fbc4cf1624
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:43 +0000

    shade brief owe temperature cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
