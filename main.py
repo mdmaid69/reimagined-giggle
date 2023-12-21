eef323e938fdb62b5bf01c5adef522137c78c487
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:43 +0000

    moment good sense cruelty regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(a, b):
-        return a + b
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
