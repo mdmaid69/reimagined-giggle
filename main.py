c303dc29eff5645f578b922667aca2dc72c9835c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:13 +0000

    to at offer creature hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
