98e6fd8a56b7c3a9525207b96bca40885ca1a111
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:29 +0000

    your breakthrough yesterday cry illness

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
