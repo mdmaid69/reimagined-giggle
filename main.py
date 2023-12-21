ad95439de9ab7c14151b55e583f795f3c3092a92
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:18 +0000

    thinkover approve right surprise belief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
