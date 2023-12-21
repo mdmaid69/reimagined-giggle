8d4eed1fe5f6697101891afd307e47f0f26fd37a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:33 +0000

    takeback against maximum between community

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
