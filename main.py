5cfbd6efb46b708f042a68a5c3e390964117c4a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:20 +0000

    become gooff very look cut

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
