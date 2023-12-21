05fea2eff1afa115dd3dd8617111410b39aacf20
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:06:23 +0000

    572enter Hh children right small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_int(array):
+        return int(array[0])
