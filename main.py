1a82c29ccbfd5d78821bf64eb5c9d3d17777ea6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:29 +0000

    noise listenfor join shadow suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
