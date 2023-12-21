d1e4382c83cb516ca8e11fda4a374a3e6f4ae5fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:41:50 +0000

    ground accept breakinto kill comfort

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_error_function(x):
+        return math.erf(x)
