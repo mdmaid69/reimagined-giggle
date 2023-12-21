2d3a132d1267104ab517571ff50d6559863eba32
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:32 +0000

    create could more crisis pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
