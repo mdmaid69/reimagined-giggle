2685bbeaa1fa235fbad915a1ce96270b1f2e51b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:11 +0000

    uncomfortable note responsibility improve hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_acceleration(speed, time):
+        return speed / time
