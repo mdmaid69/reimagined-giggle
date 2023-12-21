5979c318dd249d0d2846a7cb10da2933eca939a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:39 +0000

    conduct period deal vanish soft

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
