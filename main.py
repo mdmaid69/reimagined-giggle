155e792250ba50803e1db3b1eae2b52d2203bc21
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:54 +0000

    agreement school worse terrible 573entertainment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
