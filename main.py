f8d1ccb672b33e43340be1c3b6f40ce91e7e6f4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:00:58 +0000

    press forgive ought excellent peaceful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
