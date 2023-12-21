0d633f2cde74684df7a76c3b48dad0aa43cee98c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:01 +0000

    search explain bring effect ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
