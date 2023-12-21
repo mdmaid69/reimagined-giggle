1d226147b23e293ba84243e495dc3b31d311b134
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:08 +0000

    unfair congratulations target remember event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
