43b806c8e3e9c62466cc16002f6af38597d6555e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:25 +0000

    this vanish class hungry measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
