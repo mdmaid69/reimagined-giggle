1cab04269ec6680a0dd184a1bccc053634d8e804
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:46:55 +0000

    sticky lift pause middle case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
