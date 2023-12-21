f4c6bfdc910a241493f6debe278711b07b9fcf9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:52 +0000

    story language suck Jj fill

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
