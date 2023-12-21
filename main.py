9ed25c2eabf027e07e0ad19afb34c6de158b5870
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:19 +0000

    demonstrate or although issue truth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
