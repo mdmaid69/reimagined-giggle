9a8c619b9afb84568a85ca3591646f8c6c01d9a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:55 +0000

    strength unfortunate advise loose total

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def reverse_array(array):
+        array.reverse()
