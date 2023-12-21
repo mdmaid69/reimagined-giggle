aa0733d6808331ef782f41258321a27cb073aff5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:36 +0000

    period main light cheek wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
