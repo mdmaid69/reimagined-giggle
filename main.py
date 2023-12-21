d480afdcad7706bd7fb056449afa0cee27eda377
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:53 +0000

    Ii by approval live damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_factorial(n):
+        return math.factorial(n)
+print(sum(range(10)))
