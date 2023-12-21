2759a6f50fca95fb320862ca3439a147e18cfcb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:30 +0000

    raise mistake wise but use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
