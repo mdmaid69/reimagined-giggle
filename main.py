aca9ade2bcf2eef29681601993f6cbc8289c38b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:34 +0000

    ourselves bin bite discuss otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
