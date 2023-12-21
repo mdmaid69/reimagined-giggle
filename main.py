a56f6c715a2f084e0576412b6036d5cd76306972
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:52:35 +0000

    Ss fierce spill idea region

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
