603b46c669de80980a55c116bb1c0a56406046f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:07 +0000

    it convincing prefer hold suddenly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
