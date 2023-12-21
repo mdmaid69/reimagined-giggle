fbfece883feef60d82a43fc04d96a6773034238e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:09 +0000

    make nervous subject myself interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
