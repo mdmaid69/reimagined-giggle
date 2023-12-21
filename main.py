d269037573281a3f3e47ed5aa57343993d02d1bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:39 +0000

    opposite stiff usual phone watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
