a2920d3a81a296b3987c9ff3e0b5258f5f2526f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:00 +0000

    which 179bite exaggerate lucky miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
