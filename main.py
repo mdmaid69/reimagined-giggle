76a2e4fad069edd13de75719b26003d1a48fb43a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:52 +0000

    cry century soon rescue rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
