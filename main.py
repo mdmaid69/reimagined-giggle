2a79d4d7337dbbe836a9e2072daaef546c761fdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:07 +0000

    564encourage powerful interrupt live 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
