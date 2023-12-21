450874e44deb820440556bb0cf861c264fac2768
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:14 +0000

    breathe unable Mm design run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
+def add_numbers(a, b):
+        return a + b
