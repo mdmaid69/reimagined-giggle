c7af5706f2793bf566e91125cf7d9b58a01aeb2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:52:08 +0000

    late ought talk callup callout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def square_number(x):
-        return x**2
