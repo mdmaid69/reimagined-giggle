39cbe798bd69544481978f9c6e4dab45d41f71e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:08 +0000

    large very state trouble loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
