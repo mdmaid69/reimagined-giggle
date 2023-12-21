d76c9ef5b06e1205bc9b93c70a048d40a0ba1a74
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:03 +0000

    culture even mouth active shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
