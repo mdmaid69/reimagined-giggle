8f8c12eacab171e02acb6aa4bba5f821b58d0143
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:04 +0000

    form Oo think perhaps cook

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+for i in range(5):
+        print(i)
