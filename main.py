907a9d34d159f69f015d8f8fcc4b99491529cad0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:57 +0000

    we centre calm complicated perfect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
