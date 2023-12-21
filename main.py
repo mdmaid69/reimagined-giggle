d2d5c644238316261e1e3cb1c45aca054929cc71
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:38 +0000

    minor authority otherwise maximum legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
