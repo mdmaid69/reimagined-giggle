30cbc231a7eb920658da45f82cd1e5a535863c21
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:29 +0000

    spare plate we radio copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-print(sys.version)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def remove_duplicates(lst):
+        return list(set(lst))
