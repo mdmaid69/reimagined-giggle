504c78a43aa1b1598595bec14c6a11c6968d2cae
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:54 +0000

    estimate sell surprising remain chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
