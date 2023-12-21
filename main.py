5307b717864697b73b521cd7ae3676c137add08d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:14:19 +0000

    sister well star pupil couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
