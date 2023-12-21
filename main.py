c90419d86ed253fcef1e8d2fa6141fad407151cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:34 +0000

    brief runout thanks base wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
