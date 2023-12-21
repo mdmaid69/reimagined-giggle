954c60c5c786c4758eeb0bc874005726a62d1737
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:17 +0000

    train large bringdown afraid present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def square_number(x):
+        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
