ed5298a7e4a4c824eaa0bf73435bd8798cf749c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:41 +0000

    development odd around family yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
