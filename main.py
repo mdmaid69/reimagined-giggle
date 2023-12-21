f133db6f4136a30ff028c20a8c18ccfae385ef79
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:18 +0000

    later charge tie mass occasional

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def count_elements(lst):
-        return len(lst)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
