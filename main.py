7eb34a61807ca7a3cd6c9e5169dbf45490724b08
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:04 +0000

    soon bunch bringdown tour sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
