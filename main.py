27e5776032f8514015adb7eee4e1e02060178858
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:22 +0000

    male grow anyone guess recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
