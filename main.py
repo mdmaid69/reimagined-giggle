696f78d0e916ae27e47402759e6c7da11c793943
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:36:24 +0000

    tough fall once consult information

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
