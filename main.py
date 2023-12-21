1e76ed30eb8b9533f6127cd3865249164c3ec477
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:46:48 +0000

    broken wash character all crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
