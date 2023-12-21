2eacd7335ea8b150de5093613495984d6f3a727e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:38:22 +0000

    turnout permission related backwards hers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
