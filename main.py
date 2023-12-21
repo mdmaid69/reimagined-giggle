02fe857c6591bdfc84372709426d71cb3f28b584
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:52 +0000

    purpose suit ear skill healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def find_max(lst):
+        return max(lst)
