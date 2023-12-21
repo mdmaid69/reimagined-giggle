1e451083e98c487f2f9660be8d39a03cbaf84441
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:38 +0000

    round neat principle drop problem

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
+import csv
+def load_csv(filename):
         with open(filename, "r") as f:
-        return json.load(f)
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_dict():
+        return collections.UserDict()
