fd9124071787bfdfa4c005382378e4ceca0a3b2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:58 +0000

    study confused read short fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
