31c93ce5d78f2d0907501c9a4f9b07a3d6f170dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:31:43 +0000

    identify feature lead right design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_user_list():
+        return collections.UserList()
