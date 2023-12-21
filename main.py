990b45e4f6c39be71124fb170b519cd7e47d2aec
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:41 +0000

    sight handle people difficult lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
