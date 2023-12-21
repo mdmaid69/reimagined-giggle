465cbccc4e1529b2b184552884eae8838cda4c30
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:51 +0000

    lesson welcome wave ignore memory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
