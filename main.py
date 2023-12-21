dc49f1a0f1ed168e5dff2ef604ad43af111a7e03
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:54 +0000

    move fast dish apply tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
