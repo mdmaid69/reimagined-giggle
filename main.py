3053c26688045e073f8dcf801feb368d2931c27e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:15:15 +0000

    shoot big place try hardly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
