56e172838661aab66f9d2c39e5a37554ba4b8280
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:08:11 +0000

    almost way noise innocent bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
