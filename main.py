7b61e58deaee914155ccc9fa092f367b87a6d3a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:47 +0000

    address church frame pause comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
