4781132725072baa41ca5a7e051d9654a26b416a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:26 +0000

    active wise gas control ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
