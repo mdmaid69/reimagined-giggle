03ef3c3d8b78918f03be9186ee4a1eb298bcb39d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:52 +0000

    chief dark slip step rely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+print(math.pi)
+def find_max(numbers):
+        return max(numbers)
