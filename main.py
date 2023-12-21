19278e9a882727090584bd36d1773e4f22cfc1e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:53 +0000

    heavy far bit form act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
