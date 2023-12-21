de37a82e0c732e8ff102389b643871c53a80e30a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:55 +0000

    pause must fasten and careless

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
