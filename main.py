fa3768677723cfd25ccb1c689302d7687598b4fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:21 +0000

    hand 569enjoy complete strong above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
