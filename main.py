2ca8e6e6e098d790044038f9f22e421fe583c678
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:31 +0000

    set section wipe friend centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_area_rectangle(l, w):
-        return l * w
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
