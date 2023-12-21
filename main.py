71ef99f87ef641b5aa6089241967734c3ee2b5fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:19 +0000

    copy female normal influence artificial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
