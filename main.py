6d3966bdca9cd64135e6fac12735d3f02f08a6ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:59 +0000

    voice bringforward cinema mood depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
