1d51072ecd71dd527e6d91eeefc758e3c9afc3ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:24 +0000

    replace calmly obtain transport my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
