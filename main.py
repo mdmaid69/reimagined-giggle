72c8d3f1d481b172cee93558721d61d04aa74fbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:58 +0000

    ticket late ear together trick

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
