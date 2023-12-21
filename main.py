fda8c194bd724063050757ae215a76e52ae27d0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:22 +0000

    compete transport give remain ours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_array_to_string(array):
+        return array.tostring()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
