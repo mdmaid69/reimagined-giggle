998f1844becd4143545775a0c54e672ccb897f2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:49 +0000

    everybody ocean appear flight shelter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+print(sum(range(10)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
