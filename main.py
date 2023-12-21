2073f9d33ea9644a80e286eb73b4eaff7f54c15e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:15:35 +0000

    memory before everyone thinkthrough criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
