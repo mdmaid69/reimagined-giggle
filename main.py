323e012e8160bc3be00ff49947a835e2db544167
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:13:16 +0000

    sad offer operation sensitive or

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_power(work, time):
+        return work / time
