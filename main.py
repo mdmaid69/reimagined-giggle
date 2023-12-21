7a8362dae813dad7b641dbbf9299f1ab7412bea6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:47 +0000

    club grow beat dare character

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
