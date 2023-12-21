f8592ab24d0589c88da190f538b7da6c581bd977
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:25 +0000

    run win hide corner shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
