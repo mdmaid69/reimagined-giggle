54a75b53db3b0ba2465ac91bb46ac0028a2bc749
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:47:07 +0000

    pool cum lunch type Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
