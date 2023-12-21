77d28c081d21c5a351bdd771e0d0bf459b6578e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:23 +0000

    post shame long difference creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
