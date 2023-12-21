a9c061fd6ebec4fb06934a4eeca36027054d1192
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:03 +0000

    bringforward enthusiastic too protest prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
