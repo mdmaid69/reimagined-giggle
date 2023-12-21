6c9df826d16635d9b2029490cc14c8aafbf533f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:23 +0000

    organize bye careful able feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
