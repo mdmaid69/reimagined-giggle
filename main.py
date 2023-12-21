70533f1cb0e382167681a9f730b3c88a60a0e970
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:46 +0000

    giveback art degree sit run

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def sort_list(lst):
-        return sorted(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
