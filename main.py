20a1b8e39addeab73465e7445e1815a525dc941c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:06 +0000

    contain inform still command breakin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
