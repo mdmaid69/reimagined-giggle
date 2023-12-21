bb809124a44ea0195d975c2c4ea37fbf7d3f1eaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:40 +0000

    light art platform bound lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
