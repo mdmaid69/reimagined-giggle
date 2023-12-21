45f188331151cc66af152b9874c12c2aee1e8772
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:29 +0000

    file bite uniform programme and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
