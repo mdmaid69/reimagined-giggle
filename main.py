4042cc2d61efa06e1392087f18c067c67ffcaaa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:54 +0000

    smoke Rr downstairs content eager

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_string_from_array(array):
+        return array.tobytes()
