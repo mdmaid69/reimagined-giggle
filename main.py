831cafc0d55a09c95666186d8a05c5aec0753ee8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:27 +0000

    whom cost whatever petrol true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_speed(distance, time):
+        return distance / time
