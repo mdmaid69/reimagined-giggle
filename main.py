89cc0dbae1057847449dce73371559786d8131e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:09:17 +0000

    arrival address control Oo ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
