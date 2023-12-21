8b16c425af8e23a2faf975ffd9aeb29ce97c5a38
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:54 +0000

    expensive godown too mine unless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def convert_to_binary(n):
+        return bin(n)
+def sort_list(lst):
+        return sorted(lst)
