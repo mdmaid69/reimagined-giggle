f77e70f05c41439db4d198b88e3edde0819d9d9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:33 +0000

    unless rush dry heavy expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
