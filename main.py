c86594586bfb1ea284f11215e7aa1c8b89e68aaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:51 +0000

    choose recovery method suck begin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
