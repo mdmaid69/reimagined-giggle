9ebaa7634b950c345cae241f132c4d4bcecb3c87
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:17 +0000

    nose welcome use crime greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
