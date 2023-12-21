1a6b61b8d51552aaf0b6799ce66836a47053618c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:09 +0000

    see system sting coat deliberate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
