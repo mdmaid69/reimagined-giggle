fd57be20825ae9aad4a630f05cbb448bd3823771
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:45 +0000

    commit work everywhere somehow face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+def change_working_directory(path):
+        os.chdir(path)
