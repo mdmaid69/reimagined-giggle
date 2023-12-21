f96031a6afbd7f24f311368f995a5cee3aee5c31
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:44 +0000

    spin neat conversation detail lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
