6cdcb6bfcbf993513e7dc4aeed8634997808d663
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:12 +0000

    bury trust left continue climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
