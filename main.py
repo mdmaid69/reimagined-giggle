308c3433d9798b2cafdcf96ad04d70777347b84a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:56 +0000

    side keepup whole love set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
