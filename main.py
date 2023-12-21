0b6ef172e25b645b000b95037e98e6c2f93191ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:33:26 +0000

    beginning error work division 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+text = "Hello, world!"
+print("Reversed:", text[::-1])
