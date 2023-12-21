8feb31bd98f0c268a6b7ec1ff03d2d80cdc927d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:58 +0000

    calm birth sight opinion tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
