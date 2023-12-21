dacba63f5bdd798fc3f7d7bc2bf3209501281ec3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:46 +0000

    spring present story throw price

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_binary(n):
+        return bin(n)
+  def count_elements(lst):
+        return len(lst)
