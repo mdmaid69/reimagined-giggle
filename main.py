af8415d2fb88e8427486c2e918bada3bcfabfd4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:29 +0000

    worth consult worse recover unable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def cube_number(x):
+        return x**3
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
