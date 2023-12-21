1ab0ea461c97fa031814bdbcc3bff47c19ea6e6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:28:58 +0000

    song borrow wait discover honest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
