2d56cb6fb2331fce9161a30290ca8c7b80035d5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:41 +0000

    passenger building add health advanced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
