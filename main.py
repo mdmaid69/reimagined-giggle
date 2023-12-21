2fca46889b002282aabd4a809151d7ae8c2a2fbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:54:10 +0000

    day command add prevent anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
