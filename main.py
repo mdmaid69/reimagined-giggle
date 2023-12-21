c263987f991b58e7598e04b0ed7d7e4b7a469883
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:55 +0000

    human beat roll uponon often

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
