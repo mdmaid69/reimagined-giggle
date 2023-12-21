a16229241c7dc17e4fa3c885f480854553aa9f7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:27 +0000

    left hard star experience hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
