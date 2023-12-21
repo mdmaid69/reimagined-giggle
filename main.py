95652d909cdb64d6070f00988da4ff050dbe32cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:13 +0000

    progress rush minute properly whom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
