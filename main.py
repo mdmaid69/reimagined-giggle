69218c96b6d1f89f59ef6add46c905fb2fe12f3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:30 +0000

    passage operation translate supply weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
