a25a5e7fb7399a96e5ad90bb686bf8ab72125a47
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:41 +0000

    OKorokay put original takeout ask

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
