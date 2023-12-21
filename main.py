27078c59f90730e47f363478925e2ef3a916295e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:01 +0000

    congratulate late slightly flow remain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_list(array):
+        return list(array)
