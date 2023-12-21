9b1ef177a37410c18a938eb889f1597484ca5276
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:21 +0000

    goodbye whistle my careless argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
