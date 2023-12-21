adb3007cea93c5fc82b471ddfe815d52624ebb85
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:10 +0000

    generous start fortunately plant the

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
