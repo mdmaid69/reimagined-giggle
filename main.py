7b2bac1313413290629799ac7db012ddaf2f4c62
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:22 +0000

    study public agree keepaway present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
