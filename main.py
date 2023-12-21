b87547230858f47996ca1ef1015dcd6f516ea70f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:42:43 +0000

    gentle holdon short practical quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
