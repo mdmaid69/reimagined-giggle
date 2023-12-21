b9d8a2854b7196a2d75e17665a4afbb4e6c6cc91
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:23 +0000

    short usually scrape ring push

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def sort_list(lst):
-        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
