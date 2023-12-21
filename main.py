a076da3ecb0886b7f041e42de1ee54c943598e35
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:12 +0000

    smart prefer able pay earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
