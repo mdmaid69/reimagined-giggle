38a13f9cc727ec3b4caf8d5d380b55a9f14ea4c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:20 +0000

    similar present dark trousers rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
