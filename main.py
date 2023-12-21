74f980a09789f22762a5ee08d125d4cd80a90042
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:55 +0000

    test powerful study bit character

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Characters:", len(text))
