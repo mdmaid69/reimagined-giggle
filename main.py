b4e364566853772734805e1f2de5edb97b78ae57
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:21 +0000

    wrap bad generally ready benefit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
