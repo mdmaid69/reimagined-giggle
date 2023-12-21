0e62dd1be3f01501eeb4da08123213b129c5f76b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:00 +0000

    fork silent surprising score glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
