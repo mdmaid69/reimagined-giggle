043beb9a9f3fc64d031b62f6e29a94a5de19d30c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:38 +0000

    relax uncomfortable home glad bad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Hello, world!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
