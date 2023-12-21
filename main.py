6ccdfb2003e099f52d9fe7ba0bfbfd305fcf3616
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:28 +0000

    behave notice win recommend area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
