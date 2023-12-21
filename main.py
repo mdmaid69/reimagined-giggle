297566792d0fcad1aa9389e76dbc6c4ecb736b82
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:37:00 +0000

    violent previous swim choice platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
