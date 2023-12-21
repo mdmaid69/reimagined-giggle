f0838214ad02c8e7bde22d96f11c7c0889d5244e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:45 +0000

    share support spite soak hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
