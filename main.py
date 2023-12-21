bb2dd2b2afc5554b6a8997f719d6fc0020e16db3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:17 +0000

    repair protection by rush mend

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
