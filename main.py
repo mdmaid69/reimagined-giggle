1a82f0017fe8c97d0f14126a6d8333d570b559c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:01 +0000

    standard film sink genuine common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
