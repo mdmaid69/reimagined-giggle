c5221377800b7349924b47b4dcd60a71f3f25298
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:31 +0000

    anyone plenty create giveaway evening

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
