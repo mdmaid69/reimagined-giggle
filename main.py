8ef0198741112ead9458ca2d6f0614fa2a95ef8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:19:47 +0000

    hang tomorrow him voice so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
