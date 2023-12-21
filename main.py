59c20e9471b3bc9e3e4873293e789983a082739a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:52 +0000

    noisy pay send underneath case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
