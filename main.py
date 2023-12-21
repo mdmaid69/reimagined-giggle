0f1a50bc7fc16eeab059c0352a8197c9a8ec19ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:04 +0000

    cap match listenfor uniform beat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def cube_number(x):
-        return x**3
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_current_directory():
+        return os.getcwd()
