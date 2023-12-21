4b660d8a4435c9847b3d18e48ba07ddaa3331c6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:35 +0000

    effort dance best reserve production

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
