af23038a1ebdf71433ca8f09f9fa2c7c4164bc76
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:45 +0000

    slice mean talkoutof rough very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
