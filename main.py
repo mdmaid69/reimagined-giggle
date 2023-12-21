77ebe7aacf71120728459751a361b724922eff36
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:42 +0000

    side post comeround meet clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
