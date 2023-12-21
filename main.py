747e34848fb35e4cb607a55e6125395510abeb98
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:23 +0000

    out respect type display peaceful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
