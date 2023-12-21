a2f9d164edbb702664e10074b9b67cce40cd3506
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:07 +0000

    bring cross passage service very

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_work(force, distance):
+        return force * distance
