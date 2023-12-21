d012538655485a918a94480ad3814f94ba9e01bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:09 +0000

    bite breakdown slide present unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def clear_array(array):
+        array *= 0
