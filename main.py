79606a108378a0426b54adf03b40117099b1ec6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:18 +0000

    regret weather able way autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
