49bb10319c05098d69b28fa27d2fc50a46f9b689
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:17 +0000

    spot replace next store grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
