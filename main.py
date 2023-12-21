f6ec2b619f8f726f0e005107cdb54c86619aea14
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:02 +0000

    sentence urgent board surprised moral

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
