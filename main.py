bc333722bf84bfa7af039f3b3ca06a801b175c4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:54 +0000

    glass Cc event already truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
