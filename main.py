ec8e1f7bd3c75173750990e28f90e0dcc17c16a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:02:26 +0000

    approve pool tidy apologizeapologise switch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_work(force, distance):
+        return force * distance
