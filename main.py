7729dd92860552516fea45a7021895d334dda1dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:20 +0000

    accept damage round underneath steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def count_elements(lst):
+        return len(lst)
