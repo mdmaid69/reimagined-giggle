83d6e6f5f28f8dbd5e5aa660928548c6476332ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:45 +0000

    workout answer convenient gooff goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
