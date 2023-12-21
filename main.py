164951abb40c8379a0e3ae1d6f9f1ece6db07746
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:29 +0000

    march injury fill inform drive

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_error_function(x):
         return math.erf(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
