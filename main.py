0a8bf7ce00e4511bae60ad79f3542420c9212799
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:17 +0000

    claim please Hh print sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_item(array, i):
+        return array[i]
+  def sort_list(lst):
+        return sorted(lst)
