ba2b2e6af51ff98b783ddfdd8087ed80f5fd2d66
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:13 +0000

    him like free smoke willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
