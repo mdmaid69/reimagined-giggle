dccb2cd1a4fc16cd447e852f215a7d69178e9300
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:40 +0000

    calloff private about spread habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
