121583668f4d591a9f380df0e998c035ed83d402
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:31 +0000

    away forgive release recognize contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
