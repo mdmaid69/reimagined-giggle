f05639502d4c256998bfe93f2e2cd6c446744bed
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:11 +0000

    earn pretty copy comeoff damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
