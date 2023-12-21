4000c0a452c821c4caf862ac86f96d38f1a26a53
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:52:13 +0000

    geton mess consider affect weak

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+print([x**2 for x in range(10)])
+def add_numbers(a, b):
+        return a + b
