a13f8b2c9a207e78a8120f49c92f46bd5f32b94c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:34 +0000

    practical judge her unlucky job

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
