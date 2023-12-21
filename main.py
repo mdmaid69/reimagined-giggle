4ddf6045263551d061bc2b0243bbe51691337b8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:54 +0000

    crazy programme leave mail Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
