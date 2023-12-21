e0af370ee4ee46d5b1c6a33ef5fe6e34075cbb14
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:26 +0000

    actually plan left picture afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
