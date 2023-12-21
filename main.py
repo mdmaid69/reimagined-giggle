862bd795963b429f1cd947390f412555f0157db6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:46:05 +0000

    will big on tip real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
