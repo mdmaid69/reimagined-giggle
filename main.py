518d6b8139dc5dc869e2ade7a3575593208b74e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:59 +0000

    machine learn well near exit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import numpy as np
+print(np.array([1, 2, 3]))
