a2e86cb6a19ad46bd4b1679743767fad0dc4dc91
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:07 +0000

    dream cruelty content comein previous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
