634598aac7e62e88aa67db150e7ea9aa6323cbc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:59:32 +0000

    control concentrate rub sore early

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
