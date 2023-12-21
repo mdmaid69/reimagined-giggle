fef9721ed90c2e48408b639d4e34468c53019ccf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:06 +0000

    admire mother laugh broken us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
