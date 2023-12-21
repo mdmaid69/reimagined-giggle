f6534ace2d9263172b3b52a6f022dbff9ee6260a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:20 +0000

    send opposite operate unable use

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
