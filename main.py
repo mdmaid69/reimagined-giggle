cd57f191e088c213daa754694afa9b6c4dc99edc
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:51 +0000

    bet callup rarely tool dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_item_count(array, item):
+        return array.count(item)
