5cef454bd074e5658bae71ea9d9a80fe89c35721
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:16:38 +0000

    definite short air step depend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-print(time.time())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
