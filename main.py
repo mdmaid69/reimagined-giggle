6110071444151753bc74eecce5bf718a5a7311fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:49 +0000

    yourself sell runinto pocket could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
