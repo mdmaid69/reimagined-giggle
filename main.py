7526a34408cc304eb7f9ed13e5f3177f71bf71a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:11 +0000

    keep sky nice whistle progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
