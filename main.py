ed981cd8df1f831d1201d2615f2c73b4ced01add
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:03 +0000

    improvement effect ordinary left middle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
