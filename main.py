08464a3ddf9585283f1ae80704ac9c64878f40e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:45 +0000

    will hair base object unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
