70b2cbdd99ecd8a29a586b5295ae6a17917a8c32
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:01:38 +0000

    average across lose ring idea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
