57c92652f1a4a3436a59af0d260904d189720c53
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:16 +0000

    oil often suddenly extremely hospital

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
