8ea251b876962a2361f643666a17b213e8ce4ada
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:04 +0000

    poor personal sail confidence consult

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import collections
+def create_priority_queue():
+        return collections.deque()
