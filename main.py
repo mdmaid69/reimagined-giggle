14a5652f906ba41791b232be99b2f70fecbad524
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:03 +0000

    fly touch my regular persuade

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
