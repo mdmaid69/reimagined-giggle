38376b6b119d945331c6306c2c500d4b8dcc75fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:19:19 +0000

    onto suddenly runaway cope loan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
