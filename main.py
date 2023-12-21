8b286241472c5516557bf5f1ee3fc95c4e6b7f27
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:20 +0000

    then callback science nearly offence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
