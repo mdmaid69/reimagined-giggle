beda64f32e0cb90c9b553e39fe4403055b1cb2e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:24 +0000

    post better narrow clear raw

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
