24335ed53d38371ccba9065c4588eb882bf66897
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:03 +0000

    drop famous smile mean noise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
