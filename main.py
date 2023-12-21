55a3b14a8b22839c25434ce788fd3191da8d613f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:16:46 +0000

    blame blow than value bill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
