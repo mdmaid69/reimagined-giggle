534719d5610040cb5ec3f85ba53e6cf1eef0d7ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:11 +0000

    getoff save enthusiastic chin bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
