9e5cae2d00e02c449afac193f0c2cb85c94a7a84
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:51 +0000

    thin how fortunate extremely free

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
