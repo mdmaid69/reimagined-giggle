cfd312d52ff1b6c6ff0d639aadf3e42c2485cf6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:24 +0000

    add callup tap hit its

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
