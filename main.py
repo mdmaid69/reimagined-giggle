38b29c8d696f3c92f7f43aeb8405e48557af3675
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:21 +0000

    whichever Ww bush tradition right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
