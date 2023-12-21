4b6c79d3b602f57f1ed947d0c7148359a06670ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:18 +0000

    future wave badly treat after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
