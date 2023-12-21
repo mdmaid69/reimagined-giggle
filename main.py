656fe1a14192f765593841aff42087ea42cd58d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:25 +0000

    this guide anybody cry attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
