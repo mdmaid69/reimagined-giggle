a798ff1199b7bf107e725cbc26d07569fbe0a9f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:08:44 +0000

    cough size hello weather plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
