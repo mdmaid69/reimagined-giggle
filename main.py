4a95af5f5db6e41b9a4528caf116bcd13a736aa9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:09 +0000

    brush shade poison sort force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
