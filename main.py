e1ace6acb505bf9c0345c49bf29c0a1de540e79b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:59 +0000

    afterwards anywhere pupil bunch cover

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
