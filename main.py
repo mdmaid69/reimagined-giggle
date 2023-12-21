f7e502a870ac50dc19807c5d98bd9817639ee7b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:13 +0000

    some issue type chat should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
