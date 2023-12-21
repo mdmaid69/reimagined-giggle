22ac027a055683b9528e6c579b1a7e54381851dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:47 +0000

    agreement bend shut pretty 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
