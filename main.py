32a2f292b746a25039283cdb5729206d19de6a50
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:21 +0000

    Pp will phone hurt time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_bytearray(array):
+        return bytearray(array)
