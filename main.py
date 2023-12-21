e72779fa8f4bcc4267f464d94545d57e2d40b98c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:39 +0000

    dick explain suggestion several money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
