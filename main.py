dfb5dfee122cf25768af895eb1a3386da435bfe3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:20 +0000

    talk watch comfort stay health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
