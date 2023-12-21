94a7e1fe73b6dbb6676fd08a7c560e61f21f3887
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:09 +0000

    now band inside show fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def change_file_permissions(file_name, mode):
         os.chmod(file_name, mode)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def count_words(sentence):
+        return len(sentence.split())
