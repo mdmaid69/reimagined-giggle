24fc38ec4d15c2db831a50c699758e40d0ee7d6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:34:55 +0000

    follow also block treat wash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
