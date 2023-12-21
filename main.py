7f0554595af8b58ce0aa6d26804454acfeb49425
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:47 +0000

    interruption exist or length chief

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
