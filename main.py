95c3045693da26c8ffb60e851873049ac3fe0d26
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:47 +0000

    correct sticky walk Pp factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
