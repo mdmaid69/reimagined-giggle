73bf902cc9bf9804b59507205c5a97e764fddb90
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:06 +0000

    sometimes spray replace lunch label

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
