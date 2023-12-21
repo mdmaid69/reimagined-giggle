99eed646b5c4d88bf818648aef62306e9968ded0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:33:17 +0000

    read challenge thing arrive connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
