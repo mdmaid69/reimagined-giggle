7ef21d6509199398cb16d6d066fce167f5d2e896
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:04 +0000

    responsibility tool where improve habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_floor(x):
+        return math.floor(x)
