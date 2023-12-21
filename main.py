ece3f666a5ef089b86c81556ba33574787750a9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:00 +0000

    no previous team outside heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
