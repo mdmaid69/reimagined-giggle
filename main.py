c5115e544e94b5dfa2b67eb51c708995c8fcfec9
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:25 +0000

    music compete big home character

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import sys
-print(sys.version)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
