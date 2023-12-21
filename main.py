4b156ea4eb283c93b61d8dd34ae9cc9bbe0b6792
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:14 +0000

    ground uponon on few century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
