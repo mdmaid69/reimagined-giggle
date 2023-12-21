a2d569579f604322e329f1ee30dabbe059a9a385
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:18 +0000

    concerned general subject game pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
