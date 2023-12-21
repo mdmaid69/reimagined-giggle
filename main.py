e927447e9fc7cb16a1921c4bc28c53f280a78b40
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:09 +0000

    edge sister else power suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
