f840c06ed14e87cbacd27616c979b54986427fa0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:43 +0000

    set unfair mean steam morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
