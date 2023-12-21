8e89a349cbd1670bf8251778deb0218829a2d347
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:18 +0000

    peaceful borrow Ee Zz centimetre

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
