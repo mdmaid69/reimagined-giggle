372a6513c5442d437ec886903516d71f0462b0b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:16:24 +0000

    world equal famous abroad several

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
