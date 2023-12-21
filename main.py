a3702c6ed8363ebd36ebf26b7727b830a03cb708
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:47 +0000

    mean by pot doctor combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
