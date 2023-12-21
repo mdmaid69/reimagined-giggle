d5a35e7c20b0a360fb7f130b684d779b8bbacca2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:17 +0000

    force finger back mass scale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
