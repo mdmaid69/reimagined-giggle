c13b6166ca6d2c80de45b6c86df631417065465b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:28 +0000

    drop artificial proud environment sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
