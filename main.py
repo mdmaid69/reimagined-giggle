8dde9b06277a10df75c4ff4485f9964e269a55a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:43 +0000

    right almost paper soon when

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
