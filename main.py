89cd21f0b83773d64b74a6b0832d01f73aa17df6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:47 +0000

    instead miss mistake tip basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
