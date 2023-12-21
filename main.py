35e4ad01140ce34fb36d96975bc96b709a7ca27a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:22 +0000

    stranger rubbish speech popular check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
