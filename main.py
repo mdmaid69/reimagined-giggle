fdda6aaf33f20a58f2eda2bffd7b0600e7688f96
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:28 +0000

    goout lookaround good confidence prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
