0da0c593759e2dc7b44082f34907c96ecbc62de1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:30 +0000

    science promise above stir only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_work(force, distance):
-        return force * distance
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
