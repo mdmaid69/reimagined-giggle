0c4babe30f59c6a85b796a0fc195a24f3a3dafc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:40 +0000

    Rr regional breakthrough forgive thinkof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
