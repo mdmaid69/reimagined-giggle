80b15e1017c02bb55a4bd7eb5cac5e1ea2dc4e0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:18 +0000

    Oo alone write scream concentration

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
