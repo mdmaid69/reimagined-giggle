fae22ff7ba57bbd0b91ccc019aafb1160d8e9adf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:24 +0000

    Mm likely support defeat approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
