3c4e4c315a9d6dc0c6fdbf1936fb86551fb6c887
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:31 +0000

    send produce neck sorry performance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
