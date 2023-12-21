d8704e449e0954b6fa89809ee676f196911235cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:39 +0000

    attention right hit yourself contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+print(math.pi)
