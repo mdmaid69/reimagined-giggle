fc2fc33f18e282b93caee5cdf010ecd0c71db968
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:49 +0000

    condition dull nigga confident suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
