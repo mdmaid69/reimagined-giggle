975d35764e347fc2ff6b1b0a360c4537a704ffcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:33 +0000

    proper same fit unable shade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
