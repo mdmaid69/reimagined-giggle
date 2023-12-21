c922200e275ea5b49c0d002fb82c82539a2f6510
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:13 +0000

    weigh guess Ee advice rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
