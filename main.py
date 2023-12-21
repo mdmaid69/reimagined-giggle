42f0dd142c848d029ddc9a1f60fd9ce7820775e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:47 +0000

    frightened central tool Qq guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def subtract_numbers(x, y):
+        return x - y
