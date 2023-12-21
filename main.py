84134a18b86cf841489809e8b60a75b9160d7ec5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:49 +0000

    roof inquire switch thankyou expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
