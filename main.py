31f5347333895eb0b433935bc2b5230fe59d20e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:22 +0000

    beyond sound extremely help obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
