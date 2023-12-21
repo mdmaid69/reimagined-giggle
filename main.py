e7d27de4e636ceb7afe9f7a5892655c3fbe376d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:12:06 +0000

    majority admit soil preparation loose

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(10): print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
