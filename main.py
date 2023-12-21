c0f6b0b28071e8d355d3a38beb9ea4c0c3c34e74
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:14 +0000

    holdon long feature moral until

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def cube_number(x):
+        return x**3
+def calculate_acceleration(speed, time):
+        return speed / time
