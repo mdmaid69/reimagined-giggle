fe87157ac4b77f6771477fa97b01adf53bfe9dd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:20 +0000

    follow show unite reply feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
