7eedf41ff83a1833080ce91e0cded21419a145c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:55 +0000

    anywayanyhow tidy opposite every world

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
