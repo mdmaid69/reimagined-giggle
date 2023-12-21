91b3ed06859c6b400801d30145034e0601f0db30
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:16 +0000

    whisper community afternoon favour wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
