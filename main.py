aa7f20e3dc18baf2aeb667c79e4c8863e79b27fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:43:02 +0000

    story contain to rough relation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
