7141be93965be4417ada53ec1b91516eb7bd35aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:59 +0000

    belong slice fear silent strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
