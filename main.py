a89b53fece8da568590592d007357dd8ed335d9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:36 +0000

    release neat wash burn study

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_list(array):
+        return list(array)
+def reverse_list(lst):
+        return lst[::-1]
