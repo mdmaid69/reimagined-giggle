e101fafa70ce2664e1909c7a69ab1fa31f1b07d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:31 +0000

    callup translate march fire entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
