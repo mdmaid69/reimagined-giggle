69a0bad35e2f0624d1796c0bc0d1ff89c2b45027
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:19 +0000

    usual belief cheek field precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def reverse_list(lst):
+        return lst[::-1]
