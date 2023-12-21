451bec0cf016a4218f0f1d7279eea90e7857f832
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:17 +0000

    swing square production emergency back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
