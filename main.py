7b17de8551ccb1f340ffab750900b023a60282e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:40 +0000

    light have about announce turnup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import random
+print(random.randint(0, 100))
