ef5a446e4bbb799d9e75ae0d99706ce92d56677e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:46 +0000

    nurse result Jj heat between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
