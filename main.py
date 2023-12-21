5f488b74271f9b81414e98d44955e1b7a37c75c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:29 +0000

    breathe careful impressed remark somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item(array, i):
+        return array[i]
