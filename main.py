158e70621a576414f62c818921b26aac93023dc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:42 +0000

    comfortable behind claim 179bite calloff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def convert_to_binary(n):
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def convert_to_binary(n):
         return bin(n)
