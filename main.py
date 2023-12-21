5e8d4928334bb05768336801c9a623d417cf324e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:05 +0000

    getback forgive air trouble regret

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
