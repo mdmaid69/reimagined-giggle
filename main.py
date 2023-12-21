9f9dff3b5542e9e13d6f21b5ff2c30615cddd98e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:44:30 +0000

    school count however giveout oil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
