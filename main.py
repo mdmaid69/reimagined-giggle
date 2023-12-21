08d444269df2e1e7886cb544f672bb2af119cf0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:03 +0000

    preparation silent runaway traffic half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
