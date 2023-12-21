9c470d754fea34da0edb6bf768b251b930d0f63a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:19:43 +0000

    practise temperature wrong fun office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
