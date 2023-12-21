016fdb73f049fa0667e18eb1707585aa137b0f50
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:40 +0000

    since claim different prevent raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
