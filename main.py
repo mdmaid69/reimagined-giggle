dae83f2bf86f23dcd88ae03d3b4fc5fb989e6504
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:06 +0000

    date trousers offer instant obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
