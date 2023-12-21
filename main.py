71b15ee870fe537fd7341d48c91b97e38159e2f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:12 +0000

    centre fasten rarely extremely confused

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Uppercase:", text.upper())
