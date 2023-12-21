5f5fbf01e91a695c4bf473f3cf1dc290b05766ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:21 +0000

    belief odd shoot straight purpose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_max(lst):
-        return max(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+print(math.pi)
