2d10d591459ac7f85c1c1e6a4df499d5005a0ee5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:36 +0000

    under dry trap yesterday whether

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
