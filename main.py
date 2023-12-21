739f2181d5f0953b50b597506aa37cb6202c2040
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:20 +0000

    well round study right minimum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
