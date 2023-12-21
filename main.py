c0118d61ea0139406582914f4baac3f9ac851c01
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:43 +0000

    yourself give month rain comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
