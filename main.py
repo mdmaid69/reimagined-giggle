c577dd3563025d91d24811f7e1021a8180582c6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:45 +0000

    home call pleasant slice experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
