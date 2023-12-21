cd3e3a544ee6ab5c6e142e8313b766629a79ecea
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:36 +0000

    issue important experience must subject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
