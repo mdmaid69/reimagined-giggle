76fbda3036e8cf9d853913354e3dd9efb43652e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:48 +0000

    oil cover seat face noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
