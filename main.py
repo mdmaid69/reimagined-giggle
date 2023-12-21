74f29aa94fc0e397692314b613c24eb8272795c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:09 +0000

    other equal attach movement respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
