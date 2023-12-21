41c3cadfbd22c6eb5fdd7617ca9a9b715e0e8c88
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:39:21 +0000

    minor takeaway telephone finger favourite

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
