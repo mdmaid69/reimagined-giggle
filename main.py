6df4c84a1b5852cfaa7116b0976c5e1f7d40aa56
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:00 +0000

    joke ground free share operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
