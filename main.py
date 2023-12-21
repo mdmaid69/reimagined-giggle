bf94c31cba366bcb3f1ddb4bc20ba5a1f8a4ba9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:23 +0000

    treatment luck dear rough ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
