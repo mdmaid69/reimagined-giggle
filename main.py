257785b3b8e6a5dc72288d637e85d0431ee54bf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:45:14 +0000

    runinto mark half computer support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
