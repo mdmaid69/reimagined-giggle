082cc866c4b41927fc38815aae9d487e79e0449e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:47 +0000

    threaten practise gooff tip please

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
