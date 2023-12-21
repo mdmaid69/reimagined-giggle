dc1db44ee85f7b3b7b6ed6315bdccf91a9abe1b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:25 +0000

    sore love grateful unkind charge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
