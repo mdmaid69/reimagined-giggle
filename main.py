a30ec35763871e7ba1027851957ba902863523bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:22:52 +0000

    survive damage feeling cum prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
