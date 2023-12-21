5ccd06c751095c25fa7e96433f73cb6a2675157a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:54 +0000

    hate callback means fold last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
