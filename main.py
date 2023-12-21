e152d99437949f9c6711bbd46fd467cea8b64b33
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:05 +0000

    reject search different wish drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
