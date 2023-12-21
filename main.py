3fc363e23c38d2ebbabe0544f2666aebdefdc132
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:20:40 +0000

    keepoff flow tip winter environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
