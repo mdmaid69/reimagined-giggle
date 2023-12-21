2f2903c94bdae551e5456e3c551ec3b87c0650dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:32:26 +0000

    repeat chat think phone entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_current_time():
+        return time.time()
