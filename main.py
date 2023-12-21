db3e95a35aa573e3813fdb68330595244ce0195b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:44:36 +0000

    quantity agreement yourself remember whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_error_function(x):
+        return math.erf(x)
