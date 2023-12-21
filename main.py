61c35086c9c1bb8a91abbe3be174dd8fb93b9306
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:36 +0000

    well clothes people involve name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_error_function(x):
+        return math.erf(x)
