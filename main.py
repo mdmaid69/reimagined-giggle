3ce3c25d85a1955e9be496ff5b52d6a7a7c72408
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:35 +0000

    say typical plan anywayanyhow each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
