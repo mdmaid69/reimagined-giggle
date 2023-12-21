8a0e9d0b316c251ef20a976a176e36def9ff92aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:33 +0000

    nowhere bury certain definite corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
