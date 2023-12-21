cf7fa2cb978fffd3d62df80671f68fb3a027022f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:48 +0000

    suffer earn study nervous natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
