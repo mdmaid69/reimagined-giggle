dfd7f1caaa971a7598278704720713382823345c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:16 +0000

    weather each ball bunch tonight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def sort_list(lst):
+        return sorted(lst)
