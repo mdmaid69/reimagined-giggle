0c67a00e012193cfa754e346f4d7c728055e584d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:39 +0000

    slice play prove combination bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
