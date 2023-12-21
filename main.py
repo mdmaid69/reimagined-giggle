5938b2a305142929ddb3561ebf37132e3e02c13f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:23 +0000

    lost belong tape bunch during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
