2ed742015fa903bb477017fedf804a89d880858e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:48 +0000

    fierce somebody list available rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
