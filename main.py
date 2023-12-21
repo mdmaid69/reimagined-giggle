777366b7d60de8fc4f83f54786350075a654ef2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:42 +0000

    ability position mean cause fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import collections
+def create_priority_queue():
+        return collections.deque()
