77110adce7304309367edecbdcdd802cb3eca38b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:14 +0000

    present over skin runover gentle

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
