aaa4346544bd49205db180a9812a52e2fea6104e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:33:42 +0000

    muscle reasonable impossible opportunity chat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-for i in range(5):
-        print(i)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
