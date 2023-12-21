8d17d2eb04cf25d384e7602d7ab4bd34ea9b474c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:07 +0000

    sort health sleep flight advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
