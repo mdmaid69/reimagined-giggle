8789789a23562fc2740e9d957580633e471ebdc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:07:03 +0000

    hate fear delicate post anything

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
