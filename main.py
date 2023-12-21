29b7f8db55bfcf584382e05bd89530e4edf3aba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:08 +0000

    violent bringback news question regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
