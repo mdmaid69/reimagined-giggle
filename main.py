4620fd9c2fbe408001f3c82b8f387491faa4683c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:38 +0000

    bottom early turnoff accidental other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
