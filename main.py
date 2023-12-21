8f9c8cd4617ae26b1508145b74cdc22ce325f360
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:34:53 +0000

    exist really subject shame embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import random
-def roll_die():
-        return random.randint(1, 6)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_today_date():
+        return datetime.date.today()
