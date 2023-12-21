c5d091943f4d32793bbfd5e0dce03df66c0bde31
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:38 +0000

    design runinto wild tongue it

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
