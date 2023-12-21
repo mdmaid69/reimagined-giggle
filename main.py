a2ac706f50e8149e91e90fba272af9b0b9a0533b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:00 +0000

    meet deal meanwhile flavour school

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
