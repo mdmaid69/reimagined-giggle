c96e6d3be41f6e017b6b40f834433c554074042f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:31 +0000

    children within spray minor second

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
