329351daef4a0505d3b02f3f1b0da69828bafeee
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:56 +0000

    injury right journey attitude edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
