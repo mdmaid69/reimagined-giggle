18fb9adb5da4182e62b5e5023e604387d1b1176c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:09 +0000

    emphasis earth middle push dream

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
