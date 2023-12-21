1670ba813df24e97435c4ec0e68a9fed514d73f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:08:06 +0000

    trousers increase furniture bottom correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def count_characters(sentence):
+        return len(sentence)
