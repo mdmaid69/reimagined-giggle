206184ce8b6616420223dd402be0314b01fe2f3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:31 +0000

    occasion comment boring war game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
