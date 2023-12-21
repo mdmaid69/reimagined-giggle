bb9d0b03a8d2e074707cde98404b035dec2071c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:45:22 +0000

    means cap unless now crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
