ca7825d5f526a6cb66ebb39affb08d5226c71051
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:30 +0000

    kind pause onto warm return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
