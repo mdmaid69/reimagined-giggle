fcc5aa064b872c95d7646c4909fc3d05aea7c767
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:27 +0000

    ocean my shout especially ring

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
