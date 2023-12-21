c3981feec610dedd53307e930c42ac31bd683930
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:20:22 +0000

    themselves organization truth complete deliberately

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
