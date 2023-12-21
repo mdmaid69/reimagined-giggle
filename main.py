2c4641d827d03afe38de488ba88bd086530cfb10
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:09 +0000

    sing bother afternoon maybe detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
