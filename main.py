4c3b0a309aab215c289713534703e02021c05fe2
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:51 +0000

    contribute know dance immediate damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
