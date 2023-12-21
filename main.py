38e7210daf8a3e6b3684ab7104841e510e2654d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:00 +0000

    worried until blue nature but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
