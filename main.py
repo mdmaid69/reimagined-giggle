0beaae8dc3594a3fd8c7f4b93779a91a68849c11
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:16 +0000

    declaration break miss taste offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
