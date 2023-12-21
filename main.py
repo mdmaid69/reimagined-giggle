a799a0cb153779f1f42a17296c32615a321f45aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:41:22 +0000

    deliberate snow partner perhaps turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
