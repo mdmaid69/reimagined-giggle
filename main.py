8cb6e923ee7876fdcf63197271884d1a148988a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:26 +0000

    tax accidentally settle yourself strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
