683a4708b0bd297d098159fb7d87e1d405d4bdb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:11:58 +0000

    the try gap compare miss

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
