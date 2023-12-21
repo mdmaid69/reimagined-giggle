4e943508aa18eaeabe376a6fdd4bb0d5bfe14506
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:45:06 +0000

    represent step trade step consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
