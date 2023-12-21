bb2583df6f99c96084cf3dc47bef029e5fea58ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:47 +0000

    Ee tax wise promise slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        print(sys.version)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
