85b528a31f77d270330896b068ee59b42d9f8c78
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:43 +0000

    of follow female spring used

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
