f82354e2d733708fdd7ca13d51a5684be12c872a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:17 +0000

    Ee watch window thanks receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
