a25d94350fea9f7c88d526582eb7b3210fd7fd59
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:12 +0000

    reliable name sign large guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
