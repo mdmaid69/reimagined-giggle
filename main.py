475c7fab62c9c491b52f96fb5e7243705f5a6b2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:44:58 +0000

    job description ring bin phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
