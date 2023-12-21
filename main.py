6ef16d40c3ed0a07f458cfd76caacb675e68fe50
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:48 +0000

    complain pupil radio situation hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_even(n):
+        return n % 2 == 0
