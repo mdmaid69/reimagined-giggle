ec357912c995f35446ab907aa268c7b0ce90db0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:23:21 +0000

    case doubt anywhere shout stick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def count_words(sentence):
-        return len(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
