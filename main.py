302cd6a18dbf9e250db76a2ba5917500c3208732
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:29:55 +0000

    secret little stamp cause drink

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
