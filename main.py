5c85d55acf218463dc3d1478988901b6ad112b9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:46 +0000

    stamp invite succeed drink head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
