6be084f1c0743a9564f15043a9f4b3b05e720d97
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:35 +0000

    cross activity production trick move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
