3c8a1050e700b5c6d4abe9a324b242d16b4091eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:21 +0000

    wish beginning when tense spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
