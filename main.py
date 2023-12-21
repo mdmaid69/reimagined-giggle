6ab0bbedefd2a3f2abf933782edcf34ad592adee
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:24 +0000

    high save that quick consider

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
