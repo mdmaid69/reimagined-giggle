333bb4884bf5ad44f406e460e52ecf7e9fab6ceb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:32 +0000

    sound drop draw wind government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
