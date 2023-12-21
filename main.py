eb9ba7b11d8fb3407395802d55689af7134f69b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:28:47 +0000

    process tear press plenty sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def square_number(x):
+        return x**2
