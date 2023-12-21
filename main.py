112a77dbf369a5a5889c54baf395ba2769a5927e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:14 +0000

    welcome waste money keepout pile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
 def calculate_remainder(x, y):
         return math.remainder(x, y)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
