b8fbcd0e74367d7530970f71f7c985df85086763
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:53 +0000

    guide exciting welcome black band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
