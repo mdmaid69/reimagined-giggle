b8be85aa35eb01c4ae39e2d8b6b34b052b73fbb0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:51 +0000

    protection pool cheat fun pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
