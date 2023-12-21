f17f8eabc942e71bbebfeb65b2a75edebcd8c936
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:45 +0000

    I education giveout chase together

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
