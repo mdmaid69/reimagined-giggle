9f6aae0cdd23a6e5e107d03fb328678ecb4570fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:24 +0000

    exam give exam reasonable either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
