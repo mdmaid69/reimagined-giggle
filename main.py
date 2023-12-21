1ab0bd5d3bdfbdc9b19f8562d69fe95d164f048f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:42 +0000

    pride powerful heat hello rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
