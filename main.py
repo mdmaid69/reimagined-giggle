f4cedee048072c542f7fefb33ce4810474005dc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:40 +0000

    flood breakfast hurt length total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
