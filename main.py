c78b34f3a84ae4436cf2ae9045007320a3ea9424
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:15 +0000

    away matter company reasonable record

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_acceleration(speed, time):
+        return speed / time
