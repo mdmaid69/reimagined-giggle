a41775126752c17c560cbd83c391ca8cc7c70d7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:26 +0000

    pretend will brief temper company

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def append_to_array(array, item):
-        array.append(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import time
+print(time.time())
