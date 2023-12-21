8ff69712a292312eaf4a8ff3dd3b5e897abff028
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:17:45 +0000

    geton bang pain oil snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
