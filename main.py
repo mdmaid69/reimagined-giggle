94d5d53521514e8f2e4df92f77601e37aa3d247b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:48 +0000

    force alone project professional empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
