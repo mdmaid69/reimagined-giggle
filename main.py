fb4880d5b27e36962db56484262544a927de29fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:45 +0000

    score guess clean rescue form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
