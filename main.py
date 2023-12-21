f619a90e7c06fa4ed95e182a7748bdcc0b0d16d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:53 +0000

    secret length violence brush likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
