cb1705acb40f2a6a55ef06a42962a8d7301e9f5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:53 +0000

    border vision pull mine hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
