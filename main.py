fa9a6028550989c193096ee0280a437b04aba026
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:53:45 +0000

    somewhere hurry dry argue arrange

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+for i in range(5):
+        print(i)
