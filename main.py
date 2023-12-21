7f8b02e1d8bb43945da5ed3fd1e917fe822a27e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:59 +0000

    criticism race laugh perfect central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
