f9fde3d12b34cfd440e4c7404188f2a53e2858b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:35 +0000

    challenge copy really land up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
