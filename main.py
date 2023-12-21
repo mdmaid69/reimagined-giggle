75c12550ebf145b5290d8f9f62e19e6c0fb139b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:27 +0000

    everybody start deep replace discussion

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def reverse_list(lst):
+        return lst[::-1]
+def cube_number(x):
+        return x**3
