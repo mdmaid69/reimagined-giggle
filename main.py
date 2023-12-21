cf69dc2a1762a6457e2a9bea1d5f1d58d3bddeb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:31 +0000

    she try which soil important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
