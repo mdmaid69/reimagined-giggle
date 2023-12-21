40de8c52fe173523bdc5ef1904a099604e24e6dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:24:17 +0000

    weigh crowded thirsty statement race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
