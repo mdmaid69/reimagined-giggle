36c7789f92938cc22f1a33bf33b60eea7e7a5a55
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:15 +0000

    true family traffic listen strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
