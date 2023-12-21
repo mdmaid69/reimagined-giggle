849d79b39f195e7fcaf729a52e95390e82cd4dab
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:05:40 +0000

    simple she damage callout professional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
