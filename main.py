be448135ec63eb4ff3e940a2c79b4a880a768339
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:26 +0000

    expensive keen nurse habit light

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
