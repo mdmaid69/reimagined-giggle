1a2ab8a24d2b05d32fd8c8813dc3f0718c6add32
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:45:35 +0000

    fear equal but judge pin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
