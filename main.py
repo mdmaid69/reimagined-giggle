9e37ec9bf488f7b2b91de5199144e8a14f03dfaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:02 +0000

    drawing group eat keepfrom physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
