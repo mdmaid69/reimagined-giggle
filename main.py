e3a26186ba3b4b45b5de04cea9daf109882cd052
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:29 +0000

    his start concerned reach disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
