1814bc88e8599d00d2ad2ab6816997a453e9fa71
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:49 +0000

    watch contact survive emergency label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
