936bf70b2cf911a446bfb4827a910214fb3efd33
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:15 +0000

    fire moment cruelty mainly bother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
