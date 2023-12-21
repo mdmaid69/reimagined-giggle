c9091463ed1b169521d5a769513737c3227ad568
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:09:36 +0000

    name fight attractive fly politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
