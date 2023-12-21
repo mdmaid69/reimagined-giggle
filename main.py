2c9959a1c06e39aa042927ca5b2389711685a72c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:30 +0000

    beyond agreement dinner prefer pain

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
