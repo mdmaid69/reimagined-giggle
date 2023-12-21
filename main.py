6c029f4a6024bff801ed5726976103f495aadb89
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:52 +0000

    matter manner recent takeback persuade

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
