fc8878685ca330d3cb0116c5ecece75442af7164
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:31 +0000

    path fail star direct personal

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_even(n):
+        return n % 2 == 0
