83088df29733deb0e7eedc0b52fd5c29e411fe31
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:10 +0000

    tax development left giveaway may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
