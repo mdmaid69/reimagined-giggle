66f92af46417e213e502530295bbe970f1f8ecc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:39 +0000

    hers duty approve run miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
