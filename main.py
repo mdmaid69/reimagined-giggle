b20753d19dd272b88d22077ca254b0be83402ea4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:50 +0000

    reliable common all regularly substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
