cb606f93ba33b150fee666828f41cddcc1d9667e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:41 +0000

    dance well corner present dangerous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_force(mass, acceleration):
         return mass * acceleration
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
