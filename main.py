21f1e507fdea9d688e77d5e078086b34ac953a6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:57 +0000

    want my near justice danger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
