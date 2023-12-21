7bf3bd168b91d7ca925b7da6e7583abc6834297d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:08 +0000

    suffer something connect comeoff share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import platform
+def get_python_version():
+        return platform.python_version()
