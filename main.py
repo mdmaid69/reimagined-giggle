297bf68189ebf02431b4bbe82e3d6d3156f505ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:26 +0000

    leg advanced close getoff book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
