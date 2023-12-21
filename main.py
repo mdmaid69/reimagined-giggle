01db954990079a0441b07a24df599d997241c58d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:31 +0000

    shock rent reserve public finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
