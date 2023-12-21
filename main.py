d2c1fcf49f8544b7570cf689da207f5eee5dc5ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:46 +0000

    accidentally city design bus recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
