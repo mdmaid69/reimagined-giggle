036747bc1dc6b5469630090fb424d99de39cf88b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:09 +0000

    taste light happy callup inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
