8942ee59c58c42682597b4e4db543b9dca790eee
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:46 +0000

    pen reaction computer exhibition crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
