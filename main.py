258daeefb6623713d1d01e48017ba3a11c0dde9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:50 +0000

    crash somebody say risk let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
