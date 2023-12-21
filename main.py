fc3466e4512ce8ded55f871e1801e9e8729c40d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:54 +0000

    combine level freeze hear good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
