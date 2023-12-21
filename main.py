e92ea34f968db19cccadbed3fab9670469f6de29
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:22:02 +0000

    mark independent cry popular lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
