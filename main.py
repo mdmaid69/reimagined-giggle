36bf7720f79fc2afb1214fea047cc2dba9562ea8
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:12 +0000

    crazy round answer upwards term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
