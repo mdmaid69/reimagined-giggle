77903e695d1f3409da8c25fc5d225c45f29159a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:50 +0000

    have flow survive software across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
