207015d01a54df8b376b41e638d8ac69bac11452
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:10 +0000

    meet common education wake tape

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
