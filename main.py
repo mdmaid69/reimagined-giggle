33a5e86f3c8e1d7529a3d02bee8c74ac17c92c7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:32 +0000

    distance shoulder temperature have joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
