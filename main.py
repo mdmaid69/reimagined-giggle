0899a03a761262c7ab5074dc45831f8c53fb41de
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:17 +0000

    emphasize whistle grand sure able

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
