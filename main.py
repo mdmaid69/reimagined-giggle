88eded197e16a014e5fef5417fc7614e8b2e330f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:37 +0000

    survive feeling complaint certainly swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import math
 def calculate_euclidean_distance(p, q):
         return math.dist(p, q)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
