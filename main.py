b788964b78d53bb26912fe22a6be3da4e1921bb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:22:17 +0000

    professional where innocent backwards keepup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_density(mass, volume):
-        return mass / volume
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def cube_number(x):
+        return x**3
