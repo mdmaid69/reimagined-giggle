9aaf3441341e87e8acf30b9441fa0e51edfd1164
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:01 +0000

    chief earth over die phone

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+for i in range(5):
+        print(i)
