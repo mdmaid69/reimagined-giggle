102d2d686dad8318434812dbaf39dfb95fc51469
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:34:59 +0000

    thick search purpose medium turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
