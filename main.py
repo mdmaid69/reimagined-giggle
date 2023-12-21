f0ac098f39b0aa4df5373bb3b825ba9410981f01
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:06 +0000

    cause post union sentence story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
