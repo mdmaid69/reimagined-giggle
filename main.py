5afb8b70eefa26e32cb5f6851d6b79d35832179c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:41:51 +0000

    could news main holdup steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
