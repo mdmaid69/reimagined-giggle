73cf1699d16b280c2e3c37080d91181d0980d7a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:08 +0000

    allow temperature remove joke survive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
