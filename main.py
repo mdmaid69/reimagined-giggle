4d7e7e330de5e435020bbe1944c6cac788f3b017
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:52 +0000

    government hang age eat nothing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def create_priority_queue():
+        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
