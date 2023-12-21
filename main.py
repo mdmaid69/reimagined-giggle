2d197ed04ac6bedbf967c07fe77564fec5621459
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:19 +0000

    file record practical bad past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
