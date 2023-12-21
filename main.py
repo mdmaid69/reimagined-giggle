a6890c099ab1e9ba8a11ce4d62ff7f91c347f051
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:42 +0000

    suddenly colour breathe mad several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
