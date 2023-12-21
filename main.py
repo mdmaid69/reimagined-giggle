370f66ab34a99e9dc6c73496dbc1de998e65c358
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:19 +0000

    slowly treat close cook mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
