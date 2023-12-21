f6146a8d15b31cbff63360b244973b782da95256
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:33 +0000

    experience otherwise forgive used prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
