b768660287eefb0f4433cd7d540432d3f9e98f5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:11:22 +0000

    some recent hand even minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
