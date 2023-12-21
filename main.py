28631d1cb8e6cafa46e45768c395b61c32bc5dc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:04:00 +0000

    clean suddenly Mm nonsense innocent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
