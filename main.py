f912a4c06955a38d88b5abc1598701610e3b72f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:30 +0000

    manage if please circle poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
