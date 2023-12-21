48e187fb8283d13500010e749f835a488f23f6e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:16 +0000

    content even treat estimate commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
