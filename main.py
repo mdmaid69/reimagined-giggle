df62fcc25076947149e80da44e69d0a0290c1559
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:22 +0000

    long trick rid shame unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
