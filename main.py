e6b26d5f2c1cdbc36bfba813aed71283c19cc241
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:35 +0000

    trade feature question live interruption

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def add_numbers(x, y):
+        return x + y
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
