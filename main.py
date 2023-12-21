7da0396733f9577da340b56e557cc4a88cb0e1ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:16 +0000

    stone freedom refer control evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
