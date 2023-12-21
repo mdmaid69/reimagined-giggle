b35e5fcb08e70ddb37fd12ed4cb6490a7c05e09d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:49 +0000

    loose computer Mm whether regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
