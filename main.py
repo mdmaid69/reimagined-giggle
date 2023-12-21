1dd9245db30de842a21df8b2a1e8a25c7ef24543
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:56 +0000

    eat possibility turndown ground shut

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
