c18bfb20ea2ae7023a7689e548834dbe970ff099
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:17 +0000

    content delicate space most certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
