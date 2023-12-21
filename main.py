8542be0ed2930405dcb22170dd4f3f038248c244
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:00 +0000

    towards punish dare replace heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
