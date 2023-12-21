074c1e0dbc685db0626a759a793c8842e3e96442
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:24 +0000

    dance constantly week expect creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
