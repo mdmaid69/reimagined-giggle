255765d120b1d4fff5af0811ce9459a7c2b8fb85
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:43 +0000

    support wing workon calloff bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
