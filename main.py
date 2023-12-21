9294a3024d4a27c5a6092bee50a492797bed08f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:46 +0000

    low beside sail cap steal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
