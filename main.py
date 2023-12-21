8c7533d8dbaf9a2e9f2cab54ecaa725d2a5f2b7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:53 +0000

    care appeal excellent account cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_volume(length, width, height):
+        return length * width * height
