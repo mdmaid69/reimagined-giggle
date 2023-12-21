96b1e14912c4b8bd1c143688dbb4b2645f126f01
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:29 +0000

    path swimming straight children pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
