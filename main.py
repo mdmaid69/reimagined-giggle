2a9dae64d17907de3a2ffe4ad9099110aeec3480
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:25 +0000

    careless innocent hate display wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+print(os.getcwd())
