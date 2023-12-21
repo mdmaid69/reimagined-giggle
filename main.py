617580fc28062dc0d6d2e2f9046f4b0583798d7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:43 +0000

    steal care offence sense colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
