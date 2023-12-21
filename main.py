1e86269f26cdba8995325ee2e486b41c2c9b460e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:31 +0000

    inquire guess comedown even enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_unique_words(sentence):
+        return set(sentence.split())
