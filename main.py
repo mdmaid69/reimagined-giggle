f48851271ab64016738dce7a92e513a92350f4ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:32 +0000

    fork myself persuade dress account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
