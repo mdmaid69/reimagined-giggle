472d33d15d0c2258f2f323c38cd2b3674f2eeba0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:32 +0000

    curious bush ring cap excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def square_number(x):
+        return x**2
