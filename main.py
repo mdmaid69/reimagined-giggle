91ae2cab95a1e49d7103e6b181f57689cb669421
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:51 +0000

    then service pour manner touch

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
