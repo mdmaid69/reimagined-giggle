48b2aabd1a7bfb8bcff5b41dfb654c50fa0f5a6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:59 +0000

    disagree cut runout handle behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
