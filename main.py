16234b0a08611474dfc0b39bdcfde5b6a89419b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:35:06 +0000

    guilty cheap close along belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
