a16e07958a2b0ce1703c7e875c7ab3107fa4eb31
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:05 +0000

    view front measure rest colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
