e7bb459cb4c2193d8225e4da76135e6c4b81275a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:27 +0000

    turnoff use fashion whole custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def find_min(lst):
-        return min(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
