208294818c143bc44abf20c66dc633ee257676c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:32 +0000

    replace anything plenty apologizeapologise mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
