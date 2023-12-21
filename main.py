2bfa55d45a28d3efe5b1754b0dc5a519082763d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:32 +0000

    grand down suckme under last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
