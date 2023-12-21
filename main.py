84245ac8cfde006f66cd7180c4d4364ec826073d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:07 +0000

    disappointed again unit since protest

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
