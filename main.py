553645cb1802c2eb7abc48d7445ce86aafbf755d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:41 +0000

    spare past call blue comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
