c0f55011f5ddd4d99cf334c1542a28d8d9940f02
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:15 +0000

    climb could takeaway lookaround embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
