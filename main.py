5ca9452c8cff56741b72a5951c183421c67daaf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:34:26 +0000

    slightly front trousers rich Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
