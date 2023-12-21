ec3e5a2989446ce6f0dd3f3527ac018de23d4352
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:29 +0000

    worth trade neat professional completely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
