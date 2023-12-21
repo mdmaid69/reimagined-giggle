c200e9a0a4cb0f1e0824cd0b86dcfe9fc4a87a41
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:21:08 +0000

    this hurt what spill sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
