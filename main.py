c6eb86d8cf607497ef38d92951ef70b166670a19
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:29 +0000

    small off wrap judge art

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
