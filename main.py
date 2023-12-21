6d1e07d50d0bdfe7029f1305270bd48de00b61fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:52:35 +0000

    solve surprising borrow emphasis leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
