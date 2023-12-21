3f6d57caf28e68250f76d5c6a383512ecb926349
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:15 +0000

    sleep stamp nowhere announce through

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
