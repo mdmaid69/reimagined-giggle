b273fd251c6b8b62825d246910058aaad47789aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:21:59 +0000

    correct freedom repair smell boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
