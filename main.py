d6d86464996e6ce2b815857b516c91971a146d49
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:52:06 +0000

    split why clearly check correct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
