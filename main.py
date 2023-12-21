33f01ceba2f350b6e9a994f009f97dae64f89723
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:36 +0000

    coat idea stretch fork drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-for i in range(5):
-        print(i)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
