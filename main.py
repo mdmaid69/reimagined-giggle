cf2f5f22f1f16eae1a5ec90138464663b8b018a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:03 +0000

    correct stop freedom bit sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
