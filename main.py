17dfe95212a52e17a205561724932e3fd4cc36a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:50:29 +0000

    turninto turnout especially risk production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
