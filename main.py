c60ea0dd065716cbd75703c52f948c7fbcca6d14
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:59 +0000

    flow spoil floor trick flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
