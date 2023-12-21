dc6d0e4800710b6175da18d2065f819f283635fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:11:34 +0000

    address sell project curious downstairs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
