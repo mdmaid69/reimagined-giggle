3cf8faf4cca7aca5fb1bd5a97ba84a0b8f5f26b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:44 +0000

    step achieve hang please delicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def sort_list(lst):
+        return sorted(lst)
+  def calculate_area_rectangle(l, w):
+        return l * w
