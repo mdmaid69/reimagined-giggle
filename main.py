56ae304e82249c866b1f87c182f6e36493e46556
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:39 +0000

    opportunity indeed standard best waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
