a470a112dd0a131a6033c1111a3942111425168b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:35 +0000

    open otherwise rather branch part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
