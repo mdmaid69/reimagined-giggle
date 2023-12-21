372ec802be228fc30c2628dd2b5aa28ca6ec44ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:57 +0000

    borrow home noone dream purpose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def convert_to_hex(n):
+        return hex(n)
