6fc9299311d93f4f4db24eba9592ad0eec59738e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:49 +0000

    sell street after Bb introduction

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
