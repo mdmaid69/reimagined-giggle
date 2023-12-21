4ed907d2a7af1b51303a3133ce5530c373565794
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:58 +0000

    mix congratulations knowledge sleep form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
