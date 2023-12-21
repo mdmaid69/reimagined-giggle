0c2042481078761e100e78f65e3037741f617521
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:05 +0000

    contact lucky prefer go dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_work(force, distance):
-        return force * distance
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
