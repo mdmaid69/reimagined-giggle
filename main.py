46975c8ff06bce7a1c0428362a6f57b6f300a5ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:32 +0000

    565end comeover block study ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def is_even(n):
+        return n % 2 == 0
