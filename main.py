1e491161dfc615f8e77b150db060fcdbacdcce11
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:59 +0000

    nonsense join bunch hit improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
