e2f3fc577b0ecb74b253d14c488ca22a3361d556
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:11 +0000

    friend sticky situation cinema couple

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+print(sum(range(10)))
