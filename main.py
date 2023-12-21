112d2bb51cdbbcb021df7c5d30a0eea62bcedae1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:45 +0000

    use where well perfect by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
