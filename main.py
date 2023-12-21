386855a9b4b1fe4af9e9fc852e612087e46e4dc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:36 +0000

    Ff reject loose complaint piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
