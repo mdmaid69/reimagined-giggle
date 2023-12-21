cb43397a93f4b6c55bf2e64d7cee58f33ede642e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:57 +0000

    wild after accidentally list rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
