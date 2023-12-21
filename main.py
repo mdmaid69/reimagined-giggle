2a063ab54f1ce430f6dfc8a4dd932dfd80a14dfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:14:55 +0000

    so open rob flavour suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
