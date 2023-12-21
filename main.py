57d9fdb41ac93f952c64982648d120e9ecda5a7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:39 +0000

    get priority convenient reasonable combine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
