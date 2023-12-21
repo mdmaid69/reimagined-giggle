4442626889adbd3fac057c6264c5b3ce8489b79c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:33 +0000

    force bottom feed degree pause

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
