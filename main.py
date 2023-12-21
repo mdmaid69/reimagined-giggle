b4837c57205def97893cf3804f225422e594a66a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:01 +0000

    warning private beginning content rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
