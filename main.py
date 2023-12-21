907f25a94ffa6fe7c7612dfc333545ac73576401
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:47 +0000

    pick father waste copy noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
