4d2e7ef7cccc5f7bd0ce1a3dfafefb73f1f38a35
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:11 +0000

    computer underneath nowhere today they

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
