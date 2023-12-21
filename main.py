5df7693c64ab522113a6c93f3c7bcb262ab8e302
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:45 +0000

    pure especially dream trap intelligent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
