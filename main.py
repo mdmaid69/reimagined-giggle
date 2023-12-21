60b1470dc35f7e81f2403204f21d4b8c376d13b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:37 +0000

    though laugh opposite our justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
