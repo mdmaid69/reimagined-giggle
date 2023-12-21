ceca4ed0ee764354d4256e438e342ce5c900bb8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:44 +0000

    pleasant birth gas either drink

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
