1979a179ac04e5cfcb655362665e35c9710fc167
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:12 +0000

    confuse cinema true drop stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
