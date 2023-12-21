20b91bb48695a8f39b555dd63ddd98667bb2ae5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:47 +0000

    pleased chance mind turnback generous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
