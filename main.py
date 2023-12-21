641ca8b9c5f52a709432128cfb60134962793c86
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:36 +0000

    sing passage work issue relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
