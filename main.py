85b115ff81916ebfbf8be1709026344db8555b47
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:44 +0000

    average walk they valuable whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
