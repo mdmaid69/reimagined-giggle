86f5fcef65e2839f66e5bfd7d0b76ed97927236a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:24:56 +0000

    cap familiar priority where admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_even(n):
+        return n % 2 == 0
+import os
+def get_current_working_directory():
+        return os.getcwd()
