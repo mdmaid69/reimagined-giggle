064f7b95584210e32a017ae568ac629bcc2f645f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:05 +0000

    shape reasonable uncomfortable movement tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
