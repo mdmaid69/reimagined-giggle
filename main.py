8e52144bbeed1ec0ed41f9d312d877cc3f243ee0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:14 +0000

    to possible easy journey hide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
