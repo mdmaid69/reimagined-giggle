3bf92630fc5c983dcc83a21e881f576438fc51d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:13:09 +0000

    shop fold as ask outside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
