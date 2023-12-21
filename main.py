7dc9307cff24f36e3f3262c4a196edce84045fcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:43:34 +0000

    accidental next middle see aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
