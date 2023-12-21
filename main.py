f71ea1ef06152fa6a51786da0985f90d75819624
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:47 +0000

    shopping burst meanwhile among task

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
