9abdcd5b817e50c3b62f967cf8d7e15fa3fa7185
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:39:20 +0000

    road stupid fire press rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_item(array, i):
+        return array[i]
+for i in range(5):
+        print(i)
