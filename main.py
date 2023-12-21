41edc7b98e4ecd86c21fe1bd0ebb532baddf0039
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:18 +0000

    necessary sight fast on system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
