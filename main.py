67502ca3a1caf83fff0e5121a94b77dcda7c4dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:40 +0000

    feature necessary punishment perfect design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
