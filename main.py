aec16f8aa61dfe35c14df1bf47b318d1220070b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:46 +0000

    nowhere degree ourselves finger wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
