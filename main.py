af4f2415dad4d3e180736e8302b349671ea0e493
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:26 +0000

    drop aware to special error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
