fb817f746451e19acd0b97bb03411b258cfbea7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:19 +0000

    base thanks pattern date legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
