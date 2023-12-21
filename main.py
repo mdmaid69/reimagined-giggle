27305d35c527f7d021ea70c92ba644dacce10965
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:32 +0000

    this name crisis issue appear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
