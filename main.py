d81dd2e0c24670486d757e19b298149f4ce958d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:47:25 +0000

    getaway roll show second Dd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
