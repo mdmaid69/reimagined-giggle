489169b8c9a77ad8177ebdab3f0d9f44d6a16fb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:16 +0000

    stiff sweep hand sick declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
