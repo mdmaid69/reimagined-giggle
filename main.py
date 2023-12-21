c8bd191bd2ab771658eb9e5950b96b4fcfe2cd07
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:10:05 +0000

    afternoon vote human rise permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
