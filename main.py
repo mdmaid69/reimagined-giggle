ba6494f862727f1b3bae5ec65b270573215b8324
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:51:45 +0000

    apart makeup such season pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def reverse_array(array):
-        array.reverse()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_max(numbers):
+        return max(numbers)
