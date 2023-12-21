f2461b33b6eaaaeab480a2f2d21fa8de60bb685e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:29 +0000

    them empty during mend free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
