345c4be0d373037140228517172e108786549e54
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:39:40 +0000

    communication any group behave find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
