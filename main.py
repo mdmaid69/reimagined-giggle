1d18801f56222cfe582939ca318b9045d6361fee
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:02 +0000

    outside meet crash bath long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
