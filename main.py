9c671f2ebd6fa1ee3d4edfab7a9c0392f06bfaa9
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:56 +0000

    wonder lot unusual friend history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
