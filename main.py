bc6c4a32e564a82ac593f34286c03653bdb19fe7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:57 +0000

    protection direct thing part use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
