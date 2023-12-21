0e01fa8dd1dbed7141efa9349cf1e7f6976894b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:49 +0000

    process stay nose short tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def clear_array(array):
-        array *= 0
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
