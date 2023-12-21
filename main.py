94a5c3772a6b82d3bd54f1baa339e3138a1249c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:14:02 +0000

    also conversation tip game share

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def square_number(x):
-        return x**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
