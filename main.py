bbef31e530088ba7eec134e5240bdb3cc93f835a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:42:42 +0000

    house climb cope stranger action

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
