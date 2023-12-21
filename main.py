e6d83ca88286da67e51cfb2a6b52fe5c1508d609
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:58 +0000

    lookafter union whistle shout base

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def convert_to_binary(n):
+        return bin(n)
