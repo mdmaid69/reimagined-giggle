45a0ea31f83d262727e011def77603babfc8195e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:42 +0000

    fight mine game instrument angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
