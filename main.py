e9760a5708f803f77990aaefbcc880f8054d0842
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:55:45 +0000

    tough mystery though war juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
