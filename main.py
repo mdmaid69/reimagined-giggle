6f42ca8d64cb10d59fbf18a1c3ced01bdcfd292f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:25 +0000

    punishment manner throw operate trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
