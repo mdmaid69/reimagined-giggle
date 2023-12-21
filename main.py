6adb4762a7da9dfbbdca191899b9efe83d124752
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:15:49 +0000

    smooth comeout plain recover another

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
