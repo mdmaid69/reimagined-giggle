6f31de155a6fce8096913348801913793528e396
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:25 +0000

    brain possibly unlucky tradition only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
