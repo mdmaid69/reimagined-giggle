33cab038c788b31c38ba3615c1f199546844dbdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:44:07 +0000

    slightly myself term promise usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
