ca2f034ab519b450336e76a15a6f14ef188be483
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:17 +0000

    special star progress why culture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def subtract_numbers(x, y):
-        return x - y
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
