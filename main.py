f256adc4c3a002aba29bec7e3a53dd53e45344ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:10 +0000

    model dry advise how file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
