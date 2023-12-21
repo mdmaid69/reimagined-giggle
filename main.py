82e13cb0a2fe91d6105ce6dfa0d6995eb1d1f982
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:40 +0000

    purpose defeat deliberate nothing town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
