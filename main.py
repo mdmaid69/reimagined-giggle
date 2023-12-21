40544e4a0e0615dd7060793ac8b01069b4ab5084
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:22 +0000

    exploration trip otherwise vote advise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
