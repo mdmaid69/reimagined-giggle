64f44dbea43cd8fdf52af74d907f04b2db6bd7c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:29 +0000

    Ee lock rush regret clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
