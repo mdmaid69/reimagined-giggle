9cdae7d5eede4aae49f3ac0d5bbcfba56606bd87
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:14:40 +0000

    clothes single slow upset copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
