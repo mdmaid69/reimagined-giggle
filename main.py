bb629c4595aba63eb9acb764af3642452407d19e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:10 +0000

    condition equal regret score scene

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
