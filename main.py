76e3b15027280979a4fb2d410c5de25de993bd72
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:39:05 +0000

    drink lookback(on) wealth mysterious again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
