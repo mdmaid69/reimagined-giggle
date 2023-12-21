7dfafe225265a0c2405637fcf7cdc1cf48f1fbdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:54 +0000

    finally bread refuse period before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Words:", len(text.split()))
