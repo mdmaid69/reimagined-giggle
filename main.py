7cff722bf15ba0129927a59afb83c4025fb4e31b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:07:36 +0000

    runover store become aim ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
