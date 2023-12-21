a1b5ff357f6ebcf6d12fae4d5ef5f3ab71b2908d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:09:12 +0000

    majority education people regularly official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
