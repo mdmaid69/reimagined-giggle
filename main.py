498ea042dde6f2341c768cf74a718520ba59c653
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:53 +0000

    none side life bus sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
