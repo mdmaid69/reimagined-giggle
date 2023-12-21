25d0229df1a024a623653c7ba9b173f2f1a54dc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:33 +0000

    lookaround contain slow prevent turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(lst):
-        return min(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def create_queue():
+        return collections.deque()
