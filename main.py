69dd3b76c3dfd82e57ce0550774651f85a877868
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:11 +0000

    image terrible slice concerning somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
