76edf18b7ab30f620e92b120ec86ee5d85e23a34
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:04:50 +0000

    minimum interview there convincing embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
