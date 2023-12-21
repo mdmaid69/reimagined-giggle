0976be084992938bf006405fd69a60e316372f72
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:03 +0000

    politics support Yy keepout top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
