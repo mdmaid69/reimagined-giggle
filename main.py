fb673e645509ccf78a943e0b4dd0f06c3c8867dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:29 +0000

    strike fight wish human after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
