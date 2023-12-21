e6fc1a6aac1f052db11621c8cbc84ba7f4bf5dd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:36 +0000

    difficulty bring part tomorrow quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
