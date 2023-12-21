b0c4c6e9ea9d9b8455750bd6f9b326ef29ff02f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:33 +0000

    window talkinto computer calloff wave

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
