677a7b4b35ec10f732f6b1bd11c98398c1181510
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:02 +0000

    professional vanish special search apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
