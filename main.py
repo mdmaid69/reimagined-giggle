dc30b9d0cf2fe65cbe0fcff4d36a2e185e84c8e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:51 +0000

    share subject fear spend neither

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_even(n):
+        return n % 2 == 0
