3f1508fb65a858d7d4d4389a4667883f18e08b88
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:52 +0000

    pronunciation sight mend class clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import getpass
+def get_username():
+        return getpass.getuser()
