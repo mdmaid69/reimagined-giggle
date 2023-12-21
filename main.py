cb4283ecdcf8e6bd779239a3cf7a634d4bc60200
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:29:37 +0000

    watch dry remark life mixture

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
