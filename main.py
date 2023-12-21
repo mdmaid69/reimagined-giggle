85f25dc689a8a42c4fd17a2e5e23d6a121422687
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:17:30 +0000

    under swim hit amount through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
