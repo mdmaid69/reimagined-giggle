558b718d76d597cae7c6558220bb10983233e788
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:13:44 +0000

    language quantity safe common refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
