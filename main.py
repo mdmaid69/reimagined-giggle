82ecf04f25695214151f00892e63ecca776f4419
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:17 +0000

    intelligent sink bottom nowhere 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
