ce30ba31276de5aaf0d5daba17a6ce3d8a6d58fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:21 +0000

    period 571enough search string equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
