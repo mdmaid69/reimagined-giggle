79c74627d2b0171441993c2389deb8430a330ce7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:42 +0000

    left spite runaway discovery vision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
