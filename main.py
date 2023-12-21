9749ebc5fe49878ec0d84177bd1b36353375226c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:27 +0000

    dare major Oo town ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
