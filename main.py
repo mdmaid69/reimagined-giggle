cac82bd209380e593c180eaaf530525a7d7673e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:42:49 +0000

    culture stamp centre worth getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import sys
+def exit_program():
+        sys.exit()
