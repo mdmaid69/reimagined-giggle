0aff4942f1771096f62f7498ceee36d48f847659
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:50 +0000

    cheque drink shame march cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
