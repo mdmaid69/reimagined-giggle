8d25842093d94af4a3dbb30e31ea5f25f1ac7f68
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:26 +0000

    by adjective young fast welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
