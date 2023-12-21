5b9c85329a3eef8b71869f83103b7abeb1a07333
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:53:00 +0000

    light satisfied lucky catch onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
