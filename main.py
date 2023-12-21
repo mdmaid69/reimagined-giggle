76d7275a60f8378956ff4a590cf402cd06b1b297
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:54 +0000

    getaway wake remember health method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
