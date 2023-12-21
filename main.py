a797ac599639bda92a70312c26fa705bed4e5037
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:52 +0000

    regular world replace boring giveup

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
