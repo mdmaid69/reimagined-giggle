c9d63d61ac849cc62ed7ffbb6b6793dfd1ca3c7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:57 +0000

    dress attractive break nerve maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
