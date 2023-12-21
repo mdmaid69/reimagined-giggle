ed70381ca3ecc7dca7979368e1f5a0bdc05d0181
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:42 +0000

    dead related know go sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
