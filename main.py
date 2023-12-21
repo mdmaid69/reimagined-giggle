c53fba790eaedb6127f20b70b30295eb00746531
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:49:33 +0000

    dinner find type variety raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
