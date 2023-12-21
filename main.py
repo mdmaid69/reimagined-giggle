5c4c2dfb66b5e484f6ac5b50c16ca8b07540d41a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:43 +0000

    project hang age in swimming

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_volume(length, width, height):
+        return length * width * height
