78722b13512e21241ff120b664e8e4d6512b4190
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:31 +0000

    mention track holdon damage plant

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
