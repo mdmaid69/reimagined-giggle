0c12710c93d0bbda5243b7c735d20624996930ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:50 +0000

    run pay number disturb cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
