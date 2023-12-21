6b437f0677ec72af23fe8531a622bfb7ec9b0496
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:58:34 +0000

    real break Zz so sure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
