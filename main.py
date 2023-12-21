f77634c975f3c9f22c7bdb7f2d2eb9b8d052cddb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:12 +0000

    emphasize comfortable reply beautiful prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
