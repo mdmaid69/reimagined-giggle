061636f9fa9a12f8d0933cf567fb5418e9bb6487
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:04 +0000

    surprising pool arrive future copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
