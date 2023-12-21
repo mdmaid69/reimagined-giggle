1e3e277277474177d289e675dddd275c360ae951
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:23:06 +0000

    float comment test file breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
