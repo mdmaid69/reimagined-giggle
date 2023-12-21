3edad6012df3ddf3409acf6f2a3ca3192419900c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:30 +0000

    rarely generally culture disagree present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
