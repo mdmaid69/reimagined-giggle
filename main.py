80ae810fc2a5803b6f6b87db53a33431bc63ab6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:30 +0000

    566end port cheat speed sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def get_array_as_complex(array):
+        return complex(array[0])
