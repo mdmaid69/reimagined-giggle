f65296a381f447acf9f3d5e7952d805492316d21
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:57 +0000

    thin furniture bend already communication

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
