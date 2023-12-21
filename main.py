478cd4761137f6844ce4cc9cc878afa66f00746c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:12 +0000

    lead peace system 572enter hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def append_to_array(array, item):
-        array.append(item)
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_as_memoryview(array):
+        return memoryview(array)
