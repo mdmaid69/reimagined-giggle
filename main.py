8c4a0930fca51fadadd75baf180828c54b756afd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:56 +0000

    hit recommend middle winter path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
