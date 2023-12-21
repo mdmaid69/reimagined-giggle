0469222bebd1fdac3d51f0ccdda7d5cdafa9896b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:54 +0000

    ahead result tradition else connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
