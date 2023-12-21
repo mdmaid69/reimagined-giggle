e8e0cbe28c7ad8aa74afeaadb47bb42874a2f3ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:37 +0000

    empty pocket guess but call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import logging
+def log_message(message):
+        logging.info(message)
