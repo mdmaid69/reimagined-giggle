1a52eccae32979e8ab6462389e09620b7b059536
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:51:22 +0000

    second correct breakthrough opposite nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import time
-def get_current_time():
-        return time.time()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
