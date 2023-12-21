cefdd5e8ebbe0f7c4ab8366dde489bcac44b4f69
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:44 +0000

    his conclusion cause lean mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def sort_list(lst):
-        return sorted(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
