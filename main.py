3201c7f4aea681e681b18126e79f59f22f5a924d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:56 +0000

    snow embarrassed constantly inside separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
