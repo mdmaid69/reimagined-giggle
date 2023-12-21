922d1f4c934c8017f122a36ea235097c8988490c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:04 +0000

    search Zz check there nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_sign(x):
+        return math.copysign(1, x)
