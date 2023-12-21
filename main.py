fcc947240ad3e0d8ce7cc51d0537fd5af428c1a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:30 +0000

    train content comeoff climb genuine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
