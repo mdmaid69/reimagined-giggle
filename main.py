7a34cbba3393a01c3ab63c8f104d55ad1c966e6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:27 +0000

    need neat professional choice lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
