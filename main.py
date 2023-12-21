2b20f5767fe656fec2d723abc33e7f5b5b114485
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:57 +0000

    repair high commit we decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-x = 10
-y = 20
-print("Sum:", x + y)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
