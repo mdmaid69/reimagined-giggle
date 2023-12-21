91a37096e73fd779e233e2cab5f4afb29b827608
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:49 +0000

    letter mention earth inside run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def count_elements(lst):
-        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
