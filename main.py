ca77fb592737d84e044c601b6e92c58c2d150671
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:41 +0000

    taste preparation brief everything flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
