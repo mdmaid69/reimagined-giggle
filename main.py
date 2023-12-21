91f105461c213f4601f8569a6b32c1a5022db144
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:10 +0000

    clue quantity could public breakup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
