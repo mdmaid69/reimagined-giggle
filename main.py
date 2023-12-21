58dd4c098184c4c24b3270543164edac5f1768fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:48 +0000

    drop give fairly all separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
