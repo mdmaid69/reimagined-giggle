f61dc2824aa698e4d8548b8071c9d9f5c4d18ddc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:07 +0000

    keep eager store earn black

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
