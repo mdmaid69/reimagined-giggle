e8b40e2e299bce92e273f669e76ff41b361ad205
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:58 +0000

    keepfrom machine bringforward street start

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print("Hello, world!")
+def is_odd(n):
+        return n % 2 != 0
