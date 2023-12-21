b6fb363aafbdf9f8197324ae478621c7fedb5130
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:19:07 +0000

    safety coffee few out prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-print(datetime.datetime.now())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
