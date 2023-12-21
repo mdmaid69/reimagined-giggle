8876728728d12b75a34f900c8b6c647ccc0db17e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:59 +0000

    declaration sea fine female go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-from collections import Counter
-print(Counter("hello world"))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
