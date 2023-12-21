cefac80d9da6dbfb845abb12ec5e8711bbecedd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:16:28 +0000

    reaction instant attach connection whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
