9c75bb02e86280cd52695e6f9879b628d112ff82
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:22:03 +0000

    only ability force tip swing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
