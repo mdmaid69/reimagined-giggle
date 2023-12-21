bca29bb5058c6b9fcb788dad20d1b2bae37379b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:43 +0000

    dangerous successful cut small content

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(numbers):
-        return min(numbers)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
