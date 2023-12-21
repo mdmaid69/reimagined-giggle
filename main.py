8458c2f4dbc10bcdb19ab9b81afca20a9b3937c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:12 +0000

    post whistle fine drink difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
