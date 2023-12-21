fa99091d6d70ec3b0fe48970de7b600a74385991
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:14 +0000

    artificial now steep finally sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
