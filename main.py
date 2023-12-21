4e63c3d6b1cc734946c1a5cbc79b5d9124125be4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:52 +0000

    business transparent callup land generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
