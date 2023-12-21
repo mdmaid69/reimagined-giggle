7e20901c27c2c34efc5949627d13bd49c78f5fa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:34 +0000

    connected announce gift middle close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
