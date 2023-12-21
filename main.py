a21ee82354ec97e75aac8a61098fe9764e8b9abb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:52 +0000

    share ago defeat personal summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
