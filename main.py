07733c90e7c8e6eebdc5f07c1bbc51759832fa6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:39:52 +0000

    accident family guess argument contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
