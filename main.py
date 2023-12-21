5eead8c4fdf013434106aa41e904e72dc2e41c15
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:56 +0000

    could traffic compare characteristic then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
