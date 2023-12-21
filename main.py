c59735d9d8d8cd34d3614219852d35e9cb3cc947
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:03 +0000

    computer ago pupil brain direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
