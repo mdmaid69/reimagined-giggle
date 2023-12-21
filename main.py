09c403baeb66c328e36e726d05ec391a295a0b75
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:55 +0000

    fetch takeaway fight serve education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
