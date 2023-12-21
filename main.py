87d3cb5a44d7ac3a3e1c2a721297174faaad72bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:07 +0000

    blind left brain immediately duty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
