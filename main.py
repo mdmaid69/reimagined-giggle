0e5df3a0ed360629602842bad889b7f6dda432e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:01 +0000

    564encourage pity invitation charge expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
