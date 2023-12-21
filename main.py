b9a98634a67ed0e372714e0c721d43e3c53ae602
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:43:12 +0000

    family wide place geton bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
