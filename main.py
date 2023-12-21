579d0ce48ddfcf6407003ee0680d356e58f56aba
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:30 +0000

    music whatever original watch live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
