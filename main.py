c94ffdb32df034fc6f2c16f18669d32e75c71a02
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:27:23 +0000

    difference dinner range develop store

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
