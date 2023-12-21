ac7d23a396cb1f7303361ca37480e3366261d5d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:26 +0000

    scale head shoot bye live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def convert_to_octal(n):
+        return oct(n)
