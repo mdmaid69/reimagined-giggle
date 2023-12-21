596b8a1c303ad2e0e7fadb71bade204b7ca38bb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:45 +0000

    ball direction broad fact sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_force(mass, acceleration):
+        return mass * acceleration
