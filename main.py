5189ab78144cafa85033c41080d806aa27efe418
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:34 +0000

    threaten top uncomfortable rest soft

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
