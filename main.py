b2a13a0b440c969d8d8ff8b0266b5fa9e1f851b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:30:29 +0000

    spot development last wind general

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import sys
 def print_python_version():
         return sys.version
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
