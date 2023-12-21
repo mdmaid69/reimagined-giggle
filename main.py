e953c0f089f6c9ded9ca34c2d8c730c5f057b3a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:40:07 +0000

    sharp call risk criticism base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
