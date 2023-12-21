90684fdfc167a81a64ba7b10b539b80c926ac23c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:31:15 +0000

    now matter female famous poor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
