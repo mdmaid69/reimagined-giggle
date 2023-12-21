8df7aa675f9ce07b86074da89a8d3269b8a452b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:51 +0000

    married manner unfair sound 179bite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_string(s):
-        return s[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+print(json.dumps({"name": "John", "age": 30}))
