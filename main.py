c48473e015bcfd2a336caca7d09deb1a405d4742
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:18 +0000

    570enjoyable vanish worth fear turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
