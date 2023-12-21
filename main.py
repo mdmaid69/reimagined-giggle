8c0127e23aa12acc3cb3f3a7142fdb247856a9d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:52:53 +0000

    accidental congratulate scale nonsense basis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
