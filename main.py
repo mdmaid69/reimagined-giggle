16c32fc901b42695cfdbd14d39216439ccc14f69
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:59 +0000

    section tape trade after thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
