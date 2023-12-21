60884190d21e15c9118aa5b72c14d8e92823e8ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:19 +0000

    flow time surprised let wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
