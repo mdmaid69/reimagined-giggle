7a27e4ec1d85a1c2e6c0c612c69df1f97237d9a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:57:47 +0000

    image fit leave bank comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
