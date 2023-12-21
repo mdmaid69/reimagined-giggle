a16dcbe28c9691c6f4c4a16fd3738b048c6d8dc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:16 +0000

    word listenfor rob doubt round

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def add_numbers(a, b):
-        return a + b
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
