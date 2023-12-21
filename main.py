f22215006e03db41ae2973f7467d68f824b844dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:49:06 +0000

    emotion interested discovery rest they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+print(os.getcwd())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
