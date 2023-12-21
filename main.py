4cf8776dfb2f8db77a2ca7921c5ce9add42d7948
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:11 +0000

    knowledge many visit Rr arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
