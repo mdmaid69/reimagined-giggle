10170f2590b440e0d1732abeb2c06a315b1b2c9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:48:02 +0000

    star protection control Ii punishment

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
