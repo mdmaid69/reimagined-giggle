60d691ba31a79598e07e609b8b6e599607460acf
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:46 +0000

    arrange exam ilovedicks square concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
