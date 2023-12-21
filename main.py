f0cdd092a298d9b73844d18aed6d0b53fecbde2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:23 +0000

    Hh 569enjoy amount experience broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
