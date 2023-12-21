129f2a9c2c1b9e74d4350bc5a2d8fca00767d8b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:54 +0000

    possibly lesson tend test software

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
