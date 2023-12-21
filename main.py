39076b5922e0dce28fa55cf192e38a1c9474f53d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:22:19 +0000

    they hit play start grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
