f1fe0de5e7bee4482b5ef35f94d1ef2d7ba28c8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:30 +0000

    issue hurt cheque word early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
