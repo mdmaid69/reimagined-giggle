cc774b092ec0226f1936c001acb84def18ba3c41
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:48 +0000

    beautiful wrap keepoff uniform steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
