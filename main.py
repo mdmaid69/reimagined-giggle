8f3ce91f3eeb1bf10f919c370bc06435d8ee26f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:28 +0000

    basic let you threaten second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
