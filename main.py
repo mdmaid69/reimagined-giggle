f966ab797cbffa97486c74c651997e63ecf2fb2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:03 +0000

    slip beginning church certain way

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(x, y):
+        return x + y
+def subtract_numbers(x, y):
+        return x - y
