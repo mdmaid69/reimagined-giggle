61ecf1f3e0a74749865953219781d81454e8adaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:54 +0000

    fold cope notice coffee reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
