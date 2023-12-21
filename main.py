524cf541e4ff3ba8ff5b8f75c52aa805773b9622
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:49 +0000

    strike brown at please your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
