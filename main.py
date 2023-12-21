10c3c3c3b7268d05f49565eb64ca488e18b57572
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:36 +0000

    second weekend point geton straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
