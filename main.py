9d485e29f7710fd42e4ab8b9a1b40ae0749b3d44
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:07 +0000

    port future intelligent collect balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
