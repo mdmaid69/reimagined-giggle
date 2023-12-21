2245c5a9d1b51aaaf824610155c4db580accdeed
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:09 +0000

    measure getout star practice talkback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def add_numbers(a, b):
+        return a + b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
