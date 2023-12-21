eb669856e7c698583eab1c099e8deda4625f4d5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:30 +0000

    side from regular reject favour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
