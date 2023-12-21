69db325aa7e03cd256b4ee1394acf7f678fa4921
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:37 +0000

    voice powerful relation ever convinced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
