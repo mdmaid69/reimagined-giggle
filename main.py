c06b79b6c2f69e27bdf975967e8b43ae025ecae4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:08:36 +0000

    mess interruption arm lunch this

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
