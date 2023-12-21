8d3d51e955c5be5604677cf461b62fb1a24a1c6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:36 +0000

    holdup onto squash evening that

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
