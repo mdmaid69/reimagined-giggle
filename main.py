0ca92e688d8c5cef172f40c48e9e88580d454844
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:17 +0000

    waste progress defeat public occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
