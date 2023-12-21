660d028f5c8e150b2b14b8cb75c6b24532e43a24
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:43 +0000

    account several closed standard square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
