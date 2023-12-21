470413937f661a108e1b44c031421f0fab99bdc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:31 +0000

    gap charge gather rich draw

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
