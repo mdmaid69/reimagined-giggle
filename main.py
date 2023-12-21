7bbff752aa92ab19835d92c56af04978958b8e0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:49 +0000

    real load record getback recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
