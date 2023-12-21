7024b12fba01661c2dc769f9473d05d947b0e78a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:03 +0000

    lookafter admit die successful finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
