8776ae0b8803df5babe5a9a786de765d8485333e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:07 +0000

    subject every around stop knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
