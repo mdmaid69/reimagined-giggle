77d850a8e396d24b83d316e386c52fe76d5ad6ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:42 +0000

    lookafter basic smile similar book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
