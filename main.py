f03448ebe9d09e375211581814efc2c9e4d219ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:46 +0000

    sound forward estimate grow black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import getpass
+def get_username():
+        return getpass.getuser()
