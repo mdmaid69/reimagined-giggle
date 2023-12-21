4ba1fa9c84b98109416acd7ab958b0e19049e774
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:20 +0000

    please positive among trouble answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
