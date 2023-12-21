7373884a0a230ce79e5438c06789e551b180e1bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:37:22 +0000

    foreign if clue single accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
