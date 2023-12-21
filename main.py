f593c08a63146d88f1a485fb9881e931f9f836f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:08 +0000

    backwards Rr stop mother object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
