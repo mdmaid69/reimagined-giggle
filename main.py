ce52557e20722ff7f43edc7984728ac303dc69ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:27:19 +0000

    exploration new tend project loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
