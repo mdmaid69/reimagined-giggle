53dfee466221bd0e196c537fe743957646e74249
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:34 +0000

    him take it unless if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
