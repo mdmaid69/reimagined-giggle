8f7b476768d9d858755ae4daafe529c95e258389
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:11 +0000

    direct much stretch beautiful live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
