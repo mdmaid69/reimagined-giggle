5f0749c9837de1f40da3d3452b1b733b31387ccb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:12 +0000

    suggestion congratulate valuable avoid march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
