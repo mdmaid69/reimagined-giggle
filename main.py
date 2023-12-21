b9233000ae202332799fc7f8ad40857a233e9d5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:27 +0000

    church threaten warm present cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def cube_number(x):
-        return x**3
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
