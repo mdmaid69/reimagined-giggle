4f2ba8bda292f6a196709059c96e26e75a97a877
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:12 +0000

    effect crowd blow stand circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
