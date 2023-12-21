1fa550147454a0a163f9737188ec2e7c4c14ecec
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:55 +0000

    tip opposite sit compare lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
