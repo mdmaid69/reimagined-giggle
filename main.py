2d0022f28730000a460ceac37b6469ce3914c0a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:05 +0000

    know insist wall noone offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_power(work, time):
+        return work / time
