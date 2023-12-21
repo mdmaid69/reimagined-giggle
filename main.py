20c55d7a09911af71005c9d0e046980e8c0de09f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:00 +0000

    people jump march rarely politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
