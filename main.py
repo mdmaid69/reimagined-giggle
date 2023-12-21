a32795348cb970d4282eee10666430e8b736f7f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:39:39 +0000

    pile across frightened happen keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
