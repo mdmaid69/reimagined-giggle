5023d774a817668dfbb365eeb8c48f6d4f40c389
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:05 +0000

    sort acceptable passenger exit polite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
