6f905f2a4aea50bac509ef5d91b79fa016d3f5ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:06:47 +0000

    cut surface load criticize black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
