a8adc596c5866d2fe4eea25148d48ae86d15e08d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:48 +0000

    practice wind repeat city heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
