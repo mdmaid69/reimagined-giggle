6484c40a85d6a1d2a081aa1a62acd7650f6d9a82
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:59 +0000

    Itself guess pride getaway float

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def convert_to_octal(n):
-        return oct(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
