677c16a9b2b03fbb92916cafd5b17c53f7fbd584
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:05 +0000

    punishment everywhere fair emphasis moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+print(os.getcwd())
