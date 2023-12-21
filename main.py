3886ef346b82cee01516b32911aa03f24f2f8b46
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:19 +0000

    usual hers effort lot difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
