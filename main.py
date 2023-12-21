8c5326be58fdc1fba3f0a8120c720d4becac11d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:51 +0000

    mad load she place medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_density(mass, volume):
+        return mass / volume
