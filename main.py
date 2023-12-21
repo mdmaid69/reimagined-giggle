e791e9cb7bd2612ded171153d78efad40551797c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:03 +0000

    little trap or comment account

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def reverse_array(array):
+        array.reverse()
