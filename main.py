d8b7c03e9c2c48a20e53fc725eebd74e098817ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:23 +0000

    him spend stand woman relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
