d07b4cbd4d93a349406c8ccdbbca8d224f30a7e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:26 +0000

    build pay communication defeat communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
