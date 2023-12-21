89ae55fe55a399a98184f1ae1844344f9d9a07bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:04 +0000

    scare rub general buy strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
