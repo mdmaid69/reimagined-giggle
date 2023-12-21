c7efbf21f420f4eb97bf652adceb249adf697d2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:19:59 +0000

    breathe some pity drive cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
