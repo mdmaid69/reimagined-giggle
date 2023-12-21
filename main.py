d2c11ba6aa3f75eb56d405ec0c9d8ee63fb6691b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:53 +0000

    rubbish new jump see concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
