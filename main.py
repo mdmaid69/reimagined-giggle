04d6bc95e8805c81548b7f101871e4a592dc4a3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:51 +0000

    name favourite love towards scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
