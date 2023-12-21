cfdeafc69c1beb7edf4e2349d291a4ad408323ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:39:22 +0000

    bottom makeinto nothing produce fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
