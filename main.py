615c50435817fd7dc2d24781f289a7252a4580d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:50 +0000

    why beautiful black act sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
