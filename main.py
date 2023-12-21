f030fd9e8d24f4395795d00f19d39c20aca517b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:49 +0000

    favour one worst lookafter Ii

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
