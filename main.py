e55429a91033ddd7bde6fe0fcab89afdfe6fb9bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:41 +0000

    trap worth public worth innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
