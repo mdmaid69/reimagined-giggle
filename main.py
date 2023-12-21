f3b5096f530771e34721f79f8a749dc90e4cbb76
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:33 +0000

    I fast matter foreign goodbye

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_length(array):
+        return len(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
