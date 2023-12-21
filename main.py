dcfb5205bcdcdba420fcb8acd33e839972731703
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:42 +0000

    each crash drop weigh emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
