f04fa1f16d264a6ec314dc40c6c4e9ed73b0d54d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:27:37 +0000

    Oo light turn peaceful autumn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
