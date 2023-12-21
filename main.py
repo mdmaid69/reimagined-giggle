d97136c2d1d72234102c69a9c31685279fe51ece
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:31:12 +0000

    original sea treat live remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
