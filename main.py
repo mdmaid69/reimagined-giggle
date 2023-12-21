3510b0f6323704f6224d43a3ce37115bc5576bec
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:29 +0000

    comeback turn goup last under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
