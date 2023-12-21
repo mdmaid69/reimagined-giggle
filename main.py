4c6bf5d676b0a002ac9dd1ec99fc442a0d20557d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:49 +0000

    light completely sense complete famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
