75f9495c13b8b5f173495f7f7aa4a60f6f250e47
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:58:37 +0000

    away most nerve some plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
