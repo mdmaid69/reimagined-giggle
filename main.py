2c5f84b40ed7d13f670882d1446a6aa039155f89
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:56:10 +0000

    suit lock home fast basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
