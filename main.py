0807966c6f762f898147beec8fe6b69959036028
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:41:31 +0000

    rush lot wheel progress yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
