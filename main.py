9babb279ac06631a2855582830178cc439e08e98
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:51 +0000

    nice comeround of upwards play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
