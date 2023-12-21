bd80dc6e592f1f37439a037a15668b62c1f947a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:49 +0000

    through holdon along various obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
