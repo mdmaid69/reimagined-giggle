25afbcb48c91b85549baaf12d2e31809e1c46d21
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:56 +0000

    discussion sky minute race secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
