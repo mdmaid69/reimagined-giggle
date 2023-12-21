6c51fa24d5d1143a79684959478f3454c2cdcaa1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:04 +0000

    pronunciation see bury lend quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
