78a657cc38dd3015d29de80329e73064e8dd6bd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:43 +0000

    rise her buy opposite plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
