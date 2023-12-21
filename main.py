75f577370a442948478b1364076d3718c5576d5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:54 +0000

    loan Ee load shadow flat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
