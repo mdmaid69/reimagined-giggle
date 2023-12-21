abd7b784f558863a3a94b54998ac5ad72e0c4909
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:32 +0000

    spring shop turnon road strike

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
+  def is_odd(n):
+        return n % 2 != 0
