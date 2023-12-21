6a0130b1756a146bf234f324954942200d69d08d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:05:36 +0000

    well burst organization nearly violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
