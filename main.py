034432790aebb8a84a703ee3e38485f2bdc00567
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:46 +0000

    wonder he play none thinkof

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
