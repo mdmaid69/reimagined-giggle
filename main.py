bf51c3d5d8b14ee72655b2fc279159e91740f289
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:10 +0000

    necessary official bag unable skirt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
