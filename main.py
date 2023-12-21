b5adb7b77a60ff04742eba947720ee1b0cd532c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:55 +0000

    sea shock mistake shock marry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
