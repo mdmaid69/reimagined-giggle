6f1b4862775edb84c12c902486c93689e4370df0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:39 +0000

    field mark pay full occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_pressure(force, area):
+        return force / area
