dc2da26ad5172cf382371f2e875de7e0c8349c8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:46 +0000

    tongue where active leave calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
