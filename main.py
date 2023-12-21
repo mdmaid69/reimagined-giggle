6c83bae9f8a19b305f586482c896a71abbcc73d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:42 +0000

    escape man tap delight label

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_min(lst):
+        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
