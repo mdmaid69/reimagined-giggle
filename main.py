f99abfe461fef455d45021032fe85cacd870a9f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:56 +0000

    Pp accordingto hello turnaway work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
