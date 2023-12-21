9941c568771cec3313b5cd9f531050473378c8ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:32 +0000

    seem whistle arm Nn Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
