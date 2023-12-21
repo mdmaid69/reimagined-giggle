0fc32da2dbc16ca0471317c4528e70539fe7f3a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:17 +0000

    message 572enter man would unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
