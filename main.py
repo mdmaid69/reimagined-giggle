52babdfb177234d43ded002e337a4324912d848e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:05:02 +0000

    day see central prevent quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
