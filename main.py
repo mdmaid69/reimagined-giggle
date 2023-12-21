4715168467e1745657a71f5ebea6cd85e5d744be
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:37 +0000

    precise like dig agree distant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
