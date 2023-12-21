45ade1d2f225d1b639a089eb9de4098c5894b389
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:27 +0000

    buy life song concentration themselves

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
