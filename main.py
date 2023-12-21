c515395fae24d94421c112d139c2fcb54a6ace20
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:33 +0000

    trade design bad connect loss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
