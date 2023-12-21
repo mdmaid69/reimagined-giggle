9a23c010167c8a0fbd3045515de6e85210c9fd1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:48 +0000

    turnoff ilovedicks use dead interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def reverse_array(array):
+        array.reverse()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
