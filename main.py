37a3a6996224319972ce183155b2dea07db9a728
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:18 +0000

    too fail survive train broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
