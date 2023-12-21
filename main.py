ba58153b21fca0a14b26abe2b583dd2c49339e58
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:19:21 +0000

    relationship search of pronounce excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
