5efd56d270942b2e92de72ef92441113e336fd8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:12 +0000

    quit ourselves near example present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
