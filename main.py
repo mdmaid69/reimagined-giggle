fcb5fe468a3aa261de890e9db96003ca98b83d9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:29 +0000

    notice turnback deep scrap spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
