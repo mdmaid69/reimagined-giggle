ae3dc41ba660cd623abac830938a0dac06e91b7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:29 +0000

    original light dark nose lucky

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
