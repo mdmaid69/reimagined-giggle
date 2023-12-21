9d0c4ab40d3929153d2f0449e5b013698e1c1e00
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:56 +0000

    all downstairs pattern round house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
