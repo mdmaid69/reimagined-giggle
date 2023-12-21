2e2dd15e32a8b8853e2d254ad361152b50993126
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:36 +0000

    back sort announce platform print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
