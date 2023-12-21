57c4d00cb078677b4692d130103f7e2dd6a3f7cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:54 +0000

    delicate inquire spoil find any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
