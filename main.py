13fc6d75064c5e0edea6ff70095dc184a1bf3bb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:59 +0000

    book shop because dance dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
