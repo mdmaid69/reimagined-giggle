dad0f2f21953e99d0869f17feca9e825c2af775e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:12 +0000

    571enough offer stranger takeaway reduce

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def count_elements(lst):
+        return len(lst)
