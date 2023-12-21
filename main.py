145f15de8694aa9ee35a2d5ddc41eac47ea6b3f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:25 +0000

    knowledge see shut Cc one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
