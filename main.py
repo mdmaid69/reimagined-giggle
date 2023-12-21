3a201a87e7cfc9eec2e860b4b3f9fbd3501ea252
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:40 +0000

    object future teach difficulty dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
