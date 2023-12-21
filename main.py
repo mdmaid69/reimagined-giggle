e2b425ed102d29749989d6b480d66990b37bff53
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:50:56 +0000

    getoff match present hand like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
