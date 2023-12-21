b83a3f1f20e3075fb82337209398397d7b119fc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:21 +0000

    object similar reason whistle continue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_current_directory():
+        return os.getcwd()
