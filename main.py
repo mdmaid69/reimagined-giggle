ef22a05ffe374adfa9de6b69ec0908124a44dc5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:27 +0000

    goon think attempt habit speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
