9b055b3b93ce9c687a6e3aea07f2b56137e87882
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:34 +0000

    spite look available strong claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
