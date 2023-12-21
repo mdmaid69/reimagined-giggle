c081385675379ba8d2b7b3b7516ae9e3040c1a39
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:56 +0000

    account splash welcome experiment strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
