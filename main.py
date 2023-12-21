75d18514a57f5419d640a13c9ab2628301c35ab0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:44:19 +0000

    lend programme gas wide drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
