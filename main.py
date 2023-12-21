6e83de2fdda3c789d413dc0f5e32455ce23a2a43
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:17:48 +0000

    comeout cost breakinto book everything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_stack():
+        return collections.deque()
+  def convert_to_octal(n):
+        return oct(n)
