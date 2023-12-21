a607ab527978fc05565c43010bafba0d22c3d8ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:29 +0000

    holdon keen wish grow accordingto

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
