d2dc37c90c103e83b1b08849a7a723ebc7a1fc57
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:27 +0000

    better them gift under say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
