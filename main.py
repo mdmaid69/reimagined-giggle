ac15b36903cc577421560edec73a69d55f061153
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:19 +0000

    trousers action one crisis tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
