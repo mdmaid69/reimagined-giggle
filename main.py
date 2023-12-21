c9b581b0ce40d0f752aef7f0075cff118a2884bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:41:16 +0000

    Vv object opposite explain hold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
