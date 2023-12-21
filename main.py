88d7f1a99ac837ce48b637c4447f9ae27ad898c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:47 +0000

    single experiment picture achieve independent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
