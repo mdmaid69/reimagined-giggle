e307f3f14a5d872e50b73fd936725933df6b0af3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:17 +0000

    breakfast number dark square sound

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def subtract_numbers(x, y):
-        return x - y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def find_min(numbers):
+        return min(numbers)
