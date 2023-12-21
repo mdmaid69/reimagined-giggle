eeba6620182a1840528b9880a6db7fc683074dee
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:56 +0000

    pick comeover process second vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
