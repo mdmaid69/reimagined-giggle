9cbedd9bea54c12b89f4c44318f5f29450ebaff1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:24 +0000

    punish seem prepare in strange

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
