460aeed896b93ae27eedc5a74aed79442b40296b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:40 +0000

    guilty bath worth process ours

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
