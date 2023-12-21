554ba8893823103df3770b74c901e2248b93fb72
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:17 +0000

    tread whisper eye absolutely dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-print(sys.version)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
