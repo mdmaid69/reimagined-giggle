b4391cc7a06c6dce74878dc6d498444a6d02d547
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:35 +0000

    some Oo ocean time idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import os
-print(os.getcwd())
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
