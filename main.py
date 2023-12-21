61713f77badae76a495e0dd340848356b656c2e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:25 +0000

    grateful tired present step ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
