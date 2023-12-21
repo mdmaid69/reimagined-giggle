2548e0b8ca6f11306eef509f54eec5b8bbd503d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:42 +0000

    nature combination part lock earn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
