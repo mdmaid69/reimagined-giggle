3f97d6410f25d69a07b0420743c363b020e9a4c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:00:46 +0000

    disturb law of left light

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_area_rectangle(l, w):
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_rectangle(l, w):
         return l * w
