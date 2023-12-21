0a4bd5a3e20ec5e685eac626712723057706a17c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:04:39 +0000

    turnoff awake majority slice cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
