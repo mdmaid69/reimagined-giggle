b6f912364d5e6f4cbf710c5d98eaabade9f1918a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:59 +0000

    office food lay stage attempt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
