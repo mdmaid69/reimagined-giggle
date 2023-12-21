9be4843ae583a573334e221f9f395b56a1303af1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:32:31 +0000

    hope start pain call unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+print(math.pi)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
