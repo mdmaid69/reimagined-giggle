d7715ccc732dd38e641b60334c7d79fdd34e4bb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:14 +0000

    room second Oo affect recover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
