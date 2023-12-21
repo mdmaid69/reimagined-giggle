a9a063fd1458abd95ef0549924f998c8cf9e2839
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:41 +0000

    cinema final disagree spot underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
