abd1eac45d69cadc4e549965e873ce654d344b89
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:42 +0000

    defend summer see proof mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
