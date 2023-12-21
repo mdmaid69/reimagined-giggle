75d1f7a8bc9caf7f8a857d7207516579e66371a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:55:28 +0000

    clothes crowd rate love message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
