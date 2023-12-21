d96033e434a1071f1a155195ed2f364315b557c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:05 +0000

    refer child treat pause imagination

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
