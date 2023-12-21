089561a939c75bf1ccd1c6c8b66565eb9aa03b56
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:33 +0000

    tape sweep squash clean Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_index(array, item):
+        return array.index(item)
