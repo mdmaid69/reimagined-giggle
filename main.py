88ed791a0d40711be3f6fbf6351517627127d758
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:17 +0000

    company mean copy sure rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_priority_queue():
+        return collections.deque()
