2dffce4e989e747e95bad9654e9f641b8c811ebf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:47:48 +0000

    happy address corner exact pity

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
