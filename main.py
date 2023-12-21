1926031664a33c47a90ff7359ea1df8e88f0caa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:19 +0000

    worse pen record by insist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
