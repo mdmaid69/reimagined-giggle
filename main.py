d4595a8ca18966a21d331634e20554f8626fe489
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:06 +0000

    practise kill lie out pen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 print("Hello, world!")
-import collections
-def create_stack():
-        return collections.deque()
