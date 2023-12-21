0b999951b047ced80088db95c2ecc70c3599b0e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:17 +0000

    hit length hurt heavy disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def reverse_array(array):
+        array.reverse()
