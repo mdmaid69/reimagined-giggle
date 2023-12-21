93c0f11bcbf116a14a72d825159a0b196958a6e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:47 +0000

    ahead scheme Qq pronounce exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_time_since_epoch():
+        return time.time()
