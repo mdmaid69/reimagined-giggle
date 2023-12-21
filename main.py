8a703941f5e28c445ea9be950f8e3c479e51d2df
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:34 +0000

    name mystery appearance bread switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
