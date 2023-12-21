949ec6edb52e0ee65b2d7291d6b7dbe454d67df4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:31 +0000

    actual provide sudden method challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
