4c1a34f82325c0539372f707f8caae8c2e99fca5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:33:08 +0000

    year address produce arrive takeover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
