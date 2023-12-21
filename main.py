361f64da44f1186914200bcfd97c548c2859f419
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:25 +0000

    marry building usual something fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
