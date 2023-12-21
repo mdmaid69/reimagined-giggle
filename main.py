548c6bb7653bef6347131b34478414fa583d9b45
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:10 +0000

    ill sound Jj tip take

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
