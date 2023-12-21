fa54eae4b34f0512ef886b3766796e9369df4862
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:02 +0000

    cheque person minority combine anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_cylinder_volume(radius, height):
         return math.pi * radius**2 * height
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
