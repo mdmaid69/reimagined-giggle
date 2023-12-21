c4c152026e314f92b5907250ff90b4c1cf31d9a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:29 +0000

    connect praise 573entertainment break sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
+  def square_number(x):
+        return x**2
