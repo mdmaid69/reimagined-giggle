ca65d834fc6172e7d4edcb19c2fcf8c70b72402c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:20 +0000

    nonsense especially Cc wake gather

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
