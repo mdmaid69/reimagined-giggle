16fc7c1da96f5221af1124a8e6335414bd69c49b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:31 +0000

    smooth beneath generally theirs example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_max(numbers):
-        return max(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
