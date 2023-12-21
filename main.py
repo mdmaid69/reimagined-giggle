af945ce835e2414282ee185731112ee7a489a2f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:27 +0000

    name wide mouth feel too

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
