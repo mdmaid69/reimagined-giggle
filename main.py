ca9a862d939286287d05401267beaed639556d8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:06 +0000

    part feeling increase beautiful test

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  def is_prime(n):
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
