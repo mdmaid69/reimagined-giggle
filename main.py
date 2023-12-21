7a3ee7ff3020e5e048a29bf7e34dd3d12f572afd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:12 +0000

    name classroom allow polite common

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
