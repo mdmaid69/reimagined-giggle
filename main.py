4e83038285fc45076d4b4dd280ad591d9cdd0308
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:46 +0000

    left mixture Nn division him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
