af899f554a9899338dfa2e9923320af52d1b9213
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:41 +0000

    ticket agreement lead hardly rule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def print_python_version():
-        print(sys.version)
+import time
+print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
