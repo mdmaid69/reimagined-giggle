288613e0bff53c8facdbe3b0b9c47942952c30a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:35 +0000

    win private style total effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
