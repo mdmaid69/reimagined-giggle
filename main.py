ed3a80ee9deee1a8556e23f79e0451c7c4f7cf52
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:19 +0000

    whenever uniform patience Pp air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
