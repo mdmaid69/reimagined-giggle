3b1e1b1298dd8215fcad7a57072e189b8ec1e42e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:31 +0000

    willing eye work young whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
