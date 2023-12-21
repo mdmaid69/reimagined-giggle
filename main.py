c21538d6219b12abd87a590693868cbf7f7b1bec
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:47 +0000

    loose extremely schedule roll nerve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
