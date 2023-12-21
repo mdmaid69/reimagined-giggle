c093db77503c8ae7a8b9f3c4292df57970ac9432
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:03 +0000

    corner sick mad like best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def append_to_array(array, item):
+        array.append(item)
