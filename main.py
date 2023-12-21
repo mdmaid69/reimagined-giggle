46b9f37b42e7f9da3bc36ba51f608688b4af8b91
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:31:28 +0000

    skin tired design callat convincing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_acceleration(speed, time):
-        return speed / time
+def count_characters(sentence):
+        return len(sentence)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
