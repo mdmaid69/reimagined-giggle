23d47bee9a78104e737a714da6211709e33477ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:16:16 +0000

    strict muscle murder choose none

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
