3b391e07b68ea6dbd99d9abb83331361729f179c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:26:51 +0000

    scream absolutely home firm interruption

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
