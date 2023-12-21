7cf46a695f16392965407957c7e30b2881425659
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:52 +0000

    turninto frighten gate only trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
 def convert_array_to_unicode(array):
         return array.tounicode()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
