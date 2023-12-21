d40143f88ab33c23a2137eaa805fa286ef4c67e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:09:09 +0000

    comment possible something organization doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
