dfc2b0c33cc81eba8dccc67905fc7928eb7d5ce6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:13 +0000

    complicated future consist thankyou sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
