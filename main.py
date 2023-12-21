079eff02d80135b1779b4f64897c6b5bd4b01ca3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:54 +0000

    interrupt art wrong check hate

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
