4d09fa936fa092260e71981be75fedf55219460b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:33:57 +0000

    town wall simple used direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_power(work, time):
-        return work / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
