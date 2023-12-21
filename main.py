6884c75d4278986e4f6e1f144d820c20ed0afcdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:06 +0000

    unkind table holdout language emotion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_unique_words(sentence):
+        return set(sentence.split())
