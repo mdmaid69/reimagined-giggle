57956ee6497aafc6f63490cd0281d503806040df
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:49:44 +0000

    share club slightly close press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
