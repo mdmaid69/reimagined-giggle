b73256902eb1a725cab72f68feee78d4d7670c0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:46:34 +0000

    professional shout pair exchange arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
