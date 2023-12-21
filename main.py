1a2d616f39b4636cd5e6aa7b6e3c84c30bd2deed
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:50 +0000

    urgent unlucky keepout pollution low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
