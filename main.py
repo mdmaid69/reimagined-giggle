dfaf601f05e7a246e5094445e2b646dec9eb6e1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:31:48 +0000

    store drop cruelty well apply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
