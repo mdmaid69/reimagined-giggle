cc3381ee064fab9e28bab684e02ade9fd2439f88
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:15 +0000

    complaint light relationship bridge business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
