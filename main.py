315ca95e83b9bfe7e1530b3eaa7bc2e742374588
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:20:55 +0000

    ready wait situation event bet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+import random
+print(random.randint(0, 100))
