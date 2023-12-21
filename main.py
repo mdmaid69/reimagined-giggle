1d9fcb982add9f3f01a1a6d5541a5b8c341c574c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:48 +0000

    ocean phone necessary edge degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print([x**2 for x in range(10)])
