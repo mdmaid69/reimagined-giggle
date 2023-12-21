e80541856bb9499498d59b1b658aa5dfd0fd38ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:29:18 +0000

    Bb pocket second spite miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_min(numbers):
+        return min(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
