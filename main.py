533f4f3cd59a3c7baa82fca4d36a0085b678d28a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:08:43 +0000

    clean afternoon operate interested slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
