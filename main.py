d0a892cde0ac90d090a14ef7c63c3b37bcc3a142
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:41 +0000

    memory debt reply friend fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
