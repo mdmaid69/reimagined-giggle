7afc5d9d29a166c6604c98f5f92c691671341972
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:43 +0000

    indeed lonely spread reasonable union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_floor(x):
+        return math.floor(x)
