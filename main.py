59f65404c2a9b02683d0eb0deb4dceefeacae54a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:34 +0000

    obtain coat Oo tall each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
