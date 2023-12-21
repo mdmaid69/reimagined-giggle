3fc2ba1a58280f239b2b2193def02ad3db265efd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:20 +0000

    immediate steal loan compete summer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_stack():
+        return collections.deque()
