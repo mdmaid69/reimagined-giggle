b1fc4d24b8dd1baca2ce3d78e1717c60fc97e7ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:25 +0000

    tool only interfere communicate deserve

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
