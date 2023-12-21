c30dd95288d84278e051e70d100947454546774b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:26 +0000

    person eat thorough film style

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
