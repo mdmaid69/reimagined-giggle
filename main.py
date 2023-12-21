73cfb502180741be982f25596c91c0f5ab6747af
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:17 +0000

    behaviour luck fair goon wave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
