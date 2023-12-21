79207d829f9c7f03781901b7910ca882594003cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:55 +0000

    university trouble illness such bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
