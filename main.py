d9308b61acbc50b989185662d37a49b48db130de
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:14 +0000

    shine body successful connect mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def reverse_list(lst):
+        return lst[::-1]
