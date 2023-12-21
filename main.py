fc1ca2269a0d50b39cc1cb07cd0bd3b6ab655891
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:31 +0000

    factory confidence truth Nn land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
