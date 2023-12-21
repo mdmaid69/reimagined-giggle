76e82ae95ebb66924fef8daac4a7f6aeac5a7714
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:06 +0000

    mine everyone grateful nice play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
