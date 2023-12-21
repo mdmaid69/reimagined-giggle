5d854629497fb5f90c771b1c5fe30f5b2cc6e7b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:03 +0000

    scale turnback that connected show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
