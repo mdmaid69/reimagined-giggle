d9c334878040cadde6ec75cb06c71c88ab89d337
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:07 +0000

    offer distance new wheel organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
