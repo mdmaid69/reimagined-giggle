735089bcd829791513c265e1548ba2bd8962e032
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:19:12 +0000

    fence key favourite thinkover wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
