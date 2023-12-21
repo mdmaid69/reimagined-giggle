2a6803602a61e596025148a98b583164d04fc9b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:04 +0000

    software community tip confusing pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
