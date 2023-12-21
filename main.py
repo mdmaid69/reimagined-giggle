6ba4fd2db30649fc849b13799331e3758e4f6aef
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:29 +0000

    combination lookup dress drawing often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
