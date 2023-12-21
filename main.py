fce7daf9cb734334e4b67273c185cb3ae885ed6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:37:52 +0000

    season bottom father travel group

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
