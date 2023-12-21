88a1a5d63ce1d9267c544ace9e79b28f0909b235
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:24 +0000

    with surface raw design key

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def square_number(x):
+        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
