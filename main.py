d5d6fba9de233d897f6dbc0ec8a1b5917032e9ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:20 +0000

    delight with foot tradition prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
