576b054ac9417165eed7ab5f6a19336dc88acc4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:27:48 +0000

    so into used yours image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
