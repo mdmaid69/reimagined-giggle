740198873a320d61c2a75282a0aa5b2b769053e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:42 +0000

    gap transport decision giveaway juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
