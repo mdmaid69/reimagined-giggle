2369c50fef118163c5678314b53cb622cdf548b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:23 +0000

    discover flavour fun responsibility sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
