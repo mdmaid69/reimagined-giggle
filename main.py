5e2904daaa204cb6f65867e080ea1ca6f7db7b52
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:12 +0000

    uniform trip mystery dinner necessary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
