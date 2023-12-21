1ace5a2b7a69f9a612e10e1c9d13763ae974ac49
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:01:54 +0000

    sentence national Ff Ee affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
