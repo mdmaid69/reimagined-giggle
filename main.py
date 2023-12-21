f43a37bbd5a09f6807f287114e8643c9b141eefa
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:32 +0000

    confuse pleasure copy part attitude

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
