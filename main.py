3f6f8ec23505782aafdf475f3cb0fd726bcf612b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:24:49 +0000

    average lend night last ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
