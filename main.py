1fdd0cc625fb087fa2f3072b09b7b16443ef3cb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:09:50 +0000

    laugh mistake phone borrow whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
