da06ac02062cf605c29360037cc9fc2dbf6b9922
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:09:04 +0000

    runaway dinner fit arrive uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_hex(n):
+        return hex(n)
