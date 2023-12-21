4fd01c641fa17afcc3e8ac583c57cdfb85abd35e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:37 +0000

    neither that keepup embarrassing table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def subtract_numbers(x, y):
-        return x - y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
