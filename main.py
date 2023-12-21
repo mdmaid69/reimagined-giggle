1d2c1007a153a1887be8bc6ad7100f23aaa164f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:48 +0000

    turn considering frightened not load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
