9d657d28fbe671fc723b7a8fb88a1da3eb6386a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:08 +0000

    empty black before become healthy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import platform
+def get_os_info():
+        return platform.uname()
