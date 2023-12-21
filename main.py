3b3485e5eeace08c1da0fc4aab1357a0dd7bd86d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:32:35 +0000

    argue record correct die ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def clear_array(array):
-        array *= 0
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
