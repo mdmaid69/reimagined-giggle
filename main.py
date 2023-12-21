57493f1d6b3bff449ddfe40da96538724853af91
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:25 +0000

    uponon charge behind near content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
