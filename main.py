893cdc268a654e7301c7c2581e5bbdac4b0a2a11
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:44 +0000

    pocket usually thorough lift design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
