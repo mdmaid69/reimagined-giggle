a1d6460eca52ed2a70bb07d736f093adca98da29
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:25:03 +0000

    more bet meanwhile ill either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
