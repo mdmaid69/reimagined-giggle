c8ead07dd9fd2207d150b7d41ee7e8e92e1c67f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:13 +0000

    comfortable warning correct sound lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_today_date():
+        return datetime.date.today()
