4d839c05bdc5d418d3562f16a998924bf08fcc2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:33:16 +0000

    movement degree apart unfortunate cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_list(array):
+        return list(array)
