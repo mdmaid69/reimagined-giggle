6d13ee6282f457a65223b182356969d799f5fd7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:31 +0000

    matter march nowhere principle broken

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
