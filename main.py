39b5a5b4a56487ba2bcf5adf823c8d091b2afb03
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:33 +0000

    nearly confusing only before whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_username():
+        return getpass.getuser()
+def add_numbers(a, b):
+        return a + b
