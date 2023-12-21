421cb0f2fe0ac36295a8c500879333fc8c1cda13
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:55 +0000

    there winter even gift practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
