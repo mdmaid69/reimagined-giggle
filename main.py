a442837a4c36ac3805aadbcdac74b33ae1432d47
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:25 +0000

    receive 568energy direction opportunity will

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
