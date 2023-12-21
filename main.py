7edfc57d309b42fd2370a78fb7308458e0fb9f87
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:11 +0000

    record right actually problem get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
