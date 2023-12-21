2b2f74011e839278e9912b7d7d756aa76629d7e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:36 +0000

    best few flat cost to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
