cc455d7b2034f78cb61025dbfda8edfe4f0ec3e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:49 +0000

    plate waste intelligent minute shine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
