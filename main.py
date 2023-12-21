fd98fe8fac4d2ab764877d7da698b7b5ed0f097e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:38 +0000

    radio slight about gooff constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
