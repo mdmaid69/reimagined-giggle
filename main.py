12d7255e82bd4bb6182e04e8e37a56edf9530b04
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:51:15 +0000

    deserve creature danger closed female

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
