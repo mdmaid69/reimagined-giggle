c36d2931536bd05a98f1626d7f1db17fbe76a89e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:00 +0000

    left match full whistle twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+print(random.randint(0, 100))
