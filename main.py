8bb1c8f306de4e3ac94108e005a4a681570b29dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:04 +0000

    know smile stand equal dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
