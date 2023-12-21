93cf164ad1afd9f51ab18f392a3cbc3cb0307b17
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:55 +0000

    lucky recognize television welcome play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
