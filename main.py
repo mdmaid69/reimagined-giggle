4e2fa980d1db24ca7e2eb28c72291363bab304f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:03 +0000

    owe injury talkback soft acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
