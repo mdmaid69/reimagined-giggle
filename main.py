53b6cd4b61b985ce95cdeae692f8deb1ed15a1df
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:07 +0000

    responsible goon search take service

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
