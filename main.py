da9c8a67ca73b3d077235d4695331b53327d20e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:44 +0000

    crime conversation back terrible famous

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
