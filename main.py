9b75b665e143c3af8ee6ccdc526c6096b0e06877
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:48 +0000

    suit Tt pause outside doctor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
