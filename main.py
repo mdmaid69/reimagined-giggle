da356861d2d1f06c7a7bad1b737128708ef6ddd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:30 +0000

    even destroy hurt confuse his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
