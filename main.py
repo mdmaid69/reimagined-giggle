6356428ce2215a94cbb59cf6eeb9b758e9d95709
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:16:43 +0000

    rich Vv peaceful because scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
