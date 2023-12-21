210541d57b90eb8ab221d7fa81db1876c985e0f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:49 +0000

    mean gap equipment amount less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
