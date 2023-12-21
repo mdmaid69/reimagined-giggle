9cd6f995f76c8f1b7da52b2e54ca579b9ebe1d9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:34 +0000

    patience anxious person couple idea

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
