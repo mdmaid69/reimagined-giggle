6f1c4a12cad925b307dc35fb79adba8793e4f009
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:49 +0000

    Rr crash store purpose save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_dict():
+        return collections.UserDict()
