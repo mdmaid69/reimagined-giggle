4908bc66ca03352b6a208426f9457298c044b825
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:52 +0000

    worst true seem laugh not

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import sys
-def print_python_version():
-        print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
