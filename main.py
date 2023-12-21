1765b7ef70a73c1bbdeb76accacef7684f442b6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:12 +0000

    smile workout mess standard nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
