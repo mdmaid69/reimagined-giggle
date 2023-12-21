98d622ea9678134cc00f80a40785697553fb18e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:09 +0000

    tone tomorrow during wrong choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_string(array):
+        return array.tostring()
