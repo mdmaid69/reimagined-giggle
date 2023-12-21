3209bcf314a0466ee7e554730587737591603062
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:14 +0000

    to region responsibility deep way

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
