0dc5f3235e7943bdff599123b30780a29d36eff6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:37 +0000

    on anybody acceptable sale lonely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print([x**2 for x in range(10)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_list_from_array(array):
+        return array.tolist()
