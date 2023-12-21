183833c18c35f3238b17c88219beb9296e47ba80
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:00 +0000

    trick trap disappear birthday view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
