9c503590d9f408dd0c12d05fa894eca9221feeda
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:37 +0000

    depend proof plant claim quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
