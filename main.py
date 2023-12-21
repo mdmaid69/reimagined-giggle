5d6e53411f83f54715fe8a32cd899897c99f0e4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:49 +0000

    zero run second mine could

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_rectangle(l, w):
+  def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
