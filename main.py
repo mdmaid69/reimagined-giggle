a7a55726bd3e58d57afc56bd806f5d29aa1e1bb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:52 +0000

    cheque ready borrow room should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
