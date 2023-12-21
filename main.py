b1afeba103f6510bdb6ab1748d0cbd1ace70dcc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:57 +0000

    shall wrong wrap spill yours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def reverse_list(lst):
+        return lst[::-1]
