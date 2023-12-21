1b32801b81b2d2bf712e240f2e195e4450f67579
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:02:34 +0000

    mess however possible badly pair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
