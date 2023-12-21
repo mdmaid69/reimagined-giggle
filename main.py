53ce67bd4f9e97bcfaef728063062ae1fbefff25
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:48:13 +0000

    like average clear along innocent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
