30a79943e67d000ade60d57f6f53acc7154605b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:33 +0000

    advantage pay immediate become something

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
