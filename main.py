d634e0fcc899ac2531adc60a03860e531949344d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:52 +0000

    blind people majority cruel hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
