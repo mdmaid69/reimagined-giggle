80f92f82a2ed282f957ae6f49ab1a67258d03fe8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:45 +0000

    rich check dare book slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-print(math.pi)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
