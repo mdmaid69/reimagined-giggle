4ad5f34db08f3fbe9d0a98057cfe9b9a04eedc2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:29 +0000

    desperate completely waste handle whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
