0b7f96172c5e3f346a2c04e56ab80a016d59d44b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:18:14 +0000

    getup difficulty appearance what male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
