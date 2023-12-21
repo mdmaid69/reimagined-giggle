5a386806f761c28758d219153077440debd42778
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:27:19 +0000

    combine phone stranger jump bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def is_palindrome(s):
+        return s == s[::-1]
