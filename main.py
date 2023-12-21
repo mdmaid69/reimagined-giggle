8851620fabf355843ef3a71a0d862fa0461b91e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:29:59 +0000

    file practice clean wait danger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
