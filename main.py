8f78ef86fc2158474f281163e1088cb9fbd410a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:41 +0000

    centimetre much snow hospital ours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
