3eaf851a855143a74f31f69ff8cb37d4b024f155
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:45:36 +0000

    sail experiment talk tense tall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def add_numbers(x, y):
+        return x + y
