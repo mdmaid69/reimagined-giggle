1ad3af84cf2aa0e22a4a9855b5dd80e7a56067c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:19 +0000

    standard turnon sea wind lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
