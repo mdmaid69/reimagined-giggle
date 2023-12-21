8f868f507ff6a67d134ee3dbbc2353474114beae
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:00 +0000

    whistle badly phone exact cruelty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import numpy as np
-print(np.array([1, 2, 3]))
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
