34ba128c4a6f6263fd69e41a96c37c4474f0e2aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:28 +0000

    Mm cry danger perhaps physical

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_item(array, i):
+        return array[i]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
