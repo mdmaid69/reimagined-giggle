b44ad26249abef82929db0be4bebaafcf3181093
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:06:09 +0000

    want perform mean ear clever

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
