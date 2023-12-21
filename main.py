e778ec75df1027adf0e3eef2ffb3d838d6d8d10e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:40 +0000

    judge change concerning he check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
