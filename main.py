d6891f83770ff5a2957853573e6f1e04d0550f20
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:29 +0000

    criticism cruel government alive comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
