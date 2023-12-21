7837d9a70f567deb0c5717ebc4fb91ececf046ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:49:45 +0000

    accordingto war touch drink violent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import random
+print(random.randint(0, 100))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
