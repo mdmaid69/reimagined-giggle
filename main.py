172d34db55dcaecc3890778893fd3e73ea9f1f62
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:54 +0000

    uponon recover the demonstrate possession

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
