dc92cd38503d3f7629b90540059f26e8c1fbef04
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:14 +0000

    curious 564encourage sting suggestion crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
