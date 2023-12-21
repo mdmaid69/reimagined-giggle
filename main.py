a64f1b8c292d14aa19df8ac2c6689bb4759a52ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:15 +0000

    basis understand Kk blind regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
