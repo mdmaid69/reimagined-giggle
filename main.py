5e8bb067269e28df1b2170d940fba8d1d5fec874
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:30 +0000

    call stir week long activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
