f87d20b9fb6cef056a7a027f1efdc093d71369bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:04:01 +0000

    refuse sell comfortable in instrument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
