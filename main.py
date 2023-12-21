28b43520f7e5422f26f840ef5f8a86d95cb79790
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:03:20 +0000

    wish near light nose quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
