e78a9625cb6ce1e6c0f94e5fb0e0e3b61266a053
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:23:35 +0000

    simple discuss common thinkthrough perhaps

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
