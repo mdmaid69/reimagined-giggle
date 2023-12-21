3b0604f0056dbc6de2e4f9ef936c6ca91002a496
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:37 +0000

    amount yes escape ago chat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
+import sys
+print(sys.version)
