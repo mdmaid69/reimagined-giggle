9497facdf76fb791dfb4e0bb6109b62c1f7d36da
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:55 +0000

    lookafter married emergency pot realize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_unique_words(sentence):
-        return set(sentence.split())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import datetime
+print(datetime.datetime.now())
