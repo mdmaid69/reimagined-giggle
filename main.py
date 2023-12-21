9682c78abda417a113f8cdb7a2d5cfbdfd05d9d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:47 +0000

    comefrom detail pollution doubt small

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
