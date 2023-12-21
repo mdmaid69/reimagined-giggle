a95c387329931e93176cc19acdeb86d4f65120c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:18 +0000

    sister dish shelter century leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
