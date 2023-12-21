d20abcc57c04e02633dec9e9d48e7cd139be57b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:13:38 +0000

    trousers sentence crash contrast land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
