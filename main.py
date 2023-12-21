0ab1808a6970715e2e0ffef935207fdaa47b9164
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:44 +0000

    head error innocent cut doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
