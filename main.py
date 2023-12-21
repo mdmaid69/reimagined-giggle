8a24399372e1480c7e542fa6d08262358e24d69e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:57 +0000

    some principle pleased chance smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
