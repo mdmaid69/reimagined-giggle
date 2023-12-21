36fbd5cb3085d53c85c707482ca7d79adf980628
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:46:21 +0000

    how catch issue control female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
