10d07d946bbae9c9ce0bb532893e3a465d51eeec
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:00 +0000

    post blue unite radio needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
