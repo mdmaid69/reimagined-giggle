ea2305be555ead27d99a4d8953adb4ac550e8afd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:53 +0000

    about vote godown food worth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
