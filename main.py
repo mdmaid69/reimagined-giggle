cfe39b2984724f35d6746744185e80e10ab83304
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:04 +0000

    positive to copy left quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
