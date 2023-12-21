bbe96f61073c8b0a1db863df763c63041161ad98
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:17 +0000

    worth sound smell invite she

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
