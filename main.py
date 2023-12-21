c76ede743539c6b903ce4d2c120f2f7745cc6e78
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:36 +0000

    port sound criticize smooth satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
