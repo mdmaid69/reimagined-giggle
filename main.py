1b62409a1b520240da59b40c380feaa3cacb7d2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:10 +0000

    outside manage error hello exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
