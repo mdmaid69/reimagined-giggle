d56b3478fc2d88772516717f0d63aa9ca9a34d92
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:21:20 +0000

    snow tomorrow emphasis replace convincing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
