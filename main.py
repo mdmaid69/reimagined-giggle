caa1204a626ac45f17e72ba64cc278bfd20060e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:39:54 +0000

    bin either dangerous rain find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_pressure(force, area):
+        return force / area
