2c17099c3bb50c83fa492049958b3bdbc5391167
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:25 +0000

    turnon trust die woman event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
