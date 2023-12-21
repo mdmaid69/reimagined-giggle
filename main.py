5c0cb1590d6fa7d0537570e10985407b62f5a54c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:29 +0000

    ordinary face road laugh some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
