5dbde40423668d0db18f6e5e35575756849b48e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:27 +0000

    trouble brave flat guess takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
