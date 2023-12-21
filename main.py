eec37960ccc01ae999f21d47faae9f687109d632
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:36:14 +0000

    strange attractive sure everybody deliberate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
