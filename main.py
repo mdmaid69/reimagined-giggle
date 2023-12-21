77e70041640154e1a413bda6d2f0fbf45446b075
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:44 +0000

    land formal invite regard giveout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
