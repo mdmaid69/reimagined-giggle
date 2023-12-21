04b8d3457b15c1361483ee1c7435420412763240
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:47:29 +0000

    remember fortunate continue silent excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
