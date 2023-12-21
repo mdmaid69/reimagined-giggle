cc8eb6b867e152153743d47995a00f466b400765
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:02 +0000

    book regional ground movement weekend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+print(os.getcwd())
