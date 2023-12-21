b7d4c383e2c42c4bb52baf73e7092ba42a803266
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:40 +0000

    takeback wide negative quickly stretch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
