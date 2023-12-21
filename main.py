c21aa522f9b0b0a552ba89a0adecf658a8955776
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:38 +0000

    put crazy miss trip through

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def reverse_string(s):
-        return s[::-1]
+def get_array_as_complex(array):
+        return complex(array[0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
