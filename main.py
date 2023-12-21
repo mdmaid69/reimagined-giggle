a0497c2215ecdc6da82d8ca8a2ccb93ef79cce61
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:13:54 +0000

    anxious Bb calmly exit object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
