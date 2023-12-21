7b7f481a4ad40154d6d8ca0a9961702c8f46cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:19 +0000

    band advanced conduct regular ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
