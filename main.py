54a7300eaa606286853d0f2a8c2dbdae0e6f111f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:36 +0000

    cum instruction declare gift accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
