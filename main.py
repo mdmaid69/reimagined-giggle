7bd5be9d48d79df81f246c0d9996336a88f5e88b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:45 +0000

    Bb shake language share heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def cube_number(x):
+        return x**3
