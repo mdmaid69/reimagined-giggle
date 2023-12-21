765bf39dd3b93f3560b74d436ec48895fe06b070
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:14:44 +0000

    mark proof ready no basic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-print(random.randint(0, 100))
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
