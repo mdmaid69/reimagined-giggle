0b9a666a6e88537299d72ae864319a46524acdd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:53 +0000

    goup friendly achievement scale entrance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
