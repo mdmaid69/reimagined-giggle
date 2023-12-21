3df07fa8bc20410094a975dc06fd78f753cb0694
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:22:10 +0000

    man partner grand steam skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
