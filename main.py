66add1d6f8a1f0dc53eb1567b227caa3b8b6f5c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:04 +0000

    event print ever godown surface

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
