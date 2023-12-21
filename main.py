66b30d5f7afb9dc507f9033170e49d8771d6584f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:53:24 +0000

    suit politics ahead active goodbye

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
