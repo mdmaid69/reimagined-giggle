59202aeec5073039ea94f1f3f185fe35643f79f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:44 +0000

    software live damage we compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import random
+def roll_die():
+        return random.randint(1, 6)
