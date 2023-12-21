c638966e6e8e072df36ca3fd7e691f71835f91b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:19 +0000

    strong laugh your accidental reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
