0fc4fbd4dba239f7eaa02ae69938a32ac09e98df
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:17 +0000

    front choose smart anywayanyhow sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
