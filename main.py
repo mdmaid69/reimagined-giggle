e382d332449ba6232fb931e1c942256b48da05c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:53:03 +0000

    skill cold fast difficult then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
