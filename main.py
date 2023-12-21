66a036ca69ab6266f92b92e7ce7ad15493a3e94c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:52:14 +0000

    unpleasant fashion 179bite tired violence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_volume(length, width, height):
+        return length * width * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
