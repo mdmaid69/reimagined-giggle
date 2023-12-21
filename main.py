a799d04b41accefb99fa091db2d4627e97e5ce1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:00 +0000

    test lie conversation pain radio

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
