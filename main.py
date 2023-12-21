f62c9f56e61986ca9ca746be4cbb645487bf7cf0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:22:22 +0000

    part whisper instant trick its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
