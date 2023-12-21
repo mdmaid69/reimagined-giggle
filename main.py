11f51608fc70cb9ef10c4e25bd2f31b78eb64216
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:16 +0000

    belief police thumb stranger fairly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sys
+print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
