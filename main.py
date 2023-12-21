4a5ae257d22705ecdd60bcbc66c2fcd2b49d15ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:42:51 +0000

    use healthy arrival consist task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
