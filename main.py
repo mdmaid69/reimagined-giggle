133d37e2a1ac654c129ff93c68408c24be84d1e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:25 +0000

    satisfied achieve pollution convincing cheap

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import time
+def get_current_time():
+        return time.ctime()
