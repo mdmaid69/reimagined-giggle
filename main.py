f31b9943eac50da81c2928ec6a72298da06acd32
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:09:21 +0000

    aim ask platform worth city

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
