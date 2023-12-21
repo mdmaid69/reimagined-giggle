4759bc60b2a99feeb19ea971999c99f35d21cff0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:58 +0000

    away mind unable hour table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
