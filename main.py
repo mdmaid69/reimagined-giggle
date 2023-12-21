c50876f7ba3090f3110178f99ac38b8caf4839ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:38:08 +0000

    cough chief tough boat goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
