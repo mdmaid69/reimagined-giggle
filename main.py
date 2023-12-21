26c3f6576cc47a8203e250068627c2f4af67551e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:43:42 +0000

    speak remark ground talkoutof situation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
