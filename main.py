b615f0897ea8228c395eed505cb962806bb30460
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:54 +0000

    habit regularly object sound oil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
