e58f37705d907102415a99bce54a9c7acea3b45c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:46:14 +0000

    slice during cover word fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+for i in range(5):
+        print(i)
