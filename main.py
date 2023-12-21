f2fd68ade40635fef42ef05d53c5b1861d6be270
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:14 +0000

    possession schedule entrance serious happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
