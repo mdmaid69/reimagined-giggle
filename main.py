c0d64a453963f8e7af6cbe179ecb125ea5fbe061
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:36 +0000

    work sorry land band objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_current_directory():
+        return os.getcwd()
