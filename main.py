f1fd56d056fd7901c45bd3658dfb84dc9628c424
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:06 +0000

    kill software no Oo shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
