69905b11fb270fc402cfba6eba51e5f94d1ab839
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:41:22 +0000

    Zz tip holdup train discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+print(time.time())
