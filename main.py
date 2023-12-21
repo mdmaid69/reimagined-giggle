ce985222bec4a96296ab42986dc6dfcc87965d87
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:45:49 +0000

    artificial like rather quick work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
