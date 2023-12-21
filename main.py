49bbb283fc537c9ba10c886dc9b44b27b5bfe5af
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:59 +0000

    transport fast Oo turndown confidence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
