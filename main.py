cee932ae99f1e884ce877cd98e3d2af601adebde
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:47:14 +0000

    cheek onto my bunch loan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+def find_min(numbers):
+        return min(numbers)
