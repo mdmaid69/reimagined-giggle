135e9a6ae8903b4ec9956eeadf93c39a274ca0cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:13:10 +0000

    rush borrow emergency luck let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
