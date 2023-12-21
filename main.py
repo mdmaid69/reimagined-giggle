e4271d7d51e6944515acab8c698a42b28a1828cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:08 +0000

    trousers shop keen study but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def reverse_list(lst):
+        return lst[::-1]
