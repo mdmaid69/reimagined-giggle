fc8c29f98c223cdb53e01bc3e11c5734eaf2c6d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:18:19 +0000

    cope exciting train quarter flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
