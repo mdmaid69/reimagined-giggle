4055b4cdccb996726cd6e6dcf15d07c1f0ba36e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:54:58 +0000

    nerve its change violence past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
