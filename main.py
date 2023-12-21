043a764e1f7ac2f70602575b32d1594383e53555
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:29:53 +0000

    business range out runaway train

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
