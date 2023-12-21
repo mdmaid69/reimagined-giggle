2c5df6c0b3d03d8dffe5c9c50b19530cb537c94b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:13 +0000

    chat think further deaf broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
