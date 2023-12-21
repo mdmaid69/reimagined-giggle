3348f14b7ea0cbda82e441629c21d9268de56638
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:15 +0000

    blame traffic reply thanks no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
