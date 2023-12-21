726596be58194e429aefb602c9897a0768f1a07b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:16:31 +0000

    project wild black purpose bound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
