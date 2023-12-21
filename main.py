9337fea33f592521d6f61ba7df6decdbac9094f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:54 +0000

    since picture number recognize connection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
