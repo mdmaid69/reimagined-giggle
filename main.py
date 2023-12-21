9ef845f59369399bea2ac5283b2978f6f84c4f43
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:19:58 +0000

    head on niggaacademy convince surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
