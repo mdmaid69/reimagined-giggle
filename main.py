ea784c630e6fee6da80e573100f40ccc3f1355fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:07 +0000

    poison drawing store port correction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
