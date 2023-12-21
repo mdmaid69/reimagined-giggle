eb638dfac2c4236b0d5cb42f6b1e665dd8ea87ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:23:19 +0000

    keen top never sail summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
