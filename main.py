28247d3abfa28f817e172c1be4c7ee354dc08c53
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:45 +0000

    shut flood remove right thirsty

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
