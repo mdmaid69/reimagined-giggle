e3655e3e245a7ee3171e7197f34017e6f605b2d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:47 +0000

    glad below past actual train

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
