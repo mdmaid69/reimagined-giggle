52b23eabfd0300714f74c934dda40f375d2a52f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:05 +0000

    realize advantage too standard since

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def is_odd(n):
+        return n % 2 != 0
