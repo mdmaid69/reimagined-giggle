d9b5c3586aa24c489a9401a9eb14f1d94bba1c9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:20 +0000

    collection music deserve only back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
