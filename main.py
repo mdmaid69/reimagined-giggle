cc06601313faa75bd67d55e1121ebcfc4849031e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:01 +0000

    tax criticism love ear clearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def convert_to_hex(n):
+        return hex(n)
+def reverse_list(lst):
+        return lst[::-1]
