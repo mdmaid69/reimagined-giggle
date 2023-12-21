8c69aa37e310f60f3f9bae6cfb36bf155cb4ebae
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:54 +0000

    intelligent money protection believe glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
