c3b7ea483c02cdc121fe4b6f226295bad522924e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:40 +0000

    happen provide rate wash air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
