f00e4e24e4180e35bf93afa1c44cda26eaec8578
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:50 +0000

    fire spare start drink recently

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import sys
+def exit_program():
+        sys.exit()
