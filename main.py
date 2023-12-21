c8cdcd2abf2a10dc8d8e1b9a24fd127f95a97142
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:24:08 +0000

    further bank sink divide different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-for i in range(5):
-        print(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_acceleration(speed, time):
+        return speed / time
