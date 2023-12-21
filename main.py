22062fea8f62a77bf7e9c49638e852c76432abfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:49 +0000

    entry trap line allow we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
