84a5018823ffcd41908c2c4a9ca5da716a1dd76e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:37:52 +0000

    turnon game operation smile off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
