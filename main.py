99f42f30c15a42bf8524f3d1c2d7179825198d9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:24 +0000

    consult instant bend truth storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
