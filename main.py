55a8fd6e6cb9afca431505202cfe73eb0982d184
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:33 +0000

    genuine closed Mm colour fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def add_numbers(x, y):
-        return x + y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
