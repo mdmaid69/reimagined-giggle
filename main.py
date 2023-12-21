07e5e9f2cd6f36819835e26cd951875c10841602
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:47 +0000

    succeed cope middle wild neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
