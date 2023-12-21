34e54f54943b7c8a34eba463645a6c5b4acf0258
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:06 +0000

    open right argue reason finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
