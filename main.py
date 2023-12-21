7075c071d1aff0b30c80f347940de1951f66fe71
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:05 +0000

    correct present careful old occasional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_density(mass, volume):
+        return mass / volume
+def count_characters(sentence):
+        return len(sentence)
