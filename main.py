9b81bceeb80d8c3d18b8daa1922364d045635dac
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:43 +0000

    left right reasonable lock suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
