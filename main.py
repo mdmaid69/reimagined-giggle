859bb5b83f2a56b8ab179240abf75eedf25b35d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:50 +0000

    clear store smooth take object

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
