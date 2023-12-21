14546b0aaa17e10d4b82e2c240d1075ae9d09909
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:15 +0000

    by somebody fork him sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_length(array):
+        return len(array)
