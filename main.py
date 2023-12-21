8bcaec7a8799f90ea81388a578e73e78b57a2264
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:37:28 +0000

    group worth throughout quickly be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_density(mass, volume):
-        return mass / volume
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
