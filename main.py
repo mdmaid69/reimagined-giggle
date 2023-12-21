47addde229030aa5d1015bd2d52010a66b7f4011
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:57:13 +0000

    with trap miss degree more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
