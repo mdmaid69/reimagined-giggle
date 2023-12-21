d4a20f76dc4ed523b3f3e85d7ff3c62a768d17af
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:11 +0000

    straight neck they wire building

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
