991ae8ddf64993f23212b6932e1c4cd5d7b6117c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:18:17 +0000

    cry string before measure famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
