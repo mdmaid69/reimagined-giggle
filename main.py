7d459d6b655d32c0fa2098a61d342474dc099419
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:41 +0000

    interruption dry sudden concerned possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
