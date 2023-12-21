850e7fcbc54ec97fe59fc8c1d58436ccac8758cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:11 +0000

    surprise relief threaten offer early

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def subtract_numbers(x, y):
-        return x - y
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
