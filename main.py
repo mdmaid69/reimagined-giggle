02dc68992ed1864e037352daa34ddcf0e486c3cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:29 +0000

    future should keepfrom breath waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
