3e6bcfd216c0cef5b3254c43c4a918b6ea3f665c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:20 +0000

    hold pupil clothes during flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
