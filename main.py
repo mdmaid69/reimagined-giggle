7c9dd707a253612456174e7c8ce024db3203ac11
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:33 +0000

    swallow communication travel Kk exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-  def is_even(n):
-        return n % 2 == 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
