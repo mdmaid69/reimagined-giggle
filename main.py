329b1bc8502475b7f25f283d3c3ec8f4b7270b65
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:20 +0000

    accident slowly tall empty mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
