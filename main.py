265b56929b863587df7c4fb266d8f08380b50e91
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:54 +0000

    through thin turnaway or turndown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
