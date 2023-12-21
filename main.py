56976ebde920c203b1dad7db4059587e2986d565
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:53 +0000

    within bottom book paper rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
