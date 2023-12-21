9eed5bb82ef0bae7356da613e2188eeb5eb9622c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:53 +0000

    floor promise band whoever always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
