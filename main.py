258c8afa1fd3cd7c9d8a034de1118af77370ba7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:07 +0000

    account ball shape thirsty major

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
