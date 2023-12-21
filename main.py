845381392b0705bed38080293d10fe1ae03a6db2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:59 +0000

    flight cruelty spot cut help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
