3f289a9750e83a703318c4c4f4176f54a6d6ad66
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:03:38 +0000

    interested exciting danger near love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
