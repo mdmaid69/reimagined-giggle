edb77d60146ad66c87f8c1ea738f762bf82823d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:00 +0000

    receive afternoon order single through

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def reverse_array(array):
+        array.reverse()
