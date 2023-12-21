1c1bdd7f88375bc57420b72f01b06516f1ad6d9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:23:48 +0000

    arrange national clean pay unit

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
