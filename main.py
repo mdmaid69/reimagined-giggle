92edf775b9c50437baa24702e731976924a376bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:27:47 +0000

    sort giveout similar door bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
