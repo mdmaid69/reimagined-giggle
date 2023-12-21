82a5025d0c60de4990e22fc65fdd1bdf21df0d1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:37 +0000

    now sore brief turn stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
