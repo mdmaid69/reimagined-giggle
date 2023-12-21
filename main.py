ce93280f7905ff1bb250875e33054217ce7f387c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:50 +0000

    taste both join exciting factory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
