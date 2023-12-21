e7174c89de3fa5a0bafa96cd3ad626e2e710cccf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:49 +0000

    cap oil inquire climb travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
