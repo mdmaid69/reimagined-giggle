5ef862ff04f95d4222553053e6de392ad0c28723
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:28 +0000

    welcome risk bus raw it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
