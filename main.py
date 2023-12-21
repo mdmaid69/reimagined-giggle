3b60fb1530e02e5a6c2f611174bd0dd5e859d3b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:58 +0000

    peaceful wealth possible often anxious

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
