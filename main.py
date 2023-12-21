6acf3113104f13e7cbae353c2ce23abf31a0b033
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:03 +0000

    word town variety child paper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
