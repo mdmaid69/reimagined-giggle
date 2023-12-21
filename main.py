c668dc62aa1b1a761c691d2b89300b64214dbe85
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:27 +0000

    situation whatever murder break complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
