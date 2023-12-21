86f976f40c952d031d7ba46ed6703ca25ee32dee
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:08 +0000

    talkback whichever wash could quantity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
