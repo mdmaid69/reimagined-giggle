79e072ab3caa405a607cd7dd9ad4f3aa0de3b1b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:56 +0000

    help minority waste keepout price

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
