07ed1191d349845356edcd328d640ef8ac823bea
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:47 +0000

    side injury clothes interview use

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  def is_odd(n):
+        return n % 2 != 0
