848a7e90dcc21859e1e246f7acb7b7e922d8bd32
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:27 +0000

    standard own voice satisfactory flight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
