cf6569ea7902dd677c4684ccae5b54c1614839d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:37 +0000

    fasten government satisfied right such

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def count_elements(lst):
+        return len(lst)
+  def is_even(n):
+        return n % 2 == 0
