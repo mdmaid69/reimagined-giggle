c124dee44f550f76988d36523b57c0c6516e44f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:20 +0000

    distant upwards joke all food

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
