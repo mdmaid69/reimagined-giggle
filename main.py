e52379d9cefa8ff0e5d5b687c4acb35f272372e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:52 +0000

    manage later shall food market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
