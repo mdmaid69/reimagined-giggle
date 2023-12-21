7cc67821f578b2847db11895a5159195112e284a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:14 +0000

    appear bury involve drop best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
