44e91a7ba775424f03502fd374f79b33ba84bfa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:08:13 +0000

    war voice check air clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
