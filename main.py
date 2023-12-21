3bf698020739a6b38f01e2791469f244cba95cd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:18 +0000

    service trust quality brave dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
