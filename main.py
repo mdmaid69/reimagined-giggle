7dfb0c4eb64f122fb9173e38dacbe9393c16917d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:14 +0000

    unable sight herself refer swim

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
