33e55a6a02cc5b70141092f2828aee22d3b7a68c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:54 +0000

    instrument luck cry bang pour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sys
-def exit_program():
-        sys.exit()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
