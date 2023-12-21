dc502b8022998daccc0a570234b23f8d37805b47
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:07 +0000

    action telephone ability let quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
