08cee310e3ec638a909892f40fe90cedf7501976
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:28 +0000

    emphasize number interested cook takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
