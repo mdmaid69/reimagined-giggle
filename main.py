8953e98fb43c7fded733c9262ebf328ce589cf67
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:26:32 +0000

    standard innocent measure field protest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
