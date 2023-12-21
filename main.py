99a6e2055ef28524ff73744a029b347e5cf4a3d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:07:59 +0000

    suddenly turnback mouth write piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
