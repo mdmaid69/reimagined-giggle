18c406d5c3ea4ecbb5abc270f80c8a6622b67b86
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:39 +0000

    goout tense laugh polite anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
