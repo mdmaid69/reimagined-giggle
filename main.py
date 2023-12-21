6126ff2e54087a786d2c1354c154b961660b890a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:06 +0000

    water whisper spill upset turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
