93e578298806849c36b88b83099c2a87961bc337
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:38 +0000

    special write flight godown soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
