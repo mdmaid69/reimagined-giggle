098b333c43b9429435bce1fdbe4dabd508af01c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:27 +0000

    level profit different gift production

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_array_as_str(array):
+        return str(array)
