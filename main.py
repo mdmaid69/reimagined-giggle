9e0d1a81c3b3530d0d558d22a766e6534b1cf533
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:09 +0000

    throw line watch callback skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_current_directory():
+        return os.getcwd()
