d2d29a95f7f8e617333df70e46cb3dd4caf1becb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:29:14 +0000

    who near birth holdout sign

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
