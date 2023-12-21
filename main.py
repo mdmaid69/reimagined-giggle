9b8e04d35c79dd573381b0cf8b47b1647cc3db8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:08 +0000

    awake every every pot everybody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
