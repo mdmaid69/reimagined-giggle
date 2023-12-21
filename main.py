778b7f2afb9c1fd0ce05adef9b11b4255be4f57d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:07 +0000

    song pipe squash beside record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
