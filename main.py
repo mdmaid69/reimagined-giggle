568022e97f7024f134f094bef4e90b2be61e07b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:49 +0000

    566end bet truth smile phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
