177cbc2e42315c38742c9baa389da812c1820edb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:44 +0000

    score keepout grand practical lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
