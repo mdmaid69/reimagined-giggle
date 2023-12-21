6afdb7977e035fae0bd6f851a7df850a7c6e9e2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:29 +0000

    emphasis nervous before swallow takeout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
