1efb718c0765cb829477124ad06367968f60e6e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:56 +0000

    willing comment clear turnon after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
