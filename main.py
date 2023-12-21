b8f5f564d8dc4df04ab158f5e15ad75afd24eba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:20 +0000

    keen group flat pinch practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def reverse_string(s):
+        return s[::-1]
