120b09399730189229a047bc826f997bbcce45cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:27 +0000

    offence favourite cut scrap song

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
