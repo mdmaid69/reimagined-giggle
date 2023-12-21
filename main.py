cf88e26b18d15e3ac1e3eef19476a5ec91fea121
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:23 +0000

    calmly violence neither level arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
