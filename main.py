012b8d0525da172a6dc832d187e36a8c42d3a01b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:44 +0000

    watch content blind slight shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
