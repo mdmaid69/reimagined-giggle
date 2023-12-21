3615aa1fe1f47ff501bf09441512b5f24f90a07f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:14 +0000

    drink impressive challenge 566end breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
