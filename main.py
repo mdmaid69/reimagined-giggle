a46f650594b05f0719640e3aac3869b2565cd06f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:51:31 +0000

    makeup recognize whether during holdout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
