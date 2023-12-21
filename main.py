b1f476f97a2efd47a86b053329b8b8704d7bbe60
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:35 +0000

    advise boat receive progress outside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
