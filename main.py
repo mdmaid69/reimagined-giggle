9baedca2f18cd990e9465b330b18b7a7d3d93332
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:14 +0000

    concentrate life special inside advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
