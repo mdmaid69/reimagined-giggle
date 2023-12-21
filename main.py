98cbed3c7b6340f96c2310b10bd01315ba6701d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:04 +0000

    black uponon little healthy please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
