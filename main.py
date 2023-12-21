cd0a844c17cb5f7e644061d340bf79e85722fbd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:53 +0000

    breakout escape pass serious on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
