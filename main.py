09ec573b06aa317e78072b2b9daef8038a5d248e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:10 +0000

    positive considering absolutely ready feature

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
