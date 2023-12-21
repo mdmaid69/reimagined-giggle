075dd2c11731d7cafd10647a1c9ffd15eb5eb557
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:22:25 +0000

    minor Gg taste coffee wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
