7d1cce94c05fb1513ca275d7307e37f878d4895b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:41 +0000

    holdon formal obvious sweet reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
