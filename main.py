8b672fe0e6978c01e4ebd860c1f431e3678e8bfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:05 +0000

    rain begin himself service design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
