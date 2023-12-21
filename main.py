1695938c8499d49bdf1d418ca31b2450fcbecc31
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:10:20 +0000

    friend vote class breakin account

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
