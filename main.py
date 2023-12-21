8060e59d4eb88e069779ace84fda6a5793136f9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:57 +0000

    though comment recover Tt wish

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
