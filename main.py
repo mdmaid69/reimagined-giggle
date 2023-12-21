05c0830c72ca92aafca22a6b4fc7da35ca186808
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:38:22 +0000

    but free surprised row 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
