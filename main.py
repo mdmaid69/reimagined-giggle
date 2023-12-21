28a6abb906a31e68c50f1347b2028d0bdf19394c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:20 +0000

    heavy careless satisfied think wrap

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
