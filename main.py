5ff9fe6cf19c1814dff54b57cc03c30e3c9fb0e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:17 +0000

    Dd in translate practise factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def remove_duplicates(lst):
+        return list(set(lst))
