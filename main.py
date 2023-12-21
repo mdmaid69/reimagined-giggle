ceb99c62858d5bbba145ef922419ebc23332fdc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:02 +0000

    language hurry listenfor print dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_acceleration(speed, time):
+        return speed / time
