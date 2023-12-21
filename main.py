d09c4994821dd26f277a43701c15187ce3909f93
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:44:58 +0000

    change Pp question defend environment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
