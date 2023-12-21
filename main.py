1b2fe983dafce696f122a301cee53e53b3cac2f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:34:50 +0000

    try treatment combine offer rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_area_rectangle(l, w):
+        return l * w
