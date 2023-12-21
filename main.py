cecfec26a85149622f30a5eaaad75506eb7160e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:33 +0000

    keepoff impossible claim shape mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
