621f301592a1513c1cffd458f73cbd1a69db9caf
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:42 +0000

    arrival entry upset decision develop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
