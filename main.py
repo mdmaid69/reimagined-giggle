d1dde5df5c0ad3a46961334c880f17bea725d668
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:04 +0000

    length entry body justice nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
