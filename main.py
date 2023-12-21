91979c7e5a57238bd2ac694cf66e3ddd07ab01ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:03 +0000

    eat lift least eager forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
