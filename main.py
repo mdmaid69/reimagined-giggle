224810dab19a65852ede904d4a966edb51025f0b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:49 +0000

    it appeal wash gentle traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
