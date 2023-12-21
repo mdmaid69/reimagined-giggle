668e0c737ffa2f7da16eb885db3c64f78766e39a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:13 +0000

    favour keepoff shape anywhere succeed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def change_working_directory(path):
+        os.chdir(path)
