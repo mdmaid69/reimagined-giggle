c31b806d961117bd5daa711566d171a5941502b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:39:43 +0000

    describe wheel help comein respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
