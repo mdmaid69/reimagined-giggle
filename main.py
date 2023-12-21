7463010cefccd7530375b3fae2c3b54eb1070f85
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:45 +0000

    getout question shape narrow smile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
