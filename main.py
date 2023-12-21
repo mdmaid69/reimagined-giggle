c3ddfe20d2b34aadbf9ff26b8097043d6f99594f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:55 +0000

    closed idea least his illness

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
