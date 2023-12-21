8c9a0fbd3eb25dac0eaf79831e56ff5ef6f1dd9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:30 +0000

    married motion hand intend dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import sys
-def exit_program():
-        sys.exit()
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
