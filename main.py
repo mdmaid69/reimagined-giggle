a6ce2d1610b15b7a85d732bb62a118c0b254eb98
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:12 +0000

    rush crash order turnup destroy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
