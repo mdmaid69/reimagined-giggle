5e5f1d1fc6af9231811b74a482626d137e03949c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:20:53 +0000

    debt oil shut kick interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def convert_to_binary(n):
-        return bin(n)
+  def square_number(x):
+        return x**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
