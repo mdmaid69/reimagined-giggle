af831746dfc127db029c21f904fb440e061af2b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:45 +0000

    land tense wander full objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
