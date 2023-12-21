b60f022a21d862a6263317737a246c4544f5efc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:48 +0000

    572enter drop even still fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def square_number(x):
+        return x**2
