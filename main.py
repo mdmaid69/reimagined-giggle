5780e199fffc73741b3349a90026bd5894fc90fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:12 +0000

    confident bringforward broken politics neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def set_array_item(array, i, item):
+        array[i] = item
