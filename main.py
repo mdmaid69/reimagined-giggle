caf8a3a77d4ed26ecbc6f5265a8837ddf1a31ca2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:30 +0000

    valuable advanced rank work trick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_item(array, i):
-        return array[i]
