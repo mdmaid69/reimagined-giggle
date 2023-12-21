fabcc9907a42813bb93918957b9f404189098625
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:12 +0000

    connect base I terrible pot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
