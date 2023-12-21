b208ae9ae05e5a616e4e58ebe37c98790bf7868e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:05 +0000

    excuse shout somebody head by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+print(os.getcwd())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
