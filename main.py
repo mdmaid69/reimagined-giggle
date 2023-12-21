00e4255ed0c3f48739cc505fac4a4ca8c842a5f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:53 +0000

    distance past abroad except aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
