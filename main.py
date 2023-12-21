e74fc2b4aa7199fb1979af929dc89763bfa2b56b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:23 +0000

    keepup late various mainly rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
