6a9c522ebe8788f6a81ab91ffce7638f2a455cfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:31:37 +0000

    within leave human interest surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def count_elements(lst):
+        return len(lst)
