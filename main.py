30ef4ef6fad2ec4b0a39dca409f6e0b028891309
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:13 +0000

    steer emphasis shut about task

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-for i in range(10): print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
