044ce59980e6663ab44075165aac83e55d46e6a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:19:59 +0000

    discuss correct reduction hurry warm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def create_queue():
+        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
