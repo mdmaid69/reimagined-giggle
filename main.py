e0aa861aa29351a472aed6f74f2b5fa916853c60
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:17 +0000

    second weather knowledge bet left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
