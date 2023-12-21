cebc342e4bb36ff730aa7ce26418c6d13ec624e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:40 +0000

    border introduce tidy film home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
