3f35521e8964269a6154a4e3c5975fe5d2029597
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:37 +0000

    strong dish goodbye phone behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import datetime
+def get_today_date():
+        return datetime.date.today()
