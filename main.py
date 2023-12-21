7a1a89a061ce32a2f280ef1e35b614d40fb9cdab
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:38 +0000

    warm ourselves positive apply if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
