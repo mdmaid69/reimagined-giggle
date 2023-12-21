0c5f8ef96d50a0a4f813ac515b999bc7ce41ce7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:29 +0000

    joke experiment swing valuable regret

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
