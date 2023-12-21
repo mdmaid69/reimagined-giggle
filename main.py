4ba1656ea2531a69077ffce966b2548160d429e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:18 +0000

    for opposite go actually success

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_density(mass, volume):
+        return mass / volume
