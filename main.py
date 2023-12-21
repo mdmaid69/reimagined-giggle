01c1815fa0550a73b80ecf10ae8228b39e16b000
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:10 +0000

    bite considering judge yes duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
