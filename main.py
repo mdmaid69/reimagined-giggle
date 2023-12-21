71487cfdf868522064c1313ad641beb15613b3ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:10:20 +0000

    whisper late birthday show scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
