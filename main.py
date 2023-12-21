08b769ffe7d09485470ffd50a7d0395685c4f34f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:34 +0000

    sorry scene cost much health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
