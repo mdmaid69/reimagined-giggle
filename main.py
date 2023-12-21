3f6e75b1802198b389b13de4a1ce296c357999a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:45 +0000

    also statement her open shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
