a05ee57b01f7736e0298e397ca412bda804a47fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:49 +0000

    question mainly stare fire crash

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
