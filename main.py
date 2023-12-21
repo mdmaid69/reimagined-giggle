997ce260a2ef28fa6312ce6ac562f0635f0fe6c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:22 +0000

    future close command zero makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
