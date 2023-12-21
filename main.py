8a0b87814d628b238c59bf290e306cbf476f2409
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:43 +0000

    themselves whoever seat course dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
