c67f518ae59fbe5b4f849884e2e669f389b32929
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:52 +0000

    though expect quality cap callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
