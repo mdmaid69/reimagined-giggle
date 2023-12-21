02bd08cd2084d4e3e82f7efa76581144149fd536
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:23 +0000

    petrol calloff sit sticky fall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def reverse_string(s):
-        return s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def convert_to_octal(n):
+        return oct(n)
