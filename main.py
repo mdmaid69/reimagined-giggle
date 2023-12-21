32eb69e4e51519a5601bf5da09afbb70573a6940
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:03 +0000

    impressive deliberate child but bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
