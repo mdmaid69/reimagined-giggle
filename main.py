c6dd7956f41aafabf3c8936863f2515ad2fc0938
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:14:05 +0000

    drop comfort above peace dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
