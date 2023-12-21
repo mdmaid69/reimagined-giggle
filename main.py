167fb69e1a8d0ae29cc168c34c4faec6b40dee91
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:00 +0000

    left summer giveout crime broad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
