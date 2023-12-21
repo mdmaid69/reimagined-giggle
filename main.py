1f02be31a9e49b0f54c0774e1fb209cf23f2c885
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:35 +0000

    command sweet model glass in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_exponential(x):
+        return math.exp(x)
+import logging
+def log_message(message):
+        logging.info(message)
