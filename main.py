12060c79ca7d128cb461786a3ef347d9949f04a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:53 +0000

    rely gradually scared pronunciation smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
