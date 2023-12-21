812c5cf51a40dc930abec0d15ef3705e90c8d7f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:31 +0000

    improve urgent holdout when impressive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
