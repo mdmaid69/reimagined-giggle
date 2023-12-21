fd012ad2f3c57be270ae17b22d73ae17f27ad29c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:57 +0000

    responsible squash story opponent raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
