ff3d811b4909f9d4a8816db53d7bfddadaa774d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:52:59 +0000

    course bringdown success complete valuable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
