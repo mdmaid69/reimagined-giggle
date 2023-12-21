171054a3f721c7864a4f36d3956f511eeafce58b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:26 +0000

    care able class imagination pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
