e4106bf8855f17e50a84795131b34bc9ed1d978b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:22:44 +0000

    suffer debt genuine impressed remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
