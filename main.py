cff5c194dc3dad55c69a6dc950b9ac039742d129
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:38 +0000

    wrap worth valuable calloff job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
