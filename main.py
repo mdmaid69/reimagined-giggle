2be6d5ab9deece83a650201aa352f9d61b3d05ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:55 +0000

    hang break innocent act speech

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
