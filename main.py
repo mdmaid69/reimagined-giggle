28fcab93fb15ab1b28874ac92aff124bf93e61bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:35 +0000

    spill mend takeaway plan card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def reverse_list(lst):
+        return lst[::-1]
