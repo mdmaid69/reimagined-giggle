3ffd9af5fb085680d01f246c41ab089194abceba
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:47 +0000

    impression afraid odd him communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+def is_even(n):
+        return n % 2 == 0
