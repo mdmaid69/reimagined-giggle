febe51cdb67a047e3dbec0711e8209411b83439b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:33 +0000

    chance stir loss imagine wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
