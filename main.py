cf78aff15a0f02914b519ff1f4c95863565140ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:26 +0000

    regret popular computer figure gate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
