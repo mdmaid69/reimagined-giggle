867f87332089ff315b6d74865c416156630119a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:20 +0000

    minor connected prize second benefit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_array_as_list(array):
-        return list(array)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def append_to_array(array, item):
+        array.append(item)
