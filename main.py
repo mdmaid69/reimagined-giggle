19cdb85e7e90bcd30d86332806ad5fa424a47387
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:11 +0000

    appearance fortunately sleep leave occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
