5dec05812ed44a522532f97556cb74976c4ce933
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:13 +0000

    accident danger present succeed cold

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
