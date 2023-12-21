7038083de4bed413fe0d16f69785b6bfcd973cfb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:32:54 +0000

    book wander direct protest common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
