21c38c6f8586f21035a7eb5d68bc1d78af409da5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:39:42 +0000

    die centimetre unpleasant fail choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
