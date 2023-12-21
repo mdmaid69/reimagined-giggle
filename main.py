c3d464583df68092a6252d8ea23beec43df220b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:19:39 +0000

    pay activity poor familiar basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-x = 10
-y = 20
-print("Sum:", x + y)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
