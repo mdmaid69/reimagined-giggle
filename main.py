6bfe496b9b4abd129703e84b52dd6785c7a7bc40
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:33 +0000

    suckme dig embarrassed steady owe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
