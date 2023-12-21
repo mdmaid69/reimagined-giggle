138b3649fae6d706c1cbf5648e2fead166a59b01
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:12 +0000

    winter proof fair reliable hate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def count_characters(sentence):
+        return len(sentence)
