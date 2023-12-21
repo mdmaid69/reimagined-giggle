7ac7913fb39c82a7b24988b7d517c0ba04e05365
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:03 +0000

    partner start distance determined practice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
