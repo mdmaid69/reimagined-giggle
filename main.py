310e75efbbd0cfed19d84c00ad7fbdace3b92300
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:25:39 +0000

    tend name common pocket giveout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
