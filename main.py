ed0b341e808dc39c370e4061a90dc2fc143c964d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:05 +0000

    goup stupid hold work turn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
