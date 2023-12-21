e2d16b2cb62427562c2e21c187c49898515f448f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:44 +0000

    cheat big listenfor information stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
