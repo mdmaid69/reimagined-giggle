c4d138a8015d1b432e21290e2b61988eedc4d9ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:53 +0000

    eat goout correction mixture kick

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def count_elements(lst):
-        return len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+i = 0
+while i < 5:
+        print(i)
+        i += 1
