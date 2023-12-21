94a79638ee665b71103c73e5dbdd17adc0bf0c03
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:02 +0000

    invite comeround name care pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
