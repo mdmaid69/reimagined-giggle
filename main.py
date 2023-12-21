fb2a62eec87c309e5796fb7a3e1d802aeedb26a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:51 +0000

    supply mysterious branch public Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
