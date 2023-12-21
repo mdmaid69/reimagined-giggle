80e8565f8d18efc516a720baa81e452ae8995f89
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:50 +0000

    balance silent discussion phone plenty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def cube_number(x):
-        return x**3
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
