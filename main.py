83a33c30cbf8a1d1ecb162349801665e18f66061
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:20 +0000

    second drive makeinto lunch receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
