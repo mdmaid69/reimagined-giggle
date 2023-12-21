e981c16c965d6a6c0c88a78a8bec1c93234fb228
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:38 +0000

    related thin record own prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
