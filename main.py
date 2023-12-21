76c65cabf973e608a168270bf4492eb338174f0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:26 +0000

    lonely children cover world beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
