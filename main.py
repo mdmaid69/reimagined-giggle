2027268de67c1282bd02b54c688fe90bfa9fe606
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:52:16 +0000

    serious late somehow region history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
