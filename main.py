596f9757c9db2aa3df3f057aaa553175bc657e95
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:22 +0000

    belief our list tight Rr

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
