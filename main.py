cf1b0687ab9de175191732d942b3bd9bb5294540
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:06 +0000

    coffee reply breathe couple afford

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+import time
+def get_current_time():
+        return time.ctime()
