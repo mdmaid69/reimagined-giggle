d465ffe3c268fc85fabe10378a0e2b5a4383c455
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:14 +0000

    neither interrupt fast injury key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
