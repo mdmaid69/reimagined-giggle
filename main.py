eaa7bfddeb87f755e0e44becb29cb6fc35b53029
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:43 +0000

    object one gradually home both

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
