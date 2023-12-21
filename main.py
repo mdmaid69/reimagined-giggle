901187594b02413a11aac51a164dc61e39ff9fbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:56 +0000

    attract out shallow return line

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
