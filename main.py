46ef11c31a75435e56760ebe2d6292aa68cf9c96
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:31 +0000

    healthy book branch prepare everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
