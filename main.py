7201b636abbbc39e0b4800816817e10aa10bf4ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:23 +0000

    tight such comeover stamp settle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
