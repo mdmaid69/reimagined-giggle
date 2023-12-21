52e86b414c4eac519f84465051cd777f1cd73c52
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:52 +0000

    waste push ourselves near crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
