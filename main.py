22059cd3fa21c91ee181e2a5197f0213667ad0ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:16:40 +0000

    protest goon matter improve week

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
