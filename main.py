2e61a8820e47c38a1a37721cd94222cd08efb8a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:16:27 +0000

    measure pressure part belief bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import sys
+def print_python_version():
+        print(sys.version)
