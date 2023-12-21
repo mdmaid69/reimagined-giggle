22cd46f5353f493703324ccdf8203b9456a62c2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:42:48 +0000

    bet violence spot sea seat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
