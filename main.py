e8ba3fe9c4fad0f902a959a662031bb81e625f8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:51 +0000

    occasional mail Jj other illness

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
