8d0ca130d1a25b5cb471146837be29ec45affd98
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:24 +0000

    all love treat room freeze

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
