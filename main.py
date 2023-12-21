cb120cad9e3876ac6f04cc80e107120836ae7a0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:39 +0000

    blame reliable apart many reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_length(array):
+        return len(array)
