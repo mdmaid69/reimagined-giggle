2a3b8a512ddb4b365b7a3175c2a11ad04bf50968
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:35 +0000

    live protection born settle exhibition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
