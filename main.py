47ce46a6cc63bf33eba1a10e96f0be2f03c7e1d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:08 +0000

    form agreement forever priority bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
