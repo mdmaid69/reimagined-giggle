ba4d21a3d58aec6f7b1595cdeb36422f615e4a59
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:32:02 +0000

    joke match put once card

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
