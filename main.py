9cb0573f87efb1635268053dbf51a338ee861626
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:09 +0000

    behind occasion congratulations necessary perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
