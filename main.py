81dfe75ed5dedce4d62d78ed78c27ac8f3df258c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:02 +0000

    notice wipe market healthy watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
