63f1d153b24449fd4d8ee3979a0532c871c75bd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:48:16 +0000

    godown news crash stand prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
