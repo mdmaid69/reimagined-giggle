84f2acbc7140f34501cde78a06ce6c18ffc51991
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:26 +0000

    clean trade uncertain split borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
