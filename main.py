18433c0e9bc67be6767e6c3be6223624c2ad8b12
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:06 +0000

    culture future hope divide search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+print(sys.version)
