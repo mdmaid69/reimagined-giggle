4d1858abf685cd1917c54b0ecc21ff9ee3024d29
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:48 +0000

    trap somehow rare lock part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
