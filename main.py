47fec46b83979c5eaaff2ccd70e97b98a2bab80e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:25:07 +0000

    limit of closed nearly on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import tensorflow as tf
+print(tf.__version__)
