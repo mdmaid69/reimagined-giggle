3c64700de6c271d1bf1fcf31a0cec190d329029c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:27 +0000

    runinto machine shopping possibly straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
