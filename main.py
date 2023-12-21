94f50f59c84fb12ee35a4049515400de9675329e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:59 +0000

    deliberate mainly family learn close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
