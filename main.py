0c1cc68c4a606bc0f5c8b390e9fcbb5dd26a4571
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:39 +0000

    Hh or blame range hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
