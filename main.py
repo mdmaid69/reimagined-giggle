e99a548634b2e0b2d5df4a3a893d8711423bbe82
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:25:33 +0000

    you wind big thumb tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import tensorflow as tf
+print(tf.__version__)
