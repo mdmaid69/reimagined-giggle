97b319dccda848f30f8967fc74833537a6f7666c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:27 +0000

    rarely whisper basically field constantly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
