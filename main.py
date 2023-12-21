028372a4804a6352b6cd4d1fdff5e2afe1754ae9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:34 +0000

    apart boat moon rich embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_typecode(array):
-        return array.typecode
+print(sum(range(10)))
+import sys
+def exit_program():
+        sys.exit()
