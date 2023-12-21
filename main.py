f24138138294ef29ff5b07c5804541a39f786b53
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:46:22 +0000

    memory afternoon bright matter loan

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
