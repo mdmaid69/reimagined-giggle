5a785064fadc18ac81392ec8bb9fc523918d435e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:19 +0000

    takeaway shut even share deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
