00159d677f397f91a493924664741dbaa29bab61
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:05 +0000

    present direct murder complain crisis

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import collections
-def create_user_list():
-        return collections.UserList()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
