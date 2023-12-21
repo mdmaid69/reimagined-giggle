6138f5121ba6e10c14c2e9ff254834df0d10a571
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:11 +0000

    break comeover crowd serve poor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def count_words(sentence):
+        return len(sentence.split())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
