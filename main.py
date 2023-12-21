0bb7b90d59980f981d9b25f02895ee5184694d35
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:05:37 +0000

    judge else bright cause other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
