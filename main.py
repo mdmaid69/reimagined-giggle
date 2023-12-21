be8dd2642499afab5d860dc7597eb43750c52dcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:35 +0000

    giveaway loud practice small firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
