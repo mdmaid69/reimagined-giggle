8293750eedb48ef9267757970cd984cc5a58574e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:45 +0000

    reason poison correct choose by

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
