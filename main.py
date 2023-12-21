09e012e46319098caba5ab8cf27737775bdf05ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:07 +0000

    old expect upstairs weigh team

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import os
-print(os.getcwd())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
