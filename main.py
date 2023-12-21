75e051dfb0014f51a53c98274e022ff372b24cfc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:18 +0000

    complete the wave line send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_dict():
+        return collections.UserDict()
