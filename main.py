ab19294972df1923b961c25bdfbb69c1c99e9657
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:39 +0000

    finish suggest reject base holdup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def cube_number(x):
+        return x**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
