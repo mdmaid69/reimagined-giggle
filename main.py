7ca4ace61852c8afae47c7a0ba96c93666fc2ffb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:49 +0000

    accidentally eat callat wet threaten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print("Hello, world!")
