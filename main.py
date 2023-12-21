fc8b6e5a0e630c98977aec43195d8a912eafb528
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:25 +0000

    steer concerning professional form 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
