e1b0726fff8e99ec889cc983c2703f524242389d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:12:48 +0000

    turn copy house advice form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
