0e34d3d79eaf2c5d50d220923a3a03cfc2101857
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:04 +0000

    pattern exaggerate neck discovery bury

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
