c2dac5d69bb7bbc21ab3b36d4c396d465f189dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:27 +0000

    hate steal impression produce when

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
