58e9e435c128814e3defa604b4639cda738c34c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:04 +0000

    waste explain mean brown blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
