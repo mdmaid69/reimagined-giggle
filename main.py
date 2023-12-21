fde986da978bbf038a77c15e874c5a56e9871d8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:13 +0000

    giveaway error image force remark

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
