6ddcf414db54771b86738624d16d83a982b30140
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:17 +0000

    rank sign gate compare game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
