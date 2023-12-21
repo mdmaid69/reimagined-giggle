ed74c9c18fbcd41234f9898f4f71cb69bef5663d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:19 +0000

    interview twist variety tape skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
