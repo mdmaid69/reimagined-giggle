179f51ccf2a118e2595078fefcb814ccedd30d31
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:24:07 +0000

    pure lot maximum gooff formal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_work(force, distance):
-        return force * distance
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
