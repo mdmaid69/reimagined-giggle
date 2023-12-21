3391e0be5a23c2a8cfe17ed9dc62601f8e041551
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:52 +0000

    completely ride uncertain direction first

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
