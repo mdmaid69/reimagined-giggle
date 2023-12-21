c49e980d9af4bdfa5986d6ccdb4e4bca3341dbf9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:39:02 +0000

    on attitude stop stupid runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_directory():
+        return os.getcwd()
