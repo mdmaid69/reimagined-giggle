a53a6523dd3651f8e7a6866729767e6e867ea461
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:43:42 +0000

    brother correct forgive connection party

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+name = "Python"
+print("Hello,", name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
