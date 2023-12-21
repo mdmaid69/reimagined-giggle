94a2e4907998912bc5f3895c647773e9a2099fa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:48 +0000

    imagination snow lock statement proper

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
