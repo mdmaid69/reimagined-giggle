6b8810c2b54d7bbdc9d75eb1cf14723b2d03289b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:38 +0000

    serious various seat fork collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
