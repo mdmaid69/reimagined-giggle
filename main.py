bd3962de8f37f8ced363ddef52ad09ae217e7534
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:02 +0000

    cinema mass cope whisper ago

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
