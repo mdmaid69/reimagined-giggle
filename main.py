d08d342575ba69e451a55f61f8badc0eec1f4223
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:57:20 +0000

    brown reach trust swim remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_list(array):
+        return list(array)
