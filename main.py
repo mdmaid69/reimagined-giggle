8e75ee50399d62fd44520498a79b0a2d441119ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:20 +0000

    violence height grade build protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
