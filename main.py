d1ec1bdc472efc80115f12f4e041f62706d06346
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:28 +0000

    race century tomorrow back kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+def subtract_numbers(x, y):
+        return x - y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
