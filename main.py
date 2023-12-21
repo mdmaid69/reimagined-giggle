495adc0e9515a08a70f9e22e1a5efbc2f03297dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:59 +0000

    full that walk demand petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
