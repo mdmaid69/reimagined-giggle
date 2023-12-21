613f734627b0ad69dcc36b1933b65e31da4aee8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:10 +0000

    deep poor down knock respect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
