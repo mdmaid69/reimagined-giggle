19606ae67752184f92126b7c41abb6530639bd89
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:46 +0000

    needle depend equal her between

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
