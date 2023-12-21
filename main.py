af1f501930de1764d74b2b304ad2ce82af8b057e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:28:33 +0000

    vote soak straight improve personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
