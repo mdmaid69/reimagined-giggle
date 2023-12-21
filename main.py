bf586394f5883dae884d23f43b4b4511ab910e2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:02:14 +0000

    failure creature 567enemy profit rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
