e5468ca4e0e1a3cccb933bc3d47697b2a3fdcbc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:58 +0000

    comeover other spare properly service

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
