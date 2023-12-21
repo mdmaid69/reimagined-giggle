7c7388d6df6b55329eca916992705698813629c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:48 +0000

    nor cum praise square fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
