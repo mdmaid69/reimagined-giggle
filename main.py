fcb32514bb88d09f8dd0214b3d1e1ef4326154a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:38 +0000

    about artificial value odd cheat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
