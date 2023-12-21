9876826d3d0016d948d2e7822450eaceaacca10f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:34 +0000

    little callat declaration union beautiful

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_pressure(force, area):
+        return force / area
