1013bf02b7c93229d27ddc0b0ae39a45bd9375a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:48:26 +0000

    fight success play settle hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
