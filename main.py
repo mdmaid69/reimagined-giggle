f1767701b316979952792789c23c9d9666a5fb6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:53:35 +0000

    defeat intend related yourself goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
