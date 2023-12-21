39f33767f1b008c851fc2c1c3d24d566b271a98b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:52 +0000

    flight accidentally rush friendly closely

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def reverse_list(lst):
+        return lst[::-1]
