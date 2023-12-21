7174c86a032f0986a5707621a9dbfc79f7c0de5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:07 +0000

    certain trial speak thick gradually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+for i in range(5):
+        print(i)
+  def reverse_list(lst):
+        return lst[::-1]
