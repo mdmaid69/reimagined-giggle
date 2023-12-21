85db6fb136dcc0c763bb4202fddf7f53abba93a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:03 +0000

    loss afraid anywhere breakinto some

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-  def reverse_list(lst):
-        return lst[::-1]
+import sys
+def print_python_version():
+        print(sys.version)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
