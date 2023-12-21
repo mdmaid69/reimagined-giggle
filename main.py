d52089b73e01824a0f40262c45cdf063bae1e8a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:56 +0000

    question everywhere Gg year out

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_bytes_from_array(array):
+        return array.tobytes()
