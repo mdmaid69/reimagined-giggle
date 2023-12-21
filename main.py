49c89ad46d5e798a0ec0c43c0719fa080e20030c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:15:34 +0000

    scale pin careful complaint fortunately

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
