81247391811ec0c05a5063d3b1aac0b42ea7229b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:36 +0000

    share brown argue treat 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
