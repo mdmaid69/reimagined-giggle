25a5c5d44d4a65b2209f3ee01d8d9a0e7ae59886
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:13:58 +0000

    rest print exchange crowded relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
