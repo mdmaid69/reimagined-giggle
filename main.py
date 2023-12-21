acbd4e093ff21c0073060d3c9dc8a34fb04e8580
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:45 +0000

    swimming attempt wash opinion makeinto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
