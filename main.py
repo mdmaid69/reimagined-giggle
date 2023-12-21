701c6790d703a58676120b5448b070811fca4046
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:06 +0000

    problem responsible closed at good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
