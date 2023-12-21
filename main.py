9949065057aaff6a1a543422256a2a0a33fa44f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:37 +0000

    shopping line brother eye later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
