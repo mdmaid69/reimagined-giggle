e3ce4b8318e88289eb6c16063b22de832a660469
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:03:23 +0000

    worse absolutely walk trap block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
