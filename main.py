27b47d6fe2058bcfd943868a6d456f27e5d387e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:22 +0000

    ordinary under record it watch

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def convert_to_octal(n):
+        return oct(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
