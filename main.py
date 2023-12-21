9b4976e4bcade2146cb3fa11492b06ea19c5198b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:08:10 +0000

    reduce left trial concern experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
