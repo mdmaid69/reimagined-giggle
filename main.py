94d7485f6329ea57ececa6d4b0e76732aa6b5b8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:08 +0000

    reply both after pen turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
