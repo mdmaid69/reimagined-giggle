8ead1de4ea5b9110e08281f359161c9d90510fc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:06 +0000

    pump repeat trip condition far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
