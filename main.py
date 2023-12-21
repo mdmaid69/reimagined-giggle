f992c6444b54f25248f972f6f143850d8c1bcdf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:50:29 +0000

    whom smile fond translate even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
