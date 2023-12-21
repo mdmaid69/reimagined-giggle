e7d37188ac4d1f13ca22bf987408fbabfee7db4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:10 +0000

    concern thin create area bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
