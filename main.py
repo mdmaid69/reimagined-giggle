a4e16d7daa351578df664680c4144be06a402dab
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:19 +0000

    fortunate important instant line material

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
