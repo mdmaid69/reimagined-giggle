4f5cc836c8c7f8bcc48a467341a9cfef985cb6ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:47 +0000

    bad either day fact motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
