298fdd7e5692576f524d271505d1450a8d41b193
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:54:00 +0000

    key means broken word mouth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
