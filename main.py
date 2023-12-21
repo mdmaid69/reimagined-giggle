c15b2f448f458e2b11ad12eb5a28e3576f008ec1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:22:46 +0000

    count average firm under check

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
