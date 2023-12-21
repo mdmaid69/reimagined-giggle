8f3c8c0a2ffb9b7cc7c9715ac1741c9fbeb2ebb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:21 +0000

    on entry drive bunch pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
