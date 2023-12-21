7e314857ff28a5d2b792ba2cddd75ecd55a2e6be
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:39:16 +0000

    nigga offer firm to refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def count_elements(lst):
+        return len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
