09045ba2cc333731e3e2b099ebc36e85e6f27e04
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:09 +0000

    grateful disappear flight march search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
