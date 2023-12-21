3c208bdba3feddaa018c3d56dc4e8cdb30939a00
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:16:03 +0000

    contribute comeback practise share moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
