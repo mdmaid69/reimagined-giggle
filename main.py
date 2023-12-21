64f21c71c8c0a8aff4d94afef3308ff2daefc6ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:16 +0000

    talk hollow beyond disturb sharp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
