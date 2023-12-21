5e98d5d30fb33df442deca4043f33d8fa05869f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:49 +0000

    later stand ours miss cause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
