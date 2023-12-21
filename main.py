8b74d3cc9f1a41e376d45a70b384b39d434f3064
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:10 +0000

    easy desperate meaning thorough expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
