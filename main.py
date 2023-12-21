f115e1158bdb198931eaa65e0e38c2fddf06f21d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:52:41 +0000

    566end bringup into giveback uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_floor(x):
+        return math.floor(x)
