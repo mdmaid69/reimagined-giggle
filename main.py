04e9b1345615a868a9eddc356d8253e9ef8d735f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:55 +0000

    mark whom hurt beneath comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
