1b1d0e58a63a5e1c2ffa8bf9910ddab2d7a20deb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:29 +0000

    they collect store exchange keepup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
