241e6b2bc1d00e93b6ab1f9dd377061974c91bce
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:15 +0000

    steam conversation where reply somewhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
