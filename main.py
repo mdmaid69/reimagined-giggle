0478cddafcb3b3efd59a4b8ba2eff2d16268b41e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:34 +0000

    569enjoy comfortable chief tragedy exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
