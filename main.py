6502f6154d98839a00d23da08524f8c0352f5fdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:33:44 +0000

    sound instead cover suddenly correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
