deee07c9854469156b93527bcce520275d2611b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:55 +0000

    mess fortunate hardly priority middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
