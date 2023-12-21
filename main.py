7bcf19762481ebbad9d379d3b14d9726e8f5f20e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:13 +0000

    relation thought war pool her

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+x = 10
+y = 20
+print("Sum:", x + y)
