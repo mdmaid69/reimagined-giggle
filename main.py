c48bccb58e20b295742ea7c301597e1ef96fdc19
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:54 +0000

    breathe leave show breakin class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
