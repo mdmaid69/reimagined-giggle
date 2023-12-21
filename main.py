17a033562d635983cab7934f4ee646e6f4e6dc1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:07 +0000

    fresh possibly wire breakin respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
