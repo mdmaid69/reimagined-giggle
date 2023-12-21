284f5db5bf8c865e994b08174293c1c7d5683657
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:46:43 +0000

    card firm together round undo

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def clear_array(array):
-        array *= 0
+def check_if_array_contains_item(array, item):
+        return item in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
