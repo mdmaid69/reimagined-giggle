bba8670ef25c6dc113bf4466e9f7e4cc579d695a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:20 +0000

    closely stage guilty catch her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
