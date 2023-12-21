54edfe8eb4fbb929656c86421c3bc875525668e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:54 +0000

    show trust official wrong start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_priority_queue():
+        return collections.deque()
+import platform
+def get_python_version():
+        return platform.python_version()
