b020789673ea630a6bc632bacc5390ab5c6412e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:42 +0000

    duty importance ordinary comeback disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
