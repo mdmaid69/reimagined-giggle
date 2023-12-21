6af22e4f75adc233c51b0910b4a62c54ae432a5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:02 +0000

    either instruction spring loose until

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
