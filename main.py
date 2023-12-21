6c332ed4eb511ab3527a312a953dab8397018528
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:08 +0000

    appeal comeover gift effect react

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def cube_number(x):
+        return x**3
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_array_to_bytes(array):
+        return array.tobytes()
