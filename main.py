1eac5abb196383f492689c36047d3228bc40e622
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:49 +0000

    mass no pleased bank 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
