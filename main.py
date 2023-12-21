8997119837ed92f03b8cafce8839eeba241edbae
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:58 +0000

    help behind she performance they

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import collections
-def create_user_list():
-        return collections.UserList()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def create_queue():
+        return collections.deque()
