ef2855d9d08bbe769967b12bfc4839558e451b92
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:21 +0000

    568energy in live class except

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
