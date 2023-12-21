e0000e25e635bbc017a0304e5b65d5c349e74473
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:07 +0000

    top choose boil ring deliberately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
