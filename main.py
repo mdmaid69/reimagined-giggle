45092113a47c54e94c1c903afa4915ea8dc9558e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:46 +0000

    noisy balance fear hold convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
