609b9d1677a65ea78fe0d95b9551426f40718a5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:37 +0000

    exhibition vanish think rise favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
