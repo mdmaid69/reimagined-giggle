7afec931f24cf183d397b3bbccac0cad87089e7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:46 +0000

    prefer dig born hurry father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
