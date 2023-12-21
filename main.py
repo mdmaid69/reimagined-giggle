3dd957e2d9b136d3c6673e1156b385a42db54492
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:55 +0000

    treat praise drawing cry unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
