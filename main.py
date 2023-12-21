278c421f351674bdf0a3793b7e39faa2701388a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:40 +0000

    sport please reason deserve describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
