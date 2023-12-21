667eb4109854a31d9ff02ef9d0f62ddef66419f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:01 +0000

    run vote offer fast necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
