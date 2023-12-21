70eb1b40f8afefffc448dc2cb6e1056b6c91054e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:47 +0000

    within bang contribute relief sleep

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
