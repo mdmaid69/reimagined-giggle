25e0e342eab00ee8e7af9b006b49dc9c6cae5b6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:58 +0000

    unpleasant occasional nor clear case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
