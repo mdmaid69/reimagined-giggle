e038aa59b1b6ccd772325f843a6f323d08355449
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:49 +0000

    moral pure deal skirt wide

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
