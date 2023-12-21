740bdf10c6411ed45b90f59438acc958f5832685
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:11 +0000

    active demonstrate group immediate objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
