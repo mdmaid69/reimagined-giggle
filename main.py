6f01eb4a1a961f8b325872b709def74d8aa5c579
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:21 +0000

    strike computer birthday chin favour

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-name = "Python"
-print("Hello,", name)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
