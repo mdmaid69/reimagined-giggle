7357d9e5f93ab2bfb67069984933270ed253059d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:06 +0000

    round occasional turnon original face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
