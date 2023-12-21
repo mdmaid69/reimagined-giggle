d2708be3b7338009d566f2be543705d1931717f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:54 +0000

    ability at string exactly sea

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
