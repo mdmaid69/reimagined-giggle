de853803197650ba97d6d30ae1ad130898097dfc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:30 +0000

    favourite any bag opinion complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
