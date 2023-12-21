bd15c0c47bf55a80d14f083cffa263392cfc0f83
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:21:56 +0000

    unlikely rough water no comedown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+from collections import Counter
+print(Counter("hello world"))
