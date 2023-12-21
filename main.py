2b05c039d6c46c2fc16e1fad1677cbf7b5b47b9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:39 +0000

    strike splash geton sink print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
