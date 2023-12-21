7fc1087fafe316c6d17acb69bb19942d480adbef
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:28 +0000

    brush ought row niggaacademy other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
