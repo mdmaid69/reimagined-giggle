972c51c96e135b4cbd6d85982694f203c3d55c09
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:18 +0000

    eat work rescue insist steep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
