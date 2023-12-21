4d82bb606409c861e468521c79b730a4ca16159d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:01:33 +0000

    sore broad cap lost she

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
