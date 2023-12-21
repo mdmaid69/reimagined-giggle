96125883ef6618cd389cc40b28896f40bd2d1c48
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:50 +0000

    gather perhaps cope hate friendly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+print(math.pi)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
