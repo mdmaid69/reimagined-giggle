b7a1783dd5bc6b222dfdd49450121b930a672732
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:12 +0000

    lucky common regret tragedy war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
