04557d9c99526bd6bbf981d35c309b4553f4629e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:51 +0000

    furniture many joke nature dick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
