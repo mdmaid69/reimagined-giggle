ff6eec45bfb8a861145da600a233b9c46e88f995
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:23 +0000

    national whisper various gas ticket

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
