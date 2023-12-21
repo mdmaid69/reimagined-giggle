8969461b4ec5dac89acc4e1eb4c7a747c0e8187c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:17 +0000

    future ready Hh try live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
