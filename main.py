9370a4e2c67e653cc655835ff7fef1c95264bf72
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:34 +0000

    original power breath begin block

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
