5ecc06c42b93e686d353af5ab056b84d43690602
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:18:34 +0000

    comedown kind black show awake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
