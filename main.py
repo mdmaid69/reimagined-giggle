c8a1e8788b62098934e46311aef238ff0c55b26b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:33 +0000

    example age breakfast use stand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
