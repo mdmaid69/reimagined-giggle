71ea2a686d711866dc1f9f42870c5cbbc4cbbfce
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:01:57 +0000

    getout hit different bag trouble

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def get_array_as_set(array):
+        return set(array)
