0d567737e60cdbd8936d0669f4bd41107f43d626
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:58:37 +0000

    treatment wind side unusual gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
