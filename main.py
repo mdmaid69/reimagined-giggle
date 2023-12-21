debcf83efa215186f846e2bc2fe855290d598f7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:22:28 +0000

    free supply pump steady dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
