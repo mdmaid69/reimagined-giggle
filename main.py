c774df6f65bd0c9ed3cf2c5fab830835f080ac64
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:57 +0000

    crazy physical combine hit stick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
