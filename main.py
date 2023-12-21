3d9bd36a7c4dad37f8eea1775fca146508729317
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:44 +0000

    must demand position OKorokay cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
