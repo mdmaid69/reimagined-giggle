9d7d37e15aa9e8463f3d6ab27afb4905eb45cf1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:14 +0000

    see smoke argument godown label

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
