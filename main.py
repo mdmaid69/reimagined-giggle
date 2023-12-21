19569b3fe9b3200f3bc4ff0a610b66ca11d37918
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:32 +0000

    support emergency arrangement turnaway original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def convert_to_hex(n):
-        return hex(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
