13bc1ff731181c5e6858a10b5125acf646356069
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:09 +0000

    manage workout seem feed either

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
