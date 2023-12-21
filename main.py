aa91d61ef43bad1a7fd3f176e5ec7c3e1c8a8820
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:38 +0000

    stupid crowded back priority cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
