f400203b9d1dbc801c2172a91fb9308866b94116
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:40 +0000

    combination comeround approve turnback proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_unique_words(sentence):
+        return set(sentence.split())
