1efb49b34f69110d03d99759f793e6f1159e5483
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:30 +0000

    broken by pass luck outside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
