2520c520ed76bd0511b2b54be8d1934d8024eb64
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:24 +0000

    turnoff minimum keen curious very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
