ff42603ba6a678c0c1c9fda952a48de2185026d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:58 +0000

    adjective university no cheap come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
