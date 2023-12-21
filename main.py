2b727f54297d04d1e469358e0f6bce36ed64ddd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:25 +0000

    motion original suit guard birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
