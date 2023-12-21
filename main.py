356a65c8e92f81a7a05824cec8506f37bb3f0753
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:11:43 +0000

    heart ourselves improve undo far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
