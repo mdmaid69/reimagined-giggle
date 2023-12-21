c3529b5a6926ab35f78a4af90f98100b440c9099
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:38 +0000

    separate arrival estimate occur Rr

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
