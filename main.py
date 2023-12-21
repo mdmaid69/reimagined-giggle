5cf2cb809688565ff424bc80f3969b98b801e979
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:07 +0000

    trick minimum solve character slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
