7c24d7cc77fdc68e35a59f1cbdc573606519244a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:38 +0000

    freedom present hit identify company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
