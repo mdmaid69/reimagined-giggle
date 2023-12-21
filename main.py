2c88927e1b665c50c0336484cbcc2e75fc6bde1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:04 +0000

    bunch ignore nose violence basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
