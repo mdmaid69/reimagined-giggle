31053c2c8a8580b05f17be0502ee0e7ab069ae75
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:01 +0000

    discussion small star disturb how

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
