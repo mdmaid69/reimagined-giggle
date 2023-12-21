77c6a86a6b4e892425411cc56265196d90bcdc93
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:59:37 +0000

    coffee road entry spring foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
