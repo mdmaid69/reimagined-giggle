dbb22a8ded681613b703f969d0de8aef2de11e18
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:08 +0000

    popular whoever for hour present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
