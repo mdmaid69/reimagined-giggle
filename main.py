50080a09ba5172b564beba57ba1a66402555659d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:40 +0000

    ought even taste single workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
