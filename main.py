7249d842f782d5248a25e5136cad2b66b132d9a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:01:54 +0000

    accidental confusing lead breathe expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
