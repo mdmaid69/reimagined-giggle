d6d18159dbfcc3446c2dbf45865eaf52edcc4067
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:19:16 +0000

    clear need occur suit without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
