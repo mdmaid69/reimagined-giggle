cd3a63cbcbf73184e0aff8eb3c74f459f97680b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:40 +0000

    name avoid raise damage know

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def count_elements(lst):
+        return len(lst)
