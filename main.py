4d24318cd4511967af61c2bef967e25a8fe88cfc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:04 +0000

    wild cool throughout sorry set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
