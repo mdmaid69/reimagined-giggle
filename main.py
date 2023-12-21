c471089b8c502cd09f8f8369dfa18fb29f02ddbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:06 +0000

    weak her need more obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
