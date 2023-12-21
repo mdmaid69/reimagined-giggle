8c46ababd7632edf557b7dfe63202196f61916c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:11 +0000

    run although new area threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
