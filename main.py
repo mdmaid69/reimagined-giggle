425538c25ae923487b10141e75395f576f15e9f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:58 +0000

    strict balance gate plan rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
