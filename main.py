9f35ae8d502220296d544944b37daff17d3b6742
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:51 +0000

    convincing bank by memory smile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
