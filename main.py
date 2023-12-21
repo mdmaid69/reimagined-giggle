3d37626b7905f9685bd3a028f83d3443cb08aeee
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:48 +0000

    apply means strike thorough once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
