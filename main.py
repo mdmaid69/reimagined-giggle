fdff5ec971e040e3e3f016f646a8f7c1ce5e4d55
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:25:54 +0000

    lead correction tread funny excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
