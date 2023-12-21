8ff7108e1441165658ee8609e63a914a2eaf4a0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:52 +0000

    sometimes discover anyone quickly in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
