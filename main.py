39406b483f8613a331cd3aac61ba9b1e626469d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:15 +0000

    confidence juice carry rain force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
