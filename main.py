d3415ce0f1899e2f3c1a93d1afdbaa8ffe66180a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:56:55 +0000

    getout improve necessary remember left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
