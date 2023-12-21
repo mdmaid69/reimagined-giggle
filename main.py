5675fd91ce142cf77e221e07cc394760a16eadfc
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:54:50 +0000

    go boil disappointed active Vv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_acceleration(speed, time):
+        return speed / time
