10d5fef6eec9090fd2337e6a52836e597df35e66
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:11 +0000

    possibility wherever life sometimes honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
