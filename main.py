0b3cdbcffb2c5c78bbe8f5ebb8cf01f5c1dcf88e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:23:44 +0000

    offence scream will claim real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
