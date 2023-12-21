547afbfa34f3d521da0da500f14fb26de6e71486
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:54:41 +0000

    drink bunch contents Bb evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
