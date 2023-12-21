c17d984e360a395413feb9af47c7e215fd3e280d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:49 +0000

    estimate slip callup vision friend

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
