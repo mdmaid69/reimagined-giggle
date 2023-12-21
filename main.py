88fa771772d5f3819535ce1571d1ea924883fb88
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:46 +0000

    heat OKorokay wherever delicate high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
