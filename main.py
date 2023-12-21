4861411156cdf4adae6a3be7623af7e70d56db41
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:21 +0000

    explain detail aim container draw

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import sys
+def print_python_version():
+        return sys.version
