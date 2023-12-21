d6ae76093a3dddeb9e4416725c3be039e08897dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:07 +0000

    peace trick consider joke 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
