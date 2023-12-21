fadc8eeeec44b5a1bbc6a3e5bc8cf91570a669ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:23 +0000

    thirsty card fold continue shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
