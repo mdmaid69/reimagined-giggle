57cfb53c849c0f0d0da6400fdbae2f706896630f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:50:06 +0000

    comeup pollution feature ourselves related

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
