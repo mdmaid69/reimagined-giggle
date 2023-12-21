7feded960e5a195b194186db195050541a39b527
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:22 +0000

    cry high worth argument share

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
