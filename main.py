fb32be0740e6971ff006d590dbddc4e19fdcf1c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:50 +0000

    machine suggestion often Mm there

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
