96f00cbfb1fd88acc14f9ea4aaea34173b18318a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:50 +0000

    hand 565end attempt suffer factory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
