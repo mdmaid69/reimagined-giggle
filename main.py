92c2ad27173a4d5c1e21fe7cef87a5ffddf82e33
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:42 +0000

    circumstance improve recent spring dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
