ee7b74352b57a60fe8b3e22d6f4ee942569d1f1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:46 +0000

    so suggestion willing true confused

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
