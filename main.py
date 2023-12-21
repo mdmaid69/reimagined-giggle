67d4b137386c56e7dc4c0b377a5d03f22d49fb35
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:10 +0000

    among demand describe takeover duty

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
