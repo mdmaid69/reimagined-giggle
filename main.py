23837328d69600c57f38cb7a74ec52122dc40d05
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:20 +0000

    breathe transport OKorokay shop through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
