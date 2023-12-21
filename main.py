0455bf77f8d38b2f5f3967e4d511d9efb7b3469a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:29 +0000

    estimate creature light tape 572enter

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
