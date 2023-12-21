a2e85140033cba92d75bdffff632551f10857f68
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:38:35 +0000

    treat average neither they worry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+  def multiply_numbers(x, y):
+        return x * y
