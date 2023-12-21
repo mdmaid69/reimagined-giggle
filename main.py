76bbdd675cf696b3122878868b930d880345faab
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:26 +0000

    interest start improve comeround failure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_combinations(n, k):
+        return math.comb(n, k)
