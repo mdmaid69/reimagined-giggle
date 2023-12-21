40fe513f15580ba058d995a5ca95b69cce677b10
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:35 +0000

    dry learn middle herself consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
