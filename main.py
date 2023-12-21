3e02b6a923f3edb4b315a59eefc13253486fdbbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:03 +0000

    lot through empty favourite complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import time
 def get_time_since_epoch():
         return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
