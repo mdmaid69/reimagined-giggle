3e76ff5744f9a96a98ca1e04415e09e028d446b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:51 +0000

    boat question less dark me

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
