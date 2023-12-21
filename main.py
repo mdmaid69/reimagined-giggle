6fa6286c29ddc2dd30f14d617fabf903928976f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:15 +0000

    software hurry past stop thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
