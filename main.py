337feb93bc7c12cd47c13aafeed3eda0d36ec0e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:04:25 +0000

    sad uncomfortable head loan put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
