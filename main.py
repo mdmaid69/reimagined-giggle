eacbb0bb707d8432d6115b4421e935a135844f77
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:10 +0000

    thing contact limit squeeze design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
