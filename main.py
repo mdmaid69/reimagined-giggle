65cfb9cab98adad0f178f6d9762589cb54fcd184
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:50 +0000

    need hate throughout no fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
