c2c1c46af57bf8328f71f3d876335cda26413fad
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:02 +0000

    would pin copy course loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
