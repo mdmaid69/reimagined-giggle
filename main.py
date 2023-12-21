aa71c63cd67d3cbbc97e5c4d348ae3a9931e2952
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:45 +0000

    experience relief dig upset drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
