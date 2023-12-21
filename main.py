9d21ea18310ff5803aa9b14369fcbca68e8b04be
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:01 +0000

    exploration within track plenty see

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def subtract_numbers(x, y):
+        return x - y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
