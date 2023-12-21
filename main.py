092a0b302a18de66f8c703e786b3a670c2516afe
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:30 +0000

    he check type lesson runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
