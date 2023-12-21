3560504d9163c5a7b96133edd628b070a4bec57c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:36 +0000

    sale wide soft today it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def append_to_array(array, item):
+        array.append(item)
