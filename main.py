5006866748009062b12f2434e64c82fdf46e1eef
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:25 +0000

    back course pot breakout drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
