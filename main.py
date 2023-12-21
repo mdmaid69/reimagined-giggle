74b8aaa0a9489add33f6426b9cf9a969dea704cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:49:17 +0000

    tread foreign part talk compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
