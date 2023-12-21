4ef5fe5deb413781ed8802de02a6079776b47f6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:20 +0000

    nowhere next any right divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
+import os
+def get_current_working_directory():
+        return os.getcwd()
