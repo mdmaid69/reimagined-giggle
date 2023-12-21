186552989fa43902be35be7127b2e5b2f3012b49
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:25 +0000

    ignore intelligent forever dream pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
