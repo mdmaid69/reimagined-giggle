1cd7ea4abc8dc0fd33d64283f5214dda36462ddc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:26 +0000

    onto anywhere communicate sort into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
