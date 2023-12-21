c88210ce5833d0a56b0e4ef9a1e3387e6350ec83
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:07 +0000

    pretty deliver tidy inform holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
