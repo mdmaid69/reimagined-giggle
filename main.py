2682f886b041fdaa9658ce6304e4d7c830beaefc
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:57:26 +0000

    eat check dark drop course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
