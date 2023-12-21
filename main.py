bda703b06bf00beae0d7bdd6f845ea13ee64fa80
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:16 +0000

    pollution beat road attempt spring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
