3422fd30b2a8c16043944d4b4481eb6777563d92
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:10:47 +0000

    himself anything debt exactly considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
