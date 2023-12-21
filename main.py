44fefe39f1f9f533fd7b30deeafdab2c7fc85b84
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:27 +0000

    work drop damp combine smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_item_count(array, item):
+        return array.count(item)
