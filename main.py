d8b48ae07fb72554754115f16f52b2f14201d819
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:23 +0000

    mean must after wash whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
