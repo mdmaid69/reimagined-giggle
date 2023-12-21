df15696e35805a8e69071a3a0536e08e225f7070
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:07:18 +0000

    unable maximum try criticize count

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
