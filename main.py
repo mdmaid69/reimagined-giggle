d1ca3d9f457065ece020262fd65c56a7e7b3b3d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:17 +0000

    operation sound again cost stir

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
