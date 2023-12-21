8b87a1c29d187aa3f190fc0390babb5999948f97
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:05 +0000

    love weigh carry meal spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
