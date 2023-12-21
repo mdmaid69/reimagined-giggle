5fd4435e397bb8825b6e8b6e5b4612e5b04fd843
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:22 +0000

    smile compare look Zz wake

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
