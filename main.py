c32ab6573317122ee4b96ad0f414f4b3adab97a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:37 +0000

    bar dinner traffic quantity example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
