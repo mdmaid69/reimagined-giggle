3070d2c5f8b2e4bc1858b505776fe6438432ec40
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:17 +0000

    intend aim convince tool rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
