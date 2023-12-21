0e30c91138c712ffc1b2c2381c29ca4422bb5155
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:56:52 +0000

    too yes leave path study

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
