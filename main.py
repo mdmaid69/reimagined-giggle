71c42c67831948345719dc031fa8d3f742a697bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:43:38 +0000

    hurt call cool town persuade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
