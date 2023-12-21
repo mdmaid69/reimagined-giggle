55732f207b59b12d6f480ee497ae353c724e5921
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:06 +0000

    organize how keen happen light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
