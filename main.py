4390fdf850c597b03a677e46e4109f02dff77022
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:59 +0000

    arrange unlucky study 179bite reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
