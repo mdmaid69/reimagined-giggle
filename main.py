d9260e734dd0a1952a4b4cf8ee51dc3f6e0c178a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:54 +0000

    chin fasten from offer murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
