46c890f2f1f7da853e0e080d43ac6a5779c92ade
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:56 +0000

    club earth fall apply nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
