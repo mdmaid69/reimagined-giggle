ef83402967357d19ca250f37f03015bb4ae4252a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:07 +0000

    welcome impressed either name account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
