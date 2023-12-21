91e1b39b00fd66c6a9b23efee4a466b72292f939
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:02 +0000

    apologizeapologise create excited production deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
