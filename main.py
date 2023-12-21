85cdc2bfa1479036430c5ec4ed45febbda82825a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:43 +0000

    software line cool lost help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
