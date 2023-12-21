2099ecd19a3a8a42b8f95e0d73b200dd9d8c9975
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:12 +0000

    reject by leg offence average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
