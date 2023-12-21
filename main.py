4560a04d04b5ed4d5573b05d4b57b429ffca050b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:57 +0000

    chat friendly wherever mark perfect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_speed(distance, time):
+        return distance / time
