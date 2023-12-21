4ff3f39b633f3e396bc38236205a8de7a20c6159
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:35 +0000

    fly mention recent attach lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
