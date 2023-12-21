1f5ee2403a02002eb8443ed5c214245dff578357
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:41 +0000

    finally anywayanyhow myself star combination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
