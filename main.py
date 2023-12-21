693d6bdf8489bef8bbda49b521c17fd6a12e302f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:15 +0000

    purpose commit duty amount valuable

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
