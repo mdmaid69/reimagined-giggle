8bc88bbce06f2668dcad0c3bda2a89e7202f5fd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:07:55 +0000

    tradition noisy contribute play ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
