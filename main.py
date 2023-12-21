2100f62cac307a760f172d02c16835c767b45b9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:27 +0000

    work hospital plenty unlucky mind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
