59ca2ead63ad15f9d59b134488fa7d1f567665f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:29 +0000

    talk steer children answer careful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
