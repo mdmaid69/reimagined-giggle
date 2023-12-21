b40b4f1b3d1b2f32bffe63df67d9a2836a19f134
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:06:09 +0000

    satisfactory where file walk like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
