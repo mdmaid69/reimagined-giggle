d1d599b64be8f5dfadfe8470fe7cca14cc5b9b06
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:02 +0000

    nature interest plenty dull happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
