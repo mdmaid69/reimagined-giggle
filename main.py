729bf08db3372dca70146d7e62382012442ee338
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:04 +0000

    loose nor rule share pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
