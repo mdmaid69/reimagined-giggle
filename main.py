346d98abfa0e974ae3a8fdc7e569134989ecca4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:16 +0000

    talkinto include flow traffic actually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
