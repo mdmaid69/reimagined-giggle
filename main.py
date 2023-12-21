fe555c2215baf6c4519c54b0e508a5dcd53898f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:32:16 +0000

    base day share thinkthrough right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
