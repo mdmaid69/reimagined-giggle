bb79c3f4e5cf4e460d1eb8c410405d13163b060f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:54 +0000

    both born recently ourselves better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
