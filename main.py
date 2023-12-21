e7dc1569f05c86432a8e75a03f67e146a8d74ea4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:27 +0000

    year load shame command sweep

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+for i in range(10): print(i)
