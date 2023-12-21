11cc15ed8655bb641117648a231d1e400cf8d5bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:22 +0000

    peaceful pain past chin replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_current_time():
+        return time.time()
