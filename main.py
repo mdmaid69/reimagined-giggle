6382b41657ca9e49ee8940f384c4db1923118677
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:04 +0000

    cry otherwise blame bread moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
