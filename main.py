b4b20083621b0c15cab9292751db59f4704f933d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:17 +0000

    scale disappear save terrible recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
