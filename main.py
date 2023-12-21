40fd5a5b05068d4cd56e0600c1cc3ca7540b8950
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:06 +0000

    until stamp obvious write firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
