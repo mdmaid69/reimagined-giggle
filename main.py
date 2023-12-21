25ea7a1c30c021f2d14180ed77e7cba84a1fd91d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:56 +0000

    always where since official feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
