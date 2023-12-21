226354b53a00e93463251e961a6e93677826458c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:43 +0000

    work exchange close standard towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
