4bb298ddb87aeaf47716afb5cde795c93be7678f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:42:18 +0000

    565end destroy intend hate brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
