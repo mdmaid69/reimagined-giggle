548bb4e14234729c7d7b0c05dc9e1a580605e6fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:48 +0000

    party makeup roll touch schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
