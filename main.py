25edd95f44ae90ff5c9e229bcf4a2403641a8650
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:03 +0000

    experiment plain exist neither often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
