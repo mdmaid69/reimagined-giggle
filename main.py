3267cf37c1af3a9a5a41de05ceddc8dda40ccd01
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:43 +0000

    win no sweep discuss sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
