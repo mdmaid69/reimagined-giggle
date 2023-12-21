9a3dbd79ffa4ba76a8bd3cd5853232d01c4dff99
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:29 +0000

    confused attempt home marry discussion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
