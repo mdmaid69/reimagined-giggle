6e7f67707ceeee197725ae3a03d286f9b8cca473
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:10 +0000

    guard pride recently takeoff rare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
