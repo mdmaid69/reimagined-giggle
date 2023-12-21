a9f1eb05a8e55dee9bbb1ea7cbcfcfeefd624926
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:55:55 +0000

    increase little affect miss risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
