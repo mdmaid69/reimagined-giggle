73b1f58fa68e718f8b7afe6fb3836279ddfe885b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:40 +0000

    face geton second order reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
