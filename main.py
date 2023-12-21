1bbb3ac5fb005805b016b7c18bb60ce811a3e8a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:00 +0000

    slip clue here before type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
