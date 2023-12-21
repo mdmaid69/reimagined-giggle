40e2e00d205489ff4f95d084fa5f2f0179a4aa61
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:10:39 +0000

    chief back sweet trust bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
