31c16b102f59df17fadd78f4f533b2a022452490
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:06 +0000

    remove doubt quit accidental software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import os
-print(os.getcwd())
+import array
+def reverse_array(array):
+        array.reverse()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
