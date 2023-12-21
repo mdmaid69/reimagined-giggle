27909f13f7bd0351d872e6da92e2c5b3a8f47927
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:59 +0000

    intend table quality night far

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
