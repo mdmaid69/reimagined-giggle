f075adbb38dffe581e66a6caf6db77e987f09e55
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:29 +0000

    fun spin bang wild science

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def reverse_array(array):
+        array.reverse()
