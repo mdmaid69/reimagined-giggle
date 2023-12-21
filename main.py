a8b7e2797ca48ac0856b55ac7eccea7268e3ce56
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:29 +0000

    polite clothes her temper pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_tuple(array):
+        return tuple(array)
