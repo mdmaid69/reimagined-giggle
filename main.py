c697e5e288464bbc8c21651e1442ae6bfad7cf30
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:04 +0000

    nor awake fair cheat harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
