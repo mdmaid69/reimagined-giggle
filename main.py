59a46c5a9dd17c5532ec936e8a69352ce4e215dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:25:31 +0000

    shut ground prize Pp spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
