22aac4cdafae8fc4fa22059d617292681f898d35
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:24 +0000

    reject pool welcome angry goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
