fab5d01e782e1e2e301005635bc35e1ca19d2486
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:58 +0000

    secret sweep sink its claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
