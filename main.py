43335da08a61396ea61a073e46fe3cd6489b6cb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:38:52 +0000

    opposite hope under while trouble

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def multiply_numbers(x, y):
-        return x * y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
