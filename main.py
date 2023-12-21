5e21565ab359ccd70e2fc155059286771082da8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:04 +0000

    turndown seriously past his design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
