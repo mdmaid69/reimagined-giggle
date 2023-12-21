37f2837abb2d428ede2735c0cac1311f76ec898f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:27:10 +0000

    constantly well rather that funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
