8c4021e32e4d2ce3b8382fd68a5a368e5b40f8e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:06 +0000

    listen Ff involve land left

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
