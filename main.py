130bf0f363a2ec1778e6f9b68c3189d75b09f1ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:32 +0000

    basically classroom pipe combine responsibility

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def sort_list(lst):
-        return sorted(lst)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
