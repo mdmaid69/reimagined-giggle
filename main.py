77ddd8a673d43025d53a1d4685e03f0f3afabc5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:10 +0000

    company beneath related Ee company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-name = "Python"
-print("Hello,", name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
