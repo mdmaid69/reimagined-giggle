61d8194958dcd7acf052832196fccf92952fc5b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:28 +0000

    choice book flow forever proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
