946d284257b3ebc8388d6ce005b1107d3f6ac79a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:59 +0000

    moment upstairs luck hope perhaps

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
