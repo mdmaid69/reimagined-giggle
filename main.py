7cd35b44db7b5c1a475aaf14a1115641766bb294
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:47 +0000

    point tool similar collection regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
