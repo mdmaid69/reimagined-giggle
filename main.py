2f11be18ef271de91efc6f27c308ed6fa0893c40
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:12 +0000

    keepoff danger bringdown information tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
