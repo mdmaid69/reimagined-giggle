beb37574bf5f3d2b7a79020fb860cbfd51ebb247
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:16:51 +0000

    slightly wealth film proud hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
