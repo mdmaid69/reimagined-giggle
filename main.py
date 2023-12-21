c89925b0fabc38bb0553cb490d96212c0cf50a25
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:51 +0000

    impatient decision explain circumstance because

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
