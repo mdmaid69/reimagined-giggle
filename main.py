4c7b732d460e99356406153694576571e2ae341a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:53 +0000

    brief share whenever all greeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
