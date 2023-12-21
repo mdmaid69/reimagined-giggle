c3fc63a68d57037794d5e42d640e71ebac1a0223
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:27 +0000

    nervous form stage sound along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
