63482bfd1ad0dc42b88a39c035b5c0213401b96a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:12 +0000

    start company before computer total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_itemsize(array):
+        return array.itemsize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
