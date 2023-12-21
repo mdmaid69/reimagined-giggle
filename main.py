4967faaf0d5f187223ac1b13353c180a99104331
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:12 +0000

    ocean certain reserve hand tour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import math
-print(math.pi)
+def calculate_factorial(n):
+        return math.factorial(n)
