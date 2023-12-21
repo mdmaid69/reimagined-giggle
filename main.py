02888400c227b1a0c66ea20bca517e66e643b351
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:04:46 +0000

    tear aware case uponon check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
