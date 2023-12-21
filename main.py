b4b5e46b3eb2866db95277d2a5ae009e3db85337
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:30 +0000

    promise responsibility control take track

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
