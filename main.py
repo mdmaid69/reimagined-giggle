1e282b4e1a8c1b427473456e55abb9b6a4c844dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:50 +0000

    floor maximum cool statement discussion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
