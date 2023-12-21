46178e49f6ccfdc86011711497202cd84adfaf3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:16 +0000

    nearly everyone environment hate feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
