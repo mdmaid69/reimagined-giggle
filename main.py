b0a5f5173e84a6b110663af82d323880c9da89ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:27 +0000

    exhibition rate arrive along interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
