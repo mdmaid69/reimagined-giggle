00d2148f39dfc54cd319cc9334cccc3eef8b7af3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:49 +0000

    flat guilty himself rise statement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
