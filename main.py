3054c21bbfa25bdb276f735e7332f94a7c1c42b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:33:21 +0000

    immediate wall issue 179bite yesterday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
