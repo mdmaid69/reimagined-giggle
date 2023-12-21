bec35137c7246d3fee29829c88a8716c3edd941d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:24:51 +0000

    beside dig accommodation Itself pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
