89c3545b22771bded95d862f1e1b021cbe719d57
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:10 +0000

    still getoff actual win trial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def find_min(numbers):
-        return min(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
