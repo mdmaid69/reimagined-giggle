5207b54587e1924958790675202fb8d1079ae859
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:45:47 +0000

    regular talkoutof kind left people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
