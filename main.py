ebd6fbb78439a9f61726760ae16698f12197a2c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:07 +0000

    slow couple goout store pile

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
