e119d7e6d854d93fa24827a36d9cfe4c00cbd13a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:01 +0000

    exactly free favourite glass pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
