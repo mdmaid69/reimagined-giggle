2acada87e36ce0aa350264e97b2b5f68547d3132
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:59 +0000

    empty land colour listen reply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
