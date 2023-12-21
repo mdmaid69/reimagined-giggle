0973effd067de66bbc8c6da2872142e08de1392b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:36 +0000

    snow provide handle some drive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
