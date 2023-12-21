fd9344b39e47d26d1a391f3e2111fb472ed0f3fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:41 +0000

    watch slightly fairly software sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def convert_to_binary(n):
+        return bin(n)
