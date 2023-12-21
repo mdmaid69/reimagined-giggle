b3e5e7c12293d0b92604cce8bc079624ff3572b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:24 +0000

    lead estimate slide healthy involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+print(os.getcwd())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
