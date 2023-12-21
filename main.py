197b0426b008f42a571463ff1c2880f325502dff
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:41 +0000

    bit weigh late takeaway contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
