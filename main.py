48873de5346c8e45d0558a9fc4eb36228dfb5e32
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:57 +0000

    too along dress takeover fight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
