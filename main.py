3812480a48bed5bea4b388b1e758e2da9e1f12e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:33 +0000

    plan remain whoever disapprove detail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
