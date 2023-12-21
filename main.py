fb6335244f304af7ca1c6d3199d6b4aa28122e7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:07 +0000

    ear walk other shout intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
