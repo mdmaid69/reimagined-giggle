6691ea4e073562818cee8e4843285a26db034073
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:29:35 +0000

    come commit now everybody agreement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
