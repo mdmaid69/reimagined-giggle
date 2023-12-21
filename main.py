890547e118f8a8670caf87ee0523a7d5d403901b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:22:57 +0000

    sort offer wish drink help

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
