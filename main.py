6ad84383bd8d144a2fc737a141292451e82af9c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:40 +0000

    top lead load what try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
