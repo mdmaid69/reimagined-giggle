b7458ef592db03433055049a2afedbb43493a6c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:31 +0000

    wash spite region sudden loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
