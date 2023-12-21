12702728e3ef5f4f7eef0ff6748c4bb699bf33e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:09 +0000

    small moon violence joke wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
