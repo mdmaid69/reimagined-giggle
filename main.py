612b71391713e4df77b0e1c91e1506b028b8a605
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:07:09 +0000

    yes you gather identify kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
