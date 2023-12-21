bb6e1414ad189fd8a2eda4ec5e62a0a98c95727b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:37 +0000

    smile situation feed nurse sting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
