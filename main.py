dac56b3ae3d3fd25365e68b3efa759edca1ca58a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:19 +0000

    unable number score angry certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
