226d2281ab95783b62e38498b9b1eece0e31d390
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:08 +0000

    soft fine doubt label rain

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
