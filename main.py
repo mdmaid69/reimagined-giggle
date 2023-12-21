eddea6cbabc750f7d3c95ef39bdc972874b548f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:13 +0000

    mix spread relationship table replace

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
