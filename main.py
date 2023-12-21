69b679cc060736b3db77bdafdf15960bba675622
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:58 +0000

    pain bank market bother prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(x, y):
+        return x + y
