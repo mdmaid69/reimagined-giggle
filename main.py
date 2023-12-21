0b68c3568bf93480653afc77fe50250951d314fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:54 +0000

    tomorrow proper vanish height cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
