61cdd1dde2a37510e01c16b6bb09754f4b98721f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:18 +0000

    before though pupil travel recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def multiply_numbers(x, y):
+        return x * y
