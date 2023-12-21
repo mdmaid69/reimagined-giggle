ed95999a221bd0bd8ac1ac87077ba28e4ead4364
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:24 +0000

    tap steady change thorough fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
