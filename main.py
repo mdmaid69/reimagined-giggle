c04ba7f93e5b23ccc20efb741f40bbaa48afc406
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:41:39 +0000

    exploration back label future vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
