48b3099cf39a6846651cf3a667287998425dda78
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:40 +0000

    relative thin dance ready everyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def subtract_numbers(x, y):
+        return x - y
