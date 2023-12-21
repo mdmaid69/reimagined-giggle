c3d38cb38aaf53c01fdfaf0159a798ee8403708c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:32 +0000

    scrape steep agree contribute new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
