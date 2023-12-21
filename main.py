4411bc9ecb86f423d40214ac4f9b737672a8bdc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:29 +0000

    stay edge store change success

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(numbers):
+        return max(numbers)
