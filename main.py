d44c9c84ba6de7c5ba4071e4a74eae71919f8acb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:23:17 +0000

    language turndown broken gift control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
