3a3b7e4274e12ceece29df0e61a55ab9f2625426
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:00 +0000

    traffic powerful runout Vv prepare

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
 def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
         return a
