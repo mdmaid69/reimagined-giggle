1b7a4aa778d3535449621956e5dfa50617d4f4c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:04 +0000

    environment limit tip none breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
