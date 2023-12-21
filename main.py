4ecaba173accc6c7d9036bc32ff9fb4b8c6cddb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:46 +0000

    opponent start famous persuade now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
