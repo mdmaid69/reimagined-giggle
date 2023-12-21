3672a34bf75e4b9b35342b487106558e51e88540
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:41 +0000

    dear valuable art share 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_list():
+        return collections.UserList()
