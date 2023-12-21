db5fdfe152bb5a4c3890a3f768c0faf9edcf449d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:28 +0000

    worth promise usual disgusting wind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
