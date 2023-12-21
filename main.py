448d728841f9213e5e073801ba5c4daba25628bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:41 +0000

    lunch lookaround fight separate usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(lst):
-        return max(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
