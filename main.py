e5723c1ccca85508440fe0ea466f48f5a4acf88c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:41 +0000

    city strange once injury cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
