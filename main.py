a7127c647c3bd557cb79bd733dd1335db03fe032
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:10:19 +0000

    active ilovedicks repeat likely capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
