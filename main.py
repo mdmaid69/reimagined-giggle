44be952115f1c259184a96ca011ba62b2ba9eb92
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:50:31 +0000

    discussion notice rule drive everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
