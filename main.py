1c2fdc623c911e4544b2678e68a85f95a8a22e49
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:04 +0000

    entrance sensible order improve stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
