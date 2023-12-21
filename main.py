6f6849272fdabef8eccd08573a86c19acac01d9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:39:53 +0000

    range flow objection object suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
