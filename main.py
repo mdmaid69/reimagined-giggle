6483521a329c96de0b85621d0e1bb4d440bfacaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:35 +0000

    reject explain never mind regret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
