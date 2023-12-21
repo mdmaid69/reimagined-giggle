c9e7eb21355c746dd07ad8488404eb4a545359bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:11 +0000

    some model love car turninto

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
