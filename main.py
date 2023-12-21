f6b5f37a1025ca773ca1134dcf9ae33b51fa0b25
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:06 +0000

    lookback(on) fall vanish introduce woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
