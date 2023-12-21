6b58f6b124bd2489b6a29f320d4b15820ed2af62
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:23 +0000

    basically luck old solve shut

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
