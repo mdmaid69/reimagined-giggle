a65857704332d66e41732076c20333c0611a4942
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:04 +0000

    choose part battery substance mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
