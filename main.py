3555e3eb5a923380f8051e59c530786b70adc60f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:08 +0000

    better who can education like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
