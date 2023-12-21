31c786b1a838d38ecd55a8acfd8c88bba3c79909
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:51:13 +0000

    pencil convince nonsense occasional close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
