f52d6799a5e4a92cf22606e2dffa38bd78d6cfe8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:03 +0000

    present tight dinner fear ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_speed(distance, time):
+        return distance / time
