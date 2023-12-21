edfdbb995d537a4c5b4cc77dfd81f1fdbea72d85
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:42 +0000

    suffer scene soil ready comfortable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
