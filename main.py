5ae83615e3dab4785521880f5d28580bf61eb57a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:25 +0000

    successful quick bottom mend brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_max(lst):
+        return max(lst)
