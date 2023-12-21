08b1aaf3e8a7e0cb4822a0ba1e6f606381ebaf77
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:24 +0000

    peaceful later pretty breakup phone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
