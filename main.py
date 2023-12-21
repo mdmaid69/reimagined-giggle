4ccec6b6b0f08161d9684b65afdf0af49277ba00
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:46 +0000

    appeal wish through road pair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import tensorflow as tf
+print(tf.__version__)
