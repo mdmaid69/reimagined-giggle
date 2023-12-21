f672d196b1e91fdb69b11889893cd333a10d056c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:45 +0000

    disappear pause Itself anxious hard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
