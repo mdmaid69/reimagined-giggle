4072716963a3e8a80667f114244b28f5765708d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:15 +0000

    pure town concentrate finger rank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
