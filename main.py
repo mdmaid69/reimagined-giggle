42d39d8f71c30a0d52d62b892787de64241abb8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:36:47 +0000

    force turninto pool mother border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
