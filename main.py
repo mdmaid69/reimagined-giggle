1e1e576a8db56f87bef033c16d2f7311e1285ee7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:27 +0000

    whistle thankyou guard giveout share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def greet(name):
+        print(f"Hello, {name}!")
