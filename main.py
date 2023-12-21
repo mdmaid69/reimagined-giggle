d68b170ba7a78150802ba2423ab5eac29d075e82
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:05 +0000

    allow ignore boat brush wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
