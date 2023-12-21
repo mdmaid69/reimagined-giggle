25c272700091d885185a18403757afd74649cb44
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:24 +0000

    listen people easy when callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def count_elements(lst):
+        return len(lst)
