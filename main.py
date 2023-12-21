2e6c4d4db487dd108cf1242cc74eb8c85178a080
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:48 +0000

    world count worry far fierce

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
