700015a168c724e02a14768021fb7e8781ba0625
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:11 +0000

    impossible describe process snow music

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-print(random.randint(0, 100))
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
