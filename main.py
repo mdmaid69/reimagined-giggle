78d65a8c67d902a03ac0b3cb5da6590446290e68
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:16:37 +0000

    hungry general minimum introduce holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
