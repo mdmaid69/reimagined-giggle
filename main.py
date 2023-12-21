f1262721c37d48f4513d18133c46e5b69db3c38b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:50 +0000

    noone ignore attract above hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
