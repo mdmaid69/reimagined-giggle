04a2566a60561f70b669f1c3e780852ab5ce491b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:05 +0000

    cap connect anywayanyhow organize up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
