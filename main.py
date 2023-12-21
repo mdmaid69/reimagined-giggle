c066a9cf1394fa2c06d67f5bbaeab7f492eedf9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:41:15 +0000

    rough smooth handle term kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
