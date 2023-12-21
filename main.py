6a7e3fe1df7baf403d0a9f39532036a0c76370a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:48 +0000

    view common flow include minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
