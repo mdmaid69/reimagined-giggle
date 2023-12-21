e4c10e826bb3b0506f72b948ecd85c0af6009295
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:32 +0000

    suppose draw answer heavy fight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
