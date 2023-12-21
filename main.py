11e1b7b0a7127db954855a478a86cba478b26980
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:54 +0000

    justice steer defend guilty regular

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
