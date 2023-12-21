675937022bdbd2f8d18d3eeb275833994abdae06
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:34 +0000

    past unkind spend stamp level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
