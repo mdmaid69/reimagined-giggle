a792041763ff2a6d2476b187c4e1a6a5d1bdb1bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:20:26 +0000

    depend second slightly hang record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_current_working_directory():
+        return os.getcwd()
