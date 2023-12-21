0704654b030a57a84bffe16f6251377bc3c7405f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:22 +0000

    concerned bright tragedy disagree typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
