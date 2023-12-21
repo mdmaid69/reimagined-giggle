4381c0ed8a289b3faa998ee98103517f45e220b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:28 +0000

    underneath contents soak fat usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import datetime
+print(datetime.datetime.now())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
