4dbffabbde58744ef65ecd1917c036806781688e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:35 +0000

    any related thing pleasure balance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_str(array):
+        return str(array)
