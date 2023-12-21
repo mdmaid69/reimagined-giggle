fae7eb452b4603f6a1fa03b77f91809493d2ecfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:01 +0000

    common discuss before love reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
