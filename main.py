4523bcab1e037019bc0f651f5a5259cecbe19af8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:23 +0000

    nice arm wave mess brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
