d99b19d372b361b94c009e57c77585d143ca44ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:42 +0000

    there become welcome close journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import getpass
+def get_username():
+        return getpass.getuser()
