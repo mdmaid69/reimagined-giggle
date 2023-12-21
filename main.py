263b05f80eeb1752c7e2f0864a0ae1da9c92b520
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:17:26 +0000

    where wish enthusiastic pity luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
