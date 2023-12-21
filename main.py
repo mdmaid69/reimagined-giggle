5769b30ae034a93f2e857a35220736f6868bd854
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:53 +0000

    close tour wait generous glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
