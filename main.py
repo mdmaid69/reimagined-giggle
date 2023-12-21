0ff13c38fd43d82082ffb4945781d87d706dc6f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:05 +0000

    mind sail tell ashamed lift

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
