315aa5306026d33b67d3dc13754bcf7e31440ff8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:06:47 +0000

    command recently development style sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+print([x**2 for x in range(10)])
