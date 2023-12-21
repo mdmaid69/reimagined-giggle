e30ba82c958f2f3af845cffa4690413fbbaf974b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:46 +0000

    work attach pinch person pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
