fd83ed4e0468c2314a4ac27ec3eee0bd39c9f22f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:21:48 +0000

    there off rub thorough interest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
