47f92314cfd15ddae4462b06d76fd2c20f479b03
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:59:13 +0000

    everybody scream nothing accidental common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
