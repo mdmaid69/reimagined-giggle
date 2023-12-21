1035195529ade903d6ebf12660bb7b92078c4b7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:30 +0000

    friendly exchange proper wire relief

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
