5de77ab2e0924a95f092e07acfacad944a64b556
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:30 +0000

    wish alive add measure market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bytes(array):
+        return bytes(array)
+import numpy as np
+print(np.array([1, 2, 3]))
