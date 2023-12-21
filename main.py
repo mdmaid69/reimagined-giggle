1d41c99971077c8a7a36777329eb48f4b77dac64
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:54:02 +0000

    bear past basically talkinto strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
