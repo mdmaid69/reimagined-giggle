2da1b3c62d3afb9a4a8d9a6c41aeba6d24c27cfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:32 +0000

    certainly bet door dream over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_float(array):
+        return float(array[0])
