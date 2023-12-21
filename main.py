eb93dbcee02ec104922d74920167c5c26da28f79
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:01:10 +0000

    after contact eye which drive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
