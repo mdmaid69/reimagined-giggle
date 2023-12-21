2d14551e1734b24c1760711067433213d703b361
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:39 +0000

    injury warm string kill empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
