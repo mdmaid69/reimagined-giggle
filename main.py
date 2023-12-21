bfe0027d5d4609f93a6b15db52e3e070324441e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:43 +0000

    raw dance born physical goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
