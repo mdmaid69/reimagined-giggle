476d9138ba6c11d2e07aaa788b080ccbc257047e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:09:42 +0000

    attempt thorough sleep gate surprising

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import os
-print(os.getcwd())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
