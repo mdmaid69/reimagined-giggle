f3824ac7c861e6522ecd7d6f0c0a6c8e53c281ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:37 +0000

    wonder satisfied ocean glad scrap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
