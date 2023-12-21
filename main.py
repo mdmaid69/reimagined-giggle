fd6b68f0546ee975bef8ba1e1b126d1f2a5e492f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:56 +0000

    wake listen yes field shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def split_path(path):
+        return os.path.split(path)
