ac6b8a641d6cd761eda07485a8725c3110b891d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:19 +0000

    notice get use plan example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
