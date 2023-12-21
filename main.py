1ae340d659af3a1bd4a169867c0b986b5074cb52
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:22:06 +0000

    sign object fine relation search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
