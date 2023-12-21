3cfe77ff54b0247f04133f8ac984587b8fb98a08
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:41 +0000

    protection statement various laugh oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
