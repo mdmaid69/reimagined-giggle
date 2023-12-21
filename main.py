c86a38e5d70f2430ca13f1eda5f91679abc83b7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:28 +0000

    thought decision Ff ride 571enough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
