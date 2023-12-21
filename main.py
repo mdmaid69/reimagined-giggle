078db3e68fae405eaf1380184a1e86c38f68a989
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:19 +0000

    crime say doubt closely makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
