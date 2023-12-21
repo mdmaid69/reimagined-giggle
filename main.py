5f19d4db6ef9504664836874f5ca14973eaadaf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:37 +0000

    too minimum affect splash table

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
