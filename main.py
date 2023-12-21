ae0d407e312234fe4e4e0e26ad586f1531c7755a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:41:09 +0000

    stone equal nothing appearance professional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
