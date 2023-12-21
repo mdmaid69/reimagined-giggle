6f414af69bc6fb7b28b2c9c5275c082929a4e823
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:07 +0000

    frame born Ss somehow right

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
