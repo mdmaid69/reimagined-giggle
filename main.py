43e74948f5aab411689c894c2fdc23c81413ab45
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:36:04 +0000

    force pollution bringforward near sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
