b3e24629d7937f9243ee5f879fffb1c4faba1cd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:12 +0000

    forward suit by check slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
