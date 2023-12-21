ade5cfac6d83f65894edab9e0c4a9db22a966e73
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:33 +0000

    needle alive sign rich such

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
