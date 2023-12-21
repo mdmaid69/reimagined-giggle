b4873b42d21944a402c5ec17b01a36f3830f4d12
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:30:16 +0000

    seat fight mysterious feature contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
