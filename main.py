a20473c0477718d371c96543c9870f36e7ca62d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:59 +0000

    beginning eager speak path case

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def sort_list(lst):
+        return sorted(lst)
