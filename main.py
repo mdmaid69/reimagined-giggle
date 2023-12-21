e11d2f739b4127eaf1a9371cf4aa610d9d058c98
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:05 +0000

    record compete tool especially welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_cosine(x):
+        return math.cos(x)
