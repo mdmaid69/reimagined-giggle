394b3ec69d7217a93ccc5013a5ab7afd2af1db15
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:11 +0000

    goodbye nowhere accidentally list clever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
