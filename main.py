7fc10613c9dec7803551b1d6534de3b7846eed4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:19 +0000

    foreign bottom length hit scare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
