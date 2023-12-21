99f266001b50addb217b1e884e60e7a8f65ab483
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:49 +0000

    get connected search happen workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
