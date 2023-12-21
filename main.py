6ea82f2c4893bbaaa0fa1e0723b9f31598b3f425
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:58 +0000

    brave getoff most tidy pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
