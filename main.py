5db38d9289672d555dc4b5fd2d51bdfd569029df
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:25:25 +0000

    left school since show block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
