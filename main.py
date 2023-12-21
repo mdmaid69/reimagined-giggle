9240f004886dfa32f4360c989f38fe88bd1c85c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:09 +0000

    runaway dry spring pot smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
