dff8292441e743246775b90155a53fd72486f7cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:22:54 +0000

    aware arrival wipe cope family

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
