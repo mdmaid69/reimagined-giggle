00b0f404a351ebb8892be48605bafeeb2dac9fba
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:09 +0000

    tear support almost near talk

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def count_elements(lst):
-        return len(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
