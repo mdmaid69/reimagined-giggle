16d2bdfa4eb98a1d577c7bbb31db4954548edb74
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:10:05 +0000

    immediately female container length science

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
