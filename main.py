bd0d6487aab8caa7233995932aaafd90a5a41b58
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:05 +0000

    adjective sit suggestion organize simple

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import platform
+def get_python_version():
+        return platform.python_version()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
