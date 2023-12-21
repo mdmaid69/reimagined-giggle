74f09a0116adc70295baf480f1fe6297de0a4118
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:06:17 +0000

    press command connect school or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
