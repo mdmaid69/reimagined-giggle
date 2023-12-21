d2c4e249921a68de1ec352d56d0a4be8926435db
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:09 +0000

    introduction exploration some next type

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
