c5349d0fdfd3949649bd3dd31e3b62768ed2c8fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:49 +0000

    comment runover progress best back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
