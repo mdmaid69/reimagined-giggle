9e44cd2a61923a35dd90f7408d31f3efafd150bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:50 +0000

    material year play shout giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
