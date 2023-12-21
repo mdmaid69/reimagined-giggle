1897c13356dccc3bd3dd9298645d8aa1ebe8c68a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:47 +0000

    fall to there square arrange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
