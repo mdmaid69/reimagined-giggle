78f34c2615c12ec979fc416c54ad1bfdb9edb5a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:42:35 +0000

    group deep upwards match soon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
