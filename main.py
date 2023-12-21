c3e934a575737e877b627c0421f6c9cdcbe835dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:59 +0000

    delicate autumn birth show game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
