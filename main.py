6dea6845c3262b50fd1723888baacb4661ea8d06
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:07:12 +0000

    accidental sign view colour getup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
