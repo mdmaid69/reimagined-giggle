bedaa99838228a5c24177014ff1018a35689aa56
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:06 +0000

    violent turn run gather range

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
