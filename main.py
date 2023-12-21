53435cf10378f56a654afe32f0d304e4ad32ff8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:21 +0000

    talk light wide nothing children

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
         return None
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
