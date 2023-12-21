cf65004f33c5c6a875fd0bdb602fe4e25fb04d42
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:43:00 +0000

    left succeed seriously development polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
