1d5dd8a9d91a39cd043074c819f94c063ddc4b8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:15 +0000

    feature size it nervous ride

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
