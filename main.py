cc0f282da4debdd4895739abce54cdc2357e5dfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:43 +0000

    special worried furniture depend comefrom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_list():
-        return collections.UserList()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
