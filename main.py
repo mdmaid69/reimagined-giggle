d1c6f701a8b9ed18c3ee9ee1fcb30e7c87c20c79
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:23 +0000

    takeback drop shape you failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
