34a71484cd34dba47dfa0ac4a3b7e3b517041d1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:54 +0000

    advise direction car object computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import time
-print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def cube_number(x):
+        return x**3
