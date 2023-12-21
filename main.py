a8b79b2c6719d49af2f59cafe9cff32bc8598ad4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:15:24 +0000

    difficulty stretch corner priority money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
