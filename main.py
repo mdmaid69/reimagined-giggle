d67345fe1311c3f36dd884c600663b8b6226b144
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:10:31 +0000

    lock pull excuse gap possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
