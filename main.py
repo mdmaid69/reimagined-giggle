2cfdbf91a02934c551f7cfb46924ca59f7a927db
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:08:21 +0000

    ignore bother accept get relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
