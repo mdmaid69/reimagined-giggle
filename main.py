a0c44cf01a0555fe50eeed082b381cb648d67fb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:57 +0000

    attach hair comedown familiar finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
