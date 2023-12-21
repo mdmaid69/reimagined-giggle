5400f63982013ba50488218ebd68305e33bf334d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:44:10 +0000

    married mend medium dinner on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def get_current_directory():
+        return os.getcwd()
