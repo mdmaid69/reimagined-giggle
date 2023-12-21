a2edf6edbef691c43798344fa4237c2915e02ad0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:39:33 +0000

    explanation yourself love tap range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
