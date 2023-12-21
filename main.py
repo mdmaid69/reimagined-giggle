41d50d269b43a9e4b0f2b42554cc37dba05e4f8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:13 +0000

    but scream turn refer score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
+def is_odd(n):
+        return n % 2 != 0
