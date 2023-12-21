833d983a1dbeec3b3f72895357e6b84287e5f0ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:23:44 +0000

    check commit takeaway way peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
