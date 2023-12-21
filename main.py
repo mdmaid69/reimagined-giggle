d93128a575e00451a887c941b05208f57ac2e6d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:19:21 +0000

    getoff condition deserve escape godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
