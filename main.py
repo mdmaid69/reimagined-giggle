5e103fd8a1342250c91f5ab5ba3a050fac52c00c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:45:48 +0000

    course value crowded earn march

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
