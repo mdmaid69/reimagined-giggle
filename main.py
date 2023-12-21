3a25508ea27e34a943e7fb638e7da28c03bfc4d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:26 +0000

    satisfied change rich welcome new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
