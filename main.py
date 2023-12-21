5952f0bc6dda7064c9c2d4507a585b561efac372
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:58 +0000

    feeling drive sight simple immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
