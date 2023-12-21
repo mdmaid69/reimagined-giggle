c6a501ee3bc2194280fee762fd87e316f52cdc88
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:00 +0000

    arrangement travel immediately turnup herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_item_count(array, item):
+        return array.count(item)
