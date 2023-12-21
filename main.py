bd97f6e9092156b6e47933e619fa099dee3c8278
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:58 +0000

    dangerous report love post shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
