26e460330f6d80a998647a414fbf3484b0f99b36
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:04 +0000

    fork goback(to) benefit creature nose

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
