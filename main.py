4800313863267efaecc91fb9c1c56c8936871368
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:51 +0000

    her greeting not father geton

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
