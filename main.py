5f5ff29343168048c91f0ad1f711bb5464ff295f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:39:37 +0000

    old 566end holiday quiet constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_union(list1, list2):
+        return set(list1) | set(list2)
