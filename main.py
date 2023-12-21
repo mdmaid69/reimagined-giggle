260f02327b013dbfe344d8fb76b455f0cc10b4da
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:01 +0000

    distance sing rate such precise

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
