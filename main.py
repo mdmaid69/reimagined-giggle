e2c983d7436d6030e4649590fb63ab0b0927a212
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:23 +0000

    basically lower shock cum slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
