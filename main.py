9e133dec3828ff6c489afdbbef713c65f419f7eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:28 +0000

    anything sit touch guilty opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def subtract_numbers(x, y):
+        return x - y
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
