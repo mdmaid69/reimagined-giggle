f78b2362c85062f625a09c1a2f38b8790aac2f95
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:34:15 +0000

    listen grateful image definite precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def print_python_version():
+        print(sys.version)
+  def remove_duplicates(lst):
+        return list(set(lst))
