2bef1b3f40c46638d86b104d2f4741e8105867e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:59:58 +0000

    emotion dinner opportunity takeback godown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
