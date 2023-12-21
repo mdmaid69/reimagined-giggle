62beb66fa1bf84ea560dd3fea23c98c342c9cd00
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:05 +0000

    sink before vision lend ground

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
