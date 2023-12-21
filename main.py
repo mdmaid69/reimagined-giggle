51429f832f35cd6d3789743aa083651edaa24d60
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:49 +0000

    report tip someone field tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
