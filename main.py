1ce662f729102e67b5c46f89c14fb2716a538644
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:17 +0000

    unlikely waste please approval private

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import datetime
+print(datetime.datetime.now())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
