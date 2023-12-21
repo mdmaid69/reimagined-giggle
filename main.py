d42f71c69ff3396b5dca728e72e3fefd0f9403d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:32 +0000

    independent matter road here movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
