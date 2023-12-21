0e3a2403b83b8de1a5cf38d9b00df8223c66bcba
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:59 +0000

    sort sell window stupid bank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
