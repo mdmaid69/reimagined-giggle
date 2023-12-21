cbe92e1982e77f8cba77eee4921c5af77642bac3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:24 +0000

    fill physical by believe urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
