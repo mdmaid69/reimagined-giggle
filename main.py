e4fa730022d389c4602c1b3c3d87fb81ee1e1270
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:47 +0000

    past choose mysterious holdup combine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
