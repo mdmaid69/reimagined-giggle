610cdd52bdec452913a154aba0072817359db2c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:04 +0000

    feel design health getup necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
