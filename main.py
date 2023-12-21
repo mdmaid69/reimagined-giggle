bbb125173da8851cf50e005ce3807f43eb733366
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:19 +0000

    right everywhere cheque smile compare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
