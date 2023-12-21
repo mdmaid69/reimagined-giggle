503dc8802c9b89e3c577fc87d4fdf4f945424fbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:08 +0000

    sure comeover gather find way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
