ad046c1537f95ee3eec2bf1b1fff717b4e0c3b52
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:22:56 +0000

    Ss crowd recover left point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
