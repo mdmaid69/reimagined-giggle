16ab5b667420bc6b39d27eef78247d8e8f020073
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:45 +0000

    file tall sky watch danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
