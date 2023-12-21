00c24e5131dd702c1e75f0ef92addfb8c542cee5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:15:20 +0000

    finger swimming reliable season quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-print(os.getcwd())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
