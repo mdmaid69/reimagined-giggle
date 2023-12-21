eac735d0c09d5a2aaa26a827dcff532ab3fc589c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:14 +0000

    present delicate somehow fill group

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
