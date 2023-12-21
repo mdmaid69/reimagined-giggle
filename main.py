be8ad80c9363137d9e86895cf673d5ae6440e119
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:42 +0000

    corner spot limit confusing machine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
