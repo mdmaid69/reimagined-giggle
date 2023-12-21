2d285b963b0e7b1895185b11a02e2beda0f35552
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:56 +0000

    impatient kind convenient improvement by

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def add_numbers(a, b):
+        return a + b
+def find_unique_words(sentence):
+        return set(sentence.split())
