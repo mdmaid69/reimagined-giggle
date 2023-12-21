954e5d749622f48663827896b525f0c9cd703498
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:14 +0000

    react related minimum drawing Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
