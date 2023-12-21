f1263917107e2d6683ab676fa04339a1dd32ad7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:57 +0000

    brave flat spare especially address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
