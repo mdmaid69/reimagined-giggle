0ac334c525e660572c74aad139e52560ccb981f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:26 +0000

    workupto become spare round once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_elements(lst):
-        return len(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+from collections import Counter
+print(Counter("hello world"))
