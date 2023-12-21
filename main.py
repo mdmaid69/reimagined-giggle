6e249e921d930d100af356aef951a8cb26105c15
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:24 +0000

    hair flow blind print permission

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
