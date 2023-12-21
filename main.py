57be30a1008e953a43de6812fd95d0b8c02f1244
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:52 +0000

    disappear keepup tough opinion gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
