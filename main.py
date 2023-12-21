6ff3bd2d8ffbe546a70a21bd82d92deb617f6b9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:54 +0000

    accept taste reach nerve avoid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def divide_numbers(x, y):
+        return x / y
