913917fb14dc2a0ed1accabaff14a92fb3670c39
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:38 +0000

    cough dark notice often original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
