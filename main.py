ec0f47fb2e6c6034448e404cf04df882371c9a18
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:54 +0000

    mark fast gate top education

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def count_words(sentence):
+        return len(sentence.split())
