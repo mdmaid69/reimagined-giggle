bb3ca3c4e24e32a89ed740740eb7e8f629a2cec3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:34:59 +0000

    recover tour clean difference congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
