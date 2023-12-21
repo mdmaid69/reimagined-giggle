6b659d9f8248b8e1c3a4981d1d6ad4d104304be8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:11:19 +0000

    imagine tread side trick identify

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
