153cd9948f8b880407424c12806b0d89cf7382e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:17 +0000

    ground ride we firm finally

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
