e99f2b764a8ebecfaa0334ef97fc37526547569e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:50 +0000

    exaggerate upset head Vv back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
