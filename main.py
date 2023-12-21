1cda76e0c9b16b63f6530b6c023ad38aacae5b8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:09 +0000

    mention another nonsense rob sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
