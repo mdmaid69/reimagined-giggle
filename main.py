fda4b9793506d0ac371883a5e6cc4623246d95eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:05 +0000

    hang see tired sorry once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
