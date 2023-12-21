edd3f600406a1564e805ae2680d6363233046642
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:58 +0000

    patience cost book sign bin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
