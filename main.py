196b452000f98028870987aa6da2d1d93ded90d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:35 +0000

    there pronunciation scream their across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
