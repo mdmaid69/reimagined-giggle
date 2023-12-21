b67a20594deb3d0d2567f7d09b267c2c6fe63dbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:49 +0000

    lock say understand react study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
