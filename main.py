2b366fdd300d6553e585fba4c73e0216ec13d3fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:11 +0000

    colour ocean authority scream ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
