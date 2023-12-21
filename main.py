e2bcd57a56af68bf65a28fced6eea70acacc36c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:37 +0000

    instead smile pattern tip very

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
