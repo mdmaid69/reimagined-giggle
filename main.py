f18ee10f5e946bf0e9751ccf85d13580e5890502
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:47 +0000

    567enemy extraordinary division stranger building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
