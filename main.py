8f49804d693d4f489fa081794262c4196b061a64
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:29 +0000

    round bottom cheque flow Nn

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
