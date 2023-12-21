172af63f0c2715995a766a9bfc53168f0f0ebe0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:44 +0000

    else regular tall calm before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+print(os.getcwd())
