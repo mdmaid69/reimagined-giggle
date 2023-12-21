09ad3445bfe3b089e5d330ae0fd8d6f272cdc35b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:03 +0000

    spring worried substance sense freeze

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
