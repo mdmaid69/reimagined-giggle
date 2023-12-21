0113d36a887adb8effcf13d67a3a9dbe176472e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:29:10 +0000

    recover goaway bus impatient Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
