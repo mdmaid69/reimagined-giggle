5b9dc8db63c35b194696b83df7f6ddd38335119b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:23 +0000

    so desperate apologizeapologise preparation centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
