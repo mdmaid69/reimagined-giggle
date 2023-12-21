95d50eb2238f0ca897323cc7d2a1982e6804abba
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:28 +0000

    sore actually shadow right stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+print(sum(range(10)))
