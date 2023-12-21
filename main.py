dcb6566828d25f8d43aa6776f49a75a4a0b13804
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:14 +0000

    sensible accidentally path break thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
