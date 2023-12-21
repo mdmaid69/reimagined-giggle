61f2852fad60569662f9044205e65381e4fc373f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:55 +0000

    over distance achievement effort stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
