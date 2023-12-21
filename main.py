0b1211126f10d058be91abf718c976751b47f7f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:31 +0000

    happen especially miss undo seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
