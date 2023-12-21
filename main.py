3ba3ae19ca21631b3173e9405b435a1b8f031e8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:22 +0000

    beat game let behind experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
