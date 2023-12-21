e7f8e7f2c3848a2258947b41dec62d80925ddc3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:41 +0000

    book might boil keepout full

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
