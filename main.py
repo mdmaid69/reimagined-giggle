f18fe2850a11254f0771090436b9ea2ea47d87f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:46 +0000

    disappear effect properly afraid shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
