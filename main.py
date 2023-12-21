aab9f8a34c4a34d2f523ce454f8158d92b279429
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:38 +0000

    satisfied job attract rush matter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
