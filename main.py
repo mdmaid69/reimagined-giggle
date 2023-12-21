061c17ca6335e2f69e75c9b9f10125ac635b707e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:43 +0000

    space joke lay side fit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
