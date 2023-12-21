7eed1d4673b2b1bbe4530e8b3a359f406d37e7db
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:01:27 +0000

    song below when report form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
