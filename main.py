fdd825d11a62b314219d3ba0f7ac44ba358dc5a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:23 +0000

    block loud consist culture mess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
