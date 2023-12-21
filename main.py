e6b7d6126f18acddc7da28201ede402f1e2f3b18
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:02 +0000

    judge beginning proper name improve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
