30d665134b36608f98fb0ac940fe8d686151ca01
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:12 +0000

    involve brush laugh your let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
