c488df71964959f0078bffe1d862bbddbd8a9aec
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:29 +0000

    listen last entry Qq turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
