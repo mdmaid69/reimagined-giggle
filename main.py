6d4f4d1254cc96ef5f00e419be0fb43503e5e403
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:32 +0000

    break art less sign right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
 import array
 def remove_from_array(array, item):
         array.remove(item)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
