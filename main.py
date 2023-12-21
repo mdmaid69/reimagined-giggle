555f420903276fe448ff226cf958fc3e91b33c38
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:53 +0000

    set write Zz steady similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
