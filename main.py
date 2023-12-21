024d02d44dee39251acbab48e2f59ead0c82b057
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:08 +0000

    paper basically mind wish every

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
