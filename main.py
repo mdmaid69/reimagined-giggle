1ef4c8b0880b1699402f23da3596b0b359bb2064
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:12 +0000

    me law invitation repair list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
