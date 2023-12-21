3459ca98f04e02ec6ecbaae5dfaf13f9994cd349
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:31 +0000

    arrange habit please pattern voice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+print(sum(range(10)))
