059b33ff5dcdb638892bcf3dc502a2f37eb3d4b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:26:28 +0000

    persuade mine hit especially constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
