d3fd7b2905a5d36b0deb3a1cb1b6b00c51076216
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:45 +0000

    realize thanks instant definite offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
