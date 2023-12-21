bb6f7cd5f924051f170cf5c8e8b348c041a91360
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:50 +0000

    wet me bringdown success kill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_list_from_array(array):
+        return array.tolist()
