557a669fec01346ef00292f96b38141c54b0b11b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:27:23 +0000

    Uu fortunate beside this get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+name = "Python"
+print("Hello,", name)
+import collections
+def create_stack():
+        return collections.deque()
