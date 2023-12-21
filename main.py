55376399812be928148981aa3dac087c4699313b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:16 +0000

    rush crowd positive habit sale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
