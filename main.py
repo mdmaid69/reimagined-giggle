3d65cac27d197452eca872dac932b8aecc1621c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:04 +0000

    play picture hollow complete chief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
