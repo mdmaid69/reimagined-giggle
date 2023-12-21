a61b607cb2f3e8cd577cf1defbd5ee4f6397ddcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:38 +0000

    be introduction however anywhere direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
