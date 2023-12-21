58e86a5f4bb0dce084176e17d8c9b4fd63519017
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:26 +0000

    book pause entry bang comeback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
