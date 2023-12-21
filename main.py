5244c967830be1da62e158a56866519af3911e86
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:54 +0000

    quality burn whole bye describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+def count_elements(lst):
+        return len(lst)
