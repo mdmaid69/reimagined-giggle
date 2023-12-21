b3ffa02c9877eb268b527b671bad951b955569b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:42 +0000

    contribute comefrom region threaten turnaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
