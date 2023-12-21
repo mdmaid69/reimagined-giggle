60506e2cdb6274f1d7aff9be1e3024c3ac32d31a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:01 +0000

    recognize back case light contain

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-print(os.getcwd())
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
