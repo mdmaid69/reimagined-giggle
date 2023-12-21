5a3eb916fc3597fd95b422f28cbc76eeb9961c30
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:25 +0000

    carry rather hit equal hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
