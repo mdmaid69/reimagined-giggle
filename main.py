ff72ba3067271eacf94b74265e3c0406505399e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:07 +0000

    passenger juice alive without Hh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
