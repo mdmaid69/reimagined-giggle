ebf5bd7c7f75bc4eeabfa36c0bdc4b83e55b3b52
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:45:10 +0000

    use comeround live Jj wish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(lst):
+        return min(lst)
