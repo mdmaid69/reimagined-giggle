f3577c9dde8d80ef3bc82147bd6d3be071941bc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:42:15 +0000

    partner study basic direct with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
