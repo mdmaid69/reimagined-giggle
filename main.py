0d4daa58e1ec7b2a0cadd60e7e02dad2ff0a66b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:29 +0000

    leave mouth talkoutof roll minimum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def roll_die():
+        return random.randint(1, 6)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
