39b5ae4ea0bc05a268dbb227f30239f81827f67f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:04 +0000

    plenty often yours get change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
