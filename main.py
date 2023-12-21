032ea9b425b699adadd152d43bacc0b9d8b0f352
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:38 +0000

    mind process load health tone

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
