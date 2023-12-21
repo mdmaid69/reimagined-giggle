c688f7d991744f1f5f28f6d6a6423298b4598452
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:13 +0000

    consult ever takeoff unless breath

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
