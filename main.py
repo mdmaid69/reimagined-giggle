46719ec9c222953690687a399c195e8edee802e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:11:49 +0000

    shout direct hold slice everywhere

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
