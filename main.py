bd433721bb6f117711528dcceeca541df9eccd43
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:57 +0000

    step suggest me exciting quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
