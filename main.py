630e2c24fdac5db21c4e5a56127bdf39b5862ffa
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:12:52 +0000

    message save I already getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
