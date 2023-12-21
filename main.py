8f94dc2913b00df4aafbd57cd0773c815cdd2f0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:36 +0000

    want soft best exam ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
