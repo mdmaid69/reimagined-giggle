fcb247249b2ee2d5acd9172da31bfdf40f6d9bb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:58:43 +0000

    flat awake change plastic join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def log_message(message):
+        logging.info(message)
