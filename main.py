4af4f1e7fb12b510a2e2baf9c21a7d8f6427a5cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:02 +0000

    slight turnback task ought surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
