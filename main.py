02582791841eb70100c6560d27552e91ee7e0e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:01:56 +0000

    whom giveback name talk spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
