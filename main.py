831e40b7b48b15d458adf2fc2d6fa8da9705728e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:42 +0000

    understand congratulations arrival drink complaint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
