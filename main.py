682b3160e8bf306ea566e558cfdaa856fb4791aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:18 +0000

    holdon neither complain exam objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
