31504a5b1bd54988d1a291e35c7dd23fd89d1ba1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:18 +0000

    meaning level congratulate lower wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
