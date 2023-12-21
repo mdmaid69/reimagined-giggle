812ad94d16222398df5d8192a062b8152251ba44
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:38:01 +0000

    hard 179bite note until recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
