581130dee4a9d819374ce638e68c2cc776fce16b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:05 +0000

    crisis quick pay especially go

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def is_odd(n):
-        return n % 2 != 0
+def greet(name):
+        print(f"Hello, {name}!")
+def find_min(numbers):
+        return min(numbers)
