c06afe0a0cf76aef3cd21cfc29711fa6a81c6b82
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:02 +0000

    knock rain emphasis information short

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
