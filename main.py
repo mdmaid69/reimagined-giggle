964b3dbde9ba3246f3c943ce1012326d3d57312c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:15 +0000

    kind judge represent word Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
