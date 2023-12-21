09c402fef326decc69dc6742b8a323074db312f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:00 +0000

    listen gift bother meanwhile fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
