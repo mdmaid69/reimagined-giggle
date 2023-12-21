1938e7116cfda08070ad22fe89e147d4a2344ece
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:12:35 +0000

    partner runout reasonable limit tape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
