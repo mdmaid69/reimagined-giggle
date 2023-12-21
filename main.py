d122daed1df5c86bbe02e08e77a0f77d0d90adec
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:37 +0000

    point chase less capital show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def add_numbers(x, y):
+        return x + y
