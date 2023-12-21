4980159b670bd40419877cdc7122cc46bdc53cde
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:56 +0000

    before contact start tough bar

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
