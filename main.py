322caa19df1fd6753ede0fa15a8ccdac61899aea
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:38:26 +0000

    here slip recent forget smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_counter():
+        return collections.Counter()
