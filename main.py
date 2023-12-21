50c15c86804d99ba45a9a707e5fb9bbe779257ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:31 +0000

    gradually when popular rob low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
