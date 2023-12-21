1bd6c0a631f67a2fcadc62f6dce226ce759a7057
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:32 +0000

    one small fall clearly begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
