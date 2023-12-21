53d4f7f4cfd67a6f8e454b01508aba6514b63315
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:56 +0000

    emotion actually aware event building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
