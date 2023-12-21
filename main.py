7be3c021518f1f6221b9f9a03a1add3562123534
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:51 +0000

    lead cause neither bury language

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
