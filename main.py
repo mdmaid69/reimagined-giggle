d8f526ffba21d9c4a4c203eea0cf364e0e204726
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:08 +0000

    narrow breathe car rush sensitive

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
