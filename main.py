a7a8b868c03f89047de20c753e1642ef8a39553a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:29 +0000

    flood desperate regard mail bit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
