f93dc6fda66808d917f3a59d3ec5998c7b025b95
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:40 +0000

    fall thirsty cry object confuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
