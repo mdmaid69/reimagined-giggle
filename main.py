c30835d8d83b1b62e05707b845a1bba2e2a6c8c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:42 +0000

    hair typical tour generally why

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
