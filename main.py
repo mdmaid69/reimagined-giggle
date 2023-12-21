af179dc9fc62ce275e3bacb1bd7fe3d362a74615
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:58:15 +0000

    excuse high hold count zero

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
