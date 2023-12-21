6d2022b44968cef3fd815e1ae33a81239fe08ea9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:24 +0000

    might now side suit bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
