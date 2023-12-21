670716650b5e874fa6d6bbab5cf4e4756ae77d74
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:08 +0000

    modern wall single 179bite promise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+print(datetime.datetime.now())
