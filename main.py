fb06b078139e6a747b9e1d71ab02f707c65c5142
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:35 +0000

    suckme anybody swallow touch yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_stack():
-        return collections.deque()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
