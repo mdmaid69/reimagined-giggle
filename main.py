60225283de4fddc257b88b06f5a50df192995a2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:01 +0000

    catch drink cover contrast as

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
