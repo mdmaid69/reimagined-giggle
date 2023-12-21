a22d94012b9d2b2143c0a24059b521404ddeaa64
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:21 +0000

    wash eager let plant everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
