3f0b25825630f9aabf032c2e24af734c2e631fe2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:21 +0000

    sell insist offer goaway whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
