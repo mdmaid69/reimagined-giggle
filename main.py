7f461ba07253c31fe8ab0a4125baef252cd7104b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:48 +0000

    answer right name date bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
