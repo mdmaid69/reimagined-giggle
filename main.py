d31a2031b36351b935203f3237f9411e2de62c6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:19 +0000

    door upwards uncertain coffee march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
