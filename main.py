15533689efd60815aa7b765ac36fa87dfe453ce3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:45 +0000

    pool basis crash pronounce space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
