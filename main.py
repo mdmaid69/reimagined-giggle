7c096d196189a5c1dd016322f97a4d35665eed5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:57 +0000

    major grateful term maintain single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+import os
+print(os.getcwd())
