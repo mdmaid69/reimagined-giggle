06728e225e504d2f035c7991594dd53f64df380e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:09 +0000

    exhibition underneath runinto born neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
