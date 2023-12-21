2c1b3ef70bc400f2d8b627d76d295f1c24e5ffb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:33 +0000

    depend pronounce runaway godown talkinto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
