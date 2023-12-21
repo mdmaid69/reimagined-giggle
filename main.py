fb0d28f1ba2d7e46d37763ddcd4622b3b49d3aa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:06 +0000

    recover above comeoff fail apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
