6ed4bd6196cb4b38200c023e14b6994d4fa62df8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:00 +0000

    Yy open along cost modern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
