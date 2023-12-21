0d070c564bf0b0b93a1f0feb8e9a506cb5368a24
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:39:59 +0000

    precise anxious return constantly whether

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def get_array_length(array):
-        return len(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
