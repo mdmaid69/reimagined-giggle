c82bed118f12d910369e511197e5ba3739c28a77
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:49 +0000

    sport break least tradition necessary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
