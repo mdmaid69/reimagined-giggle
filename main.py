aa531d3cbbf16926eb69b28bf7798b754de05f2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:47 +0000

    frame artificial exhibition doctor open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
