3e84bd7247935f219e96dd6dd918fd0c1126c3e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:28 +0000

    purpose will original ability mistake

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
