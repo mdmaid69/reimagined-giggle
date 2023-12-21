41c55c2325ad5aa155b01ebe3e2ad95794f652c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:40:57 +0000

    hard recognize action thinkof since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
