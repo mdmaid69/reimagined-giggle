2863a16ae5246546444ff100e79b4ffba8ba14ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:06:05 +0000

    active war underneath stare talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_counter():
+        return collections.Counter()
