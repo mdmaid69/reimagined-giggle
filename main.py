7443bef6c9ab9edb030574557dc91032328e9259
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:25:03 +0000

    effort doubt school history steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
