8724f7d6c85819b62527e9e70ceb5fdecc9bdd25
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:19 +0000

    breakfast wait pull regret clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
