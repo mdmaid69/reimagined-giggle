1fed0ebc351df1712ba20f844b93a4f866aa2791
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:27 +0000

    cheque sometimes Tt front behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
