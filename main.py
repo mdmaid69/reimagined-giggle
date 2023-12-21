8a96a98eb1a712433694944ff00b94db54b385de
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:58:26 +0000

    single little minute more lie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
