5553322305bc3e28232c064060b651219bfce750
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:17 +0000

    hour leg calmly whatever stare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
