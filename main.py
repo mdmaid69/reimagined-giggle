c914f9a3adc6fe3d653776e7a585b7c2d8ff7dcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:08 +0000

    fix tax system free sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
