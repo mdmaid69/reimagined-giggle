e9d2b060fc55c69f224b4d016c8e5b86cb0c886f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:16 +0000

    scare ticket lookup here broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
