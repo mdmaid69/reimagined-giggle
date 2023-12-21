15d50abcf16769584a9275aa56d12a25e81266a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:06:57 +0000

    today this adjective exploration false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
