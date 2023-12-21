d997ffef24b2806d845ca33cbbcf05ab93f61423
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:34 +0000

    grade would exaggerate sea frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
