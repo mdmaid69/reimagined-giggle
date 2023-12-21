51394d904fba3bf3964093b3a4e1e9b0720484ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:15 +0000

    offend pride commit quick holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
