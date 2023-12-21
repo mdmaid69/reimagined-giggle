517a91bdc0e990d7c4d7b36d462b93a1ab812989
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:32 +0000

    drop Oo concern pour settle

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+from collections import Counter
+print(Counter("hello world"))
+import datetime
+def get_today_date():
+        return datetime.date.today()
