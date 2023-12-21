4f28afb3970a87057d389e5ed475b33e59fb693e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:15:33 +0000

    lunch lead old gift possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
