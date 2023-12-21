3168722aa76f9a9ebaea654750de4a41c238cc7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:55 +0000

    answer display body figure couple

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
