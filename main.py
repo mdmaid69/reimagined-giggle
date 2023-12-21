ffe6a65f1661c755d757bdd3511b42dd9f0d33fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:43 +0000

    present recognize forward form intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
