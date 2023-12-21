958814ee5f4b0d2219908dc19cdfc5c0995b2f95
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:50 +0000

    Ff unable seat accident bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
