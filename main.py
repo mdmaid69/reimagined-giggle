5338033b732968ec85fe431e6006f7aa6dd6b873
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:11:17 +0000

    570enjoyable tragedy visit duty key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
