879ad0e933c30b55da1a13c2111abf4c798d3f4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:00 +0000

    information play pleasure into pattern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_even(n):
+        return n % 2 == 0
