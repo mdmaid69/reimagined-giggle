db514930706e6851d14651c4dc0b8b8677ee0eff
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:45:39 +0000

    skirt my spin happy unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
