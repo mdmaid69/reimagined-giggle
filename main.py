8136a7ad00f22bb8cf46b85f4cc8eb87512f4513
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:56 +0000

    vision charge tomorrow under food

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
