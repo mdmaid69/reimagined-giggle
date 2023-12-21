1b1158d3d3f0af34daef9fa47238fc95fe8374bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:44 +0000

    marry wing fact lead apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
