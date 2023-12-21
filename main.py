7e6f4c4d5447297252ad50d25fddeff13aa6dcf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:54:06 +0000

    chase score rely vision makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
