50400051367da04f3d79a582fb8f99433774bdfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:23 +0000

    change hang somehow make shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
