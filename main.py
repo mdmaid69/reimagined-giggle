35ad22bb028545203c45007275e8fd01bf721c76
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:27 +0000

    conclusion view way afraid bread

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print(sum(range(10)))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
