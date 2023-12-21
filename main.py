1bf49fe0044ceae97e5f66df9d809cb1243fa509
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:57:34 +0000

    imagination one 570enjoyable memory throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
