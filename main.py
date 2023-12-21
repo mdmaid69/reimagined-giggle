52e8b10587ee137a1a7f251fc3b1db3ed864ac59
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:41 +0000

    improvement switch eye hers level

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import time
+print(time.time())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
