1e144ec8ba7eda13b3a589a38f86361a7549381e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:43 +0000

    hollow turnaway national nature better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
