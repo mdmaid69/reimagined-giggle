60f7fa4b572e5cc4d9c6c3995ca186d0f9190366
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:46:52 +0000

    brush natural callup choice prize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
