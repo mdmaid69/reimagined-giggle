9b6ea7748fcf9fc93525992591ee77c2c346a2a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:06:52 +0000

    part above welcome no wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
