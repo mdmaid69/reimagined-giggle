cb02c00ec0a5707188975910e9e9bc4c1b6106c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:54 +0000

    Uu maybe union glad original

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
