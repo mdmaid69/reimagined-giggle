04f0c4d6302bb79b3e18caf3d7aaea8efe1bafbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:50 +0000

    design suffer limit frighten custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
