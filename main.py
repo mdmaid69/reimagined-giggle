3c89b63d73cb594e0e2560a31c6ef2c089e4f596
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:12:23 +0000

    possession crazy burn frame rich

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_density(mass, volume):
-        return mass / volume
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+name = "Python"
+print("Hello,", name)
