407b29b38a6119e6a585bc87f9373c165148daa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:45 +0000

    leave minor foot replace while

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
