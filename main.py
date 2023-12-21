79bf75d304596d0007e532440adce0574efa5baa
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:42 +0000

    neat free nigga true join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
