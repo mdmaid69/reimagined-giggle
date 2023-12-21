4ca95273da55854d0b51ae912f8b4f7f2abca6db
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:40:12 +0000

    better brown little dead relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def subtract_numbers(x, y):
+        return x - y
