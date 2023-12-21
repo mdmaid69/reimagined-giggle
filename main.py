ee0e96e38bcbb0dbceae26b96ca9412ab05e0383
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:45:03 +0000

    born moral indeed wire boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def find_max(lst):
+        return max(lst)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
