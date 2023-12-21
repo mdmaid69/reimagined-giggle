ac3bdeb8268703b9b2ee3d48a300ee7cec6363f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:19 +0000

    pause concentration exchange perform exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
