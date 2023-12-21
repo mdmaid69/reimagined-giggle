41a4ee1d2194b1f01e77edb894249a17c4fa22d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:31 +0000

    born pile food sure failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
