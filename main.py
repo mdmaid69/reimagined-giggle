1fa4d312bec66bb4e16ea8ddc0035eda84d6c36b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:49 +0000

    basically affect everybody cross love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
