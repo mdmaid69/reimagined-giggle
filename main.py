e81e9da27ea30518845853140669faa038e3587e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:04 +0000

    apparently motion dull branch arrival

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+print(os.getcwd())
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
