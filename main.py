daaf3c295739dbd62e2676ea1d191f9be9b3fa70
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:16 +0000

    patience clean Ww motion order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
