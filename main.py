1a1525d2198eca401075c6415d717b697996416f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:24 +0000

    attempt relax blind study succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
