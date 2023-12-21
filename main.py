baca9416ea430668d300dea048d0d3f54e8c9e27
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:56 +0000

    hungry image steer wipe should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
