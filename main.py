0dc0bb62b99c67cdbb274b43598b2d27f5f97b24
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:38 +0000

    room catch risk owe attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
