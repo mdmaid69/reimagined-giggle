66090dd60260d73a9ec41e02c81327ebea2dc857
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:44 +0000

    stamp any point lock so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
