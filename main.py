743a9b4ee898352b6d45ee1ff83b1c75ea45678b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:26:55 +0000

    temper breakthrough genuine visit fortunately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
