43be34cb79a62b97fa45136fd750c64e65f1f671
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:47:32 +0000

    positive pass goon turnon table

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
