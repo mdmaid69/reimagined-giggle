5d0a4244ade2d67d8fdf8fcaccf4bbd8f23d78d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:53 +0000

    because male bush commit seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
