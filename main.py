91d1f8bb2d9a94c3408a9eaf3ac3b49c795212e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:16 +0000

    gas waste finish store port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
