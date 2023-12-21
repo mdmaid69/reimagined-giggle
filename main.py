80cbb7b0edb5eaccb1f062131a3479393b38a351
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:01 +0000

    pattern callout study cinema warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
