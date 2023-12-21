fc9800b1270c36cd8756e00266df9dc1d20d6ac4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:51 +0000

    maybe suckme track joke love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
