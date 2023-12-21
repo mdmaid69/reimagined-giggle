31a23838a05ed43b4baf9daa69483f416554ab56
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:21 +0000

    point get Itself shape across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
