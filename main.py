c865329c0435157168cbe8f4fb0b704f435fe08a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:48 +0000

    he forget feature store comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
