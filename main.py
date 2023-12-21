2be60c9fc23934b6b413e7d03353d2877711c3d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:28 +0000

    pretty woman unlikely perhaps trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
