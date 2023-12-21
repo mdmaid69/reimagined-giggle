fca0200a0e4f8ac0f437ce462a91cf02c4fc2c29
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:40 +0000

    hard sail pronunciation land method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_stack():
-        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
