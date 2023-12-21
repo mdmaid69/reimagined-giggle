4735b229ddb6c82a0aa957bb41a555aaa87ec0fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:46 +0000

    rule winter rid mark important

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
