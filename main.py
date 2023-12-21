ab8f7da6fa9a80ac27db4a819924c43c310d6761
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:18:09 +0000

    man lend either laugh speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+print(re.match("h.*o", "hello world"))
