16f6be2addfc31d831bd60816a312a97efaa1fb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:07:05 +0000

    opinion mother stupid develop eat

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
