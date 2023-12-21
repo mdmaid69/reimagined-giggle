33053566e59afe6d03aab40dc112c50a2d5e1c10
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:20 +0000

    band discuss wave pen run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
