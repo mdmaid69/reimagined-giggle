6dc601131163626762704343a8e779f2396f7d9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:12:17 +0000

    union object tend control clear

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
