1a7d9c8c8fd517ac5cf6dd596d4840f2fa90a778
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:41 +0000

    yourself vision turndown stupid age

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import tempfile
 def create_temp_file():
         return tempfile.NamedTemporaryFile(delete=False)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
