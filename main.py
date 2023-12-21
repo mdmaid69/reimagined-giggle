dc54caec25fe08595cc773aa106be13cab1eedde
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:41 +0000

    case rarely education pause easy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-print(time.time())
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
