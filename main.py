467180d75f69039feb88d59b50e528c7ba182263
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:04 +0000

    opportunity through after expensive relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
