8aac06fea9012ba928d059a8d78fed48320285f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:25 +0000

    past flow each long there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
