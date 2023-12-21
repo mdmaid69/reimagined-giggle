2934a56b85e38a060cb75e5acc58492ec8cc6086
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:12 +0000

    organize suppose justice apparently opinion

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_buffer_info(array):
+        return array.buffer_info()
