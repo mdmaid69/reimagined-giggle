5de42bc0cda9deba02a1ed1d6e96d5e6cd1bab45
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:00 +0000

    unlucky think weekend disappointed load

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
