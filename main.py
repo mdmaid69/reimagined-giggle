d8e2cd508f4a17a96759e770c60ebe51cee0ea47
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:24 +0000

    detail mind Uu length Oo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
