47ff9b2bbd179be71ce7340ca25b596ea75caf09
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:03 +0000

    breakdown worry touch conclusion bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
