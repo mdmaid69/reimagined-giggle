ec63c1a7702584aa466e36b0c159dda87db1e494
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:14 +0000

    Tt bring bottom male improve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import tensorflow as tf
+print(tf.__version__)
