b2d61f53ed9af54087dffb3bc609865fe41e9f59
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:35 +0000

    passenger dry take around connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
