b9adc9c600d4c14522d62db149b1c8a2966901fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:30:21 +0000

    base display marry everybody loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
