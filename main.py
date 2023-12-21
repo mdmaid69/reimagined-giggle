59ade07caf47188fe2899129d1c0ac7acbf4e77c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:29 +0000

    great secret nice innocent now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
