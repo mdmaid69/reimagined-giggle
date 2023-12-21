872ead8b10862f1f073da7893577233c37a04816
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:56 +0000

    spoil gas condition listen question

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
