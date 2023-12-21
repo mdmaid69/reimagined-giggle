420fd21291abd458fd1d6e224c396c10b60f649c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:18 +0000

    mention somewhere polite upwards song

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_item(array, i):
+        return array[i]
+print(sum(range(10)))
