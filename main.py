acf47abe7f027a378012d13df62cf65c297f9363
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:11:33 +0000

    raw priority light truth reduction

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def divide_numbers(x, y):
+        return x / y
