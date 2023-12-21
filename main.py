ea88a46a4be643ab1b5ee3a16dc651f65d619928
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:59 +0000

    wipe herself night wake blow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
