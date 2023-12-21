4e39052707ba0b2db2c790e3077cfceb4965a0eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:10 +0000

    get century exciting chat reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
