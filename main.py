0aeee8b2d647b4bdc081d886e0efbacaef01bb37
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:21 +0000

    switch far strict love complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
