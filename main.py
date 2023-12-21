62fa724a563daff681b584e74d3a989b380ffac7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:04 +0000

    closed achievement suddenly comein appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
