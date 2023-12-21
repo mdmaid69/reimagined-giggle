577c3cedfab360d31634e62b8713e579b6287c57
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:24 +0000

    physical use 569enjoy effect contact

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+for i in range(10): print(i)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
