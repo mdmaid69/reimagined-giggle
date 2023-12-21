6c897bb66e53b11568dd342b55f08238626c9fde
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:00 +0000

    moral careless fit act hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
