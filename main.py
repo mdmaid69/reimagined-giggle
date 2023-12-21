1bcda13cd014d74c1e7c9d0562b23cee7e1df337
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:43 +0000

    however travel loan search style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
