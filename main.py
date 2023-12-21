d71ea3015d01661303b5ce4aa39c21fbfcb2c8f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:07 +0000

    ago ours check information soon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
