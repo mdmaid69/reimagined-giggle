585b3c404ea02d4604f0914dc176aad1c6506651
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:53 +0000

    quickly track field abroad message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import logging
+def log_message(message):
+        logging.info(message)
