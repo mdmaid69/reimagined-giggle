2f6a0d1bbef7802b98576d417a771f352143228b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:31 +0000

    mouth afternoon Mm equal measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
