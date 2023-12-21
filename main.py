31d040c86e698ded94f61417e8719f32aa199b16
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:05 +0000

    direct 566end pause wipe accordingto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_current_working_directory():
+        return os.getcwd()
