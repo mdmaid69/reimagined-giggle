ef944d26bc198ac0f7b54b7bae07e33cdccaa639
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:02:19 +0000

    mess effort lower neat bringforward

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def is_odd(n):
+        return n % 2 != 0
