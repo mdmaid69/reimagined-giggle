9e35dd4005333dcf8eb41325e63b70fe62b692cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:10 +0000

    scared scream mistake agreement goon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
