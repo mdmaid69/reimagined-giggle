9866d2e9979e6f24ee545ca0d272cb4dbab22730
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:10 +0000

    lunch variety possibility pot wherever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_list(lst):
+        return sorted(lst)
