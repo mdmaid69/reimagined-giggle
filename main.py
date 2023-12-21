13845a8bb5e119339544e7dd95b4cf612b771fd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:13:20 +0000

    most raw anything likely quickly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
