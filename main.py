9e7cb9438785cf045bdf2ad77b5ceb9f6daa182b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:07 +0000

    knock way possession basic top

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
