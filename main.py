2a3330a19b4abf5dd8c1fac6b7a04b90e90e55b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:33 +0000

    both return callat plan provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_directory_name(path):
+        return os.path.dirname(path)
