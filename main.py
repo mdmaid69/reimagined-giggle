971fa15fc71c341b9a23d392dd76ac7899e1bf63
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:40 +0000

    match minute although often large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def is_odd(n):
+        return n % 2 != 0
