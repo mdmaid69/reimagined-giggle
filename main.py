48591bd0dc404ae243e335b9e4bf73e1cf21f3be
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:01 +0000

    work tidy straight bringforward active

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def convert_to_binary(n):
+        return bin(n)
+def find_min(lst):
+        return min(lst)
