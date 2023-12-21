c4e37a67e18cbf136dfb57988ef68314d103e013
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:58 +0000

    some whistle hit dry deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
