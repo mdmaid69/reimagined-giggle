85a0d8f4c022466cac51ed8f21b05fa92b9ccde6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:58 +0000

    deep stamp considering but refuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
