803e0d195469fa62d70b5e92ec5ce5fd44171589
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:52 +0000

    avoid lie hard much handle

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
