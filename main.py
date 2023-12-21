2981e6c56eaf67d012e21fb7def64811c4379726
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:17 +0000

    pay mail address stare survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def convert_to_octal(n):
+        return oct(n)
