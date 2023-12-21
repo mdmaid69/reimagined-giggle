54abe74dca66156223e1a199e441c2debc922ef7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:18 +0000

    practice drive nigga thought back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import random
+print(random.randint(0, 100))
