14fa3116c72aff9e1e978412cc9a5ff40e47586c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:58 +0000

    important neck lookafter high bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
