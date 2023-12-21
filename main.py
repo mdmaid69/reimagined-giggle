a7782d0b9a03df6cd4d8f0f5eb5808942388a1ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:03:56 +0000

    television immediate obvious hurt greeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_min(lst):
-        return min(lst)
+import array
+def clear_array(array):
+        array *= 0
+import math
+print(math.pi)
