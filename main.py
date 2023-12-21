288ddaa1b44284fe96496956906007f317dfc502
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:54 +0000

    cry sight lift surprise admire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
