0f4d44cf324603ba966541904e2b659828a9750a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:35:36 +0000

    character by warning boring Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
