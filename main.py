afbb2100fe2912366fe33e6f1c69a89a1359b32d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:33 +0000

    breathe situation generous replace birthday

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def sort_list(lst):
+        return sorted(lst)
