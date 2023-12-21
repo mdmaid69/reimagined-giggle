fc738ceb49ab90befab9a4c3d43f87695c863b2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:42:30 +0000

    stick sleep design leave workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_min(lst):
+        return min(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
