0e19b402f5020437ef8d703b3a3ff14d22e02036
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:31 +0000

    tradition replace wait post demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
