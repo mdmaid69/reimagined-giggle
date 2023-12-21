63e8932f51e0f272cb4eba1bf5b4df6aea4de582
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:19:36 +0000

    lost confused nowhere this colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
