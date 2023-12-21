72c19332086a472d9fab161aed07b9f6a2fbafa3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:31 +0000

    laugh burst national what reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
