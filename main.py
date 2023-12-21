b4e152aa8d2a2586f2b3c3aae66eb690665ef8b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:07 +0000

    bread closely imagine base maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
