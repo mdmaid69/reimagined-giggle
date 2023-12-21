519642b5f0f6eeac0fcce4984c3d2da0f87e3463
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:51 +0000

    wind new giveout artificial confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def count_characters(sentence):
+        return len(sentence)
