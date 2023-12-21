57b62971123d8faf9ec57b64a746248fa6feb5a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:22 +0000

    say impression decision heavy figure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import sys
+def exit_program():
+        sys.exit()
+print(sum(range(10)))
