b722c1dbf1926af370f3b00d37cc95971be65861
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:25:42 +0000

    set talkback slide nor set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
