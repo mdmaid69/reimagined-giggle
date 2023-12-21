890d0c278efe55e6bac325d09d2288b18739992b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:58:17 +0000

    unlucky prepare example runout now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
