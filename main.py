3b653e18531342ec88168526ae4caf1ba0e15100
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:11 +0000

    rather eye speak Zz lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
