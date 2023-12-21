517a2a46ddee8e014532bb61fd6708e86ccccf8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:41 +0000

    naturally confident across completely past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
