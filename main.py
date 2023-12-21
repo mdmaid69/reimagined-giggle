043d220f63b6a4f58058b51f77e08e469baf8519
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:10:07 +0000

    die Hh size remove marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
