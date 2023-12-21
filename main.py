e71e6bb560ce97ac513ce6ec518d7744f9252200
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:54 +0000

    trick whatever fasten if heart

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
