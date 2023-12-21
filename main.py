741fb7bab3ca454f16659ec29a04fb66e005d087
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:31:11 +0000

    proof favourite hot angry dangerous

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
