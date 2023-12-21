eb76f6c560206b526b4057e70e884fc05de906a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:06 +0000

    swimming payment calm hospital world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
 import array
-def get_array_as_list(array):
-        return list(array)
-def is_even(n):
-        return n % 2 == 0
+def get_array_as_repr(array):
+        return repr(array)
