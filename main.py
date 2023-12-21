1f6fbc6b34c71c14fab70aeda2e1eafa1b431783
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:59 +0000

    charge divide path safe error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_as_str(array):
-        return str(array)
-import os
-print(os.getcwd())
+def get_array_item_count(array, item):
+        return array.count(item)
