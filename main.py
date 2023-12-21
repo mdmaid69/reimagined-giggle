25c4087dfdc7751c53187076daabf858b7bd05f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:14 +0000

    move pair chase neither smell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
