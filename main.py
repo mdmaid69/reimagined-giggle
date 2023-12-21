02b92b5912ec0ea774492d1bef34d660c5bcebcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:59 +0000

    talkinto run bring person left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
