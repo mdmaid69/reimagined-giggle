fd2b1eac7d182ac4da862398b71a0b3f49c6aa7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:31 +0000

    between comeround heat soil minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_max(numbers):
+        return max(numbers)
