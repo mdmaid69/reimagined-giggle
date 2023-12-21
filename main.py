05812eb3b0a3776150bf828d7efb80604cd6899e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:19 +0000

    information work Cc note port

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
