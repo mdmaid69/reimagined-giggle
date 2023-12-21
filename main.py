b3e0a186a349fe3b3e95bc1e32c7eede93bcda3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:00 +0000

    discussion successful translate mind generally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
