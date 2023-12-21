01f2a6274b0c5d0feaa8cb83356d1d8ebe191793
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:46 +0000

    company instruction nervous ticket miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+for i in range(5):
+        print(i)
+for i in range(5):
+        print(i)
