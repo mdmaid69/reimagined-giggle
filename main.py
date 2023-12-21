b648bdeda97a2a4fa07c6d5dd5eef43bbef9219e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:39 +0000

    obvious common spite straight beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def create_user_dict():
+        return collections.UserDict()
+import os
+print(os.getcwd())
