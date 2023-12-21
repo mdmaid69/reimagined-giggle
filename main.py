e4386a265351b0282dff8dc9a5d3f16892634cb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:59 +0000

    stranger scared quit burst congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
