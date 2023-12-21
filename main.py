d8c6992ccb8474e113c2bba47e9f94f5756df2c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:07 +0000

    sister process no comeout hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
