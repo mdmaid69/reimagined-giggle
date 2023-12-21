c6ae61eeec71f4acbcbf1a1420b86e475e50640e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:12 +0000

    absolutely swallow scrap ever thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
