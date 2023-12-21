972757252d86176f7d745737fb697ded037c05f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:04 +0000

    receive notice sticky artificial classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
+def calculate_pressure(force, area):
+        return force / area
