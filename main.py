cfe719de6e5353651ba61868f34876cf8c76c566
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:08 +0000

    hurry decide 572enter formal type

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
