45406b5d55d4d3b641ac42b59df895efa1040363
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:15 +0000

    period quarter situation perfect convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
