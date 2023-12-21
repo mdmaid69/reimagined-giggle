f9a5582f43236a3d03e237ef23d2c4446a11e1cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:45 +0000

    moon person main seriously please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
