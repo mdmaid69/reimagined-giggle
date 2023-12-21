eb0136ac94c551bf8ce37227993c8810f8901d5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:33 +0000

    surprise black rub bringup sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_union(list1, list2):
+        return set(list1) | set(list2)
