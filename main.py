3d4c761dc8ec57e4f313554be56345114699e0fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:40 +0000

    up minor regularly stand hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
