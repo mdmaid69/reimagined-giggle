ec5419eaa1cf5ff82f7b16a28b757c6ad9c0251d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:59 +0000

    fold cap hit raise want

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
