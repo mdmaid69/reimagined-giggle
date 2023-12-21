b7e07e9cbd1e4d3952a497a10cf8de50b38dae1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:55 +0000

    mind takeout sick attempt manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
