c83d25def18a7666ca1a020d6ed3c157427c0f8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:15 +0000

    over reject repeat one goback(to)

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
