b8269639d606d7316efc517079edf6eaad2ffa99
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:28 +0000

    disagree he attempt bring over

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
