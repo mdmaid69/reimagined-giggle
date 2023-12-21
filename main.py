5ce3b1f6b6b3ed306feea780c617ff7a1a97cc28
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:29 +0000

    remove apparently earth equal consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def add_numbers(x, y):
+        return x + y
