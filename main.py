cdcd0e99fe2d24d6b6aee80213c536cce205587b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:30 +0000

    light ours priority Nn concentration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_acceleration(speed, time):
+        return speed / time
