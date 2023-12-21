a46c872651888aed7eeb890567bf66d24aa99e32
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:35 +0000

    difference me professional national kind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def convert_to_octal(n):
+        return oct(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
