bc62ede54fb114702b7e415e7ae7db22fcf312d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:08 +0000

    obvious fasten sing once brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
