7a37e3a49b3a2e68b043b13d25b3ee13338dfd01
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:43:31 +0000

    defend smooth bring private feed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
