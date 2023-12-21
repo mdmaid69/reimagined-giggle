3cb751eb54439ac8258e53599281327e1301c1e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:14 +0000

    town reject several hang walk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_max(lst):
+        return max(lst)
