a17af49573daf19b46f577f361424513c3e917bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:38:15 +0000

    walk moment prove flight father

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
