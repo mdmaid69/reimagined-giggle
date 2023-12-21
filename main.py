a4c56e29b50025edb14a4f0dd8ea04f16be31ca8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:54 +0000

    bush tooth charge already straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
