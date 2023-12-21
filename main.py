ded9bb15361176cf947180a8273cddaba5a83566
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:09 +0000

    thinkover consist run succeed real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
