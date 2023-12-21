bf6fc38add2dd794d0f6456f18c82cccbd4af0b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:11 +0000

    round dear pretty extra interruption

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
