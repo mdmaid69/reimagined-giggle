c7e0c52e63b26c92250f6eb1578ea0b9cfe47d70
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:59:25 +0000

    shop nowhere congratulations giveback opposite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
