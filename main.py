c9cbbfcde92b39741498887f6fbe8e6f893a3da4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:38 +0000

    responsibility pencil one case odd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
