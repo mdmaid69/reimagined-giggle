e11f5ebea3aca419ad1e881e16afe1b51c6b04ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:31:51 +0000

    practise congratulations relationship container cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
