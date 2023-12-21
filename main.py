7428120db4068488e269fb7f0c05773adda949ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:46 +0000

    peaceful pure position spring black

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
