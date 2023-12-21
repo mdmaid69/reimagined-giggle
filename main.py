f72adca873f8fafbccaf12ae63cca852e221eb3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:46 +0000

    blame shoot match ignore ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
