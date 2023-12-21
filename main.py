ef9b52ea538a40572b2982142f141515bab075c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:08 +0000

    giveout none but children giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def square_number(x):
+        return x**2
