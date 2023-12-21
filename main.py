59ab2d5b32ec7f5e0949ca75420dd6619a636a74
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:13 +0000

    store takeover below interfere block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
