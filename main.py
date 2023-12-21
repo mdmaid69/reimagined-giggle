6729d22c545377812d3e1b3d95ba884e5b11751a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:14 +0000

    keepfrom spot sore how argue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
