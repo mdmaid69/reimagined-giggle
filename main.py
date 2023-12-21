499f1c45559eceb20235a4dd782474c5d84ea6dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:45:21 +0000

    wait please previous we name

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
