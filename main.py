9c5d754f579d949722fab273d45d987bd2c82be5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:52 +0000

    instant rather tidy talkoutof afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
