cbc51239c604aefee01f0ff7154dd9c34f955a2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:03 +0000

    smart concentrate come minority flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
