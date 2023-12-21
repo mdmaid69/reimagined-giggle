e4b7d1b5056f2e9105c79924cbc460e354ff3be7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:50 +0000

    defend separate measure pride fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
