aa3a25c5589bd09837d7aba30f34c45a1ac50aa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:35 +0000

    block offend shade design back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
