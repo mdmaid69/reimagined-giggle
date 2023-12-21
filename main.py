5ebed62053270091f9ac9d6ede985b07be57f689
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:01:01 +0000

    school bit steep shoot people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
