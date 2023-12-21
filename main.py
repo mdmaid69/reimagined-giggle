e4d77370cc5b7ea44e6ab73e7ee2689d6c966684
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:43 +0000

    laugh concern software amount now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def convert_to_hex(n):
+        return hex(n)
