39f7b546a6cb1035408b2856a2e301349ef9590b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:15 +0000

    workupto steady release sister danger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
