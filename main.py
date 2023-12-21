75467150cf098f9ebddba4f6093248089536cdd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:59 +0000

    time drawing depend yet represent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
+from collections import Counter
+print(Counter("hello world"))
