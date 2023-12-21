dcfebab92612eb0ba9cd52ae6622511a0617994d
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:02:52 +0000

    marry correct introduce decision invite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
