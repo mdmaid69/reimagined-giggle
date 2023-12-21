a90a582e3457cefd8f2845db1bc44b3c743817e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:34:14 +0000

    road uniform share company show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
