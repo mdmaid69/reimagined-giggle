3a815c216f317753dc03df89b0191a00b220b499
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:15 +0000

    juice bad Ii frighten otherwise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
