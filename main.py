40f9f3be407e6e2b4483ea8769fd5e4b71c948fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:28:45 +0000

    music firm hospital foot frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
