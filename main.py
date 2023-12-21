def14b969dc6b9bbf63a00d84f8ad923dd158889
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:55 +0000

    Mm similar apologizeapologise start unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
