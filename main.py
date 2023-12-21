1d16a150a98cd45083569ebe48e129b8ddeda5c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:48 +0000

    pump conduct speed getup contain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
