b502509c066bd4d67f514fc15e1a78c9e1be2989
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:27:35 +0000

    joke light sound breakinto trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
