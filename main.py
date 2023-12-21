59fa05c9edb647fb6ef1971ada49269dcdd2cecd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:18 +0000

    cruelty than everywhere eat abroad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def sort_list(lst):
+        return sorted(lst)
