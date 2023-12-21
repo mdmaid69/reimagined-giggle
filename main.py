fd977d61efe1df5f3b9d7d066e4326569d3632cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:38 +0000

    available sense extremely defeat sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
