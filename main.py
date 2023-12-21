0f60bda7af19a11edce67b715fa4e0587a599e5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:22 +0000

    discovery punish service practise cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
