437179d03a6b45c9d2d5c922bdd32b5e80d83355
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:18 +0000

    pour home above have apart

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
