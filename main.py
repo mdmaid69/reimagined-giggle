1701ea2898301b8365687e764c12168ccb6c00e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:12:55 +0000

    look sign dance instrument brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_as_list(array):
-        return list(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def get_array_as_set(array):
+        return set(array)
