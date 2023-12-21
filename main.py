2aea03233bfadbb9b2ec43f4d69e7581b32c124a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:09 +0000

    common offence beyond fashion great

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
