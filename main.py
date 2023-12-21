bbe2e736c73d6e122ae78113479ce2ba08df09da
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:12 +0000

    goodbye joke instant blind attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def remove_duplicates(lst):
-        return list(set(lst))
+  def split_path(path):
+        return os.path.split(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
