aa167c595407434118bce0abae1f46f8ce777037
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:38:06 +0000

    climb produce trip tough getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
