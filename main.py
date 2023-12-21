20ed7cacdaaf29b922100828144ff418517b369e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:08 +0000

    wind risk mean definite favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
