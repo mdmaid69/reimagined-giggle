d934b397b0a377fc4287ef075bcdd06c6f3337d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:01 +0000

    dress change contrast exercise explain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
