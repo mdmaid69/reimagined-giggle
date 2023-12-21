188a4c879bb5f588ba32b1d0dd348c082a517243
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:05 +0000

    married completely goon forgive danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
