9fb59dbf4fcdad15b7213fe079941de5ce9c66b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:29 +0000

    suck example complicated hers tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
