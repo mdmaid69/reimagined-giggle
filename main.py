620be8479ef1b237c87b43c75eceea108571958b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:21 +0000

    favourite hit principle race train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
