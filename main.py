225b999dc8bef4b48c7fff84f476352410bde1f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:33:49 +0000

    illness problem powerful letter form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
