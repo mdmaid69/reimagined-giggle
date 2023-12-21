f2cbea572694a822c75a6abfc4b45cd591f7698b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:02 +0000

    explain except touch accidentally lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
