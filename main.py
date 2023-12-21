4763f2e75c77bb02f99b4205f02b8e0d70df7101
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:45 +0000

    there by concern pretend drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_set(array):
+        return set(array)
