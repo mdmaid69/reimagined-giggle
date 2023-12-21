701f397eb2f8063c2f2cc96a72cc366e72b3a368
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:23 +0000

    out empty everything attract cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
