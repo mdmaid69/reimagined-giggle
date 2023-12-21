8757d7fc7186c8b4d44ed0072c324c3bb8b8b630
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:09 +0000

    back clothes bus rain may

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def remove_duplicates(lst):
+        return list(set(lst))
