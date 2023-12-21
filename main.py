8dc5e614ae14181c4a5ec64dfb6911298943f763
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:43 +0000

    reserve clear side giveaway drink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
