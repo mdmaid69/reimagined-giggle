b2ed6c8a9b9a5c96e3aee4314054c704a098b675
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:01:16 +0000

    no Mm company obvious store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
