08cc4162c85d9d2c8d43dea17944bc85d8b687c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:27 +0000

    giveback limit upset path runout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
