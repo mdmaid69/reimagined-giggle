d37cd1789fcd878f23885ee619a4a0111ae24843
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:22 +0000

    live blind admit satisfactory fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
