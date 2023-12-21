1ce79b544b21c8a15ed17601ce1cd97ddb4171e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:31 +0000

    nose dead false grand takeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
