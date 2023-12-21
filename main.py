dbc2d75715f2c1138744580b192b50c58e05e048
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:12:32 +0000

    meanwhile bar he chat back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import time
-print(time.time())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
