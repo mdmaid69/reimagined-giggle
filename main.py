a1ca94037dae908eeba56d225dc02db65ac7d356
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:02:16 +0000

    meanwhile slip recognize start warm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_speed(distance, time):
+        return distance / time
