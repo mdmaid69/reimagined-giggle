d1715dfc3a571c15624b0791e7001232a6695beb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:01 +0000

    whom surprise weather inside friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_volume(length, width, height):
+        return length * width * height
