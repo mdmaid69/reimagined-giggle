5a2144c8d859c099b9c593876e2d4547d2933b81
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:37 +0000

    active sweep fine thorough among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
