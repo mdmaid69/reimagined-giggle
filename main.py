e062c4d567ccd4afda2b6a568b10f57ef019d7c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:10:28 +0000

    Ss once crowd comment thick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
+def find_unique_words(sentence):
+        return set(sentence.split())
