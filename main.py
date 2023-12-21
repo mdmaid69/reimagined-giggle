d1307db39842e885770b27bb45eea77bf68d9447
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:02 +0000

    government opposite estimate freedom breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
