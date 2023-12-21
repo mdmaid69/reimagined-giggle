0e6c377ec8b524bce24db30889c995ec534242fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:11 +0000

    partner less guilty plan any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
