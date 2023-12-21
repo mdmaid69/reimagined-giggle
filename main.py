7a7a7fe4cbf621e2a31907dfa49e3c3201be749b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:12 +0000

    breakin way through ourselves turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
