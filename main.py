31ce077ba1d0be78db9f4857e0695f0a4e641f4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:21 +0000

    exit modern play her consist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
