18b0bcc1fd45c9b8809745caa23710c072adf6a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:03 +0000

    beneath might cold balance stamp

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def convert_to_hex(n):
-        return hex(n)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_area_rectangle(l, w):
+        return l * w
+print([x**2 for x in range(10)])
