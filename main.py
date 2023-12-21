724a887fd2cbb7ac2bf41976d191ecc6e1e17f52
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:42:01 +0000

    nigga shoulder opportunity trap nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
