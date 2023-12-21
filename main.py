3a4843c2ea91f805788e96bcdd1f7c2db1126c13
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:40 +0000

    surprised completely least straight direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
