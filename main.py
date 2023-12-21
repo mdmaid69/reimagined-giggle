eaf5997f813472c591b7866dff20e8cf3356d78e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:23:54 +0000

    murder onto heavy need shop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
