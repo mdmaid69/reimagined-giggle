fdf6a0ba6f9ea0a6bf082e1f44fa4209ed90c89a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:58 +0000

    month turnon influence flight too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def split_path(path):
+        return os.path.split(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
