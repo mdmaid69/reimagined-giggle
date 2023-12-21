24ab5afbba4cb7e15ebeb625baaf10a4deafb4fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:51 +0000

    goback(to) why chase holiday be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
