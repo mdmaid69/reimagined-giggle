b04f1edd90322d6a8f31f9d431165f129699d705
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:48 +0000

    production tape do marry laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
