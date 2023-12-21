2c70a9888acd0ab3f1a25847426e7741b5657b98
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:50 +0000

    his exact everyone note slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
