d02fddd72ec914e7a5b2d72fa46cd9b2e74a5990
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:39 +0000

    beyond increase corner nonsense school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
