86ae558900a96553fab3bddaf3e8d46199fac3c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:11 +0000

    manage only terrible relax beat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
