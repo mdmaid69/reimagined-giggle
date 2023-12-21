25e3e3bd7b0b7a23bf4e289d0e5f35758b60d47d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:03 +0000

    convincing safe hang form copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
