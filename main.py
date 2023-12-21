cfb2043c53229e4b1004f1059f5c78ed8f1ff5bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:30 +0000

    congratulations frequent hurt love can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
