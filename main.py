d05260a15ecb6549f8e7f43882f7505ea6c4f690
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:11 +0000

    prove reasonable sweep scare female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sys
-print(sys.version)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
