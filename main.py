2d676fa7314056cdede1df722d5e22c6ba72bcea
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:42:47 +0000

    pronunciation pressure gather skirt load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
