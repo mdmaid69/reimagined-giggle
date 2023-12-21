358cd124bcb651a8e4032f9edea03782178f406c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:32 +0000

    hungry impression run cough plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
