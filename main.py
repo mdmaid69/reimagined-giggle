c6014205006b94744905accf6020a4f68d8d2451
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:50 +0000

    touch breakup whichever convincing difficult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
