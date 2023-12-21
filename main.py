fd344cc6daef1b47f5edcb540538e77970eae157
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:24:09 +0000

    bury connection file speech rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_floor(x):
+        return math.floor(x)
