f1a22906f2b3644bb6dd2fcfd548d50efaf4342c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:06:38 +0000

    rubbish squash now cheque happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
