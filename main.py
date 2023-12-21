d17227c19750e64363640a5efe097e2a9bd18400
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:50:05 +0000

    print ill born introduce image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_rectangle(l, w):
+        return l * w
