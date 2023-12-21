fb04e38f6f37c5b2f07e88c2445967b44ffb930e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:42:29 +0000

    path hospital 566end result earth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
