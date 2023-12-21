e93d035a61fc23809b181485751b12605e9ed3ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:33:16 +0000

    already sure grade introduction fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
