68ff51626bb1c8982562c41ff96066d237631c21
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:58 +0000

    type remain form or wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-def get_current_time():
-        return time.time()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
