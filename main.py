f692956b53f0b24ec3bc172e27b81a3bcf719bf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:31 +0000

    deliberate getback murder write water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
