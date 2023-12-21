51ebe6054b06abaffe5cd6281a6789c25b6e758e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:38 +0000

    argument manner deep undo pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
