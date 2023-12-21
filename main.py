637e338d197c652496cca09f73929de7df7f6b17
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:04 +0000

    marry wind wave success apply

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import collections
+def create_counter():
+        return collections.Counter()
