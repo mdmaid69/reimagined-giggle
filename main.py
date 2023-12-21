62a58f3222cacd0d14560a573f7bfc0eeb96df27
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:05 +0000

    community tired pay along stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
