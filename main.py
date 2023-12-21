320e08b6b74663df3c1d91904be63c7da14bee87
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:39 +0000

    rely object leg creature special

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
