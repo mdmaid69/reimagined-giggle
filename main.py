7afa61a778b46b2dce802110dda6a2d754afde4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:11:00 +0000

    study cause public noone crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
