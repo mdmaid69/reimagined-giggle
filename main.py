d82a7f9767eb687b2f9a42df0e9d9b5deb5c2a24
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:53 +0000

    trip display journey stop bet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
