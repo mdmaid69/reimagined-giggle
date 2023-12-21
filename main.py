240e66a050b308e3b57b41a1a0c7f9c1e2b6c5d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:27 +0000

    runinto final value equal profit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
