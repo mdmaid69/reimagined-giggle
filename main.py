5235c413db4090f1c218ddae97c3bcea0e448e04
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:49 +0000

    squash ordinary belong return below

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_volume(length, width, height):
+        return length * width * height
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
