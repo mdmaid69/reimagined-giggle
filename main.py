66a8db4f2efb290f9c1960016a04dfe84ffce2fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:06:30 +0000

    get might form block uncertain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_max(numbers):
+        return max(numbers)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
