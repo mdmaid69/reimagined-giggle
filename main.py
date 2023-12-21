b5892796a725359caf054420c649b5c34a2c3eba
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:47:48 +0000

    immediate another what play watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
