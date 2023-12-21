4fe11887a38e03327997ca2e251aa51fc3fea32b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:29:31 +0000

    yes grade hope shallow dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
