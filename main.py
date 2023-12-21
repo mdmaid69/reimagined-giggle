c0fb997013c21b05d253442c007e5382f6288093
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:53 +0000

    answer there thin sad callup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
