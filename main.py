c33e1f746743583512537d024af81736aca109c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:48 +0000

    closed though not form slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
