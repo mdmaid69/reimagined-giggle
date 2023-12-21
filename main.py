497fd5bd4305d3efd35bf2f378e74cc1832c4aae
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:08 +0000

    escape past smooth expect near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
