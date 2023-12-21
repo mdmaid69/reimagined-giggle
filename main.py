a0ed40e9a43605309f2444ded93aad15b4222b91
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:33 +0000

    when anybody tragedy modern change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def create_stack():
-        return collections.deque()
+def get_array_as_float(array):
+        return float(array[0])
