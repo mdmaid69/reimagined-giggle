90feab5badaeb27a62e79f5daab6441eefe24e0b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:35 +0000

    goon tap father exam offence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
+name = "Python"
+print("Hello,", name)
