dfcd85e74ffb48445a5e9fa3711a075c308ffced
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:44 +0000

    weekend out Oo mean communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
