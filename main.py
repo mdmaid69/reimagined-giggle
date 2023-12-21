1451e131cb184f12d181f6800bad1413f267ef51
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:58 +0000

    die pause likely very mysterious

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_as_tuple(array):
+        return tuple(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
