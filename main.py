e35401f5d82368332cdf98eef6595e924246dd05
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:34:20 +0000

    correction goaway defeat border unite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   def sort_list(lst):
         return sorted(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
