d70546315c5732eeec7e819f17c72587dde7fd09
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:48 +0000

    wake proof deaf fortunate front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
