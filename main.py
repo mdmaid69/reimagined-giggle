5bd648eaa95775975f04babc3e2529cdb80b051e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:16 +0000

    plan noise depend change also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_length(array):
+        return len(array)
