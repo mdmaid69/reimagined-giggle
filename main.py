6108c6a14ccc92be0dcff2b8c535c0a87e7ab4e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:57:22 +0000

    ride callup match bottom wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
