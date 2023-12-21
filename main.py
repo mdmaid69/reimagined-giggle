dc67815fa3dc8d4f55e8084c659f81fcacce2790
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:07:25 +0000

    thorough manage nor as market

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def convert_to_binary(n):
+        return bin(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
