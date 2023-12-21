72949bad7c2033e257643e663944f266fe44a967
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:38 +0000

    geton broken provide even quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
