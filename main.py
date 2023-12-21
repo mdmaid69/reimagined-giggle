2c4d3368ecc7a32971fef27e6d33700fb97a71fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:10:41 +0000

    drawing guard early opponent dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
