b921f79a8fe43692ef0c81c81a5f825a7eda3ce3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:09 +0000

    part available perhaps crisis trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
