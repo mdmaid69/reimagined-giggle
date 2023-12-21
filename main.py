18d4fda42e9e95606081a2d593c2e9f9ea72ffe7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:10 +0000

    properly before type rough completely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
