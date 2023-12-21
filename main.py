8aeba28e984289aa6ab87879fc1b88a1d43f4c44
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:20 +0000

    responsible difficulty use brief transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_dict():
+        return collections.UserDict()
