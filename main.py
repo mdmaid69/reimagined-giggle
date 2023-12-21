84a0fd2481a840abc6ece1788202d5392b8bca0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:52 +0000

    ask any bored fierce push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def subtract_numbers(x, y):
+        return x - y
+import sys
+def exit_program():
+        sys.exit()
