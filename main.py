8774609acb6bd96011e57ffd59933109b5534a27
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:44 +0000

    last lead should block thing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
