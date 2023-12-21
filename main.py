0a855b409273aeb2058ed96a84f78eb9c6c2f79a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:03 +0000

    after choose later discussion frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
