9404f2047161637bfe88d2f78d4e27ccb1bac8e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:59:30 +0000

    stone gradually manner lookaround clue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
