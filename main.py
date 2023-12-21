d96eb4f32afcf10900b081f4b593894d56600db8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:44:53 +0000

    compete win goon react cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
