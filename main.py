e182bd354d67f194700fbf35c291d56a2cc9b8c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:50:52 +0000

    describe already straight example apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
