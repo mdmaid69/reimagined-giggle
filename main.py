34d40968d63749343c46a7bd0e418bb460b911ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:01 +0000

    rest me geton transparent comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
