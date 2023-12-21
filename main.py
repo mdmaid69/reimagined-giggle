dbe4062f34d8ab179603831b4f22bba433b6c3e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:12 +0000

    their adjective shallow trap murder

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
