2626d8222091ed41dda7ad90f2858a66cbe5ff7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:31:37 +0000

    satisfied brief shame stretch evening

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
