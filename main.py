389ca842164f4b6f8823642e9f9b665ccbae0b51
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:41:43 +0000

    extremely lookafter approve even may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
