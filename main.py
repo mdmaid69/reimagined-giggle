eeadb865055d91c5cae616461525e8808f9cbbf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:08 +0000

    person giveaway stop else uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
