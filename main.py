822bdc3796cdcc99bd4d766f801bdbb5d8623a09
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:20 +0000

    damp turnout opposite takeback mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def clear_array(array):
+        array *= 0
