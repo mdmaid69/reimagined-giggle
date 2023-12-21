b74705a228d3aa3491e551cb886fdcdd1d4d949c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:41 +0000

    hospital at prevent ill bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
