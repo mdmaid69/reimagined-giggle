32b223a16c04cfeb98c80fa44f022e376a88c5e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:56 +0000

    original address murder meeting comedown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
