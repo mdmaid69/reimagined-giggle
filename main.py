26e7b274b4fa3dcdb1d53e2193ae15cfd052e008
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:58 +0000

    apply generous influence spot team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
