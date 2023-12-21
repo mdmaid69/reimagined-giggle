16e9ec397438b48220d62a53a9f6c64420fe0489
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:19 +0000

    high fight tool include sail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
