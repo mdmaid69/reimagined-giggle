f01a04feda2daee4a6a74ad72d98ae64b344d5b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:02 +0000

    strange government hit lesson leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
