d8cfc567ae610f510011aa5274cf7febbeb3e091
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:48 +0000

    spring lucky force Gg muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
