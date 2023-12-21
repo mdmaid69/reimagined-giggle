f26af24d8d918cfec980da0b14a50d543863f6e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:23 +0000

    edge ought joke tone steam

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
