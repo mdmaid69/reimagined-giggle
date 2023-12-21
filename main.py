b89504c9899bf73c78a25f483b29bf2cfbb392c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:07 +0000

    mad feel succeed deaf bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
