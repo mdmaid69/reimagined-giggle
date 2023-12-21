1bc12c210bf096715d57b2a0f7353ed343de9699
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:33:36 +0000

    typical pinch murder Oo language

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def is_odd(n):
-        return n % 2 != 0
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
