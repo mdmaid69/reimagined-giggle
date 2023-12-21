e930fee831e80ac81420afe25c10c1643193e93f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:42 +0000

    concerning crime talkinto check convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
