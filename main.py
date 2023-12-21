3fc03b5f4ad8ee1c8d52bed3ce9eb3f663d7f722
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:23 +0000

    force yesterday announce home exploration

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
