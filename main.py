79243f3508a8993c84157ee08a5d3288920c99be
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:45 +0000

    trousers dress lookafter earth plenty

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
