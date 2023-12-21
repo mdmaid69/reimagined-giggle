8188b33b8cdcad10e4e2cbd89e72c5245228eb3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:20 +0000

    hit city imagination meet 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
