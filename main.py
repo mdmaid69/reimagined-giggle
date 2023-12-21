e9cdc2573b2603917e80afea9fa69cb636c7ce20
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:20:52 +0000

    picture lower left serve hold

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
