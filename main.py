cd0ab123cd9c308305d26b128ca70e52e6f4800f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:14 +0000

    culture project number dream neck

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
