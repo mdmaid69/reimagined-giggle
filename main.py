a8ed289b9f12b5995c88ff97fc09853dc5ea3d3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:25 +0000

    home last appear 568energy upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
