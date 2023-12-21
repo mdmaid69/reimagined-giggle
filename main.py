f0eeb9f6ce6109b6a027eab88fedb7fee4c3d3ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:24 +0000

    shine example field spare away

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
