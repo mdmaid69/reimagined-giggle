beb143c49154e0d00dff29e939b50314f9cd2fcc
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:52:04 +0000

    build soon race conversation lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
