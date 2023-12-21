02a82125f654a0ed7ee729a25ca384c830cc2e1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:36 +0000

    challenge tomorrow black bin belong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
