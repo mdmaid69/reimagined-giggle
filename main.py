e5f67d7cf8baf1b2ae862a858b3e024aeac44d29
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:53 +0000

    warn raw grow future meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
