ebcfff1e497b80337c0d7776b637a2e7c5934abb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:03 +0000

    figure complete ball nonsense manage

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_power(work, time):
+        return work / time
