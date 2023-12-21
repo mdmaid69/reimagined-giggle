ba491d7928511554beeada28ed62deed95165ff7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:41 +0000

    difficult entry pinch rain damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
