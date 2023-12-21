bab33ec41751d3eca313b86516e1ad36d90f7468
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:35 +0000

    able person book noisy substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
