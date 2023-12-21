90ec9f223969e3e818300b85e7c9f5ef8c02d060
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:30 +0000

    runaway detail clear opportunity run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
