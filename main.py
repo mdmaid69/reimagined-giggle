c2e5daa00d200fded4240816bdf9726515614197
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:48 +0000

    Ww joke prepare blame spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
