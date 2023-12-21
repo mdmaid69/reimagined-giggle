7b6c9da6e33db0d70397930e9ab2b305bbb005ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:14 +0000

    urgent comeup male active this

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
