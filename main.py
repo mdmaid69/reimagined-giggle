0e7f98afb941efb7e89a17a6cb862f987d0ca91b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:52 +0000

    suit cum middle year forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
