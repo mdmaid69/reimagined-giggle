049468d4deed34e6c2ab0e3b9e80ded76ae872b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:23 +0000

    ocean frightened ever unpleasant exciting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
