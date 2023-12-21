45c34bb0b78962846048f34a27649d6c532162ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:20:24 +0000

    quiet naturally clothes light flavour

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
