4408a4017e594b6d0f3be30126d1a21c96a23c23
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:38:37 +0000

    limit consist far switch sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def multiply_numbers(x, y):
+        return x * y
