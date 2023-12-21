573e3f6663f499056ce078efd1c2bb33330cdbdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:56 +0000

    zero rent disagree lookup temper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
