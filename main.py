e1f19ced448422a9a441243938bd851ad2904ec3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:22 +0000

    operate can thinkover progress weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
