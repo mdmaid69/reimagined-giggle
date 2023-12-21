fa40d7a8b33ba423984e23ffbe0cf6b077ccc1aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:52 +0000

    holdup circle path weekend walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def remove_duplicates(lst):
+        return list(set(lst))
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
