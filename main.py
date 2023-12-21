2705983fd2ef38a3db186c7fa4ad77e7e9eec658
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:28 +0000

    fit rest dull none fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def set_array_item(array, i, item):
+        array[i] = item
