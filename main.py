3008b1582872b1422ecb5d166c20414b33eda39d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:52:30 +0000

    weekend unlucky Qq doctor off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
