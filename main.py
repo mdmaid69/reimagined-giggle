3296c4cf47ce4ac711f3448bef5d5d5728f81b86
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:15 +0000

    fair pretty goodbye could course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
