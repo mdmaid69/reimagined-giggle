7fdfb9179c9b21b445db881fc6d5e4dc187ee8ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:08 +0000

    apply visit vision half after

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_speed(distance, time):
+        return distance / time
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
