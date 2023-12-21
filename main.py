a15a8d9e4a76ad7ff133501cb8dc88c1c20f1c3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:43 +0000

    reply mass part fortunately size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
