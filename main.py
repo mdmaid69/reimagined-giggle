470542fb0b8361fb2728aa7c51c8814637bb53a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:52:50 +0000

    pretty immediate light reaction order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+print(sys.version)
+def find_max(numbers):
+        return max(numbers)
