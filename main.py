41f3eea46d2f92393f7fbfbb1cc176a7ca2ca6ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:23 +0000

    habit freedom Ff such still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print(sum(range(10)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
