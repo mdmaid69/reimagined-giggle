b317bf0911c16ce1246919fe41586b1dc7616e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:19 +0000

    knock simple thank breakout angry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def cube_number(x):
+        return x**3
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
