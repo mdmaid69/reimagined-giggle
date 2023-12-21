3d79564a809e2fd2f1d36ca080962200ce6dc006
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:36 +0000

    obvious put limit alive draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
