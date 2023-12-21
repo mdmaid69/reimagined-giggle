9bfc5283048ee46b99ac20aa3eea13ae91a59c93
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:21 +0000

    tonight police will development design

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_current_working_directory():
         return os.getcwd()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
