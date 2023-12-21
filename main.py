7280ae48972d087fbcd98c15bdb533022d57d2fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:29 +0000

    reason anyone exaggerate experience disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
