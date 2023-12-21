dd321c3dabca94e29db86a46fd60712ad86c99c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:53 +0000

    figure tooth call either interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
