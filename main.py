40e2ff994d65cd2f2ca8743f8adf86ef82a486fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:24 +0000

    once speak crash please interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
