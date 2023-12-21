c6cb9e5287732928a6e6a3e1ddba3bcf33d97d52
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:32 +0000

    pay car consider burn build

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
