32ab18b21a28e4da198999ea6ea15c7914ac484c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:04 +0000

    fit disappear claim cross amount

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def convert_to_binary(n):
+        return bin(n)
+def find_min(numbers):
+        return min(numbers)
