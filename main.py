4f83b46f707fc564ede2d1ca3746df591b0031ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:48 +0000

    side our detail switch safe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
