63f1358cfc9133bf2fbb6cce8ea7900776aa2ab6
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:27:11 +0000

    reject radio climb runout getout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
