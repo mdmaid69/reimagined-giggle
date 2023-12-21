2928449546845bee04a4b8218227d9cc3e6ea7e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:32 +0000

    high project artificial hide suck

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
