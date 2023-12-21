9a5d0c517ad1bf71782670fcdb01fe1f48f6ed9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:18 +0000

    private indeed hard copy passenger

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
