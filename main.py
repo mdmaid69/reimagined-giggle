0197e54a95fb3c8e4cc88948e67ba95cce021377
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:48 +0000

    after argument interrupt meet shade

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
