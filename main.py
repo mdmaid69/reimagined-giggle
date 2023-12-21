a5965054d7a68573c21ec9d407732b4cdccf657b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:57:49 +0000

    top left workupto check stamp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def convert_to_binary(n):
         return bin(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
