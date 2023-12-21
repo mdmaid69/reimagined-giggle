b0719c79cf858934d7b3f9ade2b3a9f468c6523e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:07 +0000

    strict along waste call glass

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
