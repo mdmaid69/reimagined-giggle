1aa29d99d26c8723c34ce7d950b1ed59de45353b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:05 +0000

    brush could proper lot carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
