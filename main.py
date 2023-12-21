bb2dd1dd3c46e74f8caf918cbfda11a21c61ede9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:05 +0000

    land mark touch successful excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
