fb41d7ed8a099122d42b1005cd319cf0c22eccc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:37 +0000

    smart cap object second base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
