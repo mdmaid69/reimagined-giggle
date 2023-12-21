a661116eaef8ebe5458cd6d48644b55199686705
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:51:44 +0000

    if knock hit go quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
