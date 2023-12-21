a409a1a14614c66025b3c89f83b3c93c9e016701
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:29 +0000

    tooth callup match translate callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
