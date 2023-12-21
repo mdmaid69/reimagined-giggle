a3112ad7093acbf8c7058addfb0406b2e543fcc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:27:27 +0000

    tax fun positive left about

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
