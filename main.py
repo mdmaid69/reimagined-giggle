19e044a9e05a2d76d3e6b7e27e80f4935d17b309
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:02 +0000

    name smart manner priority public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
