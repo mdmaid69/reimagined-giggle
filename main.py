6a41c88a3cdf9b00e2c77707e785ee693d15ec7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:47 +0000

    greeting sensible blue on harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
