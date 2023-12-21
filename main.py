014c1338b288c93c0c183eee2a3488737682bda6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:54:38 +0000

    unfortunate charge variety which keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
+        return list(set(lst))
