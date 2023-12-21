5dd0e3f3f14c9e6212270a474029d8626b20c113
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:08 +0000

    other circle keepup rule guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
