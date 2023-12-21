b823c9e1dab15969f4ba41848d42337252d3dc20
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:22 +0000

    noone check somewhere increase cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
