1fbee97530475ac791333c9adddca66ebe1f2b2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:15 +0000

    front number buy method tune

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-print("Hello, world!")
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def subtract_numbers(x, y):
+        return x - y
