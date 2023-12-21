7ba3d40b1076f26d4c68312ecd3e4d66fd9126db
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:38 +0000

    please equipment consult position by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
