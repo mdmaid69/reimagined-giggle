ef85baaaf70a38ee913658a540b83ea3b678d2ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:14 +0000

    exploration tomorrow copy expression use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
