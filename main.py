cc6336e02bcb505a873a90d30945d3e994fb3009
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:20:19 +0000

    arm develop shock earn deserve

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
