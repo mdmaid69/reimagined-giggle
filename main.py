7a5b3a29987b14def343d057199c3a3a7933068b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:57 +0000

    sort crowd afraid war light

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def is_odd(n):
+        return n % 2 != 0
