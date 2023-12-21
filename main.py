b79b179c91eed5811ab13ac0df34208ad57c5ed6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:29 +0000

    unpleasant remain injury choose beginning

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def multiply_numbers(x, y):
+        return x * y
