7a1a42c87cca86970849ee5848493e59677e91d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:07 +0000

    possibly pass fortunately table criticize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import random
-print(random.randint(0, 100))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
