d2c9ba18d69c3a41488dae5c5a5ca4a9a9bef59d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:03 +0000

    aim rely interest belief suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
