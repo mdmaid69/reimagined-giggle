b100a6c74321edcf81d53db8cbf1937eaeec2e0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:36 +0000

    opinion fond shop track personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
