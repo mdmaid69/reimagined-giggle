fb07d4a0b83b94cca8edf956c131a0124164487c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:50:09 +0000

    order her else comedown shock

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
