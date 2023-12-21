dbe8a9feb261f1bd4e2a516a333481994d888c30
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:20 +0000

    occur charge habit check file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
