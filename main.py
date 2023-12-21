988ca7a3472785323f7ef79fc2895a513bbeb512
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:31 +0000

    nonsense report read quiet reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
