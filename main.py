0a81a2b2924da9f2091f7122cec7abde0e853e88
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:58 +0000

    unkind principle Pp breakfast duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
