ee8670e0e5adeb475567e6cbb81d1cadfe418874
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:47 +0000

    about invitation pass bet full

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_item(array, i):
+        return array[i]
