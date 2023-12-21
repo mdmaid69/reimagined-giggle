347bc7d6050c2e40b997a939df24c96b6fbb9ff3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:51:09 +0000

    command yours care feature stiff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_complex(array):
+        return complex(array[0])
