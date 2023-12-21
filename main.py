9d817228698662617956c0d397a921d2a4bbbbeb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:23 +0000

    judge pronunciation busy violence pick

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def append_to_array(array, item):
+        array.append(item)
