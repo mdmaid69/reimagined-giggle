5f482b5cd147b045a4afa542b697f6ee866cf8d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:35:59 +0000

    embarrassed risk react floor flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
