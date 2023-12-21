bcacd805e9094653018533111f3d5f3237e1653a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:50 +0000

    tell flood his the better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
