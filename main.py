47c6fb33adefb5e1c947116b395063aebb687d86
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:30 +0000

    mail substance habit needle dear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_list(lst):
-        return lst[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
