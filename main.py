50cc6eabab2231495ed8267d36b3b4c7f88fd633
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:33:41 +0000

    direct near legal workout earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
