ec9e1218ce9c4b4b4c39bb57592eef5b3a536ae5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:12 +0000

    disagree himself twist result achievement

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os
-print(os.getcwd())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
