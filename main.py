fb93ab6fd75c847540b740627d3f9231dc85b707
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:17 +0000

    later dig teach principle Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
