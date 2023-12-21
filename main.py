8019fc3592f4fd503beb79410e3148410142baf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:31 +0000

    rush rob dry air switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
