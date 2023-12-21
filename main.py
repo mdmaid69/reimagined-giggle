423ffba4f57ed8ebebe463d7aa16b6c3888fa127
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:26 +0000

    eager occur keepaway reliable order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
