9c3e4b0be5e50f531cea57cbb2c8c0895864d089
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:45:40 +0000

    level emphasis put tend home

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
