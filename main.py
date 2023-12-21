0f623c99fb356ac0206074b7b6c016b89afe11d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:26 +0000

    at appeal clear scheme remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
