c06c3347d1e7443c82f024e666dafb81dd570b4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:24 +0000

    fear getup road dress feed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
