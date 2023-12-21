7de01050cc8ac247a545939cdab7d3a23281f75e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:39 +0000

    like lead sense block give

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
