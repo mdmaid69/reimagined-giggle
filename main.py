344131d0997b2345a75fe64d18725bd7dec9850e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:37 +0000

    let change wait live tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
