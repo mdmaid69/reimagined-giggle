edf038ebb4d1a80c459c753de91ef8d36f37174b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:47 +0000

    land difference middle complaint remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
