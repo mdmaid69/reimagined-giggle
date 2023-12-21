47ae37b272b3f25ffba73e30df6caa219594ee73
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:05 +0000

    male interview convincing must holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
