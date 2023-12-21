e89fcf43f69052581dab6f332484ceeab4b63f5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:16:55 +0000

    study tax some fun search

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
