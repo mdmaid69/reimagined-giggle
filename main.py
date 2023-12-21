6bdf46dff75a78f03ac4ad43db7109a998e2c1b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:57 +0000

    mind rain exhibition yet scream

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def square_number(x):
+        return x**2
