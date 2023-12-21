9fc6ff0aad56bd2e7822d3355ef3c7a611cd0ce7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:28 +0000

    allow schedule unit tap react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
