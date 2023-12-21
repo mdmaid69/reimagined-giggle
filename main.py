bf98a4fbe96a5b24ea2fdd185b86dd58bf59c751
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:20:43 +0000

    offence previous example understand glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
