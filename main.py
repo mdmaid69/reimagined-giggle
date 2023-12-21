558d108a2324fa0c3ead9f276baa1194fb332a37
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:04 +0000

    return name guilty talkoutof please

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print("Hello, world!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
