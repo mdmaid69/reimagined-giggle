c8a51faa96329f22401b6cd686f0ede746730217
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:36:15 +0000

    flow question satisfactory national interesting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
