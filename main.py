b04cbb6f585eb8ee5e5054aa11e9ed7a8e9afa20
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:10:06 +0000

    ask curious coffee certain scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
