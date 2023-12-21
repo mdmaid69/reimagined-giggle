e37d9b4be3ea180344141f7fa87f5719815617f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:58 +0000

    taste wheel stare sensible protection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_cosine(x):
+        return math.cos(x)
