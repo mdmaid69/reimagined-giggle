68f36a3c73beefac83e6c0b6ae4904c51b86a92f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:32 +0000

    near marry poison getaway wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
