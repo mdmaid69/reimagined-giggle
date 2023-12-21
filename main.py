da568e6a230cbaf3ca1b66f8f8b59080a1ff0c92
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:49:23 +0000

    picture final display stand tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
