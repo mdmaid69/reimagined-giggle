7470c100e6874303596669cbe0a212c9b3d01c5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:59 +0000

    completely centimetre unlucky battery comment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def convert_to_octal(n):
+        return oct(n)
