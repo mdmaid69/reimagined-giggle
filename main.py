a0bf0f61394214a517525d9f906b6d9788b3e61b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:57 +0000

    art show invite body rush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
