951b098cb56a236b3ce4e3e2fa8d03592b590df7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:35 +0000

    pin marry touch about temperature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
