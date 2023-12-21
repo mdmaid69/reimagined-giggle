b9c34a911d9a776d8848fea31996179fa8578b03
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:08 +0000

    shine familiar trip operation mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
