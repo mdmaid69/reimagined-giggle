6d2bf38b95dbf04491d2676dca315f9000ba417c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:54:56 +0000

    human gas no fit whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
