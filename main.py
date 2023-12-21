835e66dafcd9c2a71541b09d8556f01236ceba2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:26 +0000

    meal school condition interrupt area

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def convert_to_octal(n):
+        return oct(n)
