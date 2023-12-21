08318716cc016929f5848f9b1be95215223c8bdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:39:04 +0000

    true play generous tidy settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
