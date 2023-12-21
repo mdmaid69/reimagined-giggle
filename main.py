61e747ba6ee7486793421e03770d8b5babb45e05
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:48 +0000

    walk present frightened gather otherwise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+print(os.getcwd())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
