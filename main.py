7155c63fced1b07ed79d179523f5d822637b7e06
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:12 +0000

    plan thinkover development often load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import sys
-def exit_program():
-        sys.exit()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
