78c00da589c9f06ea16f0f04880107a03e8b3a3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:04 +0000

    base spare keepoff beneath answer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def square_number(x):
+        return x**2
