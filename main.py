d6a27707f16bbf87d6abf6b974cc025748325b81
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:06:21 +0000

    sound replace workupto touch extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
