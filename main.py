a07de17c88f1a957fbf7c6041a7340d7d20c3328
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:46 +0000

    poison repeat giveup emotion music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
