842efa5a3db3d3ab91fa694f538788f937fc24dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:49 +0000

    home hang even threaten forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
