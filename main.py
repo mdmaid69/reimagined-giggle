c3c57cdd1737ef710dc4bc78ea234c6362421c8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:01 +0000

    frame anxious meanwhile pity error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+print("Hello, world!")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
