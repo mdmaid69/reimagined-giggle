7392a39f4c53d0147901a23152f0b31d78031a92
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:23 +0000

    area right gentle frightened tonight

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
