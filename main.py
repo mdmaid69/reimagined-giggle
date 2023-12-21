b43021a1c7483265db536beaefabea4cc10ce5f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:51 +0000

    accordingto dance education earth over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
