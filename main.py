3e1fadb7953c05c1dd90b2bc8962fbc3c44829bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:14 +0000

    food hurt succeed noone honest

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-print([x**2 for x in range(10)])
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
