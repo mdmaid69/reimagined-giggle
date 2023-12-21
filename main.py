12f80c377a112bd8c6149480a551651ff3373e81
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:39 +0000

    rid home container spring squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
