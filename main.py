5e64e2ff311e22d2e4066114e2c231da8381b0f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:22 +0000

    skill ordinary generally explanation acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
