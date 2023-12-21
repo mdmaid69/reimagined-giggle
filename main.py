f487d6fc524146468db6b1f550972ce29b7d2c9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:15:20 +0000

    exchange grade there will instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
