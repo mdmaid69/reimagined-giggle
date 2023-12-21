a1ad4760e99cb1977b212ddbd61d172200472872
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:07:49 +0000

    train dull swim breakfast steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
