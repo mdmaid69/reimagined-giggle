d94258cd41363e2a2b0eec430183569cf1b0334b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:34 +0000

    become about protest in yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
