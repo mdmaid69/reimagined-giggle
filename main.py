4e3428c8fde1a18d313507c394374ff4ec23c52b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:19:29 +0000

    consist open ask throw underneath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
