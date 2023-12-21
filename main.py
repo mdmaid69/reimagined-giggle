d8010969ee2ad10e97cae7c3309e2e7ce1a85dbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:48 +0000

    wander shame mouth breakdown nature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def multiply_numbers(x, y):
+        return x * y
