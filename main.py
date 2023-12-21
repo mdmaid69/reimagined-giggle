abf20082ae0704a33d3758a9744e9b2dc6faff21
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:17 +0000

    opportunity inquire bad finally tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def clear_array(array):
+        array *= 0
+import time
+print(time.time())
