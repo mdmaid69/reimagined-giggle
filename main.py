9452db8de8f1334d8813740da6bdf3082fcbf5f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:33 +0000

    indeed related learn reduce cut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
