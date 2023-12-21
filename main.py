9d88a9dc33d18924f18da47248a09ceb7e0077f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:31:20 +0000

    hurry cool creature manage waste

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
