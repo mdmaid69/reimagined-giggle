0c2c0e82c579e47a1636c7f2521d5aa1fcdcdb7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:58 +0000

    imagination 569enjoy advise onto willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
