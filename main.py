7feba99260fa5dd6ba72a4a49ef8a7149a0b213f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:18 +0000

    goout forever game discovery care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
