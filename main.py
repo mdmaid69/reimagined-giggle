47cc65796ace5e6ab2fe7320ff9b1288a556e82b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:52 +0000

    adjective test plenty set danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
