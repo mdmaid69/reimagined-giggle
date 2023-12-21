1f42f5d239c6f392720e4d0b8b76ad2523b9cb0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:53 +0000

    drop fight possibility concern tune

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+for i in range(5):
+        print(i)
