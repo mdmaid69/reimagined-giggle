93ac690bc6d079c7977a003d6ce7a1bff2b9b3a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:39:39 +0000

    delicate onto Kk mention community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
