62455aa11488ea39b3640992abdd4f2a58fdef65
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:17 +0000

    never workupto set instead noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
