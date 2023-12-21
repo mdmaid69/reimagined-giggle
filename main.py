ceeff7c0f42bb94dd86fb2a41ae84586f7f94ec5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:52 +0000

    pool major be emphasize private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
