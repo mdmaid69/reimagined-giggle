99c589004fadae104e12179854d6c9c49f2fe9f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:50 +0000

    hurry event large knock to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
