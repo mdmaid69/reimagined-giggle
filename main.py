b3c899101a2468b849b928327e23002a4cd514eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:52:25 +0000

    between that tradition time choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
