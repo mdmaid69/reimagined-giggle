94087915c6e682268eb456bf42339e8ae16c3c7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:33 +0000

    treatment mood matter opportunity keen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
