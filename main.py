3c908e1542f790ed9c661c79e246774d4712c7e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:59 +0000

    serious hope impressive nervous short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
