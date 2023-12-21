67fd1ba4045381e8a3c60b66cf5b4f8d4b73215a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:51 +0000

    precise least rate score door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import sys
+  def get_python_version():
+        return sys.version
