3b18f38f706907268a862e9f4b120c6400ecf317
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:08 +0000

    whisper clean public comeover its

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_area_rectangle(l, w):
+        return l * w
