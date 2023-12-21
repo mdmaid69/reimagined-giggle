72bf92751e61504938dd411d02407134d2e09e4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:19 +0000

    authority minimum system open include

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
