4a76ba8ed54e44af5690dc3d4575ab1203361266
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:19 +0000

    national swim can month spill

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
