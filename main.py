30201ee1025e40dc0530db41cda1b385648c1148
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:14:40 +0000

    game considering priority fire crowded

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
