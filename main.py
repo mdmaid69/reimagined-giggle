9185aa926787243f597497808dc39be39f26029f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:20 +0000

    before cook regret bringup dance

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_volume(length, width, height):
+        return length * width * height
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
