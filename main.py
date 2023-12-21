c4236ad6d485e4b82d9889a1da3cc17f782fbcf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:24:29 +0000

    welcome complete cover face nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
