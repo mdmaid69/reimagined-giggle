d1736c0f62ddacb7003aa21e305762651e805688
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:38:44 +0000

    mess depend lose score long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_speed(distance, time):
+        return distance / time
