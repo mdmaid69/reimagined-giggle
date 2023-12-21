ffce489be4c141e3c43cec2fed8d98e073e5ff4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:19 +0000

    unable what expression family suckme

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
