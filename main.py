f4dcab638e195beb511e9a1b85df7d629f0869b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:09 +0000

    turn patient ignore out joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
