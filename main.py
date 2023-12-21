92ce3839c92cf8e49f21d5e46e65e54d327aa013
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:09 +0000

    hurry ago welcome touch rush

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
