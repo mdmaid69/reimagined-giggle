f2559734cc4efd288dba0c7dd13ebf5c45150e6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:47 +0000

    decision door future interested complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
