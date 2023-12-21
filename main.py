b7bf27fe42d529579f445a53d76989c6deb5f6b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:42 +0000

    protection shock conversation pattern holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
