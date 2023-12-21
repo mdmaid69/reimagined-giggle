260db21f7e8b0ccb9dc8525772c0d97820a6cb80
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:45:51 +0000

    bet special material memory unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
