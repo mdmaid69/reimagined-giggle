d5dc15e5185375f1df8babc1f7f36f558495b45c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:51 +0000

    consult remove where back care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
