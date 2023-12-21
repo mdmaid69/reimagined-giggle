bd287ab7a6455210c9e7beaef2ebd5817585795e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:13 +0000

    although machine mess uncomfortable smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
