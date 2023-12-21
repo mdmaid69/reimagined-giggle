63e7de9f71e3391aaa7ea9f334d7e48eebeb1152
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:18 +0000

    study present succeed care money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
