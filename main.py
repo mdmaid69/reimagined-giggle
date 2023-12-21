107a5e752e119193eb8dde23da08295777fa52fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:50:42 +0000

    raw cry break of brush

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
