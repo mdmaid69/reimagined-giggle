7946f80c29528ca5dbf2ee178a532525340d3c73
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:16 +0000

    nose punishment story partner frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_int(array):
+        return int(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
