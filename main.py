bd954b9cae5c634e1760fb6d4d9881606c3a924b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:31 +0000

    street congratulate theirs only unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
