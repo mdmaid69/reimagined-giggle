b6bf701d3dc0487ef881bd2e3a8df5da105e9e6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:20 +0000

    project anxious another chase ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
