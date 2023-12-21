33508f7a10ea405c4d0fff7dd8e6653b0c954080
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:41 +0000

    without their smoke charge sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+for i in range(5):
+        print(i)
