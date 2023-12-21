2308c4b80ee132630171ad3c5740705879ad8eed
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:12 +0000

    floor blind heavy fall sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
