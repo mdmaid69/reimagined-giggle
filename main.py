6fc8e6f9fcd8390f3b6424463d9b54943e1e813c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:56 +0000

    count turndown correct concentrate Pp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
