70597f9cc7a135f26d18514a280b95f011c375df
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:42 +0000

    trick answer fork generally urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
