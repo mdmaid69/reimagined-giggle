d8d6726f36666dbbc4bac828801a1f2cb29d6898
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:11:44 +0000

    translate address comment emotion scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
