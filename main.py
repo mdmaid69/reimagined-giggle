f5e1b13bf4bdcb3634b1cd03cd40955b0937eb5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:36:08 +0000

    trick somebody cross judge nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
