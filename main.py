17cdd2892918a8180b2f6452be65f5a06e99c96b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:27 +0000

    rob rarely concerning so appeal

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
