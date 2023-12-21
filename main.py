575f97d628a0cce502f2eac6b8036832e18104db
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:40 +0000

    thirsty thumb face of theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+print(re.match("h.*o", "hello world"))
