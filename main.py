600d01460c6a37a9bd310a2994cbc3d8e6797118
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:14 +0000

    passage argument favour poor spare

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def cube_number(x):
-        return x**3
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
