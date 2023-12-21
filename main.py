e4fefa7ced74f805749ad4f5fae0aad44d88a38a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:19 +0000

    damage sink possession hate no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
