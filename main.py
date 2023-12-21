2188add31b5d3b366be663b553f5f827c9424321
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:27 +0000

    hello meanwhile usual band decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
