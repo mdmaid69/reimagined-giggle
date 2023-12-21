bf719d9944b552ae8bab61fa18670b94817fd3dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:33 +0000

    dead takeback speak exciting present

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
