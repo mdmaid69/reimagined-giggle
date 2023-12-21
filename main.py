3fbd1eed82cd490bb4ee1241745855a4d27576fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:44:55 +0000

    basic bringforward embarrassed loan pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
