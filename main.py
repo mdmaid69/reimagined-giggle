ae8bfb133165c482238af24d9b67f0bebc6e195c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:37 +0000

    while hurt or string lie

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
