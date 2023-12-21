3dcdeaab7215a2e0e82275fc31e42f088c96e748
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:51 +0000

    hit weekend mean spring closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
