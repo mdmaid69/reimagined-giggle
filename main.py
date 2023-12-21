b1e9d7a671439b1d07446b5f5b79af94ca62bfc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:38 +0000

    sad actually Oo mine steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
