aa4fa57272601ce2be15a72c94b1d00ca386d64e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:10 +0000

    war spring recent refer boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
