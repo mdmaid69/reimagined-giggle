e1642cdd14df0f6d4a7eb4b2aa66bb1fb9397f4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:29 +0000

    shock start deliberately connected corner

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
