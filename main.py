7cbedd3e8cbe6d9a144ba29e16818d9b180b79e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:08:31 +0000

    consult muscle avoid moment praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
