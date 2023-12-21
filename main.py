4e87bce4e191647bbbd8b01e34d95c874bedd7ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:37 +0000

    crowded slow ring experience we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_exponential(x):
+        return math.exp(x)
