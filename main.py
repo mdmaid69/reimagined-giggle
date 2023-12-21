73d0a1ed60e0f10522ae52f484ec4e08eff633c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:43:24 +0000

    during doubt it fence complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
