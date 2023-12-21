847bd57af1e463d777d3d5cccdb8855496a9ee08
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:33 +0000

    runaway crazy thirsty poor check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
