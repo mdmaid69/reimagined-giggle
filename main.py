da6770c63868ca252b3ee7391b4c8b367a7a92af
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:17 +0000

    protect correct earth dance lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
