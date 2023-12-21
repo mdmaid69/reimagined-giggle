bce43c4d599ef1d7d4938ccaec186f9377f3496b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:24 +0000

    peaceful pool sticky brain fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
