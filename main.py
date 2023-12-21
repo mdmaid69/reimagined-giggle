0dbc669c74a65b99e0160221f51e844c534cb7c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:13 +0000

    complicated lot now back opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
