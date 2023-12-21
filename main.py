fc7ce457158cbeeec1a257ff9ad09c6c302cc986
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:01:43 +0000

    generous safety content steep female

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def convert_to_octal(n):
+        return oct(n)
