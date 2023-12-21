ea5eebe7bab204b917213c2b9dfecbf6c1bcd8cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:38 +0000

    greeting beyond mad frame conclusion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
