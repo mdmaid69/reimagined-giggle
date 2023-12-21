27c0718c8269c56bdd070c20947e55e9bc786048
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:16 +0000

    range pick minimum Ss direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
