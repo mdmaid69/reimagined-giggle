033ba7df8ba0385d1486620cd8295010b050f1e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:10 +0000

    discuss appearance grade radio lesson

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+for i in range(10): print(i)
