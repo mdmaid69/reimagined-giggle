afb1940b6a05be397cc19d7f47e224254c48551e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:31 +0000

    previous transport correction Kk otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
