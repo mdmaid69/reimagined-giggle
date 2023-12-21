755834a8cd91f35eb72510318d82e12a90de262c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:39 +0000

    like mend basis experiment length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_typecode(array):
+        return array.typecode
