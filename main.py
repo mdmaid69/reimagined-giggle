4476e0ef865c300fbc730dcce75ae011d5f7cd6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:43 +0000

    suckme connect and ordinary breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
