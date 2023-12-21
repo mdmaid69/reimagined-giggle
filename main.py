98af9296a02eac68bee2014edc1e78178a685b5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:04 +0000

    send trade negative confident window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
