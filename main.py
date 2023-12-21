176e4e9ca9af007de1fd86e26445ab7d429edb68
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:18 +0000

    national shape guide shade drive

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
