1539b4ab435f564eb5c75a9589a2aae9a86e483c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:22:21 +0000

    theirs operation tooth increase progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
