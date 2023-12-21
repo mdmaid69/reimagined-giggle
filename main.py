b34890eb2bf03d7d6eece2c21c308f9da39da863
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:24 +0000

    music confuse giveup afterwards offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
