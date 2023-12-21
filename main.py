eedf58a55a79f57c877571905a2560e52c5e186e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:38 +0000

    drawing environment image ocean goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
