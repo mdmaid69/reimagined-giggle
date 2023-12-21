45014eb142a5a4388326c0311aab7504c50e04b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:06 +0000

    still church deep exam hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_environment_variable(var):
+        return os.getenv(var)
