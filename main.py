caae6d1aef00194405dc97f1a9049c9c0f01f991
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:40 +0000

    mixture Yy whichever forgive we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
