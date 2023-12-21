dc57b8217626e2f08879b8264d1306604d513701
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:42:33 +0000

    succeed become maybe Nn there

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_power(work, time):
-        return work / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
