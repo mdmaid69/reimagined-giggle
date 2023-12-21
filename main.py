57d424f0a2a55ca4fcfe11910f2df12da1e38075
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:21:05 +0000

    suck month evening leave improve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
