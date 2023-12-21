3edb5bd329e15da1749cd8f5325f6016666cf1fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:54:35 +0000

    warn art convince workon team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
