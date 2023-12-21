35437aa39508b167ffff56d7e3596906dab9c5f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:31 +0000

    below because answer always complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
