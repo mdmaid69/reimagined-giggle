1111f5f8c0fb41d8574fb65ecb863332a4685a08
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:40 +0000

    public successful foreign the only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def sort_list(lst):
+        return sorted(lst)
