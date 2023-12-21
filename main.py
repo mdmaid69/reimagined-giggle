80628a332118f8a094a0ec5327d6d837190ea99b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:56 +0000

    university off pay early worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def convert_to_hex(n):
+        return hex(n)
