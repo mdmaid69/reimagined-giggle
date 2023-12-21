660f9246693b43afb833b449503884b256e8fe9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:53 +0000

    main wake me sure standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
