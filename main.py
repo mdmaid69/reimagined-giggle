318f26a3049a2f28b87114a51d949ff93f0afb60
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:07 +0000

    when test soon hurt lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
