5e4ff2c33c487a05cddaeab3a893f5a5d8daff04
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:40:56 +0000

    culture face ours duty passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
