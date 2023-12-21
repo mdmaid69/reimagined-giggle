6694bac113305b2c70f05f428568e5db2441ea45
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:17:52 +0000

    club convinced serve as seem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
