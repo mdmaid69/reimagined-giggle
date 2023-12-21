f40b10e9a5b56a37ccb08b105303fad2e50669c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:24 +0000

    face bang goback(to) minute certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
