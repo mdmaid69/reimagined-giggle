22d737158fd6bafe29c195e413dc297950d2806c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:52 +0000

    invitation field please prove be

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
