c91c0fec394db171614cce46b85f2e10a46cbf12
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:03 +0000

    quiet repeat consider black watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_odd(n):
+        return n % 2 != 0
