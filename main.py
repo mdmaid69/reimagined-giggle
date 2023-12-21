092c5da79dd26a4b85b2e50d830d86eda57c254b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:39:05 +0000

    trap pain bringback fit play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_str(array):
+        return str(array)
