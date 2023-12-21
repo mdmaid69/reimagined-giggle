8bfc2ce44aaf21b331c5a95bdd8143bb7ecbc2a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:30:25 +0000

    escape word build bit design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_item(array, i):
+        return array[i]
