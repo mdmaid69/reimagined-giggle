a98fa159995a211aa5c0316129ae051019965a9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:48 +0000

    discover excuse any pick start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
