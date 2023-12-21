d4dfdbd60569e99a372d7ebb0d6153ec8e39a679
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:55:07 +0000

    man future equal list wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
