d83a511b19ab243e95157189fdcfe36a9970f16f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:53:54 +0000

    quarter front measure long alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
