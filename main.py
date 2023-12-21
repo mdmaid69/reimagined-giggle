8a573386a40c80990332e13c393c23c4b618759a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:10 +0000

    lower address train distance break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
