c90dc13b8dc80511c5064d7ba9713c06841bac66
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:22 +0000

    read he nigga rest certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
