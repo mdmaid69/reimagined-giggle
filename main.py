131be15730914dcad8a8a1491645c8514fee936c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:09 +0000

    contents weigh trip wear indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
