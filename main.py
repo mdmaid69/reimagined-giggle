5a4e35eeae059329997893cc9796a0819d988a85
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:31:19 +0000

    minor house speak Oo hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
