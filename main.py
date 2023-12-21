92eec18de01f5faa8dd24292ea6da607e6f4857a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:40 +0000

    lead spin impatient brother pronounce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
