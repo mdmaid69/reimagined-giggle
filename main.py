a6dc9e9d623204eca94cd0ae7b0f18929db6eda7
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:10 +0000

    terrible top cool carry discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
