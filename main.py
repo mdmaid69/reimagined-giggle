93829edcc092a4ce7cfd7b8bfa850167385d063c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:08 +0000

    exhibition laugh clue fix bang

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
