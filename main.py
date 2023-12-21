f46d5cdb6c0e8234ae27725fe625b25055527d19
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:22 +0000

    love throw walk major instruction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
