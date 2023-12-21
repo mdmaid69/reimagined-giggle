dfad657147efd7451a4ae49454f96835d62c55c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:44:14 +0000

    lookup coffee could history recently

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
