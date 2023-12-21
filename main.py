bbc5eb656a7eb51d0eb8857d120a528b536e7dd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:31:36 +0000

    body command design runinto door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
