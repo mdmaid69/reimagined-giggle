5ebaf73c42b26ac07b5303233384ecd66193072d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:11 +0000

    upstairs exist describe delight black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
