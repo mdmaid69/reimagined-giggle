e0659757fc5c7cdf5f9a218e6456c4d244f6e9ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:54 +0000

    case secret waste emphasis forget

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
