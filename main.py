a540367d6e8780634551630b575d1e2c1def6000
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:36 +0000

    prize grand exaggerate work foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
