41856757179219a56909690b3819878bba7421df
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:30 +0000

    nearly private correct big undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
