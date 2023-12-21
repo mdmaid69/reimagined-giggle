193d0bbc58a923caeedaca86a4870fcdd5c8a90f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:35 +0000

    film scrap custom never evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_current_working_directory():
+        return os.getcwd()
