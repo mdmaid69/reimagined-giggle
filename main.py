67b37356469b4c7a88a1459d4964e91d8bddde90
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:10 +0000

    substance herself scrape port mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def square_number(x):
-        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
