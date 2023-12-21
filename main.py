3e8d0c3915b77ee0540900768a85ddf2ec27cc9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:56 +0000

    cheat talkback tired surprising getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
