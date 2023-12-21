11ddfca1b6eddcd513d0732d8838a1bb25dc0057
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:08 +0000

    hide pass factory might gooff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
