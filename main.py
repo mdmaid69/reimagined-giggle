ef7726c098ae6f4e24538b9778632365ace579e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:37 +0000

    size out damp hurry reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
