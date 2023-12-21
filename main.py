f6dbfce1dc4b05bc4163a2f3fc016e71aa42346b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:31:30 +0000

    better memory rather several immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
