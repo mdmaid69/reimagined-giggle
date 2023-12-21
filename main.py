df2e7db38ad055bc33394917cb9874ea0086dc59
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:15 +0000

    runaway regret soil display absolutely

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
