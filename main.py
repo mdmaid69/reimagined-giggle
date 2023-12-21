2d62412c275cf2b44c52aa5dfc52a3f4bf7a5b09
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:03 +0000

    still square custom or discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
