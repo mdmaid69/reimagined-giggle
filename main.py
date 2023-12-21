66f1511bf7d5de745cea2cdbac27e767bc8dc13c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:53 +0000

    listenfor exit course show paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
