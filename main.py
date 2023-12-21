61ee0d38b992d0808450db5408f3d2e718e3305c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:59:06 +0000

    ready copy best scream treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
