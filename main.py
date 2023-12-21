7d3db40933ea753f6872d0a1042aa05702e797af
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:40 +0000

    may different reserve crash creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
