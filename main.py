331f66ae04260d79a9458c1affbc5ed91bcc3a56
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:35:59 +0000

    sudden bored nature present tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
