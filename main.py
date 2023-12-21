88ecec2e5f2682c3b9fcf64fa4d25477e00d5f3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:23 +0000

    conduct might admit store mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_user_string():
-        return collections.UserString()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
