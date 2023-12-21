e62b33c5ce0720f6e0106e5844c5edac5e95f2eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:34 +0000

    square after call badly fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
