f8e431c59f7049aa838283ea56aa877657671a59
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:24 +0000

    wait send call floor stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
