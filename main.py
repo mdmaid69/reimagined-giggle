3d1f3469da3668079f5328dcbbd9f862776478c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:39:17 +0000

    closed street wealth fair embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
