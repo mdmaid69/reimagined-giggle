048807716a0d3a3e3c7dc4606fad8c6086618bff
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:51 +0000

    getup because exciting crash press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_floor(x):
+        return math.floor(x)
