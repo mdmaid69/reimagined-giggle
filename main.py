516c72e595dcbfdff1d3f1485018a52b613f8f50
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:24:08 +0000

    formal laugh night thought organization

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
+  def count_elements(lst):
+        return len(lst)
