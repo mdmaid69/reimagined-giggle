7e611748fa436a0524e77d629729bbf40c34766a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:52:44 +0000

    shallow grateful Gg record bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
