01a89a80a9c7a9f0dc9aa9c8ce8a9b5fd39c4a22
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:06 +0000

    finger how drink mess recent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
