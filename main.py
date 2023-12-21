7b9622e28ce48deb66bad65d09751114fa86c13c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:33:13 +0000

    cost against broad getoff nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
