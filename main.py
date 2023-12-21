6172727425ab033bc5c97ca01642f4e23ec38e33
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:12 +0000

    lean juice talkoutof involve confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
