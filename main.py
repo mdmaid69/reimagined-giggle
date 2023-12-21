5c1b0297c3c6f3786178f0cbbc5e76c3a7cb4bc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:42 +0000

    some professional art autumn slightly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
