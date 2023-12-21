19d7b25df1692a375a22e04dec5a20b7b385a0a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:56 +0000

    underneath comeround runinto wing ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
