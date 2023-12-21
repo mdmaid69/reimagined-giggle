ba30c4729e6c821b5ec67b8cf9efad61885d1b54
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:06 +0000

    kick promise creature send Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
