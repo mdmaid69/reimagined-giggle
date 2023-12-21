454b8761a15f58ebf70ba7719627368b4647da4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:25:05 +0000

    any behind argument upset regional

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
