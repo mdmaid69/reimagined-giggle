22a8d519f9f9453cc1de5e675d798f110fb6a839
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:56 +0000

    shape drop copy raise tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
