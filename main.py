7a540f3b28567a25b5f8c34171671dc24b0176cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:50 +0000

    remember bringdown trick round minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-print(random.randint(0, 100))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
