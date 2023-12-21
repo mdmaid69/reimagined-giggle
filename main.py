2b1259a24d0433d5fd0d096901c12b1fe9d03931
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:47 +0000

    thinkof big contrast bang place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
