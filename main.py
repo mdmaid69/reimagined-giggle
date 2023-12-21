08c61f1a66f13c0a685370b6f502ef70b5a0775f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:25 +0000

    rule praise music apart passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
