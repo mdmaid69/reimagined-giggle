7161f2d875c9d2fb2e349a716c2d0c938bc6dc18
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:58 +0000

    clever attempt shall gas quickly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_sine(x):
+        return math.sin(x)
