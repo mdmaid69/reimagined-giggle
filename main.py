4b54ae33fd7c693b27b5eb555b0ab08864ea5e04
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:25:45 +0000

    my slow bar arrange spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_error_function(x):
+        return math.erf(x)
