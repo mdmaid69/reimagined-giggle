30e94bede087c09ad5fd8d6f5cd87504315c54b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:26:14 +0000

    colour keep goup criticize both

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
