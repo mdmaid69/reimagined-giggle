29af762dc7feff4ec85274dc1aa0d2ebc8e938f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:13 +0000

    cool jump example trade base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
