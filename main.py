2364c7eea88a7e8c7446fe87bb5845d26401256a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:28 +0000

    confidence correct walk bunch produce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
