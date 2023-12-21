babaa88a128960222e0ea936e7e4316cc693f8d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:51 +0000

    loose what judge pleasure usually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
