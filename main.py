c27ecff5ae61e48f29ada30714b51c797e4f9d08
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:39 +0000

    this stupid positive getaway sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
