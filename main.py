27c3716e522a9485403c2a8f3258e8efb01bb7c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:59 +0000

    available unable unhappy match temper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
