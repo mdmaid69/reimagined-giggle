a9acd00fe61dd4172e3e8c94060997e73dca4a99
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:08 +0000

    base release left impressive dark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
