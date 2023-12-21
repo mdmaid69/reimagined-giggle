444676cf67592f1861a371848d51be96c250d9d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:00 +0000

    characteristic leave fix bring main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
