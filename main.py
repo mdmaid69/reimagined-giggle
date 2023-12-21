d21008571de329318ae05c0cf8c6693d15974749
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:04:42 +0000

    his roof importance repair gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
