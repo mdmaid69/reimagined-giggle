35029e26ab99f2ce92dbd53c38c14d8df0da1293
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:59:55 +0000

    their before tooth mad pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
