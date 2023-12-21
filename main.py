cf3d763eb3b8aa1b01638657eaa076e6fe2e13fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:51 +0000

    fast size godown figure protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_queue():
+        return collections.deque()
