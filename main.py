7293be900a9e1eae9099569be84549dfca18ae5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:00:33 +0000

    point upset but butter summer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
