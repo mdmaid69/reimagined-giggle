78b118791d80c570e0bd61d27b398eb59bf62e40
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:17 +0000

    always drive the range danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
