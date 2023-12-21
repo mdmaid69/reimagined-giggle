13b7c2a2b0890ec4ff2f93cbe72206c0ef5754ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:43 +0000

    great section combine plan familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
