8335650581cfb309faff775a398bbc5f7ced732d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:22 +0000

    share easy interrupt over slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
