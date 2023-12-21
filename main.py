c2c5d85b0acc187faad6526ac371ffb62f968178
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:16 +0000

    charge certainly lend thanks follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_to_hex(n):
+        return hex(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
