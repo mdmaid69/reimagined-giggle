451c26ae7a3a0daa80e0abf56f9b65414c44153a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:53 +0000

    rate welcome short mainly workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-for i in range(10): print(i)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
