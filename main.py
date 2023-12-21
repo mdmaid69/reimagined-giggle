9fe0393603ff099e5a3ca96b1af87bb4ecf6f23d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:06 +0000

    weak in example violence uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
