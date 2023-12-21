aef6724dd950b05cb1ae0ea63023a7c5e33fc83a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:32:54 +0000

    result niggaacademy become increase note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
