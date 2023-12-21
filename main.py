12bd64f49ad97fca6feb46d2cc9bedd504b4bdce
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:53 +0000

    how shadow active eager too

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
