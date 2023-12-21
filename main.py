4cf5acfb975e1c3d2170145fe7c806e336f7f052
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:53 +0000

    progress start hate point anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_power(work, time):
+        return work / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
