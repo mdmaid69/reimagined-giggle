4f3f4847f7bcbb0d1da734aa34f83e288b8a1f32
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:17 +0000

    mouth well press 564encourage pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
