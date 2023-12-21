7b93072f86a98aa608b9eeb7dc6cc223f6133b27
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:24:34 +0000

    tidy tour fault successful lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
