a30c1f8bcb7d82983e0e3c096e6b4275ced872af
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:44 +0000

    suppose not know write deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
