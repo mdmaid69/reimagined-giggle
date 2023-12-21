aaaac2b2a486c43fa07c8bbb04c9e8278e706a0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:44 +0000

    bag pay fortunately soon comfortable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
