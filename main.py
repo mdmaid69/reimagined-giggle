462bfdc3e5486975b9b832eaa5d319b3efa723a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:35 +0000

    consult promise attempt report callat

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
