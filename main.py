aabae481a7361c856e8168aa45f52699ed9a6919
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:35 +0000

    standard breakinto object answer naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
