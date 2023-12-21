4bfeb8c1649f207c03ccee626fe23671afbf7123
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:31 +0000

    whom comment mess gate inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import re
-def find_pattern(pattern, string):
+def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
+  def is_even(n):
+        return n % 2 == 0
