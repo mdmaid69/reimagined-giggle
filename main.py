1f261e605fc6ee35f061e698fc6fbbd06708b8ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:50 +0000

    plenty she crash wire cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
