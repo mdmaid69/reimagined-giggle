6ec244e241dcfb3017a37a634e4dbd1a4a5d3a84
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:43 +0000

    organization less bound no letter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
