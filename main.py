5924221e45041709398e79dcbda3e4fe83946b52
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:25 +0000

    basic Zz shopping worried react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
