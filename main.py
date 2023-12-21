d188ac49a49fabecf738524a311ec0b629c0fe78
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:09:14 +0000

    into innocent use broad law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
