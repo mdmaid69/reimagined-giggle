7406c666830a4a89f15e0462c04d00c66b0b0c8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:27 +0000

    cheque exaggerate loose herself variety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def square_number(x):
+        return x**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
