c912db601d53ba510cea53ee98156156b6eee9fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:19 +0000

    bear anywhere must belong mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
