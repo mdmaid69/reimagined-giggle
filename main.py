5bef505d48153479e01615b0b4b9a442bfa41291
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:47:41 +0000

    lay uncertain anxious callback female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def sort_list(lst):
-        return sorted(lst)
+def get_array_item_count(array, item):
+        return array.count(item)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
