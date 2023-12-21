e5c3b80f378190cad618e28d38b23a5cb1667e21
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:20 +0000

    face fat future success without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+from collections import Counter
+print(Counter("hello world"))
