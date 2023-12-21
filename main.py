6a1ad1382efd00905b27a006e5d91fbd35d8f97a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:17 +0000

    sign worse home operation welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
