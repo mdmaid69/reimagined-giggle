1a8fa6b31d378714154922e5415b09ca0b3c2f5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:20 +0000

    another big bringback escape straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
