535a4328b6c3cc714627f02ac9e88dbe78653e4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:17 +0000

    set mark schedule report share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def add_numbers(a, b):
+        return a + b
