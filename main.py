6254327e316f207d8125d43a38b3cc6c8beb8cb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:38 +0000

    who Tt pay address flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import time
-print(time.time())
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
