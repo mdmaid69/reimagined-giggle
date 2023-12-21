744d8f0883148393174f12f6929cc73bf05e75f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:22 +0000

    holiday spray meanwhile burn subject

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
