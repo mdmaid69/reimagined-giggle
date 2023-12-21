a83497199c0b1d0be236ee74c6c486eb0669e350
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:21 +0000

    sort without but wild fail

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
