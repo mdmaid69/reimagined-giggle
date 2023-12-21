96145b5292e9fa8028af4846f7319b35b3b8c2fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:22 +0000

    give influence out zero ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
