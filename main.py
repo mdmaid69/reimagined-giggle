86a3d897fd04095b3702e679a2b69862a72a732f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:41 +0000

    stamp tool Yy care exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import getpass
+def get_username():
+        return getpass.getuser()
