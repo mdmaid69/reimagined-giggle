fb57daeb7f2e35ad1e3dd57e6e84646fb3615dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:35 +0000

    land anything even no development

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
