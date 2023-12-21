ad192d01f0555c97472bf48dc866ac529420b3b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:54 +0000

    funny involve prevent careless impression

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
