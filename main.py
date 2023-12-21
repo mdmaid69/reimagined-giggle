84c11042f62387183d3a3d25a521e9091456efed
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:17:21 +0000

    sight no match damage turnup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
