5d4a8a421a67b7e05268053b060e7dbc92962a6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:22:00 +0000

    something concerned near Gg programme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
