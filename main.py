933547f9f2915ee48441f29a367dcad482f186b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:24:39 +0000

    present turn somewhere bill false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_priority_queue():
-        return collections.deque()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def add_numbers(x, y):
+        return x + y
