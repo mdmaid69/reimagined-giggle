0177ac3bf239806d01168a508ff100a4927b1782
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:39 +0000

    well not whole creature like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
