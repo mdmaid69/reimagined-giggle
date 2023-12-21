ef7ba06176a36a7cf0798b3dc3cae70e9458ac61
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:31 +0000

    fit Cc squeeze remain expensive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_speed(distance, time):
+        return distance / time
