6879225293ddd9ef4185529cd1dfa6aedc818172
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:42 +0000

    godown bus snow sort march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
