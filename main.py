2d88d359f88f2f677a3c2a7fdfa53b02704234d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:20 +0000

    fairly complete your succeed firm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_list(array):
+        return list(array)
