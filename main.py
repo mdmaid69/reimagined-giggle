56e16fe834d159041addb1a68b86f158c8fa83a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:26 +0000

    tour maybe run they indeed

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Words:", len(text.split()))
