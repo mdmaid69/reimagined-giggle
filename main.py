1da083ed712ed7fd6293de2d8ef73a402102d73c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:27 +0000

    juice truth interfere travel minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
