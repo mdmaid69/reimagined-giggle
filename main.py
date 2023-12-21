4ea05cf53979c018c223cff1350be55b25532ccb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:19 +0000

    sensible attractive lock come type

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def multiply_numbers(x, y):
+        return x * y
