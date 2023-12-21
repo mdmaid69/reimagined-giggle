e4dd000dcbe9212450a3da3a87f52123d87beb19
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:06:13 +0000

    float compete tour lie schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
