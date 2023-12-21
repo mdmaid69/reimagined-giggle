ac9d39a753892fdbbab436c5676345e20ecc1b1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:19 +0000

    common birth lost steam entrance

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
