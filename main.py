3c6d8a3faec9666ae1ca0a175a2bf5362d91af8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:18 +0000

    talkoutof top mood pencil turnback

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
