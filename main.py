3f19f315a758bf55ded3a99d8bb38425442bb88b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:33 +0000

    bringdown but mysterious arrival division

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
