3f4ed3b4d45b2dc71d46a1815ec71e5491bea22e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:10 +0000

    introduction everywhere congratulate blame sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def subtract_numbers(x, y):
-        return x - y
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
