f06fc631c46ecd70c84dd62bc388eae0639d5806
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:54:47 +0000

    brief stop gap like notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def clear_array(array):
+        array *= 0
