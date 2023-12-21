16f486f9a5f225eeceee1725535ef3b17c132438
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:13 +0000

    rank full pressure complicated personal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
