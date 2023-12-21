e72c268321e1aabfdf09f59aa130e2d68c56a2de
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:13 +0000

    remain leave quantity pleased slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
