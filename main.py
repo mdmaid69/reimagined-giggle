2d36677c7f4ffdbc9a9d07236799662fcd67b5a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:12 +0000

    lay responsibility remark relation unlikely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def square_number(x):
+        return x**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
