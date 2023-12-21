4cdb38eb86142ac83f6dedb71c393e0bce0f7e37
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:29 +0000

    probably minimum hate left breathe

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def convert_to_binary(n):
-        return bin(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
