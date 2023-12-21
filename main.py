d6c98a2350750ed1e80132494bd3d2ab1a80a411
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:05 +0000

    damage cheap upset extremely conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
