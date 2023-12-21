ab2b4bd60d1279e186378e128a0c29829c60625d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:51 +0000

    minimum gentle apart before attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
