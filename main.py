02527de579653bc7fa3ce8584b6807bf61b78a1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:12 +0000

    minimum rest long sad opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
