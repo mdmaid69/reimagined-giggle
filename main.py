754ac817e9609de68367f0c810b581cb77c6ed98
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:50 +0000

    his develop familiar exploration make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_arc_cosine(x):
+        return math.acos(x)
