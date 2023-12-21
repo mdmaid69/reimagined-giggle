cc9572a0da6a4e14c9d546fb293cc91326563f5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:04 +0000

    house mind friendly right story

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
