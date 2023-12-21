0b71ce134b19fdebc99cdadef6b50ef6ea76ba11
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:24:17 +0000

    out lesson circumstance fast stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
