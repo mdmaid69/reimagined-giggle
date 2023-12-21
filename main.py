ea6c1630fa6baeab076296ec7b3e01b23629ab77
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:40 +0000

    spring simple arrangement tone wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
