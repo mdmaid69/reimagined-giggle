7c13826971e52a7b252eb74ae2f1efc89c146bd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:20 +0000

    accept term or own chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
