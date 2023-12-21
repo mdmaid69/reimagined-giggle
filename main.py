19f805c08b8599cc65582cef2ec7235ff47aa659
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:42:08 +0000

    recover bill seem declare load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
