88e7dedb68c20a9f64f8dd3b403231b2aa3d5d0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:40 +0000

    cover Itself prepare note although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_max(lst):
+        return max(lst)
