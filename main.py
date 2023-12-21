444103b28c8adb69574d07952b48c7f1b8477a4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:02 +0000

    copy pleasant free later process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
