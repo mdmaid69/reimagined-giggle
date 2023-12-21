60265bf9271feb9f7b051f2135be06ecf3aa0b1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:52 +0000

    behind equal scream soil immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
