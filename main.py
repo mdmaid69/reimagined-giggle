7ce0fff8b24686bd9094402bf033d5a3f089fe1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:27 +0000

    many seriously thumb holiday disappear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def clear_array(array):
+        array *= 0
