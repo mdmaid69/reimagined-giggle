bb534a4b99d7498a8ed9a6418807ff911fb01153
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:41 +0000

    throw OKorokay workupto minority spare

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import time
-def get_current_time():
-        return time.time()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
