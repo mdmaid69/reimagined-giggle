083f854b76055eb6d4a46ae6058fc363e800aeed
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:53 +0000

    narrow official accordingto holiday feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
