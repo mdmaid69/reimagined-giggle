12460182a51c39b2d8fa9dc0ebe82a7385a32356
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:46:33 +0000

    hear authority centimetre occasional urgent

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-for i in range(10): print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
