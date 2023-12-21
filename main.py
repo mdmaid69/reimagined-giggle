c07edfe169e49e62c5a6862dd59c97746bab73e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:18 +0000

    comeround order range surface related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
