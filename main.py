9aece1b507d49ecacd3060157ec959082c83f1dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:11 +0000

    pair mistake more either obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
