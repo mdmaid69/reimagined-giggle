ebd8d6c7d0d8ec1b26f6fa99e6c2c3214b769a4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:14 +0000

    hard variety language angry choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
