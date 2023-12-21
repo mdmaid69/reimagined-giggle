d9eb01b41c08c5710f10b07dc89d290d158be89a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:00 +0000

    race cold flavour hide piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
