c35ba8accaccca215ed767902d618ac4446ec646
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:56 +0000

    weekend earn now invitation before

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
