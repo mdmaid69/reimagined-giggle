ad350b41967a89501f88bfddac23b4ff07bafec5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:13 +0000

    comeback heavy mend evening round

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import random
+def roll_die():
+        return random.randint(1, 6)
