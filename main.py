29a3fa064fa47f61dfee50d5661fdac03bf6c683
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:46:23 +0000

    flavour born bringforward star fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
