6e605627c9c2bcc45db14111aeae543fb8801638
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:01 +0000

    illness single central gooff fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+import sys
+def exit_program():
+        sys.exit()
