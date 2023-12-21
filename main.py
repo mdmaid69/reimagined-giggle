a3dbe03d0b16352902515f3cbe6cbbb42a492e62
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:09 +0000

    least arrange translate interrupt sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
