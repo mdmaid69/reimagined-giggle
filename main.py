e727bac7bc41cf5c203e5501b933f235de12eca8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:04 +0000

    deep fair failure Hh middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
