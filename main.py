575eed8cc266533a31682d674a5634571f773616
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:45:49 +0000

    spring preparation will care pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+print(os.getcwd())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
