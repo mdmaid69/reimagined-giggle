bd30d69b832c0ae65a8626b240a0e15170582314
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:05:10 +0000

    region instrument immediately friend pride

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
