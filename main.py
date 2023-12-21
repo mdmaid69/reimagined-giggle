0cce8621a7790e4c858e0826919c7fda3f03d4fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:12 +0000

    occur takeaway angry steal statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_string_from_array(array):
+        return array.tobytes()
