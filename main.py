1d84cc147a9235fcec35203e50f82f6933c7b175
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:12 +0000

    her act false tend invitation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_unique_words(sentence):
+        return set(sentence.split())
