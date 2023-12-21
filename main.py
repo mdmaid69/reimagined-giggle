50eda107d21b843fb7a474df4a29bef745094bc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:14:34 +0000

    name welcome Ee even understand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
