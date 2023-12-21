6e9d6e5ccf2eb93e228b67c55be685892bbc0b99
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:01 +0000

    complete discussion also I Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
