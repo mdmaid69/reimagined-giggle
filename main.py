d47cabb0f60696c83fd9b8d866c5a86eaadad369
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:40:43 +0000

    giveup Pp would perhaps modern

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
