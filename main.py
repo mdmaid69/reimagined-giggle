3ad2d8f4109578e0e364d90a1f58bbaf88e1e187
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:05 +0000

    tragedy spoil 573entertainment quit advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
