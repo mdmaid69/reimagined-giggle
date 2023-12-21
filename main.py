70b8b9403709358b5cd7e00de6ee4dd863841e24
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:09 +0000

    range Hh curious yesterday apparently

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
