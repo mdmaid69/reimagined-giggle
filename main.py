76a5a9d2722e8ca721b89bcc03fa574d11b48253
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:27 +0000

    decision kind rescue back love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
