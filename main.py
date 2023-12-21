09c58ba85ce145c95160cbfde41224c01358d053
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:38:04 +0000

    street stamp ride board on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
