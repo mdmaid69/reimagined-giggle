6fa8b4d8db14946e5d353e1cadbb36102faae5c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:48 +0000

    load pretty land 570enjoyable drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
