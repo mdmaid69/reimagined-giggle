ba1c00de1cadae9969ba95c743f3cacb39eec627
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:40 +0000

    test radio spoil quantity pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
