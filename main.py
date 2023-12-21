0d78ecebb78a4edb6422b60723f7031f310d3f79
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:47 +0000

    wear taste walk create person

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
