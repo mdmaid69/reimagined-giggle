cce266a0f78e8c03d202b957fba57d1e38c21d52
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:48:26 +0000

    disagree spin sleep careful stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
