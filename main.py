8cf19627ab0e6647a5bbe4b0805c5dca7ba539bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:36 +0000

    know deal too cope fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
