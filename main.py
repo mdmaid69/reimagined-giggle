92d8d58a4125315b3a227fcc666bcb4e807664c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:22 +0000

    send so early nowhere hurt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_power(work, time):
+        return work / time
