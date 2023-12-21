27296df2cde167101e18d4158a6ec8776eef6721
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:04 +0000

    heart letter getaway mail consider

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
