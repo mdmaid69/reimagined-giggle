d39daa45fcf2907e824dd461eb9ce55767803940
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:54 +0000

    whether practise common cause near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
