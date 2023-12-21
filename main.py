7017121631b940258d4b0f71894f33098a243e98
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:00:49 +0000

    question arrange stage explanation shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def count_elements(lst):
-        return len(lst)
+def calculate_error_function(x):
+        return math.erf(x)
