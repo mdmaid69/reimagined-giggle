e9dba5e88f398d5bc5f103ce4cbfd3165b2540aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:10 +0000

    confused same late majority except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
