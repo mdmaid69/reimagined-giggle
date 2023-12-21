6ad4b0695c84051b87fc430701793dcd67a0ae14
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:27 +0000

    they band fire here whole

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
