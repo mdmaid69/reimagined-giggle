6e0bfbf69fb9eab5032bc77bc79cf837e3ad3379
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:24:31 +0000

    clue aim with in machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
