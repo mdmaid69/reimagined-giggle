4fac4b060dd7d452ef16d9b97cb970fe666d8f42
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:54 +0000

    expect married improve case swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
