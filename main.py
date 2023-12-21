73dfcc853015f19067e32c9ea5f3d5856e6600df
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:44 +0000

    comeoff tooth freeze length copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
