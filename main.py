bacea7ca636aa394a6d72aecabd6ba22084218a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:17 +0000

    voice with by through for

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
