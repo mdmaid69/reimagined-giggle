8ebe0cb42c4110b01c6bc2d973be4ddfde72cafe
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:17 +0000

    experience quit Ss sell favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
