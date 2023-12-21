70584c100d8eef18858a0cfd01f7b21a39ad24f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:09 +0000

    makeinto usual majority flat position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_cosine(x):
+        return math.cos(x)
