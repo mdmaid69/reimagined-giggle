276dedf7a664d7c3e3414ba68664a3ef8c4a65a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:46 +0000

    real blue further talk comeback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def is_odd(n):
+        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
