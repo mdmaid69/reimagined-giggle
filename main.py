5f0725bb43a60333ed68d014c906c4fa21d19ac2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:57 +0000

    pot colour last rise equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
