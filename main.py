d11fce87e12459fdd537a341270ff6c7a63a301c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:23 +0000

    sail stop object deliberate watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
