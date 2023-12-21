6ed8bf0ad605a42585cc9206e4b87a3154de9c95
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:39 +0000

    tool variety software equal throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
