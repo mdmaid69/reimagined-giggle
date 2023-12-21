6ea42592640ff1bc3afdc3dace9cba5a7933167c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:17:28 +0000

    centimetre arrangement scale taste impatient

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
