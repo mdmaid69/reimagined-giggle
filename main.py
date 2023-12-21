5c28de53586874f8bd6a6877234c3c434fec3f73
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:35 +0000

    movement cook worth straight machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
