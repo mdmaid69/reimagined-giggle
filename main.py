2574026b08819ababc8ec29ea59f33d8dde8d170
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:39:37 +0000

    thanks national example tall flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
