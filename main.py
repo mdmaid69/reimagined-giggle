e748d852cec80c3a32b90c6b5dbd97f9e87de942
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:48 +0000

    person reduce quality possible business

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
