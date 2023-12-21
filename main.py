2fb68ebc0d389ea4d6e53675dc451b478c4f24bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:41 +0000

    spring delicate naturally uncomfortable delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
