7d2fa45d2db15eb8e72ab310422de374c7800fe5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:15 +0000

    left pretty tidy familiar plain

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-import random
-print(random.randint(0, 100))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
