05c6b26425e7330878496393764e9ea1a475bf11
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:31 +0000

    cinema Oo lookaround too turnon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
