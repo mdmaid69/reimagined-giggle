3709ed53a445da637351b49019960639a3ddf12b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:13 +0000

    runover compete measure opportunity hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
