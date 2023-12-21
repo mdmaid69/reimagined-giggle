af9573f83e295c81c06a26a68ee054b354ae7417
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:36 +0000

    sense dark still doctor choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+for i in range(5):
+        print(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
