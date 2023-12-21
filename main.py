cb0b0e033023640a48134034495db2639a3d6ce5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:17 +0000

    reach hour turninto together measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
