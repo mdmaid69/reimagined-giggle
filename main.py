68c03706d21af79aa9269072cd70e407605f74ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:03:15 +0000

    sure noise agreement computer moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
