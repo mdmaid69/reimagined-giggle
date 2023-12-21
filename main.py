d99ee958c848b36de9dc75457a031c050017ef19
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:32 +0000

    represent getup we face spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
