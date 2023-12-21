0ceed8d1b04163c47e9914b9ac80acd38816fe85
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:58 +0000

    stupid cry declaration type woman

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_index(array, item):
+        return array.index(item)
