b68053fd29488641ea62413e351dbc1307be11e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:05 +0000

    job shop however definite whatever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
