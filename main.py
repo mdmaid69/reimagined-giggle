2f3a668eb7fe485afff9e328e0ae87781153a9ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:01 +0000

    clothes outside rank thought deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
