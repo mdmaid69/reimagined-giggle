07ef80bb271503f093d649ef1d0cd4ca83d4b61f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:10 +0000

    improvement fairly tough join union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
