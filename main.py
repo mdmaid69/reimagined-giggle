db6cda3682b6af8edff7f9ebf95934e5ae3c5155
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:30:08 +0000

    circumstance before afford study fairly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
