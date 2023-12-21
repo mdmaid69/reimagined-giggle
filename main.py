8e67a95ed24c1ec6df3789cb43fc36f9f67fa4c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:43 +0000

    tight unhappy eye aim dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
