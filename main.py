451ad2ee7872f328823f738eeb99616b76d063d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:45 +0000

    welcome small workout peaceful form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
