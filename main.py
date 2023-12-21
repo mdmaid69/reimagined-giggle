0620c8276ba4dfa22f4a5fa662b8284faf3f45e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:18 +0000

    present thing form sound bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
