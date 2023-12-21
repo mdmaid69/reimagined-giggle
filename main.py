9463509a55867c1f68b3afdfc24d611bf173f55f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:38 +0000

    whatever everything benefit justice complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
