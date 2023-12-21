c18a6743d6e9196d0eccfc59acb2579228c64e6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:10 +0000

    brave exhibition success meal collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
