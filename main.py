08f740e89c31ada6b9708dac6e72725334582f13
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:51 +0000

    flat second national thorough by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
