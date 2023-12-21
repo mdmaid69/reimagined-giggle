708025384449f2908cd09da681f84777fdb742fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:01:38 +0000

    comeover somebody sad task cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+import collections
+def create_counter():
+        return collections.Counter()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
