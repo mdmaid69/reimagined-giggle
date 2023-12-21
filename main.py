065e4c984f6f4824c0b91cd6cfa64d78d2bcd589
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:05 +0000

    exact smell sleep dangerous war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
