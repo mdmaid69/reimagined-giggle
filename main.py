d775930a086dec8c5e30aa546a704b4d30c6b478
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:18 +0000

    tidy commit know tax declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
