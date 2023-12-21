3b4d122608b9a81135d7d0ea9ab76b1193b4130d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:15 +0000

    stone person bin account round

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
