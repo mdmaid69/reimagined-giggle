3f3fa2569a903527c9a7174c39a83e5f10cbf003
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:06 +0000

    minor discover street fond work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
