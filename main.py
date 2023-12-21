f2471f10546202f55bd8eabd5b688a0644c6ba06
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:18:44 +0000

    cough now start statement clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
