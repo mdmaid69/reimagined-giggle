6312ae6737ad690151c3115a8c4513a4905bb027
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:53 +0000

    where catch expression goon belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
