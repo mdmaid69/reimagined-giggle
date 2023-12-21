eb792c446b0dd3b6f4aa4a87c651711797c9d6d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:29 +0000

    rise keen at each central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
