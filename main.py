daa3c29b8f001d8568eab7957af54b5e867df434
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:47:00 +0000

    follow length rank in afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
