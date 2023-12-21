c93eac0a84a4eafa4cd1666481e5fbcfb5bd668a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:47:49 +0000

    unlikely rest problem switch important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
