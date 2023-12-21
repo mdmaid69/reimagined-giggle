8ae5da607d76202f71d2ebe6d8bc7e6453e72be8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:21:53 +0000

    opposite door speed each increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
