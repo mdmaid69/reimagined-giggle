21f330fdbadff26bdeed6e5c9de1b906e3e31a1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:33 +0000

    swallow peaceful your tired mend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import sys
-def exit_program():
-        sys.exit()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
