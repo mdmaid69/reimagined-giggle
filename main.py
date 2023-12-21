c2a0078786fea537c4af6adda8b435c322c5279d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:15:28 +0000

    apply loose strange throw treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_work(force, distance):
-        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
