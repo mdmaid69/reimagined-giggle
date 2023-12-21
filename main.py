c5bdfa267127f0d8f6533f3e1157edf43cc0ac3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:39 +0000

    rush man hate whistle flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_words(sentence):
-        return len(sentence.split())
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
