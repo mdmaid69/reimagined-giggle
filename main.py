8a4e988a5d518d159ee1768e1d9aad771ca1c926
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:33 +0000

    border whoever memory arrive activity

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
