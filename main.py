0c2650deda7cbaf1b65015c28faad23dbd115cc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:50 +0000

    depend that short clear gas

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def sort_list(lst):
+        return sorted(lst)
+x = 10
+y = 20
+print("Sum:", x + y)
