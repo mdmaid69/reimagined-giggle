c42ba69a4ed0d367d02a223030fe37b1821b7c9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:51:47 +0000

    basically since fear scheme period

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
