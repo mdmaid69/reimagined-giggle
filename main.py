fbd0cade19972d6449dfcd377e8e24061a235469
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:22 +0000

    jump sink goon work last

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
