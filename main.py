2519f79efbc0f8c8b41dcec6cdf9c8a568388354
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:36 +0000

    direct evidence anxious yes computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
