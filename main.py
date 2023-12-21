b2c8f5845179c991679ad5835db46f1860aa01b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:24 +0000

    life principle frighten bill breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
