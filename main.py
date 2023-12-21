872bd835ac563f1b717f7e659fdd9a07434e8fc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:36:57 +0000

    use section thinkof holdout wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+for i in range(10): print(i)
