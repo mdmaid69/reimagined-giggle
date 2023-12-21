078c9b9885e2ed955ab0f296697c38e088ca5378
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:11:21 +0000

    material wave phone best flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
