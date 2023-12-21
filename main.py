957112760ebbc3dba4fbdbd4fb61d04573f680ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:05:33 +0000

    cost cry accidental tense deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area(radius):
+        return 3.14 * radius * radius
