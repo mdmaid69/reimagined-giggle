1c312f66c24ccc3d3f821be272b0b3e9ac8796ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:15 +0000

    ourselves mind weekend nothing force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import platform
+def get_os_info():
+        return platform.uname()
