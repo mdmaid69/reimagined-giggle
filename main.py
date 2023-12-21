87483ee8089030f7fad13900241b59547bfb3da1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:06 +0000

    plenty occur treatment name takeover

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
