d60976605513b7f740eacd43730101c2ea2fdeac
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:24:26 +0000

    drive glad quick unable unlikely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
