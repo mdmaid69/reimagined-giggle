49b7e3a776e40389ba1c5eec5e3fe9fbe15b8c2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:26 +0000

    message joke turnoff fit avoid

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
