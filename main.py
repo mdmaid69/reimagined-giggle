fa80624a7a98d09cc5d081135fa229fd571640ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:08 +0000

    earn ball Vv silent majority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
