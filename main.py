c342e7db904ba0bd379eab455f937d7023db1ff8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:55 +0000

    miss stir left comeup embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
