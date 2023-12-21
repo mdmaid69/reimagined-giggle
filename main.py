e5307b8c1d7fa1f9ede1e5254264dd5c5ded75bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:30:44 +0000

    afternoon afternoon floor give process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
