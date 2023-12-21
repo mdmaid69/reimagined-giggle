e560eb170cf32b6dd22889b55f8210f154747a24
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:38 +0000

    distant justice 564encourage fast drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
