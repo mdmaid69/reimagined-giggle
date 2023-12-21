765b64eeedd8298871c18a84914d5fc6e90545ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:51 +0000

    Ww turnup flat pain provide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
