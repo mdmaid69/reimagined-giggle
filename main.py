b942b6ceb956de29d045aa1d586ceb086e041e79
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:28 +0000

    run play loss firm sore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bool(array):
+        return bool(array)
