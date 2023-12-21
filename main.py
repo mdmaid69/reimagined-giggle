415e2c2fb50e60dd2e62b759c1ccfac3e3d59eb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:11 +0000

    workon same pipe group callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
