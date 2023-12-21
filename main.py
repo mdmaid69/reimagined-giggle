e3c40e8e79adedca216e19e8ad5ce504023ca3d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:46 +0000

    let smoke lookafter score straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
