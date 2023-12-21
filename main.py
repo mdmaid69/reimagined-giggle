2bbaf18b708bdf1adf11858983d64d5cd347564c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:55 +0000

    near direction 566end arrange plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
