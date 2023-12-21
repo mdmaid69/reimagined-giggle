342afc748dcca80cf954461167d0e23c4449165a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:11:43 +0000

    exact choose suffer please interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
