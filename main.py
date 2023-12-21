4902dcf2c039f1cc0c79efd7eed8afc585e00794
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:50 +0000

    handle moon ilovedicks change stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
