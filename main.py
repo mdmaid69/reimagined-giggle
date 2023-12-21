f793efff5eb5fa8128e619f17b5c9275812477d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:35 +0000

    bringforward climb television plate matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
