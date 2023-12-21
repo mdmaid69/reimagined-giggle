cc5d76abcb1b2a219a87dceb6d82a02e8c37b221
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:24 +0000

    getaway love result know load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
