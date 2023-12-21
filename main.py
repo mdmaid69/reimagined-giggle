e817a49a17499edf5b44a36a8ce6819e3eb46883
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:20 +0000

    expect gas except mixture rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def convert_to_hex(n):
+        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
