fea1d623d974dc50035cc5acf44a076e8ecfd5f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:47 +0000

    content estimate lean pipe arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
