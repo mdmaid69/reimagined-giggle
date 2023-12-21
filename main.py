2170a7219cf9fede7f18c4bbda18725951b9e326
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:52 +0000

    safe agreement recover thanks zero

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
