0092d8c03052d722bd43ed059cac3eda1a18dbac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:53 +0000

    office decide cut achieve slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_tangent(x):
+        return math.tan(x)
