6521b99bbb9c897979fd2863eb6f578f8682c899
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:11 +0000

    twist gas considering slow Ll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-print(sum(range(10)))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
