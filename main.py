bf30317f9b08383de6873300d32c8b9e7eeb79c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:10:57 +0000

    forward blame future ilovedicks week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
