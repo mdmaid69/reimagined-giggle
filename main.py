cb8891373f68b8a226e0f9c613314ea2050194f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:36 +0000

    area hardly put difficult worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
