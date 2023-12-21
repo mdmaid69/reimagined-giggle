0718d946bcac3dc462e1b1410d0b183b6074263d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:56 +0000

    disagree ignore Ff professional continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
