b553b431c74421d4ed7423a81b5e965f473f967a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:25 +0000

    someone here yes street sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
