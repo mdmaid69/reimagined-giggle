f516ba4a16c503171a491d7ab42388851e3f3a64
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:08 +0000

    balance rain notice hurry deliver

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_typecode(array):
+        return array.typecode
+def count_elements(lst):
+        return len(lst)
