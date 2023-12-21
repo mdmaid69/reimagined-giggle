5019ddd80b6500847bcc5ea76d3fd1d1db9bd5ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:00 +0000

    beneath injury board tomorrow exciting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
