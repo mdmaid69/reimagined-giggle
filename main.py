07ae11545396f3074d5a587b98495562752ae673
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:59 +0000

    entry nothing fasten size concern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import re
+print(re.match("h.*o", "hello world"))
