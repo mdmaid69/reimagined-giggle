d293f2b835374d1e63f3ff5b457860e6c61af331
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:34 +0000

    now callback stranger command poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
