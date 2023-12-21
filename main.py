ec65e9fd1f888aa4f0a02b6ca1507afaa7e5b8b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:57 +0000

    think beat affect steep watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
