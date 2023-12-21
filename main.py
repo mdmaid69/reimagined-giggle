e76c2358196df15090ac759ba125d89773e61011
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:47:24 +0000

    lower jump label during cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
