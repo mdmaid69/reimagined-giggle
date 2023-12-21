de1f4545948d758c0848ac5f5b8063815af8eef8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:47 +0000

    battery club bill ahead certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
