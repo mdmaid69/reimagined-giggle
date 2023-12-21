270aff5f6ac2bb47963262ec555632984ce867f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:05 +0000

    any go justice private sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
