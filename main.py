897dbd1108c76e4e3a057692ddaeef4a187280d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:21 +0000

    appear godown education provide his

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
