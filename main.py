313be962eb2d97b15f2efa828f9c1aa7e599ec24
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:19 +0000

    admire 565end form travel mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
