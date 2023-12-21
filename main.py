51e84b1971df2f6673d1f8b81473cf32135773e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:30:01 +0000

    wake ticket evidence replace high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
