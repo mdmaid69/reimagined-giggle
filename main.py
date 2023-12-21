4024adcd4acfcb3f1a16290ad0a8e188c7447b93
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:31 +0000

    crash weekend pool release past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
