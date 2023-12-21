bea3477a7666ceebff95f2e5813243721e9aaace
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:21 +0000

    advise cheque friendly left border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
