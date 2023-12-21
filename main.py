52bd2a65baf2c046930c49f50d73f2d7947fa905
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:59 +0000

    downstairs arm go display lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
