3ff15c551d6e48f8adc4048c3953776bf58b9a04
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:09 +0000

    brown rub imagine breakthrough think

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def convert_to_binary(n):
+        return bin(n)
