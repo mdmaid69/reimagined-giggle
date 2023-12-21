4cb5666caa0b9ecd6a6c489f77648a78e2bd38e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:19:33 +0000

    stare scrape goback(to) minority workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
