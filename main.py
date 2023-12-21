767c7f9ea022204138413231fdde82ae391d9709
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:23 +0000

    each fight means kind present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
