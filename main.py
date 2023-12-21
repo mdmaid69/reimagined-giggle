cc6cac6cef173a8eb059c17f2aedf7e4463b2cff
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:14 +0000

    image beginning holdon this unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
