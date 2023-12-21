0c47bd4b8027752d3b406ebbae05bd5e23ceafda
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:32 +0000

    fortunate rid awake disturbing contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
