116ab361ba888ad691f3785810612e654db96943
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:37 +0000

    stage shoulder eye advice trip

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_work(force, distance):
+        return force * distance
