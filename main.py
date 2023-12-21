8dad715113266e0812322160b8e167315d5285dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:54 +0000

    laugh shade factory jump out

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
