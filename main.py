1322a9df91b2d51e05f660053f2ce2e6b04aa9a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:09:53 +0000

    information extremely experience rely sight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+import time
+def get_time_since_epoch():
+        return time.time()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
