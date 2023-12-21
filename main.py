52617338087840531562829a4bc608cc9c7e7e48
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:33:14 +0000

    wing twist wild walk dig

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
