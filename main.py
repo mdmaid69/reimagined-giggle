eb78ef20c19b7b7325fdc6d36062c9686ef54067
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:00 +0000

    unlucky why their theirs guilty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
