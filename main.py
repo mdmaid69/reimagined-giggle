bde98846772d10fe865728d0149ad9bcb8b12b57
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:10 +0000

    refer explanation steep side pin

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
