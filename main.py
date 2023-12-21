48c819ceebe30f4a8b31504de18e9de3467382cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:59 +0000

    factory that yourself side operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
