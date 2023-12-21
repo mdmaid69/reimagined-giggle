356432d9af51c4307232ee2ab134d3ce98e2e82b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:06 +0000

    shrink copy occur swing deep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import tensorflow as tf
+print(tf.__version__)
