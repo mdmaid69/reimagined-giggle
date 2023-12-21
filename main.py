cdb50961ce2878b48b0ead61eed414595be5c41b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:33 +0000

    valuable apart minor embarrassed rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def reverse_string(s):
-        return s[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
