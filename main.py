b68a8f6440b476299655f3430c4471a16d1c0f1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:09 +0000

    blue identify contrast burn practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def sort_numbers(numbers):
+        return sorted(numbers)
