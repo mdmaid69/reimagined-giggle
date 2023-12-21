f7bb6300d20b0cedcd05a8b91d7244ca01395544
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:18:56 +0000

    personal direction behave develop become

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+print(sum(range(10)))
