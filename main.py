8d61b4e58fd6fb05216949b1bc3630f0b7753376
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:50 +0000

    challenge criticism explanation 572enter patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
