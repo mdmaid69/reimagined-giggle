480c6c4e779d7ee8eb97fe7ccfb67397107dcacb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:26 +0000

    depend really yourself find art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
