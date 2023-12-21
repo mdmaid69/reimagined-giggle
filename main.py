9b892d6579cb7b652b4df8973acc07dcb1f7b9fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:47:51 +0000

    bound concentrate detail price godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
