3cab16cbeb9afd820d46f0d0e89db5d7f6ed9fc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:57 +0000

    birth suddenly towards pretty line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
