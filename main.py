527c058f2afc268f152529f8bafc8ca6cdfba60f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:14 +0000

    previous powerful fit light favour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_max(numbers):
+        return max(numbers)
