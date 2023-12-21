3c2c536e64e93322eb7eaf12137064ee8339129c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:17 +0000

    opportunity thing change abroad plate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
