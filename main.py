c3feb62ff8aa5c6886b690040a752f7e97757535
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:10:03 +0000

    getup discuss call better fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
