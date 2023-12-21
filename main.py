1e0705aec40acf3b3fa4c6cf84dccd7182d21539
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:38 +0000

    sticky comeup getup cheap big

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def square_number(x):
-        return x**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 n = 10
 print("Even numbers:", [x for x in range(n) if x % 2 == 0])
