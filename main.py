44f645bb9f4bd40f5ed6593e2a9801d43879fa1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:28 +0000

    threaten excuse hide charge visit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
