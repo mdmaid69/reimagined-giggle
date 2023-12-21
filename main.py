aa092eed20e2441000b90257dc836b8cdb36d7df
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:43:33 +0000

    disagree rain dry pool pretend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
