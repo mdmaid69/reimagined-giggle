f03e4e75a4dacbc309707df2d4d174f277661fc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:02 +0000

    record wind burst invite uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
