c7cb3b8b57d4da220d0800b5f32ad166c5a5c271
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:58:08 +0000

    fork debt practical common file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
