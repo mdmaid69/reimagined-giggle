2938b5b242904ac6402d73b6d829caec651a3bf1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:08 +0000

    may wander reliable forget talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_area_circle(r):
+        return 3.14 * r**2
