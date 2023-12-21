7394a2af49483cbcda105d4a138e77d47190b7e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:27 +0000

    exciting range glad clear war

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
