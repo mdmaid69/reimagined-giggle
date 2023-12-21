53c372caeb9d290bf81c9f6a9dbe5beaec214c4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:59 +0000

    greeting fire part face young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
