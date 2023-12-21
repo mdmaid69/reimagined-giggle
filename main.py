7ada8a96babcd3f7c80aa349e694f1dfeba5ddc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:14 +0000

    long 568energy straight children sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
