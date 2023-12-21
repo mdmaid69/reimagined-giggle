510a286757b40a43108196b29b643183fa00f840
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:26 +0000

    height fast control ahead spread

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
