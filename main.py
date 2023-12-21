912f5f802a059e511b1b9f411e9f75e7becbf480
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:42 +0000

    autumn illness lost Ii song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
