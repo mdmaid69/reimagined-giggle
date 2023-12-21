a2783da56788a83a422af53fc8bb94170137e7e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:47 +0000

    behind be enthusiastic kind part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
