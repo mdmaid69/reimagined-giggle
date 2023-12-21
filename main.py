09db18a9783d6771847b8f58c5ad26e3f8421539
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:03 +0000

    considering trick swim news official

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_unique_words(sentence):
+        return set(sentence.split())
