81a71e81d36c6c4603ba1a8d90622eec28c68be1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:41 +0000

    fierce almost afraid 179bite naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
