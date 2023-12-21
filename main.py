631b8acec7d97daceb6006460d7f7d538abc2dbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:02:03 +0000

    harm help mood last spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
