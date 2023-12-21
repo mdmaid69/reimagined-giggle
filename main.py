b370e64972a9c558d54df11373ea93c92d2f3de4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:26 +0000

    learn let whoever neither mix

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
