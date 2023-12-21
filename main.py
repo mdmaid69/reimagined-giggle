68d95f67c5ff0ba805f4f7eb7c3db315f5f4c26c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:02 +0000

    consider care before rest meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
