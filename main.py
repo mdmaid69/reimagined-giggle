36734dab45f2962995c53f47832e8a3b64c3825d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:04:08 +0000

    set bit estimate splash back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import collections
+def create_counter():
+        return collections.Counter()
+def is_palindrome(s):
+        return s == s[::-1]
