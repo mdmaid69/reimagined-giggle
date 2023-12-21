fc636a56daa439fe6363d5bf0d0f1e3664b91eed
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:40:36 +0000

    escape crime edge still seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
