aaaacf650e72cc14c2bee2489b61d6fe01d6747d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:31 +0000

    book criticism dance conclusion danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
