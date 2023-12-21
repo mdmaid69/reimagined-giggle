4f01ed29f7d9dde756a9051753c277c4f8183779
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:16:35 +0000

    the poor innocent week use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
