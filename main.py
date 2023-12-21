4f51e449a0e5e20af9432604ef946c61f6f1f5e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:03 +0000

    enthusiastic judge offer pass sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
