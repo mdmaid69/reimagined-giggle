336653533d8aeab9784551ca341d7122ad029120
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:18 +0000

    shake sudden beautiful wing contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
