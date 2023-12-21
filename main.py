6c91187c170e5ef3e43a82051f5c1136f0cfd51c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:47 +0000

    turnout sorry catch who name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
