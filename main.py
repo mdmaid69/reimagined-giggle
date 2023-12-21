d2d7585b4ae0e3a4098e4dd1f8b09047f930962c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:26 +0000

    happy but when beat already

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_bytes(array):
+        return bytes(array)
