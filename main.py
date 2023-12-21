d06a650e58ddd853d6ad2b2bfbe4fa295cf7713e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:17 +0000

    reserve possible live development smart

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
