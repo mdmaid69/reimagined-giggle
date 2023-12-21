dab58385c16d74be8d78b5ed580a3b2f328aafc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:16 +0000

    morning stand bar 564encourage it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
