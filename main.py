8d6ea8827c71e24b5697602b55b6acf25328ba4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:15 +0000

    crazy loud beat impressive replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def exit_program():
+        sys.exit()
