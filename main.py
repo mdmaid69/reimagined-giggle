156a7ac69b897e3d8794d27359e8c1c6b8a09926
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:09 +0000

    traffic hand tend can scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
