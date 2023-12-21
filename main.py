41d50d7703c3c9f5832fcf9298df4c76ec430492
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:39 +0000

    easy have clever cheat 567enemy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
