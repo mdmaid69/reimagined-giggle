422dcc14b840c88eb6194593bd72f477bd537e00
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:43 +0000

    related whenever save grow unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
