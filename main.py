0f6800510c08b4990f3d63c88abb5662b13d72e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:19 +0000

    question slight play hard either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
