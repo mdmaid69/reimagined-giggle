1278d3d64b130dc44073e0c34f5dab2abe53d048
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:53 +0000

    quit bother eat law freedom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+print([x**2 for x in range(10)])
