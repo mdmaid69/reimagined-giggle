934314f185763149edd9140f2e47b0b517e4dfff
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:19 +0000

    project runover getaway chief platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
