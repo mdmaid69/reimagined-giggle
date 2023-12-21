1fa902d34895ba09ae56198e1fa683d27651826c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:12 +0000

    exercise keepup guard hope nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
