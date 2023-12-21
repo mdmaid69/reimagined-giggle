afcf110620f5731b9e1c961fcb32a10f7014b0bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:17 +0000

    opposite children last model theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
