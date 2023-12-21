09fe349045ef959f17d6a3dc24ead6400d7694b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:42 +0000

    simple it mysterious uncomfortable wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
