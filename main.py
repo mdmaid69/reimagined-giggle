43dcbe500b18d4a9b90face6f2606ad87fd8a5f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:17 +0000

    genuine sore song lead disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
