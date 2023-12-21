87ea55d1407cbab5d71be51a6d6f4f93da0773d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:18 +0000

    sick thank oil gift watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
