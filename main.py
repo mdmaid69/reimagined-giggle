5b5b95e9bf23d5592f796b5f330affacea60443b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:35 +0000

    reply set unkind dig half

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
+print("Hello, world!")
