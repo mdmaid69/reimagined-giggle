0c948b5f4d0cb78305949da1d749529fe1c3d4c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:44 +0000

    I present alone course judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
