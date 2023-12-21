5e7a1b0fa182f8961261ed1878d02e8a2a26fa51
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:54:19 +0000

    his murder not wild hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
