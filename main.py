c191ff59250a7c922179e1433987bc3200ca9e72
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:37 +0000

    to several appeal distant profit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
