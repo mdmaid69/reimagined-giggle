4f7de70f15cfb9772f5249b77ed18561080d99c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:48:59 +0000

    miss to damage test neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
