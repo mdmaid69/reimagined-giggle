ba1527fd864827f36999c7f2e0d155ae4db98d40
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:42:16 +0000

    design skin arrive unhappy party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
