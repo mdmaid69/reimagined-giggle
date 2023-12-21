50997cdb4ac3b389e2604a3dfedb029462773e33
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:11 +0000

    idea come behaviour increase condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
