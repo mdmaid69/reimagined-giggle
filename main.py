98a1a46d67acbbb43ebce54a9140d33fffc5b8e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:51 +0000

    unfortunate minor bang sort profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
