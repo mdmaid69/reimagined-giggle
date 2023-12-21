1b5487f1f2839e65719d27580a789ae18b2964b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:28 +0000

    upstairs main story light spoil

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
