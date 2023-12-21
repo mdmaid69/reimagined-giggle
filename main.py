f6b7f8036275a8c162361452134a402ba057cf03
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:11 +0000

    alone fast pay radio callout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
