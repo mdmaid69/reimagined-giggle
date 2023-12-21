6fb9ba3a9a838251d5641fd055bc65febcdde9fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:52 +0000

    black giveback set keepoff trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
