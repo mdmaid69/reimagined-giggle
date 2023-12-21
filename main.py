a757a4bbcd599ee25af0f90e48b0c59c8cb083cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:24 +0000

    form risk apply really middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
