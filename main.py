b753ae16db7ef9bbf6b1aa26987fce6431fd42bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:18 +0000

    fork body record if intend

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
