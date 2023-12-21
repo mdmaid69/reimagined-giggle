ac99bcaeedd128b3468a4eac04815e8cbb3f23f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:47 +0000

    turn best do interrupt pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
