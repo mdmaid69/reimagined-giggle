c3a594caa656f467abd55b4aa6cdcc2cc3e5ea51
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:01 +0000

    convenient regional plastic excuse comein

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
