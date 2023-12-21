64b001aede2f71cd5b0bfd5b1fdb4242f7211406
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:37 +0000

    act noise want plastic near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
