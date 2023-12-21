1d24e902c5929cbce6a24fd005dc9c4ffcc75274
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:29 +0000

    measure chin why content line

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
