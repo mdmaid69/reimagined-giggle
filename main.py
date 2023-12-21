03d2d099335e821accaca6f7fdcb9b1cc4ae0165
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:04 +0000

    behaviour crime distance turn day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
