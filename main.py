82a22b00080ce49c80cb1ac91b04ca48d91280b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:49 +0000

    behaviour dare apart nature excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
