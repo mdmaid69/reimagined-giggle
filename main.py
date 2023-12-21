c9d43baae1da896edd7292742dc5cbd89c7c084b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:58 +0000

    sensitive have explain grade congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
