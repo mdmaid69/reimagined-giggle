0f046f15958b141e73eccbd63c255155729b7523
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:43 +0000

    excellent proud interview whatever completely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
