23c65aed078b5d0934319dfe08f2d20a5174f357
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:16:52 +0000

    forgive callup need impression split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
