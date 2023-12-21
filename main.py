5d0a4c5edbea9d6368fefed6588da66683584363
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:26:24 +0000

    strong want pleasure perfect tooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
 def calculate_root(x, n):
         return math.pow(x, 1/n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
