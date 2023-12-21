7d586f9b6bc79cae459e9fedf23b36a112706943
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:54 +0000

    Itself him several charge turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
