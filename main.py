833969f7224843c259133571f53ff8489df16006
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:07:15 +0000

    flavour collect danger morning happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
