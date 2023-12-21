c8d51cad54665f0010665bb497d0177494be092a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:59 +0000

    unlikely choice people clever drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
