ec8a60e46d75806a8e9af5564610dda5bc400e4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:19:38 +0000

    middle communicate doubt roll damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
