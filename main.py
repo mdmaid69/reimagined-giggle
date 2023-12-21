d8e10907a078301ba95bdd9ff1bca6f67ec1770e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:38:41 +0000

    pinch contain callback fortunate scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+print(sys.version)
+import math
+def calculate_floor(x):
+        return math.floor(x)
