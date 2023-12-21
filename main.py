383b028ab03e924f148cc5564c59bde1d2ebed0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:41 +0000

    kind valuable along want drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def convert_to_hex(n):
+        return hex(n)
