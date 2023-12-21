94a421f912c65e12bfbe39542adecb420e48f4ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:56 +0000

    quality attention method cut smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_max(lst):
+        return max(lst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
