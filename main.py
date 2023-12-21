8730a022fa037de6291e4b5252f7c9ecfea6cdca
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:18 +0000

    small instead confusing risk wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
