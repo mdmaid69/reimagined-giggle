ce874bbed701c4ad9f57233d211f115fdc9e33a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:20 +0000

    friendly square health light name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
