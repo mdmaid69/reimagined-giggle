efe6396e495bedb5dbcaed6be463dcff64df9158
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:27 +0000

    breath recover there bet report

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_tangent(x):
+        return math.tan(x)
