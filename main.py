84db2326679e1a4cc3f5e2ba6e7d7eba9ee53a31
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:28 +0000

    eager stir then appeal on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_density(mass, volume):
-        return mass / volume
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
