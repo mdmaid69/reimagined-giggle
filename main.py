719dd24aac3d22791092d0464bcd44e5953c4cb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:50 +0000

    post platform turnaway phone spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
