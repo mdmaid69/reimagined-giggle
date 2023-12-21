8da3838411a80dda2af10751845e9d011fb31e5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:17:33 +0000

    excellent under impression shock Oo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
