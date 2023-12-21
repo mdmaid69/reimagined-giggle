4a28e15a95dda25f7d75366ef4f406327fe11b65
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:02 +0000

    float year row everybody honest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
