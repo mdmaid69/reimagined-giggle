6c7c3e318cdc96bf54dfcdcb062a9d2ea447ed92
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:39 +0000

    getoff sit silent shame university

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import random
+print(random.randint(0, 100))
