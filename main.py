1fe0bc692e4f136ad7a02002285c25fdb5a46d26
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:39 +0000

    perform murder win join busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def remove_from_array(array, item):
+        array.remove(item)
