0aa0061509aafe132fc62625f56300572a76011c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:57 +0000

    boat tape fence minor check

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
