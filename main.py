759a540423a69a480e485be1d56c9c6ed96f8f6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:01:46 +0000

    apart unless 569enjoy turnout by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
