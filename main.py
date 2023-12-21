e846aaa9b0968f3a048c032fb29302f250ffd168
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:03 +0000

    copy knowledge judge opportunity science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
