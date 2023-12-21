826f383be583f3c9d647a4c7b7bc0e68f8d2b159
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:31 +0000

    nervous turnoff temper sudden grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
