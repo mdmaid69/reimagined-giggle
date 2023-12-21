369b449e5ae8798a06781df1214be5969ea47b80
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:18 +0000

    glad damage active type pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
