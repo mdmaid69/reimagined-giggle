2bc94cec54a7096365fe77bda7f8d9bd7b28dd63
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:39 +0000

    our attitude influence forgive mainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
