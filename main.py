74b63dd5a043e6ded67d09a43fe7fddfec404f51
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:01:32 +0000

    load regular forward strange sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
 def get_array_as_list(array):
         return list(array)
