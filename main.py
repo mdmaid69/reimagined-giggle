66fa82e8b98e55e043c4b40b8ec7a40cb44d79bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:15 +0000

    long us successful rarely film

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
