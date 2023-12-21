192009b6900936772c12a8ca9771639e8507f5ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:58 +0000

    love copy rain post follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
