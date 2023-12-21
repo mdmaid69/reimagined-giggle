adfad0c66f510f815b0dd037a38f717397eaf212
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:43 +0000

    rest Jj name gather match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def remove_duplicates(lst):
+        return list(set(lst))
