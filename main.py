82f05fc86c6ba4fe5c8990fb16574e53ed33bfde
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:38 +0000

    about breakout government us eat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def convert_to_octal(n):
+        return oct(n)
