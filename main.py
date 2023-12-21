741857f8027b0a64b02b7ef167b4eeb60957745d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:01 +0000

    attractive responsible sort getout promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def greet(name):
+        print(f"Hello, {name}!")
