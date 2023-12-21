68c13f752c31ac980919acf9a42f1802205db537
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:00 +0000

    event apply worse neither cope

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
