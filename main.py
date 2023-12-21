9a59d48cdb1ef7777995edb572ca74aba36e91ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:59 +0000

    unlucky occur shape miss fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
