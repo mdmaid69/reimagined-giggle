c090b1a50681ea055d52832819ce4e1e42ef21cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:22 +0000

    convincing indeed marry damage finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
