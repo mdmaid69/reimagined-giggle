4ec7d48a8b28a4e12f4c20765886a93e790a9446
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:41 +0000

    illness exact reason expect damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
