7785860dc2fed514bdae655b3929cedd65868500
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:37 +0000

    thirsty tax obtain anywayanyhow emphasize

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_rectangle(l, w):
+        return l * w
