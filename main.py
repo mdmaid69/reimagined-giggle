fa338d2b9253af54e79d54219b1c640577fb659b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:54 +0000

    help warm around important eager

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
