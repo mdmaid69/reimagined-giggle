101db7febb5f6768f7afc0ed8ef74465f82f5a99
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:20:11 +0000

    out throw process tense achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
