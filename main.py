eccaaace03fa9175936ccff0cbb74c7cca202764
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:36 +0000

    flat interruption treatment not shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
