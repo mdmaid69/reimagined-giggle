60d31e3ac2db5b85b434eecc60fa04b22a82d7e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:58:47 +0000

    edge guess on indeed hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_str(array):
+        return str(array)
