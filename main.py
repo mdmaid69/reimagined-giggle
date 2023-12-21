3f31f03fd6ccda4ebbd1ca2e5155ca2703325c9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:20 +0000

    dangerous period roll late confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
