598b21a6ddd51c9155a157a161bfc3a1dcf86f28
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:32 +0000

    than before breathe unless work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
