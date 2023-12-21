73114938c40fdea48e678b9047d89ebe8eed24a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:45 +0000

    contents crowded disturb 565end recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
