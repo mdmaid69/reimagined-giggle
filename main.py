cad158a412d9ea89e955e57cf03cab494d1703cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:31 +0000

    myself cheek punish shut exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
