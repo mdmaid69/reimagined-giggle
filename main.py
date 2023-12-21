6dcb68b5196a2ae7576abe0c54a7a792add38081
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:10 +0000

    plan centimetre happen callup refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_max(numbers):
-        return max(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_area_rectangle(l, w):
+        return l * w
