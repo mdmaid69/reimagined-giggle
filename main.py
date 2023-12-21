37b5f15eb1f319cae31172850301c2ec5f8a8e60
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:17 +0000

    public height form ear edge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def count_elements(lst):
+        return len(lst)
