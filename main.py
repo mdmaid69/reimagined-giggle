4b9cc6f15c888d2572be170f85270455637321c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:51:02 +0000

    flow clothes convince fashion rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_int(array):
+        return int(array[0])
