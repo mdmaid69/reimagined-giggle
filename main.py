ce70b6096069db0f293b7e1adf56703b85c27d6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:35 +0000

    lunch pour race throughout tap

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
