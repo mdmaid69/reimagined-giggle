441ceeea5c82ce10c695ea8a570abddba3155c7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:51 +0000

    just forward downstairs ever bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+import array
+def clear_array(array):
+        array *= 0
