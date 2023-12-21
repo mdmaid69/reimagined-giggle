0bac870e9eed5464eab90845065b882435b6ed7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:01 +0000

    father instead wrong unkind confused

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
