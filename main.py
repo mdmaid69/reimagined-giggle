563e9a41d7113918bae30a84dbe19056e54492d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:40 +0000

    sort afraid shout note everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
