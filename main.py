134f8cc789cd03710e1089782358dd673d084a2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:05 +0000

    goup result standard want command

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def divide_numbers(x, y):
-        return x / y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
