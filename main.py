30a8708a69fc8141cb0e421fc0f27a863609ab4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:01 +0000

    pure swimming listen well draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_item(array, i):
+        return array[i]
