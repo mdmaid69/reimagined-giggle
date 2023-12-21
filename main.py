b569f4cf40e654cea51e50cde1c245cb6a89e292
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:33 +0000

    watch wherever yourself company anything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_elements(lst):
-        return len(lst)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
