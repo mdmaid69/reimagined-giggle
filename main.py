daf2e7af4a8b16129003b6abe5ab75ef74d7ca81
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:14 +0000

    perfect his alone spot cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
