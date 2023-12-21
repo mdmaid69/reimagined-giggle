f10bbfd2719f71dc36a35a48359ac2fc5ec47f49
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:25 +0000

    condition none push hate once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
