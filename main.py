4a55a5fc9eba11f97b58563b03060516cd184ad5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:57 +0000

    tie manner connection authority train

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
