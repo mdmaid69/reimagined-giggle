61550050d44bee5ba1af0dc15708a43f6b1797db
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:57 +0000

    close anybody in right apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
