8e083bb4b1c07c1cb08fbbf8aebd6e90e5fbc382
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:04 +0000

    tough hit up label least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
