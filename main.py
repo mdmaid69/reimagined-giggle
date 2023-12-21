9f4a15b0fa7443f1aac0156ab8fc6753595c0f3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:47 +0000

    degree properly passage control event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
