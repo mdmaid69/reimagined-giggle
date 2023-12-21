2104a07b7a2f843478a627f10f1671cbfca0d3ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:35 +0000

    announce valuable straight steam safe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
