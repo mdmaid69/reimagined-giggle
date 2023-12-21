c04cb6867b24735f94b1f36567bb7144e99a0077
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:02 +0000

    or rubbish cause practice Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
