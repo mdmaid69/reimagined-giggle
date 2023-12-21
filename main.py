42985d6128618ed861437ffc93925d25741a332c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:12 +0000

    turn nearly hand mother bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
