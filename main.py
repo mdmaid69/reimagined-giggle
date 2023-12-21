e68bdb90396f4ae9c2879e9f58ebb0adad2c915f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:27:49 +0000

    moment frightened moral giveup wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
