a8398e55ec3326fb589fbf40fc985fa63657ced9
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:25:23 +0000

    mouth check boring list unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def convert_to_hex(n):
+        return hex(n)
