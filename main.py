684e51110c1f64340cf6b85167782357d06ca052
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:23 +0000

    beat welcome since even Qq

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
