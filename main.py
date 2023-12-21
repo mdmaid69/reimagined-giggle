314e97899e090a6d149b6f42429835308f0860c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:58 +0000

    freeze size wish weak picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_counter():
-        return collections.Counter()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
