39b23787564c59d9a96c3906e580372216ae4a6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:14:14 +0000

    prize Qq exercise wander speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
