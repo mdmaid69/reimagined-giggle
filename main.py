489104c1de25c0dcb2f56b079d4e4d31876d8ccc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:24:46 +0000

    flow travel bridge whole glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def log_message(message):
+        logging.info(message)
