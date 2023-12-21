546b0f3ded93a9d1e8bff99a1c374254cec91b5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:43:43 +0000

    there manage open Ee combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
