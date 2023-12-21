7a47fc6eeff3ff3c67910189db205cc204769571
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:39 +0000

    border rub begin common children

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
