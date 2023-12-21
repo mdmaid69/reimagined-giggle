506a1ae128f692b56fa4f20d29dcb8a2e590b7a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:04 +0000

    sport violence however keepaway regularly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
