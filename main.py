78e8334e7ccb07ea75f0b24f267473cdb7bf0f6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:39 +0000

    nonsense pot temper level march

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
