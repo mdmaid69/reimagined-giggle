da5777c57fcc7bde4d2644cbdd1bb064be7c1ffc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:07 +0000

    reply above official turnaway turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
