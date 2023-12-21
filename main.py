2f406b76b7ee3d76a2508598bb8cbbb6aad5328d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:12 +0000

    cum period wait regular contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def print_python_version():
+        return sys.version
