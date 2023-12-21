3c865becc0889fdf241eafb824f683cb1cfa8566
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:36 +0000

    out copy much room easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
