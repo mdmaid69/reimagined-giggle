ab6cb66db5411f0a064fbae9ba7df7969694ecac
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:11 +0000

    first stone geton eat limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
