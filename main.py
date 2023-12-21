ceb79a15b9dab75e69716def7ad1e95d80386028
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:06 +0000

    pay boring rain possibly sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
