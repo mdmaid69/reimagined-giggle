a6939587aa9f41bd9bd7b40f88f968d2f06a96da
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:10:01 +0000

    might finish pretty afterwards off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
