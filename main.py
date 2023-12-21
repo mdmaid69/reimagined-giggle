049704a8334df9c5671c9c1826ae23422c69765d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:04 +0000

    artificial coat authority remember support

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
