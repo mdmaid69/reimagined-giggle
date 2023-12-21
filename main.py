8b27808c7d4684fb6611532cb4fa49c8b1f58c52
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:36 +0000

    ground before count stand loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
