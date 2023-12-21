93865d186338f410ca1f788b09e5969814c1ff98
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:58 +0000

    radio instruction unfair sink treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
