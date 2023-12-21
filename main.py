b46018eae8826f85a6c269664d439a4f299e9f46
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:57 +0000

    favour surprise front tune part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
