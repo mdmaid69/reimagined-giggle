fc681f90c5ffddc470342574973310f99bc49225
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:05:23 +0000

    left tidy towards disturb drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
