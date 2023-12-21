2743e52dd420c4a8882ab5a140a9251019859962
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:53 +0000

    minute polite satisfactory equal extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
