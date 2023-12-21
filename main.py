3b7e76c461312bda3fe78eb2c9a271c0d16df992
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:54 +0000

    agree past wild exciting wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_item_count(array, item):
+        return array.count(item)
