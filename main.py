533f125a4a923fa24281a08f12b0496e8bed3c93
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:17:50 +0000

    favourite skill away surprised first

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
