1b7eb05a01f26957275bedc6789b58c7252fb044
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:52 +0000

    sight reason plenty turnover progress

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def sort_list(lst):
+        return sorted(lst)
