ef3bdf0a8ebf4a31ea5189bdffed51fde4aa9608
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:40 +0000

    splash length slight shock anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+for i in range(10): print(i)
