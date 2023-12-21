f8ab12931f6fc5390f1509433b7aecebd6d7047f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:25:46 +0000

    OKorokay feel contact any each

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-for i in range(10): print(i)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
