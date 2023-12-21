f7f9b4fba2ddba250451f2976d440ed099dddf69
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:15:55 +0000

    model that care one boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
