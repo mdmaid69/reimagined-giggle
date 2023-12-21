97487a373f39d878cfae21030bc922320fa4d911
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:00 +0000

    add their point mainly education

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
