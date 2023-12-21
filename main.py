626dbbb229b1df145a145315b794d026a2f01109
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:56:49 +0000

    drink exciting plain pause meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(x, y):
-        return x + y
+  def get_base_name(path):
+        return os.path.basename(path)
