ec54879ac122075d93de354f65f1eb2625931cd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:42:22 +0000

    neither concentration dream prevent head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
