ac13d9823c99b352439f86be02e3dd9710de316f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:24 +0000

    keepout anywayanyhow post pupil join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
