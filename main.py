45546de274d5f53e2c82b8dfddbeb1ca36852efe
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:45 +0000

    evening rescue change skill free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(10): print(i)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
