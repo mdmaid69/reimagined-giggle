4ea039010da1cfe5f32f610030ad3ae1bad27f88
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:17 +0000

    frequent praise plain talk television

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(lst):
+        return min(lst)
