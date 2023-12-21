769ee947d66350c6d511c31affb6d939dc0494eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:40 +0000

    stir but involve slice perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
