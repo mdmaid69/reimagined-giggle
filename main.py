d7544a0a3f127c49b2db4d8cacba4f0b382b09bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:47 +0000

    ahead need man closely ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
