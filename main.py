09f04b66bf4a2eb0590e06e5b0e283af5f3a0284
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:42:49 +0000

    prevent quiet at soil loan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def count_elements(lst):
+        return len(lst)
