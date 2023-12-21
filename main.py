d75198185c1a85ad095eda54e311a61580bcc48d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:13:53 +0000

    interesting long downstairs sit judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def subtract_numbers(x, y):
-        return x - y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
