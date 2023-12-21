52c627a63d65bde1a449f63108b31649cd9ec1ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:04:14 +0000

    arm care many concentrate design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
