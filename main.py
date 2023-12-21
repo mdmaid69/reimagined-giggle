da95c42ba0249e7cfe469abedd4503253b784d3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:48 +0000

    naturally impossible praise remain beneath

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
