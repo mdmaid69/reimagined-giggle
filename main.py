fca0ba4e0a23ee12c385c38a9de59b545c675874
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:18 +0000

    blow involve cost chase far

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
