408fe994142dd4fbccd97fde6e5d83fbc0930377
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:34 +0000

    fire level well so win

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
