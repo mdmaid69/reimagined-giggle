618dae12e79741e416bea668f22aad8a27149824
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:32:00 +0000

    especially force trousers keepup needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
