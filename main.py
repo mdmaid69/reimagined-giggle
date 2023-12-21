6e4d7578ded813c451c4537f510606f05269f067
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:59 +0000

    year church generally kill defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def set_array_item(array, i, item):
+        array[i] = item
