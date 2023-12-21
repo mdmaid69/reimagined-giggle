ea102032233786b9ad4dde7725717a7834f70d6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:27 +0000

    comeoff any middle table quit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
