dd934fc948bd1c266239df0a4a85460de941372e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:52 +0000

    freeze unfair behave street science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
