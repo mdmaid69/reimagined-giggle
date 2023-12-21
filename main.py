bb0b74fb54da4204c2d7d8c4e7d40a604c7ffd2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:11 +0000

    also round each normal slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
