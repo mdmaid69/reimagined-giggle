0e956e06747cbddaa743b232200970841349730f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:42 +0000

    still conclusion disgusting truth dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
