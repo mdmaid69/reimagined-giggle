3d078ea0268af0c5f3ec3e75bc9e0046097702f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:46 +0000

    since eager patience please except

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
