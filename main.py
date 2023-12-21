6001e0de823f534d48bed15eb0c6a087a76b5292
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:35 +0000

    loan description plan view equipment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
