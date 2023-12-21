34d101f862106d85536d7a096c437a32dbc8c78a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:31 +0000

    mistake aim damage expression chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
