b6cbb0222cc736a5ba017d339b121a4929693e82
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:38:57 +0000

    brush patience stand aim weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
