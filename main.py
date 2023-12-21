07f160bfd73a918748c83ce6380ab1dad04e94c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:54 +0000

    back mass ear dry temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
