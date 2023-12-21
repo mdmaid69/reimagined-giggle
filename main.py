eca2a639278a54f18bcf61e44951a13d006c5852
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:11:35 +0000

    up waste apart suffer fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
