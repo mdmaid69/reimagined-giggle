fd313d9d91dfe551bb02c38f6e1af81d18ee4b3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:53 +0000

    hot real education feel when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
