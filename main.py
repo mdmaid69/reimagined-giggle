0853a6710cc7b7624b063075267ffdb97eb9b440
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:39 +0000

    ilovedicks happy pronounce tall chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
