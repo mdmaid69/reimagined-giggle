bc1ed49f32a3dcc4c26b795ef5d912ee711082b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:13 +0000

    usually punishment direct relative comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
