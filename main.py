774482c21d0a2dbe782eb270c2310b1c2b37b6df
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:47:13 +0000

    speech Ww dry use earth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
