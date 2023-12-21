c942634174712ca3f5f0f64d3fd2cdf6df558a7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:49 +0000

    quickly so 569enjoy many real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.time()
