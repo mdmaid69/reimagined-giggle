a917514b5ee1c0e4bd13d7ebcb9557e00f01f375
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:01:36 +0000

    duty petrol hers mouth smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
