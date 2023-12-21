369fd48adafdb1d4ac264f1a391e3abfe7288dff
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:12:58 +0000

    base schedule nerve hope only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
