96e619bb02bad5af98573d10c037b384ea584fc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:33:03 +0000

    calmly like across spend responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
