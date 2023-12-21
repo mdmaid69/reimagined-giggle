5c1dd496d5dec0a7d7875438694ce14c8a36876e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:44 +0000

    importance organization along open everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+print(math.pi)
