0c71c0fa41c0e825e4fc7527779b10ce63a3391d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:51 +0000

    objection support emphasize information less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
