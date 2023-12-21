ac9b6627404948065c1baaed10ea6e53fe822bc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:19 +0000

    stir further ours keepoff computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def remove_duplicates(lst):
+        return list(set(lst))
