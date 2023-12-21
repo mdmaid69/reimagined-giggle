36525e8dafa0eb04b8ecf90fcc67b1a39da85220
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:04 +0000

    refer commit connection manage pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
