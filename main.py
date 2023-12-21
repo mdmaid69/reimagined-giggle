3d559e9d8624bebe957b4b64d7c1af654f1727a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:12 +0000

    usually every relationship relation that

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def get_array_buffer_info(array):
+        return array.buffer_info()
