58070ab8108124c61360ab1dd1c5ce3c49c9769a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:07 +0000

    tradition worth software forget exactly

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-text = "Hello, world!"
-print("Characters:", len(text))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
