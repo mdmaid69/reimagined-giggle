47f931ba127d40a1b3673b115801e7f7eb27cb2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:55 +0000

    relative pride comedown hide there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
