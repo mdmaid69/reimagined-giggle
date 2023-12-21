a7cb38586f8d99050fe29df2cda1228480dc0257
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:30 +0000

    few guilty mark season apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
