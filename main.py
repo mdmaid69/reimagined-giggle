935dcc9e0a7e5d5c88d1e5b4bf66548ef54b1688
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:49 +0000

    window false bit careful style

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+name = "Python"
+print("Hello,", name)
