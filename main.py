147176bacb2bc28fc57ea61c1fcb3238d1f4fce8
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:31:40 +0000

    telephone during trade frequent quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
