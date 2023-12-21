e3cf3e410c6bad7b81184d540ede9cd3646ff1fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:58 +0000

    because sink admire there punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+for i in range(5):
+        print(i)
