9aea8d6d43c2b6d914d6ddce4769bad85c6c18af
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:58 +0000

    yourself reduce car exchange land

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
