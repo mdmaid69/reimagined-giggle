b6b510a2a534a96078a32557874cdcc55caac018
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:30 +0000

    right main takeoff continue meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
