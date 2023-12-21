1ccec5feba584bee4b0b7c18f5a779acd658d00c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:17:33 +0000

    shock think message minute slide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
