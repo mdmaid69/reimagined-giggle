ac8e5eb619aa02dcaac86f7b3f84723397358a18
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:07:08 +0000

    comeout over stranger terrible little

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
