11fbc70b6e6ac70eed3fa553508f4cbd0ad47b42
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:33 +0000

    contain underneath cause duty soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
