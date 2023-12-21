3208856b4f7649b12695f5554b5c1447af6a4e7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:55 +0000

    jump situation movement instrument judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
