68d7a456e6bbec22e8a10cb517119be515d0a58f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:37 +0000

    similar war tonight frightened exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def square_number(x):
+        return x**2
