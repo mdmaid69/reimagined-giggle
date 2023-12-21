4651d849bd466dd854983a90271cf8240b795a7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:27:55 +0000

    maintain threaten hungry fix protect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
