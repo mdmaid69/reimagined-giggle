e00bf04872c64236c7fd7e727cffc142caf3a893
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:55 +0000

    urgent discover while letter whether

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import getpass
+def get_username():
+        return getpass.getuser()
