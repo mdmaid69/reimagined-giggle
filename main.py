677dd2d39d59e3f7949cf3b18b839305c22a74d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:18 +0000

    cold gooff birthday rank die

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
