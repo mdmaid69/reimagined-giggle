9972c3adcc94feb5e0822a53db7f624a3db1678e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:53 +0000

    anyone stone stiff yourself belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
