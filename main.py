326dfbb8dbf38d3a6a6699131ba04e7b8d9eeace
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:13 +0000

    honest choose prove realize software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(5):
-        print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
