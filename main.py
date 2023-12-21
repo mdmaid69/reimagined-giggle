359415c29fa724c5dc05306616bfc4c6f794421e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:23 +0000

    complete children lookaround different only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
