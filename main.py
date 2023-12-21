a1c16c8aeaca3822e844ab38840ce0b67bed1aa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:58 +0000

    swing approval clear profit apart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
