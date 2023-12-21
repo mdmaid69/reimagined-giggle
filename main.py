7f64acda4b48bc74dbdb5d51df233259eecad9dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:30 +0000

    trick female brain hurt agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
