4bef6e9c20ad32c83949ee05ea045dafd7b67ee2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:08 +0000

    accidental once go drink count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def convert_list_to_array(list, typecode):
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
