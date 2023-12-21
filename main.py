bde3c78022fcdc01874585066cb067e72abb8e05
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:31 +0000

    my exam manage worth idea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
