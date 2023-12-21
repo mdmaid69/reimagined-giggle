e5e1db669c1bb2435b7db0f30eb21654259ed6e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:58:10 +0000

    base neck compete phone wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def find_max(numbers):
+        return max(numbers)
