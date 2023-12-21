9f0962a16fcd1f16516238376cf120936744ce84
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:07 +0000

    problem respect pen characteristic street

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
