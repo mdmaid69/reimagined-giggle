04bfe831cc8837fb5675f949ffee7f89dc82ff9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:22 +0000

    ground probably solve deal search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_speed(distance, time):
-        return distance / time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
