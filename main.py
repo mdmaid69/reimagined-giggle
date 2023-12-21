471a047a203fd4984ec0f60e7a64f54c6dbd6661
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:25 +0000

    every foreign might branch complaint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
