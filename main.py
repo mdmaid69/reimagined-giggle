fdc1ac06149957f3a1d83d8c50e4c8788e17bf42
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:01:53 +0000

    strong used figure frequent turnout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
