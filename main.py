b0d2b1d2996057049634a2d14b0ea6d1193098b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:25 +0000

    gas subject spare land part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_min(lst):
+        return min(lst)
