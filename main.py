c3097a9f35c1f44d01cf7fa213a4d171552aafb2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:10 +0000

    rule attitude declare order dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
