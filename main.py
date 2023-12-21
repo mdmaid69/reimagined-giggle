cd2d0c39d9d810cbc5f5384cd9d449e135d8877b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:01 +0000

    free rest exhibition hollow chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
