5c7f5bb3d4d2b644b5adb7b80df216e75ef03eb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:01 +0000

    perform leave second please between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
