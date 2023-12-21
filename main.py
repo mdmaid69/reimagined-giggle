e85b5a65121d26168f1c0e04eab3d53c9478f608
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:51:21 +0000

    protest strict unkind cold square

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
