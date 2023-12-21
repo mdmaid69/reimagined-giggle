769b1465d2e591c7db764f241dbc32219c218215
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:53 +0000

    lookup club let fall opposite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
