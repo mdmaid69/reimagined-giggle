69f43e34d2c067e29a17975699fa9907f43e445e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:20 +0000

    bin taste harm manage better

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
