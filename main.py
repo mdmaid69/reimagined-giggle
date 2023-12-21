d9b7dadc4108330af0438a320885ef0eb6891f22
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:49 +0000

    joke pick replace score popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
