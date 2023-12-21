e5bcae870cda4ee34a03b376fb12cb746693ac41
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:20 +0000

    air that central 567enemy dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
