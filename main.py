067ad2e320db78d05c60abe95a5c47b70f236462
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:05:57 +0000

    properly impossible achievement great unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_string_from_array(array):
+        return array.tobytes()
