68face19ae75d9e60f299a3475b2d1a096f9d300
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:16 +0000

    could nature practise exit advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
