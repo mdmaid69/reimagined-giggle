5cc59543a57322e4b80fa0d070fd8688933272ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:59 +0000

    explain danger dry pair talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
