d7ff798fc691cc100687a95877b23b6cb9df86ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:54 +0000

    feed praise know result have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
