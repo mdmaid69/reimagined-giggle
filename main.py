bea1091e290bfca46a9a5e0e7bd7f1cd2de1da7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:35 +0000

    place consult future disapprove promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
