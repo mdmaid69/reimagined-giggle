c9fcf872947dba296eda5d02a250d71c6507d87a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:27 +0000

    mind nervous sudden together tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
