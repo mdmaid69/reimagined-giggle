d3e1503184ec5067736d7c5315e16a8970716d09
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:02 +0000

    community some food border motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
