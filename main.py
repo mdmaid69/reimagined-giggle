1b9e58eea012a614279a8a7eb8c2d30530ed4833
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:25:06 +0000

    impression rob history prefer week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
