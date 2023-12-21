a0f8658b23ea8cc81c6b5509773460f0b54c0ea8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:50 +0000

    hot turnback first gift nose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
