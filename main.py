bdff39edc137380f1a28c0030b251c14080a4262
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:06 +0000

    introduction fit task happy protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
