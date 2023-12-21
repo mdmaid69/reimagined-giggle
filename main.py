6d71048c7ffee87112ce69182cc98dc223bb5a54
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:36 +0000

    that band recommend square game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
