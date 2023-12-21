b6654cef475d6833d9f677ae93a011af38249be4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:15 +0000

    coat draw happy trial avoid

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def subtract_numbers(x, y):
+        return x - y
