ea979a2ce0e23d99694f90a6e6f163b85d90acae
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:17 +0000

    take majority complain sore enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
