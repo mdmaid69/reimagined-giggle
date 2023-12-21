ef749e1c60bb3f5a1a4bb2d5621a3fa21c231d6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:00 +0000

    something rule wide difference on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
