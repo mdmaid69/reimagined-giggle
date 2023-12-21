f17eb55c6134e3099ff48c4077111baae744f49a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:12 +0000

    basic feature worry be central

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
 for i in range(5):
-        print(i)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
