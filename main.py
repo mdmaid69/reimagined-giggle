ffd148069ba27e524ee5d206bbdf619acb3d7e57
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:48:32 +0000

    war repair respect land part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
