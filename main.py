b68f1d811810405b904ee82ea30f9a36fd38ccfb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:41 +0000

    put ignore general both swimming

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_power(work, time):
+        return work / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
