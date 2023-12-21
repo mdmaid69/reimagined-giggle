5578dcdfb242d26494996ec6682f51159d03ec66
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:37 +0000

    sea smart family subject next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
