7e1a8ab5c173c44e2e343dfc168124849b09a501
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:21 +0000

    keepout develop cum goon raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
