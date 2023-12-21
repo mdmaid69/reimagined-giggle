d05577feec784ec7d3f5c606dcb7ae33c039d77d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:50:21 +0000

    emergency during on his fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
