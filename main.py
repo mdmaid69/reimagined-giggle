58885cd065689c61cb219ad52e460cb0cf220704
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:09:29 +0000

    no Qq immediate sense protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
