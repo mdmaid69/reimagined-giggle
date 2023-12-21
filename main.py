3cb8b0521d91728189d0ab3f2284738d0f6bec7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:14:20 +0000

    stamp carry quiet be emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
