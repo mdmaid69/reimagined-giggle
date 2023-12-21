2617a89f36f6fd7915c4f3af57ed3069f0df914b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:07 +0000

    bored breakthrough comeout wash for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
