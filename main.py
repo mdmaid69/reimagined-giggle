f67e4d8bbc51e4de1f5fb17b0f59bb6f1db5df96
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:56 +0000

    week meeting punish help afternoon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
