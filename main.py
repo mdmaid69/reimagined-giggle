a91abf6f350b7f0015311d5319bb80c1e6f046be
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:40 +0000

    high comfortable rest rest attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
