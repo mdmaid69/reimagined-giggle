7c5b226e3f7679eddcbcf3b3b9ad54234e3ad85a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:08:43 +0000

    brown freeze own walk Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
