2a273bf2c4b9c2c9e01936c57272c54c0577923c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:35 +0000

    upset convincing day at laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
