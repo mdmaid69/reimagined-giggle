b3cfd4a055e264e0ce50c26fb68c11b4589fd70f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:53:15 +0000

    notice convincing rain quickly sometimes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
