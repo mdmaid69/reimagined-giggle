95e2fb7f158b331adf3bef3a1891a238edcd44e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:47 +0000

    reliable perform arm suppose education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
