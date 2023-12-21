d623fb5dbbcd4cb187a416e82face1b62f7463c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:43 +0000

    best combine float combination when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
