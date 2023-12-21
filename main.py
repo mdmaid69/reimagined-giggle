8ae5739534c7a3951f1eda17228d654401ca826f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:44 +0000

    through mixture block piece spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
