b5454cd11e289d581a54db90c2d72b08e2571efb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:49 +0000

    mood along quit joke use

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
