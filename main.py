8a77824bb3173f2d05d1a452530634a04d3fff1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:30 +0000

    throughout needle result force maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
