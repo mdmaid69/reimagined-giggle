efad4e5bef3f47e96dc3e645344bb228bbf24a64
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:22 +0000

    match rise fix uponon unable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-print("Hello, world!")
+import random
+print(random.randint(0, 100))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
