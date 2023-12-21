1a80f456196925c3359fa7a32d864e575b5369eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:43 +0000

    Tt consider abroad matter lonely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
