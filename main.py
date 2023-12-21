eefdd8ad91c492cc60a85f8e83177842f772d00b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:26:18 +0000

    secret event meet along term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-print(time.time())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
