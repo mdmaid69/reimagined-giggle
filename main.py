8a4fe6408171b5a92838386fdf31b596f3c0beb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:32:04 +0000

    brave fine shelter race guide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+text = "Hello, world!"
+print("Characters:", len(text))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
