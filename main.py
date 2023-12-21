b4d5172ebf39270598b11d7978dd0f4e9eefe4e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:23:58 +0000

    quantity mail argument knowledge memory

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
