d8cef961e6af323d66310f4307b305887844ebf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:10 +0000

    future lead sport consult shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
