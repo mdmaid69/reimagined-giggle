86fb91cfccc79ec28920603d74261110a5fcd131
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:46 +0000

    the tidy nigga long its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import collections
+def create_priority_queue():
+        return collections.deque()
