ea5bdca06e5849acaf688153918fa5d11ac4c24d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:01:06 +0000

    style this past flat smooth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
