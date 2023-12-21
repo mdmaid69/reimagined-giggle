1c6793d210e335eef42dc653450240bfc163a1a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:39 +0000

    sink angry getaway nowhere purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
