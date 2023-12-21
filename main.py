c69fcb7c6869f9044a11ef64559480cf0b60441d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:11 +0000

    sea use steer lookup hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
