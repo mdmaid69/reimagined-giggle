7c6e2931109db8c6c6a8f9b31ae79527da3b9fae
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:32 +0000

    trust group use Yy condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
