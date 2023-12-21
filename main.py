449124fff8b79baea4d9b2e1ecf42416aae23d04
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:29 +0000

    taste scared instant mysterious watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
