d013cc018928dbb82cb30d1627c6fdb998b4d717
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:14:39 +0000

    permission behind think mother keep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_tuple(array):
+        return tuple(array)
+  def sort_list(lst):
+        return sorted(lst)
