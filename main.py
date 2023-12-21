0354d2f579e4ef7f3c7e72ddd2ae00e66022e0cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:12 +0000

    separate score pain bite praise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
