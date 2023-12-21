3c26b6713f14ade74dd34fc1acc5bf1d65d8c471
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:30:26 +0000

    throw pupil issue call consult

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+print(sum(range(10)))
