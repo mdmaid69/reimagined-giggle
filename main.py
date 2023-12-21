7f70be05bf9fb723880ed0f18e615e8dcc2dc29b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:05:50 +0000

    comeout swallow stamp careless use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
