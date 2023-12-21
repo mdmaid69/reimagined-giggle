e8f3109dc8222b70c58cb1c2338aa6c709213841
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:04 +0000

    city stop more invitation traffic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
