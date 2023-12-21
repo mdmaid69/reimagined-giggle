d8019e7202fe7c13ca384ff7624f24b378513770
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:27 +0000

    respect notice ignore understand tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
