5ebf5c9b7f3b877217817a4bd9d20d9d59ae7595
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:27 +0000

    any stand season mean preparation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
