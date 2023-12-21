7972ea20264351aaf64af4b10d70622953fd8740
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:02 +0000

    burst slice set live sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_string(s):
+        return s[::-1]
