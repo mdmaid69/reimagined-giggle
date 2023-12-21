15c544abfbf03479a34adb0a08ce5bd0f8b74ed4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:02 +0000

    standard detail steep feel report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
