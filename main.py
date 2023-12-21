70acacfca3bbf0300a4d9b46674e403b47fdf432
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:25 +0000

    thinkof swing wake anything underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
