2318ac08e07eff7d5442e146c1522fcea3801139
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:06:55 +0000

    completely national impressive second breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_max(numbers):
+        return max(numbers)
