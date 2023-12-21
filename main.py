902d694b0676bdd29a17388b3f5ec8a7a41aef07
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:14 +0000

    nose score desperate high grand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def clear_array(array):
-        array *= 0
+  def count_elements(lst):
+        return len(lst)
+  def reverse_list(lst):
+        return lst[::-1]
