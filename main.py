551fd13385d974d37f16ecab8b2db9fa04628d90
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:46 +0000

    organization theirs half naturally blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
