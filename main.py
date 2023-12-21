de792ba50e1bff376e93fe092cd73f62e39ab3e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:42 +0000

    help earth squash weather trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
