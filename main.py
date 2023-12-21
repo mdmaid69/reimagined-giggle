63c5a1b117e3121fba51f21802b6a3e2eac730e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:06:38 +0000

    narrow interview day present journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
