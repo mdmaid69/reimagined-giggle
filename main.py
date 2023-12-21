8bfda872ee0c41ce23f1b8aa15637752a3668ca3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:39 +0000

    relation correction spot search tight

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
