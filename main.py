1a44f6a1ebefe04512857da5b2a58bd8a3033bab
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:14:23 +0000

    involve remove suckme flood recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
