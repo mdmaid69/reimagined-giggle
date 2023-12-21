8ae113e3b08eb9fe37019dd2884890849593f349
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:54 +0000

    impressive left independent whenever quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
