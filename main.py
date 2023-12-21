693e161d5ee5232cdcf7808acb6b81f3c2e83421
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:45 +0000

    wear reject exaggerate like story

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def reverse_string(s):
+        return s[::-1]
+def calculate_power(work, time):
+        return work / time
