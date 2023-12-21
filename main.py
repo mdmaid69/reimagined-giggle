55a6245a126cbb0f90342256745fcb1738fa092d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:03 +0000

    callout tragedy tool trick variety

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
