a9ce3ec260e8ab61a1c2066098d2fab18d630f7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:46:58 +0000

    entrance country confident takeout getup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
