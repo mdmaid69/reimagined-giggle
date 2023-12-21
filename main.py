32ef669ad3f4559860d1f955d6411955ea72deb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:34 +0000

    Ll getaway whenever tell fortunately

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
