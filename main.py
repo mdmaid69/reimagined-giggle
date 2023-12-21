7f1901187d11a2d2a0ee21329e1a552a6c567dbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:45 +0000

    path copy and safety shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
