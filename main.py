c2be82e5e91af3e20e74b6ddf8937d0271ee1106
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:41:58 +0000

    hollow identify ring our secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
