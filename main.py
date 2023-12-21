ca48efa80a1114dda000b388af977539d131fd1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:30:46 +0000

    will wonder across various exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def square_number(x):
-        return x**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
