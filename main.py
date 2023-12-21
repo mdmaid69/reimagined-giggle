45303916548caf80c34d41edd390afe0fb55ea04
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:46 +0000

    mean awake short ring club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
