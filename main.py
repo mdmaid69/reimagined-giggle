f8549907806f221dbcb2c6bf546b8ff52828c606
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:53 +0000

    poison steer friendly same above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
