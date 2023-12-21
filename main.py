f4b45598f8ba6fcda50624f51693ab2b0ae7ec87
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:52:35 +0000

    scrap afraid force familiar size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
