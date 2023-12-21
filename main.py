a7010e70adf0930bd34bf905240e23b3e3c6546c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:37 +0000

    cheat wipe mail fire will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
