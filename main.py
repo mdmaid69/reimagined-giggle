c60d2132a7aba678cd6ad0f6547f19dfb434d361
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:44 +0000

    after shrink ourselves season group

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
