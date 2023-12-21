3b8c02562e277f5cdf6c8baad14051e1d1a440d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:17:05 +0000

    569enjoy less pure mainly dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
