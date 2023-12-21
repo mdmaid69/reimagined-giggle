2c9c0e60eb53340296a55c089e58ba947f4eb16f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:12:48 +0000

    objection shallow suckme interview field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_item(array, i):
+        return array[i]
