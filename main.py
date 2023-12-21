b4b691935a6cf4288ec8629ed0b28b0617a43c96
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:28 +0000

    correction date size that below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
