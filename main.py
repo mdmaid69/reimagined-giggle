ec094df91471d208925136d44528797530df7646
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:43 +0000

    march exit file wind bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
