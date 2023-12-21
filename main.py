e85c5bfb1aeca66b9c273648a325c3895cf67e96
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:06:30 +0000

    charge dress stop warning measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
