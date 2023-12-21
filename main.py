d2af79b19c6f43ed4c5590353167e6776e291987
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:51 +0000

    smell lean confident reason innocent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_max(numbers):
+        return max(numbers)
