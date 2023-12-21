afd15bb769ee97b88ed154b84f5e2412dc938209
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:56:59 +0000

    surprise extraordinary welcome play freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
