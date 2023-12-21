4319d48051cff101a41721f898be576918575d13
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:40 +0000

    later listen detail feeling serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
