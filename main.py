2aca3ee61fba2e9ecbdcab1e2a69459544a96588
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:13 +0000

    join debt either set reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
