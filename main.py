98408de925065674ecb828d3749bb99e080624a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:43 +0000

    right place value majority none

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-print(math.pi)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
