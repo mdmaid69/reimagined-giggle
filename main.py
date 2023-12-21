61bff878636f174a8e7fab94fad57bad733028a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:08 +0000

    my air ground clean comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
