24209785c297b9a7c4319115786a13e37fd3b497
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:11:17 +0000

    seriously aim wish wash progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
