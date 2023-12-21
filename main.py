804121c6f7c8473facb2017fcc757b863b2a83b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:05 +0000

    correct common sure scheme blame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
