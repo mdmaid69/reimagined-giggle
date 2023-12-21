4accf584d350e4f7d017b104d1f2bb4f162eeed5
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:15 +0000

    joke Zz distant punish mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
