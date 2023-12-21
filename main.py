1d84d4bf9402047ded132c273d3ad161bd9799d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:20 +0000

    birth actually probably secret continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
