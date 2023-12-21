c90e502a88e27f8e3f43b0126601e1af3a523fbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:50 +0000

    skirt both public yes moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
