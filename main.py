f3ba6ad37c306e67e163013ccc0b2fd18bd37543
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:32 +0000

    prevent aim flat aware bother

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
