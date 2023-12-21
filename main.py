b417dee04d6cb35fa544df0764162fea0ee18a41
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:37 +0000

    pinch purpose holiday manage heavy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
