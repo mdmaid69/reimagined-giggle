ac317b36f924c2afa32f7b6a738aa1f1bae2f628
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:54 +0000

    cheek cut by joke hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
