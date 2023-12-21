2c7fcc2d9de3f23e98802a463073c7a08284f454
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:46 +0000

    bet exact confidence tone pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
