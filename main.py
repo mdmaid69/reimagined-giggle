b6062690c664a05138f27511722fbf552a328c38
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:13 +0000

    question straight tell lookback(on) statement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
