63782b68d8c14311dae75a47cf899f04390e8ccc
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:52:36 +0000

    572enter argument word drink day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
