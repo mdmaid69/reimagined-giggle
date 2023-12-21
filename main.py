f65b4d5d39e275d6267ffe3a772084f98a592798
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:48 +0000

    gooff dinner blame treatment 571enough

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
