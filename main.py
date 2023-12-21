2eb334b1075f7e2deff920eab7b0ec1f445a95ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:38 +0000

    use dream choice marry knock

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
