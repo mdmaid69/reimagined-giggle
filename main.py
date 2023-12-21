44c17aa1768c8135f5396fe5d7d38292b9afeb92
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:58 +0000

    disagree happen convenient add may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import getpass
-def get_username():
-        return getpass.getuser()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
