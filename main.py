33fbab8d86a0da78993e64127b35cdf6e4d1f250
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:49 +0000

    Mm body Tt loud instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
