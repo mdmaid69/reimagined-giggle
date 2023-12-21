0efbb736d9b4a447e75b30a64b698ea8c3f3599d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:58:27 +0000

    plan surprising use wake congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
