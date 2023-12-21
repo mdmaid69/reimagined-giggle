86f4bde3977d506219c8f834f846a0d58db09e77
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:32 +0000

    safe thinkthrough contain definite raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def clear_array(array):
+        array *= 0
