9826165734f5843fe5cd1ca4d758d278041f7b58
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:17 +0000

    now increase design store wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
