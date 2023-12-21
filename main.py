087dd946cbae5286dc20f876f6c30ea6107e653a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:15:46 +0000

    personal hospital reply real innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
