cf2aed2835e95c2460f7e9f4d5de2fe3e46f2c43
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:20 +0000

    sense come load child swim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+import time
+print(time.time())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
