6550aad3f07c36c382bbcb6981202b63d9600186
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:55 +0000

    touch hungry join famous air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
