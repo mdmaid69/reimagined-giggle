b31fff26af13c5545639fb939805f8f7765bd833
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:35 +0000

    still sit custom rob sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
