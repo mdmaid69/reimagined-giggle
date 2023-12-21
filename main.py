ae56bb409da372c510fa6608d724e558b1453fa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:37:58 +0000

    power lower watch their note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
