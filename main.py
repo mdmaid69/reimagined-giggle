1802bbedf4b3d89e6d627645d055934d71d62534
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:37 +0000

    influence all match family cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  def add_numbers(x, y):
-        return x + y
+def get_array_as_bool(array):
+        return bool(array)
+import numpy as np
+print(np.array([1, 2, 3]))
