f578ac1f6feb2981009b65bff5039c4edb42d69f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:44 +0000

    evening badly takeback load back

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_work(force, distance):
+        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
