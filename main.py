528645e4ceb6b51e314218d0179dde88d99051f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:45 +0000

    challenge value exercise effort vote

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_complex(array):
+        return complex(array[0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
