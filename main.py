ad04689b59bd04f07146db689e2fdb5a1d742a0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:32 +0000

    manage wonder destroy wrong lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
