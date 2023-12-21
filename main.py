4626f23bf38269bded6bf2c122d3f4a9f59927b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:57 +0000

    fortunate Zz bottom anywhere progress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
