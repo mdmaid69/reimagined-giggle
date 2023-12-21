d70c159e838b2f83347b224dbec429e90d2c74a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:11:01 +0000

    brush unless muscle press excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
