b433e73f0c84c4729c912dcdcb5d7e4f1b13570d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:27 +0000

    deliberate start inside politics runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
