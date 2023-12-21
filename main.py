f6f496a0294a90d3ceed2160ee2dde4c3b5f236e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:43 +0000

    rush Gg godown Vv further

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
