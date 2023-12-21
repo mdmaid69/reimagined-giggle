27697e8d66cd3c2f8d5abea4ee0f935785203de1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:25 +0000

    light thought practice amount cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
