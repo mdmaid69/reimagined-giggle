d27f557120a92aa544e56f0ee704584eb44df49e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:13 +0000

    voice idea married force be

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
