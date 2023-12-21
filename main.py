f97a80d8b7c786aa3e28056335a9205ca9635ff3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:39:55 +0000

    uncomfortable muscle major sensible nowhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
