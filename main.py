e222e087febb6748768693daeb4e77aadbc37e04
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:49 +0000

    sink impatient ago appearance first

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
