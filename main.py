c73b55033366c858710b6c7c86d612c007ac0d8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:35:54 +0000

    leave relief standard conclusion Itself

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def find_min(lst):
-        return min(lst)
-print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_pressure(force, area):
+        return force / area
