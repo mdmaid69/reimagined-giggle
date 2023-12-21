f37f0e40382cc915ce28c07c79d070b683e203f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:47 +0000

    unite borrow condition impression stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
