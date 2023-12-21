5dc108014e1425503956ec734db58b4544e4d9fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:49:02 +0000

    statement night curious successful support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
