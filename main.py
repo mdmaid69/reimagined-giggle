552269a1ba443bcd429834ed04c647d122069d3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:28 +0000

    comeback another suffer hot twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
