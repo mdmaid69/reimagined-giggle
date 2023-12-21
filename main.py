a9a35c4ccdec4c9ec0cbd127de42e114f7a2825a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:09 +0000

    classroom impressive belief mystery close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
