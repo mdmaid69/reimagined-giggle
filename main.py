a0e47c5bcc8ece0ccee85e93cf675a7e624637d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:20 +0000

    break ready interfere minute regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
 def reverse_array(array):
         array.reverse()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
