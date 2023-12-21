65a84c45fef757bee2d783c622ad4baae6764972
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:04 +0000

    direct limit talkback sound fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+        return os.path.getsize(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
