d9034374e963d59c4a2c8b382b4366033b4a1700
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:48 +0000

    pain start advantage Qq wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
