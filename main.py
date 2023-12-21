65d9583685495a09ad8e1913e40628ca9c3f6357
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:07:52 +0000

    lookback(on) niggaacademy combination question think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
