64853b45e7d650a514b9452c419c86ad4a4b3860
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:46:26 +0000

    pass true main colour embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
