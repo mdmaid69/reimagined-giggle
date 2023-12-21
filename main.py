67d6df71faea7b4711a57c81497ef66c6519598e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:25 +0000

    circle split unhappy store principle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
