6a978152f37a5e3fa32cb9aa7c547c03ba735b1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:18 +0000

    mass show doubt cross earn

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print(sum(range(10)))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def clear_array(array):
+        array *= 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
