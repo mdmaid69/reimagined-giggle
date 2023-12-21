9c6e77673c8774b588be99f17df37cdc4403cb33
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:17 +0000

    strength report balance scrap refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
