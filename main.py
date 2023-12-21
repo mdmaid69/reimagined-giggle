ac000b8f43afa6362d7254c70e3431f93375cd1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:52:54 +0000

    underneath well justice pain bridge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_from_bytes(bytes, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
