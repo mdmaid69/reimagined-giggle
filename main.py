bcffbecec93f8953b0a968232fd68e5fa3ebd32f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:14:37 +0000

    blame decide tool whistle piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
