ebad1695f82618499d79c33478d503a8aadc305a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:14:06 +0000

    speak wet call protest Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
