507e0980e7272dd05147575a69fa49c2b4dc5622
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:28 +0000

    embarrassing lunch fit taste pump

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
