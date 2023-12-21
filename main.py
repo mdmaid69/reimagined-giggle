9f326ade77eedb9acca1970b3edab6c89b3a84ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:17 +0000

    everyone fine begin smoke cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
