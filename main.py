5d7b95ce99f187880a190d3e89c55ed8eec02f78
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:28 +0000

    plain deep heavy indeed raw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
