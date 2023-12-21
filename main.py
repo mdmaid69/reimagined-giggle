a4b6a3c8124b2fee810d5aad1bb522c433b3e6e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:19:23 +0000

    recent cruelty activity deliver niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
