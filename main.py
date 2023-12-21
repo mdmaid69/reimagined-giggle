06c877807185285fbfa6cebce0a9320a8975ee53
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:58:10 +0000

    squash workout woman concerning age

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def subtract_numbers(x, y):
+        return x - y
