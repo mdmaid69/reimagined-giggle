d1e4eea33db1cec3482b2a88f60c9d94d9564d71
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:52:54 +0000

    begin estimate about part upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
