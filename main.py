349eb1d9a8e6df9b9f69cbfbb316c26fd42050ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:35 +0000

    release Tt Ii identify mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
