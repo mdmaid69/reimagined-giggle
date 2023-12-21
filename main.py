5ffc5178926e01573e0e1a7865c14a450ce4f367
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:40 +0000

    continue draw never question appeal

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
