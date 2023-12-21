c5bbae2510562d185b0c578d16dc7a786e97211c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:11 +0000

    belief turnaway habit total post

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
