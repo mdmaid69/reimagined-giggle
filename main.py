d4f66d147b755b8545fdd5510965e208dd4660f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:20:45 +0000

    best pile flood computer workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
