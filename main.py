d3b090e96696818d78223a0c0ab2bd01f9388172
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:24 +0000

    Ll miss value fortunately throw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
