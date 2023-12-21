5bd3773b4f239a17d4dffa595c84cc120df63cb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:33 +0000

    yet phone floor tell agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
