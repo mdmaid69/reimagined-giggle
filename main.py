6de488bcdc4ad2f6dd503055da6f429f2e88ed99
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:56 +0000

    nurse artificial memory safety unlikely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Hello, world!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
