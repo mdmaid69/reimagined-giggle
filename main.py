a401a356955fcdf0692ed9fa9e49bf53ecd08ce5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:29 +0000

    round flat road keepoff bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
