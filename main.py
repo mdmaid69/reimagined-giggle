49d4b0718df052008be6d68dddc7424efd9000ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:09 +0000

    on telephone date place can

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
