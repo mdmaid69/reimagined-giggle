4ecdc0db0f94e2a211ffc1b904bc0b51826157fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:12 +0000

    combination personal clue hospital consider

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
