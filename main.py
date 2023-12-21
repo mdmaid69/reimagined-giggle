dd05a1e673e7b107119fb62f5e881cefbf32de1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:00:20 +0000

    influence agreement soil safety cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
