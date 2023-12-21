f0e895d2713b1ec96718075f2daf04f659b5787b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:38:02 +0000

    murder favour feature very children

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
