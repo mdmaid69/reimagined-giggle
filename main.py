68df61a1cadfb2860bace3ed9c975405e4490822
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:58 +0000

    breathe unfair trouble tonight nice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_typecode(array):
+        return array.typecode
