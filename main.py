d2abaea04f78a8a17d3f79c974c91334e4b6b034
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:34 +0000

    degree how perhaps thanks thorough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import os
-print(os.getcwd())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
