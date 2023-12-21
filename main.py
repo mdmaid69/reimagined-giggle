66f89eeab7806b850d3d381feaa31ec1cd727aa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:29 +0000

    grade breakdown shrink however disappointed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
