2ed17e12c9e6d2d2e9d17ba002c995ee1b9fdfab
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:23 +0000

    collect respect pleasure talkinto admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
