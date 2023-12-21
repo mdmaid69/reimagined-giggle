ea12d1d91849fe84926a1bc29299518a7b379346
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:25 +0000

    little traffic wire pollution feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
