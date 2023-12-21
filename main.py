6c0f9d3ace08250940669e013b9e8c44964b02db
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:50 +0000

    introduce abroad future corner OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
