2b67d116bb3e3592d3bb720cea2c5f96ea03d6ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:13 +0000

    soon careful education team introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
