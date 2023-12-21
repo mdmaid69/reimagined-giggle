e757afda7b46c5074e3a7ecf5c28a5a87bde4ede
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:02:59 +0000

    bet minimum smoke sweet sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
