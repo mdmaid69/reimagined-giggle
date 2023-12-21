623351d674d9b229dd60777b5cb3fa94c26bb6c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:56 +0000

    job stay custom rub vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
