4bbee4c0b719a0f7ebc9c6a6f378da3bcfb476ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:31:48 +0000

    concern common note sticky Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
