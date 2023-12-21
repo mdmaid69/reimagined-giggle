2e611a94420080a2801bc183e45aa07709170b4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:49:27 +0000

    simple comeoff when contents exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_stack():
-        return collections.deque()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def create_user_list():
+        return collections.UserList()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
