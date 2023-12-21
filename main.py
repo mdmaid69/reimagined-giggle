5d05cf9fe3b80fdba6f8f4e5e315c83cd82d6b94
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:30 +0000

    stare herself especially soft sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
