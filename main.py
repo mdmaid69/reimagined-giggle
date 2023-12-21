e748db546f50845751d73c2a7f81e89a3fd0fad8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:55 +0000

    hardly partner move test although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
