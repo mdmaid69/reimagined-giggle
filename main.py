6aaa529bac64f69dace93c0d4905c2059a82b976
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:09 +0000

    factory mysterious handle pour difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+print(math.pi)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
