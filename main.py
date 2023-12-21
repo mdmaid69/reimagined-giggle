9f639e38a6e5c4f075fe616adf72af7f010fd9d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:15 +0000

    hospital himself cheat strong tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
