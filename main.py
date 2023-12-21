ac4617d1fb1b04c6b186958c060a5801d83cae41
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:34 +0000

    innocent feeling impression care many

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
