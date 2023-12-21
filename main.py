174e5cda1c411d352aea28c79979085356e452d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:17 +0000

    again language plant turnup Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
