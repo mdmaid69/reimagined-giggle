329f928381ebf77d20d6acfff4d05cc6eebf3875
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:22 +0000

    ring position share introduce 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def add_numbers(x, y):
+        return x + y
