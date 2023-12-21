0aa40873428c2cafee70feff57e434abbb2a2aae
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:26 +0000

    typical age peace ago minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
