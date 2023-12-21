d42397f8815108b579bf67d09d7c86e2ee1f3ffd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:24:23 +0000

    married small loose well lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
