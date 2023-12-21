31f9576167232560db5b578fa334f72727a9bfcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:16 +0000

    proof tell question taste experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
