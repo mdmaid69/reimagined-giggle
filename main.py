352b750fea67eefb234612882ce9b193cf54e946
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:58:16 +0000

    bury light Dd love choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
