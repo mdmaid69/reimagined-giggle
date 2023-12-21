939581a199779ab7216679e0e2ee5545a969b804
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:08:53 +0000

    maintain suit great offer simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
