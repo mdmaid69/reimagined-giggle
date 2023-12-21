4039ec081eeae2742e17f186055abe10d95748cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:14 +0000

    description change day surprised afraid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def cube_number(x):
+        return x**3
