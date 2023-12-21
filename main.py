b81aa8ac0ebe58ba75513beec9af26491981e15b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:02:24 +0000

    tonight meal result team put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
