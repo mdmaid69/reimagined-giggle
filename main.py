ffe0d4d85406ab8771cc9d36241cac2e1056807a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:11 +0000

    sign sure getback past somewhere

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
