205c52c494a16c6d9aea77f5009b1609874f85fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:38 +0000

    sound basic lunch stop hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
