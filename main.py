2306efac0284b5e459c45661c91421af57158e95
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:39 +0000

    party expect wish successful after

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
