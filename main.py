dbca21e2b1a841f447e48465971e1b787db04f15
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:15 +0000

    exact choose calloff thorough situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
