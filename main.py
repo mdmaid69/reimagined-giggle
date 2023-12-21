88f424497ac2935616c6227e4d59e50167be3b65
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:05 +0000

    value my size beautiful move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
