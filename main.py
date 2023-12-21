140abace3b9c7303beb214ed0dfa6191b259e3e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:58 +0000

    want major treat care situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
