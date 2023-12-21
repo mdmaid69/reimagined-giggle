a87c5fa951512dd4076d9cc785d0de7a4cc136e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:38 +0000

    permission cross giveout active recommend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
