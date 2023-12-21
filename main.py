8bd26364f0f4b63568d79d2b614347cdf97504e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:39 +0000

    damp turnoff fail computer sorry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
+def add_numbers(x, y):
+        return x + y
