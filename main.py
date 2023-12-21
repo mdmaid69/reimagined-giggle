d84190f5cf9505609e07c4cf6c2985113e2d0504
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:42:13 +0000

    earn brain star positive kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
