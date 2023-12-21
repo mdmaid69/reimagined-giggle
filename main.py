2d8dcb8fe8ffda2f6f89214d8ed761f86ceb1bd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:43 +0000

    each comeback whenever rush fairly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
