25427b943b432cb4ae86cb1580c306e9e4511876
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:19 +0000

    busy tight contrast rid press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def reverse_array(array):
+        array.reverse()
