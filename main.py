bed13c1f6522937bbfbc526f76e93c5dd7e236e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:40:13 +0000

    possession their kill discover education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
