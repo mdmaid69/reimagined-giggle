f44a1a25fb6219ae55860c22a5a4f1773449667f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:37 +0000

    means unless undo keepout lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
