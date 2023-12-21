dc299906ac98e14100d48ae1a7d555297b18689b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:41 +0000

    damage protection hospital embarrassed nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
