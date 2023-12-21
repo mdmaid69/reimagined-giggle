8f84bef2744a00f7327e38129855d9374cac654c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:26:33 +0000

    influence distance detail yes old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
