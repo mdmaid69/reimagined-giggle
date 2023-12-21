43a49b176ee993af18daa2183bf9f7c9c80139f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:27:06 +0000

    no trip greeting confusing row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
