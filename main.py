18886ec65636da087181730c13377418d37be308
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:51:48 +0000

    squash address badly pupil office

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
