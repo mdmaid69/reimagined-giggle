f072dadfabc687bc9207b112561b55f4772bb8b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:08:38 +0000

    adjective past pen plant promise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
