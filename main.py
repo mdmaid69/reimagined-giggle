62ed9e4de23b950273a59d4c08ce8488ceeca012
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:04 +0000

    explain basis usual list ball

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
