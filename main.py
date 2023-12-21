e5bb3a8f7619942630f4315f37f6c1b923d38896
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:28:42 +0000

    tired compare workon rent pair

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_odd(n):
+        return n % 2 != 0
+  def is_odd(n):
+        return n % 2 != 0
