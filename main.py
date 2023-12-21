beb950dd0be6744cad6836071c6c0b9cd2a030f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:32 +0000

    guide become minority minimum least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
+import sys
+def exit_program():
+        sys.exit()
