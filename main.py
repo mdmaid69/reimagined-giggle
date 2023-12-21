a10e40b22c1b058b769330ae87c864edcba20313
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:00 +0000

    black means Uu public still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
