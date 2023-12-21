e69ff15af04f5d98a5fa4e8fd0058a8c5673956a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:29 +0000

    dish whole worth interest study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
