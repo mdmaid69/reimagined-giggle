ecb4b5f1363003f57d4ddfad069fd7b5be378de6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:03 +0000

    slightly bringdown indeed match sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
