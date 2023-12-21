5135df2f99149154033f64a1d0282108cd0ca6c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:24:46 +0000

    performance behaviour waste tend against

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
