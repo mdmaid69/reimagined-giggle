484206d75d85ba1bb46c4ba300e5a3c4bb80d853
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:06 +0000

    impressed mean 572enter team rain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
