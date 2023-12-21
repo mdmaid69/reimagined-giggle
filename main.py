8f805a3944838529ca1ca6b8cbed320c34f3df39
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:26:01 +0000

    object ask earth walk afterwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+print(os.getcwd())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
