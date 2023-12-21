4c4a0e818a66b40f1f24b002c23ac64877ed5017
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:20:28 +0000

    downstairs swallow matter dry public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
