0b2e01e1401b5f5172d4d7effef971acdc36cf7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:56 +0000

    sport tradition bridge themselves reliable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
