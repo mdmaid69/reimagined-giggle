c3257eb1c16a07dfd9ee0342b849793f4d7f978b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:12 +0000

    innocent Vv before yes future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import collections
 def group_by(iterable, key_func):
         return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
