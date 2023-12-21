2484aefaa1b32980c6504d10839225d88f718b83
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:34 +0000

    charge brain bringup thing spare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
