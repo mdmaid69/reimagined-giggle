96e9aa8b881cf00654346747c34518758dd84b4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:24 +0000

    move dry chase country talkoutof

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
