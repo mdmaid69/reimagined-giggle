dd9eb7764d7802e545fc615ab92460a038547ccd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:53 +0000

    train beautiful honest band congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+text = "Hello, world!"
+print("Characters:", len(text))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
