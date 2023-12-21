be8733160b1085b3bb3b62149fc56236ec4c1c39
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:59 +0000

    building our pleasure tear slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
