f09755aaf53f661436d7477803f56c7cdff1047e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:56 +0000

    part top sink satisfied pleasant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def count_characters(sentence):
-        return len(sentence)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
