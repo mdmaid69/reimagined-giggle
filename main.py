d73619c9d14d70bfa7e045e8a81d75a9562d1af7
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:52 +0000

    suit instant defeat tax everything

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Words:", len(text.split()))
