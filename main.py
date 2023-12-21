21eb6881cb2d981255bf8ad98c464c702b2f0d32
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:30:58 +0000

    pencil charge doctor stiff Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
