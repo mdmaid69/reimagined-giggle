3ed0ea24ad4bf81010feb484a38ae0d03bda5988
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:49 +0000

    ahead communicate telephone spread touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
