5e756dbce17930dbaad23d8bbfffb62e02943ca5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:18 +0000

    value interest smooth depend callback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
