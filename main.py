14d832d5648afc2c6d75bea44409b3134a94ef41
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:29 +0000

    too art harm step occur

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
