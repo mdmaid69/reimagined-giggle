bb7902f8f3901d2ee17ccb82dfbc93a77eedf256
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:34 +0000

    between holdout contrast calloff finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
