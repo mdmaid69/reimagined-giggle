4ca78d7744ac2eff3f0be4dca4f6b362a4a2e208
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:49 +0000

    close respect tough can downstairs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
