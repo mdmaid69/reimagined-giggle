27d576519d825a433b9cfabd10c20f1c3ff35222
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:54:54 +0000

    only fight side lesson holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
