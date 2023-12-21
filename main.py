8999e92b6dd04b751a54859cb85bcf4b7ab51862
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:52:11 +0000

    recommend Ff they anywayanyhow people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
