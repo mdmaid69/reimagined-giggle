cf38a1556b61d1b4fd80453c96b17a2157cc3183
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:11 +0000

    takeback correct risk priority circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
