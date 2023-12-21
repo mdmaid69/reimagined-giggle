1e0e71ba8889b8a1188c06e82596d395150b08b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:19 +0000

    then 564encourage interrupt fine land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
