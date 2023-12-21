71a17ce060f95596e5df16bdc6c0cd27dc7e6fa7
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:38 +0000

    married throughout trade file vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
