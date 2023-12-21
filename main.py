c04032dcc697abb2e92d886615236391dfad795a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:44 +0000

    approve pool dick sort gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
