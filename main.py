f19df1b26fe9aa252d0feefacc07bfe8c0f9d7bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:57 +0000

    popular show ground action bringdown

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
