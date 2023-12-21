244a0ded8b682b8fa1c7afc0c0cf41f4c5489b86
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:44:42 +0000

    else speed label story cross

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
