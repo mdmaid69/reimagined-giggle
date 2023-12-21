42e118555dfaa4531821d8e67896de448c6f1515
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:33 +0000

    ago successful reserve path plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
