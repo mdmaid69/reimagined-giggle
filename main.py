d26831c113e345cb4dd10f15237dfe27f542a037
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:57 +0000

    comein lot carry minor yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
