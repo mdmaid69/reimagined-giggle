58e1803582a777f7a7f2f893358dc214723a70ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:52:07 +0000

    sad window famous shine dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_bool(array):
+        return bool(array)
