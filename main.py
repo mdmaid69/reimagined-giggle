329e1b22e26b6cc7d1c4c90c27ef5f3ae0f4ed0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:35 +0000

    comeoff thin consult delicate maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
