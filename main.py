425bcfc11b80e4fd7dd5d97fcf2668867dfc3ea9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:19 +0000

    shape geton just hand visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def greet(name):
+        print(f"Hello, {name}!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
