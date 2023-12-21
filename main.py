b40133daced3ea10491dfc406ae7ab8aea154dc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:15 +0000

    correct turn around regular among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sys
+def exit_program():
+        sys.exit()
