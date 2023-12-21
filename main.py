44eb81d1461705d91f6784265fac1549876c196f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:33:18 +0000

    rest respect takeout scale minute

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_area_circle(r):
+        return 3.14 * r**2
