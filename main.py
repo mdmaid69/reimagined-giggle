b3b71452dd3cbcf9a198bdbc879a7218b658f972
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:41 +0000

    already tooth bottom touch wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
