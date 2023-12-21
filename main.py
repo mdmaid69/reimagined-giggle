047350340f3a1636447fd907572e91bf3c82c4a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:41 +0000

    injury even man outside estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
