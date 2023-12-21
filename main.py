51c09def6b2d97965cf8d700874022f8d1bd4265
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:35 +0000

    service ours fence use alone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_max(lst):
-        return max(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
