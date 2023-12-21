b175e87821dcf742e0382998ec56432c7c97f56b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:45 +0000

    count report group understand listenfor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
