6c3ce66c0a48e3bb20765e6e683a01ea141a2916
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:45 +0000

    anybody collection risk stop improve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
