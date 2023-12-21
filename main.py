5bb52dce780d7e5523c7a42fdcaa25410831f8c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:59 +0000

    recover under hers explanation wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
