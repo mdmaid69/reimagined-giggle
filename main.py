7edcbd60d9f140bc09a3889ef6cfb214fb1a8221
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:28:56 +0000

    autumn beautiful hurt of play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
