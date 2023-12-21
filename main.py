14649a0cfd1971468f8332a6194043d45ee86cfb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:22 +0000

    successful straight gentle again escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
