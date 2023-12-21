ad30c677e32ce98a3c38f2d934c7ea80ca36d139
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:47:51 +0000

    exist gap especially twist holiday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def cube_number(x):
-        return x**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
