c0903682192be3762ccc018c12e2d6fb8430aa7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:31 +0000

    reject repair hurt row stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
