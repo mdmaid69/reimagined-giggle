e6dd53f19237262f05143fec2f3370a8d0f2df42
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:13:31 +0000

    to market bottom cheque exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
