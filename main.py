3f79b4e7ed627ed1e9b8757a1a9a7a1678f128c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:06:09 +0000

    bang drive talkinto use add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+def reverse_string(s):
+        return s[::-1]
