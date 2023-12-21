053d96628dc3663fd270225be95da2b21d7c283d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:29 +0000

    prove size valuable copy hers

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
