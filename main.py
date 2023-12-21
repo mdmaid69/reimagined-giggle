76b6f53b7a9717979493db449d5ae1392a12994f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:07:29 +0000

    deserve stage race spoil sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
