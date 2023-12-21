7896ea239780be218794e8e2b2818e66f3647edb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:54 +0000

    eye upstairs dangerous plain forward

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import array
+def get_array_as_repr(array):
+        return repr(array)
