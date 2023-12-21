a9497d90778b2185c8fcd31e3250f03cb162b125
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:26:40 +0000

    gather shout comfort force turnback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
