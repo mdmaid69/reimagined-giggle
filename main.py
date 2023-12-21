dfed8e90f5a0a59a89b246e67968056ab69c76ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:19 +0000

    nice reach 179bite this tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
