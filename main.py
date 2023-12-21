89ed0a73dca5de3ac4b6775e9b7eca97b37ac01e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:16 +0000

    mess degree long crazy hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
