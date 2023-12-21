d6008b59ef5d57b49c9caa05951ae918d58ac59a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:52 +0000

    plate illness discovery unfair tip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+def exit_program():
+        sys.exit()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
