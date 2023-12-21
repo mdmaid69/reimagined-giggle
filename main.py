643e335c501fbc09db005887ea0e3afbce7e0038
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:41 +0000

    cruel pure somehow I ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_unique_words(sentence):
+        return set(sentence.split())
