8429d29dcbf2d7734c6b32624512bb56870de649
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:31:18 +0000

    government weigh board sail authority

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
