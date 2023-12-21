ddbda0ca91b12062e70ea883ea08ffe3cc041d67
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:11 +0000

    exam discovery name bridge mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
