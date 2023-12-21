0681078b8937566622f7743556a0c33bb2d98d80
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:44 +0000

    wheel taste tidy family at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
