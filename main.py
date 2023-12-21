c7aae54f66f064a7bd86afc3a5a3e3e6c2dfe6af
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:19 +0000

    be foreign black system opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_max(lst):
+        return max(lst)
