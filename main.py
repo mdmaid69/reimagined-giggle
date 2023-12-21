90b39928f70466ece8ef192679ecea295d7141d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:46 +0000

    statement fun correct doubt sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
