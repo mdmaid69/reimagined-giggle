496dd9b3ac509629267b067c6f45fd4baf086b92
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:35 +0000

    opponent sure nurse keep instant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
