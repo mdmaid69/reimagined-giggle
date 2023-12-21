ab924c10b1dfb7db997d3ab380a40ba7eb996e80
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:54 +0000

    class pile war hurt mainly

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def log_message(message):
+        logging.info(message)
