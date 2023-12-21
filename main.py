2222a351bc97b05e67cac94ba61bf9f56b3f0d25
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:27 +0000

    wide accordingto possibly include correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
