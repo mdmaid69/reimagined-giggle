446a9042d5c8ab3eeb897b7cd56d65a3cf76da40
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:00:10 +0000

    abroad release lesson convince fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
