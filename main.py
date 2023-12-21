93b5f892b8554b8ea643955d1a015b74cb67abb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:50 +0000

    plan remember 569enjoy unkind deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
