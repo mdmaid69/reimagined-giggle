27dea51bd568733f0f84b53d3a6b1d608c9f4714
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:42 +0000

    guard supply herself town show

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import re
-print(re.match("h.*o", "hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
