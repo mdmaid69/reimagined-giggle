f75cd855e1734a54b130d72f9c18bf7ec3d59253
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:49 +0000

    warn tonight value finally open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
