67fe2973f46217069a3040349ba4dc5c8b6688d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:20 +0000

    573entertainment frequent cap talk important

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
