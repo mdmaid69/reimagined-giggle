61ae298ec51f0ad69248d88ba2973e06d9d447eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:23:54 +0000

    march telephone accordingto believe sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
