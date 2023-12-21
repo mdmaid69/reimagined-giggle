fb3f2c0eb7bae334bab14a5e891ae568109d3d03
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:52:10 +0000

    appeal consider drop neat such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def add_numbers(x, y):
-        return x + y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
