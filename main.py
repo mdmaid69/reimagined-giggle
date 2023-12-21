1c306157a14a47aedafdfd564b94e46db861fce4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:27 +0000

    rain unit juice label spray

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_unique_words(sentence):
+        return set(sentence.split())
