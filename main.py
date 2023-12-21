dfdff6a53cdc9f436db1cde29be565924c40469f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:41 +0000

    wheel confidence instrument tour choose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
