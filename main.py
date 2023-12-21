f2e1459201f29f1d257a6d522ea16b8f36926437
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:51 +0000

    contact call door on round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
