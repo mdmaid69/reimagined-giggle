abcfb75593d970782db451d6097af9b072e07a4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:55 +0000

    convinced include rescue contribute angry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_density(mass, volume):
+        return mass / volume
