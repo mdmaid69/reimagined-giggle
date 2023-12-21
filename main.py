11dad9f9b7cef8556f268709c0759c63fceff157
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:10:09 +0000

    idea narrow surprised dare without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
