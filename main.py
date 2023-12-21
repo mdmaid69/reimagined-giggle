9c3b4009c88bd47627ac2060e1a700f0d614d395
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:35 +0000

    combine positive light limit achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
