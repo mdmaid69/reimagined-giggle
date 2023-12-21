fdda9a33e363d04dd37dc3146b5dad12ce64488a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:01 +0000

    fit put accident phone light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
