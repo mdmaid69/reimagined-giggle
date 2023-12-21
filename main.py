11abaa04bbaa696453a9c2d82ffbd18e68ab9c58
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:25 +0000

    laugh when dry crime wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
