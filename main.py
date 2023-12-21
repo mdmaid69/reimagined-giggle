e61534adeeb91470efa31b62d5700d1dd26a2c9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:08 +0000

    goup drink negative calmly feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
