089a97b6502d901a5c7d909c8d346e61596f572a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:31 +0000

    future pronounce coffee exchange tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
