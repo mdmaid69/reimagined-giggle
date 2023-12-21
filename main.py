ddd97c4575d997cbf43141eba2dd9780d9e6a2c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:34 +0000

    point pencil possession match poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
