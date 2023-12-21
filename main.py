1b0f4ec4749908cda11cbfa539d2ca1134a092c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:46 +0000

    shadow fortunately have generous Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
