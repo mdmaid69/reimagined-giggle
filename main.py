24441ab0940101b15f34239f329e8390bc306a4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:28 +0000

    theirs skirt head upstairs quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
