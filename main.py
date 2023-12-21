7bd14513309c8643ae43d1d99c01838a110bfd31
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:29 +0000

    exist listen Tt prefer adjective

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_pressure(force, area):
+        return force / area
