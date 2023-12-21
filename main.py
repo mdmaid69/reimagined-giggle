f408d5f55b88dbc627e157be9c1fa74d518bc76a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:45 +0000

    occasional involve wire front relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
