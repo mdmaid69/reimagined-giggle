feb8f0a0a561597a0b0e1cc710d39f0c75ec59fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:49:40 +0000

    press politics bush drink please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_int(array):
+        return int(array[0])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
