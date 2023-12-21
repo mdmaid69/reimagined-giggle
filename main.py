1cc66ebb2c81466fc47232bfda532d6aab7cf124
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:08:40 +0000

    unhappy rich blame check tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
