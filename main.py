676b1f8e67d0efc32fec8085c0468284f08074f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:07 +0000

    noone subject cry nigga few

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
