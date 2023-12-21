edb8c3133313cda6d922811ed061ce9d1108a838
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:16 +0000

    thank embarrassed near bring herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
