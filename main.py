f4c3e56baf079a18b9e058f5debc96107e5ebdff
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:46 +0000

    tap weak certain recovery mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
