8a5c19f69b629720dff7e724176540d96487dff4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:24 +0000

    572enter prefer stop dick report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-print(math.pi)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
