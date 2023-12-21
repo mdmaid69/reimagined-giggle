632d4a83a2fe3c10b5c6cf115c2ce46f877266f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:24 +0000

    choose deal workupto keepaway sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
