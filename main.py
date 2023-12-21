195c1918bee65c3d6815e01c39684ebdcf3a673a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:11 +0000

    deliberately tap food mark buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
