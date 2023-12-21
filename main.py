fa0908c6c0f830fb20395b21558cc70e99db1c9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:47 +0000

    region scheme wing increase OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
