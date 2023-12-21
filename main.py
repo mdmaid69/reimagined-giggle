4f1f26beb9c339dc15e89f2ff851ed0d7fc512d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:24 +0000

    circle mention interfere record needle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
