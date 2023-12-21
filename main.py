6917d69c412150fb83c144a31ba0eae8f4a50582
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:46 +0000

    control piece rush opposite instruction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
