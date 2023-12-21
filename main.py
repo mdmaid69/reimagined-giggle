e17f7f147183d8ec32f573bd29c8f09ac937e1ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:11 +0000

    shallow married hand wait somebody

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
