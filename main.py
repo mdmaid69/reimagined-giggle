fb4beb879b4d2225019e4045a267c62cecac6c5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:56 +0000

    ilovedicks development direction moment piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
