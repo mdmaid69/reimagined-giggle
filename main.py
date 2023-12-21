8654157534fb2e2745669f960f9d9466a60376bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:55 +0000

    permission one contact fetch permission

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
