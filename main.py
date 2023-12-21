062e3865e95983120648c595922956ba757cbe59
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:06 +0000

    spring complete eat swim over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
