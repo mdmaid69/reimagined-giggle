fb379508d83148400d22308218ceda1ec4b900a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:29:43 +0000

    breakout debt interfere chance list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
