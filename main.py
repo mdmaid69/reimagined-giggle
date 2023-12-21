a6bbe727786bb4ed19b23f83ea956ac9bbfeb7dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:23 +0000

    anxious holdup adjective laugh somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
