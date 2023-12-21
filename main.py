204d9fc67bc411700c680f7a346ae0485822af13
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:15 +0000

    expression quality against match good

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import random
-print(random.randint(0, 100))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
