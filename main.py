e13b19ce9280e6f4efa4ea6ca5fcda760ecbb5e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:30 +0000

    pure capital physical start female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
