e52a8ba890ccf963fa48e4f0848280890066893d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:11:56 +0000

    accidentally noone soon force neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
