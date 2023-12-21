5c20c80ff97e9f706bd41a3a535ad1c3577a1a3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:18 +0000

    fairly apply completely problem past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
