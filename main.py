541df8ddb7f53d45c9870bb8b21cb7cf38604f6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:02 +0000

    base near appearance container corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
