7acd277ef2b23e40a90d002868883aa3277f9178
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:22:37 +0000

    start mark century level him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
