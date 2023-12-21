4fbeb3970d9b446ebba7d59687ad3f1ed6db5da9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:59 +0000

    provide slice react ability trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
