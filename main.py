350f38e89e4c3f97b73ad9f07cc86cfe2fc1fd59
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:21 +0000

    pattern address should them polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
