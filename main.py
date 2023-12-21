b928ed27ef306422c8862a68c0ee5ee6b5cafba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:23:48 +0000

    danger tragedy part thinkof today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
