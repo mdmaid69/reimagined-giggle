256c47ba810a90389066887287acc20579d71258
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:11 +0000

    there pause drive embarrassed sign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
