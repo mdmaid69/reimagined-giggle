f19ed75a7e785bc2142884fd2b332ed1d132b697
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:46 +0000

    science concentrate chase fit taste

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
