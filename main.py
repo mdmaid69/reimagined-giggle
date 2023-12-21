0c86ba6ec5e620997dd8715a4130c28fdc814652
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:27 +0000

    keepout honest defeat room ourselves

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+for i in range(10): print(i)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
