0b03a1930576b30849623936f8216eca61ee3183
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:34:35 +0000

    head talkoutof long left place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
