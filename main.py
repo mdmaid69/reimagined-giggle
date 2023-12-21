6c3479deaeb64de52bc43cce904e9dc92f2ab991
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:45 +0000

    treatment teach ever when smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_array_to_list(array):
+        return array.tolist()
