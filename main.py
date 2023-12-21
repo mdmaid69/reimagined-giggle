008e9954213830f9ce8a0afe0ebca1e80f621a17
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:25 +0000

    shopping consult previous place rent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
