01834826a77e121940d097e5b252e438819a1ff2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:09:10 +0000

    service still report search post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
