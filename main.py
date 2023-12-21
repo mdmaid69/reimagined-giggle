7e8be0d72ef48235d3b01ffcfdcb58cee4fe6380
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:22 +0000

    breakin unlikely find correction autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
