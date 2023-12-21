7fb51b810662a43d7f29383794763fb028dad250
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:39 +0000

    record spring bin sea ourselves

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def cube_number(x):
+        return x**3
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
