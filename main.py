90cac06390c8714818680fef38c5bce246209ac4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:02 +0000

    exaggerate broad us scream shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_hex(n):
-        return hex(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
