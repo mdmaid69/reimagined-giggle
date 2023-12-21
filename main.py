3a12c21d0ddd53ef9e567abfdbfa85ccde8dd50c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:23:36 +0000

    trip experiment kick instead cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def print_python_version():
+        return sys.version
+import time
+print(time.time())
