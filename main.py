244a8267045d2b8eafc5c73bfd6b89bbe90ab9ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:59:33 +0000

    injury add round about achievement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(10): print(i)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
