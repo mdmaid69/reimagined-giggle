d9e88cc84bf54b3c748ca8e8bb58f2575a6dec2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:19 +0000

    hurry send decide scale meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
