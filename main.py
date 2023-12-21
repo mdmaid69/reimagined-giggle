2a65391e6f888cf758395a7d8bfec7925f006672
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:21 +0000

    target open answer introduce shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
