385b5f30264729f8318e0b3b9117b92022f11196
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:22 +0000

    pretty charge shade maximum pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
