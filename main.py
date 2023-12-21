a0c6e41eab5b849af230a57f6133e14b858307ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:31:16 +0000

    your choice creature hurt of

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
