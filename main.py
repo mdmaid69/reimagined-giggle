91f160dd387be378e09dfd9604bbea9c0e26a205
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:04 +0000

    nervous both slice spill against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
