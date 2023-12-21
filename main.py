e625e638bebc482ae12de2a9016f99ced697e092
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:04 +0000

    how go form steer dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def convert_to_octal(n):
+        return oct(n)
