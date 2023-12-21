361d8ed851db63204e0004c9a7cb975e828bc872
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:29 +0000

    workupto dinner of meanwhile concentrate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
