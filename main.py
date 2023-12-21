51b93d1d75ce22590bc0771aec32e25d04502b28
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:16:33 +0000

    convinced environment pain nothing turnaway

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
