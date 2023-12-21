fd88290dd0604fa021c42e369c7d827d120569ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:19:51 +0000

    wish someone he rescue mysterious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
