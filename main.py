2192cb21657a98063d8460e2f25570a325fb6e5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:57:56 +0000

    man tell sometimes shop display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
