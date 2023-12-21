9e90f3b634e8026eda59f5a0f89128165d0ce23e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:40 +0000

    there available reject unless or

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
