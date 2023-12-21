b73b7216a08017e8aa6e720554ccadf59bd69b68
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:10:44 +0000

    interruption age surprising night flat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
