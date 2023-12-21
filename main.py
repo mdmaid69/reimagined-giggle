5a34c0bf996ba7a963f4691d69cd3e0a4ca1b82b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:33 +0000

    fetch year improve bang zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
