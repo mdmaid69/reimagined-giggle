f9d0526084bfb670575403c97ddd3228ae39ce2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:11:17 +0000

    her real shallow mass include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
