f50e8f021c0885125d254b216d5302133c6360db
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:42:27 +0000

    disgusting constantly giveaway past order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
