77a8b288bc03a3c9552201be56f37e466faf3cde
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:28 +0000

    order minimum hope funny push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
