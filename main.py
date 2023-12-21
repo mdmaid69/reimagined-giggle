1ac5d6d2276760b05830e3221682e890071d83b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:54 +0000

    improvement event laugh lot anxious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
