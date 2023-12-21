f269000c6a0edce17a0e895ca06b88556368d0e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:44 +0000

    excited chase makeup priority over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
