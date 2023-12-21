531855062422464e4335bc9460a06b3c130ca2df
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:44 +0000

    bringdown fear environment secret plastic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
