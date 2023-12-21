7cc0f9ba477c13aa1ac3aba835f91d4d7d5194ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:09:49 +0000

    disgusting bad brush threaten provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
