0c990b25c44a39094ebf48e62a167aae197e61fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:09:05 +0000

    balance guess skill attract dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
