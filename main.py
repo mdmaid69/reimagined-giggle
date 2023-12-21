cf10f6d08c6715eb408120b39e8b171d45a509d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:58 +0000

    Ee lunch any tap detail

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
