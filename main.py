f431e55c80382ccd0ee610d43890975a787cfb20
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:12 +0000

    communicate flood circle worst whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
