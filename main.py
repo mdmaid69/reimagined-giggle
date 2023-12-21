981d37481bf896b12107e976032562b75ebc8129
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:53 +0000

    wherever identify support blind store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
