549cea4cf1dd5c00737b0a2692ad5f5b12700ebc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:09 +0000

    free concern freedom snow wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
