e8485206cf20223a5da1e4ca481e2a6085b2f333
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:27:10 +0000

    help trust breakdown clue 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
