9221bc3344b98bf9133177aad554d3dbf5e56a5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:16 +0000

    aim official silent punish apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
