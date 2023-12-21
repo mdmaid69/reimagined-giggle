5b176c512eb0f595b7164f4429e0ee1000104577
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:23 +0000

    meaning vanish name have fight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
