a2e2b4addbdf7fa4a128294bd4c443ed60fbb190
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:40 +0000

    immediate face heat after sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
