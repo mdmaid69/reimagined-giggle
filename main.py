4152edafc29b56864795798c59f71c745d1a8ef6
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:25 +0000

    thorough send average plan friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_typecode(array):
+        return array.typecode
