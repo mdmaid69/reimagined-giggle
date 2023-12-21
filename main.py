abfe82baceb33eab0a063ca77dcb5e6a18cdba7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:46 +0000

    fat wide separate OKorokay watch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_power(work, time):
+        return work / time
