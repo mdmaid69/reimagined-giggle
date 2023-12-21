e29420c5135ddbb186db58a27fb670ed996fc175
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:15:47 +0000

    chief however fail excuse cinema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
