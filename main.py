4cb0b17fb4323843b6a4cca2e11b1c86e79ae6eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:30 +0000

    breath forward punishment wander contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
