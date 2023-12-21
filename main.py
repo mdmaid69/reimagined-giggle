eb116c801e4969250b37516948aef5f7049416b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:47 +0000

    shoot movement bring bad opinion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
