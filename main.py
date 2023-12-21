ac06eb0c5ef479b6d4d964c36d59e09826a41b45
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:55 +0000

    tense giveaway surprise everything impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
-def calculate_work(force, distance):
-        return force * distance
