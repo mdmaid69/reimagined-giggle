01ba2321a21931d0301882979d1c303304c3924b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:34 +0000

    reply surface takeaway scale job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
