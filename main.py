872e4d52b3b520a776632d5bb64f52ca4436ab68
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:05:38 +0000

    breath invitation change powerful temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
