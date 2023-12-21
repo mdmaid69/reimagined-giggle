743364ffcb7ba859b505d017680030e3640c3f56
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:20 +0000

    breathe later shopping base apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def reverse_list(lst):
+        return lst[::-1]
