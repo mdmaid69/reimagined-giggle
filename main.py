f8fe8fe294d4a0ca7355efdfa7aaafe0aa2d94a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:38 +0000

    busy phone search home dry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
