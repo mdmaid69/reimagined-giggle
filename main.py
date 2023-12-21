255ff145ccb1ed53091e548e8f1462f3d547059e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:20 +0000

    turn would protest blind callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
