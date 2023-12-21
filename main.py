659c58b707e823835eb47047d7a4973947d86acd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:48 +0000

    secret beginning shop equal throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
