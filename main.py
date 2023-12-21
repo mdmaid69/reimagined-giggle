2a59e56e7e11d923f52d90ae676b2460298ac859
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:53:11 +0000

    achievement fact tidy medium manner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sys
+print(sys.version)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
