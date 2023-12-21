92b1a255fa30bafbabc967fc2e342ca018ccde86
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:11:14 +0000

    excuse final speech pretty unable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
