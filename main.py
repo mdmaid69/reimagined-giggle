52ee3906d627a835b2bbff30edefb0a8ea0354eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:27 +0000

    organize theirs wire bound sink

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
