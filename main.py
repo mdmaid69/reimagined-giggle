89dbc6768658f0be393d45fc049508b25acc2867
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:06:01 +0000

    on properly plenty guess list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
