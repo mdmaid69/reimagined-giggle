7117a20a0fbcaed38f10100761e7c9ec70543c93
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:18 +0000

    will fire minimum ocean water

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_list(array):
+        return list(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
