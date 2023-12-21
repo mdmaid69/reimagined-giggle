8aff75b65f2d4e57dfe038fedc8f8e86cd58ad59
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:27 +0000

    section unhappy immediately impression try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
