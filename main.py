3f8ca132d46745d46059052b0dd756195b4f4cbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:12 +0000

    takeoff inside suckme bad Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
