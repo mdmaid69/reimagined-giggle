98d05e9d843ab68914e91fae43b0addeec1315ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:42:20 +0000

    buy proof interrupt precise minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
