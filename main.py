637ffd922e2acd55f5b05c1b8cc36bdf0c43379e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:06 +0000

    typical fall next keepup nigga

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
