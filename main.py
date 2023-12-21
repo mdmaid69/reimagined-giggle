f18f11bef0ea5e54d44afd7c3951bbce54c63725
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:55 +0000

    broken rare move divide goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
