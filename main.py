d8072d2d27ac7f839239146d72676b642537a040
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:24:27 +0000

    disapprove ever possibility thinkof whisper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
