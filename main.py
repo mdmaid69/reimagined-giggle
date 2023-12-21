2ccfd4baceaad7ffbc310cf8cfc9183a8710e305
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:35 +0000

    hear that worse rule crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
