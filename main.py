300b82d5058e9fbbdc4fbcab982915bfddd7f2b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:37:29 +0000

    one like foot stamp second

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
