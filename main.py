186a24a630371c7862f4537be5a4524323bc901d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:08 +0000

    by charge crash geton exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
