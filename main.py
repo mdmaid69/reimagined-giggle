5a290eaaf54b567f4408cdd44a3bd666aa0025da
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:02:59 +0000

    bend reduction lock appeal offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
