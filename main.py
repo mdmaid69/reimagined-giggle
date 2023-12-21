a57f16e38f659080204c4bd5be94cc62ddc5611d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:39 +0000

    bin hang copy situation noone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
