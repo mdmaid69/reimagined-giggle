44bb4f87f51b3e4d946c383379b40d21c28ca84a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:52 +0000

    alone stay sound walk chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
