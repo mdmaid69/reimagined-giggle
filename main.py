6bcdf3774b9f1b2a58e7f2b21b04e7b721836bfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:58:50 +0000

    slight suckme house fear narrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
