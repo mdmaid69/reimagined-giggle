d6fa168e43af31b78b8d5c001e4bfb514969e8ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:57 +0000

    us winter cause convinced offer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
