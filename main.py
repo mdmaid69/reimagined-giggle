a2115d7d8395ea6a221004f868aadd61dd1caa8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:29:28 +0000

    sticky question soon comeround long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
