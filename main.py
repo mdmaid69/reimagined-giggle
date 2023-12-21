d4407b36371803b351416de91d61906952ac3a5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:32 +0000

    opposite surprising turndown interrupt foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
