4c07691fcad03235b6d5a2e62a5f787e0fd2cbd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:48 +0000

    spare shock science difficulty gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def convert_to_binary(n):
+        return bin(n)
