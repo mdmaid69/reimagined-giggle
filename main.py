7802402cf9e47908568db2ac87547171c82eaeb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:53 +0000

    anywayanyhow notice insist pencil innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def count_words(sentence):
+        return len(sentence.split())
