2aed8ed66bb280241d76bf1ed391df887b08a088
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:13:08 +0000

    fold risk sure serious adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
