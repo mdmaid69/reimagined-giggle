10bfccd809df69b4c2d793ee173ae14693314bff
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:22 +0000

    but possibly man support arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
