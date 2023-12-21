04485127b91867edb5d4b298b7a5c0115260fc98
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:13:13 +0000

    uncomfortable miss truth fetch into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sine(x):
+        return math.sin(x)
