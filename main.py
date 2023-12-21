62d2d3ab140912b74fdf199aa0a16eb5947a8915
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:19 +0000

    telephone keepfrom weak 569enjoy experience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
