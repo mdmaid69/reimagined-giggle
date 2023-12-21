5b4bc54fb5049b961cfd7dd55a2e7cd7d0faecd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:13:25 +0000

    car whistle thing refer happen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
