7eb4aab14d8bbbf4d1b7285ff455cebbc535abc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:27:27 +0000

    fat towards truth phone total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import sys
-  def get_python_version():
-        return sys.version
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import platform
+def get_python_version():
+        return platform.python_version()
