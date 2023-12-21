20aa3b7278fcb73563b98ff8387e70d441c3761d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:54 +0000

    develop mysterious surprise cap party

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
