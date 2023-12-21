0260169fe9fe453c11d917712cea22d014d6205e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:34 +0000

    illness principle spin firm calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
