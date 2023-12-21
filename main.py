6bb7cbbfaaee4e08ac184e91ed3045f537f34e4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:31 +0000

    so young runaway load bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_power(work, time):
-        return work / time
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
