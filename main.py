48f4282ab641af332fafd9b794a32f388b81f8e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:10 +0000

    container sail list earth total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from collections import Counter
+print(Counter("hello world"))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
