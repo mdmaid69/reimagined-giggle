6be77a2d6b33b9516a28ce305e310372935fdc07
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:34 +0000

    off now absolutely treat man

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(lst):
+        return max(lst)
