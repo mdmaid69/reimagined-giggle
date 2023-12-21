ff4ff2e199707c5162d3c10244586e98faa88ad8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:12 +0000

    little tense holdon age clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+def exit_program():
+        sys.exit()
