cd57efa01e8ff12b7088b9f4be11b5c5809ed746
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:05:42 +0000

    rather best attach where beginning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
