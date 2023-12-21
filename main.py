cdcdb4608a10dbd834a494fbc14372a2852bc29f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:26 +0000

    case niggaacademy cover patient different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
