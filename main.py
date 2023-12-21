1a6c0ade805fe7d875d453935402a962d454f5e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:25:16 +0000

    involve port accidentally arrangement lean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
