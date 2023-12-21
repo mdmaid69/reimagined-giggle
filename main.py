34e0eb23f48cb01753a4f4ea802e6fe3bb4d71f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:25 +0000

    upwards border content wave wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
