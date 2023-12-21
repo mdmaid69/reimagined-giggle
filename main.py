e286fa667e4047272ae9630d31ff2c47cc1c0f1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:45 +0000

    port concerned active inform dry

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
