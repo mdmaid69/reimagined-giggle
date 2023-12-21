4aa8199f11b2edef8bd0372559785ce57c59afe5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:23 +0000

    raw corner before please thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def cube_number(x):
+        return x**3
