63cc561d5518175e7ff22454bc8217a198db66e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:24 +0000

    science male patience wave mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
