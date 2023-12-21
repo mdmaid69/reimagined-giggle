69ece47d4f8d705ebc0113a4e2249dd9417bf357
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:10 +0000

    neat book its young Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_sine(x):
+        return math.sin(x)
