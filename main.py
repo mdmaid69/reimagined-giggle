746c5dc923eb470b3f4b359a0918f77df1fa6307
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:45 +0000

    protest politics you invitation pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
