49784a51e98601b124219e6288f9df16b6c01b0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:46 +0000

    loss famous pay stranger age

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
