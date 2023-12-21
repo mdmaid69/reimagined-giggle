2014e9bf606e9bf3909cee4ac8ba34cc393330b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:35 +0000

    he know narrow strict react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-print("Hello, world!")
+def calculate_floor(x):
+        return math.floor(x)
