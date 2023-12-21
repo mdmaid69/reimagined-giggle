cec270a9a0d857f879dd90715367dde890307e0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:55:35 +0000

    smile same hope risk opinion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
