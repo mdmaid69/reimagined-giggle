6c399d840e04c5fa79fb8290c53a54777464abd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:30 +0000

    wonder judge physical favourite gate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
