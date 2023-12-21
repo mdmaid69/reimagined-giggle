9b640cbf8141c5e2d766261d89f3d2bae4bd8b58
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:34 +0000

    temperature field present bill best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
