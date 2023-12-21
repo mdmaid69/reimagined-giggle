9d22fdcb8fbb562d6fb603185d008524cbffb01c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:24 +0000

    find table school nose food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
