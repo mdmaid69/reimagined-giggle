2a90d91779055757c614c2a122d838464e869071
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:04 +0000

    trouble scream strike generous pen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
