d3003fd0b73c98e6079bfb19104e1c2e8331d2be
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:58 +0000

    its both bringback put attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
