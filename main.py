7482feb729da8d89d256f6550cbe1599ea5c02c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:22 +0000

    come pay stranger this room

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def count_words(sentence):
+        return len(sentence.split())
