ffa66a32da64ffc58b2b90a243443213c8c28b89
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:37 +0000

    figure bottom hello destroy taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
