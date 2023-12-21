32085c0b083fc5468f5cc794af8d229c8ff868ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:37 +0000

    lesson innocent hate comein window

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_tangent(x):
+        return math.tan(x)
