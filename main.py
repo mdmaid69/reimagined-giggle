5db226967e43f3719fcc9d348345ca2e27753617
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:09 +0000

    clean label crash difficulty swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
