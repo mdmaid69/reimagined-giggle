a273b79bad0f7f18a6493c4160977faec7d6e661
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:12 +0000

    recent course possibly slice under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
