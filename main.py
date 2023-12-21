3c81330d45754405acc14c454b3313b2b45109b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:00:31 +0000

    equipment rest soak force else

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
