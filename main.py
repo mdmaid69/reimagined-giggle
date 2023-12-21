11a8ae03a7494f0ecbb112a9dcbae5cc16f59a4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:28 +0000

    sound exit persuade obtain question

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def clear_array(array):
+        array *= 0
