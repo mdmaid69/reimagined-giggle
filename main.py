71274113d567976eeaabb396d08f2d10e27b490a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:16 +0000

    impression fault someone skill excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
