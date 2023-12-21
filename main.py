25d8ee51862fa7ed32d3536d1fa9a24c98f9e19f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:00 +0000

    tidy public everybody compete police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
