96c932c5ab5dfce41e82aefec75fc58ec696aea3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:44 +0000

    wish pronounce juice both age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
