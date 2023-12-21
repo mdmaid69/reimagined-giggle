55eb05fbd62fd907e9041eeaa211a08ce4225381
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:28 +0000

    actually frightened contrast partner street

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
