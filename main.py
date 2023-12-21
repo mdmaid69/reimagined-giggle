a4685b653daac90b0e2e7c2ee04ff9c8ebeac80d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:06 +0000

    also eager be people probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
