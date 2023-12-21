90677ab7f4e20ac523f3a4fe1b00d3784573de9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:40:01 +0000

    top high comeback among beneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_absolute_value(x):
+        return math.fabs(x)
