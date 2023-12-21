e2717bfc00f849a2cceb66dc1c2637308e6868a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:55 +0000

    protest fair process family fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
