f4c65ea1ba6009a8ffe71a425273aa33c91aabbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:54 +0000

    motion by enthusiastic material worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
