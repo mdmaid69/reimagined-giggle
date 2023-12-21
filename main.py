bf0cc333a2df429975500be92d85d3b77c197b88
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:20 +0000

    stand translate star clear protect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
