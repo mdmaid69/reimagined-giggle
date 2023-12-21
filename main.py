c4a902ac152a97ad5d48a4cbe0345a9e06fa0087
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:17 +0000

    room taste deliver 571enough heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
