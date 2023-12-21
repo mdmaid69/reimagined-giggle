1754a1e155b0bed6e8cd194c1f27ea8ba78e0557
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:08 +0000

    year past snow sister sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
