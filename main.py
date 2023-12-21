7f4264231c1bb27f237117931c89fe9dadf843d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:55 +0000

    result escape fault warn funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_set(array):
-        return set(array)
+import time
+def get_current_time():
+        return time.time()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
