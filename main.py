49052acdb0734bcc2c3efebb28f692546309e0f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:49 +0000

    exciting everywhere hollow congratulate ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
