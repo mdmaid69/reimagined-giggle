cb2c1acc7884b8b67e38298d049e1ce47e7a9622
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:58:00 +0000

    takeout service visit information home

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
