daca3970f1fcddc279ec2d00cf0b0d0fa8d99f70
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:07 +0000

    cinema outside cheque phone admire

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def reverse_array(array):
-        array.reverse()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def sort_list(lst):
+        return sorted(lst)
