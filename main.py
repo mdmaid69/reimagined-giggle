d036850a4e8b30193b46894713115fd418810174
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:24 +0000

    operate favourite neither head telephone

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def count_words(sentence):
+        return len(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
