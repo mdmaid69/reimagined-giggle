8a0eb35926e429926a17f2b804ad95bbff335724
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:09 +0000

    report hot progress side increase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
