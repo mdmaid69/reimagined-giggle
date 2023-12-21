282ff33e4e919034b2413d36aee0bc23b3c34a27
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:14:41 +0000

    callback underneath punish serious secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
