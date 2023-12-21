6dabf6295ecaa7d935bc6a2334382f29fdce77c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:24 +0000

    shoulder respect cover combination bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
