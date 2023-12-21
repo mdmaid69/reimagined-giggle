1cb4969e11c5b5edcb3d3eb64a5a84671c64b436
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:58:36 +0000

    edge public slide safety join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
