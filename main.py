2cb967fdb6d5fa0c0f29a31380f7f07596b5b8b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:51 +0000

    crazy goodbye attach care demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
