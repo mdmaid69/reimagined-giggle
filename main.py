48e29dfc92cc53277524e7ddfa7b6d467df3fdc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:25 +0000

    grade ordinary takeback course ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def convert_to_octal(n):
-        return oct(n)
+for i in range(10): print(i)
+import array
+def get_array_as_set(array):
+        return set(array)
