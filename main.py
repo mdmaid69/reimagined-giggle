3940f0023df0a7c0856feeaabac14dc7654e58bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:47 +0000

    worst up change breakup tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
