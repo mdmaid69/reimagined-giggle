fd43fb427d49fcbb811abca9b0216d1f5314946d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:19:12 +0000

    runout swim imagination spread culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
