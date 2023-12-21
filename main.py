e42f281e9abb319c3c0bc42f392573cb0aa055b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:34 +0000

    burst plate impossible their class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_max(lst):
-        return max(lst)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
