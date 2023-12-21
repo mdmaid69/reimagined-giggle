85742d624ee317a4ce6b4d17813c0c9fedd8b473
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:32 +0000

    load slowly extra beautiful tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
