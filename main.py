abe8da68019c744abc9a9e0259cc54a32ade9834
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:25 +0000

    show deep middle worth air

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
