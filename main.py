e668af5372a869c2bb7265297b7bffa61122edf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:38:42 +0000

    throw unless base taste degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
