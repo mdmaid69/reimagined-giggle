5162057bc3bf0b68bcf144d0cda46f4c1580792f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:42 +0000

    dance apart bridge criticism famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
