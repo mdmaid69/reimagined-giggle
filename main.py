f2c2e123223a52475901f572d632535f013b0b17
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:10 +0000

    month strike willing breakfast aim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
