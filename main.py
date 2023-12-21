1c8be1ca9bc18b1434a595cf9bbb34a0d4652d3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:05 +0000

    reason character original kind leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
