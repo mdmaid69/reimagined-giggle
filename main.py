d6baef3132f36605194fb11e16fa0585e8e8909d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:50 +0000

    interesting result benefit lot she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
