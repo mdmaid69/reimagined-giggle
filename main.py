a0b4e570e90ae7dea24bdaacd0207a1e0a18585a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:27 +0000

    boat proper compete side catch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
