ffce5467f23d9a8b7bf41e85a242fbfe034893bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:06 +0000

    union thank my house transparent

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
