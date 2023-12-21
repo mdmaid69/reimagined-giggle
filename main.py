7ae93cd953bf19b8c0d2be7350656b230f50eca2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:22 +0000

    explanation immediate suckme contrast second

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
