73bd87680281b410423c10abb77f256d4cbe2e9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:50 +0000

    official politics nothing must gather

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+print(sum(range(10)))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
