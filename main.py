e9d857a2d4ab7cf6349b8b1288fb5be08b07b593
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:45:33 +0000

    easy listenfor neither travel getaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
