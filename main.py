26bb8b665d82ea4f93134331ae6e3e7c77f4404f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:49:38 +0000

    save defend female minimum change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
