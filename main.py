0703e188e6e5cd1e69113fcec3499bf3d9e24ef7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:13 +0000

    convinced divide string spend beside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
