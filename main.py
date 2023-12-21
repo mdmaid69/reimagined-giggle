6f3f2993d3989884708f99d0080532ac4d30caa6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:44:18 +0000

    excited damage direct likely play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
