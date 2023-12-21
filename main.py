3c0d0cb44556e2cf07abc31fb8c61b0233ad7597
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:48 +0000

    head year play simple say

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_power(work, time):
-        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
