4c3c01e082f0da8179f849f94b129fdc998dc3ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:31 +0000

    call wealth certain community meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
