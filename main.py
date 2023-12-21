3ed92f02844cde0677da70048809fc90c75d4684
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:27 +0000

    touch live better fact sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
