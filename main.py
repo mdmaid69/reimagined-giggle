dd400246d13a7b601bfb7db7ac7872ce41c1a0d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:51 +0000

    her stand score concentrate actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def square_number(x):
+        return x**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
