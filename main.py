bc82ca8dcd75f9f754d31943a2a235c3fe03a94f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:35 +0000

    equal pass trick especially thank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
