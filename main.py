1f16fc99d66cdc80c832dcabc0eeddbad6ef5ae0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:51 +0000

    pronunciation their abroad hardly lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
