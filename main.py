7952100125b44024d90e640639586ee0a61f9a6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:53 +0000

    depend or number notice doctor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
