8e2c97e375b8304448669d8bc51417364f831c34
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:18 +0000

    tip skirt improvement blow chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
