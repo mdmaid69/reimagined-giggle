08aa21b41ca1843ba2277d16217458c2f3f71753
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:46:06 +0000

    basis mad mixture hospital total

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
