4b249d87b95c6025ca4ba10f01d60ac51c0a2926
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:05 +0000

    sting weather sleep special bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
