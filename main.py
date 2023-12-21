2aaaabc55c6d9d490f28f46c703c5c07ecbd8f88
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:35 +0000

    tone she top block simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
