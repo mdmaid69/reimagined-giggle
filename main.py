fb56c614e837d037784cd8aa70bbe509a3f8c612
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:14 +0000

    speak difficult past raise owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
