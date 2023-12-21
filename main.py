7f656f19033611ef2d192a8fc82476fd9749ce95
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:26 +0000

    exercise instrument come guard position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
