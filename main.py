82603f19eada2d8cb5ca81ddce48e85fb2602b8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:23 +0000

    vision Rr attention weak clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
