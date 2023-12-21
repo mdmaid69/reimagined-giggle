4865e8b14f0b919760d2391eaad64ab04c887446
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:42:55 +0000

    rub well turnon set miss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
