c7e11176e5495949797066bbe8392e8d16217bd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:13 +0000

    float money negative constantly slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+from collections import Counter
+print(Counter("hello world"))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
