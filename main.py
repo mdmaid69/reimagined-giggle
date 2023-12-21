53f92fa9a85d0389cd7f05429ea970353c220775
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:21 +0000

    convince person cry still own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_today_date():
+        return datetime.date.today()
