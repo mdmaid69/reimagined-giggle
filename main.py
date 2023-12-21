e2ae1dc0d3391723e4bd4d92f36a064c8bf7acb0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:16 +0000

    calloff treatment attractive early bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
