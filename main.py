bcc2ca5f042da0e42adc8e7e409ea6957e6987a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:36 +0000

    yes cry frequent settle move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import sys
-print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
