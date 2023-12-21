df2cd80951e73623092278c17268873ce082c42b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:27:51 +0000

    task calmly convince direct concern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
