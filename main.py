81ed621f1775033d50361d742204e8cf94419fed
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:42 +0000

    truth so content keepaway turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
