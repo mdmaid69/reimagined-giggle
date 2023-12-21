a231979672a9131345b0601c588f51b2ed666577
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:35 +0000

    history to team different emotion

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-print(os.getcwd())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
