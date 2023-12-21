c4992171f00399e87ab1a98a07973ef2840b02da
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:56:39 +0000

    beside slice vision roll keepup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
