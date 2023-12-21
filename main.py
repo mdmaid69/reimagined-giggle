a86c2e84c36e8c2fa4f9ee8ddf5f5cbcec62ac7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:07:17 +0000

    always sense substance have detail

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def sort_list(lst):
+        return sorted(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
