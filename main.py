69acaa8a4102a87089415cd2cd542dd98ed7411b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:47:08 +0000

    special burn fork least below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
