b913368053a2760d41c9aa314c65aaec27d91aa0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:04 +0000

    field move mass risk for

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
