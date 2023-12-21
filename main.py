3b68331831395d3361ef082b3d42fd4888836404
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:27:13 +0000

    copy calloff crowded rate promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
