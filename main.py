8401970bfdfd41b75a8640eb76f0de0dee2aec72
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:06 +0000

    drink arm chat serious getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-print(sys.version)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
