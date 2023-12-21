dc62f02aa24b971e95cc5225147b38db37a343d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:12 +0000

    raw difference turn train clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+print(os.getcwd())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
