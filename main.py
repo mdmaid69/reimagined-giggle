29bfe387a7aca799583a3779286a05fe07fb244f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:40 +0000

    quantity shadow needle way smoke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
