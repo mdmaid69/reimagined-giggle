f9b7add3c4fa87e2808b652e53d801ae6b58f602
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:19 +0000

    plan stretch spread announce crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
