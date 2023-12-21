ff466d73aa5a09c74e86de39f0bf8ad10b1e23ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:14 +0000

    gap left slice exciting especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+print(json.dumps({"name": "John", "age": 30}))
