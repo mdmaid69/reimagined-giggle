6a31fd312608107baa726c204d1596ea9e5ab7b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:48:07 +0000

    always touch content dick let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def is_odd(n):
-        return n % 2 != 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
