dc8e99d6be34e5cd247978ad75e2bbf443b2fa85
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:43 +0000

    drive telephone goup themselves even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def add_numbers(a, b):
         return a + b
-import random
-print(random.randint(0, 100))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
