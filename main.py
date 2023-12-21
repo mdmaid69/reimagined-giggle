96556b23a77f737e4fc95c2e3bfad0b9c1d4d6cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:56 +0000

    makeinto I juice that frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
