7eef8c6cac3bb2ec28b193e1c4291af17cbb2546
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:07 +0000

    fresh clothes sign keep city

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
