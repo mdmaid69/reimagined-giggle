ad94069ab453ecbf6ffc8455c0b2481c7ac8e6d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:37 +0000

    supply play effect rush there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
