9451daeeb9d60a1b23abf010e833d88846b3cad2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:54 +0000

    exchange direct angry from dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
