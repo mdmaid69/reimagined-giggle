fd651ff01f238116aeec5b17f5c7319e23d85217
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:19:53 +0000

    disagree bush sail shock middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
