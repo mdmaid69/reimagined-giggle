65e409ffc7988f60ae7c2fcb71e43c0574e18a72
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:43 +0000

    smooth sure reduce instruction wrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def count_words(sentence):
+        return len(sentence.split())
