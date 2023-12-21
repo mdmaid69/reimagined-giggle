cceaf1ea7edfc9f40c7cdb577aec9ce39a9eec8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:44 +0000

    string turnoff repair squash raw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
