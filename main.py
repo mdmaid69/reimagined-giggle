96e07d1fb0317356450670f218f4788531cd1073
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:03 +0000

    alive influence takeoff work Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
