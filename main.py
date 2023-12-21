edd5c8ef99a3abd94361e12acc6d6b1e23e9757a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:36 +0000

    medium consider Jj handle line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
