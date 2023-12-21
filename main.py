9530cf5add63fdc60676aa958de8682d74274905
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:42 +0000

    underneath arm create scared for

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-for i in range(10): print(i)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
