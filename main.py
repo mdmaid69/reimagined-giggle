db0d08f5ea60b689ec9467c22b5f905ded0f9f37
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:42:59 +0000

    judge get worth film hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def find_max(lst):
+        return max(lst)
