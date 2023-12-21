17b4636ce9e0596c30f67c4a169953963321a814
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:56 +0000

    connected mixture authority change switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
