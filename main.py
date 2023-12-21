201a16785a5215d5a4021179e93bfa8746674c3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:15 +0000

    occasional wash earn stare tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
