37c9774c2edc558261ed63b7c4c5a82f85b1e926
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:19:32 +0000

    foot you include delight emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
