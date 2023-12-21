d2c6703fe57da83dca77a053f437a88cf469455f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:56 +0000

    straight education peaceful keep spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
