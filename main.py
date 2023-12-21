69f89596ee50332ced2a98aa8f8d5dc5196b6970
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:49 +0000

    scare occur more wonder sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
