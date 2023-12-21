bb02142e69634fb0f3a8e4d45dcfa2a983cb7d14
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:46:15 +0000

    turnaway extraordinary damage famous breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
