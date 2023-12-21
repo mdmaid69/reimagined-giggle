08bc3b87c32b039c159a6a6c8fade823dd08e43f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:24:34 +0000

    interfere typical basic temper frighten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
