640c7d4ccd2860aeb3f3cc18fe90642fce536d81
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:52:06 +0000

    crime base near speech she

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
