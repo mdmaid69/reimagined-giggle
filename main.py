81dc180a2c7846925d1282244673f3c6d4d36e41
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:23:58 +0000

    appeal sometimes unfair row beneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-print([x**2 for x in range(10)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
