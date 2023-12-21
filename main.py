65349cd180be151244508a5b22f4282380059d70
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:15 +0000

    more like standard scream distant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
