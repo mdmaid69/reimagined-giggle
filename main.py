ab95a90faad6c74515e0b404fd21e87a352979db
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:52 +0000

    nerve inquire strike ever off

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
