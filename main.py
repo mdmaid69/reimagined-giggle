5d87494b76b4dd51ec0c4aa7dc76036f2c74dd2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:31 +0000

    too only convince block birth

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_area_rectangle(l, w):
+        return l * w
+def find_min(lst):
+        return min(lst)
