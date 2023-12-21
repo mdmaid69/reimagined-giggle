293cf1adfa2e8eb27fc99e6d4537abfae3bd2d4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:14 +0000

    swing run important nonsense keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
