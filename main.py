ca13dfbc03016651d1460fb4c576cc96336e7d13
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:23 +0000

    fail Bb problem sick fine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Average:", sum(numbers) / len(numbers))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
