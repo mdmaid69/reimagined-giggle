d576509fdeecc1e499ed2a673ecfcddeb46bd6ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:27 +0000

    press church complain attempt simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
