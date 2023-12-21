4e3a82eb626303189227502a331c2aaad3d4de63
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:08 +0000

    right season rough storm available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_area_rectangle(l, w):
-        return l * w
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
