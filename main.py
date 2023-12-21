f5db41e97e10cdee04c8ae2820688b72e4023ab2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:17:54 +0000

    unfair affect stir keen square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
