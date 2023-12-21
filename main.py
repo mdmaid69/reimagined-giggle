6f8823eea86120ed222a475caf9009115b514579
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:56 +0000

    right problem brown somewhere all

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
