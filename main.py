0361f0d6fa00d037f1c6d9a360f93c67c6c09569
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:43 +0000

    any sure suddenly exploration close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
