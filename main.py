5fac6e0d72b368a211f05c0c4b279fd1a531b6d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:05 +0000

    weekend wander wash estimate force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
