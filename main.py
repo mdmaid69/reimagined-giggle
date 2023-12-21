258b756641bafb0c10b258e16b410ab95dd5c105
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:24 +0000

    range runout angry word appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
