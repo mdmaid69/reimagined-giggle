d72de3f62b98f603b70b0f4d89575d3a28cc088f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:40 +0000

    scene respect whichever sting city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
