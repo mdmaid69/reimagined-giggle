41ca99d5710e6676733040c7c8f7eb4b707cfa23
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:19 +0000

    bit birthday influence immediate would

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print("Hello, world!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
