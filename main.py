9cf4c46295bf7d9f29be4252a2261491e36a3a2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:55 +0000

    safe direct successful tight watch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_index(array, item):
+        return array.index(item)
