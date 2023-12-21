1aca256d6c7417af5d2f561d60b14cffb997d46c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:50 +0000

    motion minority speech climb shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
