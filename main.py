e7559260ebad255f6aaf084e978a85b0d712eb53
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:05 +0000

    trousers your new colour serve

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
