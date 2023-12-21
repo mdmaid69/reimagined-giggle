014bc1cfd7f29d18cc4703be4b47be737918e681
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:16 +0000

    quick forward rise bend interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import tensorflow as tf
+print(tf.__version__)
