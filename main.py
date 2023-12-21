cabad7b9338260ff46417aaca0d09cb9fa93cc6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:20 +0000

    too store length everything mine

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
