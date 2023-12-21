ce2e1cdc89f414c9742e451c629c2778e0859729
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:26:51 +0000

    key age want safe Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
