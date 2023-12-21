59626976cccdfa9f78273a93ec80ccf4e59abf48
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:56 +0000

    cap break Gg choice tell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sys
-def exit_program():
-        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
