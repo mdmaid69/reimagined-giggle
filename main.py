7f762d187137d5551b50439925f17bb79593ce02
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:42 +0000

    every exercise set rely action

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
