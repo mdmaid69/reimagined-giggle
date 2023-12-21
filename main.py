62d6b36a582987a4d8de38d871d26270807eb485
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:30:14 +0000

    comefrom so extra left change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
