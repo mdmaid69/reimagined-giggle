0803c3eb0f202dcc9d43de0aeea1fe3d8c8c1d76
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:54 +0000

    abroad statement advanced call ride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
