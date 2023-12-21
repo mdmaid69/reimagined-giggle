7edfe5626f3927496c69545fe6a46f9aac00a8c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:07 +0000

    evidence muscle food drink strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
