c8b806ac88822c70c1e386c1124a4618fe00d1f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:34 +0000

    nervous road successful pretty quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
