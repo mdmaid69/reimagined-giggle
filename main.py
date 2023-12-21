fa8eee25bce477de1581fd8b0ae4908ebe120f7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:59 +0000

    birthday might rid backwards thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
