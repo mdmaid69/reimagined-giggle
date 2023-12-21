4613a4ece61922a8295891d03cf0556d0e936560
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:16:01 +0000

    wise go shoot temper weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
