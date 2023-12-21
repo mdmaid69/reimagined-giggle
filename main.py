690fd2d06d7b4cc56fce26fb1d0a7ca09025d0cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:27 +0000

    sink 573entertainment substance type travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
