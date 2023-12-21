fbc260f3262c434609361447d675086716ef9ce5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:31 +0000

    polite over announce rather lost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
