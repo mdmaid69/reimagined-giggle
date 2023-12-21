eaa36ec7c7b68a4e47770d8436659fb35a3b83ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:33 +0000

    sometimes kind tomorrow relative talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
