205f93fe4fdb64b643c8e670448ea9e59c78fd09
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:35 +0000

    bringdown scream summer form but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
