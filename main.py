4cc3ad6ba0c7b33381ca20cded26f25446529daf
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:15 +0000

    trip by sink dress match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
