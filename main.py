d03e158e0309f1bc313b8664601209b73146f7da
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:53 +0000

    concentration nigga owe path present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def count_words(sentence):
+        return len(sentence.split())
