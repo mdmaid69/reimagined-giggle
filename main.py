dc0f57115b98395b254ed1e544ad862fa59f7418
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:19 +0000

    other pass length equal clue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_working_directory():
+        return os.getcwd()
