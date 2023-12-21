9f5ac8f6d4961cd17409d1adc81f0c7df8723bd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:50 +0000

    judge plain comeback think lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
