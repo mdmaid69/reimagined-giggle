eb333cc254bde522a2665b9bd0f033a5ab7ffb08
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:34 +0000

    bunch early live since reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
