5f6f05cc372f60dbd1d6b459afb61f579b5681b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:25:02 +0000

    my defend somebody along anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
