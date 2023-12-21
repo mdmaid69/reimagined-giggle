ae6adb8eb9502a3877dcc847d04efe73aac7d967
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:23 +0000

    issue comeup capital match greeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
