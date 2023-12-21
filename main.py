8938dcff11aa3d143dc6ffa16f7ac9cb0917863b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:44 +0000

    cut bar game lead point

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_bytes_from_array(array):
+        return array.tobytes()
+def convert_to_hex(n):
+        return hex(n)
