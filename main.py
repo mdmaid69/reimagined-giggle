713c11d96a5109496cdd975c8aaaae4346ab82a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:06 +0000

    off cope standard complain spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
