21cbcc5b0d99c4d4a08cc3e5c7224b946b00f7e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:32 +0000

    breakdown perhaps there set tool

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def multiply_numbers(x, y):
-        return x * y
+print("Words:", len(text.split()))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
