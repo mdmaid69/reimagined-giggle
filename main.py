05da713a6a6f8cc0223f8a0d22b17b5ab39e9928
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:06 +0000

    sleep sense imagine protest mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
