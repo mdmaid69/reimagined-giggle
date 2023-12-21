b412d127d5e2611127958d4b915c9a0e595af3f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:49 +0000

    work calm side funny cool

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
