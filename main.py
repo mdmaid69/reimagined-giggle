d807aff7d2d6962c44f91d514c46b39cfb1c2cdc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:57 +0000

    dance child inform survive there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
