403d3af918c9da8f5c5966c26918a66674dec83f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:50 +0000

    fair deep minute schedule news

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
