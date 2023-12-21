322d6ab8f553f64b2ce5a838b2d008dac0cc1e60
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:56 +0000

    suffer much by noone conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
