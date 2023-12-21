fce5c2b49b365e90108ea4281249f3dcb8ddb6af
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:39 +0000

    honest set sentence what by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def set_array_item(array, i, item):
+        array[i] = item
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
