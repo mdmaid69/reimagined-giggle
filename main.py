009a412bba380c70aaa546baf367d44cae0b6020
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:46:14 +0000

    suddenly unlucky cost visit serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
