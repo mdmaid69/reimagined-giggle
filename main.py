cfdba4c4dd115687092d0c20d2a81f2e6adc1fda
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:10 +0000

    rely tune excuse translate classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
