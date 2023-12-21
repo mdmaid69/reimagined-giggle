a8ac34f2e495a2d1723882fccd196b4f99c34a47
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:16 +0000

    connection most afraid possession cough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
