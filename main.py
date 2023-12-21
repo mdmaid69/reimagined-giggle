d7a5fb3ecba28ecc8c26fbbdc143522f496b6276
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:54:37 +0000

    call track sensitive field join

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
