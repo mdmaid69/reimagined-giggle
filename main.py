1b3a8670b0743021db2185e353e0f5420bcc6095
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:59 +0000

    fear earth hello immediately fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
