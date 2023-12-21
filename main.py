f601f83aadc7d2ee2569b10abeffec3fa6415bc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:46:38 +0000

    environment keepoff smart convince defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
