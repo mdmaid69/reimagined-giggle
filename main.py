8ed2668bf8b7b79f9868d9184401ae09f3232b40
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:28:52 +0000

    burn Hh stone class profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
