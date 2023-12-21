db78f82dd34033ef7a1127f3ae2fdb337d99a7b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:53 +0000

    door that about pleasure bar

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
