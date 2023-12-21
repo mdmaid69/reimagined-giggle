8f8b4bf497743d75b0c1d0579cb019cca27f0cee
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:53 +0000

    benefit store satisfied congratulations comment

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-print(random.randint(0, 100))
-  def is_even(n):
-        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
