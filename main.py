7b89992e35637b1368962ec2225c1959a3f225e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:00 +0000

    belief confident sad swallow even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
