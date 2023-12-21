2133b20e3dfccfabb674a5db51b1dee0a517cba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:35:51 +0000

    cry shadow well try 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import time
+print(time.time())
