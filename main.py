8089b2466f43ff43e3e37953296d2ba8756e0e60
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:20 +0000

    make her trial fail force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
