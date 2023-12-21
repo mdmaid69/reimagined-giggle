199845459fa65e584ca6edc783917d4d9e2560b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:42:02 +0000

    miss pencil prevent interest meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
