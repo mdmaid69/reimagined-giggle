4e873a34691674b0b50db49e02b4954a97d743c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:53 +0000

    whichever last watch dinner survive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
