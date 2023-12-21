ca9d85a03e0d1dde32fe181535c81ac6c5757445
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:22:18 +0000

    ordinary quiet comeup fall keen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
