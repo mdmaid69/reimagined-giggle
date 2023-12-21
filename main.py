40cf47553ccaac7fe2bf62aaac49a0b6c113ee69
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:07 +0000

    once trap border reduce properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
