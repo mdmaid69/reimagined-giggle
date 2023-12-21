ee869e61a699d4b8e950f791b55f1f6242c40ba1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:49 +0000

    write country taste community swing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
