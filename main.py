82446f6f5316f8888aa547582cc783f745892a56
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:29:58 +0000

    address compare turnup flavour authority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
