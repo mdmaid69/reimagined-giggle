5b93781b16572a34fe995aa838f917e3d367d370
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:20 +0000

    show appearance note either minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
