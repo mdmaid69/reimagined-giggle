88b581f6e56da407e9240fb960193cb17f2dc419
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:09 +0000

    lift urgent shake important whistle

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
