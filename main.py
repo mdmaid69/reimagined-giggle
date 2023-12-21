245e62cc771807f8db8c6b34d778b0c702ba1d4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:20:50 +0000

    right contents block fast gift

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
