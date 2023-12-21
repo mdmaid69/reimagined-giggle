571fa5f0f1961b94175eb1b5216e27453df426c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:46:47 +0000

    ready train record although recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
