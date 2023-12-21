dd4e7f859a4946a51ac913b4ba372df2a2047084
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:13 +0000

    this lonely plastic achievement shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def remove_duplicates(lst):
+        return list(set(lst))
