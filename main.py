350a05bd30b2d375d3b36031b4d9bf49a43a57bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:25 +0000

    look visit taste tight lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
