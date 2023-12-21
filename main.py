c8c24dbebbad59adf7a7336c55f100870e601869
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:43 +0000

    aware by nowhere lonely shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+def convert_to_octal(n):
+        return oct(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
