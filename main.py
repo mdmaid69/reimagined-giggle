ebfe85548d029a29ca1e554068f4639aec0d7f9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:26 +0000

    should up not modern rarely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
