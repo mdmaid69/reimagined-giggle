0e5542e8508fdd5f90f0cb967d78fcd42607071a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:32:12 +0000

    to shoot wrong tread minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
