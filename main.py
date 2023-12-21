bb4fa8cb75d6349e87a9152102a6757e58d9440b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:23:37 +0000

    combine brush final criticize relative

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
