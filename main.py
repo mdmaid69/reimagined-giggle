48f453d3e37685d89246c17cd8333003b1ea2537
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:09 +0000

    repeat plan front cost every

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def change_working_directory(path):
+        os.chdir(path)
