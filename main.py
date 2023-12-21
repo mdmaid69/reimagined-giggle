6a391fbb1f4b7b08b68c5621c87c43f16e8a4bab
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:03:11 +0000

    tonight disagree Qq journey shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
