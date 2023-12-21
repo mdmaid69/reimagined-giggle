4ff7d077c66fd7b347faabe37bcade3eda96203b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:33 +0000

    official along convenient mystery favourite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
