b321771e49556be9cac5b289d46f71d47281ec56
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:07 +0000

    curious dig compare Cc judge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
