b8a670325d92fbe513d98904c2762d74a9f61b11
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:06 +0000

    plenty relax peace fix brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
