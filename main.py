ba31dcee2704ee0b8a56fb05ef5c44a0e52ac1be
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:18 +0000

    stone work Zz apparently belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
