3ce0684eebdc077bcd501497cd12f7deb14347a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:31 +0000

    safety bored passage rather bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
