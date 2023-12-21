b2c09822e210471db3ea6617ea5f25eb99f45436
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:25 +0000

    lost opposite reject air distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def cube_number(x):
-        return x**3
+import collections
+def create_stack():
+        return collections.deque()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
