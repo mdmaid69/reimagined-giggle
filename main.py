f362fcc010c3fedb8dd871b529d63f18ee9c6798
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:14 +0000

    give information software regular distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
