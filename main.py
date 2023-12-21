c45b3c820d5412d3ac266c0928c0742ee919556f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:00 +0000

    though after comeoff while getout

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
