68ebc4d9d2cf2a508de038351f62d9d36292b323
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:36 +0000

    minimum serve desperate satisfied cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
