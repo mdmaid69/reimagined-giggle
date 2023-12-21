6222c912fd7a74caecdfeffeb6f858a88dcaf55a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:50 +0000

    branch geton which basic safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def print_python_version():
+        print(sys.version)
