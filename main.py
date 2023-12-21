0829f28f6a3bfee4f78b3383e19f070052287291
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:59 +0000

    stamp reject stone breath culture

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
