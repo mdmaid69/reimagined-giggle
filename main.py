cddc6d98d522b0e441efa4890c422c77f723b534
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:44 +0000

    mention the poor sore wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
