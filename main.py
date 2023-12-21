5fba1c0de902866d820690f230b1af93a697c1f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:53 +0000

    later muscle dear subject reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
