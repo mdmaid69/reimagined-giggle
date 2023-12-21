8e86fc056a873f2f2648e8ae0992ff366cf34658
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:39 +0000

    history water breakdown drawing precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
