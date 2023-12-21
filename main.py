4799d42c29183592ca1e5c8f5b9215351697974c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:09:18 +0000

    pleasure succeed passenger pour apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+print(time.time())
