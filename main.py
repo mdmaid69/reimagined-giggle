45edd852124ddf9bd2cfb308334980699dd2abb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:04 +0000

    achievement amount please include return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
