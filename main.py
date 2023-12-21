bfc27b5b72e3a9bdb7cf329f2b71172754991e77
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:02 +0000

    separate brain base unpleasant out

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
