a9f615fc773386b6392e933e37012b417c31d154
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:20:01 +0000

    ill imagine tear laugh butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
