1e1f42d90ba40539d9ce86898b007262903949d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:18 +0000

    expression group result contrast any

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
