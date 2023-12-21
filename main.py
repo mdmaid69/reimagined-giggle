7cae72571289c78efe353eec2001149bc4773575
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:05:37 +0000

    swallow capital claim view dance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
