8af18913cfe7c954f7ed2ea524ef767290ebec32
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:48:10 +0000

    habit model plan natural direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
