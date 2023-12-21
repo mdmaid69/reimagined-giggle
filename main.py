24477f8ef8240bcd2b7601911dadd71a04b42740
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:30 +0000

    sad head consist community thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
