bfb7339f32233e0afaf128fcfdff2e6d30362d42
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:40 +0000

    still invitation smile maximum fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
