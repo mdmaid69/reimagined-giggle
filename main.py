ea83664df4a334b8094af4b51d47a4a0e0bbdd11
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:17 +0000

    friend shoot rest turnon call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def convert_array_to_list(array):
+        return array.tolist()
