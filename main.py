6f87340fa828f4c6a86830d6572a3f8579e84c3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:59 +0000

    keen breakdown stare rain usually

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
