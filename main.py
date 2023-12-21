88249678705459bb9b5693d461195f1727443ff7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:06:15 +0000

    after anything lesson place 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
