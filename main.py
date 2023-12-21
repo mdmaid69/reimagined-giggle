1be10a8fae19e37d949bc7ab371c4dd901e10f60
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:42 +0000

    knowledge fact tap complain slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
