49bfeacff686d04267006b362bbff71868fc44fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:55 +0000

    top turn uniform blind upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_max(lst):
-        return max(lst)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
