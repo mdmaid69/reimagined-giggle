2deb4036569e3d507c5049a6853cf91be84af64b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:49:52 +0000

    church must honest possible 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def greet(name):
+        print(f"Hello, {name}!")
