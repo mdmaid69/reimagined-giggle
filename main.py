51fef7a71e46487d1dfd5f1d160391697fb84e90
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:02 +0000

    blind answer short tradition Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
