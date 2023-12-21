358c7ffb9de1a4d2e49b8be96c6a84f2ef70144f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:42 +0000

    broad careless stamp autumn turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
