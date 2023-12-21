d5f74dd10b9320a77b5d9d7b0504602ecfa2e5b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:06 +0000

    deliberately should ready such ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
