214ff3599dde48a03e3724f484b5c8b521f7ac18
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:51 +0000

    shut claim face pretty allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def clear_array(array):
+        array *= 0
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
