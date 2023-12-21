c1b12976aa37ee6752da087c21d4100f26e831b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:20:46 +0000

    style no payment lift mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
