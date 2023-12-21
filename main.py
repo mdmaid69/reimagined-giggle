b391f13c4392376b1c94d46e27b7c961cd86115b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:46:54 +0000

    us firm about achievement single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
