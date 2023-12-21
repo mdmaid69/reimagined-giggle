ebb08f85e8db61de5c75ea96ac88942166804e4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:31 +0000

    basic average slow opportunity rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_itemsize(array):
+        return array.itemsize
