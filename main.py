8431021d30596ced3621255bf9cae278adc3c949
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:26:22 +0000

    wonder yourself no remove safe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
