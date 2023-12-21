fb676572f2f24a6154570a083838335d9b14bf8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:26 +0000

    report kind polite space thinkof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
