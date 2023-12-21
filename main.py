fa7308243e3abe90fc162ce93c704dfb4d87c359
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:17:00 +0000

    getup cook him motion before

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-for i in range(5):
-        print(i)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import random
+print(random.randint(0, 100))
