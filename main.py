0c26fe99c1f8e22c24ff237321058d82386c2b12
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:00 +0000

    design finally tragedy demonstrate careless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def square_number(x):
+        return x**2
