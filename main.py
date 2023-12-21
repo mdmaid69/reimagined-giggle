5250e687145079839c61ee1ed709b4be8cc9c033
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:58 +0000

    Cc further story loud culture

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def sort_list(lst):
+        return sorted(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
