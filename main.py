c8474870005958b3be6be64e2c72849c12706065
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:45 +0000

    mouth serious will present difficulty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
