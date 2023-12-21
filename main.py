b9551c365b7859866ea324e6833a942aeef1a279
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:01:31 +0000

    godown story Gg ocean experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
