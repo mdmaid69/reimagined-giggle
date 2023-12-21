e1574db9ab8ced8d2a267d16acf0318cf784c7fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:20:56 +0000

    because brush tomorrow wide region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
