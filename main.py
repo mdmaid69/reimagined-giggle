4a2506b98d52ae6c0146586d04cd6a5d1875fd8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:35 +0000

    stare Zz born clean deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
