e08596ff01561effb971f8a3ade59785379076ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:14 +0000

    interview war rest maybe by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
