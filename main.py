50fcb739ee426ef83822bbb6c5d79a5d5faffa97
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:59 +0000

    crime have care exit group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
