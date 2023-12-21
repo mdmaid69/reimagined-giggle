7a434bc3ed1addef71284a2312792a3a052b9dc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:29:56 +0000

    shape trousers worth walk over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
