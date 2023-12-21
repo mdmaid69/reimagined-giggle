cb75bddf69c636b59e27f2b3afc55769d2a8f09e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:02 +0000

    show bush keepaway swim recently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
