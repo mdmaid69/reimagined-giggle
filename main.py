b005d413feb16397bf989ebead3678efc1ef070d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:29 +0000

    side ashamed question consist match

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
