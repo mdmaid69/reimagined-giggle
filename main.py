033b75bfe68ae5af9ea1ba0d90474250e7d2cad1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:56 +0000

    short set mess now better

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
