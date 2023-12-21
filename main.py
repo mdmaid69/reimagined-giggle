4e6891b4dfc53d7547b6dab9c474a17580bc91d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:12 +0000

    develop tired exactly declare blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
