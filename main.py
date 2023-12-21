f5e735009008f790c496a459c19379a29a7d462b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:40:55 +0000

    instant concern present grade trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
