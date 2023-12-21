0c6213533bf6b5acdf67d80ec15f9a4940414c84
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:27:29 +0000

    class bottom mad apart he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
