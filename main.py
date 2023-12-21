ed373cbd0d771d326b3f6cad2a74c245481dac0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:19 +0000

    prepare example bringup couple development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_speed(distance, time):
+        return distance / time
