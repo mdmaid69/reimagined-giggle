d9e52ecbab0c8a5c1846f1d686b404183f00e610
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:24 +0000

    care direct freedom weak moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
