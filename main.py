80cf53039f6b83256ce9670e46965bcbe89a5a70
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:50:29 +0000

    correction look trust mess as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
