e4ef3e02dcff227472ad9e017bb42c47c5fd3043
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:34:38 +0000

    tend too flat commit length

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
