f78952a59d26985e8976425b0ce644ff5abbb5ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:06 +0000

    mean sore character list steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
