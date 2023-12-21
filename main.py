761c7e0114d053d4b2b42266c7c9999491754dd1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:34:12 +0000

    failure suddenly aware name past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
