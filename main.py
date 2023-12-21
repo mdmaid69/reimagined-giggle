dd5ec570288b6094fa0eb9688a5c39cbba9f518a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:01:00 +0000

    post combination operation community partner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
