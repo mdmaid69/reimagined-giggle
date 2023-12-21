84a4bd72ac24388f0d3fc2a280291ed9cfc732bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:04 +0000

    bin towards finger through finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
