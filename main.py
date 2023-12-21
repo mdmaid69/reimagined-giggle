4cb41235e779d5a0f1ce1253420ce7e52744a0a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:12 +0000

    already excited vote always face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
