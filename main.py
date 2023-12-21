e5ee2276c475292926e83916ee13cf4f5768a958
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:36:20 +0000

    film habit joke luck fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
