5ee6eb641a2257f1df900d573474e31d27515335
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:48:00 +0000

    pull any entry here forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
