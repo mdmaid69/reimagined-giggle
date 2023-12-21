2a72108e77a5476ef5f5605e72c3b6853e5f7ab3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:45 +0000

    deep main charge count throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
