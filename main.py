5439fd1ff4fe1c4706ac50eb2eb1f9f0044491b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:36 +0000

    giveaway water deaf under song

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
