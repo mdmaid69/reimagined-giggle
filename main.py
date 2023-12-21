d67ded91e6f5d48b8ca17f99cec60305c7ceb20a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:38 +0000

    preparation any material illness popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
