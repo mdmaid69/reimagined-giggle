58680a07fe22b2649f97f128e7cc9bf47ff474d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:29 +0000

    escape blame trade exercise advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
