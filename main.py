61415d122f3b731e91e5921de6f726694ab88f17
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:00:38 +0000

    form basically declaration even friend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
