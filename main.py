a7de2165d841f73e5487ed485aa35370ec84f0d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:30:04 +0000

    in exchange bang thirsty study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
