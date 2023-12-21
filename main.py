d7b8dfee77a382a77625ab4b3f689484d9d1ab87
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:34:30 +0000

    cruelty environment top someone just

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
