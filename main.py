73bdf6a551cab2dcf90af9743f66eda5bce01123
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:26 +0000

    secret spring deliberately basic grade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
