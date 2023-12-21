c489ba2b0fda28c39184e9eb2f1eaf3addc600e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:40:04 +0000

    mess favourite fortunately goout comment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
