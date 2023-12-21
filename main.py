25268a05a3b0a8eeed3c1a27e1ece1ef8cd80cd1
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:18 +0000

    within hospital open manage cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
