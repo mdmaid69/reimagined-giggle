a9904619e00789e52229e1ab0097ebae6d18773c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:41 +0000

    careful further Bb coat part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
