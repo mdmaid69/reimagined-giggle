e66c6179c050f653a3cbfe867220dce10621bb26
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:59 +0000

    scrape sentence object male buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
