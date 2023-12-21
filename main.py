0ff3e01b73a1cab4be84e295ec8ce5f2a77e25b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:02 +0000

    improve inform skill service bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
