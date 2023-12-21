a9a044c31e79a39593cea8ee891494e54c0c1408
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:39 +0000

    broad provide will rain vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
