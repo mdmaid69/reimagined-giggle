1a7e02141fd0c39c0f393c7ca55e7d0a9fbf8e17
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:30:46 +0000

    emotion off behind last stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
