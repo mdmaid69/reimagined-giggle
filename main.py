98a974a3b5eebe207635d7dc2864d448b32944f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:36 +0000

    distance trap their reduction press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
