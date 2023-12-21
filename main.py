39e7394b72793e04ff9cbeb60d1a4553da17949c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:22 +0000

    trap divide mainly minute telephone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_min(numbers):
+        return min(numbers)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
