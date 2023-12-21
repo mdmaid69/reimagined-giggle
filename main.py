21f9d512ed2e4bf92df588a9c61d6fd5d04c089e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:18 +0000

    charge far schedule try raise

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
