55a3920c12cb8bfcdd5d09154f85487b1ee76b97
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:46 +0000

    address cost arm sport answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
