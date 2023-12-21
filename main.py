d2da9ccdb0363bd3f8d87cdfa73702b41170d4e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:02 +0000

    bite angry possibility face environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
