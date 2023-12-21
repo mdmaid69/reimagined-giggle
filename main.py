98ae94636dc67c90b9cd196bef13f5aad931cbf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:22:50 +0000

    turnaway shock exactly breakup case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
