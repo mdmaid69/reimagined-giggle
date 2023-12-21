eb739170cb2b469e517daf6e0ba49d30714e9161
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:39 +0000

    point unkind deserve declare previous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def add_numbers(a, b):
-        return a + b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
