b910ef82b6bb63a84774889e6f523ce3cc084fbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:39 +0000

    needle image unable visit after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
