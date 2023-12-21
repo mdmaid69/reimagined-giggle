b71bd5ee8176c3b07d80a653cc1768575a64cb47
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:43 +0000

    bet unlikely high convince return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
