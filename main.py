b77311e9d326779e31508eeffb64f71d55b2bb26
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:15 +0000

    news flow pump scrap address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
