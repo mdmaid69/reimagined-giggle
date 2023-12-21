8d0564c6e8f66f1be0bd9b45da3ef3d3ac8e16f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:35 +0000

    innocent art school comeup sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
