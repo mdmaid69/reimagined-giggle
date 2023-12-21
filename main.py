1e08e68cf00cca255325ec68336db0485420bdbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:23 +0000

    amount gentle fat equipment wave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-print(sum(range(10)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
