e5172e052089bb688ceb9bcc45ac9a636759aa31
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:12 +0000

    material platform top understand strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_max(lst):
-        return max(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
