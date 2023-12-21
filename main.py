4a008ca18f8390b442fd7e0cf527ada7808f8557
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:10:56 +0000

    future bringforward conduct group their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
