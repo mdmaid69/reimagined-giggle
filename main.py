0f0a7cc74fbfd98da2c59054d01a72f5a38896e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:25:05 +0000

    cover measure trade vote model

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+def find_max(numbers):
+        return max(numbers)
