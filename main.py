fe79e024ff95f3cc1236d3b13c43b7491442dd2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:41 +0000

    fear middle pile else arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
