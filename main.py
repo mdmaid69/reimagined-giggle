c23777f4b8ebe5e9e2812c16a0e83b9c74b17d88
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:55 +0000

    through Vv split now turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-print(re.match("h.*o", "hello world"))
-import sys
-def print_python_version():
-        return sys.version
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
