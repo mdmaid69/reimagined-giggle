779e5fbbef5905245b183f18524bdc1af19893ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:13 +0000

    insist generally method possible at

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
