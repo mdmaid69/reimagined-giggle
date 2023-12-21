896d1a71a8d1574ffb0a2026ce46739d18a5c653
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:33 +0000

    ball gradually ready market fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
