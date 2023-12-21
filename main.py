878ca3297a55fb55c0b25e197976bd5020c043fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:15:02 +0000

    never attract Ii stop fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
