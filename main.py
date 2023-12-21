e2f48d24e524b17510859b5434600113b8324b19
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:16 +0000

    have land old claim arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_unique_words(sentence):
-        return set(sentence.split())
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
