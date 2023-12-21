ae2a93a5221690c0cdc207d85680c5a58ad24339
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:24:20 +0000

    sink foot impression top punish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
