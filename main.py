a42bea8c38635cee0d34b3014386092cd5655a13
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:10:26 +0000

    complete programme supply better hurry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def convert_to_binary(n):
+        return bin(n)
