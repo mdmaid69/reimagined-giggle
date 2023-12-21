4befbb66dca3516632e5dc8abba983b685e202e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:51 +0000

    perform during happen clearly guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
