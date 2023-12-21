a604608ce3d0652189c5057d03311a15dbf73cd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:08 +0000

    question silent flow container between

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
