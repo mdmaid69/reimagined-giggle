d6e40ac856a5f29874c6fed5806faf9dce5a3754
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:05 +0000

    exhibition question command comfortable treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
