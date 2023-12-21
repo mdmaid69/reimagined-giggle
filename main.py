0efcbcc700ae688c3b6703871424d7e50130faa6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:12 +0000

    between arrive opposite slip cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
