4ec67f42222f104338b7ae87f018ee8f87f219a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:41 +0000

    strong heart but weekend classroom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Characters:", len(text))
