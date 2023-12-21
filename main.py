25f6f5408b2e236fd521c21bd6b21b69eac57cc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:30 +0000

    declaration quick fashion among image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
