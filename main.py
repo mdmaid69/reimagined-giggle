daaebb9c3c668fa620d798699b43298f39f4f55f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:21 +0000

    cum course future wear low

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_time_since_epoch():
+        return time.time()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
