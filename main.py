29eceb2e28a7c101429bda87b0105c3b9faa5c49
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:42:00 +0000

    chin claim uncomfortable set information

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
