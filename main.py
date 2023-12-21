3b97bebcf554150bb38e9f25a38584370b3fcab0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:40 +0000

    mystery brush pump physical life

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
