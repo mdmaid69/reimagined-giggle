f4b8fff609c72150060c3513e2fd3e7c5724a8bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:16 +0000

    drink sight design weigh goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def cube_number(x):
-        return x**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
