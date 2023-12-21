df5bef9c738688dc930031bacf7409df063d73c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:57:25 +0000

    use quality centre travel thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
