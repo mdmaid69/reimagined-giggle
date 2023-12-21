521e93c4d1845eefedd6b637d5dd2e9ec4c8aa43
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:55 +0000

    turndown study pain Ll force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
