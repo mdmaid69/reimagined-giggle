0a1d93dc15cf7fdb854469ee3c3f94ef0bcd6ff0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:01 +0000

    lose off blame possession middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
