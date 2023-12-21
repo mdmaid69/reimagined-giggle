81718ee07a6e3e01aadc7907d169a725b978be79
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:02 +0000

    radio repeat opportunity certain 572enter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
