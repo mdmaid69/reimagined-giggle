dd7ca98b9eebdce1e259b50235434e327891f33a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:19:25 +0000

    accidental freeze beat little police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
