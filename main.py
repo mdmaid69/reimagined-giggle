cbfa2e4b77163ef70ef6d80be2d4883e3778e17c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:30 +0000

    chase anywhere she shoot study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
