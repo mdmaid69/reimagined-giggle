9ac7e8b9317d8eb9e9c441b0230761d49166764a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:35 +0000

    low private minority ought wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
+import os
+def remove_directory(path):
+        os.rmdir(path)
