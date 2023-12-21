c62660eef9ef11d2382005ecaf55d47d07998a10
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:43 +0000

    several suit mean share flat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+print(re.match("h.*o", "hello world"))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
