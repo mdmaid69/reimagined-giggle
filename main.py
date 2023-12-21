8beb166741eb38e12549b68aac2670fdbde306eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:05 +0000

    clever throughout doubt year gather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_list(array):
-        return list(array)
+for i in range(10): print(i)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
