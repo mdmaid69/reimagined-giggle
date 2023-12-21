41553070de5b3c96b6c854754adc27ed5465a277
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:48 +0000

    single steer level Kk follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
