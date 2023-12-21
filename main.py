608b1ee509e75ad38dc6e50affc065eab57e3a80
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:47 +0000

    guard drop if everyone sharp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
