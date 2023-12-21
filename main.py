f8d3422804e5f90fd8ee7a14e603ee3a5f6889f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:49 +0000

    pick OKorokay thirsty answer once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
