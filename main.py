425c26d2a07c4b2ff0c6354d3c0e959c8323d896
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:00 +0000

    decide able cheque kind catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
