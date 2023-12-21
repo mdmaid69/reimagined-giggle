e9e7b22bf9b63041791954ac7ba5d4e6ba120472
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:19 +0000

    waste leave usual succeed 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
