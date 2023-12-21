cae7da445c4042a10eed2d88324770396021f5ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:49 +0000

    polite stretch night call find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
