fa224f35cea399a941fb4ed10498d49daf6d22d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:42:26 +0000

    again class exciting welcome deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
