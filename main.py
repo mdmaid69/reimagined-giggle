cd9e14d5400d0100bfc0f482411a3053809315eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:44 +0000

    world perfect makeup fetch whistle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
