f156cb1d5a3f0823d66483576ae0982dd115a61a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:48:45 +0000

    patience afternoon attention pressure importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
