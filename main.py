ce316763fbd33614de72ef93503e81165d941fdd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:46 +0000

    mass forgive issue reduction better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
