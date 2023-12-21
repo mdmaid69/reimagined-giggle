adbd90701241d7d2435e32a3b6db09eb6f9109ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:02 +0000

    debt group geton spread claim

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
