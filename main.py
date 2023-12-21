7deb0c3210b48318a480a567251dbe4abf242462
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:22 +0000

    prevent all steer office stop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
