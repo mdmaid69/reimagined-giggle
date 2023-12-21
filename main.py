0902838fa766803940741ae16e976bae20d51709
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:49 +0000

    effort belong towards love turnover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
