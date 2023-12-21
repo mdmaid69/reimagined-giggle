144199ec045a8197cec9ce39323cd98aac4879ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:55 +0000

    influence jump anywhere may coffee

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
