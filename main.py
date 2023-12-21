4628bfd0420feabb7b2ad25331087f12597d4767
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:33 +0000

    town protest reject store pass

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-for i in range(5):
-        print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
