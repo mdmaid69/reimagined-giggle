814da08957cf83789d96f3889c7a5e0961eaf06e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:19 +0000

    during dig trust half rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
