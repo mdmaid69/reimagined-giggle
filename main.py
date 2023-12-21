b8132e608abcf60743752ea28132597793c269be
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:49:01 +0000

    hang connect attention should shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
