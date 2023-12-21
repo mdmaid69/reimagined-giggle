a37161c2bbb57ac555f3c10230ec79f45e1e4cbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:12 +0000

    else somehow sorry carry laugh

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_min(numbers):
+        return min(numbers)
