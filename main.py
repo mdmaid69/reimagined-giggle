14be05f80971755f4e4aa32066fb1f574e854c45
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:17:30 +0000

    about mine surface disturb train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
