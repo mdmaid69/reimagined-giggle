1f664b64048a5f1294ec5bf58ed08dc7b1de7130
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:43 +0000

    clean top age squash basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
