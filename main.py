b62eb280c54420976521c919e628a98f1fd56228
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:54 +0000

    godown stir recovery hurt middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
