71a527b6218bf965714415b1ca57d222e2038c83
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:51:04 +0000

    spot thank risk beautiful sting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
