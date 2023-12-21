26dd0aff5a6e605a8bb6463d5095b66479ea3253
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:34 +0000

    either ask sort pipe hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
