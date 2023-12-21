cddf783510fa6d76cccf52ad06cfe8906c5f56c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:19 +0000

    nice home classroom male smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def subtract_numbers(x, y):
+        return x - y
