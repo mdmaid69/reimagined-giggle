62ef4a75b1b093d221118810f9f06fcdd9cdb2a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:33:20 +0000

    whenever family comfort steep couple

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
