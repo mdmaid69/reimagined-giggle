3eba168adf92c04349d7eebb0c59a549368c2064
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:32:37 +0000

    clear common trouble bus fact

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+print(sum(range(10)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
