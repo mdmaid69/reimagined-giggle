74438b7c18a739972ef5a10edfc4e95d26589c47
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:33 +0000

    make getup temper direct below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
