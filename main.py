2bc3e0efb8e355fe3c5298d38572d3900eaedcf0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:40:01 +0000

    fast learn punish breathe indeed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
