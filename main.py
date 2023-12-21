2fd4a5a469f3f3f9c79c61df43b5b54fa49e9d84
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:37:09 +0000

    seriously sport science old silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
