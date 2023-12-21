32788f0e4b721ba5eef43ba68d421c280450425a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:25 +0000

    matter closely blow word hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
