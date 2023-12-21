a9d3c4bf483e7838db22b77d877e80dbcd87da2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:08 +0000

    backwards eager turnaway turnoff travel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
