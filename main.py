3dd83166dc7bfc87c36ae4fe0a9f247874e3d34f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:19:39 +0000

    building likely damp occasion could

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
