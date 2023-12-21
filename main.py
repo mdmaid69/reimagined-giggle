f9894086b3f929425b80306e0cb10be416faf46b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:04 +0000

    embarrassed explain gather reach wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
