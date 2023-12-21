5d6199cf17a65a4642737ad5a8032528c70db655
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:48:52 +0000

    plenty talkinto story claim outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
