676ef4c0b790170031fc0af08c434e07a8a7b450
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:19 +0000

    emphasize sweep solve proof pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
