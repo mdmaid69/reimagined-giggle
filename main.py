9f193cd3df8e3560afafee2f602a6be5cdeee26a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:14:55 +0000

    colour turninto bag whatever neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
