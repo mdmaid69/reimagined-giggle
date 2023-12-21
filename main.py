70dd5a6a61be981c39a0c34ca450caf4c0708618
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:00 +0000

    centre original legal worse lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
