e9c57b0002dacc21ef2429fdccde54b3d2e89d85
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:32 +0000

    transport safety consult measure law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
