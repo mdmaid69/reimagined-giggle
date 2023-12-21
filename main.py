45730c2f9e4137f35951331321b037975828645f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:37 +0000

    ability grateful serve search nice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
