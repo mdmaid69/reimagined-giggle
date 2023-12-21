d2071678a1430da56340df6068db00467ef515a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:56 +0000

    bush shadow fly funny last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
