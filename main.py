6ea2d1d47eec4c68448e4d44201e57733321e1f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:17 +0000

    rid close meeting crazy change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
