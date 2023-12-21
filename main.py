d53dd85b814a9818beb20457c39c70bca77351a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:00 +0000

    born wheel bet disagree settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
