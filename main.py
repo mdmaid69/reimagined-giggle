f67a9b10643f943ade9c2ccfe3e413d10dc94107
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:46 +0000

    ours affect tidy anything remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
