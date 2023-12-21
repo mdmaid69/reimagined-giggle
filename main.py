c193715fc4ec6d837b5a245224e0d2069c2746cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:33 +0000

    recovery right recent man project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
