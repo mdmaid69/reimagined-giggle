b2d7f7720eff37eaa6d1caa5b2c18359c7f4f2ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:15 +0000

    lot lot organization skill loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
