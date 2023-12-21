f49a798b84626912ce03934a4ddb0502b957d86d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:39 +0000

    moon gas clean free century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
