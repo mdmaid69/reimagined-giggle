c705f8dca3885cc9f3d66dcfbe6773846c446831
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:33 +0000

    question later confused level pronounce

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_power(work, time):
+        return work / time
+def count_words(sentence):
+        return len(sentence.split())
