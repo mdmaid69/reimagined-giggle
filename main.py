b96c2fd4dad7ba8e29f2a6fc751b46581722944b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:39 +0000

    flow earth Ss whoever connected

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
