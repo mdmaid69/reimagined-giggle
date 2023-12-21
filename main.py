e2031515108e45598cf5706ae311a701b36721dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:29 +0000

    hit name load store last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
