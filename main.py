b15bded972baceb32d5b323d588fa240eac911dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:16 +0000

    within final silent early dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
