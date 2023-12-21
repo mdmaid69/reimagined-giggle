adb531ce036c47567fb6fb3d58a15bc0fa81035a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:08 +0000

    fill impatient spill return appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
