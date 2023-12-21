d44df850d0512971ac55ef38cbb8802e904eba3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:36 +0000

    substance way exploration expect several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
