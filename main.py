dbe0538e1e91005b9e3f68a336bc04f8656ca45e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:32:26 +0000

    upwards custom 565end somebody lend

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
