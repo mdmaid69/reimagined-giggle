d9cca2a12301ac85d5fbb82ea5c30cc39be1ba8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:00:21 +0000

    desperate deaf artificial here mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
