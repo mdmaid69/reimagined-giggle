5b689eb826ce6de668f4af50dd0838a97d1b80f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:37 +0000

    consist bound spill successful relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
