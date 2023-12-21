3fd5def3c0570c70f44a6433f328ae7b4b1f4100
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:47 +0000

    beneath point use sometimes responsible

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print([x**2 for x in range(10)])
