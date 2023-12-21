3c5144dbe23ca98ff0d763a749ca0460b2b4950a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:57 +0000

    uponon software innocent town careless

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def reverse_list(lst):
+        return lst[::-1]
