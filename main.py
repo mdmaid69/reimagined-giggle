fe3c81c223c949473d946ada13f233710b8382cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:49 +0000

    keen way card tape surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
