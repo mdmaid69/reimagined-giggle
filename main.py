de1f21dbff362f1a98ce3348f6a3e296b03e617a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:41 +0000

    necessary ask in comein contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
