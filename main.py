c77065bf66701fda0deea07c852f7703ceba302b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:04 +0000

    capital estimate trip account odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def reverse_list(lst):
+        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
