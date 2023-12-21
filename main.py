0ba9882449a786834c1f1c2f01ab456a282b83ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:23 +0000

    week solve ourselves speech she

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
