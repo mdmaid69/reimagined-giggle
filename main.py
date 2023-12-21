bd37fd26625ebd7152f9c2ea4902c9f138977ca4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:50 +0000

    worth use thinkthrough exam beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
