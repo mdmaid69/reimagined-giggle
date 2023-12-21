aab657f64aab53dd7ac5b083e025e119d5aa8905
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:33 +0000

    satisfactory attempt space container dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
