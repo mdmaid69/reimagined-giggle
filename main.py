d72edb8458dd77447a3a5c1d41a9e9e9bb92e118
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:16:02 +0000

    somebody sticky body murder no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
