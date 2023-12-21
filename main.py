3bf4e288fd8fc1e5c22609b62abe9a62863272e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:26 +0000

    unlikely partner still exaggerate cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import collections
+def create_user_string():
+        return collections.UserString()
