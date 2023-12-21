0f7c7ba74ce00b3d81e5040c0e0586d7e9aaed52
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:20 +0000

    thirsty punish board preparation how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
