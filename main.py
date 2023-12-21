3ae91b99462b349ab2d1f3f1b1dd06c76cf44f77
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:13 +0000

    never perform work dress broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
