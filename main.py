1c78e61e0dc169f821d117cba794df91c3c52a4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:08 +0000

    emergency sometimes crisis appearance brother

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
