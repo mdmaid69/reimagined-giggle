7dc7f7fad712582092143b2413814ceedf6486ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:15:49 +0000

    waste rich kind team satisfactory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
