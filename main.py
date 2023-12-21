0a781792723885b785c59d0f914b29022466cbe3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:19 +0000

    remain Ss must charge custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
