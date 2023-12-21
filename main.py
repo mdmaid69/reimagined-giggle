5e9fb6bc0b04878a6ebe7a3ab0a7cd7940f8e91e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:37 +0000

    strike entry brief project principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def count_words(sentence):
+        return len(sentence.split())
