ece1e4a696e8bbb76de7ba7475ac057b95edb7f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:42 +0000

    science issue set male exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import collections
-def create_queue():
-        return collections.deque()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
