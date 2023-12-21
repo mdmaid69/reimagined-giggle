ff7f1858cc8c6ba531ff4e8e15391d65da701172
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:22 +0000

    underneath believe violence worried underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def convert_to_octal(n):
-        return oct(n)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
