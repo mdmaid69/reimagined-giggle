019334956d9d022bdccc65170a2643c6c8b23bb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:57 +0000

    increase force popular soft use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
