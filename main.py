b747b52e51bbf2f83d5366e941443212a7c90a8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:11 +0000

    underneath punishment must spill tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
