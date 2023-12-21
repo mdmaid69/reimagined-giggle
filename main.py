b5183848886e9d8df733fc6c6040f172ec8cb368
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:41 +0000

    attractive basic exam clothes hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
