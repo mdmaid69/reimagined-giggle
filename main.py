a393f0ed9c04403709faefebab5ab4f194cb0e2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:24 +0000

    advise handle offence runover forever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def subtract_numbers(x, y):
+        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
