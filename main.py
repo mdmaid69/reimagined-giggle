99df92aca491edf427c8fce8b28f7babe1d7f450
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:33:36 +0000

    pocket arrive fortunately find swallow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
