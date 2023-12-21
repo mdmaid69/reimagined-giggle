af74a2d4470a230d2d2637a0d78fb2669f514485
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:07 +0000

    intend bother large cross gate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
