52b23bd756e10470e5e41305e0650a34a7229a11
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:21 +0000

    clothes left offend waste eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
