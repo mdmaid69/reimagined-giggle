9bc1302b97c981d89df7aef35c9f65941f956554
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:44 +0000

    open reach expect tell yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def log_message(message):
+        logging.info(message)
