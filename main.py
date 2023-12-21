bd88e418b86ae513012ecc1c633a1826e3fc8fbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:21:50 +0000

    off sharp bringup swing tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+print(sys.version)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
