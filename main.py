157e611a3f9b46532b636ed219e9541746164e74
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:21 +0000

    agreement embarrassing care muscle Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
+import re
+def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
