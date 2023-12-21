031cf3be820e72c19cb307777d885cee23c0ac0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:41 +0000

    innocent trick telephone fasten success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
