be7c2e6791c3a333cb2da275373f5afb5f77065f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:41:10 +0000

    purpose introduce cause breakinto chief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
