a77c08434682d4794b3782d599a452fb85d1768d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:37:06 +0000

    cry shopping collect takeaway regular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
