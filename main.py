a9bb5d346e1aead69595c161dabf8b02374ae93b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:36:45 +0000

    model thinkover badly shadow deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
