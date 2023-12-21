165c97bc581c9f14f79cde15913cabfd1be09b21
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:22 +0000

    urgent upset central sensible tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
