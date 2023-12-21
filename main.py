b982bf7bda885e926632c6ee1a29ee451cae21c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:56 +0000

    come disgusting eye comeup prize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
