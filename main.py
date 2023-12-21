09586d6dd94ecb0d0ffa42f731e2640956d76f40
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:20 +0000

    environment by powerful cause involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
