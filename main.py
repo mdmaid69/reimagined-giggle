44dec64d21d82de7184f1a07de72218278b60e09
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:59 +0000

    difficulty noisy only clue identify

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_min(numbers):
+        return min(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
