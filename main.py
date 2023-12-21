63dc234468c8a35290fbac363818c3ddaf2967a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:52:12 +0000

    organize when grade strong fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
