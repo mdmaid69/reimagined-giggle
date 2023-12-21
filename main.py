c029ee1972c5faf38b2838e59d3943f3a0ea1c51
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:26 +0000

    bar rent high contain except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
