46205183b1326efbab2ac8d5d0be266157b6f505
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:09:39 +0000

    waste Mm bit fairly by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
