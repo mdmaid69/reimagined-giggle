eb89193a8f896042a22147d4f1c476ad0ec82d56
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:29:27 +0000

    mess computer 571enough excuse variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
