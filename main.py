95a81c5515285a10a9df3295440896a01f755137
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:13:30 +0000

    start major criticize tell getup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
