3ac2fe69c171ba3157a08563e9c92f435e94be61
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:53 +0000

    guard dark software suggestion sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
