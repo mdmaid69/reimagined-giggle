cf9142074e4294fd715ec8b073e97488e77481a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:13 +0000

    section charge slip original shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+text = "Hello, world!"
+print("Uppercase:", text.upper())
