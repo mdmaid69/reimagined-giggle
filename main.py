6b86c2a35d707269d9baf0650dc5ce408dfdae5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:36 +0000

    address disagree intend supply odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
