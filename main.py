2e87061ae35c96eb897a03c26f99592080f1dfe8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:59 +0000

    must perfect unkind start though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_max(numbers):
-        return max(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
