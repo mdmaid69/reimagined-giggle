caa6bb9317366ec342668b92f675094a77c98a28
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:27 +0000

    best to trip force smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_priority_queue():
+        return collections.deque()
