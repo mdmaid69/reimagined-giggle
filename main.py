d4b2bebc43a6247938a36f89fffb7023e0f6254b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:53 +0000

    famous memory attach damage claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
