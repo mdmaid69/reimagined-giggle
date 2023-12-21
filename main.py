488a852fe8a520d8a1b8ead9e1cb336f9ece97de
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:21 +0000

    fear say under rest feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
