905a2fa1266b066d6d094942c93ea9cf69e5a132
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:20 +0000

    need border set acceptable even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
