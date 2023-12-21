e8803edc40e0bb6defce9cf253158515a263c8ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:27 +0000

    wrong unhappy change afford classroom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
