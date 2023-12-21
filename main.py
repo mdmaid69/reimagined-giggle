d0d733cb58645353fae294e5e03fd3d9a42650ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:29 +0000

    could now demonstrate mine treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
