f43c54cd29e1593e811ebe8c80985b6ee7ee9855
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:16:33 +0000

    curious job basis money tidy

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
