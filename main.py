e2d762c8571a2a53b12923274856db420c321a91
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:36 +0000

    leg uncomfortable thank arrangement me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
