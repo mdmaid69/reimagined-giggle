255d2b93e4d8f1ff64bfaaaced0367c3df0714f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:16 +0000

    water method 571enough good fun

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
