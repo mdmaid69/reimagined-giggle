dac586c09293771de3b091c6638d37f27c745786
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:49 +0000

    let word plenty charge control

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+print(os.getcwd())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
