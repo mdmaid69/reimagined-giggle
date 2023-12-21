c9b2b114aba753ce8c0c0f7bbb2339d4d1ef9e00
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:51 +0000

    her pool power treat shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
