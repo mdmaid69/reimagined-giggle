499a8087c9188730293a5d37c027d6dda9f8ae42
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:20 +0000

    everything underneath marry coffee leave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
