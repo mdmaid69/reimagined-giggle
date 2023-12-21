8c4d14227c338d5eff6e9ed2fdca795ba3e30596
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:51 +0000

    teach worst busy average theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
