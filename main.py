5984ffa24bbe561f970d4803cf0f777e5db5ff4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:21 +0000

    in move shall blame patience

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
