74df61b1dd6b0d0fb2999efbe892ef50b94167fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:34 +0000

    lean react event feeling sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_error_function(x):
+        return math.erf(x)
