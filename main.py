7a1538a8e3cfb891d4ecf97166471627b397f8fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:40 +0000

    goup before decide threaten feeling

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+def divide_numbers(x, y):
+        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
