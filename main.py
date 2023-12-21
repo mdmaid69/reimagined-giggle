c4dd1df51582006988d77092aaf45c068443b35e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:10 +0000

    positive far union autumn bound

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
