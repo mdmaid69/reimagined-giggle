e614bef37979c35a1aec1160702d50d0e03fe412
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:58 +0000

    payment ride post stone actual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print(sum(range(10)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
