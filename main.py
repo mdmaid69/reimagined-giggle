bdb63f0ed0259071f6efaa88cbd1e750b9bdf138
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:49 +0000

    happen accordingto instant display radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
