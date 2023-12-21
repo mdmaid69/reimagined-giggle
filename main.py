821cade2994b8091d89aeb0d667680c2468ab184
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:23 +0000

    instrument realize split join black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def find_max(numbers):
-        return max(numbers)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
