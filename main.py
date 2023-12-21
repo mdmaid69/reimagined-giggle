a04a12c91071f42d5eccfc7d7ef9cdc03cc04761
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:09:09 +0000

    necessary Hh fail much by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
