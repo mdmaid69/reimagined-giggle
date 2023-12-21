9ba4f515d3f3480ac90ac8e1a2507868e5f67dfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:48 +0000

    tell fasten raise legal hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
