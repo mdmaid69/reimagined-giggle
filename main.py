d78f79c36771cd94440c88bcf886dc68f634c7b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:52 +0000

    region destroy design anybody bath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_force(mass, acceleration):
+        return mass * acceleration
