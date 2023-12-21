42cd4ba7cfc48900da4d65202eb5168ca8c5f4bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:18 +0000

    much weak minor estimate slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
