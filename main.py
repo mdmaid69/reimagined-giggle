17defc7f5c39224fad40754728dd41127da0b47a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:31 +0000

    first platform dark maybe study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
