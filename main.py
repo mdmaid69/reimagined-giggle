de8cb809cc0178b13b4bf9226a651759af7c2439
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:22 +0000

    task study simple warn name

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
