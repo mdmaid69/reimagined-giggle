3f60b5f26ee96a1a69b8d894c3a7b9b2103d7739
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:08 +0000

    well hot may distance compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
