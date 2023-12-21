2cfcbd80d00e58e407eda244a352d6810a9347a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:06:17 +0000

    want whistle wish standard criticize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
