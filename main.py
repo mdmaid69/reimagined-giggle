7e35812ab928aacaf5ba65a3996a12bf079ab2b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:14:32 +0000

    mysterious beat watch Yy strength

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
