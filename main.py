96aca88b9b8ad96102e5240621b9b2f11a6f2024
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:42 +0000

    female contents reach actually swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_length(array):
+        return len(array)
