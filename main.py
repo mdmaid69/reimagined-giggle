bb5c2db5674be0ce27dd46ef78623aaa072b83d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:56:10 +0000

    flow less difficult more occasion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
