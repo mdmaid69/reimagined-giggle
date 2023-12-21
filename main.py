861a30d9204e5448dae2384f1c7d64c4310924a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:54 +0000

    about switch land organize spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
