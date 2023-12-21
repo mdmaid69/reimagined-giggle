8a67e1cc6dc50fcef85ab3fa0e18dc0b4a4cec86
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:53 +0000

    callup lot phone Qq further

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
