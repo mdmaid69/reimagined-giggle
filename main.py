f297867bcf9f24c5f6787dda8d66585f5850c648
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:29 +0000

    brain usually recognize bend slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
