6116d27e7f6e8dd632ead18c15790f9c9778513d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:47:33 +0000

    minority hit certainly path dick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
