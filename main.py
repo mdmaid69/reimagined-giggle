407b51a8579d67a9ec80c9570ddefdc8639d287f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:30 +0000

    light talkback his start cope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
