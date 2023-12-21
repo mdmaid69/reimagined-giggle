6470af3de52dd8cd53a16e41098852878d28ffcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:47 +0000

    object flat fasten sky spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
