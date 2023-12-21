6af2871b1bff1ee9f2c8d64dd8fe030c6dc7771d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:49 +0000

    fairly bet priority wonder face

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
