a5fe8196b1a03be77246ddb0518bda26b6138f59
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:32 +0000

    so lost physical petrol disturbing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
