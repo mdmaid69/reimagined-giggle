361ce78714d55e33f2b5c5946cb076f8067e503f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:59 +0000

    light direct spread frame remove

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
