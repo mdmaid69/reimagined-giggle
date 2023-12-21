8d7cae36f848e3cfab070335d3c3ca6575bedeac
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:55 +0000

    stop should formal already special

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
