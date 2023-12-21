cc8409ae048f79cb4969c7eb276f0851e1567df7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:00 +0000

    trousers secret mother least wonder

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
