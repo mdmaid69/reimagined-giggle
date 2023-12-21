d15ac420620f222b0df7c4b44c9811068af34952
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:08:30 +0000

    material science skill scare shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def reverse_string(s):
+        return s[::-1]
