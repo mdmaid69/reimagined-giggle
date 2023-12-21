55ee64b7cd5e2440ac9199c2e70e44fcbd0d2ceb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:34 +0000

    570enjoyable government scream store stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
