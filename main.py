e0be10c7ea848da2924c88bd4773d0c3c9e5c36e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:52:19 +0000

    make mess coat unfair slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
   def get_absolute_path(file_name):
         return os.path.abspath(file_name)
