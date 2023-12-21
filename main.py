58ea390a77512391a322fc3ea2a65afd67a6d762
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:17 +0000

    persuade already moral time evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
