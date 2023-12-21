3712fbebc02b22147687ec12c42148b8fb0585d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:14:53 +0000

    storm case wrong brother will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
