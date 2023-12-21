2bf17a930d686a24ed1b297a4e8946b74f5c9486
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:37 +0000

    flood listen disturb another sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
