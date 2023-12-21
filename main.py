f9d6c30a93f8138982a8c79cda6263acf5fb9334
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:11 +0000

    already reply immediate reason perform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def square_number(x):
-        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
