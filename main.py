39aa082f6cc8b8aed30d404a17f336389b8fbece
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:52 +0000

    fierce Ii perhaps near disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
