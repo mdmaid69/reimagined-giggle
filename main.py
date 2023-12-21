511551f167c8ac1e3157d12a9bdf37501725ffae
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:31 +0000

    steal collect ought guide start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
