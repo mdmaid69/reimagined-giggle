25ddc18d889efab3ad891eed4ce02cda54ffa27d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:31 +0000

    holdout present system finish react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
