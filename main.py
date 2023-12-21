eb955c07aeefa8d10cefee76d4f6352e630c7f39
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:48 +0000

    convenient dear cause scare report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
