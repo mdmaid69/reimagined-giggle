49f21af3bf93551ac10512e4c69638aab5d88b51
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:59 +0000

    loose unable more ready switch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+name = "Python"
+print("Hello,", name)
