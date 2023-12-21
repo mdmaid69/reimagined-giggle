3b4a731428d68eebb8167a5b70e860109b40c726
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:30 +0000

    skill spin famous stop threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
