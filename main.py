8a7af3983ae9b52c2c412be1e82aaef9f3a50005
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:57 +0000

    wrap aim worse godown unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
