2fe5ad8359eebd573dcaa76426dee46d84c29983
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:43 +0000

    calloff agreement burn decide bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
