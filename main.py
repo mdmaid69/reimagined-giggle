4a848c17d15d70f92b22a66884839797f68af06e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:37 +0000

    move cool promise frame rule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
