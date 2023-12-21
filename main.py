16321fbe6a9ebdd9c7ee69a27ecbda0a15744b52
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:40 +0000

    embarrassed dick stamp think average

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
