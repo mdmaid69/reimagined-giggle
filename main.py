cf9fd761d10231155209e9bdae46ac06d29332f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:50 +0000

    maybe further bag switch patience

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_list_from_array(array):
+        return array.tolist()
