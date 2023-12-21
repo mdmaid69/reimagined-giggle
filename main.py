63970de2ea39a0a9236b07b4a3d50711e1577212
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:47:29 +0000

    forgive year face report comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
