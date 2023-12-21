8898f0705cfdd329d6464f982febedc4000a80eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:32 +0000

    that thorough arrive sharp minimum

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import sys
+  def get_python_version():
+        return sys.version
