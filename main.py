459ac3a9367732b26f462687c037664d3aa05b14
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:30 +0000

    workon pile cover wheel turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
