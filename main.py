7e7b26cbe535635c606dce9a9a2d039649bc28fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:01 +0000

    regular I Vv quiet Itself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-for i in range(5):
-        print(i)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
