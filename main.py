29956986a63dc8f009eba49cef79939e6908235a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:03 +0000

    tough ride criticize destroy die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
