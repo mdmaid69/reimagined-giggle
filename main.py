1eec243b85eef78c549b20cf5f5f4e07d7cd0bd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:57 +0000

    increase rubbish sign giveup connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
