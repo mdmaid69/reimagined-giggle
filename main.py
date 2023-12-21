6f5d42bb995c168a6519563d66882b221afddc22
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:45 +0000

    fault few limit improve amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def generate_random_choice(choices):
+        return random.choice(choices)
