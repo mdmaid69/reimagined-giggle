c44faa44f4a9e5d22d6304f91da428e7da33ea6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:48 +0000

    experiment live comedown related praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
