b4b40a2ca238f925a5bda19af372c5b33e5fdffb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:08 +0000

    around separate tend mend balance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
