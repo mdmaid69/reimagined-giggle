4fd92f0b689baf55f6f5ca8b4e495878a1e07bcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:29:07 +0000

    ball ear name chin gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
