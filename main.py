dfd7d7f781dfbdc8f9ade2412391db1af015794c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:55 +0000

    basic service himself comedown corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
