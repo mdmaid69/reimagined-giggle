e598158e905dd142c4a1363dfe21ff756e4dc5fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:31 +0000

    sale state truth thirsty gap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
