87579fe3de3ce9eaa5025edb775c134057c4a79f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:24 +0000

    address mine powerful ring yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
