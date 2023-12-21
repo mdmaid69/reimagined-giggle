a1c5c28317c6229be005688f3a44fffa736b7045
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:40:55 +0000

    neck exciting cheque surface badly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
