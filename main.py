410a054d300e3434f3e9de4abca4cae4bbbca4f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:59 +0000

    return excuse tall last immediately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+print(time.time())
