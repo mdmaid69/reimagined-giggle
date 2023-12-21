1e157391084ffd7e732ecb36de11c1cf60e09bd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:53 +0000

    goodbye fair opinion money friend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
