f5e347ec31a46126d556032b9f0c985fe41c3a4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:06 +0000

    comeout definite divide lookback(on) prevent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def multiply_numbers(x, y):
+        return x * y
