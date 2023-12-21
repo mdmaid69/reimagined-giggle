8c442e59a20ababc388a5a6b13711763ca0b3f26
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:18 +0000

    regional ask takeback shop turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
