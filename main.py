5a677cabf84593cfd9e08dde877ff0bf5854ed29
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:04 +0000

    deliberate war teach bother board

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def add_numbers(x, y):
+        return x + y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
