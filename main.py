dddefcb034f4da92669fc7dce601d972ac8cec79
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:55 +0000

    Nn stand sing round upstairs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
