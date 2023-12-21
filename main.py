32ca0490a1e25d84673b383277b468c4c9a94526
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:49:01 +0000

    snow boil minimum uncertain mouth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
