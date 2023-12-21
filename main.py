55a4a1ed6e33dee5a2ec339c9b5aede2130811ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:24 +0000

    order mind content emphasis card

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
