7f96920d310b387e02d03ad68fb671fae837cf7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:51:31 +0000

    always sensible shut make find

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
