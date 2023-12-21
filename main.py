be13c09233e99a3289c31cf1a763912d143db74a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:20 +0000

    order study reason communicate story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import sys
+  def get_python_version():
+        return sys.version
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
