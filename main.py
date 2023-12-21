36f8953f96f0130166ec76dff8ad71c06858285e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:53 +0000

    estimate couple lead hospital trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
