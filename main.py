5d3febfff3a154b0f9684294a0bfed58822e1520
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:44 +0000

    hardly vote shopping sea bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_bool(array):
+        return bool(array)
