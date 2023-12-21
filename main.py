5094edd1c184208f45bc6c067d3d61ba5ddb6054
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:30 +0000

    move bush freedom prize refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
