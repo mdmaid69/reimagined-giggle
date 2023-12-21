9fdc60b90d4a760f5828a1378cc5adad673f71a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:48:55 +0000

    plant education hurt breathe report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
