668fb22c44d8f4ee360bd4cbf770eb0f75aa2bae
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:55:03 +0000

    few study guilty because mark

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
