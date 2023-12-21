22a32b5c03f7d503f2896310c78b35e079423efd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:34 +0000

    slow hope violent benefit meanwhile

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-print(sum(range(10)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
