6989070c5c899db9ec15673831cb3bf3645b7b2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:47:32 +0000

    destroy frame onto male callout

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sys
+print(sys.version)
