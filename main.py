4cbf1937c59500bdd1463eff779dd8339edaea57
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:04 +0000

    there better major need attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
