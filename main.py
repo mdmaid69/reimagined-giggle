1ef9bd609ec9ff8cebc6fbc84c2184df36a7a804
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:36 +0000

    lost bother offer healthy drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
