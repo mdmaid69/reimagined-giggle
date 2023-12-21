403353a5a287912f85ee08097e5fd67c2ed89f83
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:36 +0000

    jump national light relief undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
