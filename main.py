5ec0e06ccc6a22ce9eb91c878c72d2eba0345b49
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:06 +0000

    write neither judge cheap that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
