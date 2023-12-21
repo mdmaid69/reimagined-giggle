1ace1f6f85465000f71a53313435c88b8be10142
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:57 +0000

    sorry continue worried mystery herself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
