ecd79c5dbe0d7a98ada12cf8e7aafa740fdbc424
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:09 +0000

    fight successful remove expect release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import re
+print(re.match("h.*o", "hello world"))
