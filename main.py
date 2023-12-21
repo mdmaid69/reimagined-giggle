5d23dbcad0dc6723344452cfe5817340b5d406ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:54:21 +0000

    answer if together board doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_priority_queue():
+        return collections.deque()
