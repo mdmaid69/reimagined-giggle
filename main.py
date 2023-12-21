e1a92e9c4ccfe943daae01d6edf6a74c25369359
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:28 +0000

    suggestion no cause hardly switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
