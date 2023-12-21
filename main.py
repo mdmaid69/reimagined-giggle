e677aaee356fed2c32868b8728f5b7f1db95f641
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:57 +0000

    cope between sense union ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
