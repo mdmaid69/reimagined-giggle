343bb1b44b57099c43519c767dc21dc49998ec43
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:25 +0000

    lookback(on) luck need finish gap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def square_number(x):
+        return x**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
