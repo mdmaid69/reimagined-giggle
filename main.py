0138596fb41497a0089356495d812c2b6209a228
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:51 +0000

    repair Ii contact machine watch

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
