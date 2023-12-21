cbc2d5112c642c6cdce14608d797f5f10dfee5e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:52:35 +0000

    runover again spread convincing thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
