ee4c2beebab27ea6baa85fcbcfd7700a7252bf69
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:24 +0000

    heavy bite shrink smooth date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
