4588ab11f9946e4fc5b7426b58f1d6cf79ada916
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:55 +0000

    thinkthrough back reach back character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
