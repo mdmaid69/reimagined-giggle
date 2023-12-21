57605c3f77c01e1d8fff3a1fb264a9e1b88b2601
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:52 +0000

    angry blow pair variety able

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
