1a733fcd5990e65911c81bbe4f3dd46f99c22486
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:48 +0000

    bringback Bb lay definite unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
