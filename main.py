27b75ab86c4d307513380a2841556a1b5bcb6bb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:55:25 +0000

    workon refuse list object wash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_speed(distance, time):
+        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
