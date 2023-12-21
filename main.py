4add490468107d6d5fe7b65ccff5e56626a3647a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:05 +0000

    turnoff hard may price bye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
