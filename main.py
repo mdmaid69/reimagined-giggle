5445eee7ec71c1162f5ba18f98fbdcd8def7e04c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:32 +0000

    thankyou tongue then second certainly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_palindrome(s):
+        return s == s[::-1]
