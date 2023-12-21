bd3375bdce58e245353edd75371eba1de687516d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:38 +0000

    reliable comeup reason hold education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
