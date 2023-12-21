7b5d55004fbc883dec90e1a907fe1a4d440da0ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:01:57 +0000

    target doubt hair dance combination

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
