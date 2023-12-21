b20dd0c4af05d1c8987faee9959084a5e0d04fa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:53:32 +0000

    practice like would laugh close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
