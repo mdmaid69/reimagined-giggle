fe7e65239a5e24ef9f25f71276b9b8ec0012f3c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:02 +0000

    party close scrape distant priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
