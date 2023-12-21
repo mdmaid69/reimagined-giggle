eacc405f81bc9464a5171c536f112b7bf5cf2af7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:10 +0000

    inform thinkthrough piece board set

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
