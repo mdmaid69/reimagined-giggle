573ce0a417befdb39b7d52f120eb50390d47020c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:06 +0000

    used phone talk cheek smart

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
