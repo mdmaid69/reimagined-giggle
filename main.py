3e873d70521bc37aebb96a60adcac76eb5289ddb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:33 +0000

    giveaway calm moon workon certain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
