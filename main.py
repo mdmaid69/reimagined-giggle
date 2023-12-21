7efbf7517a318bad6f6f498983e9f3bf53a32a76
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:46 +0000

    far please downstairs talk disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
