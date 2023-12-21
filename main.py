4e99be5bc3ba0065de8eccbcda42d08420157abe
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:11:31 +0000

    load slowly crash depend scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
