3c5e3685d0be2cff7a60d6a53b2b9e124b9f979d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:19 +0000

    talkoutof excuse complete same look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
