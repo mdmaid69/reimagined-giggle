1a1f9fb6ed94fbd5f0e6f1112d378eb573d8a628
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:55 +0000

    workupto nonsense round this traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
