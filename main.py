d971c3411abcc33a41825450a3f9703b10f61dd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:28 +0000

    slice spring much drink great

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
