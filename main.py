5b9ab7869ec9cb70e7c3fc16acaa58a36cf9f355
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:22 +0000

    cut proud lower turnout dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import random
-print(random.randint(0, 100))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_list(array):
+        return list(array)
