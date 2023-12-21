af2f3c92c5a600467308449f57abe6a4ce01eb26
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:03 +0000

    completely Hh dear only skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
