799e84dc85002925c858c132b86a59a14da88c12
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:07 +0000

    suggestion goon message unless pool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
