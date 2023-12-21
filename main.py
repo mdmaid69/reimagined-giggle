ee982ae73b064ad2c1e7040459f700a7342b50e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:50 +0000

    breakthrough limit unhappy tight permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
