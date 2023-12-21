f0160bf6d42678d8c45098029a7857758bcb3d47
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:26 +0000

    address favour rid again freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def reverse_array(array):
+        array.reverse()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
