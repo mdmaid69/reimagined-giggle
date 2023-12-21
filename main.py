5a426f1f954dfd8ca07b105999d4e6c86db9d572
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:48 +0000

    thing share tall interest deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
