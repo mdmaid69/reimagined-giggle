75d9e756f92f4c29107daabc901351c353a64ccb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:27 +0000

    generally opposite such shopping discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
