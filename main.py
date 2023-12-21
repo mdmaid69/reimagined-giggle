18c60013180f29d633b00adcb549af0d9e3b1fa0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:55 +0000

    table they accidental regular contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
