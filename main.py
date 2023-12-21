216a92923535ff1d5d5a8372ed642fb966a89fc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:11 +0000

    open tap when shock care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
