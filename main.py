ebb9456262cdec4768309708c006a73d71fdcdc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:45 +0000

    often could pattern left callback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
