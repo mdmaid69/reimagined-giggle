8cd7899d93338bbc3bb980305d1b40265e623f03
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:41:39 +0000

    rush work refer may fall

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+text = "Hello, world!"
+print("Words:", len(text.split()))
