a88dd580a13802de10043fa0757f70f1f2a77710
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:44 +0000

    open fine couple start will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
