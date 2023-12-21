e533469ab62bb744be6946fb245ed58ec2f606d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:43 +0000

    target land shoulder our offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
