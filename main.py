24450cb35b9e3df1a752f1a1d99dbf22c0aea16a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:32 +0000

    fast lean relative by stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
