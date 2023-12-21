f209f44caaa7bbc73aa25752025e0cc209bfb5ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:09:21 +0000

    close prepare plenty Bb point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
