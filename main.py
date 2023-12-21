2d4a3e857fb314d4ee4d3ade902bb807391395ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:12 +0000

    suckme regularly trouble content concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
