8f6097b29d993f7c08606f1b14038cb78a2c8940
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:37 +0000

    finger sick brown prepare divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def convert_to_binary(n):
-        return bin(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
