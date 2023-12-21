78e43431e2913132fac426eaf3a81c4969be7df0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:49 +0000

    shop before learn closely interruption

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
