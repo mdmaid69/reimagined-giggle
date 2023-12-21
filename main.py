d2f899579d7377bf02f9539c41860d48955f824f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:13 +0000

    low none try thinkover maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
