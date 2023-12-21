97fdbd39161f75848007c0b1f76b83375c7cb9c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:12 +0000

    worst touch wheel fold weigh

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
