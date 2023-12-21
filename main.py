9983f620a46d1160f2610333d45894f895fabfb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:06 +0000

    majority fold power objection ahead

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-for i in range(5):
-        print(i)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
