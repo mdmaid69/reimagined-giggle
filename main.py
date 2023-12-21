9f9411d085833ba2a3b268ff8be92b3a3067b840
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:06 +0000

    past behaviour finger get wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
