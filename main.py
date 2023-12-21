4abf06767cb08edc4462b91c2b82cde4df0589df
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:51 +0000

    bread throughout cheque far doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
