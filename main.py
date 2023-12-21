1fe6514654d78a0c1a4387f3b26d2e52d2c86f07
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:41:09 +0000

    sing rate dull walk only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
