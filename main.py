2eb59c816985ed152585966eee736ecc8e9486e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:19 +0000

    keepfrom century amount conversation collection

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+print("Hello, world!")
