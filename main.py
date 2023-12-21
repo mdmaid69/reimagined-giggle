d73ef4bb2c6b3712891705c052dde70ff3a49d8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:01 +0000

    shout frequent thinkthrough science common

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+print([x**2 for x in range(10)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
