a8d82b287d9f60b91d44586decaa823c71c0e688
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:32 +0000

    emphasize bringup gift along disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
