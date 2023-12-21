005bd48be02885a9acc1a264a8b24d0f72f92da4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:53 +0000

    car actual transport rough fashion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
