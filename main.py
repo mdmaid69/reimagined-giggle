ed229c72380a0556b4963b705673f4c6e9024f01
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:50 +0000

    terrible error deliberately sound habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
