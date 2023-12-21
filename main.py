5e22cb113fcba2648598c8940833858229e9455e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:32 +0000

    square neither pinch this possibility

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
