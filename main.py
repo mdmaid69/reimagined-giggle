45f1bc29aebf69f40beb22ab1fbd54a04a098ffa
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:25 +0000

    sound ready uncertain school convince

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
