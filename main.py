3a40ad38aaf2b4b0c948f5c169b529ba15ab120f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:11 +0000

    guess set goup general keepoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
