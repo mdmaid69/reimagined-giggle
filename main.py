2325f21169520e0b619a9558601e9b481ac5e843
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:23 +0000

    pay spring smoke thin task

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
