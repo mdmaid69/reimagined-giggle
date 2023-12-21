8e94ba83583c63b4b02e0c284f4edddf59765a39
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:34 +0000

    prove interesting party holdout exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
