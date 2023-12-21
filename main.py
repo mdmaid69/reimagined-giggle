39c9d6e89073d7af99ea65f46d417190ead31a25
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:51 +0000

    scene consider supply understand near

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
-        return x**3
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
