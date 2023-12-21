1a1c44954a26be04ee1dd14ef0b682e0d104d762
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:27 +0000

    pupil preparation busy train care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
