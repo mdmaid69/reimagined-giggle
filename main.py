99dceb24da7940adff1faf958058ae459e84dae1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:57 +0000

    comein protection public innocent delight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
