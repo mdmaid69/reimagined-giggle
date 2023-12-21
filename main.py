1e00daad9f36dfbca82030e76634e22e09967fad
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:40 +0000

    getoff action high fetch private

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print("Hello, world!")
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
