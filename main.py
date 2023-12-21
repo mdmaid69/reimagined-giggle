c947d59a0bfc6b23a118630b4f7114b0d90c3594
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:48 +0000

    dead transparent winter command match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
