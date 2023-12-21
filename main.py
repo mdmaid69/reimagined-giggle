3d5e3bc067677dca0446030ba16cb0088ed1e5f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:21 +0000

    I finally hate name both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
