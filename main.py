1733711d98e628302caa8427dafbcc8c193550ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:00 +0000

    vote less command over show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
