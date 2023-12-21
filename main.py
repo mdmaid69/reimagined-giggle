0d2774e1df1dfef223ca400a5bd214bb38d4528d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:13:19 +0000

    listenfor thankyou deep confused back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
