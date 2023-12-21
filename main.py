c2fdf01a453735486789366760ec217f953a2676
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:16:19 +0000

    rest thinkover drink pollution spare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
