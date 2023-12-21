abebb20873431a60b617030aedf76336d685415c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:24:20 +0000

    active over pattern dream regularly

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
