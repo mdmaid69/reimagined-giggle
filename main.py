89141298fddf2b3255a97fff5fde5a69eccd8621
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:52 +0000

    seat rough goaway fold famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
