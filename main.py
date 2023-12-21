87eed2bad3a94837bf3d6ce7a8ef5542d0cbf84a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:46 +0000

    hate sorry no address prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
 import math
 def calculate_bessel_function_of_first_kind(n, x):
         return math.jn(n, x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
