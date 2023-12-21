c29de482a50f6f6385b98019cdf690f2492eca7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:33 +0000

    smell whatever nor welcome can

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
