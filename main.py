e789110eefdba911285ab400c133532d1c1928cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:28 +0000

    minute comfortable bend nervous near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
