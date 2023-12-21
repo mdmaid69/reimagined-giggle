569a1443316087e8ace43e32258b36ce286f4fb3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:16 +0000

    smell unfair beyond fear demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+name = "Python"
+print("Hello,", name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
