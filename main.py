65c51ed31caaa52bd731ba32dfff75a7261cab08
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:13:14 +0000

    lie turnback impressive connect detail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
