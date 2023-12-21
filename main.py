43db70224955fa11528e82839f145690f1c9ca83
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:48:14 +0000

    bend ourselves yesterday squash comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_base_name(path):
+        return os.path.basename(path)
