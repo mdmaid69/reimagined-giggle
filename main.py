0eb3ba891935d24dd4617f602cbe66e4b6a69a99
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:49 +0000

    powerful she respect wherever condition

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
