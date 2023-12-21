f821654d3e49b3ef89652cf9d4e9e45bb7f76fad
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:38:28 +0000

    concentrate keepaway report supply properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
