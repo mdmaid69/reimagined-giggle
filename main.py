23e355209579253d6747e2fbf9ae5c2627f3cfe9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:47 +0000

    glad attitude address wish unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
