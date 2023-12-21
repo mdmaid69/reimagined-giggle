ba99dfd99a1f586e33c54bf079b42ff9f4a60adc
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:43 +0000

    character crash perhaps scream least

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
