e108ec62901fd377d18ac781f12265c1d2c9efa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:07 +0000

    attention later might clear like

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import time
+print(time.time())
+  def add_numbers(x, y):
+        return x + y
