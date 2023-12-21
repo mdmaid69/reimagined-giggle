72ab5f7c27840d0265a35f0abec627dc461f49e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:41 +0000

    learn any meal release regret

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def is_even(n):
-        return n % 2 == 0
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
