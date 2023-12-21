8ca6469a4ca32c2a447a1d80dd2492b002f05b57
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:32 +0000

    naturally trust entrance to exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
