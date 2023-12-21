2d14040afbc2593cc8124ede094f3a8bf9810b9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:55:18 +0000

    grand far satisfactory cross reduce

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
