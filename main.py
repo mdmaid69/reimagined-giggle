e0135cb16856227a5daf6c5a287abed6c79ef975
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:00:26 +0000

    period rob fit trade trip

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
