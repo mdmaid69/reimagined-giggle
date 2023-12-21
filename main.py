ae5496a9c69ba6aa2504f81ac9b23163c6a017f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:38:11 +0000

    plastic snow storm eager our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
