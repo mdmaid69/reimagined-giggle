31af69e41ac42d50529011bf95df23141ed1a806
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:29 +0000

    off father substance score doctor

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
