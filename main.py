6f10d4117006e72cb85f2bbf5845e6f20060bf6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:58 +0000

    advise soft explanation movement damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
