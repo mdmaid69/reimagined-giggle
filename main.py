9a68c7f1a5ad77c3e49c0ef6ca7e43da098ca6e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:59 +0000

    tour arrive travel birthday prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
