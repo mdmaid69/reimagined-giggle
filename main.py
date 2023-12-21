82dff23d75012d419bbf2562ca6a65181ff8a9a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:41 +0000

    unable clear gradually get himself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_density(mass, volume):
+        return mass / volume
