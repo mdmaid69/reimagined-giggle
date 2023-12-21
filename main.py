377fce62911b1488c3cd2b1abba8b3e2bbd12f45
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:30 +0000

    act many little hospital dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
