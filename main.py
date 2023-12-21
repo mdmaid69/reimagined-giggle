fdb197a1a026a290b4079eaa9ba8feca209b1795
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:14 +0000

    season train aim seem loose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
