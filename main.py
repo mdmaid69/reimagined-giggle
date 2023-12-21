7c02f642dd17c6eec1f7d118fe8876a6987735cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:11 +0000

    lift division spin respect maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
