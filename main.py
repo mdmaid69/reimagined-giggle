07afb92091b88ff30ab28b0e1845b4e8163c4a54
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:45 +0000

    acceptable science frame piece maximum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
