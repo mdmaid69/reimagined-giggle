fc439d67fbe0b9e27ba1168ec08bce6ecfdc8e79
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:31 +0000

    emergency reserve quit concentrate same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def square_number(x):
-        return x**2
+def get_array_index(array, item):
+        return array.index(item)
