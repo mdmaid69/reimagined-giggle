a53cc6cd155df153f50ebbf4c6cc676f2b95dcf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:45 +0000

    yet choose after step project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def count_elements(lst):
-        return len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
