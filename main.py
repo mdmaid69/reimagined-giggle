2df84ed04fe9b02c4c5c0fc754d7f725af332c8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:23 +0000

    slice uniform lonely protect strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
