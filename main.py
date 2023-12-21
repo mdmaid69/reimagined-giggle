2ca67e5a07eeb04b06f9169616fa819b1695355b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:03 +0000

    for well history what shadow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
