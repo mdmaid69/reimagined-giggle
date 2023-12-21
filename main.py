cfe7376d6f7c63a0d2518d2bc603f1fe86abdac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:21 +0000

    finish fix finger genuine same

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+print("Hello, world!")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
