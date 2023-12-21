90551e2d7797193aba41f5a46f3c2317fd6c380d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:58 +0000

    move argument study remark distance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
