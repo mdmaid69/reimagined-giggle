5e69cf7c50c2495344d320bba07d1166b56acb5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:48:40 +0000

    anybody turnout hair war hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-for i in range(5):
-        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
