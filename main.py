bf085f17bf9f01258817e9e35802a965641ddf4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:21:33 +0000

    organize 572enter breathe heart night

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
