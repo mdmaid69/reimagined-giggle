f778afeb8f75f6b3636dd410ff45d5bf037b2f76
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:06 +0000

    influence crowded could apologizeapologise letter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
