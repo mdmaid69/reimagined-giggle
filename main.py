1c539a77280ffa7d389a0aa5b46318e2d8bd2ebc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:13 +0000

    someone arrive 565end anywayanyhow both

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
