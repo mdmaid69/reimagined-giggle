3f96eb68c00f15fff7da19052b373746c8411294
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:10 +0000

    bite from smoke fast plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
