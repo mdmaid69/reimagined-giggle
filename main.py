72e5704f5ce9d24c68035665fbfc5ddaa1e89e1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:00:45 +0000

    style condition disappear pretend tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
