70a6094338c023e938185488eb3a2f0f3e51851d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:17 +0000

    press thin confuse average strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
