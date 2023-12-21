a70f58025f57e1ef2abacb538fc0e7d67b3ab614
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:55:13 +0000

    slow ground field country coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
