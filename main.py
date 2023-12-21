ee839ca8c543e6e1a625fc8cdae7ec0b6ea7099c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:59:19 +0000

    wonder example slip offence getup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
