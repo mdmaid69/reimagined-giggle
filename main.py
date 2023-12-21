ead66d826d2a3f8504bcfa771543fa7f3143d687
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:30 +0000

    but lot tough that special

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
