1d541f845fe59a750e6eb48f367fd4013f6bb42a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:53:28 +0000

    base unite watch declaration hospital

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
