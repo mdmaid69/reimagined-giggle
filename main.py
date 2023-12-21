8ed00ee9a0024720c1c1f9ed43dccac15a7e7ae9
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:02 +0000

    into skill before bringback reject

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
