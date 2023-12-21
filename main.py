dc20cc380a36277a087ec9e8bc3f5cb0f3cf94a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:24 +0000

    fork makeinto constantly table ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(lst):
-        return max(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
