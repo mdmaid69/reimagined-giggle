9c2cf41dce6c55ed55bcb8f5e1f89981b9af0c9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:55 +0000

    pin put week scream suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
