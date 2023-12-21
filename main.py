8954d1c9579320cbc2543a9ae7325f72ac448ef3
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:33 +0000

    conclusion present watch remain kill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import platform
+def get_python_version():
+        return platform.python_version()
