1045f6dd60cab8f5dc10cfddaf9ba3d475a78213
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:02:15 +0000

    getback surprising pay butter satisfied

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import random
-print(random.randint(0, 100))
+def find_unique_words(sentence):
+        return set(sentence.split())
+def count_characters(sentence):
+        return len(sentence)
