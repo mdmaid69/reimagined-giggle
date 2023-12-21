cc7a6303384a0fcf95ab4414937e3dd25d1fadf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:12 +0000

    relative truth shelter him thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
