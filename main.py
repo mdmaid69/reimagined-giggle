9eba7719f48a42affdc9d861a89b292fabb20127
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:21 +0000

    hold 566end lost way instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
