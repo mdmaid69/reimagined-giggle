0b8aec62179880b43c12194a77f42e519d88a264
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:23 +0000

    collect scale expensive flight dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
