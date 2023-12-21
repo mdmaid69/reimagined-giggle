a709b4f781c99441aef7ab2dd769e5cf67c3f609
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:35:17 +0000

    themselves closed water right breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
