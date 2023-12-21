f2cc621077f6ce098cfd7da358761792998e4f81
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:29 +0000

    alive children rush depend tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
