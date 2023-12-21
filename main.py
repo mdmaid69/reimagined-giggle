af1a128c878840490ba37a499b3c0d2657b29046
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:57:56 +0000

    willing moon appearance speak rent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
