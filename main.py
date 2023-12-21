8c60ddf2bb9e055b220506ab790cd140a6dd8502
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:35:56 +0000

    slowly far pleased classroom minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
