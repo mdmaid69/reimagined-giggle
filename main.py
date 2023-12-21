4f5abfd76161bd7645de6c24c104af7ef2854942
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:37 +0000

    dream that expression happy pride

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def cube_number(x):
+        return x**3
