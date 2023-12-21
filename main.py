14b7c5da196c994533a7e05d8a2b1fe739743701
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:48 +0000

    physical wish search broken thinkof

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def remove_duplicates(lst):
-        return list(set(lst))
+def convert_to_octal(n):
+        return oct(n)
+  def square_number(x):
+        return x**2
