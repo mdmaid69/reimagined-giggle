d846e6b33ad6fd2d44593d1e0ba5c80de88e1e84
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:26 +0000

    tough still excited shake public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
