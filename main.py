b496418861ea6f7624b5b161b7cd520b911433f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:30 +0000

    high pot apologizeapologise hurry answer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
