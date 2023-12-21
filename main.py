cc7df9f9542e2c58bd66337db8abb00addd5f14f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:16:28 +0000

    thirsty improve its before happy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_str(array):
+        return str(array)
+import sys
+def print_python_version():
+        print(sys.version)
