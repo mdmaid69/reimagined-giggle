b0a954fc661230d6b8da2a952ad6ba35c2f758a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:51 +0000

    if nose spring music can

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+print(os.getcwd())
