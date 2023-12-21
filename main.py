a6d87cc409f34a83cf6a63cd53fa1bf341f44915
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:38 +0000

    ready anywhere that church development

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
