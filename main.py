500e1aeb84d38104d0f3c9901053e59f25ed7ccd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:54 +0000

    hate bad treat emotion soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
