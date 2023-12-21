ec67602da40dcce37004759cb54cf953bedb6852
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:23 +0000

    release future damage broken project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+def is_palindrome(s):
+        return s == s[::-1]
+def is_palindrome(s):
+        return s == s[::-1]
