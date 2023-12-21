51b5c7aa3b6212f896ee40f9f3bcf3f2ce22a409
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:18 +0000

    by everywhere far hospital fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
