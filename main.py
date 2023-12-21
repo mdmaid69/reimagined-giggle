9ca13d5eef8fa49d1d1dbc773b8e381a164c2433
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:48 +0000

    anxious fair ring hard pronounce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
