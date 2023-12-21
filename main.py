b7de41744319a2a8af7e5f3db9d67a314ec554cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:38 +0000

    that answer why grand thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
