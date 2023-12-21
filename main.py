33ca6408c05d7487fe0cea28eecd2499f92cdce9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:02 +0000

    divide profit busy make innocent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
