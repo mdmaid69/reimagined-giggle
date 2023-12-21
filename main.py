73777a11eb7941aba3756e76e7d47fc2a0a72f42
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:48 +0000

    square disapprove refuse safety speech

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
