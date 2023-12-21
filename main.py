f673aa65198530fc6281b23e6df168771fe30ba9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:00:18 +0000

    translate experiment talkoutof cruel stare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_min(numbers):
-        return min(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
