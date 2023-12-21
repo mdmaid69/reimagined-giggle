33505fb2dd1c0faf5d1f74f066a2e80870019ceb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:44 +0000

    hard runinto suffer round neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
