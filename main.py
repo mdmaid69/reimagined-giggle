138715dd25c3fe27cf09496c49e167eb370e81c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:07 +0000

    manage tight tape Ss scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
