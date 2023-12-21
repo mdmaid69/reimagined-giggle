8c26087fb4fae99757d8b969a27e1ba5f9e2e28b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:52 +0000

    unfortunate really worried dress glass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
