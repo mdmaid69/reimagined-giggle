c359f23d49813905e9f4bf578b62cd1db42303e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:20 +0000

    giveout doctor gentle blue settle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
