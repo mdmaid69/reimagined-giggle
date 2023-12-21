ad7f4d7dafab97b6a9e0d1be0f8d61b8419f02b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:02 +0000

    could careless try miss same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_sine(x):
+        return math.sin(x)
