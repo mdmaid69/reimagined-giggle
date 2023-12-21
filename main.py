11ab7639401bb1f64c84674083cfbb5093bb1870
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:54 +0000

    Pp Ss factory ask 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
