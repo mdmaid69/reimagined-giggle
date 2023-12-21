a3e8860c7f2b3560eb4d44862a4ee0c1da589da3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:35:30 +0000

    uncomfortable fit comeout drink such

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
