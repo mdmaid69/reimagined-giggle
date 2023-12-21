cb62294b51ef71a6991af042dca9a288c50ee8c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:34 +0000

    key surprised precise good pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_float(array):
+        return float(array[0])
