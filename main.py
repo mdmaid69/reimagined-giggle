bbf2f07ffc2bfb780acac5133b8952cbab953e25
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:00 +0000

    realize only Dd drive less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_work(force, distance):
+        return force * distance
