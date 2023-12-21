53041eb32149069a5a1d76188b1b163ff6b80819
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:36 +0000

    sting period cheat goaway pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
