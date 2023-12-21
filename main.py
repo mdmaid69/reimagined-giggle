f44f133b10baf99156e36a5e0304a9e11684254b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:37 +0000

    roll illness subject schedule thought

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
