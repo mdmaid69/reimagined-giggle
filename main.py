807adb46daa9a1c6583905614e8b811e31f57bfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:38 +0000

    kill makeup back load anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
