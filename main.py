6dc485e0131fa1192c95df07a6136d1de38d65c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:47 +0000

    beside talkinto telephone into dull

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
