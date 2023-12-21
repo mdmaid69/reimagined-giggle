de384f296f235f9dd5431da06aeeef43880d148b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:58:00 +0000

    meet connect hide picture divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
