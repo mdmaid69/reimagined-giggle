c6a1e6f5e4a422255b192b6b43dd887277d710ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:04 +0000

    purpose medium blind than many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_acceleration(speed, time):
+        return speed / time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
