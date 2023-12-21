7b5a99e85b96b008e91cf0b8c08fcbd5620fefbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:04 +0000

    everybody public powerful sweep roof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+text = "Hello, world!"
+print("Characters:", len(text))
+text = "Hello, world!"
+print("Characters:", len(text))
