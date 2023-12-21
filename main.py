2ff2f85099624f27f460df193320006163148fdc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:54:06 +0000

    trust middle human label beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def append_to_array(array, item):
+        array.append(item)
