85b08441550fcf6ad61c37b5149c8b0ae54456bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:12 +0000

    variety label spread wander need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def sort_list(lst):
+        return sorted(lst)
