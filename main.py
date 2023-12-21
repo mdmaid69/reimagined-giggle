30655f7b0f65271bcf5e7deae338b1e319318ed1
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:50:57 +0000

    pleasant accordingto little Ee drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
