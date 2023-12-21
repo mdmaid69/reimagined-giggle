94c62465d7792a0bee9bf98597692d7c76809124
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:37 +0000

    welcome hand lesson dark attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
