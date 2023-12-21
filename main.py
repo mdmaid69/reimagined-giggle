0aef807a7107aee69d36dcb9e4fbc746636b7988
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:52 +0000

    nearly warn giveup uncertain news

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
