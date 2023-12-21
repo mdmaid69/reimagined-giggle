d0440e91164dffdd7bd62c77b074d87c2aa8c552
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:13:25 +0000

    realize upset duty behaviour confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
