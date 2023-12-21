155d28848683f3a273210fdc1ad858914dffba26
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:31:38 +0000

    whether account tour because give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
