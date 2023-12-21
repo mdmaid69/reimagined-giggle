c1041184f6c7e1c4b43c1a2fdb4e8aa9ba2eb58f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:56 +0000

    authority set create tool law

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
