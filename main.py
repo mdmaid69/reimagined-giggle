92ee6fa4c21f4193a17f372df48ca88115b8a31d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:36 +0000

    Dd complaint apply repair task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
