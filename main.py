d37a4eb63d397cce6066a0cad5cea5e3a56e6565
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:11 +0000

    force workupto normal Ee breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
