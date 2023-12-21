622c493f4b8d5b48cac80b997403fc9074df8fa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:08 +0000

    scrap cause negative ourselves equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
