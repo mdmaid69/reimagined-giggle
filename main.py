1121ceb113cb0f934f91666e19470f41ca7b32f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:22 +0000

    bush cost understand bunch cover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
