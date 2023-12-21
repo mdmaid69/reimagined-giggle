47507ca21eba102672336570d4d6e992235307ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:25:21 +0000

    dull future behave deliberate little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
