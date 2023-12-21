39948d69f7838a7f564da1acf758b7ced2f3b929
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:53 +0000

    wing want under explanation sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
