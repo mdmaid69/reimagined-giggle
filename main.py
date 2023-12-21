3d7383551f2417a273f321cab1a6eb693071aa79
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:42:03 +0000

    distant load key attract criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
