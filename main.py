dff06ea0e026c6a0082b619279311beef3eb6362
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:21 +0000

    high lot tongue man lonely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
