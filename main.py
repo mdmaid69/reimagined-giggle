3858832420b91f59d63c0726069f1ee77e27799a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:16 +0000

    disappear compare thinkthrough send last

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
