7e419ab0fef255dd2756fbb9df62ef005f3467dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:48 +0000

    solve container love violent drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
