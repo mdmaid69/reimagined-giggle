9483659b58836c816e15f2cc778869f356a1274d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:06 +0000

    furniture some goodbye reaction phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
