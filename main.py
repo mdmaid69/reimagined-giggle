0de659cd3d22cef33daf09d4d25e1c98205df0ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:52 +0000

    wake rely familiar software ever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
