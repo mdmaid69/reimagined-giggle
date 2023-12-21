15dcaa6e5a27d51c0ff4206abb81ed70180d2b99
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:29 +0000

    suckme disturb frighten extra dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
