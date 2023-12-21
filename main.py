6b743c7f6150bc81b77fc0035909df47ede506e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:24 +0000

    waste maybe rely perform my

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
