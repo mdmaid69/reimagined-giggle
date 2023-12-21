cf40dcb500a3b454a759cc54359da434416ff3e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:17 +0000

    not apart process wise refer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
