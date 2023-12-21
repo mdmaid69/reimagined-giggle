523c116ed83fab7f94adf36762405cda8372043e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:49 +0000

    thankyou light fork smile become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
