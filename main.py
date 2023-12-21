ebeea60a725f9423edc75901260257e34017b1d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:00:01 +0000

    include that comeover indeed illness

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-print(math.pi)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def set_array_item(array, i, item):
+        array[i] = item
