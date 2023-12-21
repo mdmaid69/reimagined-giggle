0df33c814ebab8ace142c2039f0923704f492a47
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:17 +0000

    put Gg ball else whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
