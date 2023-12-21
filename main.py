b61a6a0bc5e01143cbbfee435948dfe4401dea95
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:18 +0000

    little pick welcome respect obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
