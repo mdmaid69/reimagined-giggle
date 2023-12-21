4eaac669aeeddd1546c59ea87f88535e7a7f20b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:23 +0000

    create tape do Rr innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_work(force, distance):
+        return force * distance
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
