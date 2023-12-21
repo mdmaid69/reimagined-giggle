3da25a07408aaa3aeca38989ecc1e66b00141652
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:44:53 +0000

    character complicated surface blame guess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-print(os.getcwd())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
