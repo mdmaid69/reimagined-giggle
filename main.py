89cdf786748a421ec7f9799657794d4dba74d09a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:16:23 +0000

    factory breakin song protest anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
