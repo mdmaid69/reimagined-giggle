46728e67d566f83a5a71a000d3854bb2eac38b4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:50 +0000

    our heat century lift follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
