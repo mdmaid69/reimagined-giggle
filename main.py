194d10ec5cc7b78034a0988c0feb0138c16b1400
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:48 +0000

    his weigh owe generally paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
