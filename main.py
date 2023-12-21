ee05b8f9547ca5026e7ddd20857bf59cc061b0d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:51:13 +0000

    score moral love about soft

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def cube_number(x):
+        return x**3
+def is_palindrome(s):
+        return s == s[::-1]
