44da34b0443e9e406c49e8bdac699a9d9b582c47
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:36 +0000

    field feed complicated can car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
