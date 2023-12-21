17b4355aaeaec74ef0017520c84c2245dfd03c7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:43 +0000

    stretch many body journey mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
