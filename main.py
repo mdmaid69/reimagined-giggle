df9ad3acef4066add011adbaa312691da9db7cdb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:32:29 +0000

    over chase blind do clear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
