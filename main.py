c2268064c89931f5a0a958fed6da794cc533eab4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:06 +0000

    turnback ours further right mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
