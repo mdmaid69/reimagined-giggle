49829c3bfa7641e07a204bf967bdc78cf6d2dcae
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:55 +0000

    comeoff tall fresh important under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
