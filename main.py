07d9c8d529654936811fc0b951d69d91b33a0c5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:32 +0000

    suddenly Kk treat needle culture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_binary(n):
+        return bin(n)
