776159cd07ce67bd797250a1132d58870bdf39a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:13 +0000

    ours platform try Vv progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
