1988f64ac4b7279db042afe42f783a4390200d16
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:38:03 +0000

    spread jump piece turn tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
