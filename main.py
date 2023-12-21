6c7ae3a61455314f9db3a1f985ac9e6f5d8edd81
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:13 +0000

    runover responsibility me burst especially

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
