beab715776cbb5c648ca3ad0a2a9b7eda64ef9af
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:46 +0000

    runaway bad calmly separate music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
