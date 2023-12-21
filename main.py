d08f4f46ca84e5c89d56b22cd7f0025fa491d830
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:55 +0000

    satisfied block act use price

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+for i in range(5):
+        print(i)
