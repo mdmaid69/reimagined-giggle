2b902ee801cffff5f6c9475b36ebee9aa1d5c06a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:02:16 +0000

    disappointed pot degree neither spin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
