6ed7be65622e85017a78f0f93a270dea45da3a69
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:08 +0000

    used party warm worst value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_speed(distance, time):
+        return distance / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
