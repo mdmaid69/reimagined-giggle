2edc03028362c2e4daa8a4257365f5be786e7dd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:32 +0000

    tradition answer worth strength change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
