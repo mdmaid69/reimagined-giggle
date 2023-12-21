dab3954513f3630aa02e2b09fb2c7337e31bae37
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:44:22 +0000

    doubt advise against hello speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
