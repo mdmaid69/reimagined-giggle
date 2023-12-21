9d462e9b61c41ed0d1a6864012557eba6a5bb065
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:01 +0000

    typical owe warn connection confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_power(work, time):
-        return work / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
