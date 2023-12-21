1460ea1c7a222b56f51cd11750438568b414ba7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:32 +0000

    project negative warning team trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
