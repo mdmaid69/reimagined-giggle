9cc9673380149cb223cd759e81ebe28a79c02d07
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:12 +0000

    excuse collection fine scrap extra

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        return sys.version
