40ff1f8ce6cc5fe0d84e21fc2d65e841f760cb32
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:28 +0000

    single serious official calm perfect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
