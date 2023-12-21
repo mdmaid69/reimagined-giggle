928a619246241ce17359b9f05986ec3dfc20eb34
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:30 +0000

    gather hurry small gift might

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
