e11fe07505c5faebc01c68ff4320c49f36a38762
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:40:56 +0000

    cover workupto comment maybe pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
