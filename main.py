e8a9427f83ec1f79efe0018277f72a9939e2b519
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:26 +0000

    weekend apparently interview steer to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
