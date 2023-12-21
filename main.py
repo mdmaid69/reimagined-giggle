25bd204958a819772a9869a5a10d6757ddfac543
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:29 +0000

    take confusing obtain late never

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
