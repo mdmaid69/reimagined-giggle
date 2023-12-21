a460992e728646f0a47c34a091abd89110c99b44
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:05 +0000

    risk fun dig suggest government

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
