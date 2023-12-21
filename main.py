0b680cfd7c5541013e6f8a233dc8db206156fcd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:43 +0000

    perhaps share dish tongue Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def sort_list(lst):
+        return sorted(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
