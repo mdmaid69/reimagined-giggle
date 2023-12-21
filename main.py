e5a3474cbf38e36947eab28ababeed357a61bf5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:56 +0000

    tour this fit cold hers

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
