55848723a706a194a7bc4b18ece00814e9c60a1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:55:17 +0000

    moral storm hair stay live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
