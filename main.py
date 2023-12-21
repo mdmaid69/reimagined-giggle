bbfc9e5deece0e22cae5fa703fbf1818665f9ae8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:30:20 +0000

    Dd quiet when convenient chat

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_work(force, distance):
+        return force * distance
