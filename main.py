0a59d493762dba5de8d2aae9c89c50c785b87893
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:30 +0000

    moment practise objection unit dick

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
