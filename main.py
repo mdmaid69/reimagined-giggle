eed38cf31b60dfc5d36f1ffce8a6b66f0bb89857
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:54:53 +0000

    roof form should sleep bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
