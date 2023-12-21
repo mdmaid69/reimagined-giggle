8fea8a2e55aadc23a1a8b9f46aad7e3a05979f91
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:11:42 +0000

    after broken extra increase play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_sine(x):
+        return math.sin(x)
