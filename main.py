40a84f8720a38d6e6605a7054ee94fb35ff41638
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:13 +0000

    butter it nature ride defend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
