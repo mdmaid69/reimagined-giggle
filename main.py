61577a6d33817154c67797d2ab41e78f53112f05
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:06 +0000

    battery break laugh cause compare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
