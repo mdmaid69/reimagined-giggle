271b484a9019d7aee2a44e52d19bd9833b26d1d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:55 +0000

    thorough complete worth awake sick

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
