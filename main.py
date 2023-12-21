45add44a851eb34bca5cab4e636f7229f99a20fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:27 +0000

    minimum least Ee hard kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
