ccc5751e500790b3f1e8ba113174fa11b8b622b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:29 +0000

    bye conclusion deep flow level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
