035cf4cc1b0c29c304e94e9d49d6fa3242d09288
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:31 +0000

    approval consult alive chat dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
