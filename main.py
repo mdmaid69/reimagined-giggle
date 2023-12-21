e40f5bad2be03df74ccd11937e4f2af4018d0c4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:55 +0000

    increase catch crash entrance one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_array_length(array):
+        return len(array)
