ac7d8e5ed1241c5cd30290e10fa0df9dc2f4930a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:04:53 +0000

    moral sister feed instruction experiment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_array_to_string(array):
+        return array.tostring()
