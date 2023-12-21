e681248a395bf8469b7d78e14aa7cad6a9b74deb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:02 +0000

    height Uu height study present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def split_path(path):
+        return os.path.split(path)
