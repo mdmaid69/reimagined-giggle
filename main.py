e0d265e60251698019c96f6b2a96ebb024a82563
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:23 +0000

    pattern edge bend 571enough comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+for i in range(10): print(i)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
