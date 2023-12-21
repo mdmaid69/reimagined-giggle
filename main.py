088a1b2ab3c3eba423cff0b51c276347ac1f48c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:30 +0000

    decide date bang exam comeback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
