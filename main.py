e8f7ba1334b6e7fb82ff63d5d7dd89a8f9ed49e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:52 +0000

    preparation shopping commit or round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
