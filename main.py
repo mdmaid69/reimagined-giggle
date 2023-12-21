75d2b3a998239e9175fbf470a879b2ca9836d09c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:55:55 +0000

    react female plenty disapprove sensitive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
