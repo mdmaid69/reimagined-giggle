39129e7fbda68ef600db442b27d56cf668cf306e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:40 +0000

    contents sale fact display edge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
