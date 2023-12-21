174e556e3d2dec05e14c377e519948590112128e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:10 +0000

    normal stare pressure bad watch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
