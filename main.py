1ca504212b48371c156213db2f4285902c4bafc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:30 +0000

    laugh say paper Ii out

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+for i in range(10): print(i)
