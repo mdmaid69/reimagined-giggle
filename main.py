2771a507e1894ccbd5ed40aff04c987ed225409e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:34 +0000

    sink ride through dream perform

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
