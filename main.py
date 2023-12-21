d44768e64e55867f719a5feaa54aa0d67db34795
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:47 +0000

    who better bound offer show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-print(math.pi)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
