8afefd90a6c97db7c9703073f5c44a9a4541f61f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:43:25 +0000

    instead ring find over success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
