f44f1861ebd9b82a32f38ea11fbb15583e431fa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:09 +0000

    play dinner leg address middle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def set_array_item(array, i, item):
+        array[i] = item
