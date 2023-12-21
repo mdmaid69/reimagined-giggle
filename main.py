efa40c02f31b4b186d6712a397dab6dea3b675ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:30:58 +0000

    thank meal tall hurry anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
