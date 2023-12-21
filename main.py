34592e1b74095a8cac68c6f2a57a16f355e140c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:23:46 +0000

    pupil better attract organize 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
