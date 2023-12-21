1e510b4d5888b2c4c3a73bbf5fc54f10806d6fcc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:46:26 +0000

    keepaway blind breakin noisy street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+print(os.getcwd())
+  import os
+  def get_current_directory():
+        return os.getcwd()
