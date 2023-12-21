5b4ca135092bbb5df1e124731b3200c9b1bf4a9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:55 +0000

    stretch hate surface after admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
