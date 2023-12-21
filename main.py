a11d25121346fbaefc34e7294e7b4e8a04c41692
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:46 +0000

    and point small trial stare

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
