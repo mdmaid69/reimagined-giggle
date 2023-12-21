833a73712f216438d6da89825acc40ce4b6d76c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:37 +0000

    after realize press suit care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
