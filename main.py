e577f10d72a5c71aca26eb6bc1760b4d05b8647c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:47 +0000

    win friendly cover wind lie

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print([x**2 for x in range(10)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
