87ab13a3372553bdda8f079aa7aac03aa2c2dc5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:17:56 +0000

    greeting cheat light importance line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
