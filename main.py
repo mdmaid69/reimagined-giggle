5df6697dc75940cb82672d4266f170d3c5c03ba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:30 +0000

    hit copy violence weather equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
