583da86970f38191148f91c770a5ddd177dac693
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:19 +0000

    land tooth joke excellent twist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
