079486bce222f65f05e4ce2b08752fdd04595a59
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:25:19 +0000

    switch 567enemy famous charge attractive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
