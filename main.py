1c0e3abba9f3fd8a40a92b159a8b54dca9ec463f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:00 +0000

    lay minute strike public damage

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+print("Hello, world!")
