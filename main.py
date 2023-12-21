f359e28615b5abe068ae92c749907068e0d59386
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:41 +0000

    believe start Zz piece angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
