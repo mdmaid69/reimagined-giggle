3261c1e0ec4559082a9f1fcdca0ed1059e964108
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:24:19 +0000

    takeover pure unfortunate shade spare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+for i in range(10): print(i)
