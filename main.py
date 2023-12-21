08dce934ae4ba99fc8b447863dde7d88b1d9c717
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:41 +0000

    surprised give emphasis odd phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
