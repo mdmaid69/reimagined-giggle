f53d7df3d7fc1ea31378ab947f06010317345299
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:27 +0000

    serve bank consider rob course

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 def find_min(numbers):
         return min(numbers)
