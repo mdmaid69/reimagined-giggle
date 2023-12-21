b45156798f970464bfe125ab0d849f2170f2818a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:40 +0000

    just platform walk step decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
