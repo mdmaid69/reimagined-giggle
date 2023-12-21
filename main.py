9573eced39b57008e0b42c714ef0a4d7009453ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:25 +0000

    from born sense hurt standard

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
