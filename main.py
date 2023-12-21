7d736978e054811a5e49ac0c397f64815a20a922
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:05 +0000

    suggest take plate march argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
