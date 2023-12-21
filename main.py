a2a57ce658e66a100ce9a199432605db89819ca6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:11 +0000

    fat change chin talkback tight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def greet(name):
+        print(f"Hello, {name}!")
