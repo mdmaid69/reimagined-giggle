f91f71f937d90a2afc3bc1dceaf8ccbadbb541fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:23 +0000

    public bound from behave considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
