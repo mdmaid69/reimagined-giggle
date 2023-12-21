6e72a196487677f9e4cab9b46ee9fd86453fbd16
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:49:16 +0000

    outside line risk rough shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
