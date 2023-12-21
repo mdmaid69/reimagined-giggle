009de041a113e2669e711480019829995d36bcfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:38:07 +0000

    listen straight until display have

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
