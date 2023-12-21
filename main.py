78ce63cae8a00d317b32b39545692eaa09941645
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:52 +0000

    just once Dd badly exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
