179ab580be79d18f81764e90c5ca7160ed8d6761
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:47 +0000

    news gentle disgusting amount 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sys
+def print_python_version():
+        print(sys.version)
+import platform
+def get_os_info():
+        return platform.uname()
