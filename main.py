20dd78699c7847e0118753299f24ac361e8bd385
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:16 +0000

    all check comefrom compete leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
