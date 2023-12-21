d7b449be091f4f13f436493dfd4e3f39aeef25c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:38 +0000

    drop professional joke control mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
