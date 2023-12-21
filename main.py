bf172a11aed85072e341e0f26c853a1634194141
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:07 +0000

    talkback fight sad stop connection

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
