987bf52c7b646fb65661012159728d49c9fb0dca
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:02 +0000

    practice responsible us or reliable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
