d45961958e724642240b0d8b56ac411995de37b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:31 +0000

    much refuse television Cc personal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def clear_array(array):
+        array *= 0
