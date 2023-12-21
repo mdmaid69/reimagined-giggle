bc6f2c3703f1a8b13a4a8eb806ce342dac6d4ae5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:22:09 +0000

    nigga remain variety wish normal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
