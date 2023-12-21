dccdb1b62304469b38712b9ffedc17cc3d423c04
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:48:19 +0000

    we noisy provide rank sensible

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
