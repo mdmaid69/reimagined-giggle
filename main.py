67f796d34ae6d360ed20c0e321e7c1a5e1927fba
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:28 +0000

    bottom sink pain manner rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
