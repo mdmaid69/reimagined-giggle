7604bc4191cb0c3f0daa56fc3ad3c27fe9fe8611
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:28 +0000

    because scare godown measure money

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
