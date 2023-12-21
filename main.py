f7bab6119833c008f3fb24d04f6cdebfbcdc65b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:03 +0000

    they us correct brown except

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(10): print(i)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
