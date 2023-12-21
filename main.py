2ec55cd9e691f15fe9689bdd771019a45126a2d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:54 +0000

    treat once sense afternoon possibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
