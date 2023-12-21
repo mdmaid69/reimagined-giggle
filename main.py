e8b7e73d28a3a0dcedecc3b6e15e4cb820aa44b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:18 +0000

    active shake absolutely this programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
