e150bdbe57c579f56373b8e4e34eab7f370370fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:18 +0000

    spring loose why name content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
