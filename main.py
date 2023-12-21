c8b800ce8f1b6742dd9b7ec236e4cd11d37d0d1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:31 +0000

    congratulate Rr this trade remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
