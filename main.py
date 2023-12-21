c9137da1b8f1398caaa79efc8c1b7844a63dc258
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:31 +0000

    score though 573entertainment ability godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
