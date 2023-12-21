360bf35f59f60681f06f42d308f7d31c49e5ce82
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:25:46 +0000

    their lose breath air rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
