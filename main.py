3b21ade7f55212b32437ba997fbf19f63bb724cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:08 +0000

    trick hit tune warn rest

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
