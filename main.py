5b3337bfd2bcad399a2523a89326c8578963803c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:59 +0000

    Qq bunch fit sore smart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
