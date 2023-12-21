befc42bfc7728ed368d90dc0f4970ec0a9f5df23
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:23:03 +0000

    pair knowledge call hope bar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
