e225a5517a19c87c951a71bf87c0a78ecb5e6e27
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:19 +0000

    make speech hot factory OKorokay

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
