067609e6ec3fcb6c02784444aab4a4f392997eb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:54 +0000

    form clear scene many pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
