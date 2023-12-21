b160c9b615ea6ef2f8bdd72ec128493da47a066a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:07 +0000

    study plan original offer considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
