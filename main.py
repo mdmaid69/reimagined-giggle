eec3d46e9b3bad67e167379af27dede03c981bd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:04 +0000

    whoever meet about pronounce themselves

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
