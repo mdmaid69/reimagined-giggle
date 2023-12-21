5b7c529882dcb83f15ff26e723f2c410f4aae286
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:53 +0000

    complain tour trial relative murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
