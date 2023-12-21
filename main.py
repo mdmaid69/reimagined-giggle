273e8ab3723837a8e48c14af7f351f4009475a8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:09 +0000

    criticism my influence several rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
