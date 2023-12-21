1ba52a0a2a07470d348da6f8ee5b1c2d3023a07a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:32 +0000

    both speed 572enter approve choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
