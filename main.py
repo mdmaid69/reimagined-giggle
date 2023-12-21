f1236d01ad571070b5831d7f62bcdf9e97566225
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:43 +0000

    hope lonely radio circle produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
