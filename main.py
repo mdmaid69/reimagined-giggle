b0ed3f28f7b315b08835ab5b3ea6e5b8f2cef848
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:54 +0000

    dangerous myself before blame act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
