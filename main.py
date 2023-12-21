2ec331dc63a6cc206179cc9686a51c4061379377
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:07 +0000

    fasten head period lucky concern

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def cube_number(x):
+        return x**3
+import time
+print(time.time())
