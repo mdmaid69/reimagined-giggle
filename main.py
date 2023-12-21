eb70578b3aca1e84943c6f6c5112d385f60e74bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:43 +0000

    because reduction fire operation opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
