44d60234063f79832da56a7e1bef6636135524e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:27 +0000

    flow crash phone worry limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
