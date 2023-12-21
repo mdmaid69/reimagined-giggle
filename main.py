cf2134c2f3b5e44978edda17c1c2a0acc3718ff3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:58 +0000

    pretty gift fetch firm cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
