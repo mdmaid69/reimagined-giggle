94ece6ed278c4b52a10a255d0692a844627b93f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:02 +0000

    bunch matter petrol use occasion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
