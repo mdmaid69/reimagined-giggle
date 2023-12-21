cec44abe7978007f1f6da310cd1afbb3eeacc9b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:24 +0000

    event sink runout sweep busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
