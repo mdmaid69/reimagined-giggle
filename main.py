093b37567bad90a5545958f42c50cd72cb4849c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:19 +0000

    now harm land accidental professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
