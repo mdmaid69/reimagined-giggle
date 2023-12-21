65967e9921f1cf3828f3b3e5ea309d5616c0032a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:10:54 +0000

    opposite talk bang shoot complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
