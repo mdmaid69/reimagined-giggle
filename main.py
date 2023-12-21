e9f5e8d9b496946a7e38a5e3d5298a4ff4a178a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:27 +0000

    statement getback peace with upstairs

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
