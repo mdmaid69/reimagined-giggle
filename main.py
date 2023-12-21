dccac56fb9c26772aa4b6eb30b6d77cc43478f1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:08 +0000

    finally exaggerate guess nothing awake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+print(math.pi)
