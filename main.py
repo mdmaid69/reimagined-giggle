d4fb74eae555138dc16a825992327718a29e87ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:20:41 +0000

    himself gooff instant office voice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
