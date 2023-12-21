4f23b26d94e7e297211c01e42e9dcd7bc5647934
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:13 +0000

    usual lose ring time exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
