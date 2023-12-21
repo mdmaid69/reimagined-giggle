1a047855e3de78246d3b2044a8aa0eb974b1555f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:21 +0000

    object prevent feed cheek holdout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
