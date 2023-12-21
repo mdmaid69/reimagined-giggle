82c22ddcea8dfbd30994f74b19eb1b08dc37f3f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:35 +0000

    familiar repair for basically determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
