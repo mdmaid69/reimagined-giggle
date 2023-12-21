1d9886121479fd8d840ccbf97c95925ee01120e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:09:42 +0000

    switch much whom persuade survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+name = "Python"
+print("Hello,", name)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
