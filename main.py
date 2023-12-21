bb7cfbd96fbdf313549ffd8b6dceb7364af9f04c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:25 +0000

    yet punishment score hard advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def get_current_working_directory():
+        return os.getcwd()
