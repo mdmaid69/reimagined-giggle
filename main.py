9a7b5b016dd6ae7ecaf163ae8738dfa66ffab9c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:05 +0000

    pronunciation copy height cap base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_counter():
+        return collections.Counter()
