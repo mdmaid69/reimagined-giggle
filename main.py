fc3394e715cd3de39d0b85a6a6c5338a9fbe32c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:40:05 +0000

    where cough provide rest pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
