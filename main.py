da7adc83cb59f8035eec40b8f5f96aec5879f9e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:56 +0000

    load butter violent custom lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
