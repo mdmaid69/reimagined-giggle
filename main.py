dfe15e15272100fdf0ddd2977e4e14a1badefb54
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:30 +0000

    fly summer land exit certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
