04ec666b4df0237d9a9b0cbceb7733800e60620f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:24 +0000

    light love getoff surprised deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
