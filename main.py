60badba76cc9e8689534afe462ec7c93f4c7b96d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:06 +0000

    fortunately way frequent speed please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def remove_duplicates(lst):
-        return list(set(lst))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
