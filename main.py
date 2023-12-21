830cad3df673d25614226fa855af687d683356a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:43:45 +0000

    children gas pollution hard Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
