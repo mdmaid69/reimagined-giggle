d19b50db4cf084c92cf59ce15d2629178e3636df
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:56 +0000

    themselves squash powerful appear mystery

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import tensorflow as tf
+print(tf.__version__)
