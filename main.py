58ea6a5271b4f92a921974dda9382dd512153c29
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:47:55 +0000

    throw tall because settle advanced

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
