3b84e68298cbcdf498355b59184d558a6a9f4d88
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:38 +0000

    butter responsibility passage something shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_power(work, time):
+        return work / time
