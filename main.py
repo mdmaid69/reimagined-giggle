98ead43975a666a3f6da0b7df5de7dcde9b6c445
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:45 +0000

    enthusiastic skill waste relative guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
