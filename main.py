03da8f34280cd108f7203d9dd513fbfe2cb855e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:51 +0000

    solve lead artificial whether distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
