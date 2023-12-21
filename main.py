a397ad8e1d04b64e17c70f8976272ca35d6c072b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:15 +0000

    sting free she ilovedicks exist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
