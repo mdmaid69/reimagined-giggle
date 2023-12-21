798b7c1706dd887e4a336393587e0cfd0222a17c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:13 +0000

    gooff real together pause Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
