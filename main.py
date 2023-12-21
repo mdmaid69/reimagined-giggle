99deff2f05c1f3a064d4e84e94d33e239cb289c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:43:32 +0000

    bound fortunately prize physical track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
