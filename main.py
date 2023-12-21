fe25cb9bfb4b4b5cd92481ea4d2656682f62c1c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:48:27 +0000

    learn respect tour without getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
