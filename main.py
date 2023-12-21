b9cc3ed8fde7b359ecedde85ec19ddfd26ff6156
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:56:25 +0000

    broken left identify habit keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
