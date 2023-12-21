a5dbe13d3e420199bf85508b168014a8a16a40a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:40 +0000

    Zz worth Gg have mad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def subtract_numbers(x, y):
+        return x - y
+def convert_to_binary(n):
+        return bin(n)
