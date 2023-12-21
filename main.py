698c10a5369416e1b1329d606b9eca9322f29208
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:51 +0000

    agree 571enough love contribute become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
