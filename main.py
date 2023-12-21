176761e6798fb70118f2e3136f3b39703993abed
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:29 +0000

    declare extremely whisper complain turninto

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
