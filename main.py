0a3e377924eec0d2038387afdfa900c0ffdda35d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:15 +0000

    speed search throughout cruel building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
