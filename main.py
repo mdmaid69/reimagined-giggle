7f1dbea8b150091d2c7bac89d605ad5b65228e53
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:45:22 +0000

    road live makeup afternoon wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
