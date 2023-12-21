071a1f41109a787f8ba9ca2eb200f4c10a99514d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:35 +0000

    wish damp pretty consider slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+x = 10
+y = 20
+print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
