29bd849439cbf300a50c1aba3ca5d86d7416548d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:05:50 +0000

    whoever behave hang pump little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
