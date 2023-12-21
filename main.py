0f0da5093d7dd52d9d090bdc2bc5ebaa0c39fb8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:32:49 +0000

    all listen goup fasten sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
