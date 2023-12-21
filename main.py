255b14b6f2b60bae6c7b46cf6249e5abf2cc6546
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:22:42 +0000

    to exam further everybody full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+def get_current_date():
+        return datetime.date.today()
