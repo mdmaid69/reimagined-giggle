81648ad6c14f8fc86b1c3a0a48ef09d8dc886599
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:44 +0000

    regularly breathe Nn disapprove desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
