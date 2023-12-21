5b3b8229b96a8605734904d6f6d52062afbf7bbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:15 +0000

    activity scheme question all out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_pressure(force, area):
+        return force / area
