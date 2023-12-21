1de9bc5013c9d7d7df1c860050c8e6354e41a718
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:13 +0000

    almost find clear Zz past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
