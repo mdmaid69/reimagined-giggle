f78b0a58dd49d8984ef36f8cbfeddf84b1655432
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:34 +0000

    want breakdown owe place forget

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
