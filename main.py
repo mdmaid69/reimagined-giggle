02d9d8e4c76e9266ac8f12aae9626204e2845ba5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:19 +0000

    fasten charge shame cope post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+print(os.getcwd())
