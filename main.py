5e7fe740871de2049066e3fca8e9dc1e8a86af93
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:10 +0000

    protest weak maybe land once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
