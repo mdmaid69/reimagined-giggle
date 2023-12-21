56b68b1f08573a988b1996c755edbea0f831ec0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:58:56 +0000

    afford month science bath also

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
