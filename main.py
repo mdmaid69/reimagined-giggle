8ff0d9dc3dbbb8a98ed428fbb7a840a81c6545a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:46:40 +0000

    plastic suit live too reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
