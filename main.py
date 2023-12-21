6aa2954991afdb38acea5906b9243b84b0146105
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:01 +0000

    ready convinced variety crash everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def get_current_time():
+        return time.time()
