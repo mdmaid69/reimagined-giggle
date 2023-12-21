11faa1155b8b0e2e0493ae8733d5b1165ec6e0c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:14:03 +0000

    how waste medium point basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
