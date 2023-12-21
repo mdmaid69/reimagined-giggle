214b9bdb6c1480c899eccc36c05f979e602165d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:27 +0000

    know variety visit factory everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def count_elements(lst):
+        return len(lst)
