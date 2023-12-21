5456455b90f86f38d994847234d7470e4cacdf49
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:54 +0000

    grade this tidy Cc bury

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-print(math.pi)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_length(array):
+        return len(array)
