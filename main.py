eac5d3fdd70835bff7de171f4695fe31e9c3d0e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:27:31 +0000

    odd something valuable negative ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
