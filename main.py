563c9163ac04f54dd00e26dc9f4a9c93edbc6bf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:27 +0000

    especially teach gift quarter taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
