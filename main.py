fd5efb494802a5c3a9ce5ff853282a1eb5b03ba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:58:06 +0000

    least close eye get spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def get_array_as_set(array):
-        return set(array)
-from collections import Counter
-print(Counter("hello world"))
+def get_bytes_from_array(array):
+        return array.tobytes()
