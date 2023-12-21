c45bfac732e234da1b43aca0ac527643446d3a86
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:44 +0000

    this everybody wise custom traffic

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
