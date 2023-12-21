47301c3bf2095f1607ff1fc0ecfc485e0ca685ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:22:34 +0000

    exit hurry chance pleasure bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
