7b9191a6f72c91a75f1feeb98e7fa189e95916b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:15 +0000

    home spin bored different thankyou

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
