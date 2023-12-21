a9acc8453a80a07700c2da1322054df30074c07b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:01 +0000

    depend kill stupid represent we

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
