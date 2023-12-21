850d371dda63a2ef0923c1a1af6667d97d8bd05b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:02 +0000

    patience study hot improvement object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
