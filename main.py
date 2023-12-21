6f444bfb32180bad02dc05e72c759145b75990e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:42 +0000

    silent chin example introduce minor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
