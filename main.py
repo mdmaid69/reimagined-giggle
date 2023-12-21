27667daabecb786700f2a1ce1cbe77c8f15d2aff
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:06:54 +0000

    building interview unhappy produce thirsty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_min(numbers):
+        return min(numbers)
