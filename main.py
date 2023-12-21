39483a89a20e7f8fa433d94d01eab0c64fc3ab65
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:01:03 +0000

    shop behave to fashion show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+  def add_numbers(x, y):
+        return x + y
