155327f987be8b498a6a44ebef0a6b7d1d0cd0a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:34 +0000

    lookafter repair raise message sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
