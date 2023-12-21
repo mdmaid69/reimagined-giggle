e8dfc2a322da7942ac59c33fece65f9e16f579b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:33:02 +0000

    succeed beat onto sudden that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
