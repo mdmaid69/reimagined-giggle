8afc171e056d82c365f3cbbe3cfaadcb3a06a11a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:31:27 +0000

    also hungry justice back will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import getpass
+def get_username():
+        return getpass.getuser()
