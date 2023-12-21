d6c7b484c9ab6ee2a1617ce39b3edc24b5302dd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:52 +0000

    transport pot Kk morning silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def clear_array(array):
+        array *= 0
