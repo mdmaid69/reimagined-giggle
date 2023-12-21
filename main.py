2a2835a45f1a6383672079fa0cbf865b3e21c227
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:25:45 +0000

    only scared weak knowledge talk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
