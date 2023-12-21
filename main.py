9ea1ad096450b70acd8fd85a3e462b7b2c668edb
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:25:27 +0000

    further genuine power total slide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_palindrome(s):
+        return s == s[::-1]
