bb14e0bbb23b6a4f6ef4e94eb214e719299b8728
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:44 +0000

    weak description switch store software

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def create_stack():
+        return collections.deque()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
