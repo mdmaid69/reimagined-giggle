ae91b45b0796f9c873cca626c1d7ea2323fb0a10
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:12 +0000

    friendly concerned work profit fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_list(array):
+        return list(array)
