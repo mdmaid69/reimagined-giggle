d3f42ce371cbbc928e196d0d159453b5d8e5c092
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:38 +0000

    brush clear watch mess tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
