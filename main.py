d92c68f09b8656051a1947b1bf4b4112aae6884e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:44 +0000

    pleased fold organization character further

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
