d28403c66718c67ec2bfe3d41da26132b6b9d163
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:01 +0000

    smile cook must cough heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
