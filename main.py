039ea71cf0a50532ce8816e219f52bb76948918d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:42 +0000

    combination rain connected several software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_density(mass, volume):
+        return mass / volume
