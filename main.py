87bfc8bee55fc0270ddc69c5e70704c843c3fc46
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:57 +0000

    used position Ss beautiful drive

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
