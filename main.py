7eecf806b177710c96151e391dfac5e7f54050a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:28:29 +0000

    lost pour run work runinto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
