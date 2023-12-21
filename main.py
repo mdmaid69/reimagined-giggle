8b06b421fea6f2aa06411507e826045a7684f6cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:47 +0000

    warning pour dark over petrol

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_as_set(array):
+        return set(array)
