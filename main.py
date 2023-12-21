33433ddfe7e094fd922c5e10e90f0a3ec05ca33d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:43 +0000

    little drop part Ww immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
