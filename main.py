7437ba3caa23fed55a7312cc83f3dbc5a8240980
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:45 +0000

    something whatever unusual comeoff mess

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
