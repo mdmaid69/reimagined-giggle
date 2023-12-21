f4e92f754ed3813a9321fdaa742f00e7e272d92e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:36 +0000

    mix suckme cry follow fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
