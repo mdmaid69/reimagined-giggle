747a2d47c0edf495d30ca38f258ec24daaade547
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:09 +0000

    attention terrible unlikely 568energy dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
