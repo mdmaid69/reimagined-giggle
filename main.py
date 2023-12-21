3f777665e41cb8a51fc7ba3b48c3c44ea590b0c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:34 +0000

    to injury herself trust unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def square_number(x):
-        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
