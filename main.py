71afb9077ad17de1984842fde52705ceb0a2a660
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:24 +0000

    careless healthy draw preparation otherwise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
