eb9c5cd2fe865b9502f613b05daea3e7df47b45a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:42 +0000

    miss read brown later hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
