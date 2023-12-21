04be37b66241939bb5bd7960cd1003ebc2b9e625
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:43:10 +0000

    purpose rob woman flow someone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
