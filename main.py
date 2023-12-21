18a4b8a89902777397f45c93ced4454bac69bbd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:13 +0000

    joke friend proof shame pleased

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import datetime
-print(datetime.datetime.now())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
