5712d80ef6718b9dc3aa6715d9df4e3922b76bd1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:23 +0000

    wave lay study file science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
