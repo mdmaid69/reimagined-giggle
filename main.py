8b4d08aa4bbd098efe239037ca921b0bd74de1da
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:43:46 +0000

    platform acceptable issue field juice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
