9622a11b63775ed0739e65cd37a3133cc06630d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:12:51 +0000

    supply surprise force moral month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
