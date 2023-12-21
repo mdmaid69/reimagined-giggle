84f3706e7eefd406b34140b1570dcd774f8fa08d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:01:21 +0000

    terrible report bother century high

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
