8cab1a3788cca6fdf3e010ad7400218a25b48024
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:40:12 +0000

    Bb custom boil equal tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
