a0b7d6f9f038f9ecbf78c028e3d0a3db41e006e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:04 +0000

    estimate prepare separate book even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
