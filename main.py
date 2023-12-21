6fd96ea26228f17f18e542c15598557d797bf425
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:44 +0000

    avoid row range price year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
