2397e93cf8b489bb1a5d8ae10105c1080d94b0df
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:27 +0000

    win deep suffer order development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def subtract_numbers(x, y):
+        return x - y
