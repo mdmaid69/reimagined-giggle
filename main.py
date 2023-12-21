bddc31bd949ca31a4d6235a058c447dc71fc5231
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:01 +0000

    remark keepfrom attract exaggerate listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
