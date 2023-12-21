65e4583d3c65ff6729c34d5950010773d565bf37
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:47 +0000

    condition miss desperate about central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
