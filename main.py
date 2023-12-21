1fb67a8847f77c5dc733933300f60304f497dccf
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:39 +0000

    tread treatment century be extra

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_itemsize(array):
+        return array.itemsize
