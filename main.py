56e64c7eb7d2760fd48422dbf7614dde2aed7a78
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:27:31 +0000

    early inside I interrupt clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
