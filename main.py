1dd0a8a0c4f40cb006a49f49b30128ec2db0b3a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:38 +0000

    concerned bored public sound knowledge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_queue():
+        return collections.deque()
+import collections
+def create_user_string():
+        return collections.UserString()
