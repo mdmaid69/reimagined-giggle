99e767e5d3e6d76f250bdb1ea522c65a92280b64
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:32 +0000

    challenge lose shelter price lookup

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
