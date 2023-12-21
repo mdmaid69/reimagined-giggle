d602d3e676bc8f9022e32236bcbab00dc07f1464
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:50 +0000

    row grateful develop talkoutof once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
