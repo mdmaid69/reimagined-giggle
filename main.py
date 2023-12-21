d9315836a30f55dd63a9c24cf584b4d7e5e0b9be
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:50:20 +0000

    everyone minority protection guess bend

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
