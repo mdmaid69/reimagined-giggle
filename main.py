ca8ce7d3ba699891e6a51874d6bab11568c8e0dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:43 +0000

    and equal recover excuse dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
