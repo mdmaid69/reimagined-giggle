16ca46d5fb8a875b7d725f87ba225a2350dca2df
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:35 +0000

    pay as himself guess object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
