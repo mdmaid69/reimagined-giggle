59d27d4f3dd822dcf66f375945bdbee53bdb9276
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:57 +0000

    friendly pity exploration centimetre turnover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
