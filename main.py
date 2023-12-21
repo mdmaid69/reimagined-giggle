a196e968937861de0e878f4f3bce132104a7163f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:36:32 +0000

    drive they continue goon become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
