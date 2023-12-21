62a30697be5a5fd6e131b28da85d5d279dabe396
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:33 +0000

    deep turnon aim profit little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
