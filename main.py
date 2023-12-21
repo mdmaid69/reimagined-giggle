d712ab21160821f02b4e7e597913950888e60229
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:35 +0000

    makeinto advice work recommend professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_length(array):
+        return len(array)
