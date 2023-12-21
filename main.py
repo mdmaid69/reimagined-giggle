4870b131279976f59aacb4a1428b4d054cf0457f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:08 +0000

    house mixture on 567enemy fail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def convert_to_hex(n):
+        return hex(n)
