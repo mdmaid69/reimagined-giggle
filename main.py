dcb2aa21b3d5712648ef18631395e09c60b92e8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:09:40 +0000

    realize consider rely divide chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
