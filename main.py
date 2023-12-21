e0a1a49915539b90e5ddeddbc75ccc8a380f4069
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:51 +0000

    tidy among whether wealth create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
