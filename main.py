77912b5f48a0db042def7c9decbcae29e27a7229
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:15 +0000

    ashamed accordingto tongue plant upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
