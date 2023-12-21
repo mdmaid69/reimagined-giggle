4bd008bc604684f8e644f5e06ac5fbe2be0ca2d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:45 +0000

    plenty smell door goodbye matter

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
