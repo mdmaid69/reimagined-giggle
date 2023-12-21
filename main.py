afd9f7a828641a99c28f6dba888ec4667992fb38
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:45 +0000

    sit do suggestion across obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
