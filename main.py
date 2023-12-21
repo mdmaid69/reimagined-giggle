9cbc01068969c529f683d4092fc232b20e2b4354
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:21 +0000

    now line pollution preparation wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
