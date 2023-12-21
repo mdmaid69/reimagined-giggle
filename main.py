2475a5ab0d684a8d5e2e4be53c7a624b0d0ee36e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:21 +0000

    complain rather successful ashamed survive

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
