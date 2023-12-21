c7c38991671e3582ebbcbacd5212fa4d51da4f23
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:06:06 +0000

    profit grow funny any wait

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
