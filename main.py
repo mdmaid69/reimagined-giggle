1660cda08ea0a94886222bab6d050f67197b5ec8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:52 +0000

    distance rank short bite nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def count_words(sentence):
+        return len(sentence.split())
