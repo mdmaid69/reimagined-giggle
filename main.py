40033b54fcc21678e695217006f9d8e32594d9f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:53:32 +0000

    relative learn offend doctor finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
