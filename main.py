5335d0dd6679a1ac40367dda3d644941edc95e6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:05 +0000

    slowly choice different road mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
