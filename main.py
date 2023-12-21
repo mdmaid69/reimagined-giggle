0b653b2ac7b6f99e83071faaa3d558baffb83c27
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:00 +0000

    nearly honest claim grade involve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
