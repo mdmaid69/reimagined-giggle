54ceb22672c402dc3604006a8738ee9cc7bdf1ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:31 +0000

    Ww mouth inquire plain comeback

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
