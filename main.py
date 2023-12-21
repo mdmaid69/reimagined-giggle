99d47b4dd6f9172e00ab50e1a396749370a9a5ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:24:24 +0000

    aim turnon exploration do shine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
