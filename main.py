918f2e9aa4546bf235670e42ee28fd660604a000
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:13 +0000

    behave new ago peace positive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
