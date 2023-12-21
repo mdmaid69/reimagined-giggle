8648797f3c283ad90ed07619ba1c49ecb1178f32
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:28 +0000

    habit commit instead lookback(on) peaceful

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
