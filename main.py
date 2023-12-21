43d7ec2f934f5ff57d37926126d37841841b62f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:31:39 +0000

    escape promise breakup hope star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
