b59b4144b0dee5a8a8b1ef3903568d22cdf35e20
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:22 +0000

    burn line challenge yourself party

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
