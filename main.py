9cdf7b26047eeeb2e37722d4d812e806709ca71f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:52 +0000

    bury solve shock expensive 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
