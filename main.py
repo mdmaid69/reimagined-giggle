16e0d9386c3fc38eb3cb044299ab47a7d969b2f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:51 +0000

    shadow ask swim tax radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_even(n):
+def is_even(n):
         return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
