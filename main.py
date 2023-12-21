8d72323bcbb319a383965d0b3f08377ca5c41cbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:15 +0000

    block must proper possibly level

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_power(work, time):
+        return work / time
