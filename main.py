335bf12f7a0c326b01bf757b367ad24cffcdc623
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:14 +0000

    charge introduction search break cinema

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
