b3adf66ec093f9c29cf5af1abbb4171933550a1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:36:01 +0000

    expression eye public either under

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
