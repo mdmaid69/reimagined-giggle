394b222c818a721907dd09209b0a47e4ced2c680
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:14 +0000

    art different Rr thinkover number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sys
+print(sys.version)
