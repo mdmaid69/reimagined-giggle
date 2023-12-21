5d02ed84e9241e7677ef259235a187456587839c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:13 +0000

    hit wonder permission challenge study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
