8786e895cb68a4e6b8e46b05e30a079b1735bd9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:01 +0000

    573entertainment legal zero ago not

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+name = "Python"
+print("Hello,", name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
