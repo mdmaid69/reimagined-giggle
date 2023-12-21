3d70a6b129dfe40ca2da5777add96c82d40e352a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:05:23 +0000

    call exercise real wait sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
