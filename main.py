07ed6ad4c86ae58a47b360c8ee00a8e8afd90dba
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:05 +0000

    position visit hair centimetre up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
