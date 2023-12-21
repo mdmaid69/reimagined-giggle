13eef599d55200a5e56ad5b5d589d22aa17e0600
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:59:28 +0000

    where runaway question produce limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
