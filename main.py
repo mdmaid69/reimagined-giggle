ee5fa415fa109fe26713f18ef02f9a024809396e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:16 +0000

    bag float than last arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
