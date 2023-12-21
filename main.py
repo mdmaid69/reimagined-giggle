de18dd0b3c6655590991911e57b5f326fe16634f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:56:22 +0000

    notice pain repair congratulate cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
