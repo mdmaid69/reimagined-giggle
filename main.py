33679eed401a2ecc171f04673205706250152c8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:22:36 +0000

    love 569enjoy need male ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import logging
+def log_message(message):
+        logging.info(message)
+  def convert_to_binary(n):
+        return bin(n)
