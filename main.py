f84366e6fda2261c4e658793f369a6b7a7254549
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:36 +0000

    past pleased choose minimum absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
