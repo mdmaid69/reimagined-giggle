eeee727494a4cc508e46fada01c5a20afbc6271c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:07 +0000

    fold rest suggestion can keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
