304f2091010326f5be2de2918a6b74f6c81de429
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:33:56 +0000

    improvement slip downstairs brush quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
