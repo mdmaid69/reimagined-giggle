5c8566037efbae0bbae3797ee7ea7895478f5ab3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:04 +0000

    perform prize married 569enjoy regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def is_odd(n):
+        return n % 2 != 0
