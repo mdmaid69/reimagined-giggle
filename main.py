6280db0320db72e8aee1c6a6401ea12c1d02181f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:52 +0000

    among shade term base distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def add_numbers(a, b):
+        return a + b
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
