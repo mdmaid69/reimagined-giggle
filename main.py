34b8bc1e7aa7ec7dc77c82cbda183a1cc6f57464
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:14:05 +0000

    attention compete sudden region grand

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
