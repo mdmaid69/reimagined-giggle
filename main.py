3f6bd8c9f895db9686e741e9d1a464f195e88915
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:04 +0000

    arrival fun explain change design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
