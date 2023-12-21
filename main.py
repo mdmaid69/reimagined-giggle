0c2b20123a46978ced1e4c7e8206d28d1c8926ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:49 +0000

    string strong square importance secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
