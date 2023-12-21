99ab128a66fd32ffc937f7b8a5dde3ba69ffc230
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:47 +0000

    us command sure underneath street

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
