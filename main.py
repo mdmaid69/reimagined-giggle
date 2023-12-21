f8f565c36f5cbe3dcef26035af286061d4a93caa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:30 +0000

    opinion majority card slice wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+print(math.pi)
+for i in range(5):
+        print(i)
