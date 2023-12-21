8c095fadbe2fb3bc6346dc8e2f41e99acf60e4df
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:41 +0000

    your write hit week reject

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
