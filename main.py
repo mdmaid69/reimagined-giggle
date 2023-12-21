db4481c4018b54d0cd4e66a4f2041b93b2bc44db
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:57:53 +0000

    handle past think fall grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
