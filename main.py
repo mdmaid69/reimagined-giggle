d98a12f807ce6a9dcc24b214348e59867cec8472
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:35 +0000

    minute like above Ll future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
