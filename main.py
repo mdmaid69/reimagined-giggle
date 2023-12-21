fe8caa444613be8bc095be685a5547da372eeacd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:23 +0000

    even also pleased unlucky emphasize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
