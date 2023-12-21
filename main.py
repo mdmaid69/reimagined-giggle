6ff7c03a5f96f78576e29d0f634660f3db19e30d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:28 +0000

    patience welcome poor hers guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
