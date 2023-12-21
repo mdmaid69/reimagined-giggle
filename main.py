2975db0e5d86de9dd594a6e690e1c08af0b6ccfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:51 +0000

    congratulate lost appeal tap always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
