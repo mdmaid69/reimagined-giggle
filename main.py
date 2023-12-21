4257ffb5dd331973d901d520346ed72261f63b9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:31 +0000

    guess rely comein desperate neck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
