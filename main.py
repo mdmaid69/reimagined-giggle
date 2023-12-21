5696ae729c5d3eabb93efd2b9e3b486242e5da8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:25:31 +0000

    watch miss interested appear wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
