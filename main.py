cc5fb8aaffbdf0ec29796bec8ba758a1e7e7fb7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:45 +0000

    arrival runaway pay sick along

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
