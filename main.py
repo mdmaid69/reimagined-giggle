bcdc32ba69acf5ef07c67cafe087d5ed1376cce9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:12:10 +0000

    worst last with group jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
