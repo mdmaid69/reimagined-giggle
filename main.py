e04fe68fc458c13d0f000ea84fe1c902e7f33ed6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:18:06 +0000

    appearance dish mail improve 572enter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
