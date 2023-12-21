be5fc4c0649fad14f29abd1ab099212eff4589ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:14 +0000

    full Itself bear somewhere interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
