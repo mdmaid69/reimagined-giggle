51c93ec60b3fe1a41ffad31a2446b7be563e8116
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:48:20 +0000

    Ee ours last occasion backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
