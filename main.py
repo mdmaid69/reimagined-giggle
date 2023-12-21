fa66e4e4015523387ce1220b6895cdd3bafbe936
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:36 +0000

    giveup read beside round forward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
