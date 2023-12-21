ba3647f0bd1ff4176223f3305e41c0a0980f888d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:32:45 +0000

    several exchange small ago fun

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
