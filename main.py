52635df14389b070993857d0e51721c04c20d0c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:49 +0000

    Yy closed difficult touch man

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
