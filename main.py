a9570924379a490889f16e87e65ce2d25fdf7fd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:58:15 +0000

    price keen practise shall ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
