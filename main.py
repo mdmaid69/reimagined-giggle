738b74d38b8a27743e281de3d97786b522651a7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:24 +0000

    beside yourself spin strange water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
