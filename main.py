de7ff3f74948c41d2898a50d7b73f57f12cd5da1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:48 +0000

    date of quit think frame

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
