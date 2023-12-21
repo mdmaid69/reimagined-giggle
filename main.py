34f486bf243f9e0118792797cd8fa86c76c9c009
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:24 +0000

    pretty turnoff ride judge work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
