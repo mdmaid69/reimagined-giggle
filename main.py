219bbc0d4a05ed7afdc0c492decbbc1d841d2081
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:24 +0000

    disappear ticket total rely blue

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
