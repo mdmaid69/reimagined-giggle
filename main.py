0bcb95e3a70aa41442d11638e161623f513091cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:56 +0000

    once warning professional it stiff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
