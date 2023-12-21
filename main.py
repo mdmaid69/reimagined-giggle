388308aa07352e1723b3afc276e98aa765cd9787
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:12:14 +0000

    difference style dear to Dd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
