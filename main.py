31b80753c44df62e013b9569b0ac3dcffb740948
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:46:03 +0000

    every early book steep bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
