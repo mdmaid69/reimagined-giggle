9be6f7d47dccb21ece4f4b3ee0530f3f12cba34a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:05 +0000

    mass contribute shrink whoever must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
