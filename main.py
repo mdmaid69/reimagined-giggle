ccf35aa519b7217299b638a89f852e22bffa61b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:56 +0000

    square lie what second classroom

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
