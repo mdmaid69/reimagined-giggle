97711fe37359d249b74fd45f70f0f9019e2b5de0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:13 +0000

    warn obvious convenient familiar might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
