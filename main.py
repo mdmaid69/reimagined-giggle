b8487b928232eac94953bd954ad04c1a295ee23e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:21 +0000

    fierce extra ring study second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_error_function(x):
-        return math.erf(x)
