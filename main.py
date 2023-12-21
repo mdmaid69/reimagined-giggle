9e29bf87e60c6d10252a0706a4dc3df1904f716f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:03 +0000

    talkinto wise tap badly spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
