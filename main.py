5dcd48f76f90d7cee64ff0061f5a806c1462be43
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:48 +0000

    note trouble warn hope grateful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
