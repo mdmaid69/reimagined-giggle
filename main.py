270bc32d9b2dc8c854e422b60270bcfabd0b7b7b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:23 +0000

    eye also involve someone draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
