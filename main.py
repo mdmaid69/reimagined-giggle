5e14cbdc103fb32312ce9bd5fb99ebe98e0e0533
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:26:26 +0000

    priority wash peace door suggestion

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
