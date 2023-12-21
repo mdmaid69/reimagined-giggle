e9dbb6e3804f62a791fc49b2296537247e2b9268
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:56 +0000

    be music 573entertainment later instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
