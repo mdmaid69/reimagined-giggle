edcbf2da1dbc54e9d9b7ab69ef8df21a4773b95b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:01:33 +0000

    hand unkind do murder evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
