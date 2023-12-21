c6806f1f20ee473eeab84d6f9dde8a82cfb21d3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:48 +0000

    warn deaf habit news catch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-print([x**2 for x in range(10)])
+def extend_array(array, iterable):
+        array.extend(iterable)
