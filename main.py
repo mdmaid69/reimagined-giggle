47584bc3ac3324a52c211edae14acc2bf964473c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:30 +0000

    fair quiet flight dead left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
