ccbf12a0df8bc8ba603d37be60ca1c6499a2f1a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:46 +0000

    moment custom message knock odd

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
