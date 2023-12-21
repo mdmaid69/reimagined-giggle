667e80c989a516f5a65e04e2c10d2925faed6552
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:20 +0000

    float inform secret pleasure tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
