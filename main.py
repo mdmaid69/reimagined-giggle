091214c0bb3b0ade4a7c93cd54398d96a60b9cf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:22 +0000

    translate narrow stupid ashamed by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
