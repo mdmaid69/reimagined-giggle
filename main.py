25bb5c766b166b3c6ee4b814aaecbb92c1238071
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:19 +0000

    consist hurt war related tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
