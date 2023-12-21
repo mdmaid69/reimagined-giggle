6cd4263b0328dbf962ffdef492c6cb99be559b43
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:12:56 +0000

    correct give Rr sensible rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
