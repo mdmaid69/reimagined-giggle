d823ddfd27d65ce0bd28da29c443791beecad5ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:21 +0000

    across satisfactory pretend threaten account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_floor(x):
+        return math.floor(x)
