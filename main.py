717f53d5e777e1fa273966c841778c4a63fc3f28
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:58:08 +0000

    pupil let talkinto makeup forget

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
