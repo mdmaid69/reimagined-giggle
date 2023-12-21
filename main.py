56965659a2f3941f415c9884ae39293514197c96
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:42 +0000

    secret suddenly cost cost feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
