40e40cfef82826969b218e3a6191e0d842020f63
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:54 +0000

    preparation else my record market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
