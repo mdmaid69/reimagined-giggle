625a0a3cb3c899d1dc0cdcc25d22b37b17d9715f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:12 +0000

    reserve into herself emergency team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
