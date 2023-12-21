44b005415339770fc5735b2df391de848514cfe1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:50 +0000

    afterwards suckme herself hole conversation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_stack():
-        return collections.deque()
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
