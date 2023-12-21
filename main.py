57edaf9a8d0a99a40f8307945f41c7ae95af288b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:16 +0000

    half how vision Ee loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
