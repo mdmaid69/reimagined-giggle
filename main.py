7c8cd1c71164269c9d4e435a654bf66f8266a99b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:50 +0000

    this race walk carry claim

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
