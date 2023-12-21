88d3ce439fe971ff0d31218ee206f800dd33d2ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:13 +0000

    lock smile uponon trust dead

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
