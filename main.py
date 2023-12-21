fb691815f679d4cc4ab03456003760e9e7cf7778
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:11 +0000

    strict Jj disagree translate traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
