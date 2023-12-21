3439c05c71c693f4bbf5b260c154ce3caebeb0a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:05 +0000

    borrow height innocent pipe suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
