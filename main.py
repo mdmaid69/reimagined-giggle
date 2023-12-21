0576b6ab924294873527bc30360074061c75e6d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:05 +0000

    until now physical confident even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_density(mass, volume):
+        return mass / volume
