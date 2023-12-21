e61276a9b8f6d565551c179eae18cbab558932fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:03 +0000

    wake imagination deep lookafter address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
