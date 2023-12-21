f37cd929571b8b6443ff481244ac772934c9f36d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:11:19 +0000

    play however acceptable myself goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
