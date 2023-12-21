6bedfab5d567d730dd54acab62e47b0a75d92eb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:54 +0000

    contact obtain poor modern bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
