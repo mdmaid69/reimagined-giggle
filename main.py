48bfa96323bd7325c328f0f3f2ffde599015aadb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:19 +0000

    opponent reliable beside try pleasant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
