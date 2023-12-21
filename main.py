9ab2cd6a350743c5352444070f74d750390ca80c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:37 +0000

    water heat check bye pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
