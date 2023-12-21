f59c7b19550fd62b4fb5fb959c31f097515a9710
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:59 +0000

    getaway concentrate needle surface rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
