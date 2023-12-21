142ce55570f332d4b8421675cf13cabfce16f333
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:23 +0000

    pen face chase square bush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
