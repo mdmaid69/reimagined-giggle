e8ea4966a482e05b4c745ebeba7746fdcefbf33d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:38 +0000

    picture still activity correction none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def square_number(x):
+        return x**2
