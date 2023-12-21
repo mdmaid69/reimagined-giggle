47fe2dfd172995ada78683e9c875e8d1a60fd3d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:56 +0000

    well present its attractive rich

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
