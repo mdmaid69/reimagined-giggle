da732dd64fa222164ea53797a6ec2d2f90cd2ee5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:32 +0000

    closely official war forward win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def convert_to_octal(n):
+        return oct(n)
