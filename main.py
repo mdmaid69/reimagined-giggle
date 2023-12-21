12e1bdaa85de8aa63b5dbc119a03f0dbbe6ce649
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:24 +0000

    not spill attractive gift instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
