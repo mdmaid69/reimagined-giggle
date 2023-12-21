686f33f73191f9a47c6d24c0f1f9a497854cec38
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:25:41 +0000

    ticket section lose can comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
