194e96b68649737f97e90191e946f11e83446e37
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:03 +0000

    object positive operate room whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
