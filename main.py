9463039cabc0c36df1c50314b2e81b0e85195a89
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:36:33 +0000

    operate tidy Kk much serious

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+from collections import Counter
+print(Counter("hello world"))
