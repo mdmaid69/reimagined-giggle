4c7ddca5681e7235166273d12c1e04feeded6852
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:13 +0000

    list involve extra crisis fail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
