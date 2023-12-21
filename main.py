a00b71ee97a8192ea9f9c56f9acb289df45120dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:25 +0000

    behave yet winter able not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
