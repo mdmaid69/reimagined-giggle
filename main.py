a13a8c52200ab2fcc9b733cb8e39710b2ace7f55
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:24 +0000

    want sorry attach workout sick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+print("Hello, world!")
