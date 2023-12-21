c5fbec024ac364a8daf76d9dd322312a9f628545
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:50 +0000

    coat hers interview dress move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
