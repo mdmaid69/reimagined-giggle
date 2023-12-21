0a7de3dcd6a59936d92514f80a8fd5cbf857e2ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:49 +0000

    term trap charge birth jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
