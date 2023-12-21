93116c6694b952b5fe06ceffd70c73d30925431e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:59 +0000

    sweet wonder leg feature bottom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
