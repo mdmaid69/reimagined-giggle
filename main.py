c69724e4d2e8beade6651c7c0aeac0331e6df202
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:41 +0000

    art shoulder wet there sound

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
