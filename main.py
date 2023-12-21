31eda5ba618084242b2169d753af00b37c329456
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:59:10 +0000

    fire her embarrassing flat nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
