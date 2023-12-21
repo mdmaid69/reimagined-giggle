0fa219db3842130181cc937b2ddc8c1bc1a0077d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:21 +0000

    base sister blow Gg lift

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
