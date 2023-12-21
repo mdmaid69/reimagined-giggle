3faa65355c07bd3ca44cc9096f6bbff660a7565a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:53:13 +0000

    fat partner description another oil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-print(math.pi)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def exit_program():
+        sys.exit()
