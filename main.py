c0d8d1b64234f2d3393a6019dbb95b092adee140
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:55 +0000

    high collect rush connect black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
