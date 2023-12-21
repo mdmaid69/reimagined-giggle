4e5f9e8760b30d01404f71c1224c692ecbc8b5ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:40:21 +0000

    sure birth unlikely regard touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
