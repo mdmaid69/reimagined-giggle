7f12bf8b4b8e179b7b95ab00b1b8836fe06379ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:14 +0000

    near century fetch such godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
