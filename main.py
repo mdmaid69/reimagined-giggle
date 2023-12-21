939c2ebb6a63a73168df05d0a00bac014853adaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:57:39 +0000

    ear light feeling round pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
