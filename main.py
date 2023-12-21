5a01eca96a0849ac9c82ad731ab466269da99a47
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:02 +0000

    rain contain shape pronunciation sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
