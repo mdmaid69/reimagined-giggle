beb141ae705f55c6e34764a59385c33b43513d20
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:57 +0000

    anywhere extremely go future news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
