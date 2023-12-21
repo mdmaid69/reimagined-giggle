d6d34027ed915b5a07a73442e1580a8b2c33132e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:17 +0000

    fresh interruption pot section young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
