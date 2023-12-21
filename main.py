641668d7c3912cc7b04c27f0cdda8a4059572add
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:25 +0000

    ride finger evidence room high

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
