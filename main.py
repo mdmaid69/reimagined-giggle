10b71a2d31de19f43aeeaf53d9221ca87706f37e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:09 +0000

    opinion fair skirt suffer sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
