bfe6fe942ed80736934b6dd0c0ff9e1acc8870ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:21 +0000

    book authority circumstance turnoff interview

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
