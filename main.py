cd3aa219a77e3e6049f6523dd6ed92a06a7f73db
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:54:50 +0000

    better land spot reply anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
