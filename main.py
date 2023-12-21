1f0e0899ef4541f2c4bebe2674a2a08f176ea42d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:15:25 +0000

    passage unlucky direct equal fence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
