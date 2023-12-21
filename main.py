4d2b07b9236df5f13fb777217262dba25e6e86cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:49 +0000

    sick give makeup complete challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
