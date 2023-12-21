c1c620b6f6f3e3dbedce2386b51467d149355200
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:48 +0000

    active factory minority tired correction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
