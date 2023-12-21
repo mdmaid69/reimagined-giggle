869c609031bc4fdccf7c2c81509016a9cac8db33
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:22 +0000

    measure clue stir travel immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
