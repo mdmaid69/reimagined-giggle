c89b9bfb8a06ad7766de8415686787362eabcfd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:36 +0000

    naturally dark lock use various

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def reverse_list(lst):
+        return lst[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
