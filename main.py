4db1bfa3a10a68f1031dd631e1daf353c9c23ab9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:47 +0000

    instant period trust move getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
