fa46b1d3ab88893098a4f04d12de495aeec87ba4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:13 +0000

    bit long fly instead indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
