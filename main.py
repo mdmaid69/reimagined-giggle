68af356192c041946a0367106b7f6470bc67fd24
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:04:47 +0000

    behave play tune that part

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
