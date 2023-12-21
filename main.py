98ad8d932bc311ab65972db3d7b5d8a7c68e418f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:35 +0000

    concerning clear fail shut minimum

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def cube_number(x):
-        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
