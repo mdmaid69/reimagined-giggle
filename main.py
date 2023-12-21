3a397a10b8093e67dd33ec07a936a7ff2bf93c3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:56 +0000

    prepare chief build moral phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
