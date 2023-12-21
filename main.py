3e4e71e489c25e93fc50e9e6b0799f74e0ba995a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:05:55 +0000

    recommend late part dear dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
