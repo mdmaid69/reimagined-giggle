5d1d55eff22a1b642d5ec12b323a2b4ac56bc5bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:35 +0000

    pause road pronunciation suck remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
