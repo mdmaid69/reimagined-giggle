f730b8dc20e0f8fb44a0ff41f5ebad04929e2a26
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:20:59 +0000

    she everywhere slow father regularly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_area(radius):
+        return 3.14 * radius * radius
