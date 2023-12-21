f5ce8d33c284c8810e679127d9ce7d56dd0d27b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:03 +0000

    side separate rubbish giveaway present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
