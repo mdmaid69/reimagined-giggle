db8184c2cb10b9b1e081af4e967760348c53cb39
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:07 +0000

    complicated call loose market hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_area(radius):
+        return 3.14 * radius * radius
