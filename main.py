1a50ff2f43404d6c5f4dc3563f31169c19f2416b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:35 +0000

    runinto unable otherwise muscle shopping

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
