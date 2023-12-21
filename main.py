9d8a75e5003301b5b19509309eaef39af2124e13
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:11 +0000

    exhibition tone mystery steep learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import getpass
+def get_username():
+        return getpass.getuser()
