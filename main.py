c12d5c268317ed8bdbf08239457ce19fa2b2a9aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:50:27 +0000

    take pool always immediately doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
