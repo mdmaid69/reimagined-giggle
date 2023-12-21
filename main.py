e0085a426cc14f69d09db2fa8942314cc27eabc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:20 +0000

    takeover service hour sail unusual

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
