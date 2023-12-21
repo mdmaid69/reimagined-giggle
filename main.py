97e49042f4f770844e95e31ae115ffd45a3dfa65
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:27 +0000

    meeting do excited on push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
