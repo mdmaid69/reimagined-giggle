e1db7b9a8e6fa3bb0018c95924795e90ce1b34e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:50 +0000

    really ball traffic real adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
