adf4749d944f9760c4f8b8fc96cf502727222212
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:28 +0000

    stupid both why great agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_force(mass, acceleration):
+        return mass * acceleration
