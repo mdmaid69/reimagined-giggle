ced9565e61ab183301cc412ca328c1c63bafebfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:59 +0000

    yet correct need past thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def remove_duplicates(lst):
+        return list(set(lst))
