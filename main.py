c4f6ccd5ea6bd6ec6e4a37fd5dae8a9bef184b96
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:05 +0000

    swallow poison put extremely over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
