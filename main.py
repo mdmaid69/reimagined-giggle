5033efc0ce2c56093ab0e195f7d8ad79a350b46f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:38:29 +0000

    immediately hollow authority any sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_speed(distance, time):
+        return distance / time
