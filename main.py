45deebfd6de27797fa4bbc758f9ada7ef61b71da
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:39:52 +0000

    step prepare powerful broad much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
