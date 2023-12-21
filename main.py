c89b1ddda5b09d6afa274600d3a1e6bb7771fcf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:13 +0000

    wipe mark thumb occur family

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
