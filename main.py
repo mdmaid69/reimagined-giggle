36890c521d4c478cc683f7a1ad78447d490cdec1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:35 +0000

    flow within welcome brush scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def print_python_version():
+        return sys.version
