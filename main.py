32fd7a1f34e1ab9e6be3174f4d8594d11011fce9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:46 +0000

    night instant left last Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
