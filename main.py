10aa1b01a0ce84faf3eb2311d0af09ecece6507c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:37 +0000

    bring match burn convince recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
