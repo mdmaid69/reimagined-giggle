7315d72c3c5b7d8641b731c62468c5e0b4c33870
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:18 +0000

    meal cap compare right float

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
