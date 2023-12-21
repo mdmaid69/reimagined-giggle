3026c95be11b49cd09f08434e3959eb884c791d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:37 +0000

    into generous copy protest equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_complex(array):
+        return complex(array[0])
