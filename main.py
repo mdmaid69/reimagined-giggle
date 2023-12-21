f759fa62f922663e6d372ec997c294f643e8ea00
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:04:18 +0000

    cause touch take splash better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_volume(length, width, height):
+        return length * width * height
