9e87a24fd8d3f9b1aa8b6d3520245530c3d86624
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:18 +0000

    several run land exhibition form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_hex(n):
+        return hex(n)
