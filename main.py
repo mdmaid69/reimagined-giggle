f6b07df65f8c948df9d900b1890d0769d85af676
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:15 +0000

    realize pause arrive time shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import sys
-print(sys.version)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
