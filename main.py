bf673e9a9348abc4a71667ea7feee1e7992cb682
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:43 +0000

    not hollow hear language enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
