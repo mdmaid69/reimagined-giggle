9882d202dc4148e969a6093f768cb0c867a2d496
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:41:08 +0000

    press somehow present tape true

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
