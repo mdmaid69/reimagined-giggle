a3a594a8ce9db0ceacf69ac70412b15e05b616f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:31 +0000

    day land container lonely entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def get_time_since_epoch():
+        return time.time()
+def convert_to_octal(n):
+        return oct(n)
