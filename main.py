0dc82521a4ad2a7b468df96c47da4ffbe744a9e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:25 +0000

    right switch dress knock priority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
