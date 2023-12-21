f1f44a5d3f84b0ed2174f1d41bd870275d1ce8b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:11 +0000

    hospital peace artificial argue heat

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
+  def convert_to_binary(n):
+        return bin(n)
