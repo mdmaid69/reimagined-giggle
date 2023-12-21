98f9ab39e3e2f9d88e224762dd40dbc303a3913b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:08 +0000

    new telephone and final available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_force(mass, acceleration):
+        return mass * acceleration
