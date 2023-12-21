8bbd10406081b3c3bf5e1acf8523e72ddf3e296a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:17:50 +0000

    during performance hair father come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
