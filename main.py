ea04502e83860f1482da04313db155937a9c20fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:16 +0000

    workout thought spread plan branch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
