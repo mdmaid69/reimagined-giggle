4aca0bed935825d907a1f7609bd95bfa33c177ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:54 +0000

    object priority satisfied proof dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
