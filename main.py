1998c9f54931bb1bc7180b0ae97af71003b7b541
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:22 +0000

    giveback bringup everywhere cinema smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
