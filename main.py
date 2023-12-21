0477c9d17521dc71eeceb32e903d68975b453d75
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:01:48 +0000

    beside about broad protect female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
