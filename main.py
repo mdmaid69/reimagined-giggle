d8985a38f1c82df160f43053d6745f5f403b774e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:04 +0000

    curious long disapprove promise serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Average:", sum(numbers) / len(numbers))
