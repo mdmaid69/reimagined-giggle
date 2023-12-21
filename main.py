15d01c0244eb7973a8cd06db0a3db62c442f75de
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:16 +0000

    right experiment unit welcome corner

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
