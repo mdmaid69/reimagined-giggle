9a7f2b3062e274c6efc8ee77f678028d36ea4f41
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:34 +0000

    mistake many impossible explain able

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
