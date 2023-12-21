593d14f7a363c8525f34a3b992584d1c4192acd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:37 +0000

    fun arrive left contact software

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_power(work, time):
+        return work / time
+def count_characters(sentence):
+        return len(sentence)
