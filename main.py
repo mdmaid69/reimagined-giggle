63693e13348946d4db98ab4d88220cb794c6f5b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:27 +0000

    describe listenfor motion loose mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_odd(n):
-        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
