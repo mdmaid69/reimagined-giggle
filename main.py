a3d792f28e418820ba32cf5239d7f224df8ffad3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:19 +0000

    deserve wish connection crisis middle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
