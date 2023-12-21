3e6b686de1c8abb54147efba45019472fa8c418e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:34:15 +0000

    job wake out while everyone

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def add_numbers(a, b):
+        return a + b
