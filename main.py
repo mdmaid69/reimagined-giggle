036fe8fd646ca393f43615923819fd503d430959
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:16:33 +0000

    left stick impression eye terrible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
