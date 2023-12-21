2b38a2366a3f472b73a8cde49bdd5835a58aaee7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:39 +0000

    distant from home certainly put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
