4c5c494cf30d8851e4753b19d0355a756fae8629
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:48:03 +0000

    why properly majority whoever recommend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
