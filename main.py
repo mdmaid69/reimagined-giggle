7b4f991cab637bd8085f30c6459a8073303145e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:30 +0000

    effect shelter giveup funny future

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def reverse_string(s):
+        return s[::-1]
+def calculate_pressure(force, area):
+        return force / area
