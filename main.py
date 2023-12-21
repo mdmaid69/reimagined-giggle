763eb6a2dfe34a9adbd03a97469a047746a8f6d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:18 +0000

    blame sort many furniture square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
