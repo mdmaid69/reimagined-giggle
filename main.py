a727dd468cf5ae5018449e0a2d4cff644a146361
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:23:23 +0000

    deep scrape reason replace moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
