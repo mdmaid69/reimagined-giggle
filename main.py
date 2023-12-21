d5cfe9ef2da2e6974807884d10ee1cccbdc4033e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:24 +0000

    pile bang breakup summer apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_as_int(array):
+        return int(array[0])
