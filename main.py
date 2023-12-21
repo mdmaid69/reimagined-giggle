5415a99ed3e5495341313a4ad9848e0a046e1af9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:02 +0000

    club shake choose lead turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
