332481e649f54cbdf0f217eb355d87b52dee5f77
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:14:11 +0000

    speech reaction person whole fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
