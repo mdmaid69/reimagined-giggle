9f2a3aca44ec10042c04d83f733ba98ef08ba58b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:05 +0000

    heat increase fat occasional mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def get_list_from_array(array):
+        return array.tolist()
