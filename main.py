ab9b2b6d688b0cb572fb29c29b4cafb32de1b5ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:50 +0000

    after statement exchange bus people

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
