bbbbd2f6b34fd0c71953fdee646948265435277f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:18 +0000

    turnoff shine let enthusiastic broad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_elements(lst):
+        return len(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
