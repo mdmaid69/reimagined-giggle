de65adced530ab0694932eebafe824758d666536
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:57:11 +0000

    wish turnout insist break thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
