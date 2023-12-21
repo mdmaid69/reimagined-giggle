fd20e113140ff347433fe4f0a665f45c5f7f097b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:39 +0000

    reaction regard demand description advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
