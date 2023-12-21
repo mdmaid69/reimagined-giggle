f45a2261ce0508e5987134cefd15245813a53d26
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:16:53 +0000

    face whenever concentrate lucky body

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
