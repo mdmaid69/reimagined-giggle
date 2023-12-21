ed943bfac065850e493e0a0212123b3e84d7b9b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:10:10 +0000

    frequent appearance programme wash burst

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
