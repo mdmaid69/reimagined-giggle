4eacd2ffb18d744b6b94d2eb52d9bd36ac2b52f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:40:24 +0000

    flight night suit press compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
