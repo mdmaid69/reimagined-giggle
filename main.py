9add467b8f0b3f9dc36218fe9cc2b4d85dd33ef7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:53:51 +0000

    creature wheel zero negative comfortable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
