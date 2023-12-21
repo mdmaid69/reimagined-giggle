10f2eaa49109b724a1956bd57304d6ac7ad65479
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:36:14 +0000

    sea explain breakin amount matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
