4c9471fba010ff28a93bdc0cca470f3ef44170b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:05:41 +0000

    chase world lock pump takeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
