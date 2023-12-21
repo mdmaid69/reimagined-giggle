0a66095c8d43af55d5879a658fb911567d67439d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:19 +0000

    greeting only waste I wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
