53736aac3183420212f8dc2721fae4870b10a78b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:53 +0000

    day car complain manner uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
