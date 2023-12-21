93b9bf77277830e836eaf3287e8d17b0ccb2373d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:32 +0000

    will dress square lookafter handle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+for i in range(10): print(i)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
