5d775fe39fe0f0a9a62e9c5ba3bc04f1a062f299
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:06 +0000

    block left maintain important fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
