86827f0b1634a1417947a4ddbc9bb8e31c78b8ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:42 +0000

    positive disapprove influence all actually

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
