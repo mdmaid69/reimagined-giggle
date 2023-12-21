bba01fbbfac2a17da0ce74cbc0b9f5fd688a4b8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:55 +0000

    team geton unkind crisis estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
