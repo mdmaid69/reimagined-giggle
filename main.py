81a02cdd097742c64cc133f1912189d23708ec73
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:24 +0000

    conversation fill by aim clear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_power(work, time):
+        return work / time
