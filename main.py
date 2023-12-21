cc88d7b448975567ab949b35d1c48e59dc016b29
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:48 +0000

    represent clearly talkinto punish carry

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
