cdb07480c4e90437718a312f4ea34bb6365c01a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:52 +0000

    level basically joke breakup away

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
