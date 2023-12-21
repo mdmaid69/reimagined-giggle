905b23b3ae363e0a5f26b686f21dda816fbd476c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:39 +0000

    simple path usual unlikely gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
