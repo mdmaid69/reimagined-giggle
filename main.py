e6845d8950b85e719a959347d8dbcf5f5e0591d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:47 +0000

    recent bored figure medium unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
