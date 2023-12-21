7a58235f066bada24a5a9a6df869b415b055b6a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:52 +0000

    relax branch someone centimetre discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
