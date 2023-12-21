153b990f58567de53b3957f2467175104473fe7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:07 +0000

    as season except shop hurry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
