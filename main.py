2920b732f514502d7cfd177d75fd48c0762fc22d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:14 +0000

    on exit base secret chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
