982cc3aae790b8f5d67a81df0877309231aac51d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:22:44 +0000

    warn influence house sport pride

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
