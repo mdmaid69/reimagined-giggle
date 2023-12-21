689ed8f311333cc8e051c2514343998b213b9a0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:18 +0000

    stand regret control neither evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
