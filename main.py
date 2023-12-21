8deb80793258d52c7692adb08f1712e59df72c5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:54 +0000

    improvement favourite concerned contain slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-for i in range(5):
-        print(i)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
