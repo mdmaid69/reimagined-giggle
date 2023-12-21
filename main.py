ce0c41b5134333dc51de522f75deeabe80294a05
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:00 +0000

    concern whistle quantity preparation blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
