e5c7037f3bdbc49c16d4e0273bd9f5c4bc19e1b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:03 +0000

    claim during trap care by

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
