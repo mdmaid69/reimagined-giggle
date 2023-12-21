3d5ecbe15e2e29135dc68ef0fe9fae4883523aec
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:56 +0000

    forget talkback of female possession

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def square_number(x):
+        return x**2
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
