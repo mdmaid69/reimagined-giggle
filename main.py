a61d5cfb9fa2cb3c4820780c4ab0df54e081ed8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:42 +0000

    situation radio instead include fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
