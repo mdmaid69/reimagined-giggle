2d5acdb3b19d57393bb18a5e7924382b6205284a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:59:00 +0000

    interesting bound pronunciation speak whatever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
