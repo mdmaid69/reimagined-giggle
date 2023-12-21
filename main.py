9fa94ef27c8a117c8b2707b9ea485c96ad5ae3a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:17:07 +0000

    freedom perhaps apart bottom business

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_unique_words(sentence):
+        return set(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
