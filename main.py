b4b72f9b4615cb8f5ff0d8be5d5666f022dbf26a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:17 +0000

    prepare involve tense cope furniture

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
