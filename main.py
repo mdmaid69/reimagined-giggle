ded245a266a8bb77b181262cfa6c313db4b70154
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:02 +0000

    another progress play swim fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
