543df74c57df082b5d79681e6a787bb35b61cec2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:07 +0000

    answer argue special look exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
