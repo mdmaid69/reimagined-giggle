231860128ffa040cf29b4e0c0fe87cb4f0f7f776
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:02 +0000

    news form correct scream sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
