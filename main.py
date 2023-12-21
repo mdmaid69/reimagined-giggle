ef27225aa9554fe3db9d8e29f3c797b13621ed9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:25 +0000

    journey uponon spoil second hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
