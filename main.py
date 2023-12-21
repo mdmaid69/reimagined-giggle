9768daef478edd98c99308e0f434da6bbb847f50
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:14 +0000

    their spend nature yourself raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
