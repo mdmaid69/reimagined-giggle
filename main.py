2bae0b5748c5c19241645f3a1a80d15e30fff2d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:20 +0000

    conversation light meet stir get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
