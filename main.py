7c8f1451d3160c7c4127ca05793b9e42eb4d629f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:49 +0000

    fault scare next television degree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def square_number(x):
+        return x**2
