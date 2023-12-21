dae2b6726d12700d04b23eb41b08c44ce5da9d12
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:22:17 +0000

    score suppose remain size satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
