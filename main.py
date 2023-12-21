1023622368c9a592fbd2850e88266ed936ecd910
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:05 +0000

    sale tall speed across fortunately

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_density(mass, volume):
-        return mass / volume
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
