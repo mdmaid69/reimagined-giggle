9c93adde0054a7ead02b799cd8bf777392641a5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:50 +0000

    report worse ourselves argument plenty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
