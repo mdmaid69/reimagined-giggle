2bcac4db77c496397eb8a73a1fe30848c3a8100b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:46:35 +0000

    later conclusion dress another runinto

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
