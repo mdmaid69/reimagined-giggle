68f1cd892479f32016cc7eb8febee17a119cbda2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:01 +0000

    greeting exist luck squeeze glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
