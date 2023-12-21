0901ab76cf322fa5fd546f0123aea5b942445431
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:42 +0000

    tread stretch pass value tour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
