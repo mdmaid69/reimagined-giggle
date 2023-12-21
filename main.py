3e6d60e52fd5bde3babc5931a4ef8d25c821e48a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:24:52 +0000

    pump goaway underneath deal total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
