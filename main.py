9f7aa4139e71100150739d0daf30570021d97c13
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:04:43 +0000

    by law Tt pause prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
