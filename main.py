bc3cb88d0ce2393e38f90b1fd8ad5e05f9b84da1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:48 +0000

    drive today or deliberately challenge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
