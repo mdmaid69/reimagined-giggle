25fb914377b1634849a46b85f7832829e3f431af
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:13:01 +0000

    flood final reduce trust common

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
