d29dffac576b0c230c2a05094d94200cf56caeac
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:45 +0000

    make scare excuse past exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_palindrome(s):
-        return s == s[::-1]
+def convert_array_to_string(array):
+        return array.tostring()
