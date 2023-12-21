c26af95abd677e9cc5823682ded8a71dc43c866f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:24 +0000

    even here permission conclusion home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
