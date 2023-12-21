ca816fd87ffde400f4f0c04ac08c3cb69cd3565d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:22:59 +0000

    Vv stamp offer store 573entertainment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def get_current_working_directory():
+        return os.getcwd()
