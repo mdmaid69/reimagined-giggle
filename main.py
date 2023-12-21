5d5cd14a76968e3f6e759c39f10532a5c1bd2bc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:57 +0000

    conduct noone one row sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
