258dcbddaa324fcec215ae2faf127eeb1c74642e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:32 +0000

    sweet arrange both fasten crash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
