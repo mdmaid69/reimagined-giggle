ed19e3bc923bb3a15712023fd26c6a04e298e971
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:40 +0000

    run communication instant concerned brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+def exit_program():
+        sys.exit()
