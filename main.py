b77b574b4cb8404c07eface0172327d519524eda
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:52 +0000

    should on simple turnoff tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
