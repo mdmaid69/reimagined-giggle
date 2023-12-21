06ff53f93f9ca22c777ee9c3fdeb2c311a59e140
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:53 +0000

    holdout raw successful section comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
