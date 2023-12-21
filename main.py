e97e3ed82f9420cd3a47606000eff5254c10d863
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:51 +0000

    proof us check tax upstairs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_area_circle(r):
+        return 3.14 * r**2
