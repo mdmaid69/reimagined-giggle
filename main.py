da821ceb4d9e73ffbdc60fa89c5c691859e146b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:29 +0000

    lean inform before loan clean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def clear_array(array):
-        array *= 0
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
