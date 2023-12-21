9cb993cd33e3a7ddcc40d85ca140f2257781b4c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:51 +0000

    Oo classroom wrap make chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
