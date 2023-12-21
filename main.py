e2e5b459252c521b19c96fe6d863c4ef54b37dac
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:02 +0000

    minority finally experiment ours expression

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
