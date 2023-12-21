c0ad440ffa91c934b70a940ca55bdd24b1773d0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:48:00 +0000

    bear then healthy Jj then

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   def subtract_numbers(x, y):
         return x - y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
