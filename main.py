44ebbd06170797a252d9d0c337de63ed88ffd065
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:18 +0000

    swimming means interview happy boring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
