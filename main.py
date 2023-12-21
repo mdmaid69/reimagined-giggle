7f9c217cae42ae775b1397913904df0c4c1206cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:07 +0000

    suggestion dick their Cc image

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
