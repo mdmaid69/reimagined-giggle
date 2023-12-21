32a143458f50ef7928c8d4fafdd680db14ef247d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:13:59 +0000

    like clearly estimate main absolutely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_current_working_directory():
+        return os.getcwd()
