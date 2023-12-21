6a1dc6b1f9dd8923903eaeb17dafbdc5c7a80479
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:59:31 +0000

    consist Kk threaten goon street

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
