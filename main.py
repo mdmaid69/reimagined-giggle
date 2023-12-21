dc6e53079c016a46735883230eb4d2c69ffc6d98
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:16 +0000

    round turninto workon exaggerate why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
