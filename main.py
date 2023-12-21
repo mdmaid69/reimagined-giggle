3a8545fbea5e9d680778506c461c383cd127f3e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:02:50 +0000

    for worth uncertain gift ride

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_min(lst):
+        return min(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
