dcd1361298fc218dc4d747fc9d54ce755c2af613
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:35:42 +0000

    public punishment noisy never now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def multiply_numbers(x, y):
+        return x * y
