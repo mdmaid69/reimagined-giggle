80fe86cb64d65acd8606c65cef495c98977c8120
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:34 +0000

    crash deliberate movement hate moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
