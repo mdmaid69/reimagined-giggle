2eeb75671fd85a2952b5d03badd85c1d0496d301
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:21:05 +0000

    whole rub preparation available stone

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
