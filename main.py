092dc308f4158af459f3c50cbc9d548ecf71af9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:19 +0000

    band dangerous age corner ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def is_odd(n):
+        return n % 2 != 0
