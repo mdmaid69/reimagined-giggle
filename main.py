5fcc4a861d494661159085ed4f89de80976b839e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:43 +0000

    anxious university completely judge you

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
