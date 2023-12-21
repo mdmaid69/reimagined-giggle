7a2c48f86c02a24e30d2493ce94e2221d96d7380
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:25 +0000

    getaway unfair figure message further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
