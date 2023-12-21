5725811c9320f5619b32fd91c7484b64dcd8e7c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:04 +0000

    tell broad sure realize laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
