e41acf1939abc81d5982c85a7008204dc1d767b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:56 +0000

    reduce spot important zero goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
