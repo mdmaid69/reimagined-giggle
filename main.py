813f405a4839d67a61ca2862add4e3a96a6518c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:13 +0000

    gooff next meeting game lookback(on)

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def remove_duplicates(lst):
+        return list(set(lst))
