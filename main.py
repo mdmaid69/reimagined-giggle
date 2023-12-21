9ff9ea5ca40713fac33c47fa5c17284c7072b7eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:55 +0000

    instrument shock me abroad harm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
