7658a9cffa28dad056077d3abee85b6beab2d7f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:25 +0000

    somewhere listen passenger realize split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
