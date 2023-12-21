20aecdaea9296ca8d81f65177f631dc8210242a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:35 +0000

    Pp move relationship independent computer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_slice(array, i, j):
+        return array[i:j]
