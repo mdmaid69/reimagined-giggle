3b533e7614a96be82d70bbe833496a148fb56f8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:47:54 +0000

    many interfere school win move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
