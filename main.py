97898fce1f336198c7ee6ee266c2ee3cc17b19a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:36 +0000

    majority record close study glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
