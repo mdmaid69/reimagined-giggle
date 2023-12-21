059bac61cc64291379fe1f059e7398517cb93af3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:44 +0000

    injury true pattern direct machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
