5230d9afdf9479a0348d5b1f94e1f7c671f12b46
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:32 +0000

    way charge guard correct contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
