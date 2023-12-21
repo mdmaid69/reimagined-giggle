7bd1a530b529321085ac4b71e0ec491b7cb36176
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:45 +0000

    always anybody politics uncertain convincing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
