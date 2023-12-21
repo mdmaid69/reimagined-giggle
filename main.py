aa1502a106839134b2295b95debf108046b00961
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:38 +0000

    Ii leg body hope takeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import time
+def get_current_time():
+        return time.time()
+import time
+print(time.time())
