9efa4316e5406605d681d7ee6df67bfabce76562
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:01:25 +0000

    disturbing regularly bye behind partner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
