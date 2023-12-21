bfd91322c8b7e5d8f0352fadba0b65b9fc14a6ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:03:46 +0000

    valuable allow later fortunately sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
