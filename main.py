92029085e9efb9ef9bc6908430bdbf692c1eb5c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:48 +0000

    give length join mistake convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
