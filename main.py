cc6c4becffbcff155800494b07ff7573de3c36a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:37 +0000

    pipe firm arrival distance right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
