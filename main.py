87500e028e2bf2820cae7d32f34ebed01e6fc0d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:48:09 +0000

    interruption everywhere completely willing bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_to_json(data):
+        return json.dumps(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
