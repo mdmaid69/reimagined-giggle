0ff4bf35456c34554c8788983cb2068b2dafb822
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:12:21 +0000

    boil foot hurt brave unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def add_numbers(a, b):
+        return a + b
