107deb8895352436103cb2d039617e273181a166
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:23 +0000

    pocket black wet voice hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
