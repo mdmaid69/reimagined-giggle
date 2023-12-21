9842dab3c8ea26ece2c3e29e6a58eb0030c68049
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:43 +0000

    room Kk plastic return rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
