fcb4a6022384a6af498b552f5b7daa9e5b57005a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:49:11 +0000

    wash conduct point mine reduce

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
