a43e3323ad79c45acc83a165a881bc302016cb1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:23 +0000

    sign previous swing official hope

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
