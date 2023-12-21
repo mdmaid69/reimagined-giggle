b59dd3418bc7ece72f5755768f21cc828af9e298
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:06 +0000

    though shake oil good major

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
