f9f3a572f3612f8ceac7cae5bb33603333e50327
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:01 +0000

    generous zero convince cause plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
