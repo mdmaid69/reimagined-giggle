e5bdb4d1f17d11f5c7c11c0eefd76f1d593c738b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:41 +0000

    container wake belief performance waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def is_even(n):
+        return n % 2 == 0
