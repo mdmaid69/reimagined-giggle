66736c42980762c2539dad784387534e35df51b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:54 +0000

    closely mother lookafter wonder include

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
