c20a4b5a0ff759356281c281c44ef7e975863af3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:27 +0000

    Dd lose niggaacademy language test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
