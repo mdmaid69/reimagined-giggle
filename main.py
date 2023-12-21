52f256680cbaa0482a87af4e8bfcc6a4b1520814
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:38 +0000

    on must glad drop recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
