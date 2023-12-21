92b6e3db9a6555b3bcc608ac8984fd6d12e0d26a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:18:34 +0000

    responsible day expect sound accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
