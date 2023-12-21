c5b20d4c7b1dc0b2bb1c7eacce856936c5b36a3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:29:20 +0000

    belong opportunity typical keepaway live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def split_path(path):
+        return os.path.split(path)
