63977a6f7046eb4b7b7a9733c1e0ea365acb2902
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:59:40 +0000

    Qq correct shape coffee sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
