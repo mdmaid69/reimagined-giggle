12b39e27a88195bfdafedebc1b727193133b9f6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:42 +0000

    trip murder hollow valuable who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
