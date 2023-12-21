d1b1569e8454ecd258f2ee0586014d4f83abdda8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:36 +0000

    bet present separate mixture classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import sys
+print(sys.version)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
