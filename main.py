386c9715ece10f483f88c12a6af9fdeec9873891
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:21:57 +0000

    mess throw operate strong desperate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
