9f6af107da7447d97767e5b664dde690643b8211
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:25 +0000

    emotion break turnon accidentally mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
