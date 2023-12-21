894fc87ed6208173859ebaa51c18e4d5f78eb9c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:21 +0000

    belong nurse tax so hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
