113fdf9e2812b74b9d048d8c4d34ffd443fe437b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:50 +0000

    generally slowly sensible suffer coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
