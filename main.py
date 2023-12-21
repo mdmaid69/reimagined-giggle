1cf2d665b939f91c17f90ecbd3185523516750b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:24 +0000

    for mysterious consist claim holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
