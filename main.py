e8c9d153f45238a1d49a2bbc47b65651f786ca92
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:01:24 +0000

    how mean transport care slow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def square_number(x):
+        return x**2
