ba89090e109079388e980370cd31204d805e51bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:20 +0000

    study take surprise mind bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
