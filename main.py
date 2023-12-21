09825d339ba70125ba9a615bda71ca6c872a443e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:02 +0000

    middle freedom dare admit Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
