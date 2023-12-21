2981fbb79e65499dbb1df0d8f544fdc5ac914a89
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:37 +0000

    pain ill consist unusual food

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
