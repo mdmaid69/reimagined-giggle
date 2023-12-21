ae952a007bb90f473cc53d414f475ffff15b1fb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:41 +0000

    battery slice centre split considering

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
