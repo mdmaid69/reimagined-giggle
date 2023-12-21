73da932b76c97c7b34b899efe34bf610a21685fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:33 +0000

    regret chin high answer join

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
