6b75d08bdf6a54683bf2c8da76bdf14195ba9499
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:49 +0000

    occasion design hot doctor holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
