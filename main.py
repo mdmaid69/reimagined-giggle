52f81e749759432653c893d2f428c36cf2ae2668
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:24:36 +0000

    aware someone fit common bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def count_elements(lst):
-        return len(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+def get_time_since_epoch():
+        return time.time()
