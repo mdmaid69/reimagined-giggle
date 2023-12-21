b57ff67dc1b2aaac641932d15f422437fb0297ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:27 +0000

    activity nor guide error exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
