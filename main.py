aacf78a8c32f2f20e273f0cb16b78a8deae90ccd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:04 +0000

    discovery announce practice wish operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-print(math.pi)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_cosine(x):
+        return math.cos(x)
