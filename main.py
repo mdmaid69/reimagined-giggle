621c4d4fe832e27e7b1a9138be22139b49ff1ce3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:58 +0000

    guess today talkinto thinkthrough spend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
