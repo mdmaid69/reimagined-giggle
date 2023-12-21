9efab7f6bf02c3e8d8aaede12b91639853bfa700
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:09:03 +0000

    rain my connected watch somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
