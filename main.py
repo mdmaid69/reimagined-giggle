959338f68a53739e7d6f95aa7000bc7804f7e076
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:34 +0000

    condition callup awake neck visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def add_numbers(x, y):
+        return x + y
