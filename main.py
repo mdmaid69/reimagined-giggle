442b7a7ac036c40566f0ff97b840cc27dc6dd846
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:22:10 +0000

    recent call discovery dick responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
