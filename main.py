18d61120f0e3eb0320e5b2fb3934148f54351a9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:39:24 +0000

    crowded accidental below now lock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def square_number(x):
+        return x**2
