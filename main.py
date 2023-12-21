49588ed7cf25051a3979af27fed1c3bce6399a98
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:52 +0000

    just breakinto picture police definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
