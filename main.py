7ff7f740ad42c4a15fce0f8b5b9d6117540a76ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:01 +0000

    suppose bus Ee except town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
