df269c87fa7dbecb0c3766b07aa3c584ac5219d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:19 +0000

    roll reject name butter involve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import numpy as np
+print(np.array([1, 2, 3]))
