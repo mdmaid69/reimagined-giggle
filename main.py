b0208de63958ce738812475dc57a6d94e07edd95
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:35:20 +0000

    nigga shape lift cheque mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def add_numbers(x, y):
-        return x + y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
