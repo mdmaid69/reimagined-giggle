61535eb618493ce46f094de79e5cd52bcd8ae5a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:36 +0000

    choose taste steer generally minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
