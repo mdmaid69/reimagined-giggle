3846f599a28b09fb6365245124c910b2d3a92f40
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:14 +0000

    correct issue rule herself frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
