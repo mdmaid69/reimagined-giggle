f686a2e8eb849fe504692c77ba42b1629f0fbf75
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:06 +0000

    Bb rub plan murder bear

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def convert_to_hex(n):
+        return hex(n)
