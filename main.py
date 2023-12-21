b6059c5d99a42f14c61ae724b289a65129b5bc2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:14 +0000

    thick that suddenly size union

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
