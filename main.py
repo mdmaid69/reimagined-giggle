74ecfab35aa0b6420170c368b108f9edffe68524
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:29 +0000

    yet board to ignore whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_power(work, time):
+        return work / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
