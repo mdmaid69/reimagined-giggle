4f0d0ef17dda69d0e971368d26d2bdab9b2f8db6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:18:04 +0000

    paper purpose swing deep thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
