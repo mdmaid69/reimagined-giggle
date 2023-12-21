1af8ec4e2f601c6baf343d972a43bcc5ff22add2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:12:17 +0000

    runaway human live female inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
