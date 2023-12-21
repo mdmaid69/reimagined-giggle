ddff2f2757b10b17758a224bd08617c31dada35a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:36 +0000

    last equal rise stiff future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+from collections import Counter
+print(Counter("hello world"))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
