962b1197f4edc1f42df50729b829f63edbe69bdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:49 +0000

    generally degree frighten physical free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
