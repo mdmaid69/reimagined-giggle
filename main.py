74fbc5458f81c248760c595b1c403a1a550fc954
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:15 +0000

    software pen rub plan last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def add_numbers(a, b):
+        return a + b
