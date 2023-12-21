63f03d3676fed6a6c0ec4ea3ec29008ea9f59a96
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:25 +0000

    rubbish table quick holdon comedown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
