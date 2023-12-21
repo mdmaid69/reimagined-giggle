b7b7fbe8965ea692ba3dde9037204618dc4b156d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:19:23 +0000

    square well comefrom connect should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
