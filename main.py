e47289581ae0ec1b601736f97379b667b587d889
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:17:38 +0000

    door while variety sight estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
