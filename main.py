a51e07bf404bff1c0cdc96c50ba1b5c512986b56
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:48 +0000

    watch strength even appeal copy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
