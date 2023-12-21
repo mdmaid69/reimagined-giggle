2f0e233ddab29f469743028ae1e577775c4a3708
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:40:06 +0000

    identify patient attempt quantity police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
