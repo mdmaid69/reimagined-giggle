2686869f287bfea20be914f108be2e9c7514b679
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:50 +0000

    shoulder towards dark warning thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
