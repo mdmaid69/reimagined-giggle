e2a98ea6d0ede40350a7ca6aed60805126271006
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:04 +0000

    pretty bad exact away forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
