5c6e11504c20693d59d42e3da126efe96d9bc2d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:15:17 +0000

    lucky whisper dry birth push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
