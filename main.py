e4902fb3ee2f4855ba5f22fa7b5c07713f0480fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:04 +0000

    correction nature open when treat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
