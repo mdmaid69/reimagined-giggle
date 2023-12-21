3cd4d7b913fdb1db480909c6a0193a96265cab1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:38 +0000

    wave bush store exist Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
