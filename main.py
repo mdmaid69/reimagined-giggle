6bb3e58b98916694181344f75edaa2525a9f1f5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:06 +0000

    lookaround foreign doctor exploration comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
