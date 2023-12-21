cafd4aca3aa9e503157d033dfcb920ee781c7e0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:46 +0000

    giveout field but strong maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_sine(x):
+        return math.sin(x)
