f0a4a0666b5ca3cef8564f3e5c4b40088b065f49
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:23:47 +0000

    glass visit fly pause rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
