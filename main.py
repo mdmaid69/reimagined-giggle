af906e84467cd602632d3a3a1fadfe6f9125ac41
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:31 +0000

    advantage jump regular left expression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
-print(math.pi)
-def divide_numbers(x, y):
-        return x / y
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
