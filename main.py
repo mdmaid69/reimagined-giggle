50b086626ef21774075620d47bf90600e4142da6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:21 +0000

    shout corner should slice protection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
