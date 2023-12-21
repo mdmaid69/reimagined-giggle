8323fe1fd66f1f2098e7e6d88f495bbb1e3fef42
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:29 +0000

    anybody judge this course storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 def calculate_perimeter_triangle(a, b, c):
         return a + b + c
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
