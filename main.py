e1e2e7b2914b1ad57f19d89c4953f2f9ba0401fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:18 +0000

    language interfere there really illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
