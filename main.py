0def6be614e3eccc45733d3addbd7ba8afd826ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:35:21 +0000

    mine town trial laugh instrument

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+print(os.getcwd())
