99ab155b2c98174c6a0faed06c41afdead892756
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:18 +0000

    apparently warn avoid Pp reduction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
