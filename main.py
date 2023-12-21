f2264cf351ba8e462f7b06bbfbc6dc38ae9a1966
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:28 +0000

    affect glass far under near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def add_numbers(x, y):
+        return x + y
