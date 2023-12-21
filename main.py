f770f087a098eff73ba3f19e1d2513e24f8435d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:06:02 +0000

    several matter 567enemy takeback art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
