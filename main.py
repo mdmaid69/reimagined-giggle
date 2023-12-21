8a167aa00fa563beac228d77237b51434a86125d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:12 +0000

    less duty fast afford model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def sort_list(lst):
+        return sorted(lst)
