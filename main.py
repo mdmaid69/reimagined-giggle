c86bc35db585c4e6a54c9e367df530739c1511ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:42 +0000

    field display positive kind schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
