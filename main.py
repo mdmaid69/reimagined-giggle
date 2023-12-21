44dea5b1a120f03433cb952dc3de0c845f841ba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:15 +0000

    load whatever air constantly suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
