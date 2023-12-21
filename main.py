a0f11f15da3a83f44065d70501efba12eb73ad3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:06 +0000

    music tongue standard justice ordinary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
