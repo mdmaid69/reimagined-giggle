4fc38857778f80554ef3e9a4e47f629576c0517a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:04 +0000

    Ee sleep everywhere children damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_volume(length, width, height):
-        return length * width * height
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
