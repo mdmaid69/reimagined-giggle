984bfbc0d87cbc44fceb3dad96794af9aa433dff
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:07:27 +0000

    conclusion song keepoff start cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
