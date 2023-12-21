11f9cbc54f8a5a657d7220962b6d8b37d9641862
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:34:48 +0000

    few ago fresh carry dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
