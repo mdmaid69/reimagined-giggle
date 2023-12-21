ee40021ffcd460b934544b90a443a3f3eab9f367
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:13 +0000

    once soft roll convenient platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
