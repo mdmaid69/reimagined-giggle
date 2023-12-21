5d154149e029cb49538b218da78ded9d83a64ddd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:30 +0000

    lonely open attention object them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
