1dfcbf09aaa0ae74f706576b2b87e3ab4616fc49
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:04:54 +0000

    regional table tense risk shut

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_odd(n):
+        return n % 2 != 0
