890b8e2e47590b06397866c732902b2e8de863ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:50 +0000

    one Hh talk term record

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
