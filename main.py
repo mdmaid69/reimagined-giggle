32775b9bd92b8951cd68f83e90454231be074a36
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:52 +0000

    exactly performance risk holdout poison

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
