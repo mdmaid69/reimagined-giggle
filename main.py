5a762ee38c3b747027e03dc493d13b075942c845
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:46 +0000

    tread hold payment good soil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
