6e4d15cc989b561d076a0cdf1caa5a233db28052
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:32 +0000

    down afraid variety air mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import sys
-print(sys.version)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
