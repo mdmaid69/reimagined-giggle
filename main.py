5a12e1763c361997a218dd886274b1af52b84bc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:10:08 +0000

    neither agree warm send unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import random
+print(random.randint(0, 100))
