c01256d3ae3fc781bad750b7391525be0f83f7d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:41 +0000

    wave shock unhappy operate offer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
