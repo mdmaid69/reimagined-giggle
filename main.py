b4c6417895deda83a6c68afc69437953e0ce8808
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:48 +0000

    decide back dress instrument rid

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(5):
-        print(i)
-for i in range(5):
-        print(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def is_even(n):
+        return n % 2 == 0
