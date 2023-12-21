5a989267b0ca05555ed22edfa4c5000a5a70ae37
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:44:12 +0000

    crash guide somebody mean if

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
