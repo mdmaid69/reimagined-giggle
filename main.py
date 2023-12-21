abc7c99516fe9288aa643b130f87bb3d663ac320
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:36:43 +0000

    dream excuse full vote gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
