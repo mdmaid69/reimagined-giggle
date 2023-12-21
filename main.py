48c9028bb33f7d72fcf9411b72043c1273a239f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:29 +0000

    like throw know wash load

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
