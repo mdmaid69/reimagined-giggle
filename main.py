85f22d69bd4e667c0e4e4bdd6622fcee8b072235
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:07:57 +0000

    action invite slow most live

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
