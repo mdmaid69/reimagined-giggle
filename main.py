a5118eb1c75e07c220a24eebb6b09a5fdc8b7a0b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:58 +0000

    land offend sick sticky statement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
