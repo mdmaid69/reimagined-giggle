fe9d010b68512ea1e7a059cda27acb4f48c1c59e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:52:58 +0000

    note weekend anybody low after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+for i in range(5):
+        print(i)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
