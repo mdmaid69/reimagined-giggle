51bdb5a9311f1cf68484ad7167335108401ee529
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:52:51 +0000

    at arrange might centre patient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
