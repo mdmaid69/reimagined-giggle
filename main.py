df7bd86dd6bd0fc767c59e78263328b1bbed2ab7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:41 +0000

    delicate illness up interfere commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
