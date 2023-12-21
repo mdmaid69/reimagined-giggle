6fdd3647c2a43ffbcc1c2bbd40b41927a1f87ee3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:14:10 +0000

    upstairs thorough 570enjoyable eager correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
