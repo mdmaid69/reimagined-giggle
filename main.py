84f7a26360adea2643d9ee47ab5d1c592a5a790b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:03 +0000

    direct talk vision building maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
