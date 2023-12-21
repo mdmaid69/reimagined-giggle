639ad70c0139ca295a77d4d99138ee4f4563d73c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:59 +0000

    body base instruction Zz sensitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
