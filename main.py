de71fba5e9c650ba21d7cd0fab1a7683fe2c8582
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:07 +0000

    sort use organization mark band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
