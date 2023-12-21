1d56383ef8e53d7ca33a47878f1a59022cdb4c64
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:49 +0000

    embarrassed brother field familiar warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
