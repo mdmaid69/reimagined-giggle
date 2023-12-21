78442f06e2d53f963c17c46f0f543c038d948a90
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:41:52 +0000

    certainly law expression slice squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def cube_number(x):
-        return x**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
