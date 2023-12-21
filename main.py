78605ee6d6e3da128bf8098a80131c9c4fdab307
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:42 +0000

    situation the spot back wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
