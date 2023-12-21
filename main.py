479721ed27525e424fb9b93e702be3269a9727ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:58 +0000

    normal actually limit country Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
