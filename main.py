b1567ef8c4e0044bcb31682c03557d2564f7f745
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:08:05 +0000

    style same card Hh deaf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
