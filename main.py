694fe9f26c2eb6bd6c592018651f2f87ebf921c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:43 +0000

    hour also corner destroy size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
