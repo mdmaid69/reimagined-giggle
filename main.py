346c922b35af030fd4f4e6ea28dff4bd10ad1c5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:44 +0000

    create apply spot pen foot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def convert_to_hex(n):
+        return hex(n)
