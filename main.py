2ac3aae0c951906689490c5d18acf634380b2711
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:17 +0000

    sign mark power pencil skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_sine(x):
+        return math.sin(x)
