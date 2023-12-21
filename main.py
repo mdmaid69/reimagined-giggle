db7ae14112fc77d5aec0242a6344a54c258868ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:53:33 +0000

    report deep it look turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
