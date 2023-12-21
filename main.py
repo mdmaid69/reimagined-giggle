52aa2a28c9be5fa4bd5d52c60098aef3e74cef19
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:36 +0000

    activity knock play him round

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def cube_number(x):
-        return x**3
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
