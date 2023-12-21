2173f475c469b71aa05c549d46e1b52b8383801d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:49:35 +0000

    sure trade central my heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_error_function(x):
+        return math.erf(x)
+  def reverse_list(lst):
+        return lst[::-1]
