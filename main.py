9a316ca38616419d3581fe83f13583bc24615b72
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:28 +0000

    rise hair turn principle bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
