b3e1e8edef3577ac1e5d6dd041784a4b097e0542
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:32:58 +0000

    lead threaten of opinion apart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
