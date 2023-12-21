ae456ff26b6963d96b03d92540333f0357f0a276
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:59:09 +0000

    sister famous method post operate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 def remove_duplicates(lst):
         return list(set(lst))
-def find_max(numbers):
-        return max(numbers)
