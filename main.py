a7f603a71a166007643cac897fb814e5bd550158
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:53:54 +0000

    same alive production respect know

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-print(time.time())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
