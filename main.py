955208ee76a63793596b440866d51e3af0c0900f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:04 +0000

    could prepare excuse broken scare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
