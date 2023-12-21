3196f8bcf7c3bdc8ea2ca95be9e0186edf2e6875
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:24:43 +0000

    example call relax entry grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
