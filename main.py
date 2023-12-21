f070b5d825f554cdd3e99ca7daf1913600e6265d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:27 +0000

    rain declaration less disapprove ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
