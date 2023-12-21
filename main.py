3d00219c2b2fe0390a39c533a3e6d0dc8ea9ec01
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:16 +0000

    entry figure somewhere open comeout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
