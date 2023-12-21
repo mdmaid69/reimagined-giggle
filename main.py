e60308c88188146cc222e8ff5c1581dd63045909
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:22 +0000

    check attract violence which couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
