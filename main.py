807293704e23803a12421d3756edb865dbc9103f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:41 +0000

    shape join consist rain plastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
