603a4d627475f6bf441704dbd72cbd9234851726
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:10 +0000

    the sticky wave actual breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
