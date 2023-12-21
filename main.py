54bb536d1ac66ab8f2ddf6883633ceca29b94fb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:59 +0000

    town opposite advice hear herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
