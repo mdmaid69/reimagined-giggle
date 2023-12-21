204d1c3ba8737ff5b86a9f9aa0e388751b9731b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:50 +0000

    clearly lower treat dress trust

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
