4adfabc8d9c9b688fa95aac32c644c83363ee474
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:33 +0000

    grow upstairs achieve style judge

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_speed(distance, time):
+        return distance / time
+  def multiply_numbers(x, y):
+        return x * y
