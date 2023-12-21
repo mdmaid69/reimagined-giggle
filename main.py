a0829116012a97dfd04055b6f23ee00414701c93
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:35:48 +0000

    use couple comment dead surface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
