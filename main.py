0ed8a8daa6e7178dcc45221206cf388647fabce2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:41 +0000

    hit ride community trial noise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
