3108024fc0ffc3cf2b39e3e7643e7ae71fee38dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:12:46 +0000

    except apply history pot properly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-name = "Python"
-print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
