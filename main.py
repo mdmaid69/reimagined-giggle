9f903ada402cd21f42de5c94b553f5e48ab46cb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:25 +0000

    hurry commit comeoff nowhere comedown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
+def convert_to_binary(n):
+        return bin(n)
