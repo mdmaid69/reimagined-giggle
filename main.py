45a051e57457f32ac8973f173da78f2b5a98cdf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:03:00 +0000

    provide difference suit food round

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
