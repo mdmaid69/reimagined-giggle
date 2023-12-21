1fdf89cc1ba4841f27de49b88a951ec14fdfd1a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:26 +0000

    smile gooff to sticky figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
