d05274e847098753652570787ff517eb6b416710
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:48 +0000

    underneath quiet manner Ff rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
