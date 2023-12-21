1f95d43c135dc0990e8a22610d4f9495f4d366d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:07 +0000

    detail partner high place entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
