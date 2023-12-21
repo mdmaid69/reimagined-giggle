bd5d86a9d38ae2e664f7de6718543063f2de6961
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:52 +0000

    fight season yourself responsible little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
