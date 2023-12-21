4f67917e532d1d8349eb496bdfa964b1972dabeb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:32 +0000

    suddenly giveup minimum before miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import getpass
-def get_username():
-        return getpass.getuser()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
