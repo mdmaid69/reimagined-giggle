9e4f6f37a4b50d70678214b731d1ea45e9b9506c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:42 +0000

    deliberate letter me us gather

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
