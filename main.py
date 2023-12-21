088d749f9caaebe6d18038e03596e69554f7efb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:11 +0000

    breath regularly calm breakup branch

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
