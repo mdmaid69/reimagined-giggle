f13f1adcb36d9b2fd21da4e9afeecf34c5e1c591
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:30:38 +0000

    crazy push turnoff off agree

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
