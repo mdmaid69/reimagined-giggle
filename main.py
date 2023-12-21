12807c953d6bff2b20d89a3776e9390b5a651f35
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:27:22 +0000

    coat holdout only explain could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
