23580d21509b28944c6fad95bc1428412976cbb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:37 +0000

    pinch final keepfrom fear actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
