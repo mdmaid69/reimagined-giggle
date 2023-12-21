fb44b1c7d9ca4375c242444f2634a03e1417126d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:26:49 +0000

    under understand Gg fast move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
