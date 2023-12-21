d5a926dbcd15fd1fc08c182f9f1cec732f506ba6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:47 +0000

    exploration artificial ordinary either introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
