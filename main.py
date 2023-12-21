96a29358fc113303c000f95a4291754ee38c7968
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:16 +0000

    future succeed able surprised hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
