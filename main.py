38a51e92a615d00ff2e2980c783068b3f3ed44a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:27 +0000

    realize week comfortable bring once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
