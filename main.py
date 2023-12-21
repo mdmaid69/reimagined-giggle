e03e1482e283f842a9ba2f04a066f54947dadd1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:58:06 +0000

    read recommend trap 571enough start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
