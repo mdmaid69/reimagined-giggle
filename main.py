9c44af47b1d4549eb554e189eefec41fbc72573f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:19 +0000

    let weigh admire car may

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
