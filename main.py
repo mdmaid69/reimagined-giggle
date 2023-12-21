2962b2f3fd926da94aa3d04a23d0819d807466ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:02 +0000

    control wrap recent Ii pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+print(sys.version)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
