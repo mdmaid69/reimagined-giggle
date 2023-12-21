151138099425a44a3254925a62d731b8d37d740d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:57 +0000

    use square promise floor swim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
