e7a0c93347ce97ed0d1c5cc6c7b404deec74324d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:16 +0000

    post Nn off satisfactory shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def is_even(n):
+        return n % 2 == 0
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
