21a61fc03a44e38d497d0e44b5a48b6a5e5e6e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:52 +0000

    argue 565end than identify official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
