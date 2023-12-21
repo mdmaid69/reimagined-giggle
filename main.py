bf6c86155b7af20adb7cb7f32f495b5aa364d58a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:31 +0000

    furniture load match thirsty tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
