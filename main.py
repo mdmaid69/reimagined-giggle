f4b836d4307f80de5c6f416a08f972d70e5369fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:16 +0000

    situation ilovedicks perhaps next breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_set(array):
+        return set(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
