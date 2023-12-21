f632349926a1db4cab33353e98f1aa1af587713d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:20:00 +0000

    guide reply holdup battery Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_union(list1, list2):
+        return set(list1) | set(list2)
