0d707621972024e5092da2cd391953dd9f7c226b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:06 +0000

    moral sink sea downstairs rain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
