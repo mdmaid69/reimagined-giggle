b1f96775be77cb14a53b974e921d92bfcd2e30da
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:30 +0000

    method own programme drive away

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
