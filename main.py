891e3a71e2915edb8ea3aced948b96d3ca3aa540
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:58 +0000

    fight flat please excuse used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
   def get_current_directory():
         return os.getcwd()
