015138db38d6eaa75ebc44a8bb0bdfe30706c916
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:36:48 +0000

    stop loss lookback(on) tip small

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
