7374c269f9f0d30fda37a4ba6a216f9f83ab58d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:14 +0000

    note block closely legal thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
