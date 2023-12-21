fce477712f5a7b38bb5b5591f6c4c8dac3461eb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:39:59 +0000

    short refuse remove label bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
