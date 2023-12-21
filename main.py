a5d21cc471155cedb5e2f0da256a0dabe9348cd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:50:19 +0000

    right usually study knock which

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
