01ecb66dc59f465c6fc08ea342fb14bb7b4dd2cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:00 +0000

    share regret change stamp practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import logging
+def log_message(message):
+        logging.info(message)
