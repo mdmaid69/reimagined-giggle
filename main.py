6b37aa1ca31980660133be8d28b65f6e5e9592ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:39 +0000

    foot furniture murder preparation upset

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
