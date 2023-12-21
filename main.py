a0aaaaa6866365e37ad526520d1a41e5568da36d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:52 +0000

    grade contribute check wave spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
