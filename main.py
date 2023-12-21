c6fad14b56342fafd26e173d60330b92cd808e79
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:37 +0000

    skin apart actual gate spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
