d533fd24d526f33c022bc6b75732089031b7776a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:05:00 +0000

    turnon necessary now birthday could

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+def exit_program():
+        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
