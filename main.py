6f2e6baad323c93df571b3875771e1559379b020
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:52 +0000

    form promise combine thinkover fond

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_characters(sentence):
-        return len(sentence)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Words:", len(text.split()))
