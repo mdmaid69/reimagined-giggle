e527c622aa997cbbea199205d8ea7df62f3eb9f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:42:00 +0000

    regret point standard wind become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
