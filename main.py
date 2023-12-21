6b30feebcdec21352d94483cd5282df3cf0513a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:05 +0000

    straight turnout programme avoid drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+for i in range(10): print(i)
