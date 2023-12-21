a332c4300bf98e5e95fd88c00218dcf4d4632542
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:25 +0000

    separate happen public branch Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
