383358edc6f396586a4b47f63ebe7afd3a65620f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:25 +0000

    manner free ride cheek active

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
