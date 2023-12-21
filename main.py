68c7d72d1b8f28fe409a688d4094656e9a380ea1
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:03 +0000

    emphasis prevent value taste fierce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def remove_duplicates(lst):
+        return list(set(lst))
