a6e37000f8240c31dc549486f8e389213483bdd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:16 +0000

    fall weigh present gate plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
