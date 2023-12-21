45c712fb6b7762ea509be757be42bc2b32d288b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:21 +0000

    bit flat over mind round

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_length(array):
+        return len(array)
