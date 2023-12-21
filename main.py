32f8d2a76fec65ddd6427394fcd5cb00457b4e6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:34:24 +0000

    like whether underneath bringup activity

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def is_odd(n):
+        return n % 2 != 0
