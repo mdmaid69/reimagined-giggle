05b214e6b5a48b0cccbc23a2c2114e7bedd794a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:11:17 +0000

    punishment material coat oil chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
+def count_characters(sentence):
+        return len(sentence)
