dc5fb5e3663106079c57b90010b6b35f447aa5ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:19 +0000

    noise round cruelty tense Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
