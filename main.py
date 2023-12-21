e40ed2fd7c265cb5fa188f995e0b4282477ab5a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:35 +0000

    receive holdon rate use check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
