d643a78923eafa20b3469f0413b1c3c68a1ee5da
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:19 +0000

    bridge regret estimate shock probably

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-import time
-print(time.time())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
