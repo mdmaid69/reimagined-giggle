893a0f054e8c3072bb21ca2a3200b997e49f4b98
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:55 +0000

    emergency brush pronounce serve furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
