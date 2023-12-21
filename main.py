f95634cf60d793a3a7c6c46e13b97a899c0a647c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:21 +0000

    shrink region news determined brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
