0c2c8ad9b8594c7b44bf9ee548a668b49c9ad032
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:24 +0000

    to concern pull stare Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
