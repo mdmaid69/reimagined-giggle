b19fd1ca2420d0b5afdfdcd2bd0d08003825c536
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:12 +0000

    takeout comeout wonder protect head

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
