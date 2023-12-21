dd9158e9f205194498e51871ab1e103ce642343c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:44 +0000

    corner front bill water circumstance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
