c522f76f28806c3e1621db7c981583e92745c41a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:56:05 +0000

    cheek even variety wind attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
