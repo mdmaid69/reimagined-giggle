ccaab7191a95964d8a08b328a466c30b4b83c045
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:43:14 +0000

    friend man subject board frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
