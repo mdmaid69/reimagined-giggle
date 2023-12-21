dbd2985fc59dd7c505df74c0dedbd39781b21875
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:09 +0000

    today comeup communicate by bright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
