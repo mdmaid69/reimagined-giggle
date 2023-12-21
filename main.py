9538d2df0b7aff8395c34c5d228f03a5a136ee71
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:53 +0000

    remark weekend contrast fill band

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
