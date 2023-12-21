90a3d2e64a585014030e4ec7a35f8eb87be262a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:56 +0000

    immediately radio stretch music getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
