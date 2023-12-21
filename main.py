4654081f55be9e8c46c5920061d470cb03569626
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:43 +0000

    usually bath post weigh reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
