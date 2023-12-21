8fbf6db6447a84a7f83c6270effb40b45aaaba1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:00 +0000

    pretend answer watch subject settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
