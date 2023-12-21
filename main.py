b5da4a7e1476f10938a6291f1b51239e05dc4bc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:48:37 +0000

    right responsibility possibility zero impatient

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_octal(n):
-        return oct(n)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
