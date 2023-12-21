56368a6675b7be6223280a2710cc77e3d08bfe19
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:53 +0000

    stage post active borrow lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
