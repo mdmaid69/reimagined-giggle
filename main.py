d00bbc183606fb1df1964dd16e1682e0a3528cc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:21:09 +0000

    car explanation treat machine reach

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(5):
+        print(i)
