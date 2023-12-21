315f1ea42685f3279541cb6f2034ed4180fc5e8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:26 +0000

    popular mend gooff tired trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
