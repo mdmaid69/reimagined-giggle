8b628c0e4d3a657372972b2284795496cf767878
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:09 +0000

    month deep action walk nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
