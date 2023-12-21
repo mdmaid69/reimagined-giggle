ec1d5591f11b2df48404f6ceef585b23c055ba17
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:53:43 +0000

    male measure supply downstairs now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
