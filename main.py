37958132992a698f17d8a72a2c16e4057fc056f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:21 +0000

    description middle invite comeup goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
