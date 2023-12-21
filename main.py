0462fe222fa9f87b5b3b132ba9767962e32fdda0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:07:33 +0000

    day mention brush carry over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-for i in range(5):
-        print(i)
+def calculate_cosine(x):
+        return math.cos(x)
