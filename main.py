cb2aff41a9603fc2fde16d5a56bea301b8242775
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:24 +0000

    takeover difference science decide great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import array
-def reverse_array(array):
-        array.reverse()
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
