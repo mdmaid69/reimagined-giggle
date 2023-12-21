12e7a51ee733a35c3bca72a57661f0170e0dbfd6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:15 +0000

    interfere beginning deliberately party work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
