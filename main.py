a186fabd586c380ae5cd4b71bf57740979c0f8d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:58 +0000

    equal workupto base then hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def append_to_array(array, item):
-        array.append(item)
+def reverse_list(lst):
+        return lst[::-1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
