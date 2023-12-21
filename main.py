d80916173d1b09c8a6cc26e0b5d273e5568d325e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:57 +0000

    demonstrate discuss spoil hour satisfied

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+print(os.getcwd())
