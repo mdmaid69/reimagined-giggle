ec0ab2717c356f24c94867030a6a28e7bc931611
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:06:08 +0000

    spring rely sign subject scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
