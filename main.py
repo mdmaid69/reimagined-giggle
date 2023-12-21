f2901887f227446e2897bf0a2e3e172ca6c39f45
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:11 +0000

    bridge skirt stupid warning dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
