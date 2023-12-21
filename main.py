c9ea25c5e5df69a1b3d8851227bcc03d6fa5252b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:57 +0000

    pin live however politics would

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
