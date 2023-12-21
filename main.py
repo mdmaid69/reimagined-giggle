d623c819986ce58e1baf161b119f3fc62099e5a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:48:07 +0000

    generous politics jump represent central

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
