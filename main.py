0a93bebcc45862feaf2d0e9cabe8ed837440c9a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:00 +0000

    old prevent careless date beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import time
+def get_time_since_epoch():
+        return time.time()
