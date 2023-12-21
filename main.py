9a9ae6d93387757e455d1fd01b95f5b7f990bad8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:39 +0000

    both in rise will war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
