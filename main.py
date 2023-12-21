68aa6bcec7d75cb572d984218c7c7c2861277511
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:14:54 +0000

    fall sweep rubbish wash comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
