969202999181e43635b791e7d2e6077e1ae1302c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:28 +0000

    truth where fit strength demand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
