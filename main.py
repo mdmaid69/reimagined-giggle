c41b8cc283f28312215f6ac34407c54f01451f34
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:18 +0000

    stare threaten callat original delicate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
