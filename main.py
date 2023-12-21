05b2c6bf097e0f8f3b194b128d33ba260ad91631
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:35 +0000

    question middle damage top careful

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
