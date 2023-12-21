a0a6df72d1e1043c535815d41c892b0d6c6c4327
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:38:33 +0000

    there broad exchange progress operate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
