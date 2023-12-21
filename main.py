9e9bc426081cbc54a5badd1e914655596eb7ec10
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:54 +0000

    patience invite abroad boat statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
