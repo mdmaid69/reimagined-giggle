8da5d2c1c9e987e7a8a3c12550359872f752028e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:27 +0000

    once sort fail press authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_speed(distance, time):
+        return distance / time
