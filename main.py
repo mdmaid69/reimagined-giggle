dd577d9233657b39c0fd98ca220f262200d27fe0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:43 +0000

    learn flow uponon reduce smooth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
