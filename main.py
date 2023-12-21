e6cd4b5f2eb42f02f4a773a58eb25c3fce7901e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:02 +0000

    opportunity tone single strict sting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_int(array):
+        return int(array[0])
