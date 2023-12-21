8eb63dca43659cf34bbe41516841b0a34a66d0f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:26 +0000

    pretty must base command evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
