fbc99320f9c8687c00764fe801aa3e3c315b1f08
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:28 +0000

    generally cost could low breath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_work(force, distance):
+        return force * distance
+def subtract_numbers(x, y):
+        return x - y
