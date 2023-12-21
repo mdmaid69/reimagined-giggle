a9b553fc5b3bb78240e6af279336d369db9d8657
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:30 +0000

    turninto nurse country unless succeed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
