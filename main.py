e689cfb454bbeaeb0e3a9b7693b46a0205e2f628
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:23:53 +0000

    shop comeback confused shut noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def cube_number(x):
-        return x**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
