1324f5e664e7893c400edb3d0af07cffa19f3aec
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:49 +0000

    lie thick prefer thankyou record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
