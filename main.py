a2bd95c757f437a194d9107c5c9567627ce53e9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:04:41 +0000

    track day finally emphasize holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
