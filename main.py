eba2a2dd87004d3cdb0f3efb9976bc556dc6474c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:28:52 +0000

    tidy increase accept perfect dead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
