f47fe2ba4780f97fe936d25465c39cb9235dea98
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:48 +0000

    themselves along lookaround murder I

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
