a1de13c6aa221b0255db9cf2db610dc9fb4b5ca7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:57:08 +0000

    earth whom mark agreement tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
