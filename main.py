91bd5666cdbb989b4904a35132f82e752cb3bace
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:54 +0000

    duty wherever position further regret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def cube_number(x):
+        return x**3
