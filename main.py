c4fdd4320e748f37acd40a14eca451976cfdf0d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:34:58 +0000

    pure know even ability hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
