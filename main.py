aa9d91ce89e5bf916d9e001bd6938d6f535dcab5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:42 +0000

    crazy part lead small trick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
