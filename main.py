34d7d3ceb6ae58fecb7aaa8865badeafd5a56dba
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:00 +0000

    blame ahead medium convince male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
