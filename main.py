53524fa7d25deacac13c4bebe3c19cf7551efbb0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:24 +0000

    on high rubbish but step

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
