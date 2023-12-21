86a5680faf022dfdb4963365c0ec125c08ea67ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:08:02 +0000

    speech dress comein 569enjoy connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
