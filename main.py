ce6880650a8b74ba5a47c636694e058c125802a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:48 +0000

    apart more scrap lock different

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
