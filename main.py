f08101e9834b62d0a40ffd1e1f5f8f85be45888f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:52:00 +0000

    breakfast march positive cause weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
