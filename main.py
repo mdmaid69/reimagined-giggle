d821c922d313594dcdf730990a0b42561258557d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:41 +0000

    Dd lucky want word pocket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_acceleration(speed, time):
+        return speed / time
