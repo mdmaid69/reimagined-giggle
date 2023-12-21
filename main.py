b259e0ca86871fe260e936d8127775d484b38c89
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:40:24 +0000

    pin cost persuade importance from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
