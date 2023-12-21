5941e9896e8bb6c88b4c09165b074c286e446caa
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:37 +0000

    achievement language problem must dance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
