8c5cb8c743ba2a86585488721c8197dfc619ff1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:14 +0000

    as ear news so heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def check_if_array_contains_item(array, item):
+        return item in array
