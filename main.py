117df8beb25957478967be77e2f1f45189e343d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:48 +0000

    actual split smile peaceful unusual

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_min(lst):
-        return min(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
