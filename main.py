7516db107920fa498e97ff53dd1a0b0a3c47491a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:58 +0000

    mean high deliver defeat average

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
