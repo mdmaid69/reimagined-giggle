2ad93128fceac58081177c1d57c6ffcfbcb98fb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:43:57 +0000

    cap period frame shine partner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
