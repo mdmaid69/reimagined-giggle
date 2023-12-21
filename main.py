fe1be471ba144804e5bf71c60d55552780dc271a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:17:58 +0000

    quick image wrap total calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+for i in range(5):
+        print(i)
