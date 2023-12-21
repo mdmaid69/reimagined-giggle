82160c8eddac58d954f9a7494300cd3a1d49f343
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:59 +0000

    convinced accept deliver model lonely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
