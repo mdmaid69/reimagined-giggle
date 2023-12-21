7b9a993e0d3bd5a3f8422c3d4b75247606c3881c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:55:21 +0000

    vote alive professional exam theirs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def square_number(x):
+        return x**2
