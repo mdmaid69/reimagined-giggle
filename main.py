c70bfd12419b601eb338877034a52034f8eeb156
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:50:52 +0000

    fierce plan same whoever rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
