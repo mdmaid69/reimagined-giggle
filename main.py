fe8a6cfa142dc03bd988191de203e42e58b564d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:53:20 +0000

    introduction translate over flow twist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
