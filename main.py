abd0001e2200b65c966a88963b681a22409623c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:57 +0000

    object secret criticism standard Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
