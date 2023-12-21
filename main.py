63a4005022598489df432a27e3cbd24ce3676b65
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:02 +0000

    memory usual makeup dance public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
