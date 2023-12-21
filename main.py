53b89510fef7f3e328384388bfb920e2959c1c96
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:16 +0000

    ashamed use education press name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
