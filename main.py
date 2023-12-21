ce9e2a3eb99827123913edce60fdc7ae7432835a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:57:39 +0000

    blame plastic grow account soil

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
