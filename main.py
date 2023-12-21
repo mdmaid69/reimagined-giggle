12ba31500664f1474e9250358e2618eaa8bdd103
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:40:03 +0000

    getup difficulty freeze constantly art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
