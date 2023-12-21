fac3a2a7807da45e31b14179965cfde886f9835a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:14 +0000

    interruption tense perhaps miss proud

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
