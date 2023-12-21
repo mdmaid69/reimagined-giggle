2ef7f1ad173f1849fc9eb67f4b9e1066886d6382
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:29 +0000

    burst borrow exist considering arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
