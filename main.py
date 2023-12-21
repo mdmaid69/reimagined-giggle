ee4894c3a31f15aa1991800e269635e6d14c9c79
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:01 +0000

    confusing embarrassed public phone road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
