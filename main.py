102eeef0b3b01ead7cf069938af7bccc16cc26cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:22 +0000

    however create sticky remove bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
