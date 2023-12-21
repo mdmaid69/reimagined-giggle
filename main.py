a2f03df80a343507d6daff1bfd9bee793eeaf8c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:18 +0000

    recover course steam Bb responsibility

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-print("Hello, world!")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
