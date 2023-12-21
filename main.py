d433a29a2195a03ed2f976bf18a97e5d2b3be89e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:38:17 +0000

    attention heat Kk total belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
