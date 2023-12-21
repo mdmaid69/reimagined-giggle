fe577c44822ef7f7d02dd6c9f459f6fde4d50cee
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:20:40 +0000

    us company fork hope over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
