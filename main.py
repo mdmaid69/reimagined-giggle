c4082ac6778dd727417ea1debe1b7b63bb64a7a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:20 +0000

    punishment turnaway relax neither tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
