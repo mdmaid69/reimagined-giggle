08c9de8ff9bbda2f522f710d7c98661256918738
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:44 +0000

    smooth excellent closed travel rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
