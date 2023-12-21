6defb3b412c36d5f942ca4bacc3a277fe4e23f73
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:21 +0000

    genuine term similar pick class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
