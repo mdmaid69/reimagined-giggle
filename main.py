f2b53ec20514e0fe1d66f090375e4a440ea3c688
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:33 +0000

    cruelty careful copy positive point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
