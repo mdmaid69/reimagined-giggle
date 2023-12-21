851ca6d271d3844a416a60550f5ec0406ad4f1b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:11:51 +0000

    size score fall address label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
