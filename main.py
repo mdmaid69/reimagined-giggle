3e3e85b41a112f3b6ccbe2533c7eff52934c0d13
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:27:33 +0000

    before edge event in national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
