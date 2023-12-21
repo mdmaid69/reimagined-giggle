6f43dcfc66e2ebeaf36c22d97768562c1f3e1502
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:43 +0000

    violence sentence noone general medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
