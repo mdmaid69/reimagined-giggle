3a106796f3d7f6b40a0e0132632340d99b05dea1
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:58 +0000

    change close smell opportunity comeover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
