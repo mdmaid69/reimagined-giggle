af6c85629490920a4b4eaae8adf098060936e5ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:29 +0000

    separate appearance unable time report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
