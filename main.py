479abe136c6cfc94d378a57c85e5c5ddd5e031b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:01:39 +0000

    necessary attitude tap plan roof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
