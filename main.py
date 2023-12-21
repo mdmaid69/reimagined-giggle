d95fdf11a487b16fd1fe52b53425a1218f305cf7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:17 +0000

    improvement thirsty responsibility please shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
