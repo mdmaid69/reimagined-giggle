69b605027faa9a64db18a974bb38a59439606bf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:31 +0000

    over hang hardly rubbish forgive

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
