b03a6ca627c5b2cfb90d96685bad47f9ab9ab557
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:20 +0000

    previous splash give everyone really

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print(sum(range(10)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
