e11f1d918b5cb921ae0950599e8b1bb8c03f24a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:39:42 +0000

    wet free tear fresh boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
