574682d9cf14f56ae19afe71b44cf8f706d39b01
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:24:18 +0000

    professional trick table runout class

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def square_number(x):
+        return x**2
