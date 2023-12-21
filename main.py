51249363581ae23b2e3d0d7306ab3a1d2bc2fe75
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:57 +0000

    treat worse necessary keepup apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_area_rectangle(l, w):
+        return l * w
