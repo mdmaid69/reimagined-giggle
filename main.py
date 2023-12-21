b542a78d372be5bd0d86bf7380dc031ba219c3bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:05 +0000

    back tape into short vote

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
