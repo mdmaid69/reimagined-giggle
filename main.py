b16b7f7de613d37678ae04db7a2f8a2d98b30937
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:49 +0000

    mistake upset advantage kind smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
