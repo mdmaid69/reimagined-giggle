f737a8d80cb22b5c42e58d10e922d3f70ef297a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:05:21 +0000

    gentle comein point spin taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
