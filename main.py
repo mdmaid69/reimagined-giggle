cc8aee825454fab13c6dd3a66f20d9e032ee0a6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:46 +0000

    crash whole judge breakdown OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def sort_list(lst):
+        return sorted(lst)
