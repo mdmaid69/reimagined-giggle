2d48e5c4cc2434a0f4e25f29398ead48d03116c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:34 +0000

    wing comfortable store affect drive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import getpass
+def get_username():
+        return getpass.getuser()
