eba1925faef21513922e7b0067496e4d8c268755
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:15 +0000

    hot question match giveup punishment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_volume(length, width, height):
+        return length * width * height
