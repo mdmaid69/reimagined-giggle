00dbe38ce2f3c89bae14698b5203d981cfecb142
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:33 +0000

    danger Jj mess backwards possession

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def cube_number(x):
-        return x**3
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
