2895ad16065eb2fc9649b90c08080aa409e4597b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:21:15 +0000

    round proof decide share pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
