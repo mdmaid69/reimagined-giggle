ace402b30d380aaf4a1d94b91e54047563771c3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:52 +0000

    round welcome summer order wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
