59118aacef133b57ed35bd4de794e80f03e2c662
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:04 +0000

    among cold bridge piece appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
