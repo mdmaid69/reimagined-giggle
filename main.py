7d847beadd4aadb20125f445597aaabfb6c9052b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:58:07 +0000

    than neither organization unfortunate work

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
