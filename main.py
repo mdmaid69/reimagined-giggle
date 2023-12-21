f7a27df2e7c7944d46ba592c7499cd57439aa714
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:07 +0000

    believe record silent inform talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_string():
+        return collections.UserString()
