f528fe2c0b1b207d61a0dce5122c37bdf4bc21fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:54 +0000

    eat regional wish tight Ll

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
