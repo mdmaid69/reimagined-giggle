b99a82db5db5318b4e580ec03bd3e01ecf2eca05
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:36 +0000

    load invite gift practical keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
