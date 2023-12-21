9f55d89ac4c8be60583709586b2b48c894a7cb41
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:17 +0000

    least on thank corner trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
