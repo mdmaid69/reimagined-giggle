1465d23a7a026f258b61b5a484979969435e274c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:18 +0000

    would among notice careful instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
