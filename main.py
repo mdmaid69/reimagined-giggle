ac99f435623f724ba903ac4cb92b8b9e702838ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:25:31 +0000

    wander connect goon age history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
