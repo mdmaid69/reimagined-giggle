c24e9c178f139735192965a146f445ac85cb0edb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:30 +0000

    remember breakin each compete invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
