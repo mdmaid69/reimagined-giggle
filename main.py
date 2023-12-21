3eaca36e482da56097db86f426cbda0830bf0c97
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:29:28 +0000

    present set mail birth command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
