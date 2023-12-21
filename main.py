c50ad93bbab5202991a71e1424ddc13b4176ff6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:19 +0000

    section 570enjoyable year confuse busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
