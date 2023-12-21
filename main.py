93af4b38194404d161696e77b4dd0f439c3b03cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:03 +0000

    disturbing deserve false round certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
