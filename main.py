1a31ed48efeb70a337606ea3a99ff13ece636d63
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:03 +0000

    aim false letter Ii bound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
