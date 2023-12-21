d395c74efa56e96061e4163d4d783dc249f1a984
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:37:12 +0000

    heat together state lucky case

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def add_numbers(x, y):
+        return x + y
