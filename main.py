ca98dbdc0d985a1e46d994837aea9b6e649fa65f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:36:12 +0000

    crazy beyond what consist by

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def greet(name):
+        print(f"Hello, {name}!")
