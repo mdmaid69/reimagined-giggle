4f1725260eb2bb0ab5308003448741a111ea33c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:41 +0000

    teach turnover programme answer by

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
