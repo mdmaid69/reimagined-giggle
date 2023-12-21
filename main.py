3e7788352f23439a8ac717412dd826997d67ce14
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:34 +0000

    safety whisper popular child describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
