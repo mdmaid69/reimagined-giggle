811b1df7d5278e0c844e8a1b36fb101ec05faca8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:26 +0000

    touch fact night nerve fail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
