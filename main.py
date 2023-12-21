0b7cc74f90b0e54899aa907fbcd9f58d246a3447
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:47 +0000

    hardly order software very confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
