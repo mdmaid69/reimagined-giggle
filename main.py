a9affa1cfbbc2d9563ee327a163b4f12a3a88303
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:54 +0000

    breakout still opinion Hh story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def add_numbers(x, y):
-        return x + y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
