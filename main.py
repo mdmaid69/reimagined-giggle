c3852385e21687c92636c4d27a134d6e67bc3cab
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:39:04 +0000

    type furniture person more left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
