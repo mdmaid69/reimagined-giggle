aec84ccf38bd1ef66d436e0cea4030cd9457239e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:16 +0000

    agree place considering sensitive on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
