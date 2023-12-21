805e45e8f1ab79fbc8ec35155ad5fbdca66d7217
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:01 +0000

    understand fetch loud sound story

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
