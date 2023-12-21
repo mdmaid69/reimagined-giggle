829afdef243d5fb8bc8f316d20b677133e4c343f
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:58:38 +0000

    march bringforward fill hurry pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
