29fd1e06437bab6e24367151477eaec01dde0dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:48 +0000

    our wave corner nearly play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
