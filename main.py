c1eec0916f8083d70cdd1a6d03544ce576e4081e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:46 +0000

    laugh gentle die the door

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import platform
-def get_os_info():
-        return platform.uname()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
