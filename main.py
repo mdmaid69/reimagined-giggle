3f6404df35c4385e0641999fc947ac45f86962f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:04 +0000

    whole estimate tooth chase block

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Reversed:", text[::-1])
