56ca9e55eb3497a0d110d5d1fc0a7f03c31ff695
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:24:52 +0000

    mouth uncomfortable search hole identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_current_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
