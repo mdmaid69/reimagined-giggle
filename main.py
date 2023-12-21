dddd37d8029159f861b030f3578a5e76d69108b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:40 +0000

    introduction argument unpleasant press move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_float(array):
+        return float(array[0])
