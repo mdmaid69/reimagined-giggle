8182751b47dfc1ace318bb5a0501287150c3dc24
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:49:40 +0000

    cost world Nn key separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
