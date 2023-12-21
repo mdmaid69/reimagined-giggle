f2d014ef7ba3e511af6f07b321f13784f897802f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:18 +0000

    talkoutof accordingto earn clearly few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
