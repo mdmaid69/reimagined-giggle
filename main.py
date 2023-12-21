7b68a36b70371bf94301005617e405e4a114586a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:23 +0000

    deep below before mainly moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
