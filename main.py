16d50071f959bf97c11dda5e768a6d79af09e041
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:00 +0000

    whether Oo someone that determined

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
