9347152cc703b6fc5a7811bc921b7e2983cd3d08
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:08 +0000

    store respect still me apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import datetime
-print(datetime.datetime.now())
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
