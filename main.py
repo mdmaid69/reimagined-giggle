5ffee7c98ca59364e29b34a8a9415d0399d483ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:41 +0000

    murder point honest picture unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
