273889a1018e9f437edd7f3799f6cb6223e63066
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:15 +0000

    during concentrate date legal limit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def sort_list(lst):
-        return sorted(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import os
+print(os.getcwd())
