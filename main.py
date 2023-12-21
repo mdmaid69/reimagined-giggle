83816b615dd78a1253f8d364a158975ea77f5461
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:09 +0000

    approve strength Nn over by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
