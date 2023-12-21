b7aa799bee64914f99519d735242a8e247860a7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:27 +0000

    example complaint exercise harm chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
