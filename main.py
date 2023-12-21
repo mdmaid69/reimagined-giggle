c960b71cb91c04a547aa1f60a0321787b389701c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:03 +0000

    turnoff across side wipe immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
