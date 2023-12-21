4d4b659f59d310cce81251dd2ce70fc81f6a2512
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:53 +0000

    runinto fly money me fill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def subtract_numbers(x, y):
-        return x - y
+def multiply_numbers(x, y):
+        return x * y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
