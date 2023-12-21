e33861ed2b5249f8cff3c8a2a6cac295f8b50e6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:49 +0000

    succeed anywayanyhow deep clearly more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
