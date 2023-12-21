965952e72a70c9e4be4ef7eebadfd00c40bd646c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:00 +0000

    finish related defend branch common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
