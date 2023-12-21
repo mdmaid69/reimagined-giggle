79a4224a7e29dd953cd8c251e3465372d19f0efc
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:48:04 +0000

    summer use fill worse sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
