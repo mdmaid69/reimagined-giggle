76987bbbb39618ec8e682b807a9e179cdb03d04e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:52 +0000

    money whatever hardly part problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 def subtract_numbers(x, y):
         return x - y
