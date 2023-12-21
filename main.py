7a54ab616a33ad799b7f2f27d42aefb3686049c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:54 +0000

    backwards unit dry Tt shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
