39c7b6273208ae51d04f4f2ce46204227a0559ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:00 +0000

    allow instead other Gg loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
