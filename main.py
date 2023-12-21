f2f417b77c12c4bbe0f126131b12bb91bbab48fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:36 +0000

    play imagination count forget pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def multiply_numbers(x, y):
+        return x * y
