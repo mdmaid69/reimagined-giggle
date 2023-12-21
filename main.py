65b630eb68c98079f0fe3263cdd8959c993aaa3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:03:34 +0000

    do hold joke shut step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
