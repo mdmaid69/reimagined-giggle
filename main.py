f6c1ff2d3fab170fa7f54512643fc503313c6ea8
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:27 +0000

    deliver regular supply nothing Ii

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
