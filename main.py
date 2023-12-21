b748963ea092545c5e3338bba641dc5507355fef
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:23 +0000

    doubt complicated branch fear relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
