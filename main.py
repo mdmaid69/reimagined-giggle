cf51b783f857fee705af984572c5f2d3f34717a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:23 +0000

    square pattern negative image level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_user_dict():
+        return collections.UserDict()
