6efa63dfda1e715cb5de7d05eb9bb980d6567fab
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:47 +0000

    impressive instruction tape fit statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
