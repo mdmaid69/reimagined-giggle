1b0e6542dcce9163d2e12b40bf2a3cab4ec3b708
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:24 +0000

    style trade half quickly trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
