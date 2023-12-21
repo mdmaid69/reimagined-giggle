eb4f7a6584528d0bef05b40632c413174bbca87c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:30:08 +0000

    recover criticize evening priority fly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
