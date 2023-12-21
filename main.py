1119814ff25bb2783ab27df284f804539771c506
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:59:12 +0000

    something major 572enter experiment exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
