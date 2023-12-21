5003e60d1c3c8ae3a7bb2cb44d3b9a056fe83aaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:21:01 +0000

    weather search result letter understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def convert_to_hex(n):
+        return hex(n)
