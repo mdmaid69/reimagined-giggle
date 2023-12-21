4f5395f3e9bf33199209000103075c444ddfe3ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:23 +0000

    game main though contact further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
