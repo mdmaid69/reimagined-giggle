acb627bf628d745525dfe1e55b488e584dd88b73
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:56:05 +0000

    brief fashion breakdown remove improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_area_rectangle(l, w):
+        return l * w
