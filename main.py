a7089951af5f07bbb17ac5f3030bb80cb16fe6a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:19:35 +0000

    oil only learn scream interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
