a15f0975b15d821eeeba1060b6c4b0323238eec8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:27 +0000

    average slight trip relax tidy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
