b7d47ca56e363d41f1257b6631be37741c4a9384
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:02 +0000

    hit man sensitive plenty describe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
