04cff5e8725e47a19d8d81aed440361322be847d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:23:38 +0000

    character her sting lay judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
