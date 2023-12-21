ff1a4662fbe4765c6cb904eb24b90253c818bb38
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:25 +0000

    equipment file cap original plastic

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
