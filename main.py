9e789be866c9080fb2c4cf4ba8ee925e18c286a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:38 +0000

    noise sight none stamp fresh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
