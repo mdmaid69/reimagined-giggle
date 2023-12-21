1bc4f6b4a6703d14557dc28269de5366b9deeff8
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:17 +0000

    force thumb joke hers public

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
