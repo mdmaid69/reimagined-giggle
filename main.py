30ed81a89c7f641d19894a94f70913f2e42eecc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:52 +0000

    hot bill person full each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
