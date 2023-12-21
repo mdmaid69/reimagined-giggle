97a096517760ba0f637823cc0e1c09289b5602f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:16:52 +0000

    rather material announce speed tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
