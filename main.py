cd8ef7baa26a8933f096da69d0efa3deb77f5229
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:21 +0000

    nature prefer not run until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def add_numbers(x, y):
-        return x + y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
