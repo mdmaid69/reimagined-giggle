073660192430ca799f486f92fe567cdfbdc7b290
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:03 +0000

    run tomorrow I off spare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-print("Hello, world!")
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
