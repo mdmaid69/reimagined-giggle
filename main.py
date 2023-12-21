b4c33595eb9770e1c7758b20b4a5847fb4b6179b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:45 +0000

    fail strike word rate model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def subtract_numbers(x, y):
+        return x - y
