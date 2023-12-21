7655853aff03e41a88dcdca886dc290cdbe8ddb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:39:12 +0000

    much birthday narrow wall anybody

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-print(sum(range(10)))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
