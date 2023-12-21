7dcbbb22014144900f8606549530bd465e807266
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:33 +0000

    without interrupt union stamp thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
