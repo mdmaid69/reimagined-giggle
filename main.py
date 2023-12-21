8f2511ab67c49dd7276cda89e531edd4e572dcb0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:17 +0000

    belief slide meanwhile comefrom noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
