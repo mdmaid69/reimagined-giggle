13ae6cb745b988cf1e3481678605c1220299aefa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:59 +0000

    road slip afternoon tend fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
