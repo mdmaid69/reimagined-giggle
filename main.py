3b3aec5c00a7a148f04053d5361176d8956abc71
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:17:58 +0000

    around Ee event cause bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def roll_die():
+        return random.randint(1, 6)
