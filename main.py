f3ae10060705e36e0cfff470144449751788ed29
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:44:38 +0000

    usually to petrol sweep afford

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
