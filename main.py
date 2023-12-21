a5028ffcc004c7f660e96a2ff408bb41af80172c
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:05:32 +0000

    gate imagination niggaacademy plan offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
