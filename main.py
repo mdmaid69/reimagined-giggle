fd00aa4279c1d63b22d2ac6c75532075beb099b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:29 +0000

    taste create judge explanation consist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+print("Average:", sum(numbers) / len(numbers))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
