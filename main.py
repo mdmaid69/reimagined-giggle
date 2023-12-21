c3fc9db683f204df5dba06657bfa5188a70b1b8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:06 +0000

    sort mess entrance fear boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
