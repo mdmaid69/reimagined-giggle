64507528c0329981ba3415e4a06b50f6b1d3b3fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:17 +0000

    unusual takeout no burst none

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
