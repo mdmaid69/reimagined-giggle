41f09124abe0f24c393c3e83ed6fcfc5566fefe4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:24 +0000

    slight our unlucky gas thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
