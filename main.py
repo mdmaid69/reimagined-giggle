c5d2fdb7d571b76bfdc354565d410dd2223e44eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:06 +0000

    bar fasten cover tune respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
