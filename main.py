f94aac8181974c5b5504442e3b2c9027f0532125
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:13 +0000

    water task judge tax turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
