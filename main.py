087ac0b44cea98b79b39800fd110b29f403585a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:11 +0000

    become soak wing order press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_tangent(x):
+        return math.tan(x)
