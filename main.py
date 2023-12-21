134f9832ae2bfdd9e137bb6c59cad6e1e8eb7905
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:34 +0000

    centre region now clean visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
