8b7f21debc55cc5ac8a49ba7f4af27363c1979b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:46:10 +0000

    believe dick thought then sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
