80c8223a7fd9092d0d2d22f804d76b1cec08597b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:48 +0000

    unfair day guard great custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
