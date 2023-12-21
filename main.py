c96409ad35c0b18fc64f5b6a02424eef5def4ebb
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:31 +0000

    work something basis hers post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
