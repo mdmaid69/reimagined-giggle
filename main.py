38c2214937c31f06a461f67cd6d8e4d1b7b072a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:40 +0000

    release correct relation discover mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
