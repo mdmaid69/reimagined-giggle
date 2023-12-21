88acf8f5aaf72d9bcc0e3aeae47bb66d1641e881
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:29 +0000

    pay dish other ill difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
