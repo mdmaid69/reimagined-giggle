808484fc8edc6ef42c3dc2c300301db5236f94d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:31 +0000

    Nn of production full possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
