846a1cc29e006b99f2f52cce857c44eb0aaab5ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:13:43 +0000

    instead wise quarter nothing company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
