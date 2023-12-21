790a123d03bfe66ef5730f62591a626b33681116
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:45:24 +0000

    no behind holdon ashamed next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
