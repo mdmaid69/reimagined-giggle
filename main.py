e7d75e19528792f302c69fa57fa694cf3b2a6002
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:30:06 +0000

    seem whether mass listen understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import collections
+def create_queue():
+        return collections.deque()
