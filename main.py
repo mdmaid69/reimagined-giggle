db7ba50b9cadd2bf763012fec7509b3d49662ff6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:36 +0000

    seem activity only moral breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
