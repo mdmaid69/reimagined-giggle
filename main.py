fe3eb2de5146abfe1ef0a3d9b146d31933858863
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:06:48 +0000

    mouth mixture cough child coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
