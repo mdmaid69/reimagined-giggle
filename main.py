f4e901dc8bdcffdcade91e417a6a23701e056940
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:21:43 +0000

    abroad measure 564encourage opposite sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
