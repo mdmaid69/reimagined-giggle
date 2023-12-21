fa7c0427a240791adf01dd46c8ae5e1e3e7eaad9
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:10 +0000

    ask regularly takeout pocket anyone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def is_even(n):
-        return n % 2 == 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
