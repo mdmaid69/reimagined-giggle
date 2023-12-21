4ad36d6e0819ce0d4d04fb45fab7372ec2d2b727
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:19:49 +0000

    against stupid water book Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
