070478f8017db66f272fd72526e7c73c9de46eee
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:49 +0000

    destroy shopping disapprove swim comedown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
