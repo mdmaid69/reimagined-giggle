82ad539559cbdf970b2b81dce6307ddd3753854c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:03 +0000

    protection she near consider wave

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
