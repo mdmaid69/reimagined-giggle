ff7d58820f93bb0bfa325deef5f9a0e8f78f13e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:08 +0000

    chief Dd mail man sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import getpass
+def get_username():
+        return getpass.getuser()
