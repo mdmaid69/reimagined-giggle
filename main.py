d944fba7da72c99f4f7c7f3615a74d2bcbab1418
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:59:39 +0000

    correct practice hospital best fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
