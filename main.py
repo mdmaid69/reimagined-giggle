1df64de97a111aa8de7405675a31f27d9c8186c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:51 +0000

    past combine unusual minimum drink

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
