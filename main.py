08aef402fe593695c2f1bc902139e40d4c13c9c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:29 +0000

    simple afterwards giveup lose shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
