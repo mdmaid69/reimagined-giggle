acd8770b768c929ba2364a57631e1a2c36e41e1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:10 +0000

    border cut their bury laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
+import time
+def get_time_since_epoch():
+        return time.time()
