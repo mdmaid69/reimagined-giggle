fee139e2e0c34076d8f5cbf81a722ba7257351ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:33 +0000

    slightly blame hard cost into

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print([x**2 for x in range(10)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
