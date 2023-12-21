59430d6551f955b13bf6f90c7bd36a660fe2dfbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:48 +0000

    effort few Bb principle loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
