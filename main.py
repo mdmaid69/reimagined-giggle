192dff76bd2b3cb8c8568f12fc8715e5f7b3783e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:36 +0000

    quiet takeoff float watch suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def count_elements(lst):
-        return len(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+print(os.getcwd())
