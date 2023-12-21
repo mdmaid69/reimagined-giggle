4f624ed9c87172341b38d85db9951bbb2606436d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:12:38 +0000

    over bright touch comment set

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
