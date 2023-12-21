6b62b4584cbd64427e7df02c57e5d72a5f8aa1a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:17:50 +0000

    dish obtain instant trouble scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
