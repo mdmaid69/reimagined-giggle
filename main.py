ac16ba9a0d41cf36c5111c4a3a83e96d0705cecb
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:27:45 +0000

    block name mine instant disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
