e900951e3ec541bd12447c199aaabab10f592a7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:06 +0000

    bring means sometimes neither connection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_power(work, time):
+        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
