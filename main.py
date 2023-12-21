3b0e5ce43d2b13c57182408a17f5f114a7c0863d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:10:06 +0000

    awake story mainly grand same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def find_min(lst):
+        return min(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
