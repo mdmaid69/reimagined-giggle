8763090df001e3f801fce95d197696bf37e75a85
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:51:44 +0000

    season dress shop worry treat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def remove_duplicates(lst):
+        return list(set(lst))
