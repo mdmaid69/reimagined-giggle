dc3381f4fbfd2752b62f1083e0e4e866029c627b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:10:31 +0000

    word narrow pronunciation manner rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+print(math.pi)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
