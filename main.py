7c03527675793c4715d8e601cea3790241f49923
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:53 +0000

    national upstairs wide beyond throughout

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
