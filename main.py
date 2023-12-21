f6915c3a71348a0fb58ec36a3af33d6c28da50bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:11 +0000

    measure shrink terrible belong simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
