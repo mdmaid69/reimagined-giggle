05ca361547fd00d7d1b09fc8508ea67ec411bf6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:09 +0000

    exactly takeout cry interview 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def is_even(n):
+        return n % 2 == 0
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
