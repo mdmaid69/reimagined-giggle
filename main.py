6a94742a396d393df1c191e097fe4e4e671a6f4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:50 +0000

    breath tidy result convincing listenfor

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def is_odd(n):
-        return n % 2 != 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
