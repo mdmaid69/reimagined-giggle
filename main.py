433e51d3c572a7fbaddd65bb6797e0dc546b24df
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:00:16 +0000

    letter happy no brain get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
