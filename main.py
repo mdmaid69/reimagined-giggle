e849da1d7706eb70c178170ae28381639e7cd021
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:54 +0000

    replace place progress declaration geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
