df601d7a60a54648b4bf294c90ab371cac1b3237
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:47:30 +0000

    excited wheel behaviour principle breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
