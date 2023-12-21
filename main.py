e3f1db6edb4fa2197dbdcf198eae190a9a7f48bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:38 +0000

    punish everyone possibly manage tune

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
