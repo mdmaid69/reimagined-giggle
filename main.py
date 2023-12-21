c2e09482ac8cfa83ca78065efe72c0c78169604d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:36 +0000

    hide tool now disappointed base

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
