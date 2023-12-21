43b989e1acccf6786b4c8ceb5d75a08b809f5c59
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:02 +0000

    mix expect temper yours search

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import logging
-def log_message(message):
-        logging.info(message)
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
