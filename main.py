2b4acbd0658984b6ded7d19f2cbddce3173402f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:37 +0000

    minority forgive get leg cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
