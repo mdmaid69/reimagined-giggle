6b31a3c493b5952e388fb00b020d90c4f1fbae9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:13 +0000

    concern another joke equipment when

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
