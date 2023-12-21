37f18c86f62b466587e95d9e289ecc524e2718b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:18 +0000

    increase thumb translate most thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
