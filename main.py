6dd9d398365b24206f5d790361a45eb69ba1fe0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:01:52 +0000

    workon piece cinema cheap minor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
