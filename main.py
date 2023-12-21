2680e00fceeb9787ab8e42e31401fcddd71947c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:12 +0000

    remove then wide clever wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
