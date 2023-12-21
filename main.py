91a4aa40a0acc331021b02624107184267859568
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:53:20 +0000

    winter probably precise size wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
