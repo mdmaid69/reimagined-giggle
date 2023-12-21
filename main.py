ee4debb53bbf3b9c68b4ebf3fb8b85c6a184af2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:05 +0000

    view new way tear yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
