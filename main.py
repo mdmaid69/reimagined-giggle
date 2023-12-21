9a5c75d558fa080a0fa2de8d2f2cac1f194eaed0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:03 +0000

    takeback smile communicate shut try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_bytes_from_array(array):
+        return array.tobytes()
