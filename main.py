0f3c656331ea6909bf46ae59d2ad36ec34f7eebd
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:47 +0000

    strict arrival pure special extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
