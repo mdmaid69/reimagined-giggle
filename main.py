7551fd4700cf84c11051d2d1c468d8e2a4792cd4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:35:11 +0000

    defeat oil lookback(on) pleased collection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+for i in range(5):
+        print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
