7b7f6e5d2ae4833bef4784659b84096d9f8b8841
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:57 +0000

    anybody length would opponent bury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
