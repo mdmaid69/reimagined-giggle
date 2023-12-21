b3e54427839fb8b7b3706ea84741c7f8c7d0f651
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:44:50 +0000

    train label agree direct giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+print(math.pi)
