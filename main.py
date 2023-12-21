20d8f80e43b616b72b389a0df39715f178bec23e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:09:55 +0000

    total mark funny impatient speed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
