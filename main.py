4ceacd6c59c39203b047594a6a33b1f56ad24508
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:26 +0000

    worse any send boil replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import time
-print(time.time())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
