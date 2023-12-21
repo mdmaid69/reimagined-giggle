22481bc0211e53b7f60ffbfe4b38425ed5fa7117
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:29 +0000

    guess breakout goback(to) ocean union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
