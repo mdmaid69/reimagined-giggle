9d5ff280bc3d9303034feaae0a3c1244bda9a1ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:31:55 +0000

    train expect opinion identify push

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
