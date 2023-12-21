5e8a029e6a833b1ca1ee9869417e8ac98378a040
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:09 +0000

    hello memory understand interview answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
