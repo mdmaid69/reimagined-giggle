5efac4c3e69abfe1f0da12a6eacf2b5680a2836e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:36:01 +0000

    port join over seriously evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
