2d72222c9c05c4691af58419414be8747d28a1f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:08 +0000

    callback accept bank offence pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
