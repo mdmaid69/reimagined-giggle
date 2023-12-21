e085816a6be9007775b391f4a5af314f295322c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:26 +0000

    pool moral chase equal scene

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
