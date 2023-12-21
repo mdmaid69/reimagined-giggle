bcb3a1e8a1e1a22398e6b9fcf838ce876f5b6084
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:12 +0000

    quickly tie funny contain heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
