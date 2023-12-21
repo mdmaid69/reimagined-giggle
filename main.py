2e683fe23fb2c3c53e518906c9e17288f2d7763e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:12:43 +0000

    post welcome promise shame free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
