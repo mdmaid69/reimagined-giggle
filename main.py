aa68e2a6ba51e1239c15be9090d712128a26e69d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:20 +0000

    live complete breakfast against and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
