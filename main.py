7b5dfae4afcd78b27340115a55b710c4198042ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:52 +0000

    road fasten travel Uu smoke

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def is_odd(n):
+        return n % 2 != 0
