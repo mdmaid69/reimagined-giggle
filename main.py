327f4ffb550e22c9c36b6d90c2dcb507c780bb60
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:09 +0000

    climb point keepup careful display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-print(sys.version)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
