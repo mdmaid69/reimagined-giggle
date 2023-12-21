1ef64589baf9cd4e62ca121c188f46198e63101e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:43 +0000

    plant somewhere description us giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+print(math.pi)
