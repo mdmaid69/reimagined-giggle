ee5094f4089ae010cec632618fa9fde51975d51b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:23 +0000

    influence centimetre right fun fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_force(mass, acceleration):
+        return mass * acceleration
