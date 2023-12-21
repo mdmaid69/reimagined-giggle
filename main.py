da10b49259cb214d6eb68c73ddb6a265f151fa77
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:52 +0000

    572enter accordingto change wherever study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
