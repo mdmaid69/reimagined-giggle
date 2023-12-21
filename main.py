7562fcdf0dceecca7de06509af762fd507158590
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:19 +0000

    young kind off example keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
