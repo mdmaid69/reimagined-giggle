14f7e175afc179cd9fa0974b1de369c9061f9a32
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:32 +0000

    tune think shape fix government

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 def calculate_average(lst):
         return sum(lst) / len(lst)
