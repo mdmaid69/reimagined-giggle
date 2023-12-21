dca0d175dc1702d9284c98e32866f6bba8a12b23
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:40 +0000

    clear impressed leave confident connect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
