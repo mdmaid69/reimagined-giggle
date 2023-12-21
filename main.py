93896e294364f088cecab92f5fb191f6d77ebec0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:40 +0000

    tip type fair minimum light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
