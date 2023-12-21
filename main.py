a32224c6d4884583067b8118c84d74eede4efff7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:53:46 +0000

    571enough nervous splash satisfactory cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
