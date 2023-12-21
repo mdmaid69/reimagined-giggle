63ce43437facaf969a7bafeab0a1122d14566209
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:47 +0000

    unit hardly poison afraid tense

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
