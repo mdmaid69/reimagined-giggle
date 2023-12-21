7dfe1f6d01f8d262124ee2a3bf45d55a05b986c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:39 +0000

    dig slowly quickly regularly round

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
