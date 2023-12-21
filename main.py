4afb2d74297293fcffd119562f1367dbde511094
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:34 +0000

    scrape flat comment bet whole

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
