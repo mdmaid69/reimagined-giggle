e31b07bf04a4f392f4d875627aada74b591a5976
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:00:35 +0000

    shop front promise objection use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
