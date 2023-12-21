0547d1a07c45ebdf37123114e326e5b5666a31ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:09 +0000

    quality close worry somehow muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import datetime
-print(datetime.datetime.now())
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
