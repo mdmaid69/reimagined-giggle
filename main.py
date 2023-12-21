5cee8839101e7508bc429947cac7de409366213c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:26:34 +0000

    unpleasant protection flat lost succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
