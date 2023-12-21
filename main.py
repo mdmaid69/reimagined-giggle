d503fbfd126a9ece4b3b21a5706c0d7e26da0a24
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:39 +0000

    comfortable exam conclusion machine else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
