ad6dfa7962e884fb1c29829115aab1a1e22a6def
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:56 +0000

    work pen month only what

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
