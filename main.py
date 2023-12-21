ebb456bc28138a0fccf65a9b7c2509028776c96c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:50:23 +0000

    less common hand decision considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
