986b5d0614c749d6a4c759fbcf8057362d26ab72
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:53 +0000

    afraid window fairly bend slight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
