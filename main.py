6be8ee51baf1fb260b62074800fb155b6a829528
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:57 +0000

    pen moment middle well equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_int(array):
+        return int(array[0])
