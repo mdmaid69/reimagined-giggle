43b2b0e3287ca743dba1fdafb1794f1878a1d82d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:51 +0000

    lower object considering quick edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
