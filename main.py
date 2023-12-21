ba04fbdaf3ecc43e60f036f4456fd538cdca7fa5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:08:32 +0000

    nowhere high call mainly soft

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import random
+print(random.randint(0, 100))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
