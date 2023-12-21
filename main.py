ff5dee3d8ed7220cbbdb31be9c2f70e84966ceba
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:37:35 +0000

    eager modern fine pin everywhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
