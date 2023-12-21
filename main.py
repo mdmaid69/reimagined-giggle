d5f5784eb2c92883d5aa0d0035190a148e32087a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:21 +0000

    soak considering neither best label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
