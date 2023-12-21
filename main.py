ae3d7b6586298f3ed87798ed7a847808ab28c88a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:44 +0000

    mistake determined recently stop view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
