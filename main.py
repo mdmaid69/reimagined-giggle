2cac668d67ccddcb510bf735a8b5d49d6fdb131c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:59 +0000

    delicate load who equipment pity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
