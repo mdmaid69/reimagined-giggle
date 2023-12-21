32c3a59e6d01f890fb1e5c9a563507e230436885
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:04 +0000

    usual skill getup vote breathe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def square_number(x):
-        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
