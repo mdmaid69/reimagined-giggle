ad187262e6df5f8ee7a1ebe957c02397e2f6b2db
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:00 +0000

    emergency drive first poison pipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+from collections import Counter
+print(Counter("hello world"))
