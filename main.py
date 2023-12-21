53bf138a4b7fcc1ea3f8f473b9cdfb7fc5ddaed1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:38 +0000

    rather polite match pattern sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
