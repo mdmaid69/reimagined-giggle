b4ea492203a69e1d12f5ecbfca5915f7bbc7c29d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:08 +0000

    turnoff movement scrap tomorrow floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
