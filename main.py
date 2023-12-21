887d8ce972944d3e0f0a41c43805cccd401e9ccb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:49:49 +0000

    answer whether practise round sit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import tensorflow as tf
-print(tf.__version__)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
