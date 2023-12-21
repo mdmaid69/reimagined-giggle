efec911ee7d4f8fb550f6c8a5e54215d1ca50612
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:13:56 +0000

    but raise book load arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
