5075c041cd7db3d9a6c45aef439df60b76f1a4fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:50 +0000

    manage carry appeal door second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
