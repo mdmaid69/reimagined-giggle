95e98dd3943af26f44fb4740cb42bd2d1d7278b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:16 +0000

    state row stop spare plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
