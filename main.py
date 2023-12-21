a4c21e2552726ed3c856f7e045d8ef5baa293c87
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:20 +0000

    quantity live hungry meet seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
