a4c4e03db6804f2f40508430cf06e9d00c4e69d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:20:01 +0000

    else succeed money trip goout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
