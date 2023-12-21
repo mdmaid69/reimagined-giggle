1b575a62883824b32664fd98bed8e5fdb630cdb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:02:07 +0000

    noisy person home true second

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
