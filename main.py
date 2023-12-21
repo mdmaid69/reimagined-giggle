87a4fbede14710cc9debf0e6169de3f233e7b334
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:13 +0000

    pen he schedule increase far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
