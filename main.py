46603d024a1bfe98400a12604cd272c1c35898c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:18 +0000

    which also science instead sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
