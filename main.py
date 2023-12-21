40bc387ad6def5db19b3a0a32faa71a41cd37892
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:13:04 +0000

    practise involve gas usually town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
