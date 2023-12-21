7f8e7e84e178bc9b52dc130ccb4a89d6d348ced3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:52:12 +0000

    sport stay beside while suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
