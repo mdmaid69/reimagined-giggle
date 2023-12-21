719cdd2c17971d635ced84178ab192e589058202
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:24 +0000

    talkback like free offend thinkof

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
