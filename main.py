f132a1e79998fe648dc2e81e0ab0dd9abf7429fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:21 +0000

    talkback like roof pure turnoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
