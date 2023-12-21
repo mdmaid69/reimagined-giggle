6cde186cfa7e89d09f3e711c7ca1aa3430d04f49
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:56 +0000

    safe private gate roof fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
