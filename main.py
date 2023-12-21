f7e177e1bf16ea1152b7a5f87db6cb85a809258c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:10 +0000

    rough opposite fight spray address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
