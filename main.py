2430c13f0e17732c056578c9d1f662fa047a7c4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:30 +0000

    callat bar pure measure rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
