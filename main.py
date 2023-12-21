501273069ac9bccc2697244b70cec5c4ecc28a43
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:37 +0000

    able lookback(on) runout discuss trial

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def reverse_array(array):
-        array.reverse()
+def is_even(n):
+        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
