dc49365b80c0b9a0726024ecb6465dd850b840e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:11 +0000

    upset top choice surface quarter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
