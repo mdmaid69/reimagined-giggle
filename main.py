59e65548d1b355222f79c48891691d577e731e7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:10:24 +0000

    ride phone travel bunch soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def count_words(sentence):
+        return len(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
