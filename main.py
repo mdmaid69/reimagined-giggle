105e5f1a591faaca3f664a03f02ae31ddd8cdc8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:21 +0000

    maybe clever area dream allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
