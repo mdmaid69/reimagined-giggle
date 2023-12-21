15ce9d4bc33db925cf94ca6a13288d143d2a79da
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:46 +0000

    sell government Rr opposite this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
