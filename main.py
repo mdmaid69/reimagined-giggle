cb26953acb25afbf7a3a0c14ffde8e8e24a4393b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:33 +0000

    disturb among treat float near

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
