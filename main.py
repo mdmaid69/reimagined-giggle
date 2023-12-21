f1139187432e22c0d4782db95bb6f4ef1b473358
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:12:54 +0000

    towards ilovedicks drop through pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+  import sys
+  def get_python_version():
+        return sys.version
