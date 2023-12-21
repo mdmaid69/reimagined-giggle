6a257143eb96e17148240f308c9b936c43c15658
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:36:24 +0000

    way drink rush combine distance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
