6f68394657bb31905271171cac26821440ed3299
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:22 +0000

    beautiful string imagine tread wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
