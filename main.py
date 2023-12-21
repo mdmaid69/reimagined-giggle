4b1db11db0f4b27fd863761ad34b2f36ddb85012
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:24 +0000

    match minimum meanwhile scrap reply

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def add_numbers(x, y):
-        return x + y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
