7352353add3612a733e80a5bbdb6507125d76dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:16 +0000

    piece eye disturbing fond there

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
