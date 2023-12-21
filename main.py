be150e7566410909003cce18d89bb045b182eb39
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:15:22 +0000

    draw smoke limit discuss beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
