13ca6101f4afd1aecc12a0217aeeac5b159b3b4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:45:11 +0000

    meaning that mind fix even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
