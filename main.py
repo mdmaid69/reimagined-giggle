c14fa15f9bde17f099e4d3181d30551a67a95ea3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:12:08 +0000

    basic degree reply noisy press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
