8b6828260098eba347fca109bb0a46d8439739b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:11:39 +0000

    tour scheme and recovery song

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def create_counter():
+        return collections.Counter()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
