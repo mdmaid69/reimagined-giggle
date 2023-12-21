9faeea720d650e124f4ff3bd4f2e83ce4dceab95
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:04 +0000

    might holdup excellent apparently most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
