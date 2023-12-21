1569487600fd345149efd52fec48cee8dd8f2f1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:12 +0000

    habit bringup computer right pure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_speed(distance, time):
+        return distance / time
