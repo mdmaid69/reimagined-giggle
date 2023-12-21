8666e9f9451d1a22c890eeb965c077cef1ca7b15
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:08:36 +0000

    supply spare takeover pour wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
