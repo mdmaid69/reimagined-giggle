152b1e7c61936c5ddee3a191d3749dfe84171e9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:29:22 +0000

    sad exactly smell car reduction

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def square_number(x):
-        return x**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
