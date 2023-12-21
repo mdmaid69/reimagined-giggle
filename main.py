fe2390c068b0732401718ecbee7c2462745e684e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:06 +0000

    see hit fight club period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
