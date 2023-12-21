b3713bdfd2e317a3553f1e60e7b14bcc840aba3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:22 +0000

    respect normal great alone interested

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
