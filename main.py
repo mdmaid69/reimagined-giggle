b2573114fc235ee90dc0db1149c16f3a602fb5c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:20 +0000

    tip approve translate opposite same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
