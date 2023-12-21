f9d75133fc6919a1a6f23601679048a0fdc0520e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:37:58 +0000

    year autumn never ready scrape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_user_list():
-        return collections.UserList()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
