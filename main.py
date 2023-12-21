c6a6e0f36bfd9919b0aa7380a979eabe3cefb160
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:47 +0000

    view blue smile save rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
