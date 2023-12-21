1a899fdd545d6780130a303992a5b7844da8aa31
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:53 +0000

    strength occasional coffee emphasis mood

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
