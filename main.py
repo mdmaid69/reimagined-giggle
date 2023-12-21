76e1fe9492d9e91da5aa8d58f905f8b7775cbed8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:17 +0000

    certainly programme healthy nowhere cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
