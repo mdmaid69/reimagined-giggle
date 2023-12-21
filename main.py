2ccb680f9ffe43f05da996ca6e4b2e65bbf91550
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:42 +0000

    callat disappointed none delicate related

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
