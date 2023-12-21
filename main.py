66e2caa611a909b6317ec627c52ea40e7ae0fa3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:42 +0000

    slowly smooth present prevent like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
