85639aee4a54477584ec695787fceb7d5ea4abbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:43:25 +0000

    open illness maintain cinema contact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
