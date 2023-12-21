99f2d2fd6d515e971aca2e1824d315944feb105e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:11 +0000

    fill strange listenfor visit total

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def multiply_numbers(x, y):
+        return x * y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
