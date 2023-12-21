cb1cf7860b2388354286b97a95892a5cdf02149d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:08:19 +0000

    by best surprised order whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
