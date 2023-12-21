554231cb97676445c761228c9e8b441381668ff0
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:49 +0000

    rough mouth shame reduction than

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import tensorflow as tf
-print(tf.__version__)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
