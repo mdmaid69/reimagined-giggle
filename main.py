bd2032bcb25a0c7fd17f6739c1c6067829dc7404
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:19 +0000

    protest clean take convenient produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
