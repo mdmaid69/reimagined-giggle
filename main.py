17a650151716b7fd1986a42ed56d9abecd8d6599
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:11:03 +0000

    address whole tie arrange rescue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
