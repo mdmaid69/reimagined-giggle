c3c91b1178a4c666fa0480993ec719d967e5f734
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:41:20 +0000

    all something exchange spite window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
