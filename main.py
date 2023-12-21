6985e201eace3c1f407cfd924f707670e59b328d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:12 +0000

    exit correct runover crash load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
