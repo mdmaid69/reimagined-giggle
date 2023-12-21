aedb8077e58698eca866c5e54c0ede61d2c038e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:48 +0000

    section left accept hold wind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
