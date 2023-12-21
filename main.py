9c5e13e9ae27bea9143c419a9220476dcb840497
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:30 +0000

    develop invitation snow world jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
