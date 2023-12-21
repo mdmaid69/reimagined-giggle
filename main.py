dba3bbf21391de74ae881ea2eb712f378330a35c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:10:08 +0000

    whisper design habit pick fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
