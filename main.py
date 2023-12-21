ada962288132c4a45fe9a2e81cbab9b59e38e4de
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:36:48 +0000

    cost division war little man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
