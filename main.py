b75144c2a4d65f4be54530284eb7b50c657ff9a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:35 +0000

    fit ill minor willing quit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def add_numbers(a, b):
+        return a + b
