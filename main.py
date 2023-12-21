d01dd59592e483f11ffd40cb840342707b80e823
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:50:11 +0000

    out defend journey destroy language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
