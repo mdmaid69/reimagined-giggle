cb48f056af00f2a9bccee9de8fe93d6ae630eebb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:06 +0000

    there remark recently approval shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
