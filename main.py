7b922196b854e373c41a466c2ceb6ad4e68987ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:01 +0000

    accidentally run pot boring control

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
