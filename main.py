e03c154c978449f8f22f826ece33564a2a53c46a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:34 +0000

    shout its exactly interview responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
