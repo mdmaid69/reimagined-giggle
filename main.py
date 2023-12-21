b3ff09b2a2dc58baf9208c067745840405655b1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:33:18 +0000

    keepaway drink size turnoff smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
