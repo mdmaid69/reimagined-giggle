0c7a085fcb5ffbd25e3e5a9c7d20245b79e9359c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:26 +0000

    some risk chin soil represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
