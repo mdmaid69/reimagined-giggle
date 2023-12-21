fe792b4ba0f22eb5f4e486cc58e3af37539471dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:31 +0000

    violence bottom scream on unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
