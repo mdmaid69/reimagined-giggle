f9becb226f479c41ac2fb9655d20060b4ccf7dff
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:24:29 +0000

    add takeback Bb somewhere principle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
