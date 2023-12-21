a04472b66261c5cfd30b5d6d3c2e10f2c592cd9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:33:25 +0000

    laugh lay they part sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
