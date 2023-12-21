641ac07889561b1fee1c4787c6510f7e5044c29b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:46:39 +0000

    smell platform borrow stupid statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
