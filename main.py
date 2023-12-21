8ef0706cfb9c4cad381cf27983034884d97974f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:29:46 +0000

    please prepare comeoff guess main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
