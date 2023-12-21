1978e40df7072337bed6683433a8e8618f6ba5da
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:23 +0000

    dream steep figure out rubbish

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-for i in range(10): print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
