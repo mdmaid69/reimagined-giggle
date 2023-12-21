788c548d5f39a8599f4f99f5b927abc6f64c237c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:12 +0000

    use from interesting purpose keep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
