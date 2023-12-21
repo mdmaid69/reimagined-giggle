836447749a529b85e58a85025a06dd072747fb6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:45 +0000

    Mm interested typical can excellent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
