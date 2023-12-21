f17743ce57557477b2d40ce10899d80d29239fa4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:29 +0000

    foreign treat door giveout standard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
