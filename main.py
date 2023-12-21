a4eb60db57b5a41e759440fe2f6c3f5047f4250b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:47:33 +0000

    lookafter why uniform main advise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
