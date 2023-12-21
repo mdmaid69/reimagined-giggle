9fa05b59673c1e85790fe21060554f87a59881de
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:07 +0000

    seriously deep deep destroy on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
