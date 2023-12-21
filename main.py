e4174cd158e5b15363b838b5c0e30fdbab15a6bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:09 +0000

    us book proud car steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
