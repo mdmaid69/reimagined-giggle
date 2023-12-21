c31386c209f47df91ebfe48a54a3e20bd35e55a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:51:57 +0000

    but might arrangement convincing during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
