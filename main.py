ec21a4c69b124b64a2dd02c7343117182255a7c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:30 +0000

    personal study 179bite smile concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def reverse_string(s):
+        return s[::-1]
