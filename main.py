bbae1978a817e77efebe5173b9944e565a37010d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:09 +0000

    model design matter wipe peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_min(numbers):
+        return min(numbers)
+import time
+def get_current_time():
+        return time.time()
