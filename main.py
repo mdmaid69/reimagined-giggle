12a78ef2eef26464890aaf5411ef8bfe9018b009
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:53:13 +0000

    contrast shop image lower exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
