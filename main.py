1428cce658b545a40ad5b238b1e1830d0a544778
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:37 +0000

    opponent paper tie turn scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import random
+print(random.randint(0, 100))
+def find_max(numbers):
+        return max(numbers)
