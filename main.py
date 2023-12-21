313154ccc1adc885972029d4ffee829489b95a5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:43 +0000

    deliberately position add like call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(numbers):
-        return min(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
