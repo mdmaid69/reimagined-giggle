bb617284494dea23a58b06a7f36698b514ce7b89
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:10:53 +0000

    steam worried correct describe obvious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
