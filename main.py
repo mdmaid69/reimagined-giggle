3deca1d6a05df220cfdd2971fc82b3c13cf9bb96
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:17:47 +0000

    pronounce official priority accident complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
