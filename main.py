c709e17362b5ee8c5d7c670aa64a2b1c3495d697
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:48:50 +0000

    onto week culture truth divide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
