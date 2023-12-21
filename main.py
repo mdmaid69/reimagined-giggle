58342b5fe862e3d194b7972b44c2cdaeec080c4f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:19:00 +0000

    man beginning then start reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
