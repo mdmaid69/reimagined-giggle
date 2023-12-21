627312d42f036de0668fb6f4cfaeb97617896b90
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:27:47 +0000

    slice narrow money trust welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
