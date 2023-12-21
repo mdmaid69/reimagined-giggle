fa2132c6797421e131095c83589b384c9a7bf024
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:19 +0000

    score sensitive estimate clearly cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
