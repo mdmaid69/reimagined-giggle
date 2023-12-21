b7c93f4f9a9704075333518b54813954b96aff69
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:05 +0000

    exchange free against whoever develop

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
