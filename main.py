e4ea66861c4cc5fa2f8a8b993d106e5d43ffcaf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:49:27 +0000

    journey powerful over pity laugh

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
