b0b518e815aa70ac0fd574d0290b5b51ab6b7aed
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:32 +0000

    pity switch include type idea

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
