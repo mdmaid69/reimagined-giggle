89b9c9f4cbb379fd8bca5486eb48421f66417582
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:17 +0000

    exercise full its themselves between

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
