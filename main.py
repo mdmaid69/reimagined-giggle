b2169c680b765e63bd6130dbf2844523dccea502
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:16 +0000

    bread development recovery plain never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
