7b46b49af661681518c58f7cc9b4618c5b8085a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:01:40 +0000

    limit include bye separate community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+import platform
+def get_python_version():
+        return platform.python_version()
