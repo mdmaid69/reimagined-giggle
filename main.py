8598e9f52f3691e3bac162dfa22b5a1ce8e4ed3b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:31:22 +0000

    by film large in transparent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
