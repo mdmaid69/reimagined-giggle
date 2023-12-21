9d37100a8fa8f738c55b6c5dee54154fc7d1df45
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:13:02 +0000

    late law since forget model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
