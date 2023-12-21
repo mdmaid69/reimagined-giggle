2a64eeb2f379829ab5198febea87585429fac073
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:40 +0000

    snow house regard excellent scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import datetime
-print(datetime.datetime.now())
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
