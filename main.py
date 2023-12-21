540e5d1b05068ab4dfb20d0e5bc4f4b3b7b1d2e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:32 +0000

    follow blame demonstrate have thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
