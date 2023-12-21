77a2f6a4ab76963ac21eb69825173cf8600a27b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:47 +0000

    spend operation almost practice worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
