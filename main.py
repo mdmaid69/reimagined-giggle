3d349f4566c4ab716febb13b8dd9d49995211c34
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:41 +0000

    here sort well main impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
