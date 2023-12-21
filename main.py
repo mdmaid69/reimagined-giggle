e6ab8655f09ff096cc36c791f4036b50304a65a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:17 +0000

    unusual wealth challenge little culture

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
