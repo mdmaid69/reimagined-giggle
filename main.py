fe1034c1499522451d88038707a62aa7104af4ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:13:52 +0000

    fast should original design score

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def multiply_numbers(x, y):
+        return x * y
