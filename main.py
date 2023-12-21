779da9e42b3ac28337e43e8382a9f72f5f91f17d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:46 +0000

    talkoutof lost drink in transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
