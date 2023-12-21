e71fbec5aaffda52a78670c16af63800b823c99b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:48 +0000

    upwards exercise back continue plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
