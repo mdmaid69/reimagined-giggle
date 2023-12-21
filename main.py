52a36926937236115ada7c48102e9f7d4faf4806
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:42 +0000

    lonely couple tread expect walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
