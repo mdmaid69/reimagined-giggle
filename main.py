4f1a8674b223e994a9b69ca57a9af68b89c71b5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:33:28 +0000

    why sign challenge Vv dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
