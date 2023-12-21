d70a9173c62165549b3206f846ca7fdff1a64057
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:55 +0000

    stamp children autumn total be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
