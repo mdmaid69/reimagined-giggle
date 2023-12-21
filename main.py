7757d566697af4a0f58feae46bdb6c3a1c61b21b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:12:14 +0000

    release black last borrow invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
