b8f229ccd5837b634ee189368932a3c41e90e290
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:02:36 +0000

    hard accidental professional cause exercise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
