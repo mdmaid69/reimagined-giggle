da363f141d7b513ae1d73b3125dd0db133854792
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:15 +0000

    traffic world national move apart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
