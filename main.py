140725f4c0b3a8c6faf6cc09fb60b1ed710d5032
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:31 +0000

    invite deliberate as guilty getaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def reverse_list(lst):
+        return lst[::-1]
