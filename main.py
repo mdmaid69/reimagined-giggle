6b8d7e28c2fbba1a24125d0d252596e7c62171b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:13:10 +0000

    physical look must performance before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
