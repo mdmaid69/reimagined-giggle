e2866a4f240fef7f4f56f0dc95b64cdfe55a6291
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:59 +0000

    charge home match question future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
