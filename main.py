898337a79b37f11dd9ff3d17bdfe52bcb645b016
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:39:07 +0000

    sink mean giveback unless material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
