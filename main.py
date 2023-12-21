181482c0703948279445b20eccbac53a78f61d55
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:07 +0000

    any exact exercise interested prefer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_density(mass, volume):
+        return mass / volume
