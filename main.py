9e95c7773bb01906f1decce59a3ebbd2f378438e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:34 +0000

    frightened fat wonder payment always

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+for i in range(10): print(i)
