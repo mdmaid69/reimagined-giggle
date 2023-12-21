cf6f2176f294892fef7dc3e2ca07c09a0e9d7710
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:52 +0000

    failure thick it murder important

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_acceleration(speed, time):
+        return speed / time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
