5034cc2aa0eee6ea4bf2e0251de7b8e53914eb77
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:46 +0000

    I pocket hot compete middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
