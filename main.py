93badb7ecb80bd148f874ae491e41bd4a4840737
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:52 +0000

    Qq set hair national plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
