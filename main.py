e64725b4b7b30d32508800f62223e9d645fd01d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:30 +0000

    lift software put exam know

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import random
+print(random.randint(0, 100))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
