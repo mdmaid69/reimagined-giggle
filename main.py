e301c54c9efcf3c8bf367884c48d2028e9a23750
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:20 +0000

    now turnout branch far here

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def is_odd(n):
-        return n % 2 != 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
