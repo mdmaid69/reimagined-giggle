5f9c45b5fbb030470710a4d9d21c07925c2cf6c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:56 +0000

    trouble goout spin principle organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def square_number(x):
+        return x**2
