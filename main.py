7663b86072d104b98fe4a9bffc36bbcdb4684a4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:39:22 +0000

    Yy address smell yourself thanks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
