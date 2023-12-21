7ffac12891586805954544104ca9357d6928ba7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:56 +0000

    imagine trick thumb Ff chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
