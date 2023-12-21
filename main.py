1fa6cbe3b5a9566db937bfc1ff5c689508652669
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:17 +0000

    measure active squeeze ago every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def clear_array(array):
+        array *= 0
