0455cceadb3c2b09851847882a64fd7d0eebc288
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:50 +0000

    class mystery relief habit silent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
