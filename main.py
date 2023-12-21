28c663c2fafd6530e34d7893d0f14a78272733da
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:06 +0000

    dream within also ourselves concentration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
