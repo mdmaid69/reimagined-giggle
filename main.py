5aaf8e713845258a7afaf9be4ca7ac0bb06c35fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:36 +0000

    fashion steal wide receive luck

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
