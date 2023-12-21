6d6adabba0a4e98cea3aa4bb6ac8e65cff5ed6e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:15:37 +0000

    smell unusual sure chief scheme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
