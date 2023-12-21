42921054b6255e01850cda55b500c6d9b37510a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:05:48 +0000

    body below split mend state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def reverse_array(array):
-        array.reverse()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
