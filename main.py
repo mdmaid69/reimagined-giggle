e16c805b897ba68bbbb84d4748a0eef274de580b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:42:32 +0000

    miss runout track feeling side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def greet(name):
+        print(f"Hello, {name}!")
