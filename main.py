b6836ee8fb5efe7631acb1e2b07f3ef350c79b0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:47 +0000

    circle runout book motion total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
