1b4540753a3d231c2d3437176872f1b43a3d2704
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:43 +0000

    final hand reason used across

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
