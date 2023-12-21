252848dd583d1fd1f86be1a9dc4e9c1d193519b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:43 +0000

    get bend land tongue check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
