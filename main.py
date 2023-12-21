7c51fddc92cc4f2e5da4ba64c4c28f25edcb9bcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:44 +0000

    danger work small fasten surprising

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
