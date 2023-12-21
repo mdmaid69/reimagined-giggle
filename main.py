6b95bc90135ea717c9aa5266934c29ca4949776e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:40 +0000

    balance next nigga until apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
