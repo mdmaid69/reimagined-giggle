00b6c11ce155f4f772dab306be473d61bfd50241
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:50 +0000

    improve justice light getback rent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_str(array):
+        return str(array)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
