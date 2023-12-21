37a4b0d7571282d051469ad9b635447c4b752080
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:03 +0000

    thick gradually slice proof factory

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
