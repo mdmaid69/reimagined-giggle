11fcbb084a8bbfcdfe26d6b1837e8ee8458db7eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:11 +0000

    search tip burn history school

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
