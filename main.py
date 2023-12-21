4303040959ec4c2e1a0b17be098afad9adc9b39f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:34 +0000

    heat trouble power OKorokay breakinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
