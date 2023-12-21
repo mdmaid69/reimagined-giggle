6c19803345633ebd33212ae8b5a8ee4dab28588f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:30 +0000

    measure personal speech whistle classroom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
