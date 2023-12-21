449b1dfabbfef15ac37728f1ecbf9e71b17db49c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:31:02 +0000

    unfortunate warning stage inside spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
