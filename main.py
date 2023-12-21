7dd3634c5a818f37c7c9231c96de0720d860810b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:58:36 +0000

    give determined opposite night wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_area_rectangle(l, w):
+        return l * w
