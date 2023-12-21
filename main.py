4b79cd51d40de8d7e1a052270f4d7fdd3191b3d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:24:05 +0000

    worth dry brother ahead runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
