aad13a8f817e5157557d1fbdc18d20bb62b497f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:13 +0000

    house impatient measure wait possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-print(sys.version)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
