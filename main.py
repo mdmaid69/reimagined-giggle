e7b00b288e549dc018ff1d28a4bfc5d1707b9f1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:46:04 +0000

    both payment wave long deal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
