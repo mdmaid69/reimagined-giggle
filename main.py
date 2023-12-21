ac824858627b116447b5b5194e2727bcb763bb48
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:22:42 +0000

    man do right profit wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
