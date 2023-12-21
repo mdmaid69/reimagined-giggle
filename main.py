b709c16756a52532c3e77bd477ffb4e0c47c01b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:28 +0000

    rush achieve later drop method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
