45709e01a5be4e7fedb340c0187737c9fc958d49
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:45:01 +0000

    quality negative guilty takeoff book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def reverse_list(lst):
+        return lst[::-1]
