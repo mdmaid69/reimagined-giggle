10b0831847cbdfcee04a300416c753bc0a58546b
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:21:10 +0000

    stick male ordinary unlikely scrape

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
