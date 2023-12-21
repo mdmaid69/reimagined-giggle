5eee69e30068a795a0688dc5e22b9bf4b55584bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:42 +0000

    form our onto store live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
