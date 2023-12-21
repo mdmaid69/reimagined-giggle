39cfe3a78cb13dcd3a96c0d65f1d0384542b2057
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:22:58 +0000

    study able light agree who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
