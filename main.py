84b26ca98b43559fac0520b2816b9b22d74b3e5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:42 +0000

    change defeat effort apply get

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
 def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
