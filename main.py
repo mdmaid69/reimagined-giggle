54271364369ebcdc5ced4a0c1eb00ef102494a82
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:30 +0000

    music left trip later people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import re
 print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_bool(array):
-        return bool(array)
