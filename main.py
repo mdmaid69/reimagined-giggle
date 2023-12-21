c6f1009ad27a2d137e3a5033c6be8384be379807
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:00 +0000

    repair goout argue kind petrol

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
