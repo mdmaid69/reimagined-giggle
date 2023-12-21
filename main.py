0884549a4a1410b6202f8504b30db39318488bc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:36:35 +0000

    later over reduction speak off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
