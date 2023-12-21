3983cdcf7ccc80d63426a80076a12a4031527a52
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:38 +0000

    clever paper show fine apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
