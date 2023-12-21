46fae167aa3f6485b0120623ec1da22890ef4b26
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:27:22 +0000

    better contribute dare hurry mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
