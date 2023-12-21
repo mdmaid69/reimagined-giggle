bbe9044911ae468ff5e1021777944a1170a7492c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:51 +0000

    house follow thinkthrough copy factory

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
