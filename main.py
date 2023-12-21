748d534935ad24b055a52ba5a3e4d9fbbbcdb6db
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:36:37 +0000

    argue squash her necessary yet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_pressure(force, area):
-        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
