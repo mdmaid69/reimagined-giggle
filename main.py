2a461a3164cd568c24ba782b9eaf607a473b6047
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:54:06 +0000

    hardly display operation than negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
