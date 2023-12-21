e182652f66e69c543da384f0757e3c81664fc7ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:39:55 +0000

    wall quickly scare guess Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
