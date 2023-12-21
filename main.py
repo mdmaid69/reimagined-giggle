84034d567646a66632aaad74ddf434e2f9af280e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:47 +0000

    brain speak cum urgent shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
   def get_file_extension(file_name):
         return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
