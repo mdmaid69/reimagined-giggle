e655ea928718eea8eb8f256df0459eed955ab84c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:42 +0000

    awake mind work weigh claim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-name = "Python"
-print("Hello,", name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
