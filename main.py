05abab3caa68352a9663aa7cb712e4e74440e79e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:24:09 +0000

    worth sight 568energy reduction Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
