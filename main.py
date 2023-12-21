7d501aa41f0948d3375b85adfcd8d4034f7264a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:45 +0000

    exploration new couple have emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
