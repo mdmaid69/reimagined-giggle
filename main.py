8d1c38bd2098bc914be12887c1d172d294d501ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:43 +0000

    mouth bringdown demonstrate evening ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-print(os.getcwd())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
