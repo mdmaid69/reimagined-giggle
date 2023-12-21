ff7a3851e5cd94428f0b47a8fd651c54e0f3d016
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:04 +0000

    across callback list suggestion shallow

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
