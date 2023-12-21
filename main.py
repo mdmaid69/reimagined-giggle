57ac8e1719f9bd9117eca72ecf7ef7bd79a07390
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:04 +0000

    meanwhile nerve lend listenfor national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
