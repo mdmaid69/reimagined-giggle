55aa389499680c36f49a967527b22be41d41c679
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:51 +0000

    turn proper kind any positive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def convert_to_binary(n):
+        return bin(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
