3e6ea4a052b49abd1fd9bac0e594ec38ba7adea9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:10:52 +0000

    party sight each talkoutof bear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
