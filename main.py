b2de977c19aae078c2057050c96e40b7e0e1026a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:43 +0000

    class possibly uncertain fact rest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
