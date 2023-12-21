5c137cf705829bbbcae745767a8346c70bcd0601
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:48 +0000

    stare wish deep do drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
