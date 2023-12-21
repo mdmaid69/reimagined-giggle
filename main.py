61a92da18abe575df6af5a117c023c59b2ac1d38
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:48 +0000

    everything best enthusiastic skirt since

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
