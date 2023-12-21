aecf39a53ec785f8a41691b19f407bb30d7eb9f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:22:24 +0000

    pretty care question could store

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
