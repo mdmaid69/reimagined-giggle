17c4fef1176e1b8d85b8dc95c801f98d3659deab
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:07 +0000

    obvious earn your board at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def count_elements(lst):
+        return len(lst)
