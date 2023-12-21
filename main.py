31342efc56a75e7b12bcf4c124be6a95e488f69b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:43 +0000

    immediate positive priority reply touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
