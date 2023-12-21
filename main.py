43d57e7527ab0467c92acea737382be7855288ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:04:31 +0000

    cause over not organization dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
+  def convert_to_octal(n):
+        return oct(n)
