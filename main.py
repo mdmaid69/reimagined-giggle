cf70cb2492b4b3e666d9bc423945f2aeb3816f20
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:50 +0000

    previous often search explanation exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_max(numbers):
+        return max(numbers)
