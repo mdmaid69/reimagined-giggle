b720251cd6a3012fee27a00bd10d00f57116c55f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:31 +0000

    national honest give consult turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import sys
-def exit_program():
-        sys.exit()
-  def square_number(x):
-        return x**2
+def print_python_version():
+        print(sys.version)
