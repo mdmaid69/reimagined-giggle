fffccdf0a0934893f49fa9966ca15201cb322e4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:16 +0000

    order dance radio backwards any

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def change_working_directory(path):
+        os.chdir(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
