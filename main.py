c289ad9fe71cddb7a805ee45ec7e72bdc6a5e462
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:43 +0000

    please afford Tt uncomfortable plant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_acceleration(speed, time):
-        return speed / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
