8a912e4ebb70d4432a80b631874bc369fbb842bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:06 +0000

    confused attempt breakup claim still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
