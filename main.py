bd35c3d95ba9055c5e502c4bf8186cb9120f9ce9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:07:25 +0000

    shelter actual follow convenient claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
