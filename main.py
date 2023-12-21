63c467640d1c9e56d6662e8aff65f4dc6f1f875f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:51 +0000

    wild consist town minimum relief

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print("Hello, world!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
