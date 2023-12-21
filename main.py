c0f41f4d6431803a565fbf3c5699e737cbbd91bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:20 +0000

    partner behind describe achievement issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
