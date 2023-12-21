4e7b3ddf043f675855a1f31897fb8ccb01a0b177
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:58 +0000

    force mean possible meanwhile pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
