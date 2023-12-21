56f1d213a0ffa2e5173b019d326a492e35bc67d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:30:39 +0000

    middle national giveup intelligent schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_counter():
+        return collections.Counter()
