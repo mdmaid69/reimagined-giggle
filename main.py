44a5b7ffce29c008f138bcb371d40e81795e5509
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:40 +0000

    heavy year worried coat temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
