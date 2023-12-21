57f58542ce68790337450ecee873e7f5ea01a581
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:59 +0000

    lot frightened door course rescue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_words(sentence):
+        return len(sentence.split())
