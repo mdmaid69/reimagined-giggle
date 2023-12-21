244b48360ea1a0298e84bd3f23098791d62fbb62
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:20:58 +0000

    pupil town mention day spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
