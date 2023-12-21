ee41730e80ddeae21581d0a7bdc94d8c86694b6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:03:20 +0000

    identify on address anybody tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
