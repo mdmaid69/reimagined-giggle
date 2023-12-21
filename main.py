a06cfdfbc3487b0229b4832922f2216bac839cf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:08 +0000

    separate underneath floor art manner

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
