fd10600ed894bba7c4b61437a178e5b286f656ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:02:38 +0000

    fast cut community though brave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
