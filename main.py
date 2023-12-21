1b7153c31cc2b57b52756095e077d8617db267c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:08 +0000

    kind month uponon bank change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
