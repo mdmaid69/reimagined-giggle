c88dc3791f1e0b346a15352e28f029c88c2132f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:07 +0000

    identify wonder song difficulty group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
