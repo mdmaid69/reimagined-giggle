83f1d080926b2e4042e31351cc1ae4d92e697b6f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:22:50 +0000

    keep bin later crowded noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
