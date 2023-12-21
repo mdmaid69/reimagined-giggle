a38ff9471db889e46e323b516141545e61658af5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:15 +0000

    patient 573entertainment firm artificial drink

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
