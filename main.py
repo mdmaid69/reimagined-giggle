dc03075871574e4b1a361d2be1d19cfbc46ffd1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:29:58 +0000

    scene run tense shout win

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_binary(n):
+        return bin(n)
