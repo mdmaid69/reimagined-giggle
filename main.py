87bad3d2cb9605a6a66fcad1b0e115df3f2fbbaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:51 +0000

    main blow lesson drink address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
