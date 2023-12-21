a960920b252ccd29b1c20c54b5c8c07aea182e3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:33:10 +0000

    permission sit certainly increase talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def add_numbers(a, b):
+        return a + b
+def calculate_work(force, distance):
+        return force * distance
