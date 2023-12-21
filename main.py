176043f1b618971b341a57013793b5ad6aeda459
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:27 +0000

    software entrance club meet dig

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def is_prime(n):
-        if n < 2: return False
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
