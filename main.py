27a741c9331654cd25991499e4ccf7852c3619d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:51 +0000

    professional crazy meanwhile sticky what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
