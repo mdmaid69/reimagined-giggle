dd3e42753e3018cd57333880c7b85a6264e22484
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:45 +0000

    meal eager remark entry trap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
