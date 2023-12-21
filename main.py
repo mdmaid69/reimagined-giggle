3b7797bb8e718e9a5c24778878ca69139170228a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:59:01 +0000

    convince could teach responsibility drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def cube_number(x):
-        return x**3
+def reverse_array(array):
+        array.reverse()
