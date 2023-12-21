14a1b34a6292982c6ebdb7bfb65daa19fe33fa21
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:18 +0000

    arrangement action mention whole scared

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
