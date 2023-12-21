40b1ec4d9e92fd1519b15bd2c238a488919f12c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:18:02 +0000

    climb only break national answer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
