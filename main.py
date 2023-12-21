2e02f938669dac19849c106b3798b4577276bfa9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:34 +0000

    swing desperate wave recommend expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
