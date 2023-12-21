a8f0dfc048c8f97c7a0d08b47c6bdb254ba8c436
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:56:12 +0000

    dance clothes ticket runinto unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
