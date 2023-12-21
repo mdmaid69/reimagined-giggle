a650b9d0e73fa5b7d14160d8c6cbfc2f49c17bf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:33:58 +0000

    build giveback explanation write recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
