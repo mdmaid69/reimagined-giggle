bee5d5b08116e3c56b51db4260cec512461d578c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:14 +0000

    enthusiastic look survive glass previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
