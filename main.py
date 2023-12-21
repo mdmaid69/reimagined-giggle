8695e4a0830b00199806c105ebdcdc7b9a31792a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:46 +0000

    rubbish talk would 573entertainment persuade

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def find_min(lst):
-        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
