3cc5d2ce0ceb33f4b3a2ea5783ab974b4b2fae4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:08 +0000

    seriously bottom deliberate argument drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
