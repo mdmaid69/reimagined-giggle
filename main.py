b9b1bffa2ac26d3e8fc672b0ada186eb623f6cd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:06 +0000

    dick correct disgusting treat turnout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
