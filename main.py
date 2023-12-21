c40a661d7fb0b4d75fdada9478b38c7dd0ef29bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:17:08 +0000

    paper beyond friendly Qq all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
