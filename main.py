2b10b617f10475a2d7c1995274a0f7a2494281a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:00:45 +0000

    turnover positive autumn produce main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
