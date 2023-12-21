0e8393e96e6610ca5b338684ac1dc9f05f28a808
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:45:31 +0000

    place stand information possibly both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_length(array):
-        return len(array)
+def get_array_item_count(array, item):
+        return array.count(item)
