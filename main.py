225f2455aeaf5a0ff649d5fb07fa31916f0bbbc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:47 +0000

    pause muscle contrast nowhere we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def count_words(sentence):
+        return len(sentence.split())
