645f410b422498207eeeda0126936e75d21d4f96
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:16 +0000

    back control history transport mess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
