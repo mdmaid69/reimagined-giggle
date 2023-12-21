53d4459be8b561c243e0ef5ae0d8e6b54bf3f4a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:43:13 +0000

    such somewhere disapprove affect count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
