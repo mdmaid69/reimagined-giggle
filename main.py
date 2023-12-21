6de8a1cced4f9603e8414f224bfde9ed264a9ad1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:43 +0000

    shame dull talk tired group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
