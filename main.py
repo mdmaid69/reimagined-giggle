40aec2183493089db5d820097812f14da1787a49
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:35:42 +0000

    quarter interview interview pause dream

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
