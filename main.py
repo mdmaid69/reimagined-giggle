fb78325977b6e0b4ee752aa5a006c123196aeac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:09:01 +0000

    other effort pour dig responsible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
