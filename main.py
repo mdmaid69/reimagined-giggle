69f9efc9ea36e93e36c55da2fd302e79565b5af4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:14 +0000

    all instead mind sink noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-for i in range(5):
-        print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
