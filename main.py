3687ffe850614be43f4e867b392b0a8bba74d521
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:59:21 +0000

    possible compete total closed equipment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
