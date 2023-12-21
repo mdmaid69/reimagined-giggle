ebff1f6a65bec654968a4b4157bb5369ba14f1e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:47:17 +0000

    likely love relax adjective stage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
