c6513f24f38ee34f2a8fa616f956854dc328e2a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:39 +0000

    Cc middle turnout talkinto terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
