64ece8c777fe646c1d5e682da172af0d46bd4010
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:52:37 +0000

    fine flat steam tip exit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
