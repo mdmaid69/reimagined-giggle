3ae6902e19a3bfcc65a68cb7eb363012eb76542d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:40 +0000

    now trust twist spill regret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
