422659e4587edc63d5e536bef9d954e6c9afe776
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:59 +0000

    square bite he since getup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
