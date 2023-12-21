15588f9ada0a6f1f8602f8fece3df13be8ea6336
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:59:06 +0000

    crash people threaten sudden you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
