4dbfd1dcdd8e315153da66ca1ac732e66269ffc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:01:09 +0000

    walk brown range bank getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
