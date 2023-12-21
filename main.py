7dcf356cd3a28ce874a5bf9e1a9d14b10d0d43c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:52:06 +0000

    favour sore comeoff thank earn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
