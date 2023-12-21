d08300f0c5bf8fb6062fed0e1c3ae827c1aeb40a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:05 +0000

    watch trip crowd cause another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
