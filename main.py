c2dff8af6878f4014aa3cce7a3b2e45131231261
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:47:14 +0000

    watch perhaps build famous front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
