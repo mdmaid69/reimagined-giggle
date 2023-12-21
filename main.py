4857ef2ea4c8fc450771ab472fd65e687198a69f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:26 +0000

    sticky understand period clue gap

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_speed(distance, time):
+        return distance / time
