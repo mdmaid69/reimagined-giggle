80e00d3c25f42efaaf7708a887b09dd0cd7ccb25
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:43 +0000

    at while regularly kick second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
