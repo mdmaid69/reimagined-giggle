dde0333eece612c1613ced49d476d5d1432ed25d
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:27:13 +0000

    fail mine tear tax government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
