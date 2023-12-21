0c5cc6f426686546a3f003e7ab3dedb71445987d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:13:16 +0000

    frightened tomorrow connection go Itself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_dict():
+        return collections.UserDict()
