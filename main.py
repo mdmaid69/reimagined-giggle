646ef5692d7f7b5640bd4f80ecc97b441d746936
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:30:55 +0000

    again clean deep generally borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
