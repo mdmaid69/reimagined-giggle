16e4a555055e405ee0ee2325b7412d02e282a82c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:17 +0000

    fold none custom doubt takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
