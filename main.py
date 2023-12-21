ded9157b9aaa8c2ef92349e4d258e3061fec1932
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:15 +0000

    past space themselves many familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
