44c869c917b156fc7fdb7537ebe738f74e03a266
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:08:54 +0000

    suckme recommend demonstrate black speech

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_power(work, time):
-        return work / time
+import getpass
+def get_username():
+        return getpass.getuser()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
