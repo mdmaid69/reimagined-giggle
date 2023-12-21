8d4a57c1927d9a003314d4a5ac64bb536728094c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:40 +0000

    bring their message bring slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def count_elements(lst):
+        return len(lst)
