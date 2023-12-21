2ef5dcc6041b676d3434189f849da16e22210dbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:28:13 +0000

    excited able may our objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def multiply_numbers(x, y):
+        return x * y
