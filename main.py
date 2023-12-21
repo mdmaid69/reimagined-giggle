b8c17734b7daef7629ba9c5244ede59e31b3b787
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:19 +0000

    priority lean judge stone similar

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def sort_numbers(numbers):
+        return sorted(numbers)
