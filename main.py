6291ff2696aa602bb9330a8c9a53f3b637bf4ae4
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:42 +0000

    guard lonely holdon crowded comeover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
