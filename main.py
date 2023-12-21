cc5fa5657e3d9d10e4551fb4dc254f6319b131ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:52:13 +0000

    confident clean rush judge expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
