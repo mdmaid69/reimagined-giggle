8c84bd969beb9c07ea5672cf9c4529d37f4463cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:41 +0000

    compete watch storm tired disagree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
