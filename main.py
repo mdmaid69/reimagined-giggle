05068a5264990fea6d16eb0eefb2a5fdb4710040
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:27 +0000

    butter grade cheque cinema now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
