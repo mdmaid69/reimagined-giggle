026f47640919a3ee6761fdd27d962ce4396e847a
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:29 +0000

    workon plain congratulate seem brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
