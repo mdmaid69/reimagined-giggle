80c180996365a656239707e21567a49701fc6082
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:00 +0000

    freedom visit suit reduce pin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
