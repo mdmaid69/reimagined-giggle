c04e7223ad4ba0c8e881dfbded11fd6a9b0d5b99
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:33 +0000

    rest low surface thinkof one

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Characters:", len(text))
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
