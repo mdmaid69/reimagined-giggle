1d093de72e7785803994d4886ac036feada9bce9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:21 +0000

    arrive speak spring event sail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def convert_to_hex(n):
+        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
