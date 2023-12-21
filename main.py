c3d9de0a746a2262714d9b01ba50274d06f075b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:05 +0000

    571enough official shoot climb message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
