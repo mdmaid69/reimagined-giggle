f3214791c9d1dafb200316ee9f78aebc67459e9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:08 +0000

    Ww later Jj improvement pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
