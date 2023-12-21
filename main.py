d8a0c30dd8ef1eef7d99914f359fca179cdcd02b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:00:37 +0000

    occasional want tone you lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
