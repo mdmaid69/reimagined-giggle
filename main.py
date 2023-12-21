de711cbfe734f515d465aef552fd6b5e8816e1cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:08 +0000

    evening tonight statement question connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
