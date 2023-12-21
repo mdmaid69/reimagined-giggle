416700db66101e7d26bc45f7f87daedf6918e2a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:47:17 +0000

    bury table heavy coat copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
