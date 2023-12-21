72799d5c084332a4c5f85f446088457048197171
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:22 +0000

    body refer even calmly change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
