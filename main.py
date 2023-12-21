e6e0d3284367edd929b65848f21908ff72fdf67b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:18 +0000

    sit object repeat kick pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_ceiling(x):
+        return math.ceil(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
