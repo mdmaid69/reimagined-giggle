1d5af3405c64cd4014ae686fa84a9e3f536d05f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:57 +0000

    view fun food proof beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
