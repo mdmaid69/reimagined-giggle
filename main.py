ef48b4b213fa87703fc90d7a20ad137e6c2f80f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:24 +0000

    thankyou state prefer actually we

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
