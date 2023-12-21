3eeacd610d7df4905506ef4b65bc2c076fd4deae
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:21:44 +0000

    hand ability card murder nigga

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-print(sys.version)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
