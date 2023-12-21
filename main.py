6fbf63a7d9944d7d9db09835052e9b8294a137a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:02 +0000

    radio mainly race scrap slight

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
