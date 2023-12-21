1706c24a4ff3c3422138be1f7dcdb53fb7e98416
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:43:48 +0000

    middle Cc colour concern happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
