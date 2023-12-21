2cfe7c8844ea849776b52a790dd10937e68a7a16
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:32 +0000

    occasion night me permission off

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
