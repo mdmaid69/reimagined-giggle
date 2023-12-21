4710b48af6294005b489f8503688d89810f050af
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:56:00 +0000

    sore your vision gap ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
