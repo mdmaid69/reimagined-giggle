6831be356d2d3c872aa268e88ce3662570fab62f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:35 +0000

    wide urgent wave respect become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
