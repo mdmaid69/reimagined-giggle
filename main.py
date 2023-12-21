6031439650a298e5e0b812f3b0cb33ee135c35c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:57 +0000

    close reserve section television bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
