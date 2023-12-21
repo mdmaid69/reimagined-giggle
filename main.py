e5080c40898835e4df9da9495247fc68a5b9bd29
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:10:32 +0000

    harm opinion eager appeal wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_repr(array):
+        return repr(array)
