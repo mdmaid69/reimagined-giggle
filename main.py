ff9cf7b4863adbd4a5668870dcfe355cb1b04d46
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:41 +0000

    mix will continue mine loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
