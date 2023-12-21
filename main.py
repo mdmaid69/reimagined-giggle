ef2045062ae5ecfc1171d7a4e7c038a8997b6dbe
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:18 +0000

    regularly material niggaacademy slice sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def is_even(n):
-        return n % 2 == 0
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
