cdb3245655b43140d74ea739a4d45ac705fe2ef3
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:17 +0000

    equal bin beat cut modern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
