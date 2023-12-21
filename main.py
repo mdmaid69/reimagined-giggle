6bfd7e36e688e227699e0d570527c36cbeabf72c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:25 +0000

    improvement interruption regular interest start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
