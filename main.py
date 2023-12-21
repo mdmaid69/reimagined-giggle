e2e3fdfa3e664295a716e034e1ac07cf9a2c8447
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:26 +0000

    fast hole part street cheap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
