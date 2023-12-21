4ea4e1ee4075508d5553847dfaa844fdba2b6bc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:51:38 +0000

    Itself interview choose post collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
