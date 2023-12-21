194e0504d98e8b5b49e66c3fab27fdbe4b6c555b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:57 +0000

    Itself estimate 179bite stamp everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
