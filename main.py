6c61edd190bb446cf5ace98ff887cb9b5fb66c2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:55 +0000

    appear regional comeback ring stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
