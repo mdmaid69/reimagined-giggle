b08a4dd8861821521bfa2c7123adcfca3717a664
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:46:50 +0000

    action turnover tear holdout flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
