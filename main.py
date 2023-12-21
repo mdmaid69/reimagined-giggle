1181726ce4894ded8753647529ca08199f8e5010
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:32:50 +0000

    demand score runover tooth occasional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
