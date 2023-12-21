b1fd2d6c424a7afb021036e59eee00c81dc57f2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:06:17 +0000

    Cc tradition except goaway hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
