aedb479c8393d1a1f039f0f516f4d7f9cfde61a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:47 +0000

    family frequent guess judge include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
