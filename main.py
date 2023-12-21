191d10741a36604f7eed42bcb5f467287c3ad89f
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:13:04 +0000

    development step matter cover start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
