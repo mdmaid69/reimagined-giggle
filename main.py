3a7311a075dacb4ad575804ee7dded49add1b2a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:08:59 +0000

    prepare top anybody congratulate later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
