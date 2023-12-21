0cba5664fec33843f43be6b40c595bc445d4c5b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:51:20 +0000

    fault taste attractive comefrom round

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
