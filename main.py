827213d7adb73b23120f749f2ea8048e609e10c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:50 +0000

    crash relation thinkof achieve game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
