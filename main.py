6bfa1c9ceb34dfb2bc89cbfc48fa86067d4b227a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:43 +0000

    announce arrange large treat bit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
