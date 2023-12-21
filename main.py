b4a00045448da4a7e791bbfa1cf3b5a1e80ddce4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:51 +0000

    flow welcome slow object present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
