ec38c2c3a390c8893fbcccaace4141b578cdc876
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:14 +0000

    rub drawing wait term about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
