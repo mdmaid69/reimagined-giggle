a873206eedb5a07f60f1aa61d3fcf88032832da7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:03 +0000

    level wear score gentle difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
