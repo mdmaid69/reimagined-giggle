91fd91cf85e358d0af8873444c80ac7a9ebbc4fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:44:33 +0000

    play shop worth release punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
