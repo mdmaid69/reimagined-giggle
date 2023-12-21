fa1f24508c9b96bfe11bf4b66f22e6b8a64be6d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:33 +0000

    office fun private month add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
