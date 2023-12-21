5ec8ecfbbbfb631d55ec092286c647cda3631c17
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:55:43 +0000

    keepoff easy organize problem evidence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_area_rectangle(l, w):
+        return l * w
