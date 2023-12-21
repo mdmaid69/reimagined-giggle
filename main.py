81eeafcef6d5f1fe7f385093ed702cc5092fe9c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:22:32 +0000

    treat country accident strong coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
