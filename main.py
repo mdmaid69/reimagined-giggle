15d6ca4cccd5e3f22e32da39376c4d64358dc926
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:03:19 +0000

    string that favourite against will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import logging
-def log_message(message):
-        logging.info(message)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
