d2f9022d15f2b85dabe3029c505b235ca06be15a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:55 +0000

    present contact drink someone bang

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
