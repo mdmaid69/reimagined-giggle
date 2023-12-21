2c5f99cbbcc4ded035a678af0ee61cb203fed49b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:25 +0000

    tomorrow trick guess occasion possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
