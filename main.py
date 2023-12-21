a0b26b30fa499f53b0e73acd76ec61bbb5290804
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:23:52 +0000

    change wise town Zz prevent

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
