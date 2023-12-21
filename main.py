e4fad8ff1bbb7c7b873084ba62b6155d7141140e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:29 +0000

    rush stretch from after interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
