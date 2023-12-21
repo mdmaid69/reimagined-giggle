34c92674101f34e9577ed899664bacaad4ad6e7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:50:51 +0000

    ordinary reasonable happy along responsible

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
