71661b3cc3a269a2369c1aaf80d64f742e8cf8e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:38 +0000

    stone unless community lift over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
