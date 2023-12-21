18e3ff5111f156f7f46c09a67f596c8b2e7fadbd
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:49 +0000

    cost even lot convince share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_floor(x):
+        return math.floor(x)
