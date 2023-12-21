d6c2a969ea87d3448623abdfc79d0da60bc1db4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:28 +0000

    pain murder basic smile heart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
