d1d7ae933dde5cd055d798db4e9e776e6c6f9cf0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:15 +0000

    recognize round steer share everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
