23911a2bf66522e572007af5ff2f2c7238c621bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:48 +0000

    run close worth nothing disappointed

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
