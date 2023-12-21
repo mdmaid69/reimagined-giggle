cfa33573168550bff27099175dd78500d6fb6db7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:16 +0000

    brief nonsense pick cover noone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def convert_to_hex(n):
+        return hex(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
