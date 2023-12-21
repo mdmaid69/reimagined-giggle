21d47d903d3fb47f4c327f70fcd6c096ad3eb487
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:28:22 +0000

    achievement happen order tip increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
