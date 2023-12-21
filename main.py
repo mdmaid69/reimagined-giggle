7059ceede0458610a5679f1c6b7c45e3ce682248
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:00 +0000

    or that lay consider his

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
