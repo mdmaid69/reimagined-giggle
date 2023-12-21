9a07a2583d04a883abf4b3f12ed3c388c6374d11
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:43 +0000

    promise break backwards thankyou fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_circle(r):
+        return 3.14 * r**2
