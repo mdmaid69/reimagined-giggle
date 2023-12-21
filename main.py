fc1f4f7031dde0e96b13bfdeeb3d63accff3acb0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:20 +0000

    introduce deliberately woman position ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
