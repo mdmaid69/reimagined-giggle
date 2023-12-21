9f54d3bdb3f16da7951cdde4857590011ac3e36a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:02:33 +0000

    position trade unlucky weather find

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def reverse_list(lst):
+        return lst[::-1]
