87a22bd12d30945a83c6ee09d475f5cf72625a6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:12:39 +0000

    unfair short tap project serve

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
