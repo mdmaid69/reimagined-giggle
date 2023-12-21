328499565e679328ec198e155c582a3392ce198f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:46 +0000

    nowhere major develop never someone

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+print([x**2 for x in range(10)])
