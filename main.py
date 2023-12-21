1f1dce96b4ca13da761310c06b76a247f8d72545
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:14 +0000

    turnon fence neither advanced often

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
