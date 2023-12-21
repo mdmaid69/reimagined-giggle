cd5b3f06bedaebb0841d3bb8c06ae0473541f5bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:04 +0000

    forget tread deserve practical value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
