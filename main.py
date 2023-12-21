b61b21a5f2d4872ccc6cffa6e33c728ba62b6ee2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:56:58 +0000

    still like pen badly 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
