d215e8796ce98147c907ad579122170c094c2dff
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:39:48 +0000

    without clearly wish reasonable runout

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import platform
+def get_os_info():
+        return platform.uname()
