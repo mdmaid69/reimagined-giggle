fe0389947ba01b842418aa8e9d551887d2c158c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:18:15 +0000

    shame get concerning once wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
