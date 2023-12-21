2beac1ff634c86768ac4f9539a2efd451ff5f181
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:38:03 +0000

    basis noise any to objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+print(os.getcwd())
+import array
+def get_array_as_int(array):
+        return int(array[0])
