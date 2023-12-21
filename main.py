668af9056dc94abd865155e6f18f3bc3f3837229
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:05 +0000

    hungry hole description only use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
