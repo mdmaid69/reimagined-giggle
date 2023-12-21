5eaff8bf1128579f4e4ac2845170602a2208393d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:45:16 +0000

    show stop concentrate fail turninto

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
