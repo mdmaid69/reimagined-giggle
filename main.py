9c8e7b84cd63b2658853eb8510baba4fd440ee80
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:33:50 +0000

    could can there congratulate space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_string_from_array(array):
+        return array.tobytes()
