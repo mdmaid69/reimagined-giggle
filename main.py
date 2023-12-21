50133b843453291891fbd06cc449b2d33080d425
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:41:27 +0000

    hello exaggerate waste unit birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
