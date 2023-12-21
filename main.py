1bc9e1d233ad5d89269a4c6386734083ad2c2b48
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:43 +0000

    trap any embarrassed careful central

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def convert_to_octal(n):
+        return oct(n)
