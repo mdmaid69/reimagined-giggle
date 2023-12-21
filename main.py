de8071f4c2a1c6412575898eb6fb038b57249700
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:13 +0000

    talk whom keep brother otherwise

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
