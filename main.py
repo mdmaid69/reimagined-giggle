88e80a9f768d77e90d00075b4ec96eed75df7498
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:42:47 +0000

    beat workupto thinkthrough argue 572enter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
