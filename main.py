2b4accf138d68b169fa09f583ac3066e4953eea8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:01:25 +0000

    bread month slightly surface happen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-for i in range(5):
-        print(i)
+import numpy as np
+print(np.array([1, 2, 3]))
+x = 10
+y = 20
+print("Sum:", x + y)
