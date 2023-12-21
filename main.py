ceff00f679cf71141829303ed3d97c7d44b8b611
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:27 +0000

    dear rate father store turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
