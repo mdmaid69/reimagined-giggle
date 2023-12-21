9c0baf9ecc04897ad7710aa3d7abda2b79d82e8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:53 +0000

    wise tread tape offer car

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
