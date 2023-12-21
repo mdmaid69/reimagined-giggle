26ff74f5bbe41f6e5356559faa84470b21977e87
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:32:47 +0000

    provide post finally thirsty split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
