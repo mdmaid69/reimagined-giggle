96c17fda515df2a094eb85649b21a5e4d65eb9ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:42 +0000

    usual favourite pair snow trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def is_odd(n):
+        return n % 2 != 0
