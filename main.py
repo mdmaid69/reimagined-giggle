5510cf3f765ee3127e7f44f9409c2b1ee6dbf998
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:34:56 +0000

    value near sorry empty move

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
