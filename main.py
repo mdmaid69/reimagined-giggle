a78eb7b3c2100673d30117478c5ee17fef8eb248
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:50 +0000

    kind conduct call poison expression

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
