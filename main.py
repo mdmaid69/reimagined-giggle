9b22c142455773b8e075e8e5a2fcafbae52b049b
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:07 +0000

    worth chin fight impression instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def cube_number(x):
+        return x**3
