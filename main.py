9baaab82015aadc7544e55d32e56e700a4dcf4b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:43 +0000

    translate 564encourage pause Uu crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
