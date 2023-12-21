6cba4993fab99883aa4e80f5ad74110b8835c639
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:25:24 +0000

    accidentally edge kind miss thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
