a52082f2e281d30a5bd47691eb8a25e07ac01d5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:49 +0000

    busy deliver scrap excited main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
