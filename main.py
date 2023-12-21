088bea2016a162254e5f5acf23b272ba6a9abbe4
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:08 +0000

    return tax favour centre rank

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
