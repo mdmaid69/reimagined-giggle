c75702c11bde035f6c0d01fd4c74c8060711c712
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:28:46 +0000

    temperature recover downstairs plain notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
