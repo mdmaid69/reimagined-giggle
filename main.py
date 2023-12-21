17f6b65e1d1e5420491e97df01f7171620b6eff2
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:51 +0000

    welcome pattern fun cross adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print(sys.version)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
