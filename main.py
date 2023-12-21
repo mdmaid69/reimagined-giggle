93556b7be882a932c2a7b908bec9c3bd1d477326
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:17 +0000

    failure actually bush theirs calm

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def convert_to_hex(n):
+        return hex(n)
