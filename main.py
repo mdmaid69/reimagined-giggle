ec584f05ee66e056a6203715749cb703a73c48be
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:51 +0000

    shoulder workupto theirs public desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
