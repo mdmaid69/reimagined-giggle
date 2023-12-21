ee081d26979acba466136f2d129bd26343ec0583
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:51 +0000

    official flavour hope genuine side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
