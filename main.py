2aa64ce0f2bd0394c51446ebdf472f284ab07a65
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:15:49 +0000

    extraordinary century minute round however

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
