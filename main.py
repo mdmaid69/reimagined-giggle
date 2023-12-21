2a25ea1b785d5b27535d912357b8b947e04bcd6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:58:45 +0000

    power course bringup scrape needle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
