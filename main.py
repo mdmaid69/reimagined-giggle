b3d9820006d459ac18e177054e5d79cd18bcb767
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:16 +0000

    desperate future degree they brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
