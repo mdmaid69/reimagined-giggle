c44fda3412317159d89470161fddaa2b64f8a282
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:58:36 +0000

    against invite base declaration later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
