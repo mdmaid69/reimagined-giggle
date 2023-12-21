b379ec387d113f6546cadb90f68ba836a921feb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:16:18 +0000

    Ll moment statement model rather

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def cube_number(x):
-        return x**3
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
