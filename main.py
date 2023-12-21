e710b234f79f314b9726188b4e5845c0c25dea60
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:24 +0000

    swallow breath Ww aim giveaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
