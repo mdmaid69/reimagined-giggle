cbaa56a8d42cd127d45b8cef6fba4910844b542c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:42 +0000

    platform call people mark regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
