20972d82b8f0bf49a9e1cbb3cd69e7961d4ea4af
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:02 +0000

    insist environment contact result hate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
