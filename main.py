6e621bbc5fdc95b3dfbe339f9e03f35bfdf11d71
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:59:20 +0000

    shall belief rate idea month

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
