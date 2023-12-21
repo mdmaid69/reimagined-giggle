d14f0c97a7d9e5da01007340f29eaa9ff25e73f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:00:53 +0000

    influence phone boat black each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
