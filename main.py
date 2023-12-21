9a158519a986fd4a6d822cc88e83faa246ca7c5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:24 +0000

    independent special behind length anxious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
