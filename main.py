972fb9b4dba222fa511cdd6ac79ceb5838f6343e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:27 +0000

    bringback turnaway keepaway platform break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
