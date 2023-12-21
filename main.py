ff66feda5c987980c9a19c4f7970ad3880b94e86
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:00 +0000

    beside petrol their squeeze dry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
