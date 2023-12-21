0707174e6f4177a90d4709ebaae135428abdbe64
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:58 +0000

    support age relative everything pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
