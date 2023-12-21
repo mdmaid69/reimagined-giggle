d6e3e42fcd39d8741ec783d4d691faf447d8e1cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:17 +0000

    dinner region nothing bound quick

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
