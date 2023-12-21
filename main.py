ad6c99d65ae030696fb493117c79f1568e9efc33
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:40 +0000

    tap divide calmly pity education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+print("Unique words:", len(set(sentence.split())))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
