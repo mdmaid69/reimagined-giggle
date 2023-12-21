98c4f2b4de4424fce19c66f030ee032e75c21a05
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:21 +0000

    slow offence woman general recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
