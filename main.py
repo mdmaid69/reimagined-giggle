5e3e5f8d3ce4ced54b3ee234d1ffe6766df937e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:29 +0000

    lucky swallow either serious mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
