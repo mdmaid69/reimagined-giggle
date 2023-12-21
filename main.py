c6fcb7c594334a5949f5119948755eeb59fa718c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:32 +0000

    short goback(to) disappointed draw block

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_time_since_epoch():
+        return time.time()
