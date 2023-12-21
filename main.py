d80f4e24db66743b4e9d91db5f4a6206fd1fd2cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:03 +0000

    wise sentence tear snow Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
