a8242c87d41fbe38387586112eaf999a75c78c2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:13 +0000

    reduction bus previous level before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
