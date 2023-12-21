f35e4b23f5389f3b3dcab8ed1fc0dcb6b268576d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:15:09 +0000

    desperate lead smoke cry load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_tangent(x):
+        return math.tan(x)
