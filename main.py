a811370233a4f0a7e3c04ce1efd4e9d044a5a8a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:09 +0000

    trade thinkover anybody duty check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
