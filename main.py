cc526a064a33fdf04e5c7c2c08c079469c237ceb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:49:35 +0000

    accommodation hot combine gather wipe

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_density(mass, volume):
+        return mass / volume
+for i in range(10): print(i)
