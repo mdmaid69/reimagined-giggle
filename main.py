90747864be5fdfc9458c6c0951db7aacc17136dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:32 +0000

    whichever repair track test steer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_current_directory():
+        return os.getcwd()
