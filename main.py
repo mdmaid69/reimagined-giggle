c327db56ea49056009beafdeead8aed3e3e99f29
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:04 +0000

    challenge action try first approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
