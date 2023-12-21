358bea2dc385257925890c9bc149bf0fc650bc90
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:39:14 +0000

    thanks start pocket ready drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
