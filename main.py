9e56c1432f1bc9f965477710ea4f5a3a4c021bd3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:17:41 +0000

    workon make proud film behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
