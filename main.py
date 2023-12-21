e0066da1474153f8c92fcccd0059a08872d4fa24
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:54:21 +0000

    doctor getout surface fold order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
