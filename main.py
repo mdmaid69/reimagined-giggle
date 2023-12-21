cc3c94d3b82bfb731b3130dd12e024342d82d75e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:14 +0000

    correction negative swimming ask habit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
