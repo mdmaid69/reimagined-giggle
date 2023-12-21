b2157945ea6f103909b321756b64e56a5bf9d3cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:19 +0000

    terrible thumb doctor several morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
