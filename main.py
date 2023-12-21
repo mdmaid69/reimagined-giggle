0eea323eb62acb31f3c00c3c0f306159e5748b82
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:17:35 +0000

    communication shout Uu arrival much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
