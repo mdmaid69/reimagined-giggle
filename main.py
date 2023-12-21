706c950578a8ea1aa47ba6a21be234b848d78965
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:32:07 +0000

    pronounce food react ashamed command

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+for i in range(10): print(i)
+import tensorflow as tf
+print(tf.__version__)
