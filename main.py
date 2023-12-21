184ea7bec71f98eaa9b64757c0f0fb7cacd0a880
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:00:22 +0000

    hardly noisy innocent it whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+name = "Python"
+print("Hello,", name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
