d80bc2bc70a771c21f30d03a7b807f1c935dd3dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:27:21 +0000

    rob dance advise recent borrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
