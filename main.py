73540cebf3d97de0454cdcd7a4a0d145b4eb41a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:41:03 +0000

    house now exam band doubt

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_power(work, time):
-        return work / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
