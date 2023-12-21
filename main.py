5f037a865b4addab9a78e741d8839e2b6c8e5436
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:02 +0000

    sing patience cap left test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
