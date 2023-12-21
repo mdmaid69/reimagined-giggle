b06152752d607b93e5420d4392656e33265f6294
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:58 +0000

    now clear shallow explain curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
