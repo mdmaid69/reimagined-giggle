e5a41fe119836e0aed09690bdaea7cdf6955e74e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:38 +0000

    hurry marry I cut damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
