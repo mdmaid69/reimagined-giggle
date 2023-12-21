bcf4c18101610402604ae856f294099fb66783b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:34 +0000

    test Bb height rush frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
