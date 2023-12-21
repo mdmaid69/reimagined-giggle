1fe9bc92674a2dcd94a493af5fff179543a77131
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:08 +0000

    division unusual prepare somehow wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
