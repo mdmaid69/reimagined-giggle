45174b4f2fdb2af7d4c41640870d40ccfbd61382
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:55 +0000

    bring command listenfor this joke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
