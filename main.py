c06d3007c3d2aca4cf2860a894414fc1b2458726
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:52 +0000

    picture enthusiastic please mass live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_int(array):
+        return int(array[0])
