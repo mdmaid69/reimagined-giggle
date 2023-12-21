1b6b47bd84cb63ed2eabf96e96d74f70236f9aba
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:18:00 +0000

    disturbing common support region Bb

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def is_palindrome(s):
+        return s == s[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
