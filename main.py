660f3310809b628630437cc2dcfa22f8bd67ee3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:36 +0000

    challenge press score free give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
