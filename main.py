80869c828b3d7e75e0c91052b0c099001c3955d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:50 +0000

    visit tall awake stupid set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
