661a1fc5baea312627f11d1bb37e9e9cf012883f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:46 +0000

    young goup ignore breakthrough godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_stack():
-        return collections.deque()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
