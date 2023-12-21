8ce2c9f2dc0ca3130943c176af20fb01d80c98c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:01 +0000

    congratulate interview rely knock exercise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def convert_array_to_list(array):
+        return array.tolist()
