12aa5a537b8692e9a1224dfe5fbb4188a8b35997
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:35:19 +0000

    but lonely borrow company meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
