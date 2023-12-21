8279e74195bc47c8b581a574b97c6a856fb58074
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:36 +0000

    rate argument except history agree

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def sort_list(lst):
-        return sorted(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
