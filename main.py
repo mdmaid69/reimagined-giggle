51d325e198403e1e5e1fe0fecad35053c4e55506
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:00 +0000

    circumstance contrast wide call occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
