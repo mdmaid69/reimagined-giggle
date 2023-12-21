a7f164414742f956ee98c93f73e75cc3d943430a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:49:25 +0000

    vote date favourite winter sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
