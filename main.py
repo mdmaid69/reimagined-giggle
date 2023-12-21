cbe709a726ec067b9b6d98e5c723d32893f4f49a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:25:03 +0000

    at attract expensive prepare lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
