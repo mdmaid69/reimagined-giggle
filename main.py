c114953104b274be7a54c9c409adfc3ae0028960
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:38:32 +0000

    represent runaway same date warm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
