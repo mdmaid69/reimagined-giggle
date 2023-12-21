e3cf6df9755a8b2793decad3046b4680655946a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:39 +0000

    gate capital open approve family

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_bool(array):
+        return bool(array)
