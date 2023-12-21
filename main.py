6ec599b6ba53def25cc85d00372fc8d129109098
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:08 +0000

    convinced when film wave dear

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+from collections import Counter
+print(Counter("hello world"))
+print([x**2 for x in range(10)])
