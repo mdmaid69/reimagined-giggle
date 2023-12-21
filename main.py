b1578cfe8fba3091a599770df3260cf949cfbfd7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:16 +0000

    possible fortunate spring command Gg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
