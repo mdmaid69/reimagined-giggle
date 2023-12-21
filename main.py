3aba81629e220c6f7db3ae6bc4e87350fcec139a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:26 +0000

    hungry Ss relationship law takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
