408e4eaaeca2efe86abbe94c6473ceef723250c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:02:48 +0000

    travel towards respect fill school

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def add_numbers(x, y):
+        return x + y
