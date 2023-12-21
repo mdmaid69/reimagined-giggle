6ee9fe9292c4770870cbd07790951e69b88780e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:53 +0000

    cause concentrate method slightly average

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
