d8e4de788717e62baa1153a38c62428eddf31e5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:17 +0000

    brief regret bringforward fasten both

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
