af55261ba8200a2e20329d861dd9f6725369a13b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:08 +0000

    object song pin shout hers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
