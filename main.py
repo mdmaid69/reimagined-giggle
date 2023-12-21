96165ee9acba3b36a61a1e10ceb3f65626071d37
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:46:56 +0000

    judge and collection rely equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
