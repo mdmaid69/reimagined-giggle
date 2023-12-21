a88a7b13fb23600de0d4a293b175bed3160b7d5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:36 +0000

    defeat threaten safe Zz clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
