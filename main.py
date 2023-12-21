2282eb338af2d154831b6dad4390b96fd5220f94
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:46:15 +0000

    Kk avoid gather approve operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
