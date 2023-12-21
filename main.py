d604cb57bd6a31a79d7add62ede512ccd8c1d9e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:16:17 +0000

    float listenfor phone quiet tragedy

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
