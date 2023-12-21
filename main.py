1240e8eb581a6043945a83c900bd4ac072c83f54
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:59:31 +0000

    after hair Vv excellent question

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
