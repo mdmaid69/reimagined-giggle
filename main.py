9d945e84caddd36728e49861120e81f835811f43
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:50 +0000

    guess callup discuss side slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_odd(n):
+        return n % 2 != 0
