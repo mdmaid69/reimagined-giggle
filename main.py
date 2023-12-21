26ae20e68d99a5e2f76f41540b8129bd126a32e6
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:35 +0000

    interested top familiar depend admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
