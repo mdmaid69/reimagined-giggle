a5e6a22061737bb63596f06526945ab2e2b1bd26
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:09 +0000

    hurt especially fight interview consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
