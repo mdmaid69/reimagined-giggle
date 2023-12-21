2de87b461044207d742e90d4c32fb58b5a50db06
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:44 +0000

    trip tap immediately home name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
