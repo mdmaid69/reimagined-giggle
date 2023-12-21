f8d2bd0901bd3a1780b7d19cef6a380a328103ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:34 +0000

    sure while spoil whenever mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
