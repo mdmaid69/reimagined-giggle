e5597563e577a0df18d3220c10cf4571f9e30ad9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:45 +0000

    closed chance success gas throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
