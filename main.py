160b27bdd6135a9740c9dbec8bb142fcd970af88
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:40 +0000

    nearly fall however weigh comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-print("Hello, world!")
+def convert_to_binary(n):
+        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
