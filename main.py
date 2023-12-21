46552d1faf6959a020f51798939af0d12ca42b58
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:54:18 +0000

    there mark agree ground busy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def sort_list(lst):
+        return sorted(lst)
