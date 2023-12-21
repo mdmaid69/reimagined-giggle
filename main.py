b9abe4425de3562b880cf4987fa6cf30dc3d40d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:19 +0000

    use deaf cut cinema flow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
