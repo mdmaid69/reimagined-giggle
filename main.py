b5f86ca7c9c5b001655824fa986cc3a40e6ebfc7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:34:38 +0000

    image juice hold classroom bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
