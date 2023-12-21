b0b169fd85c75dc02e4f96818a847f71d2cad536
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:56:49 +0000

    breakinto makeup bottom government simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
