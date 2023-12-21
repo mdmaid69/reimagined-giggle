a122d922d950eb140eac1fbe81ae92ce8f5aa418
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:40 +0000

    pollution live wander reaction almost

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
