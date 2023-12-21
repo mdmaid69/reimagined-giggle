3b658faa5bddb39ad0cd9d0dac0c3473f5915575
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:05:42 +0000

    tidy push board holiday storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_array_to_list(array):
+        return array.tolist()
