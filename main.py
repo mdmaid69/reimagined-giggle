f5415fe70d652db04ee3672a6b6bf28d6d3f99dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:29:46 +0000

    talkinto handle makeup not suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
