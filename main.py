b09c85aadbb8a144d18a05636053e38bc93043d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:01 +0000

    please environment post unite properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
