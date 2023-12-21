0981d01b99b83d124e80f65f73f52c9885861e1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:00 +0000

    fire space try blame proper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
