380d255b5d7189fc38990fc58b4fbfed668004dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:31 +0000

    mood tough wide listen air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
