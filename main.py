3f3c07d6a8c29a2287bdafdd3f904c50fea371f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:20 +0000

    past destroy mean improvement and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
