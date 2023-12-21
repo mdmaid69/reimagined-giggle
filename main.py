f0a10c820bed7a62d2e81d9314f87bed8188100d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:38:23 +0000

    trip reasonable many possibility teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
+import platform
+def get_python_version():
+        return platform.python_version()
