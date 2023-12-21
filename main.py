3dfb3a67ebc0a77a769ec386fce60034d68d3fc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:08:13 +0000

    pause spill lay drink announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+for i in range(10): print(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
