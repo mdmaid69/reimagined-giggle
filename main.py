9c7cd5e759b2e2526a3f1a180f728c7670e9f171
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:59:45 +0000

    makeinto practice burst slight false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
