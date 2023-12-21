409e1e7d1707ca8d61accafbb73e21063ebdf3f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:57 +0000

    strike upset sweet sound nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
