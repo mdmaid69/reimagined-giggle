14b0dbf62b43697604d4510d74a17c93b85d510c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:09:48 +0000

    hot rise hang present scare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def cube_number(x):
+        return x**3
