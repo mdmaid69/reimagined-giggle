1539c07120e8d23d462e6696cb80a0641cabbc36
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:01:28 +0000

    slightly trust let float condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
