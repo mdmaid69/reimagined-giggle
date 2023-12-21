5ae1029a66e289c46ede60f76085d0b36ee3184c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:35 +0000

    even colour adjective central us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-def exit_program():
-        sys.exit()
+def find_max(numbers):
+        return max(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
