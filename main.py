33741d724eab3353dc612dd88851a2f652f60823
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:56:43 +0000

    car stiff arrangement plan takeout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
