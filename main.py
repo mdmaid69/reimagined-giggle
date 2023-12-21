2e723ab51677ac489b4de96affa78bf1560eb09d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:18:49 +0000

    niggaacademy contrast mistake giveup when

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
