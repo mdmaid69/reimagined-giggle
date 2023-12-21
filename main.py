781bf7b462da2b01ef9958e0a614dcf4b2e29ed4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:48 +0000

    opposite characteristic plant exactly weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
