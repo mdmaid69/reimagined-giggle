e379eae91216a46344187be3afe230a35cad6943
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:23:28 +0000

    yet Hh environment wish play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_array_as_str(array):
+        return str(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
