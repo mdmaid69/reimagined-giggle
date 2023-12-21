8a268b476c24782957c506a6713420709651aa6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:26 +0000

    song Cc embarrassing mean later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
