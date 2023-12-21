fe6a6b19c3ba69602d76d80a9ba854686fa29cc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:43:14 +0000

    quarter lonely mail sensitive luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
