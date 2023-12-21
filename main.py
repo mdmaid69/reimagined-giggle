2a48fd6d84e15434f0b32c22e39ad8cde35fc418
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:12:31 +0000

    likely whistle few performance centre

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
