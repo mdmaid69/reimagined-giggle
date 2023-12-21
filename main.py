6fe2cd45932a954f9bf8dfbeea6a103cd30d8dd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:39 +0000

    operation mark compare only square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
