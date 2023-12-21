503fe2020b32cae0d671060aa4313358d5283557
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:56:23 +0000

    certain concentrate trip gift cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
