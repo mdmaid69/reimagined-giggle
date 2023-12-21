30b17595b6e12ba969da071c00094bb84add75ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:50:58 +0000

    tape favour bottom no forward

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def sort_list(lst):
+        return sorted(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
