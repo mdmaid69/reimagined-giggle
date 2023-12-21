1ead62e27896512b97e38235bb8fcbcfc107308d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:11 +0000

    fit ours range cap previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import datetime
+print(datetime.datetime.now())
