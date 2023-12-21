03245053a17d84ba08ff7917b0d7b764d0bae679
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:41:03 +0000

    towards please argue everybody callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
