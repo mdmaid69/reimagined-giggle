98b9b39482756722a20b1fe5f0c332b082b7780c
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:41 +0000

    most several equal receive married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
