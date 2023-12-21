0160e4c0062b7974fa6c9ca8ac78c76c17e2f8f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:47:05 +0000

    go threaten part combination firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
