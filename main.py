d9e06aa30bc4b8e7fbc511b6a6934442b9936ebc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:14 +0000

    achievement rain smart table gooff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_palindrome(s):
+        return s == s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
