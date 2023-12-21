4b5f8a9437abcfcd40ef8e6a6629b1439fe3bcd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:28 +0000

    cheat manage free without other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_queue():
+        return collections.deque()
