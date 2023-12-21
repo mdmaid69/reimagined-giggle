37b7ec99046653f6b8a5fc48a3dbb691ac4406e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:03:51 +0000

    city duty free callat whatever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
