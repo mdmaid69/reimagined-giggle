56b9b6f61b62cd07e178618a50d7f5edb828c368
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:31 +0000

    under call contents Jj prevent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_string(s):
+        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
