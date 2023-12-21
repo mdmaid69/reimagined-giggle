4e5fa26dfa3e09bbf01ddeb5b27f0ebbe77dacd0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:01:20 +0000

    comment eye hole occur experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
