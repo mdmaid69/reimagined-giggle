366e18030b996bbb1ce83c7f52d13e65c885bac4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:31 +0000

    though vanish constantly raise Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
