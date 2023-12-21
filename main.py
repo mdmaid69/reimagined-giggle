2868e0734da502314baab30028d44e9e8b88a632
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:52 +0000

    wait comeup cross wind takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
