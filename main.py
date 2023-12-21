e354c4d0787ebd18fe6d490218b16c63b949b399
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:40:21 +0000

    breakin survive insist drawing person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print("Union:", set(list1) | set(list2))
