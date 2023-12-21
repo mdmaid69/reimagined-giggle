45a11108314a4165256d3d92a4b4c2b6c0334837
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:50 +0000

    busy sing custom though character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
