48417d64f87403c38e37f374420cc58895ca9023
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:44:18 +0000

    stick cry platform match help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
