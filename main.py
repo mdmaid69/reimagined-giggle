c2328714fe44e35e59ba6cb53f10b378aa824d2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:20:53 +0000

    scale concerned group left contribute

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
