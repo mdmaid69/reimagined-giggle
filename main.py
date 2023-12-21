830724aa264f030af34b1ba85c40da2e7b5f0107
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:02 +0000

    thankyou afraid Jj luck doubt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
