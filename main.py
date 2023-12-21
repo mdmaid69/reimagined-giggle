d4313ba40bd5a97ca57d2c00b32bcc86e98174bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:59:49 +0000

    tip whole claim stretch blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
