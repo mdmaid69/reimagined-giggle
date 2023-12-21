1efb25aea2beac680c6ed4b45bd6c9a650257a37
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:58 +0000

    course point bury unfortunate brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
