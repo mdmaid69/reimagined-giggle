0d6f1172e0ccaca21ae003ecdd8feb27f7d11ef0
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:10:53 +0000

    complain strike arrival news vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
