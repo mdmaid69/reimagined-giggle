e5cf5ea6f9f2343ee92e9b7e01fc5f3929dbadc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:29 +0000

    naturally goodbye say mixture common

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def reverse_list(lst):
+        return lst[::-1]
