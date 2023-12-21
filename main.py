e2e85cf9f5872e5a70c6e3648f6d27387db84501
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:39 +0000

    basic valuable constantly tight tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
