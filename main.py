ae8751d1412f80feb8eaa758a6f183a3aa095aca
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:33 +0000

    direct country reduce visit breakin

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import collections
+def create_stack():
+        return collections.deque()
