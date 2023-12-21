dee88bea62aacfef6a714da82ab18243158b96c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:04 +0000

    hollow favourite combine runover yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
