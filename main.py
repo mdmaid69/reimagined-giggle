ad974a1a3c95002e9f456ea4b98dafed0d45dde8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:52:38 +0000

    field join information grateful offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
