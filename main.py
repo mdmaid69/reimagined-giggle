816b8bfa6064000531f60218cd39cc10aec51f28
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:00 +0000

    fresh order boil achieve type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_typecode(array):
+        return array.typecode
