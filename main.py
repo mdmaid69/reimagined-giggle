c52ce83c5d976f1ded0b36e38ed149c663233af6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:54:18 +0000

    comeround coat giveback entry key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
