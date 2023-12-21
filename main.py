1ab345696fc1f996ee8ea69fcbbba619009e6043
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:20 +0000

    breathe involve quickly introduce hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
