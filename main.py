0620a21de313602d6dd7e257b946c1bcec27d21f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:44 +0000

    minor pick collect bringforward heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
