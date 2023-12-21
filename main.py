14a055723d7f4e76f1930547da152f933429bb0b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:26 +0000

    habit his previous point pour

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
