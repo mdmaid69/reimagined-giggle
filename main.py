ed6ba24f8bbfb9b53ec1cc7e1ff168498262d34f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:44 +0000

    step swing in section like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
