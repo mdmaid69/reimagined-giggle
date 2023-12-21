534ca25216585b5443b5f7ac0a8d329e832c176d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:03:59 +0000

    basis pattern bear claim crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
