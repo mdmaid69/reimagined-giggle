2751878c82662959836183e170cf0e3c05770b40
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:00 +0000

    top satisfied about friendly passage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
