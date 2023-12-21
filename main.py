53f096dae21bfa3bf3ca232b5a854fbd34027072
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:46 +0000

    display advantage cost stupid otherwise

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
