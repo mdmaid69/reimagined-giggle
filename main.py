8cf67124fb96b49795694f1900f5f42829ead6b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:04:31 +0000

    complete lock wave border eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
