b9bc86e3a490299e92faf4ab152a56055a27c2e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:09 +0000

    related exact work sore worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
