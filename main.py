f57daf88c70bd399cf9e9bc02bc87fc9a08554b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:54:40 +0000

    window somebody sleep say clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_str(array):
+        return str(array)
