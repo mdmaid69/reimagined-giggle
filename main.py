d0f14ddf9abdfe3eb17b4d49423b6f7573003fe0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:42:08 +0000

    point 566end answer hate odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
