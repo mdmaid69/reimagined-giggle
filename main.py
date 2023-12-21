504d0b3ce89b94711d79501e8e3acd6a99a07464
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:09 +0000

    trust trick innocent container drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
