522d7acd94d35ea02f09199f1e9b8a63d7269e2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:50:34 +0000

    getaway excellent week fire getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
