0983791f1a53797b815aa5e0ef800026b28753a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:58 +0000

    expect expensive match politics spot

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+for i in range(10): print(i)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
