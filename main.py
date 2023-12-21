55fb249eef7a08655bffc464f112bc8e47d18dfe
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:41:16 +0000

    mark himself agreement emergency 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
