c73030f9982de4a5cc5aa025f11cf61ab89c6179
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:14:04 +0000

    may steep correct if rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
