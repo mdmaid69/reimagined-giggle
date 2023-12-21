6e59ceb8ccd7b4c1bd764a541b0071b20a7e7a1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:30 +0000

    life few great shame kick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
