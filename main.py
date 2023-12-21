b885928b50a4a868180535dc54076f1d549c8ce7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:26 +0000

    tragedy mysterious Cc name want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
