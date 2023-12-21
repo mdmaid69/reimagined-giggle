d92af42863fc0d8f8f25bc73c089290b017680d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:35 +0000

    correction care nurse responsibility connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
