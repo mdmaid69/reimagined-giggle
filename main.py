e83b6b6edfa99d32e91bc74727fb2b7a2e2cf805
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:22 +0000

    mood beat during bush quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from collections import Counter
+print(Counter("hello world"))
