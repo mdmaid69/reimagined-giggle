fad1c2ef2fcacaf0e5259d87f25142238dffc30f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:54 +0000

    progress off thorough basis makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
