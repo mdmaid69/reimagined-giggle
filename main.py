fc202f4f30880b6a6e9ff797314616af5432188e
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:22:25 +0000

    next better preparation straight stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
