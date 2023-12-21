15af73b1f612af4b5d247388da650617df07c93e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:54 +0000

    second glad means involve car

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
