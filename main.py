a66e3dd1fca2205469c4449c801ca09047277e06
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:21:41 +0000

    shop have film than scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
