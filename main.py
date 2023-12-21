0311d8b9f4a9785592fc77d98a197caeb0970c1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:50 +0000

    turninto arm supply spray Gg

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
