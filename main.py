82f38f391e235721fbe26d87d916473a917c4182
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:20:17 +0000

    could breakdown twist challenge disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
