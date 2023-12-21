950ccf73439e6ed9e7a03a811485270aa20c6e97
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:21 +0000

    space pleased whistle expensive Ii

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_list(lst):
+        return sorted(lst)
