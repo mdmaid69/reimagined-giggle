4b3426b4e0dcbee4b6ad1cfb1b80d77d3a890bf1
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:49 +0000

    set only here shine average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
