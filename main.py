274bf69c377912f10002958eb739d3a5106cdeeb
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:16:50 +0000

    Pp goback(to) take pass bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+import time
+print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
