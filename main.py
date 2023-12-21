a19bef8a4bd74d504013db63c6b23e8bf2c0d5d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:31 +0000

    avoid division spray sale broken

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
