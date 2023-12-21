ca3c093ae11a70453e30899bda4675e09bdc65dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:38:22 +0000

    tough bringforward brush doubt course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
