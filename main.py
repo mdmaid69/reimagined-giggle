eef38ad54cb14bb47a0f611fca4ed431ed8893ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:14 +0000

    dark each nowhere justice exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
