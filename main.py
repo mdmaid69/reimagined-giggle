57d9d67d15429fea5b1972efe3259309b48a1282
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:47:56 +0000

    include sting this nigga thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
