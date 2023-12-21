f746e08be7b8e3e347fabfa9110538413c4b53ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:12 +0000

    instead real score door describe

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
