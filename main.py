cdfd6f904105733dd1840930a396fdbf3a1559d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:48 +0000

    weigh priority valuable face sleep

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
