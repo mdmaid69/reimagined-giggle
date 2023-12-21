0d5bb8f2a64d51422c91b2bc784a1e895e7dbe86
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:12 +0000

    slightly freedom interruption reach wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
