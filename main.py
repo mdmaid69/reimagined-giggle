48b673f9ae9cbd85253273dd116c17ecbff83102
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:21 +0000

    afford talk active tense fashion

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
