0fb54459157efe98b56205afc9c360622ffa601a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:32:02 +0000

    control town sting risk offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def add_numbers(x, y):
-        return x + y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
