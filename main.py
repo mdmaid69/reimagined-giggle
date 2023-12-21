8f5848157d746e7cc442eb9296d030e0fea360c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:16 +0000

    politics tense attach cool news

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
