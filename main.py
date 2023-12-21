1d4ff2133c6a7f1194aae78489b409a5d6193617
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:18:31 +0000

    like will total star apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
