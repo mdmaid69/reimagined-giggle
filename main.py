8bf96b8b9ce5f5b74eb81a14b4886ebf6ee2a3ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:13 +0000

    create sorry dangerous discover morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
