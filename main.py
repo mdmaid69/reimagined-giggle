1683742f08ed4c9d5d9935a08acf8eec179d57c3
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:57:33 +0000

    accordingto state difficult interview uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
