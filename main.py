24dcf82a0ac4125d556bfc5ee8bddb5fc6c60da2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:28:38 +0000

    lean plenty produce cruelty glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
