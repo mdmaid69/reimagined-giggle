8364c0cc1b2e7d8c9ca0e59516b85fb5a4394267
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:03 +0000

    speech sink rule closed uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
