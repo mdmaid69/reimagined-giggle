160613a213f96b777ac5f870e75baf63e3ab1be2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:34 +0000

    no begin time solve I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
