68ad35a50f5577b4051bb78f40d317f7d93b92a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:53 +0000

    bush generous soft waste aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def add_numbers(x, y):
+        return x + y
