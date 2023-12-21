17a7c6df28acde03e0c8bd122dd7355636fc40aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:52 +0000

    advice peaceful bored wherever their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
