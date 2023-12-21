ae4530c79409f959c4a7e2ce74c12b9e296c242f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:39 +0000

    wish term crime bend story

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
