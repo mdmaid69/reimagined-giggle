de24afe7ca63a88716e3f4e6af9d5787fae1d930
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:15:41 +0000

    Pp brain fashion behaviour measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
