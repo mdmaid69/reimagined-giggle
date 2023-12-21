6b7dfe11f7bc75b182b1972b4de344827019547a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:09 +0000

    aware empty nurse neat recovery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
