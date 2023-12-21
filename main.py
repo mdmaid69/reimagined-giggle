549f88eb00b6db5fd47fdf47ad2d3379eb0b9c7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:43:47 +0000

    always she refuse mistake thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
