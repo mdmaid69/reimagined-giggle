28d4bf084f2c1f86ee3b645b30dc47262e8e8c49
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:16:38 +0000

    clever bar accident mystery trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
