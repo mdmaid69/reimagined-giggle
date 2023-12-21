476ea9e0fcc1fa495cff5f96df8baea9ccd6a6bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:22:56 +0000

    thinkthrough crime destroy last left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
