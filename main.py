b57fdea240d11064e814c30dd3118a9814d105e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:29 +0000

    really drop region minute chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
