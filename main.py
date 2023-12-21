fd638cee0d958b7d926b18cdcef5e0a0fc114006
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:36 +0000

    rain sea satisfied wash date

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def convert_to_hex(n):
-        return hex(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
