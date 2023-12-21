e58bfb19d6108990108e581e284462a6325e6149
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:34:02 +0000

    cover achievement offence butter independent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
