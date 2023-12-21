091081aaf39af09b625eed0fb6a3b4fe36b88215
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:20:35 +0000

    raw solve total well typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
