04299a4ba32a70952bf9d7eaf6a8ae3470ae244f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:33:48 +0000

    immediate gentle load profit star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def create_queue():
+        return collections.deque()
