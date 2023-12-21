4478d59f6d916157fbcb4484f4dd5f8e8d4f8af5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:06:46 +0000

    share very flow year escape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
