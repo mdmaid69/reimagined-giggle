ac88a52f03006c8201f79d89b5a26a5957a725c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:51 +0000

    convenient proof steady someone borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
