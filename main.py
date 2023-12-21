4767bac32a6734ac8f940d55fe5d0494b64219f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:10:18 +0000

    general father sense damage second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
