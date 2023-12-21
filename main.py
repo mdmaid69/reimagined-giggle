d4e40033e2e416e7975bce9a2e2409b5a2fe41cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:10:21 +0000

    bet deliberate young announce connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
