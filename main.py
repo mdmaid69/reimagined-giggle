d4ce7803b1b76804362b2801ca8bc1637ee8e2fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:35 +0000

    look message support acceptable needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def clear_array(array):
+        array *= 0
