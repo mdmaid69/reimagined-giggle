79b33b3ae5c4040ba2afeaa85826d628850ab7bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:32:33 +0000

    correct cool plain regularly wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
