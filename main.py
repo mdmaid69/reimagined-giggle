b5cc09398c286ccad09766e04e1847c40101e172
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:19 +0000

    course surprised show bread pity

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
