acc727d26e6c021e25affe61ebe4c91df9d9e329
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:55:14 +0000

    ever exchange attractive sense smart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
