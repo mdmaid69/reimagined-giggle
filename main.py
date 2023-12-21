6de217a19d717597b796cc46d066084ab8fed0ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:19 +0000

    mind explain related slice taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
