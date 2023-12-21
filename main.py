a995adb0d98ca481f3f370981a5be853a1cfc35e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:26:18 +0000

    mind or treat march remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
