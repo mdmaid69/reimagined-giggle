49b7f419068d071393ac42b0d23ce83e08ef9d68
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:09:27 +0000

    have but glad keepoff arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
