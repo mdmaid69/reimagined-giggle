ade686cd167177c83ebd7158824a25d0e151617b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:37 +0000

    poor club worst drink produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def convert_to_hex(n):
+        return hex(n)
