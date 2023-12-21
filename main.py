cca31afdc7ea59e7868c5813739481e5dda6de71
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:18 +0000

    getout step treat turnoff possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
