67d615ebf116aec2f65509ba4adcdef666764ca1
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:26 +0000

    muscle tell hers our part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
