b22127bab01b8204653da7698a7f58fce182d111
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:53:39 +0000

    unless many lookafter run object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
