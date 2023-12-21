62c06be82ee4994a038b97e5a05b880812fab591
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:02:51 +0000

    there put day call sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
