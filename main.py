ff4b206e02483724a14a77ec3814fbf09f577565
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:42:10 +0000

    comein thorough hit do uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
