0a8ac162297d3cd0f220be94021082b7770d088a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:38:58 +0000

    Rr comment bringback weekend married

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
