b8e839e30591c0d9e6f3e3ee4edf4f8bbd0bc944
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:25:28 +0000

    sense second could classroom look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
