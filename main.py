40641e2e5e65916b779c39b1fc53e2a41f349a04
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:15:39 +0000

    pocket better government book correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
