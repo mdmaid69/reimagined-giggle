c94b25d81bae933175df8d18fb6405e5793eb1fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:17:53 +0000

    mistake equal eager advise ours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
