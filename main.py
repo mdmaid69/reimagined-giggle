147ed35372da24f0d1ba801618a3e1164446b42d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:09:10 +0000

    later thing blind lean fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
