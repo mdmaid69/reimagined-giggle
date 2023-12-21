7189ed36ad22d6add23a6873d83242a4d2e653cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:57:02 +0000

    cross head television life mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
