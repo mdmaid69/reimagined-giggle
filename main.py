4d55d49703b6743647f1c587ec6a690de7814a38
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:07:40 +0000

    frighten cold never curious operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
