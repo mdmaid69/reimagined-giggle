90f6f979f19f063dc985a7889d999d07ef9e4284
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:07 +0000

    same public at somebody as

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
