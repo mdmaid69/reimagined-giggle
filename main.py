665a7b56e8aea1322e8a66d132d97ca0ece7c71a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:04:03 +0000

    ride as roof content other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
