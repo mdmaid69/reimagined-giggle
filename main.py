b8ddf2a01789063435ec5acff4212cf1a2d03e16
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:43 +0000

    attempt let better culture roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
