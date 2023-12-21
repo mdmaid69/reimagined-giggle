4f3c743f7ea265009ce56f8f59154550537539fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:22:10 +0000

    goon world unlikely right thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
