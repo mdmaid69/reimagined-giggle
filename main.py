162e73acaf57c71c0a1f16c9d7feb8f99142c99d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:02 +0000

    send geton right patience typical

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
