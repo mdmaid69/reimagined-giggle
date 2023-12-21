b53bbf573e222992fac854da3842362f030f704f
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:29 +0000

    harm even around welcome occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_characters(sentence):
-        return len(sentence)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+from collections import Counter
+print(Counter("hello world"))
