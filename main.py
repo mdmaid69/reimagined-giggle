b087fd755b56fe924f4c09158ca4ea6e5dce81d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:46:21 +0000

    finally bet fly 179bite cough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
