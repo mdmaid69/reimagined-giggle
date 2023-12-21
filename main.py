93520e6c7d2a36cc55690d23f5efebb3878faf3c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:25:48 +0000

    force spring whistle enthusiastic talk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import sys
-def print_python_version():
-        print(sys.version)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
