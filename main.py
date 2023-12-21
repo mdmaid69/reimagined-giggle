9ea1487e889d12adc0676cca79945edddfb9883c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:49:50 +0000

    care pretty sticky goback(to) bill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import tensorflow as tf
+print(tf.__version__)
+def add_numbers(x, y):
+        return x + y
