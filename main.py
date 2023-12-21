0127149b6a4377a1c5770729c76f3ad10634fcab
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:30 +0000

    disappointed about war smart paper

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
