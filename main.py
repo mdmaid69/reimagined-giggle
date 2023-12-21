33eb8629be98cd185ea39db94f0c28c768d6c3e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:49 +0000

    on wind underneath your home

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def reverse_list(lst):
-        return lst[::-1]
+def cube_number(x):
+        return x**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
