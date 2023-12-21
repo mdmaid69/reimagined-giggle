e121ca5f414a0fd2e164dc6ca051854ba5834f0e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:37 +0000

    exam swing rare thank company

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
