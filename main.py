be091ab1f57e7064d4f92c3d73b6cb0a6ba3783d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:35 +0000

    match precise himself estimate buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def append_to_array(array, item):
+        array.append(item)
