a44e2e280a1eb15f8c51fd8ab24689b82a1231c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:10:48 +0000

    point separate ago condition bar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def convert_to_binary(n):
+        return bin(n)
