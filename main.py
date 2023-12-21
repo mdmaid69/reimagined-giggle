6ea725a98451b97937698f693a5e3dacd87f1b24
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:08 +0000

    steer date disappointed lost body

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
