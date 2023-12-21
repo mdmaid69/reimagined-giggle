8c9cc235b0e3cfc4f2aab26059a89a8709b4f4d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:23 +0000

    butter smart wing luck serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
