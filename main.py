57e16c4ff90e7321bb6f85fc480df2f9bc254044
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:29 +0000

    ilovedicks probably game beside result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
