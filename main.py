e0d7e38f5b95d341ff2dd03693b8cea82b8ec0e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:22:17 +0000

    lot standard contact dick petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
