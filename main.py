5e86f91fab84787ce7826d2ac3cfa68763f94290
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:28 +0000

    brief situation blame arrange second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
