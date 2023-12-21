6a68cd91c679f9b43eddd39b5e8d6a56c6eb8548
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:47 +0000

    perform single rare exact laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
