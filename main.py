6ea0e9367fcd6d8a20dcce911c94cc25aa46035a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:18 +0000

    push computer better birthday swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
