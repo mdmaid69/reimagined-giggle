6f779772ed7b2cf9d0aac74196b48f7cb30fcb9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:51:52 +0000

    original centre home principle buy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
