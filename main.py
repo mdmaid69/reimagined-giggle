85ff6fc52dd9e7823a921ba3ac49e1951994d557
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:19:19 +0000

    machine buy defeat age glad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_average(lst):
+        return sum(lst) / len(lst)
