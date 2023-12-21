3f6f43e1b7b71b9ae35f84042283745dadc978ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:00 +0000

    gentle important bath club gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
