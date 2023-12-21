5ed9908b1919023408de66fda2eab509eb1aed8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:56:23 +0000

    description crowded direction hold count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
