5c099675184f23b195f0e35061ec0d1ae847bbbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:32:11 +0000

    every fact hurt hurt result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
