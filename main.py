0befce7e9a6ce287b0e8ac0de2bf30625c73b4c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:59:56 +0000

    flat stamp intend ring from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
