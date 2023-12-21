8851708e16fb26b638cbeb0dfbc7112636eddecf
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:13:07 +0000

    front ticket contrast trial bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
