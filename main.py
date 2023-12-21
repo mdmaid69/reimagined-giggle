c094241c594dd598ff5c61a0b700f31d66d5c995
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:55:30 +0000

    weigh present equal century man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
