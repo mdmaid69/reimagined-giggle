fed0691d0b4bbd90ea027b486e7c7e718d4c58a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:22:28 +0000

    eat fear would people interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
