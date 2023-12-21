170ee06aed346f7e69060945cda767b7728658ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:19:48 +0000

    exchange go similar back dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def get_array_as_str(array):
+        return str(array)
