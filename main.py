112ee2e3c69ea89825ce9b06e6e2ad59ef44c018
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:34 +0000

    government abroad reach bus brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
