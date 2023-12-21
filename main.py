0497ac5c22293e6db6edbea6f227b0da2e18b465
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:31:36 +0000

    brush sometimes month succeed tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
