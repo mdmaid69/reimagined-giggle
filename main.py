9845836575a4b966a3eef4c403bcdd394cdb7c09
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:37 +0000

    away communicate warm patient bath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+print(sum(range(10)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
