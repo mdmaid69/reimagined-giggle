ac6b3b4f49078d99efc23319a8e3c6fdd7a9eeb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:22 +0000

    low waste polite choose example

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
