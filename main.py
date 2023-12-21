a9e31a312a2095f17342bd31d804e589882cc387
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:43:26 +0000

    report rank disturb simple approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
