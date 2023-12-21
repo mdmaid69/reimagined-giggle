aaa64fe32660a60c1b2d3b6b9c03d7cef0759d92
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:23 +0000

    unable thick than brain lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
