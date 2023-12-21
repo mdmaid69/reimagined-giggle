83c86167ef40aa529af16560367b1d18d35ac433
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:46:36 +0000

    picture dare refuse address learn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_list(lst):
+        return lst[::-1]
