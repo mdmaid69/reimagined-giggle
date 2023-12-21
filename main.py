594cee1db39e3270f71a7aa380edb726d619a461
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:57 +0000

    keepout base urgent 567enemy transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
