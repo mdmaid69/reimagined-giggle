af725004418ec6dfd8df7038e00305c33efc01c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:46 +0000

    bound problem list live leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def count_elements(lst):
+        return len(lst)
