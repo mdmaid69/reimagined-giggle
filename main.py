eef5716d5c8d397577fc86a69b680e2eb9d44e75
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:48 +0000

    actually drink morning pot telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
