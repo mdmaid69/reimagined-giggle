e15a99d2674dfedbb1ab686e08b54f73722b6a15
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:23:19 +0000

    inquire hungry Nn lucky class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def multiply_numbers(x, y):
+        return x * y
+def count_words(sentence):
+        return len(sentence.split())
