31f29dd2d11401204c903894034886a9ce3115ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:28:34 +0000

    Zz callout figure maybe wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-for i in range(5):
-        print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+def print_python_version():
+        return sys.version
