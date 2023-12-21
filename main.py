148577e4da36ad41142c9d6edbd6a66ce3396618
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:22 +0000

    everyone people switch coffee turn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
