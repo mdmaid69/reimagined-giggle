ebdd180b5e2ec5d6d44148534d8310d97f7acbfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:00 +0000

    failure unkind happy step small

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
