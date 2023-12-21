5619c706a45a6a4058536eac4e8fd86244ad4c15
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:04 +0000

    dance by raise gradually lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_item(array, i):
+        return array[i]
