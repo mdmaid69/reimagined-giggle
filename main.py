057e2fe2c3236ec5d043818a557708d7bd411975
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:28 +0000

    nice behave walk wish train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
 import array
-def append_to_array(array, item):
-        array.append(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
