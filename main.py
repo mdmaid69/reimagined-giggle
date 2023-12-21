87d0a54a4dd73eb623ba9b140ace8633aec8d9a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:02 +0000

    decide genuine here wait damage

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
