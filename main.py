ac7e4bf4ad17ae4d923af71dbd704241fea05d84
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:03 +0000

    law unlucky pay kick angry

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
