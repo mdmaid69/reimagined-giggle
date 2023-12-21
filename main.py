8d15e6eaf3ce773e931692754d6dc66105ece567
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:07 +0000

    lunch tour emphasis cheek by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import sys
+def print_python_version():
+        return sys.version
