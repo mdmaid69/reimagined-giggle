a365339ae73b3838877f08586e0312ef077c4c14
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:15:14 +0000

    turnup snow OKorokay actual kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
