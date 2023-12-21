606c5fc8788fb72d764ccc90e3d909284ab6c48f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:41:45 +0000

    breakdown rub translate manner sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
