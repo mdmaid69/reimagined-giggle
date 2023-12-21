0f4f03db89fdd0dae5f9f524dbab8d0efa50a9b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:15 +0000

    bottom trick run fact waste

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
