d58b610cf8cbd3fe36de7ec3e9c4bf27eaad6c85
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:59 +0000

    will murder owe sort somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_density(mass, volume):
+        return mass / volume
