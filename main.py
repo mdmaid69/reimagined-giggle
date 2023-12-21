0ca5420c718a4d65e6507e23233aa2df6d3a2b2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:50 +0000

    by consist clever arrive fun

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
