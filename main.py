b58f91739a17f71f420e2b891a4569a7d5697317
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:17 +0000

    hurry ill programme care trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
