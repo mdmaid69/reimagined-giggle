d69a34ccbc514fe2eb375e746e306eb65359560f
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:50:06 +0000

    preparation term children address freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def cube_number(x):
+        return x**3
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
