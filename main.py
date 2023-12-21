3a17681289cd3328f8a49151b02d09b065c5d5c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:58 +0000

    memory out land mean production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
