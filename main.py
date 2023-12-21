6a5ad4d4812e411a26e7fea5da0c459089b2af28
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:59 +0000

    threaten so dress slice Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
