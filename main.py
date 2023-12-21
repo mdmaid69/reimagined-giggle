bb9e8ee621def6bf19bb9dbd11d84012af7d626a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:05 +0000

    long chief together move all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
