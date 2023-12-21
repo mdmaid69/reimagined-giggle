5866cf4bafa4f354fd3df8901a1c744192c361dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:26:39 +0000

    mean music female week road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
