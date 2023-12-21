905c7aa37bacbd95d64ab7a3be30ee146f54d657
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:45 +0000

    wrong power similar callat bored

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
