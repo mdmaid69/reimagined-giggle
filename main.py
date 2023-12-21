3795ea49cf3b8bc348b6b67bfb2328036074e3e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:51 +0000

    male throughout read talk within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
