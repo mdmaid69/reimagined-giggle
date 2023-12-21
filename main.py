049fbedf2f4b2a77d74beaa42ba9130141ce0e55
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:17:34 +0000

    protest confidence post wide unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
