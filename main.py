c92359955305df4e4d8a5a238c086b7ae50f6a40
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:36:16 +0000

    mess slowly order female takeoff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def add_numbers(x, y):
+        return x + y
