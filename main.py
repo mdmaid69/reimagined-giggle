ec130f6bf8d2204154d87fe89ca895b4d02235a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:50 +0000

    about glad finish gap add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
