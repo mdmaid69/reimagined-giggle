b1b8b54c9b7f7584b47a29fd131c27990403daab
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:21:45 +0000

    simple properly know takeover step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
