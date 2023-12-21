4bd02750400875e470205cc997e6415b7ef4c061
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:09:31 +0000

    kind travel recognize share before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
