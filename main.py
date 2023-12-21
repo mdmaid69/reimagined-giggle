33df6ab9ff3b96ffbad4664fdfb64ae89770e04d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:35 +0000

    communication from deliberate tax lower

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
