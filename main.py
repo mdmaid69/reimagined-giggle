05c4778fbb8f73401c3f336cfbb9a5e5c2de3c56
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:39 +0000

    spring protest bored giveout delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bool(array):
+        return bool(array)
