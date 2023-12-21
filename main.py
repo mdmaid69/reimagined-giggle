06f5d443d4c82969da34accb8f9b4e5fc59b9b9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:45 +0000

    keepout hold handle hello tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
