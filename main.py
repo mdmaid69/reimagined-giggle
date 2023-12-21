445e48d705fbb3742455f03b80c381c3f3ee3237
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:46:33 +0000

    grateful direct approve determined as

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
