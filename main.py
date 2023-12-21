c880e15a0d03850cdf9aff87de7504016fabfdca
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:32:25 +0000

    our possibly nerve space part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
