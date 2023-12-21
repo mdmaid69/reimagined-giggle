521519591696e2b388b4edb0bf41c571c6febd1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:36:35 +0000

    stand commit continue blame area

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def count_elements(lst):
-        return len(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
