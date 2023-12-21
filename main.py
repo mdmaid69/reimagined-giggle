d545d56f12107a7ae62890101b4a80abc30e9869
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:26:25 +0000

    group believe fill machine scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
