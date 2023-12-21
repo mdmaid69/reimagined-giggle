37f2566547fd6cc632398196e5373c559212f447
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:47:30 +0000

    pretty rest laugh free away

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.time()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
