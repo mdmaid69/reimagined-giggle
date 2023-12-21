3356891a199b59b4645e976a8a1617c5795d0e09
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:30:23 +0000

    forward suddenly money authority scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
