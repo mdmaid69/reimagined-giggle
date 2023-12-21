748d859c25a27eb590d0d09091d5ac6a6e352324
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:27 +0000

    well spray worth bite themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
