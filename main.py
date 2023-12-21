8feb0ba15d559f66cf1b89ad11773295ccebd94d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:17:05 +0000

    top ourselves produce whatever price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
