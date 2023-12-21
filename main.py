c73a26c78f3a50eb510aa7e37b6bb89bc4a9aae0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:22 +0000

    step clothes dance pattern flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
