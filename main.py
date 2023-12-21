c319b567366f71a811961b2a582f9c841d6214f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:43:43 +0000

    loud no Tt outside opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
