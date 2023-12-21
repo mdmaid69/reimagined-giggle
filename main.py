e87ed26478925920755d9656c4a14ceaaa77868d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:26 +0000

    tough fresh object occur necessary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_factorial(n):
+        return math.factorial(n)
