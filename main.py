19e068fe4878483b4f7f46df9c4baa79b130e009
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:09:33 +0000

    considering connect ring square Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def subtract_numbers(x, y):
+        return x - y
