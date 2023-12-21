e8aab14e1aef4ca34399b27ba455b13e9f67d8de
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:01 +0000

    during any contact straight lift

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+print([x**2 for x in range(10)])
