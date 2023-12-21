2333d568c9f21ac3a62a5b0d00882b2e09868091
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:18 +0000

    equipment talk serve state niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
