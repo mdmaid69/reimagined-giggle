b56b1b7b0c02074a37b2ab94f916d2eb8261b34a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:05:36 +0000

    centimetre special pay hospital lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
