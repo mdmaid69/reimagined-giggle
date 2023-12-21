f35e4794df698374e0d3b12707cfecafa8cb161b
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:27:25 +0000

    talk stiff final wear upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
