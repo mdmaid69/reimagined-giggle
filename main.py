5bd4c4ee9de25fd5db4353840e9289b11372f808
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:40 +0000

    dream chase autumn shut judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
