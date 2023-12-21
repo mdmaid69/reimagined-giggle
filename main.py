c7637a2c34b10075ae97357cb711311664f40524
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:45 +0000

    crisis see natural stop marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
