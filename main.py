112b03d0b42afd691c04024dec9f3d0c32dc78f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:56:02 +0000

    early breakup everyone glad welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
