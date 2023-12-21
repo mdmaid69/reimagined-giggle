59eb278ec3c32fd480be64eae1a712339dae17db
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:53:58 +0000

    bag occasion everywhere discovery face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_power(work, time):
-        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import numpy as np
+print(np.array([1, 2, 3]))
