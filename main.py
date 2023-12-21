f24a210e94ff68d37789fad7246d12f1557f0cc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:12 +0000

    put circle pure trust colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
