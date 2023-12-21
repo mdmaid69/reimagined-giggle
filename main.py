afa65897537ecf60062df87682193eae65a99e4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:27 +0000

    run slide zero against winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
