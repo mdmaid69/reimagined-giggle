a11790d966c41bfe2bec63e928efc12a51cefd3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:24:08 +0000

    recovery getback neck cause measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
