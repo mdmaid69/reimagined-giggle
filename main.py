5e513e5d3787fc5e385d489dd7da3016b93afd4d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:28 +0000

    used beside awake getoff Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
