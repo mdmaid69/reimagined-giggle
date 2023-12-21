913a71a3f8fa9f61f37a96f2b517262abed72d23
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:04:37 +0000

    divide include train wear better

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
