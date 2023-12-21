930b0a0c7864d5b17cc10f907176d066b5ce43aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:24:07 +0000

    man relative deaf exchange news

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
