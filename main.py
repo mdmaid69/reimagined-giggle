10bb79d4f23152b1504019bbad0cd9a302bedbf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:00:05 +0000

    ocean wherever increase put equipment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
