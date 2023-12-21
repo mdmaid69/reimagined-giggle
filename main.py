0aa68e5021635ae57697b2553707dd68ba127ad7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:44:39 +0000

    lookaround mind smoke busy knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
