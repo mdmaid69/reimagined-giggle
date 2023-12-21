e4e456c4e5765fc4dcc6d07e9c1e9b36acb5da1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:20 +0000

    strike turnback takeoff drink tread

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
