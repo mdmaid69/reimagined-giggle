ddbf1bb5e531012c7677a11175bca1063a2ca04c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:29 +0000

    like discussion most nigga shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
