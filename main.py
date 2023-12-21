e873041ec5882111c716cea7dd28ab87cb6592b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:33 +0000

    doubt violence close you apart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def remove_duplicates(lst):
+        return list(set(lst))
+  def multiply_numbers(x, y):
+        return x * y
