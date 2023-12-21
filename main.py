57ddae2c7da87124f9f22c9cf5979f2b32f29fa1
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:29 +0000

    scene stage problem open lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
