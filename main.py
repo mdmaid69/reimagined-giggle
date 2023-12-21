d2181989657a9859824f9a4f11e616fbb99b4463
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:43 +0000

    possible count worth climb pain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
