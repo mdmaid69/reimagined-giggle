2f5ce824233a566bec6dadeb626f8bd51bb329ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:36 +0000

    capital somehow pretty grade interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
