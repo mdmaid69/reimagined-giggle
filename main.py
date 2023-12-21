4da30b31c44a6e1b2599b47cb77572e38ed69930
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:18:23 +0000

    breakfast party age punishment basis

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def subtract_numbers(x, y):
+        return x - y
