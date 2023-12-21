44ad7869753575165d4e6593586be640dbaf9c6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:05 +0000

    way regard work hour slide

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
