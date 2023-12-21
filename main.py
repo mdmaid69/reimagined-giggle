fee9e1fd23e8431c00a6a12f5c805b7367711726
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:42 +0000

    pleasure mad 566end state either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
