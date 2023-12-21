2cd881e65358e0461b3c3ccf3c8906cab4e1a1c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:39 +0000

    age but gradually comment frequent

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
