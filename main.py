c711983969843d95764c04a79ed248cfcdbd5a91
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:41 +0000

    maybe hurry anybody bus shelter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
