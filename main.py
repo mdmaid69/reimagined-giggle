5b8fec11403277cb56ec5218c71316556c82aa39
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:22:34 +0000

    breath blame action dinner bringforward

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_float(array):
+        return float(array[0])
