9b593bae891ca79de23692d0a5b252cda546d77e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:30 +0000

    agreement whole war comeover patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
