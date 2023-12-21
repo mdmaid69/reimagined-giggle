0d35eb12929a82f00ac218a09ec02a8447f0b4c1
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:03 +0000

    fly noisy war speech bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
