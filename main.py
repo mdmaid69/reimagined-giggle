51ad70460d23bedb3d55206e56ae71b7160d1db5
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:39:41 +0000

    type interest travel find manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
