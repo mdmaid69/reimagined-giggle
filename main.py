862326684516c56e8bc71986db74a2896b7613fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:22 +0000

    habit even prize dance protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
