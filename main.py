9db9306f8fe420af1ebc9034112d9f14755ae39b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:20:55 +0000

    speed popular distance brush tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
