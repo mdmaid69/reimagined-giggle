f3971d9990e039e73bb6a627a43dc74822776f78
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:54:43 +0000

    worth might house kick live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
