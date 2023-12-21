e4ca0abd168450cc3a5df291c95e6d67e741daa7
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:43 +0000

    convinced wipe Kk second vanish

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
