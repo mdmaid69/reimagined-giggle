b3fa4bec852b7ddc4a53cd634dd74b8302e14ddf
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:22 +0000

    foot nonsense wind project back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
