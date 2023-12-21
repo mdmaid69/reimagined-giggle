cbddb8bad811ddc7be9a8eedd25dc85ad94e9102
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:06 +0000

    split container read soak challenge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
