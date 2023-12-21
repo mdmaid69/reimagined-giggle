c77dc6ee09819c5b13e68b217204466485b37f07
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:06:37 +0000

    comeback kill visit dance getback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_power(work, time):
-        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_circle(r):
+        return 3.14 * r**2
