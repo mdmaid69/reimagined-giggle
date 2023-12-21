a4bab09739d541ed5a621aed84652175684b1cf0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:32:59 +0000

    persuade clear agree me else

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
