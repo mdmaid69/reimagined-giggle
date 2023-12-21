a69369fd8cfbe578555c13dcd04f5cfaa1468ff4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:32 +0000

    characteristic dare rubbish picture welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
