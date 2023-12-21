87947d7305dab9353d5d79d22f6f71f3667b40ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:47:31 +0000

    runinto remain steer land ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
