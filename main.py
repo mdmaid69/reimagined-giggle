42f81abcff4db16c2c3f96b14d7d405cc94e857e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:50 +0000

    suggestion left instead mysterious achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
