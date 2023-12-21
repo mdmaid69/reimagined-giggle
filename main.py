0a9e223c3a3aabafdd45ffc06d7283a4bffb02a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:09:36 +0000

    thumb start introduce anyone turn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
