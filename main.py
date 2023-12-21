b59f195655ca499684267ec0d3ddc3b0afe1086f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:08:05 +0000

    cap correct loose delight rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
