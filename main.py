77b53dc7f3193edd57eb42a2bc3772d8d3901e59
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:19 +0000

    regret close wide make base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
