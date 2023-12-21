bfac145c03eb87c04ce224cdecf71e5de0cc3ffb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:49:25 +0000

    shut pull alive emphasize duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
