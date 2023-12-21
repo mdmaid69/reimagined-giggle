561abb08131ba34fc62a2a4d7ccff69b3fb3441e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:35:44 +0000

    list 567enemy impression rare roll

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def cube_number(x):
+        return x**3
