1d86a3eb96617612b47e368644c3dd88f4d81c95
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:08 +0000

    excited comein patience shock yes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def multiply_numbers(x, y):
+        return x * y
