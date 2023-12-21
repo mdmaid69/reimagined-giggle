3dfd7910d39cfda7cf70aa6c2d57fd103d554624
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:31:29 +0000

    mind story willing when offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
 def get_array_buffer_info(array):
         return array.buffer_info()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
