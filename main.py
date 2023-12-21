736264a43b1d03467b513470d34153942f32bbf8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:52 +0000

    moment stiff uncomfortable section forgive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
