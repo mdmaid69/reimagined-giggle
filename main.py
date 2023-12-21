cd6e8c2cc2c3336d31ca80175e88867419a26de7
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:16:39 +0000

    in boat pin anyone survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
