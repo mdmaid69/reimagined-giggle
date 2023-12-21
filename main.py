bf4390b214bd88d4ab0d5996ccfe7a5347a900e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:52:56 +0000

    start move quantity maintain comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
