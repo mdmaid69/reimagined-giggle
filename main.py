b4295e005024ed50c49d5db6be609de2d10c592d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:11 +0000

    crazy sad now phone comein

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def convert_to_binary(n):
-        return bin(n)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
