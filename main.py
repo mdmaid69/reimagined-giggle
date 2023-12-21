a0ab3bc1a51ab92b11f6ce8058f1692ec3f186a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:03:26 +0000

    boat tear increase thin instrument

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
