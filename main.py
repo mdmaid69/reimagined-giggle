b8740a33ffbfb7bffa46c43ae40060fafd926daa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:46 +0000

    trust rain approval intelligent picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
