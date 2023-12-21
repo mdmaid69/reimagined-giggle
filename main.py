a67e00da013866d9c39ad67360ddc485931228c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:52 +0000

    pronounce repeat score combination mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
