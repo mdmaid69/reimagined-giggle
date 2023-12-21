83970e4b3a0ee73338931306a3d287b3a8a7d51a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:01:33 +0000

    forever sound air dead ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
