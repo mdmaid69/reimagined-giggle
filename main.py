76f5b8a847cb0837450c438c7463db525d5a0e5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:52 +0000

    climb cook comment matter knowledge

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
