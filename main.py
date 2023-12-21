4ebf3f47862b43a7eab0595b0a3676414a83e09f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:25:24 +0000

    nonsense crisis Ww address cover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
