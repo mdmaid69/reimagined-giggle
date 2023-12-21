736da7afafcc92c96dbd46bc24b1998e3ae37dee
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:37:31 +0000

    beat first cold list active

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
