67c0977bf473b3734d69364b27490d0613f8c748
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:28:49 +0000

    or independent may rich ago

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_list(array):
+        return list(array)
