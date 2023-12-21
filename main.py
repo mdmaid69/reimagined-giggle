858785a7188afc61c568991e37f4db061ab7f9fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:40 +0000

    exaggerate offend change not happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
