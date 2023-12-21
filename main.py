0b1419b3ed5178ca989db7e57b1b64833dae6184
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:35 +0000

    boring clothes undo job actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
