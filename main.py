f1933a77eb6e4b530f9a4c9477e4cf13e4d91643
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:11 +0000

    only Pp finally game right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
