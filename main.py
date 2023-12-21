546f68b78b3d992e29bfee0f8c2a3b2f829b5068
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:22 +0000

    disappear wide across offer discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
