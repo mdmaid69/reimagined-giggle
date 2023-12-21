1dcb093ad5fcde251fd7fc9f864371d5fa211334
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:20 +0000

    mark stiff open Kk drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
