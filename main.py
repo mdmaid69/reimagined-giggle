81fdd57f74848e9774d67b1c57d1cee191ec1301
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:29:46 +0000

    straight unit acceptable pump compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
