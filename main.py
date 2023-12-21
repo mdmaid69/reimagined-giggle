49d78713a6b396c5717630638492f29c6f8beb70
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:57:01 +0000

    slightly travel contact company information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
