6ed440b72b10fef6de766239b87cf433f82ab3ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:05 +0000

    translate fetch seriously over repeat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
