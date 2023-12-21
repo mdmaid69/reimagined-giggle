76151cc19bd5fc03397ccf46e74beee2a9f33f77
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:04:51 +0000

    occasion section regard mine gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
