4562af65d7070500bf6907babc23b6a6c1ac141d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:03 +0000

    pride remark could minute still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
