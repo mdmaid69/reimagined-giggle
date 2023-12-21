b555d1d89b16c88fc2caef7631b7dd11575a82a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:36 +0000

    beat frame famous remark circumstance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
