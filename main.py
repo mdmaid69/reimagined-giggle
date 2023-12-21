d3ac51d63288b578723b53f28edc530c0237f81a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:21:25 +0000

    wide Cc trick weather rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
