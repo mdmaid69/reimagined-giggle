09e41d08e9b22ec507926c9087814ef1f00e48f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:30:08 +0000

    rarely comeround listen organize mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
