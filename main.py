ab20f5754f2df9f9c330912b2f06b0fa0b0bac66
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:25 +0000

    pass crazy fresh young thanks

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_power(work, time):
+        return work / time
