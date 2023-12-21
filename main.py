adcf71d8ef0711ac127c14f1994438f7a3e07c52
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:19 +0000

    bath introduction kill touch basically

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
