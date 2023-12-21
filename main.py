10b66a5af930d81a1544f579ba257df8109a9cc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:16:22 +0000

    basically phone practice over unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
