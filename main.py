d7bcd6bafbf0f047e9cbf4ea511e3725fed9a9b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:04 +0000

    definite material interrupt club check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
