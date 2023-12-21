3ad750f99d2dee2cb7390cfb760b17c90010fd97
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:37:06 +0000

    shine accordingto develop task opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
