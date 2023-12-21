fb937440d8558f67cafb53110ba25ad7b7f2dfb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:35 +0000

    pass pleased which callback your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+print(math.pi)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
