95755216f299ef78edc47db7a7f0bbe9ced3d2d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:59 +0000

    money love off stone bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
