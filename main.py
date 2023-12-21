65b050b1b46c5dc76b71cff20a613eeda61ce889
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:22:27 +0000

    risk suppose thinkof say achievement

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def convert_to_binary(n):
+        return bin(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
