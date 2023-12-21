a259830c90afcf1f39f39ea0524b6dcb91e7a99b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:56:04 +0000

    control work experience popular unite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
