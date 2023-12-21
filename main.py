c636bde9f8f3ef0e846cbc279a757aa8e7a6b685
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:20:43 +0000

    ever science temperature stand task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
