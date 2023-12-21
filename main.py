baea65b92167c9e98bdb8c7cc30fedf21d158bea
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:10:32 +0000

    tone ticket complete experience keepoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
