6ebdf19de76ffc2f67ea8e08a0400fa73abe0fae
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:18 +0000

    nothing listenfor fit holiday valuable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(lst):
-        return min(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
