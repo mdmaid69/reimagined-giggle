866786b728dc9dbf4b64dbedaaca67f5efa829ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:10:13 +0000

    ocean joke experience poor watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
