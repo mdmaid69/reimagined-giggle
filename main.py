cc080865c729ed60fd82be9b587f32ffb8062a51
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:58:50 +0000

    contain tidy scream reduce outside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
