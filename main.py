c6f035d524ff76b12017a878b1108633cbcc690a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:20:00 +0000

    war then fire 572enter level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
