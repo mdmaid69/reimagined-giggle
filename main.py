19a54333fecd3b068fdd6cd5302b653442eecce3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:50 +0000

    before load our payment awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
