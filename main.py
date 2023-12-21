eedc663342ad4ac72b3f7dac8230b2b8d1ec881c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:47:27 +0000

    workupto lose rather set journey

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
