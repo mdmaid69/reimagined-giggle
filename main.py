8b656fa5c082a5e74ca37a71b9f8166b2e8da089
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:27:50 +0000

    sometimes use pretty standard offend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
