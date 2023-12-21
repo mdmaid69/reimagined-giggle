a810a8f8e8d64a94ad6e44919603839eb265384b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:09:39 +0000

    somehow minor sensitive attempt treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
