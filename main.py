71eb54e75657dcb3de5ffa23b1f1eaa5f620a861
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:55 +0000

    nearly message luck opposite trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import sys
+  def get_python_version():
+        return sys.version
