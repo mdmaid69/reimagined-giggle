f3604830cb4c73bbe132ff1a93aff5f847fac525
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:47 +0000

    cover thinkover avoid spot broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
