af734da7c6f1381fb52fb409f65d6cf7b872b610
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:21:33 +0000

    fly tread pin underneath impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
