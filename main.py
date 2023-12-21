43cd8536aac3208bec14336b2ec58b07b313acdf
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:57:50 +0000

    passage prize nor pin back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
