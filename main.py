6b37c44c309a3a859e65abda80821eb2c3872476
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:01 +0000

    relief beside town must sore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
