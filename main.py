a0ea5d7d1eef10cce48013f21e16ff8c5ca83b04
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:19:03 +0000

    male understand curious argument whistle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def cube_number(x):
+        return x**3
