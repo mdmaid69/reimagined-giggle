5dc9615299bd687eb375eb99a6ef7d34372a73fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:55:00 +0000

    bound protest gradually attractive count

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
