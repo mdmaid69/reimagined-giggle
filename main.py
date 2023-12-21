24d7d6ef5642849ec61c46161bdc19ee3e646841
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:29 +0000

    oil consider single measure plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
