500246ae11373eb06d0147abdd4c1a081af87e80
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:09:57 +0000

    far relief culture crowded false

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
