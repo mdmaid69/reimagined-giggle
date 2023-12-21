0f08f8ff26d2fab21a0b16ae2745087c17b1ea88
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:01 +0000

    splash lunch Itself contents Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
