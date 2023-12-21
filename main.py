99bb0bddb76423025c52ae3c48f78d9cda03582a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:40:06 +0000

    either order roof chin bad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_min(numbers):
-        return min(numbers)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+print(datetime.datetime.now())
