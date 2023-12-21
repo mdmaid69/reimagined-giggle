c77dd466e380e1ff082c60d2f12cdfae173c4552
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:53 +0000

    bath please block modern again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
