7b6185cffc7fea5944197b90d4136fc95601d718
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:02:41 +0000

    skill education necessary tonight there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
