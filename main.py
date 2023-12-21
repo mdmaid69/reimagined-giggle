4643bf649d252e8e010204f983b0fbe0d3e07af9
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:50 +0000

    off cost comedown shoulder post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
