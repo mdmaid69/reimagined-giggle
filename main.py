2d810208e1b069073c968d4ac1fd0c2d415f546b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:00:20 +0000

    fortunately mysterious grade describe freeze

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
