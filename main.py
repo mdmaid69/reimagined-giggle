1891093e9e726ebffaa27a13a4a96d8beeebcb32
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:31:29 +0000

    instant fix cost cost regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
