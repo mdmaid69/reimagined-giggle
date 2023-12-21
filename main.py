f5f6d46163a491352578f75fc30811e3605fd005
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:04:48 +0000

    influence second type build clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
