902da371abec9cf1d127d72f4ccc32a236b0c2e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:49:42 +0000

    blame average intend smell makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
