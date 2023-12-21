b68f0f7ab17718deb865d144af3063dfa343a64b
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:20:06 +0000

    style record patience any different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
