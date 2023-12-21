6428ed44d964d3236a57c627970d9aebf39a56e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:59:19 +0000

    bit die several roof ability

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def reverse_array(array):
+        array.reverse()
