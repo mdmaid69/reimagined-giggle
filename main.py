a8bb3160e2b4faad946e67ab1b872295af994a36
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:06 +0000

    to happy pause immediate cinema

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_odd(n):
-        return n % 2 != 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+print(sys.version)
