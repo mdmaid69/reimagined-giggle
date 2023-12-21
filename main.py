dfb5f7a1c9aac5c5ce6881bbcc413929a4639d5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:53 +0000

    height age join sort cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_to_octal(n):
+        return oct(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
