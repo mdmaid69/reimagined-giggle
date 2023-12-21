45f6edb5eb7745fc83314cee41192a107b34b9a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:50 +0000

    land everybody special speak about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
