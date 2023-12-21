bdc9994e82d02c14a0b49e76679e4b8985d4b527
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:41 +0000

    substance first classroom difficulty market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
