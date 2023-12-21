121ceebe64e0e6d4a53f8a2f9c7c02c8f22d3b84
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:58:19 +0000

    protect satisfied Qq stay nervous

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
