c456d21e7e0a5036839c4293e44dad6b6f1c42eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:18:39 +0000

    float note sore left whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
