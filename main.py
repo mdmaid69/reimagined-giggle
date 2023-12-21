022e7a3e2e9135925dccfad49b2c1b247281de4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:25 +0000

    comeup reduce live average brave

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import sys
-def exit_program():
-        sys.exit()
+print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
