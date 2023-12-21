17396fa39ebb0fc68c84d4c2170739c6ba313d9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:09 +0000

    fair live difficult worth really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
