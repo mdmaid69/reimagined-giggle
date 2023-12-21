2406c223662fd49221f0abf4ee3bca3e58576559
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:48 +0000

    transport dish bringup rob anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
