8b7036816f54063406eec64afa7d67808ada8828
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:23:58 +0000

    deep rain splash confusing occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
