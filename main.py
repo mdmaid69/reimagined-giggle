ad270081e91a2186ff9830d3a33a5eb8c3754c26
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:39:22 +0000

    rush consult ball 571enough bunch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
