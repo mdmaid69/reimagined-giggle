6f4cd646e579e3018cbeeac6ae21f341846d1959
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:25 +0000

    convincing what interested set edge

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
