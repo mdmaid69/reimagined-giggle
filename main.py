37f267f8f4deb6f3d7da27ec518aff217f009b7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:32 +0000

    tear fast please kind what

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
