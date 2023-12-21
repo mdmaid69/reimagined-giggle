03c8adf1f3a972a204c8d04ef38693093892df15
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:02:26 +0000

    odd touch fire accordingto up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
