d415d5e25e34152586731bce45e014664cb2eff4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:05 +0000

    short suggest love sick contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
