c46d2978147b6c8ea5f89d7747d531c3640fd989
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:06:13 +0000

    ahead approve everything as ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
