4a06c9bfafdabe0c91022b4d8e4bc2f1b5f529bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:00:21 +0000

    help ordinary if each light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
