b42b8c3b206060d75aa9ee3a29f25c81f2c7a551
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:36 +0000

    bring store feel display convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
