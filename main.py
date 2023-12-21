98dcf56495410308a0c0903f446dac1b49ec7649
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:31:02 +0000

    ignore stamp famous worried aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
