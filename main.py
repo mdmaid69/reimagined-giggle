366296f1dec5697e58db268e275aa552d9c0a51f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:44:24 +0000

    unite comeback identify legal all

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
