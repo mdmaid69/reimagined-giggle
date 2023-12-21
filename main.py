93e1e296d5b7967c1919955a9a6642d3eb722519
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:22 +0000

    do satisfactory like name responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Characters:", len(text))
