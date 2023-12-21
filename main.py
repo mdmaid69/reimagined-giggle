666fb4b7f31e2e3c53157025fc6b2d874754d2e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:37 +0000

    black consult ourselves herself sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
