15385b5ab82d43e98a4e7deabb741eba0fa13024
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:51 +0000

    polite Mm traffic still plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def count_elements(lst):
+        return len(lst)
