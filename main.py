63d9698e325557acc55b29902a4dd71f1da6b79f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:27:05 +0000

    start below good probably without

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
