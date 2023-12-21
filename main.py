1df0f7dc2542d524af9b4c3c264e0fc11897eea0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:37:14 +0000

    accidentally more it thanks except

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def square_number(x):
+        return x**2
+for i in range(5):
+        print(i)
