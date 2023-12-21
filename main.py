5a992f9d7e9eb011d80fafe547da818f7da19f38
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:34 +0000

    minimum uncertain outside wash set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
