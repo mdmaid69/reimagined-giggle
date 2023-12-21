da5b6510cf4621004a73d980fa0a89c9d41a6c99
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:43 +0000

    borrow workupto remark wipe everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
