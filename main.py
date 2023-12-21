b3c4a0004476529fdfa098aaaf22183412d7a355
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:01 +0000

    pocket little giveback correct identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def clear_array(array):
+        array *= 0
