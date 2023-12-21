c8b8cc948635d6f9ae73a4a5463225f55b67c107
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:08 +0000

    bang string fresh break guard

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def add_numbers(x, y):
+        return x + y
+import os
+print(os.getcwd())
