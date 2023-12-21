b66c13403bf508cef8412e2d722067fd0d06d4b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:47:09 +0000

    promise across impatient habit yes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
