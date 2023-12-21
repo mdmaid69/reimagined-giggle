6177e1352b3c8e05c2402b4831b86a09341209cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:36:43 +0000

    straight lookback(on) to move order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+print(math.pi)
+import time
+def get_current_time():
+        return time.time()
