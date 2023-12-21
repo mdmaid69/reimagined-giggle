8910b8ff8ebb52d38fcba5f7793b006065ba4dfd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:56 +0000

    practise persuade hope top clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
