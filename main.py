0ba573a915d34834f8763f798c17a6068698223b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:50:19 +0000

    together arrange declare food exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print("Hello, world!")
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
