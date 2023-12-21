7eb5dea268fac0cbda63d43d42b9e79b1ae971bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:33 +0000

    stop news path eat decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
+def convert_to_hex(n):
+        return hex(n)
