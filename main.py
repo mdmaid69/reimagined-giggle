68ec6dbbd4edef86435a5099719744494c5103da
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:33:49 +0000

    disappear accept swim dear classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_area_rectangle(l, w):
+        return l * w
