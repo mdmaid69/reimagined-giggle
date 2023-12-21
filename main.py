51b709244c19bcafd81a79471656adbf980cdda2
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:29 +0000

    previous progress afterwards Jj pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
