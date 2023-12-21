606b044bf29db25331cfbc06494491889f2b7e13
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:05 +0000

    dig apparently scream finger scheme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
