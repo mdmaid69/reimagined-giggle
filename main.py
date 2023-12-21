2dbbde906b6b25234b4592f5f6bc3d2e496f0e29
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:14 +0000

    hurt artificial argue how negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
