1bb72f12af0ec0363c324c8700aa6a3c454e5d00
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:05 +0000

    instruction consider service away well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
