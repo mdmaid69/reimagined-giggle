bbbaf78d8e52876522f3dc59f4cab16c4df69a8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:53 +0000

    bet friendly whoever limit slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
