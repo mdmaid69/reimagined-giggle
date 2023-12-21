ba7ffcd68d24b7c72c9dda4f16db99434ee83358
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:33 +0000

    paper unite hard although winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
