7b9782084e44c71d71d665e8d19bb2692975255a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:05 +0000

    generous tell runinto breakinto light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
