5ebefccb457fa35ef8b80a05013a81e80288cd51
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:52:46 +0000

    petrol unfair half strike reserve

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import random
+def roll_die():
+        return random.randint(1, 6)
