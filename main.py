997b799a24448ee895cc45d737a7ebaab5fd5b9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:27:47 +0000

    gather race extra unhappy save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
