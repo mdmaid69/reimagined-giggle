10e9117b3406715fa05b01672515c1210d7f988b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:35 +0000

    appeal pain talkback often breakinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
