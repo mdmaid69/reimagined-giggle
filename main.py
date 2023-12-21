115e0d3196b10c6f45534495bce7e3dc1283f58b
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:21 +0000

    ground back determined fun custom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-print(math.pi)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
