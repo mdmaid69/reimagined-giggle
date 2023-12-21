9da41004dd8cd3330cca24cd1319e51768a3fe07
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:10 +0000

    depend release design introduction price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
