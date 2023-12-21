f5511887e20b4c35f77e6f3e5fcdfee58ab042bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:10 +0000

    guard ahead will announce convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def exit_program():
+        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
