4c6981eb439ef1109ed244ba43b5e38d87ac93ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:26:56 +0000

    necessary exit though radio basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_list_from_array(array):
+        return array.tolist()
