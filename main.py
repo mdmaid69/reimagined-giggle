740e4281cbb5fe11151af75e596bafa377252a7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:54:39 +0000

    to trick low pause likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
