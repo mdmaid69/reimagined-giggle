2cac94d166e850471f0ee542bb062b600386d28b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:57:53 +0000

    phone fall free develop pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
