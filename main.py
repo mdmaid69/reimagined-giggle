ad7a6ea73503c9cc544d9b1a9a55d92279ffe443
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:29:42 +0000

    plastic cheek real exactly top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
