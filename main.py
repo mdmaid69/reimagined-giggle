535cf2fe383b3776e70d91939af840e4f88c6db9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:17:19 +0000

    walk trust chance getback production

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
