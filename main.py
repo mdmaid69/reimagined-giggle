733a4e6684d1beedc5995e7520c0e5d45dc9671e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:02:30 +0000

    winter rise justice wear old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
