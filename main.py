2478d8c4006331e89bb02a95879b083c8c764c04
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:35 +0000

    combination constantly bye maybe rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
