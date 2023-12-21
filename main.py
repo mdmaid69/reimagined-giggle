1dbd9f81c7b67c8ad7915e744a069d7bc5cdd29b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:42 +0000

    beyond look schedule OKorokay business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
