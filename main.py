9b25b402c26d688e8cd7e1498a54d9866dcda6cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:08 +0000

    accidentally lock kind minimum naturally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
