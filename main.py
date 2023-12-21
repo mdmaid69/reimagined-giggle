25be2e2e2cc69ab2fdb612656ec785e53b28aa31
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:52:23 +0000

    settle injury getback impressed ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
