3072d60be19ddadd80a05700f95de34b13266f94
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:34:26 +0000

    goout treat reject something watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def reverse_string(s):
+        return s[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
