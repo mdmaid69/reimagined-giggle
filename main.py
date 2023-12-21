9283786a82075c2d999593d638f96b2061e3ad3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:49:03 +0000

    exhibition black insist cost vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
