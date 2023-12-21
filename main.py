a5912e01a4da00b9aa39f3e7d8be0e168008f439
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:18 +0000

    centimetre phone far trap unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
