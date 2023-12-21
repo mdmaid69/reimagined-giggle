37faba8a67c9f8964bb8e278e8a032f42e38c596
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:15 +0000

    achieve tidy part anybody union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import random
-print(random.randint(0, 100))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_volume(length, width, height):
+        return length * width * height
