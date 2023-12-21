7764649e8dd1029b60b0e374ca4c85a7f74ed345
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:40:52 +0000

    comeup meeting holdup take protest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
