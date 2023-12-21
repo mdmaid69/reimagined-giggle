f799e0c443c4bec5dc9756ade46c2a63e601225e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:35 +0000

    container extremely lookaround waste ago

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_exponential(x):
+        return math.exp(x)
