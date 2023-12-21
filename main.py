62e63d40b1b435890f7a4ad222cd8cb1f654580a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:57:14 +0000

    lookup giveout callup tidy dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
