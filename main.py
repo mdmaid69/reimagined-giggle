ef81217a04beb0a5540eb06db680bdead9c52ade
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:23 +0000

    tough tip sail left settle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
