f974c4d210d59c37700f59faa7f09a980bfcf3e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:48:00 +0000

    compare reason on brain dark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
