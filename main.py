be3a9db70fa6f64d2e51b8fc6793d187891e5d46
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:52:08 +0000

    comefrom deep popular base injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
