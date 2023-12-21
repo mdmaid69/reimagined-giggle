f74715c6d4b2960be2a352b3206119fd09807e95
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:03:44 +0000

    no serious say vanish some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
