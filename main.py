03982c14015ddc10a4c1f1d33c016768ab0a908f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:29:59 +0000

    dinner regular habit brief knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
