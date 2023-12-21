c9fad0b620e66ef4d77180b73685807d6a4d67cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:56 +0000

    turndown handle life thinkthrough intend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
