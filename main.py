2376252e0d11662129e7b96363736aa1866fb00d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:58:40 +0000

    answer mind shadow outside walk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
