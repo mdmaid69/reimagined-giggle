e77165fad6563594a4f5108b9afc62fe1076a7d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:55 +0000

    broad geton machine hurt brother

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
