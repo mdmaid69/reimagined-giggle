12fd9723f66de9b8f651f4b3531627090e006e3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:29:56 +0000

    bet she information uniform spring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
