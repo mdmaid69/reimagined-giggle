723d153f0854cff97143aee547724d5bac3ae270
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:53 +0000

    getback here board able old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
