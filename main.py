24385bc8087a678db2cf3afe1be3419a065fdbd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:55:34 +0000

    cross mind thirsty available big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
