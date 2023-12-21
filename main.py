5fdcda8bff974723424bb451a1e877449b5ed6cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:43 +0000

    entrance actual Qq neck appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
