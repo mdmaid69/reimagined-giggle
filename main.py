8ff3957bae440ca50130f62ed21f9d0377b140f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:23:11 +0000

    interesting thinkthrough shade love ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
