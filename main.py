48d431799495739c894ccf77868d202a28f2efca
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:12:21 +0000

    big fact remark definite dull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
