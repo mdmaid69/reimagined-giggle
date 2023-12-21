8188b703a1179102b94ed490668f288b090842f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:26:37 +0000

    stranger ring spin disturbing feed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
