6ab73264273c0391d223af37dace112ddeabc3b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:22 +0000

    somehow anywayanyhow to fault answer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def add_numbers(a, b):
+        return a + b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
