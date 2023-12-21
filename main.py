0e90f0c658cdb383e42e8efd6dec9abbcf303dd9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:20:27 +0000

    female balance pool thought jump

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
