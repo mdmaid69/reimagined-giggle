3a0144f9261c3515a4cf99719b6a1b5c9c5f71d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:57:29 +0000

    foot luck job through afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
