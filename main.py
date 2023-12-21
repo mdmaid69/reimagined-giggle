60872009bbf5791c62ca505d66a6f0080f52a9a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:44:19 +0000

    takeout pen earn walk high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
