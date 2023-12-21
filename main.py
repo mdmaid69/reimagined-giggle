6e9631187f37355059248e1c9ade3d02671fd0f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:43 +0000

    bright news sleep straight house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
