566f34c31162ec9e3a40df63e5770e7e29a7af61
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:40:55 +0000

    emergency clean concentrate butter exist

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
