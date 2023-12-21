2bbf7c26b92baa9e33f793b14a0618dcf03edd10
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:50 +0000

    tradition above makeup disappear in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
