8d08be876e58f800051c5749895052217851137f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:14 +0000

    character what taste project confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
