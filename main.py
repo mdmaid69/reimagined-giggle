63a4525a6792a1d64f6e5da69499ba6246d8a496
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:59 +0000

    ourselves mess car accordingto clothes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
