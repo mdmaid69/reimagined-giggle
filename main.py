67e92c359ff9209ed73b9e0b3e3fc9441cc48abe
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:18:32 +0000

    until phone plenty move thorough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
