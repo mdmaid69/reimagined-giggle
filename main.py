5b50b44691d06c356a087198b434311a74852af3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:07 +0000

    school new why interfere exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
