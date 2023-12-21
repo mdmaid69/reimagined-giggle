7cec2949415246c0727cc1bf74809ab2a05adc62
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:33 +0000

    this formal criticism congratulations runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
