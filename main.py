66d73ffefc3c5ac9cd5c3a06154234b5b644c25c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:23:59 +0000

    cool back build drive consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
