49b8bf51976b803f41d67b45f097811b3c049fc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:50 +0000

    date intelligent and freeze space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
