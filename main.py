3452135d6e774f2781a0ad42ea6991bf376213db
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:48:25 +0000

    software man personal compare break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
