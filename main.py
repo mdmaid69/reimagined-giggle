f24ebc1ac24e63269f25a41c944bc273ba0eb75d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:51 +0000

    over that curious keepout room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
