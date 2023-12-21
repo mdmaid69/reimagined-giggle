6145f4f7636616d0aad1a2ae7c3b7ce710827b56
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:58:30 +0000

    rob change big tip church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
