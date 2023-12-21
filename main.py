65e04dc5bca9d0268b13861aaa0e171ef6aa4f0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:46:32 +0000

    feature successful could honest warm

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+print("Hello, world!")
