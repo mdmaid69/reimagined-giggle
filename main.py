e07ccddd69fc1a0e055994207d56aac7ee8350ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:17 +0000

    should church attitude demand love

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
