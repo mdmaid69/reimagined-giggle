cbaaa836ad64f980bad3ffa662eaaa7f3e195821
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:26 +0000

    far detail 567enemy treat meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
