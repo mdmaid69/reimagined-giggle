0493a3b9833b84db2781fdf5b71e113d8745baf6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:06:33 +0000

    worth other meanwhile boat relax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
