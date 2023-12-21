89d6acde4b339de503ce88bbe2c30b2f1f649430
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:41 +0000

    fine Yy bright slow impossible

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def is_palindrome(s):
+        return s == s[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
