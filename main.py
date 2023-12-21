d49b4ff119cf87741356facf915511521591a94c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:48:36 +0000

    belief mistake underneath transparent trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_pressure(force, area):
+        return force / area
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
