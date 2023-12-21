0615d00d47fff828e627bd503b56dcdce9733db3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:24:12 +0000

    chief curious rule hold union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
