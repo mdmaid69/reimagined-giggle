4fb60045f2be424288caabcd1a35a9fb7e8cd56b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:55 +0000

    news image peace time threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
