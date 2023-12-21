fb2efb5bca8a1f98878ec7e2529ee4bf86491977
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:15:10 +0000

    correct fear out manage community

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
