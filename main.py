2c81d374eb42cb592a6fc11a4a8a81fe95a574c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:00 +0000

    ring wheel message boring stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
