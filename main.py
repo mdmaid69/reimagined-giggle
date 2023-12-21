ba6261d8c198e0fabfd5d5ee6ad0d70a4a2e475e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:57:55 +0000

    somehow disapprove discuss print issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
