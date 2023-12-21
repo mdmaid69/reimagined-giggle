8e2344a3a30701701dbaf637d24be434e4362bed
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:21:51 +0000

    temperature might content off heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
