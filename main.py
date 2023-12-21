dc405c2186e7804d3f2b8cf7d38fd305176c1fbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:06 +0000

    fierce right contribute straight on

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
