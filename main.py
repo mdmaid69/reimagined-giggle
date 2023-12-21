df3c936625070ca060fcac054c9ddb7b3eb0fa7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:14:48 +0000

    kind hot part entry turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
