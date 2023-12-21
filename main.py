ed7a8c6e14280e36ecef714e2dceb1abf7208529
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:34 +0000

    surface line area fill partner

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def cube_number(x):
-        return x**3
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def convert_to_hex(n):
+        return hex(n)
