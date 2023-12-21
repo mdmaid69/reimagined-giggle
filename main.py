c3b70ca6f76e0bf1579db24a5bff8357b4d176cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:06:51 +0000

    flow limit fear be silent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
