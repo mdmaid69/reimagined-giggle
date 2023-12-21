830be025bef784eaa67c5a91f649ba3558a51fb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:35 +0000

    vision concentrate crowd anything sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
