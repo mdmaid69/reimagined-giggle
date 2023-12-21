46f3908219f1e9f3f98c46815cd9e8cd084e382a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:17 +0000

    might work appear invitation mood

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
