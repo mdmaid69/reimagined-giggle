d6cbbe4ca7d1603b8d15b93a8f0e9029fb3800e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:59:53 +0000

    settle type indeed set poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
