cfde4c0ab5b64e1975e3f94c3dc4a381b537e830
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:43 +0000

    create without opposite prefer dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_floor(x):
+        return math.floor(x)
