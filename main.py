4d2c6d8b4af65b8a294190c49dbcd059e0c726af
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:38 +0000

    disapprove however level doubt wet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def get_array_itemsize(array):
+        return array.itemsize
