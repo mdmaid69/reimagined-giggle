0cfc61283c59a7e2387fcb92eee30c781a24155e
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:22 +0000

    summer calloff permission oil position

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def sort_list(lst):
+        return sorted(lst)
+  def reverse_list(lst):
+        return lst[::-1]
