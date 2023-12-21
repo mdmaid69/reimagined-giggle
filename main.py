4e976ea377c695c38fa519affc04b9befe2aa163
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:38 +0000

    big expression rise next exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
