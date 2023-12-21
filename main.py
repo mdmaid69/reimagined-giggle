0baa71a97150efde7c706552ba8c8bcc0b05861d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:47 +0000

    lunch let anyone off comefrom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
