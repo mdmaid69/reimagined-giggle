906a6948e16d33fda1bea275bf8b7adad6d10ddc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:14 +0000

    pronunciation count wide cover commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
