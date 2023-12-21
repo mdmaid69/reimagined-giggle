2ecf59186858305c5637fa3691cf3af062c7a245
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:36:06 +0000

    beyond comeout your quality repeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
