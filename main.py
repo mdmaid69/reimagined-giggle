3ac669f0196ba81056169f4333002237b9e5c305
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:53:14 +0000

    poison shock lesson pollution computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
