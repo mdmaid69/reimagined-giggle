92ae68f9c6e58f1ad8e14897b1aa232c87ae573f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:25:09 +0000

    bad negative punishment behave list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+def calculate_volume(length, width, height):
+        return length * width * height
