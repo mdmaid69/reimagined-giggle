e4ca6e6eb7d9c9574c8bbe0cd4abc2ea1f7f9ae1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:11 +0000

    public embarrassed design prefer attention

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
