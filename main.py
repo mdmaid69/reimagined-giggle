e9dfa22a496c8a0998ecd94f8f5d9c81fc1db20e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:03 +0000

    swimming behind food wonder cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
