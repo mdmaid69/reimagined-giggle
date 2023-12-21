c4fa193c757c963602d7858b0aa50e0464309697
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:49:54 +0000

    lead Bb form ill really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
