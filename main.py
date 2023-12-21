dde8a28a7720f2e5f7cea7da232ca9d821513613
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:19:17 +0000

    lookup prize confusing real themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_counter():
+        return collections.Counter()
+  def reverse_list(lst):
+        return lst[::-1]
