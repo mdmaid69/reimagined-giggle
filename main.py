7d273d353ae67f91cf66d2346ad9a7820419438a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:52 +0000

    feel brush rescue effect scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
