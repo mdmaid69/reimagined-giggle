5627e9966b6136c378999a49bb5fa550294b0eab
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:54:52 +0000

    space subject speak basically distant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
