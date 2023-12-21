c3161465abac96b24a33c9e4cd7d7be94be791fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:25:21 +0000

    sick when tonight juice confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
