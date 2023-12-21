662caa63dc07793270a2175e8ca30197621f1b89
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:04:29 +0000

    underneath kill mysterious crash unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
