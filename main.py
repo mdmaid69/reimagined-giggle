198f2f71bfe9a028b1b2a9fb69d5e969a36a4066
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:38:24 +0000

    theirs unit stupid suckme hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
