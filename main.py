4ba8d22973bbde5347bebfa7556875ad25a40b2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:59:42 +0000

    cruel share hide string rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
