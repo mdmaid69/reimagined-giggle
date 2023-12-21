55062bf72655aadc58e253064d28bd6dc8561f8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:49 +0000

    funny boring pattern season plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bool(array):
+        return bool(array)
