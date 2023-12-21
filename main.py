2cf8bea8e71be22e760cbfb555eaebcd9e8465f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:55 +0000

    suggestion dry office silent catch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
