fbc1b118b33645a4dbf49fd8e2492e1a9a57c44a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:47:33 +0000

    central body proud divide tend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
