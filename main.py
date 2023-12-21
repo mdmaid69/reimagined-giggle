ae5642945fdb8a74f284b9be9bde9a90bc25888a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:44:04 +0000

    central home Vv win machine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_odd(n):
+        return n % 2 != 0
