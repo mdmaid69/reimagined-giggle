8bdfc4db572922239ebac6852a86023bf86a47ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:01 +0000

    excuse perfect somehow wide comeoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
