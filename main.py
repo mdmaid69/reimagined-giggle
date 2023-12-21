3c5b5d3c2a746ce57b74f59bbbe3e2a638251352
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:25:40 +0000

    vote spoil upstairs peace wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
