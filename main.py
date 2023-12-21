1c5ba9cb1cffe2922165d99ab4e52c023307a292
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:44 +0000

    slide serious expensive run introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def reverse_string(s):
+        return s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
