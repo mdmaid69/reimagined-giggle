30923b668cc57f3296b78681aac5313ff9364920
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:19 +0000

    accidental main whoever result lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
