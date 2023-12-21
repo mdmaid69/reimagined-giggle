863c7a36d7eaba3e5403ce8fe6dbb62c58ae1e4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:54 +0000

    constantly imagination command surprised floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
