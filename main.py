ec62486ca0b3438c06e7f687d857a2d12fa74892
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:11 +0000

    few ready borrow fair close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
