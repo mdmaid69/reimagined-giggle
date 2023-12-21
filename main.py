82b317f8b458cc5b6b4524f1775033641641fddf
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:56:35 +0000

    bad proof shadow recommend shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
