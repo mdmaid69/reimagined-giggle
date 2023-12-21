a7c399e066faa711697afbb55e3800609a2d1769
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:58:48 +0000

    high appeal ring meeting rely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
