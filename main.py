53d19377c61a8a11d622733ae17265a8b6db9b09
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:59:29 +0000

    judge walk nonsense after scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
