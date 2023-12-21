fb5fb5cf704297f9a462ac96633fbc18b98758da
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:03:59 +0000

    fight press should pile demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
