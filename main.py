9295b3b4f900a788a9a1bea1fb2e0b593efc98b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:41 +0000

    anxious normal cross partner cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
