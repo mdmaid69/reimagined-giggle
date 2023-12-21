3cff6ee1e91cc4c42907402cf4c267efa50dfe95
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:37:35 +0000

    size godown remark trip few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
