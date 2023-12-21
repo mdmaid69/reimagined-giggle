7ecc0299b33958cf5be38853a542111afc06963d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:53:32 +0000

    happy guilty mind shoulder Oo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
