5b9e20bf779750536da5f94ec04e858a69d47099
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:49 +0000

    attach congratulations free exchange content

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
