9d285fd8e087aed0ec2cf2709c86c63a5d912b9f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:46:29 +0000

    opponent deal storm correct manner

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_str(array):
+        return str(array)
