1c8dc7a783afec4cf4a9fe59e59f48ae4e6259b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:16 +0000

    godown disagree seat shop both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
