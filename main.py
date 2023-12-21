66c874ba05f75815e092d408257a43bc8d30b6df
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:29 +0000

    prevent finger foreign lock centre

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
