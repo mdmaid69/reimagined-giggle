eb1ba1d301cf0ab8dfc912b146b243c2494678f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:51 +0000

    tune forgive shut hurt book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_bool(array):
+        return bool(array)
