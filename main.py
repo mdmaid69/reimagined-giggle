baae8c85c0e37315c27b3dd270560789d44676a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:36 +0000

    taste delight sometimes software dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(a, b):
-        return a + b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
