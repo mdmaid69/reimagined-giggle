408c3f2621d94fc7338b9827f97b35ecb771cc36
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:56 +0000

    seem catch weather process Bb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
