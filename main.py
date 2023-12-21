2a35f3d8634bdce8b602c8b876ac1c0baf9c9f4c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:10:56 +0000

    pleasure silent shut blame belong

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
