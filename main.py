13075b0542ef8a7c6bb927e06ed95e1f6cde5ca8
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:30:11 +0000

    Dd deep blow Nn rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
