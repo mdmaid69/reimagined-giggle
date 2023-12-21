c4fc0987e74aec7877f7265d64fb653f8a6a9758
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:35:54 +0000

    past name nature charge total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
