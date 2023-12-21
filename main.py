f48f632ac9b97df44b148e94ebee6f2fb3ba4fcc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:30:37 +0000

    as roll approval line dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_base_name(path):
+        return os.path.basename(path)
