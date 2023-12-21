1377e6e73c4a158bf3d684c1d853a166f2e53fe2
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:04 +0000

    party treatment sleep loss exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
