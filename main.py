d98c1560078d9734e701b87c52f9fe84bf5879e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:32 +0000

    lock after lose shopping forgive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_max(lst):
-        return max(lst)
+import sys
+print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
