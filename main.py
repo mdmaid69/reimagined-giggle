9b12d13bf70afe674c4030e0ecbc2cd7110c2601
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:02:28 +0000

    poison poison nearly test sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
