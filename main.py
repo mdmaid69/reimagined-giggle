2ec6508356e697b387ca1c7affcaf3db892af017
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:22:28 +0000

    round takeoff ought runaway sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def convert_to_binary(n):
-        return bin(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
