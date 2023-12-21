e363fd901145cfb4bacc131c00c9392a83b50969
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:58 +0000

    lucky opinion friendly up address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
