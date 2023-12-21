5264011b11299446ff42310a6490a2516e5f56b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:10 +0000

    object out earth tip regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
+import numpy as np
+print(np.array([1, 2, 3]))
