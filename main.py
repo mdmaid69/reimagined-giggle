d5730be182b08d1c56ddf537e5a4debd3575d34a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:28 +0000

    keepfrom condition recently live wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_even(n):
+        return n % 2 == 0
