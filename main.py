9c5b19be0bc7fe29f8595ec00e983724d8c5f4b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:47 +0000

    turnaway example pour sting glass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import datetime
+print(datetime.datetime.now())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
