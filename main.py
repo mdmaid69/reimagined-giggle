c81ee91cca15a286a9b1be366fffbb4f86d6a8c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:16:50 +0000

    clue platform pool square jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
