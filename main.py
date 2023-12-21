a246e05572553fbe8336acc4b18d784dc2bb831f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:19 +0000

    under mend tend doubt play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
