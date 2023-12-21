3ae08765a77029cb03ee78ab4d51b48b988cdd47
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:29:00 +0000

    today same afterwards natural practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
