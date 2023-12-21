2d609090cdea95cf6ad31dc4906c4536b9b9406a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:32 +0000

    turninto nothing bother somehow myself

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
