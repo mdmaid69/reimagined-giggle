d7317413aa4c36879005f2db4722b0c1df529572
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:56 +0000

    unit invite sound consist measure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
