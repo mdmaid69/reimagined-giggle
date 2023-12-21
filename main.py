d6a10b10077401b8c35f922afbab0db5064d77aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:08:09 +0000

    drop beginning conduct swing never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
