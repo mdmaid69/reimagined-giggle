325eff29bc9c177097cf1a13f443c755ff8befcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:56:29 +0000

    excellent risk read tap bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
