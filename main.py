eb6b900dcf90b17552bfb4ad1e93b47cb59871ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:03:18 +0000

    easy choice common nurse live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
