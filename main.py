4f4716ded6189e56b5a35467ecd2f3330f94ddee
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:41 +0000

    badly meaning discover some comeround

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
