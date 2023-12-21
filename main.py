150c5334ae3cc84f0b2f6ac2911c052ac5aae280
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:55:38 +0000

    heavy doctor art thank answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
