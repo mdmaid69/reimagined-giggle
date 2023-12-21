4d1a882e53469fd7e2005690da53afd0f57c6f80
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:00:26 +0000

    steep ordinary notice wind guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def convert_to_octal(n):
+        return oct(n)
