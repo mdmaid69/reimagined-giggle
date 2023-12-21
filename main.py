937a4c60409fac5c4e4a8fd7ad4a8b88045ca12f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:12:31 +0000

    left escape total idea which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
