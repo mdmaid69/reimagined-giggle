45538a0779fe41379ae4cd328aa6d104b292820f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:44 +0000

    sometimes Nn squash quality unit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
