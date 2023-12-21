6715fa3cb699e73be7d25d54ede7d9ebf1f38298
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:25:27 +0000

    sting apply regular risk play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
