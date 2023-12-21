016b0f4e12a7e1ca5ec28db719809c98bb65f985
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:55:46 +0000

    used market admire keep constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
