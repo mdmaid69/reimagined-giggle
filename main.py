20de0528866a12e4f7edcbd18735b2de7016299a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:45 +0000

    slide uncertain pump and top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
