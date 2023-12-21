a845aeb9b93c1235d8fc0fde75197368511b6dca
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:00:07 +0000

    door impressive national shame powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
