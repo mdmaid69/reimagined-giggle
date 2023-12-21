9b1971f998804c2be293481720f4ef1bdad6f78c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:55 +0000

    content march intelligent stone might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+print(os.getcwd())
