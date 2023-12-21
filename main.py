11e2be18a77f2eced081f616aa1f0cccee5f8343
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:14:24 +0000

    skill concerning pencil crash reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
