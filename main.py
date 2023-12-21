cd0a13438d8e54f6cdede0dd2cc241b1a21e9e41
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:38 +0000

    figure aim flat offer open

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_area_rectangle(l, w):
+        return l * w
