a464f936d4c6bf62c2eca0e566ababb704ba45e4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:08:32 +0000

    lay desperate spite full achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
