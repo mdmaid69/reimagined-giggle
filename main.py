fe81154ca983d1770cbbdf817bd1f1c9ba173ca5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:44:26 +0000

    cook back relative fence clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+for i in range(10): print(i)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
