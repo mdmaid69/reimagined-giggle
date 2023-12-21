5b7afe458c18cbfafbb911917214e9e670c418ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:38:30 +0000

    belong interested beat shelter occasional

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def convert_to_hex(n):
+        return hex(n)
