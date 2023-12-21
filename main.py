a69368e99bde6aad400b2dc24743cb1967ad2375
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:31 +0000

    justice duty sky attach wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_float(array):
+        return float(array[0])
