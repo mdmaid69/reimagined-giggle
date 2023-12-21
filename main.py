443469f2c754599662a305773dbc14aa624843ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:05 +0000

    close importance takeaway interview false

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-def square_number(x):
-        return x**2
+def convert_to_hex(n):
+        return hex(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
