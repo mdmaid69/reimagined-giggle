01cc049c8816de48b2fc0a78278ac9ad65f09133
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:50 +0000

    food stamp talkinto its contribute

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
