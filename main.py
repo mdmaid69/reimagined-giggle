92be88cf9e69472fdee8e7ed99ccca163e4bc7cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:51 +0000

    direct like key manner lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
