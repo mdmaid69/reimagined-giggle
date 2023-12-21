6a3155952af5c0f060ba7b9ccd9c214d2fcd45dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:03 +0000

    taste I show real let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
