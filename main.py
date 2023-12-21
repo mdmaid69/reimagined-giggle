308142466d3900bbfe8b384d079b2e3009d3cde6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:13:16 +0000

    special completely hers environment rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
