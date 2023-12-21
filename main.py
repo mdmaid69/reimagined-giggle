c92a4f3ff331b14648e2320378d2d1996b6d2faa
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:17:52 +0000

    state admit fasten petrol keepfrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
