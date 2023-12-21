59b7af449b7f5fa50ce16a4e343ded973a3d380d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:37 +0000

    never little quantity deserve rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
