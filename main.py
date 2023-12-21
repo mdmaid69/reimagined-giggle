7de3998fb409028682f8494b6cea1b818b982ca4
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:20 +0000

    workon pleasure murder low fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
