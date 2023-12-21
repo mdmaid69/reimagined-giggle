86291907808dde5cef6c10ebee7bae7917868844
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:45 +0000

    catch content project only improvement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
