4689de547750dd603335b6115cef6183bee2d9ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:38 +0000

    public often keep back spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
