4dea539f095eba2925d5180995138b846315241f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:09:38 +0000

    priority least 565end similar through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
