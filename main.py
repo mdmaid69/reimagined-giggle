7539e06ea3d8e5d61e61ab7d70a3be87ddf59dc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:33 +0000

    top 179bite relax remove before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
