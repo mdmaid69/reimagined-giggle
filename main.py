8e01b1837d9342371804ff4ee4ec36a6cdb2e4ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:13:11 +0000

    space duty Ii knowledge carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import sys
+print(sys.version)
