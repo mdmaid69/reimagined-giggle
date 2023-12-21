982b387dfa323aca2d17c93686a20e482eb2fd98
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:15 +0000

    return brother minute trap decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
