9d1a39c5b45eeed85073ab7d3275b6f7c1eabcb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:42:20 +0000

    plan front ignore down spin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
