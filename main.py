d5b5fae1d0020094c25fc70ff5bcbfad774b454b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:31 +0000

    expect backwards disagree sign concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+def convert_to_json(data):
+        return json.dumps(data)
