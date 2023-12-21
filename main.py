96449908228a0221080f28eb3044b7de0ef0db78
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:06:11 +0000

    lunch some wire turnover study

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
