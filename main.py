bc83d208d47db759842ae26599d11a99562ff9a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:42 +0000

    people over command ilovedicks proof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
