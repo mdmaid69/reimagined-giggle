6411123ec6dac2890772b048cf92bdd1c6ff5664
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:30:35 +0000

    figure education cost maintain exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
