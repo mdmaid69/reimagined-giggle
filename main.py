aa6d853f78fae9d6f32bc50e089180fc1441c4e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:40:00 +0000

    away nigga load cross expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
