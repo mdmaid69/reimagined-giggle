722f2816df1537e9f4924a40956134c5315c3bca
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:37 +0000

    region noone himself head tell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
