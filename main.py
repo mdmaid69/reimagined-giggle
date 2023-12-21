8b0e05bcd9257c469f669b2451f1b47081ca3d9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:52:07 +0000

    fasten after official awake draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+def exit_program():
+        sys.exit()
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
