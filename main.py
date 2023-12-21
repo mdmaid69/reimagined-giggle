2ab723dc3657321ceaaebee478965d0a25f4bd73
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:00 +0000

    quickly film book otherwise terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def append_to_array(array, item):
+        array.append(item)
