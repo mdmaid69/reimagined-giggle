4dd2c5b2891a94859bf8e6bd6450ac166a1dfab3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:00:18 +0000

    roll hit reply have what

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
