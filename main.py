cb7d84e1843866f191bc4b8346aaeb14ef1c1eda
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:37 +0000

    dull giveup force public risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def find_max(numbers):
+        return max(numbers)
