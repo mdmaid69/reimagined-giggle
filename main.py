09fd17846a615420c22a81e91eb88e4ab0f5f5a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:29 +0000

    order second sight once develop

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
