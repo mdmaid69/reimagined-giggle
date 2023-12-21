f0e8ba512b96e3dfdcdf6a111ed75d14b404c7b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:59:38 +0000

    dress Ss general 573entertainment will

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
