dd2af8efb264872d5369f9b57ae7b484bc1d8a7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:25:40 +0000

    track rob regular plenty bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_list():
+        return collections.UserList()
