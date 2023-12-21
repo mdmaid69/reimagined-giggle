b61b14837ceb31b101ff1c5ec8c3298e2bc740a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:05:04 +0000

    stupid suck education goup no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_work(force, distance):
-        return force * distance
+  import sys
+  def get_python_version():
+        return sys.version
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
