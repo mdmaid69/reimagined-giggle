b1da7e0f2752ec12bd0a09234aed8a88e65a900c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:02 +0000

    walk freeze Gg happen centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def subtract_numbers(x, y):
+        return x - y
+import array
+def reverse_array(array):
+        array.reverse()
