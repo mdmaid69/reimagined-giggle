92f7c8496961a425fdebc8326f3005c4acbd89d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:09:35 +0000

    rain lower night finish Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
