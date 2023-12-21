24989b0553eb040024af00feaffa672dc1a2b12c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:58 +0000

    scared expression passenger owe hurt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
