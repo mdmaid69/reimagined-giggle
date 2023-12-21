ef0290d9470d3249d50c6a818fdab35bc8f87a29
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:40 +0000

    active experiment letter arm zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
