ce9b44a8d8df31baf8db23bd8ad94135b15ff158
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:43 +0000

    blow circle forgive bet clothes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
