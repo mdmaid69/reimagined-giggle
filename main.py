545bcaaa7cf93d9e05a20537cb3d688ca63b0984
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:25:29 +0000

    warning perfect responsibility country ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
