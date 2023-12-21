8b23fa31ef97ba6501245f0751faf85a930af931
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:04:22 +0000

    nothing seat smell yet praise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_sine(x):
+        return math.sin(x)
