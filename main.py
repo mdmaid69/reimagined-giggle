5b6c9d9acf08dbcfb859fde5d358601207686540
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:57 +0000

    movement let count side round

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def convert_to_hex(n):
+        return hex(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
