b1aea4505ea21a419c7d3187555abed849e96c2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:09 +0000

    march aim soft original yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
