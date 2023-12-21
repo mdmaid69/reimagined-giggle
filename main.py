69e7af6f0f66311ca57f9883c1f26c41b7bcff9e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:59 +0000

    lucky round equal call card

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
