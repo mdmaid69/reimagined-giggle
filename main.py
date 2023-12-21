ec1b16cbf868542a9602e483f0da38a60d8d7936
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:01:43 +0000

    bet floor arrival think sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
