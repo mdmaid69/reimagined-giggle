d1e3cb38f5ed7efd6f784c03f7cda15b55b255ac
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:48:48 +0000

    effort kind fast band sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
