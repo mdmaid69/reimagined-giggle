793801f322eec074a16a7e7e1fe4899f79eaa76c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:08 +0000

    often break outside his recent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
