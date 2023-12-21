322fd9551455915676c36e9f87505dff79d3b7b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:52 +0000

    push support either university lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
