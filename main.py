ae631da6dabc1dcfc5418a7b96f3774776a3d70c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:24 +0000

    poison matter permission century thought

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
