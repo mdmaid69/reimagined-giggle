05bbf30856ff5d798d4e634e22557697bbd7b2be
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:24 +0000

    description hospital report gather fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_stack():
-        return collections.deque()
+def remove_directory(path):
+        os.rmdir(path)
