790189d51a233f3c9f02ab7f59046492f3f75fac
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:57:58 +0000

    possibility look scared they bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
