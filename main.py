bb4f8743dc19e216db11d9d52a0c9852e82cad2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:46 +0000

    indeed unpleasant born height takeaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
