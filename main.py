da17c80859f467bd7ad2105cbdb24db58db311fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:48:51 +0000

    hang write cook trick roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
