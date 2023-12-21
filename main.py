3945bf53de785de7a7d78795ced4da9b6eae9f5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:05:25 +0000

    awake earth group back paper

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+print(math.pi)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
