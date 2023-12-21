18ff6c50e6f22ebdeab94a6fabb05ad6ef013afa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:51 +0000

    receive control telephone only shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
