5bd69501f09125428ff978866ddeee75a5f53f6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:52 +0000

    mess wing sweet really declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
