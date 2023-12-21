6da2ccee0fc2a8ae12f179490b73746292fe1330
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:42:28 +0000

    physical loan contrast whom anywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+import platform
+def get_os_info():
+        return platform.uname()
