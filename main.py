5cb19b4b850595fe95394410d6721e748c9e9aae
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:36 +0000

    567enemy usually content fierce mine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
