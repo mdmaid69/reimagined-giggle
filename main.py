c1bd92b24e41a5a7a6a4c982120a064a105c7054
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:45 +0000

    suck ordinary father whoever sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
