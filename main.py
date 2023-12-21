b61200d3cac978e487aa3621f0f40da10a51a014
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:41 +0000

    567enemy like choose their mistake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
