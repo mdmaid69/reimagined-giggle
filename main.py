dccc8e79c2c863e4aba68148c4d1a1640fedf967
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:37 +0000

    accidental fight personal male design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
