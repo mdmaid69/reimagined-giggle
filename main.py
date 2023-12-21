b65178cdac87bdfea7bb7774fc11dc7c5a22d407
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:13:01 +0000

    change row over its raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
