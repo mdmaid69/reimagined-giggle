077cd91353ec1af3bd8b6f9ec96fda0120e68f49
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:49:06 +0000

    book hurry reliable Qq way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
