60c37dd8942fd3393c4f59ec8a8d1774fc99e149
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:48 +0000

    shut forever sort study ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
