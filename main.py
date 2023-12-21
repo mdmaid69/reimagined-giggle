58ffec946b4c372cf616f36b73cec29ea64eaa6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:30:47 +0000

    Jj house forward male live

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
