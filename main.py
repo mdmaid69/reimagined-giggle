48d48d5c042b3f6edb71a57b17c6aa09ca988d14
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:32 +0000

    necessary twist bright point 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
