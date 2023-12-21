02385722b0210a6995a8a2f67a261cd7119d69df
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:04:26 +0000

    sink cool cover pinch apart

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+for i in range(10): print(i)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
