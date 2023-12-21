351f1f9ebf9d20bcf406df22278a8d795439487e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:29:06 +0000

    close car your lift lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
