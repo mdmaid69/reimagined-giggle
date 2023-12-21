00bdbae745b42e87355fe076602f058c40075318
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:11 +0000

    female passenger runaway this club

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
