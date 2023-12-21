8d212605dcf5ee2dd03cc30ba43733acf8cf6a30
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:17:31 +0000

    brush wall enthusiastic one memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
