dce1104eda07b6f1f396f5b611814e254d3dbe37
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:32:16 +0000

    possibility clean scheme gentle care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
