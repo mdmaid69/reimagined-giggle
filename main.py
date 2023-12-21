085e6e764931fe76258c6dd757dde4215fb91a0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:54:05 +0000

    once side steam walk with

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def reverse_array(array):
+        array.reverse()
