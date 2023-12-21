3d05c8e42b32d22cd2a8580cb325452cebf1d874
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:17:46 +0000

    blame giveback bridge city undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
