94969d0b9dde8f5d53588a8632e8269ee534386e
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:18 +0000

    finger operate ring deal stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def convert_to_hex(n):
+        return hex(n)
+for i in range(5):
+        print(i)
