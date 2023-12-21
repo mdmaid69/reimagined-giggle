9759916d624ec03ea3e242c9c7ba943392dc6f60
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:58 +0000

    path already instruction disappointed fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
