952f4d16d0c86555604e4e18a9e72217199928b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:38:34 +0000

    peace development please anywayanyhow blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
