0e206792fb67dcbc07feb18d29ac0de476acc9a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:14 +0000

    wish expect phone here its

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-for i in range(10): print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
