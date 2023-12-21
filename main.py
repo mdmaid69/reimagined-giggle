8eabbf44584d8f6c5632073b9347a0159c57b973
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:20:38 +0000

    cheap dry involve lookback(on) certain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
