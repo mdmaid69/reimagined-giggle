d1b43d3f6bd057649e335c6fa5e1892936362dda
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:14:32 +0000

    feeling behaviour hand afterwards lose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
