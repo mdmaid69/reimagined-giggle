9065361c0286366fcdbf47842f71bdcde49ee0e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:59:32 +0000

    autumn interrupt better agreement world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
