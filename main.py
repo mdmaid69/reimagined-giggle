95b0d99bd19af5badbdf1ff949b279b717c2212b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:10 +0000

    belief active life attach world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
