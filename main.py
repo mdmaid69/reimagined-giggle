f7e064bf809f527f2ff4a1b8b86c4a2dd7e7f10a
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:49:57 +0000

    hole mother raise float original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
