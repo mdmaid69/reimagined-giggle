c2793d0e7fb12ca98bbbca17333db14f257bfe80
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:32 +0000

    intend spread block possibility treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
