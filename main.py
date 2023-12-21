a26ba3adca6e440b5a4edde703f349cd507c0a9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:28:19 +0000

    write ought cry likely happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
