49a091a2f1a3b752db5b3aabfc1a2c927aec6fbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:42:04 +0000

    subject afford heart favour plenty

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import time
+def get_current_time():
+        return time.ctime()
