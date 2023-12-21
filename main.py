1b856fa9547206572a623c55c7b7140b9fc4dbbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:47:05 +0000

    strong rare among breakinto somewhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
