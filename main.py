ddf1a1d22b98f0378ea3200f58ee216acdbef916
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:53 +0000

    cruelty trouble rise part bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
