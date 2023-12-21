de1090810c850fc30e6902339d6d40c75812512b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:21 +0000

    roll cough shock beneath pin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
