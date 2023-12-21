d8e10dc36d4ac249dca3557058d4d03bf819f53e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:32:48 +0000

    concentration average teach tap style

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
