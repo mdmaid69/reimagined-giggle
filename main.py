4eceda220c361454f573845a6b83be032bc11afd
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:37 +0000

    pretty clear confuse school date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
