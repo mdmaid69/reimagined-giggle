71a446ee3464961381d0b6235640d5b1d7f7d4c8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:56 +0000

    night exhibition little tragedy dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
