602cc718d6c957d08f3fc0b3ed279d6d85d961b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:17:49 +0000

    fairly state finally favour health

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
