e1f60671933b3fc7e4c7e5830b16436f1a0da62b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:15 +0000

    full tomorrow ignore major fire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_power(work, time):
-        return work / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
