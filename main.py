b5c657e51a895ef78a4495196ce5639c5b046f56
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:56:57 +0000

    precise mess house wrap ready

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
