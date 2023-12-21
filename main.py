dcfbd449c599f0a86991e678ff17bf05f3937cf4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:11:36 +0000

    narrow list race waste anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_int(array):
+        return int(array[0])
