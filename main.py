8c64ae665261f8220d45aad055d1b28b1cd3b12e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:17:42 +0000

    open never count agree card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
