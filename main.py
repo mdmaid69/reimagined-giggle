8bd4878afb5d15df375518507242a202f8aa5b93
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:35:07 +0000

    teach exactly Pp pot bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_max(lst):
+        return max(lst)
