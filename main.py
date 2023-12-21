05ef1e5914e49f3b1f65a33acac415e165e95e55
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:07:57 +0000

    factory turndown vote workout push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
