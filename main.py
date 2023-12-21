2b88611ac347ef082a2800fb6bee43f0196c4520
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:15:48 +0000

    second afternoon away include snow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
