9a3fc348124f0cdd5c9f10290ac5caa4a94464f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:53:20 +0000

    winter style general crowd slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
