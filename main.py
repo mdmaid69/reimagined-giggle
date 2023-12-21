a365d0a57afa3c420d995a422b1dc35ad17cf525
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:48:06 +0000

    furniture remove unhappy pause kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
