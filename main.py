863dc86405a8fd6c3e91151b65596145a1b7e7d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:16:41 +0000

    wild term music speed joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import array
-def reverse_array(array):
-        array.reverse()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def get_array_item(array, i):
+        return array[i]
