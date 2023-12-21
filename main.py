d00d42c3201d8d73c0b48af455da02a58126e0f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:57:00 +0000

    bath me other separate family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
