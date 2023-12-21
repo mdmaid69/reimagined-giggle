b69f127edc787b36010f3ec8e27d9d3c14acaa9a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:54 +0000

    chief desperate address list sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
