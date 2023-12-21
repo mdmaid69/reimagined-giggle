924dfa46d18dc2b25b7b30e5007555197cafa0f1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:11:13 +0000

    basically repeat knock light free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
