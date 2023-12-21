2a7870b234fe49c42ba4c058419bd1d8c1883315
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:01:30 +0000

    face arrange breathe organize and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
