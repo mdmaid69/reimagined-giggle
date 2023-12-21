f6bbee9148b20c2a8023ab81a5effd49a7540f73
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:02:02 +0000

    gentle top basically slightly worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
