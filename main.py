dff12a95a42c9f671386ff76d41e60aeb188ee82
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:58:54 +0000

    fit subject important interview word

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def reverse_list(lst):
+        return lst[::-1]
