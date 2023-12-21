90cece62371f439baad5bdf9a79117df24fd77d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:02 +0000

    stick mind one laugh some

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
