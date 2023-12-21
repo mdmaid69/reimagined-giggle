5673ba7de2cf431456d46ed07ff8239b416280a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:23:03 +0000

    bear press react address bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
