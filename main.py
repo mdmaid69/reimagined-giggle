c4fea2851d64b553a08e93105e86a27ec6174348
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:20 +0000

    add standard land variety university

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
