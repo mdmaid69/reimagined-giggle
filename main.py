3962aa24669a60a78b2e9cbe286e30b753c4c0fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:44:33 +0000

    wherever getback union hospital project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
+import getpass
+def get_username():
+        return getpass.getuser()
