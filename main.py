a3ecf28ee256025bdec0673de5fc285a0e382112
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:05 +0000

    rate better urgent final earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
