c06ee52bdbdae690539bcfb5f0726d5ff7a452af
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:45:58 +0000

    acceptable love Rr side book

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
