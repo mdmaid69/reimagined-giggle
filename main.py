80beedc5bc7d86fa90ccd2bd8389856e0df7b84e
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:08 +0000

    crash objection name crime occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
