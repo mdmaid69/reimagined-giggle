921fe3a0f427dd06d32168d37a3dcf21c72f7532
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:51 +0000

    operation every contact combination spend

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
