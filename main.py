77d6984063e046d0ecd32703bbebfa513a50f88c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:56:49 +0000

    degree defeat trust argument bunch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
