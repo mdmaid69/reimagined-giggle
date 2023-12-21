88f883bb2449dedeeb44d2d38416ebaf3212694d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:49:43 +0000

    load pick building bringback continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import time
-print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
