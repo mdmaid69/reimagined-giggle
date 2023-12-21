f5f0519fee47ce94aada61f765f4c7b0a0ae3c66
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:04:11 +0000

    delight switch down contrast nor

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+print([x**2 for x in range(10)])
