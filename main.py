2999213149adb516b0812e634e0b77c280f18c40
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:16:10 +0000

    term rough minimum giveup lonely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
