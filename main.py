074202c89e59a313634f30815ef86134df029fc4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:00:48 +0000

    pinch it surprise crisis flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
