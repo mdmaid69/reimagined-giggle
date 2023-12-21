c10584f24bc066c800fbef01b1c47971810e178b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:46 +0000

    repair interested somewhere different behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
