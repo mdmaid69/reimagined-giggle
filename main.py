153f3b6cf37c5537a91c62e0a06e6bcdcf91d22b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:59 +0000

    collection whistle pretend rain giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
