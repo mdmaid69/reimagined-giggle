715c6c7d30b5e13f97ea247dd70e1b76caf984ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:42 +0000

    heavy hit score point description

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
