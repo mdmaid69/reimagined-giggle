c8cc132daa9576d89b54c5e00ebf6c98d7b20d80
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:31 +0000

    everywhere noisy steep further stage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+from collections import Counter
+print(Counter("hello world"))
+def find_max(numbers):
+        return max(numbers)
