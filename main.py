b4f2b6a135f019fcc64f781e7894fdbae5ef5722
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:32 +0000

    flood form relative front comment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
