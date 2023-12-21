49f86206d80c98c3ec89ccfc42b8fa366da58cb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:04:40 +0000

    quality bother glass certain swim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
