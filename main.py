8f6c9e4c228454c5c40d606b5204f8cd5ccf111e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:18 +0000

    certainly shelter dick health possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
