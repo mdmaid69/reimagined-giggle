9f4649f80142897917d6fa0d5a2113dd93c0c7c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:10:05 +0000

    justice sit apparently naturally club

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
