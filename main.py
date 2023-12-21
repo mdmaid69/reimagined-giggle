17a45828dedb293e28d63ae321469704b5d978a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:57:27 +0000

    right advise face crime male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-name = "Python"
-print("Hello,", name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
