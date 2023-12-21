8d5dd3b313a862c80b7e7b78dcc5ec7f4977f87b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:10:53 +0000

    will calm bread comfortable hand

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
