c93926981cc97960bbd66c0e859ab497f609638b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:46:17 +0000

    stamp file party deal inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
