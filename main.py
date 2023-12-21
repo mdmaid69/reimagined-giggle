2203f65a5bdfb6c52b8672a2ebb6ea169373161e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:33 +0000

    indeed right fire score great

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
