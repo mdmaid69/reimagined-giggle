91cde278a90af17f51f8cec064fb98e64da991e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:12 +0000

    swing open scrap fine remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
