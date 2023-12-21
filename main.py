6e27a27de61b20a29a4404b2517a116abc4e1fe3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:10 +0000

    trip similar operate science appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
