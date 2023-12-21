57d71ba814879d635ff550b1c96594b545ab1323
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:08:55 +0000

    ill probably event comeover Uu

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def greet(name):
+        print(f"Hello, {name}!")
