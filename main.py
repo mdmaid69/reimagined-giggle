eefde371f3f94320007d5fe8cad57e70eab360aa
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:33:52 +0000

    unlikely into closed trade guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
