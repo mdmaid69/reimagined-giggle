0be30c903cc17c2dd318c1472ca1fa7be38c46a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:08:12 +0000

    become freeze continue 565end left

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import platform
-def get_python_version():
-        return platform.python_version()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
