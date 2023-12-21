84d3b0cb7dcee3e31c8c256749352877fe168227
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:17:31 +0000

    church reason noisy mean wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
