3aca08e6669978d6b88e698b7eee1593aec0c73a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:09 +0000

    runover contrast guess release spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
