d75085e8afacab3764fa94c556a9e1669e8763d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:20:57 +0000

    shoot underneath urgent success young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
