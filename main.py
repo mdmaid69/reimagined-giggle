87d8fec09385148d14ea26d972628eaf6ae2b39a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:25 +0000

    total bad valuable rest ill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
