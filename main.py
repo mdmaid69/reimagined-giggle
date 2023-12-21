5ea5298d42726e6d8a7eecc32472721fd6729136
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:07:45 +0000

    brown male past getup standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
