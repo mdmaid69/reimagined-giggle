bce690fca9a26a821bc54d555067e95543ebd3fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:10:16 +0000

    shopping careful address call Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import platform
+def get_python_version():
+        return platform.python_version()
