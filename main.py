cd74ad94a93c3c0d7be476e20a6fd9d21b875c41
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:56:17 +0000

    change ride attempt dangerous squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
