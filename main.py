d91de972a724cf050b7c4d6f5c7bbc63579e192d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:49:14 +0000

    talk store exactly secret our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import time
+print(time.time())
