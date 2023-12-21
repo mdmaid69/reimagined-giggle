5598bb3b5a4e269d90fa955efb38ce898ddd70d8
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:37 +0000

    long tonight tour violent state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
