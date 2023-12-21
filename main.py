f3c9b4ccded869329175efaaa1ed737801aeea8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:07 +0000

    flavour crash anybody difference me

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
