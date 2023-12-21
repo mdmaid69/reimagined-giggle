ed8b472930d217fd2277e651bb10834a68cbbdd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:36 +0000

    week calmly family bush grow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
