f682a94a9aa0a9bd66dc4b3931fcfc3e3a986d0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:50:16 +0000

    disturbing must track round 565end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
