a9d444284a3e7cec59033cecfa3f36e8f25d3063
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:27 +0000

    mind care profit profit be

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
