903117033a5c180b9ab2dfde1770de91b7e23a95
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:24:49 +0000

    fear excuse goon afford themselves

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
