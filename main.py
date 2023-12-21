8d0a5a42fa5ff99c9634f66ac7de4ebe13a02b62
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:15 +0000

    morning free slice explain open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def exit_program():
+        sys.exit()
+def count_words(sentence):
+        return len(sentence.split())
