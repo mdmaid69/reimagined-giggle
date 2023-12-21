787b56fa2c91a09a7a3f39d165bc91cde572b3b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:09:23 +0000

    preparation average condition thankyou authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
