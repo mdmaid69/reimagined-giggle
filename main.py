ce821df310258f07eee319e30fe19734e75beda8
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:53 +0000

    bye thick agree used fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def split_path(path):
-        return os.path.split(path)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
