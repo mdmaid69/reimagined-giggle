3a615c7acfa20430cf57d81979ac4c70a1ad5bc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:08:17 +0000

    everywhere late hot write towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_volume(length, width, height):
-        return length * width * height
+for i in range(5):
+        print(i)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
