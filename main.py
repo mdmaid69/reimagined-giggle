0fa0a76575b109c2d04dc786ec5b78c1b6223d1a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:07 +0000

    gas side lose store thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
