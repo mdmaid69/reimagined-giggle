c79f160387b62cd60345854f278de59f9b1ed1ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:09:23 +0000

    previous side arm its wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
