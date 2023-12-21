150c3c7fad1530856c28bdb50b2ec3a248084979
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:57 +0000

    join squeeze up clean murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def remove_duplicates(lst):
+        return list(set(lst))
