9ee7523041c734901da57a2333d5f3590a49495c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:19:44 +0000

    attempt invitation music Ee balance

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
