40acca702e228d443684690b499371444b1daeaa
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:14 +0000

    kick various though understand brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
