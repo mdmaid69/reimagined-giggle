924034a53cc3b57cf151ca59891696f80cc6a8d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:01:59 +0000

    turnup allow force address sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
