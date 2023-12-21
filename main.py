10f866b2d94b147d5fabfad43220fb2590f54b3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:55:43 +0000

    communicate dead giveback stiff godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
