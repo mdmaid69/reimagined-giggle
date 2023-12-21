9cffeff1ee128471f1d5b372bfc5260647cbc170
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:38:04 +0000

    sister failure copy minimum excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
