2cf1f4c2d14000394b3b6856e38fdfd6fed065b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:27 +0000

    aim all goon Mm dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_max(numbers):
+        return max(numbers)
