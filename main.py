bb3efc5b9f3afd2a20b7206aab85e82dc392695a
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:31 +0000

    be much catch belong frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
