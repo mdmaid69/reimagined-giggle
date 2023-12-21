2caa692933144de27a29ff93d18e02e66229b5cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:50:00 +0000

    desperate neat hello building project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
