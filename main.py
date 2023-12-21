bba5c0f4f24d807bd8a01da8fd176a5470203ac2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:08 +0000

    callat tear unless ticket nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
+def divide_numbers(x, y):
+        return x / y
