d40cc5acf8963793b7a49b7b7c92cfb10bb64ee0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:29:31 +0000

    squeeze violence weekend total afraid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
