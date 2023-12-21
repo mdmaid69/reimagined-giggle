a398d9aa1709ef75679e5a47da19b0270ba481d1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:49 +0000

    note abroad by area science

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
