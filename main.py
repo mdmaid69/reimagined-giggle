272b4ee6a26a1394aef9336036acac4d05a9434a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:33:07 +0000

    smell wall convenient you direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
