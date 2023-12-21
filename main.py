56cb1778d480147fbe4926516fb4444870501ce7
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:57:46 +0000

    passenger violence affect noone poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
