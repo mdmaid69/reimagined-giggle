bb6788e317d66ec0c06eb00618ceb20386c5c117
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:24:05 +0000

    rush stare Jj fasten spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
