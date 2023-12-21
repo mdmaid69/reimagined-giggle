8cb836913a7a82cc41e99c1047a3a72ce8c796be
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:14:48 +0000

    plastic solve generally next shoot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
