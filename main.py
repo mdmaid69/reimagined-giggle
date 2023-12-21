d399dceacfa9fc3c4128388ea1d966ba9ec33210
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:34:12 +0000

    tonight damage fear to notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
+  def reverse_list(lst):
+        return lst[::-1]
