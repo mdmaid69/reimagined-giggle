7fb088b055bcb01f50472fe6caf8a7cdd070c618
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:15 +0000

    speed show accidentally expect slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
