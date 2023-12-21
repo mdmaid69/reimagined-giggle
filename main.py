8a60afebc4280946448f6fdb8caa47350b135f48
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:00:37 +0000

    prevent fit back convincing go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
