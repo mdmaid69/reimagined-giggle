d492c483c5f9a748293f37b2f3bf20a93ea8b73b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:46:37 +0000

    line learn possibility present deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
