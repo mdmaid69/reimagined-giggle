fbcbe1a76800b3c1f29cba2039de5f08af164964
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:28:40 +0000

    prefer field holdout 572enter escape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
