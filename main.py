72b83f849d89c16cd7884b1849f07ec57885b39f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:20 +0000

    night clever achievement health section

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print([x**2 for x in range(10)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
