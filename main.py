018eb66d3735ea1387403bee7bc70565674ed0c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:27 +0000

    begin violent convincing bet shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
