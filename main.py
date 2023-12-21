6d9831f0e87b9d96ef65e9dac9588cd61c6d0a07
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:59:48 +0000

    fix follow professional willing name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
