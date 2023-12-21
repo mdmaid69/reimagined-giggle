942f03e899ebe0abd4593ba2f7d4b08ec9894ef1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:05 +0000

    like workupto bringforward patient computer

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
