51c58c156f0fdc19db4ab5d7bd8f9ed7912716de
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:08 +0000

    light lead lookback(on) organization raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
