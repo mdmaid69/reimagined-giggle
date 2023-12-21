5dcc655203743341c361cdb67ba8a377d308e059
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:37:33 +0000

    back offer woman front classroom

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import getpass
-def get_username():
-        return getpass.getuser()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
