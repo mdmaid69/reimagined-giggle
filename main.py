89e1b51d316866873bb6ff12349fc98fbec28eec
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:40:57 +0000

    book scale borrow edge fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
