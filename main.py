0e23f0171fe8e0765dc893a51bda7b27ae41af91
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:23 +0000

    judge get all sad slide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
