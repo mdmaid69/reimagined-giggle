21a0eb9acea7772fb591f09137f7dad08d653652
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:36:42 +0000

    confusing port possible fix hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
