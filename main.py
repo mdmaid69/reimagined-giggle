82fd36a2812ff1ba9c5fa2c88ba37089b179ef65
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:39 +0000

    mention respect importance dead ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
