087088c2a19f0cfa0d1ec69fad64161adb7bbc14
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:58:49 +0000

    spend sink concerned instruction explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
