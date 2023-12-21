a370d740991c21e72c9e527a9320d253a4867943
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:09 +0000

    comefrom godown because join every

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
