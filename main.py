151af21789c10eae9e9b4b106102f963c95bbf5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:00 +0000

    band careful group chat live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
