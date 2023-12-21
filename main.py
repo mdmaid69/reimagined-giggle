a489313df148ea61a4f4a96e38d65058fea5b15a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:38:52 +0000

    snow arrangement nose opposite by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
