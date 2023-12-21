69cba886fe867d62bcb3bd95cb48536c5df10007
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:12:37 +0000

    hard bang talkback sense since

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-name = "Python"
-print("Hello,", name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
