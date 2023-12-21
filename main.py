18e81364a0e49d663e5f55318a1b7bf5894f3a2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:06:27 +0000

    join cum slightly mean polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
