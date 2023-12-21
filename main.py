5522b8af619402491daa4aad12a77b51927e00e2
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:58 +0000

    plenty physical wander scared marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
