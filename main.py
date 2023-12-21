e44141225b3f83c0e5563cf870b5c2660b9b1e6e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:40 +0000

    deliberately strong protect direct explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
