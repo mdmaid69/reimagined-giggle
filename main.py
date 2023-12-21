837126e3c0f47098db8ad0e83bb9d29f91b050e9
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:46:45 +0000

    action score square equipment still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+text = "Hello, world!"
+print("Words:", len(text.split()))
