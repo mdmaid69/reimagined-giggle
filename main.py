b91dca6b8442d02361b5a50e187fb3a186fbf77e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:13 +0000

    break error you break human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
