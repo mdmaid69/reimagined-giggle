ceaed9b49b8d015700f2235d9ef111179bd6eafb
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:36:53 +0000

    yesterday drive clear so plastic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
