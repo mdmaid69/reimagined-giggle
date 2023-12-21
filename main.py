c2e4ee4a6bc92aece7f43c2ed1d5a771388e32b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:13 +0000

    wet organize wash wish when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
