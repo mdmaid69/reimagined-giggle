09dae82c88fe0d8c9624ccc87c958946c5a3936f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:09 +0000

    immediate scare back 564encourage transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
