d480f529e5b494cd990fd8cfc0da1ba4c0810e51
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:03:13 +0000

    matter first dangerous fashion year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
