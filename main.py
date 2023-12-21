2b04dbf0c55d45a5f491166237f57e4b4a9473dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:39 +0000

    warning happy best conversation enthusiastic

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_union(list1, list2):
+        return set(list1) | set(list2)
