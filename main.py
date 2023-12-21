b173112cfee867d08acc73b5dffa7d05d7294f54
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:31 +0000

    do peace cheek community glad

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def clear_array(array):
-        array *= 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import random
+def roll_die():
+        return random.randint(1, 6)
