1bfbf7be64c243811aa800636f031dd1f84193b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:09 +0000

    trust steep land pupil scare

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def multiply_numbers(x, y):
-        return x * y
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
