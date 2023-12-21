a92a2206e5f95a77fbcd923b64b706a7a8e4a6c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:34:22 +0000

    offence obtain birthday refer attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
