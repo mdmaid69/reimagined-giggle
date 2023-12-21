3743396dbdf49afd7fb49cdec18e967dc85b7ada
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:10 +0000

    bang ready whether violent sell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
