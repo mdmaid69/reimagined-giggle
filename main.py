47301e437f249703becb86dd3efc7b54497f5757
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:28:55 +0000

    through middle normal teach drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
