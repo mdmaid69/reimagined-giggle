db36513570a67e6ab05756641105cc020715301f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:14:43 +0000

    environment food language taste inquire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
