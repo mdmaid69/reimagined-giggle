33a59ef3ad2da67aa61577f2b26edae96155e629
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:30:54 +0000

    occasional juice translate arrange content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
