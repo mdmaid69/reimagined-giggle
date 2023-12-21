7a3c40fe86fab736e0e848c51adc809d25c6bd04
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:53:19 +0000

    black step community official conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
