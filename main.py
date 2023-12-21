842dd8c10a374436a8d0d6f488b1456ab639207c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:12:06 +0000

    travel minor towards sting level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
