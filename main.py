95e0238d17c341aab2b07ba15a211e677bd6381d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:51:42 +0000

    sick sharp fork pause record

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
