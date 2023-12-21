ed52f214edafaf03e79ebf38bc71efc998cc0dbc
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:47:59 +0000

    busy tonight allow lookback(on) ignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-print(datetime.datetime.now())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
