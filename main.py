7b22ef077508506a9a52858fa7feb04aaa247811
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:53:47 +0000

    Ee welcome operation enthusiastic produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
