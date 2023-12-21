70db907aeda8f0ebef10cf17027a6dcd1a52be3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:17:34 +0000

    lunch team age survive lean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
