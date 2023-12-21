7c2e256813b93243710d892717c1985a3c041d85
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:45 +0000

    lucky freeze year unit wind

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
