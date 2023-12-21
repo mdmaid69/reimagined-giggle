aec4286f59a1c3a94ec6cf9f8332f40ae74ac789
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:39:14 +0000

    country develop because closed pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_density(mass, volume):
+        return mass / volume
