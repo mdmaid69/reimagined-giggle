66334941a866c5389e1760a46d7e76cc81156cda
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:14 +0000

    lie spin by body prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def sort_list(lst):
-        return sorted(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
