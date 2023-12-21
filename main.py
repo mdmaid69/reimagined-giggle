61d260bf631e5e2ea5ca75d7af15222b6a2b1031
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:46 +0000

    tooth soil tap use different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
