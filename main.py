56b6267d31c4d85861606eb221842e675559acac
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:59 +0000

    over whether urgent official normal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_string_from_array(array):
+        return array.tobytes()
