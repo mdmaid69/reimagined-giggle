a7dd1c5fdf19246c6dcbbd22eeefaff218d38bde
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:07:58 +0000

    immediate different probably degree common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
