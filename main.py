3a602991ecfd1e308009d97eaf9793642890bc86
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:23:34 +0000

    practise post doctor upwards lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_speed(distance, time):
-        return distance / time
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
