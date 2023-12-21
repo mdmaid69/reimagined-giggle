0be92293345fbbf707c232da8dd060efd055e815
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:37:15 +0000

    whistle fall whistle lesson downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
