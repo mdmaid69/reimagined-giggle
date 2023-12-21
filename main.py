b3b0289c87c51bd277f05dc3519fa89fd109ad7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:27:06 +0000

    search swim punishment finger fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_set(array):
+        return set(array)
