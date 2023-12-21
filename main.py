1680b610f28133928a979407e2795eb94e58ac22
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:32:29 +0000

    stiff perform model past responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
