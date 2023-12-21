a301ed6162e87bada91989befbc834256c46360a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:23:17 +0000

    sweet whistle giveaway dinner luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
