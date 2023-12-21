d9216f4547eeabcc278e8d80a5f40ceb2ce6a4be
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:40 +0000

    anywhere pile include offer achieve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
