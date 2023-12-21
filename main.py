bb4aa0e359f34d014019257ea019ef63aa826ac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:23:45 +0000

    accommodation artificial man Ii whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
