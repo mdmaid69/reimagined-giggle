54ad1447b3808d03b7cdfd3f49c73d3f01974a81
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:24:20 +0000

    answer both war kind use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
