2f383cc2a9c8cf85cf8867675aa541dde08fb4af
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:20 +0000

    natural long getout nothing education

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-print([x**2 for x in range(10)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+print(math.pi)
