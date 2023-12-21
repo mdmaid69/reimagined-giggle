9eb6d6a13cfadf91b51e75cf7659f12689bab66c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:51:40 +0000

    creature tidy find practise it

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
