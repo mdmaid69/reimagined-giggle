ddc14db26451c459289c4342d83cf8e8bf16c77e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:20 +0000

    classroom compare exactly combination reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def cube_number(x):
+        return x**3
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
