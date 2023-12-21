5670872f0b7017297dc1835aa6420abf8f939b8e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:12:35 +0000

    giveup return concern food public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def add_numbers(x, y):
-        return x + y
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
