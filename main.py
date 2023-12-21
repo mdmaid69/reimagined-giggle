c95858e851535d83573b68f5eeb0ac01ddb8fac0
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:24:21 +0000

    attract compare fit fill worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
