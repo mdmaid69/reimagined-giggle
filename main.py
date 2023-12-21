125329e1f49fb6fbafe536cd1eb0466ce8aa4e6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:14 +0000

    nigga actually bread upset mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
