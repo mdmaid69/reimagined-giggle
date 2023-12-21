6acf16f71797165eeeccd312ae92cc1f28ef4bb8
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:01 +0000

    move obvious warning work nor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
