7cfceb8aa8d8891f3453b358204d436a335ec914
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:38 +0000

    strike bring hardly pity consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
