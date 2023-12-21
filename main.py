7307346b2d62373101b46534b96bc17c67b1ae9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:30 +0000

    empty tour matter make plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
