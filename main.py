cf139a44f679e9fee36aa796eb1ed4886e1cd9dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:48 +0000

    break urgent apologizeapologise custom spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
