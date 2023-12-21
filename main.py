45309995b236849bb511a820af771dfc1ebc740f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:16:10 +0000

    tend tone turnaway flow emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
