6411cb7c047feae299208f14c636f44f753ad754
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:48:44 +0000

    check tie closed OKorokay loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
