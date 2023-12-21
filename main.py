71cc3f2bce24ed037b9604be9eed775265b825a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:10:26 +0000

    splash still swimming then season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
