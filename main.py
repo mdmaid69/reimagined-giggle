bd90eeea4c6428a97556278da97e28744035c0a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:32 +0000

    hand glass opinion occasion laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_item(array, i):
+        return array[i]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
