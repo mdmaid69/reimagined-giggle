3c8c921d866c3f41ad9850f8fc95446191e5992f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:29 +0000

    interest Vv fashion naturally hand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
