6440012207fadf57988b5e0a260e046e17e9f58a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:01:30 +0000

    seem gift trick also dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
