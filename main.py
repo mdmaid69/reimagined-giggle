11935a94de195499455d7a0abb832552637df13a
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:07 +0000

    patience accidentally tip experience sound

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
