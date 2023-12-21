a43a3caac93c45ab9489a020cca2d26de7568c4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:36:41 +0000

    suggest tall branch used activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
