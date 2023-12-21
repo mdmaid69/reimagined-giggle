98901b9781ad2caa52936c0c8439e515c85c70db
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:12:25 +0000

    generous stick mind Tt goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import sys
+  def get_python_version():
+        return sys.version
