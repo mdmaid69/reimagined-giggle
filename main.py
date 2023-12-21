031a871c9df0ae147d1431468f5357853b084ba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:56 +0000

    entry burn freeze action turn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def add_numbers(x, y):
+        return x + y
+def find_max(lst):
+        return max(lst)
