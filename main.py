152c14198230d07a54c62cb2454a3452905f62e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:15:34 +0000

    entry same never threaten love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
