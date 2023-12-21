db80c1dde9dc93788c25d59921eeedc66e5614ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:36 +0000

    support floor wise century sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_typecode(array):
+        return array.typecode
