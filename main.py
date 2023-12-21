9c0eb5a04f6ffc8e8272cc1e232c9ff47e459131
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:38:40 +0000

    travel fine talkback dress while

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
