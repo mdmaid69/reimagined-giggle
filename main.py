30421280582db8fa856917481b37851bf71a22bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:51:18 +0000

    cook project clue likely principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
