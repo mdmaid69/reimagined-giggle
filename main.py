c01aec483a1fd9355200e50aa176d37bf7a3df59
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:45 +0000

    contrast afternoon kill share culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
