ab783d21a6eafc65c45662a0e035fb25fab926cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:20:46 +0000

    happen bush lay shout rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_area(radius):
+        return 3.14 * radius * radius
