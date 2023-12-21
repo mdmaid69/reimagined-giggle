6fff71d0a8182c212b31b3afda28d804afdbab59
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:43:37 +0000

    him convincing ago tap big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
