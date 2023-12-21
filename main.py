b201a587e2992cfc06ed993583cfb59444ccb4c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:33 +0000

    organize perform fix positive whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_array_as_repr(array):
+        return repr(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
