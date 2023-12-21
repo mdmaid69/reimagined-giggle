f04521e522258ea5763b42c3f39ef40900853706
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:15 +0000

    deep over week surface delicate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
