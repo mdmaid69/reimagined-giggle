4d1763a87b6d246ba88f5e85e06231b85135e9f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:26 +0000

    out alive cut line translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
