3de4c213432de89965d9297dc0002ea92439e54e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:13:04 +0000

    add visit fat suck regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def reverse_list(lst):
+        return lst[::-1]
