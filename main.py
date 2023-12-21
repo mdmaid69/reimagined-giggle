e1ecef4f4f695ac94421b1dde86141a43026805d
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:43:00 +0000

    confused giveout explanation sticky arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
