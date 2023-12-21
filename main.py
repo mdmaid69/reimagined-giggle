adc2d43ab6f00491ff674b5a678ada576024eff6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:27 +0000

    air deep birthday tired among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
