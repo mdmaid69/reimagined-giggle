d4bab83b5a5bc11bed7b459c8345758d3ecb7a7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:35:59 +0000

    confusing they treat swimming mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
