ea581c1a2d6aa61f27857de4c317eeadfb4bcbe9
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:06:22 +0000

    lost lesson keepup demand who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
