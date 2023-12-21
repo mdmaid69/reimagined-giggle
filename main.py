233d15bfcfe6312e22aa6d9fc9f38731d52d485b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:51 +0000

    high advise exaggerate needle trust

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
