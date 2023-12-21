162590d99e11be7c900bf1aff559644c0322fc43
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:30:19 +0000

    corner luck for interfere proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+import getpass
+def get_username():
+        return getpass.getuser()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
