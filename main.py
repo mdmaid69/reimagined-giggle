08a80b3bde6c2bf524cf1e2bd37aef09bc39d582
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:12:21 +0000

    pick terrible properly add wide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
