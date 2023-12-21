99a776b43866e659214f2e915a9492a0c169c40b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:22 +0000

    it error apart warn wealth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
