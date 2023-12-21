2ef7cebee77285aaa77a839a3ea37a479071e034
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:27:21 +0000

    answer minimum feeling wide happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
