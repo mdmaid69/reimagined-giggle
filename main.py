1f7516ce29638f985eeb13400bab2a33b4cbfd3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:16:44 +0000

    community brain natural factory pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
