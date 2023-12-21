e1a8621657f72d35d198d8a62a7dc634005d3208
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:36:39 +0000

    thinkover into value key pipe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
