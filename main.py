6ef8e13577f8e774a494a7ffe8a5e2ddfff5b33d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:51 +0000

    coffee grand excellent none important

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
