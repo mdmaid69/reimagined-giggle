53cc54516799f93116fc324a8ef128b7193bd0cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:47 +0000

    lesson wrong sort offend accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
