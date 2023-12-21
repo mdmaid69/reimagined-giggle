4b73c392477647451d05178efe6994bc16806712
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:40:42 +0000

    light praise detail rely report

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
