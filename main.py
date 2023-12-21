0ebbb85393b76a8f53da3c7df4a9f1a444403440
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:45:51 +0000

    whole takeout maybe well doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+name = "Python"
+print("Hello,", name)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
