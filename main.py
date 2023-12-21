7a73187b9a0ba0150cb3e0373832d7834d66cef3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:13:25 +0000

    trouble priority must table order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
