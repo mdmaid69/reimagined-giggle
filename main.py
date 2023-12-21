a6c5b0234c5796f64d019885bdfc226c047dc47a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:28:30 +0000

    muscle build complicated equal hello

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
