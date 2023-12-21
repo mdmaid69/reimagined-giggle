9031b88fd62c506a2a9aeeeed426ef51b50e28b4
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:38 +0000

    confident whatever list late sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def square_number(x):
-        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
