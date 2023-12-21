f9568a05023803af90023a27f92677c67d7022af
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:38:00 +0000

    inform tap several near example

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
