dbfaa1bb4dbd3c1a80e35ad76b4b60f37472bf71
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:42 +0000

    charge threaten exchange last legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
