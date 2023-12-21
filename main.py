204e1dee3dfbf0f7fb5431a620e72eab7f5d40f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:02:06 +0000

    under rise should sweet makeinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
