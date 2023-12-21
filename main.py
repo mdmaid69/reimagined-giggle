73844846bea544c9cb4f818b056366d784db4bda
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:39 +0000

    fence split upset sentence fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
