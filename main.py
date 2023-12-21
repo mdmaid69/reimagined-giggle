6a0b088d32a8d6bf91e14bad7a9d5778a74c8964
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:03:26 +0000

    about perfect somehow wet protest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
