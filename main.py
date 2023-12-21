2e5e8b104b805593b1abe6800fc8b04d95deeacf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:44:12 +0000

    dangerous settle print joke agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
