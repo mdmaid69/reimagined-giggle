d7a981a2aed5cc0b4667c2a6306d1c1e35392e46
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:47:00 +0000

    warn tend comedown become murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
