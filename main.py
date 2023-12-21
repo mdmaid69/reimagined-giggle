b8e929e1ca8edd72b99fe2cfa5252e113893c8d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:37:12 +0000

    admire project come arrange situation

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
