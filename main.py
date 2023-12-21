b8cf8c157dd4f121cf3dc8934b0259e9ced70715
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:05:25 +0000

    speed snow show flow mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
