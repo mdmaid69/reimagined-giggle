63d38977f5f64723fef2b03edda7d2225f6d5057
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:54 +0000

    failure cry like sensible try

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def convert_to_binary(n):
-        return bin(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
