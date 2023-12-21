5f91501dca565524e6b9e65f64e6da68260d0244
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:48:27 +0000

    best trip square unpleasant exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
