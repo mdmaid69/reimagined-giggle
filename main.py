5e700003d696f79a50036ad21ac1db08b7e1d94e
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:10:35 +0000

    Tt grand last necessary full

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_max(lst):
+        return max(lst)
