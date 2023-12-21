272e7ce2b4298136e3ce79130b85975a7159e768
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:05 +0000

    burst ear relief afford store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def set_array_item(array, i, item):
+        array[i] = item
