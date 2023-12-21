2c106b3e501725c5a23831d8fb653be6ed1e5ace
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:05:56 +0000

    belief city tooth sister slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
