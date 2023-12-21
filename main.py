27bf45ab7cefebba19ec79d7fe94d4acd485bb12
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:21:09 +0000

    criticize illness out error unit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
