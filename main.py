de4aa2acb487b5303664dde1df3364beb2b7fc56
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:52:21 +0000

    programme regional cap bottom trap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+print(sum(range(10)))
