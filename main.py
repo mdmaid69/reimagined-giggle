9b54252820dda3c1fc686d920bade9846be15707
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:25:50 +0000

    may deep consist so right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
