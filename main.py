0b57b280ce18b223aee924a9ce58f1feea2ff3ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:44:47 +0000

    free freeze relative dangerous stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
