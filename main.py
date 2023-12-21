f607dbdb9357a8e4ed1e24c0d1eb4aac0d70ea25
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:27 +0000

    deep complaint surface valuable key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
