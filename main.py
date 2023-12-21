43cfdd6fb53db967375d4d54802026973ac7dad1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:46:33 +0000

    start mixture left skirt young

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
