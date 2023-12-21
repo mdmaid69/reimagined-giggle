e0fc00693047733f1b6ea6150a39456e30707488
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:11 +0000

    correction relax combine mend doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
