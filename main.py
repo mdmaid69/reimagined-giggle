8020c8dcb7b9eea604b6c5e6fc61d7baecc71e12
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:45 +0000

    keepaway before shrink Ss contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
