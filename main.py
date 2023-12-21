a6157dc261f0ed5c36560feb4c303b89fd6e8489
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:29:58 +0000

    back container bottom total remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_pressure(force, area):
-        return force / area
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
