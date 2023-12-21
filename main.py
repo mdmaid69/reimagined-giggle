6bbab209509343b73a8759a962af7a32808f4562
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:03:47 +0000

    floor face split man whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
