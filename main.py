101569bdad3de80e8b51747bd93fafdb20532021
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:38:38 +0000

    fail get debt instruction stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
