1ef5c5df5ec33d5d678e295d5753bc9b41b34530
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:18 +0000

    opposite hospital beginning grand failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
