982655d90d7263a80b1ba4d5b0b580a7d4211d63
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:36 +0000

    before some amount new turninto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-print("Hello, world!")
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
