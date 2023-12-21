404c987fbab085d64db66d021a02f904db39861f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:35 +0000

    less weigh today maybe tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
