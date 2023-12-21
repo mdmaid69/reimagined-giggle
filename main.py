1896c19255bdd76a7378146b157d28355c890746
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:50:36 +0000

    convince miss environment keen instruction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
