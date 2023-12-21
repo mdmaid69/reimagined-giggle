3bf40b66d9a1fe53c8870c2d59703b9289bd0926
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:54:19 +0000

    climb chat march entrance flood

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
