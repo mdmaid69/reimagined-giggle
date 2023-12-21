748f48f1d9abf92c9f8a83a9b989b75b6ed5fa9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:47 +0000

    attitude ignore section thinkof ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
