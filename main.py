9c48c30c972afa4b7cad30e88c38888c0d8cef47
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:51:05 +0000

    damage again possession blue train

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
