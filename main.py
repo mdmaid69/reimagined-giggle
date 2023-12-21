16e684a48703f0186c78e7214004092fa1398135
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:39 +0000

    where centimetre list read gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
