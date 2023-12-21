1cf2854d7d2672de2fb76620c49cd42ce3f2dff5
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:31:47 +0000

    joke wave operation relationship interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
