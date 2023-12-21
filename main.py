f0f135cb00377c2f22f1708fae1fecba46859469
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:00 +0000

    building tip powerful form bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
