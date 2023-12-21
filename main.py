7384610fbf939ba324d984ca94be00569d133b82
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:24 +0000

    badly skill draw practise feeling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+print(re.match("h.*o", "hello world"))
