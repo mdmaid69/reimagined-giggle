c28208b540bc0a8d422ea9e19e5528469f7de4e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:11 +0000

    question fence touch steer name

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(numbers):
-        return max(numbers)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+name = "Python"
+print("Hello,", name)
