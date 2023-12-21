5bb0dadf275af7f10990e0d95c217b641c20fb2f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:39 +0000

    bush air judge concerning copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
