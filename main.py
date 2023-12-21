7c558fafe083438f92f258d694bdfedae1d6142a
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:55:21 +0000

    moment tour reasonable everything that

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
