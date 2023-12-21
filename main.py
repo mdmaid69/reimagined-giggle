59684ac9fb0611df839c41b98f9d964a1a456268
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:31:00 +0000

    public town exit pencil Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
