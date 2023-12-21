373899447d97dd1bf76baead33bd8f4282cda74d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:05 +0000

    solve pretty interview programme worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
