4b9cd2aab3ede26ece815cc3b2a972ec35c45577
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:27 +0000

    stone free spring sort almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
