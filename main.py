c50b64965d1ccf21e1012acf95299200137edf11
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:20 +0000

    clearly defend floor turnover dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_item(array, i):
+        return array[i]
