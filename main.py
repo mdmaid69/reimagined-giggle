daa9f2c8e85b92124ed380fc8688d86710e04f12
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:27 +0000

    mind tread pay yours holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_max(lst):
-        return max(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
