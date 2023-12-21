8cb225eb7a207d41524c1fba717603890ca8e898
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:36:08 +0000

    practical work but fortunate tap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
