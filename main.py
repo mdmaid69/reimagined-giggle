9cdefc49133b0f6af31e45e0651a917980f5b546
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:39 +0000

    lunch while entry almost wall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
