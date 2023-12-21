01d27d5a1aea934ba4399298f3df1ad283eeab42
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:41 +0000

    matter deep continue pollution extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
