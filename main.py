be9fee5b50d037336f952d51707b16963223b9b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:15:58 +0000

    course relief imagine comeover mention

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def write_csv_file(filename, data):
+def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
