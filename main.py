7a0f8d014f533f6ee691e3d5e83fc210501b5894
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:27:19 +0000

    bush bury best experiment lose

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
