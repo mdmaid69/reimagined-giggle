cac078f4999d169b9a0addef3f7f3ee0143ef9f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:04:25 +0000

    fair thank means odd married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
