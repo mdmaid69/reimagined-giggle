b6168109d70a821834dd1f2ee7321016a28e7f64
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:35:37 +0000

    emphasis load float doubt contact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
