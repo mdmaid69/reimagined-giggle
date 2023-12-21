1650f3b5dd31add04a050c21b0e607593454da7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:30:50 +0000

    protest mood enthusiastic nurse clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
