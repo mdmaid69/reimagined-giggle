ebee64f83da1cf2427d8e93a4f842dd95ad37c81
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:09 +0000

    off sure protest available track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
