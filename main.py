f9754b7e2cab93774daa6e97236e3ef1b0531845
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:27 +0000

    scheme name listen really pile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
