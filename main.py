a18f361aebbe5a0feed4ce31f372431a35361103
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:33:56 +0000

    talkoutof break owe bringforward add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
