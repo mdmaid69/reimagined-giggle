0466e96a2bf9c4a46e1badcd359ae98e59d49581
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:15:01 +0000

    well callat walk Oo swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(lst):
-        return min(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
