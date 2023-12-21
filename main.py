86eab6a75abc594e68df8c8b530eec5e39f694de
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:39:43 +0000

    forget experience future butter total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
