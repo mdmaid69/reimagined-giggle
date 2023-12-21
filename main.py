357789e78d62f1030d16c54c4d6ad87d01899ac7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:14 +0000

    say persuade flight feel second

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
