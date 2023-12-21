2e1895801251f839af88ab8bbdde316436a45501
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:39 +0000

    translate team object under soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def reverse_list(lst):
+        return lst[::-1]
