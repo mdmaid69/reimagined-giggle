f7f0ac5161d79bace62b75a3dbe27daaeb8c9135
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:11:00 +0000

    exist generally getaway sale half

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
