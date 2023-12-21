6b4dea7ddf3f090825b8d112fdd9e277cb544e73
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:07:23 +0000

    mean mistake soil Rr up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
