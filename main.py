a8e28f1ef00437f0338956bf54bd78e62537bc4e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:54 +0000

    none yours happy project dangerous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
