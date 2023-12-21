b41cca415e6843e4a24f56ec422132f57cafd18a
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:16 +0000

    patient conclusion since error cope

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
