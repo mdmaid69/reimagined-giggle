344d3572af781ec9ca9b0931a78641a14b8456cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:14 +0000

    illness weather exam city compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
