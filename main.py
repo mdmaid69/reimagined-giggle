754d570360488bd8d07bf416054b87b095b75454
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:07:35 +0000

    earn poison talkoutof force object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def find_min(lst):
-        return min(lst)
