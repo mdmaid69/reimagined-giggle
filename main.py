b8502cc3697422f88fb07fa3d2397739eb200ec4
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:30 +0000

    than damage violence both dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
