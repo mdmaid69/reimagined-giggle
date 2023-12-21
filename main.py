9aaa049ae435bedbbcdc9a08e9bfb571e5b3bf0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:06:45 +0000

    child long lead over other

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
