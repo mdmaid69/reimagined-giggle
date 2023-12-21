729c7ed3d24d6fcae9c2d0e7a7d1afd3fe75622e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:34 +0000

    pupil wrong seat cry face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
