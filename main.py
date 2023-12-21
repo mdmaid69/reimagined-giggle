87b76ad8ea21e1fc6d2da1f167a6a366c1cce00b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:57:31 +0000

    left by total before train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
