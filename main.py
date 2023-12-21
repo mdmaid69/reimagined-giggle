2d4fea4425022e74764f6c8d387c3e3fdb1af62f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:58 +0000

    bag slide pen break snow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
