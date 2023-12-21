5a01316ea46d5948dfcf39922d0546524404a1c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:28:33 +0000

    getaway indeed report Ss other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
