cc889ec865d52836a2c2d6255ca3edf63db40ba7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:45:07 +0000

    attractive platform worried achievement home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
