154b2dd4d074e6de171944659323434d07927d1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:52:31 +0000

    collect so anxious workout that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
