57287c62628a81379aa26181758f89b53b2628ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:11 +0000

    shallow mean right afraid sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
