c05249b07bf9164e1915ffdf481c5734bd38980f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:05:55 +0000

    breathe start shop when street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_priority_queue():
-        return collections.deque()
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
