f871df2daf0cbdc2048a749ea635e9e6f9c5899f
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:25:00 +0000

    remember original purpose bringup private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
