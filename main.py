bfeca9381914633b3d0a6f674e315e0484786dfb
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:55:44 +0000

    fire ought even ocean window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
