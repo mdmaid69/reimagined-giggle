c3674084684bf716d1eb7c5fbc1d36adb9829bad
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:40 +0000

    sink obvious offend suffer want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
