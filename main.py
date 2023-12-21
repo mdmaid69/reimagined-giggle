04bbbed22c09094be13da2f6ce9a03165be0c35f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:21:08 +0000

    discovery his bridge shadow ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_hex(n):
+        return hex(n)
