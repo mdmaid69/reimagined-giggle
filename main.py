d899d197a898427fbcd613df2a9812531861ac17
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:05:33 +0000

    wire rule slight official comment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
