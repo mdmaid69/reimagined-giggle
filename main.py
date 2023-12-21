31557eeab3bd1b7ce5a0f6671f93cfe44cf2fe51
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:57 +0000

    squeeze unhappy over without address

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
