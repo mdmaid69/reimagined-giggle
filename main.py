041490fd4d70d577292ab7a5b0a4d6a3038d6b18
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:27 +0000

    pull into because healthy wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
