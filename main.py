60cd94e4aef51e15a783c4488e845cc4288a38eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:17 +0000

    claim production me need bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
