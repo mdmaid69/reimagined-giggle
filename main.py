5c4e1275acdbe259c501bf1361200924b446aea1
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:09:53 +0000

    conduct passenger today false smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
