0810b21f8f41bd55235857caa8d5c18d878777cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:47:41 +0000

    cover swing replace appeal scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import random
-print(random.randint(0, 100))
+import os
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
