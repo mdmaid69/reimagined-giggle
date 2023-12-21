f6dd1ee9ed737b26f553d1acac352c49994cc319
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:52:49 +0000

    dull everything pocket hardly match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
