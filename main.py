7e201bfb53b3b21fbca6cb04f770794e84496404
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:59 +0000

    want book question somebody scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_arc_sine(x):
+        return math.asin(x)
