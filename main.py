746844b0370cc86a3f5d74f941408ebf40a04a89
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:56:03 +0000

    miss quickly interrupt sweet himself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
