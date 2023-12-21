39ab9a137c7bb289ab67456b81f57800f350c805
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:51:33 +0000

    for week manage type wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
