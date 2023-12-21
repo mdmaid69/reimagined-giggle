80994d940e540041b8ea45df56f61f28d9a99cb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:23 +0000

    border catch remove black prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_power(work, time):
+        return work / time
