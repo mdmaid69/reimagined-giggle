323a53ed8ec3c7038648d199759931c81329b566
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:33 +0000

    badly lookup determined go world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def is_odd(n):
+        return n % 2 != 0
