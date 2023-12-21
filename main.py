4eb37aa7c6c514e67a84132f880ef2ff75883da4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:30:48 +0000

    Zz substance tell notice holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
