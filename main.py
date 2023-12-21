9664b28278bb3db767925cc5a917830a2bb5f01f
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:27 +0000

    lucky roll judge hello own

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
