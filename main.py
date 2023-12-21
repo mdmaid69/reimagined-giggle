11dc147b28487df4e006201f4362f19847c3a73f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:23:39 +0000

    point back air artificial dig

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_bool(array):
+        return bool(array)
