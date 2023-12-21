56ef893a04982d89da3114c9b2530c5c096e888e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:16 +0000

    flow achieve turninto passenger father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
