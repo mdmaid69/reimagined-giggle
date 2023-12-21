6002e2f6cd6b161d8ba73eeae1a2d6109ab6d863
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:25:59 +0000

    mood healthy foreign shine makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
