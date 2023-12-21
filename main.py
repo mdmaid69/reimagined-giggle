857c9799e1bd332bca2332951ae29d7f028407a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:03:24 +0000

    burn big best study bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
