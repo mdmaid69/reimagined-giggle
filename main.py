f25bc9b0628d4f441c292bde0f5767eea5db2d93
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:08:17 +0000

    whenever result visit race level

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
