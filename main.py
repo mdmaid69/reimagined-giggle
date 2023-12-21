fde3ff7b092062f8d42fe7c8a849a1721c3259dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:45 +0000

    swallow form ocean mine demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
