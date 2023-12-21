77c7323abf89462d20a1560128dd0ac9b1edbac6
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:31:26 +0000

    call breakfast 566end risk failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def get_current_working_directory():
+        return os.getcwd()
