79872ba7f4272ac877a40848068cefbd98825f2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:58:46 +0000

    left whistle further fun private

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
