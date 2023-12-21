5e14b2c949c6593f7ae8803a64cc809e21e8cfe9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:36 +0000

    wide none instruction geton fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
