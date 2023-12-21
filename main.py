ec07921c13b14d2afe5e02204be10ef278ae6b0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:33:13 +0000

    popular tradition interview also reply

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
