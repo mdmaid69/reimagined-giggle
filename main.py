55525d27ea250d3d373e66e18863d5659cd3443b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:17:18 +0000

    tidy chase straight bit blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
