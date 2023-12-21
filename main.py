64f4ec3aa6cb953ca89785e7c72048cfa8e82f39
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:16:50 +0000

    bit smart violent expression attract

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
