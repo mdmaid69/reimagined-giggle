295b21ddc50f8975fff75c5872d49036ab415dd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:35:38 +0000

    giveup lower lookaround ours contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
