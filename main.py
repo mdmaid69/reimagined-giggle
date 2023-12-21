d98e0cd95c70184704e0b0f22158275fde683ab5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:02:29 +0000

    figure flavour sting public roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
