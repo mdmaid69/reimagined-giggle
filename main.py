dde6332e61ec4a94d89421dabb51868a681b66c2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:27:11 +0000

    profit exchange total permission difficult

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
