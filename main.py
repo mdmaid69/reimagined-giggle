1298344ef3d7863de179cae0221e8b0a5fca84db
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:17:44 +0000

    beautiful keepup suck tool so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
