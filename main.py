9c14cfbbbd352ffc6ce0cc5c62342a198f392c36
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:19:04 +0000

    into public necessary afterwards offend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
