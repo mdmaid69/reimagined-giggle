80d42df0d0f75842669ec1789f36d335825d2f2a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:53 +0000

    tall ago but excited collection

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
+for i in range(5):
+        print(i)
