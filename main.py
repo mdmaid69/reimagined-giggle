47d4e3ab2d870c9cb3f1f3d826dd0f636fcf4f1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:05:19 +0000

    empty operation hope case scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
