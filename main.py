871a4f066587fbda27fce12b50bcf965ef1ff91d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:04:22 +0000

    hand serve the cover relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
