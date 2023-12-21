2f3e3623b5619952fa9fae163186f2efcbd85ca2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:22:53 +0000

    open emphasis Hh anxious warning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
