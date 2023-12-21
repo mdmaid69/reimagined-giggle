2e90ed72bfaaed778989f4c0792c622d108bb8bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:51:56 +0000

    excellent quit record back summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
