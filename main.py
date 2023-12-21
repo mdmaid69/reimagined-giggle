bc7fc77edb7907745964b7d7ac211f197aa58d72
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:56:34 +0000

    regret fold tone across smell

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_item(array, i):
+        return array[i]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
