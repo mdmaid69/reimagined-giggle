b4e476f5778a1d8d2d4608cbe52a2d27e18a7c8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:00:27 +0000

    look today ocean eat understand

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
