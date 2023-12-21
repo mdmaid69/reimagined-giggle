48dde38d6aa87c286fc64437cc072c82040137da
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:34 +0000

    whoever geton square extra explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
