f9c9e01f0839e04749f793f3b07c31cbe9c46ec8
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:14:28 +0000

    stamp honest world pump mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
