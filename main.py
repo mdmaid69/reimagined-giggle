f1eda79bb7de89535cdf27a9f81175836be010b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:11 +0000

    flow sure sentence rough force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
