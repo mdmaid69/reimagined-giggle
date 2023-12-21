db9ff5854034fc91ee515f92785e7e7ac6114737
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:19:05 +0000

    excuse wide exaggerate risk active

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
