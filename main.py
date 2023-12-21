7b56b2130d9f1d0dfc0c190eb09ee5a57e6cf6d2
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:21:36 +0000

    life he post fork temper

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
