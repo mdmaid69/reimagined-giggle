d76ebca47fdf954642422bb87e4f2b4e5fb400f5
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:04:38 +0000

    fine spray row sentence female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
