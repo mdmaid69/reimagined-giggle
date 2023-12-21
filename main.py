4884f83209ff9692208c7d535f3ff8841267b5b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:08 +0000

    clue shine there hang strict

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
