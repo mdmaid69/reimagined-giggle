34b54681c9ea1a3dc34abe110dbaae779607fb98
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:00 +0000

    rubbish impossible like store before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
