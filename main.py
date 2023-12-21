697145d6f995967f0f41607c6a2dbb2a37f8f490
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:01 +0000

    travel attention open holdon appearance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
