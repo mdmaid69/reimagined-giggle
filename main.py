d01016c29a42d8af1e224b0b6e993dff97a4ab96
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:31:10 +0000

    adjective notice rough test size

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
