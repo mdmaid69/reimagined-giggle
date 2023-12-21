80bd637d4937f4fb1ac1452a90fc60b3cf1f8543
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:38:40 +0000

    sudden number dare crime disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
