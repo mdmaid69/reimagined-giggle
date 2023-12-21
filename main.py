adb088826de174dd47579cc21fe4217a1870a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:25 +0000

    fault many swing criticize fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
