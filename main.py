d6baa11655a4d5a795e90172505a0b54b5ad93c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:48:21 +0000

    stiff how acceptable opposite squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_work(force, distance):
-        return force * distance
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
