fbb25d3e31831a3b0e1c63148b58823bb21f5ec7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:01:10 +0000

    reduction soon style bringforward repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
