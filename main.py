1c2209a9a3e3a855743401661d43a232684b46fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:55:31 +0000

    contain cough ready practise Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import sys
-  def get_python_version():
+import sys
+def print_python_version():
         return sys.version
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
