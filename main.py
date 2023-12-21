ea35afe57100177e57807e0a6bbe21cbc51302fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:40 +0000

    near meaning soak round closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
