780a826938940ef1cb050bf7cfa0aced7ffdc81b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:02 +0000

    cost injury common surprised thumb

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
