b022cd08e9988845f24cea14acd29c4b0c10c3ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:09 +0000

    begin letter quick underneath wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
