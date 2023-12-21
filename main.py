bdb417768d2e2e068795ef686560e5e2ebc1f1ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:11 +0000

    illness fire raw confused gap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
