6777d07fde3f2fedd9d1c3200213699eeb664f26
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:26:00 +0000

    point glad surprising kick favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
