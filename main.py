66c9fc6e7794dc85dbf4ba4b28903d4fe537d42f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:08:14 +0000

    delicate settle perhaps ball blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
