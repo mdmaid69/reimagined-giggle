b13a4dab6666cf318018d3f3ddf129ca18a26149
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:50 +0000

    fire stamp cut person into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
