589d15a505bc1a24bf87265f6c6970b71c3f311e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:18:40 +0000

    punishment name of certainly aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_work(force, distance):
-        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def reverse_list(lst):
+        return lst[::-1]
