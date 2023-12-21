49feb3a067e3c283ccd3ff850b16139df2a0325c
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:24:45 +0000

    on chat journey godown spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-for i in range(5):
-        print(i)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_index(array, item):
+        return array.index(item)
