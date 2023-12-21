459862e1d512190ab354ebc3abe882e4ebda2549
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:00:20 +0000

    walk aim hello mine Bb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_set(array):
+        return set(array)
