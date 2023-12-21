c6e649e2ecfc721de69e424fe08e3e47ed566f27
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:01:06 +0000

    agreement professional winter advanced noisy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def is_palindrome(s):
+        return s == s[::-1]
