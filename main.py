8372c5c8bb700b0107b61c9934b0c95aab5240e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:41:38 +0000

    music thorough yes judge he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
