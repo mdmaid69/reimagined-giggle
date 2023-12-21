7d0065646ce586cf858233ec33da6603d8653744
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:32 +0000

    demonstrate brush film march limit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def reverse_string(s):
-        return s[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
