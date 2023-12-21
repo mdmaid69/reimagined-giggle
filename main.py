c235099a07af1e6ecce3a8a5684d2f67fe5c6f49
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:04:27 +0000

    day save blame wipe shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
