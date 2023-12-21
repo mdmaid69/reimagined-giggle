edf516ec3fa7c1637b7f7e6051c151cb3e2524c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:00:54 +0000

    weekend room ill build television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
