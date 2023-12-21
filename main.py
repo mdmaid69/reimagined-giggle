bef70bd97d63b5ec9c65de864389da652f978059
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:22 +0000

    long close accommodation beautiful permission

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def add_numbers(x, y):
-        return x + y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
