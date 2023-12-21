839f1b91439660b40f89956db842b1f20cc9c35a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:48 +0000

    float prove house shake clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
