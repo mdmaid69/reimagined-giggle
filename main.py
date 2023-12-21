79d3793d441a43edef0790ed811dc4af53534634
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:04:33 +0000

    perfect low aim along control

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+print([x**2 for x in range(10)])
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
