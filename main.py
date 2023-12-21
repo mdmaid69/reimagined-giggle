13c00954ce342dcc2147adee491934c398382a66
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:36:25 +0000

    naturally night pinch slip bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
