0ad73b412c2e27b8311e6ba84cf71d8183d4e4e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:05 +0000

    comeoff thankyou but later pipe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def sort_numbers(numbers):
+        return sorted(numbers)
