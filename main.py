3157ce3ccb46e41624fa9da1c6258c8a7ccc9a58
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:39:12 +0000

    agree luck godown clothes ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
