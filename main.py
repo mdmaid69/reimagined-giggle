a872e07a181e295125b7702c6556b380d7dfbce3
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:31 +0000

    both waste wind bar poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
