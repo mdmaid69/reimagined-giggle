ff5b7e38b3041e7e85e094a17e4a40c9b7617915
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:02:29 +0000

    inform town none identify strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
