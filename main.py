450b9d7519a67d5436137aae3478016413b2de12
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:25 +0000

    environment interesting come disturb mistake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
