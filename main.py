18478ec73321781d10ded7aff3cf21c133b9c0a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:20 +0000

    ours familiar equal immediate admit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def convert_to_hex(n):
-        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
