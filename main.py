d93897aa996f5b075c082e576515db6403fe9768
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:50:21 +0000

    responsible sail when today during

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
