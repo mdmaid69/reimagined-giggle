a96673426602b2fe260f4b81abd8426a133a4f8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:40:58 +0000

    firm operate mad position float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
