e03484f95868ef77ff45b3dad620880878085a73
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:03:08 +0000

    greeting well curious marry lookaround

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
