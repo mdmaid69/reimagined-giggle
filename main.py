d40e571c4c3b1f7bfe167a00d8692ab3e1ad1816
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:01:46 +0000

    pause happen ocean choose mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
