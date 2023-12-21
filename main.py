29927d6c65eaf470b0629ca6611acb0f30ae42fc
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:33:53 +0000

    action stupid floor sensible absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
