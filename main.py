7de52baaa16c05512e9b3c7502a7da37ea1f4962
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:18:58 +0000

    any seem murder bother pair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
