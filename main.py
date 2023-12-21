50c27c67bfa6b3dc09c6cf013daca864a9c6807f
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:11:41 +0000

    failure effect extremely alive mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
