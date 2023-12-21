4aeecd8cf71e71cef58b1d2466d6a9e955cfae1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:06:07 +0000

    still home scale boil none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
