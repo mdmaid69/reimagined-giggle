0be949ede06bff695254fe651cbf1210f2480ce1
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:02:39 +0000

    plenty nor question regret wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
