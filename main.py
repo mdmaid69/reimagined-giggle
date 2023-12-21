ed88c686353d9130b984c76bc61d905ef067632e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:54 +0000

    roof natural bet Kk tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
