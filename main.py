73dd84c8307893fe518dca2bb16c976f1373ae7e
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:39:53 +0000

    unusual comein rescue furniture brain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
