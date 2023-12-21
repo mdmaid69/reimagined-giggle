ffe24d32ecc383262e8fce1753b2d7fa1eba4db8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:50:30 +0000

    neither persuade giveout open talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
