9d4a97fde0baca6b71c264db2e47030c4718a35c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:07 +0000

    part complete throughout fine runinto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-for i in range(10): print(i)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
