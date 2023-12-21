ed3314ef527c8e79650678ef346491df72e907bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:05 +0000

    punish bringforward scream new move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
