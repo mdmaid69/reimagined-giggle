d9503b2ab149166327d6ae90d54b250582e9439c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:16:15 +0000

    able stupid charge time plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_stack():
+        return collections.deque()
