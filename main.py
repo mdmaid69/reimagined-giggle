cad72e6c2272c737bd58413d36712edc252064a1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:22 +0000

    experiment snow Hh herself really

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
