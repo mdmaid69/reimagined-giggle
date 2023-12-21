9c0a1eef7cf844d2dac3f4b93180610013ae6cc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:47 +0000

    public lay telephone distance type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import random
-print(random.randint(0, 100))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def print_python_version():
+        print(sys.version)
