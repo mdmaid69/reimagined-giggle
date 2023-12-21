e25bad766852b55fcac61bd93f143e43ef5b30f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:35:28 +0000

    cruel pretend last university still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
