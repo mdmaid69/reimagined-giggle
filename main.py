e2d526ab169e8536c88db1082572337c89646775
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:33:33 +0000

    guide pool show pronounce fail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
