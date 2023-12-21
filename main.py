4babeb05859e7c023c40b8dc9740bc06ea142efc
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:36:22 +0000

    treat fear own breakdown scared

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
