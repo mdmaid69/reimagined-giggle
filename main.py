9773a4c8a67bc806d3c640bbaac951cb9881b63c
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:27:51 +0000

    plenty satisfactory pretend trip Vv

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
