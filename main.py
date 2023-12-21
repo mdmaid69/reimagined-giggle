5a5222530e8ac34a965d681ba7279514bffff232
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:23 +0000

    phone likely mind reliable shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
+def remove_duplicates(lst):
+        return list(set(lst))
