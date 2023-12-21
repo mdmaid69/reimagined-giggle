513e230634c1ece8df57e99f0b21919dcb8e8914
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:54:51 +0000

    dry throughout variety winter me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
