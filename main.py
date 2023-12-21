25567a7572291d40b342566bbda8529b08c1658e
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:10:40 +0000

    half bill dance pure arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
