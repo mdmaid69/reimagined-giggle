8e32f7b04e0ed24d69227330a5b55b5ef7c12f5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:27:16 +0000

    book swim minimum ability clearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
