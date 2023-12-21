dab06a47b0690db3ef158f9953e2a774d29e0824
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:06:19 +0000

    unusual morning destroy lookup pump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area_rectangle(l, w):
+        return l * w
