2cbc93f2ea7a384b0627ab555308cb33548fd6de
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:04 +0000

    trust swing callout disturbing whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
