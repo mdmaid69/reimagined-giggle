83485a0988d9158d1193e8bbb4523c1442047744
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:40 +0000

    bath trial support by type

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def greet(name):
+        print(f"Hello, {name}!")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
