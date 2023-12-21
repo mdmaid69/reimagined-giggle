ca7e26bcf58644a6a4f1ef37869fa687b7cccf1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:46:11 +0000

    her suppose back no software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
