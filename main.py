e4d97adcb0fac6b1d375ad271e5ddb837b03449a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:42:10 +0000

    along moral leave seriously nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
