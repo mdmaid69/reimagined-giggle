ce56c3109868c55f306240629bdcc4b8944f5087
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:31:33 +0000

    interest yes spare sense period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
+def count_words(sentence):
+        return len(sentence.split())
