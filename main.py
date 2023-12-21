c89c8faae8f64c85cec6a7dec91bf46d8486cf93
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:32:29 +0000

    although nature vanish race spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
