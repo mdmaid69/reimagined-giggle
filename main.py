e55a0baa90434dfaa507cadee1cc24acb196e6bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:27:37 +0000

    develop large around motion that

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
