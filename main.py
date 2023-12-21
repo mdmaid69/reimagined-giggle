11bc82c6ca098454a56b0a39e9c7a001ff20ce92
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:28:03 +0000

    than face lead radio long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
