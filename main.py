526155a1bab3987cef72c632bc50f9308d3ee703
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:00:33 +0000

    dare split trip exact middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
