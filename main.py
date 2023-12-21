eccfa58639a0df4ba35090b90c04c35db379f55b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:26:04 +0000

    most surprise rare pot guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
