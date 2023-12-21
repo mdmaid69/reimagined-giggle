2d04f36a1af382859ad1b4eef7f7b79a4cb00841
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:03:53 +0000

    depend complicated release talk set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
