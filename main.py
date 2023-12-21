9865085880b293b400fb61dedf988d9d03288215
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:16:53 +0000

    once half food release properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
