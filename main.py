d0fbedac1d6dda93c7cdc55de43bf893d88fe029
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:19:45 +0000

    unusual soak with instead exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
