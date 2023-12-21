0182ce21e6471b7f1a356b5a097393f6a7edc5f0
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:33 +0000

    thumb effort project last fresh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
