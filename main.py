9f91daa6e467ca0d4f8ce9d8cce55e6193c3e6f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:28 +0000

    with course describe cook popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
