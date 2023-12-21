8c806d6c7342dab0575a101fe064ff915c672e5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:25:22 +0000

    fear pretty since action pure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-print([x**2 for x in range(10)])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
