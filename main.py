71538a981454ea3d204d95a6ac8b42fdeae6bbdc
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:28:56 +0000

    572enter news space passage arrive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(lst):
-        return min(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def convert_to_hex(n):
+        return hex(n)
