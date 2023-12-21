020fd2ac08dde61942d8d6e6ee1c564b228292dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:25:01 +0000

    flat tonight outside fight claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
