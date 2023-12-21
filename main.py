0f31eeb8080b586218c246b10ce9ff300f186aae
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:40:15 +0000

    getaway flavour furniture another number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def convert_to_octal(n):
+        return oct(n)
