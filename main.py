5a8cdeb61b54c0f6474cf8234a95ea39253a0214
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:02:22 +0000

    Mm cope high knowledge waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
