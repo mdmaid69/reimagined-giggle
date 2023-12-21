465530911d625bdcdb6fd9e801c715b074c20b8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:55:12 +0000

    public rare slight here town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
