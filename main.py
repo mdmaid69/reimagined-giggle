41ad5c5d1f8278209590c25ab51c59d4738d6cb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:09 +0000

    nor crash run mad this

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-from collections import Counter
-print(Counter("hello world"))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_float(array):
+        return float(array[0])
