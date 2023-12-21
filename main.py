0af70026d46973666ead56e32d8fb4db51821293
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:08:43 +0000

    deal height workupto earth wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
