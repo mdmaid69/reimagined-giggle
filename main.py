e951f5043272f71eb0957d64f19a5dedbd50696e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:52 +0000

    genuine answer tend business lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
