fdf263eda41e3fb162fcd93d5bd528d8caded99a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:29 +0000

    capital scream keepup tight anywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
