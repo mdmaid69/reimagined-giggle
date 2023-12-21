7cf076f5efae2120f1d780db00eb0dfff1a1f610
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:21 +0000

    record area kind feeling weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
