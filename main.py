3b3763bbcfefb9e464bdb25969d3b5b86f469028
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:41:06 +0000

    floor practise sharp rid art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
