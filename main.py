520df649a5a486df9ef462b13948d66f9bfd7782
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:46 +0000

    doubt limit lower bright way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
