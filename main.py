130cd5182c5e7dd63bde3b197754d2dfb9d7db6a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:28 +0000

    bottom concerned price feature closely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
