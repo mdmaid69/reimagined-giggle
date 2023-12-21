d5f991406aa787ea6a0c19f1c1347d85922d65b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:43:03 +0000

    trip worth remember cost sound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def greet(name):
+        print(f"Hello, {name}!")
