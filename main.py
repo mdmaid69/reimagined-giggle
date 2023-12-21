3d4297f750148906818368de1127d80e576e3bd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:47:58 +0000

    free fine spin shop mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
