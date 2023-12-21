39489a6df849149e61a1ee75ad45b0dc3b4eddc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:22 +0000

    sweep advantage settle weigh reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
