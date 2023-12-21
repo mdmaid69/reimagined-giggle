acd2eaa571edae3de0088c7ed803b2d185ac3567
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:08:23 +0000

    music hard type team related

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
