33d53bb3535753fe0eb691592bc3104525a55721
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:21 +0000

    on 565end visit vote success

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
