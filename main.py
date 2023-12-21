5ad00fa2c5fce58c2df1ede18a6c139dbec44b06
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:11:00 +0000

    mad prefer statement interest turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
