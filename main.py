b4d26a571c3a3e522286e680bebb402c399cf7a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:02 +0000

    increase ignore convincing even trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
