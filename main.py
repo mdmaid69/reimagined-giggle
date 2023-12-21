a5aeff8b8d39e1bfb05c39268f9f858c0667c91a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:01 +0000

    sweep duty secret description hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
