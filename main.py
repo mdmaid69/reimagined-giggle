405569ad810d0a089d7a1681bdef7a363716c95f
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:38:18 +0000

    tend safe sometimes unusual minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
