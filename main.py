d07969f0fb3534a17547acd37e89cc58b9c93f05
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:11:28 +0000

    field again after uniform turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def greet(name):
-        print(f"Hello, {name}!")
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
