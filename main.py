d47280dc6f8da36aa667e8f7b7320b6f86b804c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:10:14 +0000

    right date half wealth impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import time
+print(time.time())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
