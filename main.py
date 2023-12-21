cbb9b4d54072cabf2323ff41366f162ee32dc9a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:49 +0000

    hang pour offer roof accident

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import platform
-def get_python_version():
-        return platform.python_version()
+import time
+print(time.time())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
