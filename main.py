37a2da68ac85a1ab8b2502a9b35f2c86aab03e08
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:07:12 +0000

    fold load boring laugh influence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
