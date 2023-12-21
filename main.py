98603afda145b07aba910487d6798987a507bd26
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:15:18 +0000

    letter strike age comfort less

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
