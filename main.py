0ce1138dcbb7fcab4dd1119a76bfb960c5bbbb13
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:11 +0000

    commit burn spring lock general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
