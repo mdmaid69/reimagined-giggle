8e5c644430c4f7c954af374d47aba6ccec392bed
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:00:42 +0000

    himself geton wash main bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
