77878506649a8f4c25cb402532d0fc2b340e9040
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:09 +0000

    scream rest record on attractive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
   def square_number(x):
         return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
