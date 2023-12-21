6f8712aef14f9f9f94450f92593cbe2425ca622d
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:10:38 +0000

    case middle habit boring please

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
