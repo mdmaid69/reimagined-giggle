19333361636dc450e8f8ab1a286dc304070c15cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:17:47 +0000

    design little suddenly combine wrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
   def get_base_name(path):
         return os.path.basename(path)
