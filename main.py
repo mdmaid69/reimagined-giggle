6b2d73fcb64e3e12115bf3162a80201f37e49b40
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:05:27 +0000

    quickly unlikely remark bringup shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
