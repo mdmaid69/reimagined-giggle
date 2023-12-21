fbcb0ebe7d6c3b1d1d4b44f01b41e49529642f3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:34:35 +0000

    challenge estimate pocket little wise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
