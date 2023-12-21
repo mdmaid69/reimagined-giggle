c9a96d80a42701b20d8c6b756c522b5dc663a6e5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:36:00 +0000

    weather object born down strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def roll_die():
+        return random.randint(1, 6)
