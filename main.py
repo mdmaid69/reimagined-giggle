eefa760a747abd21b006fb0e4f01271030387b98
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:06:09 +0000

    education out transport instruction surprising

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
