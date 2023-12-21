c0d7c6faad1d3d604f08984fdef9635f5a1aaa70
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:36 +0000

    rain cough drive row go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
