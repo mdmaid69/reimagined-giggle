249d45d97f72bfaf4c164293830bae06e66ca9c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:59:16 +0000

    describe throw take instead its

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
