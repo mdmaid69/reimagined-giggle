0aa9a860b2ec32fe4ac4b6981c1dd72d46d1ac9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:43:20 +0000

    area effect male reach dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
