8825bfe8f17723718745776d1c7e1ef1156cb1b5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:57:17 +0000

    sweep single dry their but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def reverse_string(s):
+        return s[::-1]
