2741a02fffd62ccd41941c9d6a7bb880dc20ae4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:07:10 +0000

    live number claim apply by

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  def square_number(x):
-        return x**2
+def sort_list(lst):
+        return sorted(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
