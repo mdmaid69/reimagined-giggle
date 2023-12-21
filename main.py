78f4476a8de682a859c7b41687d9778f6778f92f
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:02:21 +0000

    water feeling care zero minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_min(numbers):
+        return min(numbers)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
