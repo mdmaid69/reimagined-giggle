d0d3be5925d5408596e0db4d1689db628ab75240
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:17:45 +0000

    mouth comeoff least willing stamp

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
