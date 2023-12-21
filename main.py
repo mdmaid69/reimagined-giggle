203e29420cc62f9dd27660813c469afe73d5a7b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:23:03 +0000

    anyone holiday future between the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
