98f65f02ef766815b874160b9f897444fa180c5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:27:25 +0000

    benefit wall protest born disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
