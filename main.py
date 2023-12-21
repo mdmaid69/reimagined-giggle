fb16fab4ba49f4ea9f1817068ee5d9181be1d771
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:18:15 +0000

    answer priority promise equal break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
