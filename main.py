2ec09a3b304d6177bccff58477dbba95ea85c85e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:53:39 +0000

    rely just improve comfortable thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
