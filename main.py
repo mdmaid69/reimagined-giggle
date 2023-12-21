f413009e6c69d3ba16409232934e9c98a02ef9ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:44:12 +0000

    without never study wish female

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
