cb3483cbcbba71cdea30def5f9bc857135cc92f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:03 +0000

    hungry comeup free wave patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
