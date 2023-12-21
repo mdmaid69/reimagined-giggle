2458c330e2c12b9076b615387576f66cda014929
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:28:45 +0000

    Mm chat height careful all

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
