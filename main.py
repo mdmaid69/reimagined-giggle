9c8d2e6e5eccd4a3ade16ab86e7d4814f7d8853b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:50:38 +0000

    accommodation keen poor basis occur

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
