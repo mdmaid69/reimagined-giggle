f4977fb3df4e6f01977bdc8f818797b4642d3b10
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:13 +0000

    shut fine actual recognize safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
