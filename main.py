4243b6401952fd9f94506e12e1b824444ae8302c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:55:48 +0000

    aim over whichever last disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
