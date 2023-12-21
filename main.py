09e6ea60bd84ed924be4eae83d9d4effbea0e58e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:24:14 +0000

    replace show basically talkback sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
