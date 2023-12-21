9d854e3f934fbdf39b1a2e121f2fbf1df28cdbc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:32:12 +0000

    workupto train opponent hurt whom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
