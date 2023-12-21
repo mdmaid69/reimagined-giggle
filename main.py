20311c00dcedd1692437c6125f5286eb709cfcb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:32 +0000

    consult black steep think anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
