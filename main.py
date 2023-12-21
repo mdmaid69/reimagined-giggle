e2c09c894270124e2dc3dcf9cfbb9ac93dcc3f7f
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:19:41 +0000

    soak could introduce autumn succeed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import time
+def get_time_since_epoch():
+        return time.time()
