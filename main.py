81ae90fc61af1e2ea956b16d4d979b39bc326285
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:29:39 +0000

    blue money immediate flat mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def set_array_item(array, i, item):
+        array[i] = item
