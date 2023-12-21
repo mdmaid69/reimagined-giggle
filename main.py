1fd909b54a5b8d71f33d97d036e75140889bac07
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:11 +0000

    concerning prevent noone bang calm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import getpass
-def get_username():
-        return getpass.getuser()
+for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
