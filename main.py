fd301455ebacf5b3aa86447f7bddbc1a20a06e78
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:10:05 +0000

    evening art instead professional Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
