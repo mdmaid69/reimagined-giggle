7a03f7ba754e645c7fec47cb12224821038987a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:14:44 +0000

    vision married birthday near scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
