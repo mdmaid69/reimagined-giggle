5f53b2d563d8fe5bce0ce51002d2bcf36bbc9056
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:01:47 +0000

    bear treat her kick conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
