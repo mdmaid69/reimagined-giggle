bd4ec1ff6e1c0856cce558f11fcc34f09781828b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:29:52 +0000

    fear before choice rush wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
