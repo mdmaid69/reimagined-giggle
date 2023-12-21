7540a98736322b9163818732bf2d31e5ef89a5a4
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:49:33 +0000

    noise steam ready hardly crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
