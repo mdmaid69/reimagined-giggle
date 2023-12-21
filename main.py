edce53219f785f8006a9fc017ac9e2407a3a7d0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:17 +0000

    delight regular that hate statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
