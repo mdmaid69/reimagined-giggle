4029e72c2b2cc4faa9dd0217df6ccb57962bb924
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:17 +0000

    rid obtain awake disappointed wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_palindrome(s):
+        return s == s[::-1]
+  def is_odd(n):
+        return n % 2 != 0
