c0be41a7a3dcc790f483a6fe51fe9c3c4bd34a76
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:07:36 +0000

    design throughout wash opponent do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
