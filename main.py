52695e24a9b5ac8ab1830adf84c092fa76f8d856
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:25:20 +0000

    case suppose method worry step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import os
-print(os.getcwd())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
