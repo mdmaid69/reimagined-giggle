631c5bbfe69a7c7fd7b32e042755477b1723076e
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:51:02 +0000

    strike fork pass wind talk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
