7de4db20d20402272a6844b24ae9aa5a101565a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:43:00 +0000

    habit contribute year butter left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
