73f1e40f0d81f9e4fb5a87f460d039b503154fe8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:51 +0000

    leg vision lean takeaway traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
