06e2c26ff44554148249ce268e11ec825a8a4fd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:07:37 +0000

    speak damp load copy rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
