16acd8f2f8c5b7db6aae64631171cbdea8b74f12
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:16:39 +0000

    favour share turnoff interruption grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
