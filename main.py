ca5e46a40175d697d3ed806c65b7669b4dba8bfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:20 +0000

    correct watch prefer plain stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
