0f307a0a4f91baf233d8cb9a675a5942ff6061a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:55:45 +0000

    set outside flow effort disgusting

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
