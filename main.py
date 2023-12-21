42ea5d88d18b3274532ec31eef63fe79f1a84c5d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:31:53 +0000

    quantity phone development minute immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def convert_to_hex(n):
+        return hex(n)
