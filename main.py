ceea1dbb9a0ac26a3a887ac4f66bcdcd4afa2449
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:55:15 +0000

    model various rush shout formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import sys
+def exit_program():
+        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
