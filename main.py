32d6d3d38ba1fea0cfc0bf938fd30cd0c5e65584
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:42:39 +0000

    break blame shape young justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
