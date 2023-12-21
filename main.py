817e9b54581828a6d636528c29fbc7df75ee2f1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:18:47 +0000

    can pretty recognize call bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
