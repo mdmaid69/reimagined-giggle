cd5f95d02ba4b58a497b6d19915775b2af51f62b
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:26 +0000

    interview release impression criticism uponon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
