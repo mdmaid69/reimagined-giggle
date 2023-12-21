5bac80cece7af561868214374faf7a142f00fe34
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:21 +0000

    pass just thankyou beyond fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
