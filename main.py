e6a445035cc611a07c3569df80d872a2e0f32532
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:38:05 +0000

    race display type extra union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-print(math.pi)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
