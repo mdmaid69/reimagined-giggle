ae73277de3825129b26b85fb5a8030ab1251a5db
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:26 +0000

    temper various Ff should relief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_power(work, time):
-        return work / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
