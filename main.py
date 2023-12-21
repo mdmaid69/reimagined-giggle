655cc9970e4e1d958efcfc99ef523e5fe10a9cb1
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:00:22 +0000

    hate which can birth complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_as_set(array):
-        return set(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
