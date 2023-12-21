b6a327cb25cc897c97041dade2ca5bdeca6b1bf5
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:03 +0000

    translate insist ball outside dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
