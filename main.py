b8bf1f72a225b83c08bf0b286478d205ae24d985
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:47:59 +0000

    talk belong extra arrangement emotion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
