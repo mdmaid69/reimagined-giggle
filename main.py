4224399c8edbabedf0ba313485c7ea134f90955f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:54:54 +0000

    top laugh without let genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
