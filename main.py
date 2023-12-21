8625f1548faa67e22ad597a94c893fd949d6e3f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:06:19 +0000

    hate maybe fast shut married

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
