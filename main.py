4bbbdf1fbd67c47bfe5f495857456b3d7667f6db
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:01:13 +0000

    machine firm themselves equal calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def count_characters(sentence):
+        return len(sentence)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
