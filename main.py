7f76f68ea60549c745ab961b7647824c01bb80cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:10:05 +0000

    greeting Mm blame separate point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
