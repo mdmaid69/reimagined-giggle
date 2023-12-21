baeda85b293230e8c8551f5417fbe110dcf8b1f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:39 +0000

    takeback soak surprise ask swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(5):
-        print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
