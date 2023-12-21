c2dcbbb5251b4d686507b00766c754613347cbd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:25 +0000

    tooth concerning low square lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
