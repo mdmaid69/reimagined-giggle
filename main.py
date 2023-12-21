610edf561b3af901aea8510d47b921c3884f8e33
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:50 +0000

    about heart physical mean provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
