6c625d5f1a2397636212cf50be55cbe5b7b8fe2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:31:58 +0000

    let art perform rate radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
