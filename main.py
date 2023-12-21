361e6b7e5608468d0c3536b8b8e304efb032d3a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:00:33 +0000

    plenty wide pleasure mistake once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
