001ac7a9895a026a58dd5abfadcfe1b7bd1f3a0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:30 +0000

    question minor separate flight now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
