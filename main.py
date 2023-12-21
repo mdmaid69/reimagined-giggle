8c22aa69b1c6ed749b00b0c1abc32ee45f75a535
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:47:57 +0000

    limit everywhere great cry thin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
