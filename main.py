fe3db121942a0705537cc970a90db6d9ecd4e496
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:18:00 +0000

    answer fix temperature breakthrough uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
