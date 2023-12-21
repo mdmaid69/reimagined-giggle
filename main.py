5b41f2aa28ab27ed86593f568df9ff6d59e511a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:22 +0000

    contrast basic responsibility improvement suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
