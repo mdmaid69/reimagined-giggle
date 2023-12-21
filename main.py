fdec136f7222b55bd6cabf65176841518f9d7553
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:04 +0000

    office cap thing telephone history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
