54d74f583a9766be784577e79f9b11c00bb85e8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:46:09 +0000

    regularly breakinto reply exploration turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_max(lst):
+        return max(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
