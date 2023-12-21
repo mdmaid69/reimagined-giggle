72eefc3e83363eb1450ae3d06c15fb55eaff48fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:46 +0000

    shadow acceptable benefit road makeinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_memoryview(array):
+        return memoryview(array)
