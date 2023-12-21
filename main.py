5b79d8fa1f06c558c8fee6fd080c2d5371391b53
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:23:16 +0000

    gradually drop tip except touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_int(array):
+        return int(array[0])
