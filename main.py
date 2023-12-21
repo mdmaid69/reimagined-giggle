58643db629342e9d122a4cf96f398166992f5add
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:26:22 +0000

    threaten dress centimetre ability please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
