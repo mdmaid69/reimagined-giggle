9278e406f1f347c6216eea1d6fec3420e0afe4e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:14:55 +0000

    complain agreement tear area every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
