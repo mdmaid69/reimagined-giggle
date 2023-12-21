f9e40f825f84c1b630958998517d40a6f7a0724a
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:23:10 +0000

    anywhere connect but breakout dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def cube_number(x):
+        return x**3
