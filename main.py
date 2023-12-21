9da471b0d0e25dba5ff7b2fb27d243cca0082790
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:22:58 +0000

    class refer common ignore road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
