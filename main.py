41a4b65ddf14861d6a010fa0f3bf16b6cc1b6025
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:10:08 +0000

    supply remember demonstrate although risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_string_from_array(array):
+        return array.tobytes()
