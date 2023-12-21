7ca490dfaf99e3a95deb63d7a513a51abc99bf7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:01:27 +0000

    put fact report square generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
