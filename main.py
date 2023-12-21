d7a8a5b9d69bc65514d777a366c7ded64d32efa0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:10:56 +0000

    shoot mysterious our identify scrap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
