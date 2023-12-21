707718c72ebe02a59359b1a56352982020e8789a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:53:16 +0000

    conduct weak base collect hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
