cc10a6b8a2d822a45ea17f266d93f09a0134aea1
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:54:33 +0000

    sweet confusing plenty feed point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
