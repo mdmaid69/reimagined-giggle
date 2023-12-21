72073ae4e21aef60543adad20856541ab59a15ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:29:35 +0000

    noone bored bear wheel direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
