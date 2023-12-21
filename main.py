0adb3f7cdd00b03653860f2ce82ce9ff5197e379
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:45:35 +0000

    lookaround cum instead rest legal

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print(sum(range(10)))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
