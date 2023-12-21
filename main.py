5af62e460245519eeb52cd5c52b79b2ae356006d
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:20:16 +0000

    clever lunch though control hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
