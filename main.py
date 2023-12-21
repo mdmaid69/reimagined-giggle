e55fd31d1afd46cdb467a0d582276d3a220a2027
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:45:48 +0000

    comeround sing if about left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
