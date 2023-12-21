4ed8adab2de98b8dcc2c9eb0c9138a9562ae4edf
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:49:40 +0000

    equal thank rain properly importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import re
+print(re.match("h.*o", "hello world"))
