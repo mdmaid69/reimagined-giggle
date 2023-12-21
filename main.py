a100145005af8e1236640f9d63fc83df2c0c9783
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:08:27 +0000

    Ll intelligent collection design fire

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
