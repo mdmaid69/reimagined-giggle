811137100b8ab3719ebcefb5cb976955f811deca
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:27:57 +0000

    mainly create back someone time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def is_even(n):
+        return n % 2 == 0
