03ac53a80baa288dfd220a2f4c687cf5fd599e58
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:17:28 +0000

    trousers earn Rr next all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
