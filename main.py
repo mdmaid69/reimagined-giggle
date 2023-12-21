d548e4cc650d6c7ff189d221dc766794301d1f63
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:27 +0000

    help frequent bottom drink nigga

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
