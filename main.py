43b7f5d0805ead55b3bba3b8b7b8e95990883a9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:48:56 +0000

    next disturbing treat stranger any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
