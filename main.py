ea85e6700a9c9154c931ccf329a8d68edf7baa6c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:43 +0000

    convince breakin make arrangement more

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Reversed:", text[::-1])
