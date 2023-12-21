553def0a7449ac2032bf0b88b491136446cc1b5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:14 +0000

    damp criticism consider bye scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
