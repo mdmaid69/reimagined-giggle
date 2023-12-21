0827c3bc37ef5c35682ccf13d15f8cd6e075e0c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:49:04 +0000

    shop attempt frame woman rush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
