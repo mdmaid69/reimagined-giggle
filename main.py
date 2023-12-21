9bd34fcafe61ab87ff9923542fc0b72582678a22
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:13:14 +0000

    bag scream lunch learn freeze

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
