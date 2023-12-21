0798cb762e4b32d37e6da96d4fab93494489ad2e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:24:42 +0000

    yes press unless vote contents

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
