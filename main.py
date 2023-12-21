f1a7a9b87d1c601528f55962852a6565538b59a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:09:10 +0000

    Ii say many boring love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
