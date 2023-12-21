bdc3b03f265de0086c054fb1cfc05f7577f478ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:46:28 +0000

    new reliable our manner either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_index(array, item):
+        return array.index(item)
