8b863b5af06d47454579aae7fb750a84fd3cf6f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:07 +0000

    reply suit breakthrough ago 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
