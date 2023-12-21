355d9d4b50e61c2a77f9c7248d2186feae943f5b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:04:42 +0000

    rain either attention statement freeze

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
