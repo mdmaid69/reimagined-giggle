a802dbd77b9ea9c813c9695096a4e1fe42dca368
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:17:23 +0000

    spread actually spring window funny

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+name = "Python"
+print("Hello,", name)
