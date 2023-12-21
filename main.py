d38b5093a5e3a0aeb903c2ad80998000d5b3192c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:41:27 +0000

    sink reaction comment hurry influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_base_name(path):
+        return os.path.basename(path)
