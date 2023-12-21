79d369c86053922ada7ad18e97e4f877d7b2ef51
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:40:49 +0000

    Kk place mine everyone lunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
