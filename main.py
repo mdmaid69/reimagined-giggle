57d9b9c904569fd47f3958ffb1dda8870527aa39
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:02:14 +0000

    search claim scale old argue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def clear_array(array):
-        array *= 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
