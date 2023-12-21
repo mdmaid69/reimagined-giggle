2414743ea7709c7e00ca184ad8584a8cf3e4310a
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:06:28 +0000

    real admit public information thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
