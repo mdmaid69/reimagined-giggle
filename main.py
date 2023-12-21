63ab78efad6330aee282eb4af5f0b5df89c88ad0
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:01:27 +0000

    clear surprised what create consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def square_number(x):
-        return x**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
