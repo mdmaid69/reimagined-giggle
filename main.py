433f6cf9339c18cf6832e0591009ae0f0edbacaf
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:22:32 +0000

    various reduce glass on bridge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
