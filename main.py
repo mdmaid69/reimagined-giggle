17f69ee2b508fd1318b508979f3c27d3db2d94fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:43:19 +0000

    regularly second form history hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
