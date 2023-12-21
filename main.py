1e2ab9073ab1025e2ee3368894315c0ce3791e55
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:06:34 +0000

    relax wish illness excuse breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
