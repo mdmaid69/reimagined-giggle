dd4db2fd0bbde4bf171574260561e66a34a8ba19
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:26:29 +0000

    fast club zero distance throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
