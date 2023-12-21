c5884d555e653677b7376509dbdc8ab0bc7e4eb4
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:00:24 +0000

    breakup shake too strange bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
