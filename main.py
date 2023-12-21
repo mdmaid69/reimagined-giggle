1397cc426598ac0187ad06ee96ea04e693d4f3c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:09:40 +0000

    charge skin breakup would squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
