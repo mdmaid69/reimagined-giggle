e58ea1e4e3435f82a9f62d085c171362bcb44acf
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:18 +0000

    born total mystery which last

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
