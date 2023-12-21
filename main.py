51d5a5baac8a8170944d2f2efffebde847e6fd26
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:52:11 +0000

    bringup father progress declaration answer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
