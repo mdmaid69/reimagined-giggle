e271c620d5b847f84928449415719a2f853c888e
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:07:24 +0000

    Ll steep dear still wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
