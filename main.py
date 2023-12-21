8fc979a0535941d525f89bc320f1a439c9741570
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:04 +0000

    narrow care waste prove Rr

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def is_odd(n):
+        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
