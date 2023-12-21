1db3931bdda53f984e4c8e7de7b4397fd366b76b
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:19:26 +0000

    control anyone behaviour storm fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
