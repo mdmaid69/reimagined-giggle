8b3ae62710ce0641c16cd1b06d6b5e2ca4c23bab
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:05:04 +0000

    balance price apply 571enough exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
