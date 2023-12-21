c60250d2b10f493f291c84abfbfd350d2a91dd5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:31:07 +0000

    confuse constantly man conversation reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import getpass
-def get_username():
-        return getpass.getuser()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import random
+print(random.randint(0, 100))
