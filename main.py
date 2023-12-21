613d2824150f3ce51c06219beb22b3279093e054
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:53:56 +0000

    ourselves about easy operate gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
