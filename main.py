faad51e1871a9c03d0478f43f26800b4fc724c6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:22:37 +0000

    only strike risk accommodation forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
