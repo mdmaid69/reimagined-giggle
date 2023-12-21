bf36ec664e80976cf65ee9bb2271e1db9a5d75c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:42:47 +0000

    type only consult tip bringdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_as_str(array):
-        return str(array)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_array_as_bool(array):
+        return bool(array)
