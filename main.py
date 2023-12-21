3006038562de01cb368fe83e2e28f0df65fa1e9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:05:06 +0000

    between drink hurt correction bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
