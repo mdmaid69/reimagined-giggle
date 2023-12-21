e11a05edec99e045214ec4d5c3b1faa379386a48
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:37:26 +0000

    mind possibly talk cook behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
