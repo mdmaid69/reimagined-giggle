5b7c18df4368f2c912212f65f6f52dfa7702e0d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:44:31 +0000

    range regret bend communication angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def reverse_list(lst):
+        return lst[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
