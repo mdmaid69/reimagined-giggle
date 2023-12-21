ee8c51cf3c5bb036f07cf26a75033ff14bdf6008
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:27:59 +0000

    challenge advice reach deep anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def remove_directory(path):
+        os.rmdir(path)
