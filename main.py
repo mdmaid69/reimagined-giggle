579d774841a9d856d71c933b86ec1d8d1c9dbd81
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:03 +0000

    length produce workon sort wake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def is_even(n):
-        return n % 2 == 0
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
