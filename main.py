60cd5986dce53836c53a619ad832ff18d8899ed0
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:20:42 +0000

    offer born hers convinced promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def square_number(x):
+        return x**2
