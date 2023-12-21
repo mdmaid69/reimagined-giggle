d08a526b3511366c90c9b3bae51e387b5e6a2efa
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:41:35 +0000

    police firm lead tip imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
