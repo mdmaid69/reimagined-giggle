41f6e6d992c6a022fc27b2f8050ff9b98ee10e81
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:20 +0000

    round tip justice everyone sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
