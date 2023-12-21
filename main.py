74d9d6c02df4ffcb823a33f299fbd3599a8467f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:10:02 +0000

    look notice exploration use base

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_speed(distance, time):
+        return distance / time
