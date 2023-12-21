dbe8ff91869eaa4fb68065d72a0969b6caad4223
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:35 +0000

    couple Yy square much open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_item(array, i):
+        return array[i]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
