f2c245621934ba4018b56d9b87317589071ceac7
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:02:44 +0000

    punish clear century move priority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
