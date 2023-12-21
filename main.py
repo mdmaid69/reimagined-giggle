eb60fe74411d08a7d0bb71e9d5659f8b4896a35f
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:27:51 +0000

    walk side forgive square alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def is_odd(n):
-        return n % 2 != 0
