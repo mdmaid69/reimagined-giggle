0d4933c4712b204d41d11cc2f22ef1f55df27a1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:59 +0000

    lookafter Vv turninto centimetre damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
