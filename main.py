24e83d6d852a425ec260dc9e337fb04e33dc1cdd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:35:58 +0000

    party meaning runaway gift reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
