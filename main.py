6972c4f933737b112d6a56663545105c15ca6b95
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:59 +0000

    by copy group turnaway moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
