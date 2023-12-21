1244eee51ef360cce934f1559baf442166fc74ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:43:00 +0000

    position face new team stare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
