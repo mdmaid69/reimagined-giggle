02111ba1d6e94e0a0d4f224ebb1b29dd9943e0fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:02:50 +0000

    ashamed may noisy related action

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
