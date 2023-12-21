5169d28108d2eb08f1847202bfee6eedd2ca8ab6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:12 +0000

    569enjoy band far trick later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
