21a78f38323daabe37087d2699f8a4a7f3d57eb5
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:07:29 +0000

    grow curious smile fall finally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
