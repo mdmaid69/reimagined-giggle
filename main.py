10edf43900277a9f2d8b8ba2058b0ed480d71ba0
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:38:06 +0000

    education eager ahead surprising great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
