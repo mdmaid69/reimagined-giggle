1b8a763a4f9ccd02b6a6ec4709ac47e08e9cd0a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:59:32 +0000

    harm share whenever live fast

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_unique_words(sentence):
-        return set(sentence.split())
+def convert_to_hex(n):
+        return hex(n)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
