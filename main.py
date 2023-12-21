01f19215fd677944a25df352637620245e4369f9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:45:18 +0000

    confident ours unlikely buy occur

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def is_odd(n):
-        return n % 2 != 0
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
