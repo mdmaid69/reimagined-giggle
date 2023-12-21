0873487a1a395febee20380725bc02f74ff3b331
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:14 +0000

    suckme approve sense correction trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_list():
+        return collections.UserList()
