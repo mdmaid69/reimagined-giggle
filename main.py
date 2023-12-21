3ae0d9a6aa6933c75b763211c6aea04ce0dba500
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:01:16 +0000

    mean far create between visit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
