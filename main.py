7c91a4c8ce129d7fac3f32e38cace9b9b9f7f890
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:06:59 +0000

    lookback(on) control honest day stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_circle(r):
+        return 3.14 * r**2
