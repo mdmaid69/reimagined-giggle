3588ed5a3af7e05ffc10d67359b789da9ee4c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:26:47 +0000

    birthday hurt expensive admit off

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
