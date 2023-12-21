6c2ac98a005d1ee23cdbb260665fe98c618da389
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:59:46 +0000

    along key greeting suppose correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
