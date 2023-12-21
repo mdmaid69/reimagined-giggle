da51a3274a9dd6b3bf1d05b939e749937267dab3
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:08:14 +0000

    service decide theirs have urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
