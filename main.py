b562f614bad6648f88a6293ede9b72725ded8619
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:54:53 +0000

    also there spring relax once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
