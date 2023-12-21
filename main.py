3229c6b28e099e95bab292b2e6413dfb4245e329
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:10 +0000

    however gate rule pretty profit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
