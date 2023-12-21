ce7f9a0c4f975a212d2179677dc1fee6fa17a063
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:10:56 +0000

    spot occasion method sore that

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
