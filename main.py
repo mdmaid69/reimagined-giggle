8471916f417a78ba1010336b4bfc2fb6c63fb911
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:19:50 +0000

    comefrom car scene remain mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_username():
+        return getpass.getuser()
