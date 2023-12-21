389a6bde7ce0c0ff8e988c3271acea318437f80c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:07:34 +0000

    hold square tread force below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
