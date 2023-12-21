15b6caac064c4e7ec70e588674bd29866a095732
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:48:44 +0000

    offence state convenient apologizeapologise moment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
