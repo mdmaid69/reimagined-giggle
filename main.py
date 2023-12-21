ed2fd9e587a0d74844ec333850a006836c3e5572
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:51 +0000

    final grow grand her grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def add_numbers(x, y):
+        return x + y
