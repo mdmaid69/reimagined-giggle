a1da22102e69a344a51edfe2fbb6fda0471abf8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:35:57 +0000

    indeed above word second refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
