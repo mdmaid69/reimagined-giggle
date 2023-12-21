e9e26a35ce3d9f03e544ef3278010a29bd274376
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:06:37 +0000

    behaviour wall before exactly dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
