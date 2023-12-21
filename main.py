c58f9a53bb3a86501c183a14c945feedcec889e7
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:41:18 +0000

    top various Vv mess challenge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
