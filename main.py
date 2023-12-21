1131d813e88023872f5c4baac8c4f684876644c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:53:18 +0000

    schedule national race us measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
