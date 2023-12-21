f4aafdb152e878d789b5a5abf959f230c0b196eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:51:55 +0000

    without Rr proper test limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def count_characters(sentence):
+        return len(sentence)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
