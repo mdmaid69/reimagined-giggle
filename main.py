1e3ec6dfd5878269026d2605a0abcd57f11525be
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:42 +0000

    present stick beyond chase upstairs

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
