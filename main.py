84251e8f9d3cd9539ad9f69fba6a66410e3090c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:07:38 +0000

    keepoff shout bin error low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
