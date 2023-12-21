241034c80afa8993976374ae642412f6e3d1284d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:58:09 +0000

    embarrassing unusual thinkthrough recommend comein

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
+def calculate_work(force, distance):
+        return force * distance
