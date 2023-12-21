60d2ce4d7bcd32f544f8cd22e96ab45637c102eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:34:06 +0000

    dark more be stranger key

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import platform
+def get_os_info():
+        return platform.uname()
