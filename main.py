93c06bc961f55e1d590a80dac250e1bc01d51bf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:43:34 +0000

    last callat born lunch interfere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def remove_directory(path):
+        os.rmdir(path)
