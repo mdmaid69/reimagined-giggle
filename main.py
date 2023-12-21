57a4841f0dc0468d8d37a4e979e3d6e14ecccf3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:45:29 +0000

    share direction cap allow read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
