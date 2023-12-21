6a38a0d7f84af1b1df25a776b827a1e67ac63152
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:42:05 +0000

    draw anyone comeback modern answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def subtract_numbers(x, y):
+        return x - y
