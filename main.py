82c950fc19afb0ce7497980de1af817f905a1110
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:33:30 +0000

    morning couple after smart rough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
