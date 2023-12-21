b3514c73204db597a065772e05b61551fdc77983
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:14:14 +0000

    need poor only style whichever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_itemsize(array):
+        return array.itemsize
