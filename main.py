f2a876d4584ca8ebc2511518ba8260acf7078fa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:57:09 +0000

    strike community difficulty action pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import sys
+def exit_program():
+        sys.exit()
