47c948d3099b61e15713277169e2b1054430a63b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:59 +0000

    surprising excuse suckme survive trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+print(os.getcwd())
