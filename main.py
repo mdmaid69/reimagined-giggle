caeba60ab8afb2364ee972aae2dd5a1d92c3a28a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:54:14 +0000

    flow record figure through turnoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
