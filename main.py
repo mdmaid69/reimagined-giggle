dba474aaeda4caea9723e17e95f678b16f4bf701
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:06 +0000

    wander twist shut care successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
