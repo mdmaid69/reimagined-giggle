950da312a257c31c2c6b4c4866c22a6a072e0686
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:17 +0000

    Gg present juice recover opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
