8049af4afde5b1989094bc48f8e5685d0bf76b20
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:29:46 +0000

    expect honest welcome take any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
