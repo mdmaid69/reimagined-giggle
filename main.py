f6537db69ad3ff0146c7f79a2b827517a63f0280
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:31:45 +0000

    well direct pretty value dance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
