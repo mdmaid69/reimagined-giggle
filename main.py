d5dc1d367ca403773e3f39f1d441ab959b545217
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:42:51 +0000

    steer count cause rule average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
