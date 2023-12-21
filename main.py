c28276a90668eec0b8fa5ca188cf260052d3d674
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:12:06 +0000

    listen speak quarter invitation everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
