0b6d9d54db132413736f14fb52600ffe667d2ea0
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:52:44 +0000

    street trip generous nice unlikely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
