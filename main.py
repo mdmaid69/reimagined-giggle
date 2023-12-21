aad173dd5322e1a6e458c019e5834868eb73b9a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:18:04 +0000

    bet indeed Oo usual plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
