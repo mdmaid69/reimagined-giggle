61baf254bf28302b756b11d3f3493f85e403a6cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:37:39 +0000

    instead clean above embarrassing shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
