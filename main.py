523d1b3f0f4a7b4f1b955009ade5f39f9af8ef6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:43:36 +0000

    term means delight hole splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
