ba4f2017f5177fd116c2935b91deb60b340ede0a
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:32:10 +0000

    succeed tune lend perfect clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
