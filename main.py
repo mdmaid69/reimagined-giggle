3569981a1473c87d864fe41b33cd33c795639abc
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:11:39 +0000

    except start someone port mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
