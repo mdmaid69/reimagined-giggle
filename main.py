94c949224c9c66eb337c2c35e76ca0e56b732596
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:31:23 +0000

    bank impatient major ahead tight

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def square_number(x):
-        return x**2
-  def is_even(n):
-        return n % 2 == 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
