eac18807022cd22c022a36f506c37b4b7e97cbf5
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:28:11 +0000

    relax marry big even transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def reverse_list(lst):
-        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
