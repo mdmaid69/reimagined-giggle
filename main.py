80ed5ba424df786fb64623adaa78c2fd29ad898c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:58:33 +0000

    well bottom sharp trap lock

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
