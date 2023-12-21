dccb4769ae7e9a2e34f7be5bdd3a381dd614f870
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:07:38 +0000

    claim 572enter square large coffee

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
