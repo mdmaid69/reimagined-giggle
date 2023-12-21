4399342345489d9c9839302807c14ea5f8c5b55f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:44:28 +0000

    call criticism consider various nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
