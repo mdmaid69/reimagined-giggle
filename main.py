5d945a50cf1a8a5db32419a972bd3ff93a4b56fe
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:27:45 +0000

    badly sore emergency comefrom sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
