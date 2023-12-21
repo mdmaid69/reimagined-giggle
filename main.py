2a7cfc00d0a2cb499296ebf4a1cd1689622e6832
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:22 +0000

    absolutely nonsense miss purpose declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
