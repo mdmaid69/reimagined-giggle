345128d7ace3fdbf2e69cc3039745102c26f608d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:28:02 +0000

    never stone Ii receive score

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
