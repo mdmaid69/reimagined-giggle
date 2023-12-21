e2b9700ee2550433d9f73f2e7d9b8853c2953ed9
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:46:18 +0000

    involve label blue thorough uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
