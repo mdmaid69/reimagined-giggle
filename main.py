7eeb4d504fdc2dd6ea0fe57c64aa173c9dd93b93
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:21:06 +0000

    active unite exam study climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
