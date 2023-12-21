14fd1a3e913e2560fa88df6c35c0d2ff4a6b667e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:18:29 +0000

    able system mine charge scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def add_numbers(x, y):
+        return x + y
