503068dee4077f444589895f697a0fa0f61fbe9c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:43:12 +0000

    clear lose can ball unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
