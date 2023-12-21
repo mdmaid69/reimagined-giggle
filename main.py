972192459627cfb51499988160d1900e95aaf4e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:40:13 +0000

    spill would taste mean from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
