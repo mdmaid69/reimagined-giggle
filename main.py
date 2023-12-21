a0218e5058b9f1ee32a8267beb0873596cd426c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:24:07 +0000

    pollution meal fine couple light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
