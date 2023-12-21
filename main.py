e3aaf6d2378c469b9ef834ed54a53d9673f1f16c
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:26:02 +0000

    lean thirsty clue read unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
