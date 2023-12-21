5b97c600d29b62107d3cc931edc7a90a1bb314ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:50:38 +0000

    greeting whom fit if influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
