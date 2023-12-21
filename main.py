898e350bc66257a068172a173e8987687396484e
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:55:53 +0000

    table estimate hurt computer talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def append_to_array(array, item):
+        array.append(item)
