1bb11d9dbeb180848d5d6e1987aefa31f57ff087
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:46 +0000

    lead brush project result operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
