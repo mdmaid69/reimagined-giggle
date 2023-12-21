00827b381c1506f45fabe2b61a94692f5a8436ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:14:09 +0000

    complaint fear left subject find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import sys
-def print_python_version():
-        return sys.version
+print(sys.version)
