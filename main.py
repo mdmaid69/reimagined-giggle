28b3a07123dad3024a476e2a02dacbc5e7830b29
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:30:08 +0000

    pronunciation interest convincing scream test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
