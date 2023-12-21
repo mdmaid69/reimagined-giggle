47ef5855e66ab3033a25145f5e292290675ceea8
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:21:02 +0000

    occasion too stranger rent turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
