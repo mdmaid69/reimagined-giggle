9dace894e9902958d771b7be10702e0c2ac1e16b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:14 +0000

    negative music friend laugh introduce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
