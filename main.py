87f1bc6eb3ab2f20184d8314b9cb3df5ce65e6d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:57:16 +0000

    instrument see spite nurse comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
