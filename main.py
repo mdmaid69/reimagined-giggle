86ca6268aad2206fdaa10be5051612bbc402de93
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:16:14 +0000

    deliberate black only spare study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
