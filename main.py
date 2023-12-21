368aa031bb249250f50d9ad9b932958874e92462
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:50:13 +0000

    forgive medium even burst wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
