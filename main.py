44e66464d93dda8c6d751198c60bef1b3539b4f6
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:03:20 +0000

    reduce splash well match bored

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
