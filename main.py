ffdda09cc0bbb04a9831a87986bdc68a33b29f35
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:06 +0000

    relax air score head coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
