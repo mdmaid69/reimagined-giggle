2d099a8eb3271ea276d063e9258767d1ac127276
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:21:16 +0000

    rare sting accident community joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_list(array):
+        return list(array)
