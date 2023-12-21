478559bb0e12d40712c866cfa812d4381b0a1512
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:08 +0000

    no before downstairs doctor hurt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import sys
-print(sys.version)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
