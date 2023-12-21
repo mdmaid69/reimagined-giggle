ee24a2dad2e3d07d5b4e75e193fa6c773823d484
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:12:37 +0000

    ground call tall makeinto reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
