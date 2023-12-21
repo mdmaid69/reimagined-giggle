86edd17a0c962f50c768c0e0b957c0aee906066b
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:20:28 +0000

    free contrast discuss opposite yesterday

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_item(array, i):
+        return array[i]
