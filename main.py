bec8edae09bb5cc41eb2c02275760bed5fdda463
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:49 +0000

    light contents breakthrough single letter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
