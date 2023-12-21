b9f75808ac7daf655f2cbfe3ce2d529f7d4266ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:15:26 +0000

    sight exercise direct look this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
