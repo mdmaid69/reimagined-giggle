24b856529b0912e8fffa369d35336903b07e4f14
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:47 +0000

    failure especially disappear bringforward space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
