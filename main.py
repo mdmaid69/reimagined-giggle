43e0f17594876dc1476819d25da76ccbd35e62d0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:35:21 +0000

    best sing angry keen quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
