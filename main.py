38443a1a403a9e8dcc721b5c1d3008214d20f241
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:35 +0000

    tap book stop know lower

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
