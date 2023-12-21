4751539aabd671cb006e91a568de81dbacd847c5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:39 +0000

    party hold design love himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_volume(length, width, height):
+        return length * width * height
