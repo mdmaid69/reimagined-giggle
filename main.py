521ae101f1b5e0572cd692ecd60ef50968236b07
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:11:45 +0000

    left argument appearance complain willing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
