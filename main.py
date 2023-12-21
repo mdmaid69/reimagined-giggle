f045496d2bc136084918f91b4a686e79981fc26f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:30:39 +0000

    post tomorrow pleasure cruelty yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
