141e55c5ddb02655ffc407bb660e079b75ee575b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:58 +0000

    square spread flight tune profit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
