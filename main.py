bc6af8f67796da79e3ca834695038c6738da7d1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:34:52 +0000

    choose detail stamp wind takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
