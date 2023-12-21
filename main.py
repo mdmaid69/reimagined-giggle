6581423b5540d55f17b2d6b3b43d0c406c46ac04
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:27:33 +0000

    goup claim warn mass contain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
