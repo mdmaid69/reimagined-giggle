9bb835db1206877949ef39481040688b6dc5809b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:40:52 +0000

    mad surprise disturb other song

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
