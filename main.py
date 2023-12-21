b13782aac418631221d5d5843e765391a78d3a82
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:42 +0000

    earth glass argument tip free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
