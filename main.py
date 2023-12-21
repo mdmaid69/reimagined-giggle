ca2ec67852c9e7a75c3726d841980ebeacc4c0a8
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:29 +0000

    deliver may steady cap straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
