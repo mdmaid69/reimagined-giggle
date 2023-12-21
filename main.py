c6fb6c70799110920b4c70097e868f9e1c1fa5a5
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:06 +0000

    warm complete relation unite union

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
