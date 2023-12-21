236caee1e56fdc187a2d3485b2103e51c62c4faa
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:32 +0000

    pleasure pleasure print sentence maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
