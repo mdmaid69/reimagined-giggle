621d743b63159555f844a9ebe9d0cdc3e1a6f5c9
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:31 +0000

    unpleasant to nervous mend high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
