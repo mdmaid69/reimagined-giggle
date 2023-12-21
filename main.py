807a39500f489dc0b24b835783f11c391d8f9b1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:15:48 +0000

    forward summer edge mood bound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_acceleration(speed, time):
+        return speed / time
