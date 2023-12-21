902c26836c61976e506a125d8e473b94075479b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:59:39 +0000

    open if deep story weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
