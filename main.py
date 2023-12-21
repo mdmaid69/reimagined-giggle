7f56fc879eef4be001349ba5bfc1c24da3fff6ff
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:19:16 +0000

    back man commit image run

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
