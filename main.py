bf8716ec0873c9a4b8c3140d9c7250e0da9cafcc
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:31 +0000

    partner front step obvious important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
