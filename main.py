2323d5e4ff14e6764a497b73565e89c9f8f1b5f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:32 +0000

    muscle breakin unusual Dd like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def convert_to_octal(n):
+        return oct(n)
