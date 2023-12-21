b995270431cc82c118365f7280987f821c4f5b2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:37:39 +0000

    fact lookaround on squeeze takeback

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
