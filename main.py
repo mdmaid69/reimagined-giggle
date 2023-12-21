c31044e268d050159182402e0017cf895b9e75fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:15:01 +0000

    anxious seem luck strike programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
