683eb3cde7f78b2f7c4551c2269a9cff5f3ead89
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:15:41 +0000

    cover popular comeback ignore artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
