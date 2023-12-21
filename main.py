5b4992e81264e65ff439f847fe943ba797631bf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:35 +0000

    finger seriously check compare fire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+print([x**2 for x in range(10)])
