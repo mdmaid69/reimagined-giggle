f4816435cc14b035fb3558f0394fb75ba8be33b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:15 +0000

    shop sound Pp scale substance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import sys
-def print_python_version():
-        print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
