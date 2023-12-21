d909ddcde48bc6bcd36d71524dad7aa4ca31319e
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:08:38 +0000

    quarter involve language environment owe

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
