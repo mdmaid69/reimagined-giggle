fdf728aa8bfa4e7aca1a188ee3a86e8c9b9fea81
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:12:27 +0000

    reduce in hand remain touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
