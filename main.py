f2613497b620d85f4c7efb00f1ee90aacfe9b82f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:10:10 +0000

    piece load complete television name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
 def sort_list(lst):
         return sorted(lst)
