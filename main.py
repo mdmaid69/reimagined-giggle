ac7048975ba51a95ed090bc5bc417ecc2736ab95
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:53 +0000

    compare hate matter exact busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
