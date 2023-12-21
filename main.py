ce64bd1a9ca4c4093fd1f556a33e5743bdff52d3
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:49:56 +0000

    give as count difficulty 179bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
