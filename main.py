1d941d60a5b8232c603eb39504859830248d4fd1
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:45:53 +0000

    pause powerful cap mine job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
