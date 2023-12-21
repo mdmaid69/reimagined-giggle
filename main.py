438bc301e2afd4de1576bb6b9dd79c82902c0cc1
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:51 +0000

    hold above modern joke official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
