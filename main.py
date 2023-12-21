768d62ff585929850a3ba308f8687a6215966954
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:34:01 +0000

    leg sit declare congratulations beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
