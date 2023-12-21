65ba586c68716c76b1e937f7707e2ddea6e1cd26
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:18:07 +0000

    professional which hollow thumb tell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
