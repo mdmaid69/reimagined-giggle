2b6fa497ad47b23563a91ebb8c6f4becbcf0862f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:21:38 +0000

    dare good term pronunciation pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_max(numbers):
-        return max(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
