c0c40cc150994ff5b7e01e9d29acd53743a02644
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:17:54 +0000

    claim complete under course exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
