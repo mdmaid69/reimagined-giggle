e310a152735740e9431b8f0b8175dd7e68baad3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:50:18 +0000

    star phone shine inform comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
