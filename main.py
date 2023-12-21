ad7828129b34ab404767a5fa7b8223c96f716823
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:10 +0000

    sound hole deserve from pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
