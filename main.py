fc0b1a7cb454ac534f5b47a5a5dbd2bdb6781e24
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:39:40 +0000

    unlucky into ordinary office doubt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-print("Hello, world!")
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
