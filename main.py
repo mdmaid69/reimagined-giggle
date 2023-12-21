b7959f0728cf7fc45c1d963c2c985af0e8611273
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:42:59 +0000

    must public club live dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
