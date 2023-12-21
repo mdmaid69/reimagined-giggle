38396917ef937cafe354f2f5f93ca1aa3f5ebd90
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:26 +0000

    memory several unkind principle chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
