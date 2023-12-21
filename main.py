a4d7ad9737d217cd8a13f8d073d1ceb136977dc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:23:53 +0000

    pride lend allow but closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
