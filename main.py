6bd1ff981b3fd95486047e92e8c4cbb11de2e8a6
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:21 +0000

    suppose clear flat letter whisper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
