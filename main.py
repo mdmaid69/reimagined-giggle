d9662606c0275e48866b0f0a13dd22c99d5fae85
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:51 +0000

    pretend mysterious birthday mother damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
