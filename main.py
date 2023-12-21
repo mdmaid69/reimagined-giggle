e34622cba2ffc2f5330115e72a339dd59a178e94
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:55 +0000

    whistle build star height standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
