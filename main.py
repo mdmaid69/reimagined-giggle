2302ab16ec8511c9bc2b5cede88d4e31044af19d
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:09:41 +0000

    country spill love detail blue

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print(sum(range(10)))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
