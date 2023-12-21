67f74fc245c63ec5546ba5f7ffea5152666431f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:13:48 +0000

    call maybe pause improve great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
