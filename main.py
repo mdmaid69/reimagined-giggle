2ec05e3d9dc1ee11fefea3a5ae1679aeb2029a98
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:00:36 +0000

    goodbye first physical cross event

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
