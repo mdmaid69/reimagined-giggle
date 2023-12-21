ee4e9349cd7e2a7527f5cbf5a06705634a7d250b
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:30:24 +0000

    will recognize cry brave cap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
