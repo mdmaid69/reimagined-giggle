5d52be27cf5656ad16d85bc37f41750daa6fb61a
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:24:08 +0000

    short immediately shout furniture thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
