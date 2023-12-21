38e77d0037be1d9f3e8f585efc228f892f5ecf58
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:54:17 +0000

    disgusting issue story test situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
