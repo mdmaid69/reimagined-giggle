eae1cb3cff04f4e8b769687dd204906a0345b397
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:11:02 +0000

    doubt demonstrate raw wheel please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
