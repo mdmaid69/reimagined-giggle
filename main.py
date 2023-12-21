1c564d53560acc91048544cdc176e740412ff4a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:38 +0000

    float treat priority seriously water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_area_rectangle(l, w):
+        return l * w
