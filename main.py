f6159abbe4f3c4a09e648faada887aa113c5606c
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:33:58 +0000

    art nose satisfactory possibility evidence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
