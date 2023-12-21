d5754511eea4cf5c5388ab1462edbebf0e5577b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:34:21 +0000

    brown fashion cover market turnaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+for i in range(5):
+        print(i)
+def find_max(lst):
+        return max(lst)
