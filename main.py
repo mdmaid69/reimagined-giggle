2fbf022926b048a942d1dc64e7dab062742d41ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:57 +0000

    someone narrow mean excuse total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
