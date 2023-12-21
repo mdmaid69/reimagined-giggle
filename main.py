90f6b09da3a4b4a08d5394743967412714f26ddb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:29 +0000

    Ii border turnover some charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
