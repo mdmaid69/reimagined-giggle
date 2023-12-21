2e57d8dd1f5428ae937024ecb06c142a2898cc8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:35:49 +0000

    careful mark spill tap night

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_current_working_directory():
+        return os.getcwd()
