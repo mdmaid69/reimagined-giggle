1d1a1b907352c9268bb33299f361c519a83ea98d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:24 +0000

    stop entry distant grand track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
