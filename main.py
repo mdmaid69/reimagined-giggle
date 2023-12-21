fe368b9e33efbf3b0faae858ada5e4b5b989cc2d
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:51:49 +0000

    quiet close mention distant challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def create_user_dict():
+        return collections.UserDict()
