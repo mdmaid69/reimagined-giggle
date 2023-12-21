c25bcd667fe0596cd291cdf49cf8f46ddd39606d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:13:00 +0000

    carry somebody stand glad disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
