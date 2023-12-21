65482b848b6c83a45e9105ecb932b9d4362a25ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:37:15 +0000

    sweet walk war case finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
