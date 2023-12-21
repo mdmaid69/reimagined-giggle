f5f8d9d3750be4fa54ed04d8b9a93784d114427f
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:48:18 +0000

    emphasis uncertain miss to stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
