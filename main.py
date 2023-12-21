0f5885162b199347b2e4197fe476e854b32d2b07
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:10 +0000

    afterwards play call seat along

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
