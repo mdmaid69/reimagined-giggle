05c8d18cb20427e2c260fad39c2c2ece72a1bed4
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:54:24 +0000

    interrupt murder reasonable tough closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
