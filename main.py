9b8d51c6f54ad773d225658c45b4c1e451f00549
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:58:38 +0000

    crowd communicate tired picture worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-print(os.getcwd())
+import datetime
+print(datetime.datetime.now())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
