31c1c30de12b17fb95e259e811c39dc085dda6e0
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:36 +0000

    them angry dangerous keep class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
