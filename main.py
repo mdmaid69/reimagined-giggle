89820d91ed3c7d2c5378a42fa9a67af149dc87bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:39:55 +0000

    proof petrol emphasize keepup miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
