23293c70001aec9297dfa580b3375bdd02cf65bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:13:31 +0000

    string pass spray ourselves limit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def divide_numbers(x, y):
-        return x / y
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
