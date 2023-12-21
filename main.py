e7aaece5f6a8db1a168f743d35680e4015daf223
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:33:54 +0000

    whoever whether cheek difficulty handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
