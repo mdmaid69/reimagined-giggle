320ae0ecdd47e9722c8d7034637698332f97f19e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:00:08 +0000

    smart floor odd trade by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
