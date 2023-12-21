173431d8f1c943ca182d5d45c936ee51465405e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:09:52 +0000

    firm we dry win turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def convert_to_binary(n):
+        return bin(n)
