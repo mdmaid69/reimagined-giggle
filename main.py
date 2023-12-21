4dc636394ca8037db1f5153d149fa9dc3e370053
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:31:45 +0000

    long rule experience smoke talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
