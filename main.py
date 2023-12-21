c9dddbf884217bfc8f3ca4087884911cb5d77f36
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:01:23 +0000

    our rush will formal runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
