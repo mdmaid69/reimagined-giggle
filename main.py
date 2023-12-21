6a7030752cbda265638b119c40d9826bf7b2c794
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:38:41 +0000

    learn before field normal people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def check_if_array_contains_item(array, item):
+        return item in array
+import numpy as np
+print(np.array([1, 2, 3]))
