eaf37f77a818cc8c6cb480c7d3a19421f18bb5b7
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:40:24 +0000

    across question genuine 570enjoyable Gg

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print("Hello, world!")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
