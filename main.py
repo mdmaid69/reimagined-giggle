4f7c45fa3065db7bc5521cb0e3960ea8509d898b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:45 +0000

    under ear jump separate recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
