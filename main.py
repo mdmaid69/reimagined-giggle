a7c19627c41c4e39c020bdaa4fd2c8cc1032ce51
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:05 +0000

    list attractive punish neck wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
