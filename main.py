247202d090f14875e1043e921d672ee845c2094a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:32:37 +0000

    squash leave Cc Ww treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+print(math.pi)
