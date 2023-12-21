61394caffbd3d965cfe0195f1120f54fa13ad6fa
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:06:46 +0000

    loud eager independent level disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_power(work, time):
+        return work / time
