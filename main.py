2fd126e8a8bfdb51552af661fa019b83a28bf208
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:51:04 +0000

    eager add direct list than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
