b92d745d8f65b4d042a694f7a49ef67692dc218b
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:08:00 +0000

    choice care damage choose be

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def sort_list(lst):
+        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
