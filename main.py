dc6bee8fd782c422f0224385b10e1196f40e13d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:01 +0000

    fork stranger giveout agreement impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
