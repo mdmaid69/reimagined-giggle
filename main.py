25e7668ea66a6847fea9ec79ce25f55999b3aa7a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:58:23 +0000

    spot actual choice relative study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
