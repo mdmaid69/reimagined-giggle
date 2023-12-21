ae2f4195aea573723d1782911f43c568e7c6fc8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:56:39 +0000

    fast present defend forward basic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def square_number(x):
+        return x**2
+def calculate_work(force, distance):
+        return force * distance
