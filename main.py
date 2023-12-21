419fec7d5bdc0d420f4cc1f106ce337bb6fe45d4
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:11 +0000

    attach bright comedown load avoid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
+for i in range(10): print(i)
