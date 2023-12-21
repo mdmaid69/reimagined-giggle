57304fd3fc9198eb0c89f076fccb1986853c1342
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:02:13 +0000

    shut wish mix slice fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
