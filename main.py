83e401991705147690f54b7bd54cf725ee10d84c
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:49 +0000

    fact stay stone impression big

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
