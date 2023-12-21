c4d6d158641a97be2fac80cb240f30678c3e97c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:04:43 +0000

    speed discovery shine chin turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
