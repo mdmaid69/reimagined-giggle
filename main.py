e8d5b8893c33883c13e011cfc5fb4a27ef4e9623
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:44:38 +0000

    embarrassing bill strike present narrow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
