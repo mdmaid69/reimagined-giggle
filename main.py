378ae432ba730036c03fc96bc52e41fe0818bcd5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:52:06 +0000

    slowly stiff stone blame immediately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
