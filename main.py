77e2b9204437db0ff68e2d8681537f5418329caf
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:57:49 +0000

    nearly general show machine powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
