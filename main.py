99e2eb14463d1586bc22013a38e7643ad431c390
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:45:39 +0000

    pronounce physical group stare suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
