ad02fefb6220aa574acdd56e326f9b81c622b6fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:18:16 +0000

    past hot until clever comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
