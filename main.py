a5d45f70e154c5894b1c9bfada30e048573b68ef
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:46:40 +0000

    tape belief reason equal Cc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import numpy as np
+print(np.array([1, 2, 3]))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
