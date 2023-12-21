e0f372c717f9c65a57fdba0d50ac6f52b8d6f9fd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:00 +0000

    pattern serve table makeinto concerning

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_volume(length, width, height):
-        return length * width * height
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
