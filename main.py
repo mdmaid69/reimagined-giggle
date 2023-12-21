d886827838f1e8755e14a0e0be8af4aab5230b9d
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:04:05 +0000

    secret independent obvious clearly correction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
