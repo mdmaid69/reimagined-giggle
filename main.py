94773f07a498ae0427a196bdc4206617d9f73d61
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:27:05 +0000

    previous fond add excuse 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+from collections import Counter
+print(Counter("hello world"))
+import os
+def change_working_directory(path):
+        os.chdir(path)
