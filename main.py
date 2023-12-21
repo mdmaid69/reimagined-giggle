8997527327c353a77516061648d5707fb63bc702
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:41:18 +0000

    interruption persuade under front nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
