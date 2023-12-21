897e0c2e0c1b2ea334f249ad2bd3e0c7a1f20cb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:11:00 +0000

    embarrassing murder thin moment far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
