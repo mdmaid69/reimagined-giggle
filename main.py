1ac276c852cc58e506239191271886a99650d94c
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:21:20 +0000

    model warning dark clear thumb

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
