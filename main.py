f25a025d1ada578d8b6a09a94b2e7588fa8e8d63
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:58:10 +0000

    ill but person season weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_power(work, time):
+        return work / time
