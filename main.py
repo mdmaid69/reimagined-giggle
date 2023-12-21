242f5c94eedbe74a21b2c37bf87a5a209c2f9f66
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:35 +0000

    whistle tragedy joke better clear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
