2d3b500732ca6ca499a7510e49af0600cf6a7ad2
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:01:47 +0000

    used music wish reliable considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
