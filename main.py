53b27ce3a7500e5417d1bdc3fe44ebe632319b16
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:16:35 +0000

    uncomfortable free cheat by by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_binary(n):
+        return bin(n)
