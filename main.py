63ff5d969f00226d867956e159a2526ab9739709
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:10:19 +0000

    along nothing fear state importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
