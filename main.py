ae2264b10420813378985073d3d59bd0e4fe889d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:23:06 +0000

    food say last unfortunate proud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
