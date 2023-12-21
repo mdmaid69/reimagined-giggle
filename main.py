7a592c03815bc4255dfc5ddd066c92f92a5808f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:59:44 +0000

    tall breath related generous clothes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
