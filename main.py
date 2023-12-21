54913da111c148f6a1ff450d0c9af71504f804b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:08:32 +0000

    too convenient cut reduction beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
