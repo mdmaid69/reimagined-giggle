492938c123659eadbb7883a29558eb94c3476c9b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:01:40 +0000

    bye underneath smile constantly need

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
