9ef0eec6cd8eee7f70617b70dabbbad3812d4ddd
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:38 +0000

    custom measure play affect frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import sys
+print(sys.version)
