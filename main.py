fc9b81142477364c11b09b96ec5de1b7a70ca93a
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:26 +0000

    oil loss knowledge under aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
