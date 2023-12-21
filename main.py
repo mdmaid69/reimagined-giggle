78a9577767f9b51eb690cd99d63eb6a9be99a129
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:25 +0000

    wet needle finally early Ww

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
