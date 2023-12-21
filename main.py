8fa50b97b6899a40bbc671889be38f92b14ee51a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:40:14 +0000

    practice commit still equipment unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
