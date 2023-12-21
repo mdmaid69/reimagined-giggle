fd170ac58c4763aad8dfb2959fe112a744f4031e
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:59:14 +0000

    slice turnoff voice large measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import random
 def flip_coin():
         return "Heads" if random.random() < 0.5 else "Tails"
