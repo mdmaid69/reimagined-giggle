5a5afbb508eef2546966a07d07b8b8f0f5d5bcb0
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:39:14 +0000

    ilovedicks calmly geton along apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
