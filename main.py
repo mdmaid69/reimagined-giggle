ffd2828f7217c66e13cd8e75b055ff19c2e7eb01
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:26:08 +0000

    ordinary content polite supply yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
