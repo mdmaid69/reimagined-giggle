2b8def372d4f1fb1c7683680ff36d0af57596fab
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:53:12 +0000

    extra spring pour care safe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
