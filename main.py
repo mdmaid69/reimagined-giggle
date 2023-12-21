2af30faa9bfd6ddaf6670d4d78d5dcc293ac4edb
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:54:02 +0000

    breakout stranger foot shallow yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
