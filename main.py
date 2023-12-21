90bbeac9bc7c0b9c3f3687fc29e108adcd74bd83
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:14:08 +0000

    emotion border suit free realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_min(lst):
+        return min(lst)
