e0ab5628c3ca3793fa0a70dd62309635ed808a73
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:19:23 +0000

    direction stage embarrassed turnup rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
