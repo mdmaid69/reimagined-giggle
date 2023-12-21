00e47ab78432c1e79a4b3ee399c6d25edd9735d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:43:41 +0000

    sky hot region season sorry

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import sys
+print(sys.version)
