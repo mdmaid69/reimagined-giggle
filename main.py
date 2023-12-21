97e893761ec647df08fa623d91d03220c9843692
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:31:42 +0000

    close have doubt nerve list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
