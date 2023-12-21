b6469fecf7dc5534c8953840517370e7eb01a81b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:19:19 +0000

    broken remark job aware hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
