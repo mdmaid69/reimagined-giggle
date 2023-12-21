e9278d9f8ada5d5f9e9a5e2b731cbcb9d6b479d9
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:40:32 +0000

    clear meal old work greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
