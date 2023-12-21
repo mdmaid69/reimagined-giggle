92e67aa8a2438b66a11f0c1b66312bb0b180c71c
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:32:05 +0000

    smile ours law deaf above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
