0083fc2a839ac1d131ab66a7c6b77df5291318ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:54:16 +0000

    if organization turnaway cut concentrate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   def reverse_list(lst):
         return lst[::-1]
