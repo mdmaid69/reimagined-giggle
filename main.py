ab6dee99f5a1deb18004d298fc7fe11d1415ff07
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:41:17 +0000

    debt whisper music sound general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
