0eed2a69f235c47ab2011c19f332a8b7e52d9807
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:20:25 +0000

    ground computer impressive conduct offer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
