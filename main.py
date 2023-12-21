c7649ef8bb0c997ebc7a31ddb25858fd61556a99
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:08:42 +0000

    bye drop risk unlucky hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
