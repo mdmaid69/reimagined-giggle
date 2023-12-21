822785dc401be72ef234f5bbcfe8dddc7f8a4bf2
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:16 +0000

    mess cheat record only promise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import sys
-print(sys.version)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
