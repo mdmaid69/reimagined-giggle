4dce982e190b3a44bfaa23613d99863d228a093e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:13:49 +0000

    challenge platform second estimate but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
