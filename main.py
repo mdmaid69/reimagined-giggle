9014af8766a4baf69b2bcbbeff64fa1b4859ed4b
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:29:20 +0000

    professional physical us commit 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
