852068ba663ae761f6691b6770f64a9afa34fa97
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:29:25 +0000

    difficulty confuse about needle fire

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
