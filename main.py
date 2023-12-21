ec19f0426e8ce9a9432ba423fbdaa5c89967f41a
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:53:09 +0000

    ground comeround rank nurse fetch

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
