fa29dab26c92d2f8d12f149040c1b6e608d355d7
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:17:16 +0000

    thinkof Ii strike perfect certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
