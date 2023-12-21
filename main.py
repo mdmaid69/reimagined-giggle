cd4b1c8f8e3ad12c6164c4bc22327bc44608604c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:49:41 +0000

    violent when minute previous few

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
