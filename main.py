58f617378c93ac0d375f9c88c6780d4ecb9492e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:52:06 +0000

    somewhere above live any otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+print(time.time())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
