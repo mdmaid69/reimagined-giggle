655c7a68c6c94cd3094029046c7d04962f5dedce
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:00 +0000

    opportunity than runover address let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def find_min(numbers):
-        return min(numbers)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
