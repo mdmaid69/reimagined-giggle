7b5b485a4b808a028e06dd5701dfbb6e3d9ad634
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:40:37 +0000

    complaint attract he seriously several

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def sort_list(lst):
+        return sorted(lst)
