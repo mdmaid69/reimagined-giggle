4d4a93ec355c4dc4c0232e923d6ede81d6007fdc
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:32:06 +0000

    declare opposite worst ocean produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
