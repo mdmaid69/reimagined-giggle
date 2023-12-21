f855e4f000bb6484977cf0e16cea129887bafd87
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:53:45 +0000

    sort each foot thought remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
