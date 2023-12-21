635346a7e3f894dba1a769af70166f86aeb642b8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:47 +0000

    safe neither treat force respect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_octal(n):
+        return oct(n)
