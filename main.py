ccc3d1e6f1f526699eb2f6c49377c1d7282ebff5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:45:35 +0000

    Dd flow uponon hurt dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
