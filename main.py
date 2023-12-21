7152395e257686ab1a48b25971ecd702bcd4bb60
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:10:14 +0000

    begin again surface around comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
