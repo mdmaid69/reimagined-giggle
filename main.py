1b078c8e452b2536b9fb8879225d6e93d069f004
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:17:41 +0000

    like rain sudden wrap breakup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+def count_characters(sentence):
+        return len(sentence)
+def add_numbers(a, b):
+        return a + b
