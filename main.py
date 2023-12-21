abad3c1a98719470ddffa214aaa7eebf0b3c0dc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:22:14 +0000

    stick pinch bear discussion runinto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
