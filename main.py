6dd394659ab21d4634d8761e203a2dcc7c2862be
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:36:17 +0000

    why miss formal tradition attempt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def square_number(x):
+        return x**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
