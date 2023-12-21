6617cd5b3dc316ae91206acc2487e8a13499b637
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:43:59 +0000

    eye probably exploration freeze key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def count_elements(lst):
+        return len(lst)
