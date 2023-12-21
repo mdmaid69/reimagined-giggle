5ed676fb4b2826354fdfc45941f296ebb41179c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:28:58 +0000

    favourite have criticism car first

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def is_odd(n):
-        return n % 2 != 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
