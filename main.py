992d0d321d8c365154ae250070792267eb66c96f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:34:19 +0000

    up sit soon odd sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
