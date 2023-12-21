b54f2ed8fb8fc946b389364cb2f4e86c87f3ce18
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:44 +0000

    dance over crazy satisfied charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
