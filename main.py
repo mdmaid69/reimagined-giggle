18e042cc1c656addd342c4b4b4e94e386171ea23
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:57 +0000

    partner beautiful rain range possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
