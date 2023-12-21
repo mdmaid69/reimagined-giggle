3d3bba25d2d390e6bbe972432cdd8c47366465b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:57:26 +0000

    copy once order favour bringforward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
