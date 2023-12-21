3d54cc7206e0468d672e391c2f0f52b520626989
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:34:05 +0000

    hide distant together feel run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
