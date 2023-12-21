4c8a5a88515b241ee29a73232029199fc3356592
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:45 +0000

    passenger of blue become live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_speed(distance, time):
+        return distance / time
