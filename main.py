2ca76c3b8e75e284c57c2d4ab84024c8d5f0bb8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:26:11 +0000

    will complain character crime regularly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
