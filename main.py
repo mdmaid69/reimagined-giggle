e4728aa64ef981c36c2d2e6f8defa781ede4084d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:04:34 +0000

    certainly whether chin rubbish fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_pressure(force, area):
-        return force / area
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
