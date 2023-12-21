753170c0d96271e47d238029891db24db255c381
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:46 +0000

    minimum cover design target pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
