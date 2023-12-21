d17bd3480d4ed66b23c407bff4a44cc7a234ecc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:56:52 +0000

    quick giveup fail another no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
