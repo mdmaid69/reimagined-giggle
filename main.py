5277a60bfd488450c1f1f9dc931c457d401a667b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:14:28 +0000

    feed guide style roof funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_queue():
+        return collections.deque()
