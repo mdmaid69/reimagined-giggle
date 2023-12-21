cbaad52fc13bb2e158a0e622e3f9d4c2aa6b0cfa
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:40:50 +0000

    plate agree flood system trip

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+print(math.pi)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
