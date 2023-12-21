71d89ac5b54a4b42dee50e41bff0a309a1f2d8bf
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:42:46 +0000

    frame runout punishment practice cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
