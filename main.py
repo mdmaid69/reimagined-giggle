f0f8cc7918d75ca70d9cd85a2f0ef871d13ee891
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:40:50 +0000

    Cc pile strike fire joke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
+from collections import Counter
+print(Counter("hello world"))
