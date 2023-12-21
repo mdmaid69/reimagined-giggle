ac33f3fb1991886ebb3f54db61c17ceae28c45ba
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:11:53 +0000

    high charge bin list obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
+  def is_even(n):
+        return n % 2 == 0
