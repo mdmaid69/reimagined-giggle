c70c830f3c6f2af34ed25424e408915f056ebc7c
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:40 +0000

    positive share lock unpleasant report

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def count_characters(sentence):
+        return len(sentence)
