189367764eb1c80e18779db3b378aa1842991316
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:45:01 +0000

    correct swim opposite conversation bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
