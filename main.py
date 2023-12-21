ca4fd84eaebc8492da02e65f0fc8eba16dabbd59
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:37:55 +0000

    arrival belief admire theirs damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+for i in range(5):
+        print(i)
