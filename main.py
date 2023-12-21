8b014c61e04d3bab61d983dd0b9a5960b9e79a2b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:26:32 +0000

    agree string cross game name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
