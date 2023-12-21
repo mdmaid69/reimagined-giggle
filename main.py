3fd660b9d673283e017d97ca1fb7c063e3356635
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:53:37 +0000

    emphasis attach mean shout interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
