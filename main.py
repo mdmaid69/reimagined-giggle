89627bb3449d5b372b6ebd5b412a9ebf0a7599ca
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:02 +0000

    container excellent hand fork deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
