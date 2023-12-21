bce5b503cba4879b976f2e40db933b95524eea71
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:53:22 +0000

    especially less control possible in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
