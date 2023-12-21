ce0834dd54416cd68873f2681778fc4fb733b949
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:42 +0000

    become cum several score see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import re
+print(re.match("h.*o", "hello world"))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
