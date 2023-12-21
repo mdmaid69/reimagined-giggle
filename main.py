cdaa09040ef9d0423eaff97a54b789d565c582cc
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:24:38 +0000

    risk tape confuse accept spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-print(random.randint(0, 100))
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
