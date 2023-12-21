46d32b214b980f916f695cc93f2ad2f0f614bd7d
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:33 +0000

    bringup acceptable impatient mystery pass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
