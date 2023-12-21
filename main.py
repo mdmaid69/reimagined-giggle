5316b4a2b1094705f596347fc3cb302ab8425f17
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:02 +0000

    kick kind practice emotion ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
