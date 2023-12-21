ca24c3275a2406edcd9f2f7f23ae3da2c599da98
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:23:31 +0000

    intend dance embarrassed line blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_int(array):
+        return int(array[0])
