3b037c896ef56b39ab86b2312f062bbbbf39fcc2
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:22:59 +0000

    forgive brown draw wire vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def greet(name):
+        print(f"Hello, {name}!")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
