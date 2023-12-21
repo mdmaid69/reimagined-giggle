6089479ed6c5a609eac4f56204f69105ba9cf6f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:20 +0000

    actually who major easy swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_speed(distance, time):
+        return distance / time
