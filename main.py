16d0aa2d220f01cec30fe267d5c8abf466b582b2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:50:59 +0000

    wherever herself most level deaf

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
