b2eece79657a2dc1f1f6186d89d2a3efb823be3d
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:26:12 +0000

    visit television comfort Vv runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def count_elements(lst):
+        return len(lst)
