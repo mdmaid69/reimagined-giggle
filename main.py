8061847e90b74bf99647f17c763bd44596832959
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:58:18 +0000

    what way central good sometimes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
