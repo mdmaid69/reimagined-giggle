86808714062f28db3dd4b11c50fe62cabce771b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:39 +0000

    hardly repeat wish transport minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
