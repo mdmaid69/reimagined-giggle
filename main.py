0c77a965084461ecc223a577744276c38d15dfc8
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:45:53 +0000

    design money Mm room Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
