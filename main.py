43f4f05dfc60018fefe3d38f1afd02c1474c2fb6
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:41:52 +0000

    most story production deep kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
