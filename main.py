b12a7b998d0f34ea683fcb6370d7ae0ca58f115b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:28 +0000

    address handle left walk example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_set(array):
+        return set(array)
