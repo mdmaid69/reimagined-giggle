41e8b2eabc6c6be85fd44e2ab4fe4c1731ffb615
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:33:36 +0000

    fact town disappointed by ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
