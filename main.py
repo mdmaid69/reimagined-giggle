7cc559ba1e2db7eb6c682d62e7f06d945ca36774
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:39:53 +0000

    shake difficult spot development minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
