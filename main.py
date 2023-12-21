017cbf67c388540d6607874461a077269b120e34
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:21:40 +0000

    exciting explain unlucky and about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def split_path(path):
+        return os.path.split(path)
