72a1f7fd6cd2dc38c764f15a8dd25af86bdc2e5a
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:43 +0000

    none consider save other emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
