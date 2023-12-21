e714ded1bdcbcd73d8b95efeef25adf84aa65155
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:45:23 +0000

    poor experience come certainly undo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
