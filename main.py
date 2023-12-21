c7badb7c7c17ccb49bd4c6aecd366d7214d9582d
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:15:00 +0000

    that concentrate once pile building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_float(array):
+        return float(array[0])
