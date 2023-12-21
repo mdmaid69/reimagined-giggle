2bb8ac9cbe9c2177dd361043525f45df4138b6e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:19 +0000

    protection bag Uu although act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import numpy as np
+print(np.array([1, 2, 3]))
