cab353e024264b439c743261b13e32623bd34cc3
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:15:59 +0000

    funny giveback male bother from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
