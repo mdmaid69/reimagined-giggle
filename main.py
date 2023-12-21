3de488c0135fb2dfba86fe2d5413e746073b4ecd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:37:48 +0000

    moon night passage weigh attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sys
-  def get_python_version():
-        return sys.version
+  def subtract_numbers(x, y):
+        return x - y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
