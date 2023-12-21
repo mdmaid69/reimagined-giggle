8b38150854b855c27ab1d94f4acd7843e0d94ca4
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:23 +0000

    lookafter scrap thought fall safe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
