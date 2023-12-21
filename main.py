c352cda563df4028fd25b590110b6ebd45d6f245
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:39 +0000

    project original declare leg friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
