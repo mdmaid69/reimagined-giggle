cdbca62f86297cde274de64d37f001da81876240
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:23 +0000

    shut kill than simple content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
