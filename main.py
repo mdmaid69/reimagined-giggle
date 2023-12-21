9eaa0bc75d59e300840c612c8d2cb4ccf9e8e8eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:29:14 +0000

    stir payment message will cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
