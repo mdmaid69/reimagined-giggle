c14dc6b4b94203a457ed18132a4cdfcb1796517b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:03:41 +0000

    burst invitation control taste unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
