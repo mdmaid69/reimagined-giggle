53f01409232834e7bab3efd9753e392e975af0ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:30:16 +0000

    eager habit development stiff except

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
