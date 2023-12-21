7ab17942a72dcde757e9692a5d2577911856a263
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:54:53 +0000

    flood partner he ready so

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
