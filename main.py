5cae0e845e9dd5ff4bb9fbfc52aa92e78cb183f2
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:55:45 +0000

    average turnon pretty hole goodbye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
