0252c304772bd7bb2cf4a1877efbab1c2be4bbb9
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:06:11 +0000

    different national cause disagree lead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
