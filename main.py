fc55084c1da6c0da390b4e7fdd43783d3dd1f5eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:09:57 +0000

    simple after through face skirt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def convert_to_hex(n):
+        return hex(n)
