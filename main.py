cb0ed44a0dc0ba06a0d01f1d536ca9db2209c85f
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:13:44 +0000

    stand shall gate there cinema

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_itemsize(array):
+        return array.itemsize
