4fd0356746846aeaf501dd622a0b8bd13a745a6d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:28:04 +0000

    breakup necessary row new mixture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
