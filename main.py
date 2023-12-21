97a6d9acb0d7639517b8378c3174e7a49161a3f3
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:03:39 +0000

    comeround this round switch hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
