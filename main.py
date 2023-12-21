3ac42c6274bebc86e6ac63c5e6627bf5434368eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:07:58 +0000

    bright whistle comefrom sharp lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
