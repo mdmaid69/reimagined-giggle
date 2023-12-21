16454dbe42e96363416ded7dda2f6be4d72cb5bd
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:04:18 +0000

    weigh previous turnon activity say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
