28e9ae41f222005bc3c55f28bbd5a47f0240d183
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:34:57 +0000

    responsibility married pump blame though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
