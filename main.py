4149a28cda9f543e9fece0b705a083ace5fc3d5e
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:05:54 +0000

    sign march announce shape your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
