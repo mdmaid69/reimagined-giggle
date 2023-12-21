b42035812ea2c4bdaa5a454f65331d3f073b6af3
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:00:01 +0000

    admire display fact Oo final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
