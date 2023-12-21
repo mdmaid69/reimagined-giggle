55f918f7ff2877f38bbde45a95291c1844dd50e1
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:15:18 +0000

    plate legal dig charge gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-name = "Python"
-print("Hello,", name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
