afb2e071b4f795ac90e374aafabaf13c6b89e48b
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:51:19 +0000

    defend eat statement mention autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
