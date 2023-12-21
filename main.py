8d0cd9039472722e2e6b3a92c40ac0dce7950d30
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:19:06 +0000

    catch heavy operation tired adjective

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
