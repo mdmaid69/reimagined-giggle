2bdc3235ea8be502692ff19331361a7f650dd169
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:24:31 +0000

    cover arrival otherwise police class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import sys
+def print_python_version():
+        return sys.version
 import array
 def convert_array_to_string(array):
         return array.tostring()
