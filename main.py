cc695d279a02ad278ec519b3066177c5d005ae26
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:32:58 +0000

    contents goback(to) cause smart relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
