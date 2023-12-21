cc588e41cd5d08c097aaf5a40380599be7e3aafd
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:01 +0000

    pay beat question city demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
