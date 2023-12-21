3089fc8f6f1e9f8615260987748e680d7493eb94
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:35:10 +0000

    nervous callup another weekend slip

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
