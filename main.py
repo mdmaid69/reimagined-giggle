6779a8944e80dbd0dc61a49d5344d2f033d6d589
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:52:11 +0000

    thinkover dear wide aim connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
