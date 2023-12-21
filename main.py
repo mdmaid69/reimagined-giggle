f60698adbb433249caa4d0cab1efd34e72af8418
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:01:29 +0000

    cost control bar office extra

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import collections
+def create_queue():
+        return collections.deque()
