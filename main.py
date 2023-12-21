7e86d87da7d1228f688e435c84c4ed03de2d0de4
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:29:13 +0000

    slip dark what tape right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
