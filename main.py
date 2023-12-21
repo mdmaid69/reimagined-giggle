93c970ec5ebd81ea932226e4f7c1579c765ec0c0
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:04:28 +0000

    out pretty better breakout when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
