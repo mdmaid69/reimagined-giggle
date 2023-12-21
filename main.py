8b56feb2a355fbf1b076ac9eba70ad5ed0b8d2eb
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:21:33 +0000

    boring walk tour recover woman

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
