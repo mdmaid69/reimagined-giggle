62926989b257a02a26e5d962e25eff07b536bdee
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:39:09 +0000

    avoid old compete whatever hers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
