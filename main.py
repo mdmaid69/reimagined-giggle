3b5de785057657e0c19bfa648a0c0b37806ea929
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:58 +0000

    anywayanyhow later leg contents achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+for i in range(5):
+        print(i)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return math.factorial(n)
