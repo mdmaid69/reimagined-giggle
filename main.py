78b997af6b2efb34299075fec3945c8c6f28bdc5
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:55:58 +0000

    patience occasional brush copy cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Uppercase:", text.upper())
