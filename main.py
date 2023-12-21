97852b0f17ada0610a5faae166727c03b5d2044a
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:47:45 +0000

    introduction keepaway plenty will else

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+for i in range(10): print(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
