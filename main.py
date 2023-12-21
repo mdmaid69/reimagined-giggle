6ffcdfb210c9e6b2488bed029bcd000097f7506a
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:47:23 +0000

    clean store do notice century

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
