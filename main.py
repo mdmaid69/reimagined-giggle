fbe05fbff44b8e5b00a0d3dae644d4f9b5f4fc6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:51:20 +0000

    contrast down fetch moon cheat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
