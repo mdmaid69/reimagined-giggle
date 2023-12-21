7c8bb3baa673cf642705c1aab54c25728a34a86b
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:17:42 +0000

    where discovery prove goup patient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
