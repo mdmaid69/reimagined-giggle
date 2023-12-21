7b364c2b2db2529026f1a60bac7fcfab083bc08b
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:12:03 +0000

    passage calmly arrangement clue benefit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
