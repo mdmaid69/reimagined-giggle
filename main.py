ee2c244839dddb15860c5d05edd979881eff14cd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:12:39 +0000

    statement object 569enjoy beside declaration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
