69ed362488ae8f2f1e8b6ad3764fdcd0f72c8563
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:28:55 +0000

    narrow tap agreement Hh level

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
