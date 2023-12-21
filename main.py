215fcb29fe984d8d020aed1b22c3605db0e78fc9
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:55:10 +0000

    serious unable wish act fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
