317835d33e5338fb537d1666a3f8481a95fb3be6
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:48:37 +0000

    giveback exciting 564encourage Rr certain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_length(array):
-        return len(array)
+def is_palindrome(s):
+        return s == s[::-1]
+def find_max(lst):
+        return max(lst)
