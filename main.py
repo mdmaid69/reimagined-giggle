a6eafaa41d3f7b97eb8341a3f3c5c8500903f4ab
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:51:33 +0000

    theirs anywayanyhow image raise belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
