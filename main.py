90e33f67bc14c30dad034e104f853d8c6a48e63a
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:17:17 +0000

    weekend round convincing drawing out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
