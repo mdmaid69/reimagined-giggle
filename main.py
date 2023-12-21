987ab50bfd49c23086f9ef7b1d8925b333d6f700
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:45:30 +0000

    similar will he try unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
