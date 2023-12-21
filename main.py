300b0f37fc90a62a0dc6b1d0226783237b3895f8
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:48:07 +0000

    confidence dress pretty convince tool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
