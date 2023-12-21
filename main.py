7eb415d48d1394e4048e6ee7cd0bc3b61a8c7cb7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:16:43 +0000

    below enthusiastic confuse water warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
