c1edbce636974d5aecbce606b58e40756f99649c
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:54:39 +0000

    voice sink eye insist public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
