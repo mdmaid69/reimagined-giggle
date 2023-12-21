eedee19b2a6a0d5ca3f7b0db6adb8822567d0c8a
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:25:00 +0000

    any lie who sore form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
