8929443061071013a547c3f653bdc40f7e4d7c0c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:40:04 +0000

    goon brave stop mind back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
