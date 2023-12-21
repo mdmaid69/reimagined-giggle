784d6a9bad1c919bfc3c9a325def269c90375723
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:10:50 +0000

    game earn confident stamp class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
