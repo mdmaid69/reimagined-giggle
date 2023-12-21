e1b8fc7238849330c32fb28aad3a6331de397561
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:51:24 +0000

    confidence university transport opinion march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
