c7cf35332cfbb4f7ae918916de4cc99827955258
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:17 +0000

    neither loose mood lay nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
