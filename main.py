f5adc741061ac57269b1c9f391dc45c824f2f0f4
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:40:54 +0000

    usually giveout pleasure foot help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
