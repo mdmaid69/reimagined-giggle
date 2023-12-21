f28c1d83c57f9519c9017722492756489deffff2
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:57:45 +0000

    keepfrom transport marry duty meaning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
