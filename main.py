4c61f75ad6add98b20836c971b52ccf4c2d0efbf
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:14:05 +0000

    belief bit plenty neither unable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_max(lst):
+        return max(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
