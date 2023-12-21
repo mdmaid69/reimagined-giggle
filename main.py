2dd4780c320e5f3be773d527d1d93931f9e61595
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:38:47 +0000

    trust possession appeal rent never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
