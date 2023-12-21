fd3c423dc130afd210343b1c6eb21c9004de518d
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:41:37 +0000

    at although stand I cause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
