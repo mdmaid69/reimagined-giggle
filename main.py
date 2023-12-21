f0c9cf9c959a06bb9f90763e68536d3b82d440b6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:08:07 +0000

    position agree sweep that forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
