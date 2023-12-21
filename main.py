a376fbffbab0b4c7531ab5daf3ce2d7b49323d8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:54:27 +0000

    sign pain protest create book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
