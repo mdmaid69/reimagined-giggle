a2bd79c891fcb2763038a50dedc522701af44727
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:35:41 +0000

    concerning whatever impatient increase people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+print(math.pi)
