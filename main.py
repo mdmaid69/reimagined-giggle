9c9014595906fa5d6ae2db833ccb38c7646b0497
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:13:16 +0000

    period dream crash cheap upwards

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def count_words(sentence):
+        return len(sentence.split())
