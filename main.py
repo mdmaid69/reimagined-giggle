64aebd34f2fa0ff02d3772ea2273fdb127d5a010
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:04:27 +0000

    hungry workon fasten urgent underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
