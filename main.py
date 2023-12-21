03aa76c73da080e291e74b214c745b80fcd60e95
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:31:48 +0000

    drop let weak instant between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
