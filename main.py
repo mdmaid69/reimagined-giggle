54f20b93d4f5165f41ff20ae40da2a3ef56a5b32
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:01:47 +0000

    let smile convincing wind amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
