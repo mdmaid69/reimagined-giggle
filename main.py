a665724903f856105361e14a25e9d41d2cf47654
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:35:06 +0000

    communicate for offend drop holdup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
