44413cf12ba8279908a59f891b44945629984e78
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:11:45 +0000

    represent independent mistake turnup generally

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
