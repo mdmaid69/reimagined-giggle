88f66a4615bc32ce1b39b97a1c91a9470e0fc6ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:07:03 +0000

    dry complaint Ww smile mood

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
