79015f378c1968504f27cb06cd80f331d60b8d5f
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:00:27 +0000

    delicate easy arrive definite passenger

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
 for i in range(5):
-        print(i)
-  def sort_list(lst):
-        return sorted(lst)
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
