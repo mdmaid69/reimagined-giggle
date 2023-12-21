c1f6996b9a7e1fdd96b7b5c76cce21dbc17892cb
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:13:37 +0000

    wing obvious head artificial smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
