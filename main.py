ab81a1bd7ef62648a9c865697d586c8e36003b1d
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:25:19 +0000

    godown cry space solve heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
