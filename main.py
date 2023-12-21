48440eeeb1a83def9c61c90f79fd04e3dabbc9a9
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:36:16 +0000

    past thanks surprising trade over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
