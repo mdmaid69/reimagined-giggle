50d78d76a5bdd5b182bd8cafc7f629d7f10fa095
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:41:11 +0000

    foreign comein Jj shadow sale

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-import os
-print(os.getcwd())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
