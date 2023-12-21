3527e0e1ff0042efeb85a0c09c49284ba4aba18d
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:55 +0000

    exchange flood swing well in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
