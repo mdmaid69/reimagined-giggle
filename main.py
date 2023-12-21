3dfb75eb4586ac2067bc2e9a545a96c8b08cd24b
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:33:09 +0000

    spring intelligent complaint have exam

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def add_numbers(x, y):
+        return x + y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
