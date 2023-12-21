0d621d05a7ee3e4acc945fa4501d62dc67c03a0d
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:20:15 +0000

    connect stamp good near fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
