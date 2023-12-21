7127dc849118d2b1a930aaf97a542ee3102f7488
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:25:23 +0000

    front succeed successful meal occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
