48839084955caf1f7a0833e0031cedeecc0c74a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:52:49 +0000

    swim march recommend refer anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
