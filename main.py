ca38d9aa7938c9e565d0d77cae75bbdebaf9bedf
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:13:36 +0000

    slide behind trip callat mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
