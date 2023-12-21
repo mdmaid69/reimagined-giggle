a07b0c1cb149567af56e6b0f4da2fe2891bb6ffc
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:28:02 +0000

    my skirt sit in this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
