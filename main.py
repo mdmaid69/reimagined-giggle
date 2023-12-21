fbcac22372e82b347d5ff8f7f13b9ba00a54992e
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:52:16 +0000

    pattern sail concentrate sport 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def clear_array(array):
+        array *= 0
