1f39599f8ee72bb6e0fb0e5243e94805bdb85238
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:15:12 +0000

    contact minority major step wait

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
