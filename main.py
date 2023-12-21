dafa47dbb7c9d73442db77452eb8ed18a9330f8b
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:26:00 +0000

    event complaint bang free truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def remove_duplicates(lst):
+        return list(set(lst))
