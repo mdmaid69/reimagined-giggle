4db1ce3bf5949ea6beb51b35e5d373cabe56b822
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:44:22 +0000

    goup subject home report workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import time
+print(time.time())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
