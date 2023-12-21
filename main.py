2d38f03c6f8d42e43c7fb1000911b6dd71abf02c
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:20:51 +0000

    main relax collection strike chance

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print("Hello, world!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
