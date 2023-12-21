536e8237c807815d7f847163bbdd0a0feaee8cea
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:15:43 +0000

    odd lock such hard formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
