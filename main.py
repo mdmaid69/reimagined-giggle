6eec1b71be3b626b5cce034fd44bd15f7be75995
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:57:17 +0000

    strike clear slightly believe stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
