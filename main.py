d8964deb7c413380fa529a1b527d2b7fc9c14d1f
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:06:35 +0000

    cause row accept point violent

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
