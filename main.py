46cae891fc66f7d6ef7df2b0c3ffb28d18b6bb3e
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:37:37 +0000

    system battery niggaacademy swing like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
