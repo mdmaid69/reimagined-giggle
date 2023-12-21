2c457e9d9f79a067fca5015e9074ea2957b2c2c4
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:28:26 +0000

    bear lock goout fun see

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
