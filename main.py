0a8b07285b692cf795fb8045a5de5b34db36074b
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:39:35 +0000

    compete refer entrance accommodation team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
