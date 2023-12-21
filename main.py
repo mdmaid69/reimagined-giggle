05b2b9132a101e620f457f6125aec641c9551fcd
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:30:45 +0000

    level kind better way judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
