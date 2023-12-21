6ff7af3ba6cdb65fb312b89f33b5a781ccdf5c82
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:21:33 +0000

    alone someone off hide maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
