6dd34d36a5e9467245f858d98432d9de886f238b
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:28 +0000

    confused whichever some suffer still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
