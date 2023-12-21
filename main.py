0ecbcaaaf3e96633ae1809edd65e04a18d4048a3
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:57:43 +0000

    he cruelty display model transport

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import os
+print(os.getcwd())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
