105f360395dd2729f4f77245f940e3f699776ea0
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:51:34 +0000

    begin pin wire safe ability

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
