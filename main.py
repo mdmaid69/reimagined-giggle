ab78195225f636b3d8143c7c766f911563d3a59d
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:21:54 +0000

    control arm previous point comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
