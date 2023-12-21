c4e56c97c18a3524beb2ec0aaee88867d622b636
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:46 +0000

    calm gate wide skin thin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_pressure(force, area):
+        return force / area
