10bd9c8e7ae8d6d3584d997fd4d471537d9df136
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:37:54 +0000

    rush out flavour report bring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
