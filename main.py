a1c073f6962bb0d3345fd3d649903612822acc69
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:30:55 +0000

    branch bringback spoil grade part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
