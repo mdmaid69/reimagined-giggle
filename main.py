f2d0beb7b4e48d000f14c12f61198972d764594a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:26:14 +0000

    combination makeinto convince only appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
