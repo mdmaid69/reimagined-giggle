fc2fdca0867679ede71daf729a0bf6bddeb79c5c
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:37:50 +0000

    ever event sleep murder finally

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_max(lst):
+        return max(lst)
