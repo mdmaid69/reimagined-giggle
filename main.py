329e75ae3b44c652561394b2bd9018c00d8cb963
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:29:24 +0000

    summer 572enter under threaten drawing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_even(n):
+        return n % 2 == 0
