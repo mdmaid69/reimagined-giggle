ae7056d4c892b6fead250cc56ac0aeef028f0111
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:07:03 +0000

    national neat trust crash lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
