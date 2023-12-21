f0e841676dc3a7fcdb1b9b7f47b3d9172a80d536
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:35:40 +0000

    negative late speak team runover

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
