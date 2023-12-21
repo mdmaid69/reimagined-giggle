9808ea117cfc71768d62da45e24519d75682d3ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:33 +0000

    beginning rate also flood so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def square_number(x):
-        return x**2
+import random
+print(random.randint(0, 100))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
