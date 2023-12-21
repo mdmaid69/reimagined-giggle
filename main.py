cb32692a165e47166a476fc9ca64a03db1511cac
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:29:28 +0000

    rest mouth children anyone meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
