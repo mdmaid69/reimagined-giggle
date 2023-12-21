5facb8817c953ae748d79ba2807ab393645ee00d
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:18:50 +0000

    set coffee inside pencil race

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def cube_number(x):
+        return x**3
