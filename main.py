4db984a7f642d30aa63c9b581165ea1dc58b6059
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:47:05 +0000

    fit tight preparation yours pinch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import numpy as np
+print(np.array([1, 2, 3]))
