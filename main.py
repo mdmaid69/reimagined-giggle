58547bf7b94a37964db1e0f7b14b96be8f20fbd2
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:24:29 +0000

    loud immediate cover law somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
