2afdcd41a60101cd9d8c879a3639cdfff4a6efa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:11:37 +0000

    table add wait wind bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
