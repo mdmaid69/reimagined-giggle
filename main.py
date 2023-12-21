d869ad93d045e9f14ba75e87a13e3b48133883e8
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:57:48 +0000

    happen here of Itself fortunate

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_counter():
+        return collections.Counter()
