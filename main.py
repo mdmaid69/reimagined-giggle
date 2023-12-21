013dd1e998c07bca116029d073df0ac3cb6c8305
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:23:12 +0000

    hard bill breakout slice shut

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
