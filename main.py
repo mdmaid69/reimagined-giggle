33709eef4a2b86f3f46a20fc3d3adbfd607db75e
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:42:22 +0000

    call usually fail block sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
