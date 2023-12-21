9800b8bc92ee824761d9703a844806a43ae0794e
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:48:40 +0000

    cum correction last so medium

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def multiply_numbers(x, y):
+        return x * y
