88d35c47b09cbdc1b1c53259d6809a476a2b9466
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:56:43 +0000

    in surprising breath unit direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
