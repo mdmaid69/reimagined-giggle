9e6f5377734569405b4e2122b4a2f4d6836b81ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:25:01 +0000

    beyond fair point mend tend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
