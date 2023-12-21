54d4be3c060e7ea27148380db96096b8adb7aedf
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:46:16 +0000

    partner discovery introduce coat thought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
