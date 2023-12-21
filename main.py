399a8debccf530f279e70642acc058c92ee24a37
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:10:54 +0000

    Jj serious maintain organization confidence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_typecode(array):
+        return array.typecode
