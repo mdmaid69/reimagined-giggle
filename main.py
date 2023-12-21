2cacfde62f7e0378d72a080cee32c281180e65b3
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:06 +0000

    proud organization lock crash declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
