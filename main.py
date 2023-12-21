c976d42a7d90406e8b6a7916d4f5aa3bc7ccf0dd
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:23:20 +0000

    deliberately dry whisper turndown building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
