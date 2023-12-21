3c85acf7e7e8807d8028276fd5c8b477b58e7863
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:53:10 +0000

    friendly offer story refer somebody

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def sort_list(lst):
-        return sorted(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def print_python_version():
+        print(sys.version)
