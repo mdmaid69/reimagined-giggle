c599e89e8267facfabf7e0f4918838b2b792637a
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:17:31 +0000

    speak bus accept wrong behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
