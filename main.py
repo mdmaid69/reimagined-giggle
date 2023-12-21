5ba53034e07c077e1a9ad0089f329b61dffa87ae
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:43:36 +0000

    cold breakin estimate no party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
