2b9a1dd20167fd9c2318a78cac3a70252323814f
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:53:00 +0000

    further bag care any calloff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_current_directory():
+        return os.getcwd()
