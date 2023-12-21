3b8641822e24254a52adb6d6180939920eddeeec
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:48:09 +0000

    dream emergency healthy limit arm

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
