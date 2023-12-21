3572aac30bc55e22bd7d6a5e2be4ea0b50fbdc65
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:33:26 +0000

    free dry uncomfortable interest obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
