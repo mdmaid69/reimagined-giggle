2a761b14a658d6928e3c333d769c8bec8d1807cf
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:42:05 +0000

    think rush defeat tie afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import time
-def get_current_time():
-        return time.ctime()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
