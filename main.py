6107997e1a4f30c57997d8051ef756d41cbc5dcf
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:49:11 +0000

    treat even rubbish minimum complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
