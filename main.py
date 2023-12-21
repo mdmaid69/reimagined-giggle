a420815646b3d01a075139af00dacacc31ca96a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:26:14 +0000

    fortunately available scrap goback(to) thought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
