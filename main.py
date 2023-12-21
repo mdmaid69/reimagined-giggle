b63e79ce15bcdb4bd6361d297ac152f9b5e96148
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:54:15 +0000

    point special sorry join worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
