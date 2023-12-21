de5314428c31aad3a56181ab4eb82dc6d565af93
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:38 +0000

    choose contrast occur pride interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
