ed15f55eb576338d167aa670b0fa31bc8841741a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:25:40 +0000

    force hit lean big closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
