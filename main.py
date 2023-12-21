cfaf952cf7167e8a5a33e39bbb90e8307633fc1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:21:03 +0000

    voice middle model sweet ashamed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
