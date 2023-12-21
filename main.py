07f16df06a85d191febf1e0679d54e2f2d738202
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:47:12 +0000

    church black process her medium

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
