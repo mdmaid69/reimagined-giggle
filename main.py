10322c2d13c06af4f02cbd77af7a8889e92392d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:36:41 +0000

    confused thing contrast find accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
