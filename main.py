06338126179b1369280c0a84a2d98b252920831f
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:57:17 +0000

    season schedule clear communication brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def get_array_index(array, item):
+        return array.index(item)
+def find_min(lst):
+        return min(lst)
