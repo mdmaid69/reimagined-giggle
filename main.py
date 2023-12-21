8f38ca0a605faf6c4ee0939a908e40456dbfd654
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:48:41 +0000

    radio low one lie arm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
