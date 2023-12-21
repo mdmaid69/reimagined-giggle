12e1aa05dcd282f2ad33ae4f05ed7b888e7aae33
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:05:56 +0000

    ahead climb trouble lock store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
