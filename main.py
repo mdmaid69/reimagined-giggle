bdf66279a3c0a1e45ce4f2733a76aaa0bfbff38e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:59:50 +0000

    song considering view shoot discover

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_current_working_directory():
+        return os.getcwd()
