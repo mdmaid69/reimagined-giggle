da1b6fc0690f60c318d179d5cd5a8376e087f561
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:46:51 +0000

    young language break nose moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def change_working_directory(path):
+        os.chdir(path)
