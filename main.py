ce29450b21ed8d7e16c82aaf18ad94d9c73bcf96
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:55:09 +0000

    exist difference crash keepoff occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def divide_numbers(x, y):
+        return x / y
