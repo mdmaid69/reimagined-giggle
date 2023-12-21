f2a78b70267f236cf8ce31a82b93e9049e3a273f
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:30:49 +0000

    firm 564encourage little getback square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
