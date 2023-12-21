b768f0fbc9e8c88d3fad84f22c0a5136dbfcb37a
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:44:03 +0000

    friendly region tune theirs little

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
