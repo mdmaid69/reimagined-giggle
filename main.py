1dc85ecdd48b0a909e543addedd525db138d28ea
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:40:31 +0000

    turn talkinto square comeback watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
