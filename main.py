2fbe3c3e5ddfd627dc2adbab58aa35d6a7034c29
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:41:06 +0000

    disturbing sky modern noise injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
