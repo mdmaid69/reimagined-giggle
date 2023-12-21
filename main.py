5c59ef869d39e34dbcfacf9240bd0d3c1fbde5ad
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:14 +0000

    discover hungry table reject innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
