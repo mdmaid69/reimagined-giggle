8bcedf9f226b6ddc71bd0f4eb5677d00af6cff0f
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:09:42 +0000

    lesson miss pass pin turnout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
