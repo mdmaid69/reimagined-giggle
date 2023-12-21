bfd2ae374d95527a993b4c995f66fa1782234422
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:42:31 +0000

    every keepaway let set future

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
