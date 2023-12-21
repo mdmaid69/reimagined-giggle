7b0a1723045378bfb53e07605902e27350b79e10
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:14 +0000

    lose speed extremely lower nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
