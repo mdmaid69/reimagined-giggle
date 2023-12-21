3da63e6a016ca00ef46e5a4b8340267d233ec159
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:11:09 +0000

    sticky summer cruelty stamp obvious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
