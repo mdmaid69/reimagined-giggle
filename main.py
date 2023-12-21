31494c0c0ff06a99808464ed424d13d38cf3b3fb
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:53:24 +0000

    eager her reliable sort estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
