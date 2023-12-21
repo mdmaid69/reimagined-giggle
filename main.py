38bb742dd6d1de0f85622151f1088bc12bc42a1b
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:41 +0000

    keepoff trial suit label now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def convert_to_binary(n):
+        return bin(n)
