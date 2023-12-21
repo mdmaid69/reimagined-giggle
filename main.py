5cb4143e9011cc1fe276981e889830b9ddc13347
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:43:40 +0000

    firm set sister cross deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
