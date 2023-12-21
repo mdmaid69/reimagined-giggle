789bcca0357af3f82cc3f577e1e5476f22debb96
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:34:53 +0000

    respect fond estimate tie answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import numpy as np
+print(np.array([1, 2, 3]))
