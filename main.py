1abd878e841066eb8476f491234a3bc3b9365d18
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:04:03 +0000

    bush attractive perform pretty rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
