14377862dca9bc26ada31453baef8c78d4da8000
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:20:45 +0000

    rise fact tap hope similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
