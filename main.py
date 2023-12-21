534f09ed1b012b0fa7bec5daa11f3d45420c13ed
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:35:56 +0000

    loss record computer defend available

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
