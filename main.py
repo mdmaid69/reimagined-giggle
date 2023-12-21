f3f3c312e00bb66bb9be8de93b6df2727034c54d
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:34:43 +0000

    doubt feeling ticket copy experience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
