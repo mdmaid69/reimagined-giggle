f6399b3ed7dc88c2640ae66858c3a333483f8bd8
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:02:09 +0000

    expensive touch everybody Dd relative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
