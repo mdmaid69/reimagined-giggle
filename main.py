40f9319537d8bb725d5b71efe5752a7f438734d6
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:55:01 +0000

    common design badly thirsty hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def count_words(sentence):
+        return len(sentence.split())
