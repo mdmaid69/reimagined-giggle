c9b0eed9d401d14da4ccb9e95cbfaa1addcf8d8c
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:11:50 +0000

    shape mend art ball replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
