77e9e7d460964331792e61e12a24c70f927167df
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:12:52 +0000

    dare move strength forward sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def reverse_string(s):
-        return s[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
