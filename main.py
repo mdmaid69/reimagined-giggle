53cbfe3ed1abbb1fa45390337b9ef7ce3b8eed6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:12:37 +0000

    that call happen proof against

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
