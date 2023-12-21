e1881dd747834fe82c7234c0ac6a123fe4e8ea31
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:39:05 +0000

    confused join song fun embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
