5c85748642fa231f8f066e534aa0e4ed60db21ce
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:53:19 +0000

    past manner embarrassed snow me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
