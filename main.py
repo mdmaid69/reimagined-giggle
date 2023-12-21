dd30184f97407b0deac5a828653c0d08db1be997
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:41:55 +0000

    most there neither hold complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
