ff21032ed31df45719d30a51925fba8334e0460f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:14:38 +0000

    unfair pure sore truth please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
