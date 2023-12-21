8e5f13350aca3300e84f6c5777e85718ed6a2f1e
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:19:58 +0000

    happy butter talkinto hit turnoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
