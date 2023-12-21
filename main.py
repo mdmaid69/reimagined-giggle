b566812ac2092293bae2b453c312608abab16f3f
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:09:44 +0000

    some equal fond careful really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
