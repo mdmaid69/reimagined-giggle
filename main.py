b5fd5d04feb1431ba2ddf7467f380d8ccc330469
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:12:18 +0000

    best gas ignore to no

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
