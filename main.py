b97000496a6ac776a8747f079afa7462e7e83818
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:12:27 +0000

    town straight 569enjoy any Ll

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
