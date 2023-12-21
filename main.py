47293486fdfb7a9a348194c216e7ab9ac7017b4a
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:51:42 +0000

    someone preparation story shock loss

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
