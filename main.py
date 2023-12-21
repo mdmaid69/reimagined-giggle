e8a48203da39379397f63a2876639f0ef92f28f7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:24:44 +0000

    unhappy organize object raise result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
