91a0bf3a9ca234f9ed309b4940c3680d3534c835
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:10:12 +0000

    tragedy move reject past tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
