7f94bc5ecb185f3f074f178716863a98ae56390d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:42:51 +0000

    shout fall price woman plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_factorial(n):
+        return math.factorial(n)
