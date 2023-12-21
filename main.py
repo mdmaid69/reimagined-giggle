32622d17d148f4f05baf51fb23c2111abb10dbc6
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:51:12 +0000

    exercise keepup invite port acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_complex(array):
+        return complex(array[0])
