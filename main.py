17f8926f297e5c6e695512e9401f4d294eb6e8b9
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:02:34 +0000

    sister shout emergency progress instead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(numbers):
+        return max(numbers)
+import sys
+print(sys.version)
