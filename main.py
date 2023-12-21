19d8c7ebeb3d3a7c5fc8a2af2d839b93b826c425
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:05:27 +0000

    everyone present bet maintain lie

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
