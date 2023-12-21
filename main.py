83dbde7e5fb762fd2e1ed14adf9719ce69f0b774
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:33:14 +0000

    realize instrument regret shoulder mention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
