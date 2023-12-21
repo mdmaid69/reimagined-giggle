22e632a11383c60c630c0abd79f044310e49f9d5
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:50:22 +0000

    consult key cut excuse accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
