8534f45df8765b45761ffc42d4f4433f03132598
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:35:53 +0000

    prize murder ocean wish burst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
