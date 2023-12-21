b2f98278c16909db23fa039f47e6032e78569f80
mdmaid69 <robot@alexsyw.me>
2023-12-21 08:51:52 +0000

    mix relax between pause contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
