09c1d93dc17ddfa3ffe4d286c0adc84f6c5f0b59
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:38:45 +0000

    tragedy chat runinto container anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
