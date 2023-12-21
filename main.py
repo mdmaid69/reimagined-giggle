f880d3f870c719fc8d51090a6693ebe6b9b4ed1c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:27:02 +0000

    Itself snow switch division like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def count_characters(sentence):
+        return len(sentence)
