e4f2a6ffc91a4c9cb33cf9888e4dadd4c9feb451
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:58:14 +0000

    right live ring push shrink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
