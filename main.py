ea1bea27f3c45cd8e1e23484d59f6580862d4c78
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:37:23 +0000

    travel accordingto change put tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_current_directory():
+        return os.getcwd()
