3872d5dd7fe4330357853a2f39478685e65c28bc
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:54:20 +0000

    criticism standard deliver disgusting reliable

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
