2c32c3d65d0988f744e2e4638b6eebbc6544332e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:22:30 +0000

    success typical pinch dare greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
