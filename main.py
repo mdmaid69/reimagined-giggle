fdd9cc7e6ef0994799ac1dae375ecd726639c882
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:39:09 +0000

    increase memory violence risk choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def create_queue():
+        return collections.deque()
