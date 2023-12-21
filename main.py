eb136d30d8deb02683a5e912ca354a645b2c8dcb
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:14:28 +0000

    nose play steady straight design

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
