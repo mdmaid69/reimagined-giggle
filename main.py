9a8b2ff5311c55c89eb6296e332b330f972cd891
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:00:37 +0000

    develop far fasten emotion path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
