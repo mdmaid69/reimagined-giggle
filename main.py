1b39c5bbfc02b66f56439e3dea9c368893af62ee
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:05:29 +0000

    bag feel real argue then

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-print(time.time())
+import tensorflow as tf
+print(tf.__version__)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
