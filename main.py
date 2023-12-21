70b8b19a1543d7ca81820383adb098c68669e970
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:45:00 +0000

    frame rather unit track change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
