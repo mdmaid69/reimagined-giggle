62eaa48b7adf1b58e1ec99711b454cac2f15765c
mdmaid69 <robot@alexsyw.me>
2023-12-21 21:38:04 +0000

    figure further wind only safe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+def convert_to_binary(n):
+        return bin(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
