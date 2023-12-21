db45caefb8570ea2d6a01f7ae325b512bc5bd091
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:30:55 +0000

    maybe so major add manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
