0f82ad500933c2be99dcb878b2b94cbdf8df7718
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:40:44 +0000

    favour generally eye cost family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
