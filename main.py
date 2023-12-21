9b81eb2002d034ed56def36ba99b92a95864eb30
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:15:51 +0000

    city brush dry murder gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
