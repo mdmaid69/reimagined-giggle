b0cb641e8b1ced0ce92803e63f9ddafa35309639
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:11:49 +0000

    unite enthusiastic wrong bang demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+print(os.getcwd())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
