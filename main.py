5ebff0e0468cc844dd6995ff9556ab195749f829
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:31:51 +0000

    rate brown getback start far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+import os
+print(os.getcwd())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
