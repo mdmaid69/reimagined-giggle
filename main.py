e31893d368e004e95c9465a1b3f5b8b79070e29c
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:43:25 +0000

    impressed pain deserve interview me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cosine(x):
+        return math.cos(x)
