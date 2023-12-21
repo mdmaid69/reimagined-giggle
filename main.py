94486f1b7935c39cb0b25313ef6c4fd55c96ae05
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:57:08 +0000

    lost them subject extraordinary wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
