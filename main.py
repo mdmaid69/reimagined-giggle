39c9e9685411063305b30e81309c32f955e97fca
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:35:04 +0000

    give my meeting stage touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
