6e23f47b27ad7ab6200b76d6ca6955f5d5814378
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:25:26 +0000

    laugh necessary instrument sure show

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
