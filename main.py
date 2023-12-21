52c04392678015436507e4b129918274c5275066
mdmaid69 <robot@alexsyw.me>
2023-12-21 15:56:59 +0000

    invite word hit such once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_sign(x):
+        return math.copysign(1, x)
