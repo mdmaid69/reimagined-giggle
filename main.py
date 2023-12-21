c00c99b8cb3a54f4051b5eb3914b5636d381b087
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:41:01 +0000

    clearly science right backwards concentrate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
