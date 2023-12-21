1792e1ade17ce1c22099591ffd8d2fac191c0baf
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:38:26 +0000

    respect cross trick yet consider

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
