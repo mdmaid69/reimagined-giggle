2287588fe94ddfadf086eb1a5319bc58099d97a7
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:26:09 +0000

    cost for long flight vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
