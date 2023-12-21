e8b6b70358e92a59c332682a5ec5f8a7ef4212c6
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:24 +0000

    568energy read sometimes abroad angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
