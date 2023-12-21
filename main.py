19815930e512724fa07134f19d16911dd6fb86a2
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:00:46 +0000

    term appear progress politics brother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
