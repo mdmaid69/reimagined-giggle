970987b0eb50a814f4228ec500340b3c8e758fa2
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:18:19 +0000

    birthday how let culture spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
