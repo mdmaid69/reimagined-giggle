7e6f34678a1abf346ab94b3352ba9c48fd340969
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:11:12 +0000

    skill sort image question computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
