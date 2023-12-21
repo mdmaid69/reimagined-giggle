d7e87992c564d7cd07a15741839876c118dc3dbb
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:51:01 +0000

    raise both gap common ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+print("Hello, world!")
