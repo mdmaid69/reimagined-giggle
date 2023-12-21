7d1d956cfa0da98dc5eedfd40c516c90346aaddc
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:07:12 +0000

    set breakfast tragedy dance remember

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
