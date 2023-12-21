a1a63ae599f773e7de509e542aa7296faca7d491
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:27:25 +0000

    hurt boring embarrassing low learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
