43a41ee3a4ca3bf43a023eb1d862fb10d3226f38
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:57:02 +0000

    anywayanyhow war need point deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
