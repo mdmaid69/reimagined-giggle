a723e3c430238ea8d2ea4b015126b4dfdaf4e82c
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:33:45 +0000

    so begin arm fail air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
