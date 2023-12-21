ec8dc84ce83e1072dcc9595fe7959a8d33ff396a
mdmaid69 <robot@alexsyw.me>
2023-12-21 03:53:14 +0000

    major brief impression mess law

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
