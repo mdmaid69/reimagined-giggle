0ec6ac6e78863afafa0b08459caaa0908ced2ded
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:49:11 +0000

    part could shock practical yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
