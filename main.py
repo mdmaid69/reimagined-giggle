35cb4b967fd988ccfd1aee220557666ee1bc541d
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:14:59 +0000

    tread sink safety foot goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
