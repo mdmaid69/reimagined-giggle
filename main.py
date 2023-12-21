0ca8fa0511d8f7f29c4c003b6aeb814df5cd8533
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:50:58 +0000

    life breakup face safe the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
