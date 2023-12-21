41dd42d9b0b918ebc0b2837721b3ca8259f42f8f
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:03:18 +0000

    kick they equal genuine owe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
