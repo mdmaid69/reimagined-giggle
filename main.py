495522d9cb674a2eb645dcb5ebab9688eb5f9b10
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:36:25 +0000

    have basis attach phone personal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
