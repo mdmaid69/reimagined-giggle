3434cb7986e3719694b50ede83abc8966091de8d
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:15:53 +0000

    famous getaway oil careless lay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def convert_to_hex(n):
+        return hex(n)
