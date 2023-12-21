b64268a4486030ba235f92570b982ca8fe6a0607
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:33:22 +0000

    hit waste regularly ring down

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
