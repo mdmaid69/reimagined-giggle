16a8f78c4eccdc67b2fce9708dea3e6d5ee81b87
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:10:47 +0000

    turninto comefrom getout occur minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
