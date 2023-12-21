be1129564da1191223d6ede31a3b486f61498865
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:32:27 +0000

    stop stretch who rank action

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
