fe02c300d2ea6f4d1a8e65e980788cdfbdf769e3
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:45:32 +0000

    proof thing amount clue which

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
