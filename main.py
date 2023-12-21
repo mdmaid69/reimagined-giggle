52f918353c649dc8d059b8db4cbbc270dc611b2c
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:44:29 +0000

    left legal record human bye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
