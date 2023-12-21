7b8c6e872d433ec3fb8d007bf6f44fa3a2815c6b
mdmaid69 <robot@alexsyw.me>
2023-12-21 00:26:59 +0000

    tall storm compete set satisfied

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
