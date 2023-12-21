d1c0d75223f3a131b672982132ca0990a8a9ba44
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:32:30 +0000

    crowd mad main clear people

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
