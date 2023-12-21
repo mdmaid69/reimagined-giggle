b316e77fba174afbc5be74b00d53d551c76218b0
mdmaid69 <robot@alexsyw.me>
2023-12-21 06:01:41 +0000

    pronunciation education when offer meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
