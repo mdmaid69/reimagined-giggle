78228d9732d05dd7fba09343500fae7b81552c3a
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:19:42 +0000

    approve less shut them ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
