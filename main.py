208c7a7a4ba244b9c6c9cace3bd40e0aa9717fff
mdmaid69 <robot@alexsyw.me>
2023-12-21 22:22:33 +0000

    estimate mean meal Itself below

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
