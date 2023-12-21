bbfa8dff22c3f3ed44f21f439b705a7004d6d55e
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:29:48 +0000

    cool pen quiet path smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
