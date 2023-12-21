94008b6636ec0ded4cb319ca25eac9b421b5d45e
mdmaid69 <robot@alexsyw.me>
2023-12-21 16:20:42 +0000

    busy achievement present guide risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
