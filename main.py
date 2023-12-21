af19207646d13adc263cd45b55df5fdbd968428a
mdmaid69 <robot@alexsyw.me>
2023-12-21 02:06:41 +0000

    tall phone record wide now

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def print_python_version():
+        print(sys.version)
