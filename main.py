62569b350e83edc27aa1c0bf365536acfa1505a0
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:03:03 +0000

    own wet move error around

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
