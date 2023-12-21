c8d6ee37ce60677970d4153498eedafc2531dcc0
mdmaid69 <robot@alexsyw.me>
2023-12-21 04:39:18 +0000

    sick smooth sick original matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
