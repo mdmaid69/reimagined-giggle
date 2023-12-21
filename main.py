63c8d6a9ba5ea89db151fcaf4d0e82332bfa15c7
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:17:37 +0000

    everybody loan drink listen offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
