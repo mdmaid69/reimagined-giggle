ced5746440e70fb4c3ce293daf1bab73ed1d1833
mdmaid69 <robot@alexsyw.me>
2023-12-21 14:50:31 +0000

    house lost development runover freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
