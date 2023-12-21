a88586d8ae526ca75508aa02dc38a4d0b5f921bb
mdmaid69 <robot@alexsyw.me>
2023-12-21 10:29:49 +0000

    belief float pin forever fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
