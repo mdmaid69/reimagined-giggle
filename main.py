79951c751095bc33fb2e5f6b1b99793106fa1697
mdmaid69 <robot@alexsyw.me>
2023-12-21 18:43:27 +0000

    platform hour reply part opposite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
