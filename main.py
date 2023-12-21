c10b696d0728312c829439fe77a8e2045a6d51b1
mdmaid69 <robot@alexsyw.me>
2023-12-21 09:24:27 +0000

    interview start freeze brush praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
