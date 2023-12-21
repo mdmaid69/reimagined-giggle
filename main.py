1948f8f8b88aa9684c7edc6e81b13bce79e12367
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:23:02 +0000

    like effort pause listen late

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_area_circle(r):
+        return 3.14 * r**2
