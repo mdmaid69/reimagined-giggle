503610bbc3f6b3528f4de6019cbd66e4c689dec5
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:16:39 +0000

    indeed message wide upstairs nature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
