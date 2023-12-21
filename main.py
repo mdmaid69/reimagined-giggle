b10f474c18a3fc0a19ab2a141dfc3a0d5c18a898
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:55:18 +0000

    once probably under how comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
