9141aaa7841986911ce91928d1fca5f2fec7889c
mdmaid69 <robot@alexsyw.me>
2023-12-21 20:11:29 +0000

    only sad shade upstairs scheme

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
