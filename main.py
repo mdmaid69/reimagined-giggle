8d8f0d6a73e5417e3328248789889e7e1cfee083
mdmaid69 <robot@alexsyw.me>
2023-12-21 19:35:32 +0000

    name not breakout reply her

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
