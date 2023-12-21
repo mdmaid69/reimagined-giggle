8e2d163b9f4adf1cdcf6ae150ee34c84a34136ec
mdmaid69 <robot@alexsyw.me>
2023-12-21 01:22:42 +0000

    born provide sharp steep damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
