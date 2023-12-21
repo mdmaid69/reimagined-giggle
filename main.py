75fb5392f82207dd7d8afb40709d41971162666b
mdmaid69 <robot@alexsyw.me>
2023-12-21 07:21:52 +0000

    fix female she Zz promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bytearray(array):
+        return bytearray(array)
