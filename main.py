d26ce9cfe4b27ef87df7323f81a8d009ca393cae
mdmaid69 <robot@alexsyw.me>
2023-12-21 11:55:55 +0000

    about each again thorough tense

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
