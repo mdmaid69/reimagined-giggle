b6953ef9adaa0ba600f5d73490595aa6bb02fdf3
mdmaid69 <robot@alexsyw.me>
2023-12-21 13:37:53 +0000

    false pour wake present muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
