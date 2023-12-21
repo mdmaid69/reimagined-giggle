4c25dac917ee295ae110dc8ecfbb13e9eecc1af5
mdmaid69 <robot@alexsyw.me>
2023-12-21 17:47:10 +0000

    blame knock connect zero range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
