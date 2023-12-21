13a05063073b6d356f12596bbe12510268a42b89
mdmaid69 <robot@alexsyw.me>
2023-12-21 23:05:13 +0000

    peace difference snow search excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
