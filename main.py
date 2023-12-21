89bcd3fc7b0fe754fa5eb105bb5c2ea40658876f
mdmaid69 <robot@alexsyw.me>
2023-12-21 05:07:22 +0000

    pure quick address stop motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
