b3b97cf5f5c822376891852397dd38a9b52a6c79
mdmaid69 <robot@alexsyw.me>
2023-12-21 12:47:46 +0000

    house joke regular centre as

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+print(time.time())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
